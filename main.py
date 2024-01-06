dc219e4020bd3d7f954fca3ebadabf0d3e1c550e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:25:08 +0000

    capital pot meeting trap spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
