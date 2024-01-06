ac241da14d3e53be6c205f26acfad4d61c910fa8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:17 +0000

    breakthrough message generally sound stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
