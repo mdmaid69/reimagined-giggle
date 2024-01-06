d3d1b0390c8871f4ed7d4b6988bc88f6a48c43eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:54 +0000

    finish use lead nice past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sys
-print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
