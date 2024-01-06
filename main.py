bdc7a1d12ed0783d4236dd4f6fb6fe0881e608d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:40:37 +0000

    evening Jj ever respect exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
