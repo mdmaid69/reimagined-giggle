7e00ac98911c8abaa2d925461e38d586587ae012
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:25:04 +0000

    sale opinion convenient birth responsible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def get_array_as_bytearray(array):
+        return bytearray(array)
