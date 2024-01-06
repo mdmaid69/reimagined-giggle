1bbea517948d115b3a53e4ec2171fbe6bdd2b5f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:30 +0000

    claim hit collect burst connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
