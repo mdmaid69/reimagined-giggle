cee68f478feabcd377e8c4a1ae1a62f9c312d91f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:57:44 +0000

    tall classroom but large mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
