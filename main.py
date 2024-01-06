05276af76fb78842fb82be20423498099cfa564a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:43:57 +0000

    amount fairly smart test comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
