203a8624cfc4245fa133e5f28fd1f49bda928c99
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:59:19 +0000

    claim remove plate introduce only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
