8dd8c1c8478a45a473db44abd86e6ed88a4b7617
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:32:33 +0000

    side praise control answer town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_power(work, time):
-        return work / time
+  def get_current_working_directory():
+        return os.getcwd()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
