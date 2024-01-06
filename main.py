dc62eae0125f14226401eba263cdf5bb762970b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:00:59 +0000

    male rich forget love off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
