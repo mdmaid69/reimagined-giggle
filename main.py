d12493ba1b9184ae22c6f3c2af527902556a1a68
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:13 +0000

    rate treat trip worried spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
