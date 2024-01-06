b8aa82d5a899b6a7d8c5c03abac065dd36975a3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:10:21 +0000

    attitude door next rise sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
