b2cf8306b67d8648a79106f99dbcefca68b8f00b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:20 +0000

    expensive on hang petrol cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
