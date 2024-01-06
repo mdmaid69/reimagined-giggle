da0397e598f8db54498ed624a316a69c0defa5a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:44:38 +0000

    excuse bury tough disapprove above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
