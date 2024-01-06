5b38b946b3bebc8c842d7165aebe706a4d268025
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:40:15 +0000

    oil spread hold bury mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
