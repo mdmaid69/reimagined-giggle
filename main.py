db22eaffa4dada6f1dbbffd06a76d12f478a1ee6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:15:29 +0000

    improvement performance nothing keepoff storm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_max(lst):
-        return max(lst)
+import sys
+print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
