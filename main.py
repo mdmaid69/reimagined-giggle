cb903bf9b90af2c1cf89d44d9ea81852a32d802f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:59 +0000

    bottom without yourself embarrassed comfortable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def count_characters(sentence):
-        return len(sentence)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
