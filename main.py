b4e4889b29e378f6a989f831c26dede648028d81
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:07:29 +0000

    suck unusual after earth uniform

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
