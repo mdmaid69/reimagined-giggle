e3f2ce51ce8a0ae818e5dd1442beca08f1d66d93
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:57:49 +0000

    principle across trouble welcome negative

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
