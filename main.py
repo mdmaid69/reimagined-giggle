436f7975d8001f8ac367a6ea6a730e18352d7c04
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:42 +0000

    care style chief impressive drive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
