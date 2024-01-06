a124a5213663b5312b00c170fbc6137ec1b70716
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:54 +0000

    content single never anywayanyhow reduce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+  def delete_file(file_name):
+        os.remove(file_name)
