ac1ff9b8e445187d7f45e862e48d44c1b763fe48
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:44:16 +0000

    bother profit expensive relax friendly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
