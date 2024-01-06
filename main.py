0d26ce57cfb1a0b686508d3efddaaeeb344b7112
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:58:39 +0000

    fact piece murder vote uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_max(lst):
+        return max(lst)
