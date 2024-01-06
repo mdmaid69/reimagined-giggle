afb695cfa5b45a6e990e2ffeeb60db844881d382
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:26:58 +0000

    sort level fashion both blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def append_to_array(array, item):
+        array.append(item)
