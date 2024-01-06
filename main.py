8e751de9f1587294a19587a8872695fb08d27e08
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:37 +0000

    wise backwards low pair risk

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
