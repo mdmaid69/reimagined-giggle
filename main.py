2e4fec826a8f9562055d5f1fe0312ffdaab32137
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:59 +0000

    slip flat runinto bunch match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
