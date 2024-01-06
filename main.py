6bc22868ca05a9ed2de8f4fa9df81a20495bc5f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:43 +0000

    history sleep mean black recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
