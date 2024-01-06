7f4d6edb84da6b356807c1b94f1d3a6754f016b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:07:35 +0000

    bend increase trick doubt improve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
