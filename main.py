46bbee6f86c5f13319752e34cb75b1e3447cacd3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:55:37 +0000

    turnover find copy memory land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
