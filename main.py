ef0c077086334dd7c46c616f846f76fbe19050d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:43 +0000

    wide around chat music brush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
