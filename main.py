2270892d1c008777e81f3671563bbddc31715e0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:19 +0000

    experiment central accident trip owe

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def get_list_from_array(array):
+        return array.tolist()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
