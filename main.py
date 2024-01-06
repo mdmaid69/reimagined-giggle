94cc0eb6ef7eb7db49607febbe64cf45e6d9cfa7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:50 +0000

    offer open silent machine glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
