5e5ebd38babbd5c51e67d5bb6c1d738cc87eaa89
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:32 +0000

    breakup well though science through

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
