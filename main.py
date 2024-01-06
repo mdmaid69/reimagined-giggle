b2fdf4638617acb51844fed10e5df15a031fea48
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:29 +0000

    year trip Yy nature read

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def square_number(x):
+        return x**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
