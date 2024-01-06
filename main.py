dc6d26dd383372e4ab1e9d3044c2830bfcc8c8d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:33 +0000

    punishment poison keepoff claim may

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import datetime
+print(datetime.datetime.now())
