aa4db0824b81b36fb9c5749a8568dbc400733c82
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:16 +0000

    expression runover rich how subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def check_if_array_contains_item(array, item):
+        return item in array
