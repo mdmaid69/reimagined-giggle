96ee46e2ec277ff90f9bb150bde700a72384ceb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:47:49 +0000

    file successful rub cause though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
