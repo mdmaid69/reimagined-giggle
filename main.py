bcee958ea689c4d0c04e7e3d82190fbbb70db5c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:29 +0000

    face act squash call spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
