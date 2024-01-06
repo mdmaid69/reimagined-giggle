00e98c63888fc452c134172fb9cf023bdfbf7ee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:24:57 +0000

    important finish try shape finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
