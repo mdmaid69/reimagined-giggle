65e0d5d7853fd42b11234bc248e397adcbb0f322
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:58:46 +0000

    meeting whichever song fly wave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+import time
+print(time.time())
