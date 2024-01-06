6392f40f32d127cefb77170385f0fc2e0996c4d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:39 +0000

    copy correction advice provide dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_priority_queue():
+        return collections.deque()
