936a26e229a1ca63358c9d8823c574e5e31d69c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:27 +0000

    thinkover benefit up need card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_string():
+        return collections.UserString()
