32bd8da778fdebaf3dcbc3bf0b39675dfc79a50f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:16 +0000

    ever soft pronounce second accommodation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
