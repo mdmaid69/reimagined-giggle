86af92fd3b253f8cf872f305ec2fd2207a22a0c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:18:06 +0000

    similar contribute lookafter connect urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
