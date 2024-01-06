5b402c2049f707f8e5d81aaacf44ea1efb6bea57
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:20:00 +0000

    here can maybe breakup Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
