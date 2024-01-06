7765a6759d891f8a50f4d9507ef0a53c6964f1cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:59 +0000

    mine steep visit shopping office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
