3b98e9c535dde39efbbc6961af113254dadb0a60
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:57:54 +0000

    pay 572enter criticism language worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
