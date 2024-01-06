e48c3ab657e16df07c7a0237281375f79609e69b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:22 +0000

    soil manner mine length 566end

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
