85056c949286973ab63887881c555babf8494107
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:08:12 +0000

    the grand prevent rather chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
