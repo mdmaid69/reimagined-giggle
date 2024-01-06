7c51146fd0260679874e7d3a58d1e6ea3226621c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:46:01 +0000

    neck fence cruel hurry scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
