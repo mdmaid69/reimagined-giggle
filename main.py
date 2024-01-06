37c45f282539333c9f185a2b4c73af44bfa0015f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:48:30 +0000

    brown comeback nearly practise wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
