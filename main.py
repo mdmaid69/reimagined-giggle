9bb8327f30891f1f0be5066bd031bdcdd6b09daf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:11 +0000

    collect begin nor discussion unless

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_max(lst):
+        return max(lst)
