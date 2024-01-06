acaea308ca5488067bd45d7d69fc124483a7fd44
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:13:52 +0000

    count yet test welcome hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import re
-print(re.match("h.*o", "hello world"))
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
