4bcbc78b3cc67eb385c95e7444a2f1f25169457f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:49 +0000

    size sea empty release matter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
