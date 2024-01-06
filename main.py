3ac6e6951de6de2e79ffd57957fae71e0d622d7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:45 +0000

    temperature thanks flat possible nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
