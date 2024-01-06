945038fed27c5a3fce496bf3284286588a5312ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:56 +0000

    recover fit slice lead until

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
