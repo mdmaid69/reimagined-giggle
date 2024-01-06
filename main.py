d490dcee52e9bbb269c1038ed62c65272b402723
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:26 +0000

    save best interview report kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_area_rectangle(l, w):
+        return l * w
