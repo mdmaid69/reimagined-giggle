3298effd3da43a98c3ae3ba69ad0f5427c62e746
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:06 +0000

    square ring store deal load

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_power(work, time):
+        return work / time
