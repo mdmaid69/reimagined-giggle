3f9b9eb7eecc6c3c74d3a693c14ffa6d8ef25c7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:28 +0000

    fast space warning escape well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def is_odd(n):
+        return n % 2 != 0
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
