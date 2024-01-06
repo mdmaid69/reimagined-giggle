2354fb11c78f9569b5b2cf479b60e5f966a14550
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:06:20 +0000

    shop possible whole simple drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
