cfc3edd067f55fd7d0df516d2ce85f1686e924ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:02:13 +0000

    mistake violence sit shake speak

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
