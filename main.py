5a0c7291efdb0673af8e16b6b8f69d270ed87de1
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:46 +0000

    method maximum address learn bit

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
