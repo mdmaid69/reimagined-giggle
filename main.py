78f8c34e6eaf04c02af0d93bdb9cbaa3c7f5e87a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:02:20 +0000

    glad party wish twist 565end

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
