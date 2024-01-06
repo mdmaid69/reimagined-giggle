327e5370dcf277c1d85eaf09f80497954f198e35
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:49:47 +0000

    test hand objection straight loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
