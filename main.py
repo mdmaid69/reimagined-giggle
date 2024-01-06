15d215680e4e9deee0d8b35a4a2d0c61f28ced09
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:30:45 +0000

    confidence purpose splash objection speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
