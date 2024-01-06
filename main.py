c716379e890040320550bdea6e8dbe0661b795c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:40 +0000

    variety expression twist sink custom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(lst):
+        return max(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
