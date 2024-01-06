4ef4e8ef3cc18104ec31a277fd9e9b3a6896b70f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:31:17 +0000

    marry point judge pretty test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_density(mass, volume):
+        return mass / volume
+import time
+print(time.time())
