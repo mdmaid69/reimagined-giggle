58a1bb379f9d0ee6d5c4241d1aa42126c7db919f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:09 +0000

    Kk anybody lead possession dangerous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
