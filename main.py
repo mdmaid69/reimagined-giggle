a05127eb6b5e861416d51303e7688a7e52762199
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:50 +0000

    breathe fence morning nice although

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def count_elements(lst):
+        return len(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
