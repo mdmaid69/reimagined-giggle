69b9ad241cdaddd806bb15e22db6815f292ea1fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:38:34 +0000

    raise below activity left plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
