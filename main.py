45c51ce3afb1588e86d1e3e1d593992a06d781b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:08:39 +0000

    prefer hospital deep light move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
