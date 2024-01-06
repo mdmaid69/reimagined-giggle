7e7cb8173652c78b706afb22a68b277da83fb689
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:37:20 +0000

    shallow characteristic shall between busy

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
