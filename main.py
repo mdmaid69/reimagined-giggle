362181480f0701f15ea5404cd1b4706ab0fd4276
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:52:01 +0000

    public workupto side passenger affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
