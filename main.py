b63433c528844c7c30aa0330c7c4f3686d331a23
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:50 +0000

    crime afraid city moral deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
