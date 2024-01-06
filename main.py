4d63f505710050be4fa231289a3bf2cf6a4fbb2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:56:31 +0000

    line picture smile prove something

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
