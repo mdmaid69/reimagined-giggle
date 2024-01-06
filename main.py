48e0d94184d4e732047b8a0ef9aa4b9ca11ff8db
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:15:53 +0000

    sometimes interview stop basic exist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
