feacb2327ff1f647489d34aa992864413a76c0eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:49 +0000

    suck convince boring over surprise

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+print([x**2 for x in range(10)])
