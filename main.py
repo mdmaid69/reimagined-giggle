3877a59c7cf0ace6ee41c53a11a11912d362e294
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:12:51 +0000

    lose sort meeting about number

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_density(mass, volume):
+        return mass / volume
+import datetime
+print(datetime.datetime.now())
