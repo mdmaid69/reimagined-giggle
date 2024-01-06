714d17c64cf7f73598d88d6643a8d08fb318db7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:42 +0000

    contents rush but full turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
