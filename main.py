fe5c4196196936a3bfa3e59eb795b81722fccf5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:35:39 +0000

    one mine gift story create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def convert_to_binary(n):
-        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
