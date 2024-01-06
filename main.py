ac9386ec2667708da09360b5eb2a347b97a85b37
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:58:56 +0000

    young across underneath used land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
