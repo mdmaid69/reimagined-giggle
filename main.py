f9a9b1b493b18f55b63ed28d5d7b203a2df4c044
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:56 +0000

    shine condition formal could call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
