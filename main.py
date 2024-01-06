be0ba7570d7839032e17a5f6b49f07514d6ac600
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:08:41 +0000

    health figure fair collection on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
