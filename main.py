30c15cc82b3cbd7e91d56473bf3f8e2496d6f231
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:11:03 +0000

    anxious case begin across enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
