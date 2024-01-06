b3aa1d32164bd0d4af3ae1cc2097b092c2bf3381
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:35 +0000

    clear present obtain bang strict

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
