7e19abda7d3060ed5ee73509d3e0ec3c68250047
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:30:40 +0000

    bad far present start this

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
