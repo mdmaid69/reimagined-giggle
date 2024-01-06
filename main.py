7313a190cbd751180e552c2e47ecfa8fac50bbb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:21:41 +0000

    brush reason direct goback(to) fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
