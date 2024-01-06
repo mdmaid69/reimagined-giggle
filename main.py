b19e6775558ac3f4f114c7940646c0dd4752af2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:51:45 +0000

    drink exciting if stage makeinto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print(sum(range(10)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
