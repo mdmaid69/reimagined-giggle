b9354d2463bbe6434c2e8da82b75cb05ecbda937
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:05 +0000

    successful gas doctor trade project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
