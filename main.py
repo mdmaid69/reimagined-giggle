f8ed0f8cdd0480eacd4e5566a9883c4fc31d9c78
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:43 +0000

    unlucky complaint spot correct pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def square_number(x):
+        return x**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
