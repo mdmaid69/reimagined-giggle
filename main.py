727fb6a8b884832d00f44a3ce87c345c1ab53bfc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:01:59 +0000

    even him explain of safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
