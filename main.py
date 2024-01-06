e2b2389d88800a8afb98ddef1d85e7cd9f27c212
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:07:11 +0000

    share now after they how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
