c476ecb5b660e2f8a54033f72480be26c7dff57c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:25:56 +0000

    content bother conclusion debt model

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
