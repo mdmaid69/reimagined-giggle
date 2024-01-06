b9f39c4f09f69248c5bf58b54f6f0af251b8743b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:05 +0000

    slip wear tonight either else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
