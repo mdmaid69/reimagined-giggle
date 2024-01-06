cfa629074128f6abd92498c92fe860a9eb7cc9a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:56:52 +0000

    bag stick foot distant appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
