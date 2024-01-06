0ba5685e718694df226251415289d2fca859ce5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:28:52 +0000

    dance grow callat moon plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_length(array):
-        return len(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
