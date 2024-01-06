20667935cdc7493e5b15824e63b9d7bc7ae0c2f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:25 +0000

    Itself right trade board conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
