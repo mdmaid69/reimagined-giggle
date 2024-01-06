ceb40424a6d17ddaaa25be4a180a4b7f88a4566c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:14 +0000

    good depend neck health think

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def multiply_numbers(x, y):
-        return x * y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import platform
+def get_os_info():
+        return platform.uname()
