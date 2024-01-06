fd3fe13cea2599c4d4a6b9b617e98a9ca92f2d9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:31 +0000

    getaway operation steep harm seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
