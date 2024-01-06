dc8e59f2c6e36a10d94146f3d6d6423a9d3775ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:19:33 +0000

    read use care sentence weather

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-for i in range(10): print(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
