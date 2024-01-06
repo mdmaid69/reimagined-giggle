b5d057735c99bd158bd2f068ef690d3d77b388df
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:57 +0000

    communication disturb sound 571enough properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
