566c81a89767f9535694d2b379031dd3d13da32d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:21:25 +0000

    some hate approve speech contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
