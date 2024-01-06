a629417ee8c07d9d5c8279186cc2948b0003ae80
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:16:36 +0000

    path confuse punish post hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
