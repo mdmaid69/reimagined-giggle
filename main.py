d44dd30e3299f106975467163a72e3ffe56d2c5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:24:50 +0000

    except ought spare negative OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
+  def convert_to_binary(n):
+        return bin(n)
