444df2b76bea3b4fcab44422ab84d535cadc9c94
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:31 +0000

    dress shine standard sing standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
