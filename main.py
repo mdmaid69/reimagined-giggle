f5647e6f004cd235125fefca1b245394992ab91e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:48 +0000

    through against calloff brush aware

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
