a6b3c46e1c986ae4376ec803801d93f789b8759d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:39 +0000

    refuse nothing attractive compete organize

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import os
+print(os.getcwd())
