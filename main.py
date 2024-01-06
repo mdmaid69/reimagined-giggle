91c5f5624db3acc7ba64a1317609ff339c667567
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:39 +0000

    will progress pressure breakout border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
