e68043ff8564449d51c41ae81aac834ff23cd185
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:38:04 +0000

    platform ahead forgive bunch exhibition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
