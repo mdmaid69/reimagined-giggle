53e0632e47b8f6a60fcf981514ea803315ec0038
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:40:56 +0000

    excuse turnaway hurry change before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
