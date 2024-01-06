d3ae7a728708fbecc4adac3ef655265bd9a031e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:38 +0000

    learn test recovery interested obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
