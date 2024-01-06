d0c0aad738fe62304a15d6a08f307e3b095ea63e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:43 +0000

    report unfair sensitive over 179bite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import json
+def convert_to_json(data):
+        return json.dumps(data)
