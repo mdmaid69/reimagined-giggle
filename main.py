fb7799ae67ec3fa33a247d1355f018b0c8069d7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:08 +0000

    appeal block emotion month branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_power(work, time):
+        return work / time
