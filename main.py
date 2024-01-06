c18697ae44692559a4deca207e9ad25445477409
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:58:26 +0000

    claim laugh practice be phone

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def append_to_array(array, item):
+        array.append(item)
