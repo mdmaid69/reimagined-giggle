23b5a3262071b1a9b4bcf1f29e7a4bccad56bc51
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:53:29 +0000

    disgusting truth available talkoutof development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
