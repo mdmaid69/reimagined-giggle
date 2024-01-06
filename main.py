6392ba18e0d48d8fc42655e1751bf70e7bbeaa72
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:46 +0000

    shoot copy clear pause pause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
