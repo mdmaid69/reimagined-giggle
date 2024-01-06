d48ac10ef62b5e226d6df0ddcc14bfe4c900938b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:10:04 +0000

    mother height its juice necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
