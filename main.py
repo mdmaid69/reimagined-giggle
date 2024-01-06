ec0b30ac5d55da2e855299817e57d23526b20695
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:06 +0000

    introduction interruption include whistle again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
