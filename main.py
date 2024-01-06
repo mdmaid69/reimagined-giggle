d53821650286e5e7ec10f71114a3f943dbf19981
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:53:49 +0000

    lookaround run force occur take

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_item(array, i):
+        return array[i]
