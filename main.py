999fbf7d7d055b5cb8b8f52c6e8f2b4db51a2a00
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:47:46 +0000

    angry goback(to) size worth quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
