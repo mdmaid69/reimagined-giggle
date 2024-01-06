a8d706bdf2b5384335e691a4c5b0ba62b9fd462e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:37 +0000

    attract rate them regard arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_unicode(array):
+        return array.tounicode()
