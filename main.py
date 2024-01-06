e360121b172f3fd8780ef052d7ef380f1675ed82
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:53:52 +0000

    all heavy traffic tax climb

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
