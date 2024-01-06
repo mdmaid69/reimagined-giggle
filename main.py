94ad1b195a29c08c5759d15593f9d8fdaf8ee9b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:37 +0000

    unable comeoff party within butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+print(math.pi)
