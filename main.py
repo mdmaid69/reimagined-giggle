1a6218eb6cfd9e687eed279879d28acdcae3eb19
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:58 +0000

    Uu kind limit realize regard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
