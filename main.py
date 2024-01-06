6660a1d6df4466a47b45dd8e6257de1e9dafea36
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:14 +0000

    table confuse poor fat but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def multiply_numbers(x, y):
+        return x * y
+  def convert_to_octal(n):
+        return oct(n)
