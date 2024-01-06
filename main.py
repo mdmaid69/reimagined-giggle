ff28894044819b92dd3c3b77b2e0983da34cd693
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:09 +0000

    not tap each sound comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
