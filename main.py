41db0e01d6e821e937d0cceb00316456141da581
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:18 +0000

    appeal confusing wipe service before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
