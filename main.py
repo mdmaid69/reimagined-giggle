af403aca6e3c0f9ca162940155ed993af203e86b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:50 +0000

    567enemy compete address disappointed lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
