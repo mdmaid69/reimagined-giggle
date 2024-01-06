5d3dbd6fca94a68b305596b0d1f623dcbb39c31c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:20 +0000

    happy agreement bringback Ww invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
