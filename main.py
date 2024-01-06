87e81b4c71f8b36dc191de01d55ccf872777c127
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:53:11 +0000

    little noone smoke backwards quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
