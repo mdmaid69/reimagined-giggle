8e41e862e612c392f6fdedc2648afde14e06f04b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:49:38 +0000

    else they lookafter produce getout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_max(numbers):
+        return max(numbers)
