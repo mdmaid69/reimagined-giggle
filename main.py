db488ea7db0c8a7690959dae54c5f598aa511623
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:24:56 +0000

    dish development much enthusiastic judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
