48edcc24befb5ad417a523a447e1ea9450156ca3
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:39 +0000

    science content experience anywhere thorough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
