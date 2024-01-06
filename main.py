c8c83b4af3dacbb93602980483cbfd60334fe1f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:57:01 +0000

    frequent pity juice sad about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
