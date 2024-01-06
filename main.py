54a864183d342bdf2dcd29d2afe5029f1321cbfa
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:37 +0000

    birthday label season in picture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
