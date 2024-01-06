a8d0dafed90072e53dc200a0104b89072250dceb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:11 +0000

    organize help forward ear rescue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
