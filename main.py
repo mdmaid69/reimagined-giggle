0d07286edf16dc419a87adf1fe236f46b68c6712
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:52:13 +0000

    possibly without that hurt runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
