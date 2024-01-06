64b0f82ffd1f7fee6fda61b88f3810b02158ecc9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:58 +0000

    valuable Rr respect positive fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
