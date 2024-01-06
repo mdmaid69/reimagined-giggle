b3ea2ad0da87613021a817ebdc3d79e448d20f6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:23:06 +0000

    into tidy ill recently pen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
