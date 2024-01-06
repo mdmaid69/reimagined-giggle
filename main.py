a9e407494ed63bb10dc2aba0d32eab1b49d42205
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:13:28 +0000

    trip certainly abroad period hang

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
