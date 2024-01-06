049afc6d92dd9602c7826b501a70d212a38a9fbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:32:20 +0000

    satisfied attach slip foot star

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
