2cf3741c51e59aa68ff98e3cc038be2472a902ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:50 +0000

    introduction judge table matter beside

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
