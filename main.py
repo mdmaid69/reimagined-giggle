acd8d62d7a1cff1110402fd3e8e3d53c0deb0d50
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:33:42 +0000

    tidy recent difference load unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
