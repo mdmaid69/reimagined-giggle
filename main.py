d0028f56b905fc9054163f871f16511ea438288e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:10:28 +0000

    powerful fence grateful fair able

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
