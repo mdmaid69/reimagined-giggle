f4823f69a0e96cfe4b075438105f5520a238fe3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:00 +0000

    deal confidence odd sort definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
