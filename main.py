10e8d8b92215756576b8fce0ee1ba374415123a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:32:52 +0000

    lesson scrape find film develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
