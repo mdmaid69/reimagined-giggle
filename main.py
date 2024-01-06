3cbba4121c88978e109f011eeb76cfc80955e8de
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:16 +0000

    game sea cold wonder black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
