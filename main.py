d4a5d9c6c4c6ce506cb4d177574dca3a8b824ee4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:10:59 +0000

    goout react drop handle room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
