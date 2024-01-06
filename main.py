50dce46a6e7662acaac33a081f8dea8557ef6719
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:35:33 +0000

    tread watch past meal raise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
