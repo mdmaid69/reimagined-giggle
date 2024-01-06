d342dcc3af83b0f9666517808474b0a2d0aa8806
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:12 +0000

    everywhere fork shadow shrink division

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
