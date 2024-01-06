9c3e00a2d3ba985131e8eae90346451a713e1d79
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:36 +0000

    apparently port crazy gradually take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
