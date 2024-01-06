46ad3c90ff66a59f965f196163c5a766bc4a5eda
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:08 +0000

    leave immediately patience available rush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
