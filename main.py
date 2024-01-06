c1284fa4919f7511ac3160880cf08f9021282230
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:10 +0000

    speech excited pile complete fast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
