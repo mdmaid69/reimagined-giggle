03b0cc9e54e803af5ab62a7f8e7d86bc6dfe02fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:46 +0000

    tip shelter squash church body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
