8bef2508487c83de63c3b56ac40ea839e8217514
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:30 +0000

    dinner expression moon bye stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
