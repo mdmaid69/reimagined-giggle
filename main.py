f2dde1ab5a802ea374eb6adb40808950b75821b1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:17 +0000

    please outside split cheek theirs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
