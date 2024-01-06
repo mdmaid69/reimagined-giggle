7b0a6217ffe5ae997637b4544406ca2bc3c5ead7
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:47 +0000

    use no there rise modern

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
