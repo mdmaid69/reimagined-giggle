6b3bdabf468dd85acbde336b83c9c1151e3c6ff9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:11 +0000

    unhappy as slide blame search

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
