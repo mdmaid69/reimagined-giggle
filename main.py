886d50214b994d1aeb5587b0235333399bea4da0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:49:59 +0000

    up love standard relative flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def cube_number(x):
+        return x**3
