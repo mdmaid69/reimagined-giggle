5ae43b6c7b13a816b5151ede2069a5a21a5eee09
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:14 +0000

    bringforward connection high wish right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def square_number(x):
-        return x**2
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
