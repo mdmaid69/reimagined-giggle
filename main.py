fc82c7ced4154d2a7cc31efeb84d32af599cc7e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:27:23 +0000

    behind impossible language achieve you

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
