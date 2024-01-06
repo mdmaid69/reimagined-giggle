58f52ff2a691790453753f4c5700d7755e6c8d80
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:05 +0000

    worth low opposite single full

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
