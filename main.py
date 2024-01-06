14e624734a123bb7b959df04694c529595dc9eb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:21 +0000

    I minimum unlucky new favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
