2d50fe5b1a914a4da2d6597b8397133f2b9e65d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:04 +0000

    interview comment anxious mess spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
