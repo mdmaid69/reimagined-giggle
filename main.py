a2bff744ab89c900306781b8e9af47102bf0fea0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:19 +0000

    scared wave content whole accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
