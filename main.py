37a2c283eec1edc2fcbfc5d8b092428d761cbb27
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:42 +0000

    yesterday carry tragedy announce secret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
