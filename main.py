78effcd4d4a8f5142ea6d317de3c3cf2eb1e8b7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:21:20 +0000

    spot along degree people dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
