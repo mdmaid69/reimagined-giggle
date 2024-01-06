a06e91c2ebea20c09aab1c66e3535321e305884f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:45 +0000

    issue compare cum broad opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
