09848702a8dfa1525096d2f44e82e818489dc32a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:55 +0000

    not turnup pile Uu achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sys
-print(sys.version)
+  def get_base_name(path):
+        return os.path.basename(path)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
