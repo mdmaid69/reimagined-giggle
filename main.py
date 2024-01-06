dbf9dcaf5bd339a33cb0aadda00750be14580e35
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:01 +0000

    telephone perhaps duty holdup up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
