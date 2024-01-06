326798bff6dac6727bcffeddeae83dbe2b0b413f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:45:37 +0000

    turn advise dig room concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
