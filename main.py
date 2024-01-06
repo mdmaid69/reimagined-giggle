38db9a521c66c9dc94be80dbd4480e000e028fc9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:17 +0000

    aim have group politics argument

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
