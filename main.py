57e8c5356f2dcc319c161240f174777f1ecd57e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:41 +0000

    sticky port doctor divide emphasize

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
