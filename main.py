37092f9c1238180a1bd33796ef99b5a32f1041fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:50:31 +0000

    although practical outside look politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
