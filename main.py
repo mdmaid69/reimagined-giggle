80c25d73fa0642581ce9361ffd2404aff6318a42
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:21 +0000

    single consist which Ww workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
