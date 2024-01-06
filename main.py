38824d189089b1df0ec3b51aa3a3c9046ce5d763
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:16:16 +0000

    show wave weigh exciting morning

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+print("Hello, world!")
