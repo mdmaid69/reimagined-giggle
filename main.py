ab2673223d04f5aa5a7a2d5e43fda3707596361e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:41:57 +0000

    square pull comeoff talkoutof perform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+name = "Python"
+print("Hello,", name)
