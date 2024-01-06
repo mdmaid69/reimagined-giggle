e91690b0be402519d277a1fcc5884910b4c6af80
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:52:23 +0000

    duty least wide meeting ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def clear_array(array):
-        array *= 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
