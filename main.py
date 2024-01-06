9e72666563b3d5f4bb6d85aa2a7e57244f9d3dfb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:40 +0000

    city judge cool government under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
