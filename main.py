aff1ac1a06ad45a4ab9c029d0bcc16eef7eaabb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:20 +0000

    attempt cheap anywayanyhow pause street

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
