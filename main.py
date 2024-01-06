4b2dcd986fdba640650fdcc03c42ba51afbfb532
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:27 +0000

    achievement swallow his circle challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
