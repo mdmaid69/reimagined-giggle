74345491cee93af7720157c0dbece9147b9de1d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:32 +0000

    cost improve reaction letter characteristic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
