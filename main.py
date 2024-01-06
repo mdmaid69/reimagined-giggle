3eba6d1b2209bf72bb0f3a393bfbd173a91661dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:25 +0000

    cause drive pump size further

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
