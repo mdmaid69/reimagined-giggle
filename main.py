72339de7e0a773d5e8064d1e266e343faa1067d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:04:47 +0000

    grand exit will pot guess

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def add_numbers(x, y):
+def add_numbers(x, y):
         return x + y
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
