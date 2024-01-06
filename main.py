11de77a689780b7e43f065820ac60b85baf61ca2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:28 +0000

    rest comeround near famous dress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
