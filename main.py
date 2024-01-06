a5284b51972bafbd7c961658661d2e8d9871c656
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:51:54 +0000

    pin fat law prove sound

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
