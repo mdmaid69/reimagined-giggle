ffc8271914e5f952f2d0d25095fe2704116ccba7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:30 +0000

    between friend sea force dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
