adbc3f6a63b71ec153408fa5aaa45e0fcff5118d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:40 +0000

    report protest in you advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
