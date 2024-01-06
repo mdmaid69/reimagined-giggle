fe28018d0b6f3e875d5ffdb1e4b9d4c50af88870
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:09 +0000

    action criticism clear breakout strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
