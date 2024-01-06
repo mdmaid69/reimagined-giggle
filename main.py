4ecd8a14b42a45280f4a98830ab739a40cff5cbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:59:08 +0000

    hard live wherever shall excellent

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
