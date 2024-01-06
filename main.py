b278767793dff531cdc4b258f4293c4db5e5bfcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:39:40 +0000

    snow appear though least dare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
