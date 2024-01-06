77c03ce0e74dbb5b2a3fe1b43a8f5cdb8b301834
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:40 +0000

    grand top nigga smooth traffic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
