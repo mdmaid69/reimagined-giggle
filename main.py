6f53bfd3558273606e92bb4b282edb103a868446
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:05:07 +0000

    bye fight accidentally soft expensive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def add_numbers(x, y):
+        return x + y
