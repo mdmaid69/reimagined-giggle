12741540dcfe29ca48d1563e878939cb670446ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:55:09 +0000

    pleasant bound Hh opposite surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
