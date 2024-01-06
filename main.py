2a21c834ea6c93631afe7adf605d21af42d1fb5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:48 +0000

    fence stranger yesterday tired marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
