892ca3fb57f999037dac0bcd74e154e13c57b71e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:33:42 +0000

    peace perfect insist turnaway taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_base_name(path):
+        return os.path.basename(path)
