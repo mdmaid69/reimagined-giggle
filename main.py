c7d804f92e252899a5ec1057deaab8c52ba97c40
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:01:23 +0000

    declare happy song near sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
