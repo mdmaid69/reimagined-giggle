ecdfe8f5bdbd77745f9234b70ead47868cdeec05
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:52 +0000

    might develop die pleasure radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
