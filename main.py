2e22b5d1e392891f9c59cc8edc2f395839f37b37
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:36:44 +0000

    today separate dry hard effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+print("Words:", len(text.split()))
