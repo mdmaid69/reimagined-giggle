d8d270596152bc06d31c568219b61832dd977297
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:02 +0000

    careful swimming discussion fault object

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
