4550ea89c09590816ca38aaa743867b0a3f7e87b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:31:30 +0000

    defeat solve send business laugh

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
