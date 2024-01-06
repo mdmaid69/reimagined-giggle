5ba28a0e8711c9059b207d7e88bc4bde8edfea2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:33 +0000

    pay middle spend occasional turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(a, b):
-        return a + b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
