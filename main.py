8dc6e6bf0acafc7737d16babf9a3d96c48cdb952
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:58:08 +0000

    software central react offend less

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
