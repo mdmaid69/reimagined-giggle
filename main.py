ded9d01559ee38b9b5a793c198e1bd09736c59f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:26:10 +0000

    benefit anxious surprised no issue

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
 import random
-def roll_die():
-        return random.randint(1, 6)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
