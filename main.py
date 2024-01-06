410116c98c8958515c47b3d127227bed35b9f91c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:23 +0000

    tight get defeat dry up

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
