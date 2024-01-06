7bb38ca6b627ef75ca38b256bd762f3f04d0f0d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:29:43 +0000

    rule Ii breakin capital like

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
