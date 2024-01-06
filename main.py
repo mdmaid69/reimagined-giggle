6718e2f1674c520ccb4f527ec2033c64164c08d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:03 +0000

    trust brief store refuse direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_sine(x):
+        return math.sin(x)
