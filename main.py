9c1f194b8ccbeecb12fceaed556eb31e91a5ffa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:05 +0000

    left arrange cum not car

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
