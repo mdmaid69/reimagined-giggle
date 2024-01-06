bbe4da71d71b33342013b4968359aaf92b13959a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:54:57 +0000

    couple breakfast human reliable bush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
