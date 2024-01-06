7b0a91b53b04d3f8a86edf2fca0c36c97f6bf02e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:24:41 +0000

    into attempt wake voice silent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def count_words(sentence):
+        return len(sentence.split())
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
