aaa9ee38ce0f4e52821aaae01001cc2023f5db2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:25 +0000

    band raise runover tired sharp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
