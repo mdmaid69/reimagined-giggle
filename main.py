737322030cda4a2808b7d94f4e1fc6e2a19563a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:03:08 +0000

    morning message complete aware effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
