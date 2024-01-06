325c44848437123af3a5ef2e43ced506fd25f2a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:03 +0000

    disagree do constantly down giveback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
