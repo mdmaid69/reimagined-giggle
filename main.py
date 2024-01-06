66dee65317058854bc4c0d3ecf1cc76dde7541de
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:49 +0000

    exciting turn brief development actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
