e5f83d4eb2ea489df7ace5359d784e8b1be57f32
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:55 +0000

    pretty soak protest hot certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
