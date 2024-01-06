42af0d3793deba74698206b1ec7afce6f01ef558
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:17:08 +0000

    intend certain possibility play father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
