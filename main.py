d1aa7e07bff527c008a47b2556a11be1522f3ece
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:42:16 +0000

    land respect still avoid reduction

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
