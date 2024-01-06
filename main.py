b96283462743a2e77423c9bf5b4065191463314d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:02 +0000

    explain pay holiday jump there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
