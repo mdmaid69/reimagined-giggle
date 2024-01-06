86ad11984f1187d6d7f3b078c54b6afb60a27881
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:47:40 +0000

    blue brief describe character holdup

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
