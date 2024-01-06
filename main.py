8ae62fec91abfdb29c1e723074c291e29ef56767
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:06:10 +0000

    claim interest will squash become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
