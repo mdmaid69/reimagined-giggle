870d0c603d1547bfa961b2f1fe5ec45e62ace200
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:14:08 +0000

    advantage free frightened Jj eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
