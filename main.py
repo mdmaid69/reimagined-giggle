02cd9e8973a4e19d7427687ff07dab8fa8be62b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:01 +0000

    clean glad out after call

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
