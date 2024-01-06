790ee65ebe610dc74eb963fc0d11c5a38c695197
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:04 +0000

    certain beginning separate hollow put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
