1bcb3c9bfb1d828e73a9d9a915a24ed546316f7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:29:52 +0000

    argument speech on often over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_pressure(force, area):
-        return force / area
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
