b81f6015cba8344345f4028e3e5e1cd9e088ed9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:02:47 +0000

    beyond sky rubbish profit fierce

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
