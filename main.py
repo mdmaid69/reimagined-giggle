c8db4c25e2c9ab8ab3e4b919aade951bf544b908
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:00 +0000

    meet pollution vanish listen anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
