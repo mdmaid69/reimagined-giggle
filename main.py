79cc5134d90a4c7e6bbfc7662e9171c9d02506f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:36 +0000

    listen character runover anyone education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
