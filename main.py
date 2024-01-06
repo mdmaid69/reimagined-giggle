fbf10a6320305e07c0184b7cbb97abdb8928c1bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:42 +0000

    permission thorough terrible behaviour opponent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
