d8f4ac236ff8c87b593b4ffbad5e227ad5e74ab5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:12 +0000

    comment frightened ordinary flat century

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def convert_to_hex(n):
-        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
