67b252af0587186a954d68105dade26b405f7bf8
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:36 +0000

    rule around person sudden thumb

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
