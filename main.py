06358c2683bcf0ec78580c130fee8dca6c8dc5cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:28 +0000

    hungry accidental for set around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
