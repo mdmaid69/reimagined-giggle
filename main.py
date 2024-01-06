7106ef017487bc35e90eb9b5350e93df389bc21f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:46 +0000

    lie somebody survive Nn give

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
