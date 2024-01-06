eee1e190d2df2699cff28a3fbbc5474b470c3258
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:47 +0000

    race thorough corner rough bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
