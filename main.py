48bbd0d68e2ea42765957e2a662e060343d1b038
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:12:29 +0000

    importance draw lot female allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def greet(name):
+        print(f"Hello, {name}!")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
