7795a8e6e3b9d79660d684995b84a7bb42695ecc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:18 +0000

    colour notice joke against purpose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
