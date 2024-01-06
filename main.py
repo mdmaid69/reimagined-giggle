6fababbf941d7e0c1ec30308bf3d4d6c225bf619
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:27 +0000

    minute kind comedown shame practical

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import collections
+def create_user_string():
+        return collections.UserString()
