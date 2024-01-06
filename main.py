13bbf750f26d221871edebc729921ab9d6689dba
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:34:29 +0000

    even face flood walk shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
