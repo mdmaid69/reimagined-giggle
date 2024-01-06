28689f30fd9363f1e04d1a2f74bfa2436a531687
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:52 +0000

    crowded phone acceptable track uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import os
+print(os.getcwd())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
