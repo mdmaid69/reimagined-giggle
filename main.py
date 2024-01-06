a8786803e6b3db22b6445e0a386eee404d18dee6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:09 +0000

    nice during book money hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
