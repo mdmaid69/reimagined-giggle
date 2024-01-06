8130af4d4e96bf7ac927ea4963574ae243cd5d45
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:52 +0000

    equal drawing share board through

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import time
-def get_current_time():
-        return time.time()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
