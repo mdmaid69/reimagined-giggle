864ff88f96d49eb8c26cdce3086e36bf024084e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:44 +0000

    call better back maximum cool

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+print([x**2 for x in range(10)])
