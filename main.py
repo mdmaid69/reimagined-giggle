207d190fb7ea43d5c3fe515531223a3236b81332
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:26 +0000

    hair me change method far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
