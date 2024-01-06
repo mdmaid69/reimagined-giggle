abcd1d3a62ddcd88a697a8c7834f9f8cc6b75fb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:30 +0000

    nature town lot pump base

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
