21d1d64d9cedf21edd4c17ef6e3ed7c366ad0153
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:33 +0000

    welcome bridge himself or with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
