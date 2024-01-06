ff62d14db3ddb923620b787adc4fb0403d0b0438
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:28 +0000

    offer family problem official eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_square_root(x):
+        return math.sqrt(x)
