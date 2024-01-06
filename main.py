80b7878e3e6ab2e30fe0ce0fdcbcf0a85c9a284f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:57 +0000

    especially term along year stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
