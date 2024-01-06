e7f34debe065c62b43a9a81147bc635e210b018c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:22:09 +0000

    around winter juice hang tune

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+print(math.pi)
