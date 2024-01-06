c6c3867040062260226b00aab7c6c2971c93b1f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:40 +0000

    second business agree step take

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def square_number(x):
+        return x**2
+def calculate_speed(distance, time):
+        return distance / time
