7481f7c816119c2f266980678250ea0c12946d10
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:15 +0000

    outside aware treat pupil strange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import datetime
+def get_today_date():
+        return datetime.date.today()
