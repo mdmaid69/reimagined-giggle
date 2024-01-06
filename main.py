4cded79743673213a5d7eeec310fa36ebcd69f4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:31 +0000

    although dear peaceful less practise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def count_elements(lst):
+        return len(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
