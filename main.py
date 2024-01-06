452be717b2e74be5facc082c5ad04059fbb39934
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:45:27 +0000

    point problem shock talkinto usual

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
