b8d3ec6b8bf8472ab2530a6559c3e1db9918d394
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:39:45 +0000

    draw sure track black bend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
