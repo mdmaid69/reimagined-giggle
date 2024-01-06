556f82c84280d360ec050e5cde4d9569f13c9428
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:31 +0000

    record wild patience prefer event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
