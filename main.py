0de6de1fa90941186bc7d1a97783cd2d6af346eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:53 +0000

    description takeback development amount value

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
