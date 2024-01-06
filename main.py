dfc624668f9fd04d8e74c1f24d7eec2f582694d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:10:03 +0000

    but organization hurry then summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
