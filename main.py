06e7e5064692481fd36088c06f3986aee3ff745a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:14 +0000

    laugh never vision already way

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
 import math
 def calculate_circle_circumference(radius):
         return 2 * math.pi * radius
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
