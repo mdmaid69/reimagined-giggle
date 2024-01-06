5317fe2e765cf53adf97436a1f7116a7b21ec672
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:26 +0000

    money branch line pretty form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
