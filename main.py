fef5ba3553e79911cabed3c860fc6fc6070892e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:39 +0000

    more mix Pp balance present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
