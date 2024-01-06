de4793241cf54934ca575a394adcfb278b57a678
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:34:44 +0000

    owe act light protest rough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
