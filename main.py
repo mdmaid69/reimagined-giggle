4a186313e15ba2ddc421ea1716675001a2d564a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:53:03 +0000

    corner turnon forward in male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
