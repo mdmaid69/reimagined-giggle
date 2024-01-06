774f788f7f97e9d07cc1b03afafd1cf3711ae8f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:56 +0000

    conclusion bother crowd abroad occasional

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-def subtract_numbers(x, y):
-        return x - y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+print(os.getcwd())
