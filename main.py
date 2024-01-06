252f77ebe86169e033bfca35834699285df9c330
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:46:06 +0000

    foot term home yourself central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
