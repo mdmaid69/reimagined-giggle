757e8cf1126a44c1d6af3ef88e135fd36716e6c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:21 +0000

    part deal effort service mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def get_current_time():
+        return time.time()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
