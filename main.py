0c4a5157d1775fde6ed9949bf9bc544d55f659f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:03 +0000

    concentrate pleasant means become precise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
