eeea4a82e409549c81337d49908899d1c6cfdd91
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:56 +0000

    another wash pronounce exam inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
