69abfec9a089858f2008744a6270412bb8fc03a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:30 +0000

    learn talkinto oil perfect name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
