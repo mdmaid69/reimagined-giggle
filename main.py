bc1a2a8adc363ea4ffcf2fd452cd5c1aeaa7f590
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:21 +0000

    excuse measure frightened admire shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
