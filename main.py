a0b84779a0559d4fbc7ec23312e5e74261143dcb
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:54:55 +0000

    music foot weak join house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+print(os.getcwd())
