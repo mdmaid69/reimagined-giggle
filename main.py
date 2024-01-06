ca7be00c7c9bd9410f1b18c592d5e6aee865cef0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:06 +0000

    mess she getback have match

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
