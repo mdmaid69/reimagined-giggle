bdad39b12c18aa7cff40cbc350f9e629a19c76cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:31:39 +0000

    measure picture strength blue sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def count_elements(lst):
+        return len(lst)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
