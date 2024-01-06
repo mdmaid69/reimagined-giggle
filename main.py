f68ee18fdef3248009a49cc2e097d7f2c7df456e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:44 +0000

    cost bite giveback chief split

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
