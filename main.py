f66ef08370a9a7c670984b203971e88b03b2fcee
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:51 +0000

    geton collection convince unable lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def multiply_numbers(x, y):
+        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
