86b1ed240da6f6441f220c12981bdc9f927ee056
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:10 +0000

    occur mother design effort dance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_queue():
+        return collections.deque()
