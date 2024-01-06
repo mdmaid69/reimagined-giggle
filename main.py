8d228fac5d2426650cd3ba6accc2f5fd092c12b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:30 +0000

    label hurry old let start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_work(force, distance):
+        return force * distance
