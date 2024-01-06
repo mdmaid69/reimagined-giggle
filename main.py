e99c4aabdfb3fc1885456bc903894baa9fb85755
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:12 +0000

    length memory rank vanish illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
