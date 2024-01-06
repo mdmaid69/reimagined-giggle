2006354b253bb1a54e5a9187e681a1d0c1910cb9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:46:27 +0000

    sometimes disgusting nothing wish back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
