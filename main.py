3255cec8e83d624a845fb470e86e17380c8b5003
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:21 +0000

    supply absolutely evening soon active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
