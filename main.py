5393107b494d1174d17cd25368b2ac983ce08020
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:49:02 +0000

    minority space major cope drop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import tensorflow as tf
+print(tf.__version__)
