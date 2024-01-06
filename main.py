8d55732c8fae2df7b2d8f81b4f1ec17497d3950c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:25 +0000

    pretend measure sudden clean bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
