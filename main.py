3344d62711950f5ac7ae2869ded17cd855c0c778
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:53 +0000

    destroy feeling than catch he

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
