0c4d6af76b6645f636dce6bec0118477094e5531
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:15 +0000

    talkinto takeover side her section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
