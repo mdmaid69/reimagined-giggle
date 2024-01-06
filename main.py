54308aea801330e181d20036f7ffa3cc3c824903
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:01 +0000

    breakup injury disappear frighten television

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
