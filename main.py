261ab8bc7827dcf6dd436e74e5c6e893e72c2a25
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:32 +0000

    slight would stay medium bring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
