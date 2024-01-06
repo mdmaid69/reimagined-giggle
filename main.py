313ceeeaade9d211794ff7bff2c96e5fce49c97b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:07 +0000

    exchange question greeting dull opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
