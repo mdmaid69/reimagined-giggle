7d9d8bc26a7fc596f5a6dfd1cf0248ef0713859c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:01 +0000

    black live twist yes excited

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
