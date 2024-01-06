a06a29ab1bc4582e507a955be146cb00fae85566
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:32:56 +0000

    suit trade classroom explain hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
