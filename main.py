2f384afd018dcab65d9b087797b41d4579087ff3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:40 +0000

    correct expression only next roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
