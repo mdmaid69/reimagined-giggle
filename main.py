f588bd2995a1d0aacde42974b2abb7b99ef1df4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:08 +0000

    splash get laugh lookup turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
