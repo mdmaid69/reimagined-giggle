86a019f08ffd174938d93f3b9dab8f4e7828c2cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:13:39 +0000

    admit speech public underneath ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
