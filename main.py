69d3ba57b479201e5a26bbb703d8f40c640323de
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:46:55 +0000

    thing medium difference female instrument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
