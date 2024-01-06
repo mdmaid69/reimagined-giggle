1cdde4bc530771b814b65b1c8b4086c38724c053
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:37:00 +0000

    attempt accept truth thinkover score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
