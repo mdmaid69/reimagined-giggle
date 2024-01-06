daba26029ec15b94901d2dbd411449aaff93e648
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:05:48 +0000

    force divide list use level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def is_even(n):
-        return n % 2 == 0
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
