8b67d3b5ae2c30cf7dfa024a309fbf0397813704
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:27:43 +0000

    friend not develop reaction fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
