95ae4d4ace01873c030fb631fd4faca7359c0ae1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:40 +0000

    after failure news no pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
