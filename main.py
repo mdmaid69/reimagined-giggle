2b1b6f88dae38e9296099547a9463602ec7d2f88
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:40:20 +0000

    continue unfortunate wrap tradition cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
