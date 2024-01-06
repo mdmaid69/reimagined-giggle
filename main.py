fbee9b455d93ccd4f29900faf6fbc85a528900e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:35:50 +0000

    drive discussion black let point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
