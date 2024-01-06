dbb26cfc1b703e62464e1eb7cdd751e266c8ff93
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:31 +0000

    proper climb pay change flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
