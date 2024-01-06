fb2176df1d1bcea2f2c7f45d20eff24f29f8cf1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:11 +0000

    wish shelter lock bunch disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
