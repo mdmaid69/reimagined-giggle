233d43440172c688f06f21c4d0b8820d4b02f74b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:53 +0000

    freeze forever then meal air

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
