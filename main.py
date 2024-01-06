fe4fd69ea858e5e4232b6eb7f3b2756d9e62fca0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:45 +0000

    memory only type over hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
