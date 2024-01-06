6174abd0ea897a52ba7f2fdf3030b4df467720c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:29 +0000

    necessary drive in distance above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
