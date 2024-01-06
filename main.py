b352d8444a0aae7d7b988a7cc64069468242f0dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:12:08 +0000

    kind punishment away cook spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-print(os.getcwd())
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
