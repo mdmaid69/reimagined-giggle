d6e95ca8b6016c408624c72e6f3cd5b512ecfc69
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:03 +0000

    figure priority concerning life account

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_int(array):
+        return int(array[0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
