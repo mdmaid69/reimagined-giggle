686311f28a3a270b5f282545d3eede4cbdda27da
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:26 +0000

    apply within beside before disturbing

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
+import os
+print(os.getcwd())
