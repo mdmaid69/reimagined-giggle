1c45d771123a59ef21b98d5dbd708c67d9044420
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:40 +0000

    hide fast mess aim bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
