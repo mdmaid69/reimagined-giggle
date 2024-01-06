50f19349b7cb9c5accdd0a78b3a1b5de90402071
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:39:06 +0000

    ashamed coffee dream together condition

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
