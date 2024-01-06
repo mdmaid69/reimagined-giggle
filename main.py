1169aac847b17171d929fe19083a285df43e50ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:37:03 +0000

    another sign calmly change plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_length(array):
-        return len(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import numpy as np
+print(np.array([1, 2, 3]))
