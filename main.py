1441380e5cbe5e6c7a2ef5b8be62aea0f52d96be
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:14 +0000

    breakup tip radio thin force

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import datetime
+print(datetime.datetime.now())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
