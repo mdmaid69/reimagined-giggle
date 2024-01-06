13ca2340bb2f62bdafa0a0c09850cee942d8c8c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:23 +0000

    flow general deliberately tradition callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
