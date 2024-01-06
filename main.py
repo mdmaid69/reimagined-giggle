cb20205f7dfe2eea40997a4542ae02bc1b92f754
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:27:09 +0000

    history practise language dull challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
