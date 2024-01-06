cff1295b586d4c66b2ae652d4b91b76d49b0665e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:37 +0000

    surprise rare holdup sudden change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
