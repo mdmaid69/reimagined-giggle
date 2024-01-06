60dc3cf1356e05bccf999898f147e78d8a72c28c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:14 +0000

    realize takeout progress giveout computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
