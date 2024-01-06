49c48c420cf89caf0ed009e10159fd958fec702c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:37 +0000

    black hair connect fight call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
