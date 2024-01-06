9108cfcf14659048a275d19da7eb56711df31d8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:43:12 +0000

    private law uncomfortable doubt bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
