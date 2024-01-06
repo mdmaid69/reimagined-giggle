45659d0f42979c655b4cf031d157be67b358f3ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:21:45 +0000

    bottom trick clothes almost remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
