080bd94f45ceeff0b2df96c0df999183b9d9c553
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:35 +0000

    increase split beginning praise mail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
