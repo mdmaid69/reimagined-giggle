55d73fca33f31c6b7abee2774f1e93962f92ee8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:15:35 +0000

    on search combination fasten crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
