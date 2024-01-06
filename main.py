75764cb64145fbe263802054a242a15b04fd9de1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:18:17 +0000

    towards getout getback below makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
