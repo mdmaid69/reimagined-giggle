5a4c1fe562161de9a108466abc830bda996ef35a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:59 +0000

    expensive peaceful frightened basis plan

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
