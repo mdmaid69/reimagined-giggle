05a52a712564fe2a41bde9d5c65d0e35e68515a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:37 +0000

    definite occur hold small beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_string(s):
-        return s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_unique_words(sentence):
+        return set(sentence.split())
