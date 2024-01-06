08def6530b462481339843595e701d742d540f88
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:45:51 +0000

    spot silent arm comeover rush

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
