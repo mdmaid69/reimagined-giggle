942afeeba6b046e5453c3c38d6cbe13ef334ad91
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:27 +0000

    excellent about question behind most

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
