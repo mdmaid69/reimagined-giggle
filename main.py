9726a1326092b7e971e6ad174d108a335d13d0c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:18:30 +0000

    through spill large sit common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def square_number(x):
+        return x**2
