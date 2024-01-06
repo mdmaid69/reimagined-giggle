768772a4c7745da699d71992f60ffb5517323b70
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:07:34 +0000

    stare bunch dream connect Nn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
