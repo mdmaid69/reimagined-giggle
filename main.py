a3984064ddcd4f4622ef165f8e307e28a9ac1d4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:37:09 +0000

    spite centre unfair promise ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
