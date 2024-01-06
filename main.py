76c8d91d2525a67e6c11c1690dc527aa310b2201
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:21:51 +0000

    shall burst top hole Ss

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
