531b00e6d0d592ec1a06c1faf166b8a9ba96b0dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:27 +0000

    Rr straight relationship drawing hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
