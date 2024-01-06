2ca52a69ab53ccb88189bf681025d041e5e21cc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:13:08 +0000

    difficult edge no bringback handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
