ffbb54333ee156eb58186c7814e1e46a362fd806
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:49 +0000

    care noise song street maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
