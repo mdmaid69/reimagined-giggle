fe0fd572654e12193a3175bd1ef10eec27fc009b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:52 +0000

    catch careful Yy ever skin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
