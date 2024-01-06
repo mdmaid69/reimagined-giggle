bdbeffdbfb952c7705db161b815ebb5a5fee09d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:05:16 +0000

    record suffer cut peace book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
