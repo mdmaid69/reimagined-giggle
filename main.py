d16f3b5c2b3865cd9f96e5c075cfc40757ec348a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:22:17 +0000

    tie loud public worse crowd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
