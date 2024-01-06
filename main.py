9fd7e8933bcc5f7f130e84d9685f6a4c6afc1cf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:29:18 +0000

    meeting spring favour smell introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
