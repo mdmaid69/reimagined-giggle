b675958197898ff5065b74121cc912044cb4b169
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:22 +0000

    form healthy manage future must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
