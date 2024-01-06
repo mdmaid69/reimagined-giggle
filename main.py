3e60624182004f72af3d9df2cb1afe5525d40daa
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:44:44 +0000

    hers middle work breakdown minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
