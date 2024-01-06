9c73e60205253e09a801504773dddc3aad5f4794
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:37:39 +0000

    except cold keepaway ignore uniform

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import getpass
+def get_username():
+        return getpass.getuser()
