5deb542cdd84f4727a4adb02ae239fc6f601bf0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:13 +0000

    sign trade ready strange burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
