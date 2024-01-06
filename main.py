8a9c979b8e029703c5e1478e0951b11dc6e8c4c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:16 +0000

    bin close temper bound side

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
