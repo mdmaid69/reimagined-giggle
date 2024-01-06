0fdc5febffecd9eab612aec9d760db791cb15743
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:19 +0000

    worth action fine possibility open

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
