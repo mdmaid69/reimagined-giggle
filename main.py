034c005b151ddeed402e15ecdd37de3471d33ba0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:57 +0000

    vote body fresh during steal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
