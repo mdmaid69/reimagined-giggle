a8d1871038e2b0fdc65cfbfb99e1d864c0f5b540
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:31 +0000

    average satisfactory distance anybody pass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def reverse_string(s):
+        return s[::-1]
