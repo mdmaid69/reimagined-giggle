28508cfc23b96e614e7384fec313fabc8bdbbceb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:19 +0000

    doctor happy estimate rate mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
