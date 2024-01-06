90da47e68acc762875bf70542ff9d3f029eef420
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:03 +0000

    passage bear nor reason instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
