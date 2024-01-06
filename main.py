b3c74628a3964dbbabd6b395a254af8df09d239b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:19:43 +0000

    test downstairs transparent comeback drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
