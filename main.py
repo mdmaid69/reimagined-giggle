4c77f3a76ee8d71b2c74d4f0bd256dbae880f437
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:28 +0000

    giveaway same connected spring store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+for i in range(5):
+        print(i)
