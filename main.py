b89e544317f8f53e1382d20ecb41a3cecbc9968b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:56:43 +0000

    regular description call crime deserve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
