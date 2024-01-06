aa0d232cea27a3679aee100319028fee82714eae
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:29 +0000

    can chief backwards expensive mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
