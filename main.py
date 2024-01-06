6bcb3da7c9de615ba6d87aee22283cff01d91ece
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:25 +0000

    whole mark rush reply space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
