d17c8b1d19661379b317d4a04cf2626b1504a997
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:54 +0000

    show joke bar reduction such

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
