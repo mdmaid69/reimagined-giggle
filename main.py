4e39089d2ee225c20d306c1b413f36105fd89b72
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:40:08 +0000

    everything block must happy low

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import getpass
+def get_username():
+        return getpass.getuser()
