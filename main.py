043c759cf5d9f6d99ec2dee02c2d620bcf1255e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:36:10 +0000

    real ball excuse curious education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
