c17cbb8b9a76401764b35ac2c4dd65b94ce52379
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:16 +0000

    anybody increase forgive related low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_area_circle(r):
+        return 3.14 * r**2
