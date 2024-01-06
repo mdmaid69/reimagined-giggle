479d86ddb93ec01dd3559626379f0a6e0669324b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:28:46 +0000

    exercise comeup ball 566end breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sine(x):
+        return math.sin(x)
