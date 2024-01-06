b4a7df9a9ea24aea478ab1f81ab1d4427d7de259
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:20 +0000

    friendly arm lend worried case

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def count_characters(sentence):
+        return len(sentence)
