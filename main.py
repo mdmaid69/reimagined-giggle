625b42c26a3c6e8106e1415d53e836d7b2400155
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:09:02 +0000

    hand hear Bb cross curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
