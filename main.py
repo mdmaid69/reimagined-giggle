30820765b6b4cfd417e88df18669290a7b7c3f96
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:10:00 +0000

    then sign mind basic wise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
