19ffed2b77b0ab830b7bdc0699df018a7bea7915
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:48 +0000

    post cause cross slowly delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
