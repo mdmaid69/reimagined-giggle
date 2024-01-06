d1f94f02deb0e89f8f21811db77809e05a5ae4bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:52 +0000

    waste bring crash put suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
