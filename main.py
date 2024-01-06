c8e03bd58544a1c4125a6231016b66e3366f36bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:58:34 +0000

    period high rarely pass usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
