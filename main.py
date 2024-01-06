48a43a5618a994d7ed6066b5f1ca3fc4fc2b2a76
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:54:46 +0000

    arrange fierce clever similar sea

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def convert_to_binary(n):
+        return bin(n)
