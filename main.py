2117c2ccc16b7755ddca151040b0f10ff1d2b592
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:20 +0000

    break this offer 571enough flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
