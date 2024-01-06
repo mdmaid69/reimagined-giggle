001682914c73418801e41f47ddfef75e93603103
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:11 +0000

    report sort concerned tidy level

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
