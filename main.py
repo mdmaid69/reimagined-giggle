7413e1dc9d4dd26c4d6d21112a73b964ac5e674f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:50:43 +0000

    relative where comeover bet house

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def is_palindrome(s):
+        return s == s[::-1]
