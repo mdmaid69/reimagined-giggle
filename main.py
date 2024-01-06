8d98d5cd467884c7bfee43bfa0c9b710d8b06f25
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:45 +0000

    damp cross hold laugh anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
