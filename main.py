2fdf5b2055d02ea1e14af6acd4265e3fb422e0dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:25:34 +0000

    happen scene wheel will short

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
