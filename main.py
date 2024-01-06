1225529fdf98c04308bc9c9c5c601d40111253db
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:14 +0000

    ignore likely welcome straight punishment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
