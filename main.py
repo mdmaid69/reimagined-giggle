0da67953da3cae24f0d731abf333c48497ca01b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:39 +0000

    advantage cause comeup edge dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
   def list_files_in_directory(dir_name):
         return os.listdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
