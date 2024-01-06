32fd356c808a6f81bac7b0044cb72f2203560da8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:15 +0000

    while sticky reasonable city broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
