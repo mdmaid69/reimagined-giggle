316a953b09c5223dc103c970e1e1103afe4db1a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:47 +0000

    side rain giveback behind mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
+  def convert_to_binary(n):
+        return bin(n)
