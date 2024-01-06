197d054468207af30bd6bde4df098fb17b02831f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:06:56 +0000

    advise result progress cum problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
