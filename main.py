120f8d98e2afd8b1a541d1c8538ea71f1cf4d306
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:42:59 +0000

    wet tip ignore lookup train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
