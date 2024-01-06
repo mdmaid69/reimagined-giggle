3c9361db37ddae7dad0a6a937ea4e4cb517fa7f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:47:03 +0000

    principle everybody mass pipe nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
