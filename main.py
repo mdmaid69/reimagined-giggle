b4c3c3a0ea25c2287315b3f7a7180b9e6efb008f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:45:38 +0000

    attach bill nice extremely letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
