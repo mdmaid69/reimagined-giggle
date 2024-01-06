8db520fb125c35797bfff63b220774d88fa212da
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:54 +0000

    change style division active sea

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_priority_queue():
+        return collections.deque()
