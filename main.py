07a7081fd5331dd56d268b831eb310088bd4ec02
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:23:16 +0000

    crime note stiff shoot sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
