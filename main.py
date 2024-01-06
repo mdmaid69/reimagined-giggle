1d2f2cde3fd7a5862aaa75a8ece361346f5a1d5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:04 +0000

    set opportunity edge usual heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
