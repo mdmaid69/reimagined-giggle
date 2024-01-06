162983416187bb5a01172851b0e0961aa34d3005
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:09 +0000

    must value broken goaway must

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
