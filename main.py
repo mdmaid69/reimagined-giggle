b962c33dc9e394d17f6dd5709cc4b3a69afb8e5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:20:41 +0000

    sort loan everyone storm door

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-import sys
-print(sys.version)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
