3283474c12890998ba5a2ca7cd7cf24b9ff402b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:54 +0000

    cap single slowly either kind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+for i in range(10): print(i)
