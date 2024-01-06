a461e158235d18d09122183bcd805a9070c3b85a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:53 +0000

    shoulder country notice defeat thinkover

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
