8e726254a7648d3e6b6a026a632d5772a9eb104e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:02 +0000

    anxious female address interruption far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
