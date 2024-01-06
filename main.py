049f063aea2fbc752260176c358dca75db2d2dee
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:11:18 +0000

    apart smoke visit upset sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
