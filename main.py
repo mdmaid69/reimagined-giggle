f2d8a7fac61b92d7d28afeb7ba5042552b402910
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:34 +0000

    part contribute criticize eager peaceful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
