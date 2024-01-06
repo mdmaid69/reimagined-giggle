52ef5a32288d03bca76ea2d967979d78e2ae6990
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:07 +0000

    use dick expect ready power

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_list(array):
+        return list(array)
