6a3fa4fe2ed35ad0d9b60b240bda8a39727e59e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:35 +0000

    criticism quarter everywhere someone Jj

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-print(time.time())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
