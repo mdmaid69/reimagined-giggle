6233d72d500c6a32f3992f559edd2c3f77ef9a69
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:32 +0000

    waste number century ours middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
