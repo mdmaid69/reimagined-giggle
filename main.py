cbbd0154f05c842462130f21ddd99a00a8b65a48
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:18:42 +0000

    burst luck order spring activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
