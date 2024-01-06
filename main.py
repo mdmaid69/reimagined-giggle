f59960b6fd2fa6788e75c48a544085e0160a3151
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:25:57 +0000

    swim live dull hard along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
