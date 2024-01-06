8c46ba4c5de1b2611e59355905f1e90e3123f10a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:31 +0000

    broken lend characteristic petrol test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
