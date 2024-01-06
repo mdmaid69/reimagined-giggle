0d467ccd872a64e3b091e0772829608e961f9285
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:31:16 +0000

    anyone comeover watch break school

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_min(lst):
+        return min(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
