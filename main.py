a1dfafc6067fe796fe47a996fc563e5b223e6fa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:19 +0000

    proud kind release your runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
