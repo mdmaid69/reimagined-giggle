40490962c9f07230e9822ed2b5dbb7205c378950
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:32 +0000

    no regard cross far glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
