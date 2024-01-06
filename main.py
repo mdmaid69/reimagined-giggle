60448cff08c0be88d0bb5620a457cccc069d7003
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:46 +0000

    cap work high late consist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
