987c92fe976d9f76aa77c0a058b27d44a4b9dddf
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:38 +0000

    petrol sentence physical culture collection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import tensorflow as tf
+print(tf.__version__)
+def calculate_area_circle(r):
+        return 3.14 * r**2
