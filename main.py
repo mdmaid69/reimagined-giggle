e8f712dce7efb0fa6b9b75edb3a5255a9686aeb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:44:19 +0000

    outside advise matter friend live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
