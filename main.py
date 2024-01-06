f3428ae0887d39a2f4567792152075574eee4ba1
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:36 +0000

    information sort rarely edge prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
