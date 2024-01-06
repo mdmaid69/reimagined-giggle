94ca807c9af38b14c96a2429e7351a0fe44b6c1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:22 +0000

    judge separate positive tonight Ee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
