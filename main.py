a106b60945cd36e6c856842ff07a8b61e78d996a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:35:13 +0000

    shut suit survive value set

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
