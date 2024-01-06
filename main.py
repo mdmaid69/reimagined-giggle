bbf6b6249e4a858985f82b23273ed33fb86cf1af
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:48 +0000

    round pretty film dare production

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
