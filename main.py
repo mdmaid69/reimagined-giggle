0aa60f5e13c6137578bb845318f7766604458cab
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:06 +0000

    wet weak carry callback lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
