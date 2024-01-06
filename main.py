4c86c32debecf17094309ef2d9b65555a95af82d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:38 +0000

    Itself want follow may odd

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def convert_to_hex(n):
-        return hex(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import re
+print(re.match("h.*o", "hello world"))
