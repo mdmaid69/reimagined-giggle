34b3fea200736922a3a6a0fe80024d9e0fb34868
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:54:46 +0000

    time carry bother rescue read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
