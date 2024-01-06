874c95d5ad6262ba245b1b895f5cc016b3b478a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:27:48 +0000

    switch early keen murder entry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
