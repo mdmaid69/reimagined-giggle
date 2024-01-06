4b786c1a420a482d4441e3627d16c14e6f91f23c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:19:03 +0000

    stick accommodation make last die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
