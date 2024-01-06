659c6b2d84181bbe6720e71c5d99143b9e87ea6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:43:51 +0000

    future 569enjoy transparent bush fresh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(lst):
+        return max(lst)
