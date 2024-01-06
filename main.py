43a54a5cb6715b987b2f7cfc39e1975a2e93ac06
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:04:04 +0000

    plant pool neither near meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_tuple(array):
+        return tuple(array)
