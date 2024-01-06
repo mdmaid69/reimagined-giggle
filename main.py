729e825ec6a451ed7848be1a543f7c5fa87a93dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:22 +0000

    colour eager hard if along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
