ca69cbf25297febf3f0f26311ab70a6e6c979637
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:03:19 +0000

    explain area holdup belong work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_item(array, i):
+        return array[i]
