22b032f1b0d0ddf57e7078554a7d31e7dde46147
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:02 +0000

    holiday beside change believe strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
