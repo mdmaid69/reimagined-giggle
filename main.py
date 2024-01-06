7f31e23ae6ee06e85360fc5468583076f1eb8621
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:34 +0000

    stop moment luck protest slide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def sort_list(lst):
-        return sorted(lst)
+def calculate_work(force, distance):
+        return force * distance
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
