ed26f85b565bcc73089794350891e5c67cff2eee
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:15:14 +0000

    march start neither classroom Ii

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+print(sum(range(10)))
