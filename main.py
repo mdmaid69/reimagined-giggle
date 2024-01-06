4c54e59076d67cdff23839e197387c15e80ff5df
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:46 +0000

    soak exaggerate exchange relative soft

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def set_array_item(array, i, item):
+        array[i] = item
