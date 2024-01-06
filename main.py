3e54d1b0a68a7a3cd5b6e2c78743862921ba0944
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:48 +0000

    cover properly bag mistake keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
