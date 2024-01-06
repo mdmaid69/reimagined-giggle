4ceedee5e0a7bcdb0185abf1a6ea5919ec2f7eda
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:51:36 +0000

    stone negative gas square excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
