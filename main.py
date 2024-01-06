252066cb66a50e5755aa7d905e7b9e737ac5c839
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:57 +0000

    contact exchange treat entry fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+def print_python_version():
+        print(sys.version)
