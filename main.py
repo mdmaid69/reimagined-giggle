035efa8ab7ffd48adfebf92d2550e346144712f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:46 +0000

    throughout probably limit gather business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
