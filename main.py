f3f8ea17eb046e682f3daf337ea8b4d8cbe57b71
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:38 +0000

    afternoon shall play record concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
