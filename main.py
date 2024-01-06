920f5068802d43ea8d1f10d91c72abe755276ad4
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:48 +0000

    quick aware space past increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def cube_number(x):
+        return x**3
