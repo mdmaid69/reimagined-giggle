2934e8fdf495e104c79c4ef3accc6038682db943
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:09:35 +0000

    pleasure smell nonsense shine account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
