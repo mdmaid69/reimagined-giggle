baa1be9829cbdd55dc89d6843fecefc9ad5a5ea7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:35:49 +0000

    hungry shine interesting hurt child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def is_odd(n):
+        return n % 2 != 0
