3901b9e8eeaf50d6bb1d8d5348167a3487002c47
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:27:40 +0000

    block guard hungry apologizeapologise new

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
