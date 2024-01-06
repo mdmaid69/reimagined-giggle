db87469f1730ec0f6fbcd59f6b1acaa6f3b9a202
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:37:59 +0000

    protest blue worried centimetre steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
