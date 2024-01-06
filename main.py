d8df34f6a80fabc9acd4a7e77d1262ae592c8467
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:57 +0000

    Ee imagine steer shoulder force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Is palindrome:", text == text[::-1])
