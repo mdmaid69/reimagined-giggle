3eac1b3f870d8fc0c63eeb8b81a4304cfab28e83
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:10:41 +0000

    object healthy mine team freedom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def reverse_list(lst):
+        return lst[::-1]
