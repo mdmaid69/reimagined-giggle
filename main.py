2480e2c8cca19519630e5f1dba2b642d6afd6a8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:04:25 +0000

    late though square letter mainly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
