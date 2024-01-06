be048629c323ef4eb57f25e826fc27a81f3b6c5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:32 +0000

    later anywhere outside finger rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
