53210fc615e5731296deabd97102f1ede9a1a502
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:29:27 +0000

    string because situation view no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
