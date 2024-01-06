3a62c24a4ef54ef2ffab955810fbad0f74875bb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:10:07 +0000

    excited wind nervous like Tt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
