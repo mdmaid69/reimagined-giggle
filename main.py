eefeec962497f36cb704b72099365c914f12ef3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:11:23 +0000

    month style loan convinced fire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
