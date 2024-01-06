9a40908c67d207d24050f5ffc755fe3e9c62e53e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:51 +0000

    song model steer range niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_area(radius):
+        return 3.14 * radius * radius
