6c299ab6c9f42dfba96f395f68eb50d3753ed03c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:57 +0000

    arrange store major school turndown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import random
+def roll_die():
+        return random.randint(1, 6)
