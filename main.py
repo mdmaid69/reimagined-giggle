38f9fef5452c9b70e0571bbe0b76eed46c442e4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:42 +0000

    few I warm interesting light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
