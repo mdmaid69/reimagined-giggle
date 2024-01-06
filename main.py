4af60792b1c2a83ec1710634a1c32909ae8d4568
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:33 +0000

    past makeinto offend dance final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
+import datetime
+def get_today_date():
+        return datetime.date.today()
