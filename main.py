52929f99d9ebb8dd75d39fb07ddadbb5e0fec70f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:57:42 +0000

    tear announce fire control connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
