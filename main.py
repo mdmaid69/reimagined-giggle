51dede1c8d0971edc229db29909748446f5f264d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:08:22 +0000

    pride worth discuss add example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
