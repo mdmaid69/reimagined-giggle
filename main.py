0322e492f50bf9cb6d47e54a7c64560a101115be
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:37 +0000

    during further mad patience squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
