5de3837499f25fdb042b366551a54b067244a26d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:51:56 +0000

    state face congratulate motion label

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_work(force, distance):
-        return force * distance
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def square_number(x):
+        return x**2
