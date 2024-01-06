c6ffcc67b6aa8b981817c4100d5195c0e8e8a98c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:27:35 +0000

    project swallow least plenty confidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def square_number(x):
-        return x**2
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
