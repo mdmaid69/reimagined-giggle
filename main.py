6e406b3831cc0eaf52e8c18b8d32fc87e4656005
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:52:51 +0000

    its near face write uponon

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_max(numbers):
+        return max(numbers)
