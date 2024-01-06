0479b30903792532837a7edb11a4dee9dbb632b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:15:18 +0000

    dare boil matter busy various

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def subtract_numbers(x, y):
+        return x - y
