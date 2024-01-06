fc402eff605d28556c4507da257c7b6f369bbc05
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:29:32 +0000

    similar proper fasten could material

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
