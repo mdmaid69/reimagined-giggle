5da759f7f9127da48da4df0eebe49e1ae6655fbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:07:51 +0000

    warm laugh frighten familiar reason

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def is_odd(n):
+        return n % 2 != 0
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
