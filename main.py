8c3e0fdea997017e3a33d08578ea64ca750ad774
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:46:16 +0000

    feeling new confused die conduct

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
+def sort_list(lst):
+        return sorted(lst)
