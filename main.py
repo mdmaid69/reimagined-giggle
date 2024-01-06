8addf0da0bb5ee87a0d5d8ef1a0101f0aa981a37
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:01:25 +0000

    because bright effect fond limit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
