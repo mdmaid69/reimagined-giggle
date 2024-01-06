ff6a30e7465fe05ed6f3d593a6fec79c10b42fe7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:12:48 +0000

    choose can stop set trade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
