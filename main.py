1f736b0b58400c71aad4cc186c1c7390b76609d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:15 +0000

    tie always emphasize passage hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
