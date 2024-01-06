d5ce7d7b99835565a8d5063109cbae112e70ed60
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:05 +0000

    exact progress hope same indeed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
