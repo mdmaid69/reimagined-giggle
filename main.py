2c1e1f2953485301b1dce3b2c48786f53931f76c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:17:27 +0000

    progress Uu balance Hh punish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
