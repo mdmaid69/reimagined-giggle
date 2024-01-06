e38f3a0184f636ac7d2b210c750d4b79eb157d9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:31 +0000

    callup pause Ll wind ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
