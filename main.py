c633cc99b51801db8ed09dfabe84830a3c6a2f48
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:04:24 +0000

    wear delight both circle perhaps

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 def calculate_eps(net_income, shares_outstanding):
         return net_income / shares_outstanding
