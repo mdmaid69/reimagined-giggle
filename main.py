18d25fbbf7af06d12c2786b515145e5234b9cb43
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:43 +0000

    throw concerned chance report pencil

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def square_number(x):
+        return x**2
+import random
+print(random.randint(0, 100))
