3478e5438f4468def110234cd11aa677f7d8dc11
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:31 +0000

    birth break time onto taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+print(os.getcwd())
