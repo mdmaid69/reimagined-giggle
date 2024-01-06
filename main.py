005a3ec79a6dab4294b67200e6d95b29471b59c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:03 +0000

    born understand to spread key

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_stack():
+        return collections.deque()
