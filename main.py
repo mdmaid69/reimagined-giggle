16e92c7caf0350e496331f282d787805283e8ce4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:03 +0000

    continue relief model study dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
