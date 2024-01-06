4ccc7dab9c8ee66d8addcc56730dff7c4bfdb026
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:26 +0000

    suddenly secret another getup exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
