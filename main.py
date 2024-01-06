04b719e23396a064ebbe32644320e37a9510c9a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:19:04 +0000

    comefrom suddenly geton soil sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
