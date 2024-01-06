7109ffcb2ee50f747f5c56691807ee8550749d64
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:02:42 +0000

    hard tie experiment muscle until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
