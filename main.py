9a88a35f465fa5351a124e3a86dacf41b373058d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:57 +0000

    complicated everyone dry underneath anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
