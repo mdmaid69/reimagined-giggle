4464fdaa4f3f6a2735b432d17a741e087671b50d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:00 +0000

    still comefrom pause people class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
