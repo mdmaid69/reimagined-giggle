d5a7a661d1fcd813f61d3aad9b4d211614b9e34a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:19 +0000

    turninto threaten nearly trouble back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def is_even(n):
+        return n % 2 == 0
+import getpass
+def get_username():
+        return getpass.getuser()
