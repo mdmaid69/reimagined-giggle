70cc032d54e32d4ff66e23a16c4d6a26882d19e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:35:12 +0000

    not shut send edge fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
