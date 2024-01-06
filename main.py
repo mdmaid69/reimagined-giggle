8760c033a80208467929317b0f099f3e157577b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:53:19 +0000

    divide bother well before hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
