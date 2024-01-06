711255c3c51b18e6f5728db28390693f40abf948
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:50 +0000

    ill people lost communicate prove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def square_number(x):
-        return x**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
