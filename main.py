d3f77681abc06bb33482c42d209a8623f79082f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:33 +0000

    central left later brown similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_set(array):
-        return set(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
