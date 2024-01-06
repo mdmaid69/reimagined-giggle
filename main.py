364865e8df879a576025f52716371a60cff5a444
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:24 +0000

    steal cover clear watch brush

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
