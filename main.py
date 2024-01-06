f24385489d90a1aed60edc4fa93843d0735afcb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:28 +0000

    sorry explain have undo whistle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
