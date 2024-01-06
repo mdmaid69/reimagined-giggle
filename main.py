e62f409144c09f38f06bbee320a9252cf8ff968d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:28 +0000

    bye easy same sensible distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
