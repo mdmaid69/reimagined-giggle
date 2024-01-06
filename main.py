26bdcabbb5f1624314f17d5222a4fb885ccabcc9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:50:52 +0000

    medium our issue accidental depend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+for i in range(5):
+        print(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
