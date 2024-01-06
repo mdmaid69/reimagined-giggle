5539685853beb75f2426147033a98146fa439b73
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:25 +0000

    probably disagree activity doubt raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
