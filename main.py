5bfb2c4a172a5e950f34ebe5fe4f8b4f66a03e87
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:05 +0000

    stir hope price use myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
