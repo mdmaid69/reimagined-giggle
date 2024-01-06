a6ceb05a2b3f25d43f1d46ec46f02f9c70d99032
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:21 +0000

    regret community minute sort summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-print(sys.version)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
