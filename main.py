da115a8b0605faeeef27655c56b81b23bfbe0b4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:38:52 +0000

    used drink promise view whenever

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
