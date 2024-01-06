ea94e89b819f10f18ad11e5eaba7f904b28ce6cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:33 +0000

    final frequent comeround mark beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
