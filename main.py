1090513747f08de3b1774764c485a7fd64503284
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:00 +0000

    life form difficulty sense wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+print(re.match("h.*o", "hello world"))
