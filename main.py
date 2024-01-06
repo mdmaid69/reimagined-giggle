a81a5aaa8c88917e91b84a71fcfb3ea228841ffb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:06:26 +0000

    couple meanwhile trip deserve accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
