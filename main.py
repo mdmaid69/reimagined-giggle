12c8245ef22d689f50d9b857e2d6951cb0ba97b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:48:09 +0000

    mention spare general name left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_str(array):
+        return str(array)
