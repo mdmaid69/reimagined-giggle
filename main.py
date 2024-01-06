0caf4d8ca6d7b57770cdff016db765b993608772
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:51 +0000

    book play negative its comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
