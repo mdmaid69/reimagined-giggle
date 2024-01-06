e88e8cb811dfc27440c86ddf8380af7101928771
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:38:58 +0000

    acceptable lonely correction hungry improve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
