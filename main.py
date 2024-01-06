384fdefb7ebb0c21eccd467e7527c86a8ee265eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:49 +0000

    turnaway callup priority fair spill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-def get_current_time():
-        return time.time()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_work(force, distance):
+        return force * distance
