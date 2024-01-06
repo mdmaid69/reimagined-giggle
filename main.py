6bcd07cb07f5abc193a5c08fd73adec2e6bfff39
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:22:56 +0000

    match spoil book display then

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
