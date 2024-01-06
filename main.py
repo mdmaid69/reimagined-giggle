570c3babb5b9382b10484f87b2f586e1ef258446
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:10 +0000

    expect burn account paper hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
