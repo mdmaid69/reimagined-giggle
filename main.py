b66705435570948727db5b841fe4d32f68fd583a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:47:52 +0000

    turnup description mess sick slice

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
