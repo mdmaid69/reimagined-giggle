542f726a62920a507210ed5aaa887e2bc9dfff09
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:25:05 +0000

    land national spill sense true

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def get_array_as_int(array):
+        return int(array[0])
