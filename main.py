6b92c337ddac1b7cc10d49c90d9128b903daa928
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:10:47 +0000

    easy separate reply body mention

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
