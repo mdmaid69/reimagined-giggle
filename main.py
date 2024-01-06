3460f56fab2ff3b8f822f7353d8ad97323fd922b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:59:51 +0000

    emergency tragedy right move uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_speed(distance, time):
+        return distance / time
