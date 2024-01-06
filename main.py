8267d491be882deb0081ce978fe344885ca02d96
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:33 +0000

    mean who afternoon long wing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+import time
+print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
