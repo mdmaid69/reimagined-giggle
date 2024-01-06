b33268d46a9cc6b9bbbb75270d3e142138f4e5ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:03 +0000

    hair clear somebody left practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def convert_to_octal(n):
+        return oct(n)
