df14caf5289ed19292e9f9a20a728960f439f575
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:54:49 +0000

    mend getaway sweep nature interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
