2e55e2c5ac1bb98015f88663e35beb21eeb94b1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:24:24 +0000

    previous funny result badly clean

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
