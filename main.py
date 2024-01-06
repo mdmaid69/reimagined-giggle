12c1c29d993d603809b357a2b20f80c0037ff8b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:09:24 +0000

    Ll method comefrom limit within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
