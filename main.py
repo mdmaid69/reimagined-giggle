8d5c2c57d6a724ca0f835e92b540a6b95b5afbb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:23 +0000

    rest storm comeup heat among

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
