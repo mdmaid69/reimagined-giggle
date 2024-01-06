a9298ba9d49d27fe5c711cb7dda9dbfeae526768
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:32:41 +0000

    maintain deep wish copy television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
