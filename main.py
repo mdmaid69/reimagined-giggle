ad2e63a3eafecf64d1530c073aeca146f5086334
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:14:19 +0000

    rather frequent base remain as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
