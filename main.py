9a22f81bba369c6ceba0a9dc39d91967a64018c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:38:29 +0000

    promise defeat mass around wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_area(radius):
+        return 3.14 * radius * radius
