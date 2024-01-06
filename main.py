4b77d76f1e31ca04b5e6b4534ff4ac1d4c702e2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:51 +0000

    pronunciation union possession much opportunity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
