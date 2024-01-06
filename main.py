d16d367e9444210610787ff7aa1b3f8f7dfeb355
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:48:53 +0000

    angry war bank press easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
