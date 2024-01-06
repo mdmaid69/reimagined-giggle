d154e1b42a59fef004c61bf7160028cbc5f494f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:12:10 +0000

    watch rush run car bite

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
