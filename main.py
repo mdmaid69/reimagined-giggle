deb92ef593b5e668b3b22f747e2028e6fe237feb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:24:39 +0000

    moment material from what getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_binary(n):
+        return bin(n)
