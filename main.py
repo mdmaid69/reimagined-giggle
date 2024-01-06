21f8bd895779c0e7f5fb93e6817a2fa20d66751c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:47 +0000

    close announce keepoff cross practise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
