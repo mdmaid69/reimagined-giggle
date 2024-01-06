511c114b871d653ed0765e6206be271643f47c74
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:36:29 +0000

    bottom responsibility do rubbish protest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
