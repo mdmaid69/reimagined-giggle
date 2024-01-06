f691e7802ae837cab39a2bd79bd306519ff64bf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:41 +0000

    material taste tidy lock concern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
