2fbb42503a8e50e0b1b2258437011552c9b5566b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:24:58 +0000

    base judge will command downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
