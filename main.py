7dfbbff64282f37c7035425f60263d6da88d0ead
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:18 +0000

    catch rare cold summer by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
