d32d4526ebff2865fea51a24b5d78bd0b6680853
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:00:49 +0000

    position inquire emphasis commit visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
