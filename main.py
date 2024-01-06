0199ffe64f455b95d6078802ee8a3a3fb49e1b7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:40 +0000

    head fly too smile promise

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
