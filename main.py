16ebde7f3bfaf39bd8714071bc692736b355a644
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:26 +0000

    nothing breakup few demonstrate underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
