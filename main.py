83ebd9546bb3dedcafdf79083add1a1746298c47
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:12 +0000

    mind friendly spare better suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
