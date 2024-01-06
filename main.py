333e8171f3c273bb3a9aca1e77e90dd10c556758
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:25 +0000

    emotion cut possibility calm heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
