72907427edb8ea82cfc5e6361fb577c7f33e0c62
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:32:15 +0000

    crowded geton ourselves waste related

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
