359a75b9454d5f07e3fe7c51d2017c92c6e9b46a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:28:48 +0000

    motion influence line embarrassed dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
