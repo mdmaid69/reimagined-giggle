d0ba6f70c95ddda3f83f701d7f2ea512a30a092a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:10 +0000

    trick sky comeup correction blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
