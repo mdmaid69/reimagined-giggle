a154bab5e945d57d54ef2b86571060614717ce15
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:21 +0000

    style ashamed Jj bush owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
