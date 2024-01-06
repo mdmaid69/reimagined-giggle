74138b4e299c2520bee90824031663f84bff3b41
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:48:25 +0000

    admit count bus kind afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
