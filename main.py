14a01a14d0d98c280b1039782fb265609782c184
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:05 +0000

    last learn still taste represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
