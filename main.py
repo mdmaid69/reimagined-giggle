94663ec4b21741d1e7ef363a624b5302387bee95
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:18:30 +0000

    imagine moral wash trick cum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
