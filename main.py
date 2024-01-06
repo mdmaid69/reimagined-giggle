1465339be6d9b4aff22db68b351bc2960b96b65a
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:12:11 +0000

    conduct plan hair home nerve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def multiply_numbers(x, y):
+        return x * y
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
