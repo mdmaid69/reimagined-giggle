b85af53161350f1ffb704ffbb57d7ef4f6b20a66
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:19:55 +0000

    Jj recommend pick result later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
