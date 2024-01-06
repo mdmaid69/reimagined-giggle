deaeec16c1543b98d76ae83eab1412f8427df8df
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:23 +0000

    realize this live head keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
