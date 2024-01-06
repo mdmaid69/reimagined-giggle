a5c929f4ce6faf411d8cf778b2f382b0f22c0565
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:47:06 +0000

    create without remove dangerous purpose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
