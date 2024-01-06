c3419b2a611225c8ddf972b15835b1eaa1dd02f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:29 +0000

    pipe comeup callout charge job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
