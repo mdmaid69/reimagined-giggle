702501f2ff03da99552de1d09792e79501a3f6f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:36 +0000

    original deal good ask lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
