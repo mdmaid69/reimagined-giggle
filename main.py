d9384df0bec0958b0330cccddaffd5fb4c0227e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:42:04 +0000

    foreign conclusion note backwards idea

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
