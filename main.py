bc2b0302eb03207a42e69aa3e2e525bace10826e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:33:09 +0000

    mark silent our attention performance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
