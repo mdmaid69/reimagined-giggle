a18f4922c607bc77806e7c4e6f2f7c4d5d71a50f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:05:09 +0000

    suit argument drive organization absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
