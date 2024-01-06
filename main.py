02b5cb231675a14cab6f918281106f84d7da5cb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:39:41 +0000

    coffee worth pay recent book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def remove_duplicates(lst):
+        return list(set(lst))
