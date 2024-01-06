433edde4429a400bdd1173577739a36c3296ca75
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:57:45 +0000

    attention unusual how scrap holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
