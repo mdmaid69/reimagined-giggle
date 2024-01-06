abd613583b389599d60dd8522cf6631c0b975ebe
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:02 +0000

    short related stupid ride want

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
