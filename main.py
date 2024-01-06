982ad5a7f39d8dae38078bad2795d48186baa0fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:47 +0000

    have travel communicate he walk

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
