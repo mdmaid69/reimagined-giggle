5775d7b658029bcb27800bff1b6ca40abfcef46e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:04 +0000

    kick tip right lookafter body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
