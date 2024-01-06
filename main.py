709ed35dbdb9d30a9048ff6d9ea928a3d7d65dfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:25:30 +0000

    order hollow sensible wish skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
