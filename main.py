0a96f1560456310f58adadd38d4eb81cbeb29d0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:22:40 +0000

    contents around agreement advise sister

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print("Average:", sum(numbers) / len(numbers))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
