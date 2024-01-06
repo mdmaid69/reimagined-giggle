2cb4e441d12c591e495437d87ee7a0cdf791765c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:54:03 +0000

    accept funny scare simple suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
