926524ee69c8e997760c9c24edc1268b283a936a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:05:41 +0000

    emphasis kind event pronounce steam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
