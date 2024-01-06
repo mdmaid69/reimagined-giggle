e06b2ddef1168c4f7e5c8480d22944ae78c14785
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:40 +0000

    depend something consult or look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
