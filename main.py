c8a1181a124ae62ace80e4553e53babb47274733
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:08:24 +0000

    way ill prefer scheme narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
