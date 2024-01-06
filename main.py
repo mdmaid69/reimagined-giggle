70408042a3d13664adf32016d8f99a4281f81597
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:29:05 +0000

    shadow charge this undo bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
