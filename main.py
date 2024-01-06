3781325d77a1867b4f8ab7311a0b39ca42bc58f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:48:46 +0000

    they angry expect takeoff spin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+print(sum(range(10)))
