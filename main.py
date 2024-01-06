d8fd162a079ac91d59c71741c736199fb3c6c096
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:51:25 +0000

    character near beautiful surprising whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
