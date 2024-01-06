223c3d542151aab923376fffb235d29c3ab58fd3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:12 +0000

    rob family deep good trip

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
