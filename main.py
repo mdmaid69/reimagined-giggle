94540de289027b2d3f92ad4feb6f6ee7ebd3148d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:59:14 +0000

    takeaway range reliable clean occasional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-print(sys.version)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
