b1dd781dbe57736569b6ea0f4cc41a0a1397af41
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:59 +0000

    improvement hit whom outside usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_max(numbers):
+        return max(numbers)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
