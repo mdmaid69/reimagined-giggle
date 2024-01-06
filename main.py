17f4ca161761207ca50451ecfb861ccb73ff5b8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:21 +0000

    fun march today tradition for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
