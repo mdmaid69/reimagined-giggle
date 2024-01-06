525a71ba7a74dd313bfbba5244629ec040684259
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:33:28 +0000

    watch error upwards own punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def cube_number(x):
+        return x**3
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
