baa98d90dc6d626f25208036eea3fc7691fe73ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:27 +0000

    mess ashamed refuse show crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
