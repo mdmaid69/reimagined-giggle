263578f717f2977a3826242af628cb101118fd7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:54:02 +0000

    breath noone afford peaceful well

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
