dba3da958c0a672fb21ac7a439f2e7c357606873
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:58 +0000

    plan past none nearly city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
