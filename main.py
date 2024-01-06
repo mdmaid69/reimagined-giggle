00ee30135d823037967a69e8a0f4512c4b794afa
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:12 +0000

    dig disappear argue skill maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
