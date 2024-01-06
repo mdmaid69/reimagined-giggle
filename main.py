0b43e5b011db334cc0e37c2443c2147dd70e1e21
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:29:26 +0000

    sometimes trip as lean attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
