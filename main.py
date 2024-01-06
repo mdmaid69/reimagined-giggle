dbcaa47f6b470e88a450efb173923efff2ecbe76
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:51:36 +0000

    cheap ocean rubbish whether test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
