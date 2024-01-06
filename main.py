bb9b81fd0c3d8eec773f083977b4220e83d5d937
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:38 +0000

    clean consist yesterday write behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
