384d801c4315bd7ddbfcfa331e80f1561dc788a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:34 +0000

    cry load actual lower process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
