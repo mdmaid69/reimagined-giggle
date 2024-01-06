8741cc91e24c830444005e333bc4bf908da4f7a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:04 +0000

    only extremely excuse give prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import collections
+def create_stack():
+        return collections.deque()
+import os
+print(os.getcwd())
