d074355f91ed413d4ddb84b6b067b957cfde9a53
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:32:52 +0000

    ask clean understand exit take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
