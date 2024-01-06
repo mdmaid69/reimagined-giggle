cf21fb20b056c745dcddda366de18d2187ee8511
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:50:00 +0000

    tear dress win bet deliberate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
