c4f909316e2c544dad23a37eb151da06a7d88d2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:14 +0000

    fire measure wind perfect mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
