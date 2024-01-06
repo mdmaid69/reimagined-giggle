5c768ec5ed349ef359ab1c3e15fada92673fbc6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:17:02 +0000

    familiar behaviour opportunity Jj wire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
