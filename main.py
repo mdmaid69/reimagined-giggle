d22dddfa0f5a2a4c8306d58cdf9b647279ea204c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:11 +0000

    bang state favour prepare practical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
