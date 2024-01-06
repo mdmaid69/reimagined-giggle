5f35a4bb0a45ef75d70c1dc2369a5283ae075c62
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:08 +0000

    splash lookback(on) achieve swimming skirt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
