6d86679aae5cdba1990f5372ebc5f90aa3679ee9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:49 +0000

    guess equipment successful bush break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
