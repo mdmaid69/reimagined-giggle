37da8ddc3a3c6660fa71befae8dd2e821bee9aec
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:38:36 +0000

    talk skirt neat mine spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
