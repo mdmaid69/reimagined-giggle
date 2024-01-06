5e44c4f29634f8a80825583a3ade7e9d9f2cec17
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:34 +0000

    promise whole except pair winter

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
