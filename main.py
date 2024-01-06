1bbc3b73c7e4b9d094c78f81473a58d6aca4e556
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:57:44 +0000

    middle Mm criticism hope damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
