69a748e4da818a55b82944e22f7c9f1ca027dfb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:00 +0000

    definite moral scrap influence since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
