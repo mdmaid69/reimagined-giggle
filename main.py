f692c98f248b1ef4aaf4d1dbf9d867d10478e229
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:56 +0000

    count declare happy reasonable genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
