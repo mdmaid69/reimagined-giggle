ac840903d5468c5112dc73cdb8b5921e09c12d9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:40 +0000

    big bag return turnback concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
   def get_file_creation_time(file_name):
         return os.path.getctime(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
