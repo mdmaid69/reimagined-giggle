291aa892a0fd53d1efd2e67349a77bbbe60aaa2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:05:08 +0000

    thanks aim jump mess slip

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
