a0c4d4783bd83df8640615230c38f96218e11109
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:12 +0000

    foot unfortunate firm confused plate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def divide_numbers(x, y):
+        return x / y
