cb7fb268fdffc74009a9bbf84366c9817b861df9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:20 +0000

    car fierce stamp hungry issue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
