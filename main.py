3ea5125b4b73568ef789e05da65c2156bc148906
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:09:23 +0000

    exploration spot statement rise spill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+print(sum(range(10)))
