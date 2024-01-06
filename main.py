8b5a562b0c70e112db21442a3b604a6a013148c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:34 +0000

    marry own splash fold party

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
