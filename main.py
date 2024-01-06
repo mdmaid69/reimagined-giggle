f5587b3fc6ed9ee283fcbe742d6b22d34b7e7996
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:16:22 +0000

    method meeting range reserve joke

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
