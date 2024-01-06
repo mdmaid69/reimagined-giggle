fc03ac3611dad52583fce660f5cd37c948b4ae3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:53 +0000

    task come in goout set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def count_elements(lst):
+        return len(lst)
