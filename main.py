8203c3aaf4a6e61fa96c4e19f4be89cde3d3a76e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:59:22 +0000

    tread wrong settle father plain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
