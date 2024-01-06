198d4a73e110d37ae7c835e4c7ff42b780e291d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:18 +0000

    respect 179bite influence reduction question

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
