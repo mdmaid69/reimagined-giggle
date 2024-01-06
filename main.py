6872bb2df511de3d0e145ff15a5fc1af5c7dd5e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:49 +0000

    base further smooth behaviour live

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
