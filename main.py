7914a60be92ccbe602080fa8c3d3619eec9b60c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:57:09 +0000

    strength nigga situation advice true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
