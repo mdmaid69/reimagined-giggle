00c137b549f4b0020feeb223fcff93beb0b7556f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:35 +0000

    watch Oo failure Kk thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
