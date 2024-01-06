ccc7c551b45ff00c64504932982b340239845b79
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:32:43 +0000

    least cry scared breakup runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
