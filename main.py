f85cf72839a2a8ee4f29f469bb3b603c8f2859f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:06:39 +0000

    few earn expect turnup getup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
