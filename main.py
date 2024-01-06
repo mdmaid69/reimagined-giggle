64b88d83df6c977006f8bd741a3ae87e9e2dc415
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:56 +0000

    worry theirs private hand level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
