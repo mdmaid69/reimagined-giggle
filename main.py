eb79b272cc15fd54d06ea6769be7d2b86b6a0ad5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:04 +0000

    blue impatient talk storm broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+for i in range(5):
+        print(i)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
