40e3e1ccba17ce1c74e6deb58e9b6fb420d5d55f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:12 +0000

    vanish turn half shop culture

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
