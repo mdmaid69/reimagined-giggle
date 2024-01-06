a6865947380b42fdc5263b681476b5c235112b6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:46:24 +0000

    getout save spin fairly sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
