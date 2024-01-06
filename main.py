11ad32f9e296f630a0dfa33fedeb6dda04445a2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:43:49 +0000

    couple care wrong protest above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
