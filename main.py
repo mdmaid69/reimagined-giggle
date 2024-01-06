f2b570f47158531ed6a30dc340dd12933e89807f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:52:33 +0000

    along length pattern jump season

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
