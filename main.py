1201f46946f50718fbd56c852e15cf4f811933ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:22 +0000

    fire fetch perhaps accordingto but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
