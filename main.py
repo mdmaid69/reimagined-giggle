ebe574f2a4473fd826f8fcfbdbec5ef86f9778e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:16:23 +0000

    reach it promise offer smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def log_message(message):
+        logging.info(message)
