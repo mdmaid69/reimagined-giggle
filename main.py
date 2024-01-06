dc123b9d41d2110600de13a9bdb594980d3e9493
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:46:17 +0000

    warning relative bank translate Cc

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
