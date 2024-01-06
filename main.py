e47625a8d87ba8035f73d1b4bc8232556dcca35f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:48:31 +0000

    will opportunity wise marry blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
