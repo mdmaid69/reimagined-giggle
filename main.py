2470da48e0976fbe22fdc6bf4455896034d8bb94
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:12:14 +0000

    gap finish miss from mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
