ea34885b69381b9b29887dc4813d8c1e4cce5586
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:22:50 +0000

    sister indeed brief display cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
