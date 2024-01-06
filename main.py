bcfd352a592581cc2cb5a2ff7efacad656f9bab5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:33 +0000

    breath rest front small public

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def reverse_string(s):
-        return s[::-1]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+for i in range(10): print(i)
