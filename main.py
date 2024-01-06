80f6f956556127ea72aba785bebef375db171df0
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:17 +0000

    tax exactly factory base hit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
