d8050fb9614b5a08255b3f6810f88f74ec423c9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:22:10 +0000

    circle true search advise than

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
