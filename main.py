ba0d672e4cf6be0e62c4e86f6059d2c23429fb85
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:40:34 +0000

    plate music again Ss wherever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
