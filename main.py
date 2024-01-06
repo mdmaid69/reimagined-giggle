71bc78c12e65ab0e3947bf8459a44b9fed8c2528
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:54 +0000

    tight concerned appear union professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_words(sentence):
+        return len(sentence.split())
