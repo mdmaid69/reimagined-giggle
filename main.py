827119afc0dc5c88f1954f70eac1a96c58df5047
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:39 +0000

    mean moment surprising inform commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
