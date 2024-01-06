a13a544c46b7215baa5e8ad78be525b3e77cc020
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:45 +0000

    and hole performance result flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
