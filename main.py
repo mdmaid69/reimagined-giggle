3a36147c2416c3ee1574c9b9f549b135c4231ab0
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:33:29 +0000

    upwards bit callat pity sit

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-for i in range(10): print(i)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
