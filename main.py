67eb224a663b7898baaa2eb3fcee94bebac25973
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:32:58 +0000

    mass me worse store smile

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def square_number(x):
-        return x**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
