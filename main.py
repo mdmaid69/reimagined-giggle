2b0e2cdd1e5c3b88880b1d686ee1a4eef5c3cbea
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:05 +0000

    remove arm trade spray develop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
