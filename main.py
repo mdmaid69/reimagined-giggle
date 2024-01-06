bc075048543cb91894e2d97ee28409ae7d34ebb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:02:08 +0000

    size suffer comeup welcome sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
