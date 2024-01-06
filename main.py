1d9c8b3842ad3b901a14dcfeab2fe02981e13dbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:18:08 +0000

    squeeze splash journey second shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
