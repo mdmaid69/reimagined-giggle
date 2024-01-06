f766cefde457f81e73944273028a424222c6cc54
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:53:12 +0000

    compare to knock unit noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
