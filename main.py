0e59ed6db8cab4f6529c993951b9ceb0c774a4e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:27 +0000

    full holiday out fence immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
