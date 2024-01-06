a792ef5f7a12669bb922a16fc0ab2d18e75efeed
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:55 +0000

    seriously fast teach ourselves convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import re
+print(re.match("h.*o", "hello world"))
