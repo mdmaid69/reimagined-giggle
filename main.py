652c6f458fef094762969acdef4f2726c75d2ea3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:03 +0000

    once goon much you favour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def square_number(x):
+        return x**2
