7603d6ca3ab2a79d6cdfb156b02ed0a47155b600
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:09 +0000

    noone wild there cinema cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
