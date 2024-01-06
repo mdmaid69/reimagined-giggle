3355ebdf857dfe1a9529578ef1d216df417bbd85
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:25 +0000

    lose club see would pot

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def convert_to_octal(n):
+        return oct(n)
