21e129c83a04f7325ad48aba30de8561c06ea6ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:44:11 +0000

    usual original cut minute Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import matplotlib.pyplot as plt
 plt.plot([1, 2, 3], [4, 5, 6])
 plt.show()
