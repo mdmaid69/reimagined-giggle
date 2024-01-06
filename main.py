20e8217bab25216dc54855a389bc9e5d73adc51c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:00 +0000

    stop brief criticize service please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def sort_list(lst):
-        return sorted(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_length(array):
+        return len(array)
