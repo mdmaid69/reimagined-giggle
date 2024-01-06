7afc64f74d4868e15a564b4e5024d466664f6cce
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:24 +0000

    snow family damp serious with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import platform
+def get_os_info():
+        return platform.uname()
