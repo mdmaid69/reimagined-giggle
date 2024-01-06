d1530a3d2d54e5bfe3e0fb59d859468d13a4012e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:12 +0000

    part runinto chat boat talk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import os
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
