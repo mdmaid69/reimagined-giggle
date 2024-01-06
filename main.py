0124d6879a14369b420dc7b64208150dfcab22bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:32:45 +0000

    swing meet then catch crash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
