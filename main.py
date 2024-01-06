e38e7bf771ec4ae5070147e4a6e643208aeeff5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:26:32 +0000

    use wave pay tax naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
