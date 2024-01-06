212c637b1793ebbe6e8688fc0cd98e8719e8efae
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:25 +0000

    walk failure chance importance crash

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_str(array):
+        return str(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
