bf2fbc7eabf465b1738603763c90d5b13a108fa5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:31:14 +0000

    great similar dick foreign crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
