ef1f294810b799e5ec07da5e4195b1ea70e427d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:09 +0000

    contribute lend undo traffic keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 def is_even(n):
         return n % 2 == 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
