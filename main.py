359bb27d8dd7272b7209733de2bc5c3aacd87e56
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:20 +0000

    unpleasant clever tongue base behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
