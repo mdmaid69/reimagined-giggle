413553e13755e9d67f514dd55463a73164fc9934
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:33:38 +0000

    rid worse people exaggerate cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
