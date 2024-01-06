77daa2920aa23d232ea246ea924cc01905bf7b32
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:06:12 +0000

    accidentally Cc form uniform form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
