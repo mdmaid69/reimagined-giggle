ea807f1bfd6a90553ec71c40957c73175313ee0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:49 +0000

    replace remark thin receive protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
