861d346c9bfd776355fc380c02b5b136f9329cdf
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:41 +0000

    smell repair sing major door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
