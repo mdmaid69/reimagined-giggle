535af578cbe8b87fb4ada40b4b9d380a769092be
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:51:08 +0000

    share under suggest closely dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_volume(length, width, height):
-        return length * width * height
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
