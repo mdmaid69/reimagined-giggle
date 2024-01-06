d262b8cb2859e31c67d2dba56f3f731bd44dac09
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:17 +0000

    single hear wire emphasis fork

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-print("Hello, world!")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
