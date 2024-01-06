6a24373c0df5931ae330e7a49f3ea5ac93a38793
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:43:02 +0000

    every debt somebody though bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import sys
-def print_python_version():
-        return sys.version
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
