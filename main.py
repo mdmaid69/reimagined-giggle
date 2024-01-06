21ff63f7991ce09d841ea2d7a5e07266abe17d79
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:48:57 +0000

    earn string respect centre plate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
