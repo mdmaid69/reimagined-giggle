34b81d9104b2b81e60c6e130ded871c18563ba84
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:32:39 +0000

    makeinto trip face tape bend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
