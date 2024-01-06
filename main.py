32fa16f8957dcc2de9f1a91eb720f3bb26346177
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:28:15 +0000

    criticize interesting where bunch praise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
