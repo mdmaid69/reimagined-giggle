8dce0807dfae013bfeae254b70076180a3a83972
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:16 +0000

    protest shout central season various

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
