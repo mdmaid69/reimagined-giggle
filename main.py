6499e85dc34ffe2ec9bf2f38f83c801895a1cd3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:43 +0000

    occur 572enter start rough pass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
