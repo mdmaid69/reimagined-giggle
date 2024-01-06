de3a9577de3525a7b5474e3698665949f618427e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:21:35 +0000

    identify wake original one instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
