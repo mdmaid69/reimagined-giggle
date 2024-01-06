47edc45ca5cb1c29d9b339de00628c3931462449
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:58:36 +0000

    read pen gap imagination hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_max(numbers):
+        return max(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
