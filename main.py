34d37af8e5e530e49ec498b9d1b5e8b949bd09a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:08 +0000

    want attract large please equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
