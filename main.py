7dccfa28c5553ea5f6f700f2f53d7595456310b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:40 +0000

    shrink react exhibition official autumn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
