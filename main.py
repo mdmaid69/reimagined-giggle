cee0c6e15cad9e5fcbe787104f1004cc9b769822
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:58:50 +0000

    inquire block content left rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
