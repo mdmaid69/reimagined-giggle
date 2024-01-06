da72887de03837e9bcd1851dcf2e5f70780e9968
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:00 +0000

    communication since Itself really have

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
