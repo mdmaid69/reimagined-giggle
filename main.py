4e59423f3778084706ede2b3f1119d4f40e19183
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:06:05 +0000

    eager speed account terrible into

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+print(os.getcwd())
