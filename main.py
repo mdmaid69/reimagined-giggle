7cd260719fb8f1845abae7e1e64bbdc7dc659b18
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:49 +0000

    future or 573entertainment impressed late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
