b756cddc8a065b638c8d985b0533f9839336b851
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:03 +0000

    protest central basically race problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def sort_list(lst):
+        return sorted(lst)
