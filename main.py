c323c08961823093b4fca0fce4ab4ceda69b69a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:47 +0000

    reach skirt reply curious ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
