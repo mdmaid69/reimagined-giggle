dcf7a1b8b5b9ccee5d89719fe054863f0f534a4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:45:12 +0000

    urgent stranger congratulate drawing favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
