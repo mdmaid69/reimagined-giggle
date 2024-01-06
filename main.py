76e73ffcc24c5b0d54cf2cbd8075e6b287c7c4e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:11:16 +0000

    brother lucky guess remember independent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
