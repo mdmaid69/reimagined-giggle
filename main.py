8e1ad44522ac6d13343f4371eeed332aace7b149
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:05:47 +0000

    thin completely advantage wander recover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
