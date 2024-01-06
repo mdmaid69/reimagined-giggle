4544c361a81775030957276ad0dc37203a1f5446
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:21 +0000

    attach just skirt slight alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_array_itemsize(array):
+        return array.itemsize
