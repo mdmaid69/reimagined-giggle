cb909939f1922f438c85949342c8d9e866b17630
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:49:17 +0000

    murder behave eye unfair group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
