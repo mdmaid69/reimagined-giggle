527526bfa8ac3379ac5622f46421b13058625730
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:53:01 +0000

    measure nigga admit bus discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
