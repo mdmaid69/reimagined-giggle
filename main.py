b96d344739d8d3117aaa3b8ea4ef12d6507488f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:15:29 +0000

    hole public pretty show smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
