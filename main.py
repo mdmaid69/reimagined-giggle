9c617d5ff7b26545e6530997ec27aac9c7f362ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:27 +0000

    respect use music bring imagination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_length(array):
-        return len(array)
+import getpass
+def get_username():
+        return getpass.getuser()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
