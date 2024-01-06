a97849f8c4202192fa547162d96e0c36354e3d02
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:29:56 +0000

    wish bet Oo class part

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
