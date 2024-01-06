fc860f7451d4b48676d51cfe11be9bd16cad0b69
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:48:55 +0000

    recognize board surprised work hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
