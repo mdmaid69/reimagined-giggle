2d05a0c90aa767c8bce04710db17c615532ffde6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:14 +0000

    environment transparent better stretch block

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
