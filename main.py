0de993d7c46b8c973599dec3de9108f4790c486d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:58:15 +0000

    earth swimming correct corner interesting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def square_number(x):
-        return x**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
