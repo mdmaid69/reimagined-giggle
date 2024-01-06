858af086523b2dd6bf2a89395ab664de67ad6bcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:06:37 +0000

    whistle back joke fire breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(a, b):
+        return a + b
