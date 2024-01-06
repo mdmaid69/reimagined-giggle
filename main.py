75ac745466ec5aa88c41899da4cbc5884a2bf0ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:53 +0000

    explanation question difference crash direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
