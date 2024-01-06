53729002aa6e2e70b0dbb97edeacd7bc64132ba5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:48:15 +0000

    maybe absolutely breakout majority violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
