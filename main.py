1b48b0117d50020365507386350492bdc6b20145
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:08 +0000

    black talk mouth squeeze winter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
