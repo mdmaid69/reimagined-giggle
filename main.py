4003ad90027faa6922d22181bd65b62b03fbb109
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:22:31 +0000

    callup workout thing price smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import getpass
-def get_username():
-        return getpass.getuser()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
