b598d7c3fca497742dee5d55b09eda5ba928fc08
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:00 +0000

    admire advice fight battery to

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
