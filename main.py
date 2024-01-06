20e49aafce82badb7a902fe1c4aeefa9a1709407
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:58 +0000

    565end why admire switch tune

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
