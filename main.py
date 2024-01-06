c02094f5362defe3e1adbff45eee260fe0008bf2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:10:49 +0000

    connect almost sky money goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
