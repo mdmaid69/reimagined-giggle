1898f9369bb530b6ef4eb40ffcb87cd8e72490d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:04 +0000

    report comeoff tape impatient exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
