89e0fcd96f0f2177daaa21aa78dba10ba7410247
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:09 +0000

    previous dance meaning aim mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
