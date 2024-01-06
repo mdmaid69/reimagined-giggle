eee8aee5c5376f0ed4a8a36b2ee741e6271cf8ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:34 +0000

    combine lookafter smile wide damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
