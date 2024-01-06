e906c71d9d9d808082431f9586ac35ae2a636912
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:56:25 +0000

    class confuse computer after fun

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
