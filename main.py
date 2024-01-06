64e092bc7e38e079b285ca82fd0c2f2212260b1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:25:11 +0000

    prize remove trick tooth Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_error_function(x):
-        return math.erf(x)
