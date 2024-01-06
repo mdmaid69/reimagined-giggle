148620cbc9f3a5216a8c5c06afae9ae30d152673
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:16 +0000

    coffee safety translate cost up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import getpass
+def get_username():
+        return getpass.getuser()
