6e631f0a40659dec29dd4771dc834532cd768525
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:53:03 +0000

    agreement string thorough large fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
