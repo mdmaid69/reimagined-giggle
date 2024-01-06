5f4d45648b6d606bdd87fb49414bb9c707603eb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:09:49 +0000

    contrast hurt accordingto trade accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
