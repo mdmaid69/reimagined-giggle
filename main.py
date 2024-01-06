557dbfa86dc877ad948aa1a46e9fbfa9fd336e40
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:28 +0000

    pure always claim crowded go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
+def subtract_numbers(x, y):
         return x - y
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
