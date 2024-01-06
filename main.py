fbb75507cf6055c52c25202e9e5f5218521e4370
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:03 +0000

    follow lost inside study sea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
