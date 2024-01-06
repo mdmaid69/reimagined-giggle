b1488786a60fc4cc74debc25e750274b1e49781c
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:37:36 +0000

    design money forget ability often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def cube_number(x):
+        return x**3
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
