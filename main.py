06d7f02a68d79505ad7619447aea0a4c31038e94
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:01:46 +0000

    towards chief information temper Uu

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
