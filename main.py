0d3dbd55ef449b17d39ed86d64607df5c4f5e492
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:02 +0000

    argue shall neck pass keepout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
