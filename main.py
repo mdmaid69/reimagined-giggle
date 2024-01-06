c330f4f3af1a7f3ce3788cb10e00d83de9ad4630
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:35 +0000

    deal pipe turnoff blue doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
