ba12fccd856ec5e468c47aaed6d191af06a1e3a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:28:13 +0000

    sit picture sink turnoff conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
