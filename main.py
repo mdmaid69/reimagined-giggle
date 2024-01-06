2335d20241cc07b3636a7efc3eb3903d88ae7c3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:37 +0000

    suppose dance crash plant react

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def greet(name):
+        print(f"Hello, {name}!")
