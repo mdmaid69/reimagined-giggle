83c384445a30808981a4e4720ec69ab07be37076
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:54 +0000

    full method damp object mistake

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
