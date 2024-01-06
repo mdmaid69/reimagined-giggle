e8fe3100a7eed1c2da97bbb77ae2b44a18d6e892
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:43 +0000

    child escape feeling whistle myself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
