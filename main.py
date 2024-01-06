9c518e52ac52babdabae73cab9df8312485d273a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:00 +0000

    especially occur satisfactory mad mysterious

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+print(sum(range(10)))
+  def reverse_list(lst):
+        return lst[::-1]
