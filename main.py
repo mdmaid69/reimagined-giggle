a5088f62f3beaad12838d7726ef8958e8e8244ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:00:55 +0000

    minority speech similar demand shop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
