5a3a4665d02483ad95e5486607b43b00a49a6486
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:36:24 +0000

    skill before remark telephone brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
