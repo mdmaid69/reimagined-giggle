f8fc4342de7e4ea023366065d2ef26d5370627d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:23:24 +0000

    group wild this zero embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
