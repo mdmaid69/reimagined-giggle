a8a14404c2bd55c42eb99f1733dd062d79b0203a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:06:17 +0000

    how tidy television shadow damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_float(array):
+        return float(array[0])
