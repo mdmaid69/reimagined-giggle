8be12c9178d8e80994f3ddd2af814a17712175e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:44 +0000

    calloff week situation floor mail

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
