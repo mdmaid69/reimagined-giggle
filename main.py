9840da53f0445be830793cfd1ac24d446b061f60
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:40 +0000

    age track concerned unusual advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
