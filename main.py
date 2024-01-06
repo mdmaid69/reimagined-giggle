92110eaa4deee417c2bee9455de3e085a1c78ebb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:03:09 +0000

    last dance eye him opinion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
