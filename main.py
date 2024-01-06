6db9b3309b0ef07bf8d86a8892695ef18f816566
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:21:16 +0000

    name freedom drink feed praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
