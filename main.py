0382ee9c0ffe62046864ee8567c24b2a00f90760
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:35 +0000

    production through than rain teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
