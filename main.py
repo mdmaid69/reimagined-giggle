ea74323a1d890f282613ccd4ef12e033738a6d41
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:14:07 +0000

    improvement exam attract protest crisis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
