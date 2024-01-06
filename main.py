8ad88d135ae20c09393295024c8bf00672f46d8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:30:16 +0000

    neck pity replace sign hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_string(s):
+        return s[::-1]
