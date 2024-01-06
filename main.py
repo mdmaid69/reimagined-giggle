de0a735a58070e70c3fbf86f47e5bdd244cb11b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:46 +0000

    buy instrument worth would base

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import getpass
+def get_username():
+        return getpass.getuser()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
