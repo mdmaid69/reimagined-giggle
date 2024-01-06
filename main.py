74c4a0004a7f31a0a07d90e085e036dd957c5863
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:04 +0000

    react total rush crowd course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def append_to_array(array, item):
+        array.append(item)
