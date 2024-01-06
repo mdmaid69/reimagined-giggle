2551e57e2bf954e0f1496fb92291e2c38e27e136
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:44:44 +0000

    however listenfor command later shape

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
