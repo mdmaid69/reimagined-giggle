b16a8cec8cadcf49917f9e6e6ca7383f97035e28
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:32 +0000

    rush job value worse badly

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
