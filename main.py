e82ccf71dd74b98d0df4264c1d57e53fbbe01a50
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:43 +0000

    stranger 566end miss mention perfect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
