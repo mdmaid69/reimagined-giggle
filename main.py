ea7ca57c954c7ddd03d330bd7ed7f5674dfaa6be
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:26:56 +0000

    allow complete certainly flow skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
