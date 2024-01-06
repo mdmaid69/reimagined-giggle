f3d6188612a9a3c737907a8fa708d32c0c0a3262
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:32:11 +0000

    score scene scale judge 564encourage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
