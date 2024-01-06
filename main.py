49426d88c3683155d74e7c310045b659b6b22a0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:27 +0000

    shout fear ours yesterday general

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
