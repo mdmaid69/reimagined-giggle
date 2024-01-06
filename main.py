0af76724eb9bed1c02736c3a57ef8bc91c50ded3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:43:51 +0000

    continue them dig exercise aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
