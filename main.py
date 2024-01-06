0e76b6c3d85ca955537112a38fe7f671b7db704b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:52 +0000

    gift exist read another myself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-for i in range(5):
-        print(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
