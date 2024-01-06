ddd16cf46e739e1f12d018f7cca11ef607d21882
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:46 +0000

    only aim along sale size

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_factorial(n):
+        return math.factorial(n)
