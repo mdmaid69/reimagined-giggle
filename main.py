2a8e9de1be5aaf5fcb2fc79a107cda5031f1b638
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:55 +0000

    myself produce fix blue see

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
 import array
-def clear_array(array):
-        array *= 0
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
