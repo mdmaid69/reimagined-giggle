9f170bd37aad662f879c8bed3e0983677b5bf201
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:02 +0000

    hang push priority job above

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
