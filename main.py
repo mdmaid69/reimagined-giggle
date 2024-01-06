8bd4f4316ef6a13ff3090e503a14e18197683998
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:38 +0000

    expect no authority failure nice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
