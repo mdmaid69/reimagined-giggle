cd3c969612750c8773ed39dd935c3fa847188d5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:35:36 +0000

    trap show choose rate jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
