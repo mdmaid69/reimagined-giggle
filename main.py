97394e54bb33c2fa97634bcd0fd9d7145117c27b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:18 +0000

    survive all advice realize work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
