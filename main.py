e5b3a1476f6abab9239f646da28abcc92ac1e165
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:53:24 +0000

    swimming right cook pretty proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
