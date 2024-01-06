c8c5a7a0b3b812feda37fe2cccc8a1d2afb1fe81
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:09 +0000

    organize 569enjoy stamp communication thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
