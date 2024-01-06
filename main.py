fa9e8076f11e8094172621ef352919edf0d8f8cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:40 +0000

    warning tune design freedom little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
