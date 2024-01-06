a9a0009d9051f2271b33cadf2d923204b951b8c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:56 +0000

    comfort high mess trap invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def is_even(n):
+        return n % 2 == 0
