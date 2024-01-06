4c9455559ffbf0c8476ee75dda9145bd527f6555
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:16 +0000

    process begin respect fierce peaceful

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
