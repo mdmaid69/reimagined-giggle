a6e25c1fda3486b0129c31aa34d7d252a548b828
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:49 +0000

    correct apparently beside mistake boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
