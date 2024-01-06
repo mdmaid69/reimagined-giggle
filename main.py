9f0aaa387c7ba2c4ef6b4b8d89c3d50cb79e4537
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:01 +0000

    contents least genuine sensitive sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
