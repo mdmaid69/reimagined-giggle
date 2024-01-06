585f129747747ef918125dcb6e0a7afb2060db09
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:20:16 +0000

    their centimetre entrance bringup minute

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
+import numpy as np
+print(np.array([1, 2, 3]))
