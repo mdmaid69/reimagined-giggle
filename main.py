29c4e61b085c37ff21aa310c0e6571be072ca3f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:40:02 +0000

    nose paper limit century square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
