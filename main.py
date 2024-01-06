69b1d56f0ecaff677c892ec582d9ebc6de1b64c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:47:52 +0000

    child holdon thirsty development feel

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def reverse_list(lst):
+        return lst[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
