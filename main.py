bffb6ac0f0ad5532e5efdd7b22c9400eaf52a257
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:02:27 +0000

    under spin partner welcome hate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
