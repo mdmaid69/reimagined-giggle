836e68f3b30ab107f4160b3b4944f358225f86cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:53 +0000

    for tour collection whether delicate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
