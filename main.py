ba959ac8cd736f9c4fda3bab54d333c58fe709dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:15:25 +0000

    distance slide confidence can temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
