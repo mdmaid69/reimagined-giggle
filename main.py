6b7c4d1814c64c97885163246dced68034f664d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:49 +0000

    lookup shape slide slip kind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_max(lst):
-        return max(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_cosine(x):
+        return math.cos(x)
