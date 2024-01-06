e9cfe5b81369ddb0a69c63800079fd9f1b58be5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:01 +0000

    last design complicated start this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def subtract_numbers(x, y):
+        return x - y
+def multiply_numbers(x, y):
+        return x * y
