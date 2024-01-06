1491240d7944d9fb069b395beb9fc0d60ee2e27f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:08:51 +0000

    survive under big thirsty certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
