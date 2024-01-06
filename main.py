8163bc04eee897875b5e09d5bc15fafea0ca1af3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:57 +0000

    test out begin Zz major

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
