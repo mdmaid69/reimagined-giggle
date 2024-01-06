103140a10f01849ee52fe2e494dd205a33b3a120
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:31:53 +0000

    idea for strength speech male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
