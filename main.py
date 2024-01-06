a861912937501e9e70e2f5d4b6146a5dc4bc39a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:15 +0000

    gift hers sorry inform physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
