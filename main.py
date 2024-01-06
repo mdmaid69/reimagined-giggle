19cfbe48e52de022a6731535695d4f915ae56f2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:52 +0000

    life mark recent naturally dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
