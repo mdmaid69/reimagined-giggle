bf216cf5b875531dd197c058fd4f84805b6ec971
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:05:55 +0000

    win scared pronunciation computer tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
