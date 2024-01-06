7c4f9133a6247f6099f1a9be81c06314dfc01892
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:14:47 +0000

    weather squash scrap walk so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
