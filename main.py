5d573c76b1afde4da8eb457b5a2e28713f23aea4
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:04 +0000

    hardly Itself picture central workon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
