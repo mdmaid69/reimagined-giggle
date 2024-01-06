d55efde4b6972a6eea11d8bd40ea68293fdd5490
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:48:45 +0000

    form not official goout study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
