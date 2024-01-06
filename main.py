aed2b39bcb9609e7846a838df57c6d9db28c4941
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:56 +0000

    disapprove about flavour picture fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
