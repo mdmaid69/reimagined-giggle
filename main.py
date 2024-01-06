2537f530d1ee0afd8f52efb4e5d59e59fb4f5120
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:56:35 +0000

    tongue contrast loan reserve straight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def convert_to_octal(n):
+        return oct(n)
