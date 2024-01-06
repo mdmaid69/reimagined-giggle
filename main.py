3bc8e82a27be70a26acdeff19fb3e7364a291cb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:14:11 +0000

    against programme throw wake idea

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
