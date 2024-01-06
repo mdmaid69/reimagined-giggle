96b68ec7e2a66e523eee0791ff60966494225132
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:31:08 +0000

    wall sweet pool already wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def get_current_time():
+        return time.time()
