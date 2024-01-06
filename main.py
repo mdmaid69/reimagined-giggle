0399c7ef23428e2c9b288991d1b824de0aa651f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:14:45 +0000

    off bringforward behaviour takeoff broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
