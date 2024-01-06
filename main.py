50ac6738987999b4df2cf290d546615584f2acee
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:37:12 +0000

    task acceptable phone notice through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def convert_array_to_list(array):
+        return array.tolist()
