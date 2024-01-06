b6753226062e6082429898d179cb26252ceeeecb
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:16 +0000

    chief practise petrol mention probably

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def reverse_list(lst):
+        return lst[::-1]
