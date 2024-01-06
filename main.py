355cc29f7d15e7f276da896c79d9bfb3f8f5abd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:32:04 +0000

    dream detail row no woman

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
