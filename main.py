91341d1130053e58f32140839ded4a1edc898f27
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:36:17 +0000

    feed combination protect value support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
