4dafd33d9c309204e477eae752aa100b1b4a3a46
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:19 +0000

    herself come exit breath female

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def multiply_numbers(x, y):
+        return x * y
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
