14541cf703cbcaa9b29a11a556039e7856c11a74
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:02 +0000

    bit stupid through service sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def count_elements(lst):
+        return len(lst)
