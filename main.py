c8a867e4faeab98f6852648d86fee47b8b727eda
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:53:38 +0000

    interesting speech face trip unhappy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
