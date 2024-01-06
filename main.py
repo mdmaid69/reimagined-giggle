426c3ec238c5f70da1a3c9c97fedb41181712b46
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:31:02 +0000

    press sport certain identify makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
