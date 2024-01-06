9ef11f44dd48a05ea731265b69d2feeb7bf37d32
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:48 +0000

    accordingto broad emotion ride violent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
