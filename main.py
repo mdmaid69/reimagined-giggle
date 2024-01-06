e75e54663cda80dd13597afad6aa56f4cc0a237e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:03 +0000

    suffer keepoff ball threaten deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
