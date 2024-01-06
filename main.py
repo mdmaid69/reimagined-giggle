ff89c5e31a4c3615bf8e874900ab8980be0ce10c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:07 +0000

    any note freedom combination division

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
