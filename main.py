b2f9dfd65bde0b294c1126a79ef89bd51d4ff6ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:59:50 +0000

    makeup dead journey across terrible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
