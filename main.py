eb66bd387da7b89d6187987a656ac1c05a9f2f2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:11:20 +0000

    ever distance health colour bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-text = "Hello, world!"
-print("Characters:", len(text))
+def convert_array_to_string(array):
+        return array.tostring()
