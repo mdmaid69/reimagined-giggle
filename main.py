385b1da6abfe010ceff8857c210b18f41c1a3db3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:58:32 +0000

    back habit collect criticism fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
