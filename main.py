971dc34252d663f112c011cec91b5144c6b3beda
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:32:42 +0000

    sport shadow wide crisis sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def add_numbers(a, b):
-        return a + b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
