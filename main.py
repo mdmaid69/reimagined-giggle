5666425df6ecd9b5e1eeaac1e4c9a44749df6c7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:30 +0000

    embarrassing Qq loud attention dream

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
