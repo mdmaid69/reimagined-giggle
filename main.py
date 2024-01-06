8f7d4f289b7409af971dc1654b542e56a786c76b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:52 +0000

    cope sea no brush pump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_density(mass, volume):
-        return mass / volume
+name = "Python"
+print("Hello,", name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
