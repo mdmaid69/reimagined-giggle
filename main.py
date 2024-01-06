87562dca03d4934cfad4be5c238f2547b81603af
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:31:24 +0000

    there spot burst taste help

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
