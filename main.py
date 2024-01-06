bf7d6150d1c5a4db91443f6a0ba4544632437027
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:10 +0000

    anxious operation turnoff aim often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
