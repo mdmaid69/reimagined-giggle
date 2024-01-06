e3bfe04db8ab2de1660dfe1003a4acd3614fed90
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:25:30 +0000

    Ww there spot combination unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def sort_list(lst):
+        return sorted(lst)
