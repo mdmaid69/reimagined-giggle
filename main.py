a26b4f3eb8a1dee1a3fa85c9d2eb4dd69a4a26a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:42:34 +0000

    trial say basis form mess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
