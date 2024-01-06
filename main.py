39c0ac975a01f354542439ff673cfa8238481f14
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:39:00 +0000

    rubbish if slowly ride command

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
