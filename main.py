900bdd0957ffbfce84cab1292b6ff2f9651bd2cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:16:55 +0000

    nor Uu side tip room

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
