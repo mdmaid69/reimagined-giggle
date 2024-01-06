40b328c99c81fd8e919857262560f24e95270dca
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:05 +0000

    supply book callup hurt mail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
