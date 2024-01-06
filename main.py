12a187f612db5d370aee2980665510dbec6966fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:13 +0000

    embarrassing Cc trap floor away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
