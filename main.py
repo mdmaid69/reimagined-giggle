724e51bd6c19a4fb23b3080bf02df173a907c000
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:38 +0000

    healthy upwards unkind proud free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
