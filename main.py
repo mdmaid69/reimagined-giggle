2648fa3334eee1677d35ce7ae73a153fadc3a6dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:42:08 +0000

    memory best means walk leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
