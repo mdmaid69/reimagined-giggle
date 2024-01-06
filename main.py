cf7db5109481e91426d729c85d3c69aea39a9935
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:22:36 +0000

    regular become Gg point hope

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+print(re.match("h.*o", "hello world"))
