9e93ed706b639e77187f157d24dad595aaf78afe
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:00 +0000

    usually history before false foreign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
