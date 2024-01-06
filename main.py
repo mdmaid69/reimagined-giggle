cc4b0e03ada878f03e1200ef8f8cf4f389dce796
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:37 +0000

    content share interest across community

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
