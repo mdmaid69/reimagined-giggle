e98018bff38e7c879a8679d4be0d860beffc2ee8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:48 +0000

    environment mark getaway swimming lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def greet(name):
-        print(f"Hello, {name}!")
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
