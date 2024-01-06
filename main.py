8a45ed1a79c35d9211bea687978f5646368a7c59
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:30:56 +0000

    alone doubt blame angry handle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
