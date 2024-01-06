e6b8808d384eb26e9ee2a57e313ce2b289e6b285
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:41 +0000

    fun thinkthrough sense safety Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
