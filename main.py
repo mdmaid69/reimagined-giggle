0bfd0e74e88881eac75be95b484136e4775cc40a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:51:35 +0000

    gentle thinkthrough mother opposite shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def reverse_array(array):
+        array.reverse()
