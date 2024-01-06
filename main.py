80df844d8f5b62f077d888d8fb5c7a78171ad04d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:20 +0000

    risk doctor taste opposite like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
