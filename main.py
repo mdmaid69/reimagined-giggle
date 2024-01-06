20a788d0b1c43f356ab82e5b1f29f1d40dd2ca89
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:07:59 +0000

    town excuse rule thing her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_tuple(array):
+        return tuple(array)
