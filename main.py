eef8b5e2c4cb422e96c34b1e6e7ace80eb4c25d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:42:44 +0000

    serve way water it hit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
