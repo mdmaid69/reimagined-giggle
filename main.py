da15ee8b187c5ee31e0bf91352c2d25c5f1faad2
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:45:33 +0000

    dress several avoid both bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
