1910136e4fe4168fc3d22b9655770caecdbf725e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:46:39 +0000

    classroom ask other except freeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
