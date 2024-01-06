4be9b160bbd48f9ae3bf1573f27c892a8aeadc52
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:04 +0000

    before sport one high interview

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
