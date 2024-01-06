41685baae0a2a432aec0ea7c751fe5dff30a9b66
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:03:17 +0000

    beginning space lookup variety year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import time
+def get_current_time():
+        return time.time()
