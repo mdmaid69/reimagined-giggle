0b4bd638b82f67dccdc3635d8751780084ddcfeb
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:49 +0000

    entry form might upset consult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def change_working_directory(path):
+        os.chdir(path)
