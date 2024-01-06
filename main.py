3e8577bda541ccad764a7864e41df2d309d106bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:11 +0000

    thick bend definite shout usual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
