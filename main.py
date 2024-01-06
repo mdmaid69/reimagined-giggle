3f61e08a86d508d24c16aa8d696a953b2ed93b1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:18 +0000

    peaceful grade moment separate noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
