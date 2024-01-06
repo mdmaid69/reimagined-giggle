2347ec8356beda964d5dd02e5610af5441ce6bc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:04:12 +0000

    drive solve silent heat slightly

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
