e9e63d2367d04c5ae7cc556631c0d0ccf43fa4de
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:02 +0000

    turndown hair weigh early meeting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
