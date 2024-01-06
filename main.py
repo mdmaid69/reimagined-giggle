8bed7b87e48231827ee3e3c4abc8002b3ad4645b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:11:02 +0000

    repair national hollow punish lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_power(work, time):
-        return work / time
+import os
+print(os.getcwd())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
