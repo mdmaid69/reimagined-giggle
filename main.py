8cbd705c95f72a7690e554cfb7d379d1ca53abf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:01 +0000

    interview chin coat no convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_density(mass, volume):
+        return mass / volume
