3655d0a8df8de822d421a198be330d10bdef817a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:23 +0000

    gradually last alive seriously keepaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
