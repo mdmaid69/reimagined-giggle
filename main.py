eddaeda5043c33745e8a9f7e1e6018e36c83bf01
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:26 +0000

    reason and correct accidentally service

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        print(sys.version)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
