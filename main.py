6eff424ccc7eb8b28b59c6a84b8f208453add271
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:07 +0000

    its situation practise while sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
