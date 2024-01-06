4e0f580e049f790de577aa28df42fe87c45b4ea6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:24:43 +0000

    phone gooff learn guess hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import logging
-def log_message(message):
-        logging.info(message)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
