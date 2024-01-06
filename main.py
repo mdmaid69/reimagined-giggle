f21d5de549814c58f8e9f090678bedd6223f8a54
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:09:08 +0000

    polite air worried near thought

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
