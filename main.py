fdda1e2c95c526db3dae15b6db6c32714224af0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:55:55 +0000

    opposite harm development related Vv

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
