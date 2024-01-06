866590dfd8fe3c91568f0c25d343f0fc087e23e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:54:04 +0000

    ring lost after turn expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
