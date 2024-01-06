c3a9f23db63be8713aa028a85ce6c087a569c29f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:02:33 +0000

    hit bad address advise often

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
