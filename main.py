69cb2d443440872e4eef36d832207516421fa28a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:00 +0000

    accommodation straight later unfair rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import platform
-def get_os_info():
-        return platform.uname()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
