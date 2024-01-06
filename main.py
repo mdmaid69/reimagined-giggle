ff1b21d7b4a197110d04caf12519374efd9f1cec
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:37 +0000

    estimate accidentally hand mistake repeat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
