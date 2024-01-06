90d63662ff87f989c72fa9dcda2db14bb29ad600
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:15 +0000

    boil giveaway small tragedy rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
