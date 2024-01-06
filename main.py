83fc552a82dc8456e5a37d374a42df182bbf6c1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:47:18 +0000

    aware family between false priority

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import random
+print(random.randint(0, 100))
