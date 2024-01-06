eceed9d980fd1b8cb57445f57fab94e2007b2ff9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:28:36 +0000

    broken yourself rubbish tone entry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-print(os.getcwd())
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
