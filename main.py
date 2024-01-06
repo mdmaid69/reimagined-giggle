322bfd9a9cb117754944d8d66244b97e8589e5c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:24:59 +0000

    guess male poor lookup damage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def count_elements(lst):
-        return len(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
