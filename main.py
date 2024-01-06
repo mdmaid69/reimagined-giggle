693a26cf9c61ec3d16f24cdfda7549ec38f4de1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:36 +0000

    brief gift no gas spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
