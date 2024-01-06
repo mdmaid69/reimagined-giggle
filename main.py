dc22e77fd4050aed2af35e13b8d75607045caa87
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:22 +0000

    train yours advanced black fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
