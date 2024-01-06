a99df11666535a1e44c2705670ea3af3dad38239
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:04:45 +0000

    insist difficulty improvement achieve popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
