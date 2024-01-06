dc03921d4d1a937f35a1a912b939b69508762c97
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:21:07 +0000

    Mm string also central attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
