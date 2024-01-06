c4f79322724d389883ed903d53109e0618b081ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:57:47 +0000

    single hard hurt bound emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
