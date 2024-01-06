acee7deb2f7a12c4fb70d5f720fa56578dbb2418
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:27 +0000

    Ll within include speak rub

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_sine(x):
+        return math.sin(x)
