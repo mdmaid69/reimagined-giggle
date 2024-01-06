5384894345df3270c6147a7c420487636f160acc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:36 +0000

    both first create pain juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
