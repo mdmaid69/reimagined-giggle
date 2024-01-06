67a0dc77b8571cf1b1ae099b2ba4d09a3d51ed87
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:43:54 +0000

    sting principle comeback wash exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
