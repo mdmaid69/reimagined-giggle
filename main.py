7676c84720b7a82ae2036ff3f2a4d7667558b831
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:52:42 +0000

    act fight drink brush wish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import re
 def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
