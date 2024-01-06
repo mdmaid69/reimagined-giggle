771ce238266a398934039c9ec9d7047a398b1d4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:49 +0000

    under slice wet sometimes performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def add_numbers(x, y):
+        return x + y
