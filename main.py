0db3dfd841c111ea59bac76c40f2684d484c3ec9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:06:28 +0000

    congratulate Gg treat love row

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import time
+print(time.time())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
