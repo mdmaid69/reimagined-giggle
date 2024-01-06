d154c3858e8fb0652d3eb578b2769e1d62191ea5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:51:13 +0000

    method takeover protest cost choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
