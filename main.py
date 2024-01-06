e8980aafbb3b68d0a78779e6afe5f613068d5c56
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:53:17 +0000

    slip smile smoke declaration listen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def multiply_numbers(x, y):
+        return x * y
