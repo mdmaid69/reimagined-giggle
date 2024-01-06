6972706a11df17c84e7e0daee08150ae50e315a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:21:28 +0000

    shut Vv knowledge help size

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
