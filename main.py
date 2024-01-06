1afd96aee4a7e4a7fec72d63e600202d8da156ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:35:04 +0000

    cough set shadow great scream

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+name = "Python"
+print("Hello,", name)
