61b75a7be87766579e57c612dfc3bfc907ca1987
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:47:41 +0000

    firm talk godown Ff disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
