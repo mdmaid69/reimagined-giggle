8c3a4ec2735dc4cb17a83aa5f6d4c940fff0de9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:26:09 +0000

    off black callup union example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
