7ec3aaa63f1bd59d1bd2f9579aad76fa2f895a7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:21:16 +0000

    sad child finish cum satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def square_number(x):
+        return x**2
