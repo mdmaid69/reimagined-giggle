7a83815fa1783c4cbf37ec8ae07f973f72d61550
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:59:24 +0000

    say bank evening work delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
