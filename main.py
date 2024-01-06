27453f629bc6109c1a33f36d8bf7a4250c85e358
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:37 +0000

    bet child ill sense instant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
