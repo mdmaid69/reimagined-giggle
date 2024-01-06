e351669fb4e14e992c834daa36e0e47ce36a662b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:47:11 +0000

    slight occur underneath risk badly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
