12e38704ac37fe37de45d6eab232104d5c7f8eea
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:51 +0000

    friendly turn place 179bite drop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
