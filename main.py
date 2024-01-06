4d682d386d2a34431f55ed4f9965eeebd29b87bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:42:50 +0000

    over answer frequent bet relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sys
+def exit_program():
+        sys.exit()
