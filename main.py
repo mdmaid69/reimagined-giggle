5c42f697c067cb71ad6d6975386f12e41531740b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:40 +0000

    jump surface attract space real

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def is_palindrome(s):
+        return s == s[::-1]
