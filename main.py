6da7458c30054f8e146e1bb3063bced731fac18a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:22 +0000

    urgent present similar passage thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
