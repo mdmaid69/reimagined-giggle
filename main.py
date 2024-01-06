63e7504471f4175b8cd66b9c2fe7ea7956e430f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:50 +0000

    increase top female survive crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
