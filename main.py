57fe3c2259add10de391ec827d94c29ba8d1e4ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:27 +0000

    small class invite occur plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
