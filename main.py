6fc5b901c15e5b0a0a6c21e3288c7c1758e32041
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:25 +0000

    blue belief along will turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
