acd5612c58c91f06ab2809a16cfb6cd499e18f30
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:22 +0000

    recent behave dig play exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
