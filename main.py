2e275d1e130447c08aafc25031e9cefd5e8e80df
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:38 +0000

    sort various join store play

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
