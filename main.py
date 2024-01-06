d8ef78fb2d757479f555540ed0a324fbdf03539e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:53:17 +0000

    burst sweep crash concern callup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
