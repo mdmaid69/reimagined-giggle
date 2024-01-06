126b75f05ee6e35456a9ddceace8e1071e4b1afc
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:19 +0000

    black phone regularly clear possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
