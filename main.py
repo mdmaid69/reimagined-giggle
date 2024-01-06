78118e1144e1797de6cf53f08574981464963f69
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:52:41 +0000

    wake closely round moment concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
