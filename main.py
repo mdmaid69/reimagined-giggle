23e872c21f068327b493e78df44728901a82e5fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:03:18 +0000

    stand bound appear under regional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
