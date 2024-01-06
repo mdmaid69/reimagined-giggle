7eeb62edbd68fcc0b4853aea9497325cd95909a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:13 +0000

    trap throughout pipe worth detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def reverse_list(lst):
-        return lst[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import time
+def get_current_time():
+        return time.time()
