bb6df95949df71e7248510cb3595f467d8b73b68
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:07 +0000

    profit worry divide out always

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def remove_duplicates(lst):
+        return list(set(lst))
+from collections import Counter
+print(Counter("hello world"))
