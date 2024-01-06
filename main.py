36cd460d3e15526757006a207818943e81530915
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:20 +0000

    impressive gather impossible well doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
