dfbdc056421abeb5bdad0d66045bdba44c8ec65a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:09 +0000

    twist one suckme ago cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
