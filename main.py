c885817fbbbf44b79624706e48a9947bf0484e18
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:29 +0000

    congratulate reasonable point convince activity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
