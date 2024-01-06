38eb535f18c42bc11fb39c966a612d33cecec54c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:55 +0000

    satisfied beside shadow serious relief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+def print_python_version():
+        return sys.version
