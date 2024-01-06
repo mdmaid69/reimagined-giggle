1141c434d4c1520148920aca484d51776a75a2c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:09 +0000

    bus stir wander protest hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def reverse_list(lst):
+        return lst[::-1]
