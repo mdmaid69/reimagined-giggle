d80210f3d76f227251269cc2f3d423fb47206566
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:33:13 +0000

    trip suggestion usually squash company

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def multiply_numbers(x, y):
+        return x * y
