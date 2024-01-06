73b9c098ab945d5e8a4c97478cd63781aecbbbc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:11 +0000

    petrol rush burn teach settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_directory_name(path):
+        return os.path.dirname(path)
