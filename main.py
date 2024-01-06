fbc9f53a21bbf8be02bd26dcead3018bcacc46d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:59 +0000

    argue public skirt prepare makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+def get_current_time():
+        return time.ctime()
