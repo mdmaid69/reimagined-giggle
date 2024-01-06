e6fe158d56012e20e89ab673dd86ad090ee0d00a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:16:30 +0000

    land shut here emphasize OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
