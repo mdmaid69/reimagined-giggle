0d3925474eb0b1d01bfcf22c1693ba4a57c910e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:28:23 +0000

    hurt main concentrate gooff relation

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import getpass
+def get_username():
+        return getpass.getuser()
