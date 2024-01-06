385eeb9343533c0f0883b2e06dea88ac4333173c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:48:11 +0000

    read hide frame uncomfortable cheat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
