8752771cccd7e83b03da111faab47815e51eea7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:16:56 +0000

    loose necessary entry gentle stare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
