2d9fe71eef93dd968c67010a79564398a70d538c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:10 +0000

    goup as probably too anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_min(numbers):
-        return min(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
