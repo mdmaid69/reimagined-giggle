1fbc326c7afa79bc518872b9a43950ef65f16202
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:43:35 +0000

    one hit scale general tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
