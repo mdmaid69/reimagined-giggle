9945fbea2d3ddd378813f38f6dc388b8c1d60aca
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:27 +0000

    funny sick rub unlucky quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def reverse_array(array):
-        array.reverse()
+def get_string_from_array(array):
+        return array.tobytes()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
