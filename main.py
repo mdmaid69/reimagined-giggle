3796a773170d2540e3f9d9176d167a28312e06fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:49:01 +0000

    build constantly tax row noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import time
+print(time.time())
