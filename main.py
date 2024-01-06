6ac96b23180b66a8a172a2ca683900773fcb1e6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:05:39 +0000

    takeback thank bored dish birth

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
