160d624ccaa47a7f19e12f3b6722206a181e704e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:54 +0000

    vanish fill unusual length future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
