4c9ab22e310db75ace15455e2f60c784bc700e0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:26:43 +0000

    cool scream handle boring underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
