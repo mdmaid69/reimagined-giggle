01c930d8b6ff89e861153b2458d44a1d2fd11283
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:34 +0000

    below base share Rr trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
