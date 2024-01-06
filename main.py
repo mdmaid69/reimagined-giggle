63bf757fa7f5ab8d3a1dc265f5961ec082a8faa0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:32 +0000

    round tongue treat brown mixture

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_str(array):
+        return str(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
