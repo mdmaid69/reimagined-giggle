58793025d48c4978c5704518843cdbba54c575c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:21:25 +0000

    shrink adjective close cum figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
