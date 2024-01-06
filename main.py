7371b35929f3b874d7c565eabe386dd942cd4225
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:08:12 +0000

    reply forgive congratulate embarrassed slightly

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print([x**2 for x in range(10)])
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
