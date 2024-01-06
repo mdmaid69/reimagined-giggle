f81f88580a39b559c8c74191459b36267fe0356d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:12 +0000

    exam occasional slip workout Ss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
