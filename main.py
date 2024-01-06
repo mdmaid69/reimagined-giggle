a96837dbc29c14aec956b9de1ae3ab76480406b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:17:25 +0000

    plenty part everyone lead show

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
