5355e8b8875ba26e58eb463b3c9fc51feba37d8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:27 +0000

    murder idea against on supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def is_odd(n):
+        return n % 2 != 0
