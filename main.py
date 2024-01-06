0ad382e234a29fa9570d0abad9eee127cf104293
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:38:10 +0000

    completely mystery Mm ride regret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
