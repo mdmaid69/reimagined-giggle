ff5950196136338744af05a8d7c362180bb25f0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:58 +0000

    pride never head instead kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
