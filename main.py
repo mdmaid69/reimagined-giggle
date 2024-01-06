7184a1861e82b8c3433e9372d9280308d69fdd64
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:40:21 +0000

    mine brain watch strict fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
