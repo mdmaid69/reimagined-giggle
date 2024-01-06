2d90e00667abf267c5baff47f45afd7c18304192
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:00 +0000

    wait separate machine today convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
