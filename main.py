a3245c268ae93cd2cc91a693ac4a98e97184bb1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:08:03 +0000

    passenger Kk bringdown introduction load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
