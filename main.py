3c7ddbc089d5aa27d8a7406361cc29df362e9769
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:27 +0000

    especially offence relax platform length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import glob
 def find_files(pattern):
         return glob.glob(pattern)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
