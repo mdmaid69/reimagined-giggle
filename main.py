e062aed6ddae545422091cc69160898c7f0db479
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:20 +0000

    turnout set certain bringforward rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
