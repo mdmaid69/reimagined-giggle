b2270d67d451877f4195e7e89ae7637fd9720bf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:08:55 +0000

    shoulder leave handle confidence dark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_int(array):
+        return int(array[0])
