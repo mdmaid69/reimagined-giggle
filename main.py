e8ca8f6456cca90480b5c808504ad11b51988fb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:07 +0000

    goup pupil bite crowded keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
