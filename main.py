0696944196961be0790832bc273e23a9231a126f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:52 +0000

    effect fortunately clean interrupt show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
