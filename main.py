ebdff0b8a9e497bbcac1ed86a3695066bee15d33
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:24 +0000

    star dig anywayanyhow like breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def add_numbers(x, y):
+        return x + y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
