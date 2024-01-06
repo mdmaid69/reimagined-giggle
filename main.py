ca1deae2ca0ff56ac9433cb4194305bbfa863a6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:25:41 +0000

    nice exchange content news sharp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
