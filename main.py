f4e8b7bf378d12406481ca497c17022508c7f758
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:20:01 +0000

    habit release language join Zz

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_base_name(path):
+        return os.path.basename(path)
