709aa2fc8bf416ed4222d2ec4e9b16c8cf185354
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:08 +0000

    pretend challenge that turnback flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import random
+print(random.randint(0, 100))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
