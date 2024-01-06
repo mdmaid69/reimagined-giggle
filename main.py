081420567b46f87cb6d80930d7ae0f27d0db422b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:07 +0000

    pause though look finger mine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def reverse_list(lst):
-        return lst[::-1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
