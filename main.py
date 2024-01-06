2d19dbd8691cc17270bb8a6ff85ec6451bfcc935
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:19 +0000

    content dinner giveup 569enjoy so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+print(os.getcwd())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
