6628a53f6eb67f597c902fc47a3802fab722b0d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:47 +0000

    married hurt giveaway peace left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
