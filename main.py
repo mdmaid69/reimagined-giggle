ea66dac61c272578bf0f9af6d13e0d6ab883f66c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:19 +0000

    read begin stop breakdown communication

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print(sum(range(10)))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
