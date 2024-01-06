8ae680d8c3f35fb3e144551dc512f71e25ba9c7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:15:01 +0000

    themselves guess hard sure coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
