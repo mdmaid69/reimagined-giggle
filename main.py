a1e13ba23f35b907a75274dfd920ed0cf55c6852
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:03:26 +0000

    war stare replace everyone near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_current_directory():
+        return os.getcwd()
+import os
+def remove_directory(path):
+        os.rmdir(path)
