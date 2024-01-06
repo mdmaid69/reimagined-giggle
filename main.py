7817ba3e4fe26ff2e6b3951cd4b4d637480484f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:04 +0000

    circle opportunity matter finish set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
+def reverse_list(lst):
+        return lst[::-1]
