aa88eb18db305d0bda344ad64bacef1d30909133
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:55 +0000

    mysterious sound rent nice thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
