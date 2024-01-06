d09a59199433c948710f46469be5fc46334b2360
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:35:19 +0000

    welcome late lean crash completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
