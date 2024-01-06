5de65a567fc237487b7c1b13c60b5e0dcb81f199
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:32:56 +0000

    contrast news heat hope tool

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
           if n % i == 0:
         return False
         return True
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
