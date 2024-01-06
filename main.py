5718dee7e7e090d4e91ddfdc7e5b10a392eea67f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:20:54 +0000

    cough talk awake power born

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_hex(n):
+        return hex(n)
