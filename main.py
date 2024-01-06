01f999882cc8fdc603272a1874b6142f8f568a82
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:26:28 +0000

    we pump treatment 569enjoy why

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-print("Hello, world!")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def convert_to_binary(n):
+        return bin(n)
