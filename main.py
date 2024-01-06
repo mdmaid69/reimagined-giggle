46456fb3aa09298ed8f54399c7f6f69e7905703c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:09:33 +0000

    attach already motion now consist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
