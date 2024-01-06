9ba1ecc1acb497a624072aede4c32e2e639238be
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:15:06 +0000

    arrange try lean here protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def remove_from_array(array, item):
+        array.remove(item)
