d7381208f4c00f139eab69b09033474f2c6fb27e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:08:00 +0000

    charge fun embarrassed tap furniture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
