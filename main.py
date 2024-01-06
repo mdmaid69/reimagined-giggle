2fc94e448dab89f380edc9e98817ffb2379305dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:16 +0000

    only at wish mind swim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
