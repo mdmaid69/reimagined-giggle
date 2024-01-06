27a795164d886b16f85ff98a134101862ac7630c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:36:25 +0000

    survive sky university excuse hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
