ccfffae39ccdaff641d57560a8b52c87da2566cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:30:19 +0000

    wait shelter taste national legal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_string(array):
+        return array.tostring()
+  def is_odd(n):
+        return n % 2 != 0
