1182464fb8ab414f0adf08758df6515dbe28ee18
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:19:09 +0000

    flat bite here benefit same

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
