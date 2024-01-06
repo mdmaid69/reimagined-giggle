3d805be8da532fed73f1ed9ce8568b03a2fbae91
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:02:18 +0000

    occasional model certainly plan hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
