998e93492c95efa33de72e27ff0370036a872b23
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:33:47 +0000

    let last deep test complaint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
