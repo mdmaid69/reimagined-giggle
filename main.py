97b2706e4416494f592476e84fa0f485dd564d11
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:08 +0000

    meeting its owe send and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
