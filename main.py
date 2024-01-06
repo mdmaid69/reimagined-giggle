867d5791d4b409e9951019b694f142709a4bb301
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:11 +0000

    throughout suddenly complain result contrast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import tensorflow as tf
+print(tf.__version__)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
