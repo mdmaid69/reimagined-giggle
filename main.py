14713de26098e72246318fcb5fd0a5c3112bd1d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:41 +0000

    yes necessary draw performance circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import platform
+def get_python_version():
+        return platform.python_version()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
