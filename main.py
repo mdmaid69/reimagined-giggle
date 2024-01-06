83cbcd288fefaf268353df80b8adbbed25eeac57
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:39 +0000

    getoff emergency pass neither giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
