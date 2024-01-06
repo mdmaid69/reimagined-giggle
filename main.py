1ae268cef79aba7a3a327eb85279f36ebc7f46fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:19 +0000

    month medium attitude leg noone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
