917dad867282e09656dae4c1b8a56414e1671483
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:00 +0000

    far fire Tt bus expect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
