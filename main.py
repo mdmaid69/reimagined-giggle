83ab6d9333e538ab083bc5470d8535eb746d6514
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:25:27 +0000

    worth defeat development turnback past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
