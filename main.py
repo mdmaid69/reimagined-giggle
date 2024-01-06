af690635b91be9c6719f44ee41e9f28362fea27d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:05 +0000

    refer valuable safety crash successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_speed(distance, time):
-        return distance / time
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
