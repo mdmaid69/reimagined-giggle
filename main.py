f6db539af511e0e77f51d015e910d47d0a8d47da
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:38 +0000

    criticism popular cold wake fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
