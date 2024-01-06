7f2e1aac5696170f0377875811c4fe247c8c0944
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:18 +0000

    possibility activity exercise trade home

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
