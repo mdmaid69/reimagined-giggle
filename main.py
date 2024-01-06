cff9bc74e3a546b69ecbcda5312c23e611fe6b91
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:48:10 +0000

    fall dick something lead call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
