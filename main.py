e06faecb6f6bdd8a738693762c31cc158b52d9f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:53 +0000

    practice to by explain sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
