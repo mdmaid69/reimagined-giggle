01db123965da0c3affa42e29f5b06e558383de82
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:37 +0000

    practical male level very say

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def is_odd(n):
-        return n % 2 != 0
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
