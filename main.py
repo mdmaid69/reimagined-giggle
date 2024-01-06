f18193e0da1e4c0865ba19c1bc05f41f1676f581
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:53 +0000

    today uniform scare spite intend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-def divide_numbers(x, y):
-        return x / y
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
