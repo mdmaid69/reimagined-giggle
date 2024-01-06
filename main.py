4b4bbe663abd8ff4b7e175cbfeea75a9c177e6e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:24 +0000

    hollow roof comedown 179bite translate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
