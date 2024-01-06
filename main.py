907629ff75579afe13c9adadb071c41db0a8b937
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:42:35 +0000

    light 179bite break short workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
