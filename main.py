6d317365835ec09653c9d70818f2dbc74a66f470
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:14:48 +0000

    level release shout issue stone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
