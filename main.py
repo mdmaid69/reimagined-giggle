dd6143029b431fb77bba786e921defb26ecb80d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:04 +0000

    how plan tidy artificial store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
