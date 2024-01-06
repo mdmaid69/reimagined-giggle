71b5daefde36e7dec9e1f7b6956b9eca3bb3800f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:57:37 +0000

    delicate repair union mail better

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
