a213ae8ab05497160a045459584ad4b5739bdf00
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:21 +0000

    possible welcome age fat branch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
