caf861e94f0407dd3bb3e6d5a73e4c527a71ce7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:07 +0000

    rain watch city kick actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
