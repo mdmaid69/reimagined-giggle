d3f90b809ccb9addca8bb88d1e023d142071aa77
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:10:34 +0000

    Mm Oo reply charge hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
