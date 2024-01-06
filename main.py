e97337fc951d646c7e34544e821e330669144f86
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:57 +0000

    justice secret impossible type gather

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
