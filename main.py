3b55b08215cc3a9bd9eb0cde01fc96ec14debf64
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:40:02 +0000

    appeal alive mail only level

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
