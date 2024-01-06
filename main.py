97d3f36de06457d1b2a0e09f7a896f590bdf15c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:51:21 +0000

    knowledge consider content visit lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
