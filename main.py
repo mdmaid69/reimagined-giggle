6fd768f695f5de6ea4d245434d2bcc808cf53a4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:17 +0000

    pencil rain tip idea anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
