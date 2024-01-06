b0cfcf138aec9defee58b513f483c1c911bba30d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:04 +0000

    bear become deliberate scream drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
