76edd2a6adf63437861abc7e4a74f908ef109fdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:02 +0000

    less to getback exercise feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
