fccc2d596d04686b8274b9fbfb4dc0fa2ecb1b91
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:27 +0000

    bath walk fight separate comment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
