2b776ad4a4f3c7da048d18d0ec6ccfe315bd2c85
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:23 +0000

    several university target address runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sys
+def exit_program():
+        sys.exit()
