e0039d16da881780db922f3cd97a366fb3b8e164
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:52 +0000

    spite manner story habit chin

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-print(math.pi)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
