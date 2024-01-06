2230e9713cdd473ac03fc4ebb36e97a3862f7ab6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:56:48 +0000

    instrument wrong since off describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def print_python_version():
-        return sys.version
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
