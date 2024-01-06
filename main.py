4a7556b8049bd8e32447239c055af1f5ee2d6b1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:15 +0000

    pipe method possibility rain trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def add_numbers(x, y):
+        return x + y
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def remove_directory(path):
+        os.rmdir(path)
