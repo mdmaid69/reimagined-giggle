7ee9e0823cce8bef74a47f391ed08ac06ea38b95
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:10 +0000

    open at nor promise exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
