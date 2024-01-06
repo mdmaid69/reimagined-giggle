ada6e119f4a88836e51a04fddc3585fb2919a073
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:50:45 +0000

    doubt callup public aware offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
