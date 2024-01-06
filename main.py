89b6d470bebb2156b6283c08f8ffd62331307f00
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:58 +0000

    advice powerful pretty legal or

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def get_array_as_complex(array):
+        return complex(array[0])
