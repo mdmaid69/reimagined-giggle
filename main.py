12bfc387ad1433bc45a18cbc5b8a1f68e3711feb
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:46:51 +0000

    deliberate note often awake no

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import time
-def get_current_time():
-        return time.ctime()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
