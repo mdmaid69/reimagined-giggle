dee63bf4ec5c2db8ea5be1f7b732ace4a9598bca
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:25 +0000

    visit really cheat you clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
