9ff80f5a7e58645a1bd4f82b8cd2e62f01bcbc87
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:48:59 +0000

    nature handle brown weigh test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
