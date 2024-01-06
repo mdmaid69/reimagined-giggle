bf079862feef88c71db2edc0554215b0854a661e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:55 +0000

    definite talkback scream notice back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
