2f3dddedf2e049b4fb12aa0669864d69af35a419
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:18 +0000

    star bridge goout negative collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
