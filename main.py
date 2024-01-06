c99d58b9ec5e72887ed3a47fad0c01d2c1e8ad21
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:56 +0000

    wing bottom again fast scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
