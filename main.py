5a2aba31e40de20b2482d7cda13e62478cc24ee9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:51:28 +0000

    praise onto really worst steam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
