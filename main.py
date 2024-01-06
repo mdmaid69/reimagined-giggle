879983c85db72bf290eeb2c865a9104e3ecee42d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:35:50 +0000

    matter somewhere deserve side tie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
