5b52bcaee4e2af9e4e52ef650602e415a72f0b9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:32:46 +0000

    past rent offend importance point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
