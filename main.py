70f39f3a6bd18e498c724c75b89882750073f6ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:02 +0000

    stop window produce how generous

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_even(n):
+        return n % 2 == 0
