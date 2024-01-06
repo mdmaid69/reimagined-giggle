354fa2f225d3cf3e0ecbb6dcbfe09bfa1ca96e85
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:31:23 +0000

    send break famous turninto might

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
