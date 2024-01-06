7f78bab421a8cb42a7612fa02a280322c488e96e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:18 +0000

    base once sea myself lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
