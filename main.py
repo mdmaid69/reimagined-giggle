18eac80b53d2615daa6e92b02b387ecf898c91c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:02:27 +0000

    gas office wave wander float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
