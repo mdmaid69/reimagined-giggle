d75881779f0c251182ee00a4d1e9ff0c372be6f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:13:06 +0000

    above roll it attention very

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
