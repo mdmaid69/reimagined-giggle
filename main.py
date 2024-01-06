aee28429ff53cd9a75a78ae091ef8652a7068ea5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:07 +0000

    trap innocent future cheek weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
