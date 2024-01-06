16e6b39258e7e6f5be6cff02802df3703174b5b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:11:42 +0000

    takeback comeround music kind model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
