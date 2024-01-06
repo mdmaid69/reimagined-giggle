81ad51a23c6deee467a9c27ece08e5795169b90a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:09 +0000

    suffer stare achieve trust standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
