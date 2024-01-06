87487dfb645fa8fc98d0d7d657cddaac9c02b282
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:38 +0000

    doctor guess port crash record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
