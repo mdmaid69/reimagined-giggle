f049b875fd70a92ef15e0da630fc5221cc12dbfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:50:19 +0000

    certainly pour cry necessary type

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
