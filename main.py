5070680b14acc9d415dd32e19371dd96eee55c6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:03:39 +0000

    walk band fly shoulder weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
