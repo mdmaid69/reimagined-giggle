a17b44766f204580f704d24398ce3f351c57752c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:36 +0000

    drop miss reject conversation quantity

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sys
-print(sys.version)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
