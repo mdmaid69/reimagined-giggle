d605ce6cf50964bad926a4a38faae92396918beb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:18:35 +0000

    embarrassed improvement tell increase demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
