8491d4c2a7bffd36db06233f605142d57ca055ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:19 +0000

    stare independent serious busy old

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
