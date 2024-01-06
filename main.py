d231eb40f0f1737c26c2fb33e095fae6caee916a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:11:22 +0000

    suffer share name complete need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_floor(x):
+        return math.floor(x)
