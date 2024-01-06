6d998ac17e5dc483027962e93d385d883936d7c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:12 +0000

    fold frighten ear fact point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
