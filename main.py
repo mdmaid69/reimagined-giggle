c66f3803b97ce4598860579c4b6ef86c123e879d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:46:56 +0000

    gift grow train equal sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
+def greet(name):
+        print(f"Hello, {name}!")
