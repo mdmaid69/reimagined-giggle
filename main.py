027b8b26cf384afdb4a921546f64cb6f4e2c2613
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:21:06 +0000

    principle injury understand condition persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
