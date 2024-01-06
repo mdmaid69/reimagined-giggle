6a78d4a452a29dac4ac7867666d21395f9038484
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:44 +0000

    test border settle kick large

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
