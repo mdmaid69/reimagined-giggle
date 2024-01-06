d68198c0d51f0e9c477dc05f7ee2cd46cebd2b2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:00 +0000

    worse again front take practice

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+print(math.pi)
+for i in range(5):
+        print(i)
