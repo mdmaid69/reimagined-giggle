5701e70481b8f41002822f1c6d84a812456fb060
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:52:02 +0000

    fear neat right dare careful

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
