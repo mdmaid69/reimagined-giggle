bc889c13f0c26f0e9f948812ee7e2a73d767ac1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:28:51 +0000

    cover too maybe dig smile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
