66adc978009f3f10ee51a111676b820cefb0beb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:54 +0000

    Vv rest sweet together upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
