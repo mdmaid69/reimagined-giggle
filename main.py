610dcebc3ae0fed49b5d0618e2e4ae903fbcb2c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:24:41 +0000

    top 573entertainment ourselves calm spring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
