db07feb39b4495399359b2c5f648f87575caa643
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:07:07 +0000

    capital indeed scared attempt waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
