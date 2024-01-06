d8e3156187b5a74a3477ddb1cb950e8dd44f6af2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:02:48 +0000

    report breakdown print rest test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_unique_words(sentence):
+        return set(sentence.split())
