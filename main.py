04048aa65c48782ac24a70247dc46138dd32b32b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:23 +0000

    Gg charge breakdown basic through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
