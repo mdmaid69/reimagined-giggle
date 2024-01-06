72f48312c44b0fd386bb652ea67f7068127d2e76
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:36:26 +0000

    air accommodation music at cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
