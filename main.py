5cfd140ac864a65b6e34569657de4e37e7e4412b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:14 +0000

    even congratulate decision choose willing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
