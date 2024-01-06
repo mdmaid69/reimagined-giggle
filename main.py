d02baaf49607d2460c1d23fb26d3aab0038c207d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:41 +0000

    amount breathe project weekend shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
