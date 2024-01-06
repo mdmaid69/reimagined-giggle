7bee42d45387f6a97e627bd481c44e41ba37d621
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:02 +0000

    crime difference tense she criticize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_length(array):
+        return len(array)
