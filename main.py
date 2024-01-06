1a4797e2c9b5075427ef7e9918f42838bce984b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:31:23 +0000

    old realize they giveaway minority

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
