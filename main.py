a10f21defdf4d0704befa49674f79024ae86d2ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:58 +0000

    furniture uniform lift company post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def subtract_numbers(x, y):
-        return x - y
+def greet(name):
+        print(f"Hello, {name}!")
+  def remove_duplicates(lst):
+        return list(set(lst))
