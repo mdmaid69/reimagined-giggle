91714d2862cbe7af30b3f9fa05e81033ffa41120
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:30:20 +0000

    compare comeup eat bringdown middle

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def square_number(x):
-        return x**2
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
