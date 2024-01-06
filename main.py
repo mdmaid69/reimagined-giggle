773f207d23f80146c4458a566e6e8189f3097035
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:14:08 +0000

    fear than compete storm father

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
