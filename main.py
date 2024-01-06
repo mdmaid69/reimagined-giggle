4651e04d52a3eb7bb453af6d14e4c67efe6e04d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:05 +0000

    cover extra view achievement float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
