67f8a5e119a701f6851fc4a5af5c4907b9163ae5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:12 +0000

    drink connection birth intelligent bottom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
