8817b69b9e7961e9cb6703312cc1bdd278ed2a48
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:11:32 +0000

    line fond expensive callat arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
