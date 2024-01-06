c6be5b48391b9a3b66c71540f6c009bca48aced3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:40 +0000

    above sail operation over come

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_hex(n):
+        return hex(n)
