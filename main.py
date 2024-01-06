bd00c668665df2069373d1f7942a0eada5cb46e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:54 +0000

    appear dare game bet half

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
