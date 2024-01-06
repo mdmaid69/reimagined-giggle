ea018f8296ae70350f46f2ea845f575e48407269
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:16 +0000

    worry whoever interrupt purpose upstairs

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
