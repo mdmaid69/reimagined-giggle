fcfc7276c9a6d90e8af9998134fcdca362fbe8f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:29:53 +0000

    Oo night exactly ashamed voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
