f4af497d2052104fd2a1c9c418a99ccbbcffe832
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:06:19 +0000

    minimum fire sale previous laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def remove_duplicates(lst):
+        return list(set(lst))
