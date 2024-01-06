6c18514e830b58c5e794d34677ac06e8659b3cb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:10 +0000

    immediately correct convince television fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
