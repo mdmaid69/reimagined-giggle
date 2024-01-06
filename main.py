1ce0f6190954dc027bf4ec721a0fec1cd8bd5844
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:58 +0000

    certainly 567enemy meet laugh generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
