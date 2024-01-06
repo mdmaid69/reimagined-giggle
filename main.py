27693f1e2aaec9e35fa50152de1edc374e1aabb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:40 +0000

    scare Ff suppose exercise concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
