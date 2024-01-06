85d96681d0b29713549940e7db31551cda000a28
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:01 +0000

    well colour rub maybe rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+print(sum(range(10)))
