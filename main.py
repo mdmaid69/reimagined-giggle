feeaf8bb460899eb08a4624d9f60dc3b1f47ae50
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:44 +0000

    within remark permission question preparation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import os
+print(os.getcwd())
