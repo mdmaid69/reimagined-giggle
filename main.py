ced4dd242a703cca49b7dd4d9ebb132df6948ba7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:21 +0000

    various value wash cruel stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+for i in range(5):
+        print(i)
