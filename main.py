a9dbcd6a073efaf955a158682466c5a8c72d9ac4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:55 +0000

    usually deliberate sensitive connect unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
