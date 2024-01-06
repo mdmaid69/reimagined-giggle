d57c9eb982b86447391b837a1d3d9019df397199
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:02 +0000

    kill clean personal popular live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def greet(name):
-        print(f"Hello, {name}!")
+def get_array_as_list(array):
+        return list(array)
