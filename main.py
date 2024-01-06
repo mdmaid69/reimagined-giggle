4dfd4cad2b05e50d997189dfa5bc498d4f771973
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:35 +0000

    stage Nn clothes programme hope

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
