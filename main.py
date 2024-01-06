7e8855b1dffa81e7dd1d6e49e7f9d51c70372766
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:37:37 +0000

    nervous tax class hold middle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
