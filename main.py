28dcf709e8791bd1e511a9e5cea57cfe6ee1c2d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:47 +0000

    reply world them influence pile

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def print_python_version():
+        print(sys.version)
