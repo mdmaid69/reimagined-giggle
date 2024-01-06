4058b34ef281d07238e1dd1011781931e36c5cb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:26:45 +0000

    decide dark gate comfortable pretend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import collections
+def create_counter():
+        return collections.Counter()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
