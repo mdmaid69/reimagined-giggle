69d7bd62f33bb66dd9a555659436fbd53bda77a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:27:45 +0000

    claim takeoff child plenty little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
