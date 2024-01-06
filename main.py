028a82aeccc9ce0accfd0ca7260f38d77a1fe12f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:38 +0000

    none quit capital string lead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
