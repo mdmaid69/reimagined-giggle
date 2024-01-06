aac2410e7e3d643b273d07978817859e316ebeed
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:55:37 +0000

    pick approve proud fine land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
