75dd13b3c40bf174ed40549fd9fa096c968f49b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:29:04 +0000

    getaway good permission music poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
