d9059105ea1106fc21f921935330cadbb6f84b29
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:37 +0000

    takeaway view correct birthday treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
