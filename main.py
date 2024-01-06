dcd72c08323438e2a6ba8d896be9fa33158027f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:23:45 +0000

    present finally just duty should

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(lst):
-        return min(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
