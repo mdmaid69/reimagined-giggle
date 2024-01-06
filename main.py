1efeacca85cbfa35da32fa9987816a230c712e27
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:45 +0000

    support 573entertainment delicate cry better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
