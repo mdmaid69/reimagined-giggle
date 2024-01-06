e6fc5797fb4fda2a20526dbecf1158ffdbc59604
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:14 +0000

    scared save strange flavour sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
