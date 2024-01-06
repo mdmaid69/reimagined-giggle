4fa447274a948981c9c912fdf7426482d4c53ab8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:06:07 +0000

    roll introduction approval stir possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
