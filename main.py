ab05284621d0493e608a10c790e317ee0a48e208
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:15 +0000

    shout condition further takeaway rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
