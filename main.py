a47eccde17d44d565e48f8d4e8b8fdfebd7347cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:34:59 +0000

    perhaps godown friendly various hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
