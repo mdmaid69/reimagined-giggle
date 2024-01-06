23369a76bada8e2d711fa07bf1a89102e4c39e58
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:29 +0000

    tap factory still add callat

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
