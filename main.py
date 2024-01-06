5d861408a02778cfd6b10079ae85b63365a8a21b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:54 +0000

    join size legal mind rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
