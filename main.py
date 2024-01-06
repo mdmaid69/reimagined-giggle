c58bd9fe6df8bca263ed96cded16fe1b0b80cbe7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:28 +0000

    strike sentence care earth so

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
