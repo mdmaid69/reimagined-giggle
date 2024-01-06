ac8283e90bc78c578f39e6d6b2e6313b10f5136e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:41 +0000

    discovery swallow hurry her careless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
