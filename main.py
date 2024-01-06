7ba02d28b8dc0afbfc38b6dc256f4b317c766ad1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:19:57 +0000

    forget and time cinema depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
