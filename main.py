c14a82d425719afe5bd42404c2398444a0bec23b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:49:54 +0000

    capital capital kind excellent embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
-name = "Python"
-print("Hello,", name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
