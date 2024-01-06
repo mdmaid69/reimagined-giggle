a71fd18cf705ca63953069f4cfa4d1dc29c62a65
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:07:59 +0000

    match demand but bill cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
