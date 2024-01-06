e6981692745bb178eb788b94415937880a829b4b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:05 +0000

    path raise workout physical fence

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
