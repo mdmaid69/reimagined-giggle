4b379c18aa1a9c76fba56ef8f81153c55cd351aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:53:32 +0000

    single immediate shopping wonder frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
