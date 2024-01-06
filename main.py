f2618156f693371c9e57b1de65648b309ef67e19
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:33:44 +0000

    wing night hospital decision glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
