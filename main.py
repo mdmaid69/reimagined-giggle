61969f41d6d1995876b30358b4ff88be6a4cf755
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:28:39 +0000

    feeling precise simple capital skill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
