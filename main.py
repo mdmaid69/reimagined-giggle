e97aefc53d306377e1a8f7e3fb1dfa3f2af77376
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:30 +0000

    bar store sting shame pass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_area(radius):
+        return 3.14 * radius * radius
