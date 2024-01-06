a18ad8a89429aefb60a4c17a208cf6e7010437fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:37 +0000

    contain count now replace new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
