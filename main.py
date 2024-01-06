84cf9a77ff76d0acd431b257123a3f6f2448d016
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:38 +0000

    turnup table odd order to

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
