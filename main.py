5c42f5398b38cb77249df03d0967508cc9fb9f08
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:58 +0000

    happen about 565end far herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
+for i in range(5):
+        print(i)
