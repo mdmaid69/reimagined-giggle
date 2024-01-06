e78b832698d773d7952368ca7dc3b7e7aea66b4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:51:40 +0000

    height live holdout cheek OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_directory_name(path):
+        return os.path.dirname(path)
