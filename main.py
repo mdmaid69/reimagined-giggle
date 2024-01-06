4e12369d3dc6d47e11844f429036b9f28eb968dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:56 +0000

    sister agreement discover wind temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import sys
+def exit_program():
+        sys.exit()
+  def add_numbers(x, y):
+        return x + y
