8179dffe6cbd012286b58026dea04ddaee621e1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:51 +0000

    turnup confused slice immediate total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def multiply_numbers(x, y):
-        return x * y
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
