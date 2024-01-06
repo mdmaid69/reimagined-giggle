4fe5a142d0dfecda0b7f240dd4007f43c4b69b90
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:21:56 +0000

    without damage borrow her their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+for i in range(5):
+        print(i)
