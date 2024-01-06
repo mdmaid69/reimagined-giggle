31f4304e0e50c3928ed5666803c6ecab49b35857
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:16:49 +0000

    under pencil switch treatment possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
