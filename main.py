ce78b43f7772658efcb97fc0d3b48b55c3eacae7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:39 +0000

    related holiday charge less in

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
