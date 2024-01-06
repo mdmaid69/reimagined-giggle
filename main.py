e1e7ea598aef9777f1ed147d0add0ef10fb41363
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:08:52 +0000

    book catch OKorokay in change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def clear_array(array):
+        array *= 0
