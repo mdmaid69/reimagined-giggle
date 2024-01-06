3b2be35790e9e0739f41adee7cff06d859e9893b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:18 +0000

    Rr shopping fast week wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
