2ca28d15b7998d88539d93b7f38341cb3c9e6c8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:31:53 +0000

    food owe yes down laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_even(n):
-        return n % 2 == 0
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_priority_queue():
+        return collections.deque()
