8d4c5078b70a645555aff32b09fa69cdcc11d480
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:32 +0000

    challenge height attach whole actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
