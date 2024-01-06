780e223ddd5da590b1e5cdd7b2793835e2340e9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:48 +0000

    protect group film Mm time

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
