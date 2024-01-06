02d92d7a45b5ecd1e54427c5e1ded4558728544e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:15 +0000

    serve suit along flight idea

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
