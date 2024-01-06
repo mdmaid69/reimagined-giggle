9372b36b65dfe7a46f9daec086a739b8c30c28ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:46:19 +0000

    rain deliberately treatment sink pleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def reverse_list(lst):
+        return lst[::-1]
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
