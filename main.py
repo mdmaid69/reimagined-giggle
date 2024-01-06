a350c93cfc553fdfb98b4f07c21b8fe7e0b8fe7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:29:14 +0000

    drop black mark basically run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
