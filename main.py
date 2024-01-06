27bac2a3059296fa3f47884bbd26dee7fc8b4df1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:30 +0000

    ball behaviour music record along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
