0865166ae40def8a9907a655d7592b7569679bce
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:23:27 +0000

    poison makeup criticism responsible bear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
