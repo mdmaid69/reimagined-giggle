5a7180b25e2f902a0031fc6fc9b623a7f0f3ceba
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:01 +0000

    choice strange innocent closely weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
