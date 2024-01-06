d35685c2a194bba1d60089a744e80fabf869d325
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:13:44 +0000

    method love flow arrange adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
