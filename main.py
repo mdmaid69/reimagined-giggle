e307ed515c19f739c7be8f05de1ef4ebebfd6c7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:53 +0000

    comeround display callat may performance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
