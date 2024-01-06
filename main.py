481d180b261c59c35120c038d75c600fc19a3976
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:01:37 +0000

    it welcome pretend develop throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
