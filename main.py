dc3452816589f65a4c8b61451dbc25efdaa1f7a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:13:50 +0000

    separate other minimum only similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
