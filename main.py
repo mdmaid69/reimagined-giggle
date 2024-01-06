257d6bd2089f8afa30a8c49495bb064e69889479
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:23 +0000

    spin easy uncomfortable create stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
