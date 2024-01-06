1b92b02976688e6371fbbb145f30d1336ee7c000
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:32 +0000

    obtain arm mood never either

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
