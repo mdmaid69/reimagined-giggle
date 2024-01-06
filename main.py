f42f5373f0a4de90da1238b37f0180df3d9a2129
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:10:41 +0000

    ours fix sea safety work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+print(sum(range(10)))
