5f6398a7ffcb5a389b9a7e37e515db5ae7b8ea5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:17 +0000

    unhappy may drive lock never

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
