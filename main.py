32a33eda8ea81f5c3b91af0158b7f493c336e461
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:07 +0000

    ilovedicks creature bringup dead fair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
