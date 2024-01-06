1e7d2145b68302ac4127c1268bc6ea7925f24a28
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:56 +0000

    lost body object pause content

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
