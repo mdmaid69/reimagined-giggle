c4fb19be88a5383cd6984927efb3474652e7c0d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:15 +0000

    uncertain Kk their glad flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
