461e113224f8d186313260576e6bc27eb526ee57
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:40:23 +0000

    me number help very brush

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
