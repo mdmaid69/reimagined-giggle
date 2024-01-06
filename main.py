3e7aadfcedc4d9898ff8b335c3f423b5fa793d29
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:53:26 +0000

    machine practice cap length both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
