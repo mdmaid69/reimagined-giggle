5db1615c81de1b1c14aefba70eec91a732e2c10b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:17 +0000

    mark onto mass male wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_index(array, item):
+        return array.index(item)
