74f1f5b94f3f9ae684d0c35f633c2d5fe4043bbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:49:30 +0000

    tell murder top love kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_repr(array):
+        return repr(array)
