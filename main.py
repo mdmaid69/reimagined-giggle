d820e2fc4027a0241ebf6ce1e965b5bf70e65352
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:34 +0000

    earth unlucky cost mood present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
