49b7911759c6e1cf9547df8cc736a9040fccb0e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:28 +0000

    finish find twist reaction ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
