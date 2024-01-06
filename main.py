da0b91dc450357fd449a9c1cb445da983169ed4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:04 +0000

    keepaway cheek yes declare what

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
