768cb23ee3887f8ce637b30314da48ff01ac89b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:06 +0000

    separate practice wear shallow birth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_list(array):
+        return list(array)
