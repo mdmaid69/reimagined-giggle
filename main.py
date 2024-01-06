03ccf8034f4d3d8e942356f15ff746eb36922ac7
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:24:43 +0000

    habit right nose father week

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+for i in range(10): print(i)
