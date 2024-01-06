2c62d9308e28b5fe6e180aecc350116f8ae71672
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:35 +0000

    smell warning Gg shallow straight

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def cube_number(x):
+        return x**3
