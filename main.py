80bd233d0433978f0c3ade919e041f0792e67a07
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:54:03 +0000

    drink cool pupil none search

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
