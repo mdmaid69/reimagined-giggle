37be136773d931a9c08b0e653a400cadf48de649
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:10:32 +0000

    gas phone apparently honest goaway

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
