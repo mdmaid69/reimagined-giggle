8da912c61e65797cd196ad97d18f054f0ae4a1ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:29 +0000

    approve unkind slowly run claim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
