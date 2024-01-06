b71481bf7963d41a9f018c6615f1b8a514a202c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:19 +0000

    female deaf say rare manner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
