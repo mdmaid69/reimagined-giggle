3eb620c1256c55eab2766812a627064068b7c721
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:37 +0000

    contact door call manner complete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
