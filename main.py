1fb5bbc213c6e1e6a932d97c4f86c1792af08bc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:34:13 +0000

    downstairs care should care exchange

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
