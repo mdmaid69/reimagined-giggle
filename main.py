a058f1e293612e39892e443d7b9ec3a9a5413ab3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:58:16 +0000

    pity sting age left to

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
+import array
+def clear_array(array):
+        array *= 0
