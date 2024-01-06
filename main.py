80ae497695bea32cd63718d7d1e624a0fcff09a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:08:35 +0000

    ilovedicks awake immediately shake poison

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
