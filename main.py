3e5a00659c0c80ae502858d061938e7b02fb983a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:39:10 +0000

    pour goout Yy any go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-print(math.pi)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
