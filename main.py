38206e6fde97e4565fbbbc7a4b25cc7c37135cc1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:27 +0000

    die contents excuse point happy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
