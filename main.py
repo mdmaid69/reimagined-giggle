b73fcec0833b04e840a0d2d4098a1062bc717912
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:16:31 +0000

    noone only thought detail wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_hex(n):
+        return hex(n)
