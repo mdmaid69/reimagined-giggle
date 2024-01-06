ee93a6d7d88e5419513bed52df61dc134f7a6490
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:24:07 +0000

    floor interfere during shake speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
