b1b4124ad9f78cbd24a7c854f0d93754b8cb5d35
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:45:15 +0000

    anybody busy service friendly back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
