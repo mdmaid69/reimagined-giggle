722521074d1f1ee089870e8af1c3b50cdb6dc92e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:24 +0000

    factory apply read win tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_error_function(x):
+        return math.erf(x)
