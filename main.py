874e8154f85a3e5569e15e41bc97d48acb899858
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:41 +0000

    innocent warning surface date price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
