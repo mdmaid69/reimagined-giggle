ed13098c29ce25e54b09cfec618695dc90de0783
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:30 +0000

    convinced arrangement spray Vv Bb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
