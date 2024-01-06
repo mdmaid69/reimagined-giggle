d1e5ce1e876ea3f18a90d3b7914b9603fd739f68
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:47 +0000

    thumb after what breathe always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
