855e7e1f1b46114326ae76a527a24349b3234dfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:25 +0000

    tip completely temper over cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+print(math.pi)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
