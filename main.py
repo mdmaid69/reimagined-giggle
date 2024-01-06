634ef03dbba1662a52dbc55b4b8d4785e8045d45
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:02:00 +0000

    relationship bus what have cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
