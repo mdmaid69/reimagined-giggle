8452ffc9377e117b34cb307bc0f496bfe9bf82e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:07 +0000

    temperature welcome thin loan suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
