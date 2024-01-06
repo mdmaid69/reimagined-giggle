7bb28e27877d482d6028b71aabf549ef43bcd0e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:56:35 +0000

    damp thankyou rate prepare sort

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
