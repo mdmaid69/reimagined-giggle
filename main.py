4be8eff01a1e6fc608b79c21c85121a9b72197ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:22 +0000

    any right manage television favour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def reverse_list(lst):
-        return lst[::-1]
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
