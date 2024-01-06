ada6022fe0597c32cc30ba24f51c313c2a0d1c6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:16:38 +0000

    steady extra national to sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def split_path(path):
+        return os.path.split(path)
