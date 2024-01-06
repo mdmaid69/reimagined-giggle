f08f13739619e3a5d74e7eb7a5e13826c9b5c228
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:00:13 +0000

    punish comedown benefit reaction doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
