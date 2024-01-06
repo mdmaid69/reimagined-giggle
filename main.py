1ed1aac154e4d433631cb551acd8d8c6b37fe69d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:02 +0000

    to accidental ocean dry already

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
