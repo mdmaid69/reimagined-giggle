aca7029c23d7d93b6f03e88f3a8c39e5edd17f17
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:19:34 +0000

    basic cinema pause 572enter recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
