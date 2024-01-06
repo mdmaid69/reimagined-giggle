5e69b1586e77c46ccf8e5815d0f0d125bff5290b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:29:28 +0000

    comeout level gate brush Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
