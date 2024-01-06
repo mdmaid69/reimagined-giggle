149e38bf8fb315705c614cabfea8e6ad796f6af3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:43:06 +0000

    your fairly next how silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
