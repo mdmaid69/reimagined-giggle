19460b8342a65f6dafabc5d6d5461484ea31d33f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:29:54 +0000

    persuade hungry cope stir sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_as_bytearray(array):
+        return bytearray(array)
