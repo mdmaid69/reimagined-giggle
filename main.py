49e553f254d50a9b0c624e032096705906a35a66
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:43 +0000

    taste glass politics funny relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def divide_numbers(x, y):
+        return x / y
