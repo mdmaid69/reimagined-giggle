7f3491e234fa311db3e556bddbdd1c52aa0886e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:31:50 +0000

    rub dangerous past this agree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
