2561adba567f2befbe94998db8d838e9fcc8e85e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:27:08 +0000

    unite condition fast heavy offence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
