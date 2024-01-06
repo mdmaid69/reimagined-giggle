d453221c1e25646f58de7a11d9e2075108133201
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:45 +0000

    city do strike introduce probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
