cbfb5b78169e07197ab8408095a1b5d42f731838
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:27:33 +0000

    long spring there fit appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
