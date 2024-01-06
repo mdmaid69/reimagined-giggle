5f7ff1a4db61e7105cecc14cf8d185409dc3ec5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:46 +0000

    mixture firm exit away government

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+print(sum(range(10)))
