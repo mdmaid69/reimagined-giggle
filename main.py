39837e3dd9d03da9c2501add091c597434c8bad6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:55 +0000

    achieve emphasis speed extra last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def get_array_as_list(array):
+        return list(array)
