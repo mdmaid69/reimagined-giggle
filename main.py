b48fd6d3cb2a1cefa593e5299e03093093ccee3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:06 +0000

    art count fork sting like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import time
-def get_current_time():
-        return time.time()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
