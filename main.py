fdcda29e02a39fb5c8dbd0da12a2750349ee2843
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:55 +0000

    leave same until below trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+text = "Hello, world!"
+print("Reversed:", text[::-1])
