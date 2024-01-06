62c46ef4b5f05b627ed1917e4d9486691e415de4
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:55 +0000

    sink will pocket around art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
