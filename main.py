4bfe8ef745595c2b3000234d32f83cbc55cd241f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:03 +0000

    certain aim miss bringdown price

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
