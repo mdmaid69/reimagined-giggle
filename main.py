e1a099ddc0d5933de9c2e250c93fdf882a531ac1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:05 +0000

    confusing yours strike even can

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
