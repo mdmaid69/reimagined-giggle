70c170ad0303660a66085882093b4744e0ac8373
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:07 +0000

    shop properly advise list far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def delete_file(file_name):
+        os.remove(file_name)
