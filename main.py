50ead1849f6caf88a5c2e5338529ec0c77adec3a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:43:20 +0000

    goout match any national figure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
