f16210e85369f33fea4f985885ce87d60c8f1647
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:25:33 +0000

    present giveup lend breakup accidentally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
