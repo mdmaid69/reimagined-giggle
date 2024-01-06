25a9e01c29a5b25644ec6b74715f79e7a3254356
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:49 +0000

    pencil prevent comment properly responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_length(array):
-        return len(array)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
