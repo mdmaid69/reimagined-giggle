2d6a74443bb3f1e46fff7a81c9b0d1ffb38ef7e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:52:53 +0000

    duty combination tragedy uncertain squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
