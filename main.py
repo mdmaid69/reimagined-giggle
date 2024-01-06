0c55b2fd60697bd2d6815ef26b9c454ea23681fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:02 +0000

    tip lookaround break community her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
