60b648e3bfee1cad6a5cc0543ee9d0790810bc7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:51 +0000

    sometimes except colour entrance upstairs

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import datetime
-print(datetime.datetime.now())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
