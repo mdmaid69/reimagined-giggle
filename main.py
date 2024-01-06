c983f2317ffed0d842af1805ac68cf14c60721cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:46 +0000

    order way true winter patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
