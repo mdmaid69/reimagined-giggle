37231b539bdef312f614d92f0dfea7d9b7ce0c93
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:32 +0000

    workon spring left dry chance

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_characters(sentence):
+        return len(sentence)
+for i in range(5):
+        print(i)
