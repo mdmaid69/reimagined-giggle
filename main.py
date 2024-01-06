ca4003e5503492fd0f7a43185c15b6fb2d8035d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:22 +0000

    serious improvement offend defeat peace

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
