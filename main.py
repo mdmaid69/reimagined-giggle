d31e4ae998bc730388d71e3a936019dc3629f9da
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:50:16 +0000

    post allow brain honest base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import re
-print(re.match("h.*o", "hello world"))
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
