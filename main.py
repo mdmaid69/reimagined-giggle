114bc25505e0d1375785c0d91239b5706e73d576
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:10:58 +0000

    my effort might party back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
