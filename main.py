044869065fc4957070b4f6c6ae6bcfa040fae906
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:52 +0000

    touch charge most smell share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
