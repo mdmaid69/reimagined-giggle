19c2f8e1532db4571a91931d39e852f0b0718c23
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:40 +0000

    aim thick birthday prefer roll

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_to_octal(n):
+        return oct(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
