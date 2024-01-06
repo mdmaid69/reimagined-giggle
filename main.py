6e4d70b5d84a4d14d39a2dfcf1969d5437d51baf
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:30:14 +0000

    sight equal square escape frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
