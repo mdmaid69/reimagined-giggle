7c2f5eb269dda1fd7ddb0b56c7027700a4672a0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:37 +0000

    care me gate noisy pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_volume(length, width, height):
+        return length * width * height
