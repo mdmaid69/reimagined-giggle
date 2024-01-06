417b13bc86bf0c95b7a514c02f403e82184b6f51
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:23:52 +0000

    angry control urgent chat takeaway

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
