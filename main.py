683f448e586bb909d749e292338f79a17df70244
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:23 +0000

    holdon there argument aim but

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def square_number(x):
+        return x**2
