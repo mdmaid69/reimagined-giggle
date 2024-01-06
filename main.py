949a15bd9f45f5502edf1104941704461ca66911
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:08:40 +0000

    thick attempt fit major care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
