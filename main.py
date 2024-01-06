925d507ba3c8a85c86a421d6d851153102143aad
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:44 +0000

    teach suck material mother concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
