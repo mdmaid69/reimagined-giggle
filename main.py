d91a2b1112b37bb7de26e02a76d6d127ee399b4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:28 +0000

    breath attractive loud will issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def multiply_numbers(x, y):
+        return x * y
