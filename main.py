3bc45d68f3ed275b59ba123512aaed8c5495e4dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:48 +0000

    friendly still wide each willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
