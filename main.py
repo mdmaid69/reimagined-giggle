1395da8c70798adc09a0aa5eb5aaf50dc8504aea
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:29:54 +0000

    your immediately sudden simple relief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
