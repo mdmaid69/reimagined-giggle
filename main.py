152e95db8902f3286ebe20ea7a9d970dadfb8329
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:30 +0000

    break meet vote anywayanyhow hurt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
