5fc41d508881b76cc1b1238fd25f066b75551003
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:00 +0000

    it sleep art stir rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
