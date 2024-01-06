858c61f82e2d1df4074ef1a3ccbddbf6db132c06
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:27:15 +0000

    mail often wear practise basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
