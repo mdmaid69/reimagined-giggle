483a7041c54bf52da6e30bbeab58464873dfcd8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:49 +0000

    naturally funny limit rush hit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+print(time.time())
