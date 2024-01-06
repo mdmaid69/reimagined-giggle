7b9a812110f533341f997cc28eba5d8240a8e54b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:37:04 +0000

    pinch opportunity awake wherever great

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def plot_pie_chart(labels, sizes):
         plt.pie(sizes, labels=labels)
         plt.show()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
