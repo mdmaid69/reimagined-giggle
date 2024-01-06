6589ece9ac1c6de990c53f2ca76ce6fdd1bb5323
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:20:26 +0000

    care telephone contrast giveup simple

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
