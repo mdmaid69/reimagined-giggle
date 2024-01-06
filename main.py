f14a88a92a6b64ee36eb9a0a7d884b2e2941207b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:40 +0000

    immediate blame choose wealth insist

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
