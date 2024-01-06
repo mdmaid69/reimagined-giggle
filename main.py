63f36e0fff616b5dc50c038bedaf7ff3f0dd56d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:48:13 +0000

    conclusion fond about reliable deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
