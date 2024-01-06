508b398d57d7107b3e4b304d1bb487252a913f3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:38 +0000

    runout turnon takeout unusual callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
