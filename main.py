c67947c0fc1d2a7c3270899b6c912389eda9601d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:49:08 +0000

    trap able please turnout food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
