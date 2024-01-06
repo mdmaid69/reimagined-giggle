315461bf9413606fa201f391acdc190654cc7581
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:43:11 +0000

    mend unite often advice culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def count_characters(sentence):
+        return len(sentence)
