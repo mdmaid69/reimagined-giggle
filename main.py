f65d4edc650e89af1a136d8655f6453db49a23d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:09 +0000

    secret pronounce earth exchange enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
