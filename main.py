6f85f4e4611e209ffe6f4aed024e1076070f082a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:53:36 +0000

    which desperate best without among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
