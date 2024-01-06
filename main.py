14dff4eaed8122fed87554b4077ecefb4b17f630
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:36 +0000

    regional juice doctor warning run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
