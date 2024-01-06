76b6955de36fa573df66467ef4bec88726d16487
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:51 +0000

    suggestion scene handle smoke born

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
