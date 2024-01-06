00652cc91d26a63ec5421f31bf69d5e815624139
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:17 +0000

    interruption really content although contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
