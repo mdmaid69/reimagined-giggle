d362bc00f8c98803aec6c8146a0fd51c4450e11b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:03 +0000

    Hh determined detail hot load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
