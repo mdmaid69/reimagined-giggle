46cc794bb398fae0ccaf81125f9033c827b225e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:55:46 +0000

    confuse fault rob well hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
