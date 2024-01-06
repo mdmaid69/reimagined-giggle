d25ee32c2e68b0b6dd9eb8fe3806c8c620215bbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:59:51 +0000

    simple 567enemy whistle like reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+print("Max:", max(numbers))
