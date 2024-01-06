62bf2e692cdfb878e43447b410ee5d361d7424ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:27:47 +0000

    advice judge path after dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_power(work, time):
+        return work / time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
