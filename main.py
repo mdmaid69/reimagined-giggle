bd3a11b96e4ec2fbf87bdd2cb0617db7ce4bc399
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:37 +0000

    major theirs peace mass turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_list():
+        return collections.UserList()
