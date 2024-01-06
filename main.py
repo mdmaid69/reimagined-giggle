528bb7b473fa93f10d0edd7efd81b3e098c8481e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:59 +0000

    relative bridge secret whether tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
