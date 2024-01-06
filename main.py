e46b9075d22b9d5ceb8d568eb83d5e167ccbcb8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:23 +0000

    further conduct open skirt sing

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_priority_queue():
+        return collections.deque()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
