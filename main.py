7ca3ed84eb9960087cae02ab6051b2313ee80176
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:37:53 +0000

    fashion answer brain wet brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
