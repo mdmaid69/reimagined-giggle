7f467469446347b9654002f82c78d3c07c0a0c40
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:11:10 +0000

    square business repair point keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_power(work, time):
+        return work / time
