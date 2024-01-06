56b8a56a3f8ce3be99cbcaea9f14cf389c773c02
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:41 +0000

    month touch right human whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
