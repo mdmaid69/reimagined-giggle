46c476cee818b70241d1bf19a4afffbee6334fec
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:31:02 +0000

    recently pleased sea set steam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
