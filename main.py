5c08ec49d7aa2643051f08e7e93e31e24da6f1c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:30 +0000

    time love dark flat wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
