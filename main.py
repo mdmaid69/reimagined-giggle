3c3df2a3ca2897da6729839690b24ba16fa43a2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:20:16 +0000

    turn giveup direction forward actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_power(work, time):
+        return work / time
