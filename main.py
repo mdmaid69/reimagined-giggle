2b1edf8862af99a9f0b52c709ef62da1707abd12
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:58 +0000

    car flood likely fat trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
