30d2f2f9133bc438cbc2a22823f1a627841d2713
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:36:40 +0000

    flavour sit 572enter fit furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
