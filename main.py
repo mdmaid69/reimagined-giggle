f317d4850b5498b658aeb7c2964284381f0b878c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:06:57 +0000

    rich minority whistle although careless

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def square_number(x):
+        return x**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
