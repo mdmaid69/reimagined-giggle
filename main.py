80256d2427ca7392c36c5b41e13ee035cab38c9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:59 +0000

    talkoutof backwards harm test month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
