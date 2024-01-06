734b9ff91edfbf4163a77e591dd05483c33d5468
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:44:06 +0000

    their unable only sticky land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
