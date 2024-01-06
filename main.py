fe005d42c47f62b415b1c3e8101a925b97936ea2
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:10 +0000

    either appeal convince appear sudden

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
