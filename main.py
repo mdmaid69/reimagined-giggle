bff6c991623c8f6faba26f031c3beb895fd5ba6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:49:42 +0000

    their equal eye still it

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
