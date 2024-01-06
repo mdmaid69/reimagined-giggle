e632eaeb510314ec5862f414259eb19630d45b40
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:46:35 +0000

    wander post consist trick instruction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
