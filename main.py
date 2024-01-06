d051ee16ec08fc934e7bbe047185b93cf96dfb72
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:52:36 +0000

    drink stir tell would knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
