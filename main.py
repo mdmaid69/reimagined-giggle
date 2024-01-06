59404c6124e72d2c320f6786cbf8e640823aedf2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:20:18 +0000

    herself Ww although kick teach

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
