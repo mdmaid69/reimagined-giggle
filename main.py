1987b3ab1886f51a7bdef47f2a4a2613caab484b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:17 +0000

    unlucky so list quarter fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
