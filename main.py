9cb5eac58faed67ae7e1105a93b039ba12fba824
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:50:51 +0000

    frighten maximum cut noisy suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
