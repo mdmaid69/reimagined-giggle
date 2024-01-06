9f4307bcb18ce9af07d83088dc756e2bfe2d7f0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:08 +0000

    drive fresh colour pay deserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
