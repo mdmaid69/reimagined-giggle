6d18a3609ddbbe45ebae179254eda8489b1ea1f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:46 +0000

    wash anywayanyhow many organize dream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
