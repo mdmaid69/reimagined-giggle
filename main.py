59c34c14a27a20e724c90af49ce46518129a45d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:34 +0000

    on Ll outside the clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
