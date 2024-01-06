4766ffbbda96f27bdedcfb30fe60e8bac6a80071
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:45 +0000

    result belong partner as thin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
-print(datetime.datetime.now())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_today_date():
+        return datetime.date.today()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
