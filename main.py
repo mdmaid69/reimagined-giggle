90e56693a9812f7b035accde634975737ac51c69
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:17 +0000

    deliberate turndown whisper lonely crowd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
