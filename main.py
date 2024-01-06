04064d75c8549d83b6439ced2f7a0cdd9e868711
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:47:23 +0000

    most nurse let develop off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
