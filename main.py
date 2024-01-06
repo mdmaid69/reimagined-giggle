d05f3c1427d26fd8c1d3e897ca96e9a344c5c3bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:20 +0000

    up store nurse except cover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
