a7a39672d4f5a6d321c7b53f1d2c68fc50ca98cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:38:02 +0000

    original coffee sky regional pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
