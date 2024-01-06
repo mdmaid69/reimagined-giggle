f2c42564c645e357c6d6c0eaeb7589e9e7a348d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:19:56 +0000

    season rest good confusing at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
