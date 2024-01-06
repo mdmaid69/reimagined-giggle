b1e004fadb8733bb92fbeb50911065c5de4ed581
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:59 +0000

    control scream uponon bridge hungry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
