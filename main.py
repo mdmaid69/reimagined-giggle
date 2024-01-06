738ea86e66d69e394b8a0e6f7fb8690075146df7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:10:03 +0000

    wherever safety shake display tooth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_palindrome(s):
+        return s == s[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
