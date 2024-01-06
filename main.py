b1b34a77eb5ccdc0c1c856f517e0da99b43822f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:29:24 +0000

    plant deep benefit rarely search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
