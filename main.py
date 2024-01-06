26776f5da5a12bd619587cb4dfb6614751c764b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:22:30 +0000

    movement border small persuade famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+print(os.getcwd())
