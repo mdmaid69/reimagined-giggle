8bb2a4168d381927feca6b7f198f06da6f3f3863
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:44:02 +0000

    young inform stone roof illness

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import platform
+def get_python_version():
+        return platform.python_version()
