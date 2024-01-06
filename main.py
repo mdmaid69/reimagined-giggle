c39601296a6ee923e150889e10aca20231a7f287
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:34 +0000

    machine bit excuse dream original

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_float(array):
+        return float(array[0])
