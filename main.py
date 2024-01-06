01e526bd6f0390b8a90e8ad71249798c2203cc0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:57:14 +0000

    increase while ill peaceful yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
