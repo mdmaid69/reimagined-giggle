35545a77b545a22bf14ae44587066d0a04f76106
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:07 +0000

    account improve list expression way

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
