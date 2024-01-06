e89e09f7757304c33b3d680debc3801c6845508f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:12:32 +0000

    takeout shake talk independent medium

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-print(math.pi)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_item(array, i):
+        return array[i]
