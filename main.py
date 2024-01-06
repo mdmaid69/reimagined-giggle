9d6be121b89cce7e6badfd5302684809118991a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:53 +0000

    mysterious whether wherever bite start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
