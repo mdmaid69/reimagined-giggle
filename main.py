f5f3a39a6f895ef39e5431f66d9518d1f72cdfa6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:50 +0000

    just morning influence risk measure

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def cube_number(x):
+        return x**3
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
