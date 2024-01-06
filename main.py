83392f297ee26cdf98c770c1dc112f3c97aeea83
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:09 +0000

    disagree separate backwards unhappy discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def add_numbers(a, b):
+        return a + b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
