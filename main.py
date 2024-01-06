57afe406a1c143fe4d9abbc484ffe8885e73abec
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:05:43 +0000

    thorough thick strong fall name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
