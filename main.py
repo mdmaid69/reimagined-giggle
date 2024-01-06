7282f8ee4b646fb1e636aaaf505a728f6b3718f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:25 +0000

    very pool rescue contribute bus

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
