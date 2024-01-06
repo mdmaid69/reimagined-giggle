4dfcbd9b09983ccfff33ade70c6da8f0d5764018
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:51:51 +0000

    bang tip colour bother emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
