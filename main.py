803179b6097760b8ad0b2cb0682bc805acb6ea09
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:52:58 +0000

    closely shout watch party guide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
