73ff703ebe185f06d3e763de6c7564ff434efbef
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:24 +0000

    lift hollow apply blind science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Characters:", len(text))
