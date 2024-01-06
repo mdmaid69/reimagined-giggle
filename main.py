c1599dca8e8ba071d9d3e6b699227a037878b5dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:12 +0000

    Uu else way otherwise discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import logging
+def log_message(message):
+        logging.info(message)
