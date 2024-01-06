a82c9492a084919cee22b76cdd73a53c3b7e143d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:18:59 +0000

    hurry offend loss last callback

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def reverse_string(s):
+        return s[::-1]
