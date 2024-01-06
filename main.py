2ecb7024b7cdf5fe9a50f2c54a093b5aafc1aa86
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:11 +0000

    lead similar modern trade herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
