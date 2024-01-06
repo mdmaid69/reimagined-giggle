5615bbd3fec25b02c0b3baba798299eb7bbe2801
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:35:30 +0000

    among mainly responsible intelligent improvement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
