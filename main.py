1921cb4a64d68d61892a88691a92fc3d8f0af5c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:09 +0000

    relative surprise history visit question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import time
+print(time.time())
