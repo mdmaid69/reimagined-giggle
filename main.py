b1c4345905a9fed7f4e798f5cbd462f0eedf57f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:27 +0000

    official command children clear interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
