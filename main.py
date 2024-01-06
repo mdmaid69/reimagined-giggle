31bd88f4d4139b541a880bc84d3efc65b8087cb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:52 +0000

    independent sing Ff arrange turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_power(work, time):
-        return work / time
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
