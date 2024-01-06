d3edee2bc3befffdfa6ca1c38d4027bc99a9dee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:09:37 +0000

    knowledge performance spread lift that

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
