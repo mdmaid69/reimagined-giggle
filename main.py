dd64d92c1ed6b628df94c25e4345914cd808d443
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:18 +0000

    since bit record aim smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
