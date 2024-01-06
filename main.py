a1188780fbf48cd9e3133ec982314f4f599ac1f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:58 +0000

    sore joke matter spare plenty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
