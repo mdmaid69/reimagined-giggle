f5f339c7549479e1800bd1633153fb9e6be92127
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:28 +0000

    bath object raw hardly of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_directory_name(path):
+        return os.path.dirname(path)
