43877b474e8ccb24a73f99becdd64dc0231d1c60
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:09 +0000

    apply alone everywhere thin preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def reverse_list(lst):
+        return lst[::-1]
