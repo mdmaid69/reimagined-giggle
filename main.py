70d6b9261199f1509babe306e54aa04061f28ead
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:26 +0000

    affect worried cruel attempt anywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
