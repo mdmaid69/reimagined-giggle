e9bb0729eaf049f840d70b9124b9deb7b9521c41
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:32:39 +0000

    disgusting sure somebody round lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
