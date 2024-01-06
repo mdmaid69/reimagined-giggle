071b63be393bb96f932efa4fe7029000f3bb41e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:18:16 +0000

    do offer set deliberately watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
