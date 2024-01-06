41614b1cc14ae0548f00fe64c6beaeb547f46522
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:14:15 +0000

    here reaction change often all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
