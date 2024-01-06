1b18111a2ba03b163dbe36aef4f03e90807366e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:59:49 +0000

    generous debt blind tear advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import random
+print(random.randint(0, 100))
