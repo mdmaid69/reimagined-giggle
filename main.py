f869c60231e0aff8e8b0364865e72ffc0bf89325
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:51:54 +0000

    attention recognize eager dear refer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
