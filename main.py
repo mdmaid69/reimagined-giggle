2ad70058a8f83303598484d88391e9b1fcb2a3b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:32:50 +0000

    noone mind original proof giveback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
