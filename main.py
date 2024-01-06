9499e9865da6465b4db806224424bb7c77717c60
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:09 +0000

    march either figure allow smile

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import sys
-def print_python_version():
-        print(sys.version)
+def find_min(lst):
+        return min(lst)
+  def convert_to_binary(n):
+        return bin(n)
