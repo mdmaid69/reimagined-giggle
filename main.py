15756ac34879625173f47bce9ccc846fc7d9fab5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:32 +0000

    takeoff pair then phone discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
