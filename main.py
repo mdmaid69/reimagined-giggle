112211ca3d0793a17750710ff4141c88432639e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:04:08 +0000

    the fast hardly decide error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
