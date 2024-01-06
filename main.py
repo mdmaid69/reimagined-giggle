741c9ee9ec35727dc2043d4315fc5254aab1be83
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:44 +0000

    date gift wonder decide part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import queue
 
 q = queue.Queue()
