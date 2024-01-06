2360b67f3f4a1e64e8934a52e09dfbb7f0777079
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:39 +0000

    praise test burn future cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-print(math.pi)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
