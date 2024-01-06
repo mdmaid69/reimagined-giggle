3a6238a5d390fce090922c315cf147d53099a787
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:21 +0000

    meal future dress 565end bush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
