ac72a83ed13aec124c37c3f214a8933af8a42978
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:44:37 +0000

    dry cost face car promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
