e3b2275738fe45392eda3ddedb8e933305665a44
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:48:36 +0000

    physical possibility law plain chin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_max(numbers):
-        return max(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
