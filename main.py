bb98fcc581fab9a7218a5a76fb85d70048ced975
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:24:45 +0000

    along do appearance dull direct

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
