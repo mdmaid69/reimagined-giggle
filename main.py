5f4dd8c8fc8fbb50a827be26186891c6944f8482
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:57 +0000

    ear pretend equipment depend characteristic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
