aa1cdd500b1b957271d83f31b32573f38f302c36
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:31:54 +0000

    speed proper method complete destroy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
