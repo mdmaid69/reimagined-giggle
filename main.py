d101d43c4b0b97d40bcd4cdbc5b8ce8ce81a4369
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:56 +0000

    mend complain put condition cheap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def reverse_array(array):
+        array.reverse()
