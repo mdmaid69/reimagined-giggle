cf951ae5f90230ce9923dbe18b48d12910d24b27
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:41:33 +0000

    fire trap that where actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
