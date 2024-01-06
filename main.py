4ec8f05ef26b2b9950243e7f2f43e8264ef95163
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:14:47 +0000

    shock poor understand surface clever

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import tensorflow as tf
-print(tf.__version__)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
