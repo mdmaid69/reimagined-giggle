642d4f72a46a1b2910bab04fdac3ace1c546ddf9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:45 +0000

    reduce classroom manage rise any

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
