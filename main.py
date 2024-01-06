e82d4e8f4f46400c49f4b04ac154d81c0ea3162c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:28 +0000

    system careless breakup pronunciation back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-import os
-print(os.getcwd())
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
