744e04053f422be52bcf7baaa49fbbde031f32f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:54 +0000

    bring exercise greeting lookup fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
