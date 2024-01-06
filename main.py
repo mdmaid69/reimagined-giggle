2de804e1cc53416ed3e686e3f1cea6c23417fa08
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:10:13 +0000

    stare gate nervous message takeback

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
