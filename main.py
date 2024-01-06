e498ee0f2c824dd8d148d8ce979c58ef5d2ba7b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:18 +0000

    die boring row advantage 179bite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
