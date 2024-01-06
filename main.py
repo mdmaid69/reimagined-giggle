61a71e341cbb0cc143620745ea68732eeacc8aa1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:24 +0000

    pencil up choose up machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
