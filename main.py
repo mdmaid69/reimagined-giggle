980d687ef054c4f23d8abbef50f4e75faff84d12
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:32 +0000

    neither argument song guess clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_density(mass, volume):
+        return mass / volume
