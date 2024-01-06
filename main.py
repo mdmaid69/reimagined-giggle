faceeecb39095a26f321b2c0318c6a9053b31f8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:23 +0000

    tense bill demand hang stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
