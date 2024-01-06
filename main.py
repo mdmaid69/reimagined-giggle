190ed9b518b2bf9d2927f57d8d11cfaf20d75135
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:40:32 +0000

    Ss minute until loose change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def cube_number(x):
-        return x**3
+import array
+def get_array_as_set(array):
+        return set(array)
+  def convert_to_hex(n):
+        return hex(n)
