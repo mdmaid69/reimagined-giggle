6dc329caad117f0f99a486da396c5dca35a48f55
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:34:21 +0000

    leave effect voice shut equal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
