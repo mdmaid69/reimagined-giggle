6ce48c4e1bb2eaad5f313632edf6596e11e74f16
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:29:04 +0000

    discovery shout mean treatment bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
