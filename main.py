8ba7c91e225675d1a1b3841d05f1af4d72870b22
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:26 +0000

    shallow steer several busy set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def remove_duplicates(lst):
+        return list(set(lst))
