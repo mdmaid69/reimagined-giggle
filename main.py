279e6ecc5447ab5bd05baf79531f795a3f4adc52
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:30 +0000

    bringback great problem should last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_index(array, item):
+        return array.index(item)
