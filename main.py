9af19af754ec957ece1db729dfa4aaaa9a665bdb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:13:14 +0000

    Cc powerful pool glass satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
