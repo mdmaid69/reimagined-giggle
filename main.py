c749a221c4c0f3ce3c4184e9e2018a7b7b11969e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:58:15 +0000

    write comeover precise activity beside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
