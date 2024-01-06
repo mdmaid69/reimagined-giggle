9de358cf735dbfeeb8d3ef1ff1e08d872e729f6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:51:11 +0000

    interested post workupto compete centre

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import logging
+def log_message(message):
+        logging.info(message)
