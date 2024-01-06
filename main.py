45a6ff3e506f6ce5fec1b29fe7c96112d9b47d8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:45:46 +0000

    Gg loss rank complaint danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
