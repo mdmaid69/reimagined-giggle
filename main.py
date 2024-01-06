db7641f94bafb55c6e8c2294d6ea5790bfeac03b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:39:47 +0000

    fall correction street cause vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
