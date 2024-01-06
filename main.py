bc15b785644282cfa0efa6e02e21c1c84df73177
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:32:58 +0000

    tomorrow statement skin inside report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
