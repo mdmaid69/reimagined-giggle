dba8f732d8895eab19fd02e130a2df23eacdb39b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:03:08 +0000

    guilty giveback hurt though frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
