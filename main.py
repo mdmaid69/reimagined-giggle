9713f6a76c34a2b84f798440e739547efbb28013
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:23 +0000

    cause nice dry heart start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def reverse_string(s):
-        return s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
