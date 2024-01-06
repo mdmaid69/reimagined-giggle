45441d2e924934ccdea19a8ef2ad1b9d09fd0eb9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:06 +0000

    experiment butter cry anywhere style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+print(os.getcwd())
