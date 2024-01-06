32da7d14bf09d3b1bf1718c473cd203ec7f96029
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:48:14 +0000

    service legal from forward lookback(on)

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import re
+print(re.match("h.*o", "hello world"))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
