597424b7c8df5f65341990d653295e014f7d7a4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:40 +0000

    easy sound later taste computer

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
