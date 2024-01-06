ebd22483ff3beb40805268b288611eb2d8aa71a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:33:05 +0000

    visit against thinkof song arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
