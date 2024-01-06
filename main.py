766be4b4b2517e224758913361e2592a17ce0021
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:58:22 +0000

    recent prevent clear formal generous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
