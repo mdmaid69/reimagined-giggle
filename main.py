687bd73827dbb03767cceb86754f5df201c0bc5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:54 +0000

    classroom spite consider corner cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
