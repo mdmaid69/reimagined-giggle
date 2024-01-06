99ffae0a3a33a241d00f4ca8be00540fd54680e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:04 +0000

    burn burst law office and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
