c65afb2e693fa68a1a1a43274cc583ef4ad0e9e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:34 +0000

    original act Pp last advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
