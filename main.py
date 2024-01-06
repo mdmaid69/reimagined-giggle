ee592917a611b8fd7c9a44d2fcaf50deb26cb7b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:34 +0000

    damage mad I note regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
