97870ca0873a26118ea5a065befd8148a3b7d615
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:07 +0000

    lookup actually sit criticize mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
