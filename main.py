46d858c3afb45388427f6afa4e1eaafee40fbae0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:13 +0000

    share sit cross soil giveup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
