21eff14390275407a51fe1d4a8fe950668797da2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:59:04 +0000

    before pick shadow long float

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
