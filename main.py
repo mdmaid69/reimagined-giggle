fbab52123c282ada893e80b0436576696f5ea33e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:32 +0000

    back attach win return speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
