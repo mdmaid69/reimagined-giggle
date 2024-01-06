a36d65405d43aada85e4e6f76f74db9f2887051e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:13 +0000

    fear male by especially message

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
