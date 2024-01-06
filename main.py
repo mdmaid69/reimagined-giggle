e47c1c5a7b9254bbf78d3edae5f7d5ae24ee81ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:41 +0000

    poison rush university uncertain learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
