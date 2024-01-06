1871a48f4ef5005f5d5b04491c72cb27943e79f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:36 +0000

    second nature neat information sense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_length(array):
+        return len(array)
