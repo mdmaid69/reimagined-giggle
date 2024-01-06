f1f842416a9b3476c52d014613f8fdbd858c9078
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:54 +0000

    passage train practical fall final

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
