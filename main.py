09e1c664d95de4a36f161c8994d228b064717e2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:54:54 +0000

    treat choice success many hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
