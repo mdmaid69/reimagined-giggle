79314625255cc7cb50fd6393d6e997aea13da462
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:40:59 +0000

    regard glad emphasize limit material

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
