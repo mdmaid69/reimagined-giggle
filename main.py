0ef679b061f643499d43a9ad13b33c03c44f2e74
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:24 +0000

    full effect empty glass divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
