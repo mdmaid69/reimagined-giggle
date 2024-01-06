67696e1ddeb6806b6b6e3f688ba6ebab7ccd2656
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:30:29 +0000

    unhappy getoff hope write Itself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
