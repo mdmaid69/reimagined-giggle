fadad1ec747ae59c5c6cb7d45bd434b68dec7834
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:34 +0000

    burst pleasure commit necessary spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
