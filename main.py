bf853d630015f86dbc4499a5b4b594ca25a196a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:17 +0000

    begin Jj runaway embarrassing intelligent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
