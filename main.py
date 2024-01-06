4d10062794bd08c1eb797df2b3b1b2f4ae313fd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:55 +0000

    pick cheek giveout connection later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
+def convert_to_octal(n):
+        return oct(n)
