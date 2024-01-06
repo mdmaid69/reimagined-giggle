0ba42d5bb1485cf09ea11dd293c7964a0fe639bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:18:57 +0000

    Itself decide block that passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
