63d39550df72c440573e5f08f6ad2d15dd4fc1b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:13 +0000

    already attract chief alone beyond

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
