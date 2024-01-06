9f9eb669c3ff420c5fd89e3390565c2dc3a6a532
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:38 +0000

    accidentally miss affect nearly brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
