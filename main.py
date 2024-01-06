d882ff36789268ab9406c72bc5d8b104b0f560c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:16:00 +0000

    damage control woman sense mend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
