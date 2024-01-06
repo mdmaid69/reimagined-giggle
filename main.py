14e10abcbfd69d9cb559e3f1a0668bb23c628a77
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:10:21 +0000

    stop imagine grow complicated turn

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def add_numbers(x, y):
+        return x + y
