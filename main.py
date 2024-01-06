52da97d941d46e354f252e7096a2c442ae755a87
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:31:42 +0000

    crime pronounce furniture breakinto over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
