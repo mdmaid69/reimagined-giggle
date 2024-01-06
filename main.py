039864132d3e576f5b191a769daae918e93f8549
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:21 +0000

    order make murder organization book

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
