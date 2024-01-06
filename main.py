704e0fcf8f6bb2803e6024eaa5c7ee7e0e4acad3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:11 +0000

    than build behind miss involve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_stack():
+        return collections.deque()
