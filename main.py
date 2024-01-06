56d56d3f2d7595d96ec45403b178b9252b182fa8
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:17 +0000

    telephone Itself scale measure prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+print(math.pi)
