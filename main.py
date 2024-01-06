78347fe77d16ee6c68ab145e5fc8c5af0400294c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:15 +0000

    gentle recommend question keepoff use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
