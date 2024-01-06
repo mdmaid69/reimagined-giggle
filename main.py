3f09deea02f68103003777dd81c97a4a112e6ccd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:23 +0000

    cook pencil flat ilovedicks noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
