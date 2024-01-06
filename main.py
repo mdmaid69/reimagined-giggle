0af17ddcf8bc1103d6fc977fa5a2bd5becbde08a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:06:25 +0000

    reliable repeat awake Yy gas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+print(os.getcwd())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
