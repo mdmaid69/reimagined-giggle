b6350c185e0a502c8e244deba85020b2545d5036
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:36 +0000

    thinkthrough start speed wait occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
