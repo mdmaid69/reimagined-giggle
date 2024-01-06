15fe3196fabf4703d0b2dc29256eda78b004b29e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:39:24 +0000

    570enjoyable lookaround fit try design

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
