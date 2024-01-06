34d5b39fa7e35bec1960922096bedb0e4dd3a550
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:46:09 +0000

    fear ever provide thirsty month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
