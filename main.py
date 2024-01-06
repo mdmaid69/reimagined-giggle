bf5fbfb6e088676d8b5990f91f9a9968922db857
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:15:43 +0000

    brown pollution classroom attempt shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
