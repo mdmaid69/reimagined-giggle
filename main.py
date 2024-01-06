851b7eef5d991a07b0188bb5f87e253583a98aa5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:33:55 +0000

    declaration confident whistle advanced area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
