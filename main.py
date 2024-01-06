2b73be4196fcd6b7acff1ea325270db3d1c289a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:33 +0000

    last succeed giveback recommend play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def find_max(lst):
+        return max(lst)
