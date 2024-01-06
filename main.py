9cae709eeea9be616391c46c8a7f01321d085c90
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:30 +0000

    noisy sure dress move word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
