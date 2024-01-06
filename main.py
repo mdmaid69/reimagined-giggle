c4bdf31aaf323fb7d084902b50711edca2881d1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:49 +0000

    concern finger underneath communication meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
