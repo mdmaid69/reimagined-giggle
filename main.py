b68195beb2f8b66b96dd0278ed7c199faedda711
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:52 +0000

    switch tie fire Yy money

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
