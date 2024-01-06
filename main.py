d2c08bd58aa378c179f40d79f43a1b98d1f916b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:17:51 +0000

    imagination either poison about nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
