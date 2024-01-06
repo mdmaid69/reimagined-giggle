c8f04fb5ac7f288437295708370d3edd7fe4306a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:19 +0000

    grade blame trouble hold shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
