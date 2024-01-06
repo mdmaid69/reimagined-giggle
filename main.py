f933e3bd4d8b10c753725638ecf101bdf20c101a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:58:00 +0000

    mine though kill pass example

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_area_circle(r):
+        return 3.14 * r**2
