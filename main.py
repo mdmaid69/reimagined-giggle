f174654c140c78709e6e50514c4bec1e4c7b4db5
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:29 +0000

    round goup over flavour ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_unique_words(sentence):
+        return set(sentence.split())
