6f663b26918b6086ac5e064ac71dd9a1e40581db
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:38:20 +0000

    principle blame mark wander trip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
