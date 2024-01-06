161ec44f87b89dc6c79166d66d4c9b88b8da7f29
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:35 +0000

    unit pull ought sometimes young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
