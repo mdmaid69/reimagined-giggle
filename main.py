f44889480a46aefa1479e932e4b1450a655c9783
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:42 +0000

    butter stick loud estimate breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
