a01b8e16ffea011fec7de4edfdd465d09c59f391
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:43:25 +0000

    bet border run score runout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
