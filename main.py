749a46fae45403d7e4c3c415844e0aa8ffce16ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:19 +0000

    lend step laugh include drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
