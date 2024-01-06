58b2843f27b025db267ecaf4b169bba50ed8d1ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:11 +0000

    outside everybody plant food within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
