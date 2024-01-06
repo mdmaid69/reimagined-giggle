be2252588451d31352187a100fa0dc6298f6b0f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:57 +0000

    dull anyone quality handle succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
