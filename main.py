833ebafb851644b4acc36e8e9707c459ecd79d7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:27 +0000

    level study tell comeup comefrom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
