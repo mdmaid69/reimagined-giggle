8ba2b3793274d6beafc336738377a8d4068431a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:23 +0000

    base odd patient belief 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
