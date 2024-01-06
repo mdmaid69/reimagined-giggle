be842be92076a018898a3c7dcc5771cfb1ea64fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:41 +0000

    Zz actually long tidy either

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
