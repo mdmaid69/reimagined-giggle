446f32c6fd0649c4e25769f8cbb9e27ce6b79de3
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:21:24 +0000

    sticky move be expression kill

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def multiply_numbers(x, y):
-        return x * y
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
