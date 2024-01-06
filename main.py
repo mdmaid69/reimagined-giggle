e7ea10beed064bab04e8450dd1087fc0d3f38853
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:20 +0000

    pay nothing cause idea fault

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
