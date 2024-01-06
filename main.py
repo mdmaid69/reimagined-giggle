a810a6b887aad555549612abb381e800f177a62b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:12:19 +0000

    want need drawing stage getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
