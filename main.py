6941d57e19daeb89b338bfc8b13a7ecda1df9d9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:53 +0000

    after equal in upwards surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_date():
+        return datetime.date.today()
