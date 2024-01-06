f5837788e048252c147feac78a9f5efcc1695c94
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:25 +0000

    crowd music season university flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_speed(distance, time):
+        return distance / time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
