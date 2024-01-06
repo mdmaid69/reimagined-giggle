11f298336f4bbb4a03fe4fb74a3f97c2731bce53
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:07:52 +0000

    improve break legal argument shade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
