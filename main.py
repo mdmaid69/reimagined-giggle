c2d88eca155f32c654e7c18aee94ff194d7c2b1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:56:04 +0000

    wrong law look deserve slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
