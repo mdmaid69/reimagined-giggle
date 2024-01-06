f7cade31733feb9d6d59ee4d27e82b438b7f85ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:58 +0000

    live friendly himself own at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
