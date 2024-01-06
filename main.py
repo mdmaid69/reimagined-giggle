58385acaabc2632feac2c1af627de529db19c372
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:23 +0000

    clever can drop protest lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
