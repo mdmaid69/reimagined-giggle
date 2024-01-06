d42d420875f7e0f6bd59de8573f5b622741f4f26
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:49:09 +0000

    special arrival business practical hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_tangent(x):
+        return math.tan(x)
