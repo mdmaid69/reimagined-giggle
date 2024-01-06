b68f21e0580812d85133adc76b80b9fef58bc343
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:18 +0000

    waste boring agreement difference keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
