507ad97a2a7d65232eaf92d7e35de6829e1ace15
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:28:19 +0000

    degree operate hair note compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
