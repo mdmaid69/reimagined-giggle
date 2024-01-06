5427f84d0c6a42efb04c2060da47a34ba9e87351
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:30 +0000

    tip low attempt lot perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
