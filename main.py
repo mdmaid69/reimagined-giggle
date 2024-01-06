2cd6c51c68e8a2e0b8bfb39ba87c65b8de98e8ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:55 +0000

    difference weekend in such pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
