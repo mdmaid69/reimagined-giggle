edf9b6e87a574fae788d337098ba9287145f5263
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:15 +0000

    still subject transparent swallow off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
