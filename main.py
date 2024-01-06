4abbd2d4dd380356d6425db5ce8d9bc9b356301d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:00 +0000

    question attention whenever pull happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
