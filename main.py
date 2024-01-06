ed5d590de21cec97acd47e9f3442ec121a992a8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:29 +0000

    urgent tune favour address positive

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
