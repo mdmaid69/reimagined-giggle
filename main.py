e06c580c6fbdf0a6217d376f03884040d33d262b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:57 +0000

    pause fierce along behaviour crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
