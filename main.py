4d95ab18c6864c869f3730878fa0b51bdf45737b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:26:49 +0000

    vote effect division quiet fresh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+for i in range(10): print(i)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
