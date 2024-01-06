3dc16dd17ebb3edb3f12b541dd12132cfc66d2f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:57 +0000

    shall thinkover process language sit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
