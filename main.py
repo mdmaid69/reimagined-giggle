0082fd977ef12b3ec2746426086e86ed8b1554a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:08 +0000

    ride normal someone especially pull

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
