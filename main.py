cd4cdf06ab345c2cc008e93d05c81ac864bdc6c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:22 +0000

    proud he sail left open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_length(array):
+        return len(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
