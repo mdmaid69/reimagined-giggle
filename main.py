e4c15e598e1e8f41748149a61f300842c03bfd96
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:08 +0000

    friend so unusual score phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_list(array):
-        return list(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
