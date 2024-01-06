07e32cf21274d779f08da512499fcd301b7b2e2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:14 +0000

    rely freeze nor rub gate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
