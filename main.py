4d4f5836a3d04dfd44a504afdc42c892b8697a91
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:49 +0000

    actual empty your dry challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
