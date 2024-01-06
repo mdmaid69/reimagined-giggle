1555a66979199d53b5e5df36257f87d8e99896e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:36 +0000

    gift arrival cool bother stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
