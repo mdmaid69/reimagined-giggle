309e656c51c46d3e45659b57e831c5a97d007ede
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:27 +0000

    neither off invite Ss house

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_min(numbers):
+        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
