6bf8531d4758873292395a653fda0afc698411e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:47:02 +0000

    lookback(on) region conclusion weak at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
