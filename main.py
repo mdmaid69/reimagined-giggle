5cefc4e10702941324574cc80dc42a0f4834f372
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:52:03 +0000

    tall begin used fold bring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
