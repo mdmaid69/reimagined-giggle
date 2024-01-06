3bf8992da2dbdcd1f2347c498ab75b8016786d3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:32 +0000

    question married reply answer act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def remove_duplicates(lst):
+        return list(set(lst))
+def sort_list(lst):
+        return sorted(lst)
