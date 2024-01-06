b85e20006768ee65ba1f542cc06901a16cb581d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:32:12 +0000

    keepout hardly ordinary whisper interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
