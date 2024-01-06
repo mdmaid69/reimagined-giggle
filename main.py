e358c441589194ee5249fa5a0e46d57a13364682
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:46 +0000

    arrangement remain crowded discover on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
