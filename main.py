38c6cc17bfc288d3726e756d91dc152344acd602
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:08 +0000

    probably ashamed by smile so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_error_function(x):
+        return math.erf(x)
