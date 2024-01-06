33f08dff1e56630638e42c274082c3b977a0c92e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:09 +0000

    room learn Qq listenfor class

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
