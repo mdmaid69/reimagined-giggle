494ef7220250e86ff8d1ec0a27c9f15af0409766
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:02:32 +0000

    snow entry pour say sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
