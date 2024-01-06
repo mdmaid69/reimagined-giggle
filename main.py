1cd95aba3cc5ea67785678cdce310a5417535c93
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:52 +0000

    not later charge mean beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
