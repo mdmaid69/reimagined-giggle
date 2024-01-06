f2498eefa2bb471f316e1de51f23eca12fd6ea47
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:06:18 +0000

    team Qq prefer characteristic plain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
