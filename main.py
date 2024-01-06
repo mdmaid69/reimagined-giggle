48776f1fb37f302b3b1b14024d5ec754aa34c205
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:20:32 +0000

    long complete if comein public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
