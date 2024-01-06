d00b356fbcb47a4f7c3ff3703226f7660560b54c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:20 +0000

    visit tell story awake contrast

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
