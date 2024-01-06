9fea695d4fe05c3238d44cf1119cd371c3031146
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:27:29 +0000

    thanks exhibition ear serious share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def greet(name):
+        print(f"Hello, {name}!")
