f1d110e8c5d65d88681e5b3838ea09fc71d7432c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:36 +0000

    pretty offend physical main tongue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
