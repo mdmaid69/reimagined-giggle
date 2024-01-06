37a98ae63df6c4bea4e6346301eb6fe7efa50368
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:48:51 +0000

    shout tired criticize today pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
