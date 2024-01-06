df5d606511971e92f436a46192a8eb5ae8196f60
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:19 +0000

    body list permission Tt deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
