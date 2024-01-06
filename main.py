c02ad526af927be9b2b5eefa9519ace2ccefb1eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:19:11 +0000

    test plan meal thin statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
