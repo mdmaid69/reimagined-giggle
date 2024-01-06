b0d8b046fda8238933f5e6f8905abfe9b10c9b3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:07:46 +0000

    secret display handle rain plenty

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
