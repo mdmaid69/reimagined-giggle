062957231a495d8dfaa0a41fb43cd07546d5ed17
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:07 +0000

    steal plastic regional mistake true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
