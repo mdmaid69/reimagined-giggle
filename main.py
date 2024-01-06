8914c73cfc67254d419f595dce106eaf3cb81b0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:13 +0000

    pattern common body war century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+print(datetime.datetime.now())
