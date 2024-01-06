5aacd39f9fe70fc835c0319ec58a315e7675b5fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:41:28 +0000

    born 572enter summer family rather

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_work(force, distance):
-        return force * distance
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
