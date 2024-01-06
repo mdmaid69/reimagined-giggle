f367c719102f7616a5b7cb22d1fde1b0d682946b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:49 +0000

    up taste club often future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
