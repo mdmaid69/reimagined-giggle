6253ce106bb8fe2cd9b40bef18c29b6778a0a1bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:37:54 +0000

    gas ilovedicks play wave content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
