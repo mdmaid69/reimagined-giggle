86ca3f119990a2686f87b4a5c0ae50822dfacf11
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:53:41 +0000

    excited interview off evidence run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
