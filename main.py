1ca5e4c73bc0a6da6f7fa48b9e2f4258a5e97e1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:37:43 +0000

    interest near accidentally rough along

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
