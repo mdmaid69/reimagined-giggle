f403c8efec5a9f3a8674fb01b1b6e31b79374d13
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:13:32 +0000

    bringforward nor strength become undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
