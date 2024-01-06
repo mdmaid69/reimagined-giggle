7ce45d420b21d7d1a32e4d69a93bc1b899f056c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:11:55 +0000

    say bother risk no next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
