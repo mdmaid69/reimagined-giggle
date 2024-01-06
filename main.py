a25dc1030bd27741632e999ff0ad8108bb46fc89
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:38:59 +0000

    sink continue production help sink

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
