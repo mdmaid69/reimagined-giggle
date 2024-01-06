ff42f00cd7a914a28b549f6d54c7fb3ea3c7a933
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:57 +0000

    narrow journey similar subject uncertain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
