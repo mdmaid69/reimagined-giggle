7156abdacd4fde2618973271dc02249419e45a33
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:09:49 +0000

    special around escape object little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
