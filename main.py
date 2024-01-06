87c74984d51deb206a9584d75ce3908f8d74df68
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:29 +0000

    succeed basis slow handle live

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
