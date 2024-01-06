78f61f4fe1e02d718618a78d5e87d3100ab36eef
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:42 +0000

    along since risk final mind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def multiply_numbers(x, y):
+        return x * y
