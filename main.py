8511221b2aa1c4bd0fe03aade03f5848732765da
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:25 +0000

    row sing anxious inside science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
