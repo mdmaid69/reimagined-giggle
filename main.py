9d8f5fe69c827ec157572c501f8f60e85ff10cb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:12:41 +0000

    present somewhere factory offence language

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
