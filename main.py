72345086e17973e83c77815790bfdafe45341c17
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:33:48 +0000

    admit respect unfortunate beneath suggestion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
