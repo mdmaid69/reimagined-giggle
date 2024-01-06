5c69ce95940671170fa794bcc8387cf3af101bc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:33 +0000

    although gradually life rain gap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
