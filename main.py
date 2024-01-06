ef438fc6891b98bcf4193fd9110190bdd2547e6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:52 +0000

    bag keen sail near down

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
