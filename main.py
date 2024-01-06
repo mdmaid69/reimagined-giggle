feb2f5188f0b7278b5f2fbab073eea81fb002f2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:39 +0000

    mistake still kind shame respect

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def convert_to_hex(n):
-        return hex(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def remove_directory(path):
+        os.rmdir(path)
