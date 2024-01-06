7663bd1a6fa71be1f2aac2d457fff6de16752fb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:43 +0000

    Dd buy middle hit track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
