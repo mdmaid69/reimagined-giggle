9f510e69a230b3dfabf26dbcdc9e11152ae0bf66
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:51:44 +0000

    business talkinto term rain port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
