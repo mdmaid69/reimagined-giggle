c4eff910ea6220e421a429543e322319c8076c10
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:42 +0000

    middle innocent introduce accidentally talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
