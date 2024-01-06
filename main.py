71c0a427aed5e1ab850dba2559c449d61c19eefb
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:29 +0000

    intelligent everybody destroy fast approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
