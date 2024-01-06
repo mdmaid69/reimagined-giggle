93a8dcadf05b1f3c1fc0ab0b2d429c41dd0ee189
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:18 +0000

    related anywhere television row fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_current_directory():
+        return os.getcwd()
