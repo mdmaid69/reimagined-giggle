37c602845b338529295eaeca9e3bacc318ab3719
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:25:09 +0000

    workout warning scream term centimetre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
