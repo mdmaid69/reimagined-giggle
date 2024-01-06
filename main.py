7c555599ed0ed1ec439390a63dd2a4123266fa35
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:02 +0000

    thinkthrough turninto hurt boring produce

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
