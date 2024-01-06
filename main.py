a3a133e771ae152a4e959d58aafbba36719ef3dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:08 +0000

    pronunciation closed getout disgusting cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
