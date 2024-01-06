f144f1145ee5c5cb659d67027c713469b85f3749
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:25 +0000

    by noise handle activity material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
