59e491acda70c328da7dfc9bbf201250b73a6291
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:18 +0000

    impression immediate respect method lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
