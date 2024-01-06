e0790684607d1e38c91b520945ed50536ef3d6f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:57 +0000

    information spoil region visit crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
