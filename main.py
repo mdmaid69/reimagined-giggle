121c4b6e691e68971b14a662e9c78078f217c66c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:09 +0000

    delight makeinto operation shallow earth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import sys
+print(sys.version)
