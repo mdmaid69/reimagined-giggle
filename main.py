8cc9ee3b9966e7cd994151500a7ee664f25b37c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:55:13 +0000

    tool whole rest possession part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
