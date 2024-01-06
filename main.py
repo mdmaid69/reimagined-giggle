7ef8cb153ccfae02eaa96fb781c6d7ce7026620f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:08:24 +0000

    force file over nerve Ww

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
