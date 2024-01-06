ba4f231160f290a45c32d30788ea277df258e8fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:34:43 +0000

    tour round container 568energy pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
