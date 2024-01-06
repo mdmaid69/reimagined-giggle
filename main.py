24db22764467f00ac3b6d889c9a024eaf8224053
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:56:00 +0000

    put either regular major upwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def calculate_speed(distance, time):
+        return distance / time
