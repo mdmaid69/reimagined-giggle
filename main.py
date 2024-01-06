6aed9286329f4c7fc1a04544d07f54edd6091164
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:37:58 +0000

    proper under thank possession operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
