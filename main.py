c88c0585486d2fb56aa38872738f8c97102f531f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:52 +0000

    so concerning swing hard temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
