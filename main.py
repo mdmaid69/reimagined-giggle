55a566e2f65d6af7225e6c0606dd038b8da49cdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:05:38 +0000

    put trade obtain bus shut

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+print("Hello, world!")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
