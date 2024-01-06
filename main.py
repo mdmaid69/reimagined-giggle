e9cc89f86f77d590ecfe321a751ce11ac2f368df
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:14:11 +0000

    will none welcome scream art

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
