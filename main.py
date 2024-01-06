4dd002a1927530c4a0c20b391ad5a3bc82197142
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:17:27 +0000

    upwards interruption describe find treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
