6a23af4f4b884eb672963537bd0438a5a1b4960a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:57 +0000

    behind change ocean sensible turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
