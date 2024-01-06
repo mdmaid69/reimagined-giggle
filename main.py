fec6dd6c2540a6f443e093a503b3b8b4d5174cb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:54 +0000

    judge right judge experience commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
