cbc0c24c088d3f7714df0cb5ceb3b54ae0f64899
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:44 +0000

    report chief extremely raw apparently

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def convert_to_hex(n):
-        return hex(n)
+import os
+print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
