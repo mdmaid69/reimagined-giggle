45791725bd6e9c40a0ee11a5de31f486fb375699
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:31:26 +0000

    address tough sad row summer

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
