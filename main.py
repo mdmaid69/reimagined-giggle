a5e34c7720c3b5bb5f5902171a19bef2a0e79ac0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:37 +0000

    family run enthusiastic disturb work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
