408f0c69cd853dd02b8355bfeebea0bde076117a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:06:27 +0000

    tape frame drive principle visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
