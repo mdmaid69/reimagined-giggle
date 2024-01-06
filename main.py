ea13a1f3527b3d2fbaea2c2d14d7bdcaf30c9d85
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:30 +0000

    bear middle exist shape goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-print(math.pi)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
