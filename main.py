f5fe9f8bc70a3f766f22e8d1f11b7b09f2a2f577
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:38:47 +0000

    wherever send create lock soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
