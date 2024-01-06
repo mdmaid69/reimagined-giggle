3299d915b2be6312eceb4c3200c0829215087535
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:44 +0000

    mind outside getoff typical mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
