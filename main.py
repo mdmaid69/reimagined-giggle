8d5bb2ff704d4041272039a57ce21c6bfaf92b17
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:31 +0000

    relief nowhere tap expensive aim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_binary(n):
+        return bin(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
