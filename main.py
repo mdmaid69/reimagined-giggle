d9df61b2c1391143d85dca6e763b59f23cd2ae31
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:02:21 +0000

    win frame bother return light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
