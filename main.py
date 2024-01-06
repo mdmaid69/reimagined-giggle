87ac027d28dd21a279b306b1057458b4066b4ddc
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:08:28 +0000

    giveup Ll fresh she hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_power(work, time):
+        return work / time
