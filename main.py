bf68884754321e0965634cd91ecf134c93b724e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:07:05 +0000

    worry read talkback neither shout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
