6b5ce67d22dc126ba0280818524274e4bb16b2cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:12:33 +0000

    drive long bored total service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(lst):
+        return max(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
