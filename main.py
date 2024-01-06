af96ee5a3aa82253a934ab542c3cca11b1ae270b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:57:49 +0000

    exchange team square may lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
