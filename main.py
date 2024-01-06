393d78ff149a9134929d3c2951ddbafec9242402
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:27 +0000

    standard about something little equipment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def add_numbers(x, y):
+        return x + y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
