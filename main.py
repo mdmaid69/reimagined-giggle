d67d2706a25a04613306093d030f9502304c286d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:56 +0000

    grade profit meet breathe however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
