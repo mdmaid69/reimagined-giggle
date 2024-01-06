d4214e6be9626343daaf13b99ffa98c230468a93
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:14 +0000

    depend expensive whole again ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
