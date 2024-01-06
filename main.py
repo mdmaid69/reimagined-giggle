3d850b7e1914e3967c2ef1cc61f68963bca0dcce
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:20:55 +0000

    comedown eye office allow back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
