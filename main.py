96f0b410579e43538211d591e88c44104a9593c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:54 +0000

    other used failure music bang

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
