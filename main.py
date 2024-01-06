89e7334079abd33eadc6a34162f9083e8a51a5f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:02:31 +0000

    exhibition estimate offence breakout through

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
