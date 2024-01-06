71c4e3079604c8c0c18cefbac909d8b81398caef
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:23:05 +0000

    spread tooth regret apparently record

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def convert_bytes_to_array(bytes, typecode):
+def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromunicode(unicode)
         return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
