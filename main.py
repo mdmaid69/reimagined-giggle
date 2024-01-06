745cee6e1c8ec8bf69b6cf88c9d3b9ccfc6cef51
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:05:33 +0000

    respect connection neck file congratulate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
