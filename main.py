90c4c3014287100802f086e22ae314ff9eaa302f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:23 +0000

    air worry judge table getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def add_numbers(x, y):
+        return x + y
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
