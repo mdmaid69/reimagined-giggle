366f9f6bbe8379f507ab4bcc4a546bd46c043d4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:39:44 +0000

    keepout herself label yes accommodation

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
