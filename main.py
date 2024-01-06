48ad9123f615937d58f268a87c265889e28019d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:41 +0000

    gap doubt try total flow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
