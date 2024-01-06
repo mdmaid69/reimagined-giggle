4fc12f0f0e5afbf2646c86fedbd18576dccfc883
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:53:53 +0000

    rub tragedy content describe language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def count_elements(lst):
-        return len(lst)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
