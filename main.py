c0ffead878e982132236d33e333690a0c1d17934
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:25 +0000

    yours proud Jj dick bring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def find_max(lst):
+        return max(lst)
