72e016c9ea8f20d9f2766bcb98e875e7c6b9240c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:39:36 +0000

    tidy better event less sensitive

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+print(sys.version)
