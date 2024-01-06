a6599a2d76c6411ec778063718ccc702c9d92c96
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:33:02 +0000

    568energy file break neck floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
