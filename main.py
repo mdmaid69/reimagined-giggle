4d08959cf60bfc5aea95c91d4e7b96a66c30f90c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:37:18 +0000

    star together finish protect pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
