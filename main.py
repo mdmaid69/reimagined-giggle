a72fdfb6a9b26fe188b08e87e9383f8e9da9594a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:36 +0000

    fresh firm quality beside breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
