c7414b8a0773e4ad88264b63e2fe18004c7b59bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:26 +0000

    vision above government bet software

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-for i in range(10): print(i)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
