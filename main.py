1865fea9da2cecb9d59a1a21e369f69a33ba9e7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:40 +0000

    steep yours act under possible

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+for i in range(10): print(i)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def clear_array(array):
-        array *= 0
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
