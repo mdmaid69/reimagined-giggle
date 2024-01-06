74198decb0bac3013a3a94936b71934fcc54649d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:14:21 +0000

    below row takeback wide cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def add_numbers(x, y):
+        return x + y
