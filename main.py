fbdcf48d75924ed1c351421ce357fe993a1c5c17
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:15 +0000

    story very floor empty plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
