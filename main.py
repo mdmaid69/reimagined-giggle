e69efdfde03ef332a4479592eef9cc8014e5c65f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:40 +0000

    through sure formal suffer before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_length(array):
+        return len(array)
