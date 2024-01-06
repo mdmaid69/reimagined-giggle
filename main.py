a0d8682686976f80af5a74d8ec01c798494f3739
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:56 +0000

    bunch mail Bb wander home

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
