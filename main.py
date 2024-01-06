55b35ab8af3b4d4da56755fde9bb8801a5ccfd11
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:12 +0000

    second sea environment suppose achieve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
