5fe673e7587529e5ed74ba2dac570ade2f22c601
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:57:10 +0000

    person care level invitation scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
