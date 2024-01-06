fa76e2ebbcf5f953c82e9defdc10be30abda0609
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:17 +0000

    tired spread detail live dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import array
-def convert_array_to_bytes(array):
+def get_bytes_from_array(array):
         return array.tobytes()
