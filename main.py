b5145af27cdf9c570eacba99190e2faf01a6546f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:04:54 +0000

    shape regularly apparently regularly first

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
