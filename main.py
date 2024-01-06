f9b852aa94c4c7e37389daa352cb7676a25de847
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:35 +0000

    order demonstrate contribute light guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
