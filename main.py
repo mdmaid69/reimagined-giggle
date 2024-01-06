2feab20e992ae48bb095526c9a47789b8c11ec13
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:05:05 +0000

    rain niggaacademy unlikely shallow near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
