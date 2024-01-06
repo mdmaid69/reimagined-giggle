ff0291c166e5ff0e7569a93a112bea015de988c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:06:37 +0000

    rise pronounce music godown government

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def remove_directory(path):
+        os.rmdir(path)
