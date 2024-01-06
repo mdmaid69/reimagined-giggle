80d09f75deb3d740134d75201ed4c787d5e41ccd
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:11 +0000

    takeaway taste impressive wish polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bool(array):
+        return bool(array)
