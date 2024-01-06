c218c7c64dad40bfebf4d846c0d21ebc8dd9d0f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:37 +0000

    fence sink firm private pretend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
