4a4552cd8492fa367d79e72e30af4f4f42a2b176
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:01:10 +0000

    healthy hour city voice turnback

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
