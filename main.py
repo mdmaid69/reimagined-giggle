763185038566e2bb08bd60234a35e30e41ebc145
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:49 +0000

    wipe discussion loose field leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_int(array):
+        return int(array[0])
