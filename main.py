3ecea5321da3c3c840696679de392a0f7718f443
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:11 +0000

    instant order perform neat spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sys
+print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
