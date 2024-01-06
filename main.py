f3349eaa021bae417610593470a5bca66252e7ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:49:49 +0000

    uponon rich wave thick cheek

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
