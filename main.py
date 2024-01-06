bdb75440c973a590985663360750edbd5794e9fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:57 +0000

    coffee also ought rush night

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
