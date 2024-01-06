8477a5fb16de75374e1deb978013875d4bb6c73a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:30:17 +0000

    might sort day control film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
