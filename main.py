98bce385e48beb52dcf16bf4b701d6ccfc92f219
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:06:44 +0000

    open trap escape whatever task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
