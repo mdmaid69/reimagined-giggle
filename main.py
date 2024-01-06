87b26ca406ca558ddf5b3d9044317dea41812692
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:44 +0000

    transparent surprised listen cost peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
