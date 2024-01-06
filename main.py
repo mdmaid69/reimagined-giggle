eb5a3411d17191279bd59318e0e253016b1391a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:24 +0000

    bringup leave indeed destroy closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
