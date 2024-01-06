af68c6af7d9ad3af74a17d016da495b577db3f41
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:20:58 +0000

    secret force else means warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_str(array):
-        return str(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
