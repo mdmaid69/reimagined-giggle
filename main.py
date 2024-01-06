a5aede49a48160188d857add707175babff6d532
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:43:32 +0000

    Tt sudden high speak bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_unique_words(sentence):
+        return set(sentence.split())
