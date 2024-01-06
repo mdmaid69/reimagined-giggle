6d9c7d64b9731870cf8f0981b129e08aefa42437
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:20 +0000

    interview warn giveup criticize fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
