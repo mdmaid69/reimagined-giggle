e7da262cd9ae8da41a1bfed7b44090bf95dc9e8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:13 +0000

    keepout show politics beginning other

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
