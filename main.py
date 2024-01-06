bb6e7845fd6b1bac88017fc035b3f5b83be94654
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:59 +0000

    card stand secret disappear conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
