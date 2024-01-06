84572b98977daceebcf503c8770f6a8d25444c94
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:23:03 +0000

    height favourite positive touch plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
