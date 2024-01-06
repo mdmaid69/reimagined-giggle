c9bd05a709168959044417398fcefcad92e79c38
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:48 +0000

    occasional doubt sorry snow couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def sort_list(lst):
+        return sorted(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
