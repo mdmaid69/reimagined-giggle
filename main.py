c4c661d09dff41b65096be8a2d92d371d50d17b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:45:14 +0000

    grateful explanation ordinary communicate frequent

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
