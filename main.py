d08cbe0b087ef8bd3e8861be2d4ecb8d8a17c8b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:28 +0000

    evening maintain community attention belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
