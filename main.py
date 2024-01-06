b31e1a724b72a312b2671c715e6e32a061c87f98
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:40:17 +0000

    direct ought patience branch fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
