6f4701da58d2d0d10d497206952fe91065bb4dd0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:43 +0000

    soak connection ahead apologizeapologise whatever

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def convert_array_to_list(array):
+        return array.tolist()
