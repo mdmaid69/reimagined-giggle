27bca765ce0ff486582aef0376bc2560f924747c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:26:49 +0000

    everywhere sort introduce compare reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
