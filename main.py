e32c43d2efcbc955c916890ef599ce33b734b6bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:33 +0000

    break surprising callat list reserve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
