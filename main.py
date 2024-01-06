5799c6187678a5ca400042d139c576c1dffdbf55
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:55:35 +0000

    spoil visit pool talkoutof climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def convert_to_hex(n):
-        return hex(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def clear_array(array):
+        array *= 0
