883c25d7a45cd4c448e892552ffcef518ad5d89a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:42 +0000

    set its trap your lose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_power(work, time):
+        return work / time
