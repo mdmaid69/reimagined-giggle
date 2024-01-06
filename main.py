ea37bafa40a13f96000833113225f573f3707a61
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:19 +0000

    reasonable tired peaceful by each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
