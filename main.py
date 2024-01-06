dd1bf866c4906711ed8fa704c2874fb39024a167
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:44:11 +0000

    crazy close computer successful murder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import time
+print(time.time())
