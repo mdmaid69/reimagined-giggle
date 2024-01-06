3337e14653a29fabe0984690a601bb5bfdf6ddca
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:56 +0000

    remember hollow yourself permission stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
