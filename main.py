7ca10fd011501cf94fcf74981cd2661746c06a63
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:20 +0000

    responsibility tax contents ball goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def remove_directory(path):
+        os.rmdir(path)
