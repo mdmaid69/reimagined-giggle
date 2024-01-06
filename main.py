1c7e6801c2f9b32c8ed3fd618f660180adefa37e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:54 +0000

    ring make whistle score country

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
