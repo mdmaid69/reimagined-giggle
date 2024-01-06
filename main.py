4f74a662291b437063c9a3df5f982b15f67f3dcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:18:12 +0000

    light certain I call travel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
