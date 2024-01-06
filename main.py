37083a3872627a42e18f9213438df33e5bfaa8ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:18:01 +0000

    accident among wave careless programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
