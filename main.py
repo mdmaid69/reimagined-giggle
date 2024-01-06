81ee2b3f481439b3b2f65d2328ad95152f9c6614
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:15 +0000

    food male disturbing excited wall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
