27c394e1bd705c43993ceeaf50c691043a45c8b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:32:50 +0000

    arrive why limit sort sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
