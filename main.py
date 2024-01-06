6ebe73489e97c7e996e91811753c7032792e7c0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:29:37 +0000

    poison priority month window taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
