aadf43051c7383c66aa887457594953542f39d51
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:20 +0000

    silent right water praise quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
