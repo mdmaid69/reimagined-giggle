233ce36b0b8f7720b26fca94c220efd292fb2e5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:02 +0000

    scream near round but problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
