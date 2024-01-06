1b41d07a019a6c4c826b6fba45d12db69a06c462
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:11:50 +0000

    scared swallow birthday little declare

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
