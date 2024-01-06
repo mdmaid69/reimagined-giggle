093c8d4a95fa777aceba8c44b41168c59f36e001
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:43 +0000

    centre autumn act pattern hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
