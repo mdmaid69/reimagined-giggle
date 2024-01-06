4490e0246038cfdbf9abb4118e330b651531f8fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:22 +0000

    persuade thick instant really influence

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
