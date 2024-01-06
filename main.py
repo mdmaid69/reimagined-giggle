b0478be82525c9dae0299f6b49d87f8b3fd108bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:46 +0000

    want important spring wing fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
