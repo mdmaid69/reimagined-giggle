7a61c084198eb4e57c1fb7e9959856515ca7c345
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:34 +0000

    compete later thanks wave somewhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_typecode(array):
+        return array.typecode
