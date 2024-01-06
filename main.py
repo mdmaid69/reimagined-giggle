5a29d0ab203838780d85a36ed57f92e510d1054d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:05 +0000

    set solve blame crowded contribute

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
