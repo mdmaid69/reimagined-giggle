0ebdb3fbce72697410885db9a0ed52af2be99b89
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:17 +0000

    shade spite definite above confused

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def divide_numbers(x, y):
+        return x / y
