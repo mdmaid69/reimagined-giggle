e185a027d93b5f3cd67ca0d562b1ccbcd65821fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:58:59 +0000

    dish fortunately market mend funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
