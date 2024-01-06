8f346e0a4d7ff381755bb04d28f2a021265b0fea
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:46 +0000

    cause content bury commit area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
