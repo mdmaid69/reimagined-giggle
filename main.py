fdfe8b1f915e49e74170d3551b6d5c1c3ef3ac2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:44 +0000

    body Ii ground hungry however

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print(sum(range(10)))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
