7623705412e0cc94939a99d1eaca48a0299c67a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:15:31 +0000

    drink square straight confused light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
