1cc7a9d1695340a52c76dcf1e145bccf84f085d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:40 +0000

    compare Uu period trap cover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
