90a38fa886c5cc1911005df4c3b74f760295ea07
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:08:20 +0000

    still rent interest firm impressed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
 def calculate_average(lst):
         return sum(lst) / len(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
