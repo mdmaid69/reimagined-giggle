bd36077adbfb5739c4d373de75448c23fd40c2f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:41 +0000

    invite giveout dinner weak several

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
