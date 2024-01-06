971b1f72fdd0f6ea14e70a4ac628d334ef656952
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:14 +0000

    considering record material power sport

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
