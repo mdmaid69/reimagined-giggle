7359a6cbda203bf97f2d52125e9c6ffc9a02147f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:21:58 +0000

    butter final occasion lose edge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
