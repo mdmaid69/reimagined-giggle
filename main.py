a5c03ba4a341b754e3415b82a3a96cecc70bab66
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:45 +0000

    list regret improve maximum 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
