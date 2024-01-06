6664d6483a7efd3291c6363d53947dd62b4d1fb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:23:55 +0000

    spill hurry wise content makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
