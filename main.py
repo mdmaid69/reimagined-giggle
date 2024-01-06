933401efdac881f79acc7e0771b46c9dfb196417
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:09 +0000

    open crazy major goup care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
