fc72789a5cbd37c4a05bdbf92fbc814f45be9d88
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:33:14 +0000

    custom her conduct before grade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
