d3bac65e558492477178a04a661fadcf8a98f7ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:01:52 +0000

    without advise rise both prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
