a4c7e4c0289eaa695a12bc9454acdfbbfc88e762
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:49 +0000

    nature bus violence curious anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
