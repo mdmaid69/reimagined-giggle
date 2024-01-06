cfb30f87611c32fa66670047e957428c572692fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:47 +0000

    record silent slice put cinema

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_power(work, time):
-        return work / time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
