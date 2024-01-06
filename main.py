551783672f7b30aa32d10d63a55bedc42e7a6cbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:16:12 +0000

    circumstance hollow harm original time

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
