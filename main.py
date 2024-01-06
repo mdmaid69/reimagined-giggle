4b0c2d0518057255abc99a467f83e361bbacfa5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:09 +0000

    second damp mess wait sticky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
