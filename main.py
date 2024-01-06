869de1f06b60fae9259855da794e5d8b5fe3ca46
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:35:52 +0000

    class rest real bored they

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
