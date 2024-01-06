fca231ca6c263a25775d1e8bf22b75b50314ec7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:41:20 +0000

    lot whistle wise comment cool

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
