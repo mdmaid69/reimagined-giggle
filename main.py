baacfb22a9c0b458560540fcb942cbab7e985ec2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:01 +0000

    proud bus nothing female warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+def get_current_time():
+        return time.ctime()
