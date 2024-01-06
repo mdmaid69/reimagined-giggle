749e573cba86b82290fa38e99fa3565cb0a564d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:27:52 +0000

    regional object car neat recover

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
