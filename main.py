1ee59ea23f2fc1884942fd5c8104b829757a0ac7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:18:11 +0000

    interview fail this blow mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def multiply_numbers(x, y):
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def multiply_numbers(x, y):
         return x * y
