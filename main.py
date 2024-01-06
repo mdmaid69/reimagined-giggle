6122567d687e371dd0ba5b327066a82a84074ac7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:37 +0000

    shine grand bear free hurry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
