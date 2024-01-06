37bba73615da3bdeb690bc77ca8333c9930b10a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:02 +0000

    someone bringback married when forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def get_current_time():
-        return time.time()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
