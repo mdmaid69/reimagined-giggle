81a23557da5cef2a5260a2d13b37982365658170
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:50 +0000

    pleasure care its Oo cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
