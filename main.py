f465d97f01977f236a81d7dd2a6ccb4ae7cfc9c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:27:06 +0000

    complaint responsible pain amount copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
