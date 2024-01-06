20e198ef7c025366c4d15b7595ec1bbe978d66ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:49 +0000

    lock look tooth file game

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+def calculate_work(force, distance):
+        return force * distance
