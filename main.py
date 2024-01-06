c200251cb5aa37624385b481d1cce39136471316
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:39 +0000

    neither business wall runaway cap

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
