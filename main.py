f2cf4e4e5ae3427bee742ba1c50a7309acfa3eb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:17:37 +0000

    example throw hurt comment cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
