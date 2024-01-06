8b1ceb1a435bb3efa707b80f6e3c8eb85f5e20fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:31:12 +0000

    road responsible steady behave nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_factorial(n):
+        return math.factorial(n)
