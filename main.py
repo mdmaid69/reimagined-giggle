244d1f6069f7b689d8cbe8489cbd7e2c58781a0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:54 +0000

    scheme admit instrument hard early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
