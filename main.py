cebb3644008f0458320a8bed99a2ec99f66a1ea6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:35:10 +0000

    meaning supply dinner can mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
