f74f8edc0a61e4ed3c349236866b01f92f066af4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:20:45 +0000

    seriously explanation allow before possible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
