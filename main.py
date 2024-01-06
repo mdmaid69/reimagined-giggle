e555025918cd843ce26444cdc903cf34a720a048
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:39:49 +0000

    soft terrible traffic difficult turnback

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
