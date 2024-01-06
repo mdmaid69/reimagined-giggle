07ccc442aba772590abd38b6c55d6a4f117d3630
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:26:10 +0000

    turnup least dance in dress

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
