cd0e0660530ed6f1c95bc8cdac20388a8d1e2b6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:09:12 +0000

    usual call form content really

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
