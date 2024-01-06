43d1dbcc9d130219cf877bf98de5c69cedddf9a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:01 +0000

    error instruction drop memory afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import sys
+  def get_python_version():
+        return sys.version
