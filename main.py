6a13115a04567c88beaf81bb387e8873aed3ad40
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:32:41 +0000

    taste emphasis let unlikely Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
