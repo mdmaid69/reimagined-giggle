c0dfcbff1b2ce9c62bc8039a65574d8717138300
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:13:36 +0000

    female keepoff bound part difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+for i in range(10): print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
