36fef86e967d927b434712270bbfb90b7ba8e7b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:49:30 +0000

    border thin border wave circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
