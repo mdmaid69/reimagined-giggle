0a133eb103c42f1c5edd24fd9a886922b199fbad
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:36 +0000

    wire silent television wire story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
