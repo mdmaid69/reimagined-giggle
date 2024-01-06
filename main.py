7edfba4d68200709a4ae86c92ecf74a4ea7c95fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:03:41 +0000

    workupto afterwards runaway bend speed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
