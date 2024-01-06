50f01c086559792ea1414dddbe0c5647568dee7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:54 +0000

    each fetch manner patient machine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print(sum(range(10)))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
