ba7e8a35c49dcd4e3bcac5ac896154c06ee4fa7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:35 +0000

    damage your involve false surprised

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
