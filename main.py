ba0e03274f3c02263b80847ee5506a0181e52dfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:00 +0000

    unlikely forever rest check Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
