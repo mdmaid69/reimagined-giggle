eda2e4e8ffcadc4ada836cf7019e1bcfca6a8c4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:52 +0000

    moon thinkthrough approve dance contrast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
