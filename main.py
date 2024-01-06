3531cd946871d20e3194390140e12016750ce28e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:01:32 +0000

    secret reply organize disagree direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def reverse_array(array):
-        array.reverse()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def get_array_as_tuple(array):
+        return tuple(array)
