091a069aee46e03f5464479755236428fb83a148
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:52 +0000

    invite small some deliberately work

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
