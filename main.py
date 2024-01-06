dc348549c48a49116c991e3f46b1470c343b17fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:40:20 +0000

    strong nearly power fit search

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
