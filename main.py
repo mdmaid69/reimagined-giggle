5b33a54506b5e07c9f4d9d2abf35f05744fd3e50
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:47:42 +0000

    last fat switch brush food

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_queue():
+        return collections.deque()
