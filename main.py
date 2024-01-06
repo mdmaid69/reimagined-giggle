7ed8836ab6269d7e91e4d252836c5bd518a85151
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:00:29 +0000

    continue ground still relief abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+print(math.pi)
