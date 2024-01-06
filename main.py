92d2bdde5bc4df12d3a15ba67f88cd1d950cb2eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:19:47 +0000

    across yourself free black journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
