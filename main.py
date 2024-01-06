0de28fdcfaf4b63a71119c603065f2173071e786
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:04:39 +0000

    direct mean cut telephone rare

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
