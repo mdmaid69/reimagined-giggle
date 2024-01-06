4ffcd7e812ccfd01f39986cbdabc22ffa694e85d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:45:26 +0000

    basic knock quickly disturbing search

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_palindrome(s):
+        return s == s[::-1]
+def sort_numbers(numbers):
+        return sorted(numbers)
