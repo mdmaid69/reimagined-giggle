1cdb1a7fcc208f18755d39521c12563da167c406
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:06 +0000

    character danger petrol Itself runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def subtract_numbers(x, y):
+        return x - y
