11ec18c5a251de37116d2c1e81a4c57c49c6d825
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:37 +0000

    break length difficult class awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
