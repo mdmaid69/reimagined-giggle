de7836271b7e140089715696427104a7b1c178c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:48 +0000

    couple apply position enthusiastic stage

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
