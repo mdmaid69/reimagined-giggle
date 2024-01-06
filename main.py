d9bb8f4c8789a0bf37a35e7367629df2ecc053a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:12:43 +0000

    plain base uponon frighten cough

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+print(sys.version)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
