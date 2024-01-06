a229b0b812a7b960fceaea4bf6530e3673f70e34
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:32 +0000

    building ocean thumb sound mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
