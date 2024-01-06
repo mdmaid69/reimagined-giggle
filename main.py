90dd8cdeef2701772c09ce5f64e734660b71c1b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:49 +0000

    factory say address failure attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
