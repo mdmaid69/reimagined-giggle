96c5aa0a8d268afa427323a313a555782cb2d2c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:43:11 +0000

    godown getup people office treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_density(mass, volume):
+        return mass / volume
+  def convert_to_binary(n):
+        return bin(n)
