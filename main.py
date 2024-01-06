566a688718d093618bdc5ee36c70de841c36c6a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:48 +0000

    consist pick earn tongue style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
