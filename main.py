882152811ea09676d9de96d7572e64e67280be61
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:10:08 +0000

    task concentration gradually discover seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
