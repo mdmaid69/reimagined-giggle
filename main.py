63dec131a6b36fe44f1f98172131c03f51f1d2c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:48:35 +0000

    buy suit base promise one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
