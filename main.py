d2bed9e7d27bce8219f1db23ca5b39635ba2be0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:32 +0000

    although use serve stand something

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def sort_list(lst):
-        return sorted(lst)
+def convert_array_to_bytes(array):
+        return array.tobytes()
