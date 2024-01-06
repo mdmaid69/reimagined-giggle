1edd820995349f1863d4ca86bdec8a299b996914
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:43:56 +0000

    light couple occasion tap juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 def calculate_force(mass, acceleration):
         return mass * acceleration
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
