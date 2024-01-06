4952a18250a17bc6e27a169a158f0c326b848b37
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:27 +0000

    week hard adjective communicate help

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
