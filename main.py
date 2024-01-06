f98bbdd3a7a13483989d24479354f895b8ba282f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:26:06 +0000

    past Gg getoff steep bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def count_characters(sentence):
+        return len(sentence)
