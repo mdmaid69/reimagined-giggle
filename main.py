f1d5b1a366fdb756701816eb96ededb99f273f19
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:15:03 +0000

    somewhere cut trap perfect present

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_unique_words(sentence):
-        return set(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def convert_to_hex(n):
+        return hex(n)
