38cdbbda5e9bbb4d9452247b8028a304ca1d51b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:36:04 +0000

    limit single tight unless among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
