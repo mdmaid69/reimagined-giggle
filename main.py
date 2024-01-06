83aecd74f5c30b3e79eb575442429fcda8344c88
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:49:31 +0000

    Jj debt Dd age hospital

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import sys
+  def get_python_version():
+        return sys.version
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
