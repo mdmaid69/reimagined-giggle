21f2922cb1b9533cb63984439bc3f9ba77459848
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:31 +0000

    water both give marry hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
