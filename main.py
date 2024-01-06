5b6e3aa744fdd33109f36f8fb9188e9934e407c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:24 +0000

    congratulate cross shall manage bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
