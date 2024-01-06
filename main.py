be23577e9f0105c11bd26ed4843a2b157f08405c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:15 +0000

    dark tap me general least

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
