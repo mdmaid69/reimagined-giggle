ab4b6f3342b4a4eb12806b8b7eca653e1157443a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:33:27 +0000

    exchange put treat empty schedule

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_speed(distance, time):
+        return distance / time
