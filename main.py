3ef2b4635ddcc30a068c1df24a87b93831382ed6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:41:04 +0000

    way all each artificial prefer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
