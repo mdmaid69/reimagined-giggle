b83aa4f0809552a57c6ae6cc5e0353b2848ea289
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:29 +0000

    brother workon trade season manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
