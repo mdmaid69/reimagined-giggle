aa2cfd10d8e0ff692b3dffa96e977b7ebdccfa5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:05 +0000

    occur impossible comeout edge pleasure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
