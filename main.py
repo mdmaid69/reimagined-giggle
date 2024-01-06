128a7a43462cb7eb2c5c2570f6d686cb024a43eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:49 +0000

    over ground recent leave lonely

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import datetime
+print(datetime.datetime.now())
