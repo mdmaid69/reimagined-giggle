aaa9f3513788bee18e4c1a95fc8a8eca89b07897
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:56 +0000

    surprise listenfor bringforward separate getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
