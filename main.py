c946719859f72429837e831de02b1abbff15023e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:37 +0000

    determined fill point low beat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def clear_array(array):
-        array *= 0
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
