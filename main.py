fad8d4d1b0c07d3f97255296808f9574dfcf2a47
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:37:54 +0000

    fail moment consider save school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
