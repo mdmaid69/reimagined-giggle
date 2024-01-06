2170966d4a0c29b6f7986842a40162ec70b07b84
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:18:08 +0000

    guard interview tap mistake strong

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
