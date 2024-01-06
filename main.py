60c8b3eb56a85142c9c3d1ebbcee92da058c64c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:10:13 +0000

    full cover guilty whether flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+print(time.time())
