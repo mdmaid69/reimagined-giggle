3c19d72f7e96175476e51bd95d3c02d07c0ca244
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:22:43 +0000

    tidy their awake Mm great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
