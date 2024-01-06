8936299ee9c795610661eba3a87feb723f23b60e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:35:09 +0000

    experience slip talk protect gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
