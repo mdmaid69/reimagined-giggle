d0799949f1bcfb8c73617561aec910ec95b87cc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:25 +0000

    level except calloff still measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+for i in range(5):
+        print(i)
