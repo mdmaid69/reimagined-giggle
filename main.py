94c86567a087cfb779f2e796e43a69b16b0b5713
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:44:20 +0000

    punish mother country act declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
