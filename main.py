1bff25dad0bc9db2bccb3f689d8dd69de8e8edef
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:04:26 +0000

    shelter right do emphasize play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
