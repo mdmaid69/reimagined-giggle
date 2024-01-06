03fdafa5df4620ef36362daaa9846651dbf9b050
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:42 +0000

    equal confused have clearly emphasis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
