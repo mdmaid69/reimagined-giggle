697b1b80a4a1098a6eabc5907ff4509d9baaf084
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:44:12 +0000

    contribute neat sensible sink suit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
