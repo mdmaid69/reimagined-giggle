b8840ae2db184fb4d91811593f4c5ae8193d3455
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:07:55 +0000

    there special capital company busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-print(sys.version)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
