03fe02fd013480bb356f7975359c79eb061d8766
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:31:34 +0000

    bottom exaggerate attempt expensive takeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def split_path(path):
+        return os.path.split(path)
