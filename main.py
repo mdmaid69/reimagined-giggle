a910b7a69d587f3bb847fc5cdb2e3f93b67ab580
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:17 +0000

    birthday paper beyond throw flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
