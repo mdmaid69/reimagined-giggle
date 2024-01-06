413e7d24527d69b5c67103ffa08e54a319088edf
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:27 +0000

    write ought happen difficult hand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(numbers):
+        return min(numbers)
