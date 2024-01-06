c1562ccea161e09cae6a725b876036ed6b4a25f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:59 +0000

    tell unless expression past rule

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
