be35c9ec03f3ecd3ad3ed2f7b9d9b24e8bd384b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:19 +0000

    want dream introduction comein fork

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bytes(array):
+        return bytes(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
