4ed6a9f4a41ff411fb0db0e1b514e9b57d7e0b2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:30:09 +0000

    guess shrink size fact the

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
