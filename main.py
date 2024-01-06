f8e34d345f96adc16fea2ca067c08501afd48e90
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:26 +0000

    eye slow recover nice just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def remove_duplicates(lst):
-        return list(set(lst))
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
