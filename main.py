1b9d3be13fc67cfe4980d386453d1cfb95518d4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:50:27 +0000

    suit region damp cruel pressure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Uppercase:", text.upper())
