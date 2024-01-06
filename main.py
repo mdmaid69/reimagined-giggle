8313b6823911840907afab2a82c263e57b70652e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:23:20 +0000

    disappointed minimum software admire identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def count_elements(lst):
+        return len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
