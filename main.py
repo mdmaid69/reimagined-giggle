a43ce7ac8a43f938cea772cefd732de24cde07c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:44:10 +0000

    lead bag news bridge operate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
