469108f860521ce60dfb872322d223df026560ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:37 +0000

    will sight whisper fall race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_work(force, distance):
-        return force * distance
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
