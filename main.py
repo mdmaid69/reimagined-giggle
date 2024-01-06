173146b09fc49d9ba1925190a51daf3cc73d3b24
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:25:19 +0000

    giveout face high rely rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
