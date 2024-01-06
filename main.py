6d78da0e536675ef1da66bdf3f3f1973887f374f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:52:39 +0000

    suit breath memory once cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
