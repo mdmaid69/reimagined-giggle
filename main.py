0c3a987e3d8142ecaafa061457bdbf664d1e8e25
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:02 +0000

    either accidental road clear though

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
