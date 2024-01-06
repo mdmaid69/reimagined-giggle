e2d65bbc160a5dbb57ff54ba2521d71bca0e2883
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:23:07 +0000

    phone seem their who length

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
