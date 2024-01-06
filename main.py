4ab6e9e5d99fb8acbf9d407f51151d75ae29d8be
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:47:53 +0000

    invite before street excuse money

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
