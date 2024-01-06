f27674974d8b2a9d44cec745c02a0e95f83fe1c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:24 +0000

    small workout close free achieve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
