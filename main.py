5e60b5f4c1b0e55fd7b47bda0a4bfd148d7c82b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:26 +0000

    people community imagination quick ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
