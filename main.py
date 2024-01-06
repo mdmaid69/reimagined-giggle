bbdec5e2fb421538e9bfbc1ca696f133fa44f36c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:51 +0000

    sign callback public cost hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
