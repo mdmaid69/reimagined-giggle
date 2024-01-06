4faf95611cdcaf424244ac30c7c5cabe430c7785
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:37:39 +0000

    hour lunch excited collection pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-x = 10
-y = 20
-print("Sum:", x + y)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
