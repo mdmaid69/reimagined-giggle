e360701e54b0c05feffb14854136097ed39e907c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:25:42 +0000

    broken sudden rule memory benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
