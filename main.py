fb31883219834cd6c382ca81006cae20bf7c1034
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:06 +0000

    rubbish satisfied responsibility agreement band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
