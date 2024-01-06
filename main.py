9e530fda02e4c8d080c68dc59e1f5526cc13f815
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:09 +0000

    road collect ourselves lucky solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
