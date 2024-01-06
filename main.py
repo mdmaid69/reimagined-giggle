380ac2077f10aa09592c827dfce6dfa755d22eb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:31:02 +0000

    Qq foreign need right gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
