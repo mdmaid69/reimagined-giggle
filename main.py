68169b80120a99131ee7e4a1819536f3b5906c93
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:17 +0000

    tape shout must want among

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def remove_duplicates(lst):
+        return list(set(lst))
+print(sum(range(10)))
