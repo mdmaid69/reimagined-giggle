7026ba005cc1c0a73b83a100c75798af4eb573d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:18:05 +0000

    instead for broad admire minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
