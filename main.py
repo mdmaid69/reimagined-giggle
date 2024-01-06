10e47f2fe125ad489e29c664994ff2a1a8667eea
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:43 +0000

    argue help advantage reliable balance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
