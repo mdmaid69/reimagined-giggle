459f0a73c2bb4b9ab41d9b77f692adc8d638852f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:48 +0000

    gentle target cheque scared news

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
