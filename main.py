c5ba9d7472f227d07726d557161b1ef14f635241
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:05:29 +0000

    argue steep bear petrol insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
