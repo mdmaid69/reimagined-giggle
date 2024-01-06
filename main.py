a3e43f5b85330d70f9f2fab4bd1d6c7476067c62
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:21:12 +0000

    generous sell suckme little delicate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
