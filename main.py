428baae0b64e76625cb2c428540da68fb85404f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:49 +0000

    party whistle colour away calmly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
