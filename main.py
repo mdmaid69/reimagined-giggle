3cb75624b428422b4d0aa33f7ca86809437204a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:05:16 +0000

    embarrassed survive movement takeback unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
