b3bf66268b34e3b058e58339de3102a7999b1a5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:24:30 +0000

    custom his hers complaint anywhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_acceleration(speed, time):
+        return speed / time
