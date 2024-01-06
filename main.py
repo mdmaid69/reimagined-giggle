e80fb0802480bf0a541b84e97af618ceed6f443c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:01 +0000

    vote dead across handle foreign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
