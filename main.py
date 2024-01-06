f2741fad83028660a359ae36b947611a52965722
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:07 +0000

    recognize brown list after quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
