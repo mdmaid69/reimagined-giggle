31ff7c519fb5805fbfc13de9fafeaf9251b550e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:36:17 +0000

    use brown return normal recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
