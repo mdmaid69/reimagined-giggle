decc3ba2906ca01ee0370e150987bebf441c468a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:23:33 +0000

    able suddenly but answer yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
