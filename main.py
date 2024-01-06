47f388a340d49ceaca9b5cca386967771c319ddc
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:25:10 +0000

    carry runover spoil lay hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
