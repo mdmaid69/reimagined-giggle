51b292f2149e7b880dcee6e88300f0448bd9e245
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:16 +0000

    block last fashion lonely judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
