b0b4d09ce30e5c89755913fedca3602c9699ade3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:40:28 +0000

    comeoff strength plain quantity other

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
