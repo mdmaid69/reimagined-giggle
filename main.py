734061f9afb40cd731f364d4bc042ecaea612607
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:12 +0000

    bottom movement successful unpleasant next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
