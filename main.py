477451bf76b77112d37828ed7c8e708e8f4fc0d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:33 +0000

    film grade report open low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import sys
+def print_python_version():
+        return sys.version
