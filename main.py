2533632a409f0be7b6b04b5e5ce0fdf94a4e2468
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:00:40 +0000

    in last regional Ff container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def cube_number(x):
+        return x**3
