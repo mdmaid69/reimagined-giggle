d5916bc699307ff8a6ab97eb14fd3c48634fb02e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:49:54 +0000

    doubt mind tall except tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
