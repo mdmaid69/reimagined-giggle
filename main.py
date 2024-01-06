05cc9a92dd67fb6b6a0c56e55105249012f2018a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:19:56 +0000

    clothes total alone careless quality

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
