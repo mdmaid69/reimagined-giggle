ba6bd2c67657ae56eb5bec9bc6e856572f817435
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:59 +0000

    wrap shopping raise vision thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def split_path(path):
-        return os.path.split(path)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
