d5ab5bc2324e25a96f2a583b32f615e181170505
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:19:24 +0000

    afford shout crazy heavy art

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import tensorflow as tf
+print(tf.__version__)
+import sys
+print(sys.version)
