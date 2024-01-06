560203c542eaf70381eb9bef7328b0384d5c4516
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:50 +0000

    pocket control unfortunate careful different

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-print(sum(range(10)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
