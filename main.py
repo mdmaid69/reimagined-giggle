3f73aa135e646a6d03bbb21ff966b70ec264746f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:12 +0000

    equipment hungry dress book finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
