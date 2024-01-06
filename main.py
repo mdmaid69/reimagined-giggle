c0d6e4fbffe70b1e92ff5677a10e75ddfc90156d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:38 +0000

    piece copy at move hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def add_numbers(x, y):
+        return x + y
