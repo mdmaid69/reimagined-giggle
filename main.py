81e19f2f66d9ea4b9cb20664dd41ddd92c1c3763
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:28 +0000

    tap reply comeround full advise

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
