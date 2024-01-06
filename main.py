25cad495cfc2ff735c9d45cb83d731d211f9dea9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:33 +0000

    always pencil own strength free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
