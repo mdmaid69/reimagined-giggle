fcca34363e0ab816e147f1380b27f51dabf4f8b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:20 +0000

    runout in murder organize entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_list(array):
+        return list(array)
+  def reverse_list(lst):
+        return lst[::-1]
