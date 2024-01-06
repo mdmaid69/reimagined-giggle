f2d686f19d6733097461a846497f4e191bc299a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:33 +0000

    before concentration hit blue suggest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
