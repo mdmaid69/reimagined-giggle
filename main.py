11af659fad7d051faebefa3e8c1a875cccaed7a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:32 +0000

    push success habit distant walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def cube_number(x):
-        return x**3
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
