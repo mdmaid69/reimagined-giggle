03f0964ed497c3ab087ed458ee8c4b382944dc07
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:14 +0000

    remain face film office him

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def convert_to_binary(n):
+        return bin(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
