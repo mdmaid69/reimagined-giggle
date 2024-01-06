63779065cc32738fdd479212c22b5305d61f9696
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:48:04 +0000

    example public hardly juice switch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
