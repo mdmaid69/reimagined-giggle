e575a07229814a4ff371d79990e8af46407bac67
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:42 +0000

    unusual say pin quiet put

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
