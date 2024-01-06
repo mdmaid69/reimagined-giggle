4a2f2a1b5dcd7b28467eda74f0908b8dfe8b428b
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:13:30 +0000

    yours threaten factory close tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
