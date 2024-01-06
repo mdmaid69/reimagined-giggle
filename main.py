f0b636ee6bb683f62b355e0d28949e589e233ccd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:30:45 +0000

    talk chase ourselves feeling Oo

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_palindrome(s):
+        return s == s[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
