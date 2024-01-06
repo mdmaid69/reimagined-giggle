39b4ab1feaffcc68f4420f9863091bc3369de9b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:07:26 +0000

    off lonely out rob snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def sort_list(lst):
+        return sorted(lst)
