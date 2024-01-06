7ae17f4b29adca9667f0aa401839a0d30b020f95
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:04:56 +0000

    seriously gooff hair round more

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+import getpass
+def get_username():
+        return getpass.getuser()
+import getpass
+def get_username():
+        return getpass.getuser()
