5b9daa0ef3392b4966f7353aaa91f2affbdf4fdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:01 +0000

    trade married equal embarrassed Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
