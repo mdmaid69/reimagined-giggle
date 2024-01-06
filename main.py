8c54880ffe2297121c3e6270bb6d9183b3c6fe42
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:12 +0000

    land Zz odd mind trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
