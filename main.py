b32bbaee79f28f82365ac268dab46ec3a6b31a0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:30 +0000

    mess behind fortunately realize reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
