207c22ace0083904fe207a105fac8379b37844f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:47:36 +0000

    available store meaning file breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
