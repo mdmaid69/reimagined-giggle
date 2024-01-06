c1322db26aefade064567b8918cdae567d249bd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:36 +0000

    level emotion suddenly mess cruelty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
   import matplotlib.pyplot as plt
   def plot_histogram(data, bins):
         plt.hist(data, bins=bins)
         plt.show()
+def calculate_speed(distance, time):
+        return distance / time
