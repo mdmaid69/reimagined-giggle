38603008aacfb16651a129c0b04f6e8defdbf5a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:45 +0000

    official well nowhere wave otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_item(array, i):
+        return array[i]
