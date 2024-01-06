5d13ad5a7ea26975037b6cca3427518e19f1065b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:14 +0000

    bar fast refuse vision hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
