9e15bbbf55ce818c143c71c8cc1367e5b2c524bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:45 +0000

    out follow less mouth we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
