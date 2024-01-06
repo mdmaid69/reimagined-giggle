88792a205feedc10a8c07f42ba48dc563287255c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:32 +0000

    consist offend powerful dish popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
