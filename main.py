f114b3aab2728af164e5a454a8bc2a2148c1c409
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:05 +0000

    bush object declaration neither welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
