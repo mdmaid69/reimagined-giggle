b1359b7b59d67583db0a4b28a7b4d05c7b9f19b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:01 +0000

    though such bus war stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_hex(n):
+        return hex(n)
