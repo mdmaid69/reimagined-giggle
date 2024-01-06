d8f26f2123dbe7e94fec7dbb74a0755290618f32
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:35 +0000

    pronounce whistle preparation soil stupid

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_volume(length, width, height):
+        return length * width * height
