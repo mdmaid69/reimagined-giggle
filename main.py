725edd2b352084facc286acdba69c30439edfba6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:09:42 +0000

    coat rule apply operate star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
