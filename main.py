efca960f1884795712ea225c8b1885017148e222
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:54:09 +0000

    to peaceful right in worry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def exit_program():
+        sys.exit()
