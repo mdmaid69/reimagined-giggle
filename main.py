1c9f84ceaa464ed06e5782ae2baa2a8a678e1e5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:56:48 +0000

    dry damp wild teach takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
