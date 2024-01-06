65962d6271e976df6a08b10f55aba0e305effa03
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:24:34 +0000

    attempt exam name slice platform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
