b011e96caf6136389e322be1e3258cfa7d3348f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:49:42 +0000

    consider develop public murder Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
