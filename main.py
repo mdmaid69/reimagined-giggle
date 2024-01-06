dd8e8144588d30f8eaa7ff9b480c4b3713840f1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:06 +0000

    precise attach able convince admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
