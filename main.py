cde1cb731f5f89ed4affb0da5a19f661c3ae025d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:39:44 +0000

    unfair stupid discovery feature lookup

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import time
+print(time.time())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
