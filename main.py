736b0fcd7dcaf89208ed34dcad61d5826dfb4ee9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:35 +0000

    pity lookafter week safety unkind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
