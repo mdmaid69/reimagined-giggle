302b7dc6f76e0af56f2819a08e8fc2737df98805
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:03:41 +0000

    point exciting several crisis laugh

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
