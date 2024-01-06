84bbf54f76c3d75e2af384b2213a0e1947671f30
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:18:17 +0000

    activity priority sorry when sad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
