17298cf9e446e4d0cf802a9ca1e2d844d1a38d86
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:17 +0000

    heat left square disappear everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
+import os
+def get_current_working_directory():
+        return os.getcwd()
