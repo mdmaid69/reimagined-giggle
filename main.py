320e9bd9fdcdf581c3c34d40435acb677ba43cff
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:03:24 +0000

    could stamp surprised curious getup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
