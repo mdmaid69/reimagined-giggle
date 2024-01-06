87e756f1f6d2ef4343d4da61767f2d63dbf31562
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:54:19 +0000

    shock even expensive set serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
