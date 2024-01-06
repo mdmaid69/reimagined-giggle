a2487c2afb4637025d4837de6415be82a16784d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:08 +0000

    fortunately least holiday drink cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
