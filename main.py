c7d652d4ae95fc93463f195230f268f5bd9a541d
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:40:40 +0000

    excellent giveback comfort 570enjoyable anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
