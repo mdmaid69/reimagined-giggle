3e7bc11e5283f9cda6b1cd8fb2af9a80a5115b64
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:08 +0000

    meet always introduce deliberately prize

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
