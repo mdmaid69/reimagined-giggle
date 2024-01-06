b411b4b8da5af51f575baf32527dcc82230f061f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:53 +0000

    warning answer weigh pencil blame

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_speed(distance, time):
-        return distance / time
+def find_min(lst):
+        return min(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
