722b97f8e359def7196083964e04d74743c4050b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:03 +0000

    only only surprising happy word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def clear_array(array):
-        array *= 0
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
