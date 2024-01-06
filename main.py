0d05fad40d381aa20523a736a5f00fdf56ab7485
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:56 +0000

    connected line protest roll touch

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import tensorflow as tf
-print(tf.__version__)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
