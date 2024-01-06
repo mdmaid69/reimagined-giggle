6fdcb95bfe6deb79d44f5e10538519b9cf9f1404
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:06:17 +0000

    limit anywayanyhow seriously breakinto Mm

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def remove_duplicates(lst):
+        return list(set(lst))
