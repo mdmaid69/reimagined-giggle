9ff215cf89edc9dd3e25a27ffa761838a73bb0f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:44 +0000

    actual curious turndown occur workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
