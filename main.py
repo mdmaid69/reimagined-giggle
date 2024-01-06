3f2bebeabf2e80f53711096f84ba85070ab87d33
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:52:35 +0000

    morning ago care sort turndown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
