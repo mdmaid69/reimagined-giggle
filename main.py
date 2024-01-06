d15301824bb8bf6933887fa1f5ad1e1797636bc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:38:03 +0000

    hope call address sister lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
