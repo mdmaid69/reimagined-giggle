e6bd85161cb06b490e0a9d890b15c04bd83f8ed4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:52:20 +0000

    shock lucky suggestion perhaps stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
