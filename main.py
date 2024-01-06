ed27dc8020fe8fb5f49a1710d006bb6e5ca9e6df
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:35:25 +0000

    onto edge beside closed concerning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
