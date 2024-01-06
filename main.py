c5a6bcbd16ad2c17c942479b1acf6a29f3296491
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:45 +0000

    popular means hear brush immediately

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
