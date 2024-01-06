c4e1326fc0966598ec7d7ec150c3c87f25b58c8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:39:02 +0000

    exaggerate power about car brown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
