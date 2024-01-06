aee036700051e567f6c6ccb378a0217e65925b6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:55:12 +0000

    she opportunity cover under weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
