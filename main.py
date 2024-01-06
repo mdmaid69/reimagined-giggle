ba214714deafb2265ef81f47da209c17a12c1937
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:50:40 +0000

    pride answer brush me whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
