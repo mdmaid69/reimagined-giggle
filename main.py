91d4525c8e669236f4227f09088df4fe7f59dac6
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:57 +0000

    himself talkoutof reserve formal again

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_unique_words(sentence):
+        return set(sentence.split())
