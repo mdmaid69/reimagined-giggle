4bca25aefc2a4c79f9ef9c55ae284753af8d87d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:19:47 +0000

    hate fact behind complete create

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def clear_array(array):
+        array *= 0
+import collections
+def create_user_dict():
+        return collections.UserDict()
