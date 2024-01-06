9963e60a3ebd99907b7182167697a5afb6a40f73
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:48:48 +0000

    picture pot solve turnout earn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
