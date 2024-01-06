c151260aba30bbe49b1b813c9ac15a44dab9c7bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:06:41 +0000

    another ring set war tidy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
