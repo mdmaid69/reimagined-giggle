f0b1e2c79fd9f7000d33e9e0ecd966b954b5bd5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:14 +0000

    children copy difference thinkover walk

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
