e67c649e809ee15cc553ed96fd03c65bb953789c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:56:05 +0000

    charge getaway should bye design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
