2dd5f783ece644b7496943a19835acba1ceb239b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:51:13 +0000

    pay provide weak dig well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
