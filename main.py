609776f6c03c17e90565718882a00f4108306d89
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:33 +0000

    doubt scrape section rush breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
