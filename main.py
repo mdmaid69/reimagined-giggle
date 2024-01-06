bc932b9c02a9d9d4a681b97d57e721c19aada416
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:25:08 +0000

    anything software already rough chance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
