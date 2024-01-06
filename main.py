292b707328d320d3f4a47bfbbbacd6b0672542ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:04 +0000

    talk say water project dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
