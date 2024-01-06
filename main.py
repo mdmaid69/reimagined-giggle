5213340f3dff947faa7342f2fba8d17f06b96973
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:59 +0000

    tap instead connected hang fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
