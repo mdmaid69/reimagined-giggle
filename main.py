e4ad16819fc725196e56249029c4485b894e993e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:03 +0000

    active sore interest contain there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def divide_numbers(x, y):
-        return x / y
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
