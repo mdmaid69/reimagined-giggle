428ad114fa077056dcbab116d556fdb12303be18
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:54:19 +0000

    unfortunate rare decide where might

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+print("Hello, world!")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
