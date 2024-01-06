89bd8190ef72e274227a30b37bbdb58f5fe42719
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:37:26 +0000

    spend foot scrape performance actual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
