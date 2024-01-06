53e0985435dca71cfe9ce497eedee5e7380fe237
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:10:48 +0000

    ahead by underneath by division

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-def convert_to_binary(n):
-        return bin(n)
+def find_max(lst):
+        return max(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
