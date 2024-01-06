f34ee2a6165d28a5070fa871a1d44ef2b659c78e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:50 +0000

    welcome hurt partner pressure touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
