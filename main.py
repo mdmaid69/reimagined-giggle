d3f29aab8f5d7d2f7f7cb0c2b9525bdc83b35954
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:43 +0000

    lie note speed wrong except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
