d54727592be75e28ad62b4b6b05e0534eed50cfb
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:32:52 +0000

    comeout earn fashion nurse speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
