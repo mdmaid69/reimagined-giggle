adba09254bcfe370b747e44649cbf48e26c7ae2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:52:08 +0000

    pay mixture form skill related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
