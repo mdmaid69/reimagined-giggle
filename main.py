8204fd5e211687314aa25a658d13323e100d4fef
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:51 +0000

    body afraid cope callout such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
