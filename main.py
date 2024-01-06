1400137573c5a9825881c68c5366091ce2e7c69f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:40 +0000

    imagination wise tip taste would

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import random
-print(random.randint(0, 100))
+for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
