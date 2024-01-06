91c1eadae58c10617091f85387d0980be2231232
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:11:56 +0000

    surprised example cry organize declare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
