4acdab4f2735e1ee7caf5c4b3ba31e42f904183f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:08 +0000

    image feeling environment believe exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def square_number(x):
+        return x**2
