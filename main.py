ad30e822d27eae5c6daa546448013e77b4125837
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:41:28 +0000

    Kk Oo such mysterious do

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
