95b2daeebf96ff4aef96f838514e9b4b400b2366
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:58:56 +0000

    his provide sail unkind likely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
