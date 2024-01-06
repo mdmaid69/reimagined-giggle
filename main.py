ace25ab581a717c92cac08196c05373d97fea6bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:27:24 +0000

    guess peaceful lock sister front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
