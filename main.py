4b8be42df483758933cdb752415af3b7ee111cb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:52:02 +0000

    friendly shape add unusual past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
