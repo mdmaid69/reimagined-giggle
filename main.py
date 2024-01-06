96831736bc2828681d4855cd89443383e94596f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:15:44 +0000

    slice behave phone exactly on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
