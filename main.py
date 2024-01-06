7f5dcbe80ab2510819f7286b1cc592a091976c67
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:16:48 +0000

    goup fit mouth mixture several

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
+def square_number(x):
+        return x**2
