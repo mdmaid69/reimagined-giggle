991ce8d7e0608625e6dc009b22a6f4f6a9508f79
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:16 +0000

    congratulations concerned Uu speech pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
