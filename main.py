e519ff9f682429b7d88b575c2b436ed31402a6d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:13:00 +0000

    see hit shelter speak road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
