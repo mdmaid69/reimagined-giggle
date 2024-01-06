32b1eca21ae45328b452b968504b4509e50c0426
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:04 +0000

    570enjoyable owe ground nearly admire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
