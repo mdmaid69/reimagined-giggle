988866cd9ed9640f47dea96cd5f882963ec8fe22
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:39 +0000

    plate brush interfere powerful will

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
