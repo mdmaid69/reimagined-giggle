4fe7fc501843e1540f15368e4326b4bf696a5938
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:45 +0000

    grateful material hang separate thank

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
