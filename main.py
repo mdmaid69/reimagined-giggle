c445fd91f36db3a9b7ccbda42dbaaa19011a3c76
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:26:36 +0000

    correct callout office develop accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
