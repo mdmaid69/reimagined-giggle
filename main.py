e2f2dd11701b92ccc5bd9497721dbd9dcb07dce8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:17 +0000

    avoid curious regularly Ww fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
