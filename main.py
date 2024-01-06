5f39cd505f6e5124188d031b6fc472c39b7fdf34
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:18:15 +0000

    sensitive pay type flavour transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
