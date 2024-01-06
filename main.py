fa2709645e4ec70731ac32ae002c289a902eee4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:55 +0000

    some record very loose later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
