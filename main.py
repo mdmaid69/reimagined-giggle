d171561073ba2afee8342b1f5731a8b7a1ae358a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:31:43 +0000

    slide criticism example ought satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bool(array):
+        return bool(array)
