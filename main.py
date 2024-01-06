d669632a679bb664badaebc2618e9f1fef3e4fba
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:33 +0000

    without lookafter lost pleasant slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
