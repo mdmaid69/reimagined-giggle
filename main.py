d63f11b03351f1d087cb46bccf8278a07d33631e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:30:14 +0000

    wonder out hope final develop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def sort_list(lst):
+        return sorted(lst)
 def is_palindrome(s):
         return s == s[::-1]
