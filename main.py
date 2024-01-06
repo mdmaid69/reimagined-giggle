da8a6e0fc63ae21fd25fb165a68e699595466b17
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:42 +0000

    task scrap pollution gradually themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
