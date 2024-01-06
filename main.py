14686ef425cff40b53e606a964f1675cb10e788e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:41 +0000

    achieve blind think face sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
