6b9e6581f3182ee02c88eacba7557955b7aadd54
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:46 +0000

    apart damp emergency commit wander

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
