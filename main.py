9628c8eeb997a9b5ed7699cd565eae8a8b1ac1bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:18:56 +0000

    block thank dry content everybody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
