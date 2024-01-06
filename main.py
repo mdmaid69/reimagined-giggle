9159c126dfe1c4768243e24076009b5b6876142c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:59 +0000

    rise surprise only why level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import collections
+def create_user_string():
+        return collections.UserString()
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Powers of 2:", [2**x for x in range(n)])
