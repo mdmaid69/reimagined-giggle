b60001f234fc565aa8f55cca4acdcc9fcca66614
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:45 +0000

    average develop aim room pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
