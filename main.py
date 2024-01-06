93c7b0dc6bd37451c6f15b3a7b853151f8dd18a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:40:37 +0000

    steady either cinema increase favourite

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
