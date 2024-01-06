37fbad26cd4a9e7a6f32ba92181602b6f92ebcde
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:40:38 +0000

    anywayanyhow opinion world practical step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def find_min(lst):
-        return min(lst)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
