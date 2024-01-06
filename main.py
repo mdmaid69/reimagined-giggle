377e5f7208da36fa65076e949dc87059c44cfe8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:50 +0000

    majority really gate start own

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
