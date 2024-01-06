ad02609cd8bcf0016068fb13e4cc51e789ad8139
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:36:31 +0000

    take shut ask shop similar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-print(random.randint(0, 100))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
