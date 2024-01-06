f7baa66ea2ed02b7c432eb8851db249454504765
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:55 +0000

    shrink include tone thirsty air

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_sine(x):
+        return math.sin(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
