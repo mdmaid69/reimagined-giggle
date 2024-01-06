2caeda21243717ea0ae58634712cbf6014019b8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:02:27 +0000

    lot press tooth grow shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
