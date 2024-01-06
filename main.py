f9c89efb35779be6ff3db122f620d19a2de79258
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:10:23 +0000

    case part recently bad sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_density(mass, volume):
+        return mass / volume
