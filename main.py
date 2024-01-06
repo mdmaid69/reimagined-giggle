196ed02f552fc915032823039053b59345e4a457
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:16 +0000

    prefer through activity track attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def cube_number(x):
-        return x**3
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
