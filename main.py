74eb7a5bc76cc21607a76bf546174e9b3db2bc5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:19:36 +0000

    godown breakfast emphasize advantage advanced

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
