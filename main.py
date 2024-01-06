292551dfb373a1981871c49daa94a509cc00d761
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:01:26 +0000

    gate spin closely keepfrom everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
