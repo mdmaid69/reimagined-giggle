3f9bc4beb5d940cbd77ec649fecb5994de014841
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:51:40 +0000

    or blow geton shine pump

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+print(sum(range(10)))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
