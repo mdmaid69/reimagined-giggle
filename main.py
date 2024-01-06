af7a63c59706e83b79f0631c10a73a45636ee148
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:20:07 +0000

    rest direct advanced partner sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
