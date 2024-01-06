a2f9d276ea9e54c7bc91c9f70b9231aaaa0f367c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:15 +0000

    escape free tragedy strict become

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
