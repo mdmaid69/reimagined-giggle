324cb1bf4340b94194eb5d8c3b5ed1ee816a12ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:02:55 +0000

    post language may mine storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
