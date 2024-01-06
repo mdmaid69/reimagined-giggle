af7b52ef4acc4a2a4ff5a727b8753498ea1f77b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:29:45 +0000

    wish several pause head later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
