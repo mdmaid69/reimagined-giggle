42059085aef2f940c2337bad5f1f6c72a3b69ed0
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:28 +0000

    loose picture believe from subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
