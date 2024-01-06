38a383fdaa9c815a16d2bed03d11116bccbf58d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:41 +0000

    wind divide means minimum embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
