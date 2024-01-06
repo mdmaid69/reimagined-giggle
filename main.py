514e9e995d23531881c90191278855a51f127a1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:24 +0000

    street float occasion yesterday anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
