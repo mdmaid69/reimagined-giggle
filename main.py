0c92ca1f94e45396604ab15d38ceb4009b2df896
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:47 +0000

    turnup deserve protest leg prepare

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
