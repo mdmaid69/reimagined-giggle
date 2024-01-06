6a396ff4b21806647811654d4171e2ab532958aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:55:53 +0000

    turnup on late sell mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_speed(distance, time):
-        return distance / time
+def create_user_dict():
+        return collections.UserDict()
