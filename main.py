4d1ad33a66729d0c603f308aa47f0f494da3ed53
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:39:38 +0000

    distance chance physical measure create

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
