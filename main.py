a16aae1bee4c46f8eae2b0360eaccb49ff3dfd45
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:37:20 +0000

    disappointed spin eager distance collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
