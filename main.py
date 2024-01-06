5e948b4dc4c250b51d4724c8ab720064c36dd1e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:48:12 +0000

    naturally age sound watch such

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
