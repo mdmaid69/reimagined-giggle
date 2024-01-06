426cf39209394cea6464d53ffad84b66530ce9a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:59 +0000

    people everything judge interest shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import time
-print(time.time())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
