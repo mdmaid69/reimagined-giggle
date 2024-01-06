ba46e29c315221d60596a6e6a119219b4fd03602
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:53 +0000

    brother otherwise important along powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
