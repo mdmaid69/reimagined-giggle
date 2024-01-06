f61b5b39f395f5a39fc7b8ea57167a11dca0c796
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:43 +0000

    Cc petrol waste platform often

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def convert_to_octal(n):
-        return oct(n)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
