dbb9e72e3696bd62e1cac91a4d7d384689d93a3f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:57:24 +0000

    blue through ourselves dish fair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
