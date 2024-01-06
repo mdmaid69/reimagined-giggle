054745dea9fc7433f29c2bd1a2b147313b79ada1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:34 +0000

    rub reserve rush rather position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
