f0ea3c971d390f9b08c08b2987a2484879a2e814
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:15:02 +0000

    cheap Mm so bus go

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
