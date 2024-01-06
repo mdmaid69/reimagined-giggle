725e4c57e358ffcc7a3ec6808fd9a7606c4a7edb
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:53 +0000

    basic fairly refer have mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
