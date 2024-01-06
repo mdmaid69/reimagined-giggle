2cedec4f06c75831f7da1d66b2efde8dc045a2f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:16 +0000

    mail father collect awake speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
