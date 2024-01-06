909275e463e5f8208443b492965c63e6479d6ad2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:36 +0000

    breakup moment concern ocean floor

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
