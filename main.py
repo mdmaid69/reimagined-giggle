b5cbc326cdc4d5b2fc411d5756162fe794ecfa67
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:22:25 +0000

    takeout miss consider mixture talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
