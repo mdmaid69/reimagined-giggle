85da6963aa7f590d4fc358a87d060128c87c739b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:51:33 +0000

    watch pretend deliberate do shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
