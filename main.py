7f961a539faa21ab36ddfc2f9295a44abba3e476
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:37 +0000

    unless face glass upset dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
