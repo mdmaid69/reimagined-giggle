b25ded0d3f2605f5810c2fe26388b5a433b19af5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:00:12 +0000

    backwards break large minimum night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
