9750fd4fbf41fd281561f02223e9e0eb56f8f12a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:06:44 +0000

    admit pin business announce sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
