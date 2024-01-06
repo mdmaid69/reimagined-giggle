2b4382e33941084998fbaa559107534f313ce2cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:50 +0000

    my listenfor pile dear favour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def count_characters(sentence):
-        return len(sentence)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
