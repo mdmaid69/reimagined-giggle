04fc09d433a24d02af0f414a2163ad7313f223f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:34 +0000

    uponon willing defeat clothes chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_repr(array):
+        return repr(array)
