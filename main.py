b91aa1c7471daf48921771c2bd5781a52962148d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:25 +0000

    job jump urgent big where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
