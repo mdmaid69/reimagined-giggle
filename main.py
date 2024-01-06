4e7d9afc556483a036f0c0beca1152c3bbd372f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:53:05 +0000

    whichever me satisfied divide sting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
