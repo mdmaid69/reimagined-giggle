7b56ebaa5ac30d2ffaaf0e14f4daeb9cfd9063e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:04:27 +0000

    can conclusion rid list Gg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def reverse_string(s):
+        return s[::-1]
+def reverse_string(s):
+        return s[::-1]
