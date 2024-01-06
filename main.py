40d387863d055cb233cb1342ad18c69fdfc4bb81
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:40 +0000

    violent unit pump careless temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
