530bbed92c9e6fbbfdb2f6612b7a9e36e7af1cfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:39:40 +0000

    temperature married mine lot slight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import numpy as np
+print(np.array([1, 2, 3]))
