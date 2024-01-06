31e622f386bf9bafa7dbcd9600f514d33c0ab55a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:49 +0000

    team bin dig bend again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
