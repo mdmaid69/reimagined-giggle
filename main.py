0bd73eeee95acfd284a96182e2199d4ddf8d9ae3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:23 +0000

    scared honest once spin under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
+def count_elements(lst):
+        return len(lst)
