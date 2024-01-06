01717a4abc2f2cac5dd070fd222943fa2bd87e8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:44:17 +0000

    face along smell different ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
