0b3e87063293588e41fdc43a47b5f7db96c986f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:36:05 +0000

    able bringup born home whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
