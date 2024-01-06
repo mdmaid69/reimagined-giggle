6093973a0e299d6eead8bca537b153c9d65f7fcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:30:54 +0000

    stage furniture come fast like

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
