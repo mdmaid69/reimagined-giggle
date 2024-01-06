74188ff97dc60dc677791136a8fbe690c87968d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:03:14 +0000

    shape partner class beginning no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_area_circle(r):
+        return 3.14 * r**2
