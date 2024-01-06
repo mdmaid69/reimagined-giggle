45e81e5244063f041544d34c6abcf096b8f04a49
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:20 +0000

    earth cheat post nose rush

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
