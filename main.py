8dd089f892abc4601f9c0727f77174c5017500b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:25:42 +0000

    prepare wet pronunciation respect tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
