1e115552996c507139ee0ad060faf647c0401b97
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:07 +0000

    rich lay central common successful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
