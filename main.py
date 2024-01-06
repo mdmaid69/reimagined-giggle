fa460976bc4220e79516de5bd3e05afed03afd50
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:49:05 +0000

    notice family regret congratulations valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
