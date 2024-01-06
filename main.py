9f1e7b29cb0b3863632f38acf46f5d0c1957998f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:50:37 +0000

    satisfied about choose upstairs each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
