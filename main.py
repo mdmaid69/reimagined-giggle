d54f839e2335a3a2db89b46402833ecaaa5da0c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:46:33 +0000

    taste stop thing ignore swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
