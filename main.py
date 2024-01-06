3ae4262a28e8d9e9f01b7cf3369d30d5eb6ef1df
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:04 +0000

    count separate matter makeup certainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
