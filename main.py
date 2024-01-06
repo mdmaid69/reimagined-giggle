7c8f14e104cc010bcdd68bade3a82266abec5d8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:54 +0000

    agree study earn office fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-def is_palindrome(s):
-        return s == s[::-1]
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
