9b6c40b90bf3baa6af9070799c22969a8ce86ad1
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:54:45 +0000

    crowd very holdup trade do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-print(time.time())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
