0ea426c528d0d20673c239eefd8a85f84af904be
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:41 +0000

    hold turninto shall dish phone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
