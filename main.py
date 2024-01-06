7934cadb0f5e00b76c58304a38d5c0bf2ed6bfe4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:01 +0000

    file smoke sudden Oo indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
