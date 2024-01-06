8db8be34788a77c1bef60657cba02235d1e976f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:51 +0000

    shock onto say heart characteristic

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-print(sum(range(10)))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_speed(distance, time):
+        return distance / time
