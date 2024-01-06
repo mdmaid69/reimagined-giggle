3e0a1e76326c317a40c683638135287e9e579c6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:59 +0000

    task extra favour dear wild

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+print(sum(range(10)))
