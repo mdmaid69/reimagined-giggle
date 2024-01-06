c35b42e0ff22e8ab209ef6687249a89c17dc6a2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:39 +0000

    plate but thick loud fail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
