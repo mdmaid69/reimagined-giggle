c23f71c9f7971a127d37302cb2605f060cd209dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:46:27 +0000

    minority regional find congratulations kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
+import getpass
+def get_username():
+        return getpass.getuser()
