599eaf159461dd62910727342431ea3f6aed2855
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:57 +0000

    mood further strange crime danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
