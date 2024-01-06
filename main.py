031f0e1286a0568e608e7c24d459b0f78598bee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:15 +0000

    defend keepaway paper object simple

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-print(sum(range(10)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
