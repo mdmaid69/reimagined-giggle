ccaee58dcdd5a3e3009464f77f9f1f67e954ce0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:13 +0000

    road happy convenient raw comfortable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
