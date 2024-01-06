1843f2bf5781dd8151102e3f2d0413451d146e5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:46:42 +0000

    do compete good middle say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
