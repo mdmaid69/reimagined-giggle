835b67539d4184d8f43461c3120afc8b1babfbb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:26 +0000

    service start describe window late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def sort_numbers(numbers):
+        return sorted(numbers)
