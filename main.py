807f87250e134c350235e1cf3a71620d716ddb95
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:08:56 +0000

    block gift negative goup release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
