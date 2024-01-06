324b8571598ed96f3abf6ec4eb20fa48c2aebe13
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:32 +0000

    height preparation fight fence lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
