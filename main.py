b708543bad483b7a7c75b61cd45aa362d9471bc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:34:58 +0000

    turnback enthusiastic success 573entertainment brother

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
