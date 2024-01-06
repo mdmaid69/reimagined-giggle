9287b502a289a18837deaebd1b02667ca71b68f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:22 +0000

    below attempt accommodation skin criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import json
 def save_json(data, filename):
         with open(filename, "w") as f:
