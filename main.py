cfb6355fa07351006357bf52ef7faec7ddf2a1c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:01:30 +0000

    phone tense forward under wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-for i in range(5):
-        print(i)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
