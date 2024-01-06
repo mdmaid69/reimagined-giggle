b7cddd5d79ab0e32318de14d7c7d1df7badcd677
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:21 +0000

    shallow colour separate by another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_list(array):
+        return list(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
