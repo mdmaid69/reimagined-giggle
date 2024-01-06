5e1ba5287c589c0c0be237edc12a78e52ae37cc2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:00:51 +0000

    Itself now kill quickly unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
