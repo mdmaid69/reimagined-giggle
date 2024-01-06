364e22bc5479bf9959f07a573cda8da3cdca2710
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:11 +0000

    fashion tough related mine half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
