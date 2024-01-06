00251174a0709b117b512bace0c88c899ed05d27
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:15 +0000

    herself sweet agree point regularly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
