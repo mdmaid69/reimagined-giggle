d7a22e4ea10c2a1592fe82a5f3fbb72a1ae52085
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:45:33 +0000

    Jj position interview idea agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
