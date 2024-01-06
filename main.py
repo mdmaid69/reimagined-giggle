ef16b7808298f752fdda8120e124fd36f098558b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:33 +0000

    gas year ready desperate feature

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
