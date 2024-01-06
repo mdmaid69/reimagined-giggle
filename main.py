b9179708bbefd46deabd09737e45f6966ffb0f37
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:29 +0000

    suffer little rise under turnout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
