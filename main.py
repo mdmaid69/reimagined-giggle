20ae122245a2af5ec7d36002e463e9eed076a0a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:39:51 +0000

    recommend description bored shelter hand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
