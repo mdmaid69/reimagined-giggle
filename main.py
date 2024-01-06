685d634eed41e1d0a9724b77281a3ff526c7e43e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:26 +0000

    music wave dull start fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def convert_to_json(data):
+        return json.dumps(data)
