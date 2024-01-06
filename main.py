454f4cb90cadbeffd278b348e42265fbe27e6cbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:36 +0000

    scrap hour grand gas period

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_power(work, time):
+        return work / time
+print([x**2 for x in range(10)])
