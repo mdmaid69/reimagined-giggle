646e9596bd468e3e61b0e396b9de1c44ca17aa9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:37 +0000

    kill sail excited whether safety

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
