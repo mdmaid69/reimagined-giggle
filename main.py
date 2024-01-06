9e71f1b352aeb44a45cb7e47c09e978d48b40918
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:18 +0000

    once load true star past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
