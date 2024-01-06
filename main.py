78d5f33b7455dee43b379a1711a1ede1f7c38b05
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:40:27 +0000

    defeat file use pull drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
