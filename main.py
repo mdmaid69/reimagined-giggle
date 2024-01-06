3e52f21dc45aaf3e805fc7345912882b471b5d59
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:56 +0000

    punish more deserve error track

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
