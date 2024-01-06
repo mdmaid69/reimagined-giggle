7ed3664e52e78fd917a2a5151ac52c2ede5a2c85
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:36:07 +0000

    steam limit result every warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
