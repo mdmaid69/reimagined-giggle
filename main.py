d5e00031b9811fd32107ac385b05943e1690e600
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:30 +0000

    direction 571enough choice emphasize trap

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-for i in range(10): print(i)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
