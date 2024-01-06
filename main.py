7465bf4e5c32451a9c38f2b70bfc8c98191048ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:29 +0000

    disgusting official rank advantage bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
