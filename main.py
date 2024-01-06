8c73d5bacf3305dc3da8abf6de2928d628a1455c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:18 +0000

    waste balance contain influence spare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 def convert_to_hex(n):
         return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
