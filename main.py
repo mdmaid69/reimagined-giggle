fcdc2b84bc8a8d880befbf87212c580cd08e826a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:51:35 +0000

    feature tough collection ready chin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
