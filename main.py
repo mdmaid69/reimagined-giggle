e1bf0436ae258a3553d5c37aa8fc638ce8cc3f25
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:37:52 +0000

    arrive fact gooff butter fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
