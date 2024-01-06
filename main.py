ebdcecf7ae172d0d9c650546fa9daea9dfd7ae0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:12:20 +0000

    voice heavy matter doctor recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
