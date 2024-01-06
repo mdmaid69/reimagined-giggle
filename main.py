32fc145fbc8461f885e2cb8fb15433e76a5c3126
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:12:06 +0000

    shallow easy thirsty hurry fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
