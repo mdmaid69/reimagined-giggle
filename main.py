2cd185541ba575eae30e7962125f05e726ace9a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:31 +0000

    flow guess on lookback(on) anywhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_user_dict():
+        return collections.UserDict()
