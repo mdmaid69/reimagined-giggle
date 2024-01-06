abeac06d97693b267ea88740955f7b4901bc6e99
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:20:33 +0000

    become sore slow truth prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
 def get_array_as_bytes(array):
         return bytes(array)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
