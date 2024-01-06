3e3d0a1fba5e4734057a598c80d1913cb2ea65cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:06 +0000

    ever occur morning shop exchange

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
