bdb8108808e496085d1f8d010908bdf274518d75
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:13:56 +0000

    politics invitation building punishment environment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
