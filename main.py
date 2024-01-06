7eef139b290aa0eb80f61d6430fa70afa42eac20
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:05:17 +0000

    with at drive increase stare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
