6f3f2155233bfc6b14ccfda740d1a8fb452ba62f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:06:25 +0000

    natural can butter loss catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
