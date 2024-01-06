e464a600f70d502b1f18de95a7ef25d50a7ffdfe
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:34 +0000

    ought truth once ask any

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
