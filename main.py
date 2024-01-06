3b4b9489148b7aa396f0516ff2ad3cf27f9a11e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:07 +0000

    eager stare 179bite offend show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
