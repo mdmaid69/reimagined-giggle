1efdccc5787ab0602dce117864acf2f8978dc761
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:50:47 +0000

    pump exam Kk wise dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+def multiply_numbers(x, y):
+        return x * y
