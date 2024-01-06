7b0ecbf8eccd77b26a6cc660a2e47bc840d49e45
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:28 +0000

    neat broad depend habit butter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
