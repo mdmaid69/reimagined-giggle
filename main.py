f9f0bf520233a3d76f1bbfc6dbb1d55bed80bf3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:40:44 +0000

    imagine yours Jj smile 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
