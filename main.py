b62ec7a45b4bf0949ddce264310bf740b87155df
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:32 +0000

    pause story unusual ilovedicks introduction

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def count_words(sentence):
-        return len(sentence.split())
+def convert_to_hex(n):
+        return hex(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
