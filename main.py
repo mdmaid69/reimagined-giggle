ce3ebca81ca4d2676d9c0a326578e62d18ff4a5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:47 +0000

    handle hollow properly once level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-print("Hello, world!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
