60d0fb0d7e8a7994f6ddf7fef36f5fa26b50daeb
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:47 +0000

    rate wish Jj phone detail

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
