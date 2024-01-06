b4814018fcd8f78312fb29277079ef4a3745cbba
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:10 +0000

    at owe deliberate offer importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
