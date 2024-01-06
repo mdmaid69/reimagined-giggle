7f056507e0508fb913dbf291b0b478adb8c36d2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:01:57 +0000

    confident activity soak letter region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
