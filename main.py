633d4b0f4f84e3124aa8f6615747837d8fad22f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:32 +0000

    reasonable naturally borrow slight emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
