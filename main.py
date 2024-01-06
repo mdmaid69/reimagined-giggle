aa5f180935c795dd83df5035566e095fe74968f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:34:49 +0000

    objection yesterday theirs smell active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
