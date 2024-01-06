10d544ab6699bd12edeeccf4899dcb56cf9087f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:51:43 +0000

    little them breakinto smile earth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
