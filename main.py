93a76fd92d54826074d8103c23f95ccfc815780e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:55 +0000

    arrive similar duty peace must

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_odd(n):
+        return n % 2 != 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
