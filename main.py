9378b3395914bc84dbad32111e8c4b3624a69d03
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:22 +0000

    family Bb breakup deliberate bin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_sine(x):
+        return math.sin(x)
