054ee416ad3b36ab21d06215c8899f068eb8f336
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:51 +0000

    ready sore Kk punishment somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-print(time.time())
+import math
+print(math.pi)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
