b26c4d75c086662fb608b3ae94dcacfe8ec24abf
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:20:53 +0000

    pile sore market though minute

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_area(radius):
+        return 3.14 * radius * radius
