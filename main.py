aa463adfd11338a6a15e3a888dd6cceaf39a432b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:51:12 +0000

    silent Dd hurry pool sky

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def convert_to_octal(n):
-        return oct(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import getpass
+def get_username():
+        return getpass.getuser()
