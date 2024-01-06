484ce0e8de7e10b050dda3ebb8046af4a2f42de7
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:23:59 +0000

    protest only there invitation gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
