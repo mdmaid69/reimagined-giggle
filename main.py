a465dd27b405f94d799a615e35bb542a9d304353
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:26 +0000

    not cry coffee seem move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
