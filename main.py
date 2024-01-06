d0368ce43e2d956ad34e268bb696ed8c6f3dfc4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:49 +0000

    long dream wide discover rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
