439ecc82d8027575e68e72f1c7daa76cff92cba6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:46:22 +0000

    sometimes exploration pile class harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_typecode(array):
+        return array.typecode
