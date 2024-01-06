7bec1aafa4e96795c7c9e16528ae66a367e4171f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:22:12 +0000

    wonder valuable juice calmly wealth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
