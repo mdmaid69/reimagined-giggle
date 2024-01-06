908e5920047dd7638014e04fba9a59f8947b1d28
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:05 +0000

    view thank right stone quantity

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
