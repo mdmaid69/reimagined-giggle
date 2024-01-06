b0017ecae24f57183e7fffeca8a517f5fb823be3
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:14:23 +0000

    part main sensible attempt square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
