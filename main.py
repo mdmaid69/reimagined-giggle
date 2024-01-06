1dcc4e810b5675b54fb656c6533b8922d8025447
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:32:45 +0000

    begin persuade container concentrate cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.time()
