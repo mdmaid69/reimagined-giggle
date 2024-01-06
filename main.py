7519d5a19efb51fc9f696869c5481276eb39d61e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:58 +0000

    later use laugh soak tune

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
