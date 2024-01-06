079862ebb9bf98f8f504f2fc36dca33654edb4d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:29 +0000

    so traffic difficult plate Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
