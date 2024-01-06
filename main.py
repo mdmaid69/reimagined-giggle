cccc233cfbcc2137620220c0ce41a349d8eeddbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:21:29 +0000

    hospital hair workupto people position

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def is_odd(n):
-        return n % 2 != 0
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
