288092cf9246c7bd4e1681c5b3022e76906c54bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:51:56 +0000

    bring acceptable discussion if Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
