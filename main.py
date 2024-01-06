6621d8d6cfe52f2c2d1ad8c85ced1e4c92560a0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:04 +0000

    aim lift power face charge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
