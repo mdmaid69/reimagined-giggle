b3ebb36447cb4f8794a5427e6516cc2a9ed397b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:39:07 +0000

    friend rush match check write

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def sort_list(lst):
-        return sorted(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
