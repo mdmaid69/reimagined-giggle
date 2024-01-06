08a093bf2340f61db78909dc4995fb9f077bba9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:34 +0000

    correction reply hardly similar dear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
