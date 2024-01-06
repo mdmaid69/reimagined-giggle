ab48e39f8ddf8555f5bb4bb85cb672aca0805f8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:19 +0000

    satisfied stretch field wind enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
