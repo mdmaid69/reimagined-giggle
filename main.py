681b8f5ff549623d822d14573cd61f6789ed0637
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:22 +0000

    onto public plenty children back

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
