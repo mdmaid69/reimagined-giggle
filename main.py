ec4c2eced43430e1cfe732427c6e33eb3c293200
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:57 +0000

    runinto tell instead avoid sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
