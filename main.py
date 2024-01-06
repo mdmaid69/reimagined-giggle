862d0534a907fdbcb5c5d5887884effcde605ffe
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:50 +0000

    appeal nothing tonight holdon towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
