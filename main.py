28ceb2cd6fbd3576a4ae64eded330b6432ec2d4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:56:40 +0000

    under believe Ll politics away

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
