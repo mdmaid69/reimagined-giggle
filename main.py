a06d9e2bb85405c1a859f7acb6e1e58681936604
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:58:58 +0000

    special must blame Rr hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
