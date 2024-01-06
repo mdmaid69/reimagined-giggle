bc9a18b6083eb2ac825ded9aa19b947abf7f55b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:47:51 +0000

    mistake attention ashamed comedown count

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def remove_duplicates(lst):
+        return list(set(lst))
