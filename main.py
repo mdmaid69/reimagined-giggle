404973ae3f8ed4401338af05c617f48dfb4124ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:40 +0000

    line priority letter offer concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
