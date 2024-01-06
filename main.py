bbf4a33396ad141e339872275dab24ce017352f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:01 +0000

    communicate culture light live everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_stack():
-        return collections.deque()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_float(array):
+        return float(array[0])
