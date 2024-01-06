a5d74fb443c530a4183dfd165c58502c4c59b182
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:15:17 +0000

    trap doubt stir tooth suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
