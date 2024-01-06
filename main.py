9c2a11e91a890536719923f2ed18ec27920831bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:34:22 +0000

    habit language unlucky eye bound

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def print_python_version():
+        return sys.version
