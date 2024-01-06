cd49ced4bf0ae6a8481690b59a1c22ab9c33b316
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:52:15 +0000

    bringback surprising risk letter gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_inverse_hyperbolic_sine(x):
         return math.asinh(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
