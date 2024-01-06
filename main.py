c765a16f0028d9894db638b2593ff8af6161400f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:34:17 +0000

    pupil space thank afford handle

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import os
+print(os.getcwd())
