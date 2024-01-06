1e4498344c2d92f66eb371758746d28bb30c642f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:00:04 +0000

    deaf transport church declaration exchange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
