334392b66ef2cfe9ba140c737cc2f3c32d9b390d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:49 +0000

    passage disturbing coat plant water

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
