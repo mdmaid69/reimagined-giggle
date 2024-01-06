e8e83a5068873e46ac9af7ea37b76fd972a382cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:58:25 +0000

    not tidy describe area about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import time
 def wait_for_seconds(seconds):
         time.sleep(seconds)
