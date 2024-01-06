4dc7b5e087d22b858d66652ab1cec1284cb0af23
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:04 +0000

    Ww argument ignore identify come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
