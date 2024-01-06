46d9577c225cd40513f88c4b10a9f18807884871
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:38 +0000

    fit miss translate form car

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-print(time.time())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
