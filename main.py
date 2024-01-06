7f87a213f8474b848996bcad9654fd77dbda0606
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:48:58 +0000

    even smart common statement equipment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+print([x**2 for x in range(10)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
