9e9ca985eed1c6a438dd3d9c28e317a27f741f98
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:19:10 +0000

    tongue free large hungry sometimes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def print_python_version():
+        return sys.version
