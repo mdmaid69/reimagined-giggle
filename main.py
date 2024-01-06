0ff7092e1f24d5a05126dcfae0eed605a0732802
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:55 +0000

    something that connect rely window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
