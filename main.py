8f10b66473adbe60e535cc5c1854bbe36114f5a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:01 +0000

    damage Vv change body prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
