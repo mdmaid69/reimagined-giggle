3181e266c76b16e42e9f23fc8f6cfd826b242089
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:53:10 +0000

    smell basic sentence correction slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
