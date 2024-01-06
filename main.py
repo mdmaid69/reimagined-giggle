5975789bfd92020e1bf9d7038c7a9d102977560c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:59:49 +0000

    crowd transparent service light book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
