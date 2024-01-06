6b0574e6a8c0409f4c3eb142b9eb7720f9a84355
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:56 +0000

    mother estimate enthusiastic friend limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
