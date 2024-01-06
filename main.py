b4ac9dff414cb65a5746135c8f294a58aa6cc6ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:30:26 +0000

    reserve experiment centre colour keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
