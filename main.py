95b35db7802ac517a0c2639d1be3f953b1dd7738
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:01 +0000

    famous Jj glad at poor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_length(array):
+        return len(array)
