0b506e5c7cbd5392b7b320f9f2c6aa7a7a0813ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:37:53 +0000

    load glad wheel support away

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
