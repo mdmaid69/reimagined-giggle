241afc29f5f929bc2e105b2a8324f52eeedef8b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:10 +0000

    statement turninto last possibility sister

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
