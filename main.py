30839102045d9fa8a9a1af31de3e41c372866d08
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:06 +0000

    above poison tomorrow fix gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
