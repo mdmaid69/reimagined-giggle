9dd10de47f9126cb63ff2ab528d07891a150cb2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:22:58 +0000

    suit especially makeup model thought

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
