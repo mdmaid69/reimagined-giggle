eca3c2c8f9e35e2237518da5373f102c9fe7e564
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:36:02 +0000

    next skill home throw account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
