e37008cbfbe6c06a71a357cfd4d2de93a4cabdf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:12 +0000

    design disappear order mean door

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
