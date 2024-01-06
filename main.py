ea4348ba16576f2f28d0c57d4ac9cb10e1c32b4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:52 +0000

    OKorokay act welcome cap please

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
