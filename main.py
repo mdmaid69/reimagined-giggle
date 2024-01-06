23cfdc2a204113ca171396e9d69946881130f73d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:33 +0000

    blame while warning play nonsense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print(sum(range(10)))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
