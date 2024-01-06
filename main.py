dea6a5edde7d73d305c5af1c4a2058a7fb6fc41a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:56 +0000

    live report lower party heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
