33c35f866199e891b7a857f136a3ce29e734286e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:07 +0000

    form doubt punishment exchange breakout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+print(os.getcwd())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
