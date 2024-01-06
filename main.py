104c0d5db60187dc8fe62f9b79056ddaddd98d8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:52 +0000

    trick final welcome lesson work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
