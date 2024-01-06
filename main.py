d954881329e11d7024e858b5efc7a6c23de49f1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:23 +0000

    too wish finally point nervous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_elements(lst):
+        return len(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
