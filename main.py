eaf56225f39786fed1afd3870e279881c13b748c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:14 +0000

    about tough dance attempt this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def reverse_array(array):
-        array.reverse()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
