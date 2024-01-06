34181ec47a9e0f4c13170de03468a06706c86ee7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:54:40 +0000

    suggest claim watch play direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
