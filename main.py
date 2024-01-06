60feba77244d89654cfee1f837b07654a5b5dff7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:05 +0000

    platform neither them nervous hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
