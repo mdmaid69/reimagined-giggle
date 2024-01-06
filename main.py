6f7c8df7db0998772a1917c39ac0e19ad7038a2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:13 +0000

    powerful block demonstrate measure impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def read_from_json(json_string):
+        return json.loads(json_string)
