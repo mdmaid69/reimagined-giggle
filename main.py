3e7328042c20a066089b1ae51d3648cf53001fad
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:09:05 +0000

    gap low cost speed guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import sys
+def print_python_version():
+        return sys.version
+import os
+print(os.getcwd())
