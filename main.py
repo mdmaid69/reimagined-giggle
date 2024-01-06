ce88fa78f13395175f537e9fe156a88f21549a14
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:11 +0000

    fairly great sell treat bank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def cube_number(x):
+        return x**3
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
