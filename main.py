323000f597956beff0a8323e355e2a7e26a8e536
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:16 +0000

    listen war autumn patience environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
