dd6dbcfb659fb3b85c8c9fcee5dab00d787c5be4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:57:01 +0000

    congratulations time appear delicate action

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
