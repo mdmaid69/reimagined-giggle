fce0dbcf60c5b47fc668dd10d2b98f053f75464f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:55 +0000

    even loose hear small would

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_rectangle(l, w):
+        return l * w
+import time
+def get_time_since_epoch():
+        return time.time()
