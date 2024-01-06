88435c38d49a276d3b5cc68070fe49735d65803a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:38:14 +0000

    trust discovery children glass release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
