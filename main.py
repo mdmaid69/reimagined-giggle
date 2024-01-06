96931ffc7b9b9f0b99c171100249e92e752a74ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:17 +0000

    keepup callout remain feel front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
