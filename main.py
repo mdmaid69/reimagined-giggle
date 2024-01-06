6f688280330bfc2257a78e444944bded82c5ad55
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:32:55 +0000

    shout although prize wind goon

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
