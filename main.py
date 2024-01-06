a7fb36dfadf32aff0bbf55e07350df2a906ccb94
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:26:22 +0000

    pretty deliberately stage interview little

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
