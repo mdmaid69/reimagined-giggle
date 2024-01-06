828ec0f0b53576c051051054b60c3b69ea2df9b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:34 +0000

    shop official capital wheel dear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def get_array_as_list(array):
+        return list(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
