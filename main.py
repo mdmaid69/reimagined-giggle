fb395942188dd6dc9cdaa37f306b864729086511
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:54:15 +0000

    promise reliable bill young cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
