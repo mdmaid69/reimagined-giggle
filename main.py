9a1e55bf4457170eefb20b031895012224ac7c4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:00 +0000

    quantity thought high system appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
