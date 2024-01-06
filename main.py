68008894afc923aff2b91184775447f4ef838183
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:30 +0000

    score dark illness relative valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def convert_to_octal(n):
+        return oct(n)
