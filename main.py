40fefbb494d2a97f687e70efde99a1783defd8e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:01:34 +0000

    quantity character peaceful guess both

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def sort_list(lst):
+        return sorted(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
