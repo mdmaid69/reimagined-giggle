c618d1792e0f52340ebe38b3f86e5ca6b3ee7171
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:14 +0000

    week born work rent learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
