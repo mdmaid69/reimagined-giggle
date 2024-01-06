f95b49a5a546e9b747ebec9ba8aa9935ac1df094
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:36 +0000

    there violent bush bringforward gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def square_number(x):
-        return x**2
+  def convert_to_binary(n):
+        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
