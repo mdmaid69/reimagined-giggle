e7166077fe700edb7d0f849283237afd012678e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:37:51 +0000

    hair damp reason early our

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
