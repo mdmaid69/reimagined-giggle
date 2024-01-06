53b21ccba742de051fdb061b0ca8cbed734bae54
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:56:48 +0000

    calmly callat hard ever spray

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
