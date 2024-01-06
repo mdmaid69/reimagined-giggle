8cdbc4a599cfb73c43b6b9574591e81c19568e45
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:02 +0000

    sweep contents need operate prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
