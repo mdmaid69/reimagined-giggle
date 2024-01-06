e05c91ea7b0c637b154ee9f467e7f9f78b4cb825
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:39:04 +0000

    say minor 566end address present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
