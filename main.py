36d8b7486e794947d944535cda12b3e01b70416c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:24 +0000

    herself show defeat interfere supply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
