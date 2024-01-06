95b6afdf301fc658bed5fa4e3ece504512f5e66d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:09:37 +0000

    close mail pause heavy plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
