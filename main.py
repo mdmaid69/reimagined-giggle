25777c4fd6ec1470e451eae12dabfca93f7de248
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:37 +0000

    loose acceptable commit almost content

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_octal(n):
+        return oct(n)
