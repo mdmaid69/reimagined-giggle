7833895fbc8d8b9638eb8fa0cebea33cc775fdf4
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:17 +0000

    yes drive yourself trip first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
