50829498f54eb7b0abd5e107207d5199e627c671
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:29 +0000

    your middle hate nature wise

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
