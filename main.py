0021c64761de5e977f5de1aec0035dc2eac531c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:52 +0000

    nothing gooff me except after

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_speed(distance, time):
+        return distance / time
