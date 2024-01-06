851a85d415f59be0425226b928b887d0f06095fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:11:47 +0000

    mystery call object so cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
