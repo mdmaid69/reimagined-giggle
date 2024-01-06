fd902d08c5fd7a63365bec7f82ff4275716cfd75
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:06 +0000

    combine right dance tool effect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def square_number(x):
-        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
