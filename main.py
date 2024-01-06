308685f6a4a26bdfe873bd4d06c1fb768fb2b9c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:07:49 +0000

    drop sort murder communicate cheek

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
