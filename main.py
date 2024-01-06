2334c256f2d7ff77177ad8c1a9fedca296192071
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:58:27 +0000

    date order circumstance 564encourage common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def reverse_list(lst):
+        return lst[::-1]
