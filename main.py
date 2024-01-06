e070f1f685e0bcb7457bdf763556195e0a5a87ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:38:09 +0000

    urgent day scheme holdup tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
