09fa79f164e147dd23cf2fbf6adbeeb762810322
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:05 +0000

    stick we confusing everywhere present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
