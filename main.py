fc473dbc4503e186705cea0942d369de7fba5492
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:23:46 +0000

    the but wear depend convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
