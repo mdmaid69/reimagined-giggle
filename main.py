655f0ed9255c8409b565e8d3836eef599b8bdb53
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:45:35 +0000

    compare completely tonight partner border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
