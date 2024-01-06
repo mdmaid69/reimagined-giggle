b9325e4e558f0350fdd77c568ef943e48561a5a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:52 +0000

    second inside hole keepfrom gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_priority_queue():
+        return collections.deque()
