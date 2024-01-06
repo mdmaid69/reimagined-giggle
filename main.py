d8e4f02598d26f0a8eea8ed9b5e5caf8db8164ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:02:14 +0000

    agreement process separate community mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
