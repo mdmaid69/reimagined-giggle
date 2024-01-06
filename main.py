8e102f793d11e7ed3c6b961455242d11111e14c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:44:38 +0000

    high doctor choice loud trust

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_volume(length, width, height):
+        return length * width * height
