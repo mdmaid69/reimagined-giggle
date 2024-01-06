2f220ae33c63420b91b5bfbca6b5dd6a195d4fe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:36:40 +0000

    blame tomorrow outside wheel home

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def add_numbers(x, y):
+        return x + y
