ef95e6c4bbbfafdd28f8ce5de0f8454acbba22e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:13 +0000

    ear crowd state present acceptable

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
