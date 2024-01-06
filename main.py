8194f354f687983e74d6779f72dc0ceb46c07545
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:32:40 +0000

    petrol afraid cry spin top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
