9cfee39f7620f8cf741dd494a4d00585b4b525a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:54 +0000

    immediate hospital thinkover runinto control

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
