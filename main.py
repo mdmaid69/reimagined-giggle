8604c0cd789b6518c9b543cd20177fa0b7807800
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:54:49 +0000

    fine trade wipe path pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
