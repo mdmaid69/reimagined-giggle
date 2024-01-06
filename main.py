9b2c2bb6a86ab799b91ed2055d8956c72f5c3dc1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:51:59 +0000

    communication permission for nurse subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
