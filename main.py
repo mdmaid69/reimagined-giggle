885bcc0aa3b7a7a7a90adb34c7e8b2cb43ff6de2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:41 +0000

    against fortunately cross blind cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
