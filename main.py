309e1febe7372a04c22ca5266a3c9af596a4bf2f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:17:53 +0000

    accidental stupid song file city

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
