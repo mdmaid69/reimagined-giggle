ff94c15b353deb76173c2d1ebefd55af418968f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:40:35 +0000

    excellent to can away minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
