c773e222a9c84be9c8867fba95aba3b1f6c5be72
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:15 +0000

    bend flow holdup seat total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import re
+print(re.match("h.*o", "hello world"))
