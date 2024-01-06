27b365ff4ec7ab5c04daf0abf4c694d95e1a95c0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:33:10 +0000

    single please wave beneath false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
