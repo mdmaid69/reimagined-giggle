038cb9ee261a583ed94b3cf63c3952183cf7f185
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:57 +0000

    eat closed busy yet sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def exit_program():
+        sys.exit()
