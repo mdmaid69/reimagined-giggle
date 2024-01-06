2354bccd791de234109646a620605cd4f34ce730
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:08 +0000

    pass cough dry share represent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
