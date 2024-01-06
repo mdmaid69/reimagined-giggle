50ba49951a1413d397e9470720c451a2011f1797
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:41:30 +0000

    generally heavy journey combine birth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
