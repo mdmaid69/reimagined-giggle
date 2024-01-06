e52d8bc5693bc1eead3f5d128201c69949efd7e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:48:43 +0000

    freedom environment combine comeover commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def append_to_array(array, item):
+        array.append(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
