f4857b7f00b300f70c78b366c837d43dfbe65b24
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:47 +0000

    please sleep herself fly experiment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_density(mass, volume):
+        return mass / volume
