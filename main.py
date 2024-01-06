fe3c0d94f758986a77ae7104b4205734b3abdd55
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:25 +0000

    point nurse heavy fall trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
