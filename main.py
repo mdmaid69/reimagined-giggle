b2ad4b286a8259b8fd021f13d2e61dee86125c72
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:09:47 +0000

    temperature allow exactly best fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
