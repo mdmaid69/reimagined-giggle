051852d609f3ec12d9af124002e3469ea89ebae2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:45 +0000

    cost damage our past roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
