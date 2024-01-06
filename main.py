c65850ed7f8be8cae6abf9147738ce3691a57985
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:07 +0000

    conclusion neck meal eye determined

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
