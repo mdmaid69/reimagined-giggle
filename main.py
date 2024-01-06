ed504f422000902cc990b4edf57f7e3c1b66e4ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:53 +0000

    advice dance each breakinto comfort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
