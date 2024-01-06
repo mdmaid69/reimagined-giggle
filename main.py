5066ca713d5733495168ab8ab10383ea43b96772
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:37:32 +0000

    attention wheel aim start unite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def sort_list(lst):
-        return sorted(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
