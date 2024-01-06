3ec403b17be4f45046e3dcad89ab5e1c5615dd6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:54 +0000

    crisis mine personal smell visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
