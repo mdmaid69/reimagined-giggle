5bad06619ed8918e5103fabee4724c26532e76f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:26:21 +0000

    slip regular unfortunate rough belong

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def append_to_array(array, item):
+        array.append(item)
