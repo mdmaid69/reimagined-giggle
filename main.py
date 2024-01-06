656bcf5f240a47bab836403eadaebe1496d79bbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:08:45 +0000

    ocean rest strange move meeting

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
