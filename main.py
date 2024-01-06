6f206dd62051e30f2e1b5d2bd87dcc74cfd8703a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:52 +0000

    notice similar claim surface speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def count_elements(lst):
+        return len(lst)
