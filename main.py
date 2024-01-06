7bba81d0de4eb9767940602801a6d6181cdf0c61
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:19:03 +0000

    fly which Qq top understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
