f69574727fd93295cc6a1ae5f8c391367f958a3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:08:47 +0000

    talk stamp yes gas open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
