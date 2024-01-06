fee88c64d36152d9eb4b7fe6ebc3e0c3bf727092
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:14 +0000

    crowd window niggaacademy understand OKorokay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
