fb8fec43ba3307f5f3ab62ffccb7ff30b3f55b5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:41:00 +0000

    meanwhile stir waste anywayanyhow act

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
