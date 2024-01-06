c847ad4d114f87c04a121a51c929bc075b2eb786
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:27:12 +0000

    such order national subject tooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
