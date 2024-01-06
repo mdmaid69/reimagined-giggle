a4eedcdc05f5e4d33663faa4cbd05b70a236cdc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:41:11 +0000

    backwards takeback conduct Rr grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
