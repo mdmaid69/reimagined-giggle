e169d1590b473a7661fef58f4b410bd298266192
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:22:36 +0000

    before terrible tooth person gift

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+print(sys.version)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
