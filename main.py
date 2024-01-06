a921a12b0fe01d4e59c1be59e465aec85d28b635
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:32:06 +0000

    head actual get contact blind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
