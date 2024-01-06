3cd37cc6eff82417e96e02ee1888d44c66ff3ba2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:55 +0000

    nice 571enough interview operate glad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
