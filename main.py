679aa0500d05a98eb250d025fe15cce53e20a90a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:58 +0000

    imagination art mess answer deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
