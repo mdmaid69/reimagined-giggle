4f6d39fafe47e66926cdb44664b8baa04a59a7c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:57:11 +0000

    make deep previous band heart

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import time
-def get_current_time():
-        return time.time()
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
