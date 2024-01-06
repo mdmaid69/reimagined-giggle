4c1ac37bdc17eff2bcf286e845eb973685709fe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:05 +0000

    yours now eager large wander

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(sum(range(10)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
