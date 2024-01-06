257c66905d8cedfc7a40e40c1556115da82d2a30
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:52:58 +0000

    experiment congratulate month load exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
