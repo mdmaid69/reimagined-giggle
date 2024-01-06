d36ea5f89b60d06f32a5d3b5114f7d32e5e065c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:50 +0000

    trial pile present blind invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
