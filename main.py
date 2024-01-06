599f62a9ca6909fcb686dd61ff3cce40cad92ee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:12:02 +0000

    treat married little remark comeover

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
