5d0616b43884fc2acc4e3723ee8473f9fbb467f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:38 +0000

    fierce special someone interfere tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import random
+print(random.randint(0, 100))
