fbf86efa4263d14c87910a3c616203c323a112af
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:04:24 +0000

    clever slowly match wave middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
