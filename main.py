d5824109cc3788a57bd5060af719f82cc7323d21
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:18 +0000

    evidence clever giveout like plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
