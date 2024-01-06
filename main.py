60c01527458d456a2735804fd2ce68b247445f92
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:53 +0000

    roof actually push anything to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
