86126b772b8fde1e97c0e054507736d74bf577af
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:14 +0000

    strong away consist patient snow

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
