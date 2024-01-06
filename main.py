1bbca111c50e06b105d6209a2d6110df3a9a29d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:39:33 +0000

    callup sail balance trick criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
