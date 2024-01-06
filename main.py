aa7c7013d6e8d005dc0eaa8b005ee45a5f9f313c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:51:52 +0000

    will die mention goodbye port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
