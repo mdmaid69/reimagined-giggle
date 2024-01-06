1b639580e49b109d6e219e1d2abab87c46dff25a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:40:33 +0000

    fire independent instead quick hold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
