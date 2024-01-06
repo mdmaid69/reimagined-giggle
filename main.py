fc4204715673f79a4687e3a83a8cd35b1ec5a186
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:55:34 +0000

    leave limit trust lonely cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
