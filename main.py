683602087c541baca48af31c530d76fa121c11ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:49 +0000

    variety hurt far press general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
