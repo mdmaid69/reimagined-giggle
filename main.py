636d5cf215d861de2f333b0057353c19112970cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:38:04 +0000

    question can welcome pull card

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
