750f92884b3f91e35474f03879e5d299eeff58db
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:11 +0000

    seem put suppose runinto together

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def square_number(x):
-        return x**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
