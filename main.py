525402dbac128b44386160392b7b3c23e946f600
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:38 +0000

    miss people notice card before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
