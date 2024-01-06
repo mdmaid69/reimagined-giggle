361e5bb75842c9d524d7ff37c532bfe682eebbe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:13 +0000

    complete original old nearly point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_acceleration(speed, time):
-        return speed / time
+import sys
+print(sys.version)
+import os
+def change_working_directory(path):
+        os.chdir(path)
