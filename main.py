0b24b882eee96827777e98f0fb142045455bba79
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:33:17 +0000

    its mixture deaf steal vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
