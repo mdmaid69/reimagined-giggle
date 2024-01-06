bbbc9e2a66306043cfd7713c49dde037c30b7436
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:25:29 +0000

    supply maintain block improvement fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import time
+print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
