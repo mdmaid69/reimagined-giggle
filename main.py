758aaac0996a082e4192288105a8c9d0fc60c848
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:31:03 +0000

    fetch everyone wonder impressive quiet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import os
+print(os.getcwd())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
