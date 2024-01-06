afe2b1e35feea5ff0c27fe223b7b44b2ecdf5bd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:52 +0000

    underneath condition progress whenever flat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
