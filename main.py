681946f8f529b73e54593cedb51773a0b42b509d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:06:46 +0000

    whisper machine kill let minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
