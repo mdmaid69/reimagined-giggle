eb985de047e6fca1bce559bf1ea88bacc75cdc0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:22:01 +0000

    glad attract case group neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
