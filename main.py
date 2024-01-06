64a5f376a14d95077a22a271d663e084da9a2b61
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:01 +0000

    progress television low idea too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
