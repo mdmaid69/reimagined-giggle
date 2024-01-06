99393e62627632687dc838f968bd577a43542443
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:01:30 +0000

    connected abroad or influence show

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
