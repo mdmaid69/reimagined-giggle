ec141a23549114d5d4d5587155af4ac3342e553c
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:08:50 +0000

    swimming part town border meanwhile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
