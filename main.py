befbcfd023a40d68276e3f182d11e5185a5f8067
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:47:18 +0000

    creature union buy record natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
