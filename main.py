4df6fee46b6f8612209c2b694dc2df0fb4941e8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:34:57 +0000

    carry cross have star interesting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_list():
-        return collections.UserList()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
