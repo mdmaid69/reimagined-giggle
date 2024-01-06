8ed87b9435ef09e0b4fe6bcbfaf181e7998e2470
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:03:29 +0000

    shop present recover behave noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Characters:", len(text))
