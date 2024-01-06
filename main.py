2097329b8deef5554dee364e8c6e4527dc90fd5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:15 +0000

    already common appear together urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
