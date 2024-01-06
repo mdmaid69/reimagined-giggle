0b22a989d6867537038aaa0bd4c95f6d35d602ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:33:50 +0000

    closely type space neither front

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Words:", len(text.split()))
