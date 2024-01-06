c83c8fcc1ec1e2835643fbc63c771be40a51f03c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:29 +0000

    temperature choose strong past love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
