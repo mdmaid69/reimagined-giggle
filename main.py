ed828a94bbae62b3f22dcd8403abd423712fdf90
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:09 +0000

    disappear surface take score loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
