2a15804129f451493bb8f4e787d3c72b3ac0adb0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:42 +0000

    behave undo modern slice stare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+print(time.time())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
