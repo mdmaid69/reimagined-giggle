99c2bc989a7f42f75ae6a8db17b68c6441a96e2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:12:15 +0000

    available Ii ticket like final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
