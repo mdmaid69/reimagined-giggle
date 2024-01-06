d2e18a7bb76ea43c79e0b728fc015aa26ef7dbc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:02 +0000

    impossible sensitive last hot pity

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import platform
+def get_python_version():
+        return platform.python_version()
