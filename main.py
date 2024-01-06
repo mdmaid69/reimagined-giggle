c351a351388e22ff2f7ca9bb7e8df46b87627261
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:06:33 +0000

    hurry hurt dish tour scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
