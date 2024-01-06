7f117950cd39361e89929aff84877410f5724b40
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:53:09 +0000

    increase funny extremely high snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import math
 def calculate_logarithm(base, x):
         return math.log(x, base)
-  def remove_duplicates(lst):
-        return list(set(lst))
