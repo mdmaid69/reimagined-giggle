e1ee1d9791293bc2127bfbb2fedc3910138595cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:09 +0000

    traffic narrow let level hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
