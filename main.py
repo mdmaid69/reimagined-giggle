82c1adf0d975fc9693900c95a4ba1b3d45c529aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:58:42 +0000

    lock learn dish strict pencil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
