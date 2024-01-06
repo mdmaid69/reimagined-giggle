32153a348a8f7864b1a950200548e776cac48a42
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:17:34 +0000

    keep sure rule of body

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
