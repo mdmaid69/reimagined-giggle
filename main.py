b4607843bb5290f118629f9bc24ed98b7609d67a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:26:34 +0000

    change private advanced influence home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
