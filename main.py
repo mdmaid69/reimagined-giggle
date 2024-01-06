5c7e025abc899ad34bf83638ada78fdd45c907fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:02:37 +0000

    party carry ever male range

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
