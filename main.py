8d65bc8659097ac0211cbb21bab64448abc1b82a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:59 +0000

    smooth talkoutof human content side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def sort_list(lst):
+        return sorted(lst)
