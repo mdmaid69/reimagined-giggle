5b107ba79a5e1cf9f35dc9ed47bbc5c925fd3761
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:01 +0000

    else wave point fortunately in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
