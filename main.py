e9e7b713c738753ffa57f76fb036f7ef5a176e94
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:05 +0000

    impression thumb contrast no chin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+print(math.pi)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
