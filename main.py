6041bf93f17eb20b1fd309e4e4908086fe926e05
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:26:19 +0000

    early mark everybody sound concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
