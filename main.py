8ec68cd9aae944f8d9abc92022b50b754427a844
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:45 +0000

    tomorrow claim shock comeback description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
