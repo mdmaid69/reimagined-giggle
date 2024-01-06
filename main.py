41e88fb8f7b46aca19ede556637f62993eea8ce6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:05 +0000

    ask Ee much afternoon building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def cube_number(x):
-        return x**3
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def convert_to_hex(n):
+        return hex(n)
