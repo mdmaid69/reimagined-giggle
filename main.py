bede9b03468b2dda97ee7186ea208df3782d7f19
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:31:18 +0000

    drive at pressure getout sort

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
