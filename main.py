905a577047429e940f4c0d87236b06c5302eac19
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:52:11 +0000

    beneath butter along rarely exit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
