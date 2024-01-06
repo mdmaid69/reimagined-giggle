2c2d3ccffd05913f820345c0576a9f92ee096669
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:52 +0000

    tense fly calmly light woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_density(mass, volume):
+        return mass / volume
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
