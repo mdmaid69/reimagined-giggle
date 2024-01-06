75f194ceb7de08458c845b8d1326444fc203527d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:55:19 +0000

    play form top strange steer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
