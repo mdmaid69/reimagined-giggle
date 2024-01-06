5bbf86592c7947e09132de85b3d07540beff0978
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:01:42 +0000

    murder fat spare fit she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
