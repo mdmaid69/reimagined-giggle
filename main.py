a054dfac189beb1a2f52b33489697dc9fe2a8a10
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:02 +0000

    many guess distant else alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import os
+def remove_directory(path):
+        os.rmdir(path)
