73587829bac682c7d2b4402d14584e241e53c8f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:40:47 +0000

    lower immediately accept used view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
