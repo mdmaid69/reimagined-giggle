e43e6bd41342c16c604e6b01782247bce84bcb28
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:04:05 +0000

    while hurt married discover immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
