919274ecf9f163b00298d6b7ab7ba4724585791a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:29:12 +0000

    well guess centre sting classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def square_number(x):
+        return x**2
