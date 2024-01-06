f50460a9d90bbdd9005b4a61d9f459dacdaf742a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:02:32 +0000

    speed equal like greeting welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
