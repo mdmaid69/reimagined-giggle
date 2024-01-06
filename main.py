f16878b41cda39fc8b6452af0940e2119599725f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:58:05 +0000

    earth rule workon last wise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-for i in range(10): print(i)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
