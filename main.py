8bf39fca6578992eebc94abc93fb10766523b1d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:37:03 +0000

    use black deep cough office

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
