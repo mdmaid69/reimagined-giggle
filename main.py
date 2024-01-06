a48ff1a318dc5aa1b3fcf18f156f263e1a69a4d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:32:31 +0000

    avoid firm keepup swim quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
