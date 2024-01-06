3bf0d3d35ae457ac47b5f7c50832b7fdadd6fad3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:08 +0000

    date breathe edge dish mix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def sort_list(lst):
+        return sorted(lst)
