dd068545100e150a1ca2be7df7bef5e48517b6e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:44:19 +0000

    involve meaning branch amount make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
