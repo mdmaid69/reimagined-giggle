384c4c6769e9e00283d67718610df9d0745f0f93
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:22 +0000

    above past pain breathe congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
