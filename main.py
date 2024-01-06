6d4dee4318ccc13dca792a90c2ed90498855aca5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:08:33 +0000

    anywhere worth lower there along

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
