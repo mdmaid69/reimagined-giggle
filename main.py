e5b78f616930f0694a6ee543ac2f01eeb6973c05
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:46:04 +0000

    eye odd distance fill safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
