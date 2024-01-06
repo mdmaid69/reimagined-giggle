80578a929374e88c087347683d095eab75ead888
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:39 +0000

    allow ear form their escape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
