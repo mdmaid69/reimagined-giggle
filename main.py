c2eb064db04cbfd917f0841c58a23d9aa877ab49
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:30:08 +0000

    trust scream control like spot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def is_odd(n):
-        return n % 2 != 0
+import sys
+print(sys.version)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
