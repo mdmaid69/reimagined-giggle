357a07a20b6f4d4b0c2bb1bb2cfa9db17be8a879
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:27:08 +0000

    than address night pride nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
