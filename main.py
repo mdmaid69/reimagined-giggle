3a2c798fabc87d43ebf10cbc9882242609cef132
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:16:46 +0000

    hollow save alive lift back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
