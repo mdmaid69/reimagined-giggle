b2b81531e46577a493f2fbc297e4a0585505c61e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:06 +0000

    apply buy board turn dress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
