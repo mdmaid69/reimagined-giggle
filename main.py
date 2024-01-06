2a71e829917af29ede271270a43bf443cd8617f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:06 +0000

    vote lookaround suddenly maybe smooth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
