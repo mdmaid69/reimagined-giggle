99c09fe0b78d012fa317b03a457aa9e3883833c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:33:14 +0000

    drink goaway accordingto reduce command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def sort_numbers(numbers):
+        return sorted(numbers)
