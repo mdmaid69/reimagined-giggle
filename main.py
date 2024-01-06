c3f7262e58147a92079cee631a85bc5dcaf1a731
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:17:06 +0000

    water 567enemy return relationship moral

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-print(sys.version)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
