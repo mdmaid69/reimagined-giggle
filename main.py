a075b2180ac4806d46a5fddee9a0070b1fee939f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:14 +0000

    injury fight crime awake when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def convert_to_binary(n):
+        return bin(n)
