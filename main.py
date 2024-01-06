c0c6a1a637aa2a28564939b6e04c974990d5eee4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:19 +0000

    aim explain wave bet classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
