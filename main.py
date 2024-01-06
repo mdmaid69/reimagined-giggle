e3e935bef63fc69bf66218b816a10b8786463706
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:26:39 +0000

    drop dress welcome sure zero

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
