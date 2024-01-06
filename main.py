83b4adf3768bff0188aaa7f70d8c60d102134e5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:17:23 +0000

    any plenty please goaway solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
