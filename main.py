1c1665ff88ef61c0bd6770899ade4d0a735268f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:24 +0000

    stamp problem rescue giveback getback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
