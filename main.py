7a575446f7e87098d97d18544f29b724e5e98e0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:51:25 +0000

    part equal object interview but

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_stack():
-        return collections.deque()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
