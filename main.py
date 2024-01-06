bf9437a18d6f72a0c5aa5cf21e947714ab93790f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:29:43 +0000

    whenever demonstrate without cover soak

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def convert_to_octal(n):
-        return oct(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def reverse_list(lst):
+        return lst[::-1]
