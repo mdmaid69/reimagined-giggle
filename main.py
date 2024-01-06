c4456e274667a82df0b202c73864be16cd2ac0ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:15:50 +0000

    criticize middle its pair perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
