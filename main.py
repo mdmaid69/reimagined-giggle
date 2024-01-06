cc46536cb10431e02d8f64d23763574687da25c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:59 +0000

    sometimes better minor fail breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
