926587aff009fa5810c1048f41849e83099e1a0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:53 +0000

    opponent tear moral crazy opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
