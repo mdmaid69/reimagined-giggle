a597afe5b70c8c70a6fb802eea756d75c219bc7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:39 +0000

    sail turnoff film passage importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
