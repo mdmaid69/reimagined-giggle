8b3d0dae37cee5c213d4444be42d829441646ae6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:21:37 +0000

    opinion getback home guess inform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
