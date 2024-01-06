5353f64b79af53cc4b523d3e939e999ae465c709
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:38:17 +0000

    plant demonstrate share save behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
