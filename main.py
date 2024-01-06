a65b7b2a71013b3a674f4113b8a5187b029341de
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:25 +0000

    cruelty occur replace instead ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def subtract_numbers(x, y):
+        return x - y
