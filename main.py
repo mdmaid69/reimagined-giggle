29899966bc93bee77fc0f24041fce727988ab66c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:54 +0000

    home brown broken image communicate

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print(sum(range(10)))
