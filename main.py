22f54e42bc60aa83d4a4298b9cbb59fd2ba37efb
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:14 +0000

    vision politics experiment instrument sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+for i in range(5):
+        print(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
