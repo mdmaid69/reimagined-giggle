1882c42878426b49b4442b08fee0bca902e37e5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:53:11 +0000

    excuse callat legal slowly nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
