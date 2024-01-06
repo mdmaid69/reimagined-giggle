ec9283d991f98bad39126e8fce2d1a27b6a36f3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:26 +0000

    illness affect power hold battery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
