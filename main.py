a4e7f7fcc1f8cd37920b69c981b2fa16cbfc0aab
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:39 +0000

    preparation bath glass attempt before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
