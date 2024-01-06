bee47a0385ef1439740083f499b2a4f74d0b12ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:33 +0000

    breakdown major sore warm however

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def remove_duplicates(lst):
+        return list(set(lst))
+def convert_to_binary(n):
+        return bin(n)
