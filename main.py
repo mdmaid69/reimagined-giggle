08c27d77be12e95e13fa3bc190df646697c405d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:24 +0000

    sensitive terrible any Ll downstairs

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_power(work, time):
+        return work / time
