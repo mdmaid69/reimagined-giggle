a2b72e1dcd30b8cb4a468751304dfec48b69f597
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:58 +0000

    great beyond height firm damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
