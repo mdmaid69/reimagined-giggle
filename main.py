f1e8d286d4570acef9c91854cb135b6a9b13c059
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:25:15 +0000

    slice how in choice off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
