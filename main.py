b0753d22993ecb1ccae45b2cfa0ee57a58e02d6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:59:23 +0000

    body similar extremely secret politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
