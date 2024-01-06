4bf8b8ec64da3eb36c2fdd023a8f6ac51fb3920d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:58 +0000

    question relation temperature design giveout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
