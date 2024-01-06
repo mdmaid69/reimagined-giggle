fe1a14d5d12fce66d4ae9f322b3bb45e1fad714c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:25 +0000

    thing twist shoulder after hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
