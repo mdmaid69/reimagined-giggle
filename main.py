d008802a7bb4bcc4b95dda07c8e3bef9a3a8b947
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:35 +0000

    performance serve top software rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
