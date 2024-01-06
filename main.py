98cd1e0bce2d02735dbeacf3b023fb4b454868e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:38 +0000

    crazy gradually guess nurse under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
