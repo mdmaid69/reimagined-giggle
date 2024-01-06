9b70ca6a814d8042d4e0e734b24b07af7fa59ed3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:01 +0000

    star top swallow invite swim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
