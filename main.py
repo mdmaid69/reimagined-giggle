bc1e76b1d1157abbdc10ecf00178b02f96aba319
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:15 +0000

    tomorrow difference right near form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
