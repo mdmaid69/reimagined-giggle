8c0dae5970f5266fba945c1448c66618e32db64d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:38:42 +0000

    rush tour conversation exact suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
