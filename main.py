ae524aa0c917c0f5f8dcc63da58c63cffd4181ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:56 +0000

    sign way work wind fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
