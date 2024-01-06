7bc2d16e2d916379280d547545b309646c81f546
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:44 +0000

    actual figure certain film indeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
