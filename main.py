83b36b49698c39a47e77dbbf852b4c03d1e4d2ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:25:02 +0000

    chief even store little rain

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def add_numbers(x, y):
-        return x + y
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
