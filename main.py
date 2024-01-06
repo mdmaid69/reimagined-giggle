f66932d1cecf72eb5f977322567f14937f2afe7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:39:33 +0000

    deliberately confident arrival rubbish turndown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def clear_array(array):
+        array *= 0
