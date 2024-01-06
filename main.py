ee91323ce405ec7abc5065308513f3dd54729099
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:59:00 +0000

    admit getout plenty lay agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
