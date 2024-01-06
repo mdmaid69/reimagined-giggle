f93dc08e30d0f1a54aea5681194258e8273e8955
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:32 +0000

    declaration power shake dry every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
