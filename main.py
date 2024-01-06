f178d293108a83e03f4a954ce40661b8e88f72fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:38 +0000

    relation Ss calm characteristic train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def count_words(sentence):
+        return len(sentence.split())
