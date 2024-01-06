cc5888e128ce308b8e7c31a66bd95df0cbbe6123
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:30:13 +0000

    want lock apologizeapologise suck custom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_speed(distance, time):
+        return distance / time
