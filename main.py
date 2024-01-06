ab81a6723cab32ff9516491726146018bb803187
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:35:40 +0000

    half area satisfactory soil half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
