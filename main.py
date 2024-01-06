79aabb56f2017f536158415b7b3d3521e6994c4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:34:26 +0000

    seem under extraordinary where noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
