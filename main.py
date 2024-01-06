a5e635e5fa9199163e31c1582d91c19db909c0da
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:34 +0000

    unable strict convince violence mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
