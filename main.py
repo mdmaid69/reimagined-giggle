d0c86d8a1b58214ab3de5eb7d5811436d85f150f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:30:00 +0000

    warning great next compete switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
