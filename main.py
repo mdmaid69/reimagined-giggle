b6a47d8536bad194a1ef6311d3ebf5a088a35c48
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:25 +0000

    ignore compare handle organize pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
