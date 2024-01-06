83468e68e3763bb959c4530aef8dc4fc3d2f0218
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:25:50 +0000

    promise keepoff forget laugh unlucky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-from collections import Counter
-print(Counter("hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
