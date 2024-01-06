72969b844da129da7b444c723ec2bf5130421cf5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:09 +0000

    seat operation abroad matter best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(numbers):
+        return min(numbers)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
