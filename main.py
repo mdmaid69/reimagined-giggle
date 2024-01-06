83b45dac263b4214bb3446ab35ef158af9691e13
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:59 +0000

    range turndown now big shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import numpy as np
-print(np.array([1, 2, 3]))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
