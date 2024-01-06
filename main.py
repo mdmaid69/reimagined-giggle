5558b00ab2745a5bfd9544011785d5e0ba6c84c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:52 +0000

    prove pencil male area flat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
