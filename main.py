dd60e9d9ec8f0ce82f6be3dbb9d1326fe388f8c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:54 +0000

    start influence choose aim centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import time
+print(time.time())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
