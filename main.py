f46753f6cd6ed0869bdeb761b78db059b8671244
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:10:25 +0000

    neither cool speed spare last

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
