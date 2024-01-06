f99d1e06b2b3bd7329ee6b72273845a6b099d237
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:13 +0000

    test greeting advice powerful family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
