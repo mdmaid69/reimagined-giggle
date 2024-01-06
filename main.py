6ad6316bc01920710280a569109296ac79c9512e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:13 +0000

    house improvement scared turnout birth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+x = 10
+y = 20
+print("Sum:", x + y)
