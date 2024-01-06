f4a0d73efbcec82eddb7a406f285ea0029034474
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:20:38 +0000

    whenever pretty than furniture why

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
