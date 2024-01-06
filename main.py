e2413b5586b4c58482867b866ae2b5e532370a38
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:39:30 +0000

    future study shut later loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
