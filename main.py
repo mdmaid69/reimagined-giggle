9d356947e876132d9945f15fdc6817d104f2faad
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:22 +0000

    below ocean contrast nature their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def add_numbers(x, y):
-        return x + y
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
