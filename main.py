e7d7bf150aa8aa7270050f1750cfa5b6760d5392
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:23 +0000

    another side remember importance tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_speed(distance, time):
+        return distance / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
