f0a65e5617162e47c9490c1271570e0ff71addfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:21 +0000

    flight make successful charge rub

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def convert_to_hex(n):
-        return hex(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_density(mass, volume):
+        return mass / volume
