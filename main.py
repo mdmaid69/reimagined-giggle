7230ac875b6275ef93c3c0fb058ab94f4ffe56d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:28 +0000

    draw myself regard through movement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_queue():
-        return collections.deque()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
