bfaf3396dde659e46e9a5a8b03ecaa95a95fe7de
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:17 +0000

    chief uncomfortable hope ability actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
