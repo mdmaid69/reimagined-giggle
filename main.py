67f5b779a6479a231bf057f913fb7dc45199e8ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:31 +0000

    probably voice partner disapprove frame

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import time
+print(time.time())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
