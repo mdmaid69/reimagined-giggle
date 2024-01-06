44516b6e129d337bacdb6b9186ff481b421cf331
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:33:12 +0000

    string wing suck tool classroom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
