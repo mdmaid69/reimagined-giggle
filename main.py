230381ab245797e78b3586c23ab2874984a8fab7
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:31 +0000

    surprise majority damage earth tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
