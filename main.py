12fdd80ed558f365c25a734d2d6c4da43bfa8ea4
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:14:13 +0000

    official show experience environment juice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
