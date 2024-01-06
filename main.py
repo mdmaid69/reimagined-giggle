0bf5abf56ecc3ce532b94bfa838d0065b82a8d3a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:32 +0000

    clothes consist moment hold she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
