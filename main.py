f355087726a0eb5911db480fd247bb15e23a44a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:09 +0000

    emphasize Tt communicate good in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
