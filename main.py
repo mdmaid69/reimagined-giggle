74eba2c2002a01c12ac7002b26142e2e1bb367ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:04 +0000

    region question one nowhere justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
