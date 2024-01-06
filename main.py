5e3789335b30375654452f759e87168681680dd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:35:57 +0000

    refuse ground scream declaration prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
