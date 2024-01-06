f4cb78f543d9afd02668ceb1e538fe280dc677d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:32:52 +0000

    brush ourselves join bar tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
