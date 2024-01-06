68cbc8168d8607e593cd566cb636e24f1cecbe9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:10:26 +0000

    design total earth average upset

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
