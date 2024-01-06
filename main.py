8a32a14619131e5e409ce56b6631a9b5f5a4874b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:00 +0000

    guess plan mine sharp command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
