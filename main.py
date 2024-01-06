83e5a9d1a6c028b0a5385dade9e6347e22649df9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:48:53 +0000

    hospital quit kill tie case

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
