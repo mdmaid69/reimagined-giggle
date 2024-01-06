328b8fa3a8fea5c30bd8728d75d4056c6359d767
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:14:24 +0000

    protection harm spare telephone calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
