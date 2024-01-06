6160105f2e56e57d3e25bd623e21cdcc79d50d71
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:07:24 +0000

    brain usual relative nerve bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_as_bool(array):
+        return bool(array)
