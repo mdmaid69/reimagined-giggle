e756491043106c93b1aa26bd1520084cb3b0ca54
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:25 +0000

    power natural connect laugh probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
