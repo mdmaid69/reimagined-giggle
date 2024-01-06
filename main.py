440f764ee325eb0c1ee46b3bc30fc8145fdb4855
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:50 +0000

    adjective insist past plenty war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
