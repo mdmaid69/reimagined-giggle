b0120403f66d9ffde0a92b839704798048d5ad6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:40 +0000

    own morning turn love skill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import re
-print(re.match("h.*o", "hello world"))
+  def square_number(x):
+        return x**2
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
