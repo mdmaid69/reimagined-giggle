a39b63cf96254aab0bc5b4a75e0439119d9bfea3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:23 +0000

    Gg head pause lost comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
