6b86af4c53f0a37be37b79aad83a03d772e05e31
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:53:33 +0000

    letter 564encourage bit operate within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
