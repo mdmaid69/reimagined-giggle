ba0c3bb39668f87c2f719eb44c79eae33f1110e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:40:02 +0000

    trial branch whole match be

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
