04626da49f141d7e33fe4b8b50cd2ef57471592b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:27 +0000

    visit declare middle meaning blue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
