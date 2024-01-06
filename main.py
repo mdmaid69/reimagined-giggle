19183b0689349e6410ce031fd21b9f4715108b99
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:09:23 +0000

    introduction appearance draw experiment fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
