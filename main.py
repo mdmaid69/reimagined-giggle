e74c18c0b523a1579031937255a46af8816ea716
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:21 +0000

    commit care division square tape

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
