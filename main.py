12f30ec5f526bab9953c3ecc410bd6f668f30a96
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:10:37 +0000

    exercise event valuable Ee gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
