0d85aa49aa1dec34ee960076f2256b0b418a3af3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:41 +0000

    Dd problem slightly steep intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
