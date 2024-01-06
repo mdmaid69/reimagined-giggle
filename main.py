125fa25a98de18910c22527f715f70971da58ab7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:50 +0000

    bang once lean sound otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import sys
 def print_python_version():
         return sys.version
+import math
+def calculate_error_function(x):
+        return math.erf(x)
