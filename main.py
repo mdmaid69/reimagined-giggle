725742afe9883b74d31b406f50b79d67ec170c26
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:15:33 +0000

    song clean criticism pretty tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
