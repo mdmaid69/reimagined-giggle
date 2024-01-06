97ac741d5bfe143e6ba05605b54589d190c0b3eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:55 +0000

    shame correction push estimate summer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import time
-print(time.time())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
