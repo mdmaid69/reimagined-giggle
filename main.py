af9a6f4fae4a35e64d734f159e2d5d2dbcd59d21
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:12:01 +0000

    before emotion generally live stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
