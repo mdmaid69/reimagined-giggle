07ec2f2d73a5f103cdc55a8e54b703653b4f6424
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:09 +0000

    accept risk look unite easy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
