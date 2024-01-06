544b6e8721dbc086681fdf638c35cb136a88fe17
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:36 +0000

    where cheek home thinkof surprising

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
