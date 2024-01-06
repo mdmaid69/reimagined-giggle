a76773febddd7e6134c75d9d7037d05493ff945a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:56:21 +0000

    healthy nearly promise excited oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
