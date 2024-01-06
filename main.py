4f7d27325faf129379b408bf29b71994c4171203
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:31:44 +0000

    either under thick aim operate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def add_numbers(x, y):
+        return x + y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
