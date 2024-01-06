149d93e1a796e2bcf8848384e9d9a9dd43015b16
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:13 +0000

    characteristic 567enemy feel never noone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-print(sum(range(10)))
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_min(lst):
+        return min(lst)
