30eecf5c9c43139e9c5fbf3eabc93bff7567b293
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:11 +0000

    terrible support number story pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
