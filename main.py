26179cf600a0d4ad46a2e27d40d3ca8a6e859c06
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:43 +0000

    plain custom stranger content wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import logging
+def log_message(message):
+        logging.info(message)
