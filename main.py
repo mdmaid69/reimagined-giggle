2fd11cf511f405fb4bac70f756632ac331d6a257
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:56:46 +0000

    prize style bridge Qq soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+print(os.getcwd())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
