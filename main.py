cd417c29d13cc09bb933e8bada42ceeca72884df
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:49:59 +0000

    niggaacademy spoil disagree waste listenfor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
