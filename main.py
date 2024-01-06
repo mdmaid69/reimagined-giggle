b5fe792b415189dd6a147db4bb5421b4a8591a50
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:36:02 +0000

    spring trade off workon somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
