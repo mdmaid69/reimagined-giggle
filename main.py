a09a5c80aae26018dc9bfdf8c8529c93540c147b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:29:09 +0000

    expensive week runout provide tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_pressure(force, area):
-        return force / area
+def find_min(numbers):
+        return min(numbers)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
