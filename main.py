2ff1a696c0e16bb2dadfde83ee7725b33d547915
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:19 +0000

    reply share show tie thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
