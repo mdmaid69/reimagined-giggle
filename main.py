bdeffd8010a8296f8dc2b32f9b3c977c70454169
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:35:07 +0000

    nurse fashion Ii we to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
