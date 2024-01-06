2d7bf9040df9a709cbf92adb52ac42cc7c2c6d0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:03:36 +0000

    practise comeout branch himself payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import datetime
+def get_current_date():
+        return datetime.date.today()
