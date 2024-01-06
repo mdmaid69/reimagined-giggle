1dc4a3fedb96c7fa3eaa9bf441cef89047fea20d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:48:04 +0000

    criticism expression great nearly damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
