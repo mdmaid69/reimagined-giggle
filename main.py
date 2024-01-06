88ad58f61cb808a2c38e833e8000d97f39c1f3e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:15:04 +0000

    play pride patient anybody embarrassing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-print("Hello, world!")
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
