b5bd521b6ee2987046b0caf2c7d5187d7e6b6339
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:21:50 +0000

    turninto likely pay muscle tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
