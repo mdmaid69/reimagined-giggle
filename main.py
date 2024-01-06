e6adac63fbabd01673fc9692ce62c031d52023d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:55:17 +0000

    back suit risk car least

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
