2886cb68564bc160c6fa34d37d3b2f4c08e877e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:32 +0000

    firm contact there whistle immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
