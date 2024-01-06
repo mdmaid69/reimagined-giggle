35e23dc2e32d8b8a399aa2ba49b849e00e4d2488
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:33:14 +0000

    guard never prevent point owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_arc_tangent(x):
+        return math.atan(x)
