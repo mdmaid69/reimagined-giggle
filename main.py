19f693987591cbb302cc22b88f7b070365dee483
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:19 +0000

    calm cut road someone spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
