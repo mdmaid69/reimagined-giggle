517463dc8522b4886538f41458449fb005a746e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:41 +0000

    disgusting that corner right gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
