ea3a77cfb6834b6830d6ebc2f56212c10e2cd77d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:31 +0000

    suckme confusing marry warn rough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def cube_number(x):
+        return x**3
