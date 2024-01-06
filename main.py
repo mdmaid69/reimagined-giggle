a3ca382ddf68ff1f64d4c73e60f4be1bd975cd5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:15:31 +0000

    vote balance delicate error favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def delete_file(file_name):
+        os.remove(file_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
