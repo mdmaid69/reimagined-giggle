3a9af4d43bc3468742905aaf7cd47d26bde8cfb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:26:59 +0000

    somehow rescue important worth lookafter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
