897af9aa6473c65d049839d4489fc496d5d34cfa
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:07:08 +0000

    habit course price describe new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
