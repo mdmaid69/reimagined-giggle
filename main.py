99a1cffc2431647c27869cab12c383b090641663
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:21:27 +0000

    out care fairly provide interfere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
