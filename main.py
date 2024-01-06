f26f2947fd02927f32bbf552d9f23c3fbd0d7ad5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:47 +0000

    sweet towards polite talk we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
