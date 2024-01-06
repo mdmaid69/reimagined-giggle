2190931b517b8d9fda26be05ed906be86662e31a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:11 +0000

    level last open seem answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
