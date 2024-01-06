4a2ba3e661f5819bee583c98f301cb83f9a6ea44
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:43 +0000

    begin whenever dick next ready

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def square_number(x):
+        return x**2
