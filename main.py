1d6a5bc218d224c0d964ae73d28a9c9f5e4815ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:36 +0000

    thinkover club over judge comeout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
