a71de27e82fcf81d3408b70dd8407676c54108b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:23:25 +0000

    single top accident square happy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
