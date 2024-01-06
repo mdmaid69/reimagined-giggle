4f27fb4f0bb53fcdb1da5a6c4b63ff8d9c35da0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:03 +0000

    accidentally image and form big

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
