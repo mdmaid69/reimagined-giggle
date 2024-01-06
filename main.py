0142f114be4fb26406fe8e23461b9e97aac1eb62
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:28:53 +0000

    label except right organization mean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
