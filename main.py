06aedfef56bb27b9bb1bfca354e049e2f29e7c5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:44 +0000

    know nervous recognize once giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
