38db692a1d78ee6626606c4a53683548e70e3847
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:19 +0000

    tell warn late against much

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
