80fea3dbecfc2740b42bcf201715f6d10771742d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:26:15 +0000

    fight he unfortunate fast wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_max(numbers):
+        return max(numbers)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
