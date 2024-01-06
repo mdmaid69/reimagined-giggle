27d81bdd9b2c08af2932e83f6277d005ae18ff99
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:11 +0000

    long mystery Uu thanks effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
