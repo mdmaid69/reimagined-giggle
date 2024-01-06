8fa4e20be5adf8e27b311b13dbe03649d533d460
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:43:09 +0000

    greeting partner turnon pen line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
