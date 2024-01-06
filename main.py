546786a7bfa5afd61fe25fa0cbfe1fe883c5ca92
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:40 +0000

    offence however objection happen 566end

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
+def find_max(lst):
+        return max(lst)
