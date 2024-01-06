4d19b1fc1a5e51d5797a82f5361b85391033b627
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:27 +0000

    pump 569enjoy trousers message calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
