94865da2cfbfd66e1b7ef550279d78157686c330
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:53 +0000

    570enjoyable shoulder why fast slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
