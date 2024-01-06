b61aea552d91f6843dafd21d3f1b717f987ac72c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:21 +0000

    man home right war rain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
