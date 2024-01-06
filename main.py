49bf10daffa3f7e1c4d09d124b4c7bd64436aa6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:29 +0000

    only basic indeed follow taste

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import platform
-def get_python_version():
-        return platform.python_version()
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
