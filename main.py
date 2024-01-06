5a59461b8c5d3e61b24eebd562d772132fca8e95
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:59:23 +0000

    university rest concerning sharp across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
