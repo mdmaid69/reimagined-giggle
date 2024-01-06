07bc0a79ece7eb64780af05154723c8401f23856
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:28:34 +0000

    fierce active eat reliable patient

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def reverse_string(s):
+        return s[::-1]
