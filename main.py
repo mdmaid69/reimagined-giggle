d12587e3b800c3a963fac803b049a1b22cddc75c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:17:06 +0000

    study afraid pull every perform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
