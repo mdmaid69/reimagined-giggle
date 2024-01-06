80f9860ad686c8c70bc19f07d71c8a0ea238ee8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:00:30 +0000

    commit bet hello move row

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def sort_numbers(numbers):
+        return sorted(numbers)
