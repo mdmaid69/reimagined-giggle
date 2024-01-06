da81b6fd894a1786ac4904bed7028ee788c6ddb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:23 +0000

    getback side error meaning lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
