07be0f5f0409b04535f1167c1c7ef209bce22d74
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:46:09 +0000

    turnaway battery yes check maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_typecode(array):
-        return array.typecode
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
