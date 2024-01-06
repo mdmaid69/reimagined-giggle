f73896cbaf5fcd2c37cc635f3242813347e03d01
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:10 +0000

    attract genuine trap judge sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def is_even(n):
-        return n % 2 == 0
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
