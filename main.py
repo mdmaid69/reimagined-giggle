b191bf18fcd5d5aa1fbfa1653df010ead5c44e52
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:03 +0000

    though bother unfortunate smooth various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
