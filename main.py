63e455fe00ee668ed25f5e3d1c1b60b357b58fae
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:15 +0000

    mistake nigga arrangement twist formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
