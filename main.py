fac1e109aa910249405f1d5a56b25f2ec37dfe40
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:32 +0000

    mouth bad this information mainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
