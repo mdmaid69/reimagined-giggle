14083d1c1aa50c6dafd68818c7640643289b8282
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:35 +0000

    authority half spring money may

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import array
-def convert_string_to_array(string, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
-def calculate_area_circle(r):
-        return 3.14 * r**2
