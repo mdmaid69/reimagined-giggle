203961c85376b9e3dabaefb4d10635ad29f8a472
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:45:45 +0000

    the disturb careless unit bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
