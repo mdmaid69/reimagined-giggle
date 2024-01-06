1f22e64b0b87376cb55ab14b96e5a04323eb75ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:11 +0000

    personal total eager excuse defend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
