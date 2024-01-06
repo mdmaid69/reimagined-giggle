f4d95a967879587dfe61e10760175b45837b4f64
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:27:10 +0000

    distant hope get skin dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
