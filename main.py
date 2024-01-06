d96be8c9ea009dbd92bc7b3aded71028d3c67f47
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:39:00 +0000

    owe often suppose interfere measure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
