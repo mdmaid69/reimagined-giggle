ef68b00031f32fcdeb978f805fa2acd9f7d0eb07
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:44 +0000

    fond exaggerate sign friend shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
