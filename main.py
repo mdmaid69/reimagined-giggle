9a51a22c589601d586517124165defd24cff5227
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:30:06 +0000

    turnover fun importance advise visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_current_directory():
+        return os.getcwd()
