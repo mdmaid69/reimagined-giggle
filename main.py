89a23993ad06d204d4e462d96820649e0b9d6fd7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:01 +0000

    usually single criticism need refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
