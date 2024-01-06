fa09d305f2a8ff64efb3e3555c604a08113aac56
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:21 +0000

    Zz car accordingto plant course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
