cc244b113de8cf21e5a0ae2500c2f1da0b548d2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:04:31 +0000

    decide considering school trip preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
