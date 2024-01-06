5d30a182b946851346193662c7cce61f0bfd1666
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:13:02 +0000

    compete unlikely Uu manage care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+print([x**2 for x in range(10)])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
