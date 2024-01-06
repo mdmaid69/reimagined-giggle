4ca420074dfd4b255913c0af52bbe9bc7ae9f70a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:37:25 +0000

    material turnaway temper soil classroom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
