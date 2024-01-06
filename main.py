d6537dfaf5a8748187067f31bfd3f566192546af
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:35:09 +0000

    length expect moral dark storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
