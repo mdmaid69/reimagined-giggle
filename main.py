c4d3c317f7f72f2881fb06b2378989e272666734
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:54 +0000

    central gap state remove store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import sys
+def exit_program():
+        sys.exit()
