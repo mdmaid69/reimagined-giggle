907b1cc15e1d5db6edb342a4bf43c692bc9b4c9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:19:17 +0000

    over great takeoff point snow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
