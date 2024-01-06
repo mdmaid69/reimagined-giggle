b0a1193e4325d3e575df13f080c078f7faf2bc04
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:46:35 +0000

    gas suppose burn still survive

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_itemsize(array):
+        return array.itemsize
