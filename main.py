c0abd4539598b452dca95f838a566c44a76ee324
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:29:05 +0000

    freeze entry burst acceptable quantity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
