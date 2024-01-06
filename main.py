88b55a6ce6913c9568fd2d3a7365842d4eb3d87c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:42 +0000

    deep everything pipe idea culture

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_complex(array):
+        return complex(array[0])
