4a0ec1941bd466c261231ec18eb92b183d1a7920
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:01 +0000

    fire understand goback(to) train importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
