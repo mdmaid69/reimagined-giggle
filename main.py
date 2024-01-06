e16740a573c6acbd292c725d2d7e110676aebe3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:19 +0000

    butter it there juice far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
