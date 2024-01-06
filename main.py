6bafad853440443429aab99a094aad4e70086b7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:04:01 +0000

    provide cum blue use exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
