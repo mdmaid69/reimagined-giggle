2fd809755b73200e92228dcfed1bb26533f93f69
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:27 +0000

    instruction round recognize surface road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
