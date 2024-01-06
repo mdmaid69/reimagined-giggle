899c07cf2c94cf3b20cbde96bb3c112760e0d19f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:48:26 +0000

    suffer listen promise tune period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
