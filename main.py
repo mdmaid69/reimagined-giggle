fe99b27d73e40dcb053f06292deabc3a9b46983e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:53:07 +0000

    workupto sink hot safe wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
