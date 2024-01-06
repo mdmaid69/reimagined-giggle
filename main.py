d62d73768a184c42982ba6a9f952667cdcae1e00
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:48:42 +0000

    him official or speed lay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
