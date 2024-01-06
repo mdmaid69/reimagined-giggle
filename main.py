124eb415bbb468ea10d1334d58995cf462e93402
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:54 +0000

    loan suffer top birthday present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def split_path(path):
+        return os.path.split(path)
