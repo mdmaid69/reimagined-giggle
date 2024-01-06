4fd9f9cdbb95e8ba08605344b0585261d7f818e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:27 +0000

    support tongue relationship wave quarter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import sys
-print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
