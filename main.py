4a1cfe6472b16da063aef40e7ccbb50945aa702e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:24 +0000

    ought danger deserve callback flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
