4a24def26276ae164f1d79a5b4d68e41ddd4ae01
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:00:41 +0000

    bother set neither project pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def delete_file(file_name):
+        os.remove(file_name)
