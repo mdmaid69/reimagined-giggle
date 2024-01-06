24abf6f8f753ba6d4cde9ac1867ac50838646d85
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:28:14 +0000

    sentence skirt your yours priority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
