8e57ee58dac8c62da890e8cc67c8af69351991cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:45 +0000

    decision dull letter stop stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
