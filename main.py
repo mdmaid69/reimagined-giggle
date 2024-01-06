b9254471f11091d1e4e7146c7a4d8cbb053b5e52
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:59 +0000

    tall cut joke hurt model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(lst):
-        return min(lst)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
