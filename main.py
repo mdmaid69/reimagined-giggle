6162d6e140e82542ea0f31e74a62782d02ea9236
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:51:25 +0000

    whole future receive cut sing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-for i in range(5):
-        print(i)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
