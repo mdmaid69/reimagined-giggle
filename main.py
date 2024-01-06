7c6e6fcc6074d6e8ac602c58ed06c3a38edccbe6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:07 +0000

    order anyone cause remove child

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
