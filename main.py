e09be8e159dbf120e468cdbaaa9e58dd60099c42
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:07:43 +0000

    sort willing travel entry sort

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
