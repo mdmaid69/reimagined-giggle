a9e8c8d04dcde7ee70c19f698ba1f0e332f4ac7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:05 +0000

    favour risk key kick float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
