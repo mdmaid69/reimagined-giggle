30e9c5ba727c1d4d48b8af8ade9c61a514635f74
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:59:39 +0000

    interesting present runaway dress scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
