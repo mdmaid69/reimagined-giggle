3ed732e6754b3d925e0a01e825577b64f997b7ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:56 +0000

    weather cope balance at hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
