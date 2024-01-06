57d6f682fa90c931f257d76dc60b750b80c65c0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:54 +0000

    read Cc test even final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def subtract_numbers(x, y):
-        return x - y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
