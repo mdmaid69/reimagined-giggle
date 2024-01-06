91de4410c7515278d97b510e1b3961529bd00adc
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:17 +0000

    Mm first noone choose hole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_equity_ratio(total_equity, total_assets):
         return total_equity / total_assets
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
