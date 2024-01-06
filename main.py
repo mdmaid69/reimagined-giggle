2e6f7ac6012ef4c14040888ad44445c7338b7250
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:36:05 +0000

    wish money apparently waste equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
