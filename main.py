48cd19a6d8ba46f7a69edda8a684b83d8fbef16c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:52:32 +0000

    think slowly finish Qq fold

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
