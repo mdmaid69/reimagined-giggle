ffc263f1f6de8eb5f675253d6728b86af5cb0189
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:34 +0000

    risk onto replace takeoff together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
