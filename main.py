5e8db9ae128415f9caf6d3051c5ffd81d80fcafc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:15:14 +0000

    plate actual Bb church laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def roll_die():
+        return random.randint(1, 6)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
