b07eacaf228af45e82571a3f863f0306becafdc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:45:11 +0000

    able confuse appeal tradition rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
