baf6de65cc1a9ecc98128bd5b594858f458afac6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:30 +0000

    want violence necessary spread attempt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
