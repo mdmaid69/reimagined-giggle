04ecb931d6392eb4df3ed76f655ebcb7f592f4da
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:07:12 +0000

    communicate phone cap delicate bound

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import sys
+def print_python_version():
+        print(sys.version)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
