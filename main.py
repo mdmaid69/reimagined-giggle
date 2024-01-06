0fb277f9e3eb1ef3d8600be229ef86588ee5084f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:51:58 +0000

    admire under recognize fun convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
