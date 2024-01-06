bc60b70f1c43bfc6cffa294db7b2f7ddcbec08b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:19:20 +0000

    change price congratulations zero swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
