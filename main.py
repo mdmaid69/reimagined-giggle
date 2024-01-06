480b7246ea71fba69a6bc4154ab8336f49455c95
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:54:54 +0000

    land except little Kk many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
