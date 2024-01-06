8daf342adb6aabb613ac6957ab5cfa7f98f1cae7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:58 +0000

    reasonable criticize mark keepoff convinced

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
