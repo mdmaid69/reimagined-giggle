a4bc20267fc0039540ddae62d1e41f1556b1fb19
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:44:01 +0000

    improve congratulations card bin sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
