150683d3220f3cf40263cd469eec8cd87351fe32
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:42 +0000

    kick trade wheel must quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
