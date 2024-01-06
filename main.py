10269556d2dc705bdc68cf5fb9aef36f0f9d4e35
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:51:48 +0000

    workupto goback(to) back community emphasis

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+name = "Python"
+print("Hello,", name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
