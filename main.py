fe8ed8bd770bebd816b6dc8691ece30c4f644433
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:14:07 +0000

    under type fact them black

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(lst):
-        return min(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
