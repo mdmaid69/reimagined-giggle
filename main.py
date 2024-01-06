7022d8edaafc3da24aebe49c9869920ee49f3af9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:33 +0000

    climb create exaggerate person swallow

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import time
+def get_current_time():
+        return time.time()
