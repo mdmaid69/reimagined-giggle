6c0c3ad1413b1336266474400f2f272129462d03
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:41 +0000

    matter still mail instrument recognize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
