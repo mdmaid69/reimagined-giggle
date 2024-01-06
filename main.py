d3a157fa91a776f1f96cec54554afa17cffc42fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:59 +0000

    wise bad pride sing platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_floor(x):
+        return math.floor(x)
