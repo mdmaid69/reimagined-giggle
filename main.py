cf2235aa440f019b3ab4448bb97f2537adb20b48
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:43 +0000

    bored concern ilovedicks cold spend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
