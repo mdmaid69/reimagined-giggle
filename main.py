0654b7eb28f7f439457be68613429412122986cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:07:15 +0000

    right mystery destroy callup calloff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def square_number(x):
-        return x**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
