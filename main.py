75a2d7094829084de9ca365473fce4fff1bd01d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:29:34 +0000

    unable natural bunch pile whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
