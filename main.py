1c35225a03b0909bb90f89f45e6ad3b82a44e7cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:54:01 +0000

    amount amount any cheek lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
