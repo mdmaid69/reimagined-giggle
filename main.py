e4d806443b53544647f8121ba9b067e1f40ca367
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:58:08 +0000

    closed still height breakup hello

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
