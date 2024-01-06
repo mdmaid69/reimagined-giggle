9c54d9055c2cce90c1c9f4b10ed8fa579e9d17ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:56 +0000

    perform drink own far public

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
