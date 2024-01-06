d145785616ab6b6972e335fae3e6c1eac457b72c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:50 +0000

    press disagree poor uncertain fire

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
