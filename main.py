2428da4ef86cf3dd252f32f769b4b7f5ad647aaa
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:28 +0000

    face rush gentle often pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
