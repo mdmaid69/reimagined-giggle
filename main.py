d9d20bf25a11d699338c45e69e4fb28c2f306754
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:05 +0000

    related cut Itself dish doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
