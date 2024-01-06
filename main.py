37b066beca9413e759e551cf851c6e77c2da892b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:02:34 +0000

    load gooff patient before payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
