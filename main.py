dd14241c4906049f3f2acad657f1846d3dc925ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:08 +0000

    sore soft theirs wash burst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
