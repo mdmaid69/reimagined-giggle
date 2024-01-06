2b8f2bbb3388444289ca74b19cb527388fa5e37f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:54 +0000

    cross free nothing reasonable lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
