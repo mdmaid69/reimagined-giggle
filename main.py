a48b810cecf7b23e709204cf90cb22f34f72925c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:58:15 +0000

    sore reliable affect become leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
