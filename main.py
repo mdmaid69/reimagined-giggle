cf49909b4bc38ddbb35dbbc292106382260ae03a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:23 +0000

    bang above available goon software

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
