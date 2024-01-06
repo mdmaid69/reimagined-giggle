19d7aad93bf18e6a3d8d2776b991f3b8e5183bdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:11:52 +0000

    complain Jj like stamp expensive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
+import tensorflow as tf
+print(tf.__version__)
