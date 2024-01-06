76b29d1d205b9fd18ca25ba56dc86d47c0ceb34f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:04 +0000

    turnon congratulate all fear cough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
