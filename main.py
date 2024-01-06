fca087d10642bd8a9452c9fd4be1addef1a5c15f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:50:44 +0000

    drop after split attach constantly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
