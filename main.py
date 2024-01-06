ec7d5bb3ca69a005d26560c92a187b18a109091d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:45 +0000

    charge can journey react window

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
