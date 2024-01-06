b88dfaf646bf37a2fe0613808df0dda1db15d829
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:31 +0000

    through complete reason count with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
