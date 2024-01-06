08585adc3fbeec87ec93dcbd61898aadd2440f24
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:43 +0000

    section talkinto hollow shop closed

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def cube_number(x):
+        return x**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
