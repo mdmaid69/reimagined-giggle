8ba972e2b05df0a4a8c7bc7e1eac8760625d53a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:20:19 +0000

    so continue once manage event

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
