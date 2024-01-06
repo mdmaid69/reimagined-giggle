f2db105aae811aaf8a5072a9fa12948b4d8ac15c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:58:47 +0000

    past quickly divide cover memory

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
