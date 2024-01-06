8f3596bb7289d7c51c704b91e499c9bf4faca002
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:09 +0000

    suggest method interesting certain free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
