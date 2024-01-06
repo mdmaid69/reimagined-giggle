2f893d88333c678cd8c66b49e7bd32d99839f55d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:48 +0000

    disapprove fortunately float naturally off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
