1b75aa91908d08e36e461a7badf8fc362665d6fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:20:17 +0000

    claim lower tip summer aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+x = 10
+y = 20
+print("Sum:", x + y)
