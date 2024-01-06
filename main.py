d5d2ff0cee511bbc595301ea4665941d1d9df385
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:05:14 +0000

    door bit agreement should goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
+import array
+def clear_array(array):
+        array *= 0
