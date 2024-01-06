be6060ee96f411131413d2ecdcfa026454e464df
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:20:21 +0000

    bang satisfactory above goout not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
