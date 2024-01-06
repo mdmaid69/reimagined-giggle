b215b09ed9d6eac2ee8b311d95ac46242ae96e16
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:59 +0000

    at fix present tomorrow listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
