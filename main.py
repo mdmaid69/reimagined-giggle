4f7a35935acb137d23d3705c408ddaebe35876a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:44:26 +0000

    official become imagination order steer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_characters(sentence):
+        return len(sentence)
+def reverse_list(lst):
+        return lst[::-1]
