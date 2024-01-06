d7b32f7345a965711e8f9cb8d94aaa8a65ee2eba
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:28:41 +0000

    set put all stare public

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
