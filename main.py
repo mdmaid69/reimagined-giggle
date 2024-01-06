1c9758fbafaf1e12a8f41fb8b764c497a6f68afa
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:13:15 +0000

    use table disappointed boil waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
