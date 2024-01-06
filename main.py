ead385bdcd9eb01556734978fc946b4d55c790df
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:29:22 +0000

    hurt thank better tour concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
