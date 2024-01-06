69092e8945f3cfa039855e594c2fdf88c49f721b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:48 +0000

    evening connect height small trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_floor(x):
+        return math.floor(x)
