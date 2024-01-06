50916a6f22c01800a0f1bc2233f0b6d54a154b6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:11 +0000

    stir welcome rubbish occur powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
