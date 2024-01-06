6a16ebc96186ade6d5d4f497e9031c4b9ace6a7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:36:16 +0000

    force respect bring 568energy dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
