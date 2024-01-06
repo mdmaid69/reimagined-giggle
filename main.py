6f2105e2719ba7a897e1b9a524d7116156920e38
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:29:44 +0000

    think comeup attract pride whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
