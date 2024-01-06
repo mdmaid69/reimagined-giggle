72c25df3b3c2eb70aa73c20d210c516af63b9bd1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:14:46 +0000

    detail square talk pronunciation they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
