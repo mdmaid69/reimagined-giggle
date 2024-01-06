55552f0e1c9c1e8f47d12357d4f2540cba150db2
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:32:22 +0000

    workon miss old bringdown sorry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
