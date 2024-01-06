99cfff574a75ae656f5ac0a26aa6bac939e6f56f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:11 +0000

    aim cold interfere best further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
