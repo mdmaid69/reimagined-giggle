118bccf66dd55ecc4a604f07cf25bb41bbd6522f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:58:16 +0000

    freeze sharp dance buy refer

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
