170ee752076b18eb8370465bd0eec93885c9e393
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:18 +0000

    reserve about male unpleasant success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(lst):
-        return max(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
