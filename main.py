9a51bf54ba451d4a6e71c704653c32f3fd073489
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:56 +0000

    571enough material now bringforward object

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_even(n):
-        return n % 2 == 0
+  def count_elements(lst):
+        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
