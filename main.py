b3afac92c8380e182990c0206d39f9d58e551871
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:59:51 +0000

    radio shoot funny divide say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
