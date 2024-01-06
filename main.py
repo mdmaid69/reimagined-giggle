886cd7f5b52394a878493db6ea269c238d4350f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:41 +0000

    hold fierce trip want run

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
