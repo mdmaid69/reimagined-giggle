6dbd764acf9c07f9a675f785ab3b6abac3d118cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:10 +0000

    use bar tap pretty cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
