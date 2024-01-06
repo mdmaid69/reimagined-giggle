b1c1e85b122da3102d779ac96d5f753b86b1b369
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:17 +0000

    exploration earn copy swing takeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
