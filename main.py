0dd1c4d51300a7af95cce5acb9e6db11574053ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:31 +0000

    instruction cause likely design bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
