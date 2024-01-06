bf3d80e34c0a009c8b7fa81302dcd3dc54d5fd12
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:56:35 +0000

    after lose exam load need

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
