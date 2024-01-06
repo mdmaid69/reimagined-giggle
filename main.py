602f3e2c16b75fd0303fc4017b18fe23b6f2eb5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:59:20 +0000

    attention dry result tape rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
