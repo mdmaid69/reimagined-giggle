bec8c355cb1027a72c07d5416d87d9e222718ea2
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:59:57 +0000

    wake Hh lower underneath used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
