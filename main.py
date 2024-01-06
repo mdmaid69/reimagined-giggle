acf1bbcd047ebb85787d67ea5053393561ece74c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:09 +0000

    become thankyou glad takeover afterwards

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_area_rectangle(l, w):
-        return l * w
+def count_elements(lst):
+        return len(lst)
+def count_words(sentence):
+        return len(sentence.split())
