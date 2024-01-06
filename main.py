b60b3c7e42d535ade590c96ae0b6d12d1cfa4c8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:41 +0000

    home suppose bottom plenty concern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_odd(n):
+        return n % 2 != 0
