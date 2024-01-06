d6238f67971ecceed9f2173a57a77279f9cef5db
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:55:06 +0000

    mean ocean petrol blame hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
