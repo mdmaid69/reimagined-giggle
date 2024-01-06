85a11c93907cbc29cd11dfc1618913bbbb50f004
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:59:04 +0000

    cool attach advantage immediate picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
