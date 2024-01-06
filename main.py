582ba13add965a090cc28f9a0ec25f06cb1a30e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:09:15 +0000

    business blame appeal climb holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
