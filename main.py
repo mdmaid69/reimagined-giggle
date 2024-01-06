cf1ea50b90c624d698de8a39e567035dff850f52
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:51:49 +0000

    godown see under young 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
