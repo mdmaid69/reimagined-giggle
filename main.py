7fb0cd133bd407882eacd69bebfc61206806755e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:50 +0000

    tight turnon 570enjoyable represent let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
