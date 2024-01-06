422db44d7faa480d384d8b8327597fc9a931d085
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:42:52 +0000

    flight not absolutely shake although

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_sine(x):
+        return math.sin(x)
