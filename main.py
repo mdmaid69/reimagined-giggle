d0edffaf5bad874751bd3a2baaec80f244b6c983
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:24 +0000

    future press first crash blue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def delete_file(file_name):
+        os.remove(file_name)
