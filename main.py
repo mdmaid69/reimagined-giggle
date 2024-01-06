b29ab5343afac51fab2ce56a06e28dab07eb87b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:38:51 +0000

    form clear clean spite drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_work(force, distance):
+        return force * distance
