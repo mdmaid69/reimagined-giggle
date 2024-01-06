8a163823b53be2cf14340196ae1e10fd9e7a5410
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:49:12 +0000

    sensible standard hole punish machine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
