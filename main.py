75241c8f9c5753117b7ddb1b79c40fb939268e16
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:56 +0000

    stop reach goup Itself blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
