c5ebc26972c8256a3d29d8dbaf54b4a9f9da1caa
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:10:23 +0000

    prove pass forgive remain ground

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def add_numbers(x, y):
-        return x + y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_user_string():
+        return collections.UserString()
