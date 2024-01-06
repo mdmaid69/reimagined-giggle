53ac11544af1f22e99ed200bfa6d844dc96e0509
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:33 +0000

    add past pool mend tread

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
