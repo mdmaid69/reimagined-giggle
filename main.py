bd151e06f025e087619b7ad75ad38d9666eaeb20
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:19 +0000

    fierce then window eye style

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
