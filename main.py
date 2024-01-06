f6b0a4df966aa14b00bab1da3253242d5dc54a95
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:16:15 +0000

    Ii work look careful its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
