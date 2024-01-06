ca3b3dc18ec3f1423cae6eb80fe64ca44a528015
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:57 +0000

    brush dish information block fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
