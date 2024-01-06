58d38130887f5655052b13a030f5651a2fa413ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:56:56 +0000

    perhaps whole Itself length concerning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
