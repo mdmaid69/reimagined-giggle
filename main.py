dcdf903761fc74bf44317d88f6df5f7d4ddb3675
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:04:03 +0000

    top throughout attitude slip flavour

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_speed(distance, time):
+        return distance / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
