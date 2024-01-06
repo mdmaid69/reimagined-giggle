f034242168f42e627ccaa78aa528babc51a3a81b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:14 +0000

    body interview turnaway which custom

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def clear_array(array):
-        array *= 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
