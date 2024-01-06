4d54ba03ad6a2dc42ecd395c10704037b322f556
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:44 +0000

    circumstance sport estimate interested branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
