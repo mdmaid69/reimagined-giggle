39eb130a3f8df7dc2d9d4778e9ab8258ff0aa633
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:45:21 +0000

    drink winter contents Ww disappointed

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def count_elements(lst):
+        return len(lst)
