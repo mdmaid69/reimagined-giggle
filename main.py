6ef38e8f4db1aad2006fd9c256071a3d617f34f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:23:33 +0000

    guilty plastic block nice reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
