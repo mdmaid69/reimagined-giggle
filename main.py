ca2c3de5e6b63172ad655069f46f6344a8ecc485
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:32:42 +0000

    male jump cold idea escape

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
