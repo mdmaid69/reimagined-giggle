a01d15b5b8fcba4554c91567bfe7379345a9c4f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:24:30 +0000

    temperature close neat course feature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def delete_file(file_name):
+        os.remove(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
