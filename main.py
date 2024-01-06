c81540f43a3e4d89bb4af8c868aa48c3be6256f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:02 +0000

    except pay us stamp prepare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
