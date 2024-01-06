58fce87a7fca3a50fa77ad6f6a780ac25cbd2b05
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:28 +0000

    broad confusing definite corner everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
+def count_words(sentence):
+        return len(sentence.split())
