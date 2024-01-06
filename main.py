afac177225c56bcf81f0763a2c63862c8cb36d01
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:45 +0000

    phone spread office watch cost

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import sys
+print(sys.version)
+def convert_to_octal(n):
+        return oct(n)
