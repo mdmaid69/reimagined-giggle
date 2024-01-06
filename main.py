c343117f52c97037019713c42a800bac5e235aa6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:34 +0000

    possibility uniform crime feeling long

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def count_elements(lst):
+        return len(lst)
