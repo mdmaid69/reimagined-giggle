9e80207fdeeda888ba7da691a28c9e9ce0738b8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:57 +0000

    wear distant see whenever concentration

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_list(array):
+        return list(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
