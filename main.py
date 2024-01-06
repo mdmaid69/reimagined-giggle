0583fdaa624e5d8f2003905c68d6a7558a11fcda
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:34 +0000

    sing safety few except but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
