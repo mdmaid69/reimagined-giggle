abf5722f596562f7d1b712d7e39812e39d1ca9e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:18:27 +0000

    unlucky instant scream day rest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+x = 10
+y = 20
+print("Sum:", x + y)
