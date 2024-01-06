a812b591cad180f3db24869ef9a9a81fb248f44e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:32 +0000

    summer mark drink sentence mend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
