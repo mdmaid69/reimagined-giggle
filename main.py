086f20ba76a9c517086a2f0bba3150800be37a5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:32 +0000

    callback over wave intelligent phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
