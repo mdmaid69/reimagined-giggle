cf7a88eb0fb552c41a5af09efcc17ae26acc1a76
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:02:14 +0000

    switch here too either schedule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
