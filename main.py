54a2c854a2bf355c93533b7adf6ca2035c2cd2c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:25:27 +0000

    cost copy extra shadow tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
