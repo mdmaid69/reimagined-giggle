cc2fafa0a0dcdd610af13e00a8ba9eab98cf3174
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:16:21 +0000

    rush rid weigh train square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
