25a77ac8c29fac8eae5f126d5716bfb9660192ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:09:44 +0000

    or thing bother wash final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
