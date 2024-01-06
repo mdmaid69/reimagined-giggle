3c2945a9f640a23fe43a1a6c7cfcf6a5b165cee8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:34:42 +0000

    thought condition one bend please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_speed(distance, time):
+        return distance / time
