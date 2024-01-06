7f51c74eac4b898c05872ec80a7193cf7cda8cfa
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:22 +0000

    undo deep left lookafter rid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
