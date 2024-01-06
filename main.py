e2ae0928604680276292208efb307d3de085764a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:11:35 +0000

    moon chase petrol near smoke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
