dcd6679d6d7f99a47d962eb4d922611466350f54
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:12 +0000

    pause crisis back quiet away

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+def get_current_time():
+        return time.time()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
