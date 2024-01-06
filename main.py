a52794941686308cbc2bb366cf52224bc289ec2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:05:05 +0000

    step takeaway stamp load storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def is_odd(n):
+        return n % 2 != 0
