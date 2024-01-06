3572ca9357f84077fe6bd69c288897fee0eaebd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:11 +0000

    capital brush birth effort easy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
