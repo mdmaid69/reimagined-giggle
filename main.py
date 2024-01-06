d516a553cf49e3666c5fc7afe1b140143ef77f96
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:50 +0000

    point group influence silent break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def square_number(x):
+        return x**2
