bb093ee1c7fee3027c14a5e44f69746ca2e16c67
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:02:26 +0000

    ill either guess run doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_current_time():
+        return time.time()
