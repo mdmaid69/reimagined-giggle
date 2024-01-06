766a75cba0a1295af67241116c9d6c99aa6748e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:52 +0000

    fasten make fortunate watch comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
