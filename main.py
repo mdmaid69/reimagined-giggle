71f82e4d0e88119d9f2059a10269354ec07480e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:01:12 +0000

    bother uponon ours play embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
