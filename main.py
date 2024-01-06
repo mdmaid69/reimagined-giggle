4d5be669ddc187a4cfca608b30c143d475ccbb00
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:40:48 +0000

    comein however combination shout estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
