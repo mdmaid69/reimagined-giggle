a2f73ea4e8678274fc27192f47f186c50925a558
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:54 +0000

    Yy apply apparently dance trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
