aae01352b4b01a214fff18a784655fd387b4fc7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:41:04 +0000

    evidence boil come mark during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
