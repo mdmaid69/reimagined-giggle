8add81b8f562d6752e38cda4ad95694a41be4a7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:42:12 +0000

    degree well light duty big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def convert_to_hex(n):
+        return hex(n)
