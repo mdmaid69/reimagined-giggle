e0c0dd98a85d85e7815d9043611582240f70167e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:36:49 +0000

    demand bury shock successful appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
