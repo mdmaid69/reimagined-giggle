ac101a594f265d0c94c460273d1b2d1e412bf887
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:14:37 +0000

    brown act objection ear flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
