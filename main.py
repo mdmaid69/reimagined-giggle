350240ee6399a8518c60d57e46bad68aae09a6fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:32 +0000

    surface cheat my account increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(lst):
+        return max(lst)
