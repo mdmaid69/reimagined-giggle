e0a063da8a3235c47e5bf8b6fda344a008499325
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:44 +0000

    share wheel hour cross shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
