0f4015c0ebece7cf3a8bc47a07212606bf8b4d75
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:59 +0000

    bad national reply along pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
