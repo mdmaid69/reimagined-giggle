395c1a763243067d245b436a0181d4fd726518fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:47 +0000

    comment twist will work narrow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def multiply_numbers(x, y):
-        return x * y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
