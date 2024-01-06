4c1b3888cf88aee3e32d8389e4615b4cb51beb13
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:13:46 +0000

    threaten grow old rule transparent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
