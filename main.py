886c02e70f82e6b3ba9f9f512d1cdee3ff541dd3
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:36:30 +0000

    standard indeed cinema deaf total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
