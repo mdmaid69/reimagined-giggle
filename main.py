84fa10603a477386cb3dbdb915094ba9855047c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:30 +0000

    likely pretty use dream before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
