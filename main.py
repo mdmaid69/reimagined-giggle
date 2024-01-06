6f8ffcc40eaa36ca4608e8d906b262ae393b7510
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:44 +0000

    terrible takeaway range brave in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
