12147ee5b561301efa0d42051380caff45d4a04f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:36 +0000

    lose do science joke study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
