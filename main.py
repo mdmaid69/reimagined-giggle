7e2768b376042c18be6c7c6cdee75cf20e195112
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:02 +0000

    wind reasonable upstairs roll charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
