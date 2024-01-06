e2f91445c7465e0074de5e65004f2f25e393b1db
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:26 +0000

    believe make alone equal repeat

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
