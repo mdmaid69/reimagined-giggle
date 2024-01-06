180dcbf1a6579e0396fce33b5006307eedf2e2db
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:58:43 +0000

    press best creature success convinced

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
