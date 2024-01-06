5b6d0aa37fd597f5273e583ce59f2eb6155346fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:20:04 +0000

    circumstance add children kick dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
