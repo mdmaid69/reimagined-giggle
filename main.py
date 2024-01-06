92152c8356eec0b8a047f482ebdeda78d4fa5b43
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:29:30 +0000

    hide method intelligent giveup middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def is_palindrome(s):
-        return s == s[::-1]
+def is_even(n):
+        return n % 2 == 0
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
