76879e19fa71dca807e25563eb0e218d2fd7eaf6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:40 +0000

    getoff equipment basic no know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
