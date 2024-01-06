0b66d20262e76f26dd2155443766cbb34b9fc289
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:55 +0000

    begin raw full rush contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
