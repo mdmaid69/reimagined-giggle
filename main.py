d949b8b51a3db8480736d81379a63ef80f737f34
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:38:24 +0000

    cheek division scare steer nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
