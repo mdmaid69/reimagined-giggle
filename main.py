1a6c91ce218a335339641b8dfea1c943d2de8877
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:13 +0000

    still quality consult otherwise wire

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
