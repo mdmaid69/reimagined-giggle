ad608a1a219adeb7fcb8944fc9a8a3e95f472399
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:40:24 +0000

    Itself pity between gradually confident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
