f7c96a10cc21cbc36fc80d18e64c2c15c02fd3fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:20:10 +0000

    combination almost escape satisfied after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
