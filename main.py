e744311134f012395e3c6ebe31c79777b54b1577
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:02 +0000

    shock wise walk chance apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
