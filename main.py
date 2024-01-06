5ee2174a5aeb32e3d967876bf1b0cbde92908e26
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:27 +0000

    border clean behave authority apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
