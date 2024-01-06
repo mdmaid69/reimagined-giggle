5e02b69702f3fc0af11ddab61c6ab44460b8c952
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:34:09 +0000

    turnover creature hit chief body

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
