12fff55897542e4fa7af7f629e7ad68e578d7739
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:52 +0000

    off climb sound ill laugh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
