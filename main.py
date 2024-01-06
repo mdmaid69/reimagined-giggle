e19da3c2dbb99e670ec7a301918c8ca203f63591
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:29:02 +0000

    dull help direct miss half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
