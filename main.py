7c23b1f8e95bf7b34ef6f1404a51183efe123b51
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:41:39 +0000

    separate level sign meet married

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
