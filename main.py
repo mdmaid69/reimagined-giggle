fadf0092f7b7e076409c9702deede1319d2a2921
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:30 +0000

    shrink what indeed disturbing community

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
