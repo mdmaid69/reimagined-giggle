3fb3ffe83bcc51b61901991d164c20e60a3fc159
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:30:03 +0000

    comefrom list main turn cut

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
