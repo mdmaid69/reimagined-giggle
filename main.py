64551c56541aeada427e209bcad753cb9d13e2b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:20:39 +0000

    climb special effort Yy somewhere

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
