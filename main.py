08625a5df1b533fd5643fbab9bd4d940039bbb5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:29 +0000

    transport pure sale wild spray

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
