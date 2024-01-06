10b15dc131cea97ab1db716132ac729b8bf58189
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:24 +0000

    step year one empty again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
