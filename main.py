e83b20a718aa7a605d3bf3cebc63ee72bd62e8ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:39:10 +0000

    usual no nurse demand balance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
