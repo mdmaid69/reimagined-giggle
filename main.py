16d9c09e74a53a84d100fc99f391477f2dbfd1ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:16 +0000

    male train still create crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
