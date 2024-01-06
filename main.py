0735bc11e0b17f8f5870e7695c5ccf58639512e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:38:37 +0000

    answer scrap study proper object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
