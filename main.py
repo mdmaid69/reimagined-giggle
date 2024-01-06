b2c5eb9d739bf1cc347067733918247655ac1bc5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:00 +0000

    important direct arrive police case

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
