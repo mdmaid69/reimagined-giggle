edefcfefcd98eca75eb46c9bf65e9228e6008d94
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:03:05 +0000

    depend suit until personal emphasis

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
