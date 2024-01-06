006eb1adfdf61880cc7b4a3c31fee3cb4b5659f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:43 +0000

    tour shoot usually weekend difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
