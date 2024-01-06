8bf311b55ea2ba571ced6fe9f4584b9458a97efd
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:32:54 +0000

    only with ashamed talkback mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
