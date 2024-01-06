bff1fac5c1fa8004688868da7b3ef0dd53513ef1
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:00:31 +0000

    closed base line 564encourage however

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-print(math.pi)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
