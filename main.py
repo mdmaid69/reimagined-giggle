d341d5c7c03369d3b1856c0b28321b1a7b4fd349
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:29:27 +0000

    piece tap wash history movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import numpy as np
+print(np.array([1, 2, 3]))
