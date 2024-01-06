7f2ff607d18b76458b63ef849533d19d8742ac7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:24 +0000

    gas hour calmly little however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
