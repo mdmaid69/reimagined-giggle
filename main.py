e9454942fb09f27a76cbb1930e7c3a606b309fc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:00:36 +0000

    kind 572enter contain strike danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
