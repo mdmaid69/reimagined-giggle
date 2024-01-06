773f2acdd39e98094fd50794da12fb8edbf10046
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:51:57 +0000

    save sing factory label along

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
