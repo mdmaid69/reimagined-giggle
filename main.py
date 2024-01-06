85b8629dfaa198443e854d9522406deb6ccc79fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:40:48 +0000

    clean main snow bang gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def sort_list(lst):
+        return sorted(lst)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
