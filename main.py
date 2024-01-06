baabb2ba6fa29130cd000553c25159c627a7af73
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:03:03 +0000

    shock except likely hello face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
