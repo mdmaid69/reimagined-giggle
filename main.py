a5352f81c25b47b5faa14648c902426efff57416
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:21 +0000

    Nn contribute secret name scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
