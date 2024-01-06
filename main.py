52f687e029921b07ebc7c459958f317d6162a7a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:43 +0000

    finally address value nose pump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
