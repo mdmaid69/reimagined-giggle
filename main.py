30ea91d2eebac3a13b536642687adf876cc070c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:04 +0000

    fun rubbish sting practical steady

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import tensorflow as tf
-print(tf.__version__)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
