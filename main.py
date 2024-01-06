8dbf03f4577016914876e91249ca0ad532dababa
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:08:20 +0000

    rub scream ticket body base

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
