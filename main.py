23997b33f4ebc7af3518532220979e6c88a46606
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:31:11 +0000

    cry child reject entrance shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_hyperbolic_arc_tangent(x):
         return math.atanh(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
