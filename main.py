09024f82c4ce357e0df1268ed8b217f561a345eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:09:41 +0000

    whistle rely pattern movement train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
