0dde959a42805f4659a41a5faabf676ffe21e00d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:15 +0000

    bill matter blow determined emphasis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def cube_number(x):
+        return x**3
