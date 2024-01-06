5c782c4a14ef218f63838962366172b8eba95be7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:44 +0000

    breakout hope invite line final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
