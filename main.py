a6dc708fb8f80ce31e60ac206818928b50c7eb14
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:31 +0000

    stop born quarter opposite arrangement

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
