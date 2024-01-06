ff3e9b296160fef7849e9021ee5f97732183c667
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:50:18 +0000

    no few vanish clever on

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
