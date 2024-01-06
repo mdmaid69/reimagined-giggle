f78ec7b1098c3418dc438c2d9b11af0e3f47c477
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:04 +0000

    badly attempt Zz personal free

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
