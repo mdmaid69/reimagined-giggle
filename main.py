5755062ff5c300c5dfe8d96f11efe993ae49b186
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:11 +0000

    punish collection false offend behave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
