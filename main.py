73fd923ab2a1dd4082fd6eb2df35af67286c2b00
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:32 +0000

    wish imagine me ourselves fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
