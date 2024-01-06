15993ed14cf24aaab626e6ed4bd94bec70de8630
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:39:17 +0000

    room Ee shoulder discuss gentle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
