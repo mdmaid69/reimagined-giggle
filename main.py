d3d2a469db24879d38b86f8e7f79c97faf79ecad
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:43 +0000

    movement warning there file strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
