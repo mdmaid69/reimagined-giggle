7dc452ab8a241fab6cec39e697e303ec8e3b4fa0
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:08:36 +0000

    table interest inside blame seriously

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
