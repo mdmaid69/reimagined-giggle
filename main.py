97159e470e592ad87e27312271a99fd6ea6b117a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:39 +0000

    bet below Gg pronounce uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
