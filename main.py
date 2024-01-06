17e427e4bb0824a7a364c7643a3ce2483d643452
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:04:41 +0000

    noise trust centre holiday please

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
