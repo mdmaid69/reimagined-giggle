d4d7a527668f66e693122390695e2e920c464b6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:37:15 +0000

    clever interview complain impossible occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def count_elements(lst):
+        return len(lst)
