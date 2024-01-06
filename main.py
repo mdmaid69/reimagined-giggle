bbb7ab1f98686ab9351780bb1c72f6c4d596de14
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:31:01 +0000

    move fun shallow butter still

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
