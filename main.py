517ea6f20701163e687783cd8aeeda3ec7354e3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:05 +0000

    available last believe bar slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def clear_array(array):
+        array *= 0
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
