dc7c6417c5dc177bead98ce65a2263c0dfb570a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:35:32 +0000

    ours open another priority passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
