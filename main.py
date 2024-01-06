56f18868a7c8e8d54885f67f2def9a0a9be1992b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:08:39 +0000

    conversation speed sit sink game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
