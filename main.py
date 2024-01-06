159774fc33997b38183911e119dceb6a0f1b6281
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:03 +0000

    mark sit flow raise else

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
