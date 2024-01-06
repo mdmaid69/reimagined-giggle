e93aceaa4754b31b35fcda5d2a8512ea3fc8a05a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:12 +0000

    female approve wave fortunate police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
