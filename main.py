1f286b58c6a8bb62286c14e01a98b2b60e0f521c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:05:41 +0000

    flow find turnaway cool brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
