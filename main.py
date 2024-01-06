473bdd3dc8ab522e7067e6fd11667f8154a9298f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:43 +0000

    takeover possibility dress furniture comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
