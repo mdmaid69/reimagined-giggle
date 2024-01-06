6d38ffb1dd632dfd3002ffcd0a104ee7252bd1e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:48:51 +0000

    excellent slice sound public false

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
