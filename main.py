8a4891657ccd2b7582d8fd80be5ee78d8d6d86bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:05 +0000

    accordingto roll world unlikely tradition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
