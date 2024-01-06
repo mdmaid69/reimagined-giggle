12ad79285c3c60b9fd055711f8ad3fdcef528c54
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:52:59 +0000

    hurry morning tread Bb involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
