8096095c82f59dc40f7ac5248d05ece9f51f8b6c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:35:01 +0000

    and black most wait may

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
