8d2a7d064ffb6f643141044940b4cc42dda39d62
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:45 +0000

    dish chance wonder rush band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
