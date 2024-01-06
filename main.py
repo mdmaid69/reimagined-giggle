a5027d7099e483bebbb34a71defd6cd16ca5e0a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:35:58 +0000

    variety nowhere callout fire blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
