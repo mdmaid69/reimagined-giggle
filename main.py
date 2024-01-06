54f7ddf10dc35d5a3a52d297bb64f618b238c167
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:09 +0000

    public another doubt close phone

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_even(n):
-        return n % 2 == 0
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
