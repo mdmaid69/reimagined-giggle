eb3a4ad0f1946e12a47e8236406a76b5b174140c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:12:27 +0000

    keepoff warn Ff big music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def find_max(numbers):
-        return max(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
