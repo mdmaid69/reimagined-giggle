fe8bb28755f22885c4f7c5d5d5e97e1a983cc9c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:46 +0000

    set burn pot couple serve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def square_number(x):
+        return x**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
