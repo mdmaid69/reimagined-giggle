fe681aca31a0fa2ad968f3aa1d2ec96511bed6ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:03 +0000

    sticky quality on Nn spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_environment_variable(var):
+        return os.getenv(var)
