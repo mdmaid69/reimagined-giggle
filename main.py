13bb4cee2e2849d43752e56399b11be0f57ee0df
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:23:31 +0000

    gooff deaf square apparently recovery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
