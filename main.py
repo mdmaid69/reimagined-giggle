9b243890b4ad1cf905b6aaecba9949d1b332b5ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:19 +0000

    mistake middle love school visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
