d13c60f7bd0b07c130448e980ac187ca153e76ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:28:23 +0000

    motion pin recover tonight succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import os
+def change_working_directory(path):
+        os.chdir(path)
