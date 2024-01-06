0393d9f515d819758c4b410a38c27932fbb3d88c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:40 +0000

    own mark complaint 571enough hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
