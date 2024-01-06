3ad2295905a91ca01f1d20035dab08b168c19cf2
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:32 +0000

    early add light uponon choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_max(numbers):
+        return max(numbers)
