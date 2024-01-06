ad0b3f929e3e558018651735af9d9bcf410e8aff
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:34:47 +0000

    occur test interested surprising sting

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_acceleration(speed, time):
+        return speed / time
