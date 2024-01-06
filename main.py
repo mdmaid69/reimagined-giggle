42c64f3befc3b13ee758934f80b19b25729eaf88
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:23 +0000

    gift busy complaint obtain open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
