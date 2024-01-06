4c41d03a7149a82d8606acbf837cd854b5d04e89
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:02:35 +0000

    like lookback(on) dull back pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def convert_to_octal(n):
+        return oct(n)
