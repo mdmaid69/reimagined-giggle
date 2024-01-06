caee7eab122006cbc68a3732e8a7d8b99f2a57da
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:43:21 +0000

    break interested explanation clearly keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
