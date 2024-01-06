9d5d95309c575ce03474171edd5a0ad35c1b92a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:33:59 +0000

    character design tense outside set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def create_stack():
+        return collections.deque()
