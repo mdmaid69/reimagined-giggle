d5247182660dcaca7d952e3ed334405c1b71e1fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:11:29 +0000

    considering frame bright friendly cruelty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
