c8fc6151bf19858ed1942b630e61c35ff94658c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:18 +0000

    shall 572enter lock though harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
