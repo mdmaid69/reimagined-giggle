6f1feb98cbebb52a1daaff3431f577d66f360234
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:50:44 +0000

    share weak practise fire wash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
