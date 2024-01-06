925fba1c0de7ed10b50f945d6cdea515f2da856d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:52:54 +0000

    hand wear mean praise ability

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
