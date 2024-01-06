99f8489702af5cc22271015ac08369631f84a720
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:44 +0000

    aim either cook sort instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
