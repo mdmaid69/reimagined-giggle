0e62e504f012a73b82753226201d15a041597d64
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:21:31 +0000

    prove bar discuss meet tongue

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
