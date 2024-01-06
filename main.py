39aa97d1463c71aae75c17232197f436cefbb34e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:33:16 +0000

    glass punishment dangerous means spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
