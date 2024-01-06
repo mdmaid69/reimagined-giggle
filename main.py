685f10711a3d34222474902125494b2613515201
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:07:40 +0000

    sort real buy godown back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
