8e002449263de34d418cc451685a58a780975169
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:48:37 +0000

    door ignore schedule pay cruelty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
