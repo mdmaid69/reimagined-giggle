bb0044001a84eb582f0846be8e397aaa7039a66e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:45:44 +0000

    nearly exchange key vision high

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
