82f158bffadb66a99c7c9573f8ac8aa3830667ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:37 +0000

    swing pure nurse blow ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
