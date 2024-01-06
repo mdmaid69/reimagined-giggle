d03c6a16cd529dcf6252441af8a1dd5b79d04c40
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:58:11 +0000

    wish behind breakthrough contact fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_string_from_array(array):
+        return array.tobytes()
+import platform
+def get_os_info():
+        return platform.uname()
