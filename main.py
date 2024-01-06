15f719aa44d00bed9c68763ef198e0f16a4424bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:13 +0000

    church takeout hardly ear through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
