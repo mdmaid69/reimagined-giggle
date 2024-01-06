3eea2b4df13d1bbe50f1312188016f1a3e2fc0cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:54:41 +0000

    give stay fold break computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_volume(length, width, height):
-        return length * width * height
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
