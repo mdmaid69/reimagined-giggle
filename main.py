4d49c8e77f39b93ac58f7ed84651772e8dc749ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:43 +0000

    thick save talkback manage who

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
