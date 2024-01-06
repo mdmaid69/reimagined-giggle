b0547c3acc86d30ad5b7c49d830bcafc8189ea43
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:51 +0000

    light makeup scared statement collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def find_min(numbers):
+        return min(numbers)
