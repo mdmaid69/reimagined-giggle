f61b7826ef30baa7b50cc3018c2f580599356bc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:34 +0000

    anywayanyhow stop fire tap anxious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def sort_numbers(numbers):
+        return sorted(numbers)
