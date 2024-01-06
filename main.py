9e9fcbba64e857ac39dd57aa6c6f1c303f3daa3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:08 +0000

    smell improvement pin niggaacademy anybody

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def convert_to_binary(n):
+        return bin(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
