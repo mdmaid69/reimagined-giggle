66418e8242730698945eee26721cb4aadb7a08a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:35:33 +0000

    beyond town record fact state

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
