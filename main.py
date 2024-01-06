f64e21d87bfd15345d7a310cbb3a49fa553db10b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:00:42 +0000

    motion shock only provide bang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import numpy as np
+print(np.array([1, 2, 3]))
