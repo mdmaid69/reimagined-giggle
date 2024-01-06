1095bcdfb0a12d8104a57c35501c415941b2c856
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:52:16 +0000

    positive guess recommend tune concerned

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
