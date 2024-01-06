e9d518feaa20e2611939e0f11e4b539706949e72
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:23 +0000

    fear of heavy record greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
