5665719dd2627a3ad306f50eb868412a136d2228
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:22:34 +0000

    track long field goback(to) objection

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
