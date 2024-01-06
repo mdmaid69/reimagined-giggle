982fa0aaf63223dd13f0cbb4350b88ccd5ec1826
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:01 +0000

    cut push leave shadow bet

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def count_elements(lst):
+        return len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
