cbd514d076f7c7b9fe521cea7ef2f12f4557546d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:31 +0000

    pupil base cross direct fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
