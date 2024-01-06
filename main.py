d3f2163edfcfa5f2d01a466f98363ee34ae17bab
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:24 +0000

    estimate opinion thank load like

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
