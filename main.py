45a16a8b048d53dcf27559938234ed7e70b75e24
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:19:06 +0000

    bad obvious minority middle personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-def find_min(lst):
-        return min(lst)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
