9a503f2b8e114002a0ac40abd13f91a0bfdc1f2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:10 +0000

    original exist back behave active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
