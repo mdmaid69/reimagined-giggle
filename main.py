aaa0cddf9871288bf729dba97700d6cbdc948995
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:12 +0000

    regret call news over last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_str(array):
-        return str(array)
+def count_characters(sentence):
+        return len(sentence)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
