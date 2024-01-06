06fe4a200364c38f5169aa5f066a46dca1f43d58
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:01 +0000

    cause pride card open private

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_max(lst):
-        return max(lst)
+print("Hello, world!")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
