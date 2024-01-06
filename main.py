e78e1715449e4ea6a1be9037cb7e2d2fde72312f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:28 +0000

    ilovedicks ability within flood difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
