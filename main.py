ded6df766ab2e1062f305a12737c13a3f85fbaa4
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:04 +0000

    badly comfortable object confuse mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def find_min(numbers):
+        return min(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
