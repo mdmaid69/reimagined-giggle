9141f769b30bd9be999718a1a540f8391bd19feb
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:12 +0000

    round along touch result delicate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_rectangle(l, w):
+        return l * w
