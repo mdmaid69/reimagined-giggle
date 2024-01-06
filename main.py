cd20c9e9c86b0a33fb5960d97666f4fa2ad147f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:57 +0000

    ground must separate sort within

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
