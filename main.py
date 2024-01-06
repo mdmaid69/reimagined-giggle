4743d7500f359f0ad2d4dd535cfaa701dc2c2dc1
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:48:09 +0000

    rubbish spring cheek confuse warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
