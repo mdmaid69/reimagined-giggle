6679ac6aa61d5de94814a6db8149860a87c5372d
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:45 +0000

    wire upwards although because protest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
