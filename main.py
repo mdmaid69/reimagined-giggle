3f434e87e521f505b6462b6356ba3e66c7962ba3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:25 +0000

    string shake downstairs unless bank

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
