4bd501d36eaab06afc125174707ed689e5a009a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:08 +0000

    busy talk hurry OKorokay wander

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
