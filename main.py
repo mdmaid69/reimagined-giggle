5fbb7eb16dfdf92f236dd8c3c17a125856754502
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:02:30 +0000

    bar skin past prefer need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
