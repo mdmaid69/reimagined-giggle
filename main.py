93c8c61544145b387d8908d4d66981d68df03a25
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:15:03 +0000

    kick tone actually appeal fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-print(math.pi)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
