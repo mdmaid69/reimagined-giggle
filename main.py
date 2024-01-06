c37c0826a51823dad4c15c90785ba365f70bab67
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:25 +0000

    closely wealth hurt call full

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
