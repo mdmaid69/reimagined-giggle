0cf1a3b6b62f792ce499c347b033e1235bcd4a73
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:16 +0000

    combine television clean like pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
