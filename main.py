5216859bd02b512ac05166fa9bd3ad63bd644ea2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:27 +0000

    shout split bad square urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
