fb3bba0deb5aa7ff0e9891857edba61c02958845
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:55 +0000

    tidy blame damage wrong protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
