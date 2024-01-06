5f3f27203ad7ad637b4dca93c1c16590b28659b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:14 +0000

    build afraid course wide both

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
+  def is_odd(n):
+        return n % 2 != 0
