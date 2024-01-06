72610e5d235fcec948da01ec5e7bd2c77f222e8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:55:44 +0000

    hate goback(to) mind 573entertainment except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
