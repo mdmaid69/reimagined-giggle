547366f5177d281221d3d5259b4590275374c875
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:34:13 +0000

    water bite inside trade like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
