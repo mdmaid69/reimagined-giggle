2778d347052c32cadbdbe0016d79252a21ba56a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:13:51 +0000

    appeal callat worst skill goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
