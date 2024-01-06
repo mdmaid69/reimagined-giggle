d1fa891c2260313c0d2a2e21462f19159e11c122
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:33:00 +0000

    Nn OKorokay Qq small better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+print(math.pi)
+import array
+def get_array_typecode(array):
+        return array.typecode
