20ed31144cd856aaed40c67a8d77adf4edaf15cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:49:48 +0000

    comeup rent worry suppose try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
