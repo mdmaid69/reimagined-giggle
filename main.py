1e62ce2251c13015448c2d62f65432c33ef91863
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:40:29 +0000

    blind turnover continue future any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
