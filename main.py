82d1d4213f2893fd2719c2a542c38987ab197e35
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:09:49 +0000

    quit emergency finger project pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
