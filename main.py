1442bc2c95f360b7d6f3a2e597e2d9d458217c18
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:10 +0000

    getout shape like pretty reserve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def convert_to_binary(n):
-        return bin(n)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
