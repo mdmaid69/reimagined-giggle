baf83f211d42190e96f92ff8a56e68bde5b84db5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:58 +0000

    immediately connected complain wave power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
