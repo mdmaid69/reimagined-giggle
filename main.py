205424ed725674e7adde679a0408c2e764313386
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:14:39 +0000

    flat protest empty he constantly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def square_number(x):
-        return x**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import sys
+  def get_python_version():
+        return sys.version
