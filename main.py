321fe0dba840766c79e869332c4ec9c84281ef87
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:53:50 +0000

    environment interview hollow private tour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
