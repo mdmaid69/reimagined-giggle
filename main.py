6db426bbfb9824f0487fe63bf56f2672fe1434ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:43 +0000

    accommodation turnup inform worry attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
