1cc5d679cf8580aab102740857cde3bac9106978
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:50 +0000

    organize calmly confident over shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_float(array):
+        return float(array[0])
