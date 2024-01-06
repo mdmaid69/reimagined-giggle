d63c2be5a54a1d8de4876a3ba7a7564c07c0798b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:04:20 +0000

    needle sorry leave hit port

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_even(n):
+        return n % 2 == 0
