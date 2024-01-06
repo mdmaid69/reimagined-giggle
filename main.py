dc988cf65e8eca892c4fa7fa5b637ae4088c4f7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:12 +0000

    matter true fond purpose work

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
