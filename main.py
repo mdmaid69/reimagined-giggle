95d5a2ffe5afbc3e13f2bf7ae528a1848677c0e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:33:25 +0000

    570enjoyable whichever remark language quick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
