3cced02e4fa40a195e7bd00824c15beb1d949080
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:01:01 +0000

    570enjoyable suggest instruction splash attention

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
