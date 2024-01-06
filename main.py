724e908881f74f09a9d481cfc7f4a211eaa0a6d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:47:40 +0000

    news tall cause result mine

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import collections
+def create_stack():
+        return collections.deque()
