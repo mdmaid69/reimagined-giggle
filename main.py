f766393c71a2f1488240829a1d5d0e46baa5e210
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:30:56 +0000

    join sweet demand shut tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_array_as_complex(array):
+        return complex(array[0])
