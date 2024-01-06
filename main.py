11e7ed9a3c30ee7aa143621ca3cbda01a4bd8d4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:07:59 +0000

    everything defend stiff wipe imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_set(array):
+        return set(array)
