0988b3c099336fd785eaeff15cccf6b3f2271d12
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:29:40 +0000

    stiff freedom university idea pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
