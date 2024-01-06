5882c6676f1d36b5a9cb58662ce9810b79b78b70
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:57:24 +0000

    normal throughout shut priority fill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
