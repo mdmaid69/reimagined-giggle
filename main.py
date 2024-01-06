c0106c7f4c85f9f8849c7471e511e765d80fd549
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:34 +0000

    comeback danger choice proof condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_sine(x):
+        return math.sin(x)
