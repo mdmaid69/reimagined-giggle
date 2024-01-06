a3f9d5bbcbaa3c8e1431198e2dbe7a3609ecc115
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:46:56 +0000

    beautiful bringdown round escape bus

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
