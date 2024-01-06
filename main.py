60fe5bbb7a1b183ef51c7da1a7231991d9714c18
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:06:48 +0000

    ilovedicks emphasize attention weekend thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
