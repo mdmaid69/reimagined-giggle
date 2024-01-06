e042147a5c16a7456f0b3b362887dea150efff5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:12:33 +0000

    visit top chat weak road

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+text = "Hello, world!"
+print("Words:", len(text.split()))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
