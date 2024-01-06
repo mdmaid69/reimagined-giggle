b9fc94051805c118c8ca6a6e76f999e954f605a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:58 +0000

    chase badly custom afterwards test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
