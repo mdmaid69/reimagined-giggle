988773b3687347c5a17b61a06836f50191f311e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:46:24 +0000

    pipe pair authority comeback breakup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
