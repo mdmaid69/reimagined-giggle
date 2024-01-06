b6ecc35847634426921f235b0b69880b67f0288f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:57:34 +0000

    no perhaps month crash lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
