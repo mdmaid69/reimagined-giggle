399cd7c7630f3a9a03d5d328fdda2cb0e16076f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:48:03 +0000

    recover programme touch passenger drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
