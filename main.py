724fba1a57298681dd6d83060776ea1e77270f62
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:10:22 +0000

    active however threaten comeover stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+print(re.match("h.*o", "hello world"))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
