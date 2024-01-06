fa2a1f5d55f4c22c38c7cb576ed09c04613c3dde
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:09:09 +0000

    turndown pain solve love blind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def is_odd(n):
+        return n % 2 != 0
