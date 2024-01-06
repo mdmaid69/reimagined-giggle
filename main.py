e0b593e58e9eed7fb107eebdeaaaff9b2cac4005
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:38:48 +0000

    tap suit unfair Uu my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
