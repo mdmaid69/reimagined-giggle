41207cf929538c8c72bf144a275fd31595b10482
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:12:10 +0000

    lonely protection feature picture round

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
