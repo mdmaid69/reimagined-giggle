0df713e71ba405e1ca76f5cf3570e5a956e8206c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:54:57 +0000

    description hurt rush illness dick

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
