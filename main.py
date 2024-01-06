33a9151073d0cd66c6bad98c94f4ed10ca52dbc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:43:49 +0000

    heat lucky shoulder separate that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
