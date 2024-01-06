1db80af14a65839ea7326575d39894815a0d4723
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:38:41 +0000

    method board steep connected disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def square_number(x):
-        return x**2
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
