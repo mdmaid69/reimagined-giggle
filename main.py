0c07ff4482ca5570838b29daeacd489070b1ffe6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:53 +0000

    myself quick who cause star

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
