6f6185d668679f8238986ef616151aeeb38d86f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:06:09 +0000

    cook track occasion steam buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
