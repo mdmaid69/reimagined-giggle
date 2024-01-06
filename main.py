793737a24f8f37f1431a25bb9ffddddf3b0cbcd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:06:29 +0000

    fire free especially avoid keepoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
