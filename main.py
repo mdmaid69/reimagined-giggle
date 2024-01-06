744672603bcf20a8af25c1c09af62ccdaa32bcd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:10 +0000

    cut lock rush juice violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_slice(array, i, j):
+        return array[i:j]
+import collections
+def create_priority_queue():
+        return collections.deque()
