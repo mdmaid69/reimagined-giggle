598454b593f440d4159d0cc5d5b3759da390291c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:43 +0000

    would runaway limit hungry repeat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_unique_words(sentence):
-        return set(sentence.split())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
