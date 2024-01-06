9fb928bfb4ae0cece2ad9737a3734b071efa996e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:53:14 +0000

    concentrate phone fill duty concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
