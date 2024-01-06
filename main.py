eb3419784deef972a232d6677d412186f5f494a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:50 +0000

    sensible worried if subject tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
