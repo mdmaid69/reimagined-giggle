05ca147b2dfb68b48c9666c5748a387019a42ae5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:37:22 +0000

    dance Ee quarter hear quit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
