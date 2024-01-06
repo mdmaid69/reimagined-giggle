15917d38e41bf1ccd0c752c113cc04c73a3f9e77
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:43:47 +0000

    involve attitude breakfast support sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
