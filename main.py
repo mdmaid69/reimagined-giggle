e998d71c39f387d12f0e30860519b79fdb6039a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:04 +0000

    release speak system show consult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_max(lst):
+        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
