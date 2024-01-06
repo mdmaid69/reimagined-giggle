0a65cfe1fde3613b5049e9504215f11bb26196bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:53:48 +0000

    file Qq Hh legal scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
