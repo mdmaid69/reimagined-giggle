69caecb0325ad9f5744edf6a12a0186f39e4a362
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:18:45 +0000

    comedown every distant available trust

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
