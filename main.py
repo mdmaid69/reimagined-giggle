36da962b59dd60f59c119841a8c03fb42597f1a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:50:21 +0000

    care woman flat study war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def count_characters(sentence):
+        return len(sentence)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
