35b5bbac55883f9208f4c43e3f04ad35b3a777bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:04:59 +0000

    outside influence sign that top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
