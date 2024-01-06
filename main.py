77c54a6fa6dd950530f7f8aaecb54bf4f8ee8555
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:43:57 +0000

    safe heart than pull talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
