4fae7c0efd51755f844b0adc346786c9ed60dbab
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:11:37 +0000

    prevent about aim breakfast computer

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
+import re
+def find_pattern(pattern, string):
         return re.findall(pattern, string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
