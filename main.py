400a1b33609ea2070f7fc0b7488d77983959bd71
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:53 +0000

    perform yours apparently before everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
