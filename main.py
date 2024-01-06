6299bc3f89ba3be5b463ff458b15d5ca9b5a6e55
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:41:49 +0000

    steal cover pollution grateful illness

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
