5573d602d03f18878ca36e82c4c5644d482ba77f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:05:20 +0000

    future accept Zz dig evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
