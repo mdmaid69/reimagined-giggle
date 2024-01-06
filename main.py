e0795b6ff3c3141145bdc7cd7dbf93dd5ef48043
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:32 +0000

    business dry Pp miss not

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
