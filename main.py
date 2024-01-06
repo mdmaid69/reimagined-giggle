daa9bf605a8a130aa38621a7886943cbd9ee200f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:48 +0000

    like cause be mistake pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
