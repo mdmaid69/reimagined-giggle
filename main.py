f9296e69d310782529bd71c218943f18126bb22a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:57:00 +0000

    mystery getup strict past type

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def square_number(x):
-        return x**2
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
