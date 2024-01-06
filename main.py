a53f9e7486c8f27576c93363412b7d7eabcf131a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:26 +0000

    suppose accordingto within nurse turnout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def count_elements(lst):
-        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
