3f66ad881a97da5997b6a208a39268d00743f5fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:23 +0000

    cut both today improve happen

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+print(sum(range(10)))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
