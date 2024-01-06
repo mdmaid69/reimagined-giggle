a4ace54e73e61689fce7eb53deba4b5450715e9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:44:42 +0000

    within hang education band along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
