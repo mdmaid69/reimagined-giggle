00efb0a9e64a90463dbc29875cbbde7c123ca71f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:56 +0000

    forget moment scale tear sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
