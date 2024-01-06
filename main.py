10fe87370014350c78fe7067f5698a5ac97d5267
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:40 +0000

    inquire imagine swim foreign rich

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
