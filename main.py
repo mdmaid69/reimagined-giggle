3a06ef74bc5950c898af5523d5e7561318cffb98
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:00 +0000

    their quick difficulty free marry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def convert_to_binary(n):
+        return bin(n)
