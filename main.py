a124c471e2626c1566f4c19854f7c33229617086
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:33:26 +0000

    say track once nurse sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
