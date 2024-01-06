317bac1de3cced882802f4e2d4c00a489004927c
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:28 +0000

    into mean same tap slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
