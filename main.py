f018fdd3aff071e3dc04b15fe35af068a52a8124
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:47:37 +0000

    dry 572enter tight day getup

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
