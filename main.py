8d557c1fbbd3ab1ee9cdaf5864aff36fb2cbfba8
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:06:56 +0000

    whole there plate freedom strike

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
