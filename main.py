dd375281cea5ed566094e249f4cd98d47a7efc09
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:31:27 +0000

    waste guide influence less continue

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
