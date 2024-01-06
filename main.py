8675d2c14b98243ea94af3d99745e216855ff3a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:27 +0000

    fight tune slice trick centre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-print([x**2 for x in range(10)])
+  def convert_to_octal(n):
+        return oct(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
