b1ab931a9522fd5ed99e812219ff6230a84af6df
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:02 +0000

    comedown promise complete getout mark

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def count_elements(lst):
+        return len(lst)
