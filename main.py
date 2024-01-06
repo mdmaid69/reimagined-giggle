4ba247761e39aa6d56cb6f793d590b9b01222501
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:25 +0000

    row late shade week eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
