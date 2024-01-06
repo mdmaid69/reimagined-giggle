e889116eea23892f94b206635d84405e6d283191
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:35:17 +0000

    relative remain edge mind determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
