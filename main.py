6d1ad03b8fdd8c5aad2cd907d50b14b100db5a89
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:31 +0000

    book sink take design visit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def is_odd(n):
+        return n % 2 != 0
