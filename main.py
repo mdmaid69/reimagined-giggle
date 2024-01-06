ca4d5c5a67aa14a2b2c831241985ad747a80b353
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:19:37 +0000

    interesting murder stiff somebody earth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
