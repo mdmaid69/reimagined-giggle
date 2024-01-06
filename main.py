8f50c66a116c422e2b3ada8645f3fcc4a7adeca2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:16:32 +0000

    improvement forever work past pen

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_volume(length, width, height):
-        return length * width * height
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
