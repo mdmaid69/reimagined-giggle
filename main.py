3b8285aa1b405a80624447251c29ef50b4e91e43
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:37:11 +0000

    pronunciation term interrupt thing situation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
