71788cbdd17e0439400aaf3f3eefc2212f2c6497
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:11 +0000

    right argue he wave speech

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def is_odd(n):
+        return n % 2 != 0
+def find_max(numbers):
+        return max(numbers)
