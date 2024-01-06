5564f9abe6efabe4568dea9436f1058c40e631a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:34:03 +0000

    press house sense bye cheque

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
