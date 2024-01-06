6055e3af48a515a97462045711262cfc652dd1f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:44 +0000

    bottom beginning workout free until

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
