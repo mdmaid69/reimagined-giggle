5a566d5b9e806c2514670d24a4b6894d8a9d53ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:28:38 +0000

    back shallow towards after may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
