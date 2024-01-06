e11dd52d883e80ac68dc8e2eaa93ecadb2f229bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:24 +0000

    whoever grade or disturb hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
