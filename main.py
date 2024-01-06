68e42d01e307f5ab781fdaca922ebe3ed2c6395e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:25 +0000

    key bar learn perfect regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
