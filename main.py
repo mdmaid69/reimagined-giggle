1f9b8708cf090ec1f75a4c2461cd9d3ac4b09452
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:58:24 +0000

    think nerve seem unfortunate rich

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-print([x**2 for x in range(10)])
+  def get_current_directory():
+        return os.getcwd()
