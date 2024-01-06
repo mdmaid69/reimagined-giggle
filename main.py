c30310fe6b727847484f6c836ea5fc2c14c2b105
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:28:03 +0000

    shut behind we date dance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
