0dac100f890350cb437f30e9b89e960ab5133449
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:28 +0000

    cross custom which last central

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
