5a50ef89d491ac08da51d406d438f2e3372011e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:02 +0000

    imagine right history swim indeed

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
