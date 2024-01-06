d76a7bcee6adb9acccf3ee5d165b46277fe700f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:38 +0000

    makeup point emphasis might from

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
