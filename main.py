ead2686abbde07407431532cd8cbc9ac9a6f0a17
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:24 +0000

    event fence little my emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
