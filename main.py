3cc5dba458e8884b08e42bf866c27cc36345cf61
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:28:59 +0000

    Dd your modern respect dear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
