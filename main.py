14b2dd4101f311985b4c091cefb8b12a1b2032c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:56:01 +0000

    birth everything about bank wrong

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_list(array):
+        return array.tolist()
