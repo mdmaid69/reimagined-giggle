4fb22cce7c4fb19a4bdce5ec8986adf72e5932ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:42:45 +0000

    bottom start Ss object bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_list(array):
+        return list(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
