badfb6571389580bab9c984c279f6ab733b3d4e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:50 +0000

    than name decide clean left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_item(array, i):
+        return array[i]
