e2545d201df2d13fbb128f6552a187bfb53aefec
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:36:56 +0000

    amount soon shallow provide getoff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
