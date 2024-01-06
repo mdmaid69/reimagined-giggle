bf010299c70fc0b8ce3ab7aefd2b0a27aee8398f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:24:34 +0000

    bored scrap settle can tidy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_list(lst):
+        return lst[::-1]
