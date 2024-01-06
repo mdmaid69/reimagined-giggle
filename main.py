97ecd8d108348175f6ed99ca5c90d50f87fa4c5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:17 +0000

    opposite old add occur 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
