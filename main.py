2a46864ec8240cf8d0d9be702961e7fd8c242690
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:10 +0000

    ability yourself part fit argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
