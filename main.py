2220ebd1d7c8445135be6ca81890006e28847068
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:51 +0000

    callout act emergency boring cheap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_min(numbers):
-        return min(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
