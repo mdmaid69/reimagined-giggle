dfcd868a5091167229534536a56bf5e9a595126f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:58 +0000

    what attention mad hour steer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_typecode(array):
+        return array.typecode
+import re
+print(re.match("h.*o", "hello world"))
