b4bfe100b86f7c514e1c7dac7b02a2f45a05fff9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:49 +0000

    edge runaway yes mass turnaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
