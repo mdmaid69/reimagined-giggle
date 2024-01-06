cbeec7568720b28f507c8c92505ddfae72043c84
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:08 +0000

    blind production empty beside edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_floor(x):
+        return math.floor(x)
