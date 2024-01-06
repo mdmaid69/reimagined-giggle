174054356faa5aeee1bbe329928c4aba0214aac4
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:02:04 +0000

    live steep sing explanation share

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import random
-print(random.randint(0, 100))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
