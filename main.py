3ef9b9554e4d4295ada4f0dc7738663814839af2
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:56:44 +0000

    neither secret tidy sea skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
