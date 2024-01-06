32f3c6bfa4f5c6bb26e9c62e5c1e191723742b78
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:32 +0000

    shelter clean polite scheme fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def get_array_as_bytes(array):
+        return bytes(array)
