02da37268599460da4748b9417d053a3edce2466
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:42:34 +0000

    goaway start treat hit cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
