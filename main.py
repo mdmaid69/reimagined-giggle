845bd6055afd727375c26ce1840a735ac85e8ea9
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:24 +0000

    wise you use material ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
