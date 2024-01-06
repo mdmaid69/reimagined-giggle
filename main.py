9c558ddb5a2b9f37159c8e5543519a7b78fab9f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:04:10 +0000

    busy pleased main about pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
