bf0d3d020c70aa82f71b64e27d735a319cad43ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:21:55 +0000

    age dear lock mass throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_item(array, i):
+        return array[i]
