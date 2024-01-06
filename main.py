78319a572a1f7aaa37bfbe056af5dd9083dd2f55
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:37 +0000

    group theirs unkind easy hour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_repr(array):
+        return repr(array)
