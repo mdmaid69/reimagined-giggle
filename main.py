27d428ad607d30972020a9b64ed7c0f4327c8df6
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:28 +0000

    couple down doctor area on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
