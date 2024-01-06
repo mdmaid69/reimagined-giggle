3b822e390f09fb821d6e39e6614bb4c9ab235cb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:15:08 +0000

    obtain firm unlucky complaint slice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def count_elements(lst):
+        return len(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
