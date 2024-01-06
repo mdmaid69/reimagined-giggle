acb886853ea14b51fe76b9731ba574bf37e66a1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:11 +0000

    curious issue day violence round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
