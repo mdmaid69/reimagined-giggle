edf83699cc0257458985711303e6edd43e4e5b5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:37 +0000

    adjective dress child year neck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_set(array):
+        return set(array)
