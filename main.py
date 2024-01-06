6441329b4e19fa0451ba7a502bbfd765d28d6e0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:48:53 +0000

    sea period should return accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
