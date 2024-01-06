9ed1ed9ab3de80c851468cf49b957d2ee133ad28
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:38:13 +0000

    greeting dream invite direct connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+print(os.getcwd())
+import os
+def change_working_directory(path):
+        os.chdir(path)
