8cc38e8dfe3b63e60f5414faeba88ee17334df8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:24 +0000

    exact giveup belong notice because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
