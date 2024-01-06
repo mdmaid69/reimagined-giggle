c6b0d859b89dce32249afbd14ca70756a35bb6e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:09:17 +0000

    side later unable breakin free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_octal(n):
+        return oct(n)
