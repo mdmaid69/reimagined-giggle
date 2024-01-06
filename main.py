64c54f6aabeac6594fae3b4c9fc1dd98f7ba49d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:41 +0000

    dangerous coat Bb face and

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
