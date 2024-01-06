0c4a3ec2eef37203b2d89cbe07675bcd181620ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:19 +0000

    address negative occasional which crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_str(array):
+        return str(array)
