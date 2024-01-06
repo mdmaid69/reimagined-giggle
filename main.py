13d56ca77b7efdc38d51dbe2a8c33938036c0cef
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:01:05 +0000

    know persuade story embarrassing bury

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
