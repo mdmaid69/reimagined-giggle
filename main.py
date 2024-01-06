7d016c898fea0614fcb1074e35862cb38eb87493
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:22:03 +0000

    process blow respect Dd relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
