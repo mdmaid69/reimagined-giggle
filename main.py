06ba1d79ced71c8299b584265122989f2892a27f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:38:34 +0000

    firm unable environment comfort direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
