d9d98396de8f6cb1d47850495bbb6f0cfc25e79d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:58:20 +0000

    fresh letter method everywhere gooff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
