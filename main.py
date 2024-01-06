7e144af88f294e30e3b235e1a34173bbe569b10b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:42 +0000

    565end kill exhibition thing object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
