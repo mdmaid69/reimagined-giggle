54a343b64250c1130bc69113b48fb8b28e242d4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:47:39 +0000

    judge Ss intend finger trick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
