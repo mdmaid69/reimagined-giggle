d27d23a61465113ed707510e6966e83c86db579b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:43:05 +0000

    arrangement turnover responsibility development earn

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import os
+def remove_directory(path):
+        os.rmdir(path)
