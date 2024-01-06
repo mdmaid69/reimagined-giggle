b9102376589a15672c772241b7d0d986fc1a286b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:51:26 +0000

    Bb giveaway mind distance disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
