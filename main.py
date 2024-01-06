203318c7ff6a2d88bb56b9f16365ad5646b5de0a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:23 +0000

    once compare turnback because music

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
