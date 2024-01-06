c63f9fade78fb94ef8967519406135050f2e1894
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:25:45 +0000

    dress where nowhere silent steam

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_union(list1, list2):
+        return set(list1) | set(list2)
