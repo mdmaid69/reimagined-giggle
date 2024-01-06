5383354f25519bd19c400e918b67334256332031
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:08 +0000

    far jump private work practise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_user_list():
-        return collections.UserList()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def reverse_array(array):
+        array.reverse()
