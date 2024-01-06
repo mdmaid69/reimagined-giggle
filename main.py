ec17a35edb4bf68ba6a8b99415d19413a2ca93c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:58:38 +0000

    splash obtain success open speed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
