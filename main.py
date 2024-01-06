dd10bec7b998997768c54b62e94909e12f23a493
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:00:02 +0000

    rather tape imagine strange slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
