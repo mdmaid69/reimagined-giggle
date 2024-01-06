23eb929c89d7cbbbfee1bc11e26bd6e6d31948dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:38 +0000

    official runinto same easy date

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
