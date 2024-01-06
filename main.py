95f0c6799b2e2f5bab266ce0db4cf978be839392
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:37:10 +0000

    cool keepoff as with die

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
