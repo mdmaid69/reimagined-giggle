a3e8f8ed61969260af98d812a826379716636064
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:02 +0000

    runinto old control nose cost

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
