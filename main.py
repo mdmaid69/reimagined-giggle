6bd815b4002996af5113e8b4dfad7c25a173dc47
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:00:10 +0000

    surprise estimate obtain grateful surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def reverse_string(s):
+        return s[::-1]
