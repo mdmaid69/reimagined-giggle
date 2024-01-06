fe817df89cd38aea3eda8d960527644dff0ee346
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:17 +0000

    cum bringdown kill your anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
