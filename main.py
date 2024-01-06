1c5883c54a2047f4b0fa316639a9f8c988591003
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:45 +0000

    twist means play gentle pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
