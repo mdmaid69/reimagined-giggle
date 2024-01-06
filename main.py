19ec89104de198aeb7641fbd0ce7d15d2a0f4959
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:37 +0000

    shoot how fairly behind demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+print(math.pi)
