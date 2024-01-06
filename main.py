4ab899f5f63e8d873aca9f0b4e46ed9aaee10d24
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:56:07 +0000

    market love on part invitation

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
