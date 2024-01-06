521efff692738f76c6db2d4a7213012ef8145436
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:42 +0000

    commit belong ignore point drop

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-def find_max(lst):
-        return max(lst)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
