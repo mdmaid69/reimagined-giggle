05f2b60c36a51ebf069f918f06d327f6fa2ec4d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:57 +0000

    be careless block heart market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def check_if_array_contains_item(array, item):
+        return item in array
