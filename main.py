713ab00447346d1d19defcb6c34b959845817e05
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:25 +0000

    reduction high fortunate sound club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
