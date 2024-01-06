3cafc068b00fd646e6372c19de86dc4d04125014
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:41 +0000

    earth way room trial authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
