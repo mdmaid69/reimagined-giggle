3a7bbfd31c88b71a1a29e9c026f4565371975ea1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:47 +0000

    towards fit break step drawing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def divide_numbers(x, y):
+        return x / y
