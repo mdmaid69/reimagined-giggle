32ec36a489a5b1f04fb2926c94442ead1cf062d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:04 +0000

    Cc turnon fit fast this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
