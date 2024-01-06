38c90e9ccbe0fe7660c85ba6f88747a61e1cebbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:36 +0000

    pause clean scream delicate past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
