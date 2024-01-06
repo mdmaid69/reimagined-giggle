5b87665c39c33cf78b7be4e186643b2bcbd24a8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:34 +0000

    walk recent black soak tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
