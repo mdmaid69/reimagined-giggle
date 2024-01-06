f59f9a73332ab374fde432a01851fb959d06e643
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:55 +0000

    dish influence home soil shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
