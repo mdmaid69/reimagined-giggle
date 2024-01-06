0fcb7021f8c458683ecc83f55f9afb4181bfde52
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:51 +0000

    generous woman address spite it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_unique_words(sentence):
+        return set(sentence.split())
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_floor(x):
+        return math.floor(x)
