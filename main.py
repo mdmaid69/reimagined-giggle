57a8137a78560212431fa93a1d7a4d7c428f1b6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:26:20 +0000

    steam change contain board raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
