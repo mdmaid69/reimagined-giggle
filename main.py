2dd8e7241862187f813cbad953ce1b023755006c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:51 +0000

    be suggest heavy heavy basis

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
