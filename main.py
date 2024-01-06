b3d96e6928165524e78210cb570559d279277201
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:17:12 +0000

    own Cc change content always

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import sys
+def exit_program():
+        sys.exit()
