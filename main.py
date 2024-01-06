55be84081e3420f87aa1d599fe5bbf03f7067654
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:22:01 +0000

    offer house handle especially against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
