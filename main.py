79e029f9b7182f0f3b2f3ec7a65d90d3e2845220
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:06 +0000

    seat rob precise nerve room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
