de2b2e76154db0bbed18ad8ac0e74d2fe69e6546
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:56:21 +0000

    but actually can visit conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
