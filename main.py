480de0c8f077a2c5094eee940d96a4127178cc4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:10:56 +0000

    public door pupil visit transport

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
