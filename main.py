e9a8ba69a1391d8b44a6786325eea055cd42d06b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:08:08 +0000

    Cc feel day task press

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
