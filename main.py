9e6ac0ad1f90ceeabd17d28aa7df8bbc34e4d606
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:53:05 +0000

    well forget tidy formal cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
