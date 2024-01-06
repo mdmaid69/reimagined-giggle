7cd37ac8906161eea08148d190906c9f39cc31a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:53:08 +0000

    gradually method Gg phone 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def add_numbers(x, y):
+        return x + y
