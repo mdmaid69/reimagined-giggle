6bdc6ae15286f7445ca340e35e7b5323bced3b67
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:11:28 +0000

    extra empty favourite style disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
