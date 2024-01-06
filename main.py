d0e82b0d076066bc48cf0992f2896457a4c5fc34
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:47 +0000

    hot strength unable problem movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
