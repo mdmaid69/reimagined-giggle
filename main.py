0d41b8edf525223c9df7791eb190aeb0207b5ce0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:43:51 +0000

    realize score long capital every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
