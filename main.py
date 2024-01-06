bc9fda01127065da20b9e4ab04b4c01caf606316
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:35:17 +0000

    die cheap middle arrive themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
