8ac64d454e7c7d3681e6e3d63a90ea2105c273ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:53:30 +0000

    bound breakfast let squash practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
