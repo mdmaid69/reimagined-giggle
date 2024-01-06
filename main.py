9cdd99113ba10ed274909de35a1582abcf5d7995
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:28:33 +0000

    pull certain habit rubbish cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def square_number(x):
+        return x**2
