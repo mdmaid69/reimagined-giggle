dcf74956d6b68e1fea3de361b7d399996dc01e58
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:03 +0000

    dress tool benefit opponent cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_array_length(array):
-        return len(array)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
