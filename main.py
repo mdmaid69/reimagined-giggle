1650825d23c9fd5389217d11660c01cfd8d663f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:33:35 +0000

    challenge popular mother frame beyond

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def reverse_array(array):
+        array.reverse()
