13042ec42c929a360f5ca8207471fe4140e8d153
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:36 +0000

    sell possession in special worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
