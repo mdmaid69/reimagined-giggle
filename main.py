81b37dea46072bd507f8faee2f807fcec0480518
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:25 +0000

    Qq Dd section surface failure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
