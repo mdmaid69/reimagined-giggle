3151787c2f948d8360c7d0474d7cee9f913c6038
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:03:18 +0000

    over taste wave comment less

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
