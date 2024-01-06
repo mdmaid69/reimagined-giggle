f3a421c4477fc86dd635ad31f337aeaf49932f9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:59:19 +0000

    active risk criticize secret valuable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
