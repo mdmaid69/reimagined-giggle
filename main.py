52edc7ae5a7fecc74b7a2700320fec249f52434d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:30 +0000

    estimate much dig bus illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_floor(x):
+        return math.floor(x)
