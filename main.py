8b84290dfca64e02e1dba932ff2bc8f75a8c2a8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:23 +0000

    several congratulate Gg feed pity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
