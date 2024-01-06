b814abdf86240c15dfc7a8304c44f94999e2b15b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:42 +0000

    properly smell drawing present regular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
