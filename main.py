384cd4d6eb32d95acd835ae046b87fac4b2108da
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:24:59 +0000

    another basic circumstance except accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
