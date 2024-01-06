466f97aee12310ae6c3bde13fe48280d51d3f6f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:04 +0000

    way platform juice by question

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import platform
+def get_python_version():
+        return platform.python_version()
