cabdd928a121d3a17ef3e3b38e06fbff19cbe595
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:41:50 +0000

    him deliberately pour impressed morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
