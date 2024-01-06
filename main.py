348e98699a5451a12c937b837b196b3e07ac4bd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:38 +0000

    pile wish system brief surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
