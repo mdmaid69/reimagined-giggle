abf6e32af2c51497f4feb2ab9db537f42d8700b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:08:57 +0000

    read murder score thinkover come

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
