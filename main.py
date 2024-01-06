663f950ca075dbf20d5d26d1f9b68aea5cade50f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:32:58 +0000

    convincing something which cruel pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_queue():
+        return collections.deque()
