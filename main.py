7b0203296169b5ef78a85316f6ca123ba0360c1d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:14:32 +0000

    across and pupil relation pay

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def convert_to_octal(n):
+        return oct(n)
+def is_odd(n):
+        return n % 2 != 0
