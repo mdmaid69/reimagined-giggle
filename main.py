009a56d97a5f6aa706d8a723e59b7d0d21f8def3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:44:57 +0000

    through principle doctor there mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
