548bf06c4426ac4560704753ed2a378281e22413
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:38:00 +0000

    smile ourselves worth argue failure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
