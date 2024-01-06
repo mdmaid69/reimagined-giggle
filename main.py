61ce8e73933105ccd0d853206c638f5837ec55ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:55:27 +0000

    rate shadow shop available the

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
