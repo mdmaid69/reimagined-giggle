1ef3b7aae55798009d9b2edc2701e9fd91c7a06d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:37 +0000

    doubt report capital long trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
