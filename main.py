ca446b40c74d59a75fa569851eb6ade9f3217b0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:55:43 +0000

    home contents runaway embarrassed that

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_cosine(x):
+        return math.cos(x)
