ce5c8467ee703d403460d89f77113139e9edd553
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:02:50 +0000

    design paper die attempt here

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
