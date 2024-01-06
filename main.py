80886ef074b3b3106707e72c482a3778c5a413b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:10 +0000

    consider mail small travel allow

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
