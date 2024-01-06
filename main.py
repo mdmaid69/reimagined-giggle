a25c47fdd4cb1dcab530b7ba494005b5d2a6ef33
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:00:06 +0000

    rest general rate build answer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_power(work, time):
+        return work / time
+def find_max(numbers):
+        return max(numbers)
