38c0707424585fbefa6eb23a0508099550a023da
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:08:38 +0000

    start voice shout cum finish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_power(work, time):
+        return work / time
