be19abf84aefda59b10d786d045f5cd2066d9697
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:12 +0000

    567enemy risk heat speech affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
