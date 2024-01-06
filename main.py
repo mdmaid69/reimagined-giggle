fa23e0b94e1e96c204c5e63e212674a1a5e67b73
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:37:12 +0000

    alive tool help rest poison

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
