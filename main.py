bd527f340744aec33d9e30169bf11fd635b2d60d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:01:22 +0000

    value short accommodation such drop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
