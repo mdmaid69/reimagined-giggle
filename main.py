620bd87148c04b0bf5371243cbbf5d10ee5d40bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:30:42 +0000

    mind vision public rarely pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
