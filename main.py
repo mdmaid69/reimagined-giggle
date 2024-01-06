702aa51cab0d6690e4e7d4869142e31e9a879a28
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:53 +0000

    callup right machine goout pleasant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
