44bb09f29b6166e6224ccbea1de9e429a2021413
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:08:56 +0000

    help whom consider believe congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
