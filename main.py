721165c276b5410a53ed5dbcc71a48aab7217096
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:19 +0000

    where teach arrangement record pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
