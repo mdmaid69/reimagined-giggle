77ec20a556101a28ef3039e435c929c6dcd16a06
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:32 +0000

    occur ignore idea car crowd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
