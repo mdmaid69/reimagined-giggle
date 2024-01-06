840e2f1204f13d02be0c77190cc3214d3796154e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:06:28 +0000

    scheme simple help bread press

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import datetime
+def get_current_date():
+        return datetime.date.today()
