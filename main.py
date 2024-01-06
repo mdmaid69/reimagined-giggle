afc26fd99b29a13b7e76dc786cb90473af8e51dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:33:24 +0000

    ring spill example stir crowd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
