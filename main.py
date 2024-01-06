e506eddbd847c329312db18b0709c0d10fde630c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:37:20 +0000

    dance view workon exhibition pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
