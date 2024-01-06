8731dba529b1040bcfc9672d036ca2b2de0cfc8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:18 +0000

    lost bite yes peaceful forgive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_list_from_array(array):
+        return array.tolist()
