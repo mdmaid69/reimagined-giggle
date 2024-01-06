e988f5389f09bc330afa46fed757dc1e60bc6d6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:53 +0000

    entrance noone ground different fence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
