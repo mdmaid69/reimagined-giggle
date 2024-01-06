d63b72243cfa88a7244272efbdad7890fdbd0026
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:43:04 +0000

    coffee Oo cry quiet form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
