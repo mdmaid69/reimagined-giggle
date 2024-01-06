4a9e1fbbd007cf44377ab8b174747552aa93c6ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:09:16 +0000

    thing murder ocean comfortable band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
