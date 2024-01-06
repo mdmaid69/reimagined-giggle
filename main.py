0addfcc805a72c5553c67d18828f9a762183d35f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:21:01 +0000

    smooth exploration file second pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
