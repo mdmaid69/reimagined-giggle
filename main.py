635e88fe0baa7ddce992c39e38bedf36091826f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:38:36 +0000

    ready coffee post generally morning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_power(work, time):
+        return work / time
