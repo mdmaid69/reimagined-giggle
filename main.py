66b4d9319f258d19870ab2b8d59e76f3f330cf8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:29:08 +0000

    accordingto breakfast think correction really

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+print("Hello, world!")
