f13daf79c077d599a5539441441cb8d5e2e79dbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:11:30 +0000

    doctor show house guess low

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
