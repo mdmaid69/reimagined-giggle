59410d01c9e8904335cd385af15d0a8622934112
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:14 +0000

    object powerful mix confident by

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import time
-print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
