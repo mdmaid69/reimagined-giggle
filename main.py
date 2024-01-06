5365ff144bde9c6b3333147d87b8f1d65c3ec5cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:24 +0000

    movement snow distance shine card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
