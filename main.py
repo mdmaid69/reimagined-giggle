2f9f1836a4beb7545e923670a939366c5734bcee
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:15:03 +0000

    attempt lock offence guard force

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import sys
-  def get_python_version():
-        return sys.version
+import numpy as np
+print(np.array([1, 2, 3]))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
