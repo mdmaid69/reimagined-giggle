15018f11bc2de3b1ae9ccf9f1ed508e8a2f0b455
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:43:03 +0000

    move make comment male afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def square_number(x):
+        return x**2
