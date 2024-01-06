aa448d569def458a208f2c9a6c05e770e4963d51
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:13:45 +0000

    favour teach sell keen in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
