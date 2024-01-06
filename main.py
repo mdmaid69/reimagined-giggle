6b0daafed28aafa55cd0f2404b685ec6e5753cd1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:24:26 +0000

    pleasure difference father confused lookaround

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
