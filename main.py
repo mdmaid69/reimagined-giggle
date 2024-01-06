7153f517c296feac84f1fa84bddbd1886d47782b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:47 +0000

    hit still welcome for remark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import random
+print(random.randint(0, 100))
