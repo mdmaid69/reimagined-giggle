9214c45f651369c161210b56183d94078fd81ff5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:32 +0000

    sense enthusiastic we matter comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
