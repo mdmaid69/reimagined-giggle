14cb63b6276d7ca16c9722aa14bbd5230c5bc2fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:50:46 +0000

    active impatient our distance find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_max(lst):
+        return max(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
