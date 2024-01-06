e877511f3dbdf80ce0d071cbe7aa2fb966f85e18
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:49 +0000

    wish achieve grateful paper quantity

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
