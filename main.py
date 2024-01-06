65f0f66999cbbd8d38abe7022233678438ada976
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:14:28 +0000

    polite tune dinner charge personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
