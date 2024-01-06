f5eb7776e3936d2fbaae834b8bfd74d51b265c25
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:32 +0000

    food sort getback maximum plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
