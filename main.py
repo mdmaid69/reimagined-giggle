67bc16c993182d245f5384d6b080fb92143f88ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:44 +0000

    considering innocent goodbye past refuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
