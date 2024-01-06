d9e80c4bee864d201c4bf7a9bb5634df1a2516c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:52 +0000

    otherwise by thorough make determined

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def cube_number(x):
+        return x**3
