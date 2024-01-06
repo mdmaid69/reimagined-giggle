039a66bef920e57a60ee9decca2ce4549db64e17
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:08 +0000

    broad jump interested go negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
