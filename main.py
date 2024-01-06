681f167bd34fd5a2e910db0f7bb38b4d9ba92262
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:25:07 +0000

    suddenly either so run difficult

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
