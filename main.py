000e3ee56850df8425635d10b6fc93771c27f2f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:09:20 +0000

    matter instruction busy open everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
