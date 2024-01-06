59bfd51356c10a7108bd582b700decaa945c0111
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:29:55 +0000

    short explanation escape chance stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_item(array, i):
+        return array[i]
