dee192edbb0a3eb661a675189baf63aabc24018a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:26:25 +0000

    tear afford down challenge swim

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
