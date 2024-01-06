9d66b0f087471ad3b4925767638ae8e51de575ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:03 +0000

    example round top remember goodbye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
