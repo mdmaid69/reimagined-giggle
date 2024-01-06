1de9ea5e1f4945673c1b010c0676afd7e882bb6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:08:54 +0000

    take well false meanwhile dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def square_number(x):
+        return x**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
