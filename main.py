425b5f4fa536a9b04e425e9287c640c34ae3520d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:15:35 +0000

    consist order extraordinary callat whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
