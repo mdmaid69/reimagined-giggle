0b3d1a7311f42368de8a2644635d838f3dfad908
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:51:19 +0000

    personal force than offer repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
