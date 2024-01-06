6733a5f3b2d1634d9d0efd84c0244913fd4140c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:24:11 +0000

    who might exact maybe medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
