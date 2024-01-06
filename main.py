a6c721c1086cdc75621f3c2b2fed77e72e54b295
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:13 +0000

    view door mind event improve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_power(work, time):
+        return work / time
