41037424cdf8c40e394267e15879de8acd9714c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:22:30 +0000

    round favour border relative likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
