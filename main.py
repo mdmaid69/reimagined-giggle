c662e1a661d9c643c5824e3dc08097b24bd0ec26
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:56:14 +0000

    breakthrough scale opposite range acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
