cb2f1c9f024b3ed63dd77c69e30545b3489a3db9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:55:29 +0000

    trouble settle recommend seriously hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_str(array):
+        return str(array)
