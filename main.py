d88a1381fe945ad8a7b241c9bfa0b867566484b3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:38:48 +0000

    load unite tidy arrange suppose

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
