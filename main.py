3ae7c1ff007879d5a1fad35cd83f9340ab16e52e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:35:24 +0000

    press fit characteristic land present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
