4e314543a1b355897d4073428ff7ac2567b14931
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:06 +0000

    favourite waste general discover rather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_current_date():
+        return datetime.date.today()
