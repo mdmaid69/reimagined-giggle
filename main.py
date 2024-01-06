cb07f2fb0e1adaaabd6afef569c15fbdbd2900e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:16:41 +0000

    medium group afterwards in comedown

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+for i in range(10): print(i)
+def convert_to_octal(n):
+        return oct(n)
