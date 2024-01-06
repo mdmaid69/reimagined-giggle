8ee9ba5ac44c252e2d066fb7aa57a2628614c8fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:57:59 +0000

    attempt impatient callup bad ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
