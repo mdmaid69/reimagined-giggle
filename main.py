b6b995c07b66a749a9ead38ffc98ae09db2e469c
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:48 +0000

    touch organize mainly be wash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
