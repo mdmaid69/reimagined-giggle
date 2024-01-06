35cebb21f5fa6c6b9f541121b61b62d932efc802
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:40:48 +0000

    imagine rush choose dead pass

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
