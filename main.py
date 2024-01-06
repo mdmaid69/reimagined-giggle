5cff9b34f672d6fb95e725005332484509843ae4
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:08:47 +0000

    please film niggaacademy market describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
