d8d51c304377d75c0e47bcc3b2b9185388cbab22
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:23:25 +0000

    strange avoid cause Uu continue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
