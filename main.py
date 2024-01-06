a1c7f343ef7b8507cb2d5891de9254799ea39e1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:54:26 +0000

    twist break anywayanyhow connected sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
