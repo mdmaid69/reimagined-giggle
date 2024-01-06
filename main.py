64b6ab68c09f88862ae9be918b623a4ab4964055
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:00 +0000

    study among people building team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
