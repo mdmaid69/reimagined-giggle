a02eddedaca30698e898ef65bf46f9df3dbb5aee
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:17 +0000

    head moon fun hold marry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+print("Hello, world!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
