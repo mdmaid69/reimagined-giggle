415c23738b50748ed84e4308324528f80ac96391
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:27:00 +0000

    kick fat attach up mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
