d87ff469aa3cf8577ef6bcdb489fa3a113bed0b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:10:39 +0000

    thorough relation sign strange friendly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
