26dfdf718e8e844a94459870053df2babbea2ee4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:24:43 +0000

    breakdown object recovery reduce the

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
