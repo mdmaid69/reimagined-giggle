7d5713bcb2eb892c53215fc3d8c6da9ab1b1131c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:07:41 +0000

    brush bread speech tool quick

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
