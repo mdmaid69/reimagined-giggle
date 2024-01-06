4193bdf814634220de59c61e83fc25e040906f1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:04:32 +0000

    meanwhile kill under may deep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
