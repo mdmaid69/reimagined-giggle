523d02d39d26069a27e6187206ea4992bc4a493c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:54:52 +0000

    top welcome place giveaway cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
