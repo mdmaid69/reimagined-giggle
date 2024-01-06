b14ed763370ce5f94957b516f9fc7140147382f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:48 +0000

    odd name surprising reserve that

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
