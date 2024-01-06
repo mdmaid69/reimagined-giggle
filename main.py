e14391304adc48f4e628ac25856fa5ca572bb9e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:19:42 +0000

    play shoulder drive concern cover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
