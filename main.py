a89b23df1fbce36503c509e9b77e15d201cfd14e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:05:35 +0000

    nose soil sorry inform practical

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def find_max(numbers):
-        return max(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
