2e4633b45365123c734fb7543080392cd18b1f09
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:00 +0000

    city ignore drawing temper sweet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def sort_numbers(numbers):
+        return sorted(numbers)
