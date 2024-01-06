056bb0825e607d36103d4f860212b8522896912b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:20:48 +0000

    please pool else bring safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
