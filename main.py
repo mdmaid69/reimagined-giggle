5ebb55426a4b40aba5c12c332907df2a83e586f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:39 +0000

    large ours time scrap gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
