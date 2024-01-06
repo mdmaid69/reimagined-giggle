26357f0a35f6dc0cda2dc5fad8c06a624c20fc96
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:03:56 +0000

    attach size lay 564encourage show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
