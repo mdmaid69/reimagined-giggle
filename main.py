9dcf737676b02e0a7d1d1b586fdef51edd4b2fe0
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:51 +0000

    564encourage calloff emergency exaggerate front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
