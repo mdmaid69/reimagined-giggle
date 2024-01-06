a8c20ba33b0d4ffd3a940a83e170673c0cd4ca94
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:24:47 +0000

    satisfactory thought cap begin degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
