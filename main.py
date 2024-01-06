6033c5c00964a5e1e653df6c3d036ac7d71fe5d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:21 +0000

    makeinto pleasant worth air shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def convert_to_hex(n):
+        return hex(n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
