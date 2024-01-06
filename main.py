7a0677e381373c38638cf9c7c50ece8c4b967084
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:34:15 +0000

    estimate bottom pronunciation pressure his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
