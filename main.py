23165dc2daa1bf81b480c6f47ebb06bbdeef835d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:37:27 +0000

    cook walk knock together quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
