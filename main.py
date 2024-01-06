80b1f4e15a50c95bb0cfb48c77475644d85298ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:06:26 +0000

    urgent order shock without spring

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def convert_to_hex(n):
+        return hex(n)
