1a94474e5364feef932e1771a7549e9ea1f80c05
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:27:44 +0000

    up opposite bus save demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
