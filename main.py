363e6873521b04f23411f3a23c36c1497610b17c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:09:55 +0000

    discuss switch bring all accommodation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_array_length(array):
+        return len(array)
