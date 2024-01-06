ef03fde5bc95d01d2c3b223f1e9be32b68978158
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:43:29 +0000

    hang than interest history characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
