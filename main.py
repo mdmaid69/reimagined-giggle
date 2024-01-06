809727dab3f950f80b246cb7c6f0e028082e1293
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:46 +0000

    ashamed sure about deserve proud

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+  def reverse_list(lst):
+        return lst[::-1]
+def count_elements(lst):
+        return len(lst)
