7a08e51a00737ae6f08b47192a79bb19fadba02f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:19:38 +0000

    fence top fine justice recent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
