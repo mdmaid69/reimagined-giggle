a5cb19cd5194f6ef552ca5ad1d815b8554b0472b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:01:48 +0000

    bit opposite information to accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
