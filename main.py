8127fa9010ca1e2ddc042e3881c55ed258eaefb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:42 +0000

    trouble slightly traffic burst comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
