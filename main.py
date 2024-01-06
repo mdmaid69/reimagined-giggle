e2d210b0f46a74e6f0a7b18649e9c765415aadb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:50:07 +0000

    moral system interested show funny

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
