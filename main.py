c1f6ccb70c17bb0b39b279036062f7ee23831c26
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:55:14 +0000

    trap draw concentrate least have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
