ef5bd620bb05443727d826c9ec7607ebc34dfb49
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:06 +0000

    disgusting manner long warm class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
