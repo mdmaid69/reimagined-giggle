a85300c16f04d12bd528cadaed862782abe8a624
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:51:27 +0000

    add ball favourite tap project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_float(array):
+        return float(array[0])
