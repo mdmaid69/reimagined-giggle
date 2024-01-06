717242bae7c346b4afe2c1c97892873dd9a8509c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:53 +0000

    save activity idea message capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
