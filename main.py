189e6894fcab6b19cbc1f256ae2870cae5356015
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:08:20 +0000

    steam stretch indeed agree major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
