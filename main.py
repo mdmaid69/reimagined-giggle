46f08d865b9ca3d57a4ad0087ffd79d5762d0d2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:50 +0000

    fetch this somewhere suit name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import os
+print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
