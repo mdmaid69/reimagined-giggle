11ab6fb6e435d7a701874bcbc61d29f8b7cd6fb4
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:30 +0000

    lay less brush somehow unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
