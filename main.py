b8298216618ca3fc9d3ead4404f0362808f504f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:59 +0000

    impossible extraordinary produce Vv sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
