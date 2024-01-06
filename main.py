b7223fe1597a0b82842b522f1fe5814a9cfc1644
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:17 +0000

    cold separate satisfied unless cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
