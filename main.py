0f6937422c2f721ff2c74ca0c541acf335c0c407
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:13:43 +0000

    reason permission urgent wait tend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
