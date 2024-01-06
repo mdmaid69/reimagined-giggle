248ebae4ab007a7e0a453396cfcee80bf69f18b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:40 +0000

    suit rate now close right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
