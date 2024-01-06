2a669c0d119571c604b36f11df25fe968b347eae
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:18 +0000

    correct trust what pretty hate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import collections
+def create_queue():
+        return collections.deque()
