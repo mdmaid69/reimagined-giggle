542c10268955188fd4841fb45e084575aa5ada91
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:30:01 +0000

    violent runout common interfere family

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def print_python_version():
+        print(sys.version)
