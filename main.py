2dacc89c49c640aed24d3944dec6a0a9abb1a20a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:25:28 +0000

    dear they problem possible mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_max(numbers):
+        return max(numbers)
