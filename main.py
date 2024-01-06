fe4e1af3deea0c91467dffbb42f2277eef4a11fc
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:02:24 +0000

    ahead turnon by spring warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
