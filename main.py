93171a352a0d78d079a4ba2b04e627e13df3a04c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:15 +0000

    protest movement star smoke please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
