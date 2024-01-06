5aa39c83f6f7adc65e80471d5569ed4545362ef4
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:59 +0000

    description special teach order goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
