9fdc46c3049cdc8295d9198365bfda155b061979
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:44 +0000

    rate talk fight minute upstairs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def square_number(x):
+        return x**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
