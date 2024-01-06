9b5e73c38b3d5c37aaaf17ba5f78cc24ba08f6bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:19:28 +0000

    turnon dear convincing declaration damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
