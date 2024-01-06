cbecc44dbc8e433b251114a3da95c34a74f280c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:40:08 +0000

    course flow ocean ask pollution

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_max(numbers):
+        return max(numbers)
