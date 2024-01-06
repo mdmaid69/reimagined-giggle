d0ac225c6dde9e609e0d5664de80f5995144c163
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:06 +0000

    war too getaway 571enough right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
