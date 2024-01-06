40fa82c1235a8dffe7288c7949742588c1afac04
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:15:16 +0000

    behave whole false among correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def create_counter():
+        return collections.Counter()
