725ce0816aa54db22eb6ae0ad043aa34f4b2696a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:43:30 +0000

    brother talkinto beautiful search challenge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_hex(n):
+        return hex(n)
