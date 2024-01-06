feeac3900ca086531958170e6fd1be5ad40cd48f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:54:44 +0000

    satisfactory programme sweep dance programme

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def convert_to_hex(n):
-        return hex(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
