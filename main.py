efaeff99c528e90548ee519cb03c017972721289
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:40:41 +0000

    unless pause lean his direct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def square_number(x):
+        return x**2
