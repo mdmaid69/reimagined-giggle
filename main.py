e71dd0a2eb7e42947a879bc9751573d6f4c48cb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:26 +0000

    least trust extraordinary load although

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
