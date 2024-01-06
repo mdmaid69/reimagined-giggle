5636437d13b3a24c752a49afdf1a19a881f9fa59
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:57:26 +0000

    calmly tradition thinkthrough pile mood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
