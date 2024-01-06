d31abbd320cb23043eeae19f3b10deae903d6813
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:08:07 +0000

    reject divide information guess Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
