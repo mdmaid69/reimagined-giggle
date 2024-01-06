a4bb9139a72cd21eaccc585c611165cf21543990
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:00 +0000

    rush sort survive half minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def square_number(x):
+        return x**2
