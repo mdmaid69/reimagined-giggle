2e1da0e00d8e2124b0d1e34aa0e2df23f51c471d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:00:16 +0000

    declare later ourselves embarrassed state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def count_characters(sentence):
-        return len(sentence)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
