c238099427813e46149d184069965896c6ecbc88
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:59 +0000

    guess bite share soft possibly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
