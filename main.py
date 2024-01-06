ef58449302bb20624f8ea4ac2e8c3161943dadae
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:08 +0000

    grade brush typical pipe thorough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
