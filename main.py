dfb2c3f2e9eff66179c8a7b37455a7312d6dc9b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:17:01 +0000

    apply squash Dd use contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
