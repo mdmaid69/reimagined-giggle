f1f991ec3c8be55d7b59e9b785ecfbcf344a64fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:42:32 +0000

    beginning identify never Gg reduction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def remove_duplicates(lst):
+        return list(set(lst))
