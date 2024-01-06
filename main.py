c0f54211213a167fef93174484df12a91bb13f34
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:59:24 +0000

    quarter shine content lunch fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
