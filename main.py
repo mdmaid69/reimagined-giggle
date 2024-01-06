6fd4bd1610f466ddec68b8a60abe4f848df61b65
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:12:23 +0000

    soil variety breakthrough strict able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
