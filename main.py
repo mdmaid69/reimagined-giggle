55a73926d12c73d6570d569f3ac749a62f2a2283
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:15 +0000

    niggaacademy year rescue really person

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
