f6dbf84b3b5a9c0584d1cc5625148fa17ec7334e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:09 +0000

    spread address wide noise rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_area_circle(r):
+        return 3.14 * r**2
