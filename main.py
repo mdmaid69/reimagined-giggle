c07a9ffa572ad3b45f4e5038380d915b7de3f171
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:18 +0000

    black teach trick never ordinary

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
