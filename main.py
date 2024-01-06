eb7a1e60197049971416c0f9fd3a4fc639fdcf19
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:19:55 +0000

    increase love father hit demand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-print(re.match("h.*o", "hello world"))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
