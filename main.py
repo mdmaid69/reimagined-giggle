240deff84055747cd965573055a1dcb25e9f0598
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:57:06 +0000

    other birth follow behind situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
