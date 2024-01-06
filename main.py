a9aee9b0110f8a3a04ef82ca68131bf0c8e92262
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:43 +0000

    sale fond central hers ability

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
