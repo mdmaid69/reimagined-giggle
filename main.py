a40b72d95a8b67088eb60c98d85b1e925f65179a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:03 +0000

    bridge embarrassed possession matter shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
