3b6a71cf46ecab955c480fb904c5f3521a9a8001
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:32:38 +0000

    switch suppose somewhere uncertain hear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
