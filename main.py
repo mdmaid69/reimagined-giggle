bb8462b2c14fbc5408ee5a5f861587e2f45765f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:32:01 +0000

    free turnaway someone unfair 179bite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
