395883fed1400226cd753a3bc0e804c641c5bf88
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:00:09 +0000

    confident centre public risk future

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
