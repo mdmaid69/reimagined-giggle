d5650be072919d74f0d286fb8b7aa982efc4297b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:49 +0000

    separate blame neither form decide

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
