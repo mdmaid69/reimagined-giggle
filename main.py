f7e63396e2f543a8d713df0b97a0640f3f9b8822
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:35 +0000

    Rr broken bunch stay thought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
