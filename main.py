2de49b3e9950c46b4a2ce11364bf78edf4b36f39
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:12:46 +0000

    trap me leave pride space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
