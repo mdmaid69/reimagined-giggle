84864df75c634cb962d0251ae8d0a19789dec52a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:30:48 +0000

    undo muscle bottom greeting declare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sys
-print(sys.version)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
