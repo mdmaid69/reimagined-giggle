bf33fba11d14c9ae552b672da4f0d708a73c8263
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:14:14 +0000

    sting large but bridge plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
