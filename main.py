41114b382f4ae4da9c1fc847130b560a43abe356
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:03:12 +0000

    stir behind heavy gentle only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
