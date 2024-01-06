bf438c0db1cd019407aeac0632deae36c731b333
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:42 +0000

    clothes praise sound move custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
