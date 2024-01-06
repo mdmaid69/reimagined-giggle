64679460b096f0291c982e462386d5fdb1188f50
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:59:38 +0000

    tax careful tread limit sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
