f2374e9184a706f48b1ace8f9e005c248b8570ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:01:17 +0000

    lesson meeting temper circle completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
