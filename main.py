6e97bda61ec65beb4ba5e4815db83123d55b2871
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:12:41 +0000

    decision kind ground tap dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
