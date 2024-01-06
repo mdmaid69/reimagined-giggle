7528645436e356fb757d3a0006ff42981da36439
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:58 +0000

    attract cheque crowd work appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
