ccb2f17fabcb91ef29e974104d6aed1924b6ebb8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:42:00 +0000

    tidy quantity information now must

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_power(work, time):
+        return work / time
