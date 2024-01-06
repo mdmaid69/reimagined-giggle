72a0632f21dc083c13f49bb9e6653da6718bf9cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:44:50 +0000

    spray dare present active afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_list_from_array(array):
+        return array.tolist()
