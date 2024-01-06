44f525a6e7025d7f088ae1f1c75857d1acbd3185
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:57 +0000

    broken sit total stamp contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def change_file_permissions(file_name, mode):
         os.chmod(file_name, mode)
