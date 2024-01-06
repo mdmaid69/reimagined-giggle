9fe78e3fd2634d30255e32d13589e4bcfbc35d5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:34:03 +0000

    squash day power sky spill

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print(sum(range(10)))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
