8f5b1152afe4862a283fb9122a1165c905a62102
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:13:57 +0000

    middle great attract workon among

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
