989f89ec454361c755cfc90e2042fdd13bce77be
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:44:54 +0000

    bill dish eat winter break

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_item(array, i):
+        return array[i]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
