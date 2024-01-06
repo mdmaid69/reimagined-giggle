20edeef4f27e4ae211a70ec747d8b14fadda688c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:17 +0000

    angry disappointed easy dinner card

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
