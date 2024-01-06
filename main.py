ccf223dc8586e2f2df3c3eca01f8ba335744f324
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:04:14 +0000

    odd apologizeapologise community quickly agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
