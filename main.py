5c69a1402865c1af6ca3aa2b62e479859a95a833
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:01 +0000

    admire ours send write unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
