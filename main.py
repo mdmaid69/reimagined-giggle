630e573edbe1e6f259f617e628499b007751dde1
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:49:15 +0000

    cry description always giveout cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+print(math.pi)
