e0a55bc5602f2d57190c30c34c354fe180883856
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:49 +0000

    eat blow explanation flight usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
