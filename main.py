04d176006ef2b1b68aea039174de293bbdd3d19e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:24 +0000

    sensitive skin afraid start follow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
