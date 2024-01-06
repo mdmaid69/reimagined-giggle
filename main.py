6b03447385430bf816c7df9b7e54e07eecfaaea0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:17:23 +0000

    shake also later surprise male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
