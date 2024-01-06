ea6ea377d3892864031b2df20704ad6c8006f0b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:40:18 +0000

    whistle this suit world fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
