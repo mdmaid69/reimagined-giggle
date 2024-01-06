60607cd7a6d48ba1776572e99805c29e44f3e879
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:40:13 +0000

    positive learn worth zero central

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
