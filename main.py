5e5bff1a1cab3cbcb4002d2801034f9c2d7dfb54
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:15:51 +0000

    each short quit exchange honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
