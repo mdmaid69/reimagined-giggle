28f1e687fdd21335b8aaa849b0aa799a0063b5a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:08 +0000

    angry bet guess secret behind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
