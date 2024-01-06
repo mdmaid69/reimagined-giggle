7245dd32f6eecf4c0054434c474fa9cffab25259
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:49:13 +0000

    brother wherever word responsible male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
