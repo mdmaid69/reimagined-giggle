85365b00f2b7ff596c85af3139cdfee285942bf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:20:14 +0000

    study go offer sport between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def find_min(numbers):
+        return min(numbers)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
