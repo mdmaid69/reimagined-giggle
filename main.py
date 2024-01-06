ba966641e357f56b525ebe287ddd3b329eb9ba6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:18:42 +0000

    change rough frightened custom his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
