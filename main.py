5fe317a3b43a1c5bfe26e03221e2f30ba452f7d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:37:03 +0000

    row task group board develop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
