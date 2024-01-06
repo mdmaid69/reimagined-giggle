8449632347cf5cac7a6f8f729c6d65f19ca73daa
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:21:23 +0000

    nothing ignore mad table lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
