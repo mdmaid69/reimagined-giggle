3d70a9df2fe6aaf6e6fc989d7dde67047c586eec
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:50:47 +0000

    hold relief admit brief degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
