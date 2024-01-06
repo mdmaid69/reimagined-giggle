4b2ff9e685d720d495d3a468cb83519692d132e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:29:40 +0000

    scare breakin powerful there keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
