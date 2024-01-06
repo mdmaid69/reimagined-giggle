f59c0cd2e87c6e3c5961e44b48e4b69b50b40aa8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:51 +0000

    Jj hollow song zero approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
