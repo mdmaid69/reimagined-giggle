5520334eb285f0f36d3d6c9ed938e01091d73fe0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:40:41 +0000

    sky shadow repeat press region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_volume(length, width, height):
+        return length * width * height
