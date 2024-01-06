ed147f1ab55cbfbefbe7a5eff4c4e2d1ede3fbe8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:16:34 +0000

    think such somewhere new something

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item(array, i):
+        return array[i]
