ecccdc943f26b385165acdbc472e20b885e83f1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:18:06 +0000

    understand sometimes replace job weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
