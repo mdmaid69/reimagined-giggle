d8602334a5c840db0f8070ddc3d637b3cac679ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:16:00 +0000

    innocent wise attention takeover arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
