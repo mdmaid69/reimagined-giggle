5fe568a67c7623f8db16e5d9e297a69628eefd81
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:57:30 +0000

    grade spin print middle shape

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def find_min(numbers):
-        return min(numbers)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
