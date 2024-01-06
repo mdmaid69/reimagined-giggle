c48b136610405c715db46cdbbf4a1832a4f420ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:13:56 +0000

    disturbing suggest cut ours storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def subtract_numbers(x, y):
+        return x - y
