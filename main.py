83b2ff68ad14901625e2357533eb34c78fe729e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:01 +0000

    there like fly many quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
