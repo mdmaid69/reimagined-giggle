ea4bfd54127276cb09a012a681042af9c33c4504
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:12 +0000

    Dd behind warning interested birthday

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
