8ebe4ef35c224e531c42231f0ec34d79bbc6e43f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:07 +0000

    uniform wire swallow introduction balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
