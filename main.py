01c6ae319e9ba67a71ce8662eee4c0f8fbad7452
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:24:01 +0000

    respect project software meaning shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
