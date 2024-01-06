b01b4ded049b317630600b1e337420f0d90396a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:22 +0000

    character could enthusiastic base content

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import sys
-print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
