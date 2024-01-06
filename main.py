1bdb2e790c92612b8e656250f89f9d3a1b5a698b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:44 +0000

    shake treat fire no government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
