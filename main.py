c5e357e7060b127c51dca1fdae5ae12e8dc593d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:22:25 +0000

    cruel expect close dry excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
