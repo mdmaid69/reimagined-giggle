6143b9df0dbc519385d2321a6a3f2ccc098bb8f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:20:56 +0000

    just worth family relief score

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def count_characters(sentence):
+        return len(sentence)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
