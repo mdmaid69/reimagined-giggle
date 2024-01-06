ed8631ae15ac829bf0feb221784a45f3cc334082
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:09 +0000

    weigh total 573entertainment spite recommend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
