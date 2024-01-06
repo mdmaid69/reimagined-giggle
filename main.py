ffe07b75f3dd2d3b79cb1ad1e5e5f518df42e432
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:43 +0000

    little boring concern fence measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pressure(force, area):
-        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_perpetuity(payment, rate):
+        return payment / rate
