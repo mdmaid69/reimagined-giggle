e2df8b2c23295b05bee07d108d5693ff4e7b2acf
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:59:17 +0000

    scare Rr usual yet squash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
