e85c00e5051cdc81f7dd42a68d12a3a59b1ff557
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:25 +0000

    difficult hard freeze snow ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
