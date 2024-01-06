2ca913ec8034718164f5d85212ffb830841cfd02
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:38:09 +0000

    if sticky rely tonight today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
