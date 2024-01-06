684d835e272233043f6d3b218cbbe8af195a35a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:18 +0000

    wheel touch kick runinto confuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def subtract_numbers(x, y):
+        return x - y
