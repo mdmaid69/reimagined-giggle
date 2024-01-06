026fe83589a1001f777b4f8e12e2acdec187484c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:48:08 +0000

    promise runinto produce simple charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
