a8d3a405ad8bb39102fad88637e6d105cd74ddc7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:54:16 +0000

    phone safety thought injury its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
