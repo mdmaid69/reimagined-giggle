ded9417429da4ab6e99b4a25b914151bfb890f39
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:44 +0000

    clever accommodation image total turn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
