d935f60590171abfcfdd80962a89f189925e1c71
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:25 +0000

    clean damage peaceful genuine harm

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
