d51a4a10dafcc1428e64a6ff73d31795b168fd27
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:23:59 +0000

    scale ride nurse as age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
