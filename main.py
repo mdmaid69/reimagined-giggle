03840566d94de33365d795dddaac7d0a15316613
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:40 +0000

    their anywayanyhow make mess splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
