808d31b7222569954f372dccc9c4d0d6519fcf54
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:26 +0000

    prove excuse anywhere powerful hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
