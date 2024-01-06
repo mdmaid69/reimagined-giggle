da3d7721f1119e1fd8d264582e3db009d8fa1743
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:16 +0000

    further force earth spread bad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_os_info():
-        return platform.uname()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
