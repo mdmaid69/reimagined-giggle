9605adad0601fe19f97fa342b6ba7995d01e7949
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:04:22 +0000

    thinkof depend sky list rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
