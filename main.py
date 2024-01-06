a277c78810f5ecd7ac8730b8a22da64e4dbf083d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:34 +0000

    go shadow rubbish challenge world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def greet(name):
+        print(f"Hello, {name}!")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
