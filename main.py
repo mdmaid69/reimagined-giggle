ae55a80875ec2285d836a71901eeab788515b8e4
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:04:11 +0000

    slight do figure smile jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-print(os.getcwd())
+  def multiply_numbers(x, y):
+        return x * y
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
