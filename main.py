bbbf21c0bb48b9f1b13eaa964f187bc603925fdb
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:53:51 +0000

    legal deliberate surprising mass rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
