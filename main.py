6e3830826daccef4de16c0971b7e9304e7ba9a14
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:37 +0000

    Ii somehow minute hope mend

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
