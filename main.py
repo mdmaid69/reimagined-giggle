febfc178367f84a7d035f2ffc0acef9383176036
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:40 +0000

    add course middle part attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
