244c4f99cb19cd7d0ff14a95cfa230bfcc5e761a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:54:35 +0000

    since would regard knock science

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Uppercase:", text.upper())
