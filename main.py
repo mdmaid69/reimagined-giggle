e1b7cca8889b7b7ce9e138a7b107f91adb542fe5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:28:39 +0000

    mean cinema shout owe sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
