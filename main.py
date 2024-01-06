61182323a619c07d82958bcb89e0bad965241b13
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:32:51 +0000

    range shock permission solve his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
