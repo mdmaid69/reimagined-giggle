0d528e54f28e2e4bd563971ea4074ef619c75ea0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:56:55 +0000

    adjective protest shallow swallow hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
 import math
 def calculate_exponential(x):
         return math.exp(x)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
