a6b7269ad241c99875282ec134dd2e1399a095fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:28:30 +0000

    left independent confuse runinto today

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
