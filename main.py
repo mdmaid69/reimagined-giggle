e2bd0e201452d0c99788401d2f5e54194decf0d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:52 +0000

    group what impression exciting cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print(sum(range(10)))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
