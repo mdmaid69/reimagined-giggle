c063d64e11f1f7d093522f21634e70bd93d939ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:36:49 +0000

    turn to Kk square ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
