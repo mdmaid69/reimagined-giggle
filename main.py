9771e45704c9b0c1b930c05af0b8175d26dd3bdf
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:37 +0000

    sensitive throw call sale ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def square_number(x):
-        return x**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
