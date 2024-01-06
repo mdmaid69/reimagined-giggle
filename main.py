31f060a05847b3b96a0b96e9db9de74439dfb9e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:44 +0000

    569enjoy nor save blame coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
