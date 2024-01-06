174a2ac167bfa29fa2abd6eb8000c91098020eff
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:49 +0000

    content wire reject note pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
