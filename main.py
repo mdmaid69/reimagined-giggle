5bb10676a7a3daacdd710f545a5b97761698feba
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:35:14 +0000

    show care bringback home occasional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
