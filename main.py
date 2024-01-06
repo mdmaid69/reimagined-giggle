d8926f821c54b7501bcbc93b442d6aa99418981b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:54:12 +0000

    judge doubt downstairs still getback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def sort_list(lst):
-        return sorted(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
