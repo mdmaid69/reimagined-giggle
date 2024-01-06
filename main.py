231b023fa3338c52065ebc876e4cad440d3d9d0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:28:50 +0000

    twist high tear rush doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
