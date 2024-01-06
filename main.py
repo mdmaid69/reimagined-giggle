7187e4a0716d147198a7db80145edf38094cc31c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:40:34 +0000

    last fence greeting freeze difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
