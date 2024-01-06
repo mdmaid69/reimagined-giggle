fb7f4f9413f3edfb9f6c7db37d1c47c352633cf4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:18 +0000

    dress blame dance passage male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
