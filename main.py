c220564768e8df3d4d0fdaa656f7ed7847ceface
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:07 +0000

    level mouth plant follow journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
