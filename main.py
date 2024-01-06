ee9da95c1e2a253149e3e387a8975f99c22a4628
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:26:26 +0000

    impressed fear main since do

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
