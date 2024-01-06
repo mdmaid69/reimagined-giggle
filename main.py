61b13c30fcea6555dcdabc5f7e602bea954e16a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:08:48 +0000

    statement squash study question snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
