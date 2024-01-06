3f68d95324af73ec44d481aaa7af747a93a0379d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:47:04 +0000

    free problem turn achieve term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
