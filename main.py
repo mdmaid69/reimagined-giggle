06a917c059f84e61d111ba09396914bbeed85b6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:50:18 +0000

    borrow mistake life air result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
