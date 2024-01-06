39b99cae74e27ed9da7d623a846e3450257e048c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:25:47 +0000

    delicate clever near pull favourite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_list_from_array(array):
+        return array.tolist()
