5738c5812a15c8d379d75d9ce1617028dd8d3093
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:31:56 +0000

    expression offence shoulder never remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
