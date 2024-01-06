dc38b81da4effbd3efbc34e4cbfddc6ea33ad940
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:53:40 +0000

    power spread hello effect hold

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-print([x**2 for x in range(10)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
