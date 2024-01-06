1a7fd503b31f95bc5d665597844e2ffd1e49cadb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:28 +0000

    light about praise let yes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
