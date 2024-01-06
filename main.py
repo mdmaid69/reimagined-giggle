7b93c3d88e2d619f49ccd7c6492e79465b53878f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:14:49 +0000

    giveaway closely 569enjoy wise full

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
