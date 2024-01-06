cd3934558fda6a2e5b548207105e271bc9aacb94
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:30:59 +0000

    Vv swimming rescue properly feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
