e8b10a953c838202c9c723f369f0aebfa9513e09
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:14 +0000

    rub either fence detail swallow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
