5da084a05cbf356f6713653317f37d44d1d45660
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:35:41 +0000

    once valuable rob in set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
