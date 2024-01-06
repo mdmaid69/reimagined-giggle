25d86dcf32b514f167a099ee407fffde1fc687e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:06:55 +0000

    hers punishment run punishment moment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def reverse_list(lst):
+        return lst[::-1]
