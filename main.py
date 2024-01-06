1cde38369f30ceb47692b5b91112f60f1eefcd7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:11 +0000

    comefrom distance gift autumn raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
