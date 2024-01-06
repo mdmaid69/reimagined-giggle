5f0ecbe7567ef9c79ca8348b84d8d93ff34bd4f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:56:21 +0000

    hot total underneath uncomfortable by

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
