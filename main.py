60a314f40e79a57f7495cfc3a561797fa6d669f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:42:14 +0000

    rid degree proud moral ability

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
