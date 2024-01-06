06f5b0b6610ffc8078095fd6f00db185272138e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:28 +0000

    ring goout typical fear pay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   def cube_number(x):
         return x**3
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
