d93a55edf1d97db5eaf3de3e20ac01f6564c42f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:28:51 +0000

    schedule cause state together song

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-for i in range(5):
-        print(i)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
