7715fa27a23171085ccfb6858aad81f7748df302
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:57:45 +0000

    drink mix race noisy please

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 import datetime
 def get_current_datetime():
         return datetime.datetime.now()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
