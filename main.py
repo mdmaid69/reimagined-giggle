ec6765a9f5275c385e8a459c783295c16c637b78
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:59 +0000

    load before bright shop lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
