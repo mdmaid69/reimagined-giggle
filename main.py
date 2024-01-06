9a74325906bca177e0ef7724e3e8043c7bbf8ea2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:57 +0000

    getup Oo shall certainly take

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def count_characters(sentence):
+        return len(sentence)
