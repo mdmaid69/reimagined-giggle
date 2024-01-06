6c79b5e9bef34114c5284bac0e2069cd06d750e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:53:10 +0000

    habit major sign juice track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
