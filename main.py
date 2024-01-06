771c44140bafcb0051d490c0db00f2f7327b4061
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:24 +0000

    disappear necessary heavy rubbish experiment

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
