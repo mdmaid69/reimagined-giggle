c931ed1b2ac4a499dea56e2f8d451a718e0d8e8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:46 +0000

    test house space month importance

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
