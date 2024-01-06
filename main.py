5a141ac3f1e1f261b309b5182fcdee61d462bf5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:04 +0000

    hope identify wet claim deliver

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
