9736fcd94f55e8ae0cf89b410781cc2237d6cba4
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:31 +0000

    average Oo eye interview form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def divide_numbers(x, y):
-        return x / y
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
