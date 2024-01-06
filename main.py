d043c0c11a9ba125db8c2e09a38410f40496195f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:16 +0000

    slight control welcome Oo go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
