7121dfb645189dc5927d91e5aa94745c5a5b00d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:45 +0000

    wash convince equal permission wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
