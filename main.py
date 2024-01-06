c195f43a84a012d7205d5a68c2fc7ec40e63c7c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:55 +0000

    possession grow even anywayanyhow there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
