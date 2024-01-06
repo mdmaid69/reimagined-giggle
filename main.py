b0b3cf6204c0438ad23aa167a91f7750f3b87640
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:34 +0000

    health poison hurry keep room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_priority_queue():
-        return collections.deque()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
