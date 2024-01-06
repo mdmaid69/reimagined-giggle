e91aba296fbe8d722f26443ceaa61e0517080ac9
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:04:19 +0000

    finger form intend roof dear

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
