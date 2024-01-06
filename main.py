958d5f9cb7ae6befb2a59ed05164f44b90ce3c47
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:16:43 +0000

    face bright wipe talkoutof abroad

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
