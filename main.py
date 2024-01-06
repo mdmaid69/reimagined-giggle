cef4d27186281ffd9449efad2858ce3dccceda62
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:10:24 +0000

    tie health favour characteristic cover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-print([x**2 for x in range(10)])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
