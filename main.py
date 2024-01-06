fe41e9e763b13eb9996e16697d36ae8b8e3a39d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:16 +0000

    repair hair official wonder offer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sys
+def print_python_version():
+        return sys.version
