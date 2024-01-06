9eaaf0ee19decf9ef8061c71cfbb3410a266f1c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:51:37 +0000

    dare limit fill grand right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
