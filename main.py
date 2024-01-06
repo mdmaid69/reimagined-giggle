b13617570960609066ee2e27936b649222e3e29e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:58 +0000

    opposite heart convenient high best

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def convert_to_hex(n):
+        return hex(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
