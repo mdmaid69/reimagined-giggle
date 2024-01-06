d484d90d336e065b986bc1b74fb2bc88741be128
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:35 +0000

    eat goodbye rest glass building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
