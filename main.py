c3cf37b5dcb39da06f0e77249d2e7221651bb22d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:04:34 +0000

    nature declaration oil tragedy considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
