252a89906231fe8ef4a924fafdb7ab92cb49d1b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:27 +0000

    why keep pause degree act

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
