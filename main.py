a1f7d72a0f323d25b6801a2fe0d71048e630ad85
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:40:12 +0000

    conclusion lie furniture find excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
