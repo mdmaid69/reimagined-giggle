bedac29995b7a08dce9b451127772959644526ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:15 +0000

    rarely violence character left label

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
