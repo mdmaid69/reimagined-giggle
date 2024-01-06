aebc79a64c082d5a257a1d00e0be2c415f6399f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:59:52 +0000

    top letter scene solve well

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def reverse_list(lst):
-        return lst[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
