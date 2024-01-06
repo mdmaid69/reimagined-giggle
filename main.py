f055e7ca0f7badf9aa83e4519ad4c9496f77180e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:04:57 +0000

    Vv bottom feeling admire rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
