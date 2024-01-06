bffd8824a5c08efa694681ba6fb28aae0d68fe34
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:15:37 +0000

    central past interesting ill beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
