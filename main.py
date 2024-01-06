7383466d36fde8f55cde6cbaef3e0e5a084995fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:15:28 +0000

    objection callup value instrument this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
