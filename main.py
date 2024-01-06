135e906d030c4ee5c1ff67309778d11a603421cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:05:35 +0000

    climb dance holdon miss like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
