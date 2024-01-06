84112a4afdcc002b771f9a2a0f3544a4c9b38bcd
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:30:51 +0000

    stay breakinto large habit than

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_current_working_directory():
+        return os.getcwd()
