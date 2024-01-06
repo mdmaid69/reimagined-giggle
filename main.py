c14c212c01a7c98cc67a70f86b6f22b1775f110d
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:10 +0000

    respect Gg equipment let shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
