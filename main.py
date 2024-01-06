2c8b7ff1e1180632cee1567c6a62712e308b6528
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:40 +0000

    realize occasional apologizeapologise sort talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
