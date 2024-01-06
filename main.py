69e19686d10ec32488d40ba014f9e7ac981da5cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:23:23 +0000

    twist please this Yy not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
