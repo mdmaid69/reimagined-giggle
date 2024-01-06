0f89758bcdf1fea013024efcd8bd77d53bc788c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:13 +0000

    quality thorough meal score run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
