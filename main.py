5dc4ae898fc72f4f98b0a73c23e2391a7b63fb47
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:07:25 +0000

    preparation comfortable uncertain seat open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
