05ff2676d3fdf6a610a3f9aed56add94f5d48aab
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:22:06 +0000

    fear open calmly disgusting feature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def remove_duplicates(lst):
+        return list(set(lst))
