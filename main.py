5650bed3369b1b15955369c47554f3654d02ac80
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:53:51 +0000

    everywhere uncomfortable modern shut bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_cosine(x):
+        return math.cos(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
