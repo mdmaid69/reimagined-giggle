b618252e0f0f321e4f46db8d7c79211a4303d559
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:13:11 +0000

    treat car period hospital exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def add_numbers(x, y):
-        return x + y
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
