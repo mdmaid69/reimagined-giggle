94aea54730e337e9ddd0cbf4cc3b74dbb3a4f564
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:52:13 +0000

    song jump separate murder afterwards

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def is_odd(n):
+        return n % 2 != 0
