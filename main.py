6504b81a18d41348bc5d37906c87e07bed154a93
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:28 +0000

    soil claim warm attractive ride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
