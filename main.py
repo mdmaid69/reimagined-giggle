2bdd06bed32b3f4b34c65d976a501068b1030449
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:01:26 +0000

    without increase plain search name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
