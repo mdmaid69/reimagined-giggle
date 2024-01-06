ecd197f6b62cf3a38e8133ba58a23c00f744af3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:24 +0000

    risk over heat fold shop

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def square_number(x):
+        return x**2
