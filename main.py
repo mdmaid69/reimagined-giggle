fd259e51eea42baa349c8b1d033fa87ed0e0e240
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:23 +0000

    single remove themselves normal tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
