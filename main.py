7604b44b98c598a47373a36274f0eb1c03f6f12f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:32:49 +0000

    force bit soil tape snow

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
