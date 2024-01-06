0b0b29aa8725d87f44c1f91df7c45d3a4e42fadd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:17:09 +0000

    sore advantage night team passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
