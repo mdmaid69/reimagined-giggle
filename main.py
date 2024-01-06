5db6f0d5f865aac3bbd643517dddd3b6542f5ffc
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:45 +0000

    pause when check oil sport

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
