0ef5468c807772719041ca52b0a7d1678db88d8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:31:54 +0000

    wet experiment earn wing possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
