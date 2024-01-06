19e654c4cfb41b584f7f8d42eff4e5c078f1df8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:37 +0000

    getout want ocean drive workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
