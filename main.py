bcb2d6226c17d56b6af6b9050bccc94cbdff2731
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:12:40 +0000

    question Rr expect positive wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
