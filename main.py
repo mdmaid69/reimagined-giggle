8ee52e56a53b469d59ddbd351fdd1ae3af7434f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:36:36 +0000

    film voice blame calloff appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
