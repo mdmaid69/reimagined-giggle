74498299fc20240d088f9b35cbf00e099f46779e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:50:08 +0000

    average family scheme believe gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_python_version():
+        return platform.python_version()
+import sys
+def print_python_version():
+        return sys.version
