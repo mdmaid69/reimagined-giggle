c4f40ece5cffe0d18fca2e5c5f06955d8521ef6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:38:54 +0000

    touch character lost process tidy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
