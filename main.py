6e90dead89073dd8c1718eadebd1b447812e3a84
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:04 +0000

    whenever collect area familiar present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
