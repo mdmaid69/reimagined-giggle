fb6ce6cebc3ab57a967d16d06b32be214f283468
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:58 +0000

    especially ask break goup love

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
