c1904bc033588f8c869398ad7a10a1dd8e548961
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:18:58 +0000

    family offence divide skill if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
