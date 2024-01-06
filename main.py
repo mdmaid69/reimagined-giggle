97b2db44566bddf58543b390d0d9b82f813cd5c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:08:40 +0000

    patient duty contribute past material

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import sys
+def print_python_version():
+        print(sys.version)
