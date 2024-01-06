a4456bc0bd5cac7c3bcf759aea27325cbcba7cb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:14:11 +0000

    patience image rest worth tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
