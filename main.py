5efdc120d4413f979305029ddc91dee1b8e5e623
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:36:40 +0000

    run concentrate slip wrap hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
