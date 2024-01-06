dd3b6f3af701da3de4ff9629b91d1854af70cced
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:56:12 +0000

    party commit terrible image few

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print(sum(range(10)))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def split_path(path):
+        return os.path.split(path)
