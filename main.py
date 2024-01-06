fc41ae64e749af8c8aefc7989832cdfa0c657f3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:17:14 +0000

    keepout squash that word information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
