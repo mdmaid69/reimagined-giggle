79ff7ff7910ad02d610b4ab38b511c53ebb249d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:27 +0000

    block owe drive produce impressive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
