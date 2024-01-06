662439f7588098152237b3b273a34d532d24bb01
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:19 +0000

    substance transparent sure forgive same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
