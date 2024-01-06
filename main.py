89f3a07bba7ac8e5a384464876b73fad87620abb
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:33:59 +0000

    giveup thinkof thought over coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
