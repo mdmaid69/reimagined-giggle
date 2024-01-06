f64bb4a09c9bd9f2b27cf3ed0ebbfc65d5604065
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:15 +0000

    ask to petrol copy lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
