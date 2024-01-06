e6f738b7f9ffbef1a4806481e9091bfc0b8efe2b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:27:24 +0000

    566end explain love birthday interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sys
+  def get_python_version():
+        return sys.version
+name = "Python"
+print("Hello,", name)
