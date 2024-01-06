cbe6e95572b3cb345dfd289aebeb30f83051fdba
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:08 +0000

    software clean range nurse niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def remove_directory(path):
+        os.rmdir(path)
