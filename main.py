40df95c72ed5c9685c5e12b028bfc89bc613ad8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:13:35 +0000

    mine list body process organize

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
+import tensorflow as tf
+print(tf.__version__)
