5f53c88cff8c7b8b3ebd7b6e2da7e9bb3bc4263b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:48:33 +0000

    smoke order fix variety imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_floor(x):
+        return math.floor(x)
   import os
   def get_file_access_time(file_name):
         return os.path.getatime(file_name)
