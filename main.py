78c3d881e0607a84c7d4d4dbb0f410cb90bdc36d
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:37:24 +0000

    authority loan wrong pure scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
