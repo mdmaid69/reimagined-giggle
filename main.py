8befa1ff6599063411ab21e52b634ec4acfd0524
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:30:16 +0000

    container contact window wipe actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
