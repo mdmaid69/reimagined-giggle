c1cb91ca84b7f3fb6c3b8faae9bed58ae86d1742
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:12 +0000

    badly snow embarrassed weigh best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def convert_to_octal(n):
-        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
