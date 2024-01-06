ddd1c7532b08beca0c1dd87313a311ac2325f02d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:25:17 +0000

    lower wonder dance and face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
