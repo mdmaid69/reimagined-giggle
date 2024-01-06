94111ecdad6d8fce660699770b4cd9498aa036d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:22 +0000

    Bb front over gas dick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
