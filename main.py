4e1cb246a8792c57aa8732a64d79929c7acaaa7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:36:55 +0000

    front noisy address climb thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
