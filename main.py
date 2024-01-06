3e4157759ca3d673198a3f9441a7984b6e1a2b60
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:13:31 +0000

    blame lesson decision lesson stop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
