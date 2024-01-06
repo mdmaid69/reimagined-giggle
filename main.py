b9d9e81e4cdab0b0602c220491dfdf26ff373883
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:56 +0000

    star allow slow slice plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
