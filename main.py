c7ca93a2f456b8b416dd9ea4c8a7a4e7da7b6d96
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:35:42 +0000

    lose spoil continue holdup even

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
