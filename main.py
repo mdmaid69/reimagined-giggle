be72caab681f1bde8fc585892d9c70d7fb4ab0f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:43:30 +0000

    instruction only anywayanyhow remember ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
