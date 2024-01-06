6b4746d478d72d19805ef71e862a7f8d7a8dcd25
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:22:10 +0000

    shade little care both second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def exit_program():
-        sys.exit()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
