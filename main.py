fd849627f87f60d23565fc7bfb7f2c6d3d9e7f90
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:27:09 +0000

    suddenly rescue yours happen nerve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import os
+print(os.getcwd())
+def is_even(n):
+        return n % 2 == 0
