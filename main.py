c6f15693038d003cd470a887c9f3cedab9d3994e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:16 +0000

    clear arrival up determined market

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def convert_to_binary(n):
+        return bin(n)
