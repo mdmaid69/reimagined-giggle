32f8090899c6480713a4dd2be0a1cac78d391a93
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:49:36 +0000

    reach favour female group person

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def get_array_length(array):
-        return len(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
