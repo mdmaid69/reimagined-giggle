68b4df5012193b792c6061ccaf6a35d90e21e2fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:10:18 +0000

    platform round constantly gas comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
