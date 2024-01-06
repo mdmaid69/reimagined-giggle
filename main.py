934cafc12e0d350b332d22ff0891a0390bc6f4c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:45:33 +0000

    dress protest until content adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-for i in range(5):
-        print(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def clear_array(array):
+        array *= 0
