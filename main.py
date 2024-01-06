92392e91219e2b52b6c9fc51c538d60ccf8ec016
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:48 +0000

    hide correction shake shopping she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
