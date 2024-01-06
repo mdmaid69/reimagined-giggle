dac21afa9a15246b5b8c6a1824e2b75b313a12a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:11 +0000

    hit run shall each satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
