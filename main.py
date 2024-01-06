98ce7fae91e206b628aa887d5249fd9dd63bbb85
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:57:47 +0000

    confused size ear kick fast

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
