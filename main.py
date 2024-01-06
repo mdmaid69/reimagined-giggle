d411560bea52af25f41af5459932c0aa37251429
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:14:03 +0000

    steam underneath explain set squeeze

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
