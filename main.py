2ec6b972fc97ae0601541b48d314dbd2255f305b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:37 +0000

    table 570enjoyable every no future

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
