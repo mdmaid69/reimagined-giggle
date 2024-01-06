1d84deafe388bda4cb81fa7bebea67f1615051a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:49:03 +0000

    gate risk steer scale colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
