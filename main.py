c5c2667bc4994324beaee1d7e044289fce852e83
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:50:39 +0000

    quickly obtain live turnout main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
