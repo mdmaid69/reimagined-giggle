adee69668f64353f88896df3547eac660a8dd409
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:44:45 +0000

    matter hand suggestion help 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
