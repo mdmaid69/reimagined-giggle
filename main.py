f6b675060aa3395c3dc8b8edcfbaf63140b2aaf7
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:59:11 +0000

    delicate plain head walk healthy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
