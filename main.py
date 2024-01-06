25e8d2a8e243d580d2331094dec4a3e034c5fb9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:49 +0000

    common soft difficult scale arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
