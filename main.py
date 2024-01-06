df34a5c0c20b0cf80d052834a1f29602db29a63f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:28:11 +0000

    best forever drop method equipment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
