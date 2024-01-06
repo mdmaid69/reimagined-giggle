da01d92f137f3f76a278ca019bf3a152043973ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:36:50 +0000

    ground next handle favour notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
