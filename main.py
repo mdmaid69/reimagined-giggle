6fb557c2559ed090678c593ee38ffd675a569a92
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:06 +0000

    bottom one thank takeoff use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
