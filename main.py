22ae4e073fc46fd8eb60b4ecc1beeeefd991783b
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:47:21 +0000

    certain habit unable control operation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
