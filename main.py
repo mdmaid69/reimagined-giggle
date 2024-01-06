98524d76186b1ad05e09e2938739e0662b981fed
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:15 +0000

    brush warning trust tight duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
