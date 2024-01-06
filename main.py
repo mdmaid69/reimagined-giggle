301de067df5465aae8b1b9120b8c1cf08e493f42
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:49 +0000

    borrow taste plain neither once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def cube_number(x):
+        return x**3
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
