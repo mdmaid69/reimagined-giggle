ba7a5ce770853e9a4b0d935ff3a85705c29fe05a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:34:34 +0000

    measure read off snow variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
