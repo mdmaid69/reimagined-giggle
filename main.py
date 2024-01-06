8a126821a0cb1aabe4197812d34d64d390a865ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:22:51 +0000

    use short condition show nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_speed(distance, time):
-        return distance / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
