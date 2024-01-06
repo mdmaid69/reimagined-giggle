54709cc65f2b83460eead890117f43a3207a6489
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:47:37 +0000

    swing colour store deliberately freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
