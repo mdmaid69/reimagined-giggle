010dfcf98792094650242abdd4174ac9a4eb10a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:51:36 +0000

    cross produce system some explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
