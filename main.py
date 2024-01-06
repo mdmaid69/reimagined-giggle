c91ebc042ce82b9cbd9f78b56296e138ec3b7c1e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:22:25 +0000

    like through correction ball surprised

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def sort_list(lst):
+        return sorted(lst)
+text = "Hello, world!"
+print("Characters:", len(text))
