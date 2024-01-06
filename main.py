192f1d237509ab4f4dbbb155bc22bac6d291a079
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:13 +0000

    try case event kick unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
