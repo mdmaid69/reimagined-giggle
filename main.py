73de6cc6282e32989f3d12a4f0b789f4166e9c8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:25:05 +0000

    succeed part as fire immediately

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
