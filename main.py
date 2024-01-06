7607f637498131a8f1ef51eb19926d4e1e4c5491
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:05:49 +0000

    steady advice safe back friendly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_min(numbers):
+        return min(numbers)
