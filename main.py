61dd89a761d802fb92e51122cf14bccc83c6c761
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:48 +0000

    free deliver lift talkinto boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
