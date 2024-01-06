3f1571ea3eff05c0935e569d915069fed7e12615
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:04 +0000

    under talk intend cheque afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
