367fe41a59e2b326dbe5319cecff29670fedbd6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:37 +0000

    its importance reliable path loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
