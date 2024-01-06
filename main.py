b5dfab9a3d1030c8abb3b6817029c2cf2eaf5512
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:37:28 +0000

    broken wheel warn usual excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
