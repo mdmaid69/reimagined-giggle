8b4602798ff3bec40e78913c906cb31e91ae2cca
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:03:54 +0000

    floor why while idea meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
