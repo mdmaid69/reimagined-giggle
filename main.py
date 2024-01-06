43775bf120a8f399bbdee1b955889512feda704b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:49 +0000

    beat zero evening scrap although

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
