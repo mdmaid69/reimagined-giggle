fc89f1d1a72687e51c2c8df4c8c1728cd1138e6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:37 +0000

    translate their apparently accident stone

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import getpass
+def get_username():
+        return getpass.getuser()
