d6bd85e8dcf82ddc9eda293abfbdff819365f8af
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:25 +0000

    freedom lock game company difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
