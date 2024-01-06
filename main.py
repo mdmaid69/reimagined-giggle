72ee266df32203a59fb9166d4bd232b9e5a6c212
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:07:33 +0000

    debt port fear near thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
