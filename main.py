cdbbb4584acb68bfa9dd8424116e3b6df8c8179b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:20 +0000

    strike cut turn skill number

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
