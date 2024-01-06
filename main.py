1e5f48b2f769172cd5ac0be4f94ea1295d35599b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:22:46 +0000

    chief instrument future shame cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
