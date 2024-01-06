22392781d025a716ec22a020c9746d2f54f7c213
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:55:23 +0000

    commit over steal bang wear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
