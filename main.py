35586f8c1b6810330f0184204fcc081cb47cf7c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:02:46 +0000

    though shut approval however wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def cube_number(x):
-        return x**3
+import time
+print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
