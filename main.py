b78eb220cc75b2641785fb0862401f8ecc9b3f72
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:38:44 +0000

    continue part prevent post border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
