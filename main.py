5001b4c4044ae4fc4a42f7613ccc494a9ac17da7
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:56:24 +0000

    way respect view hope loss

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
