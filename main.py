0f3abaf7ad6cd1d52f167d486a78ced3a0c662c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:52:14 +0000

    official Ss season straight measure

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
