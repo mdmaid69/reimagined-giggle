e05c0b391c8e94a01e44a383843676808e2f8fd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:33:33 +0000

    definite steady beyond within tight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
