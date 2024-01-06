95150490d82ad2ec58f93c4e17ea96bf8e44bc0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:17:49 +0000

    vanish spend like contrast sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
