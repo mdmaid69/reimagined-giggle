7e512cf95b010e17794adc436a913649f6fdd504
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:00:58 +0000

    bite flow shrink aware holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_queue():
+        return collections.deque()
