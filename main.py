912d0a1aff2b2a2e92479258d79bbbfe5f799b72
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:31:39 +0000

    comment point glass ourselves cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
