4fe3e6e2c1082ffd73e063e761d46f498cfb8ffb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:25 +0000

    blind let less phone sleep

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
