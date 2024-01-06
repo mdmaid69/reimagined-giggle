9594fe252512c6f7c1f1cdc10aea26cab1b13f84
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:18:43 +0000

    wide comeup slide regret stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
