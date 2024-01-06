8e3f0bcdaa9f1501089afc76c9cf2204f0d3ace3
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:20:14 +0000

    we flood fail better stupid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
