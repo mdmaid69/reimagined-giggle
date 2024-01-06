2caa12b67800cba3cb82dd76b2030777ca96ecea
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:03 +0000

    miss our set interesting general

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sys
-print(sys.version)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def cube_number(x):
+        return x**3
