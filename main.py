a6e68a131a98b4ad2de3754cfb8ac2066eb93051
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:05:11 +0000

    relation vanish sound plenty hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
