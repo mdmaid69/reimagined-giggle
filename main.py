98836c8ea723b91097b4babe7577ba9e0a645b61
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:15:50 +0000

    whichever stand few they rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
