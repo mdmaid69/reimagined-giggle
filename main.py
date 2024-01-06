cc03ef796ab45ff51c066f728c488935ce475c30
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:01:33 +0000

    pretty history muscle soak inform

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import sys
+def exit_program():
+        sys.exit()
