e0ec422400e1cefa20eaa4a7f9c6eaa3167acc6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:45 +0000

    welcome advanced little rather meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
