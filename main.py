406067d266b3a025cb3539906701d762a20df2af
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:23:23 +0000

    tall sight look accept innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_acceleration(speed, time):
-        return speed / time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
