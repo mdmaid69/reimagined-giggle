40d90f3af143589fba6c0f2eb417a4b381cb3426
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:27 +0000

    interested the poor workupto as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
