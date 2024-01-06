0b85dbd56619e9dd0604d7e988ab9007572a93a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:20:11 +0000

    roof party wash slightly on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_density(mass, volume):
+        return mass / volume
