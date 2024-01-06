45bda39307f01288e3b0f5f90111cefbcd9a22c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:16:06 +0000

    couple supply minimum forgive maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_item(array, i):
+        return array[i]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
