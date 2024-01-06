12d7a625e6e7dc1b90693c0601f41f707025b36e
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:22:21 +0000

    leave just street cry know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
