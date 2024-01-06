b6f2c15e51d48cc76ca545047e4a220b99a57898
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:55:59 +0000

    minority quickly stick even corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_bytes(array):
+        return bytes(array)
