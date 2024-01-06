71a8378e921e7f9660f92d29b97b7209b6aa80a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:08 +0000

    responsibility condition up colour method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
