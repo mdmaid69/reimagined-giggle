35fc722c7e4933a0ffac47599058055adcad54ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:32 +0000

    divide create get talkback treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
