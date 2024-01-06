5d9ad07c8f245fc2af081fb61895a07b519253ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:05:53 +0000

    season interesting charge pronunciation lookup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
