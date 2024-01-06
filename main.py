23460dc5f3d62f75ca979599b3eced8ee932b39c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:36:32 +0000

    centre independent under shut walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
