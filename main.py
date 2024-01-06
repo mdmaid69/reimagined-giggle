8e091aceabc76cd22dc548902a8e3ff9465e80f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:43 +0000

    principle 179bite solve road after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
