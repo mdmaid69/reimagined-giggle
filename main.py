5102182638e6fcf022256fd83fe4e2bdafb33e42
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:10 +0000

    worth pleased stir patience sore

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
