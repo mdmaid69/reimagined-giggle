4f63863ecbf922ac85e6ef5cd9a12cabaf41addf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:25 +0000

    provide worth flood month seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
