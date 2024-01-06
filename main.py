a0a3dc3dfd6fbcccae38e299e88f369e93d5572f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:16 +0000

    relationship woman scared play trade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_work(force, distance):
+        return force * distance
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
