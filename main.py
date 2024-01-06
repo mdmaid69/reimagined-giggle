79509014dece336cac1c7966730fd2ad5731941e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:44:27 +0000

    single except do reject breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
