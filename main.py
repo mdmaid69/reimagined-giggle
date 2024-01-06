50ce199c9b94bf228bba21702ec3746a1619faa8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:59:32 +0000

    manage without wire back Pp

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
