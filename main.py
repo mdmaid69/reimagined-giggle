a63a6bf7f01342906acedd3081fb2265aa351a10
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:43:53 +0000

    example breakout welcome mistake sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def multiply_numbers(x, y):
+        return x * y
