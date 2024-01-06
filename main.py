6cc97b8f4e54e8c7e1a379871a3067b20dfa944b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:17:46 +0000

    fair break bet better start

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
