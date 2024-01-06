a3d7facd7ea47b034002401b527b59c7ce7f849c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:37:53 +0000

    muscle risk inside listen accident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
