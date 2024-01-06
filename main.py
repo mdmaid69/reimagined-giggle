daf2a1bd52c82700376ea85e2b35f43ac9824589
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:51 +0000

    union plan dance noise beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
