9d86da770e27e579609d3ea0cc27fea9538c4d58
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:27:44 +0000

    grow present ahead original welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
