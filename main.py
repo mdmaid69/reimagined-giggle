32dd8785f0e772d2c2904293c0ac6c49d70019f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:57:04 +0000

    successful yesterday worry sleep regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
