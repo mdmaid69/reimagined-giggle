7fd8f958f41a2126c7845438881b5753ef1dc8ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:04 +0000

    fix collect 573entertainment deep create

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
