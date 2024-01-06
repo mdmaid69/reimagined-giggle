f59f98eeca252d26cd56765253b8668097f82c64
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:05 +0000

    organization Yy stir public life

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_time_since_epoch():
+        return time.time()
