52f7b0ad6a2ae04cd382359996245ad1ae7cf2c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:01:13 +0000

    here post himself visit evidence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
