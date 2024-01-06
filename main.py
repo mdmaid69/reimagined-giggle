938555daf5e93514ce6ec4f908fae81263d5c9d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:53 +0000

    ever search spare rest finally

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
