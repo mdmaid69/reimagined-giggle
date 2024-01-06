e496addc350cd1ce1190a428d574253c28db0deb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:46 +0000

    shake dangerous contribute sick transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
