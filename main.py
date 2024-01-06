864c4fb0a7d5a3aad664481fda7d178b3698aa1b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:56 +0000

    false below constantly press operate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_acceleration(speed, time):
+        return speed / time
