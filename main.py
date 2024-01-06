0c970825e0c6a3fa3c0d0d7a5e9e97fe1cf8f4c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:25 +0000

    introduction home sound club ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
