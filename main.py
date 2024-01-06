864898e0ca4b1b8cbb409d9bdd0cf98fd0d9b221
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:35:07 +0000

    mix possibly clear accident grow

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import time
-print(time.time())
-def calculate_pressure(force, area):
-        return force / area
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
