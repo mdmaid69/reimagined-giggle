0f3b4013df55ea8dd9ee5dcb20a0ce3bf1512d63
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:34 +0000

    early business aim average check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
