96e8695fcb7a60666f4fdf5917485465c7b6e69c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:36 +0000

    justice grateful hold dance feed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
