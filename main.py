a855b84e117c9bcad1bf0039933d68b3a26739d7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:10:07 +0000

    translate Dd boat somewhere occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
