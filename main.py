7b91d0f698be7926624503c39d9282e74b0a0237
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:46:32 +0000

    bunch never pride bottom borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
