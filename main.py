7f4259fa519bdd6f35183bb129f9e36760984954
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:07 +0000

    sweet rescue flight reliable far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
