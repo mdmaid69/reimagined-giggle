20c6fc1b15e4b4cf79e45e390b59baf329668e9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:15 +0000

    impossible track find priority recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
