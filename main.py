739d0513b41d3a169dea9755b9b53996665f7b12
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:53 +0000

    convince name song however danger

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
