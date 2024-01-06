8a31d8f35ef8a9d2caa3cff7fb8ad553b0907b98
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:21 +0000

    work secret mend foot sign

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
