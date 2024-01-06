7bd4d59c6f331a7b050d5a6b3b680076057ac767
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:19:52 +0000

    big shopping so health charge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
