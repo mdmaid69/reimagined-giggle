f8e5263ad7b75ac32c90dbf5786fe9f996fb5ed5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:52 +0000

    runout century shape object taste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
