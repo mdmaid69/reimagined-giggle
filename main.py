534872fa7f250cc18d3143cfe401a1e749a35638
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:38:23 +0000

    story uncomfortable fit long goon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
