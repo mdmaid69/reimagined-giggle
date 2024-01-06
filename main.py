6e27edf3f65c5fea5fec93093ce7c07db6ea6858
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:04 +0000

    under recent mixture view apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
