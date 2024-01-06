e5d3f4783276945fa4f6be54cd7f7cfba84ce314
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:53:27 +0000

    conclusion keepup uniform type herself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
