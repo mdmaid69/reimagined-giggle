821a9b50323f7cc78228e49bc6d96ef5dbf8b38c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:54:20 +0000

    who especially traffic board record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
