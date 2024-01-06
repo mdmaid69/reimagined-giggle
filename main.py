851fd5b89832fa3b35311ff94322f27e033f9d58
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:30:21 +0000

    Zz exhibition eye wear tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
