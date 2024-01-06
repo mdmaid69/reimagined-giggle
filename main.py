33d0af8a677ec61214e464e6f91cffe0f22caeee
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:20:43 +0000

    react breakin change bar comeout

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def delete_file(file_name):
+        os.remove(file_name)
