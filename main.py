2e01af11b2d7372a36508f855a8082c70fad5ae9
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:32:38 +0000

    example result dick bright bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
