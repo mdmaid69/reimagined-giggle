2a0ae04c1aa33cd3d67734ed8f7a30df7b140a71
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:48:34 +0000

    dangerous row feature sink picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
