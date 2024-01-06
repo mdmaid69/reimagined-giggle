911928029a6dce67c90f7205830bb985d0c1e2ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:05 +0000

    block about allow brown fear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
