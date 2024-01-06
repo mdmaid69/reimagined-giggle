315bbc201ef43bc01903b9b2cd371018d62ca217
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:17:43 +0000

    influence light still size exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_floor(x):
+        return math.floor(x)
