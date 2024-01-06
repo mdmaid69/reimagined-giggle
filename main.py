b2e872948f9ba52247c74fc4ac27c0396ed4a815
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:26 +0000

    smoke hollow cross anything either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
