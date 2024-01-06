9a75ddfcad4b585cad78c4a033903681a98a69e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:40:31 +0000

    stamp obvious measure statement bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
