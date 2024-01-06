7cdf7c26741d7e1249d27074f9812673bcd9825d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:34 +0000

    getup turnover someone collect jump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
