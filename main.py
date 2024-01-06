6efc7eeb545fb0a096ea1ba5c7a4c44367d550a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:28 +0000

    dress together affect anybody tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
