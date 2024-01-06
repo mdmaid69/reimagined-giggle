94f9b56ff1c3d18a2f2bd0b20109442f02a8cda9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:03:56 +0000

    settle perhaps Bb order I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
