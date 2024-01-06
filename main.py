17fd366a3770f779b715bc18cff20e19b1f310de
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:20:42 +0000

    deserve point best respect Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
