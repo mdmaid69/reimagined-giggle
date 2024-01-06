d76fed3fd52f96e8d82a74c53a13c570b32f272f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:06 +0000

    stay slight weak tear speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
