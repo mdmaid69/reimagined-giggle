4e4eab86121b89268aada6d47dc066296f212850
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:47 +0000

    pronunciation have basic 564encourage test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
