bb52071d24d31a24b3c6cd4841e873e426693a29
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:37 +0000

    wear ground interview note lend

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
