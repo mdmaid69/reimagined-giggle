a7860445c0789f97282ad76a0762ed7a3c9708ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:29:09 +0000

    freeze fresh lookback(on) bang bound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
