dda58d3b42f180bc70062fac856ccb794dfe84ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:04:16 +0000

    off can system ashamed possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
