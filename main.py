965b66d6feff4008baee571e1753798d91f3438e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:15:38 +0000

    find season copy plate easy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
