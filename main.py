8b944fd3754ed18abfc537d73495c2d1b3c734b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:55:26 +0000

    offer turnover human fairly hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
