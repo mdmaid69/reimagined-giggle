2c17afb35d8399f205e4f13bbc7b23b628239b03
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:42 +0000

    expression brief honest moment rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
