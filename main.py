7c6ecd53b433bf2f884260a9f5f718e1789369ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:37 +0000

    nowhere impossible use try actual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import random
-print(random.randint(0, 100))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
