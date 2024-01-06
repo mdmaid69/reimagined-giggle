efde8e6fae2f1187dffa33cdb614e3c9f5fafad3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:58:26 +0000

    tax fond direct opportunity private

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_palindrome(s):
+        return s == s[::-1]
