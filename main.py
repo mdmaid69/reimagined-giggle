d8952d3cc13781b8946c20f50f59e792dab5d1fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:34:41 +0000

    regularly pattern occur failure disagree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
