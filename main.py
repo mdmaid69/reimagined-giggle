f95ec448b2fc39900de62eb345f76c848c22f000
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:24:01 +0000

    needle while much solve term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
