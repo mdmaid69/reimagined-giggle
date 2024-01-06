021bc5dc48f26811f8c077e8ad8795b71bb13691
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:17 +0000

    onto prove make dish majority

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
