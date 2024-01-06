8ef5f73c52909ace7040fec390fdface23860b59
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:37:47 +0000

    film careful outside drive urgent

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def add_numbers(x, y):
-        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
