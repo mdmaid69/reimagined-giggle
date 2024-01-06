af8f02e95549887ba60a1acb80271040aac5b4eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:33:34 +0000

    through Dd unfortunate die smell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
