2a4de6626e0efaf847afd6bfa8b2865307a983a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:03 +0000

    stone excellent style answer determined

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_str(array):
+        return str(array)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
