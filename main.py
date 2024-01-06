0138a2053abce191a94dcba6420471ab9debd26a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:31 +0000

    machine disgusting admit beneath lookaround

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
