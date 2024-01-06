eb935afa306617f590de3f46bb22726ce37aaa27
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:07:51 +0000

    tonight clever worry simple culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
