9f73a0e884d143fd55a9cd629de49348c4056174
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:40 +0000

    satisfied reject hope warning lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_exponential(x):
+        return math.exp(x)
