3015a4e609997a9c8341a3c5d90426ed01ad9242
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:50 +0000

    same table greeting callout destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
