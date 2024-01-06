4e88015727f4ce13195e5596ef39368028ee8bce
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:41 +0000

    collection balance glass poison close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
