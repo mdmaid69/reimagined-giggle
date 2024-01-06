cc4c1fc31c3d0d4f73b3901b6223b4cc974b236b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:57:53 +0000

    we name name crash breathe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def cube_number(x):
+        return x**3
