9511a992950cdc1208a96f593a4307b5a3f865d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:39 +0000

    interfere grand hers kind behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_speed(distance, time):
+        return distance / time
