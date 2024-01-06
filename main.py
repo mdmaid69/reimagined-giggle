70080ff36eca23cbb78a634d3935e85835d176f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:05:57 +0000

    cruelty travel opponent future find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import platform
+def get_python_version():
+        return platform.python_version()
