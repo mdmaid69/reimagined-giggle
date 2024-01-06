2bf8a7d89046bae93ef236a7585a313e3e3f85a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:30 +0000

    reach tense cheque dick voice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def square_number(x):
+        return x**2
+def is_palindrome(s):
+        return s == s[::-1]
