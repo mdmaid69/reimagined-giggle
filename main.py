9c42769802e2ca2c914a3d77a8d9a8a37c29287d
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:05:55 +0000

    opposite swallow section sharp glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
