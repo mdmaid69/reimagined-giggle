b60f4e8e127fc8d38fd2f0d3613b21aba201481a
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:51:53 +0000

    any defend bring chief convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
