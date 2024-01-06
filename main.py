783b9fe3b775e031c78d203e998d106a0e44bb8f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:23 +0000

    tired this into public now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
