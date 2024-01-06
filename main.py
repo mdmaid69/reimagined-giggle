d588af4ecb59617c1187b25af72726a4607a5137
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:45 +0000

    pause mean also check distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def add_numbers(x, y):
+        return x + y
