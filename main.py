630ec9072845c283721ff8f4c1c8eef078bd756d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:30:07 +0000

    sign agreement sea beyond do

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
