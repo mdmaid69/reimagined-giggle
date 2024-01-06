49903a12a8a39eb75b8e585f285c998a6513e7a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:31:58 +0000

    suggestion mend pile equipment basic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+print(sys.version)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
