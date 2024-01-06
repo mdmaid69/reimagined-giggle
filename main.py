82981c17c2a7e4932eefdcd43b7f08f5e0fe3ff8
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:05:38 +0000

    over paper minute port independent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
