f37a31c793069aa7541aabcb072b7678503e92dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:00:01 +0000

    pen escape why half slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def multiply_numbers(x, y):
+        return x * y
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
