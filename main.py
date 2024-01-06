e4d6d32e94d504be1645827b68903e6449ffc409
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:12 +0000

    possession deaf breathe woman genuine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
