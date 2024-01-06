10b15760043d7d578584ca5326df3b1a487939d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:35 +0000

    weigh drink building light turn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print(sum(range(10)))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
