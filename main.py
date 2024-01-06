b6cfa38b9bfd53df23c2497cdc703d01849e8692
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:22 +0000

    study moon prevent slight them

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
