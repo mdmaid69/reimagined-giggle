25a4dc183bdd3e3f252e462928e20222de063862
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:26:04 +0000

    mean unlikely tonight apart object

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def reverse_string(s):
+        return s[::-1]
