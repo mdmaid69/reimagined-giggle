d1e3f58722dad63e029e075b89d755f7d6e3476b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:16 +0000

    message pity deliberate common mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
