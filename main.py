3f292db8057c40a56e060c54008e45c6405b96dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:38:02 +0000

    round block here attract slow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
