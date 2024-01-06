4a1accdb75ea4638abdf12344e172f30b96a9d29
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:16:57 +0000

    age regard consist strike typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
