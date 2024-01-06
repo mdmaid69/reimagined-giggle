8b8397b25dfb035cec4da1df058207ec0569d528
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:54:20 +0000

    threaten shape runover manner fact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
