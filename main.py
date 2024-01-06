2b2bc040674f8725187f860df2457d09f644db2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:59 +0000

    evidence heart rubbish estimate seriously

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
