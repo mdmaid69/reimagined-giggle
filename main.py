001a3d26a1eef89f8a88fc0bfd59e9feb271a9f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:23:54 +0000

    trip onto injury surprised later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
