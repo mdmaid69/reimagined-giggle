90d952eceb0c49e599ff9979328e2541e2cae882
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:03:10 +0000

    escape vision enthusiastic estimate future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_username():
-        return getpass.getuser()
+def subtract_numbers(x, y):
+        return x - y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
