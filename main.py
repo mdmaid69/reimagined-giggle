b9e40bbee0d5955cea9aac9cff4ea93cf6d9fe75
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:47:45 +0000

    closed extremely criticize bunch mistake

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import collections
-def create_user_string():
-        return collections.UserString()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
