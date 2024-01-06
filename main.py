2c370748987cea48913505523e6398278f753bf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:33:08 +0000

    flight help fresh near central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def reverse_list(lst):
-        return lst[::-1]
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
