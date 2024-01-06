d42b21ef622231d9b049f4b902033835f68e75a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:10 +0000

    keepup shut Mm spot save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_sine(x):
+        return math.sin(x)
