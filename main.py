28cdfcf3fa01b71ec0c0693f4fa195a7f1078d13
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:55:51 +0000

    oil meanwhile search bunch rubbish

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
