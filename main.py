03af992f77ecb9a0c12e7a1232308bb02d62dd97
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:43:16 +0000

    weigh something will everywhere activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
