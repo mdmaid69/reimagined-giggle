0f865c1ec4f8709126f3ed24aca9fd87200ef908
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:01:07 +0000

    terrible star until private back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
