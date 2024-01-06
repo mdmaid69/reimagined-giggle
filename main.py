df4ffe6f0630b48fecc7fd0f61fd51656d6bd645
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:55 +0000

    cinema fine grand fierce wake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
