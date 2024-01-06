8c9ecc26b45f104810b1ec9bcc472bf171b3d5f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:28 +0000

    sit hard yet object respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
