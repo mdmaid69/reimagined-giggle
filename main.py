995c26ba3793d7786ca7c744ec694a70b4fa6adf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:20:13 +0000

    politics scare public guess bye

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
