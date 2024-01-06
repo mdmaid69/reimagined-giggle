59c5923b7ded80243cad7f8ff45ec450b6bfafae
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:39:53 +0000

    begin ourselves favour think meaning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
