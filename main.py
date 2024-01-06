a97f3788dfb8bbdc7d74fdada3bad81619dee9a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:41 +0000

    square do waste urgent hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
