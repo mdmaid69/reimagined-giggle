d246c7dd307d7fbc98272e4856781b7c4df502bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:24 +0000

    able near arrange describe flow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def clear_array(array):
+        array *= 0
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
