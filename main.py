39eac962f445bef77c10ae85620f3da3c520b225
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:54:53 +0000

    cum difference thankyou tie holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import random
-print(random.randint(0, 100))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
