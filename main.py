2d9d6fc4a7708c569e0cd57f833c8499eb2a5172
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:35 +0000

    spill who identify dear file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
