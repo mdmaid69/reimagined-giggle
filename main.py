9ea2f39306c7630b9431dd30d8c60c1f53b20cf3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:31 +0000

    Cc sensible height public finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
