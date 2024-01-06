188d0082cea6b80aca97562c4022115f28aa9f96
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:16:28 +0000

    grand safety welcome prove beat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
