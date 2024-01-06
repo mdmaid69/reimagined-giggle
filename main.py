a46c51f298bfff994ec01fd01cf9ff1d7039e005
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:49:27 +0000

    notice pile impressive light Gg

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
