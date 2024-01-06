71db426bf62d80b85e47da798ce2bd41a6c07ae0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:30:57 +0000

    top come breakdown modern arrangement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
