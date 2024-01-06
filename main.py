7875d0769192d962bcb38c81cdabeaa5df27607b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:21:01 +0000

    wing smart goon lot apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
