16158b9615207c0cdf484d673374e043f33886b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:45 +0000

    picture towards dream excellent produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
