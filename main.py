f540d5480995e210c150e0df2927dc400409c008
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:47 +0000

    journey copy recovery trip glass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def clear_array(array):
+        array *= 0
