b5d58a178082cb7b22c98b4752628e4ac3f1f642
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:53 +0000

    wait everything concentration start sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
