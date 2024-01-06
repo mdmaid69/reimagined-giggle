0cccb9ef746908ecaabdb0efa4dc84b96db02b21
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:38 +0000

    bar perform sound label best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-def get_current_time():
-        return time.time()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
