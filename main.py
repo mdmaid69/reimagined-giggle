bbb4f4a60025e1d8ebb95e32dc76182c6dfff0e9
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:12 +0000

    uncertain cheat new sorry reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
