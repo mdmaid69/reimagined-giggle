da332946e249105f30e5c5d33ec7495dd569aece
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:58:14 +0000

    freedom blue ago legal day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_elements(lst):
-        return len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
