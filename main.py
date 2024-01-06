301da5486007a1099f8133045cddff15c35238fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:59 +0000

    typical strike shake fire recognize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
