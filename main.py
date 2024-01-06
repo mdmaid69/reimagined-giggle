d076b7d818c689f65ee237fe940703d4f1d85ae6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:15:08 +0000

    straight sound crash odd deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
