902283ddd6303e8eb3a4550593ee5d1971c675bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:21:18 +0000

    proud relative feature need get

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
