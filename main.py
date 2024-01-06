691319afd25e2137e6ddb4ef20d7ae4cdc462667
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:18 +0000

    used neck area service admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
