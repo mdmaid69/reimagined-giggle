b79705fb00fdf44e335def3e8f1345b830920332
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:08 +0000

    hope bet design characteristic eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def remove_duplicates(lst):
-        return list(set(lst))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
