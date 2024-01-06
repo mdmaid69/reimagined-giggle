bbf205afe69ae3ac104667fc1ad8b82a930ea4f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:29:12 +0000

    quality record funny plan people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
