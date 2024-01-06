b04408ca1f54c80ee19d34ac4b9a621fc77678b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:16 +0000

    otherwise godown pleasant here yourself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
