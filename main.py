68a0630d4a62bb06ff933c8814bdcbfa49da2ad1
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:50:55 +0000

    flat ill damp direct head

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
