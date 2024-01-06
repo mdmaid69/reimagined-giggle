20d07ac2cd152d5dab51e0ef17cf3ef5c3f23264
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:21 +0000

    may cry sure responsible serious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
