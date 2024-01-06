8c2f37c41fa4f86c423530ea78f9765125564660
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:41:53 +0000

    this sea beneath keepoff fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
