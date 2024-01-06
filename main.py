d1df3463a0e7e2d0dab571fc3546aff7dd9bc417
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:49 +0000

    warning there drink getoff back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
