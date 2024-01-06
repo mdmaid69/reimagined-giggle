d4095a9d48066d0382ebad6ddc6d9d919975a747
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:04:37 +0000

    impatient flat sting mistake recommend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
