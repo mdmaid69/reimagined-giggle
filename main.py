8bf68881432caa99b88db89f5f3d3f357185e7a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:27:42 +0000

    rather carry will fit extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
