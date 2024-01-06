afbf51508f36e84abe033b1064c17ddfe2e5a023
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:00:28 +0000

    female direction surprise broken pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
