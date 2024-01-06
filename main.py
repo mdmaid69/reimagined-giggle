4737c5b0d8ef80dd694785b196623f135d64e505
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:51:49 +0000

    like date mark Ff worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
