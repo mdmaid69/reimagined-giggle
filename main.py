a7d5b64dfbee9c7c3416604fb505faf3ce535510
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:04 +0000

    also fresh keepfrom let shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
