3b106a5a3ea20f9376e07faa4c29443c07ff80de
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:31:36 +0000

    so unfortunate environment action talkinto

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def is_even(n):
+        return n % 2 == 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
