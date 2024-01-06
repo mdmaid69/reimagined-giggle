0b675f49d0054d4ba1a1ba251a37841ad345b432
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:48 +0000

    ask system summer send declaration

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def count_elements(lst):
-        return len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
