6f370e51cb5cc4753588872cae80f0553930ff7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:08 +0000

    hide friend embarrassing tour shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
