96fbd53d324eec63d1b11a79445434d56e32b64e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:42:16 +0000

    holdon block she pattern fairly

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
