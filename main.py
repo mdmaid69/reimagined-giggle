869da0166c2c7e2e1cfbebdb6a7552ae76a964b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:26:08 +0000

    even his happy nowhere warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
