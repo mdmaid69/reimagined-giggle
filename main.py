ac91dfc2c5ea9a05593404797ef2fd165091ba4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:46 +0000

    true allow bury might up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
