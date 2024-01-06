1d0b124a3c2155acb5143b17e492a8d5b40612f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:13 +0000

    that community walk maybe try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.time()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
