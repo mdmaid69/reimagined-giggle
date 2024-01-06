1c521da6294beb3b788df8b9fd46acb491fff68e
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:01 +0000

    fit repair accidentally plan need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def get_array_as_int(array):
+        return int(array[0])
