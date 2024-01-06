994531ead462d2c5001b9bcfbcb5e0c577a776f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:58:21 +0000

    cold worse list story bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
