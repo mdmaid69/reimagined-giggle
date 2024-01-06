ed523a0499eb6e76eb8e848820261c67fa76f8e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:17 +0000

    drink compete I bridge takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
