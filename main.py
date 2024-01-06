9c90b9f7ffb8fdda3eda93f2d571c91f85702f9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:21:47 +0000

    war send immediately confident summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_float(array):
+        return float(array[0])
