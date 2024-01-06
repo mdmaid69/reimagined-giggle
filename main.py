70c388bb060e9aa3479166b1fb51355bd9bf4790
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:07 +0000

    war accidental along hurt serve

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
