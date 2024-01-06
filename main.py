fbb6f75348d6358250908276dad6a7c907abc110
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:48:27 +0000

    doubt tell hers dinner side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
