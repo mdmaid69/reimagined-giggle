8c8a2b07de4c6928288620277b27480ba219042f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:52:43 +0000

    gift time then glass calloff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tensorflow as tf
-print(tf.__version__)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
