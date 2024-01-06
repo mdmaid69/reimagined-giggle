d6188174ab5426727b9c4bf4959e1a0fb36551fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:46:37 +0000

    while cross style spread power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
