03a80ac731dcd9d18200a6489b29dcd2a290d9cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:40:01 +0000

    short space damp description pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
