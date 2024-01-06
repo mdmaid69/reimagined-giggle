7abf461b464dfebc4be9015509d6e6970ec6b944
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:07 +0000

    please split slice upset safety

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def sort_list(lst):
+        return sorted(lst)
