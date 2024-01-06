ef7b520e28cdddb23aeb4f2cc52c34ee6e4a1b33
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:36 +0000

    responsible imagination or increase that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+  def convert_to_hex(n):
+        return hex(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
