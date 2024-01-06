e7d6d74bbbd65bf7f22258584ce58347a14e0cee
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:09:56 +0000

    popular confused suit owe belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
