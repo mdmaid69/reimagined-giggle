6c13e6e661c858e8bdd5cfa2ec89388549fb7520
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:45:08 +0000

    valuable move pleasure runout listen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
