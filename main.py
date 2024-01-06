f8f0440dce724c7abcd991bf3a16cee833da07e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:54:27 +0000

    fashion treat lesson breakfast pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
