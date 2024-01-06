03220b522b84a5416670f44c20a65e3adc19569b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:07:11 +0000

    art check slice advanced behave

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
