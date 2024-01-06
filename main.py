7421dd324a9049d1b192f46ec47b993a93596d5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:05 +0000

    healthy no none government pile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
