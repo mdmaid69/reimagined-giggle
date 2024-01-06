47cf56ddc8aac45ec91d17d9d2f1f20b18fa31ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:19:47 +0000

    soon grateful answer suit direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
