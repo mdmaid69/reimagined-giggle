147b059fb46ccb16e213e5598a9726124414a757
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:24:02 +0000

    mother science spot tradition question

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import datetime
 def get_today_date():
         return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
