470d4ed4bcb1725a7e44b10970e05aec9c3c9ed9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:53 +0000

    careful aim like suggestion travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
