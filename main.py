257d0b129c7964045ccba3e0bb6a87d5ecc55a28
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:23:48 +0000

    beginning couple computer preparation ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
