e38f31f7f3ba1697cab39b192e3bec36287181a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:18 +0000

    late destroy crash 568energy operate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
