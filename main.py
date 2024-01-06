01442fa044b298f364889e74086f53e6df73c768
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:17:27 +0000

    path task shine meeting warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
