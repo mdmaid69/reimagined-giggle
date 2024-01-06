a136aa4a73cd1da7b1fe30bd12a6f98596cf185a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:14:34 +0000

    water drink fashion takeover confusing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-print(sum(range(10)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
