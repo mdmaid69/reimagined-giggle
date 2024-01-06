f5fe29bbeccd094a4b71ae2c397e7dec92383e6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:51:16 +0000

    influence miss lie mistake exam

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
