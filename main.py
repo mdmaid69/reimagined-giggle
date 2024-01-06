b02de7a791193059d5b4002f911d22d615ee6c15
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:17:45 +0000

    permission man turnback phone switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
