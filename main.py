d7fe85e1cbdd99eeb59b31d5733b736066ecc978
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:33:35 +0000

    comfort break never actually maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_python_version():
-        return platform.python_version()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
