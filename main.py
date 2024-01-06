df405a1bae506df401ef51ac28259cfde45c7777
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:34:12 +0000

    fire sound deliberate runover announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
