cf2487d55c7860c4193b3f7ac32f7fc47fc6adea
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:16 +0000

    card happy group top quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sys
+def exit_program():
+        sys.exit()
