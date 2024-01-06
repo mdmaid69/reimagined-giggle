8ee580d09fbc8b9327976da3d4de4462d24d36f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:42:39 +0000

    earn repeat anyone niggaacademy permission

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def count_elements(lst):
+        return len(lst)
