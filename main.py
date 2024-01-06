82d90e5283009ae4f810800cd575a080fb14d945
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:30 +0000

    complain model less steer handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
