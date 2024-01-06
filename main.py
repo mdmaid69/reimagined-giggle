1359450365cd843e24dec0244971eef71b9559b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:25:06 +0000

    prize invitation hers advanced spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
