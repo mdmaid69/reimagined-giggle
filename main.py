a978859bdcccc5945edddcf62b8a35e42f6eb90d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:22:48 +0000

    sale star pause throughout autumn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def get_array_item_count(array, item):
+        return array.count(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
