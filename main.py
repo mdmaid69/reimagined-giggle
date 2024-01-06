832235e7e730445fe128ed7ad854386c59039b4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:56 +0000

    protest maybe afford sometimes impression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import numpy as np
+print(np.array([1, 2, 3]))
