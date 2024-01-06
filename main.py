6099f4ab4a7e99500377c21d384d132d18ff3ebb
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:23:25 +0000

    consist long count trap relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_speed(distance, time):
-        return distance / time
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
