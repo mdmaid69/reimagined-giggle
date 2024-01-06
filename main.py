8684d13adf8cda64c591702a131c3c1d4e2d6805
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:45:30 +0000

    new temper vision demand standard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def square_number(x):
+        return x**2
