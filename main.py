c5b5abf23f72780c24a5f24c2c05f3a6fdc3b275
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:51:38 +0000

    false thorough sore little size

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
