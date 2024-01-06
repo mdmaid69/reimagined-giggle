67d81121659ffd3ca15095d8209a4b42a9e3b8f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:37:14 +0000

    later promise loan suit notice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def clear_array(array):
-        array *= 0
+def find_max(lst):
+        return max(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
