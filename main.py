f835576a2a8c8e906d7e28a421de2541bac18452
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:53:42 +0000

    if ourselves away practical beside

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def reverse_list(lst):
+        return lst[::-1]
