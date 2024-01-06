7fcd20c615f06aed203ddc34ce98d79c5ca11a4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:29:33 +0000

    leg difference like flow pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
