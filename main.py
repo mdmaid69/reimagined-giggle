6f5ef63514cbd23be2d5ceda8bd82fb600d19cb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:48:03 +0000

    name upwards get once steep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def greet(name):
+        print(f"Hello, {name}!")
