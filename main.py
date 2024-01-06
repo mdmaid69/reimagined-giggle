9f7014575eadaaa578894bda858a2951bbbe1ef0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:13:58 +0000

    form content describe character mad

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
