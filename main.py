e8b64b0811be64b1479c8cfa61db3e732330c68f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:30:33 +0000

    comeout feed one cruel error

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def multiply_numbers(x, y):
+        return x * y
