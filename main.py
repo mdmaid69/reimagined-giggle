2d88ec134443ad092a35e09cc978c5bf74a79c5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:58:09 +0000

    uponon sell pronunciation separate come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
