769f18f07bf2b33fc87d57c6fb59c5fdcb4ef7a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:47 +0000

    ticket window where question switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
