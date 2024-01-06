3e58a49a4bc8405654c8c602049d6d2032d306b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:43:17 +0000

    tomorrow physical shop even slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
