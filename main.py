789d02a4cbe710fed286ea452cdb3113cf1c3fb2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:48:28 +0000

    guess complicated else Ll protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
