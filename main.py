0ad5790b3458ed02a6a5924a3388b87009625ec1
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:32:35 +0000

    fix protect relationship innocent notice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import re
 def replace_pattern(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
