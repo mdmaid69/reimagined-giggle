d44902224726c8e61e9611b3edcc26793b548122
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:32:11 +0000

    right bet poor wander corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
