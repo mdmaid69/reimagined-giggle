3f9cded93ddfec4037fd7c978955be7d778e2879
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:40:22 +0000

    edge interfere honest extra piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
