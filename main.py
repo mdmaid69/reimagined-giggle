0ebb81fd85357d7afdc89118f6fbd3fd410e6eb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:57:12 +0000

    block no comeback but quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
