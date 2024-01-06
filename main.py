2e267a8a7668bbf963a372eda02619b78791b932
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:54:19 +0000

    pay fork custom nice distance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
