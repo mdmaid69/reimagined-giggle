da9804a0bb755740e224863e5b75b17a9f80cb8e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:00 +0000

    substance keepfrom record material cover

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
