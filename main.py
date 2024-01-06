f67b078f043943fe26fb8685086e9fe786da0426
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:43:32 +0000

    heart copy public design nervous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def clear_array(array):
+        array *= 0
