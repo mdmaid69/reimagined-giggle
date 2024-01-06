60fdceaf8e24824c4a356b6c0f58ef1859e7059a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:33:36 +0000

    see least regular chance bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
