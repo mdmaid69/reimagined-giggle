c97e30190220a8074cb0ea3421b16767ee1bfdbb
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:28:33 +0000

    exchange through shake impressive strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
