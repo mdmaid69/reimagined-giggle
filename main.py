e553dae5ec023c7a600a59ec74cf3305618389da
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:51 +0000

    cheek born whether power bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
