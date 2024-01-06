30b02ea95a592e01ec2aec43efe6d0fc67816fc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:05 +0000

    same cry butter measure once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
