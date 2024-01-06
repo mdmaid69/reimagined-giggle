d132ec2a8727e01ad66d03d598730f0b8a07d4ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:43:46 +0000

    consult mainly price difficulty offer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
