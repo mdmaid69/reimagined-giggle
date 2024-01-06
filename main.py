ca0c1125eb6470289ceacfe4c572f3d60d43437a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:42:23 +0000

    mouth skill second clothes upset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
