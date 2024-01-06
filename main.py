cdf294fc28ee6b3ee50d2be877b0f569bb1092dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:30 +0000

    nurse pipe practise cook go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
