12b991f899b1854eb4093abea51850227885ef4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:16 +0000

    question rest separate possibly upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
