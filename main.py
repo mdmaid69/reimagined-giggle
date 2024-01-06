1743d56bad12d39095e6964e0c3267a2f9de9a9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:18:24 +0000

    carry tongue shrink rather represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
