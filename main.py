280340dca6a5b5c31ae9abf800bd2137585fed92
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:27:56 +0000

    row keepup deal my warm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def multiply_numbers(x, y):
-        return x * y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
