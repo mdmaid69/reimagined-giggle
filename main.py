c06a75aef12b453c4335bf85a72de86e619820e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:49:07 +0000

    pause legal rough recently who

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print("Hello, world!")
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
