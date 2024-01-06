adab94595dd67808c3b65350ce4c7e64e21d169a
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:52:10 +0000

    run next exchange little die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
