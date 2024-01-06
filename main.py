5077959b858a6aed4d024835f0b25b0fed45f886
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:36:49 +0000

    year trial bank wherever satisfied

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
