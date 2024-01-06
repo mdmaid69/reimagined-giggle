dd10d25b492e8a842939a123d31c77de1f50d878
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:46:54 +0000

    swallow about sharp major uponon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_length(array):
+        return len(array)
+  def sort_list(lst):
+        return sorted(lst)
