6b50d628a2b33cf09f2f1783a2d4ec241ef7a917
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:45 +0000

    to sit yes lead holdon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+print("Hello, world!")
