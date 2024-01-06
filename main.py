4632ce7868e7aed705493e170ddfb17b863fe46f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:45:15 +0000

    unable ball turn priority badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
