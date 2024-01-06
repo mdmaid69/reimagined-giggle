0d53ccfd51d1dfb2e26fa72b3a3f6050ee31dd43
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:43:05 +0000

    chase quit listenfor because cap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def convert_to_binary(n):
+        return bin(n)
