d25e9a2704700a297e6cb0997654affdc89515f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:37 +0000

    thing slow contain matter say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
