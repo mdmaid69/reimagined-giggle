ceb3464a304c0ac435695abeac4fbb043cd08dbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:09:36 +0000

    standard better score able head

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
