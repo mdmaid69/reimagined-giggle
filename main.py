e62cb24f202bb8a0e997d95386cbe04ffb06f411
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:43:57 +0000

    explain start anybody fine birthday

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-print(sum(range(10)))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
