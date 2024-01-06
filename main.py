e88ac1b23cc959d1ddf75f4eeb1b9ec00182c217
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:52 +0000

    ocean pull we wherever wander

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import platform
+def get_python_version():
+        return platform.python_version()
