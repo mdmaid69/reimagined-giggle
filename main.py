d84f553b965938fd58ca607417a4b6d0bc436d93
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:22:46 +0000

    your speed issue gas platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
