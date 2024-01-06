c11b716ca6bb970abeeff3d9875ccf80c06d3a6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:37:10 +0000

    middle far moon trip while

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
