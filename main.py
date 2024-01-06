44d5aa0c65943608ccfc511e3fdff8fed809034d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:31:51 +0000

    smart recognize sorry open greeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
