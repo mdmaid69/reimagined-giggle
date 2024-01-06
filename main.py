8424a1bf7acf43ca9494fe65f29831d2eafb689a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:09:27 +0000

    comeoff office favourite name side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item_count(array, item):
+        return array.count(item)
