f0eb38a17a27ffb316183e63f074fd7568fd8829
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:56 +0000

    answer divide doctor noise alive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def is_palindrome(s):
-        return s == s[::-1]
+def get_string_from_array(array):
+        return array.tobytes()
