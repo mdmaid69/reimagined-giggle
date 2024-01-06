9f87cafc77259ffd0a1ffd680d90ff919716e7a0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:59:32 +0000

    rare thumb simple noone look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
