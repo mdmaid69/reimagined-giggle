29c46099f994ebd09cac320186f93dcb031a52af
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:11 +0000

    present even fair aim climb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
