5c79687acb19249cdbac17e1feca6cbd52b65676
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:17 +0000

    borrow authority on opinion available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
