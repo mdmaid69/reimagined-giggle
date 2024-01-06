ed30c13f51d6cc4d96bbc2517f4735959bd730b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:57:51 +0000

    likely way approve since deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
