d3b2256e654772348ebe46335f3c6d0ca654d983
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:41:17 +0000

    solve fall rise neat grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
