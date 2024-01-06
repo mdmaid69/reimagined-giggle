f87ceac4b274910459270e760c5688b9a474d3ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:04 +0000

    mood story reduce dream wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
