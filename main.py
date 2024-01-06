d1657e2ce16b475166f6b87d08194cf53cfbea85
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:42:42 +0000

    from all plain contrast more

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
