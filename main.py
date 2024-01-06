b753edb9d47a153f29d11c970b0c88717a24ce20
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:06 +0000

    life visit right you proud

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
