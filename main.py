36e688cda7e21b3cf8dc47b486c207282081d6f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:38:10 +0000

    naturally snow callout recommend car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def cube_number(x):
-        return x**3
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
