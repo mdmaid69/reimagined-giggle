d7e0d6c7ad4900a97d63d71b0f1aa6c00d54d768
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:55 +0000

    onto opponent relation watch scheme

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
