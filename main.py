485c710935b539ac4a4ec731d565cbfd8307854d
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:11:43 +0000

    rich cost nose control maybe

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
