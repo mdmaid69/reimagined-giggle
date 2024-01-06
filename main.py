c8d33d0881e55dcf5ffb16a9981259d939331351
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:46:26 +0000

    cross belong shape plastic over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
