c4f27e9d6df469d7b474bccab0e1c3168e496ba1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:57 +0000

    warn picture now discover out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
