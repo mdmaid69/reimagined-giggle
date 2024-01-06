4e89ec5e7e90739b806653c6cd73f454bee71f3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:39:46 +0000

    564encourage powerful acceptable loss rule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
