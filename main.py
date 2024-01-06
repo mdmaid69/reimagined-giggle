fa401e7177c467c439731abc65dee7dd4a422849
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:23:19 +0000

    admire break medium closed measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
