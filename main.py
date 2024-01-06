5c50c55086aa33e110d7a450a015d297c5fc1843
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:34:39 +0000

    mainly prepare track operation equipment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
