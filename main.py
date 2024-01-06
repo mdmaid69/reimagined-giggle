2c6793e906220159df6d218b741c9b6ebcc8db19
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:51:48 +0000

    peace moment introduction tip difference

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
