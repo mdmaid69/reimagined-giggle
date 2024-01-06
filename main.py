37346799563f4b06d2ec3fda9f70091c6c361b06
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:25 +0000

    tongue they fit information badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
