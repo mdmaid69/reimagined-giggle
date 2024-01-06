07297ee27444a6be12e4ed9e715fa7c850a4b09a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:26:17 +0000

    object confused immediate regional damp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
