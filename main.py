696e503608b3ea8638a411a350efab110f5da4ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:28 +0000

    join another figure call tour

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
