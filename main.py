7e9b95ed64f35c518a5b33a2d71fd2c26d55c402
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:48 +0000

    warm convinced belong butter turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
