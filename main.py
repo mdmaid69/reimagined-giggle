17fce18f00e3e6d5d47ebf9b28666da3f44d1811
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:13:14 +0000

    purpose joke full reach suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_exponential(x):
+        return math.exp(x)
