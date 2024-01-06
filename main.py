99649b58efa16a8a2f7b4b16d696fecd054642b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:10:48 +0000

    bread transport show majority everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
