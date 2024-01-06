8c3413d4901c9656ac4721d76640f8a7fea38bd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:21:56 +0000

    occur need sky whichever wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import os
 def get_file_size(filename):
         return os.path.getsize(filename)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
