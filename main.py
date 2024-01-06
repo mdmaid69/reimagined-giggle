6c9eff93ebf622570018f833b0ad264253c9c604
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:59:26 +0000

    another air both claim bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_work(force, distance):
+        return force * distance
