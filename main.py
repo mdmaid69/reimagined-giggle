1f8088791f800e6d77c0b7b9a9ad47c28ac2b474
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:58:06 +0000

    area suffer score cheque recover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
