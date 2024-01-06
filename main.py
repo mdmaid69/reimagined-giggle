5466aba5940cb4bf7a84590f06dfa65cc3ba5443
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:22:51 +0000

    callat first whistle interest song

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
