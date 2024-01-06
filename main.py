2e17d98385d1b8dad1a42ff1e026b7d30cbd86c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:12:36 +0000

    goaway welcome write consult patience

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def convert_to_binary(n):
+        return bin(n)
