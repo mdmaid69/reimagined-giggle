6a70926d90a3cdd1f5c6e8dfeb2344c3cf509887
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:12:41 +0000

    smart smell whistle then Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_base_name(path):
+        return os.path.basename(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
