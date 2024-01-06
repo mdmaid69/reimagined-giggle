68bde2c1c7daba287e0f3c15cd388218e83bbf98
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:53:25 +0000

    child usually blind own gooff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+def find_min(numbers):
+        return min(numbers)
