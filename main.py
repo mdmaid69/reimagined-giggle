2e1c480fb399c6abb6de62ce189a1fbb193f51a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:26:20 +0000

    label shopping lookaround hate beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
