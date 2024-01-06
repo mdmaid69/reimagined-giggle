014a07a43bf8b3664f9d9cd305dc8678130a911c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:49:25 +0000

    aim paper brain train never

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
