abbe860d91cad55e6571d405c00090bd872e32b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:01:03 +0000

    fear touch trousers public 572enter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
