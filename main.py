f9d1793c9465058a077439c4f40e7e3f07df8a73
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:10 +0000

    machine face let manner undo

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
