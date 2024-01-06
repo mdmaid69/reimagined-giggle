b51eb4312d8fef1c4d420c0f46f918ebdc1eb52e
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:06:54 +0000

    wise dark scheme discussion wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
