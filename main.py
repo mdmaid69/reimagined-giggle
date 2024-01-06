62dc06902ff9e5c350b40af4aff3d2fdd4b90f4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:55:34 +0000

    exact worse exaggerate win education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
