232290b2136064a0a3a1fb626b3085c04f3cf742
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:55:33 +0000

    subject character refuse know question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def append_to_array(array, item):
-        array.append(item)
+import time
+print(time.time())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
