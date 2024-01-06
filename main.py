492ec7e84a0c735fa75a8207651460af1ccd06ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:05:20 +0000

    lend about behave clean second

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
