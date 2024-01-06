d891137403fcd6c811586bde2db9097bd2676b6f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:16 +0000

    view under fast comeback exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
