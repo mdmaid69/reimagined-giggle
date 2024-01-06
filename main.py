7c8591a9f935fd76f972297917f64955ff5aa760
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:23:22 +0000

    wish correct split not challenge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def sort_list(lst):
-        return sorted(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def multiply_numbers(x, y):
+        return x * y
