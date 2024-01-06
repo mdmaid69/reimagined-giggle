e676af82f6991d3d1e77883b5c80d2a3cb0fef2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:51:59 +0000

    stupid surprising careful centimetre quickly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_speed(distance, time):
+        return distance / time
