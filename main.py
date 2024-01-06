de0a656df4f03b1d23a3d5bfaf2203c4a359f0c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:30 +0000

    clean need seat decision lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
