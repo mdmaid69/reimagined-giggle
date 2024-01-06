41be021ddf620cf2d97cb5b4faa8b81ff722baa2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:56:47 +0000

    smell translate land through cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
