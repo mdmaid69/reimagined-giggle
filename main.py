2d2f73c4945f3af91e07bd8302507f6e074818cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:20:48 +0000

    their basically goback(to) breakup share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
