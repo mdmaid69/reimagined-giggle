9c32877bb6af5acea1a2be8a4cbfb80c070f17a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:47:42 +0000

    minor embarrassing term sing operation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
