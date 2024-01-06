25e9591159632e6d0b0fb65f8c942ace0ec421a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:49 +0000

    worse age really hold along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
