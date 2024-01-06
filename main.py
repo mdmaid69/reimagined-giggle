fe83b9ba2b3abbc7cecc54a2517d8c7bc194350f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:34 +0000

    reaction find noisy during invite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def square_number(x):
+        return x**2
