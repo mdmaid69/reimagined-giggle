b680225ed4d10ddf2bd8c76fa88587ca3c54f48e
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:31:41 +0000

    interest dull gas flat minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
