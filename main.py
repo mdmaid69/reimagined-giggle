b94170093fd4138b34eb14b8c4979fe92fbabb60
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:31 +0000

    wrap unfair anywayanyhow thankyou yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
