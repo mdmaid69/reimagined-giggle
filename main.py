7e1aa87022c6f242818d6a9e1179648c7d29c340
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:30 +0000

    over string contain party use

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
