c99ac77e8089dc707fa4744d10fa1bc80503d811
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:12 +0000

    union lookafter model comeover rare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
