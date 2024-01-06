4cbb1afe3bd56e7f8ff43e9cc1c11be70b01cb29
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:10:48 +0000

    glass make principle belong nowhere

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import sys
+print(sys.version)
+  def is_even(n):
+        return n % 2 == 0
