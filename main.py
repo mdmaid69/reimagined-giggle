1385cf182f913407f8cbde768814b5bd6187e041
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:10 +0000

    long object wrap appeal chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def calculate_area_rectangle(l, w):
+        return l * w
