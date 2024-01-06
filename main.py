53199ce21b40856a6a8acbc14ec4071044318b69
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:42:25 +0000

    any typical earn congratulate exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
