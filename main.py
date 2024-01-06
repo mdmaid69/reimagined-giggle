c5b00a91ff3a61d3039aef11a54a5b087d3d2f44
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:08:10 +0000

    could suppose correct none lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_pressure(force, area):
+        return force / area
