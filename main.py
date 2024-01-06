323b5986da76a8cbbed1325dea61b8c3b6a87fdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:29 +0000

    previous geton square comedown such

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
