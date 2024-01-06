2854ad6c2fa6414bf804a39b715babe8e5e5ee51
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:26 +0000

    length bringdown search he mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
