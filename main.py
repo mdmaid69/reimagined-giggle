0d5a00d852a584cb8475baea1b0995dd2a68e5da
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:13:56 +0000

    unfortunate remove offer mail holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
