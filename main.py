6795ffc04ccdeafc32a564826c7e96a66251c9f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:33:48 +0000

    further bridge gather else negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
