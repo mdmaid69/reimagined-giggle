c1c3d8fcbc265fede67428cde6b3ab754aac1292
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:25 +0000

    escape tomorrow circle example apart

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
+import sys
+print(sys.version)
