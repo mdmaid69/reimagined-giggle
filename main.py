db9749bd7b9832b794e4cf4e82d230bbabd153b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:35:05 +0000

    uncomfortable clue this out reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
