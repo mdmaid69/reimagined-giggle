c02d63ed5a9d603de26ed6814cf8541735bf1ae5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:38:40 +0000

    lift over none man collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+def subtract_numbers(x, y):
+        return x - y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
