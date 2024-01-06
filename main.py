8e75c5c697e5d7bcc6f660e0312ac201d234c076
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:07 +0000

    laugh wait top sound arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
