c9563f83649f0f85520d6b762f46518cf9b53226
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:21:55 +0000

    excuse inside culture shock welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
