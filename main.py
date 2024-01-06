4cbad619b65bd248bf447b19181edb396b069718
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:28 +0000

    slightly justice consult wind effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
