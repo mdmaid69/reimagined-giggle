eedb9cd8935b9c4b6e5abd18c5c9a6650c5bd742
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:32:25 +0000

    thinkof callat zero scale regional

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
