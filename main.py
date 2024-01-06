ab41c563b407c2fbbc861f8aa4a789bcfac47559
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:19 +0000

    hair lift return trust actual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
