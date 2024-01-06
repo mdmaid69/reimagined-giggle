5a2df37c2ee21b06676df1d37ec32004d4a8c5e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:59:17 +0000

    again wise imagination save classroom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
