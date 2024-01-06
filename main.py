ae9da199f55ba1032fd0183e7b40b2b19cdd8307
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:09 +0000

    punish dear contact dear dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
