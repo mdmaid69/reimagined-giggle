9aa1c75edadb6b3076f75a06c81f2e1b6758fd4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:10:15 +0000

    right plan holdon switch valuable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
