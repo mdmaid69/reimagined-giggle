a34db6f37dc195fa3dbecbc863a4e7bd3f580bea
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:43:41 +0000

    tread point grand describe holdout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
