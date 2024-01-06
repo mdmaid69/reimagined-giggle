28e77cc4fde494f753f265cffef5bb8e51e8def7
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:54:43 +0000

    and goaway degree best union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
