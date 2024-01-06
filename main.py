f9925c1a9bfa02ad5b2bec7dba42ed2311e45026
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:18:55 +0000

    belong common block recover direction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
