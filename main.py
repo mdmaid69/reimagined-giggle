90598647594484636cb53064f4ad65c99184c568
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:08:37 +0000

    transparent commit health tell takeout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
