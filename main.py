1aa68f4f4c015052f5ad9012c16b1a0d85e02295
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:20:44 +0000

    story cross straight sort confused

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
