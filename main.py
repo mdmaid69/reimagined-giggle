be9689c37f02feb3fce1b220a71c751bdbeb7718
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:17:14 +0000

    task news black brush rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
