4e1161ba2ad8650d3ad58f85db5e0c1259120cae
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:12:09 +0000

    shade offer false convinced downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
