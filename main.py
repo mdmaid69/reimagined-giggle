f3fa79ddc89d96dcfee8941ef4074744a60bcd11
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:28:17 +0000

    considering mass fashion cheek special

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def cube_number(x):
-        return x**3
+  def get_current_working_directory():
+        return os.getcwd()
