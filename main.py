5ab39441ebffd9c41e99ae82d07044012eef940d
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:09:08 +0000

    reduction tough the rest offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
