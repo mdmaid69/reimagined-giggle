92669cc5188deef464add95cf41784507bf3aed7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:18 +0000

    save his occasional woman holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import sys
-  def get_python_version():
-        return sys.version
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
