c81553a7d5374f85d719d9833f09d47092f680ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:13:12 +0000

    hit contact combine car Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
