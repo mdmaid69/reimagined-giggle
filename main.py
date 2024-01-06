9d2a9eaa359edbeb908f040d3faf5421dfe8f1e8
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:39 +0000

    blue large top edge fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
