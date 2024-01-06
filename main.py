20ad800d32cb205e1829e21f66df99feef8f9e64
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:45:44 +0000

    compare level Hh cold appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
+import sys
+def print_python_version():
+        print(sys.version)
