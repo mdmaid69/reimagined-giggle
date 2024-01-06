a224e50bbdd66a5b65ea4e867e4082d02e30c61b
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:43:10 +0000

    find valuable any entrance under

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_area_rectangle(l, w):
+        return l * w
