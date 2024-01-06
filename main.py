8571a03226b8e318323192993c5ed4efce71ba55
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:16:10 +0000

    all rent polite freedom apart

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
