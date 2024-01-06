1bc3d63b1ee9457aceb722833deacc5727aebd7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:57:18 +0000

    spring remember thought away before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
