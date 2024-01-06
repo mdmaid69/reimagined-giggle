4d76af5ab0327cf22ae33ca4bf8f415b43991c9b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:03:39 +0000

    anywayanyhow wrong wave dull would

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
