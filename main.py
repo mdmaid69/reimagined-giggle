290fbfd22a02310445989b0182777402c9d9f86f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:37:33 +0000

    somewhere smile earth floor previous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
