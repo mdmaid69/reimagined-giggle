8406e0baf71f5651b2d4b88600f4b620d9445da7
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:49:09 +0000

    down chat him tape indeed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
