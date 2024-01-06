0a2584c70eed8cde1d0f44db2a8f4e5b6a149104
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:58:18 +0000

    Kk breakin react bend sad

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import platform
-def get_python_version():
-        return platform.python_version()
+def convert_to_binary(n):
+        return bin(n)
+def calculate_acceleration(speed, time):
+        return speed / time
