97d8dfbde7710c0b10e8522240cac71f6979314f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:42:23 +0000

    reserve give burn thankyou country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+from collections import Counter
+print(Counter("hello world"))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
