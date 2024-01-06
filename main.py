65105dc54592ec1ff0fe31dd46134a071b86a0fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:17 +0000

    matter field major runinto body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
