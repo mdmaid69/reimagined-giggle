2bdc3b8a1f0dc7c86b0e2bb3d1cae35200338a58
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:01:27 +0000

    shoot future impressed admit us

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
