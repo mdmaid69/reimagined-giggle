67420ed7f3a440e09f4f548cbc242f15ffa4b810
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:52:56 +0000

    pleasure report fun guess raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
