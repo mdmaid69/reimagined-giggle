9eabce23c8f48608aabc83768fbbce53217dae46
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:18:35 +0000

    agreement beyond myself smell pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
