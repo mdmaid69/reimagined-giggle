c7c7e8487386fc29c816c8b45d9a2bcebae19710
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:05 +0000

    right creature process accidental main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
