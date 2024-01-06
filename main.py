764a84f477aafeb7244ccddee2da94c427813c67
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:27:35 +0000

    bill switch compare mend settle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
