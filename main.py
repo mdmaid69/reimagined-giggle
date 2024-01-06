60c4f0bcc3e1e2f7ba4bb571cd9148e345504176
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:23:40 +0000

    question rest bank careful lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
