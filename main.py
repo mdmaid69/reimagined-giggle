482ac931e3a74f5eb892556569429543eb728eef
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:59:44 +0000

    telephone extra country sail murder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
