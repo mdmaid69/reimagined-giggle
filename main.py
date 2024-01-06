f66445c7f7d2ddd6df9bef85ae914a000170135a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:12:52 +0000

    open hot quickly sell anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
