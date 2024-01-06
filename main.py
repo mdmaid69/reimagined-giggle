7065694227ff509ff3a4592fe5775a79ecf957ec
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:20:43 +0000

    need touch pleasant pay regional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_bool(array):
+        return bool(array)
