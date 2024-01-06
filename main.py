4a0ab72e8b80bbe3287a6318ad4ab2b840a8a966
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:54:49 +0000

    hour plenty full experience serve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
