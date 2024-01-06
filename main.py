c3e4422c05cdf00a05c629baf1361329e178af4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:09:28 +0000

    smart age female service tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
