7cf2f1cf8bf5fc624bf9c0d04d06a440da611afc
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:42:44 +0000

    win hang late far narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
