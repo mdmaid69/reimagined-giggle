2626f61c56e7f487c15762cf6d1a31c0e4bcfcbc
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:27:25 +0000

    justice warn direct off listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+import sys
+print(sys.version)
+import array
+def get_array_as_int(array):
+        return int(array[0])
