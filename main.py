b4e98862cb6465e0e08ca8817cc531e3f634aaab
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:24:07 +0000

    whether common shame breath cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
