5ec9c3d96d35fe44be17f47fa448119f5735b4f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:57:38 +0000

    quick relationship rank before spend

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print([x**2 for x in range(10)])
-for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
