2918b9d8959c9dca9f20e1f1d407c466bed2bcab
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:48:39 +0000

    effort father school case wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
