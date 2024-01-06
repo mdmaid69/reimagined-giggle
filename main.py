2657eb6282d026e96d1d6ce5805e22acd9a9e19b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:13:06 +0000

    progress comeback press early object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
