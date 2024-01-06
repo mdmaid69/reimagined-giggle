1712ee74c6518c6edbdcbf3cfc0f9ddd829fc694
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:13 +0000

    concern about post more fashion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
