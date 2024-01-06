22c64f6d88b69ca7d99ce05515fbd3e6cd8334e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:56 +0000

    easy mouth skin thankyou this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
