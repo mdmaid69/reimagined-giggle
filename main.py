913b3cfd0d5dc6f6d62d08fbd695c70073f6fdba
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:30:17 +0000

    warning imagination active comment high

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
