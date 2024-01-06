73a99e4c156212b2e6467840b8d62f5491fb2c3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:12:24 +0000

    calmly permission turninto complaint later

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
