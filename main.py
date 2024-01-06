da80b33baa9a2ac40af7bb19a595c7635bff7a0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:09:49 +0000

    wish off Ss block chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
