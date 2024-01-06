c4fc2870c8b9a860dfeaed07c8f25bd4c02afd88
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:16:24 +0000

    left hard peaceful murder disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
