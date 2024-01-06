2b61f7a452c409b8efeacf22adbe29fafcd7ff58
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:44:32 +0000

    healthy car television intend organize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
