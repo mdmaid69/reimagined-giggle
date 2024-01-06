367fee238cd6464a16b4574f4463d1a03305b1d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:24 +0000

    odd not murder fond complete

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
