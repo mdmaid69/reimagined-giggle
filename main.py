e2fe27aeeab311062443df64a4ecff942f87eea3
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:36:45 +0000

    spare this holdout floor hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
