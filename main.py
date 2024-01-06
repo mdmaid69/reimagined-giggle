eb56e354e5416c501fc6de1a7005a0b9abb771d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:36:03 +0000

    sit sleep complicated street top

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
