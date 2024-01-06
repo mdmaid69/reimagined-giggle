cbd774ba65817690ed98c008462737094cc01304
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:13:01 +0000

    round hour hit decide no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
