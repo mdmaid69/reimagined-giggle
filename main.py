26e2a9b07a75f5638a8ea845520cec8bc14725a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:59:35 +0000

    always still live aim shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def subtract_numbers(x, y):
+        return x - y
