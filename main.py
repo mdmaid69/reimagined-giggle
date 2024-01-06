cbf4ee78c80d29ee94951aa95206b75632cec31c
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:13 +0000

    bunch brush professional lookaround mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
