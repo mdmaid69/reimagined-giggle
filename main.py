7b6a32acfb60833dde0bb8f55a6dbc1a4cd3af6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:14:45 +0000

    tomorrow somebody damp correction sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_exponential(x):
+        return math.exp(x)
