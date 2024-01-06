637bb68a7f7cc291812bacaf2caae8f43cc7ef60
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:06:58 +0000

    from too decide trousers play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
