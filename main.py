84b36d25e49691eaaaafefd5106c9c82892e95dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:07 +0000

    ourselves judge negative pot yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
