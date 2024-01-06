052600248fb79f96fcc136ff0517455f43c549b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:45:20 +0000

    relative balance class use maximum

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def reverse_string(s):
+        return s[::-1]
