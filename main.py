0dd962507e3dce79d24d1985262252916e12c436
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:37:33 +0000

    dear our lower skirt downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
