3a957b768e0632b87be99b13e3e23cbec6f16837
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:10:28 +0000

    quality wipe make frightened delicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
