97af75393a573f2185d423241c43d08e5fed614e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:50:32 +0000

    criticism manner should quantity legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+for i in range(5):
+        print(i)
