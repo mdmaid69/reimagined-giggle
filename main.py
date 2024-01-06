d6674fde9ca70acf07524ea6a91e2efd97712e7d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:28:04 +0000

    sell fat probably uniform soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
