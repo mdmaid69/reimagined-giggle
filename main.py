d245396c20e092892b83517407c1e90235df2f43
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:47 +0000

    climb unable agree quit heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import random
+def roll_die():
+        return random.randint(1, 6)
