d3a6716becc4ef3b3ad771c3f0751ed8c74fbd39
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:43:12 +0000

    so education cold praise work

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
