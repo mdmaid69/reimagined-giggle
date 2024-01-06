c3f5393d1726b43cc34f536390eae4ef06dd3376
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:02:14 +0000

    best opponent job lock butter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def reverse_list(lst):
-        return lst[::-1]
+def change_working_directory(path):
+        os.chdir(path)
