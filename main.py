221b6cbfb7d980b518d7fc06f32e76bd8495d503
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:20:58 +0000

    confidence frightened rather cover once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
