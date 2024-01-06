67c61c720a7364ed6ba56b9530c9e29e164eaee7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:08 +0000

    wild weak Tt snow everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
