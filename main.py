bda1d024bd30ed775e3e8aff13902ddb354718e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:17 +0000

    innocent sail sale satisfied recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
