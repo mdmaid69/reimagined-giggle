88483fb7cb5ee0632b417811c375a2ebbc433431
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:26:00 +0000

    protest nigga deliver Nn circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
