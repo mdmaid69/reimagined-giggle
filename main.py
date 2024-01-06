468181af4f7d54ab40996b4a4f4590be072d5e12
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:54 +0000

    opportunity stupid innocent advice strong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
