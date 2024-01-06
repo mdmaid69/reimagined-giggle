f25ce31f1424869082d473347a47614106b67781
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:37:07 +0000

    row pleasant Ee treat swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
