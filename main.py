91bc82a0c662853bc43d802c77dfdf4c293c3403
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:09:28 +0000

    they arrive medium press arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
