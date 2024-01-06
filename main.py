8f8fa9e4b8bb37fccd7ed3eec7a56004dc083efd
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:21 +0000

    turn score quarter war tough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
