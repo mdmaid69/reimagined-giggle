3e963522c92a3248aa7e34b859ab9afe9e6bb9b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:01:54 +0000

    table burn fear who cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+  import sys
+  def get_python_version():
+        return sys.version
