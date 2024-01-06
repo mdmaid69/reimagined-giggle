abe4f4513b92f80f00444fffcd6b0055f2676303
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:02:10 +0000

    foot tall rest suggest beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bool(array):
+        return bool(array)
