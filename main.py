9734534f9b67e8b88cc7a21302e3f38986fc7125
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:21 +0000

    guard rise pain cause into

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
