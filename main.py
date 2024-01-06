65d2cd7650d06ea5b2f8c11f263344d48b268221
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:14:05 +0000

    evening fault everything compare pride

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+print([x**2 for x in range(10)])
