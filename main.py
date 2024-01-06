bf3615776c40f4157b963b8efe4543e34d45901e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:20:09 +0000

    promise father stare far score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
