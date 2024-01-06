c2548bbc8944ace034b54bcc73dceeecb9c03edc
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:10:47 +0000

    grade branch bunch trap right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
