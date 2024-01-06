89e4f499d1dc52d7a0b40e405887f27172f4c003
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:35:36 +0000

    date write calloff phone song

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
