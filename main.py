adaa26f5226691ce27716c8188ffdee4ff346672
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:33:50 +0000

    little season delicate quick aware

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
