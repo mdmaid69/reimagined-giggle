b6a76c9cdb4511272cd6f1e42d57822bbe5d6365
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:10:35 +0000

    hate wheel loud strong Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
