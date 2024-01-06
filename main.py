35b19c69f5fd6a6b2bacacfa2621c0d2014f25af
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:17:08 +0000

    cook scrap chat surprise scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+print(math.pi)
