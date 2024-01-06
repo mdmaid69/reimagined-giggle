581b49a9799b14669590af945ea59e26ec313ace
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:01:08 +0000

    each case yourself straight important

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_floor(x):
+        return math.floor(x)
