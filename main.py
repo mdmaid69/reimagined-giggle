1e3aa8243fd018c88e207db035a9edbe991454d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:22:06 +0000

    later unless sell away professional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
