ecec38b87e3289c2eddaa973f89eed6c1080f78b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:52:31 +0000

    prize underneath judge through food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
