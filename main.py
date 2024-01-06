156e9423a184acc68ea1425db81df98c187cf638
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:58:26 +0000

    woman both pool disapprove perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
