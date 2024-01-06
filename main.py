1e7623849404a4446b6e4ac9f6b2cf1d5afd9cf3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:42:34 +0000

    bend too scare relationship capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
