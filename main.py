4bcf05f39797466af01ab0a2c871cdfb140a7e21
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:40:46 +0000

    importance creature hungry address judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_set(array):
-        return set(array)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_stack():
+        return collections.deque()
