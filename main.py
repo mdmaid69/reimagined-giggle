24e410c1a0b8ce56e2e4024a9f78c0bdca87ae23
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:44 +0000

    cost too line love turnover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
