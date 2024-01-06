f39b12a24c51f7e6732e1453b6e98509b53f649a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:39:30 +0000

    report hit doubt instant idea

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def convert_to_binary(n):
-        return bin(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
