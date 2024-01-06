388e4b60b4b7c1267d808fa012104df6453fafaa
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:04 +0000

    turninto creature gift free raise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
