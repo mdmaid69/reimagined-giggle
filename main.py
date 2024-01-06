2126120668c6ee130cce1f02e64604ca646e8a3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:10:50 +0000

    modern discover benefit concerning partner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
