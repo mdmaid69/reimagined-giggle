5b75f31fd80eb8554e4477a341a804eccf275d60
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:27:07 +0000

    herself adjective pain region bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
