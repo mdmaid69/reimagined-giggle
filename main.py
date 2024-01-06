9109fa112c74f164c5fc028cff39da96cde75205
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:13 +0000

    decide wipe law shoot later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
