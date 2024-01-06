fbc8cc49afd1ad2432277fe808f52cdf8c684c56
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:32:30 +0000

    distance holiday valuable none broken

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
+def calculate_area_rectangle(l, w):
+        return l * w
