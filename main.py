16d1d472009d0d0e9dc2683e42979cb35b5e2b63
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:20 +0000

    beneath unite stamp pay often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
