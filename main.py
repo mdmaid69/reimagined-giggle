e72ea9ddbbc13cf145a6f20905329ed0fd8ecc13
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:58 +0000

    travel each carry manage confusing

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
