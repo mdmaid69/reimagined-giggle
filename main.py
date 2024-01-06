5dad7a2673d2bf26355c9eb07f1d95c32ca28c03
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:58:29 +0000

    healthy back holiday giveback ashamed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
