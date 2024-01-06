57bd12bb10e712165cc64ebe6397aa4d8dd705bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:55:52 +0000

    collect love outside task turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def clear_array(array):
-        array *= 0
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
