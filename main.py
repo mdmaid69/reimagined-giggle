b9d7eb16863110547ba00f791743b06444c53696
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:28:05 +0000

    unpleasant tip switch somebody case

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import platform
+def get_python_version():
+        return platform.python_version()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
