b4ff86d551d54ee2873985f6748efb6e1bcdf03b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:21 +0000

    neither confidence opportunity passage level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def square_number(x):
+        return x**2
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
