64e40f6802abc26244fd429133598297f12008b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:03:08 +0000

    nerve boil mess who track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
