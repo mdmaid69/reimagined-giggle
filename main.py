b0633f9d8da0c9abb6dd051cedb10647c2d59eaf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:11:06 +0000

    proof note hide sit nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
