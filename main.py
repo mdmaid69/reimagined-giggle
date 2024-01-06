e5dd401a42a9c838209ff2f7af390a42636d3fd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:00:48 +0000

    hurt laugh car drop feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
