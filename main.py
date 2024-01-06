11dd9e33c38ae86c687e548020541abb9af7ce36
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:09 +0000

    want ability us remark intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
