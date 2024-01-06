5a373e9e1e281b4b0edb5a9fd0a338057a563f1a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:03:43 +0000

    drawing comefrom friend name pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def split_path(path):
+        return os.path.split(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
