65dd4bb5806eaf78047e1feb78d43abd84ee7a6b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:34:47 +0000

    mystery level underneath stand crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
