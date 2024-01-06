5963e6322c741aa21ffdae063d77900f7d8394db
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:36:57 +0000

    city pause consult announce thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
