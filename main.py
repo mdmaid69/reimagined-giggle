96d785a42ed33d35b63982db3e8fba97d11fb0f0
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:36 +0000

    make concentrate sport add report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
