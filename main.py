da3a619c05f8ac1d40f6ed3f70e1afd54495d156
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:58:33 +0000

    adjective centimetre authority case tool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
