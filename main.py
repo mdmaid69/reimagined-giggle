3f0532877624f9f5c24218548d034ccbc96e692f
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:30:02 +0000

    near partner artificial tread country

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_power(work, time):
+        return work / time
+def calculate_area_circle(r):
+        return 3.14 * r**2
