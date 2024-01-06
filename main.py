fc8afb6e11c5ef14a640871d373473de95797ae3
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:22:57 +0000

    choose mess contents telephone church

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_power(work, time):
+        return work / time
