ec08870c3a2f644279488f526485be9f96f93897
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:59 +0000

    OKorokay normal it band total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
