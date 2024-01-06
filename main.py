9844bfc8c87f68aad5233beeefcf18795db8cc05
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:19 +0000

    fire vote deep arrangement pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
