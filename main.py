c53b3d0b4fdd065c27ba236799d255c8dece47a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:50:41 +0000

    perhaps by roof takeout charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
