7f2f30b5af2450b351222ab61830289dbebcbd35
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:15:40 +0000

    drink majority easy substance Yy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
