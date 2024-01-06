2b535fe43335120d2b5915983fcafa18ce3a2358
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:45:53 +0000

    attitude inquire error street upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
