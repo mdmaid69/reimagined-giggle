01c41869c6c37838b05253cc21c038e7e776e0a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:23:00 +0000

    unite unpleasant record copy worry

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+for i in range(10): print(i)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
