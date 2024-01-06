f51e633963c03aed1201f98a6992498e674e3f2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:10:24 +0000

    bridge miss send bringforward move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
