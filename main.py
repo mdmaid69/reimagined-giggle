cfbbe485f2d293a96ddbccbfaf38a903a3b5adfc
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:43:06 +0000

    mess urgent morning opposite place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def sort_list(lst):
+        return sorted(lst)
