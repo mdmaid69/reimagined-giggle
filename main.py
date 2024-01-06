8b4eba521e939ce5cb2e8c039bfd9d0b7dde3d11
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:54:47 +0000

    tone shrink strike eager patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
