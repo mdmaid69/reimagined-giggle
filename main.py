fa0509f00f3471a45475b0ffaca2390fe403ba3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:03:58 +0000

    dear influence many trade sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
