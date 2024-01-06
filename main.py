19db8a1d65ae2e9b7e44e5f2547a36be6f089356
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:22:04 +0000

    doubt money but standard nerve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_bytes_from_array(array):
+        return array.tobytes()
