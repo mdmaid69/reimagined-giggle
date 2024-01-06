3dd6de4ee319a1a86997002bc2d969eb39cac8b0
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:06:58 +0000

    any arm experiment life getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
