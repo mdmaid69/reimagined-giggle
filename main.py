fee7d05204721f2959e792e44588cf6e97302501
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:17:46 +0000

    general direct ready spring seat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+from collections import Counter
+print(Counter("hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
