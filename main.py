dcde92b0ef7b388d0f48fb7ef0b82a92cea05f3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:36:58 +0000

    kind Nn although law square

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_floor(x):
+        return math.floor(x)
