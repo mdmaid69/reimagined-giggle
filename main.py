803e23be661e4ed62cc7ab8592874eb2fd1931b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:30 +0000

    important dig plan task that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
