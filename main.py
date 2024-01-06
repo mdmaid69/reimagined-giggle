4168e7c6d4c822009b43bf38afcbcc902a2f09df
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:23 +0000

    inside outside attempt whole poison

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
