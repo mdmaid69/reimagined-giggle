7487a04ea217394a1b1c7e3a1f651565511ccc8b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:17 +0000

    common look contact battery breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
+from collections import Counter
+print(Counter("hello world"))
