2f04352a952ee7bb82655b238172112eedc5b124
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:34:59 +0000

    ring settle capital interest luck

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
