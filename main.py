f4bf18b9e04c9c63adebc08c29dcff5bb0387712
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:32 +0000

    glad opposite train close tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
