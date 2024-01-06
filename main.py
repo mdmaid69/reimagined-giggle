15d7fb17a9231abad3bf6e8c0be5e06838f5c3bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:56:47 +0000

    down move trust pupil violent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
