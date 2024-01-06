f73b40d86fb4450c28f105c7e0d8d89d9d2d0f70
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:32:15 +0000

    whatever dish trouble win his

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
