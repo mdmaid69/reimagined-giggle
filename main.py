dcd71033783c851db76b142b7ebbc6c4b406df04
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:26 +0000

    prepare last 567enemy pump with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
