3a1a5deff840f9e74f42d598c162c2b503f66fda
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:55:13 +0000

    reply house trade surprise firm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
