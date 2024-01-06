b102070f2eb691fb15b09662583dab297047c06c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:54 +0000

    occasional terrible past deliver accommodation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
