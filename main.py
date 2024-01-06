1dbefea67919e778c64d5384b167c44c64c43dbe
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:43:29 +0000

    swallow innocent comein trade away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
