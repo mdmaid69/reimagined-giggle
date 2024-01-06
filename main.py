57e96d8d33dfd19ba8f315996987331e8c10577f
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:13 +0000

    back hurt so software aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_max(numbers):
-        return max(numbers)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
