0d13bae950854a9051c0bee52cf2de0c62ac2334
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:42 +0000

    deliberate tragedy priority pupil soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
