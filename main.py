ae9560dc3ebc5a18276a11c816ab20020e8d0a02
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:23:48 +0000

    road dream tap mystery score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
