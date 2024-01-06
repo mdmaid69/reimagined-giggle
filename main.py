cea14162d7be856e7550868a68bcfa3cdb68cb2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:11 +0000

    invite average bill process lunch

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
