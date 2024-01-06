8a8a118c46de18eb6810b3c98709684b21a90a9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:47:36 +0000

    present turn bang past advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import time
-def get_time_since_epoch():
-        return time.time()
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
