f202d7193c15ffdb9383ab6f8df58a32900f7c26
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:34:20 +0000

    defeat season hungry date fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
