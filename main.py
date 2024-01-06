9e1fe4ab079a704c06dda4f7ca15f6c548d7dbd3
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:25:17 +0000

    smell 567enemy agree difference reduce

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
