78fcbcc388224f95ce4aeaf2f49622f3f6f427cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:52 +0000

    holiday herself brown between ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
