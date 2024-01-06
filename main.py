a53256d8e183e5ad9f4e638e7dc8e9fb6062dce3
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:41:55 +0000

    marry deep allow Dd shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
