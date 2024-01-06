45504f562b6b5323c99095f827d46c3d617660b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:59 +0000

    tooth compete definite shrink I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
