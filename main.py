1b5179e9721d221bf2746e19727bc8b646d0f2eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:15:26 +0000

    sea collect satisfactory type one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+def roll_die():
+        return random.randint(1, 6)
