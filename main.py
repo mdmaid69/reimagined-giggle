5ebd6f88d2db2a499911de78a31e5341e3005dce
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:39:34 +0000

    brush use emphasize wet needle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
