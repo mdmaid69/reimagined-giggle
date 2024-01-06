ec9341ee88b625b993bbc06c6090a1e2efd9a78a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:37 +0000

    herself comeover calmly guess worried

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
