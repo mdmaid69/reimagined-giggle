e114df6d2931bb0daf114b6f86f81896e4d15032
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:33 +0000

    lookafter load else face goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
