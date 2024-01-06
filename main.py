9651b92087fb6338584d50258153105a4329d21a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:58:04 +0000

    whichever die already dance shopping

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
+def calculate_acceleration(speed, time):
+        return speed / time
