2b3ff0f7782c4eb016428b3b8905bc0a2442a560
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:22 +0000

    tragedy famous hope pen serve

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def is_even(n):
+        return n % 2 == 0
