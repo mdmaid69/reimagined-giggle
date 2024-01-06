6e7316753cd385495352bfbba9efae4b9ac1387e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:02:41 +0000

    twist expect thankyou doubt performance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_max(lst):
-        return max(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
