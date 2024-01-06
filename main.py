bf0dec3d2664a4710d763c498d669a537f54ff60
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:50:39 +0000

    neck shame but hello finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
