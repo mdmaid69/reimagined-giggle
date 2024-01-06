6aac631344bc98f083ca811a2bf559d6be3fab74
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:16:48 +0000

    shout include cruel temper visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
