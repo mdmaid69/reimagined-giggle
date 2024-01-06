9ca38b581088261c62f33e28a414f96183e70eb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:22 +0000

    settle leg message comeoff window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
