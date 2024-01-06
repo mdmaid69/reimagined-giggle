33b907ed651cb94d39ca8c14bb1a5495a344a67d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:54:05 +0000

    572enter size shake brush chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
