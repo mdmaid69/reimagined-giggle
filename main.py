332e6edeb2164ca538a071d900c98baab9f508ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:26 +0000

    must recent objection independent properly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
