57057fa80bf7af4f24b945accf2e8c22e13205ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:36:40 +0000

    I instant up bring start

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_area(radius):
+        return 3.14 * radius * radius
