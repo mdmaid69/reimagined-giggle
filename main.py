abc1ce6db9df7574a86633c23cb1752bb0e75a26
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:40 +0000

    children mention offer start slight

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
