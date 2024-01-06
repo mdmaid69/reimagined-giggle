4d4a0e481915952789e90657e8cdc34c54648527
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:25:09 +0000

    section fair takeout chief sport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
