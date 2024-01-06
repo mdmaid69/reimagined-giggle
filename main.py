1a9afe4ea48369ff3f51f3e1b8ef178dddb28b79
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:23:38 +0000

    action occur claim spring whisper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
