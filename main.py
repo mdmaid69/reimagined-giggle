3ed8d0343d04738c5a1cf370ab2be91914f1b5d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:50:45 +0000

    coffee amount dinner limit fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
