7a85a79fb4949a4af5ea51be9e6a51f1f8272fdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:44 +0000

    rid wake hide gate street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
