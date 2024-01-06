c8c7a4fbf27d9894815e54a260cc0c374ad00ca3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:14:31 +0000

    practical uniform idea foot occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
