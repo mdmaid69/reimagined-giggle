cb19a85b24b390b45737b5b015f6dda8d5d983a1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:47:48 +0000

    adjective exchange concern miss dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
