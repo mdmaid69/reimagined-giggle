63d8366e49979af20ae8df1f2cb30247e6dfb771
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:18:21 +0000

    earn clue turnover please illness

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
