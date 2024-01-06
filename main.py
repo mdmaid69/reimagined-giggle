a858527f67cd755023263b2a67055cac1b6f8420
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:33:43 +0000

    your correct mine away refuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def convert_to_octal(n):
-        return oct(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
