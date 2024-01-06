ba31e849166342de3113c4a5da35d5421e55d7f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:30 +0000

    I now evening do anywayanyhow

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def clear_array(array):
-        array *= 0
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
