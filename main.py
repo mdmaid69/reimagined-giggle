e3b5ad2cbb8f880a00fc188f77bcc92bfd2e0cc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:29:06 +0000

    advantage mention flavour scale card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
