7eeb61d61a615e16d96a0f2a4721032520bfc27f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:03:55 +0000

    benefit left problem wake event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
