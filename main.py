49d60e613df0317a9418ad26aca83279d630106c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:46 +0000

    excellent rain comeround indeed naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
