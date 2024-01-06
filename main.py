9b0f1c7229bfeb09b1c07af30361f63729c429d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:22:43 +0000

    pencil number nothing crisis come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
