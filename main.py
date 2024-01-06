f1ea09278b686df3cac9e180fc173e615d608ca1
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:45:46 +0000

    habit used term just thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import sys
-def print_python_version():
-        print(sys.version)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
