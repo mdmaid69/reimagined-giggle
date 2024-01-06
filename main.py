7bcde02c6f56c29cff1838582b27df5b52d8e7ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:06 +0000

    disturb female pleased process improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
