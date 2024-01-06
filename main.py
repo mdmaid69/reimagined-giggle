958b8fd84a748b9d854095b34a705d447b35d63e
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:40:32 +0000

    glass serve duty avoid keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
