b0595cb87359e9ee0e1138c5d8c0f23705fb7540
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:03:04 +0000

    empty point loss switch worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
