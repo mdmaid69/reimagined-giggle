dbfb9f1cbc0fd5982dbecac4f628037de3346fba
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:48 +0000

    cause round unkind dry wind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
