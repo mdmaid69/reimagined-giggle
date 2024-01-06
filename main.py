54ef2477b238e2624733b0fcbdeefb9f3c9bf0c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:17:58 +0000

    settle trip duty impressed concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
