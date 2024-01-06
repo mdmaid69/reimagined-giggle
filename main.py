55331b81fb88f2c8d7767aedf7e1ad459a713d03
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:40:07 +0000

    bank wrap crazy down contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_stack():
+        return collections.deque()
 def calculate_average(lst):
         return sum(lst) / len(lst)
