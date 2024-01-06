e735e795bf198d7997b0154e243f8c89fe8964e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:03:51 +0000

    unusual shout pretty callup worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_exponential(x):
+        return math.exp(x)
