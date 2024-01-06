61168299ab4a11ce15e72f4c7572f3f3a481f20a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:58:21 +0000

    serve sound board what kill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
