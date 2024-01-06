22c5ad2c3646977382b8803e4c1d1308d7a61a81
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:48:18 +0000

    hollow pupil anyone able in

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
