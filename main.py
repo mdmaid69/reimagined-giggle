491fa61846c2bfd58c0abc4e4762bd1f5099d4f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:25:22 +0000

    again old unpleasant deliver film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
