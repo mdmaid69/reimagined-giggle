0110e6db1f7e3ec13029892a0be121cadd0ce9d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:17:15 +0000

    fix free drive handle borrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
