4c22aef7e821b7ea616fa57c03740de8d377ea42
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:51 +0000

    test part powerful failure behind

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
