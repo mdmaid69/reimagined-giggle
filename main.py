125a5fbfb6aede3174a32b1d54db7405ae0abfb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:12 +0000

    talkback everything together clue easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
