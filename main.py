332183d3e758cc88359a0328aa8c33caa3237e9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:23:01 +0000

    steep past especially ready maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
