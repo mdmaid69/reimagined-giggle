72e9f6c64097803c4c83884963a0eff6693a81eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:10 +0000

    than mind guilty nor bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
