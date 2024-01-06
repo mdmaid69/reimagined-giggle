d893ce052c8aab0b6f5b8a4eab32c1edfcc26a23
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:50:53 +0000

    nothing Jj description tend production

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
