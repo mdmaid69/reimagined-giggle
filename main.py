ac21b13fb70623eedaa4897321eb29961029d533
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:50:24 +0000

    aware somewhere bag comeup tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
