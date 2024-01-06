b9c19a91439901bddc8efbd8d46159d5551391a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:25 +0000

    indeed many borrow eye trial

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+print([x**2 for x in range(10)])
