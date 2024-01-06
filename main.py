fcb51ceefafa91c7b3b2a6a37e7f4b1858a6517e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:04 +0000

    behind fence scream final talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
