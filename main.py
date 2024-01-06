93e7c2a5bfc07e8e411932497e489909b7079d91
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:09:47 +0000

    pride fairly bright protest breakthrough

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def find_min(lst):
+        return min(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
