39c7e6e950f5fb43fcecca7d996d05d0e51424c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:48:24 +0000

    tune crash pair taste factory

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
