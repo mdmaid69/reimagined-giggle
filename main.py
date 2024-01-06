d1439d765098d3de44efd3d9a8b97d1c9566e93b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:34:24 +0000

    provide might comment above question

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def count_elements(lst):
+        return len(lst)
