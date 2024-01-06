3fcbca5466eec6c354d5ff99422d1234cd714ce8
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:24 +0000

    reject certain relationship stamp Ll

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
