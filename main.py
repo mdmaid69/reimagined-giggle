b48010d92f9d74070085ae35181de94dad17e0d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:56:56 +0000

    sudden teach lesson strange treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
