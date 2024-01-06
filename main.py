3c42cb58911db7e01a751dde573134bb8b12583e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:47:55 +0000

    meet follow takeout stage taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def square_number(x):
+        return x**2
