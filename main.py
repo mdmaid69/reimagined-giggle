8d0de0607df29e825f98243ff938b2f08b3b84f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:50:34 +0000

    recovery zero always clear own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
