cbbf9fc5ef528f624c66d2a533866a467cfc4d9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:39:28 +0000

    congratulate damp decide temper equipment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_as_str(array):
+        return str(array)
