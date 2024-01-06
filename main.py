2fbbaaf320870a7eb16085d29eaca62bf6d20353
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:55:19 +0000

    family change type experience often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.time()
