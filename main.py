e5392dc4a2dd7687fb48a38fa571b4175142089b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:46 +0000

    duty just reliable mine of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_item(array, i):
+        return array[i]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
