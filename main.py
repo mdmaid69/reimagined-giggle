7d9f9702b3630bb13c32c2754762c96ee6506c81
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:49:13 +0000

    crazy action loose side geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
