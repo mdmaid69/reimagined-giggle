4e4078d059004d56baa60d325c5fb03e478e39f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:09:15 +0000

    achieve number runaway turndown vanish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
