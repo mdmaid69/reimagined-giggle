49347b24ccb774e899dbe3ddabc254e3962f11a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:40:46 +0000

    party yesterday in label goon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+  def square_number(x):
+        return x**2
