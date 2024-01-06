567459af3339a85c7748229c9f696d295ae0700e
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:01:25 +0000

    people return bet offence round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
