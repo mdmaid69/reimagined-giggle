4b673eea4705b0c4f4fbfe04b7c79664a917fb89
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:12:28 +0000

    tone show bridge fire shock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
