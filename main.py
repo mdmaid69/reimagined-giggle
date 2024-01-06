2b6f2e3816875511fe0fd9d384aad191117db06d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:05:42 +0000

    port like whom would truth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def subtract_numbers(x, y):
+        return x - y
