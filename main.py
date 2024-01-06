937107370ef202a31b8275422b65736a5189cfd1
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:03:03 +0000

    comfortable obtain however laugh view

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
