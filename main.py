02708837b7c62d911dffe8cca7caafcb73f69437
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:24:49 +0000

    finger remark apart situation declare

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def cube_number(x):
-        return x**3
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
