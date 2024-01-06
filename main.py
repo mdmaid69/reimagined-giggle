00b4e099cc52e8cb8f3ca561cde0fa0057f8efcc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:32 +0000

    next 569enjoy see concerning identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
