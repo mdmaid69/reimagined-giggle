d7c3448a3a5d2244bff6508787a2483ad3b51808
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:32 +0000

    married design tragedy zero expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
