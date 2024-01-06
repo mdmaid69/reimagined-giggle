c75b353781f1feab3f4401932c43242a0ff68323
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:32:24 +0000

    sensible set live have translate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def reverse_list(lst):
+        return lst[::-1]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
