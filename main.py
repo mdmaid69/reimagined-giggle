4f9faece6f27de72f1b66c8e57210bc2e6a3515b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:01:26 +0000

    borrow Zz almost too cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
