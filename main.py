d7cf2c6e75329e243eea04d2d572a4fcb9bd5345
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:56:05 +0000

    improvement official better identify body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
