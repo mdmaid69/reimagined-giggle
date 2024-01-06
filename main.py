91dbad1b43c46819d11bb5bb432af2c5d0be5f3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:52:41 +0000

    interested fire use bread our

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_density(mass, volume):
+        return mass / volume
