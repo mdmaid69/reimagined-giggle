7a79e85c34ee51e9e5f0b4bf392155756e725c55
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:49:04 +0000

    once Bb perfect fine wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import numpy as np
   def create_numpy_array(lst):
         return np.array(lst)
-import tensorflow as tf
-print(tf.__version__)
