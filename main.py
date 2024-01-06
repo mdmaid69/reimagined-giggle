1a5c7d53352b715b53ae5b8b98f74dd2c54e2ea1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:43:27 +0000

    suit almost convenient start class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+  def is_even(n):
+        return n % 2 == 0
