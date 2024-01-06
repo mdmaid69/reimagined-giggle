b7eca679a4ce63a5a2a0c1aaf34083312af7d728
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:47:54 +0000

    turnback contain flavour Oo store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
