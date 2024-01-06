663e772ec7721c91b97ca70364e178a3776ef00a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:04:28 +0000

    fashion modern area low work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
