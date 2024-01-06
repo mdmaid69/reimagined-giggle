f93f3166d884d0a8261d7587987260bb0ce1ecf1
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:53 +0000

    keep scrape please bet escape

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
