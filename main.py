c8a2419c9624aa9cd5f14e71e5ca9ba501513f44
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:24:30 +0000

    date getaway side shallow on

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
