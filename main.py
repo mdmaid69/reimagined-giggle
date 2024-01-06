56b5d3796f5e26c91cdb2d91172ecfefd5d1768f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:50:11 +0000

    never over far process boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
