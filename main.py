79f48f2a79344d474eaaec4f97e405e0b8a2ae39
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:33 +0000

    rid clothes together ask problem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_union(list1, list2):
+        return set(list1) | set(list2)
