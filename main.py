48b91cf04b7a6d2b466e12321a99e7885a72d816
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:54 +0000

    if character school involve yes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def convert_to_hex(n):
-        return hex(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
