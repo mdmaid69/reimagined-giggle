c18f333fca815c264773af3a9564daf6b192c6ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:05:48 +0000

    spite copy upset confusing reject

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
