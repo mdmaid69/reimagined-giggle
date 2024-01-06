de8347cb7df5faf9c6845a9c8d2543ce668cf56a
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:27:35 +0000

    ocean prevent image transport used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
