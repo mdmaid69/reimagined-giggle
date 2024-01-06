1f1b089e1aba62163e35ac5dd04ef053c3a4ddd8
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:07:34 +0000

    minority sound giveback tired achieve

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
