5e86cb02db16053dae6a64b0a89ceacc774e2a53
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:04 +0000

    rank practise possibly run transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
