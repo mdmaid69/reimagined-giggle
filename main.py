c64ac7b80a385b78e3719eca4bf0314c10195640
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:23 +0000

    some failure card damage holdup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
