576981ad584786ff3d964243754629c39723a9a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:35:03 +0000

    disturb fine deliver different copy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
