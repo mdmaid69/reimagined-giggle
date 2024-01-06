d3afd855c90e85fe90e271f2a4aded0f292d303b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:44:50 +0000

    section exaggerate traffic duty arrangement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import time
-print(time.time())
-  def sort_list(lst):
-        return sorted(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
