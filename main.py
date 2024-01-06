ce93e001def909f9bb4f0b13e1168a3a7183cd39
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:03:28 +0000

    top fault tomorrow both bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
