bc5bbdbe38b69f6d87440fe7b9202c341a1ff3d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:58:37 +0000

    only love repeat after form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
