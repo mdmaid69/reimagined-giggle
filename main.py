887a3714ef5bcf33b68b896b3e8a77b8e5f6c3ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:31:07 +0000

    attempt cruelty soft measure rob

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def sort_list(lst):
+        return sorted(lst)
