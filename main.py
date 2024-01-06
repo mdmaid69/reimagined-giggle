eb2ea6d5fd5f51f0160411d408f5f0c6cb9c1e80
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:08 +0000

    hide watch thinkthrough my pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
