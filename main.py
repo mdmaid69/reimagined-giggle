a6dc1ae0f0dbf8b47795b1ec68759b41098e797f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:21:28 +0000

    Kk exchange closely record few

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
