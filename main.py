9978fb7100bac8222b4c8ff128bc8ee26e10f491
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:01 +0000

    commit region activity translate collection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def is_even(n):
+        return n % 2 == 0
