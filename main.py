cef81bbef07ef8bbee8093e6fa6b06cba4e69d9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:41:26 +0000

    fresh nigga order port takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_work(force, distance):
+        return force * distance
