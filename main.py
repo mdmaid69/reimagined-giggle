11f724b0bf3866038d6d77ab5a12e6d6013b2b3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:27:20 +0000

    hers method physical imagination solve

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
