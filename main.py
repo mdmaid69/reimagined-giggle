c9449e7a687c08dd4ba493661905c59c99c20187
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:03:46 +0000

    die square operate would emphasize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
