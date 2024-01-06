fe10388776a21d0393f9a0f39b62f03e87ea3313
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:59:05 +0000

    war trade deep shame smile

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
