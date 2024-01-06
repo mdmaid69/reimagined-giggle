60a6385f0cb812c38bddd2fa8326ca2a7198a3bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:30:39 +0000

    system just shallow breakup nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_list(array):
+        return list(array)
