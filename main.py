bc7ed858a085f5b11acf21ea26b35805d21e48cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:26:27 +0000

    world improve test danger do

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
