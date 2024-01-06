c4b29995f09d784adbbd4d3fc4d6ae854f6f8181
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:31 +0000

    turndown wish afternoon expect want

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
