38629e87b2d94d04f2d3c6ce0f4b5147556e4246
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:29:02 +0000

    apply doubt load delicate anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
