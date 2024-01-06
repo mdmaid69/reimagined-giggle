3291ee2c25e0f1f66b45e9e23e00195bc252494e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:08:37 +0000

    chase knock turn bright inform

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
