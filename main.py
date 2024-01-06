bfc0449fb219d7d4173c51eb42bab18855c9c2fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:54 +0000

    talkoutof bar everything nose ill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
