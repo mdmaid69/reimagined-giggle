efabfaa63cd7a783674582e1b68b09439fb53421
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:32 +0000

    treat steep extraordinary whoever strict

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import tensorflow as tf
+print(tf.__version__)
