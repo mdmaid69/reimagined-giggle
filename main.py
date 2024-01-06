72508b9f3008541e9ec425dc8127bd0282fb8a7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:32:11 +0000

    level group wing arrival brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
