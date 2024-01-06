2c7aa282e79617403d4870bc6da4e1d6f2533f88
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:26:10 +0000

    cold discovery mix concern point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
