19e081b9fe7d36207a7c1b1bfe09786f7450a64d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:11:33 +0000

    climb authority history couple tooth

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def is_odd(n):
+        return n % 2 != 0
