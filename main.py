45221e3fa6f533a4b7841f0d8cfcb6deb1a626c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:53 +0000

    angry cry lead people stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
