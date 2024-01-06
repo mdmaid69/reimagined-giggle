ca7cabb8bbd14e35e0aa8896f30981d21abab5aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:53 +0000

    doubt vote bringdown attach pass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
