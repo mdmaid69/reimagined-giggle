f473852c69acc6b2bd760b2d226eba4378e45052
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:59:05 +0000

    design deep wait plan over

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
