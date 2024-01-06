a79a7561125be0c62e3b147ee119f796a4210926
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:03:44 +0000

    silent false than level fold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_octal(n):
+        return oct(n)
+import re
+print(re.match("h.*o", "hello world"))
