eff36e67b80041510cb013da44d9725c1f4dcac6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:56 +0000

    law difficult suddenly behave speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_power(work, time):
+        return work / time
