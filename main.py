dcc1eebc82365ec9663485545f1aa92766a93547
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:03 +0000

    extremely unhappy period almost scheme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
