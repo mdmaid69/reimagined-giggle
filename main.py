d079e8f13b1874c574ac817cc38a7a498af30ee2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:38:45 +0000

    neither piece plant university ashamed

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
