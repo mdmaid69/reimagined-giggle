7b8f597a271bbc713fa82efd6c6017e08560c566
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:25:45 +0000

    in correct peace mention obvious

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
+for i in range(10): print(i)
