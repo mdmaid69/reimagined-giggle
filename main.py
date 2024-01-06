94534794153ae7b6341fd1b694f718d31c7679a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:06 +0000

    defeat section address discussion trip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
