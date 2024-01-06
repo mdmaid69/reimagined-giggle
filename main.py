3b56c1e425df443fd391f5bd705bf0f109cf981f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:10:23 +0000

    sense bush turninto pay listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
