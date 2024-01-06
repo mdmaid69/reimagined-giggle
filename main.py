c056a615760fb9392253cf918e91e8dc8d31b693
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:00 +0000

    real fast tip fall other

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def reverse_string(s):
+        return s[::-1]
