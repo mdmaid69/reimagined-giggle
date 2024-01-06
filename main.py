1e19838077b55d95ebbcb169055f66cb117852b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:14:17 +0000

    suggestion trust danger where talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
