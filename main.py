61b83d09caca5e109cd9c44217ab84b9d538867b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:11:24 +0000

    this Rr yourself majority effect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
