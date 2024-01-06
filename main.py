d4411962f36a2db5ab8d2081b93ab1db5ac6b070
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:24:45 +0000

    quiet us forever rain rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
