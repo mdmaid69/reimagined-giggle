42f29e190461162a1ea61c49c6500b369a65eb5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:50:15 +0000

    honest as influence comedown replace

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def add_numbers(a, b):
+        return a + b
