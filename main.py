f4b1c4653b714bbe1d6f565de3996256dda32499
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:34:30 +0000

    pen right cover Nn defend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
