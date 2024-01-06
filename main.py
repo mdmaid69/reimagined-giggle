49090b583ac2aa07b05c5b28292418d398f6f26f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:20:35 +0000

    cost thinkover responsibility dream me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
