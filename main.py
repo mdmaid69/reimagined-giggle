8e9045b107dfea7af2152aaeb7b5ee8a43f3b356
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:09:55 +0000

    familiar where quantity container snow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
