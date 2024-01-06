5ee128fa2eafee9e57bebfc08a5cff598912aace
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:36:39 +0000

    glass frighten country part Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
