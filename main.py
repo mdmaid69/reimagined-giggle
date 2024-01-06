12e2827cd15c1901af1eea1293f28d9a523f91b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:28:19 +0000

    frame character afterwards church by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
