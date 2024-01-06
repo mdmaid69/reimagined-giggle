f66fee7cfd35f2efd778677b8b81a6bb3e781470
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:46:58 +0000

    treat exhibition open battery clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
