4f880cac2d6ad5a71d74b0e80176c5f6e56f86eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:50:41 +0000

    adjective drive start exit strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def divide_numbers(x, y):
+        return x / y
