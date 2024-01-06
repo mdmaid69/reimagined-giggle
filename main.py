4c8184b2bf7324cd43cddd18cee1ade370d2b22e
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:06:00 +0000

    experience release bunch getup stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
