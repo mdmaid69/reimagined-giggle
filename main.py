551fbeffbd86fa718775123ae4f3505102070efb
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:23:07 +0000

    uniform dress thin sick pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
