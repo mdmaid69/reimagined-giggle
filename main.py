54e7e9926486420b65ba1ceea5af48f3db6171de
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:57:28 +0000

    greeting meet middle impossible right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
