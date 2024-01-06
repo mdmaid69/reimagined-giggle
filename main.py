19ebfe2f40ce484a54053f9bb039591c7420aa43
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:57:17 +0000

    emphasis cost bad recognize match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
