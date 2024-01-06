78204fc9ed3828fabccfa58165c7e570ba66934e
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:41 +0000

    bet nothing mad cope speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
