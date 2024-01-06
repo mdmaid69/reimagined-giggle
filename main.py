619665dc8a9041fb94e16b001b10d640037deec8
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:46 +0000

    meanwhile excuse announce price spring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
