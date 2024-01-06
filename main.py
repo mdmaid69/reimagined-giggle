f9094d484670defd02b4f2d5b5079947163d10ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:04 +0000

    pity dry perfect waste Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+print(time.time())
