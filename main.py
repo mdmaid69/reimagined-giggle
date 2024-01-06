8a3d396208f3d31affb7a91ebd302264a0a0b8e5
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:55:12 +0000

    turnover opposite lesson carry there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
