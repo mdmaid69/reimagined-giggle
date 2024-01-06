e8634d7cd0a55154158f2c966cb305354b091e70
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:41:46 +0000

    strike fall money bring spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print(os.getcwd())
