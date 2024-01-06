47007491f7df158c471d2118239455497a723118
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:19:05 +0000

    crisis about duty rescue congratulations

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
