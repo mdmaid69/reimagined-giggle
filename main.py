dc08067f8f072d15b3288fa505c6ca540ea70991
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:53:22 +0000

    post address earn any twist

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
