605d535c43783b0ef579cb5ba9b5b95d7f6ae139
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:39:03 +0000

    little uncomfortable begin then between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
