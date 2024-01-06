dc39611a333f07ab3589bbe604a452552f7d08f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:41:00 +0000

    shine idea watch fun use

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def multiply_numbers(x, y):
+        return x * y
