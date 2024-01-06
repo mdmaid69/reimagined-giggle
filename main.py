58211e8900c77b6afcf25e50c5bf19d27ba5738b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:09 +0000

    hope great include upset take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
