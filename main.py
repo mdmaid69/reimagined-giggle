5384cf0bcd4df559088ec76b21f05833b4f8b1a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:15:59 +0000

    stir convenient limit snow write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
