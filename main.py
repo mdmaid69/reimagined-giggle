1f7fb54cee2974e8076fe0beeeb50700c5acaeb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:17 +0000

    obtain love could something wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
