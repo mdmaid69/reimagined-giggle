8f79fbb8de0ddf97734c8607edfada62bf185393
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:39 +0000

    key exciting Yy term regard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
