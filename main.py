28cff0ddea2e9dc5bfe8a4f0cdfbdb381e69300b
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:31 +0000

    yes properly recommend back problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
