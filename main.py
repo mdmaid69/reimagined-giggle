4a5239fd51c327f8684fde3d2fd48f6855b5e1d8
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:19:24 +0000

    takeover round treat same fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
