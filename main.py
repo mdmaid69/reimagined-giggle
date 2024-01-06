9919ac07d56acc2ad6510fece16433872a6dfdeb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:13:08 +0000

    happen first butter realize considering

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def clear_array(array):
+        array *= 0
