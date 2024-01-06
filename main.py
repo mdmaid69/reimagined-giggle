900b07953839f5ee7a2eac34a02c8272d60c3e08
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:50 +0000

    takeback lift round usual production

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_area_rectangle(l, w):
+        return l * w
