c9a9f0efcd5d94bddbfb762acaf8ddbd16f6eea1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:14:43 +0000

    drop brain government unless ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
