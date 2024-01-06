9f25a3db15571cbf46f7c160ce4e08193d87cde6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:12:27 +0000

    tool marry surprise married everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
