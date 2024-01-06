253b396ada67254918a6ba9132f1075c3f65ec48
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:57 +0000

    correct visit approve beside dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
