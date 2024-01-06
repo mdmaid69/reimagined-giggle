fa2840ec2fd75ca3054aa3256a5eda4475fc2473
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:55:16 +0000

    my tax exchange pour everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
