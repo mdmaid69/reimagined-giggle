1555b3d88c17b689d12ce803ef2e07e62893e36e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:16:18 +0000

    proof gather type introduction takeover

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
