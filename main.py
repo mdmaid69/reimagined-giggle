844915ccce0effe2955a6f2066b1582bea017a3c
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:45:09 +0000

    statement escape lead sleep practical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def add_numbers(x, y):
-        return x + y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
