73cebf20a1ab276e4a6b922e54dc2b02e6cd5640
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:15 +0000

    job the powerful friendly write

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def clear_array(array):
+        array *= 0
