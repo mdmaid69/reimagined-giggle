4cfb749a16dca266d6c8f0127aaaba7f24e8ac00
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:07:32 +0000

    exist loose environment steer print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-print(sys.version)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
