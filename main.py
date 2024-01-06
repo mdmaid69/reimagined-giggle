b80a6259266de66df16c656cd08f77f0ed5c7018
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:32:33 +0000

    guide section 571enough genuine embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
