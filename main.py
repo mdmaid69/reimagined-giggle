90434ce9d3378f17ad7d4ec341f5873a0cc0757d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:33 +0000

    hand across say Cc separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
