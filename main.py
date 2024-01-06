9f4d0cdc19ed501c3e395667ffee192cfc3394dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:47:37 +0000

    leave surprised time or afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
