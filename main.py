1fbbe68ba0eab1bb16a36c664d70c15d9d4c998f
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:25:56 +0000

    thing human weekend principle stick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
+  def convert_to_binary(n):
+        return bin(n)
