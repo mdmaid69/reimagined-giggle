b06ca51e8ba58545146f7c0f95aa79d456dd1e29
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:36:34 +0000

    circumstance recently turnout after get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_volume(length, width, height):
+        return length * width * height
