3b28f829c2520797c80aadab3678ef39f34040a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:50:55 +0000

    prize challenge purpose answer fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
