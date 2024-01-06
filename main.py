bd2e66ce9bbbc05e9322688027f673f5f44672b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:24:50 +0000

    plant clever combination muscle level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
