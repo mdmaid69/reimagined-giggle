bdeac339ff6ab824e3c822a9f9d378a6de919b6a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:27:52 +0000

    gradually OKorokay offer find wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
