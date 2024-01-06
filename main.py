67254dd6428a71e19e21a97f03bc200d9cb251ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:04:42 +0000

    hang happen turninto space word

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
