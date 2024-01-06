ec40e04c5b74862dfb29d80b416881a9819a4d4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:13:28 +0000

    order bin wind block point

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
