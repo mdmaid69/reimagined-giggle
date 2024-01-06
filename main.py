77a1f8c09175b98d38b1c1e0f01ca9e28f3232c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:06:06 +0000

    visit warning although fight cut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
