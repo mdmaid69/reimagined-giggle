5fe6eb005c6fce386cc345a49f92efb2417ed7bc
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:11 +0000

    glad again warm callat that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
