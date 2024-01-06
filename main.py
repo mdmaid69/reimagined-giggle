7614f7934c3460aea22a4bc220b10fb0a50da3ae
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:17:00 +0000

    harm seriously begin future statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
