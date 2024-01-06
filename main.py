7f9e7c894c05e7445c1f079a34011295ccde7fd5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:04:02 +0000

    watch market almost send so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
