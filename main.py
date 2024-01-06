a743c63bbcea63e7a81863a7f9f74b36dd55b2a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:12:20 +0000

    force sore give open concerned

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_min(numbers):
+        return min(numbers)
