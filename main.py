c9dbfe61085588f1a664e29bf245c7dd492727a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:47 +0000

    woman wall everyone cold criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+print(math.pi)
