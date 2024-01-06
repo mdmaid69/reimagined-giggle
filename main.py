7f784d7b3c17c891315135f5b790a532c60445ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:23:31 +0000

    distant lie situation picture sight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
