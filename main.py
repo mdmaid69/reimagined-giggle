f0a85fa93adec805121ebc7baa994ffe5affc72f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:46:04 +0000

    indeed round through party secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
