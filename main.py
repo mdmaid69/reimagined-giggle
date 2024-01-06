fe7a052176c325c36ad66182b52a5a08e4d4151f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:14:53 +0000

    challenge stare condition big call

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def reverse_string(s):
+        return s[::-1]
