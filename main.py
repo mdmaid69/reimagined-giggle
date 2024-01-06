23a96b5699906323e7600eb4cf012fef1fc0ab28
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:20:03 +0000

    fit meaning when apply 179bite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
