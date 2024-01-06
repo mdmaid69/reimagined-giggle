db48cf0f17f8ef6271bfd4dbdff9860fc29c9396
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:11:04 +0000

    deaf reduction characteristic blue proud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
