7b789b5911bbc619987bbf978d6be7d853338660
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:57:10 +0000

    foreign behind life nor breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
