be73bcb8387eb7bf8e2227907e3d12936e55bb96
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:53:34 +0000

    organize poison organize replace whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
