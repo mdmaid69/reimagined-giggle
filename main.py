30a968256d3504014db80620ccc743f89f65b89e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:32:52 +0000

    mixture deep general doubt Ee

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
