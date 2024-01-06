cfb5f28e0bef6aa5a837fde80b1bb313bda99c2e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:58 +0000

    delicate bear perhaps determined goon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
