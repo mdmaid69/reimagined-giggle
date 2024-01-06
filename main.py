d3c9ed51df35c2d4f41f9576c974611a83f7fe30
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:40:46 +0000

    general idea trust sort on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
