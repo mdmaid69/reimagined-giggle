09dd118555ba0a61358e61a149a6ce2593f0aee0
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:25:36 +0000

    study dream basis high pick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
