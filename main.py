2385275c62318a642958b5d1b9ce13375dc0a20f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:32:15 +0000

    owe offend judge thank company

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def add_numbers(a, b):
+        return a + b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
