b0151dcf6bb1c3255a66ed254d77fd11df7a7fbf
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:31:46 +0000

    entrance aware just travel wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
