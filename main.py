856b0cda934d17ca8bfad7e64b31a4379eaa756a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:02:38 +0000

    admire indeed sea ago unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
