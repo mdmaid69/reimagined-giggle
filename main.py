314928fa1a79a3bcf6908ab1294c4eb300395c25
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:54:15 +0000

    warm keepup smell worth phone

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
