0e48c7738ed17344f858935d6eb0eeaa1b7939b7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:06:15 +0000

    child abroad scare keen show

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
