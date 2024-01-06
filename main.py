bf2600fd28d6f4298f79974c83551dd227cc0d45
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:50 +0000

    rough night dish exploration include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
