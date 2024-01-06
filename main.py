6d99c0ca65e564520bf7bd9230c3c7ba294fdd1f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:49:35 +0000

    reaction group ill draw exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
