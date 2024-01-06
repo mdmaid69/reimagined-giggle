f6684cbe14067917b31a07eaf74f69eb82827f08
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:45:58 +0000

    give draw touch claim have

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_speed(distance, time):
-        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
