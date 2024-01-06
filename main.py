76781691d30729938df65651a540bf8c10c7c1b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:39 +0000

    target left strange hear across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
