0574fc181f6590e8a7bb4da33de93a87b691206a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:30 +0000

    original fetch remain journey relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
