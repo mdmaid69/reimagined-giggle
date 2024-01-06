664ea3f973a7d87e4d35043511e8b6fd564f0dcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:49 +0000

    project really switch somewhere impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
