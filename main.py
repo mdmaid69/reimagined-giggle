c8deab9afc1dfc3b1f669a9de11ebc2365fda3a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:43:26 +0000

    proof meaning least runaway now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
