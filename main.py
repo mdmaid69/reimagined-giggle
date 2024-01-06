d71a559a7068944a1d9fd7bdde36f98c65acdb90
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:39 +0000

    fortunate murder fortunate each heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
