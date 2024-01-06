48a8d50a7c472b9aea5f71ffdba6eaa6f2f95211
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:22:05 +0000

    opportunity difference careless exam gap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def append_to_array(array, item):
+        array.append(item)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_str(array):
+        return str(array)
