8f0d6682c37316a9228996a0ee6376f73af1ca5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:38 +0000

    question over her ring only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
