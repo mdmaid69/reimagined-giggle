e2f0d6da6f15fcfdaa5178f76ef72cf1645cf491
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:12:04 +0000

    beyond love tread radio something

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
