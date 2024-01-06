ef75735cff979760fbb888d1a6d577f97b42d157
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:44:21 +0000

    official refuse extra short far

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def sort_list(lst):
+        return sorted(lst)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
