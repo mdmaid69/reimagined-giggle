0153ad7811fc7d3c0f5b7ff56f8992bf95296f03
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:10 +0000

    spring across could connect hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area(radius):
+        return 3.14 * radius * radius
