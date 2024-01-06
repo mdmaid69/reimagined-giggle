7d567e3b8fdbfb3b9a94b9d43d8029e211fbae53
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:55:57 +0000

    traffic relief issue quit flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def count_elements(lst):
-        return len(lst)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
