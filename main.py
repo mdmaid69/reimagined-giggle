7004b6b2429432e71b2da59df56db4bfced0072d
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:47:14 +0000

    police mark harm door emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
