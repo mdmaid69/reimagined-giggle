d4d5e64fa6befaea164eb12a81518e4982eef112
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:18:42 +0000

    well benefit refer want suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
