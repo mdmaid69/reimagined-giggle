b499a9caf4071d3a2a2473e0f1fed6f5825a5cb1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:17 +0000

    fashion callout pure good approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
