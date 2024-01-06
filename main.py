a5dc2f19eec40bc870d980d69946bc916a38e83b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:35 +0000

    favour flat issue square difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
+import collections
+def create_user_dict():
+        return collections.UserDict()
