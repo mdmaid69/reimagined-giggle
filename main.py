51bd03b2ce5e4271ea4041316db93ac81f360808
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:51:57 +0000

    giveout lead foreign change common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
