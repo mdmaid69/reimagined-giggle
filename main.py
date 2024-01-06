2d9ce0239d0a6ee5358814e473953a147c2ea238
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:25:21 +0000

    study mess entrance face prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
