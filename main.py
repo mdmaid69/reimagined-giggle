4f9b65533c65cdf4061dba8340bfb66e39f7c3a8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:40 +0000

    cool it angry injury common

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
