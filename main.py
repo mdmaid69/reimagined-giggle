e0fc8e3d212b9c9b459c9342c586f69599353298
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:06:13 +0000

    declaration whom complete actually popular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
