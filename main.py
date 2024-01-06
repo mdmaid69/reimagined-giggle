811e01ac8144f516349a14dc18754270a415e414
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:49:59 +0000

    row official start drive unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.time()
+  def count_elements(lst):
+        return len(lst)
