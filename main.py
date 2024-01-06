3c36b81fe11b1b937969d51e60f2049c45c1b93b
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:06:56 +0000

    second thumb runout might professional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
