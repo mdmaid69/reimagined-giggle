44934b441cbf47ec61f44ea81fec1145e029fe6d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:14:28 +0000

    drop forget promise game aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
