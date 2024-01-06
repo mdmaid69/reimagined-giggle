c0b8abaa59c5d942860879622b12ac7dadbf5a97
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:32:02 +0000

    light leave favour live family

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
