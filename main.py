5f898f36592f92afb73ab31a19f07244486f7151
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:00:23 +0000

    declare club square collection gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
