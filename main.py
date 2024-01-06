4ed60a4c3a69be3b1a53e4cd2e3e91909762aba4
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:45 +0000

    wrong pair attention respect quit

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
