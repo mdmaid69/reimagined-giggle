49205c1da612a304a760554d215837d2e83ef27f
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:22:35 +0000

    long mysterious mainly clearly involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
