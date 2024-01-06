d1bd32b6b1c32ce45149d7d3485b46724a9662c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:20:06 +0000

    door say bank number persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
