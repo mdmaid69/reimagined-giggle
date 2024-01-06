fecfb353355910f5bd1fb5b624cead579200652b
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:20:57 +0000

    since extremely day public feeling

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_min(lst):
+        return min(lst)
