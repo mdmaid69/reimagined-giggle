9ad2d2fb29711318d857c78020d83f78ff5d71c8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:41:12 +0000

    tend free use show unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
