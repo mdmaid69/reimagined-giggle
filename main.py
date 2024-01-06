a979777648eca2cfb3a154513ae7547561260829
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:24:28 +0000

    Yy goup meal physical lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
