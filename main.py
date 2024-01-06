6bffce86ca57ba04e5703e2ef990e0a146bbceb9
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:51:33 +0000

    sort transport detail worth any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
