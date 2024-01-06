5c4f69c44fd47246ffc8c9c6bbd030d74e0f5122
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:22:06 +0000

    meanwhile visit new shade finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
