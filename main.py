d629984cb357cdf2c05d05a4d7316f15e0107957
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:49:52 +0000

    light expression willing dry on

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def count_elements(lst):
+        return len(lst)
