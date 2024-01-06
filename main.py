230aa44cdfd453363aa01d5256d2e125de84bb60
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:29:51 +0000

    bin Uu already better badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
