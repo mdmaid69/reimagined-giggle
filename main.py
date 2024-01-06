f072b8caf355ce1729bbd58ab5eeb7b2d57fc893
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:01 +0000

    blue feature discover bright interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
