6132ac7aec8f0dd3f6a5647b5ee53327db803b01
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:44:51 +0000

    science waste instant along discover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
