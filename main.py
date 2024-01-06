44856b81dcd0bea2b058074a8d3e9406a3c791d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:05:13 +0000

    gate giveback ordinary left label

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
