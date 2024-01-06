6dde892b7077bc83e08c4c4d3055c95a2ee6605f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:21:36 +0000

    stage print tour commit tongue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
