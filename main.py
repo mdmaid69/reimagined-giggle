32a76bd3ee8c8b63237405fb5e21cdff6450df62
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:35:14 +0000

    turnout fork since nervous brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
