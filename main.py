26b27f6a56fad5922b165344d6ccf324bbe0526a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:25:53 +0000

    godown luck another less callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
