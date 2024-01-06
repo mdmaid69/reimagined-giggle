c789b0edabb08452aaa68edc6625bd4605928575
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:02:20 +0000

    guide bang sea tomorrow fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
