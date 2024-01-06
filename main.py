ad55d74beb91e32dec7bb3d0632a29a0d99604bf
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:52:00 +0000

    over tragedy attempt punish measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
