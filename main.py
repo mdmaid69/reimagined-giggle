d834d9a68e6a4ed30f3f90cd58622cb386700f39
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:16:07 +0000

    wander normal keen sight record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_counter():
+        return collections.Counter()
