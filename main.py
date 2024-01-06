9080f683a620ffd1ddc5f29213acc97056bbbf9f
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:57:09 +0000

    drop flat cook approve grand

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import time
 def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+        return time.time()
