cde9bd25b5322903549ce08773f226d76c1a0664
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:00:06 +0000

    rest machine such also bet

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_min(numbers):
-        return min(numbers)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
