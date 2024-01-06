bbf9b273917bb9f8decadaa5bb1db74f5cd8e9b8
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:35:26 +0000

    place worried practical direction lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
