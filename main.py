fc72680caf9617523312a974da0a55c2bb365b42
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:35:15 +0000

    everywhere gradually satisfactory wall brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
