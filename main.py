e7f2d037bb00532a2a9cf54398284c477ffd8599
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:17:21 +0000

    become refer spare mark fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
