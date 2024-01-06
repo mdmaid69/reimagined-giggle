4cc5680b9c79d8abd17c94adb6b3a856706e420b
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:34:24 +0000

    callback point play border drink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
