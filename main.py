0dc973ae7d42e2fd2ad732f6e99e2f93f203deef
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:39 +0000

    duty dark peaceful brief speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
