e6f073bfa8d2cb7393fa923ac360714b89a9f286
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:53:24 +0000

    thinkof container bang emphasize anyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
