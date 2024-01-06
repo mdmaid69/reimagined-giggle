e43af399566aaf711234e1c2421d235239d5db7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:34:13 +0000

    medium government object score service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def square_number(x):
+        return x**2
