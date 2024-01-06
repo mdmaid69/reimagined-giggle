20baafc4f6b163e68cdd4146e5654631538866ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:12 +0000

    national conversation above bright war

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_current_working_directory():
+        return os.getcwd()
