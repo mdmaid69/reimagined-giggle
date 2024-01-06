f0a9bbc7e736515a159b591021e1f983199feb3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:52:28 +0000

    aim wish move squash house

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
