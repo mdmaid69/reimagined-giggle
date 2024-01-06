e74587197d2c99dfe54c6916e1cbdbe140f99e08
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:25:33 +0000

    weak ought ring instead mail

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
