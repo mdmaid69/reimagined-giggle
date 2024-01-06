8e47c8c7385ac73e6228759a34b6d5c01081d2ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:52:13 +0000

    road back as wide tonight

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
