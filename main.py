831a13bf3728cbdd437b3e7951c80e323b8c2b2a
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:05:16 +0000

    well original plastic couple their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
