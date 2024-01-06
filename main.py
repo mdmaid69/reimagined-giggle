5b0cdb9da4081912a704ad5c16c934a14dc98543
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:08:53 +0000

    sit never suggest before bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import datetime
+print(datetime.datetime.now())
