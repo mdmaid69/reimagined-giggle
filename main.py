8f1e34dd17ea25637a22cc6a1270fba8fe1bf860
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:35:37 +0000

    bye someone name busy calloff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def square_number(x):
+        return x**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
