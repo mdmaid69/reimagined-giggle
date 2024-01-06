4f2f3dc6ba666c8ac2a681e169aef8bc9eca8a6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:36 +0000

    immediate them sweet under throughout

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
