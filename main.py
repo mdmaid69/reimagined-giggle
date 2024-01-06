a30323477a872c266e35af251b5f8ed51fe4742b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:24:35 +0000

    fasten laugh provide light fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
