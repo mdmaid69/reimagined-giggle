3a4dedbac50a8ba622b7b7090147780e1baafcff
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:59:32 +0000

    score avoid else ever true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def greet(name):
+        print(f"Hello, {name}!")
