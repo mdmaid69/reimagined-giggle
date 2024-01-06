dafc69a0450afeece839f318c260802dd8b887a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:16:07 +0000

    comedown report respect mistake violent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
