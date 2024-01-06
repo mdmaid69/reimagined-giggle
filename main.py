375f5460e426dde2bce75bfeac6e6837417519c4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:43:26 +0000

    admire grow container judge fall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
