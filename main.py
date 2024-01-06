3ecb28e4016cbd6fed83b7a79acfff86f2bbc769
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:26 +0000

    urgent fix giveback comein either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
