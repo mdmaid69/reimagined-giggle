b57f9f50a3dd8eee76210af6f36db6a6a5e60380
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:55:50 +0000

    uponon pretty grow comein variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def clear_array(array):
+        array *= 0
