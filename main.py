40031772d9f7db3e248bd748fc1f14e6284e3651
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:24 +0000

    set forget firm invite limit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
