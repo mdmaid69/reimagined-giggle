f7b27e023aac2c79c53dfb3680652574e4876087
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:36 +0000

    plant unless fence standard favour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def extend_array(array, iterable):
+        array.extend(iterable)
