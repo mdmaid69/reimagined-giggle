8f6bb3accd459b77b75ce49c7540adae74616cad
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:54:54 +0000

    basic protest burn talk sing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
