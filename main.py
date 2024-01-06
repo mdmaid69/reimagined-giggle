5c268da7b1edf39f86442c4612a90b43684dd3bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:12:04 +0000

    sorry range name getaway hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
