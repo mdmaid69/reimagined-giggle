90fc7b65aebf0397ae9ed66920c0c287544f622a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:16:05 +0000

    pin about important appearance custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
