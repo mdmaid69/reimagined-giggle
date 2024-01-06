56decf48f82540c67bd1bb12a660cae4850f35f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:54:21 +0000

    attract rub television no tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_list(array):
+        return list(array)
