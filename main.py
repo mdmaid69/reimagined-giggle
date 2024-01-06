40490f1c594868905a2d8046f43446abf991688e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:36:06 +0000

    woman computer popular friend full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
