689e7545eabfc1bc6439365fe96e97118f13fbab
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:42:07 +0000

    advice breakinto relax debt sort

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
