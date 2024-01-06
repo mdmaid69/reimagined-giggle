fbeaab89f5bd70498aedb24c23dfc7695c483895
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:26:04 +0000

    smell model fold authority care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
