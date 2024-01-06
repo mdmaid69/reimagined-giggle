a56d95324dd04c8b00d1d095dda5d668b058f138
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:44:18 +0000

    attitude represent method justice butter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
