22d5306566331826251c972f4306453165e0a983
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:01 +0000

    line behaviour certainly expect cause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
