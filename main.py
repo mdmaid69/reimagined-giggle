c1af985be394f568afef69ee6d14ec3fe7e7f77d
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:39:11 +0000

    judge number dangerous building comeoff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_repr(array):
+        return repr(array)
