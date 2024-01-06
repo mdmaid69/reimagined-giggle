75af94baf607fdb26c8c006907df2f588d3bde5c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:58:43 +0000

    circumstance star some keepup listen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
