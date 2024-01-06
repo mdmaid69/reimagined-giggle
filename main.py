a310bf94266c007f559aef1d452060048bd902aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:28:21 +0000

    acceptable claim memory temper job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_str(array):
+        return str(array)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
