94be21343eb6ac4a9c60be5e10f57d1bfbc230f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:48 +0000

    breakup lead Kk regular complicated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
