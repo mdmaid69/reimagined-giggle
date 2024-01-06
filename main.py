c6c9aaf0f4e707fa755ac6c8b289345302ae0c53
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:11:33 +0000

    behaviour your bye sky worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
