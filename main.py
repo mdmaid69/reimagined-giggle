2c27ded656b87c2baa3208199feabbf82c37db42
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:24 +0000

    careful mass sweet message sick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def reverse_list(lst):
+        return lst[::-1]
