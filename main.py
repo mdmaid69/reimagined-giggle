a7c10c08de5f490aacfcdb53278b4910116b4b33
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:54:05 +0000

    giveback complicated trap possibly teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
