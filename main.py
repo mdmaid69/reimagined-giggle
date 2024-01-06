12146f02a2e62898c239db8999d0e1b9c5f727c5
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:57:50 +0000

    takeoff thick watch special mystery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
