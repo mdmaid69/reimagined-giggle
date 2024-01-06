9f19b470e75067a106f6885289de163effff1e0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:55:25 +0000

    theirs guard town now watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
