614fcd05ec45b6d274d4f576f35e1fd902a9ae44
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:22:02 +0000

    crash rush surface left turnoff

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
