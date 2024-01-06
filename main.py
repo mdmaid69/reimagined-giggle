d59c364cde26a8ab4a4518d851740f99b79fe470
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:40:41 +0000

    recover corner voice matter piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
