dc2c48e712c5aec0545a291e05a7054bb6032e0d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:03:25 +0000

    fashion occasional public major second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
