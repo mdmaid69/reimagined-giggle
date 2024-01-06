c7f1dea61a640ca5515f8dae845e129d9e222fdd
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:04:56 +0000

    reduction age getout getback real

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_floor(x):
+        return math.floor(x)
