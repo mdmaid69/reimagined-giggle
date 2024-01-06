5ce93209b03e6c7a9a14bb332fbffa1320aa2781
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:44:31 +0000

    generally comefrom draw ourselves eye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
