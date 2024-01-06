a315dd5daa3cba745951cab88e597a0932e62aa6
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:05:07 +0000

    welcome take defend normal authority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
