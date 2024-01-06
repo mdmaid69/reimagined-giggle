bd64e2b7fd0147c5722f28ff849dacbda1ac323b
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:07:42 +0000

    belong bad holdon air turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
