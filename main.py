fca352e426687b7a24294bba7641157f176b2959
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:55 +0000

    hello indeed bad black deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
