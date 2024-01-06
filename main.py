a7e04fab93bcec55aea491a90df0f44def8f91dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:18:14 +0000

    fond see opposite have bye

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
