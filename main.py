143d1166217d88986b01df675cd42a16b5cc8a77
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:19:46 +0000

    patience exchange cheap push detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
