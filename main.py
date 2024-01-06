8cb8b1cf638548d17d36bfe3cb6012f723d06bae
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:14 +0000

    classroom straight approval gather raise

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def count_characters(sentence):
-        return len(sentence)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_floor(x):
+        return math.floor(x)
