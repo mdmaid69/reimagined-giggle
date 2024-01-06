15f8e8ae067e158bc472dd55926822e44dda6d98
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:10:24 +0000

    complete fierce watch lean stir

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
