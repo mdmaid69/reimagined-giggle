a0440091932bcbbed36db0cef2842d6a6d8d829e
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:54:50 +0000

    model apparently consult bin murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
