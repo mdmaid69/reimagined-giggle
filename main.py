4e5f16c52d063628855120c4b2cfc75af99dba3d
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:28:39 +0000

    tip dance fly punishment maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
