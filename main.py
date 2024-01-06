012d75fe3e530fde4185fb97c96118180e7bdd4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:19:22 +0000

    already law agree whichever tall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_volume(length, width, height):
+        return length * width * height
