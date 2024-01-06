905e0644c89770584bf63c3331629c4c8a3107d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:06:46 +0000

    squash tend occur squash object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
