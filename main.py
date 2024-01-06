85024c31275d05adba256d2a07643ae6573d1d10
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:09 +0000

    next explanation feeling congratulations central

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def count_elements(lst):
+        return len(lst)
