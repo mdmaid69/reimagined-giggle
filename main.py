c88260789019e55806096633c3debfb05558ef68
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:04:44 +0000

    every standard listen comment calloff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
