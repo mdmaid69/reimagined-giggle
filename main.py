df605b0978a4fe4780a5d6a2f991197e0a58a6a4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:53 +0000

    build majority heart sea sell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
