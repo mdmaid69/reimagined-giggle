67c8eacea9840e75d8836ef50541ebb4baad6bcf
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:40:17 +0000

    cheque squeeze immediately cut lend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
