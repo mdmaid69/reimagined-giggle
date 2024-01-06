27bf3cecd57f002ac4b954b20360b74de7a052fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:04:06 +0000

    field shrink friendly performance exit

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
