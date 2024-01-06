059765ae3b9756d9c9eaecf62306ce8136cb0c07
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:07:06 +0000

    boil age best throughout goout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
