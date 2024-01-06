2666aba92488f903a901491283b0ae4ee3a8c615
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:45:03 +0000

    excited shall confident leg meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
