d31c74f63f7efd0af1e94cec7d3bcb3fbc78b690
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:56:46 +0000

    pain interesting want snow search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
