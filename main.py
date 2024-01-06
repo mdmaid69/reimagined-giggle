ebf8b5adbf5b01d3b94c6b81627e636431761bb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:08:43 +0000

    positive thinkover introduction sell none

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
