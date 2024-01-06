ee1084c2b3528510e3d51e0fcdba7b85c44c76df
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:03:36 +0000

    miss comment disagree illness move

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
