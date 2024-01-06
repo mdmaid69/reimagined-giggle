719256594f95efbdbc8de673a7e5577da79c3bf9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:46:45 +0000

    order Gg long character lookaround

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Characters:", len(text))
