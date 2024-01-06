bb14926ab1bfd1f471a34991fc0491a72f7b1177
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:14:21 +0000

    fair runinto uncertain Qq experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
