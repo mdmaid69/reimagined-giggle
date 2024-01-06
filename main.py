dbe2fe1a634bdf591f6c6482d58a95790fb7b11e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:57:24 +0000

    Kk shall consider muscle shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
