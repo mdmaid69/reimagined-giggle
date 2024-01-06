09a517248743644b0981590d1fd44155d72b32dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:51:04 +0000

    uniform accidental strike 179bite memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
