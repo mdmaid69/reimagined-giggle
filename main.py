e408860d86b3e8aa2ac15a7d282178a3b65da4ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:09:00 +0000

    nigga tough wash autumn lookaround

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
