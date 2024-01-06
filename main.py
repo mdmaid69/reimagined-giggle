2c099117c1015b088f07abc3d91e1f2060962a7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:36:59 +0000

    demand damp size juice mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
