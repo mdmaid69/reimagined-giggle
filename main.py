932fd0c5782fd32bc17197ec1bc424e430726621
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:55 +0000

    whenever study pause hospital sure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
