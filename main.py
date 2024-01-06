b5e86a6cc093c13244b132785deb04e6fb2cbbff
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:53:33 +0000

    somehow reply to prove disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
