3c591f0d9a1f4618403e853a64847a89bffa82ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:04:54 +0000

    treat special wear afterwards pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
