986ca63bb9b11abdc182e26f6f9085c4da6648a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:56:28 +0000

    original speech someone protection its

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
