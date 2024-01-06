89827230b14a71f33681f65a7f75a2119aa3e87b
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:44:49 +0000

    afraid have protect meanwhile cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
