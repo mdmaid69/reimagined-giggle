765c2a3d40bc477e2785540ed94945ace25fd196
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:09:58 +0000

    Yy destroy talkback square since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
