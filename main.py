68fdd83c81f4fac5783b9f2b214344fd19298d15
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:24:32 +0000

    opinion unfair quantity reply disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
