15214f5ef5200c87fc801e10784a5d6c1b76cc8c
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:23 +0000

    draw before position cost declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
