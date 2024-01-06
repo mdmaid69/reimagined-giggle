1d986c15e4beb8769f5980eeacf827a4c15d8ee9
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:28:54 +0000

    behaviour wall wish official goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
