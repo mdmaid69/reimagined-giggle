31936c5b917505bc9e5157d1e498d54431fde362
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:36:11 +0000

    stir pause in home behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
