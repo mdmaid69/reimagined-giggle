b60dc997e9772bfdb21b92568faab7af16b68fd4
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:22:35 +0000

    damage recover comment niggaacademy manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
