798a82981fc91f0a42964e357eaaca43cbe91505
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:56:00 +0000

    discover appearance private feature moon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
