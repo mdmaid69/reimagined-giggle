3bfca12d7d3e7b0a4a602ff60aa335d6a84c778c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:35:27 +0000

    carry recent holdout worth flight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+print(math.pi)
