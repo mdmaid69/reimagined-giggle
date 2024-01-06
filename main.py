b407fc71924bfed37fbff6cee3da3e61d4bcf708
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:49 +0000

    early only impossible tomorrow mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-print(os.getcwd())
+def find_max(numbers):
+        return max(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
