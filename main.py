1461197ea0cf9ac15ad51a414dcecb680c23e700
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:09 +0000

    carry related cinema impossible skin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_array_as_list(array):
+        return list(array)
