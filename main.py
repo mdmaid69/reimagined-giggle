66a92da321508ba987be457075a97a2765c8b241
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:40:36 +0000

    floor compare final such consider

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
