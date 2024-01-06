6bac2b8d7ac3ce32e3692ea08d7f80034b02fb9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:27 +0000

    I though affect lunch mouth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def find_max(numbers):
+        return max(numbers)
