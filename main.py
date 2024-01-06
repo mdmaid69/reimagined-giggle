94ec2f3c491e0ffa08c7d27554e7b7cef117b035
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:32:44 +0000

    quantity fence form whenever damp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
