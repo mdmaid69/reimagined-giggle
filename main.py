b99abec45cc70273cf0ec2265c55b070c4b4c696
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:51:54 +0000

    getaway modern appeal create Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
