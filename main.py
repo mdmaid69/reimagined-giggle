96bf69d898fa0c3dc761d8fae8035a8061fbb6f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:45:59 +0000

    miss treat rain fun behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_acceleration(speed, time):
+        return speed / time
