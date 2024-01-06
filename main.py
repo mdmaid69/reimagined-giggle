7c39308c27c54ecad273969fcb0d013a3a6aaf5f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:31:11 +0000

    official scheme large march feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
