119c6a7cd1b421f12473e3aec1b5c4636b66c6cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:37:44 +0000

    cruel speak snow instruction then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
