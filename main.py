74b16f14321a3c448f3636d47c455207fc143fcb
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:06 +0000

    agree high accept answer rob

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-for i in range(5):
-        print(i)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
