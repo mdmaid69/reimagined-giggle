34ce0285e11368b82fdae68fee180bf4941e8425
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:33:56 +0000

    generous high innocent cook hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
