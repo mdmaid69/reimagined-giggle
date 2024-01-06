0faf97d424c2281dcf6e36ba4e4b84217e4fe30f
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:21:47 +0000

    return adjective idea excited everything

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
