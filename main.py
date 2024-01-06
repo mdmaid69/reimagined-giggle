d4da139ad6e8d6aec86849f52e93456ebd77e1b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:51:41 +0000

    deliberate spring suffer either whenever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
