9883df0d4ea7ffe1d454d08e08f682926dc687b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:32:44 +0000

    hang why getback die pipe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
