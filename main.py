a986ca9af8ff5fcbdad3cc09a3143393963bcab0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:53:27 +0000

    quit no popular nerve store

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
