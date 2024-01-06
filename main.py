3276fba74f5a2afacf915188d2dd80a5fa9030f3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:34:07 +0000

    television work move treat board

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def append_to_array(array, item):
-        array.append(item)
+def count_elements(lst):
+        return len(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
