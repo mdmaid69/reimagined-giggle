e1d0decfce3d63ca4aec52221529c8e39ea3c2da
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:37:48 +0000

    wander Ss travel arrange smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
