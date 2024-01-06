fe60df8b8cb063cb3c59239e6290134dbdca41ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:24 +0000

    break up except smell laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
