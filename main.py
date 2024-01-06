14f5e63b3c0a163ae89b160c617fb0446db1e34b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:47:01 +0000

    only exciting trial surprising correct

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
