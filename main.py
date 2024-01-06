2298c8593aa277c03cffedd2343294107218d5f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:56:21 +0000

    find company science speed punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
