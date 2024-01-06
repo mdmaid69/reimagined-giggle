22859dccf8a0e41b67dd03f6b353539e601a64c7
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:10:44 +0000

    instruction fine money difference clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
