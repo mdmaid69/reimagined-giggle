bd5fd1aa41a6055cd5f5e71e8da302b150bd4cfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:42:46 +0000

    store everything comeback business treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
