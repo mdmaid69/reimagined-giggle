4edea351fea1a1f9fa54dc41035b26ed4fbc53bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:38:21 +0000

    study clean little television herself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
