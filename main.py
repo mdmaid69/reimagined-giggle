2c7d164224fbbbee2d274cd211c9e28ea8e048f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:41:29 +0000

    please throughout gradually gooff task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
