5a0793d1bf696a8910f20e7d7188db19442d777e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:49:44 +0000

    threaten steep raw pain team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
