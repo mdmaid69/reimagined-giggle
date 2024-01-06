52a69373dd45fb399da55dc5330ed9864d8c51d1
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:30:17 +0000

    grateful transparent badly score feel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
