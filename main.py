ae8a889ef54ee6ff88cb00a059cb2cf8c3e0f34f
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:28 +0000

    regular greeting opposite holiday platform

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
