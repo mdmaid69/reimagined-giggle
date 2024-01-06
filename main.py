bd753d49bc8e27ec5edf9144048fe4ded3b7e8a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:50:49 +0000

    advantage impressive furniture habit movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
