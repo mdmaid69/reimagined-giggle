a0a7e2bfb46ad61efea488773cc219869ab9ffc0
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:17:47 +0000

    whatever late both do flow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
