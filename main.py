8c90aa511f3f4e2ee6ce7980d84e16189de57b57
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:08:21 +0000

    almost indeed charge turnaway place

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
