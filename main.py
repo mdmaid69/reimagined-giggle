f16122c8d4ea5fa0a93b9474783a697427a53028
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:19:16 +0000

    street Oo most appeal street

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def square_number(x):
+        return x**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
