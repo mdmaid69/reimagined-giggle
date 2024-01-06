6327b76344eb5084e59a0cc79f316560fe2cf5e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:21:12 +0000

    principle yesterday pot present badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
