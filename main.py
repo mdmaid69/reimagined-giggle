00587fc291df1b25b83810f9863a1155651b513c
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:45:40 +0000

    cost cost place train fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def reverse_string(s):
+        return s[::-1]
