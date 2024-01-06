9908cd47528bfd49d1607fe184b7165633491e7f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:05 +0000

    offer plan slide present wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
