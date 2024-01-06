5aa4c6505f3ac3c6981cea2156e8d5637e303754
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:32:35 +0000

    between advice especially telephone later

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
