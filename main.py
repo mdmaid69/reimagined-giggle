7ce8098afd8b2586790f9c0d02698aa10a5b82d2
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:04:47 +0000

    birth sing theirs suppose guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
