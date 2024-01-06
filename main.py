0f13a9bc2562453e9375a8bb5d82eb617ac5f058
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:04 +0000

    second birth brown laugh problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
