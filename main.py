a8cef9d06fb9c8b268c10e84d7ec36fc113f7b3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:47:30 +0000

    together character firm immediately picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
