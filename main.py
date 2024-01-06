fb36be8890376bf99e753764cf4f4ef3b44d687c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:18:10 +0000

    doubt her for course advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
