f8a17e13b309a1d79596e8ab17d3018618823a10
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:48 +0000

    forever lunch switch account rain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def multiply_numbers(x, y):
+        return x * y
+def add_numbers(a, b):
+        return a + b
