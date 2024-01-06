f6ad2c39e2691f90ad0f8f353a55ed3ae11ba489
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:15:26 +0000

    guess rain usual cheek neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-text = "Hello, world!"
-print("Characters:", len(text))
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
