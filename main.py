b8c2740faa38e961cee1776e29dfc15a73b10e4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:06:35 +0000

    rather rain over too crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
 def calculate_debt_ratio(total_debt, total_assets):
         return total_debt / total_assets
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
