254ca17a6f55a1ab4a8811ab59c0a46cda01dcd0
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:29:41 +0000

    everywhere behave easy against Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def convert_to_octal(n):
+        return oct(n)
