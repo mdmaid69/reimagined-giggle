9d4c3696a47202675187001947afc55d62167d8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:35:00 +0000

    limit spare breakup favourite board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def find_max(lst):
-        return max(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
