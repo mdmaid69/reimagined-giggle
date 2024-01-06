755746527a5a0624c87b5779cc38e1b25d873fc2
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:00:40 +0000

    whether draw breakdown closed doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
