8624bdee6a96226320208cdbf4113a1f44b2b636
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:14:03 +0000

    steep that every hurt repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
