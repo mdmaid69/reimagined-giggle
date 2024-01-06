200a00b3bd6a213ab95a07d6ab6384d2dae74558
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:10:43 +0000

    turn shake artificial skin miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
