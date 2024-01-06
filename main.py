132433645adc5af66b07bedacfb572ef7e365954
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:56:04 +0000

    right belief further mess shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
