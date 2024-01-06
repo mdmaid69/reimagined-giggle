461258a82e856195d5d6743c5dbf53ef2cab91ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:54:18 +0000

    Rr start beyond he condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
