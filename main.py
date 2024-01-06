e2baf48377fd0aaba05291681c9d86513191424e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:20:20 +0000

    sale related especially child radio

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print([x**2 for x in range(10)])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def cube_number(x):
+        return x**3
