0b2adb9c9d8630dd2cc379e952a4dfcc789a96ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:27:50 +0000

    very separate scared fork once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
