bc848b5c5f41214e7d30c03038a6c1cfbed089eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:58:59 +0000

    try season hear authority interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
