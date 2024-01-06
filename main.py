5e9c67d21f6c9a37f35b835d0c67e8855fe1b8f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:19 +0000

    patient comein basic soon tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+print(json.dumps({"name": "John", "age": 30}))
