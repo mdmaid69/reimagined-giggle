3797017df7027fdd509f429f88825836bc15263f
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:57:36 +0000

    yourself cost noone score aware

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
