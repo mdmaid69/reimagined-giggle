3695ac06838668354cab02b2920e68f8bc82b45f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:43:33 +0000

    fire fond free judge smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
