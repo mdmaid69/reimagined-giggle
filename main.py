c04bd5e45473274c15c7b89108b1d790a74c3725
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:50:40 +0000

    speak chase scared exercise quiet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_palindrome(s):
+        return s == s[::-1]
