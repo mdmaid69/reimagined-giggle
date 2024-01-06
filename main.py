d9e9fa82f55cefd95495dd7a55989652ef2e569c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:44:34 +0000

    dinner holdup paper neither tragedy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_odd(n):
+        return n % 2 != 0
