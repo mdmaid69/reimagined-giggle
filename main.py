c34dfea2a948ea2342adc99be365e84f0325fb73
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:00:56 +0000

    spin bury way load know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_file(file_name):
+        os.remove(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
