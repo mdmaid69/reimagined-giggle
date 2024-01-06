078f30a5966adcce3eecab5be6e6bcbe91d5c74f
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:44:43 +0000

    suppose unlikely production achievement shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
