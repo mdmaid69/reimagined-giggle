3b7b17c6e5925c8440e7e42af8f3d82f1bbe2438
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:12 +0000

    accidentally equal cut chin why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
