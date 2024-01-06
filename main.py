10b907226f55160b0ee62de03afb8b7f3184918c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:23:46 +0000

    hurt imagine emphasize hear all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
