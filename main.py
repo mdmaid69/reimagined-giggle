32a6be05e8a2f46086100ff8e4d4bd674fa30ae8
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:06 +0000

    somebody education contact praise interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
