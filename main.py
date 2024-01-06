a31d93f0192ae52e873c1c779a04989f26a20af6
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:26:23 +0000

    go describe above month include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-print(math.pi)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
