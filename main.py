4684a3201d0070065000b42fc669ca580e73de41
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:13:50 +0000

    nice but window pain genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
