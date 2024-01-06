da49cf89f91a43bda75780566cc1e02b20e6e07c
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:51:18 +0000

    swimming quickly above art age

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
