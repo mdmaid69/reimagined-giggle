c68239d1ab26224cc06c0ed472aea0fd7d8dd73d
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:43:20 +0000

    second address before borrow now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
