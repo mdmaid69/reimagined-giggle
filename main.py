77389977c0b521fe3f281799ea1230bb397f6ad6
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:41:20 +0000

    win risk damage passage consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
