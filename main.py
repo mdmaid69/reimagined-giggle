14b31bb703288285d29843b2cc1f85f916b09862
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:12 +0000

    swim recommend find Itself accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_hex(n):
-        return hex(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import platform
+def get_os_info():
+        return platform.uname()
