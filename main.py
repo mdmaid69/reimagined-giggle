e32cb317bc222f2be0e46171887af9f0cf3c9f33
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:01:17 +0000

    give achievement proof bother shock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+print(random.randint(0, 100))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
