8952dc216619b3f71c5e4e60a0fd24e3ac9fc071
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:50 +0000

    brain regularly store harm professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
