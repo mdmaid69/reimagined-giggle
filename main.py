d121cc184f4e0d720c8d39797b81af1c636c86c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:05:34 +0000

    mistake minute Bb undo section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
