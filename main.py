4b0c19d846f9e25434e8884e2791eecaefcb4415
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:49:06 +0000

    convinced crazy factory country basically

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
