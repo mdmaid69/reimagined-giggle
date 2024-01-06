47d422ac350acd2f4cf890178244b9f71063aa4c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:29:11 +0000

    flow relationship save trial visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_min(numbers):
-        return min(numbers)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
