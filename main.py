d309a0abeb03417111f99e9e62ad7bbc637db75f
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:14:44 +0000

    goup record wait minor attitude

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+print(sum(range(10)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
