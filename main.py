aaf2d03bdaeda8308d9debd6e98b85367de90ce5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:31 +0000

    pin begin trade discover apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def print_python_version():
+        return sys.version
