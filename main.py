1d6259121535fe4d86cb4e176506dbfc943fea8a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:36 +0000

    comein foreign the exam organize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def multiply_numbers(x, y):
-        return x * y
+import getpass
+def get_username():
+        return getpass.getuser()
+def greet(name):
+        print(f"Hello, {name}!")
