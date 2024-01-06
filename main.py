18b1de7e4dc7ff6a10be775e749ac49a26e71e02
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:22:10 +0000

    water up slightly sudden form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
