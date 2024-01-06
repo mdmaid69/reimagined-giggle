84e20ebdc6242843c8f383b6ed25ded29b0cbbc4
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:52 +0000

    winter soft summer develop freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
