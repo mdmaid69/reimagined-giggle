b04a2f90a6c87d97099ad12b5973cb39fce6fb45
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:45:50 +0000

    arrange speed bin increase smart

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
