22f9fdb461f7f89ba48d00e29dbfe8305c13bffb
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:10:31 +0000

    cheat float mail tap unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
