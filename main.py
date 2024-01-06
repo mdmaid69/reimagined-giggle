09ca170b41048d2e5751ec733b76cdec1b7c5a74
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:01:49 +0000

    building usually fit please lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
