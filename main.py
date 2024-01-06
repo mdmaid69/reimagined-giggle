1c3d50a6fbd3fe3974c7d288d8b81afca4945a63
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:28 +0000

    grand keepfrom pollution debt blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_acceleration(speed, time):
+        return speed / time
