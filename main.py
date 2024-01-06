3436e41e9182aa523eb33b2a4ac7babc272c7206
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:13:14 +0000

    tune size happen private load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
