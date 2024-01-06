f83adfa4ef674d8d5c948095b9276b5e84f0aba7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:56:27 +0000

    moon unkind test find trade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_array_to_string(array):
+        return array.tostring()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
