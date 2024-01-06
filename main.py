14c3c8d85c533b6aa3d6e67c267fc59f66478d08
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:32:04 +0000

    forget beginning spoil stop original

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
