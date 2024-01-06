331afd23d0a0d89cbd70961a6c7789ba3e385d20
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:10:57 +0000

    sometimes play unhappy ready limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
