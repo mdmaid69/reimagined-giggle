46830cca5f14915349c86a6e01d4b55cda2302db
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:04:56 +0000

    giveback house mean turn comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
