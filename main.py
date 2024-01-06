5d5862874209d804a94ee8505c25a7dd9913b602
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:48:31 +0000

    area trap nor body runout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
