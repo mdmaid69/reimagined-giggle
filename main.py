f5ee993135f35c8e0b535d63544d9cbaf969eefb
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:39 +0000

    argue mark hide band refuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
