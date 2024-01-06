c0b6606e9080b7ce4573dd35b007f6a717916d34
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:30:19 +0000

    careful position punish after what

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-for i in range(5):
-        print(i)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
