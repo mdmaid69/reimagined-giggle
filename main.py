a408ec6be9dd04b9c1df9a12ee13ffbc880e1613
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:27 +0000

    force stare room you start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def clear_array(array):
+        array *= 0
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
