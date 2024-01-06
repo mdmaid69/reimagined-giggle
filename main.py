8b772f68395d074b1afc515f9715cb83da411297
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:08:58 +0000

    alive different show family healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def reverse_list(lst):
+        return lst[::-1]
