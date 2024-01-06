ab86b0ab4dfe091bebdea74c3d7b7e272ffe704f
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:33 +0000

    bottom much human disgusting sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
