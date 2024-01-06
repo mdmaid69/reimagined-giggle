c2616a1d21260026a1e2ed7f4ffd5d0ac14f5e3b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:59:09 +0000

    roof just press convenient example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def append_to_array(array, item):
+        array.append(item)
