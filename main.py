20beea0256c6ce53a75b1c78856958d0fc5f7dbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:02:28 +0000

    expression available style collect black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
