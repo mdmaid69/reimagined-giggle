d8dc2697493c11fbbd11b5bf412e8806745f6b9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:49 +0000

    transparent praise concerned popular future

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
