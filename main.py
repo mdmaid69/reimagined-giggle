0374b94733bc635f20e5258d19d7525125eab4fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:46:36 +0000

    interview splash last experiment at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
