0a5a91937099e308bd1ba72d2c9b8552f9f50cce
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:11 +0000

    ourselves much friendly temper error

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
