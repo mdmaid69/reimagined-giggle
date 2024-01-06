7011819f04acec32d2020728ac7a0283855e4238
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:24:04 +0000

    nonsense anywayanyhow group fold inform

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
