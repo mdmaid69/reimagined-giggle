d93deaa37c11ab9453d697fdb0d76d0582fc835e
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:35:50 +0000

    black means Ll mark tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-print(os.getcwd())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
