64721e641d141b5a7de2bc7bd122ea03bb76f05a
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:36:19 +0000

    contact drop cool move mood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
