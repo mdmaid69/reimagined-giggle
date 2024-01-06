31700f277755cfb43a10b4166a69943870536c33
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:18:33 +0000

    load turn pay still rob

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
