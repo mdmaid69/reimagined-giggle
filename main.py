eaad2dec3a31b5e64aef180c85be67d9530bc1df
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:11:00 +0000

    foot duty event partner fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
