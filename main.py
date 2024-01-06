d9f573346146bfea71642a392557ea8aa5f0595d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:41:05 +0000

    call house cool poison thank

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def count_elements(lst):
+        return len(lst)
+def is_odd(n):
+        return n % 2 != 0
