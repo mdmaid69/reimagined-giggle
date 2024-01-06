356b86376a94ed1e43ad2fe0730240ff76c123da
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:22:04 +0000

    peace related workon stretch control

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import csv
-def load_csv(filename):
+import json
+def load_json(filename):
         with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+        return json.load(f)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
