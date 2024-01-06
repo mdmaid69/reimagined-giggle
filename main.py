139f7b306a87e128544a67d13548390976af8b41
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:17:09 +0000

    instruction variety needle watch in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def count_elements(lst):
+        return len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
