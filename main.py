2f41375eaf2fced97986a530b9922524e0eb662d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:32 +0000

    pity seriously debt shallow press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
