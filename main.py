22a76e6dcd908edcd45a2a89f308ef02d3350c35
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:22:31 +0000

    disgusting exploration rescue bound lay

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+for i in range(10): print(i)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
