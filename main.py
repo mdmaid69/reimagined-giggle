480d133d04f91e3d2f3974bee63b4f3e473e8548
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:07 +0000

    reply Rr doubt glass show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
