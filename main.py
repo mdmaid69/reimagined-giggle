ecef6120ad769cfb802d5ec3bb73d69bb4bd7826
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:15:24 +0000

    action past factory dare attempt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
