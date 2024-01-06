dcbda522bc6feed1324fd7e27bb3827b161519e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:42:14 +0000

    sleep previous contain satisfactory before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
