6cea2765ac62793d070693145162a2d10f2e5949
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:46 +0000

    ticket lower along sore generous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
