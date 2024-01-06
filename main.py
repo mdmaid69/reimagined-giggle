db7c880f22ab70f2848a0dbf6a4d66449add0f09
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:06:22 +0000

    rate plain control basic waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
