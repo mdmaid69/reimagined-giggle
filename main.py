b5984e383b7a53846109900f78eb18c72432be02
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:08 +0000

    target choose determined company mass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
