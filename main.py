866be074eb7f22a84368f9cfbcf4a4d20e45f3ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:29 +0000

    finally bottom challenge have can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
