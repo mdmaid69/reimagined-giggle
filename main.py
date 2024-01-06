12a9dea2ba60fb69c7b4b3ffc2497886095f7b17
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:33:11 +0000

    tear back hole want child

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
