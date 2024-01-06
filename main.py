3a57327b7c181e88f9aceb296bb1ec67f55140df
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:22:28 +0000

    compete total business extra charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
