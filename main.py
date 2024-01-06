b6ed8a4d210198792588c8947696a0d300c76fd0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:30:10 +0000

    without motion practise after downstairs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_min(numbers):
-        return min(numbers)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
