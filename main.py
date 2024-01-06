1772d36bb8744d218e865fd001fd311872d7f8db
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:40:39 +0000

    original free instrument draw fine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
