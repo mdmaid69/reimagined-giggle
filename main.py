dafb0a493687ce950498e4e4b46dedbb0cc893e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:21:23 +0000

    large high else mine badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
