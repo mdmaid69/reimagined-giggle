ccff0e0257a6cc103a3192c61291d49abe9ec741
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:33:17 +0000

    rush image business seat prefer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def is_odd(n):
+        return n % 2 != 0
