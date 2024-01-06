1b18af6a6bb35cf0810a6a9bae5841e9b75e87fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:28 +0000

    acceptable frequent collection they rich

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
