4a9b9f0e23674ead620542515d2a929fa2429ab3
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:27:44 +0000

    flavour complete band measure to

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import math
-print(math.pi)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
