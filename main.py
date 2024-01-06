1fc3918e893dac1ccd5de7dc0765a4e7b5d71949
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:09:20 +0000

    date new pretty bunch message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
