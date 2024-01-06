57d17fc507014a16f90425057d67fac3e63d2652
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:52 +0000

    instant scare line car but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
