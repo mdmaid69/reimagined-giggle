073694eface3275a9d22d7c58e99325246e44c28
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:10:36 +0000

    interview keepout previous goup test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
