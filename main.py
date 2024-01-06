603e83867db111a4f8ab25ee919d557ddbaa8c58
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:14:13 +0000

    turnout risk godown study deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
