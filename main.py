fccde11044f321a37881c096da7cba910064f926
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:26:20 +0000

    flood bend worth better aim

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
