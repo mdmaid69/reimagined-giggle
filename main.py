1d01a2f99c5683e432445042989d5615ecefbac2
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:12:11 +0000

    sure ours keen pot morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
