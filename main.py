dd920d176ad2723966aa9abc5e98e7233c041be8
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:38:39 +0000

    possibly hope keen buy go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
