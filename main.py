9ca2c6b62bb687b3c3f0f308fc41a2e88499eec2
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:31:16 +0000

    number hospital point I then

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
