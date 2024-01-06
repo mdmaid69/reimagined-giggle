3ec4059b7b6c536c95df10bf8cf04ba391aa7aa0
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:47:02 +0000

    male lost crash yes sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
