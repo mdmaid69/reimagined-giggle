0a3729a3eb6a577e01d74c9a0491cf9201fb5d5a
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:24 +0000

    spin heavy study thought 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
