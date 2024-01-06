5a6bac61ba00a20416eae4489de7b1a42a419097
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:17 +0000

    number afternoon choice tidy easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_hex(n):
-        return hex(n)
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
