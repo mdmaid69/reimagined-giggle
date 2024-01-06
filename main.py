a1817291132b20d24e92f4db0ad096a772e1cf94
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:10:37 +0000

    sore hold Itself together 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def remove_duplicates(lst):
+        return list(set(lst))
