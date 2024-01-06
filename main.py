3c83673eef25fdd856db37e71f545b0ccdf5a86d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:41:08 +0000

    same deep respect guard about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def split_path(path):
+        return os.path.split(path)
+def find_max(lst):
+        return max(lst)
