0fca3c857e83ec44a2041ba0ddee6a7cc8c7653c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:35:26 +0000

    excuse shape flat properly section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
