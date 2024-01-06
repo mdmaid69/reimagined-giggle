68f6e97ea85832970a079638740d17749200541f
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:33:29 +0000

    decision steal or balance guide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+print(math.pi)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
