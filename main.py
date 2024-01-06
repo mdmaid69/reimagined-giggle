5ce701c4b6d5f34401ecc43615d001936dd837f8
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:33:59 +0000

    mean 572enter write let luck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
