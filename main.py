c393ce1e8923382710e285ade3596f3d220cbea2
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:48:22 +0000

    Oo burst phone wrap holiday

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(10): print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
