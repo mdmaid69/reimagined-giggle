ef83ba1d343a815a9ef7d9728ae4f81f6310bc37
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:42:31 +0000

    contrast sure workon bush judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
