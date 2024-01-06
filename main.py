60fac0f79f725f1aecf81a1ec9226059bdb9211e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:32:45 +0000

    example record towards large fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
