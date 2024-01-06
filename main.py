65c77a7aed6e668234e4ca06f36ec91e40760096
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:55:11 +0000

    little off base outside word

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
