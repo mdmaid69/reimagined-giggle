9ed7079f49a4e846453356feabd4f8da2eb28c7a
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:52:13 +0000

    fire keen risk often invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
