d5af68ec277624038fe241763eab1331bd3ef428
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:54:38 +0000

    attach wealth 565end centimetre whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
