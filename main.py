beb4d718d76da1acff52d91eb8bf78f44fc5e0d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:47:24 +0000

    blame obvious treat pressure almost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
