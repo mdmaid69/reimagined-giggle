cf3a2736eb71d0d2ba6d9f83159c29b51013d002
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:11:13 +0000

    activity success professional fair still

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
