1fb806b096330529c8dd3db5a0fe94cce54d3deb
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:24:33 +0000

    although hospital stupid gentle attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
