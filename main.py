0233c652dfc01f0c425183f808ac930e599cf30c
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:20:15 +0000

    impatient smell care top or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
