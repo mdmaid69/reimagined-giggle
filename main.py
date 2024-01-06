e6027655776096c00ea584b50693e9729ba65145
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:17:03 +0000

    maintain nervous sea shade keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import sys
+print(sys.version)
