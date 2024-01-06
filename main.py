8462cfcd0d42b36dd14d95bb9f45036b1ce390eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:34:34 +0000

    politics thorough birth pair plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
