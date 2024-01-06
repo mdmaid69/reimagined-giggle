b0daee7380fee7a96f6854efad8d6f61b8db669a
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:26:48 +0000

    edge scale success special behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
