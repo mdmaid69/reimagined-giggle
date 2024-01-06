f0b7840386441e789038d1fca10631b57afb63ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:01:36 +0000

    challenge mention protest seat birthday

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def count_elements(lst):
+        return len(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
