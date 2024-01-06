9561758078abc33a1cf5f6c93b7b128e481049cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:09:30 +0000

    reply high sticky card squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
