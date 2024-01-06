519f14cdb86caa460299f3ab97c03a010e453944
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:42:03 +0000

    official fit talkoutof take message

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import getpass
-def get_username():
-        return getpass.getuser()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_max(lst):
+        return max(lst)
