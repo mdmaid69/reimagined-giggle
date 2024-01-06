f623d47ab6f2a71cb021c9fb3feb7285656620cc
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:35:59 +0000

    card frighten rest without debt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
