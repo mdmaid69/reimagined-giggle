dfd8cee6aef57e60117b8b66c54967c420fe49d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:42:09 +0000

    evening disgusting knock section long

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
