9b3356e89b01017d7b909d489a176fc693548cf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:23:56 +0000

    bread press balance uncomfortable make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
