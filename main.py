ede0249a9e8e5bd89ca81db10967a3471d5fb950
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:26:54 +0000

    age behave pain deserve never

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
