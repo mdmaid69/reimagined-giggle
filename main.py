325317093f9366c21ebb5a0f8c24dea31070d9dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:57 +0000

    aim support middle plan sometimes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
