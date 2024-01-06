3cd9424a53428cb9333a91e7856c1b796b848cdc
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:43:42 +0000

    adjective cause region pain quit

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
