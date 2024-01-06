2e44daaa33e6718762d25d926d9b3fa624b264d0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:57:49 +0000

    dead shoulder bus suddenly race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import random
-print(random.randint(0, 100))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
