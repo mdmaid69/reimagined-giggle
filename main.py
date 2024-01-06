0bfb9a56cdaeaf59101481074d7046f67812f535
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:00:19 +0000

    risk seat busy scream average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
