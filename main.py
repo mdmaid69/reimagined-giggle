4101d859b6507d5df243f3daa0415abd9cfb01a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:33:52 +0000

    letter damage minimum character team

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+for i in range(10): print(i)
