7c39a8c45d17119c566848cda7b27c2fba7320dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:11:41 +0000

    ever house join score do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
