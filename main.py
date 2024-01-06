e1ffd11104148c8409172527fed83f649147cd36
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:29:26 +0000

    change most admire lock fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
