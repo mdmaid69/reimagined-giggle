1ef35a342fd1f9d277cabbc4189108decf8aad81
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:20:51 +0000

    mention foot mainly heat gate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-print([x**2 for x in range(10)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
