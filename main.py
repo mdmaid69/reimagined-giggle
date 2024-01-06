6ffc2c5723f5c88abb88c91bd05329ebab6e12cb
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:02 +0000

    height anything reject big mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
