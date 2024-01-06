a82b5a1ea5524a1f918c974c125776fcd6509a2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:21:30 +0000

    yours none aim impression callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
