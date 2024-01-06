7d3599d3636ba8deb85602d36386ec0d4d896669
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:25:50 +0000

    wish effect scream reliable slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_base_name(path):
+        return os.path.basename(path)
