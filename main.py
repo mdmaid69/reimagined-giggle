bd0603bfcc560d15f649b27c053fc1adcd9b4a83
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:37:14 +0000

    not practical importance interruption little

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def divide_numbers(x, y):
+        return x / y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
