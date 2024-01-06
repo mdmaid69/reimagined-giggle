8e49753d6135a6d68ad85ac567403628c135b528
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:25:03 +0000

    kind suddenly turnup success trap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
