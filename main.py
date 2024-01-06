e550a2add387e6afb6338b67c781b7bbeecfb73d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:11:28 +0000

    trade shade bit culture approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
