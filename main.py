26e28d95cdbdf0395b5016659f3d9d79eee23326
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:49:06 +0000

    clean length partner move false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
