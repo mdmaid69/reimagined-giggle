3d6b51261eb6d30d811d54fde9b9899f26f48e42
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:25:10 +0000

    present die water emphasis ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
