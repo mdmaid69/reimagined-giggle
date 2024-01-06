97e865ca36f0db1256e7cfed66801457505357fa
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:30:12 +0000

    fortunately rich not force normal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import sys
-print(sys.version)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
