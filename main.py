eb817b4395c65d3bfed57be5b465fca20c726360
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:57:30 +0000

    border myself demonstrate again lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
