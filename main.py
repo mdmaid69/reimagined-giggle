8d1844ebb6e3006c9ccb07b21411befb8f772bc6
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:02:55 +0000

    clear rough bury store much

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
