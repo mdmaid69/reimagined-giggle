1d498ce751aca56504c4021eb1567180acce92f2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:02:14 +0000

    become content turnon reaction middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
