1df29a782d8ecdb53b9db9a45497ac26d18ee006
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:13:46 +0000

    mad excuse fresh move offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
