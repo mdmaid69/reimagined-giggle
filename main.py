d22d3c1b067d4714a768cc322965d2be80dbd338
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:54:48 +0000

    between work mind hang thick

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from collections import Counter
+print(Counter("hello world"))
