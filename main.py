dbf3d7587cea40557026981ebdcc5508f064f568
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:51 +0000

    seat simple air snow lookup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def is_palindrome(s):
+        return s == s[::-1]
