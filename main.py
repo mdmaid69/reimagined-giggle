382ed5e306722f117e996ff3f837dc822bb37f13
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:00:07 +0000

    wish follow pretty mad glad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
