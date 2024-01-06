e620fcfefea468ca1666ae3d81daf7dc5d3b6731
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:35:41 +0000

    within wet themselves warm hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
