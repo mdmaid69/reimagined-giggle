98568ccce80bca8ca4fb34fdf99af6ba6e0ebb32
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:48:09 +0000

    charge claim sweet lucky bringdown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_to_binary(n):
+        return bin(n)
+def convert_to_octal(n):
+        return oct(n)
