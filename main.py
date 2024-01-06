3fdde691f10ddf01ec7b38d5426a636d1af04331
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:36:10 +0000

    moon weigh moment lead method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
