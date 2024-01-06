7b5f4137cac686a5b977bf510f52cbc405210ef2
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:20:29 +0000

    clothes possible sign spoil Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cosine(x):
+        return math.cos(x)
