413b2a4fa9bef1d8945bb495587c08d12448b1ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:07 +0000

    hear challenge means they cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
