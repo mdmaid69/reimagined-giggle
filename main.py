3bae64ea6f2b3b9edff3aca290d1405a7f1ab302
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:05:45 +0000

    nice one holdout deep introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
