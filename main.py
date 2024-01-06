2453b4b338ded600989a22fc429cf5f6aff21262
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:55 +0000

    tough aim natural guard open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def square_number(x):
+        return x**2
