4a30036ff58b2cdca3da3aa495b16e949bdef2c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:31:27 +0000

    damage design hand fond out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
