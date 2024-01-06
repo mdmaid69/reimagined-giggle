f20006bb61c6c34c5bc8d0e3063d2eeaf6acc6be
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:11 +0000

    train feel order blue partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_binary(n):
-        return bin(n)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
