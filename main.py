19eaf07285ae819783b40caa4accaa637cb503fe
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:07:56 +0000

    journey scare own shopping day

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
