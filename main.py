f153137f22c9bf3c7da23366738e9e69acaf79ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:09:08 +0000

    downstairs why mine unlucky wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_current_time():
-        return time.time()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def find_unique_words(sentence):
+        return set(sentence.split())
