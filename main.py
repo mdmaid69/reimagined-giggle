71d6996ccd6cb689eaa95976479f108cc70dae9e
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:37 +0000

    constantly under autumn welcome typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+for i in range(5):
+        print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
