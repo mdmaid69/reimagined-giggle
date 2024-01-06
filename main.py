6c5eda5e59953dba33329dc359d03d3ba75650dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:49:12 +0000

    short blame hard start mistake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
