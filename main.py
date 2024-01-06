af3d5a606ce5128986190144ce90de5369253260
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:33:17 +0000

    bright interview range challenge afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
