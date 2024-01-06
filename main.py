849d34ad647f48137f54de6ffb8f0ec82be3302a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:16 +0000

    correct before limit join give

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
