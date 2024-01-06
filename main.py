65fdb6000fe85326fdac9e4e9dbaa3eb12ff1bf0
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:04:08 +0000

    night fetch badly marry season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
