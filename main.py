7cc68a94fea51a78b0cd99db9ffbe17ac034383c
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:35:33 +0000

    truth transparent comeback contents goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
