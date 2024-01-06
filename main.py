f5bef0184c4e2e21ac3259b779fe7558762267f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:48:07 +0000

    cost cum regard issue tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
