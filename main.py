b8e6b38c7b4165373a96d4963fa297c65bfc4a97
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:38:24 +0000

    return though happy argument power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
