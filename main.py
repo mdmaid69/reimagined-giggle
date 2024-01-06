d06d763e79f5e37045807a2696eb2938fa24b5c2
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:34:48 +0000

    let drive mark lookback(on) behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
