b8bbf64e0ef3460700c0457e259821cbc4c09237
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:42:49 +0000

    criticism train judge computer me

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
 import heapq
 def create_heap(iterable):
         h = list(iterable)
         heapq.heapify(h)
         return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
