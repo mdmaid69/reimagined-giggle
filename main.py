9da21a09bcf495ed0416b5c5dc776cb0e117a916
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:14:32 +0000

    prefer constantly love give difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def reverse_string(s):
-        return s[::-1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
