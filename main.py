056b4f8b2953a041c8423185af32e591a6032d50
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:40 +0000

    hard skirt health scream during

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
