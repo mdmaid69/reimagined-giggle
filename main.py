0684a19dc6792304cfef9ea43ab78d77008acee9
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:09 +0000

    surprised claim fit getup problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
