887fb3bb2b1eed730a5ffc405fd643294d2ad8eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:12:39 +0000

    pot town print plan coat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
