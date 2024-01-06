cb776958e42595fa868ce682cb696f12d3c05d9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:34:02 +0000

    look wash tell left cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def print_python_version():
+        return sys.version
