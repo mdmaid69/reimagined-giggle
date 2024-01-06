beb21a39b40b54d400be15c386397c527338e94c
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:13:30 +0000

    though point through start treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
