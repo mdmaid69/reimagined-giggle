b0b3face178525263f44b0dabd5eb8a99eedfc0f
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:48:56 +0000

    evening argue block bang butter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
