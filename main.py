30cf0560af5c362db56a972912f62e1d24e1f895
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:12 +0000

    fix squeeze pencil throw tend

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
