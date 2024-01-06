db1719ed20c58b212ed3078e6f220b1d593b0029
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:45:29 +0000

    sometimes follow tap thing reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def append_to_array(array, item):
+        array.append(item)
