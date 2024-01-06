3c881ee0ecb60f8b8d206166a827b50a1c1e9e5d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:10:41 +0000

    rare middle band cough show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
