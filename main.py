d76b84968525db0d3c610944782f838968eca70e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:00:11 +0000

    beat inside clue trouble even

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
