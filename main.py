afb6fdf9fc65aa9d0c9cb4b368db47fdc5fb996c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:16 +0000

    bad quality turninto cool embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
