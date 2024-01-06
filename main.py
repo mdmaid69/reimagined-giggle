3da5abcdd791857e3c5384e6422e0bcfb0de96f9
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:41:37 +0000

    turndown steer round murder pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
