5ab6f2f355749eebb8c91b1bcad99d270e6216e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:21:43 +0000

    themselves lead mind goon live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
