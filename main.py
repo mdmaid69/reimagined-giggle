1421b4f78d15c38f8898003a6734da19cd7ae8f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:41:39 +0000

    communication knowledge bank national getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
