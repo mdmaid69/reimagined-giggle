e729d413cbeb2bb128c79df0be1608a42432e0c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:23:34 +0000

    method over pride target destroy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
