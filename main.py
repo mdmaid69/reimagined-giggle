24976a75a547298f85c5a861361b07dec7d34b66
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:28:30 +0000

    involve runout bank equal conduct

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
