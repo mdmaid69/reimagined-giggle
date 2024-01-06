83231d0fa0409aaaa5d3ea2fff0a637aaa27150e
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:50:10 +0000

    busy eye over if pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
