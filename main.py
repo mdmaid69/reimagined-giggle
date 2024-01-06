a1ee2124761c2f4635fcc6586f0a3fcd6f623fe9
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:50 +0000

    disturb see example still tooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
