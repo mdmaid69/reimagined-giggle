bbd64a9a56fa5746d0c028d0b16bd7684dbf9f14
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:01:57 +0000

    experience confuse fat try black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
