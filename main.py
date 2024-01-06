9ac90f1833ebf8684b726c0d41baeafc7555ee84
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:17:19 +0000

    picture programme write will turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+print("Square numbers:", [x**2 for x in range(n)])
