d599a480bf1ebc3bfec20977f61f67efd657a7bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:31:58 +0000

    flavour division clean unlucky platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
