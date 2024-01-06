35fd1c64eff43d3dd85290919bb3afed663f3bca
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:08:09 +0000

    bringforward limit pour correct entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
