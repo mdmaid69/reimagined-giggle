92b310092baa2788012e9eebbc2b11be125744c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:23:47 +0000

    motion obtain tip she sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
