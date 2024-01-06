b6e9d4a52ce5a0e7475c2713b67cd65f458268ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:52:04 +0000

    listenfor break total busy fasten

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
