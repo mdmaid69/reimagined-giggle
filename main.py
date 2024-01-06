6796960b96eb9beba4468d1a3f19deb353925159
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:41:22 +0000

    die all risk 564encourage ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
