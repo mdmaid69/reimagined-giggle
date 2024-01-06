239f8ca56628a381d34aee9e65548fcfb0796858
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:39:16 +0000

    false any minority together earn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_floor(x):
+        return math.floor(x)
