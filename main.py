41e13bf614279eeec8877244d7cf265530c450ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:56:21 +0000

    relationship chase brother bottom television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
