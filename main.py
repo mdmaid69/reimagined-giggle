7ee65312fd4258918ea90f8c891b6ae4efad21b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:30:27 +0000

    runout best other lock promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
