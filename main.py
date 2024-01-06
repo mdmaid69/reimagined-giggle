7af38591f325032dc21c5d3f615937f0f20fc3ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:33:31 +0000

    carry offence quickly mysterious wherever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
