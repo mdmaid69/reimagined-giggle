fc7a9ce00b1c1c2fa0e19fa66b1b152b6e99b71c
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:17:09 +0000

    wise leg fire comfort ourselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
