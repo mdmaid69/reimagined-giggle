e12ac06728e3e507b15411515d6b0fe04fc557e0
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:36 +0000

    wealth sad past trousers lose

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import time
+def get_current_time():
+        return time.time()
