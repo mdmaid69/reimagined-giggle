e9ac497326515674ad91e6e6c0c9e152b75036d5
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:10:34 +0000

    hold discovery peace gentle noone

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_rectangle(l, w):
+        return l * w
