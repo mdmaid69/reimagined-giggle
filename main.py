1d738250c8661ffb859050544abd8756e922aee1
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:33:59 +0000

    comefrom holiday wipe fall tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
