94c9fa63be3ab980b52c7b3c664ad277db8a1915
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:26:11 +0000

    interruption light team basic concentration

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
