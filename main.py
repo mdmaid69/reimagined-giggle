1fc0799e859b77df17a2d4e6c15262d428cc87ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:14:47 +0000

    split surprising read mine classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sys
-def exit_program():
-        sys.exit()
+def cube_number(x):
+        return x**3
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
