71c7dd8ed886a3864dab430397519b7f1ca1179d
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:29:50 +0000

    for machine profit comfortable final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
