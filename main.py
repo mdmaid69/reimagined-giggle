ffde0b17431a5a80f796114f31958b8d777824ef
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:31:09 +0000

    congratulations used character turnoff strike

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import collections
+def create_user_string():
+        return collections.UserString()
