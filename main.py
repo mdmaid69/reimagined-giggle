1d007965ce4856ebd3ca6b7d3abf515106db94ce
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:03:12 +0000

    shop raise mark brush lay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def clear_array(array):
+        array *= 0
