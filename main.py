5a80e9a55589a6999a996e6d1418093c76136762
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:13:09 +0000

    cover doubt difficult forget whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
