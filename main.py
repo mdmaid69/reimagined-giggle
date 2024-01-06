c1dcc49fbf6d84613fcd044b147012d4fdcec69c
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:19:45 +0000

    wet question talk shake runinto

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
