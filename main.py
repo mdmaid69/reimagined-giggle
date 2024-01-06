bf2a682dadf1373808a5e229f5af6e91291b828e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:54:25 +0000

    plenty breath chief contact freeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
