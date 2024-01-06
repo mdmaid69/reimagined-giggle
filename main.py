8926ec9a0910b5998f51181eb8979ebdf93928ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:27:14 +0000

    discussion description should substance store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
