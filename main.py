3da899f9ba664a58c74afe7b48c368ceae9c32b5
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:44:35 +0000

    otherwise section breakfast gas consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
