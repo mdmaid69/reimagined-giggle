f35467c7ece808d581763a7efdb251f4bb96ddce
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:49:24 +0000

    all law most object plan

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
