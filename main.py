468b326d44bb26b07e6175a3933a5a3494527317
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:15:41 +0000

    sing sentence morning mark our

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
