60da6815c192efd61dddbdaa8524e74b21536500
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:54 +0000

    bet broken probably fit roof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
