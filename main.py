11504f5f8d34d01f914d67b7c4aff62a1181aefc
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:17:58 +0000

    team crowded organize of shall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
