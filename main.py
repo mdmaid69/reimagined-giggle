10375848048b8e71580fe0d3dc7884b67b91fd86
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:04:09 +0000

    forward accident warm trust compete

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
