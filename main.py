5e58c04f5ed801cc9e46dd90bdf2b3da5985b002
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:19:22 +0000

    floor remove pride back Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
