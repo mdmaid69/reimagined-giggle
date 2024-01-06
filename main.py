212a25a65bca39e541ee777d6a20101467bac76e
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:29:47 +0000

    swim insist exaggerate wild year

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
