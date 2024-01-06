e8f6d8b2f6ff48523b52cc7e8018b80273383667
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:08 +0000

    expensive awake shut label listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
