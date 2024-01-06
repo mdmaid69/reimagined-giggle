059f5a10614db3ddd84d85b55eeb235b2b99b5dd
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:01:50 +0000

    mysterious matter treatment voice range

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
