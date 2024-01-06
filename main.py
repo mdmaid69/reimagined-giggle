74a3b23027a06f8386dde7dd4c035c7b646d6d81
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:29:55 +0000

    ashamed shelter false true trade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+for i in range(5):
+        print(i)
