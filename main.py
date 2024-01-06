e09ee2519d53c0320e11e9790b22a6fb3b8bb328
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:38:27 +0000

    public wide class cheat leg

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
