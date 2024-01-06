c9eb79cebc492d4fa27bf9235b464f38fe547a50
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:39 +0000

    considering number range circle out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
