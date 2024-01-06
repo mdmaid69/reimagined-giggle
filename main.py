c2ff0f4dad788b4208bac5b3fd4d1104bc60e874
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:41:35 +0000

    top busy still turnoff combine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def clear_array(array):
+        array *= 0
