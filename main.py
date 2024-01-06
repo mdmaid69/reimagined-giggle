5e162f0ebadaa4f9cbbdf8a784a3ccb0ff1c20bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:48:37 +0000

    difference stranger interrupt culture themselves

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
