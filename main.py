d01f2cd4e96c1fd49d21c3bd93872025327b4436
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:51:53 +0000

    breakout national interrupt only want

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
