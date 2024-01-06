6c1c840a82059d2d67709d3a8ea18f764fef6d23
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:23:09 +0000

    software will run in Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
