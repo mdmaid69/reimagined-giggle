e766136ce2d34ccec0031df9edc5fece8078a7ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:49:43 +0000

    no taste walk not earth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+  def square_number(x):
+        return x**2
