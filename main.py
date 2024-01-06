e500c9f78b5773ea1edb4da1ed7e31c96df7f247
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:04:42 +0000

    plant boring burst conversation would

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
