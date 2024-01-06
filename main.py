83183aa626ddd24a429ee8903168811cae128a9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:55:50 +0000

    give cold chance band or

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+print(sum(range(10)))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
