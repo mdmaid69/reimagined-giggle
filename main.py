16eff1e666032e08f8e7c41462a619005a24e605
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:53 +0000

    their rather sink true reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
