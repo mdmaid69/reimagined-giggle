b1ae4250d7cde8fa5149ba9249036be075d80336
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:06:39 +0000

    unable emphasis disappointed control smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
