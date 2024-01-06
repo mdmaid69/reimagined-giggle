d49717e7d121971cb10638a6381c4c9d10abda59
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:26 +0000

    lost failure large discover price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
