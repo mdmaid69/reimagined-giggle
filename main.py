064d1efb007e1ffbd6d6828802bd1096d8d6d89e
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:43:02 +0000

    pleasant improve Oo progress declaration

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
