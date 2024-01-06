9594430d48b499abab8c00cb9a502b3eb9c9f6b2
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:01:28 +0000

    switch wrap contrast ill bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_user_dict():
+        return collections.UserDict()
