5c0f5dc785475d6d2cbbd94b62a2994099d9cf8d
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:45:30 +0000

    smart be war bus payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
