e5282eac4e60917a416de20448c0151f81ac99c9
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:36:00 +0000

    partner nowhere visit whom stick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print([x**2 for x in range(10)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
