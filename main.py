ee97e1d851eb42a9c53c4ae1f8c4cef5cd84db75
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:25:04 +0000

    giveaway think snow majority book

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
