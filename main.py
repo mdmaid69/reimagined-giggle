6b8aa374fa445808dd6820d6c9ad25c1571acc23
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:50:17 +0000

    little mean female criticize use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
