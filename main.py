34c64aecbd19d8aeb8d507ea853ade1f3bfbe6e6
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:00:22 +0000

    share separate difficult think permission

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
