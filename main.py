47ba78a265ce456738a4689bdb9eeb86c99f0f28
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:57:54 +0000

    perhaps getoff cheat little command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
