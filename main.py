bba01c25c95463a5d2733ad548e6ae3c85792c24
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:11:02 +0000

    contents admit already behave comment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
