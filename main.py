425b656a8e6c5a73c6c0958c3711e0e215d16013
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:35:23 +0000

    hang noisy Jj any gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def sort_list(lst):
+        return sorted(lst)
