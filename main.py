aa7af417234532c927ad927964e3080696d741a5
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:43 +0000

    bet holdon whether relief load

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_palindrome(s):
+        return s == s[::-1]
