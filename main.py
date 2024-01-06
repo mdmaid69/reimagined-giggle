ac7b092c1c22c3b821a0bcd158d8cf96a41cd1b6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:48:18 +0000

    flat develop geton upwards expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
