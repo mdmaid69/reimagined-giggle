fae70887f9927e2f917a7af222c45d9e29211f0e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:53 +0000

    justice keep shock injury lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
