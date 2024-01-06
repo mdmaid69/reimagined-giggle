40fef1f58dba748a7505d9bd431e5cfe09b31111
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:24:52 +0000

    brave describe bang female bury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
