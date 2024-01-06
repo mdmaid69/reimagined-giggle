592c53e6b78545829abfeb6ebff956ed11d1d2cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:26:05 +0000

    block stupid however knock close

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
