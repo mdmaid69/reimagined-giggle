d86f2cc6a61193e91e5a83bf4ed0a05cbebb8805
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:53:07 +0000

    impression excuse recent married hang

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_speed(distance, time):
-        return distance / time
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
