e625eb3cfcae4804f253c8d2749585c24ed9f8f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:38:36 +0000

    part improve pleased spare comfort

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
