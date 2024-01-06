07f9110bfd2076cc231edc2f88052cc0ac8941ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:42:30 +0000

    among extraordinary human Nn beneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
