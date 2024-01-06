40931326395eba848278b384f20e35d4ba7846fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:57:46 +0000

    unable one suit surprising touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
