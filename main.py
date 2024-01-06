53c8532ce71511609d6ad4f7206908413a0c543b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:46:47 +0000

    decision want success on talkinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
