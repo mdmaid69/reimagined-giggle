f45ee8fdbf63c69b363da92cbe827c33962ff047
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:10:45 +0000

    key such different clue new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
