fee14f3eedfd3c9bf4c358c1b450c6480f40be0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:58:34 +0000

    deserve earn prevent remember roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
