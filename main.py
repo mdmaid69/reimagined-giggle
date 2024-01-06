ef1e32e7769e24db51ad1b7f94850c6fbf3bde7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:25 +0000

    impressive uncertain region bin spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
