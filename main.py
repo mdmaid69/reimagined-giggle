c08a6f1e2b92b95b58da8dcbec42def432607e2d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:18:31 +0000

    admit stupid run consider secret

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
