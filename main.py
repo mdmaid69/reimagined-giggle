c6ecf7f0cf394f88684dfb8f5512bf6c32061360
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:19:22 +0000

    equal value introduce soon whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def reverse_array(array):
+        array.reverse()
