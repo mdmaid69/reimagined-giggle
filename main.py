53f70d95c6f4161597ae6c890a9dc3a6805d50da
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:16:49 +0000

    summer perfect age properly side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
