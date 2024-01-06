8b9b4e6187a9038cb1545a86869bb9618686905b
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:44:50 +0000

    scare mean term charge use

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
