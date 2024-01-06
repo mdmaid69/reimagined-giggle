24784e2db23bd44a7ef2f5a74b3db17ccf75e2dc
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:56 +0000

    of sing hollow brief separate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def subtract_numbers(x, y):
+        return x - y
