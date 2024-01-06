99f041ee4039e7fe0a9034627a33bbbbb7e0b96a
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:58:13 +0000

    stop reply interview amount comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
