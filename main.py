f75e5332c22037154b409640c6d163f05a7fe8e3
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:53:41 +0000

    pretty 569enjoy aim please back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
