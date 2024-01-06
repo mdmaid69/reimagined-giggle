c8e903f5aec410797175c963b5748dc6e4aa4f57
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:02:47 +0000

    flavour past us mood deal

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
