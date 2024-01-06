79ffeb2a31dceb7981db2d1353e7aa387424618d
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:05:39 +0000

    ours knowledge repeat defeat narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
