044c80d3feec70085d36bac13a18b1e4b1a03ddf
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:14:40 +0000

    war firm motion Pp sad

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-print([x**2 for x in range(10)])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
