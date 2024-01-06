adee14c90f6ae8fb9bd5e78dcbfb40c813c0db7b
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:42:14 +0000

    reaction blame news Zz send

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
