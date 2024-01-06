5fc05dd24e843101eb340182782bf02a990758b4
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:51:54 +0000

    company danger Pp party about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def get_bytes_from_array(array):
+        return array.tobytes()
