3e4549d19d6b86e64516f7de97a89831a0fcacd4
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:31:46 +0000

    experiment bury decision part exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
