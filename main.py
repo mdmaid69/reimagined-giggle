72709b02f24d5f5cf4f02478933a1c0861e67e4d
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:46:59 +0000

    cry track stand sound consider

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
