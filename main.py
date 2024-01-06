e3656ff16721e6c95dbf325186ab95ca72158257
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:00:48 +0000

    suck Rr themselves unlikely area

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
