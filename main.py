18696e96bbae1a50d4a2dae9d6c69bad2e65cbc9
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:42:32 +0000

    bet during lie break terrible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
