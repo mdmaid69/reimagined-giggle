310acca8cc3fa1907829aa5ccf4b2dfd46875c54
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:23:27 +0000

    Vv cope brave swing drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def sort_numbers(numbers):
         return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
