ede6fd98f460037e5600e9bd7f02c17f538e8f7c
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:38 +0000

    beautiful match heavy desperate some

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+print(time.time())
+def is_odd(n):
+        return n % 2 != 0
