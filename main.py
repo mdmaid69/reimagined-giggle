1a2ea32220f0b0ad54adc9b3bcefa637640a5889
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:49:20 +0000

    plenty whichever why company basic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
