f8499e2b6a81d5870744b9bd82856a694e244422
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:11:49 +0000

    strict cum dinner translate wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def sort_numbers(numbers):
-        return sorted(numbers)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
