dbca27142cc8da525cad4967a8c8daa893702f71
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:48:51 +0000

    mine drive possibility phone guilty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
