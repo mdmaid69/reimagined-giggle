e14852d5e3e5a0b326c848c35e68f53e365131bb
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:06:18 +0000

    extremely profit which feeling circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
