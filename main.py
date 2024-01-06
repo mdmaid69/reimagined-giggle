a112bbd0119ae6ec13bca46e07985e38006faa48
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:56:35 +0000

    reject roll count sensible return

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_work(force, distance):
-        return force * distance
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_int(array):
+        return int(array[0])
