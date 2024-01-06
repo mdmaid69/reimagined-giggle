d19698510433a5de1abd43d4df7c44546f85d3c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:15:50 +0000

    characteristic use sometimes lock prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
