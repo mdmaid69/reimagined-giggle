b2d5427bbbd753993ada6211836cc9fd2f156173
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:37:49 +0000

    unfair test performance store funny

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
