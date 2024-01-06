15f9f746367f14b884bfa201a6e6bae53fac1655
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:48:20 +0000

    always offer answer below downstairs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
