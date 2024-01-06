55533657ad07702267484b62147f3de84748ef9d
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:31:30 +0000

    recognize pair talk correction inquire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
