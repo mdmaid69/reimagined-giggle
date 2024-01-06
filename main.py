8b1ef815b8dfb1c7ea1fbe1ba9ec50fa0236ff93
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:50:17 +0000

    warning refuse stand well Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
