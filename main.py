a2a38974f5948ab3ceff59dfe9dfcfc05aac3900
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:52:07 +0000

    shade turnup have kick sensible

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
