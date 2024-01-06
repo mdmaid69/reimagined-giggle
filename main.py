b089dd953b6fe089c17a71b435271c49f97e21f7
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:27:23 +0000

    direction grow increase shape march

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
