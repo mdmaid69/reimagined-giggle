f15617f8ad6821f92e1356c734cb18d64cd8fda0
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:49:10 +0000

    lunch our unfair least pleasant

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def convert_to_binary(n):
-        return bin(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
