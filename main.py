cc5cd1dd267f76fb51d07581f4b96d5ed5fe332b
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:20 +0000

    there strike justice 179bite account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
