301167cfc4e5ed77ad1260b2474c42abe95119a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:19:41 +0000

    land curious arrangement Oo key

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
