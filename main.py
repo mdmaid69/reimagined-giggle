d3ba9b15616be354c0fb35b09932646f57b45060
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:04:55 +0000

    idea statement run to slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
