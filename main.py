214bd8b46950293f1bbb2a6f9e4bb448c8b7fc6e
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:14:19 +0000

    separate demonstrate fire land start

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-def is_even(n):
-        return n % 2 == 0
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
