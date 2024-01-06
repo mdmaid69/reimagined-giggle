eb08d85b1464fecbb55eb1f3ffd90beb244e2b4e
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:59:38 +0000

    walk slightly completely calm now

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
