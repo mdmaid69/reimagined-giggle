233fd69d8f80e60fb696667aa06585dea9b07056
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:36:55 +0000

    centre blind theirs dance climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
