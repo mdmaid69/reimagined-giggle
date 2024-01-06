dc70897e8197a884241bde6d5b492ef33cba9dfd
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:26 +0000

    indeed closed balance evidence involve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
