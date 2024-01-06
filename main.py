1cd2362b57e15c19262e493279b48c4a4bfd4f88
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:58:41 +0000

    wipe rain welcome exercise unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
