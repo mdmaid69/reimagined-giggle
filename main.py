9b50a93bf663cf0b0a4f8956b5a1f93bd3db6e07
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:24:40 +0000

    environment wave long understand believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
+def convert_to_binary(n):
+        return bin(n)
