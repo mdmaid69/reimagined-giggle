7200a31a7fe70bf42eccbbbd5bc04d2ecb1e04a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:11:03 +0000

    practise mark wind soon anxious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
