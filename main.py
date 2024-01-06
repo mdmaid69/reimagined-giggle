7e6ac85ae3b239aa9880bffa3e437df1b34e7c49
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:19 +0000

    march beginning experience manage silent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print([x**2 for x in range(10)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
