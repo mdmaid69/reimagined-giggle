411d921ea8286b35e7e65ad3f6abbffef1c71b42
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:16:21 +0000

    raise flood down pocket any

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import collections
+def create_user_string():
+        return collections.UserString()
