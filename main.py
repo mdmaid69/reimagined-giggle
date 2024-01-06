bc81a7067e99117fcaaab7909f1aea22b25c6245
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:01:34 +0000

    talk mother remain joke effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
