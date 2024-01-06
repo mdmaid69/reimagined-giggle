620fc2e98da5d40fde9ad747c579254528aa975d
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:18:01 +0000

    practise skirt how clue big

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
