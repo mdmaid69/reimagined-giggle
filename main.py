6c0f73a78a1e23cb280410da3525a4dbceb1ff90
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:49 +0000

    OKorokay dress belong practice problem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
