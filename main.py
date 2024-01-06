8e2406adc2ac7456883a480cb972721ad585d146
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:19:28 +0000

    in emotion formal face interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
