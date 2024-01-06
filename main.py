edcc4892f2606bb509d45b95017d03ec6145cf09
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:35:43 +0000

    accidental attract drop present degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
