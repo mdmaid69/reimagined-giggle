75a888b91b147c3b5d84ae48b015ec7061e23120
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:49:43 +0000

    total attention object average minor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def reverse_array(array):
-        array.reverse()
+def extend_array(array, iterable):
+        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
