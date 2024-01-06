0875cdc85d276e55d994d1d3b9c66c4e502e2f96
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:07:23 +0000

    account bound Ff seem original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_as_float(array):
+        return float(array[0])
