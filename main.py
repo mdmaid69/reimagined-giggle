64f0ee66384e90b7f02d4a99f148c59dd42aadbd
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:38:18 +0000

    passenger ride though heavy however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
