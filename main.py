0bfff6da6a4e2e40e024696518edd63febe0ac1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:10:31 +0000

    careful careful bath destroy heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def count_words(sentence):
+        return len(sentence.split())
