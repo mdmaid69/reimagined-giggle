7b75fc9fcf37e1988ead08d1eb906cc8158de75c
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:47:55 +0000

    shout small communicate pity precise

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import tensorflow as tf
+print(tf.__version__)
