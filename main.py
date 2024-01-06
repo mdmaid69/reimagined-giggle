c110ac7ba69dfb042803141b2ed955f8be3ac793
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:39:15 +0000

    succeed bang sight level culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
