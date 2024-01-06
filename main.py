9f33c51d7db05975551df53a121f71359ecd2640
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:16:06 +0000

    anywhere scale limit generous commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
