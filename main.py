2caa960de99ff2447e1c77d117e6fdb8ea715422
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:26:18 +0000

    search genuine rise violence 570enjoyable

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-import sys
-print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
