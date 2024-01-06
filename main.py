d568d039e7cb821c5266beaccba50e9165a31168
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:28:12 +0000

    allow receive Jj difficulty suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
