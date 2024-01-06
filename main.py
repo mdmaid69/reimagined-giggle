81cd35a05478b8e5de4edb7d6b1de25c541a503b
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:51:45 +0000

    pollution round bank pause still

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
