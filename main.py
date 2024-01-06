34ef134a26c8711b623d930a74c23eddc9d56e0b
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:57:33 +0000

    view chase complicated risk challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
