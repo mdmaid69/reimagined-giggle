a938d76f1bd76ef99c519d71e5cae8bc86016261
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:55:45 +0000

    judge suit such survive steam

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
