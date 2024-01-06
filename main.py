5a04878ee8a76282a4755692f7e0ecd6304071ed
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:59:22 +0000

    contain second fault size yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_rectangle(l, w):
-        return l * w
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
