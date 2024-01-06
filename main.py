797a2b17d3898bd7a410e01a10deb4603bd2e79a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:57:46 +0000

    learn section mother shape gentle

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
