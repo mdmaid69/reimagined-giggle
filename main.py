d099df0971d7de4a9cc7a094eaa2800ab1f36882
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:19:25 +0000

    fair move pool hang represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
