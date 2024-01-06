6b96c7439270ed36c938b339a043f5561c2bf300
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:45:06 +0000

    maximum empty proper talk mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
