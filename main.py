55fd2cab30327a4505daef586771ad63959758cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:38:13 +0000

    getaway man describe performance healthy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
