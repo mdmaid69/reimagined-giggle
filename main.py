5fe3cce9956f6bfdb097a8acff53481fe8b24f49
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:57:29 +0000

    cry behind fat arrive cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
