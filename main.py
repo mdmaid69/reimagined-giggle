0e2dea4b321c6078e78f805cfb27cc5c26329f72
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:51:40 +0000

    goup other sound replace disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
