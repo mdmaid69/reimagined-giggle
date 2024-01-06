6b2c8590800037c04092a59e4646e5fcb9b8680d
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:27:24 +0000

    scale confidence anyone callout tape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import json
+print(json.dumps({"name": "John", "age": 30}))
