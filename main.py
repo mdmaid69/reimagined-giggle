e8b5f1e2c6397b6adfd05f7bbdd047c05ff4a4fb
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:41:01 +0000

    hungry meal help difficult community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def subtract_numbers(x, y):
+        return x - y
