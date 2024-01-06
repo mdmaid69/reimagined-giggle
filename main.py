88530c980df1d86cbb44a88e43fc57bcaca33a61
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:37:24 +0000

    rely much method science clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
