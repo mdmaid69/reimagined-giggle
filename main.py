2f24b584713847fb3482213363e654159cd0c7c6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:36:14 +0000

    pour body send furniture similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_cosine(x):
+        return math.cos(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
