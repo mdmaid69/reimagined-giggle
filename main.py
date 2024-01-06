ad9ddd28d7e75c69a76c4dabdfa3d1132c4919e2
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:33:52 +0000

    proof best coffee hold willing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
