a0bc668e130c826de96b690a939dc0e87abd681b
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:45:57 +0000

    offend nervous ashamed light capital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
