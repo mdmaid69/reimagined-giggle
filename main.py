8cf2217a58d74538e8bbad87b0421677cf65b985
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:31:51 +0000

    contribute view last crash genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
