10e17c159f303966915a00637ca035a48ed3e88f
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:57:42 +0000

    divide imagine neither connected hate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
