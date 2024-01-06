907da089d35838ea7a4f2df20bfa7cf444da64ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:32:43 +0000

    share popular listenfor disagree cry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-print([x**2 for x in range(10)])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
+for i in range(10): print(i)
