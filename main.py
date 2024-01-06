6e4a5cebbe53c3b4ad7605370a40e9dc85b30ac6
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:21:56 +0000

    Ee right search priority well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def cube_number(x):
+        return x**3
