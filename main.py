d828a7277d5f628484e86a92ef5f1d636bf8ac81
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:14:33 +0000

    swim 565end keepoff past edge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def is_even(n):
+        return n % 2 == 0
