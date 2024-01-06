eb070483e6955800aa76893b4e6285f35a15d085
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:11:04 +0000

    plain load food like getout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def convert_to_octal(n):
+        return oct(n)
