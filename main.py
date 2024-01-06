112e7da76cdf25a96f394eaeffacecc6e1f5037a
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:55 +0000

    bar quit shopping Cc much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
