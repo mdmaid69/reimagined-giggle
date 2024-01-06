5a1a4688f7e4340a4a747c0a836dbdb89b3c8bce
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:21:00 +0000

    repeat trap disturb attitude succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
