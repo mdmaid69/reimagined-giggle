d8a04d1fb1a08a4eeae35dae0849947c925b0582
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:44:17 +0000

    his avoid base while breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
