8ea85fff8d1bb9981bf82eea066963232fbaad20
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:52:51 +0000

    wet education path memory pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
