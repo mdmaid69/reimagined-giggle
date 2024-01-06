74260fad858472c1b89384907c7ef541453cb68b
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:26:46 +0000

    bringforward to could male bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
