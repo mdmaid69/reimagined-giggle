ec18abe38fa19eeeede1c01d337398c82f3a1259
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:17:30 +0000

    scared believe second emotion regard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
