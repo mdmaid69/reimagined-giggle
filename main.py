96e3ae89751c6a7cbfff0f09099e378fa2c7d900
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:52:10 +0000

    standard ready statement rate everyone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+print(os.getcwd())
