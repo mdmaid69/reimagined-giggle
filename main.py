35728048a99d00953bfa84c1ddc04268028c4190
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:04:58 +0000

    win exactly complicated keepout beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
