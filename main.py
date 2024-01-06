f8a4b0eef6a5961650fef2b893212724b0fdf42b
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:09:39 +0000

    replace prevent sensible land project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
