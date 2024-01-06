708070e5cc0512364f308439ffcfc7ab15635282
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:10:46 +0000

    shock godown possession recover cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
