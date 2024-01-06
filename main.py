78213573c1b9ffed50b3c74019563345a7895431
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:38:00 +0000

    squeeze importance welcome mess minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
