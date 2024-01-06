0ab588ee55821038cb731147396970bd3a74e3d6
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:12:01 +0000

    wild match accept view pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def clear_array(array):
+        array *= 0
