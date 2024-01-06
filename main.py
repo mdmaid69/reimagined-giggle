16825a475c460a130c57e6e7deba947c2778c409
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:17:55 +0000

    quiet suckme breathe language welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def add_numbers(x, y):
+        return x + y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
