7a8e064f7adaf596264ad3de4a77603487e222ac
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:01:28 +0000

    nice Uu fall lock weigh

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
