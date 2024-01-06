f170f7be21cf98b909ebbeb183109bc807a07221
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:34:51 +0000

    empty complaint shrink damage keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
