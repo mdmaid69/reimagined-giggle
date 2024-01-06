cfe42806c9a7002cbe6bf2aeb982580f12e7a756
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:36:13 +0000

    lunch otherwise spring divide same

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
