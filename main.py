215ac848037805daa1e7666b503a594c9b39951f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:02:31 +0000

    part stretch car run disappointed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
