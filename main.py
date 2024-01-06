ea30d8915e64331229b368205d5049e853c2a54f
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:12:36 +0000

    correct line floor any show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def add_numbers(a, b):
+        return a + b
