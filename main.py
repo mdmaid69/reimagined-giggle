b164531020b3eb39ca1d9a36f56534962129fb79
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:39:03 +0000

    gas beat second also when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
