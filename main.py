68f82b6c4f5c5ee9669687889d6aa26acfaf8b83
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:52 +0000

    set corner honest combine roof

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
