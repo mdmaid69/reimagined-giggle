18ac62e9f367988b7bf63939096992374394573d
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:51 +0000

    brother just nearly family comein

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_max(numbers):
+        return max(numbers)
