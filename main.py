09a59173ddb16f52b96e707f94762341082d345e
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:54:32 +0000

    place question getback defend chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import tensorflow as tf
+print(tf.__version__)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
