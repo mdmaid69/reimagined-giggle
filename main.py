9c25adc72163d6a227414321bac0cfdf634d37f1
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:09:26 +0000

    control one Kk complain neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
