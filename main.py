c48da173b8269ae56634ee215084fa7c3c2f77f4
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:16:51 +0000

    upwards behind each gas slowly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
