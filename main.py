7e5334e9b7c383f7f6bed620a286dbed2d3afde7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:22:41 +0000

    keep respect equipment comein television

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
