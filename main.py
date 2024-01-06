0b5cf3c1b1d8d118fb552049f91d46b79cb9157a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:46:03 +0000

    club comment closely height hot

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
