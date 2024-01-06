e3e887b8b19c14075b7f707da8573e5962e4cd3e
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:33:23 +0000

    survive comeround example standard clear

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import sys
-print(sys.version)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
