7d21cdff79d4536978868dba278d53b09a41258a
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:31:11 +0000

    lean cut flow just direct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
