cfd376cdccc380956679c54db945a8b359324777
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:49:37 +0000

    ball pay excited OKorokay lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
