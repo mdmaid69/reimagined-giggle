af1ba5548b5baab38e87baaa03162a02b38155ab
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:19:26 +0000

    unkind pay roof front store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_speed(distance, time):
+        return distance / time
