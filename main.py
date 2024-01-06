f077cfb3930f6e36eace563f4a6ff80e4cda07ee
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:37:34 +0000

    mad brain excellent pleasant about

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
