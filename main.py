fbf990884664a05274f32b3744616ef9ff39ace5
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:57:02 +0000

    care unusual through taste provide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
