3a33467a7ac13dd908dbb1035eae86a5a9920e74
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:31:52 +0000

    opportunity suckme although fix mind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
