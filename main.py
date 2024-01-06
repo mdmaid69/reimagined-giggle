bdaa4235acac241a2418f9962a9ee2113c9763d4
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:52:45 +0000

    recover right attitude wing maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
