24f04b7955768ce2b986ce57d93ed568aa750264
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:08:11 +0000

    present mine married seriously many

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
