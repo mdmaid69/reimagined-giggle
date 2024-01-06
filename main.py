c92b8dce735d8a072f055fd364d2e0dca4ae5e2c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:48:15 +0000

    comfortable breakup old people impressive

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def square_number(x):
-        return x**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
