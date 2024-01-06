7eb8d4bf19354fc8dcf0cbad916307747db9b7ba
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:51:47 +0000

    confident prevent half dig piece

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
