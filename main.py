c2d1b0f3b29415d755d2ec6ba307ad27f944beab
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:16:51 +0000

    ought hard thirsty tomorrow still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
