0c6bc78ca524033a0fa3e87da1504ace7663dc61
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:21:24 +0000

    open late development throughout protest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
