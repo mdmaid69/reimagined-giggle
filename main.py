079fe793e49c546a68a8189f86518d58ebd4bbdf
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:26:12 +0000

    warning shrink block fortunately use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
