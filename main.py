a535cb4351058bb9f36a150aef40b61c79275240
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:03:42 +0000

    contents freeze official warm block

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def convert_to_octal(n):
-        return oct(n)
+def find_max(lst):
+        return max(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
