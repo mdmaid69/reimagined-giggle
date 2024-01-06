3315cbe251bf0dcb3947dff68b4cf3bf30a09af5
mdmaid69 <robot@alexsyw.me>
2024-01-06 19:03:46 +0000

    lonely cheap pause greeting size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
