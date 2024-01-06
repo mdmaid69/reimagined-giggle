c1df3e89cb55c71637cd5698fae34adfd627af5b
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:11:18 +0000

    interfere only 569enjoy mainly full

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_item(array, i):
+        return array[i]
