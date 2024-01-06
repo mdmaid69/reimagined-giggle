d9845ecf238f7bb1fcda06eb4aad39ee56c94ab8
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:07:00 +0000

    wherever everywhere fight exist undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
