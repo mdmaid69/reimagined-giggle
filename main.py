a2c9aec8fa018e92e8e68fd44c986b87e2b29b4f
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:23:43 +0000

    unhappy price subject power talkinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
