0f8293c1d2ca0682e427ecd6904bbb2dfa11c414
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:53:23 +0000

    remain feeling let guess lift

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
