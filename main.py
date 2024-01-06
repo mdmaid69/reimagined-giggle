482948bacd7eea859238184ae6f73cf213782287
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:47:29 +0000

    spread already laugh safe right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
