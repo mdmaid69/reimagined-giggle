ccb5303b1c5448f344ecb7d336cf66d9e18ed1bd
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:27:15 +0000

    573entertainment coat so hurry about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
