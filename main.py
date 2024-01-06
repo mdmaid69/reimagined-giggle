86aacec30388d0c4c55d69e1ffcd7315701e997a
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:47:01 +0000

    pretty rule along school tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
