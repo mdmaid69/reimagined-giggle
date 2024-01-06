3f4e2f541957ade1b5e70783e5ed2dd3a2e9b0aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:07:53 +0000

    dick slice production breakin guilty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
