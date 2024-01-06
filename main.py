749764e074d35a99c37906ae4fc34beb73c4f143
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:45:15 +0000

    like impressive today inquire opponent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+for i in range(5):
+        print(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
