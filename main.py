9af465b818690289dbbaddce823040409302a36f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:43:07 +0000

    plant general stir rarely piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
