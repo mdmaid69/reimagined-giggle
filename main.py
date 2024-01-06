891d88f75ae2f51b9c442a192d2575b993691503
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:31:52 +0000

    wire offer equal her edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
