c894641f9cf2f3c46a44c4d4bc931289d0515734
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:22:50 +0000

    Ll greeting close getout common

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
