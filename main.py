31dcbf4895f82d01621fe970bb2d756d6c818255
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:11:40 +0000

    polite roll new admit no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
