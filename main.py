beff564b8a80d245236bb1171067342f4a4ac50d
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:16:40 +0000

    neat guess sink instruction scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
