40064242d0f735420d9329d5741a9dea64b85eb5
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:41:45 +0000

    touch Vv difficult emphasis needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
