979c5992ce91423fa954a547d4dfe7b79908a8a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:08:17 +0000

    over deaf room face breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
