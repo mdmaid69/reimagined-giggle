2037c5465846fdd98179ef12616afe0d264e42fd
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:36:28 +0000

    education over majority him Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
