a7ea918b603b80c5d962f3c79d73643ba6721c26
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:19:40 +0000

    promise rough lean description conduct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
