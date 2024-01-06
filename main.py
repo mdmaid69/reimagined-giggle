d3651e419ec6f5ce1e90b312fe5b5aed00362674
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:48 +0000

    deep body remark taste wake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_item(array, i):
+        return array[i]
