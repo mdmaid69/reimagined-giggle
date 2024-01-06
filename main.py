4d07d0a47cf1a667c7e20b382b3ad8bccd4de7a3
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:54:53 +0000

    lonely often hurry spring right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
