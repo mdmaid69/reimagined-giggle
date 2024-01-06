2829df18a3130dd3c58d2d73d379069058ccf590
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:46:04 +0000

    bringdown belong group drink offence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def reverse_array(array):
+        array.reverse()
