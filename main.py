2b19cc34eee801f830dadeb8f694b756f072dfe2
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:11:13 +0000

    bother afternoon Bb suit goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
