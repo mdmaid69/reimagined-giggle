dc28f00dff5a24327849aa6c8e9a3e882258bea6
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:27:22 +0000

    water exact spite during relative

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
