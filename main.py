41117fbe3894e84681a51e42fcd90efe8a69382b
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:41:31 +0000

    perfect upwards look private report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
