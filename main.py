6806e16008f89d6dac7705776183fa9b68833193
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:51:15 +0000

    disappointed test deep able cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
