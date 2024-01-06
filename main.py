0cce7cabbce24474220a912d889df43a55eb9efc
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:38:15 +0000

    turndown shoulder record share whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_base_name(path):
+        return os.path.basename(path)
