46b0d76b825fd3162d38b5b5e7c240635809215b
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:39:18 +0000

    experience sting business estimate platform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import sys
+def exit_program():
+        sys.exit()
