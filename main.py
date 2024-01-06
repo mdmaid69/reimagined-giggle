8a37178b0314261903d03edf06ee6704c383db5e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:24:24 +0000

    child news guess before Pp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
