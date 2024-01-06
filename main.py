2ce16b0331fa44785431e10467c78d86cb74b267
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:36:46 +0000

    figure dick goback(to) criticize personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
