e2161215e99e9e352300702ea90991ebfa04ba17
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:08:01 +0000

    failure rough total pronounce week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
