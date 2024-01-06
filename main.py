05ca4cb291049a1edba5d6c479cc0f2d1b3dbe4a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:01:08 +0000

    otherwise basically guess important once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
