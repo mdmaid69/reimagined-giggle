6048cfe84668ac0d38c11ae3c5bee4c7fd29c9eb
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:38:42 +0000

    extremely bet your string press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
