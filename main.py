4b554879f3bd745c27ff61a77d44478c93afbd3a
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:40:06 +0000

    brave breakfast sometimes design attach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_power(work, time):
+        return work / time
