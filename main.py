ef5612a6ed51bba0f125ffa51be9f4d5230229d9
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:27 +0000

    sit unit Hh sink letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_time_since_epoch():
+        return time.time()
