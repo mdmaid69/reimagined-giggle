0b4de03575773b0ca39879c7c2f0770b17ca5914
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:38:34 +0000

    thanks warn anywayanyhow maintain change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def square_number(x):
+        return x**2
