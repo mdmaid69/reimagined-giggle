89a478d3b81482196f331ca83836137c83d3b8de
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:52:20 +0000

    lay exercise gentle past fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
