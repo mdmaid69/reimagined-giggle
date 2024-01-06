a9a1f6d48b72dd297d632e7ec271c4fa8f607cf3
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:58:55 +0000

    exam evening worst goodbye future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
