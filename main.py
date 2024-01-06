8dd862edf49dddaa627578a37d7a416ad9932c16
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:24:17 +0000

    software discovery over definite upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
