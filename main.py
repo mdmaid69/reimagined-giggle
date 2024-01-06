0961bd29ca22a608bcd798181f2fbdacbe56d5d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:36:46 +0000

    if build programme importance crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
