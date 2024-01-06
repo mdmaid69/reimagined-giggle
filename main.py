cde41f1b5625f412827b92c5fae7f5de310ce42f
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:18:44 +0000

    release break expensive either rough

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_counter():
-        return collections.Counter()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
