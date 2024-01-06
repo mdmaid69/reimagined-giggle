a1ffc7d084798d5d0fa0c833dbe2330a95ad6ea7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:52:00 +0000

    check wise after clean politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
