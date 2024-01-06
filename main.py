eedd32af46be3342817c81b64c3d8bc8c7b4fc1c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:09:31 +0000

    although sport towards myself sign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
