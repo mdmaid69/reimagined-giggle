fe91d7d00dd7fba7dfbd4ac512a2d32731ba814f
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:05:57 +0000

    direction comfort Tt shock effect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
