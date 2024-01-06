5da491e23a541d7686335d9cb3575557c9afe4e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:07:12 +0000

    basis blue pleasant boat ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
