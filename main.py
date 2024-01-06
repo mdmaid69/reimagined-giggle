9441d064166889f8606f8c0f852af46affa542a7
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:58:18 +0000

    fond until Yy right shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
