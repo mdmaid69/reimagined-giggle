2ca728afd1a1b9ddb3347488649280a3bfc80c12
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:39:08 +0000

    mass unlucky bag adjective point

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def is_odd(n):
+        return n % 2 != 0
+  def convert_to_binary(n):
+        return bin(n)
