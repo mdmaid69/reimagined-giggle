24ec55ebb57d383dbdc3e1558b465368cb2ef149
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:17:30 +0000

    compete wash ready chief keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def count_elements(lst):
-        return len(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_exponential(x):
+        return math.exp(x)
