be958027e8ea7e218d8014dda380cddc7137b98c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:47 +0000

    small Zz truth skill bunch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
