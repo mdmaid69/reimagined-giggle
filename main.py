a8bc78417dd3ab764986c7e7892676df5e00f102
mdmaid69 <robot@alexsyw.me>
2024-01-06 17:40:00 +0000

    back fault bin impossible feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
