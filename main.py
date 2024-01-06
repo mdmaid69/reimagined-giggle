fe3a872265903e391a486a234a6cf88c6e1f9fd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:06:00 +0000

    steer pour stamp table card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_absolute_value(x):
+        return math.fabs(x)
