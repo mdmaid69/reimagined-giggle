435eb15f2c74825820e154781875c8536bd04cc8
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:33:22 +0000

    system taste story phone go

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+print(sum(range(10)))
