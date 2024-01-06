3368b6b5a4f104be0d5f690ce32752148d48ad02
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:30:00 +0000

    interesting violence cry face stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
