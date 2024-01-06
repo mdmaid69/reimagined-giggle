a2021718d1cacfbaaf1b44d77de6440fe832b150
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:20:47 +0000

    makeup strong steep small right

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import array
-def get_array_length(array):
-        return len(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
