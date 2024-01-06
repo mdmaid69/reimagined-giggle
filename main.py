7c3742a313c5611283b9af1e198b3ee363447934
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:09:52 +0000

    getout couple turnout snow old

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
