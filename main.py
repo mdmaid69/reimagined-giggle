c1b8a6530da5834d131143806ff130b500161186
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:50:33 +0000

    workupto tip available old final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
