dce6b617b5bb45ef28eeeb5779df47e5a1b05ba7
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:21:55 +0000

    petrol better relation guess square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
