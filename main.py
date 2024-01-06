bd244bc99f61fe736a6cc75d13fecf116a103ae3
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:17:29 +0000

    mysterious illness Ll runout reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
