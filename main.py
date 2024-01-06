0b5122e988ce9c573b15a7089138dfbd91fd6d25
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:06:05 +0000

    lot cheque still tell direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
