32ef7173b5c7c89bc7b0977c0b7a0f90e45031de
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:10:08 +0000

    news face bang star sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
