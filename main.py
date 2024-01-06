169f259f603878c0d049217e03180cfd77381f89
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:44:12 +0000

    afterwards card start come computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
