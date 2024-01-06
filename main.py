90d4b9b0332a6c828e793a74f80f58d49ac2d071
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:29:55 +0000

    explain foreign guess themselves getaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
