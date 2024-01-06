7a2207fc1fb725a0ec46fcfc065874bd05bf02a9
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:27:14 +0000

    keepup tidy fashion display avoid

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import sys
-def exit_program():
-        sys.exit()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_force(mass, acceleration):
+        return mass * acceleration
