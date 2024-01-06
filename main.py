a32df4f72eddba5fe64f82da712e199e9d9505e7
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:23:14 +0000

    proud uncertain sign performance usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
