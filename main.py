e9bcf2df4be7b793e5c207fdaad94b3e7cde895f
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:28:54 +0000

    waste stick keen under expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
