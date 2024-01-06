4360f53b124d5bc85e688dcd1a197589b3d0420a
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:13:24 +0000

    tip remark press impatient trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
