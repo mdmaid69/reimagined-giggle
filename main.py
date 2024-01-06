ff5e738b90667656d7791557eeaeb28cb0879346
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:01:58 +0000

    legal age brother free copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
