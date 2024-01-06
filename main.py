c61ba695f9056ff20eff60c05419113610da9503
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:29:56 +0000

    thick distance backwards noisy damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
