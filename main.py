d3ecc539f1a22bd8a416df9fe375fe2c34a40439
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:11:59 +0000

    mean afternoon life disapprove immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
