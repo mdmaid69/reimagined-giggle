94d1c1c995e870c9680c688e3b0010b5f63965ad
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:57:47 +0000

    science pile kill temper object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
