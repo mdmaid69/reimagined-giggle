7a65d0ee0958021967b4caa0b3514e343dcab1d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:42:54 +0000

    smell somewhere walk geton nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
