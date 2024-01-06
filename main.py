2f92796dabbacd5a0a0d40c17598f7e873ef1388
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:14:21 +0000

    satisfactory instruction who involve stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
