4affd6338f5b382218661897ce86facd11836262
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:39:11 +0000

    better always protection relax dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
