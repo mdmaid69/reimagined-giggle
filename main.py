501d1d7d5c07cedc11e49e918422d3c63533366c
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:45:14 +0000

    next fire think trip excuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def sort_list(lst):
-        return sorted(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
