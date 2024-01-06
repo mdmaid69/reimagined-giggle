3e09ef9f18b6f2fcda2b894455c29e7c31adddab
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:24:11 +0000

    speed collect public train fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tensorflow as tf
+print(tf.__version__)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
