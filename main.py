05d9aba0a9e0f41d5cdf656598287b2fd26b038d
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:38:47 +0000

    distant side fear reply spill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
