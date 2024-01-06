e3d9702ffda1c0f32f817799d68070cd449b0581
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:41:47 +0000

    ill nothing ourselves past force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
