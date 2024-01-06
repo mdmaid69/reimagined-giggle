d2087732711e1a26897af6d04462b57e26cc533a
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:31:10 +0000

    separate no unpleasant interest chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def is_odd(n):
+        return n % 2 != 0
