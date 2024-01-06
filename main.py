cc98f9e5fbff77ff20178b63493c9146f8f838d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:13:30 +0000

    car knock race that funny

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
