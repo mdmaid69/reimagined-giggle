c9a8474491a193d449579966ad52a4315d0f99cf
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:33:32 +0000

    letter include protect beginning nervous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
