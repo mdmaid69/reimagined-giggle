7002f5cd5b41318cad067de998b9f329a25690b9
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:47:58 +0000

    market myself innocent belief ask

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
