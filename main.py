78cea0f1e0cdd94ab934d6840f681be87a42a09c
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:56:05 +0000

    how correction model our recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import array
 def extend_array(array, iterable):
         array.extend(iterable)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
