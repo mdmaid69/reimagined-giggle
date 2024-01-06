c800869c9e2092f911e4b230797f61ccc7ab9ada
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:03:28 +0000

    book shout only plastic fault

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
