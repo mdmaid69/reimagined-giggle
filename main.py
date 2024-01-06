44dfb00c7dcaad3fb920bcc36c10913bd457a4a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:49:54 +0000

    as dress already break squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
