b4e6de56d3ae24f9b6cc9c0abc1207bd800b9aca
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:02:26 +0000

    centre minute press glad takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
