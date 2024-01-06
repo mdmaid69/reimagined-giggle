2580ab3c4aed90640823530994bd0fe7b540a434
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:12:07 +0000

    get unpleasant picture software goon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
