ad84f1dffe020e2fa8141b638034675bb99f41e1
mdmaid69 <robot@alexsyw.me>
2024-01-06 21:46:43 +0000

    bar cover future callat approve

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
