ca17af17d9bab858101daecbf26118c4573aaf55
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:33:21 +0000

    weather school present day keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import time
+print(time.time())
