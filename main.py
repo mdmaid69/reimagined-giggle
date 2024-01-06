8dfdb2a11e5d6c631418aa19cb0e26aaccdcd74d
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:46:26 +0000

    preparation could well behave pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
