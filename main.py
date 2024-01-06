4cff40f16d1a3fa4fec7166dcef806fbaff29bb7
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:23:49 +0000

    upwards custom breakin when unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_work(force, distance):
+        return force * distance
