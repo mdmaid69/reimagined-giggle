bcc749b337a936134918da3278babb6efb45c2ea
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:47:11 +0000

    light flat pain rush traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
