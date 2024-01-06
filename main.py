8a33b84e432c5ac3a5b09a85f92c9c21dae8c9f5
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:05:53 +0000

    violent inquire opinion wrong concern

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
