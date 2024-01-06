ab08bb99dfe95515bbf723a7ce8dca7a95e2a947
mdmaid69 <robot@alexsyw.me>
2024-01-06 01:06:52 +0000

    blind city confidence open wild

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
