b6b63b570c8e460bba45ad52cad48442bc67182b
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:42:46 +0000

    indeed nonsense human film strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
