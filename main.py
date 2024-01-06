daef38d6fd65bcb11c08118fe5a392e3974625ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 03:50:07 +0000

    sight unlikely meaning gap sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
