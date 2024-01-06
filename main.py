0b60b56a3f1cfe2c42eb9c96cb5300dc61bb58a6
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:47:59 +0000

    manage represent smell condition present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
