c26774bf1332b322b6c224403e118728346e51c1
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:18:21 +0000

    picture rest tread remember look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
