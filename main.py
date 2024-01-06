d0e41c3d6f0f8e4562b353b3c96b50e3a27b3c85
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:43:21 +0000

    spare least pain tomorrow anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
