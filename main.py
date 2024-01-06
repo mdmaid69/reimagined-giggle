3c9e4f728cc7d8ded9c193ad1112838c9bf01358
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:34:07 +0000

    fold comeout convince surprising now

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
