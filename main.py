781f4ccdb0398ce05cd6c29f18b3cbe988866e9c
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:16:51 +0000

    accordingto fit near name lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
