dfa7ab24efbbe98a5b8f8a7e72bb231ca1aeb82f
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:16:42 +0000

    progress bright rush brave one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
