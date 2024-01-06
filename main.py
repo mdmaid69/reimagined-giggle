f658d0f69916261aac47d23ac4a394a6ba6c83c3
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:12:53 +0000

    plenty conduct major swim stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
