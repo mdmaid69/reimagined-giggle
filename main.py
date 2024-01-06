3483cf64f097ce7645f33496f1dcd886298d06ff
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:44:00 +0000

    scrap workout date memory outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cosine(x):
+        return math.cos(x)
