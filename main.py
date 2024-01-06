bc6c3f8fa361f04c86ae974fa18d8fbb564d362e
mdmaid69 <robot@alexsyw.me>
2024-01-06 04:13:26 +0000

    cool hardly unhappy comment imagination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
