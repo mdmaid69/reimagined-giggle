46de2e41d207b2e9810c346561b8d03dfa50be04
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:00:07 +0000

    frighten sweep hope challenge himself

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_list(array):
+        return list(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
