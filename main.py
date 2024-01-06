42d22bf23b8e14872ee1b356466eac61aad15856
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:24:55 +0000

    first track community sing shade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
