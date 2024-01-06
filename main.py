8304a61124d886419e95746f18503555ccc0d5cd
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:24:23 +0000

    back if skirt centimetre use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
