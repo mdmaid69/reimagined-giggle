425331bb248a2e76def9eee3cdf1d9dbee8c5ca5
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:31:49 +0000

    worse complete fold mass target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
