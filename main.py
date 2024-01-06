87cf52b295304b6f9f1801dbdaa38182d686d8f6
mdmaid69 <robot@alexsyw.me>
2024-01-06 00:47:10 +0000

    black cheque branch Ww feeling

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_power(work, time):
+        return work / time
