ca5f9890fad6aca60cafa8bfa77b696e7e5fef0c
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:11:12 +0000

    dangerous classroom reaction advantage head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_power(work, time):
-        return work / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
