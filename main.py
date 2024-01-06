6c4f99511ef466807ea25ca8ef7125aad22d5166
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:36:38 +0000

    convince scared appearance crowded remember

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_tuple(array):
+        return tuple(array)
