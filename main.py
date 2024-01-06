19420bfe3d1f48016a87c0ef3e3008231bcc297c
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:46:49 +0000

    wrap sound crash themselves form

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def is_palindrome(s):
-        return s == s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
