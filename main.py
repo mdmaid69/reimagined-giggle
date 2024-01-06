ae7fc4a08d35e70523af98016c40ce43508ae945
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:44:25 +0000

    future contact degree depend activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
