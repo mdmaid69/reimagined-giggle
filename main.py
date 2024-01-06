eb1adac62a6c09561363e29701694a09fd010f24
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:55:33 +0000

    hold pattern thirsty partner treat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
