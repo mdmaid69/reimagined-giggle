56fdd1f8106d278d5633581b1a3b74e215c46a17
mdmaid69 <robot@alexsyw.me>
2024-01-06 10:01:05 +0000

    earn excited getout well away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def remove_duplicates(lst):
-        return list(set(lst))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
