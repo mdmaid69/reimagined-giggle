081bd777df3bc5369ea9153f30eae27eb8c3332a
mdmaid69 <robot@alexsyw.me>
2024-01-06 02:54:31 +0000

    inside smooth beyond opponent 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
