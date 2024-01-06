9799102884b77a29b58225577d9de3ff86d8f869
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:12:02 +0000

    lock university low unless bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
