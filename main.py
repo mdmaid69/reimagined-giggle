bbf0704dfab88dae6807746734dde34f60ca7312
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:07:42 +0000

    climb ball sometimes everybody under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
