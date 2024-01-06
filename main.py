fe0d4008ac1aa68d758effe5251678f46ab07783
mdmaid69 <robot@alexsyw.me>
2024-01-06 14:22:24 +0000

    only contrast pour except broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
