40f6125bcc656631148a3bc17cfd61b81b670566
mdmaid69 <robot@alexsyw.me>
2024-01-06 22:41:11 +0000

    entry comeup lonely visit concerning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def split_path(path):
+        return os.path.split(path)
