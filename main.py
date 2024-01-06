26e672045b4fddfacff345357194ec14e69c714d
mdmaid69 <robot@alexsyw.me>
2024-01-06 05:27:57 +0000

    avoid every scream whatever bridge

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
