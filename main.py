a3d811d2db483c4347698a8e272dcca8d9a10a47
mdmaid69 <robot@alexsyw.me>
2024-01-06 07:23:52 +0000

    interrupt amount runinto miss pin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
