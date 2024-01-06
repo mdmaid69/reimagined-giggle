034a862cfa55df2285e24e8b0983e711866dc1d3
mdmaid69 <robot@alexsyw.me>
2024-01-06 18:09:08 +0000

    know course this flat soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
