9e6abf58a527e9c1e1cc36b3c8b088ae1e850f7e
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:25:28 +0000

    car bunch careless exaggerate touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+from collections import Counter
+print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
