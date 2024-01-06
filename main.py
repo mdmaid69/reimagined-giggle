24794abb003bd411f6b2ed0ea355cdd5cacb580f
mdmaid69 <robot@alexsyw.me>
2024-01-06 12:06:34 +0000

    peaceful through treat means worry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
