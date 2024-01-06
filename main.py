8f31ee22cbca29f675dc7094c9d93ff90e1a36ca
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:50:54 +0000

    objection air entry steal probably

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
