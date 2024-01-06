1bd6e44a560beb0315a93c438ba9d290cf928fb3
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:20:38 +0000

    field command getup hospital land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
