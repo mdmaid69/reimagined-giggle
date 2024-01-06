14121cd4c4220f002b91e5095493e2370a2c3fb6
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:27:46 +0000

    approve supply dinner pronounce bright

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
