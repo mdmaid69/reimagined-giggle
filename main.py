97c12e4c7b54be735b5944a565b643c779f964aa
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:16:13 +0000

    flight sticky police temperature calm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
