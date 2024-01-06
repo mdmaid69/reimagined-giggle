5da0f7534ecce9f1cab4c8a21dadbba599a33231
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:31:56 +0000

    sell spread accommodation second computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
