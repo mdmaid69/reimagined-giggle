ba4bc1deb0f3c906123695b02b87368635def5a2
mdmaid69 <robot@alexsyw.me>
2024-01-06 15:59:21 +0000

    mess advise treat bring runinto

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
