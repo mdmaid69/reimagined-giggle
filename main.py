58193d171c85a697bb671ad9cfdf88afc1384831
mdmaid69 <robot@alexsyw.me>
2024-01-06 13:39:41 +0000

    564encourage 567enemy style comeover carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
