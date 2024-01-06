32c5bddf5d8ec9c2c438da2214ffa6314109cd9a
mdmaid69 <robot@alexsyw.me>
2024-01-06 06:00:43 +0000

    heavy backwards not welcome basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
