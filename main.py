97dd279137fc89f192b7593572a552003e128071
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:11:26 +0000

    keepoff loss turnback wish guess

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_speed(distance, time):
+        return distance / time
