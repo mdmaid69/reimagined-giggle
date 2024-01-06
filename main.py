84237fec24daa1335a15e4699f5c51a5e2bf8cd9
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:50:03 +0000

    solve confusing city honest general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
