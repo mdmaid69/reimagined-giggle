5ac6b75e1f5f6d5024a481ea4acaccc510dc7c06
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:52:51 +0000

    influence unable continue long warning

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
