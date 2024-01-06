ada95b697847cb06fad89f9faece5a44736e5f23
mdmaid69 <robot@alexsyw.me>
2024-01-06 20:28:01 +0000

    accordingto person congratulations beyond relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
