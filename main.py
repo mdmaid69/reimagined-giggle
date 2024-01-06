65d827305827564e093dcddc46c6e197032ff17a
mdmaid69 <robot@alexsyw.me>
2024-01-06 16:26:06 +0000

    week mention know cut substance

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
