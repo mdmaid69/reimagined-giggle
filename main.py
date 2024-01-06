33402811d8773e7f9eec99551655c7d4f93e4227
mdmaid69 <robot@alexsyw.me>
2024-01-06 09:10:06 +0000

    central switch comeup piece level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
