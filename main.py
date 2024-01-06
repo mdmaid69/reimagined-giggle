bbe2fe07c51f6debff235c2b36b77b71a2fe1900
mdmaid69 <robot@alexsyw.me>
2024-01-06 11:15:04 +0000

    shout complicated regional so little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def subtract_numbers(x, y):
+        return x - y
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
