4eb362e5124431ff0611ee84f8504ae153e62e95
mdmaid69 <robot@alexsyw.me>
2024-01-06 23:39:19 +0000

    store possession name seem ticket

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
