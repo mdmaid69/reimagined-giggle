2db340026b151c1bf19e84f176fc1a2638411945
mdmaid69 <robot@alexsyw.me>
2024-01-06 08:58:37 +0000

    war similar measure range apparently

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print(sum(range(10)))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
