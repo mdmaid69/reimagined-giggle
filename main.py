cb3909e500b2f106fe891074856d44617d626db8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:47 +0000

    chin produce bunch divide relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_queue():
+def create_stack():
         return collections.deque()
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
