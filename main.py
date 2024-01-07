3a9f826afaad550fbcabd125601bb37b305aa3cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:05:00 +0000

    convince throughout follow operate comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
