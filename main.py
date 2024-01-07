c69cfe4f7aa76d68bb07e1deb8aa6287c54624bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:26 +0000

    tomorrow miss night runinto hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
