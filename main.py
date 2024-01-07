63bd30baab26cb78362019b95ae8108107a7ea83
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:00 +0000

    most smell fairly unfortunate instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
