c4d24cb352568d6662ca570c9bcc909e23de3dcf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:42:34 +0000

    Ee recent entry for learn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
