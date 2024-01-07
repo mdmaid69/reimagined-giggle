2a81cfc0b6fea67fd590dc0e0dcaa069e842d5b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:17:47 +0000

    insist next burst anyone important

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
