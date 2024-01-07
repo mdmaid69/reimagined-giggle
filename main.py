738952e7dfcca46277ee7ca8cde607754ab0626d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:59 +0000

    direct grateful search level point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_min(lst):
-        return min(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_list(array):
+        return array.tolist()
