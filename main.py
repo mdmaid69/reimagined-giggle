672ea86ed6b385162816ab1bdfee036deb859bfc
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:18:39 +0000

    waste hollow public seat expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
