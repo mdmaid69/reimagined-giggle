e39c783e16b3356979109d0013597e16a522d640
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:02 +0000

    yes lucky early convinced offence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_from_array(array, item):
+        array.remove(item)
