d907353a5114ce97d88023eab15f2e1f813bbe6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:45 +0000

    deserve fashion border poison even

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
