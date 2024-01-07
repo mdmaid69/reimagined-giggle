ebe7c73fc6a3cd884950196314158b957032c629
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:10 +0000

    war quality language long dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def remove_duplicates(lst):
+        return list(set(lst))
