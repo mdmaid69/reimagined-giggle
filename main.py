3393a6ede3e8ff7cd2f031e719d7b3a62b3228cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:39 +0000

    congratulations however attention building prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
