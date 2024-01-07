a748f859af773a5ceb2c38d6f6d3107e43abaf94
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:26 +0000

    forward extra takeout mystery value

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
