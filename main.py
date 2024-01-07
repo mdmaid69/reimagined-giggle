3cb28612b1455248d088ed87116ba4aed9ee469a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:09 +0000

    climb talkback heart software easy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def subtract_numbers(x, y):
-        return x - y
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
