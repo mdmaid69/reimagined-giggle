cef0d55852aa2798feca09cffdb7b84471467293
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:08:40 +0000

    event nerve below way opportunity

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
