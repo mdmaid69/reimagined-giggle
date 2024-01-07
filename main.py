18f5b6f87bb612d8b434bc746ca397ab91563864
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:54:05 +0000

    impressed punishment best uncertain warning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
