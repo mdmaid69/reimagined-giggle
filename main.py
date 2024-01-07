e2effaa227beefdc1244e011ea9eb1d6db07b2fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:37 +0000

    public lend world thinkof book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
