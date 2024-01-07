e07c8a8162a8312ceae07529f4989ef9b71617bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:08:55 +0000

    last recognize sort ordinary label

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
