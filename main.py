b5a9d0c069b121560d74c3cb41b7f5a385f87376
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:07 +0000

    slowly relative run contribute impression

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+name = "Python"
+print("Hello,", name)
