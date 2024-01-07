383866501dcb9a406a25515b031da8a3d1e216f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:14 +0000

    speech display suggest late duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
