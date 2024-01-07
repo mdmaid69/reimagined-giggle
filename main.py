3f10ad8bc77d8faeeb92c6e7384e63480dfb436e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:33 +0000

    freedom rest everyone blame pay

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
