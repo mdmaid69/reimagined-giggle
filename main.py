dbdc5f3c25be8e9a5f547749bf6a1dd3a363f87e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:32:52 +0000

    thinkthrough group evening fierce steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
