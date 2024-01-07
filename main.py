9889f0203aa41818d8cb4b3de4e50caa4248331e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:11 +0000

    hand still regularly design female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
