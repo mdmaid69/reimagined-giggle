9648b69e0fa2ab22794a96fd7c11c6dd10bafe98
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:39 +0000

    recommend bringback though fall walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
