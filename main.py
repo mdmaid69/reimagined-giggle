735b356bf1fb3a0e2ade7176623ddf5a0b762a77
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:02 +0000

    exploration exchange force blue to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
