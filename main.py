4b08a2cf82e8e233fa891b301e7d15fb94e6272c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:09 +0000

    whom ago wherever room another

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def add_numbers(x, y):
+        return x + y
