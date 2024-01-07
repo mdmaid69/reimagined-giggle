183316c8a3217a0863c9fbb1d7e8598a1cf3c51e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:56:54 +0000

    bother borrow tough branch underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
