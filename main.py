fe6875b447fad027ea2b057ef2bdfab27ca4cb9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:38:42 +0000

    sound size which concentration worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import os
+print(os.getcwd())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
