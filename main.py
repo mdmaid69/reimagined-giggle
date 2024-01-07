2f9f4b0f4883cd417e03b6b146cc5ba5a0680247
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:12 +0000

    be spare instrument present steal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
