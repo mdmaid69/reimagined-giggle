66e93ad18bce559deee269f4a4852a14874743c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:30:43 +0000

    neither space affect bend eat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def reverse_string(s):
-        return s[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
