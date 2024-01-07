258a7739c332a3f0abb24f0a281b28378afdbb5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:08 +0000

    proof meanwhile beat possible border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
