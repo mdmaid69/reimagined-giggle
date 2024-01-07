21dc7aff8aaeb9d792208824f28809abdb07c644
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:59 +0000

    turnon lookafter instead satisfied holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_min(numbers):
+        return min(numbers)
