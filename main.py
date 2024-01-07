b061f51d6762c532a3007a91ab3105a0fd895ade
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:32 +0000

    sure year collect charge survive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
