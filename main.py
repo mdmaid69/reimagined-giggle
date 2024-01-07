842c099743a67cae1882e177fb0e4545520e3e73
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:32:36 +0000

    cook upset convenient tragedy moment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
