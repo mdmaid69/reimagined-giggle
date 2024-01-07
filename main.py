f3e70425cdfa8c88c709c5746a5caf2ad4c95768
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:07:17 +0000

    family forgive defend strength effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
