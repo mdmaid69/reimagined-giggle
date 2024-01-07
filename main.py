5b2de2c635601c60636a9e530e0dee27856f562f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:38:09 +0000

    hurt want immediate receive unite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
