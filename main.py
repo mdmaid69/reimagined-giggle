ae4d276673c7d5dc66ad5d27a5dd39cbd21ebacb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:34:16 +0000

    common check plate ticket necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
