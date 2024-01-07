731b483b3458a8a9b0d78da662e5ccff3d3fbd5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:01 +0000

    drink satisfied summer will powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
