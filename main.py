dfd59ff78b5c9af37f1e65058eaf7797303dbc97
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:09 +0000

    live straight cinema blow dish

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-print(sum(range(10)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
