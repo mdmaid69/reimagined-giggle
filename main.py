feb7d6cc1ad977950466763b68276d89ca082193
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:38:55 +0000

    bringforward pattern whistle takeaway thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
