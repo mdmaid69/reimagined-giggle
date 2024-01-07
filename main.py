4454e507b110c78b4e8c480b20888b638b8565f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:51 +0000

    police cheek so typical blame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
