ac94a2510de2da9384c155571bd6a38230f78b52
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:16 +0000

    limit by cruel mainly hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_density(mass, volume):
+        return mass / volume
