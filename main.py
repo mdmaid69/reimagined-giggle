53e628cc3698f7977258f7afae1d1c045776c3a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:14 +0000

    interview past far software target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
