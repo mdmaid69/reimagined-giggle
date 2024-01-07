76cfcb68149a74aead9aefb083315e1ed3574ea7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:45:14 +0000

    honest already university sorry pool

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import json
+def convert_to_json(data):
+        return json.dumps(data)
