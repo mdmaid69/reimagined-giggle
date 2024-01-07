6ca45b4c845775f6504a689a3654090ce7a7bc94
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:49:09 +0000

    back wait rule discover pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
