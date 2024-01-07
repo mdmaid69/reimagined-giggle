e20f0937a4f6a8f87e6a702954f0ac1af88dc87e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:53 +0000

    night performance matter anything much

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def get_array_as_tuple(array):
+        return tuple(array)
