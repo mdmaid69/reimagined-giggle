4d08fa7ecf29548f5a0ea2e3d420ff0faef35b0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:06 +0000

    send so soft food right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
