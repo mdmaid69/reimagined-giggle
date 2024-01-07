53b3ed92e579de9dc39a03a2e773cd49373139bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:18 +0000

    church sale impossible heat draw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
