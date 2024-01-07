a0fb9aedfe4dcae3a9034a3c3a4d8adb09199d25
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:01:21 +0000

    consist stone fire touch near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
