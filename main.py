478e4a94d32769caad0011b84ba83dd0a0bf2a00
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:43:35 +0000

    formal switch advanced hard leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import re
-print(re.match("h.*o", "hello world"))
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
