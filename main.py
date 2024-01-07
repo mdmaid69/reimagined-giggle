ad3c89d249070b627e79e96e0876810e33334cb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:32 +0000

    both satisfactory trip innocent steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
