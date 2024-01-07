ef53fb8ad6f9aca2d5a739435de0bc7864f577b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:33 +0000

    we rich introduce spite nervous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def divide_numbers(x, y):
+        return x / y
