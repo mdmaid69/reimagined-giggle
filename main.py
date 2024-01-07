dd80a14b1894ab54f4d01f4e5065b2a951092a7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:30 +0000

    popular combine couple father tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
