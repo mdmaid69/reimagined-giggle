6ad20728711d040d38fa04098385f520c166617c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:41:36 +0000

    room avoid Vv wish switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
