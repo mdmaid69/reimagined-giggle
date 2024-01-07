b5acae3d0850b9d13f62bd8747849e473b851325
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:44:01 +0000

    comeout country 571enough feel entry

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
