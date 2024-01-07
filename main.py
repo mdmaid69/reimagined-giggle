5461852aa3f038ab0bdc6d9eee4c6897c26a1f66
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:29 +0000

    muscle famous progress together goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
