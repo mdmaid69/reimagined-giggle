900521bc7ea3bcf9f199ac0bb5bf212c2526a80d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:38:16 +0000

    whichever certain bag actual stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
