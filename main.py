744b9f4a4b550f39c60d7cf190fd130cf349f7be
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:35 +0000

    accident man both advanced voice

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
