66a9568eda8ce10a2a1af5adc6c269fe147b92c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:13:48 +0000

    tomorrow reliable dear rely medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
