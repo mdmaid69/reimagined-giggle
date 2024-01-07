0c60060fde037af428b330761cd12c284f408862
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:27 +0000

    be illness juice trick wait

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def is_even(n):
+        return n % 2 == 0
