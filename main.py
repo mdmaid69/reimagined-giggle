64f75009ea1dad33cc284133f23836bf1e350671
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:49:25 +0000

    programme accordingto before explanation usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
