bbf0522d6ddd8def15f7c901c29d07d8b70d837c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:27:11 +0000

    exit constantly notice hate beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
