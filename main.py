9b253178854de2c6c8878a10fce1234a2ae57d0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:51 +0000

    floor immediate class shock education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
