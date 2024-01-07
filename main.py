69425b85a6b4a5fd7e444150463b4f608a41909b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:57 +0000

    rent information freeze stamp dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
