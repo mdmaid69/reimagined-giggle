5cb9ec61dbc8ba229570e369fef7c20172b2af5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:09:41 +0000

    prize treat reduce inside bottom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def convert_to_octal(n):
-        return oct(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
