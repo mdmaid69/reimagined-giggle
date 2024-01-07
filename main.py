21cf1501d8b64032adaaf6ed786df568bd4b9bfd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:16:02 +0000

    handle fasten turn occasional worse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+print(sum(range(10)))
