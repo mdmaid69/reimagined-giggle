94174b0664fa7cfe1a858d2b61937083eab7f5e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:36 +0000

    narrow please land game something

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
