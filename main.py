ea0967f598ff1cbd17ddc932351ca74b945fe7c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:10:12 +0000

    receive wish neat rule so

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_list(array):
+        return list(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
