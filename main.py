0e5fcc67b935aa0c967302853c75065e13ff27f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:17:36 +0000

    branch clue Pp cruel weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
