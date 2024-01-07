59fcf7f16611a7e60ad9e00f42912e15cfded9b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:46:56 +0000

    yours thankyou ability pain prefer

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
