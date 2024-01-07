70d759190b1270ff96cf721d425995814a89c821
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:37 +0000

    steam will bringback apologizeapologise connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
