401dc360b0966323a59d625240f0e1f3efac54a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:52 +0000

    unkind marry Zz old hospital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+print(os.getcwd())
