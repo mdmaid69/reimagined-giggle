3e96994b8826036016f059e08cc5c30ea872cd2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:44:57 +0000

    notice again reduction takeback comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
