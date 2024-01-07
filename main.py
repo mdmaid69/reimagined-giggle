e79f8a939d887137df3dc34da0852a07886638eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:49:09 +0000

    shall pronunciation arrangement used ring

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
