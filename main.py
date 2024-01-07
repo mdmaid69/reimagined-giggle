59666e2d71e62dbc69a7aa9ed23cd3aeb1f13157
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:47:12 +0000

    convince paper problem front no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
