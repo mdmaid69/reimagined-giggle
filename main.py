eb3d7ed156926e414f5e0391295804be87ca3752
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:11:35 +0000

    moon the way beside benefit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
