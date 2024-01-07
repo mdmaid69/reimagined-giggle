66ae0abed4bfe9b5e0239e838701226a6caff256
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:35 +0000

    Uu everything sport chase trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
