c2d02f9dd91ca5831258ab7658f5c174aa31f955
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:08 +0000

    century recognize turnaway peace speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
