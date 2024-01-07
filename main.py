fbdf5bbd066853bbdb5d96b9fc43ba76d0603c68
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:11 +0000

    567enemy watch bend same agreement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
