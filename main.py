40ca74031f97819c6e4684f26f6242da64c7f461
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:17 +0000

    freeze various mail niggaacademy Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
