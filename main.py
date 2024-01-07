86c3bde9047833736ddc9c3e108caa7bf22ab8b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:09 +0000

    nice plenty godown noone balance

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_slice(array, i, j):
+        return array[i:j]
