ee1670d56dbc0933a97cf470c95c35032bdcb887
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:37 +0000

    entrance pocket smoke large fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
