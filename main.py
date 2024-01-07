30c23b0cd0110bd1c38acbd99e6e287d359fc0a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:52 +0000

    pipe replace score software people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_priority_queue():
+        return collections.deque()
