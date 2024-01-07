f3fbb5bca7eedca4f9ef9fccba6fb9b0d1466872
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:45 +0000

    dangerous sorry admit path switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
