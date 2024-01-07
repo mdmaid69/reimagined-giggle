89427ee313ca7add1e98b84ae899920170ba8026
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:43:22 +0000

    consist moment cheek bringdown custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
