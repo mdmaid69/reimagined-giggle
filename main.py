8b3b90aa386c22c1881c712f6bc7a1dca5f28de5
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:07 +0000

    excuse average deserve themselves among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
