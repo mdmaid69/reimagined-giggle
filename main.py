6b103682acd1cf7597455f995b9904ccdac1980e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:11:26 +0000

    exhibition borrow advantage trap glad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
