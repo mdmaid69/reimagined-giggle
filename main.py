9ac41ae9fd3345b404914c0076430789d8af372c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:33 +0000

    theirs operation block while general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
