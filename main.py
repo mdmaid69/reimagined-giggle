11faf1f14604f1627afc2b9a33ba13973e216bd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:37 +0000

    door disappointed become open juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
