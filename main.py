ab23690d1331e9e5db300b40ef842e0ba9cf1c6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:03:27 +0000

    wash accidental deliberately getoff so

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
