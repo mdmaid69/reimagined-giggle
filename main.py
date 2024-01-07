fea5e2d0a15ce8779f980ca117d1136b5522568d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:40:05 +0000

    stamp not personal meet rubbish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+for i in range(10): print(i)
