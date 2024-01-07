278dd807801094bbf05f6af1f6c65e9a78108a80
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:38 +0000

    university wheel but arrangement myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
