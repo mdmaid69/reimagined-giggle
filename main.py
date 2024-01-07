72d5b27eb4f135f79d2dfc80cf3adc09e7232e2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:31:03 +0000

    wait line zero artificial stage

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
