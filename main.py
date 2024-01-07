ba9154c691f79b5aa9f19a3d3112e2dd24563562
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:02 +0000

    favourite interest them splash Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
