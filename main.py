cecc5863cb10cc5f2ee5fd048d9e78533a8f26ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:11 +0000

    away right phone side paper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+print(math.pi)
