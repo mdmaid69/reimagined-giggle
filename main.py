3899df11f94432bc0944ef23cc6936d6d58b661e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:49:30 +0000

    accidentally repair copy beautiful mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
