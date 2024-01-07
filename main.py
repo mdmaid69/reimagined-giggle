8bf5023c05f300baa3eaa98d456d8d9eef4ca41e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:19 +0000

    gooff hello exit base trip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
