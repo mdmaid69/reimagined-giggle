57234ce21302aa21dd527c5575c854d7d8b5fb86
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:06 +0000

    protect almost tip this form

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import logging
+def log_message(message):
+        logging.info(message)
