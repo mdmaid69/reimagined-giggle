1c0aebbd79ebd0e1aa0631dd66146c8c8a455971
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:08 +0000

    voice tear sort pity force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_tangent(x):
+        return math.tan(x)
