5f8208156497f75c6e4a41a6cd91a2bdec948188
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:37 +0000

    comfort please pleasure crash mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def remove_duplicates(lst):
+        return list(set(lst))
