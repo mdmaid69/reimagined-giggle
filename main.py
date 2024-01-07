69d68bb9be74b91f25d1a1c0c3beb21589d76f87
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:27:36 +0000

    unkind discover forward letter swallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import platform
+def get_python_version():
+        return platform.python_version()
