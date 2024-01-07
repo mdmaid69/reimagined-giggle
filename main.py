dd9ed5cca0aa8058a485a1e4e29203f031529772
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:55 +0000

    coat burn drink open sale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
