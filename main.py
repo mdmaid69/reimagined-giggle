c066494da8451d2702ed0c6e56e3ddc5de216b34
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:37:06 +0000

    range guide simple explain close

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
