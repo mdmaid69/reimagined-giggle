b91803642dabd687e6961e50ba099a5e9d02b62b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:49 +0000

    standard catch tread reason tool

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def convert_to_hex(n):
+        return hex(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
