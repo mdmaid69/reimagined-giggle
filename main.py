1833049c75eef219796389aff1edd9df136e08b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:40 +0000

    patience empty disagree welcome connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
