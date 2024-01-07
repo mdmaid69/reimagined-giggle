43e7fc3ecee4ba7b1d1f798f49b6dc8b2a4494d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:42 +0000

    activity somewhere normal couple criticism

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_list(array):
-        return list(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
