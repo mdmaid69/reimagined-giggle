d073971d694456a684a981f6b4d049073dc26392
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:24 +0000

    load lookback(on) always confident possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
