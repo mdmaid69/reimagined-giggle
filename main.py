5dd32dde2ab8f01e7590d9e2f9df2e502ebfa226
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:51:19 +0000

    change man black convince open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  def is_even(n):
-        return n % 2 == 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
