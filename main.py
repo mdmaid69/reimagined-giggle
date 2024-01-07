7628ca1b13157664e23ff35fda7ce3a3d828be66
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:28 +0000

    rule neither business pump concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
