2876329f7829b883c09a52b12ea4c77b35b359f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:21 +0000

    winter Mm space grand explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def greet(name):
-        print(f"Hello, {name}!")
+def get_array_index(array, item):
+        return array.index(item)
