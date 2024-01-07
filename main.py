a10f948f2f3da9e140a33a586e9e8cc6bcf4fb3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:22:01 +0000

    generous mood stop never clothes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
