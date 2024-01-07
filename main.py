d40d5755d491a56257a9b1da135aaa9f23414e6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:52 +0000

    health permission murder doubt broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_current_directory():
+        return os.getcwd()
