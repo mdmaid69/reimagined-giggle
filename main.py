cc37cd00e1ed2ee4eb893bed86e2d2d2c60ef6e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:55:41 +0000

    large motion choose everything fetch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
