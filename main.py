d9cdd242acc64b64de2acfb09122917f72aaeccb
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:57:43 +0000

    dress opinion emergency race pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
