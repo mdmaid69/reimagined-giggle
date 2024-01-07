7acdd1dcdcf3d755aa586712563164ea65df7ca2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:18 +0000

    birth aware nerve goodbye entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
