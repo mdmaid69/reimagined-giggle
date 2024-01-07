a168afa83572275937539f5f9a85f10db4e7e798
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:42 +0000

    scrape shame waste through until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
