e65dec2bdb5a4112b70c56bb096ac237a83f89e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:04 +0000

    eat far often concerned inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
