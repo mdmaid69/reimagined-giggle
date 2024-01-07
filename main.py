2f3f9c722d757f067295f53aa84caf3700877aef
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:53 +0000

    play none than track light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
