17aaba2aa68744bd2c8838fe885dc41bd7966b30
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:25:20 +0000

    makeup silent operate crisis danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
