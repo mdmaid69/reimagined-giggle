da67dc8fc27ec0ee041eb1138e2e8ce9a76af028
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:39:18 +0000

    comedown brief best late moon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
