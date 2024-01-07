affc970a92170357a697c42ee5b89ab383b1c827
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:44:09 +0000

    famous share involve bother breathe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+for i in range(5):
+        print(i)
