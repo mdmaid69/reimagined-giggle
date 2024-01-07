dd63952454163a392861293794ae0d9aa8bc2af1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:09 +0000

    increase season model belong happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
