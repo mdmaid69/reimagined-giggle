78a44b92c6fba09e2cff57b1ec915b997a0747e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:33:38 +0000

    learn suffer onto trip earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
