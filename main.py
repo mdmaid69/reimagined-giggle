511e8a3c26b76dcacbe55b5842d4720d85525fb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:58 +0000

    team every seat few paper

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_acceleration(speed, time):
+        return speed / time
