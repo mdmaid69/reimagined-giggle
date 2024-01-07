0949716bf45ad39e13d87e30155e5d9fb0de701b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:58:35 +0000

    while find bottom least once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
