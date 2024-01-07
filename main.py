79a186c32be571a1e87ef650991356706b36733b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:03 +0000

    sing lift like switch satisfied

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
