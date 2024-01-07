d972b60f440ce43154e7acb9f941c0cd14fc4fdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:41:37 +0000

    makeinto defeat imagine unlucky present

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
