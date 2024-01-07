17bb5a87880580a17ce9a09c793912fbf5304c76
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:06:06 +0000

    extra dress crash child apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import collections
-def create_queue():
-        return collections.deque()
-import getpass
-def get_username():
-        return getpass.getuser()
+def create_user_dict():
+        return collections.UserDict()
