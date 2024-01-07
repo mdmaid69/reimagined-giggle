72bc7db75fab0d9da7c3956faae93d141e2f186c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:04 +0000

    complete target however coffee declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
