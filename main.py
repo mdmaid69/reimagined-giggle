da4455d4d0228a204b5b9ca4fcd6fccefdec1546
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:09:11 +0000

    rather course transparent load before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_acceleration(speed, time):
+        return speed / time
