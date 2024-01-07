143b98c4f877b2276d6c0a2d90530e622149d7dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:54 +0000

    before mad deaf tie he

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
