dfdeb536bab8df1199029dde7c1071cb7c0f3ef6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:46 +0000

    warn ought holdon pinch smooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
