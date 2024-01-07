cbc1e3bbab47b191ecec788f4d995e0c2eb5a55c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:33 +0000

    emotion tired corner noisy meal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
