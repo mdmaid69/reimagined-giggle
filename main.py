68c4490b192c870a5cd38c76f1eef9dbcf2f98ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:24 +0000

    ask answer crowd power total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
