bceca5aa1bfbd7a3039563e7bbe99a8ed28fca0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:09 +0000

    whistle depend accommodation trick nurse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
