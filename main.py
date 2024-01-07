e1a3a94e8c81ee549e80538c9ed919038555aefd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:36:13 +0000

    work accept town skill group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
