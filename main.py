fd8a45c0a72e68d7b0d72d9a67e4cebb7264561c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:33:25 +0000

    happen could race furniture comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
