bd7299b27e95115d38225bdd119fef0dacb3fc32
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:09:30 +0000

    sound boat grand dream bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
