0d8f4515393829ae23b4a0e42a37049e63c6d1a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:59:52 +0000

    false lift fast steam school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-print(time.time())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
