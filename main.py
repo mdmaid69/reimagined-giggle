6aad2818024670cdfa539ddb3256b0159648d3c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:30 +0000

    organization right distant sleep join

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print([x**2 for x in range(10)])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_list(array):
+        return list(array)
