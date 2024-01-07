ff9fa343ee0393fdd2e0c9ec58fa240954734723
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:23:00 +0000

    region face sweep scream record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
