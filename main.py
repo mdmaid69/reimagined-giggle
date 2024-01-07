e9396e5bfeae4925fdeea4f65a995bab1897d461
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:55 +0000

    tone genuine generous size light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
