46bea4e31c3a4f82aea243cd34cd94ca5429abd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:05:19 +0000

    I head schedule object uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
