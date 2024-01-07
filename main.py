0abdf84ba81e72dd614af3817ff4c5e342348753
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:19 +0000

    getup keepfrom cut judge drink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
