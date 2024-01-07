93926d225e407c4ae3c48578d3733032ffc12ee6
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:29 +0000

    opinion town pride complicated pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
