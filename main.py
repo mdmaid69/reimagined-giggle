b28945e3131b0c707b629087c1d0bcaac63b09dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:19 +0000

    bottom muscle hope urgent practise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
