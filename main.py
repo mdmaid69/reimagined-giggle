918e23d022bff8f912e6d915b251dcf38686e61f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:53 +0000

    hers succeed pinch control weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
