c80a892830e3fab8e78a1d3ee996421ef9d688db
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:28 +0000

    bet run illness tell speed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
