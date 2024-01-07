6f71755aea164d532f91d8b3c084f1815fa51792
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:11 +0000

    takeover operation difficulty tone drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_directory_name(path):
         return os.path.dirname(path)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
