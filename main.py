ddaeadaf3f39bba08fc9b291c8256de5c70c9276
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:32:36 +0000

    sound stupid greeting surface direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
