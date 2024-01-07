4b80268021b7f8d467317f943a4976b3321d644a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:50:31 +0000

    dig wind trip colour move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_item(array, i):
+        return array[i]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
