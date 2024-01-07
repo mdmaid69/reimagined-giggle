87ffd340ca51f098df98d6558536ec5515ea8fd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:37:41 +0000

    list sport Gg scare lot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
