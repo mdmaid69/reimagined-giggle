94f8e5ca7a84a8204a8657c55eb87117ada8b60c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:31:57 +0000

    someone rise exciting visit comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
