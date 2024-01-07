27d19fef9a221fa4e7b70c0bc449b028e39dd9dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:34 +0000

    tight estimate objection loss soft

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
