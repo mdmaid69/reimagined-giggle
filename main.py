170ebfd0e7c6189f39ac251554ef1002ca60582b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:28 +0000

    sight mark wish sight breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
