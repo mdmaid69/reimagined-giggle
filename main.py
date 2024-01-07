c9351aa930a14b76cba17225815bea5693a5e189
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:16 +0000

    belief close uniform floor tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
