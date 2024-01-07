de2d8705bdc659be246585fb295e4abcf378abfb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:34 +0000

    furniture reply noone production Cc

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def change_working_directory(path):
+        os.chdir(path)
