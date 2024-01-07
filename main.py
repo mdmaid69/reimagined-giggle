41dab4bee067f21a580f1c4ccad4be01b73832e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:39:44 +0000

    lookup average language moment seat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
