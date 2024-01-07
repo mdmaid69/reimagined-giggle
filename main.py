72ca5990bfd87d346ed7fdf1b78bd7db55ca1dae
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:09 +0000

    report medium noisy where corner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_speed(distance, time):
+        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
