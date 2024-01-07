13a85634947cd5832a4ab52d3997a1b948ba1837
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:57 +0000

    temperature send cheat sister inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
