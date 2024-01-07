02c138a5a4d55dcac75fb48179528a6df859baaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:15 +0000

    shallow apparently burn mistake appeal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
