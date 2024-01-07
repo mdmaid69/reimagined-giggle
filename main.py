dc34e25b13af8672fc6c115230a9ba6f5b917196
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:53:34 +0000

    dig emphasize climb burst classroom

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def sort_list(lst):
+        return sorted(lst)
