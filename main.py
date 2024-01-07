9b0e32efaf6c4d8cdfb7afa7708486ffcfb52132
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:13 +0000

    moral comefrom professional start careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
