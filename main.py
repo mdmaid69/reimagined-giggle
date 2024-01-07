7f5aefc9a77093b0ebacec0d48254a855f282ffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:17:41 +0000

    chase bottom abroad separate market

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
