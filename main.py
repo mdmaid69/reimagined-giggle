0e6c00079a17c204f0eb337e5c258d49c76d15f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:54 +0000

    stamp abroad well supply take

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
