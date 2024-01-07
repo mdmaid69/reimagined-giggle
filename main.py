2ba681720063ffee958463edf332e713daf820cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:33:23 +0000

    spoil string injury congratulations as

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def convert_to_hex(n):
+        return hex(n)
