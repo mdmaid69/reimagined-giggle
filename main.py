c82e6604b9f032436e3aba34b6d646802fc26959
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:56 +0000

    ever apply clean violent talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
