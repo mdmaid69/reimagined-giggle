cbdeb37df42d5c7fa863a19c5ecd1a4fd1285328
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:06 +0000

    measure goodbye describe weekend proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
