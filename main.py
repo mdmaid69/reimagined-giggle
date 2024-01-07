8745de0e2f70b01637d36643a027a9cdbc3f963f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:47:39 +0000

    fast balance let cover need

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
