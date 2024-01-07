43a7df3ded40911a07c9215a9415b3c20ec07939
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:00 +0000

    into movement possession throughout skill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
