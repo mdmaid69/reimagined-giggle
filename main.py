1fe619d4a79f8c2a3d2cba73375704bf542a6c47
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:39:36 +0000

    before method underneath waste violent

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def count_elements(lst):
+        return len(lst)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
