c51ceb8d87ff9736342121cbcc0ced9ce599dc15
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:01 +0000

    on use keep patience handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+def count_elements(lst):
+        return len(lst)
+def count_characters(sentence):
+        return len(sentence)
