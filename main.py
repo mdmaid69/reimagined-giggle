7078d6b12833a89832cc1bc2919550427613ca69
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:50:45 +0000

    score pretty stir instruction wall

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
