e424365998bcff29afcc610d348c41e304728554
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:33:27 +0000

    spoil share nearly reduction settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
