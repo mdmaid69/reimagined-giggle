494f8e45f890908b2f6d1cfb85a944d5cf66fa7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:20:18 +0000

    separate meanwhile quit far convenient

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
