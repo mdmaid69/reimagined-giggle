a5757fe0c6e147f271298518cae11e41823abb6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:58:40 +0000

    permission pull friend vision 565end

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def reverse_list(lst):
+        return lst[::-1]
