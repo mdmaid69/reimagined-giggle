4cfca00abd70bd20f2c2b20a2ce1158d35e89733
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:56 +0000

    reliable determined warm rush mean

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def square_number(x):
+        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
