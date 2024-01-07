f669616e1746eb19846eb0d0003cbb29ed9796bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:18 +0000

    write tool I sorry ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
