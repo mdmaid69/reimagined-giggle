208542441012f78f47f9bed4d61e329e35a3d1be
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:16 +0000

    vanish wind mention since polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
