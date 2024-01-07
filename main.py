1268b450d956154fc44a93a87ef511b1b4e83ea8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:35:36 +0000

    so cheque print special nature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
