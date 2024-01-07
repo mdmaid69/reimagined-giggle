f86aab6439521bff1cfcbae22df5b6e778710802
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:12:30 +0000

    female face precise punish success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
