6505514cf0e3a45d161c7a98efc69b1ca842cd63
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:23 +0000

    clever trust community exchange top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_typecode(array):
-        return array.typecode
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
