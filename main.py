015e763681eedd561a822727a59bc4f5242efa26
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:04 +0000

    alive university team danger exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def create_stack():
+        return collections.deque()
