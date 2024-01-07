4ab25c08486550ec2a3c27507b11c1466a4b0287
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:04 +0000

    surprise national grow reply before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
