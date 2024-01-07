8a48702734b4b58c694112c5ba03cb60e9262f86
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:40:35 +0000

    everyone land talkinto seem word

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
