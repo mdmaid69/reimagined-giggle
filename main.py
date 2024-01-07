3bcb04a4eb4d7156dcff66571c292fc1ba9ce3a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:09:53 +0000

    house reason left funny turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
