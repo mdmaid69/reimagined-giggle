c80f68a3f1b48eaec2684fcfa8cbd25888b1babb
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:39 +0000

    comfort worst method church snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  def convert_to_octal(n):
-        return oct(n)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
