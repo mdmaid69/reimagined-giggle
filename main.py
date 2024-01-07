164ed04349c79695a1b332231dacba1a313edb7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:14:17 +0000

    nose past drink read blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def greet(name):
+        print(f"Hello, {name}!")
