4d03f77f2db595e381b2d83f48d308ff6e03f96d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:00:30 +0000

    awake can consult discover sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
