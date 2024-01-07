e25022a65eef3cb5de8243123464dd259f3093a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:07 +0000

    line surface escape check problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
