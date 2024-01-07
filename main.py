486e4ea39452e3bd21ff6b67d2d5ba1be631f438
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:29:44 +0000

    continue spare keep direct authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def square_number(x):
-        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
