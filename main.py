d37ee3505a2e3b930d6ceba8c035f3db32f983c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:14:15 +0000

    legal thankyou message punishment wonder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
