628adcc18fcf49dee69a24c0a276913990d829ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:24 +0000

    sweet achievement account believe quantity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
