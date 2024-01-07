e0b7c5445a815def92bd50f0bf1965dcedd67e87
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:54:00 +0000

    vanish buy hold clever up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
