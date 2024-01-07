754d1d4cf82b2dcb074718c61df233ae99d1cbef
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:28 +0000

    mend mention emergency pocket moment

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+for i in range(10): print(i)
