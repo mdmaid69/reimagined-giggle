9f290f6949fb0124c5e39dfe25f4d5bf94baf190
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:33:27 +0000

    turnback immediately impatient talk divide

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
