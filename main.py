6c99339e74d05a7406e7edfe0f2ebbbf6e0edc77
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:10 +0000

    plastic practice affect carry illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
