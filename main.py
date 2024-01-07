5b8ed8919c5dcd5d8d567a8c068bb03f6a707f86
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:20:42 +0000

    calloff shape body give quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
