17b6c639df397ffeb4e264d497f571df61ab0d81
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:10:56 +0000

    stage pass freeze report feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
