1f67ff3c8a449a1685957a1e575f4098d20ddb7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:32 +0000

    jump perfect stay argument do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
