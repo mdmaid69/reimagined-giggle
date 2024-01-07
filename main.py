8a4a77004d688fabe98033229968a9df343c5061
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:26 +0000

    plan thinkthrough rate suckme corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
