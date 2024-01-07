57b562f89e5e0eca1eba423e205e9a314b2c5e5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:03 +0000

    file interruption risk copy until

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
