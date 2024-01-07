737b24a0041561b4792db559c6ade149b7977a5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:29:37 +0000

    blame warn scrap late official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
