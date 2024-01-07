8886aefcd3aef5c3c50175775e5a6b57f5fdd506
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:42 +0000

    get stranger nor warn claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
