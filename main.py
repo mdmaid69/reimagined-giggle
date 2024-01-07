69124e92aaf61f9feac3afb3a3044d4092ff9064
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:31 +0000

    care rent general embarrassing develop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
