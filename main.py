aafcd8afe33aa21a5b3f0e4ce65dd8b469961677
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:53:55 +0000

    male well plan use the

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
