e74505033093bfe7d6769c5f7c0270337e69a99c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:40 +0000

    Hh yet allow turnback tall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import collections
+def create_user_string():
+        return collections.UserString()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
