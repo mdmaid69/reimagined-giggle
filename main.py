ed94d9b9fc05719cc12e1f08276cd1d64673c3d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:18:46 +0000

    their violent mend light my

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
