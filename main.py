61c46a3dec632527246fc1b7e5b4561413446136
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:07:47 +0000

    dare finger better way defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def sort_numbers(numbers):
+        return sorted(numbers)
