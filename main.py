f48bc69992cb83315416c04bc043e9ed533bd018
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:05 +0000

    the practical pour that speed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
