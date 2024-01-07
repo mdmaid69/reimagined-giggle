3a2939566b3e193450bafb169d577cb3949fe841
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:56 +0000

    truth bite schedule Zz way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_repr(array):
+        return repr(array)
