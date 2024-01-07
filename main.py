5334d3d2969fb0c117b930898e3af77025306882
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:19:23 +0000

    goback(to) run them concerned favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
