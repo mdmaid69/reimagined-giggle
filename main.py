fb12bcfa08ae331bd1a5259c585fe4d3dec7fc91
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:06 +0000

    argument some string mood load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
+def greet(name):
+        print(f"Hello, {name}!")
