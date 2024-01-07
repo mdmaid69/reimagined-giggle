38ce1aaa94c46f8594f769b76bd75190ec3c9f87
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:38:20 +0000

    follow wind fear conduct wise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
