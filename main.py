384756d1e8323902d5ec03ed214337d42254b29f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:36:52 +0000

    dress fail smoke estimate piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
