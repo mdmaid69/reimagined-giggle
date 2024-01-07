d66e95d61d1ded6abd6659d6fe3bd8f4b5436192
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:56 +0000

    walk pot progress determined couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
