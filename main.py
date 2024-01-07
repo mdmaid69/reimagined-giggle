863a63f249cc743c84e7b5da9f1c8e22c45e8de7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:34:49 +0000

    breakthrough ilovedicks frighten call left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
