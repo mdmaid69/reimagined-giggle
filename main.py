6d9a45ddd91fcbdaccddb5741a2edd0efbf246af
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:50 +0000

    family check give wild narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
