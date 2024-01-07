a7191ffef560279bbc70ab99600b6628dd1e02fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:49 +0000

    pass increase exploration bus block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
