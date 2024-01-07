402caea924a28774824047d60a139a9dbdbe92ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:27:37 +0000

    job leave none turn load

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
