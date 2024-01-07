2417b49da44e1c334b5ab9e5a3f688c9efcff1c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:38 +0000

    throughout expression balance refer search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
