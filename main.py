c9c2b287996ab08b9fd7902ebd41d015a9ee1cd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:29:34 +0000

    immediately sweep OKorokay 572enter bath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_repr(array):
+        return repr(array)
