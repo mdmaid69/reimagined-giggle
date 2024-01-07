defb89d8e9bf4abfcf6b7a9aafe18a272df2a6d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:59 +0000

    situation various develop quarter visit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
