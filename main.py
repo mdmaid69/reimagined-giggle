bbf21b66195a60c9e18cad7cde4c1d8bd163e476
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:56 +0000

    extra market exam section nervous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
