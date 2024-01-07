b5309c48f3ebc13aee176331a498e918c4a1d8c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:06 +0000

    away purpose approval idea next

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
