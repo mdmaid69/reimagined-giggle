eaee32edf9b47c63e6d80137d7d2bef277ee65b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:18:31 +0000

    set speech after pause remember

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_odd(n):
+        return n % 2 != 0
