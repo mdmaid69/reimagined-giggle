e723329afcf661b05a3c7e76751fe01b8517793a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:04 +0000

    bang bite danger recently regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def find_min(numbers):
+        return min(numbers)
