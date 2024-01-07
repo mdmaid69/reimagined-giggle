72024b816f3c6c479a6ae280024f4e09b1af610a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:27 +0000

    put steam small rough success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(numbers):
-        return min(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
