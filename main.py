10fb115240a15ae8ae7199c8f00165e8c4d3dda2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:30 +0000

    degree office under strike birth

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def square_number(x):
+        return x**2
