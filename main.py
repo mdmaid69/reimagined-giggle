90c2e8fcd55e62a899814bb8bc7d6293b63707d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:18 +0000

    anywayanyhow regret unpleasant gas scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
