852e845879c49d30bb0768a20249f25a5643e446
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:43 +0000

    everywhere as ahead squeeze behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+from collections import Counter
+print(Counter("hello world"))
