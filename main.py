6e8b79cb0a230c3bd9b94634e50af01e6d09b179
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:13:50 +0000

    thumb artificial polite blame point

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
