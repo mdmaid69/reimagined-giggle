588ba77b098ed20de83e65c0f138b6f732d410d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:04 +0000

    also advice pair as notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
