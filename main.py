7db7c48771f3ad2421b47757d60cc52521004c5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:10 +0000

    ill backwards scrap on cold

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
