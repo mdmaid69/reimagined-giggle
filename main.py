ba3c8466c6d1da1f4fec9e0ee6d285d92580baae
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:29:25 +0000

    control ought listen move press

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
