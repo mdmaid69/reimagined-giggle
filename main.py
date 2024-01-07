849b99b66720c9598b15963f11d70eaf8546d84b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:52:13 +0000

    ring grow steep ill sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import os
+print(os.getcwd())
+import time
+print(time.time())
