f147898d8f9e7084d23c26eb787790a29a35762c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:14:44 +0000

    smile clean steam unusual estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
