50bdb20e2cc0b16cef850cdea456135be26918b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:41:58 +0000

    target middle basically deep yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
