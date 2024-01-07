db4531144b1d6453940476dc6e0fa3a6fc43eadb
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:49 +0000

    Cc punish class tape interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import random
+print(random.randint(0, 100))
