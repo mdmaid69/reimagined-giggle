caa3def1fabd089a217400e3beade1907dce5d07
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:39:28 +0000

    style hang here judge sound

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
