e2e801daef56769e57bd6bf628eb6cb9f64c949d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:00 +0000

    follow stick lonely hold keepoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
