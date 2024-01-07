d6d4bbbaedd3b4dab8837cb2675af47bb511311d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:19 +0000

    break furniture sort afterwards race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
