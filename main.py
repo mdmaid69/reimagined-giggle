e00aed987d6a5c0e12425f69ef0bf071e061d08c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:22 +0000

    trousers inside follow now forget

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def count_elements(lst):
+        return len(lst)
