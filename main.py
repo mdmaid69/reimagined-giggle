71113eaaa7877cd5a582f705db5f1553371aeb89
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:03 +0000

    might geton run classroom fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
