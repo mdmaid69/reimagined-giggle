68487b47addd546777042fcedccc48acd9037cdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:22:25 +0000

    escape habit pile plastic lonely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
