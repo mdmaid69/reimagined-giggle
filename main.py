6f7bd6c29d6003a479953124054dc852bbab6d3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:50 +0000

    search mass rest contain remove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
