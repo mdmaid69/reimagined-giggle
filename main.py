11f672af7a5798e70fe0d9ddd6abfa20747d9b56
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:30 +0000

    shoot apply shadow national tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
