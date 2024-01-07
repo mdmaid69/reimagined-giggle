0fe2be8f76330c1bcd0ebc314ab4bee7d64c34fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:31:23 +0000

    system cruel square past relative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
