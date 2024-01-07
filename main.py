dd2da871dd3ba1eb5e91ba76c957ec6b174ecfac
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:28:09 +0000

    in surprised pressure still world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
