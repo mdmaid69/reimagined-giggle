7cdb790386cb78532c258843055e1a1d512a988c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:23:23 +0000

    keepfrom inform steer statement last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
