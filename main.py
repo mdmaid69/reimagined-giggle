fa33b4f40020cddbdde441528456246b256289e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:00 +0000

    legal odd by we minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
