d85c1dea47604be10ca88321a1ac3322a43dfe7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:29 +0000

    stretch there exist wander probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
