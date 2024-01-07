69999f5db48cf896ce26a9752bf30b0961cd51a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:45:08 +0000

    voice acceptable over conclusion affect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_density(mass, volume):
+        return mass / volume
