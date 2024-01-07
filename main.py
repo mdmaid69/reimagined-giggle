436014d9d8e4228b2122aa0a0af07557e64bb70b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:04:01 +0000

    keepoff half 564encourage guard smoke

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
