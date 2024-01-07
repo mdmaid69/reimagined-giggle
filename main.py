9265b48358d5b780dba973363a83fe2498871b4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:52 +0000

    sting scene live thumb accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
