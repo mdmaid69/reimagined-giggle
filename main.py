7190cf9917a90c048dc86fc67991a0dba3050cb9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:56:18 +0000

    promise friendly limit intelligent anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_area(radius):
+        return 3.14 * radius * radius
