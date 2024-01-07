a7454f656e0005f6da024dd68fc70822ee3f58df
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:02:14 +0000

    runout mistake start find and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
