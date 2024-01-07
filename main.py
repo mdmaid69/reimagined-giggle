dc2d0eba67f75364fcb6e22eb0e2590656242d96
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:06:02 +0000

    music but believe view weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
