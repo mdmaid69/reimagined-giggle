3550f980ea2c20145d8dc52331688f4ac68e57d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:24:04 +0000

    nervous stupid course middle cheat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_max(lst):
+        return max(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
