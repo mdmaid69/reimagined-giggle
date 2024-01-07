da19eb5967ad48a7ca76f1e98c7ecb1868823076
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:18 +0000

    late measure possibly worst explain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
