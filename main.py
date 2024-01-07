3715da54f63dd21a225e915c863e73e090abe108
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:04:34 +0000

    cover reach only waste waste

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
