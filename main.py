fc93b80673fef8c5a466513a3b3e198865dbf23a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:14:01 +0000

    between opinion exact blame hard

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def sort_list(lst):
-        return sorted(lst)
+def is_even(n):
+        return n % 2 == 0
+def reverse_list(lst):
+        return lst[::-1]
