deb0915c737cf69adf3f95729d9e170591ef6962
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:37 +0000

    sell able butter please hit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
