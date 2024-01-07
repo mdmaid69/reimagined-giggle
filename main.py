537044d7a31ae81e897ab02447b476e0ef92718f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:25 +0000

    air piece scrap slight maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
