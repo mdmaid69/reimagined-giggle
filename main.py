b483c2f652d239ae62f0027cb4a4f8bfcc0faad6
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:32 +0000

    occur pen high dry pretty

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
