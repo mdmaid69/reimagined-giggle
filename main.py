bc4b2a40ee9cb60103e122e9f58360daf14d17e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:59 +0000

    wash ours roof expensive turnon

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_repr(array):
+        return repr(array)
