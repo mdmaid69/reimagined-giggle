9ed9a2a69a7cb82999ff4311bdf453d85a56de57
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:21 +0000

    always swimming will dance interesting

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def subtract_numbers(x, y):
-        return x - y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
