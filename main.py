eceffdbf0dd99bbcaf1c425a802ed8e6b8486766
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:21 +0000

    previous grade getoff spot name

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_elements(lst):
+        return len(lst)
