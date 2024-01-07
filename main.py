0d4823d9bbc433b8968cb82750ca7491179b5fbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:00 +0000

    extraordinary crazy loan mine onto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
