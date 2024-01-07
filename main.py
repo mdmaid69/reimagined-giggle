a71ae5355746ceecb6d25a357f26d2c4dbb0d5c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:22:04 +0000

    type geton drop not weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
