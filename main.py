ab48ae40fe2b3a131278553538c20675b017f529
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:28:54 +0000

    quiet familiar freeze pay confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
