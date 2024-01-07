18af56bde03338314951d4a2d95b3a2c8748c777
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:20 +0000

    wipe me nonsense expect foot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def square_number(x):
+        return x**2
