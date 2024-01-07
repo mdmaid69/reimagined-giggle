d88c23d24d5dc2e3ee53b1525b94c33a26b996a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:41:17 +0000

    petrol breath target shoot pile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
