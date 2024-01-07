1211ec486cfac8d15aa00d67da1cb69bd418b342
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:50:45 +0000

    reply dear available play sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def square_number(x):
+        return x**2
