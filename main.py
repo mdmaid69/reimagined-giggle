4805cc4a44cb4582f854847ae7c7d9e9296100a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:45:01 +0000

    tight total smell respect fold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
