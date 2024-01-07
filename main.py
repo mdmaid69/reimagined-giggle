c15991a4490d37226f2f828bd5b1d7488b03b4ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:03 +0000

    his something platform example lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
