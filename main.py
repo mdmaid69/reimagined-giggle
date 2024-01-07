c83186b05a36d1c09274bcb5c7caaf7e85c5f267
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:25:08 +0000

    everything successful mad approval test

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
