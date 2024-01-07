f8936b3b58a1b5d1e50f509c5141c0509a6191d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:33 +0000

    achieve tall afterwards visit duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
+        return oct(n)
