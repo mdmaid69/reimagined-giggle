0d4b322ebeec400af905de07d54c926ad1674793
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:49:23 +0000

    somehow tough design truth unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+print("Powers of 2:", [2**x for x in range(n)])
