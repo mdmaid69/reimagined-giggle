7e7f12a664ff2f808dc324f6656d3e40eb0d4e6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:11 +0000

    our recent float eat chance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
