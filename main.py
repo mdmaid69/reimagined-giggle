5f33dbae1e470d4d12715c6192d10e389dc2d47d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:48:49 +0000

    allow large 571enough guide pleasant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+x = 10
+y = 20
+print("Sum:", x + y)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
