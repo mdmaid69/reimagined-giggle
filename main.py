19af6c8bd74f95e9ec040345d55b0d941129292b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:09 +0000

    drop card cap dick delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_error_function(x):
+        return math.erf(x)
