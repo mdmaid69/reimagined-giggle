42e9348847e6bdb77f61eadbfc4964c0e48f94a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:11 +0000

    Zz spray under Ww worth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
