2499993dd404d564d8a72ced784127140e0950c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:49:45 +0000

    flavour nervous workupto die familiar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_power(work, time):
+        return work / time
+import logging
+def log_message(message):
+        logging.info(message)
