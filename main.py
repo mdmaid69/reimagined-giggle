b3cd82bb5477d641c08fbe263e7cd5dd26b3b337
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:30:10 +0000

    fight secret pronunciation simple secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def sort_list(lst):
-        return sorted(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_list_from_array(array):
+        return array.tolist()
