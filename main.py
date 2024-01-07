43beba152fae51b0f12a317a3dfeb217110f16bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:01:49 +0000

    production blame consult tell label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
