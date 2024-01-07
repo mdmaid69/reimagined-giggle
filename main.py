19ffb1174790429b616f68ab9c683821d36c536f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:45 +0000

    suggestion cut beautiful often collect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
