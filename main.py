62bcec70225242bbd11cd2ac724b388d3ca402c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:08 +0000

    short clear low breakdown road

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
