6a331a7968d1c40c7b1d4921ebac45e57af840d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:11:47 +0000

    new rob action principle listenfor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
