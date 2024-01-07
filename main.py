2b054bf904b2bfdc38a522c5328c07adcbf515c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:45:08 +0000

    pretty aim hear disturb his

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def find_max(numbers):
+        return max(numbers)
