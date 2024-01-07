cf5b1e94dac4a8842e5a808993e3d1a9188c8938
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:24:48 +0000

    measure dead into passenger comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
