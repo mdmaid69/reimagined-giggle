c89ceb58fa398efc9949edb573599f3c2130a567
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:25 +0000

    basis bring style show health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
