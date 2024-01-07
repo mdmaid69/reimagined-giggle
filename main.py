be70d575bace1e42b554c87f7c5d278ddeb8637d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:43:31 +0000

    star like fear getoff before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
