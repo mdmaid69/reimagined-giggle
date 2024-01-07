798d833d2c303431a511cb47c9d4f6a6a058e213
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:14:10 +0000

    entrance little success shelter runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
