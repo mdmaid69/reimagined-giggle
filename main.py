1dd0f498fa1a5c1408a8a1f975af36d59fd8a844
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:09:45 +0000

    work able brush character kill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
