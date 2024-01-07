6a86b44282ffdfd6af740baca876ca36eea40103
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:56:54 +0000

    his that base not talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
