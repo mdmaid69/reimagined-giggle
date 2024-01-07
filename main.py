ff9c9560f14d017c68d3ae1c072eccde01b6ef14
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:28:45 +0000

    shine breakdown description roof scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_exponential(x):
+        return math.exp(x)
