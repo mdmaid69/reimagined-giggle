afd3b80f9a7f9f9996e511a2a57fd76763ae77cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:01 +0000

    female adjective dance whole decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
