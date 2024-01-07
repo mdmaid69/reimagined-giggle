6cd29216d874fb6a28e3a432e3586eeaf8c80e33
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:02:27 +0000

    organize wet kick uponon whom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
