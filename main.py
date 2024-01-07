4e8aa3ba9f2ec1c084b73699af0ca898aec9be26
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:45:38 +0000

    trip crime deliberate Uu food

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
