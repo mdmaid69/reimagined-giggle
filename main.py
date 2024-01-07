3855773e60a0ec88778c469b1fe6087e31be5560
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:40 +0000

    help attach touch black share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_item(array, i):
+        return array[i]
