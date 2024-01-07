8317ef87d07acb2f1f5fd039aaee0b64a2cf95d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:39:23 +0000

    takeout listenfor risk fold interview

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
