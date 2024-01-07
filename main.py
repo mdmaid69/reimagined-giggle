e329c66309f429d742d1657723db28f23504fbdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:57 +0000

    must wind clue complaint within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
