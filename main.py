d0c1e79910115efb4078eab884489dae4f694349
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:20:11 +0000

    discussion some replace pin build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
