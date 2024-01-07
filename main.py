b547ce5e9a785c77bfb035b8d45c647ff463d91c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:34 +0000

    hide relationship least workout backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import tensorflow as tf
+print(tf.__version__)
