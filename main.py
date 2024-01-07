38fb77f6b8d55147c9a2b6e9a4aaad65b4718ee7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:18 +0000

    press centre quit quiet suit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
