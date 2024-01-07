5860e3f17f15a597af08b0774da97401f452ef45
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:17:46 +0000

    acceptable choose open do concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
