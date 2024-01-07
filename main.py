306e3dac9015300e2d78c489767e8c9940eb3d20
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:36:53 +0000

    anxious burn later pipe weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
