123e970911a92231a02e031f78af75752ef67c07
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:42:38 +0000

    bunch other profit long workupto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+for i in range(10): print(i)
+import array
+def get_array_as_set(array):
+        return set(array)
