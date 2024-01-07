4ca8c06147c3a047c3b82445fd664c716551648e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:22 +0000

    criticism exhibition steam here compare

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
