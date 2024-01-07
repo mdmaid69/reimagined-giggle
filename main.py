2c8929d2d7f0044027508b56437b2ad2ff8ce9aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:24 +0000

    roof purpose wide square doubt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
