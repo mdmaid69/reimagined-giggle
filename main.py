0ca6f089d71b758534ea0a28afe3dc9a784d9ae6
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:44 +0000

    usually pay charge know giveout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def count_elements(lst):
+        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
