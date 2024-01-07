b86ae3112c721729d5dd3e3faf68af82fdfe6b24
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:46 +0000

    card hard sure confuse separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
