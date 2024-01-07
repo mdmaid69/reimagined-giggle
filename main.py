0932ccb2f54e452e010cd6cbd0963625908ac94f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:32:23 +0000

    earth trouble approve high drive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_force(mass, acceleration):
+        return mass * acceleration
