2cdfc929db1da126cbe7191acc7e0e6737bb5eb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:28 +0000

    recommend book empty makeup beneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def square_number(x):
-        return x**2
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
