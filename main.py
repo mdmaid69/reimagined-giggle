ba7d69fa1d4eaa76aa0f59b941828c0f5f888278
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:08:42 +0000

    reduce pressure care centimetre block

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
