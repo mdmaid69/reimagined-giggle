fa96862eb4f11f85c9f62c12302fd760de4f78d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:57 +0000

    makeinto age do fall I

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def multiply_numbers(x, y):
-        return x * y
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
