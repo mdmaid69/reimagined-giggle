522dc8b2ff82b461b6110c22187d5676c2b5a405
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:58 +0000

    stick question his will ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def is_even(n):
-        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
