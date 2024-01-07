e31318bdf3c4b1fcc03e865a78c4641b6e1289f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:31:04 +0000

    lift result finish normal about

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
