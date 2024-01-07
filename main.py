d15a34754f619a3f9b5f19e3b3b47651f1eb77cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:17 +0000

    funny bury close accidentally getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
