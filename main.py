761f7461e4c9763901c2b871caccde8d785126ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:00:59 +0000

    Ll style blame problem goup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import time
+print(time.time())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
