f45c6b5978fca97c5e7ecbe0a058605d42c348a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:48:48 +0000

    takeoff mess taste his touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
