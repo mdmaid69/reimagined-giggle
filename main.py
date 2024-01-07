e90bf50f2126d3b4f6de2cfa65f06d59f302362d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:29 +0000

    almost excuse approval send ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
