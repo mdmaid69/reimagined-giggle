7496ef6e66d14daa86b2c7ff4f4febf01b26ce3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:31 +0000

    Ll determined sea level trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
