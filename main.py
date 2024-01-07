c590d6205a8759183837b41247ea0339339316eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:23 +0000

    develop form flat raise tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
