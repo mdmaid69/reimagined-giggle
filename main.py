4a5c896ea8e7cbe40e1c7d4685d3bddb2f2c353d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:58 +0000

    lock home address dry heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
