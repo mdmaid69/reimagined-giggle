cf9e3e33f58dae218d71379cc53c26436f441681
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:12 +0000

    within bound clean good 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
