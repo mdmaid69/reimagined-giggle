2b19d3765778437a25ae2c57a99be698e8642dff
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:40 +0000

    collect to complain vote bin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
