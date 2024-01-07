c16bc85de88ea62dcb4a8edd6ab5cd94a7eff42c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:12 +0000

    black outside image pot workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
