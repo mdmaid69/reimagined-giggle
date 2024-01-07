0e3b06374cabbf0c7411517f8b17d22393753ad0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:03:17 +0000

    influence plan past admit distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
