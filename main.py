7d2d37a9694504065e99a6787a72c5af0cdd167e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:02 +0000

    introduce worth last married object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def is_even(n):
+        return n % 2 == 0
