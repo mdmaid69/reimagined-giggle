142046619b36af70828a085a3058ff5e2190d8d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:37 +0000

    cut temper communicate except defeat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
