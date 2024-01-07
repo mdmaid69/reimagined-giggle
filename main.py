eadda54309f1ce71a527741c3ff668d34fd2b7c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:33 +0000

    flood fear away part deep

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
