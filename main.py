edf9ded276df6f0c13dacec3e85ef660a6b4358f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:29:54 +0000

    build force authority polite round

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
