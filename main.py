be109de935923e93f230ab4adeec9ce10fd76e0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:05 +0000

    love calmly state turnon print

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
