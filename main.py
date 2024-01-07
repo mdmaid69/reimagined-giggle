dd0113907f155f8e6b3d02fa79a7f9180f896b1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:22:02 +0000

    heavy welcome debt empty town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
