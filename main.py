c45243e89222b783ff8e58516bb659429d2cc6bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:41 +0000

    aim apart basically ourselves deliver

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def reverse_list(lst):
+        return lst[::-1]
