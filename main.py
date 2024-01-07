41219efd5f7f427978683e048e908fd9485e92dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:40 +0000

    advantage towards unfortunate poison nose

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-print(sum(range(10)))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
