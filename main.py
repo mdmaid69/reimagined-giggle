d91a85e7d4ea4098cc369bc0db3f3f94283a7a2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:40:41 +0000

    kick talk no gooff because

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
