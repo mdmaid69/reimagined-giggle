33e45e42441308396594616d6f629043a9c3fd06
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:56:03 +0000

    description listenfor open reply Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
