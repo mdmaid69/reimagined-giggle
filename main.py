0f89c2b39542fca62c6bd9d420a3828d597781bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:58:46 +0000

    tidy after hello official best

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_area_rectangle(l, w):
+        return l * w
