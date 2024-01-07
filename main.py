14b6dc75ab96fe0ebb92f56d8db2084433f0ba69
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:43 +0000

    black load sink your straight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
