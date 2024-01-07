3a5dc24dc1146b1d2cf15b338568c86f87e18f2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:40:43 +0000

    turnon key during stupid impossible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
