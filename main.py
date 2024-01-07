38c77ce9399ef8a87af24166d8aa4eacdfa92bfb
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:48 +0000

    attractive taste tap later forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def add_numbers(a, b):
-        return a + b
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
