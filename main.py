4661076d1f8e55aea00d38085230a0bd69f93171
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:45 +0000

    activity blue now about shout

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
