ce7c86004ff99b31ded67ac5c41094b65a5c9d1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:45:21 +0000

    pour lose it fight wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
