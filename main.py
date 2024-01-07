92eb2eafab22d9161467fd8076e86c2a7943d83b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:12 +0000

    goon runover peaceful forever exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
