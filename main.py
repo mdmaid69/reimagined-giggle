3fc79106a4967d85342373d11016351404c1d416
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:01 +0000

    relative workon report today love

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
+for i in range(10): print(i)
