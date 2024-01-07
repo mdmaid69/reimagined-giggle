8b8d76e257107611c26b2e957e30ecd43e1545d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:23 +0000

    wall separate comment instrument lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
