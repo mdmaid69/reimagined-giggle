fea469fa199c49170da8f055db56e1cbe354dbd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:24 +0000

    scared eat equal different mouth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
