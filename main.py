d4fba0e2a6c78c44c2d21cbdd9cfe3d4fb6eb71f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:33 +0000

    detail freedom sweep certain introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
