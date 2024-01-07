183cc3db8bf9be53ec0578be25899e4b38007b38
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:01 +0000

    lay yourself something plain silent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
