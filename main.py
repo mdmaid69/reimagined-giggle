0df19648558276969939a9d0de37cb29ac55e23a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:57 +0000

    slightly talk strength silent bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
