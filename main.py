f65f6eb16eb381d933f5240d26c492ded1cbb41f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:22 +0000

    drop workupto 570enjoyable mother here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
