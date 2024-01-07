e1b99181c5f047dc5dabaa3f9bb9c33bab9cab75
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:17:38 +0000

    figure partner usual arrange arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_repr(array):
+        return repr(array)
