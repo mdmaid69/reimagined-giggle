7800625ed5703534bfb96e0bf851aebeb08b4759
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:52:21 +0000

    goback(to) destroy now next basis

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
