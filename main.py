14bd39d1c7d2979c3fab10deb526b6c408fb920f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:41 +0000

    proper thorough least direction official

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-print(time.time())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
