70c420ec42fadd7d335ebaebae4c791e46ef6d0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:11 +0000

    sit us extraordinary runinto receive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
