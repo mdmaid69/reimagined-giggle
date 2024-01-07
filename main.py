131d5ae49b275783dffadd3b01f2666547f41999
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:05:05 +0000

    wander whatever combination medium balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
