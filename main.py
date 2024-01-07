d2c62f532d630d35267518a46d91c5b14a96def4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:06:43 +0000

    operate remain sick message loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
