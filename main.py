5a404564f80d021218326a82bc3eb68ebf602a70
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:13:15 +0000

    agree beneath split drive city

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def split_path(path):
+        return os.path.split(path)
