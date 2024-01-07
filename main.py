967b6004c3f8665b56f05d8bd819fdbc4f61169a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:52 +0000

    runinto relation chase somewhere doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
