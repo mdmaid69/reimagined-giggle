d878bdb4b48b3a6cfbb7c635e0ab38ef8af0766d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:21 +0000

    exciting position delight want down

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_max(lst):
+        return max(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
