7e29f5dbb6094a0bd102194d519e52b87f14e553
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:50:59 +0000

    form there that bye woman

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def clear_array(array):
+        array *= 0
