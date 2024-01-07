298a7180ae48fb21f66c9170008b85e59908c13d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:51 +0000

    watch hot push fear teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+print(os.getcwd())
