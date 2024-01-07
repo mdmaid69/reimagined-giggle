99f6324175f23c213c56b360329e4a36dce46cf4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:17 +0000

    storm trousers natural offend complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_words(sentence):
+        return len(sentence.split())
