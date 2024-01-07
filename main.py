1419081a2b596ae277911af2984288dc9e228641
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:35:25 +0000

    bad afford can ear building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
