9625207cfb38dff8976d8881a383b0f7b02b4845
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:24 +0000

    call always geton rain different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
