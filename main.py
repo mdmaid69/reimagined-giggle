c0394fd91a5f91d603c56b57c61fa28bbdf439ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:05:54 +0000

    heart come back test method

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def reverse_list(lst):
+        return lst[::-1]
