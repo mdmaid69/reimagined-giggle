db3de9434343162c2604aa5e841198751b7085ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:46 +0000

    manner quarter tonight offend muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
