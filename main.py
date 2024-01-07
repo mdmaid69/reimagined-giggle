bcac1e619d677c638c1afefd2c19dfebc8db41e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:33 +0000

    rule circumstance amount break realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
