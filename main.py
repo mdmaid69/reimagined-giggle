4ff5f833a62ddcf640035bcd14b016cc167b01ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:59 +0000

    your amount politics share sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Hello, world!")
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
