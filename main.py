f89d8a382cd0f5ad81ba80abcdc45ff703fd2667
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:15 +0000

    rest comefrom slowly spoil calm

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
