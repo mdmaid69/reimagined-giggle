bda9ede60d2c99bedc1c2bb7907e72c24480d044
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:55 +0000

    fortunate sleep connect why colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
