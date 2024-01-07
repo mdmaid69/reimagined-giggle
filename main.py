5f45ab578279d7e4b74260c4ab356c19d0620913
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:05 +0000

    travel market tell protect familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
