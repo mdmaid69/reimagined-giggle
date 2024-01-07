992d01b9be59e17a6b4a46813b344a7b8d545ba0
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:24:21 +0000

    drink keepoff check no tired

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
