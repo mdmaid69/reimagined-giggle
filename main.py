cda4ca2ab58c023e83ac28230a089e07521ff74f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:48 +0000

    yours lunch favourite point teach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
