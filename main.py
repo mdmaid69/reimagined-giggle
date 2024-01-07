ff09855fc5f648198f4c9cb93a7d3c5426d7fc33
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:07 +0000

    body hers discuss complete flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
