a3d8f4c8890ad7aeb9a0fe10398fe733f307e766
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:47 +0000

    next beside bottom stir protest

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def is_odd(n):
+        return n % 2 != 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
