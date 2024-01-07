50502b0214ccf14620839646a5ee223d5e89692f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:30 +0000

    exact shout tight subject pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
