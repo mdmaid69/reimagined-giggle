fe331622d254a917240663fa4143d52c7cfa1e4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:10:24 +0000

    permission murder belief comeround 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_min(numbers):
+        return min(numbers)
+import random
+print(random.randint(0, 100))
