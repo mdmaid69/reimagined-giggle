b811954f4d6128c91c980b10453a8ac5ed1e5e21
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:01:43 +0000

    sign obvious basis memory talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
