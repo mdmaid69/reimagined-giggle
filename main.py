89efc57b3396cc12fdb4d28cc1628ea89a094e65
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:33 +0000

    return post wish shopping Oo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_max(lst):
-        return max(lst)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
