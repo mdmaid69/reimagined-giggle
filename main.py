c52dcf1658d4f600e0a69b552cd3c4cd1961b758
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:28 +0000

    while accidentally word comeout memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
