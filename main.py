59ea3b840a362beceaaae53d62e6e485f550d7f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:40 +0000

    care suggestion whoever switch check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
