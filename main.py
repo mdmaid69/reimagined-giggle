ecd6a050f67b0caadf71305097bce76ea69a0a98
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:54:28 +0000

    mind either remove target out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
