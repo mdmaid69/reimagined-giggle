1ca1347efdf1ab507341622747fc63f418ab75da
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:05:19 +0000

    passenger free carry bear care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
