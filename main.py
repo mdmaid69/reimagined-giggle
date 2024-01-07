3d64988706916da136296682841c2ca74b5b4602
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:57:21 +0000

    sad lean worry advise national

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_density(mass, volume):
-        return mass / volume
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
