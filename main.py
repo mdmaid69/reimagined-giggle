47a91a10c9688a9b62ba314a3e06e8b1e74e04ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:29 +0000

    invitation actually path scrap immediate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-def find_max(numbers):
-        return max(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def convert_to_octal(n):
+        return oct(n)
