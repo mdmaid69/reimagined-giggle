e79b795afda843b8ed475710f075ca4eb75fb9a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:03 +0000

    pile goup dry exit view

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
