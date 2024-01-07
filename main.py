ce6065ca6032713198c173d11cc30a3293d82538
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:26 +0000

    seat story dick low favour

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
