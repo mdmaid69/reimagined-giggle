5d121dea53e56dacae73917649bb355e54727ecd
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:45 +0000

    trousers dead substance 569enjoy waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_pressure(force, area):
+        return force / area
