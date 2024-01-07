14ea995cfc116acedb5085420bbafd0409874721
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:41 +0000

    artificial wing sting involve tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
