f3670eb47d0ac05d46b32702c0d2acb03351c45b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:42:32 +0000

    keepfrom disagree wash protect show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+print(os.getcwd())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
