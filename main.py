733bf751a831116f60af993e2b8387d37e0b593d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:02 +0000

    comfortable table worried near tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
