aa6b43a21fc7d32061c15e7f9bf23c03fd2db3dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:05 +0000

    state demand damage rain desperate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
