e8816087f58a84a04551295f41e10b488e95bc38
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:28 +0000

    Yy light step uponon count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
