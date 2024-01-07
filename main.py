d96b520e986d61c771657c0cef2be367bd4671b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:11 +0000

    not collect there body grow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
