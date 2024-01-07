7d17f0c39e8530b190c1280231716580572a93aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:46:47 +0000

    might little married unusual gooff

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
