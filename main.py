d0ca5b3d76b3e779c1d91136d4835124482303f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:08:55 +0000

    compare wrong borrow progress excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import datetime
-print(datetime.datetime.now())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
