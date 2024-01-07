44abb20f5930ae128512fc672cc5c33b9f3418aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:00:23 +0000

    speech in let organize information

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+text = "Hello, world!"
+print("Reversed:", text[::-1])
