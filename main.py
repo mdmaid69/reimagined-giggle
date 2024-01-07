da8f4b57a550fe8f8ae7a012b03743cb674cab8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:03 +0000

    circle splash top knock embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
