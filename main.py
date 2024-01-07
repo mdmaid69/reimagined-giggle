a7322dfb1832d392f4a2450aa2df253d27296e81
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:16 +0000

    hungry national machine be expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
