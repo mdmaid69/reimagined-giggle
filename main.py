ec133fe96404f7feea89980f80e3fdbba0cdab1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:36:26 +0000

    operation period worry bet quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
