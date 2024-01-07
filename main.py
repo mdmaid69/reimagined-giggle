6d83ddb77fc1a84bced26b3f514dff8cb32c403d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:41:48 +0000

    exchange rather beginning hurry grade

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
