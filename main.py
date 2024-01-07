e5bb1e5e1365d6d8deb43f1a35c99c065290142b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:35 +0000

    uncertain notice believe suggest the

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
