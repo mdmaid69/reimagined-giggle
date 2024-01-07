52b7831662e07ea9b80cac0814a7285829e09e52
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:41 +0000

    complain language branch light proud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def square_number(x):
+        return x**2
