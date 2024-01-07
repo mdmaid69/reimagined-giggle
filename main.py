fe5ee0daf944104a2979ee50a817247e584b4546
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:20:49 +0000

    later grand family talkback while

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+print(sum(range(10)))
+def count_characters(sentence):
+        return len(sentence)
