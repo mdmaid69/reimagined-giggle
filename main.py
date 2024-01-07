22a1ea72e4fc2df8e94519153daa3f9e9c54af7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:27 +0000

    bill that change personal closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
