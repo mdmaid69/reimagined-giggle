6e35e6d51a116cd04a077d3b16e5fe8a93089d1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:40:19 +0000

    ago hair obtain world lean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+for i in range(10): print(i)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_as_bool(array):
+        return bool(array)
