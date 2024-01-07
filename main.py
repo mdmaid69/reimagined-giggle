963f65c20ce4672c16c725fb43aa0bccf0b299f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:47:58 +0000

    stone left character pipe motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def add_numbers(x, y):
+        return x + y
