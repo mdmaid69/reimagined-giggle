67a4c792076d65cf1624fb7ff91a7fa4ed684d57
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:56 +0000

    normal laugh number dead climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
