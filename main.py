f4d4fb724ca978e12e4fa3675aa8f1025686199c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:00 +0000

    himself programme equal dance keepoff

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
