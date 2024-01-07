b977a3aa5a04a69925b75f3baa10544cb3d7e647
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:00 +0000

    please steal measure expensive guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
