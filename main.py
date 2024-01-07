9c4ddd06f9072ad63ef334b724eac27616abb830
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:59:55 +0000

    forever right godown news age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
