bcafcc50cd4076a76eafec46cc4f11e9967e2d4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:11 +0000

    flow worth treat amount wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
