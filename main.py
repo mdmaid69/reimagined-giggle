c67a9becbeb66027e6c54224a730bba3852627bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:21 +0000

    win block within plan face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
