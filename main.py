f0100f3e041d92451ac5de27bc5e2322bee923dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:48 +0000

    battery attitude squash beneath burst

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
