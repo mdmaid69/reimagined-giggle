9ebb78ac869433275208a64365edf96e057507ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:36:12 +0000

    within platform seriously thanks undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-name = "Python"
-print("Hello,", name)
+def add_numbers(a, b):
+        return a + b
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
