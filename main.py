8f0dec16f2bfc088b34ac3238a0cdc70762aaee1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:16 +0000

    degree ourselves nervous performance support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
