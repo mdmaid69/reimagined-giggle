db6dc33c1bc15cca4b8e453078ceeaf3ec0e3946
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:59 +0000

    disappear rate after name shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
