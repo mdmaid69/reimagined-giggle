f7f26c653c48bcd74dfab1d94584a3ab353ed5d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:00:01 +0000

    keep possible interview way story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
