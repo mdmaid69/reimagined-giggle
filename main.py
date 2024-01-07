de62f13f50f4aa0c032c2735d7b64b046e6259be
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:47:41 +0000

    plan break separate upstairs nose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def convert_to_octal(n):
+        return oct(n)
