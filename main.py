a6efb0eda9ce05d11564c6d88dcc0090ee6be65d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:10 +0000

    importance angry influence thankyou still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def find_min(lst):
+        return min(lst)
