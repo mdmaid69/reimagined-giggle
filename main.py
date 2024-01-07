26d76ba7c8657fa7cd8fbf9f7f6f2154571deba1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:49:29 +0000

    tour compare strange able left

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
