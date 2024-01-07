09420e8d91e179f06286411b0691485d4ee61c31
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:41:50 +0000

    bottom hard climb belong badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
