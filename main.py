3048307a3a0caffc41c7b9e2b2985c927fbb81c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:19 +0000

    below meaning round occur thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
