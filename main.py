08febde8611dd8084966d41ebdd72194fb6faff6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:48 +0000

    thinkover other fasten accidentally getoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_string(s):
+        return s[::-1]
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
