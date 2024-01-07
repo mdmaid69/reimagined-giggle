3c57ab172b535699f1690ceaa7e643a454c5b7cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:49 +0000

    intelligent telephone nose runout past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
+        return oct(n)
