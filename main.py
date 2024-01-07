6b8fff2a20d45e99db920ac7f767ed1d9a70ac88
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:51:36 +0000

    serious autumn operate discuss uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
