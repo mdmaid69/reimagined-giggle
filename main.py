5b2ab672bea5147a530c901314278cba6d502da5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:41:12 +0000

    level rent complaint thinkover operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
