439ea8ef7c2d5065481014dcf388eb1c2e89bddc
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:36:02 +0000

    Itself move above city lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import numpy as np
+print(np.array([1, 2, 3]))
