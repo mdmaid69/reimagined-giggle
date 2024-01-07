f5cdc62725588879911b0c7d47fb5ce02dd433cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:37 +0000

    practice achieve buy female Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
