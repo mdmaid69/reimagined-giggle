24d7b982117b4c7ceb597a645ec89c7b513de9e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:46:45 +0000

    past face Cc kick about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
