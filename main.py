c57fa3c1f3f4a1c726c13fd133e6baaefede339d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:35:14 +0000

    move deserve glad bet pump

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+print(time.time())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
