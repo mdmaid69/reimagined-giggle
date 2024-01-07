8f68cd1f82e12093149121aca7c524926ff1d323
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:28 +0000

    holdout trust defend some touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import sys
-  def get_python_version():
-        return sys.version
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
