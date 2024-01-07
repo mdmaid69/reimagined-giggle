e7e43c70286f48fe66ca2c279ccc57e3bd406698
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:01 +0000

    they callback it giveaway cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
