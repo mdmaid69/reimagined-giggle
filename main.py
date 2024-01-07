eb1843904c3c15ba0b7df26eb76289e560de1b65
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:13:02 +0000

    boil picture squash politics broken

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
