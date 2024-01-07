61c4faaa8f962db63fe0e7395311dac8bd7cc6c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:48 +0000

    runout borrow getback between Zz

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
