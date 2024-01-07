a1bd573b0eb5a5935066e8ea9f322344a5c974d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:44 +0000

    new interview connect rest train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
