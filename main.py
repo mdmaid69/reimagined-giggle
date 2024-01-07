2272be5dfe398dda03311e201be7a1e292767f5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:45:19 +0000

    stand base make live mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
