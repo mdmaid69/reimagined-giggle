6d745cc58217f53a03f05e6d55ca973dad793207
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:01 +0000

    tax hole soft introduction giveout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
