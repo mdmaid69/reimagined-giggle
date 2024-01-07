3cf184d77495570ad8d712144520a6cbbdfc5509
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:45:07 +0000

    570enjoyable likely provide direct home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+for i in range(5):
+        print(i)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
