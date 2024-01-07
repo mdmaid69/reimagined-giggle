66f60f11b23a52381f29ae1c19a18863fe698137
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:34:56 +0000

    permission though circumstance whisper possible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
