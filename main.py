ffdc15225389e68917a372d0992223c9b829d019
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:39 +0000

    closely callback play complaint tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
