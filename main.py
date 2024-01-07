6538a6a9ec2f1225faed9d24ef433febedc505c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:55 +0000

    except practise greeting one violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
