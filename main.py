2fb8e618ceb041e8752eadb89707406cce884cc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:26 +0000

    inside cause sport keepup yesterday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
