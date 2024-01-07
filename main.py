e4ed85c5d7a478795a0a9966fab31af94ad1ced8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:22 +0000

    once fail easy birthday stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_str(array):
-        return str(array)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
