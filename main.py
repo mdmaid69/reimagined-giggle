470a2b19c4ba408f7b9818353177e7681f425ddf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:05 +0000

    turnon refer show detail Gg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
