1ee55bf7715d37f5d647cb8f2eb6494448c2698b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:08:03 +0000

    near train cut underneath each

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def is_odd(n):
+        return n % 2 != 0
