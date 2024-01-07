02f2890970a68f61479fe98cd040845ba42f24fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:03 +0000

    step seem just price another

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import tensorflow as tf
-print(tf.__version__)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
