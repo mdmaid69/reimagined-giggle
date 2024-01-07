92f3879183d0fcc25ace32717642dd7a570e8730
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:56:07 +0000

    flow glass minor visit feeling

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
