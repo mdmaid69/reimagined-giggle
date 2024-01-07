69090d7e43d5ac9550df046d82668fa99a51f445
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:10 +0000

    exploration hit defend lucky fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
