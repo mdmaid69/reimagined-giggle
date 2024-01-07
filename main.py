79e9c5c3d4e7c4ed17c8a54f0b0541f11768f7b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:07 +0000

    find order debt pattern factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
