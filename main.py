ee8563943cb05f272b6294a3997c93149f7e90e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:33 +0000

    criticize judge professional until badly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
