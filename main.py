ee9ff5952d12bbe28e7e2e253cd02e4c20a510a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:54 +0000

    accommodation long eye minimum apart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def is_even(n):
+        return n % 2 == 0
