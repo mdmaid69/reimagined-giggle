99e998fbc6577548a3da9e376cb23a7914f3a14c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:56:45 +0000

    problem bath cook Gg sensible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def reverse_array(array):
-        array.reverse()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
