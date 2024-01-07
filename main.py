44f1514d0bb6d839fc43640933461ad6f84e922c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:26:16 +0000

    possibly measure bury until embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-text = "Hello, world!"
-print("Characters:", len(text))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
