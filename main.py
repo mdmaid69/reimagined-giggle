6c1c93c19644ca48286ada166275a4daeaedc085
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:26:55 +0000

    agree scream correction Ff appearance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
