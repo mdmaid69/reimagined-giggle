482c662092922df4fc4a479ae4e4b6a979cc7967
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:54 +0000

    thinkover distance job advice pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
