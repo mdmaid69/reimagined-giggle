5146b5510b5e09634b74f1a1b069c5378c6d241d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:46 +0000

    wheel always hollow match pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
