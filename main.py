fd59e175636cf9b1e07a19459e4670fdffe3b207
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:47 +0000

    shopping every valuable embarrassing spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
