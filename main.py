dd08508f5579ac32aaf75084e2c88a9114569cec
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:52:20 +0000

    frequent prevent curious means wander

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
