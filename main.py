a9a9a5b5e205ac49a80f1573bc875ecf5abd6760
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:43 +0000

    poor birthday future pass nervous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
