1949f06298ba038fd6f1fc5bd8ad4ac13d8b81a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:03:16 +0000

    container success in often different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
