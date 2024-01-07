5a28094e865cdef613ddaa0d253d74598dc02ed1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:48 +0000

    discovery grow tragedy far key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
