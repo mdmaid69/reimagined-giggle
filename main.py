094c1c51282bdfa8107f9e5a6a3788540d8d3cc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:17:58 +0000

    news exploration themselves however she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
