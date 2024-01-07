87eac4a0db75e961742aa1815d850d3df0f96b12
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:42 +0000

    sweet introduction slide level closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
