feaece62c9127ea505292771f66b005c89e4d9b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:38 +0000

    breakinto blame sweet draw string

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
