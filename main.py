19da33e35c1216da4f524d06cea6d12fb1f89039
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:13:00 +0000

    tip substance sell family noisy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_work(force, distance):
-        return force * distance
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
