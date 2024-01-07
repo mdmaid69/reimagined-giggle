6e53e204945ec41a16ac8cff95e75f05e5e85e3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:46:18 +0000

    soon tour pleased Pp that

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_area_circle(r):
+        return 3.14 * r**2
