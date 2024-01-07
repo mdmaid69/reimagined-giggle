de9af0ff31822894f850e22ca3686079dedfdda5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:42 +0000

    carry punishment study introduce strike

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
