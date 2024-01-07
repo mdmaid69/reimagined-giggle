0ed591355805a99812b7949981db443422422b20
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:38:09 +0000

    giveout suit here loud cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+def exit_program():
+        sys.exit()
