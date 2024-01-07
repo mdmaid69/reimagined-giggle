0ba5ad5cba867ded1ce672f4928be8c7426a03e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:54:48 +0000

    keen breathe close comeround ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
