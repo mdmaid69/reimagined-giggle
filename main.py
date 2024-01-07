55c669ee72cbbec0ec483be11906629689f65818
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:21:35 +0000

    neither unusual run Yy giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
