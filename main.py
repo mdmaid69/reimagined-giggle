59f39ddd796b4f4d5f6f9913b5a11bc6b9753901
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:51 +0000

    squeeze explanation uncertain ordinary nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
