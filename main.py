967b14c462a320ee753356befb746694aa8cc075
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:00 +0000

    doctor back succeed around operate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
