c8eb2ed7777abc6279b04f66cddf74990179f70a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:45 +0000

    swimming throughout better who both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
