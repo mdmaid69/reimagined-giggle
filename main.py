09f91181951469958adcf476b7c17b26c5a6de20
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:52 +0000

    line foreign contrast stiff talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
