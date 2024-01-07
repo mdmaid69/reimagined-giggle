c4ecbab1e80031a4e23058dab5eff23426ee50e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:34 +0000

    cut enthusiastic treat modern family

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
