a3f9f7d0e82c5d585649bcdabf409a7df09083e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:27 +0000

    Ll makeup sister peace scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
