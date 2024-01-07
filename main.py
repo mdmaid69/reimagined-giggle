cbe85898b0696c2ecc2f30b143ddb6a79d04cb34
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:31 +0000

    into heavy explain debt form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
