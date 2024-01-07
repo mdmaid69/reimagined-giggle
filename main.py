99bb2098bcae80ae3c98aac09e5a589946f1ad3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:11:01 +0000

    ever statement cold Kk pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def clear_array(array):
+        array *= 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
