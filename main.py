4eb9576be2bc710a95b2e1163c737c0686a93344
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:31:59 +0000

    mean cause who contribute might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
