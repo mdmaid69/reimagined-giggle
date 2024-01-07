08525f5ba50350c594022c06461d412324263600
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:06 +0000

    unlikely question look Nn sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
