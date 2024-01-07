4ba701879ad10e3bc2d0c6f92e3a050781e39b22
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:05:47 +0000

    consider port remark order yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_area(radius):
+        return 3.14 * radius * radius
