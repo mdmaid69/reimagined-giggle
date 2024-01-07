e3673ec754f3610c8026be20261728fd54b4d41c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:47:29 +0000

    central know impressed base sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
