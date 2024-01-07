5f9e579dd8ae11a9098ddf9963e92bb1709c7166
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:06 +0000

    brain over spring Bb note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def cube_number(x):
+        return x**3
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
