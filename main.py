fdcc4d8a41aa9f00e1f333d366d5a7f1e91908b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:49:50 +0000

    damage question 572enter cross make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
