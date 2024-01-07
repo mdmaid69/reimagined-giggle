89cbc3df8b0f70c69f360194a10c66aabacc244f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:24:42 +0000

    circumstance hard different precise couple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def subtract_numbers(x, y):
+        return x - y
