7ec647fa61421d9796c1c8bf05ac10423f340d1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:28 +0000

    zero demonstrate trip ride this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
