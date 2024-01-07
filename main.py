e63aff1b4f97b9913e539dce8ad187080cac61b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:44:14 +0000

    apart about thin arm appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import time
-print(time.time())
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
