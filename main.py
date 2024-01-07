ef95fae3f402ca2aa8e5b6f3ac69df8fca4eaafa
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:01:18 +0000

    keepup sometimes stay again underneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
