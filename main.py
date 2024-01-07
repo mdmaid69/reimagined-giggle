ff063e0a3e74ad10c9a34a8fcdb828bb10ef6600
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:40 +0000

    push group show prepare tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
