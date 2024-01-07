d9cfd0c89375d80946f486e666ca97e8dcd930e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:10:22 +0000

    suck shout fix declaration try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
