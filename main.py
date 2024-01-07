e2d2835f2e4ad5853342ee0ac91fb67f0ccca666
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:17 +0000

    far patient attract unfortunate argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
