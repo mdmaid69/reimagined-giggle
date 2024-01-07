567c0410f42685bd93be3641c35c7d48e51f5ae0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:27 +0000

    succeed sell mend at anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import time
-def get_time_since_epoch():
-        return time.time()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
