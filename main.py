ab6205174d95755441ec59c9c81e5cfac19355cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:57:32 +0000

    turnoff command smooth slip talk

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
