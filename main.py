3e797ec844cad5b591dc7737407d71cadf981e15
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:25 +0000

    Yy tie estimate steam amount

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
