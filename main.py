1c0fb9e4da7740691a9a2835d6ab6005124ad37e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:39:40 +0000

    meanwhile clever tidy accidentally tired

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
