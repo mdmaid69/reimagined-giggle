49655987dbe27a5bf86197d62f5fe7458da511db
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:35:38 +0000

    avoid takeback their first makeinto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
