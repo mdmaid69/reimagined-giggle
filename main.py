3838df7141ab468bac07487633f1ca5a20f59ba7
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:24 +0000

    judge broken soil art thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
