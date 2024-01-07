fbc58b70606949e5bfbec440f97c91d97254aa84
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:25:17 +0000

    emphasize platform clever bored along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import datetime
+def get_today_date():
+        return datetime.date.today()
