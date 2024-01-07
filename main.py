4f25016ce055267670546f1b827092dae3d94f02
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:52:36 +0000

    purpose sorry hard grow wonder

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
