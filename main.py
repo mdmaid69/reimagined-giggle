04a337d6f9a5027c3898121e53c31ed8e79957eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:17:23 +0000

    poor really artificial sink bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
