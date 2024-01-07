bc670cc9a8c976020e63374399270ccde48c14d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:15:29 +0000

    away otherwise lesson breathe squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
