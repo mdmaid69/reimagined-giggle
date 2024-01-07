41fb86bd43d563ce59603aa080c299d5d0b801d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:40:16 +0000

    clear possibly wheel low loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
