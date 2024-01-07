bcf86c99a33f0eb35503ad6ccae822a1c5eb848d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:34 +0000

    recover failure normal getoff trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
