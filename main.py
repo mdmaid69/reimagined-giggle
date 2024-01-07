f70aa780cdddf25177f79084ab55dd20606dec87
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:04:34 +0000

    describe organization partner situation joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
