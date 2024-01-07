0209ae59b0e50d65038baeb9acc6f798a40962c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:32 +0000

    dream direct nigga will black

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+print(datetime.datetime.now())
