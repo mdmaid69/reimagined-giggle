18a3302c3af10f420cb3dbb394da65148830abf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:49 +0000

    keepoff description sting pleased anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
