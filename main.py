56798351b56236576678692c42496b9546996402
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:58:57 +0000

    wash Hh off produce keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import time
+print(time.time())
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
