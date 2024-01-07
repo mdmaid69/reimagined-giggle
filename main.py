48a4a63ecc731d47c3378989c211426a93fb5a2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:37 +0000

    coat contact wash machine street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
