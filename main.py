57660ceba8a7ff5dab2a7e090edc922f7d6a9e42
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:34 +0000

    thing store check want bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
