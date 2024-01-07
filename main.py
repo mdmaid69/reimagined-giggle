8f8cfb685b0a50ef7f3c2fdbb0cb9dcbedb4f764
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:00 +0000

    across standard otherwise release justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
