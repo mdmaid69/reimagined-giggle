b496e928fb7de23f87400a4fb75111a25a721fa4
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:34:28 +0000

    belong right design ticket towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 def calculate_force(mass, acceleration):
         return mass * acceleration
