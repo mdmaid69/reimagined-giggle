8adc8e39993b3ef03e42c3019e2f1f357e9a5dd3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:03:29 +0000

    unit only next conduct spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
