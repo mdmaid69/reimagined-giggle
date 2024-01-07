f886e0747e8fb410ce05b62a072ae171581891f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:15 +0000

    hand frequent mouth gas to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
