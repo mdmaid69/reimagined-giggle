6420de49a0fda843f1b1104bb1b94397d4b66b23
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:08 +0000

    violent attempt person snow especially

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_power(work, time):
+        return work / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
