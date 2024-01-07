5fee322616a244ce1d9c433b11594f4e821d54d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:23 +0000

    569enjoy market pressure example Pp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
