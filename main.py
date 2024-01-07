60fbbb240352034efa676e00ca5366b17c3546c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:33 +0000

    phone thing ask share block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
