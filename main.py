5d67094297d73d25c03373fd07167f99751b772b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:03:31 +0000

    low interesting find search but

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
