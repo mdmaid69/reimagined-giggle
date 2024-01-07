18b543e8c283ffdf72c16d9a3b7ad6f122612907
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:37:22 +0000

    whistle promise freedom forgive holdon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
