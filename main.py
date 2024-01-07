edf0c4ee54738b037c7381016c76b2b605d1e4b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:51 +0000

    shall develop recovery claim error

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
