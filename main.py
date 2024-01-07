7222d3a27466601c50c622d213079fa983c13e6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:02 +0000

    display sleep urgent basically manage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
