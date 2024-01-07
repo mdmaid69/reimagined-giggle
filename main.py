07996a57899a3494f789c9fb85dc84be4dac4653
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:17 +0000

    rid thumb how hardly risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
