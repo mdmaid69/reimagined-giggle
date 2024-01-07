e1a96e7faaf85b621cf628af96e8ba7425546f5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:53 +0000

    worst major listenfor miss guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
