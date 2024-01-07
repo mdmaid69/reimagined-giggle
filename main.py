8704dc9f1333c5bbd0fb47eff576673e5281f325
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:11 +0000

    none joke contribute bridge solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-print(random.randint(0, 100))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
