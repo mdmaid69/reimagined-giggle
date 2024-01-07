52496a7a64d749d8e9b39de96b1d6748bb33eb0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:09:47 +0000

    level lesson danger list laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
+  def multiply_numbers(x, y):
+        return x * y
