fc52cc25928cfd698a25ae00582c7c9a0cce4ff2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:19:55 +0000

    freedom already treatment reliable display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
