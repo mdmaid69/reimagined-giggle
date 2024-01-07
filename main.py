af4a9edde8e031e7ce9014f37d361cb0b5c0abef
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:57 +0000

    regret actual use heavy wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
