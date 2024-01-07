42fefab4dcf4cddcfca8f50e4d2e0654b4ee1ee5
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:32:30 +0000

    hungry heavy anywhere concentration goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Characters:", len(text))
