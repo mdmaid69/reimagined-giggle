74010db36805ee767db9e95464a82e77f14f927e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:12 +0000

    appear temperature degree day of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
