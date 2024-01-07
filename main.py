b707ccd37f90b433cfef1789360b8e6393c97b16
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:23:24 +0000

    favour issue happen cruel belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def reverse_array(array):
+        array.reverse()
