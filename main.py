c33bdd8f90934898eb03f2a9901bb46ea227c4ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:32:38 +0000

    criticism belong answer holdup still

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def set_array_item(array, i, item):
+        array[i] = item
