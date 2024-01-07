a5a5c01cf125b4db86c9e3d8851f73c62fe5d98b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:37 +0000

    good string blue far theirs

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
