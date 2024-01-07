3ad98937b81a4ae6a59aa4aa186ae783101429b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:19:21 +0000

    rain care long argue valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
