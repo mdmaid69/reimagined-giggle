1e57e1df9bab7a3083d5d0c22f2c9021c37ef031
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:04:44 +0000

    comeround last buy under brain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
