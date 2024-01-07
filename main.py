83b804d778a5e3ea33ac1e820fac8b28190bf23d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:08 +0000

    degree measure rest approval cruel

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
