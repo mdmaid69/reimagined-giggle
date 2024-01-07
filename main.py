ee2e556f78676d44f6af61d10f8a1d52f17ac44d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:01 +0000

    remain high spread brush satisfied

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
