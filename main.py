f81ff693c31eae52b9db02f7ca481666658fd2ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:19 +0000

    walk noisy work completely convincing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
