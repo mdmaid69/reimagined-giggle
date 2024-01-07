053bab403e1d03964215afe262507fa118eb1b74
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:44:05 +0000

    sing hurt female nurse surprise

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import datetime
+print(datetime.datetime.now())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
