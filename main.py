05acd696bb3eb83689cacada93e49833d01f6773
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:50:00 +0000

    clearly bother strong support eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
