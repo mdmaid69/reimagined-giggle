6ae60b9bab5ca1913f2787d5511eb2b47b8431d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:56 +0000

    single return sad slightly promise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_max(numbers):
+        return max(numbers)
