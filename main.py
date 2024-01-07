5164d1c101bd4a8e93f92f4451df45cd8f6c4262
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:41 +0000

    only small turndown him obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_pressure(force, area):
+        return force / area
