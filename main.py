fbf08bc6cb27589ce46d24f08092e9a241a840bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:46 +0000

    secret soil kill service first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
