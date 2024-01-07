96257ef51f70e79453a8fde73b5ea8f6e1f09a72
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:49 +0000

    like recent better valuable brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
