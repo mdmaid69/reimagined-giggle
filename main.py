7f7a59ba1415f1d833fa3f4738b89e58491b4c64
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:31:00 +0000

    culture common shake light age

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
