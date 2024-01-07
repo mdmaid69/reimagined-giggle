ec37257dffff35528b4c8c52dc3fcb9fa7c9250d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:04:34 +0000

    confuse open great female declare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+def divide_numbers(x, y):
+        return x / y
