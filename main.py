72727f8d64df117ff845f5e32e0a7fb0d4bc65c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:43:16 +0000

    weekend laugh substance left I

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
