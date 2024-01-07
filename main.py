422e18aeb8ec94e391429404b17352aec34aa2c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:17 +0000

    possibly slip coat give look

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
