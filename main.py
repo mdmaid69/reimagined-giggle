a1984bea0fde3ebd651d2afed1ec54f4dfcea2cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:26 +0000

    tell disagree choose basis middle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
