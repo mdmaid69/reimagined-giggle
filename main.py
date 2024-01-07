9512185fd229ee7220603a54acab0e391fce3dc5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:28 +0000

    turn sign pupil hurry waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
