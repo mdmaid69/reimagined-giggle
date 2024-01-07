8144c2ed215803de7458bc779e67810971850a8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:46 +0000

    nurse life in keen drive

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def exit_program():
+        sys.exit()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
