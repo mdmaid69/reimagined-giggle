cf68f060545123d8053da0c58439ee04e75736d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:28 +0000

    empty instruction positive travel environment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def count_characters(sentence):
-        return len(sentence)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
