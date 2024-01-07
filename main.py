2c9a5b7d0b0f9c8585f4dbe58577374fd1ae6fc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:47:32 +0000

    account reaction distance declaration light

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
