c177bcf4fef609264915fd2a669eea5fcd878f12
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:51 +0000

    himself ticket themselves friendly illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
