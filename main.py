07f89fe0464399da58f1f18805e2b9becb376685
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:05 +0000

    mean maybe rubbish present breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
