68aeec5afca29eab150bd77b374abf4978e5824e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:27:42 +0000

    lookafter sell morning day score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_pressure(force, area):
+        return force / area
