ae99079d5b32a45d596e3cdcb2c60779b141b576
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:57 +0000

    plan judge keepup date rob

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
