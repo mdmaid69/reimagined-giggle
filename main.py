ca35ab8d502610826f107fecb8bb17721803f2fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:10 +0000

    deliberately certainly occasion holdon then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
