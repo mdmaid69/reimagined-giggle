75fbb9d873bb99d910f7d1a8300d0594d8c71d9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:44 +0000

    Kk plate file explanation somehow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
