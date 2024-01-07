6c10e094bad32f726d74fb7ba595ed0e17fe1e76
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:25 +0000

    interesting tell hello Nn murder

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def multiply_numbers(x, y):
+        return x * y
