8edcb13811f4c0efa97f408730cbc30feb5849f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:43:53 +0000

    square beside read march treat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def find_min(lst):
+        return min(lst)
