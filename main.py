7d6859d4cba5a7eb84dc7079489009a611dfe975
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:20 +0000

    programme wonder catch connect hurry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
