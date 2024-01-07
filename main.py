f7a7cc684361d0be94ed9602aa9b85539d8d200a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:08 +0000

    write explain reasonable old excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
