a44b28f7d16b8b8ea4e0359c66afed5b242d8759
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:51:32 +0000

    copy action fear cheque dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def convert_to_octal(n):
+        return oct(n)
