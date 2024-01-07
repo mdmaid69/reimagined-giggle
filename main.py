2f73869de3e70efd718916940f220a7769f746c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:39 +0000

    split sight impressed square nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
 def push_to_heap(heap, item):
         heapq.heappush(heap, item)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
