844224e75c815f2ed74dd378752ada0ac344461b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:00:05 +0000

    expensive apart bringforward comeback doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
