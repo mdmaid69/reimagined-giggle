a1fe4cac3820e325cef0ef78f5b870da11149c37
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:42 +0000

    boil region zero search base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def count_elements(lst):
+        return len(lst)
