e1b40ecfd2725d37db6eaf67a13dee3fafcd6d38
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:25 +0000

    cinema challenge comeover slice search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
