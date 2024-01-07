b469e41afa197648a96cbbc60b4fd8595034dbfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:13 +0000

    quality exaggerate most adjective reduce

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
