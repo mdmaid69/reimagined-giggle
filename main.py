66b603ebefca0f9712c904900734b4fe4527e272
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:32 +0000

    Itself skin cheque branch deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-for i in range(5):
-        print(i)
