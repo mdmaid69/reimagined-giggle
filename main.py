61f621a95684ba120d37f8ea7bcf27398e35ec70
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:11:36 +0000

    giveout pretty criticize public closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
