e23c6de953025769a6d787ed9b7773be00961e03
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:00 +0000

    571enough where bad suddenly effect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
