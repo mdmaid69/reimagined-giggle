228d10c005812d2ad6737675178e8855e55aed11
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:53 +0000

    direct film law not breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
