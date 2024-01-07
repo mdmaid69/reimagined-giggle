287a4779c0868030c96b3a70340dd9e2972c6180
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:41 +0000

    win sticky describe somehow pull

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_slice(array, i, j):
+        return array[i:j]
