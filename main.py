44a010d84f98781667eae58a595086eeaf301645
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:53 +0000

    violent fly mine oil answer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def greet(name):
+        print(f"Hello, {name}!")
