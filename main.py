db16a402bbfbb74570279d9c3ef19a3b3ebafa26
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:00:31 +0000

    swim watch question beneath talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
