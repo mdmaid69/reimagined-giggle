7ace35fd4103096745192e4ad8cc9f9f1613a7a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:34 +0000

    port guess rank standard most

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
