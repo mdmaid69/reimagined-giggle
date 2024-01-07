76c960989842aa1c76c8bfe33884a8e5aa6745f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:29 +0000

    help stretch risk progress sensible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-print(math.pi)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
