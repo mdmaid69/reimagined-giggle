ba057099e2c7051986ebeddc93096bf021713118
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:47 +0000

    communication everyone by turndown hardly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-print(sum(range(10)))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
