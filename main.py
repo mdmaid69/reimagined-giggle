df00a248d3e32c7a92a7b0f61d3dd51a1cb1fc96
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:56 +0000

    near strike send give error

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
