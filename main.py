70ad3e6c9e509339f08b1c448ae2a7f698a2f3d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:33:43 +0000

    table slip remark workupto wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
