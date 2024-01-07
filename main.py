754af010f2238260756c9bdf856a0f47b1f18f62
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:55:37 +0000

    address live turnaway number lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
