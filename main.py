1eab84624a69aa34521d8f99d7fd6319bc4c9f76
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:00 +0000

    entrance report touch cry feature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
