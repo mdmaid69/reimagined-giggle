2801fab68c74e2c071a1aa0a98e763771ce95454
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:13 +0000

    finger copy stop your phone

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_volume(length, width, height):
-        return length * width * height
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
