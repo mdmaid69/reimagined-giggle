c9554bc4e79809cfd184e18252dca34e318a4253
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:20 +0000

    slow permission discovery once again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_as_repr(array):
+        return repr(array)
