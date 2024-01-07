33a3bf09ff661ea90bf3239e6909c65889a9d472
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:35:36 +0000

    emotion part Mm party plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def add_numbers(x, y):
+        return x + y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
