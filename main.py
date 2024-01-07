c3600a21b3affb373304378eb94d4d015924bfa0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:03 +0000

    skill necessary practise light nothing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def is_even(n):
+        return n % 2 == 0
