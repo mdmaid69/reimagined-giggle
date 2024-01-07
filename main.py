f79e7479ce40ac8c8ecd66171d5a844c5ad0d942
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:55 +0000

    ashamed second match keepout criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
