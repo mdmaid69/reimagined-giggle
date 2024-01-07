5d015d94c28e4ef8e5ddcd466a40e44554957ef0
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:34:06 +0000

    year recent he happy nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
