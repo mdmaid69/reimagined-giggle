6f90a28e4016a13c0a558e32fd65605db5d5562e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:47:13 +0000

    convenient strength base limit spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
