c84dbb6321dc3b1341e52431b1c738166d57160b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:06 +0000

    address safe do lot godown

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
