431851e2a6d6584eabd0bdf1f861c7887c71c75b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:53 +0000

    thinkthrough teach usual tonight Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
