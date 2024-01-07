260f656287c3180e1e72a47a125399cdc7604289
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:09 +0000

    strength correct trip meet area

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
