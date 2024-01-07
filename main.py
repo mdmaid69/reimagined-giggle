9c9d1505e4cd08581c77ee381951ecc3a6d15279
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:04:06 +0000

    harm spread post choice hospital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
