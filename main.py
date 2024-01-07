993331c21ec71842d66302caf81795cfea354fc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:50:17 +0000

    eat since earn treat address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_user_dict():
+        return collections.UserDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
