dc219e082817edc4cd6fa12948264a0b76e29bf4
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:44 +0000

    kill recently wheel raise couple

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  def convert_to_binary(n):
-        return bin(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
