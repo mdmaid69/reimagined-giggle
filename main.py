354b23064dbb76e1eb71cd8265b7ceac600c534c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:36:41 +0000

    mouth act shut should satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
