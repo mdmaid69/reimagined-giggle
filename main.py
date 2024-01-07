a9b5a528d30024e811e7d78e4ce3503128970d46
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:05:01 +0000

    bored power authority behave blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
