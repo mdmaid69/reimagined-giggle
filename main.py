0e2c234bf076554fc4e12e0261d4956a57b7e67d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:10 +0000

    equipment much community steady pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_work(force, distance):
+        return force * distance
