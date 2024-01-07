8d78c460d453e78752c858475be6f5a1cc9d2a66
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:13:36 +0000

    rely show belief pretty account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
