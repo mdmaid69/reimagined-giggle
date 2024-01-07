4116da8584b9f06d6c222e0164774c38e00d0eba
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:22 +0000

    car bin abroad speed broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
