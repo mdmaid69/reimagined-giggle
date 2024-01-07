0354abf01567fb5578904b27ebbdcaba18f5bd7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:33 +0000

    characteristic Uu acceptable mark steam

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
