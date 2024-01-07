830a0e5d29e2b40239b2291876f03798a1cd4c25
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:52 +0000

    bringback politics knowledge spring wealth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
 def log_message(message):
         logging.info(message)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
