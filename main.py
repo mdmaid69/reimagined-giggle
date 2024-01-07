fde081259be7d4a1718bfe06d8d5ec7b988d709b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:48:27 +0000

    whistle belief shut waste holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
