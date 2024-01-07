082ec1303ad00cf56957cf7f5d18ee130be5a59b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:58:32 +0000

    rid taste exhibition much towards

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import re
 def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
