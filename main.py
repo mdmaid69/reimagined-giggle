65531c481fe59f0dfda0b8c80874b39efa4ec271
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:47 +0000

    urgent but wise bit shrink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def is_even(n):
+        return n % 2 == 0
