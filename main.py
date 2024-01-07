ad5f96107d88eef49ad6bf25b4f9c0f379a69c64
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:41 +0000

    best nowhere even game trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
