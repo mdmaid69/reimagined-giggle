8213bf8234d2d85f8805c88ca3588b5160dbaa6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:43 +0000

    expression brain friendly fasten keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
