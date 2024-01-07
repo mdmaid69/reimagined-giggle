a42741f1811ef009a88f61659cd0217fc8ad13ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:00 +0000

    wear coffee embarrassing healthy interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def square_number(x):
-        return x**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
