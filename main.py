c3a7178794c4b477ecf0789d01f69ab06adde0d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:08 +0000

    better our underneath permission original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
