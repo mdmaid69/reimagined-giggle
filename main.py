8e3b0981307b5ba991f02bfd7130b1d1aebea0b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:12 +0000

    consist radio rubbish whistle would

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
