755e53e3954cb3b2063231d94311040e3865b2ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:14:53 +0000

    test shock raw pure smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
