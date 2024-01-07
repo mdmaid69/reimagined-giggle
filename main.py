f7b6dc529f522087163ce4ae08e8eb12f959ff0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:18:53 +0000

    reaction giveback joke snow Itself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
