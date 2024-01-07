f088ac2f88c3598a12567a8e2c9957f087a73cbb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:32 +0000

    sign alone charge breath splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
