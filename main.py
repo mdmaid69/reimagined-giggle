15892e23391ccff404357adb41f54f78104b1a39
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:14 +0000

    school happen house about over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
