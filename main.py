0bf0dcb4b6842e4e552e1811bdca315da8c548ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:30 +0000

    active need study interesting trap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
