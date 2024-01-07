65900be5943cb20a819bf77d2a8c53a3c6d35f3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:25:23 +0000

    disgusting remember giveout business intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
