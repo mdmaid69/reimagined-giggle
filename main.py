de9653f8590fceb9b7d0e5b97f3b689d4aadc5d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:27 +0000

    main detail maximum uncertain fork

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
