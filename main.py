e908cde77b08ac50c2e2b324c8334942dfb4a642
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:46:00 +0000

    over would long foot block

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
