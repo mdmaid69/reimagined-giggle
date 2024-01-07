2a62a6ae3c5ab57ade3c2b673ce7e100b651f335
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:00:43 +0000

    equal excellent Bb block modern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
