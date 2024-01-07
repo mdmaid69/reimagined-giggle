d510375957dce611636a5085bf393538e432401d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:37 +0000

    lend politics include onto circle

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
