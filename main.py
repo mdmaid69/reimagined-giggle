d374fc138eb35b299793965f59f40a82093a2e0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:38:45 +0000

    discussion keepup light manage criticism

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
