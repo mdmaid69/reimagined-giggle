52d15adc2faf74b20e150c73cc000623ca9cdfb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:00 +0000

    possible furniture concerning control lonely

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def multiply_numbers(x, y):
+        return x * y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
