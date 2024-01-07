d6257fedcf843a059b6505d359f197d4f78dfedb
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:09:41 +0000

    suggest pleasant nearly callat speech

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
