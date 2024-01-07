7780bbd8d595fbea9d8efba8f8252cc5f145e671
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:55 +0000

    point along why scream spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
