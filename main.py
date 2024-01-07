d1c4c5fc1e0a9dbecc2a98a8d28b53ba83235965
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:38 +0000

    other less watch schedule pipe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
