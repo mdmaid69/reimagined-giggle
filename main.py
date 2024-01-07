87985c619d75a3e679a0c0e3a090827ffffc3abf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:58 +0000

    vote substance smile include probably

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def divide_numbers(x, y):
-        return x / y
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
