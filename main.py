860d7c87c86c9b853d3fe21278410b8c13936566
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:11:55 +0000

    bend borrow true personal too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
