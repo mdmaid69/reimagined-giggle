6170bb91934f457022f4525d75de3e237be64340
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:14 +0000

    embarrassed equal dry sure surprised

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
