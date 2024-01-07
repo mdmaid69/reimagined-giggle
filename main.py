6e3622a5f328b712bb9c584858047aaa303a8b96
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:20 +0000

    cinema old help takeback bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
