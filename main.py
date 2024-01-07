6f8a06a4e4d325860d8fb21e890e689dc15b5ca0
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:36:03 +0000

    real lookaround dream hang warm

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def square_number(x):
+        return x**2
+import random
+print(random.randint(0, 100))
