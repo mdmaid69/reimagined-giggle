f21b64f83a1b498607eb649a2a6a2913bfc3a2b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:03 +0000

    container necessary phone break once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
