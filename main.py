d7df6f35892c8b776eb3860628455e2bf996b151
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:49:09 +0000

    tonight traffic use fall tip

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
