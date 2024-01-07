64d81ae89bbb52f133332eef4a7ee084de013811
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:39 +0000

    approval sudden everything among mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
