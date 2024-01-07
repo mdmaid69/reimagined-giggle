9e7d074a6afc287f47b07f4c27ad8eb9f13b1f7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:34 +0000

    thankyou almost little identify cough

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
