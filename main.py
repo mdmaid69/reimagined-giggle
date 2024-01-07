09204b7fb7e40c0a86e1aafd0a1edbad445d13aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:04 +0000

    unlucky onto ago proud fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_today_date():
+        return datetime.date.today()
