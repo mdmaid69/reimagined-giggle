4067d4bfe274b51aaecc71a06c9365e663e03660
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:14 +0000

    be target connect actual example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
