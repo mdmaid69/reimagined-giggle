fa5e395f36ce51a28664897425f3ff901a5b1699
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:26 +0000

    standard surprised cost flow right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
