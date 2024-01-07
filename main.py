1276884f07c473b1772b83a4d1e9c26794d9e5b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:36:16 +0000

    mention order family declaration clever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_density(mass, volume):
+        return mass / volume
