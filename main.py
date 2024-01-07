197dbf06ae26ebec281883a7971521b2e0a88838
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:28:15 +0000

    male godown funny luck cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+print(re.match("h.*o", "hello world"))
