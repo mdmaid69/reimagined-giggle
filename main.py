45e299db232b672feabb2ee3d7217ec28b29be08
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:01:31 +0000

    union rescue disagree flow 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+print(os.getcwd())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
