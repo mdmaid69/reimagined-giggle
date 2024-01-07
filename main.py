db45dea594576475a705136ac230c11579f761af
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:52 +0000

    angry statement correct let used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Hello, world!")
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
