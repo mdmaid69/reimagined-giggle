3b4c3fb708fe92ec19b50b341f56cd73f053e560
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:12 +0000

    criticism false stamp start mark

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
