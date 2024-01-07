b9e1946a10b93f28bcde9039dc7721aae5453702
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:07 +0000

    tune flow afford zero trap

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sys
-print(sys.version)
+import collections
+def create_queue():
+        return collections.deque()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
