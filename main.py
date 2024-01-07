b5b58d53b37ef58b6d97989922d9cb83c62360cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:40:57 +0000

    contact throughout understand ear spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
