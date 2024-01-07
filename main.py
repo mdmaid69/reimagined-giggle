2a03522e32c5fa369e393e2cc0cec65485b1fdb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:14 +0000

    noisy plant threaten single slip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
