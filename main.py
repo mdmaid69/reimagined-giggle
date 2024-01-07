a7b729b008f6ebb8d6673b0fb5ac4846129f92d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:46 +0000

    convenient declare pride recommend wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
