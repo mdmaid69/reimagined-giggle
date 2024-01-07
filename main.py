0ff4b36391ec8b165b53f8005235af6b466f676e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:50:31 +0000

    hollow expect throw warm safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
