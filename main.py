1969ae56bf32fa44498b599d7e5235766199613e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:20 +0000

    university make reduce anywayanyhow common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
