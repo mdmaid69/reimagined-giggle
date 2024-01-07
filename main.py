baa2048583053b1848252320e49d0e5f26d9549d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:52:30 +0000

    begin wish steady fine same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
