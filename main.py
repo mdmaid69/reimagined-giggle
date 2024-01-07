f6a636e2b0512b957ba54bb65da024d27985a728
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:40 +0000

    wall workout love instrument open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
