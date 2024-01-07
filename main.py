b8db51e5d793b0c0c87c4c9670510ff81b238d93
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:53:01 +0000

    port challenge traffic full settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
