2d497f10cc28a172c4d6161254e3d9b39518edc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:05 +0000

    will please bath mine song

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
