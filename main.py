cc47afca48a41ab1cdadb63700b1b0065c98e5d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:09 +0000

    shock sink 564encourage tonight carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
