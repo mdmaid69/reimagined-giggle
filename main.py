abd45d5cf3afa191dd6c6e8872b3df87f8b6daf0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:39 +0000

    on child demand breath off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def square_number(x):
+        return x**2
