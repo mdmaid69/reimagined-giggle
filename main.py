a2e0b938c55f0052c439a7b127dd3806835edf58
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:05:05 +0000

    loss else test child wet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
