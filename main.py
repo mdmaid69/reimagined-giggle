955a7027c3a9ae35af048dbeaef1010ed619679f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:50 +0000

    basis check realize squash path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print("Hello, world!")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
