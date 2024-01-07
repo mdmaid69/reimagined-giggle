331fef99fe7b05479f84a3983b572d5d2e9d93d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:08 +0000

    mean willing price better smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
