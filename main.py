2f00397f5cfe01487dd6202c1be56731b26b5fc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:15:24 +0000

    ready very become appear floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
