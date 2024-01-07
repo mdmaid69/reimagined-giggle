84d4dc94f3e52d2fb4b0e488c1a06f0b877e0a51
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:34 +0000

    however well extraordinary expression common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
