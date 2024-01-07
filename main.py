8dbd8746fa43bc9dd03b0eb112a618a7f4b3c73e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:32:24 +0000

    grand walk turndown getup arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
