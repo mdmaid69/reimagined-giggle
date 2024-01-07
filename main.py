5038fcb311a03eacebdc67cbbe189dcb2c45bc71
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:16 +0000

    announce wall national play truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
