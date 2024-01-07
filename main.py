79c64c6eae1c74037c57f492c5f2948a6583beb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:59:02 +0000

    while release language range protect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_work(force, distance):
-        return force * distance
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
