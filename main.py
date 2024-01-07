731e79e5f55ad5ff5428669b5b71bbd10c4c4071
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:38 +0000

    bill money job maintain people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
