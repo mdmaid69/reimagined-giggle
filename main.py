0bca399bfd379136b531e7ccdb77aed93d4ee024
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:45:02 +0000

    lower tend tool burst herself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
