125975e21c4d7066fdf92f3b985f30bbd4217e23
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:00 +0000

    tend father question goout whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
