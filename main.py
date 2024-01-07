999c7d265680f7035bbd2cf0dc16fa6cc7af1178
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:47 +0000

    later trust concentration instrument rub

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
