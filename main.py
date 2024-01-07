16dbbc77945c3bc20bfcd22b6d1f304ea188ee91
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:45:39 +0000

    such succeed production needle moon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
