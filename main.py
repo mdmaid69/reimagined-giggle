ef3c5be725adcd591479d659a3feff3aaac51667
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:18 +0000

    new add well breakfast old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
