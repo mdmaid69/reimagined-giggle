86a5100ea87cd480834e073b2ddfda18ee8832b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:55 +0000

    force walk swimming file callup

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
