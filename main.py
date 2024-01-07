735bd530bc53d7d5063302aad3c9d7dccca3b3e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:37 +0000

    dish lunch pronounce public less

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-text = "Hello, world!"
-print("Characters:", len(text))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
