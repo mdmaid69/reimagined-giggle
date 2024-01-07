d7cc8fccaceb3737eabe7f36919cc9c9777e1f08
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:19:45 +0000

    tread community us foreign hurt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
