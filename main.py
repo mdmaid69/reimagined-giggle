97640ef37b7651015f3c1574abd09df2235507f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:20 +0000

    interfere another if book with

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
