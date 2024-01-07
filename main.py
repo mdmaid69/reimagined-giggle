a2a339cb5c88086fc97cf4257d9e44678245ac59
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:47 +0000

    famous land term clean way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
