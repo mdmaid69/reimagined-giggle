98f1942c4b035015e0d01e8126c74b187b89cf5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:21 +0000

    plan insist afternoon wide how

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
