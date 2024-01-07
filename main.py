31a314f18d487b15b02943412264315ce09f9ef1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:31:19 +0000

    feed skill side rob 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_queue():
+        return collections.deque()
