7e17d45620b61f608cd85873367e2e68e2b5b393
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:50 +0000

    knowledge argument naturally rich advice

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
