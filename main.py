aade4a96a95f3e15280e2a6b9f67bc047b82f348
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:26 +0000

    emphasize store earth bringforward choice

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
