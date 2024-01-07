9d0c47eeb879067aebc44cefac87a81d0bc4201e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:26:51 +0000

    range match smart behind would

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
