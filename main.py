aa45528456b128c711ca8904a9875e0d091c2ebf
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:56:09 +0000

    sky shout tall unit tight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+for i in range(10): print(i)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
