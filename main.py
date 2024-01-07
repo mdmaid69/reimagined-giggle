32a9c006d8250fde3cb39e6f89f659ca7af91ab0
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:28 +0000

    refer mad your bin score

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
