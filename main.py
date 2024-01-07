3145aed4283048d61c5d8ef364515637582151a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:51 +0000

    aim occur exactly wind peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
