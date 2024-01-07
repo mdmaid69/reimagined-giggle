d26b51b887ff91d75584ef2e2607a24a2115a1b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:42:32 +0000

    second and opposite there broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
