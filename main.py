4aa8cffb4e500accd12cbbbfaab7f0bc93d05cb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:03:07 +0000

    human exactly confused push I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
