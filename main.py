df6882422a124c62f0ebf855ffbcee524fc6f212
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:57 +0000

    deliberately as crime why wander

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
