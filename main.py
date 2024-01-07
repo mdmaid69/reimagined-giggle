7b859b9698991095f684c9f3c6222283de09a8d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:43 +0000

    worse effect poison wish large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
