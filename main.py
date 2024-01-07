0cfa164d7c612949d011233b3e07b8049d49096e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:44 +0000

    journey vision convenient keepout suggestion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
