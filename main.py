8ad8d25ce3e4230550542e04455627b9c9d89485
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:13:13 +0000

    telephone complain holdout pretend difficult

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def reverse_list(lst):
-        return lst[::-1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
