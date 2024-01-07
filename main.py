f223e50e9f47e5007c4da8abe0c0a8c66f71feb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:16 +0000

    opposite pretend direction chat positive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
