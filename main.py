e8658d6618b4eaea3948ca68eaa91b0a04332454
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:00 +0000

    report old cinema breath advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
