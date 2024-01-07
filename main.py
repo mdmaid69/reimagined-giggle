2066844d5f37a0d45d2d3845b7661561f2965198
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:58:16 +0000

    that calloff narrow affect which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_priority_queue():
+        return collections.deque()
