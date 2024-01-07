ae450f84c8940885843a4d83bad6ed2f3e43a856
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:50:47 +0000

    powerful like measure detail bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
