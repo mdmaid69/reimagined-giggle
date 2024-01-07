5881a08cfe808085a8b7e6a7f267ea9cb7a68a7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:50 +0000

    finish true hungry so children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
