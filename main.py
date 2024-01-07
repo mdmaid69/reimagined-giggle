0c52d37bf80ed2759af7f78e381024dd074ee4ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:51:09 +0000

    keen marry straight related powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
