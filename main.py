67b7f5f718444a846034beb29116823d5d914d3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:10:53 +0000

    fortunate tomorrow correction up give

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
