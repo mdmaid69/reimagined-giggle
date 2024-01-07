b04d2412f8c2bf10264bcbc109ae6e0df0ca557b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:15 +0000

    now commit worse again memory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_floor(x):
-        return math.floor(x)
+print(math.pi)
