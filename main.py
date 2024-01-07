cfc382869fcfdaae0e7b409f1d8a55033bba3379
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:22 +0000

    search unlikely scare relative final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
