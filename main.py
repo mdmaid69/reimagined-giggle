646cf611f7ffee626c9b01ed96ccb5fc1a4adab3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:58 +0000

    confusing plan appearance size dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
