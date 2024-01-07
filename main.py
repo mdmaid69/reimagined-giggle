6f817f1cd367667f0b161af4d5e57be30f8c3fde
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:20:52 +0000

    Ii shine report development search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-for i in range(5):
-        print(i)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
