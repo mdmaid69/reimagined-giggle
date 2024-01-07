f4b961aa9d2918af8e0e4f256ae263c3cab3328e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:35 +0000

    in division welcome beautiful in

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
