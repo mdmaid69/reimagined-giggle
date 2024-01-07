ea33ff7401e59dff401e6624154be8a3725e9a67
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:08:23 +0000

    pour book lookafter declaration care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
