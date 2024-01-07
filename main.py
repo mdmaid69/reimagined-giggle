97c9c0357a45c8c1c4d1846340ab299a2e99b1bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:36 +0000

    imagination clean brain unit too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
