9235f4b070cc3bb1ed86cb5a3b7c577a24faedd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:32 +0000

    send 572enter clever whichever upset

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
