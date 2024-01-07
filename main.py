2e587ed2e6323ff2228d6033329938b0c2536ace
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:25:01 +0000

    gentle either centimetre leave operation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
