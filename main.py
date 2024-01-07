c8ce7bb7a09f4f6a533deee00affd77b735fb92e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:21:33 +0000

    see harm home family return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(10): print(i)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
