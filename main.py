17ef59667c0cb577ff644f312877ff5e90e4a604
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:16 +0000

    picture round better ever contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
