a4736faf041525c123f20880cfb76d5d1a3f4d56
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:17 +0000

    cruelty amount admit debt when

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
