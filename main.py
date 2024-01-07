bc4a9289e09ca3c89823ecae468c75e4999be018
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:47 +0000

    thick damp history vanish personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
