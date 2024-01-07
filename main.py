3cd06c1ec2a829c0609123484f0d1ce5164442c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:06 +0000

    worst find pain wave butter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_current_date():
+        return datetime.date.today()
