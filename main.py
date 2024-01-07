c55356464749abdd54431b8b7b919d3cfc62c128
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:01 +0000

    such beautiful movement deliberate night

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
