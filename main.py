db5fb3940806d551f33939eab0fead066619ed51
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:55 +0000

    fortunate reply usual or slide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
