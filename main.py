b3060ec0311d07552142f30e1eca10476c293742
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:18 +0000

    ear imagination sport wake high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
