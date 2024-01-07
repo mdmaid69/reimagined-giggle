02def5cf91957aa59ffae4fc515cdfcdf99abf3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:40:58 +0000

    state hollow genuine drawing small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
 import array
 def get_array_as_bytes(array):
         return bytes(array)
