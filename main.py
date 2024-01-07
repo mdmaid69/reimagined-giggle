1ec03764cc0374390488795e602c25af11731810
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:03:19 +0000

    treat nerve anywhere friendly arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_length(array):
+        return len(array)
