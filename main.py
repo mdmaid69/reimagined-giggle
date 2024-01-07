a5686bc27ebecef083a68d9392cf2e412a575491
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:57:03 +0000

    harm lose eager reply finish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
