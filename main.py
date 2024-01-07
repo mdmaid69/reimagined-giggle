67d8ae85cb7cdf9442ae91148de4a234f43575b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:19 +0000

    usual battery if high behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
