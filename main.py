8fe82571fc6e054daa92e131ff9baf5c4c678039
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:53:28 +0000

    Ii laugh division climb lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
