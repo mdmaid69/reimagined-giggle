e597b176a78be0ee658509fc3741eabd48548904
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:39 +0000

    fix rescue towards common born

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def count_words(sentence):
-        return len(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
