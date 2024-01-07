baddea9c696d4d571259f261dd549081fcb15a8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:20 +0000

    in every steal party regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
