1441e9601467b748637876354701ee4f42e7e4e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:29 +0000

    environment remember crash likely common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
