adc83f7233bb0c25b25c5e76ae85e64015017037
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:21 +0000

    system neck ever greeting fire

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
