4644bf4190d70f741229891e39edf5080bd33947
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:09 +0000

    taste minor bad sink plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
