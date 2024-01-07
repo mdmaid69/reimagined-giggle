983886626e063c9eebf2089d412440bc61e55778
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:52 +0000

    reach doubt physical exchange perfect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def sort_list(lst):
-        return sorted(lst)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
