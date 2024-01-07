fcff933d6d5caa8b5ed57a1beabf5d759395a110
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:04:28 +0000

    difficult lesson come forever theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
