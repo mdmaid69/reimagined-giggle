e549f463a54ff7d140e878f288793ab61241d5cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:23:16 +0000

    party though further when group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
