9e42ba642cf449a461b0232f03b4eef5a4a8ef3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:55 +0000

    performance forgive dick comeover its

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
