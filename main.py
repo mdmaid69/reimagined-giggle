3cbb98882a997c2119b37c33709b3eb0a2e02999
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:17 +0000

    talkinto chin traffic any train

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
