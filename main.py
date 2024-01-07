93f06e5f30d229e90f08c8ac463a93a91c671ed5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:18 +0000

    authority minute upstairs shout reliable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
