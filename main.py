38320944af86c4532159894eb0ceda6611111042
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:53 +0000

    music ilovedicks environment temper face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
