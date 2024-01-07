64d25fc5dbe5b81df3e10a647f1f6b72971de19b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:05 +0000

    reserve reject influence name definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_float(array):
-        return float(array[0])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def remove_from_array(array, item):
+        array.remove(item)
