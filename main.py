ff96ca4fe2b2c2688afb7cf1a61b385c73e63fba
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:48 +0000

    guilty hungry permission turnon regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
