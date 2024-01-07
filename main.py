81aa6cf3cee820bc91016b5044cbf32dea595df1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:48 +0000

    crowded reaction Cc first influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
