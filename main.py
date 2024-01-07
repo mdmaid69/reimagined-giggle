66af9249532f18f5d585372df6c9fd4f1abcfa7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:00:43 +0000

    invite sure copy wander press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
