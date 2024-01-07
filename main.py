fbd42c183fbd5efd5d1d512633edb24ca2fbfe66
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:59:50 +0000

    bend children concentrate repair contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
