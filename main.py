7b1047f3b9b5e9fae72a10fb76114faee6385b4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:33 +0000

    number late short hot closed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
