da83043f1ac88d6e2b09872b30a5de0faa35138a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:35:09 +0000

    right thirsty memory world every

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
