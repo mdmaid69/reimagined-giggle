fe70ea567eac29c191f6779989280178ae405009
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:38 +0000

    expect kind slice situation likely

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
