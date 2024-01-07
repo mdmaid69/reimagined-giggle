cf47af69a0a09664cc97355d592c79a2acb50a3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:12 +0000

    drive criticize piece round better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
