04617df1002cf1396e75133b25a4c3798330c7fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:37:44 +0000

    block polite heavy interview suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
