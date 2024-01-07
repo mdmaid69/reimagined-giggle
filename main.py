51b2318ade1145c237161751e7df70f2b093969e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:14 +0000

    shopping bear battery scared breakthrough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
