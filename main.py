4994692b28898a903ecdf0e72c0c428ebde3d90b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:50 +0000

    oil giveaway plan design entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
