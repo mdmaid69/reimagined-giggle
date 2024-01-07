ee2622c8d892823a3b1f1639772f979613ba64fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:55 +0000

    everyone head defend burn exploration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
