a6ce2291852515bfff3fee14d8d1e21db8fa3de0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:13:51 +0000

    cap wrap curious it lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
