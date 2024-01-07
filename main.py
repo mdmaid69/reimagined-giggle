bf36c84771756f2f08f38f7b5eb84a99e6915066
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:01 +0000

    amount race base drop meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
