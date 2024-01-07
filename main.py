ed3af52557de54e920426567f91972b442e83ec6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:13 +0000

    handle sure fix bag birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
