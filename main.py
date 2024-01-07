1bd11b695e7f29514105425df89bb4b2c55ca41f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:33 +0000

    improvement equal once sink mistake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
+def calculate_area(radius):
+        return 3.14 * radius * radius
