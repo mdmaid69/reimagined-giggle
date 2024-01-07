5d9ab503824df8e53f6aefdde5d07d2b548ca3e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:48 +0000

    holdup set speed comeout past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
