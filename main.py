1c76b0b5be6b37a7cee09d7b8961204383033e21
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:17 +0000

    detail occasion anxious dry tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
