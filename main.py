1bd395a43dc472e453838222c1defef33cb3acc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:54:44 +0000

    your Kk herself size fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
