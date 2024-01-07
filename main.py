1f3dee050efc50f05815db3a5acde2414f7d4f05
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:36:20 +0000

    obvious quality steam during debt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_dict():
+        return collections.UserDict()
