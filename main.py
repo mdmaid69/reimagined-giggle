3155c8b95b69926c5fc9fb6f19f9ee549a9ac415
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:40 +0000

    side talk goup line and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
