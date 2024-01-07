c71e486bba2978a194240378cbe36bb7be7d992d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:42 +0000

    emergency journey dream comedown young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
