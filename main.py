4f7e5e680b4aecd1cfc8b3a2f75b39bab95729a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:59 +0000

    sensitive protest shut dry surprise

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def divide_numbers(x, y):
-        return x / y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def square_number(x):
+        return x**2
