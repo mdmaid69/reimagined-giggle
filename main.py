293863abcb5a26d1dd80abc5493aa0ff652b09cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:16 +0000

    possible forget recover squash prize

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
