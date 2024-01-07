3a3c649ac28ae509a46b1e06bdaa21fd79a52f39
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:17 +0000

    someone correction attempt peaceful challenge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
