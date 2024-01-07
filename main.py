3b4929f247413228db3e4b4c1220318354d745da
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:57 +0000

    goon drive no whisper fair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def square_number(x):
+        return x**2
