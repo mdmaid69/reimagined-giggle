33fae458b778c80fdbdbff21f6e7137492464517
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:43:51 +0000

    throw urgent temper form either

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
