de3e688abfe25009184f56be65185381e720fc36
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:48 +0000

    turn appear cruelty rob nothing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
