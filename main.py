e9bc717346280bdf5ee207092d056eadc6fa886c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:17 +0000

    on discover pattern firm star

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
