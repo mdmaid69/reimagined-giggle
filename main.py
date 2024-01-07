dbf12157d5946cc29a740346bef16ff290172c73
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:26 +0000

    wide become statement square thinkover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
