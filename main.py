a3bd270233b80025f5db62af4be7505b8c7f8b53
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:59 +0000

    typical high dear strong better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
