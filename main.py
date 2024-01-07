70eb4efe3ecb8c80fe5be7b928dce5dc5107eaae
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:50:58 +0000

    relationship goon sale exaggerate holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
