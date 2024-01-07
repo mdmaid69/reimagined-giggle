a9598d2dd31a8a5acfa55cc7a155e2875442ded5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:04 +0000

    real round pure pocket promise

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
