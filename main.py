b1c82e271b7c362b5dd98f9d805a66f50527389a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:48 +0000

    apart allow skill calmly discuss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
