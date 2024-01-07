0b16b13979b29bdb7bea0e11285cd7b7477b2b32
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:19:59 +0000

    damage passenger to within fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
