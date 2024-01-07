ae0ee2125e74efb19f84a303258899bac8d25fe2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:21:52 +0000

    mark expensive object priority simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
