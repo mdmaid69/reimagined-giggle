66810db897255da1517d72280a54475f946b4925
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:12 +0000

    spring need telephone often even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import tensorflow as tf
+print(tf.__version__)
+import sys
+def print_python_version():
+        return sys.version
