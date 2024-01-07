f27dc7a32042ca940f2bda219591d52ac2ac69b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:18 +0000

    separate murder art fact ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
