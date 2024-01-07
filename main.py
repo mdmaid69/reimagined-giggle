d7846cb9cf3f8e9f36599e1198d2b7c637e0a8e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:09:15 +0000

    regular correct ask interview instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_list(array):
-        return list(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
