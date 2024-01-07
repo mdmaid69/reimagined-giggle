fe55d385b9b999d352d814ae929705c435fd9cea
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:29 +0000

    preparation wave move anxious cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def iterate_over_array(array):
+        for item in array:
+        print(item)
