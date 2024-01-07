c6445e9fa841abda888acc48002d1a652486ba6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:30 +0000

    order violent sound lower practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
