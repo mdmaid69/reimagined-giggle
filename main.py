ba17156f32ec6237d99b11321f856c688d3bf81a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:55 +0000

    fault shop breakup wet happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sine(x):
+        return math.sin(x)
