4d468418c8fa0cdc9ec361467da2fce105653156
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:38:58 +0000

    wide formal vote means outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
