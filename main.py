9719d79128fd8daf10cdcd6d00616e8ebee2f347
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:59 +0000

    into characteristic boat nigga squash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
