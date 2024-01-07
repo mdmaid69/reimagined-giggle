8193b55a1060e2d24b92cbb869cb57c6cbae1ad8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:42 +0000

    otherwise interrupt divide recommend supply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
