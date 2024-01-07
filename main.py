c1a63a458e375b713b54a36b695a1c1f4f5c6a71
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:19:23 +0000

    alone serve seat greeting hot

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
