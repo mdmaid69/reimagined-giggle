abc85968a1eba74bba75b8712d456f986deab368
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:46 +0000

    actual Gg lot full pain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
