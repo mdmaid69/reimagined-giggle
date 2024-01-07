6c0a796fd8bc57f1f83b3c9bd85b0e57e85768eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:24 +0000

    sound description train control contact

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
