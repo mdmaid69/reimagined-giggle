ea1ced19e055e000aa146e2af34c2de391dcd09f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:24:53 +0000

    roll crowded payment love rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
