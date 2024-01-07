0f682855dcfc31caf3feabee0aae7c3f0db52ca6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:10 +0000

    rough callout such mysterious fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
