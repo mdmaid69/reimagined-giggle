a2f4a1f9be3773f78cf158e9cea613da868be38e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:08 +0000

    neck worth please picture importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
