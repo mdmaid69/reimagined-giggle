5ae5c255f689370d6c0560a5830d5a0acc37d637
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:28 +0000

    yours occasional buy uponon little

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
