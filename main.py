9736fc120183b93a513a06e8fbac474e59d40b47
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:10 +0000

    lend rather extra worried must

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
