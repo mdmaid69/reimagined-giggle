49a17f1b73d9afadc035b8de48695c5edfb9a43e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:58 +0000

    their brush so popular means

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def clear_array(array):
+        array *= 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
