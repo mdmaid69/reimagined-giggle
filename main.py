4a80ab69b46eaa9600e19b3d3b448e9c39868aff
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:36 +0000

    smile flat but broken fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
