898e6aa517fdda43b8617eed58073162bdbab08b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:54 +0000

    platform polite comeback strange minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
