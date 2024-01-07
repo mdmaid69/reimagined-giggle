7f462e04e8cb23c93a6a855577cd83fba95cee1c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:41:16 +0000

    spend experience offer congratulations lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_bytes(array):
+        return bytes(array)
