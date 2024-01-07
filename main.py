8f8d599b48abf31539f64d31c8466cce36faa213
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:36 +0000

    ocean mean difference treatment nor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
