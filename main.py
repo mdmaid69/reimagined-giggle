9d42db6f8f13da5995181fa9793bd4508727108f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:41 +0000

    water pupil goback(to) drive makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_queue():
+        return collections.deque()
