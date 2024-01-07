adc0ad1675af5b31f841b0f8331e0986049326e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:14:55 +0000

    small stand talkinto person sensitive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_array_to_unicode(array):
+        return array.tounicode()
