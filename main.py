dd849c64a0cb9ebf0a24bb8a2a03edd6ee4032bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:36 +0000

    beyond this close car unusual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
