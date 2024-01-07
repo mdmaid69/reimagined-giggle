eece6267b3943bb1d5915946476c32e507aa4c39
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:45 +0000

    appearance comeout female professional sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
