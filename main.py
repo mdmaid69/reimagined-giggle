bc87715a8548e92d630baf6aa4acd9aa19d5bf70
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:39 +0000

    room lookaround what trip chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_queue():
+        return collections.deque()
