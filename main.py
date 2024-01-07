00ad4139f8530a37796bd65880eae28f00361824
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:23 +0000

    married definite tip avoid deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
