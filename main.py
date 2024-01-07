1e91ecca0f90cf5cdf048784c2ac0d964d3d08d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:01 +0000

    colour touch behind scrap wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
