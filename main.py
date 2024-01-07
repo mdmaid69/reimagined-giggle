c77d80c40914dc56f70ce7611c988ea77636752d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:27 +0000

    doubt repair excuse innocent smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def get_string_from_array(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
