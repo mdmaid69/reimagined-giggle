844e70cfe289924f4055d7095646baec3ee790fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:03 +0000

    event rest burn fix common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+text = "Hello, world!"
+print("Characters:", len(text))
