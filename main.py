00292a5d58c92f0ec0d49c491a4794eca6e8fd0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:40 +0000

    arrive drawing label meal mysterious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
