7c16f171451f52ec8207a42edb9dcd0a5759c446
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:50 +0000

    shop standard thinkover health belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
