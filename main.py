8d0d9b16e1229157efa6c681bb6306acd3f16121
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:03:31 +0000

    create both importance they single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_speed(distance, time):
+        return distance / time
