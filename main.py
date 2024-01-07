147c2fa64c24090f5c1c6e9601881e5589107891
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:36:29 +0000

    bye aware live experience crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
