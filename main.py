a0dd8956bad6aa8490f367f805cbc816f2b0dd55
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:14 +0000

    more getup downstairs Rr walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
