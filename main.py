bd9004b9c753918cb160dbf89278fda3d043a801
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:18:07 +0000

    extra afford beyond game thorough

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+def exit_program():
+        sys.exit()
