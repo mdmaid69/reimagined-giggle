2b07a7d2915f169a7a65c5a13bf8b3f183c24680
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:11 +0000

    Kk smell tone whether stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
