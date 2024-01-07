a815a5ecc383522a2fa4e0bcf2633e44d2b39e10
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:33 +0000

    hope do but state rise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def find_max(lst):
+        return max(lst)
