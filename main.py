fd1b990aec20655048c876da80e419ffb48f00af
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:43:28 +0000

    light bringback sink light steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
