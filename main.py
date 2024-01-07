c91cb70fc006b4a860b3275039d85a5109c7d408
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:52:12 +0000

    through right so welcome clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
