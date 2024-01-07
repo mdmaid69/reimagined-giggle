02f63f4231d74a4836978c64d0bcfadd5dd1aaf4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:48 +0000

    claim jump tell age spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
