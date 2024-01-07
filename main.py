b28c75f0f9787449bd4526f97c4b02568b1bb38d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:50 +0000

    quit air justice minute serious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
