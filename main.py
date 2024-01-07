6bfa15ca45b992806b3c82f4e179ec57c7133874
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:41 +0000

    Ee beat damage display season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
