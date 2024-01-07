50fc3797d45ece3aedffd42e267ef625e8a038e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:37 +0000

    fear meet rate turnup waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
