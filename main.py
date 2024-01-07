ee6df45e3d3fcf4bbe8a56c799fa3b8a5698eda5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:39 +0000

    disappointed say opportunity defend comeoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_user_list():
-        return collections.UserList()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_even(n):
+        return n % 2 == 0
