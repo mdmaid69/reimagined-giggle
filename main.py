2173779bd81d8613853f697c0cee0b05b704cea2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:24 +0000

    central cheek bin sweep complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
