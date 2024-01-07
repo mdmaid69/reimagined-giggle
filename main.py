b18c8c42643ba223db6972570d1566d238bca152
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:24:23 +0000

    stamp include medium conduct foreign

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def multiply_numbers(x, y):
+        return x * y
