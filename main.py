915ff13ef79d3f7456c5e41001197a5203c08aea
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:00:31 +0000

    however actually statement takeout extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+print(os.getcwd())
