f985dbc62ebb04a69a1c997cae67b8d06d083663
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:23:11 +0000

    angry plenty minimum tool work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
