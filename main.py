380defa40eefa1b3c435e89565946e42b1fbe9a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:28 +0000

    but 568energy our introduce after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
