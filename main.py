b62acd842a6372eaac655384468cc7b4a48ebdfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:47:03 +0000

    concerning comeup holiday comeround declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
