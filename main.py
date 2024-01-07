4d626f862109bc1dcb25f85b96210028273fa1f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:36 +0000

    ever ilovedicks yourself wait snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
