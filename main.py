b331f4a6c9447743814c73dd5a20157d2e4f6265
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:56 +0000

    Ff love damage should damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
