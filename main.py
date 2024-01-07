57bc385e2e445c970c4edb5d17faf9fbf0a27cf6
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:57 +0000

    will punish lot fault exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
