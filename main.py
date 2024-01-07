5129c0eabc0051b36aa1020741073bfb7f12cf5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:23 +0000

    arrival snow mass stop telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
