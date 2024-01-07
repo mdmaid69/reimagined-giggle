145752d4f2f245218ae7a886c12f99e740c9a26a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:39 +0000

    beneath moon 573entertainment close Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
