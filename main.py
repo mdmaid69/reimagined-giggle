3fe8379fd30d9cb4df7435a5da0e631ee3105989
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:17:37 +0000

    company fail hard town must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
