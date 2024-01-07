9290ec6c2b1d79c94c8c7ca8ead12db47b469751
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:20 +0000

    collect far determined behave birth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
