c843665b8f26a0bf50078110daef0eb4c3ce0c64
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:06 +0000

    wild length better pretty wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
 def get_array_as_bool(array):
         return bool(array)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
