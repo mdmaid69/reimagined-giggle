7d37b14c72bce36181081d5b5de14f0b03481f86
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:26 +0000

    bringdown thought attention neither equipment

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
