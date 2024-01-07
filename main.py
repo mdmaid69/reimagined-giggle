ad954ca8eb71aa8df0d5f384b434808eed12945f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:15 +0000

    back flood excellent conclusion disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
