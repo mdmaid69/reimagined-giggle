ca5f21176fd68f3a37d4225eca8ea59d3f6138d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:45 +0000

    apparently shout place Bb address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
