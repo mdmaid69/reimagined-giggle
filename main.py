85c4b79f271568787b51823846436422497b89b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:11 +0000

    Ll bringback entry label quickly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
