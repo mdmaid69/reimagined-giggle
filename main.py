5960413ffeaa8d85da020b80b1dad308c3ca684c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:52:18 +0000

    interest judge concentration practical I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
