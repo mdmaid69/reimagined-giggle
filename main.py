6b786d9fabbb84d3106a6a6950388e3adfee6a2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:34 +0000

    breakout education spin year track

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
