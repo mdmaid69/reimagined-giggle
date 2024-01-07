14798d489743e2c0c1dd84cc93c4dfcda2a3a084
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:23:12 +0000

    flight exciting watch possession reach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
