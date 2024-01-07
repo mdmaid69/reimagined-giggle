54038efa2f9d612682f80fff069fd5ce8810f3c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:11 +0000

    Uu example later season actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
