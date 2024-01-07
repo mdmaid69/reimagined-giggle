92c695d68e38fa7c9d2217d536d23a80b4492983
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:07 +0000

    comeoff doubt spite amount turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
