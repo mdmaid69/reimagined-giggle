6ebaa7e3c83140eca13765912ece103334523ba9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:17 +0000

    568energy case sweep attach sudden

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
