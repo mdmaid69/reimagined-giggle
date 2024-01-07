f68751a7d66937a6f035f2b67354b629ed6a5ffe
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:05:13 +0000

    string accommodation mind view interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
