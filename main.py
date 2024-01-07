fb3bfdf844e6ce138156dae749ac2bb5c25c38eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:30 +0000

    glad along fast wave market

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
