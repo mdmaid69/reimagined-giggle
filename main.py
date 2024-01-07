e63e89fddcf720fd83a7b7cb481838ef86b82c5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:43:32 +0000

    plan level deaf do afternoon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_speed(distance, time):
+        return distance / time
