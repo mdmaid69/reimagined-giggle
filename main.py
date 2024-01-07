8ab95bb963c4ffdd2dbb7f99986a50ed075993ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:48 +0000

    comein suckme child part plenty

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-print(sys.version)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
