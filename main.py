9beb20a45a4596298b6e344b312b2c1ee2048d28
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:09:49 +0000

    private turnup better damage drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
