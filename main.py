9b11f7fde950310d6e97a23ff1613992e651e88e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:45 +0000

    owe clever believe closely bet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
