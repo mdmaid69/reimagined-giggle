9c80dc678a5b1a288712fdf47b731c17af3c0751
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:26 +0000

    tidy pot official professional line

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
