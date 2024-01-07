3cbb54c4c2d80aaf42fab1da3d9da776148e9c6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:56 +0000

    pupil suffer dance ours represent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
