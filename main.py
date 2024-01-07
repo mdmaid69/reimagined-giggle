268f09f1ff7cbd3bc2421932bcfc6ae73089cc24
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:04:10 +0000

    gradually instrument offence regard immediately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_octal(n):
+        return oct(n)
