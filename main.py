e4e2498a8075f52801c38814137e6c4c1967cfa9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:11 +0000

    music action everything charge turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def get_file_size(filename):
+        return os.path.getsize(filename)
