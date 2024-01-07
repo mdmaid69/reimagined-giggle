534d7fce2d81c86a2e6067cd928c4b37df2c84a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:05 +0000

    cost corner keen introduce benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
