3b95409a50103151fe6847c75cbdba04b1b6782e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:05 +0000

    protest earth suit priority Bb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import random
-print(random.randint(0, 100))
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
