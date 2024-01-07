455bdf34a0ffa3a9e077acfdf13e26c2b782233d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:04:06 +0000

    usually out excited closely duty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def find_max(numbers):
+        return max(numbers)
+  def square_number(x):
+        return x**2
