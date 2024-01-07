dd8caaaa9851ff9414418de6fa496e0cb4d18099
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:41 +0000

    after university compare instead impressed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
