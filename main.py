737567a9f02e951d5de897e7acb7a15378644d9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:31 +0000

    unit decision force grand home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
