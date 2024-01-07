c0b816d0e982ac8e00515189202eac9480d28e6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:38 +0000

    ocean command keepaway allow jump

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print(sum(range(10)))
