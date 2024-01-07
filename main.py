2f64438a7bbf7f0a5601679a1833baa0458b7db6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:07 +0000

    family stare getup zero pin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
