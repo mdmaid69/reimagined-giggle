9b904410986396574e722f582df9e72e552d5309
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:12 +0000

    path perhaps invite watch punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
+  def square_number(x):
+        return x**2
