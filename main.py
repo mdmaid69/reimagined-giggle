246fb646d874185ecaf8ea4362b5e23f04f84148
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:39 +0000

    bus size live live vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
