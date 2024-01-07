42e726c3cbdd6fdc5fc23762a10b74864d72000c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:16:55 +0000

    either float trap we rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
