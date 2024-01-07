4f12cec3c7a3c3d1267aff4cff71411ad26d9dc0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:10 +0000

    that deserve always runinto swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
