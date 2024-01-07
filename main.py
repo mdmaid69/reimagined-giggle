b684cdbe5aef6e8c63e559e939dac30d16b402ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:12 +0000

    complete workon proof rare something

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
