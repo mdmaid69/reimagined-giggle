8adffddc4890ae90c3d429faab2da4df6406db58
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:16 +0000

    past tool delicate evidence typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
