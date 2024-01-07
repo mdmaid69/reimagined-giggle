4603b4376afbeed1bbe86dc3b6620d9616be4993
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:26:32 +0000

    busy carry finally takeback children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
