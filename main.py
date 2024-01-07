105f06c98f34a7a31483fd21d08e19da553064e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:34 +0000

    strict between oil weekend repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
