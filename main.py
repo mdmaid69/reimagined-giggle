52ecf21a740b1b54ee0157672c7a3df0aee2396d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:41 +0000

    house weather Tt about noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+print(math.pi)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
