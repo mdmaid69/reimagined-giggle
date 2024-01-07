d23e9b149dc2bffc31ea08c94c5587c2517afc99
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:58 +0000

    remove cook reduce classroom behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_max(lst):
-        return max(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
