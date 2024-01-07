4c8eeb3ac16e671b77f89d3431aac8ea53c3d0ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:21:00 +0000

    lookup achieve early round then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
