ea33743763cc37a25248f426eb9fb53667a7d05d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:42 +0000

    back next chin tap child

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
