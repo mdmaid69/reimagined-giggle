c4fc94205accc4fa69546dcbe4e3870031a501b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:30 +0000

    before quiet pride act violent

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def add_numbers(x, y):
+        return x + y
+import time
+print(time.time())
