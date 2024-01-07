910d8c5665a4614c2e7b669cecd738d32dfcb24f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:26 +0000

    face available lean get greeting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
