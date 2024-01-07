e903e40035c42e85e3621448ad65e6adf0edca48
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:34 +0000

    best peaceful market flight pressure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
