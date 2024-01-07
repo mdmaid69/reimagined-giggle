8ba72d3b8664c7ff7a8bec2c3a301f9a426ccb06
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:32:55 +0000

    thanks copy full true throw

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_rectangle(l, w):
+        return l * w
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
