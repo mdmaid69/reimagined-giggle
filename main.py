65de87b1dd028b80bd531e623734d38a4a15ed9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:53 +0000

    bury convinced cause if fix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 text = "Hello, world!"
-print("Words:", len(text.split()))
+print("Reversed:", text[::-1])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
