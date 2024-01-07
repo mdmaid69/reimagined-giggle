810aee64c70b2671ba07fa1cfa7253d3a1ecee62
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:45:52 +0000

    hot finger original us temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
