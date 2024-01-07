a02515654fa3254e39705c1113a09410f7dabd6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:33 +0000

    wherever sting hurt wave thankyou

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
