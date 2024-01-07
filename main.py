abb897044554738fcbf2e797de61ba65fb479bb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:39 +0000

    put fine range score variety

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
