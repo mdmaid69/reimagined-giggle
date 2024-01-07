51663da6b87d97f1c8c6145c2cf40097e9ac846e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:38 +0000

    strike clothes danger hurry hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def square_number(x):
-        return x**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
