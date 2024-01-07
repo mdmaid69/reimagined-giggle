6ade28c460a7b950ddee1153d3bf92975abe1ef8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:51:26 +0000

    tidy collection succeed water loss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
