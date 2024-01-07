caa91cd2209dc8c5c0bc56298ad93f0fc3c5cf41
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:17 +0000

    game zero back underneath finger

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_str(array):
+        return str(array)
