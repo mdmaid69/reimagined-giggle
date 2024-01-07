5a144e98675a27ac686cb37661d78084bb8a0fe2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:20 +0000

    base empty cost steep row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
