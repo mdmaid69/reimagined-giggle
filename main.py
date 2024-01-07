ec99a9b6e7d158afeb91b28816d01b42da8c34af
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:51:26 +0000

    at dream swallow object workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
