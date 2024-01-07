0af5e2685c4eb83881c2c92ebf9f88915b3edc75
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:51:05 +0000

    your float strike stupid return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
