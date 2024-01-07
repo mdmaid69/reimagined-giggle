ec22dc64ccf81a53e88d844126777884d9fb1086
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:25:45 +0000

    label sit line getout breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
