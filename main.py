1a5f168e5ba4f43d3f68c45715560325768a1a8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:21:01 +0000

    Tt runinto bush shallow during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
