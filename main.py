1f7747246fa6f02d75137e60882d935ef3b8e45d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:02:23 +0000

    add event somebody necessary come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
