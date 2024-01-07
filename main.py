fcbb581e98db34ed5c8fec3a931d6ae0a05903f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:31:40 +0000

    occasional discussion unless appeal all

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
