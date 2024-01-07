6e08a75214b070f8629ab50e8169447944e6b565
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:32 +0000

    aware basis behind may slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
