9fdcc2082930384655febc5093d7bd091c522520
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:34:31 +0000

    over human on really black

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+print(math.pi)
