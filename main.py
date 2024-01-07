6a728c99b1aa39454661d3cb8799e099eefe291d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:45 +0000

    mean familiar tap fly lead

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import csv
 def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
