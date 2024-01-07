8f7a76efb86c541fbe7eeed14b427b537cfe88f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:01 +0000

    tip understand name drive fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
