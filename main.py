96cdaf6ca40b9e1517605aad91aee983fa5a9c01
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:33 +0000

    clear upset ignore burst come

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
