d9bf336e8614ef348dd2a409c836d3b7a7938118
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:09 +0000

    transport whoever freedom change interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
