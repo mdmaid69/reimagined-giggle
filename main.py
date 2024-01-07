31e4ac84fe03994044788020197401613edcd922
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:30 +0000

    uniform state quickly path holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
