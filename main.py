2cb1b76b8a39ace12d0a7344c80db2a6f6d19e4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:28:22 +0000

    this after wall look space

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
