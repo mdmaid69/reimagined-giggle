cae2dd6dba5ea519ec5e63654e3ed2507b24e149
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:19 +0000

    total time provide band big

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_area_circle(r):
+        return 3.14 * r**2
