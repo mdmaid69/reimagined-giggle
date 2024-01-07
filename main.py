5a109a9c61fceb8391889b012b83843b9b313629
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:19:31 +0000

    couple step pool sleep thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-for i in range(10): print(i)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
