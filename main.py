8acfa1d5bd2251b53b5d49f084d0603a731852bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:26 +0000

    surprising ground myself we imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
