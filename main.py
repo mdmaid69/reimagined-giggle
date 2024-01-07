0740ad55b7a0258f81f5b32a049bbe43db48f0cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:08 +0000

    sticky false walk careless yourself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-print(sum(range(10)))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_error_function(x):
+        return math.erf(x)
