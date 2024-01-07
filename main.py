6cc48fe2c31287e3e5abd6e3d4e6cd2dcf6179bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:46 +0000

    confused frame tomorrow rain famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
