e44b718625d3efe6b9e581922b86eaa8df78a470
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:16 +0000

    comfort point brother bright house

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
