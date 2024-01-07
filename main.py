43673bc2e848f3808961d7b457e6f73e7b88c9eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:42 +0000

    late confused tidy likely collect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
