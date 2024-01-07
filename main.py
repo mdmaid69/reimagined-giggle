436f5afa6079a2a8ef04c0d0fe3c8d51def456e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:03 +0000

    eye sharp shop before touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
