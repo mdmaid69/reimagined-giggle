8ad9abd9875adab3cb930ce0f83fd29035d3ee5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:22:56 +0000

    unpleasant top still concentration concentration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
