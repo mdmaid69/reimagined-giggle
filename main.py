47b35626c9e3467f52e687ee7840d5abb6d6578b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:34 +0000

    she cum introduce fire goaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_min(numbers):
+        return min(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
