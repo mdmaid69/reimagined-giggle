7882b6015b5e5c754dfe560b6f9c8bb1a5f89d56
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:35 +0000

    upstairs crash breath border wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
