f51b5c79a4f28078b822c95c5eef97f1e7f792b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:38:53 +0000

    past path possibly sea apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
