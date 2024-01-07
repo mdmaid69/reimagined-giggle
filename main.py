6a55665de16b7f216d7cb63dc49d42edad0a4992
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:07 +0000

    regular name work Cc Gg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
