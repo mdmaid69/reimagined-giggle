ea01027ea77c25b7809c573e46e2eedf144229dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:37 +0000

    learn state tight work later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
