261d1f6a95914b6ae121610c489e4ed91ab1a647
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:10:01 +0000

    world offend sweet someone breakfast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
