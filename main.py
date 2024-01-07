714df6539966a708725c33395b1f7add58b93777
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:21:23 +0000

    cap unusual translate mend nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
