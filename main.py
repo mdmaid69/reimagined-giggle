59da1dc7cfa6b7b9f86d4c56c23def2552e9d01e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:37:43 +0000

    underneath speak consider oil programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def multiply_numbers(x, y):
+        return x * y
