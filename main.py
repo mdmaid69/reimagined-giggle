b35a14a85a6f41a57ef9a589bcd5db0bbb1ea8de
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:54 +0000

    switch target boat strict affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
