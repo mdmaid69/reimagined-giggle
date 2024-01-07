d4821ba51767009ae9d10554e6379da0a7bc9aa4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:19 +0000

    surprising talk runaway squash expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
