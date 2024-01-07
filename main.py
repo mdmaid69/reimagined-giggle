827377b46c556415b0e3a9285843696c33a7bc27
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:35 +0000

    appearance hers marry cry getout

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
