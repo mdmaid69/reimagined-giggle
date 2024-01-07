e3622024a9ea5be086059849a2b8eaf8889d6937
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:08 +0000

    another peaceful perhaps murder winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
