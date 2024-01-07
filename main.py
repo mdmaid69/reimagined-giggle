b5e28308fcaa314bc26894709b6808aceb4ce4a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:18:49 +0000

    trick work force pocket strict

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
+import numpy as np
+print(np.array([1, 2, 3]))
