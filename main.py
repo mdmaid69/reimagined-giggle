fde3de866605598606f85d148bc9c2026cc1ee33
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:50:21 +0000

    professional type generous pile fierce

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
