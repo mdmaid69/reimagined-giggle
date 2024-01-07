fa869d29fb2f0741562fb1d144c927f1bf56d3ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:14:21 +0000

    careless getout valuable house activity

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
