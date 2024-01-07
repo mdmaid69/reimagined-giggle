17d4250cbf32182ecbbd25527c95194718ca53df
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:37 +0000

    business thinkover vanish alone soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
