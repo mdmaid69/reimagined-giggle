2abe89fe13728beb130ddf11ca9ae72d4a222f77
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:20 +0000

    pretty powerful turn crisis matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
