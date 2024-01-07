3430e87fb8a66bf4296499203ba1ff951d669845
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:09 +0000

    hold spin shop pinch shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
