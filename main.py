4a373e5f2c0c0fb10922a541ff648ee797c3233f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:50:02 +0000

    excited Oo giveout different feed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
