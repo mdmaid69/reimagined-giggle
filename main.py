05bed7d5800f6eec47e1b8ab477479e2fdd0e385
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:11 +0000

    fair run minor wear continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
