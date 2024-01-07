0902dfc5c0f477866830416ad88a32224d121a07
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:56 +0000

    shout water shopping note estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
