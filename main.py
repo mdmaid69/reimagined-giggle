fcb504ae6850fdc3376102293d467c85da7ab680
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:17 +0000

    class thinkthrough unkind charge hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_floor(x):
+        return math.floor(x)
