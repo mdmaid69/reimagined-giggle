569838a8e38f7dd8f495a86dabce5d2823ce95de
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:41 +0000

    reason clever much chief thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+print([x**2 for x in range(10)])
