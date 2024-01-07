edbb9ff9790f40f72ef98d9593450f2a781bddc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:36 +0000

    thankyou race file exit describe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
