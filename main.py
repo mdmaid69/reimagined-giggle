f8ce4432e805eb2458e606ae9a35c21f04a271d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:16:41 +0000

    obvious mess tonight but quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
