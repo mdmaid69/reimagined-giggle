7633c3e192e9039a8e7007caf6eb239730b1ab7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:36:01 +0000

    phone careless father raise get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
