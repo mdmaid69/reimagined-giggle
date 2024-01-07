56df6c32954d484a7c11613ba8892f2ff82e954d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:09 +0000

    tear jump considering hide address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_max(lst):
-        return max(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
