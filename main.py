50c27d2f38ac8196076a6cbf9e31c5790043900a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:19 +0000

    basis tour spite tall behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
