af57ffd8eb69900a6cfd9943e90edea4fa1759c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:23:26 +0000

    runaway advanced slight bang butter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
