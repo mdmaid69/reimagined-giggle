4e95cc1ba54cd8406880c791f8617eea2c7f3a07
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:23 +0000

    project belong simple shoot suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(numbers):
+        return max(numbers)
