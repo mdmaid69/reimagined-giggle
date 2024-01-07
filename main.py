f44d9836c931bd7dd470d4984f72ae48b7bb8f9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:40 +0000

    pretty onto business divide Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
