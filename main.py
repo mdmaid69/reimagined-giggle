967694c717e02481253ec0c86d130fd5749d38cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:54:06 +0000

    stamp change continue consult bored

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import time
+def get_current_time():
+        return time.time()
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
