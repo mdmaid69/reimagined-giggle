94d326d638fb9385f0728049d8dea216fa87b287
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:51 +0000

    rush party equal rush dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
