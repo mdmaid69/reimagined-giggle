d11cabb27a080b1768876ac7274d590c8799a56a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:13 +0000

    gradually edge section settle best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
