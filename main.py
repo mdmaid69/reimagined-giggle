acd1c67eed28c9f728eba3cf4a5989013e0727a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:34:39 +0000

    protest gather straight left above

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
