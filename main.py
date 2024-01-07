f6e09dbf8066a3ca10cfd9ab1b58b701d5ccb53f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:13 +0000

    apologizeapologise cover eager round cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def add_numbers(x, y):
+        return x + y
