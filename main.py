08195a6a038c6453cb25be62b7a1b2c62068eb6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:57 +0000

    word workout building subject describe

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_speed(distance, time):
+        return distance / time
