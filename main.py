f0c46fb2834e05fecef2a14116752b2ad8b9b75a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:17 +0000

    of thinkthrough play fire centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
