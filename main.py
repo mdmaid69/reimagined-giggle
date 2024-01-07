fa1d608dce3f4fd3543458ca2c786d4970e46cf2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:59 +0000

    just hollow dinner keepfrom speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def sort_list(lst):
-        return sorted(lst)
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
