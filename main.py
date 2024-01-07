f69c456b78db4a5ec3017cf2fa52238fa2215704
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:38:07 +0000

    gas tomorrow search defeat public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def get_current_working_directory():
+        return os.getcwd()
