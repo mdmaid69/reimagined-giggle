3d36ec2baa607c4f50b7c4a2df1faa75933bdab9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:06 +0000

    feeling myself breakdown traffic we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
