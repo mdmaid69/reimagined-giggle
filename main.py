b841784a058e2631b17a0007a57e71a5f535b281
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:11:38 +0000

    handle circle reliable couple appeal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
