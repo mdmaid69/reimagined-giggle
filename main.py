d90a990706f8ff7806d271abded3c9b4dea3e920
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:46:35 +0000

    minority welcome separate issue progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
