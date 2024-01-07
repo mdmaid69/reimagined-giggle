7b4e004cc9d1c2beca2042352b45e45d84a647b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:22:30 +0000

    further where suppose left mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import sys
+def print_python_version():
+        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
