cfebfe351c226c60a51016ec0a3bcc203ae9ffd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:23:06 +0000

    pretend charge public basically get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_exponential(x):
+        return math.exp(x)
