737699e9ed92370f1fb36703711e4e6d609ba1c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:12:05 +0000

    post majority behave move instrument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def divide_numbers(x, y):
-        return x / y
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
