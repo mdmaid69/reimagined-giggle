325d740ba145cc4a00599255cf4b7700aaec2d16
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:57 +0000

    care who excellent imagine enthusiastic

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import math
+print(math.pi)
