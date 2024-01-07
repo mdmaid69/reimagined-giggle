b3513c93d6bddae50ba25dd0d7b96de0a44cdc30
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:49:32 +0000

    goback(to) car hurt hate hand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
