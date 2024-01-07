b7dd8af691cbfd575c06de9a8cb989d948309599
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:49 +0000

    Mm possible offend quit message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
