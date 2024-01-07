90c166ea6597de35e66a31a55830a6117d7db670
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:30 +0000

    pair beside coffee fast wake

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
