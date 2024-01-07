e2f55272907567c8c0fa32b659d37ea20bd6492b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:19 +0000

    label full decision trip exit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
