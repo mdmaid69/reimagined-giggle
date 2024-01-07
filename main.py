98b80df190f609cb23de76ea89ec3a32068482b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:28:55 +0000

    introduction level beginning whistle earth

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
