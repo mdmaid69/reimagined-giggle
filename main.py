24631445661bd946bc66aa7e6a2a1b7109562269
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:41 +0000

    complain fashion suggest sell ours

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
