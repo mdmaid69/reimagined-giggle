393cac570064c280b9c96915422b875487c74c61
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:11 +0000

    left feed treat breathe suggestion

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_pressure(force, area):
+        return force / area
