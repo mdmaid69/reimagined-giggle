b97e210f839d40799a9bd5b70368fa568e2de7d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:30 +0000

    original open cost measure along

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
