fddea72b692f6f2f2c14f0ef29d29f8f345bca16
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:46 +0000

    smile reduce keepfrom sticky pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import time
+print(time.time())
