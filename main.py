6ae0890f9dc76868af267f6c03fc09db1b122523
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:25:38 +0000

    mind praise usual prize anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
