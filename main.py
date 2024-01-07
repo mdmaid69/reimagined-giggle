9c835bae78400b0ead8d3878f06423fd533185c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:48:55 +0000

    light slice talkinto uponon punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
