7eb0dcd9d8a04adfd18eac29160de282f587f6c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:48 +0000

    party beside with pleasure shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
