0ab446f61dd3c37ca14422f9b490e19b456498fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:51:24 +0000

    wild communication future talkback nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import time
+print(time.time())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
