c5020013c31603833bab454cef476e5430145fcf
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:51 +0000

    before profit giveout Dd movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
