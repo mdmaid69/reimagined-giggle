121a76a5e3f17ee6d97636bc8512633e47121575
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:40:35 +0000

    steep why tongue smart health

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
