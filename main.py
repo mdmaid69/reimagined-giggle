26f6fd3e15945814f6df88d1a9128674233b860c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:49:29 +0000

    warning yet contribute sleep window

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
