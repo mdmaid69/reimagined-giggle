870835babca03722e23a14c8c1c265d69d75ff39
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:07:33 +0000

    illness right please direct improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
