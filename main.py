bb15b7fbc873d97724493fce96e5ab711413ac98
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:59 +0000

    guess difficulty split hold moral

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
