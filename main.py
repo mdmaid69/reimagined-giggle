fcc6af225ebeff92a2951e4b694056e86a38860f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:32 +0000

    married communication letter follow rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
