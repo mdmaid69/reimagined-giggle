fa60d2ec68754ae063ba9e62a201b0d018d03759
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:57:27 +0000

    secret time dead lost though

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
