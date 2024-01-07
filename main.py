b45608a2a5dc3c096590f16931a1d558dff51f20
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:05:38 +0000

    squeeze team sick succeed scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
