a884812260074db43f0a435cbb16c09607af8b92
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:59:21 +0000

    behave scrap still tap on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
