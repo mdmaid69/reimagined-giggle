50b44d0586765cd516766092bb209aa1e63d723e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:39 +0000

    language container connection anywhere dark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
