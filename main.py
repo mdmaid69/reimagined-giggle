0ad17bd7db0dede0c1d8b9cf808c5c1384c6330a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:55:26 +0000

    breakdown calm behind everything private

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
