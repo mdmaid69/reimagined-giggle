b14ef316dd645e6f18328ee853dc8b90ec14b881
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:21:18 +0000

    confuse light hope I bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
