6a1108740e20a3631bd9261cb77d4482cc9dee71
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:56 +0000

    past boring love declare lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
