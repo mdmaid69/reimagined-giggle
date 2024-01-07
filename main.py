92c7e6a3c680f2a2187eb04b6262fc836604f493
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:49 +0000

    claim seriously return address relative

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def subtract_numbers(x, y):
+        return x - y
+def calculate_work(force, distance):
+        return force * distance
