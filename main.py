a1eabd9776f30e262f962d2a49948f72cea51962
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:06 +0000

    cross tell account opinion pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
