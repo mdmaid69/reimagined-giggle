46e6d4fc1c9bc4027bcb0ddd04b1069b26a183c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:34 +0000

    accident recover film interview reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
