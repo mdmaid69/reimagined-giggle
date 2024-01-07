1ef021cadd86bf78321a593cee3fbd1311993d6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:39 +0000

    possible constantly circle point suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_counter():
-        return collections.Counter()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
