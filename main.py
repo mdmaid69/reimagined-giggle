06af71eddebe43479e6453c800fedeece5ddb110
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:12 +0000

    remove arrive bag question hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import time
+def get_current_time():
+        return time.ctime()
