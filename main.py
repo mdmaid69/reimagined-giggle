1e4bfb989506e33ac30d8251d73692bec94dfa70
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:27:50 +0000

    hang other lead compare agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
