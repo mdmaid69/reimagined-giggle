2007d069f612f8afea9bddbc5025dc00385b8413
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:42 +0000

    impressed unkind amount display emphasize

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
