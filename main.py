0d51d56ce9c059955c6a809616cd053b1e088417
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:35 +0000

    demonstrate size doubt everything first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
