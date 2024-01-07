51f27b8141683849e80ea4a3d96df431d6c94a61
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:19:24 +0000

    holdon jump why listenfor glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
