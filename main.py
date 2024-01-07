1884430e243dc7eb8ae1adff4e8a8fdad14df998
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:20:01 +0000

    total order image name scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
