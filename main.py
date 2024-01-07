7d77b1275c90b774812266c9bd161650b9edcc4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:57:57 +0000

    community illness travel rush where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
