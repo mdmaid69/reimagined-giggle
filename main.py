e10d0993d4c0655ee5c623c941db4f5b79e7d94b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:47 +0000

    any temper lesson disappointed fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
