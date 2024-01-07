0d3a48724a6126db325eb7fb1673cb895a0f36dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:54 +0000

    fetch frightened set bringup comment

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
