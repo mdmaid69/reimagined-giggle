bf932dac8f5c9999b659aeceba14696356cf34c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:02 +0000

    do weekend whom combination crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
