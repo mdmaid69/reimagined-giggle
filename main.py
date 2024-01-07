771891bdd1ef7dbf247b04adbd02ec557d0c33a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:10:45 +0000

    old disturbing meeting apart knowledge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
