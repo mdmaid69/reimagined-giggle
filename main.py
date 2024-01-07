d2f04c3d7e2203cf4e12b8b5f07c4c65f8783580
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:52 +0000

    discussion blow similar weather release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
