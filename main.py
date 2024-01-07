e4580ec703a91b7390022d82529425cc33252efe
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:30:09 +0000

    once about naturally border programme

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
