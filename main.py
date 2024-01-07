622778c62d4a4c068d30c6bc3e6828b01de2c7ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:01:52 +0000

    level call favour stone completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
