c14ba00accb1ec3ec8a05d6e2d9821d6e3c29f79
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:03 +0000

    straight then lay compare rise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
