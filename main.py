69f9d2df3b98caa4b7906ba38936cdd063509345
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:55:26 +0000

    office independent much town act

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def find_max(numbers):
+        return max(numbers)
+import collections
+def create_priority_queue():
+        return collections.deque()
