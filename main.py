be1e95a954d4ecf3249df10d251b4c332fe0e23e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:51:15 +0000

    play almost please suffer crash

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def is_even(n):
+        return n % 2 == 0
