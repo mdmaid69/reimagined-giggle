0fe564234d03046c8045f2c3c14278e605fbdc6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:00 +0000

    disappear lie Itself bang but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
