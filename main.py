c085076df2889d6471300ffa53cdc4860e1aa3db
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:18 +0000

    ever pump gradually happen wind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
