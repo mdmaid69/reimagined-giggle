2eb1e4ddf7178c16e54d4555955faafcd85f87d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:42:57 +0000

    regional design direction sting bringup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
