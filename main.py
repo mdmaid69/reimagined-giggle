6ef4e2ca16b19d39f115a9983c68d38ecc742a45
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:05 +0000

    clue anything count rough rush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
