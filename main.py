1052bcaf8de37efc1578bb6572597ea5714bd13d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:07 +0000

    rely road interfere detail standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
