2943c669528d2acb84b7082454648086846f16eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:40:09 +0000

    instrument rush fair region concerned

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
