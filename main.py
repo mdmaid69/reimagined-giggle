72ff86cf288379f4201c3a96c2f58fdf42e17c2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:17 +0000

    distance split stir trick sure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
