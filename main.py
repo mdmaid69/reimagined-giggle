4424d96e83ad7bb02b79fc3d4c13e0fbbf5f06c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:37:38 +0000

    jump runover fashion can count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
