76055eb742317903ff9885f42653571e64694575
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:55:41 +0000

    other bury blind frequent mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
