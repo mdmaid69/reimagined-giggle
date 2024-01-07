ad132e8e120fa3698fd70829509073739fc35bb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:37 +0000

    that mood yesterday good create

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def remove_duplicates(lst):
-        return list(set(lst))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import platform
+def get_os_info():
+        return platform.uname()
