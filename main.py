b1e4015edc69e91090c88d55848411706140e0b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:27 +0000

    turn finally sink cross bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
