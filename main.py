da64b51f365a009035830c15a5d4c6ab9cbc1d20
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:55:05 +0000

    getback pause backwards morning quick

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
