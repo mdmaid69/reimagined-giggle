5472eef138083e64c2fdfb653a611aaf354165c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:56 +0000

    leave breakdown case if far

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+name = "Python"
+print("Hello,", name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
