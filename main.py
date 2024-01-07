d0f995411b6fd1b3d47b5e45df1bfd07383edee8
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:21 +0000

    bored related walk fall forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import numpy as np
+print(np.array([1, 2, 3]))
