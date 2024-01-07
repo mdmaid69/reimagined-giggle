87e5a7139834386b54b9546830d8accd44b1ca0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:23 +0000

    extra loud whom hollow game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
