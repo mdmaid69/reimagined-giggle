dbbcb0d3b0766e6854f2e70c0146ec3a3f86a8ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:33:22 +0000

    scale before coffee runinto happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_speed(distance, time):
+        return distance / time
