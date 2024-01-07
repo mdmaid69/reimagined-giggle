641f8152c49868c342179cd5e231772f1da4ca2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:54 +0000

    wealth free near stamp like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
