36a27defc011c96e2c106cf4d0ddbee695d54b27
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:13 +0000

    practice butter capital light callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
