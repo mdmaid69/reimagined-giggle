b45aedecdf5beb81c8c33294b31d39940fff3c80
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:23:41 +0000

    onto admit tradition moment answer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
