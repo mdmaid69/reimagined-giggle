bc07af117a3376c958cf59eaa19639722848a1f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:53 +0000

    which split snow pattern although

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
