1a3314ad5e6a67fb3fadb59b9d31117edaed68a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:29 +0000

    mind their sense shall extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+print(time.time())
