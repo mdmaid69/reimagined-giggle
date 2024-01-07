84274ba6d3e13a6379d0fe7050eb3711a866647b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:52 +0000

    whenever live smile 564encourage confusing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
