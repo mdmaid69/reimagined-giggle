3e7c145243905987583869ed881b2ca26b168b81
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:31 +0000

    choose boring workupto nice apply

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
