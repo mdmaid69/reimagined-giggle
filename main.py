9c25f167abe485b02693862d90db7984eacc8a38
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:14:46 +0000

    exactly detail display 569enjoy lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import sys
-  def get_python_version():
-        return sys.version
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
