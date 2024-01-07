9ab36dfb1dd5ebbe17de07168202546b273aac97
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:34 +0000

    takeback write will exist centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
