60220dfa30f61a0cfb298ed7b4b2b30db0b6f389
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:16 +0000

    skin manage pile love famous

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import os
+print(os.getcwd())
+  def sort_list(lst):
+        return sorted(lst)
