4dd38dbfc99a7fe2275cb7c65616df15313c55dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:18:22 +0000

    crash represent through so die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def clear_array(array):
-        array *= 0
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
