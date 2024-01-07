6ea957c7a6e3354a971984941b67bd1bbe4d7a6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:59 +0000

    goon geton preparation trip Ff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
