52d7dc2590c76163e5606a077dba5726fef33e0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:12 +0000

    difficult limit problem pleasant flat

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
