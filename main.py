8fac72ac776af3c7765dffc08d97172d070f6d03
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:56:49 +0000

    wake final exist bath without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
