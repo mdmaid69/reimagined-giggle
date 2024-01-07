17e558de2cdca8f761fe57a9279b3c3972b0b146
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:53:29 +0000

    mail violence spin temper now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
