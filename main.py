62d241a9196cab9babf62622fac0f298539124a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:28:41 +0000

    fly throw cinema Ww there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
