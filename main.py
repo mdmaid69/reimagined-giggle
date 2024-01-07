d530ca0e9f5a7c8beb342eb68fc1cc211baee796
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:53 +0000

    quit trip row sudden almost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
