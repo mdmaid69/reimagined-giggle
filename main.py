b314b225759b3de1481bc586f04c3e261ebc5e16
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:41 +0000

    justice share urgent stop ride

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
