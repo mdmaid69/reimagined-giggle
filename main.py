8a4e7707fd466d97394fd0e245deb43286358b1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:02 +0000

    theirs smart keen passenger whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
