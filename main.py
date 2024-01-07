c915c03b1eab4d4e9e8a4ecda3648887dcfcb3a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:24:38 +0000

    shade risk father grateful muscle

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
