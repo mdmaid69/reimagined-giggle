0f7432be4f04cf99976b61de759b41415748951b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:52:53 +0000

    cheque make brown medium decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
