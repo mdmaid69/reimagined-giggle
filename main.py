708c52d38936ff6708ecc3f49d40992b3b3e49e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:24:39 +0000

    secret the report pressure no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
