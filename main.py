47080586261c487ef970a68c4ffe02607520449d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:28 +0000

    cry film information present serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
