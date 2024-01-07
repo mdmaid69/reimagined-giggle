1e31ad39993741db88c59b365c64bc6482610ffe
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:03 +0000

    control visit hear along climb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
