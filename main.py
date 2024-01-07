2daec116744dd5efdee5e85ff87ebb39f0748ef3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:45:55 +0000

    previous anybody crowd exploration film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
