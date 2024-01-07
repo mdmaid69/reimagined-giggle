41c6557ba33369bbdac6ae913e31eb4fc6f7a2f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:00:10 +0000

    tip where who centre maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
