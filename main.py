117c9dc2fd6bddb2bc30e727bc086d5935d9bf11
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:15:15 +0000

    division present laugh belong inquire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
