354fb24714ab468e9046832d740f5d486c45b96a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:25 +0000

    mouth birth turndown Ww rain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
