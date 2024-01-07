36f4d00ea197b57b602a2c4eaba85a655b02c3b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:49:19 +0000

    sweet communication rush argue now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
