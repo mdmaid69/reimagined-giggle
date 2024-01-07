e0fbebf77ccdb26354ba6b39a9be89ec5634c550
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:50 +0000

    total on mistake blow it

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
+  def is_even(n):
+        return n % 2 == 0
