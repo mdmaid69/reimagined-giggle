4fcee8b9e7ec249d4ed2a563563633f3b144f845
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:09 +0000

    murder get admit criticize fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
