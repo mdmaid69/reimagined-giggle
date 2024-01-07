1eccf0bd5bf49fbe89196c69c72cf68fe1d30ed6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:31:09 +0000

    slide throw record everywhere know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
