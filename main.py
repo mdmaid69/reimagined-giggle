5dc16f4ba991f903212fc37ecf7ad5cf607fbaf0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:29 +0000

    exactly ahead same central press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
