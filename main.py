a919ffc8ab6170bf5c67a7f850e468d70be3ebd5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:51:17 +0000

    react but standard like grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
