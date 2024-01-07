651762a62997086e5931b1fed36308490fcdd9d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:40 +0000

    duty control tomorrow sing tape

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
