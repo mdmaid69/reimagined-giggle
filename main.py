28e74e6996b7a02fe65b8a6118dca6bf7526ed98
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:38:32 +0000

    attitude tap steep journey post

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+for i in range(10): print(i)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
