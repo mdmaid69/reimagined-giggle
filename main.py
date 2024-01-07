1953eaa902ac3a98636fc12c4a5d66024bfcd1b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:01 +0000

    set knowledge prepare suit generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
