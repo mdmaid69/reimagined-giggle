346a1b73356941626cfcd32404d653400f109a0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:38 +0000

    sweep reach married recommend aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
