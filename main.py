ff217d4e6af68346e7e5fdb21d57c39e6ace7e8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:01 +0000

    any outside luck bite circle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
