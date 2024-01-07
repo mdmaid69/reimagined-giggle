c85e49873eda851fab18cee1fbc326c906ee93cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:14:18 +0000

    number print slide emphasize estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
