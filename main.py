ac91816daa157de27f75b5e26858613d6ad44aaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:19:11 +0000

    noone clever busy wake consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
