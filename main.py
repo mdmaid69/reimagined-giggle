926a065edff0bfa10c9a84a5e40838a5223e95ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:34 +0000

    naturally fix take bottom phone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
