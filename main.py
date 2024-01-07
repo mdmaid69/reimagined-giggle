988eb796b11a53c752faacf66c08302765010e51
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:28:27 +0000

    contrast word weak bath slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
