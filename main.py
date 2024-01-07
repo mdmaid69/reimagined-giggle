141a0de23b2fbf946ad1eb7680694ed818efa403
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:22 +0000

    lend can pollution date dangerous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+print("Hello, world!")
