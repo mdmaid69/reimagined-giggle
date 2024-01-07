3eedf795ae5a2dd17049b0c25f078d7f206e0eb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:08:27 +0000

    frightened mix accept quality case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
