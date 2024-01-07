7c1d5ba84b15ec7a4e5b7f834c37382fc627d020
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:17 +0000

    unfortunate music maybe word who

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def convert_to_octal(n):
-        return oct(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
