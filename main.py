1136f937387f33d59be95feba62f3b680ea3064f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:37 +0000

    warn spread harm Qq shoot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
