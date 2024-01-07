b3744312ef98b3236fb92906e832407bad7c4f57
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:47:13 +0000

    squeeze shock both year storm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_floor(x):
+        return math.floor(x)
+print([x**2 for x in range(10)])
