196020a48e779e6f938a2009f204ce0ac3864259
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:29 +0000

    other burn shout reliable deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
