3487211cef26a28eb2bf01124e60275462809077
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:15:34 +0000

    shine movement born spin connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def remove_from_array(array, item):
+        array.remove(item)
