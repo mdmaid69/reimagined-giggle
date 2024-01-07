75aba0586d97bd91172b2b2b4efb3e94afea4c37
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:19:52 +0000

    insist add to mess turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
