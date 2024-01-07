75a9bb2789d603f26612d4d6a7619db7d1598b5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:39 +0000

    dangerous safe plate dark hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
