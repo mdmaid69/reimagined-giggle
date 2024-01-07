5161f5e677d11bbbe607b55f9105fe27d80009be
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:52 +0000

    compare duty form new trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
