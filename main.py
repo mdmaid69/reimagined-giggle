48a486aed83acc167bd03a238a9585a5ec669373
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:33:06 +0000

    still smell different shop period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
