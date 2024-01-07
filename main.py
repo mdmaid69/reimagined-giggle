f6013d7ba5870b4ae3fcd03696d0b4bd6fa2c7de
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:27:37 +0000

    any sit benefit take evening

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def is_even(n):
+        return n % 2 == 0
