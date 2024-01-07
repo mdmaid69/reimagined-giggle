b004879a9d6c441d8f709cb1ee4a784c171a76f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:17 +0000

    world agree explain giveaway hers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 numbers = [1, 2, 3, 4, 5]
 print("Max:", max(numbers))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
