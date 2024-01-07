90d412226cc740db8ee2da710ff8d65a1e9acc71
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:17:16 +0000

    rate shut pity introduction year

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def convert_to_hex(n):
+        return hex(n)
