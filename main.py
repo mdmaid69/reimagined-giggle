ff5ca6f0b8db54d51e08a7369e63d4d16919b708
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:11:06 +0000

    string trial battery emergency be

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_power(work, time):
+        return work / time
