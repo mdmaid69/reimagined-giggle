a43612a9c7a937986de8755e4eeb93509fa9a870
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:42 +0000

    disturbing wipe rather give note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-x = 10
-y = 20
-print("Sum:", x + y)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+name = "Python"
+print("Hello,", name)
