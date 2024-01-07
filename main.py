6f7e188793f73f55b6903d1995e0d8b2d64db138
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:50 +0000

    emotion accordingto care how pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def get_current_time():
+        return time.ctime()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
