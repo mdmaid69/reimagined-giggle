7faf26d541e0bc18dd2dc56e5fb3fe4d2a4fad15
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:36:01 +0000

    injury hear mistake 572enter history

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
