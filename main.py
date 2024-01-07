e5ff2d7552a57357bdf3ac4ddf04ab53d1d2dbd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:54:46 +0000

    Ii safe lead answer ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
