00468488989427c5ba1aefad014308b8fd565f92
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:29:35 +0000

    reasonable treatment back callup confidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
