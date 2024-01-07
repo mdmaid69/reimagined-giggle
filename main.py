26a45bd786152b857f2c30a8b021d8ace451ef15
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:23:17 +0000

    brother raise poison recover interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
