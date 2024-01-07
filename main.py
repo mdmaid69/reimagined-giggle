e2816c76dacd695fe4caaaab2eb04be43a18701f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:54:49 +0000

    expensive zero after throw trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
