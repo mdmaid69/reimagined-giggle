f3d527a6f3ad3db9ffa993744a1f22214fc517fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:47:23 +0000

    star makeinto sweep spoil close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def clear_array(array):
+        array *= 0
