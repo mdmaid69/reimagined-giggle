4af44624a89a8711b1056ee4fb3860462b94857d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:25:48 +0000

    weekend negative surface cruel light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
