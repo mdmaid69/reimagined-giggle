c094703c7096e6bd81741a52ca60036aecbe0ef9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:24 +0000

    fold group wake anything bet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
