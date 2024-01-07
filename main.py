d1485c8c71e6ba6e19decb8db517a36d7f1e1200
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:16:26 +0000

    Ii suit stone high mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
