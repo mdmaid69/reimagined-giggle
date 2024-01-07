8bf17e244bba699f30e982465f805f2886b7c207
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:12 +0000

    around condition opportunity board destroy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
