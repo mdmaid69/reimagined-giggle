31a239cd3cc44c97caaf4b8550522608118440f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:16:09 +0000

    if emphasize gas holdon government

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
