c22cab29c1db5a863fe297b18b42b305a84b37e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:59 +0000

    pencil wear whole possibly sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item(array, i):
-        return array[i]
+import os
+print(os.getcwd())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
