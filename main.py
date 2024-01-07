518b5ebd4d5563d7496e32d1b153ec600cc34336
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:07:24 +0000

    belief teach rain dish choose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
