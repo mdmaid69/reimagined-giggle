0f44dd096fc3c7722e792425ea153b0b62afd2ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:16 +0000

    start criticism answer peace convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def remove_directory(path):
+        os.rmdir(path)
