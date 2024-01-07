ed70009d9188481a7c275c3efec6d5be5ea2fb34
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:53 +0000

    service food term advantage cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def log_message(message):
+        logging.info(message)
