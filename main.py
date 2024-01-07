a8976c8975aad589ea616adf7a03e49537893728
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:25 +0000

    comeout fetch early invite passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
