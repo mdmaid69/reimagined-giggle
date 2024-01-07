5a69aa186fac0a281df925ec2e664e16da31be4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:39:06 +0000

    telephone actually Ss use female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
