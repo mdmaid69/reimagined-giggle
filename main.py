03d49e631bcb9ee027667b1be09098be0245dba2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:46:26 +0000

    reply cheque workout himself reasonable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
