ede0ebaac26736e8c07afa84122b27a5f3de55c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:25:46 +0000

    approve pair effort visit especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_itemsize(array):
+        return array.itemsize
+def remove_duplicates(lst):
+        return list(set(lst))
