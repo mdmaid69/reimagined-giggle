bea5705fdc6577fa7b912c65d0e166b28833ce24
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:47:13 +0000

    law eager broad advice since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
