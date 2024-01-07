2197ffb9fa49b14107f0ef7c0a87ec907f7bd058
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:49 +0000

    plant anything indeed slightly freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
