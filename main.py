6f9fd230e03a3401dc9dd8d8611034f48fc08b96
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:07 +0000

    success makeup future communication obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def convert_to_hex(n):
-        return hex(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
