87df5e6ab96536bf1d9fe83f44e9ac109483bd22
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:41 +0000

    throughout name better speak bringforward

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
