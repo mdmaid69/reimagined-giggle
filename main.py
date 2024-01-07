7a5d6e692fd1b3707ed6040ac49e27ee64d009e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:41:40 +0000

    classroom sudden round dry speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
