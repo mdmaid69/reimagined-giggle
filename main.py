72f37f3f40226254d829d7646c7a0eb892822013
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:38 +0000

    rain occur style rent next

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
