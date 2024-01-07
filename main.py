dc9e75753235c8be8ca0406706611d78512b292a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:37 +0000

    answer letter contact university front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_pressure(force, area):
+        return force / area
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
