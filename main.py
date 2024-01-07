bec9a055a9dfa7a7f982bd989ea1761855397e47
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:40:25 +0000

    temper several result transport hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print([x**2 for x in range(10)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
