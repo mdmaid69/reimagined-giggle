f0d1c3e2ec04a59f11cf170316825329a36e6d38
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:43 +0000

    soon frightened admit whether how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
