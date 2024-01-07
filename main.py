423a57643d56bbc1e6e3ba986f99e551200a7094
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:42 +0000

    round impatient centimetre loan whichever

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
