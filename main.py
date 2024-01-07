b5f1fc5704f6cc209103a31d76d17b3060fb66bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:35:17 +0000

    underneath warn display myself probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
