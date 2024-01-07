0ac26d86839b247e92c25da234a732d6fccc64f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:51 +0000

    risk shame favour mood step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
