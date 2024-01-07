a97dd2555b94d9dfa2a418689d06655f3a938fdc
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:36 +0000

    motion glass long contribute declare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
