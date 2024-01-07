1f451b2476e903ea2a34781f73c2006e04819f8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:00:19 +0000

    activity now exist compare place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_current_working_directory():
+        return os.getcwd()
