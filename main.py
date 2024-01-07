0378406aa3935292addb7c9933315f965b5831c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:22 +0000

    play flow anywayanyhow independent pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
