d92c93000f0a1cdb2932a40a44e7c3cba30633d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:10 +0000

    pile best scale lower unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
