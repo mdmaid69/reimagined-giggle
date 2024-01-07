cd0ba0814ac5fc412f4e4c2e59a40e67aba8feb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:51:03 +0000

    pot expensive scale various bringup

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_to_octal(n):
+        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
