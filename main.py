11eb74e94aa04e815c40b79123917ad7518c52bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:28 +0000

    back nothing keepaway zero father

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
