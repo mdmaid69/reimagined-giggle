47dd57dc4ef1d1f93d7d4457c0246f446e9acf88
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:33:52 +0000

    by safety probably something condition

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
