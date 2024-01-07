8ed87408b582359ce4da00df0523ace5044da951
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:26 +0000

    intend turnup suit run now

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
