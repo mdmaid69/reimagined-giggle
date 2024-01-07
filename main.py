8512016c0eb7d0191d21bdd06ac9e41b8bcbba4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:27:53 +0000

    embarrassed reason sound matter grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
