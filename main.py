05d748b7087d9df40a178b1027c310f0b3373435
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:58:00 +0000

    behaviour leg body little crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
