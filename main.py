dddb7b2a9a0239042cb7f3e9bcbc86a9fb038910
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:15:08 +0000

    world worth be themselves me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
