c04030ae91174f5ad5ef31f9c6865e6edeaf0db5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:38 +0000

    store pour knock practical anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
