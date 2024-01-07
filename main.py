3eabcae968c6b423574a137d5a40aba19a4f177a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:34 +0000

    environment brother from check never

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_user_list():
+        return collections.UserList()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
