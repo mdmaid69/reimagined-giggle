f95deb0fdd8937a0c5a27395fb5f7c2ca8da4123
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:29 +0000

    constantly look breakthrough less great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
