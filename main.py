d2d8ba465a6da96a10dd18cd9dc2efb0d5290b29
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:16 +0000

    nowhere minority swim print spread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
