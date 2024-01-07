ec64239a2b0494d3bef43264e10911f8f546872e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:08:50 +0000

    train brown declare commit politics

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
