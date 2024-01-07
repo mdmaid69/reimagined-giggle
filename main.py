ab5519b9a52529013665e2b549f1a9fd4a5ce8d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:51 +0000

    wipe recognize suggestion stretch all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
