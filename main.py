da5c2f67ae83c495bba6c1e24291b6040aaabc3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:23:46 +0000

    closely runover roof most grateful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
