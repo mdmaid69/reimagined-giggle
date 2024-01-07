6d50f41cc2b59e38c8d62c6b996dcbe69667f97e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:45:37 +0000

    affect thinkthrough switch drive high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
