547301d9764001d55843256bf960bc8fca03547a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:12 +0000

    off question shall slight loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
