89375e44930cc5fa01a054f0f6b10c1df69059ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:48:13 +0000

    alone study pump Dd behaviour

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
