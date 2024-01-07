8876b40fb85b8312826530c5842aa1aa0454b5ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:19:49 +0000

    Kk show loud steep burn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
