23bda4390799244bf4c8f406190b6ec6789ea37b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:12 +0000

    unite flow patient present rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
