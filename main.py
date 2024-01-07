882afa96e74db85e48c1d148d163b10c35c85f03
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:49 +0000

    spite their confusing dress equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
