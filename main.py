86cf5df18ec4a5a51d71c99e3a42e605bd172790
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:50:57 +0000

    fashion breathe principle tap way

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
