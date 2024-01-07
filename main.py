2d2523cf4205cb06891f6bb313e541d9f71e5ba2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:29:16 +0000

    precise him surprise pair year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+from collections import Counter
+print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
