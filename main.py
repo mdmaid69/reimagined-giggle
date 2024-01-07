2e5941ba89ac7fee269f20243e374c2d83c9b36b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:39 +0000

    shout arm face side pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
