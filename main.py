15ddd76577afc6fcca95d9c14c3d37d0be15a623
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:58 +0000

    boat address far week thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_stack():
+        return collections.deque()
