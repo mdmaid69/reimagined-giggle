265fb8dcabf2724bb7a3b372996d3efcc1cbf51b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:25 +0000

    quarter ask war pretty division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def square_number(x):
+        return x**2
