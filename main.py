2bb856e58031be4db6a7ec3859c2211b242f1bb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:07 +0000

    limit reasonable record minimum pass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
