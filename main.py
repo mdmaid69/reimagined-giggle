6e022ec717d881d55b37ef4e83ff99f3af98436b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:06:30 +0000

    guide herself if Hh reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
