f8d2d448f678778ba6d5980a39d9e7def12da4ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:29:44 +0000

    pressure word child finger open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
