9da44d916d214d3b6caa56081346f3c7221ce321
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:16 +0000

    news mark sign avoid display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
