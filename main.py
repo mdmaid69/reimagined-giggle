ea0605334d7802d224100032a85413616d6a40b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:45:07 +0000

    goaway bank now include comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
