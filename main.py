c47ba3acbaad4f2240d5f70944b7aa68384ff707
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:21:18 +0000

    comfortable tonight appeal quickly reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
