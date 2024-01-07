bc9c055a0d7c7d97a84cf96382b9a1afd1ba6dd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:25 +0000

    Yy sink responsibility punishment relax

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
