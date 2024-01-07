4fc654148c1afdf70ae7cfe0b4e7d4ae7f840d53
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:19 +0000

    travel freeze difficult mystery in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
