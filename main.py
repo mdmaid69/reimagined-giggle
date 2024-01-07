6fbda91455c4859bfebe09a7a1dc9e25e08606c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:04 +0000

    frame point concern fit scare

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
