a8c1bc80053da746fccb0f07befcf2ea3eeb29ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:37:01 +0000

    rid post land desperate Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
