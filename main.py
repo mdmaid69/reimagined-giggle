38af5abc5e6f08ff119e4d38ed5c19f533f12e64
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:59 +0000

    point accident yes sound pick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
