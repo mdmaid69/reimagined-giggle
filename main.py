2362cb4a013f5d27f1ddf5ee667ccfbcae85d212
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:48 +0000

    fire lock choice more certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
