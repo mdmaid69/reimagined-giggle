aa853a5e7e0995bdcf84cc83fc80dc038df65ea5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:51:45 +0000

    reserve goback(to) collect reason comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
