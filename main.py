b253caa6e77905ce15ee27453f5fab66ffd03b31
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:10:19 +0000

    bend warm town guilty search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
