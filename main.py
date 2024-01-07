5f1839e49d914c106be32f29750a98662a1fbf9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:08 +0000

    love winter exam belief top

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
