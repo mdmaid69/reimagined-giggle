1f28ce44721a809cffda64c02ac93dd2c69ddb2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:42 +0000

    next strange within throughout set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
