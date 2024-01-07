e5a766ce82474cdff33b6009e95a68eb9fa9f45b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:06:35 +0000

    again when chance must hardly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
