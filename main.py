27bb70209cdc301288a7c80103000eac479f7af8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:26 +0000

    interview runinto idea hang thought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_power(work, time):
+        return work / time
+  def square_number(x):
+        return x**2
