737f20faa47b7d9744c87c84e2e7bd449e034359
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:10:42 +0000

    have injury long thankyou past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
