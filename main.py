05dc80ca64c342369f9c3064fd59c3ac38259eec
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:14:33 +0000

    afford up belong pretty straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
