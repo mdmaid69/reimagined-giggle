760f28d743d23682df8260dd9038634bd0b70f0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:17 +0000

    treat surface heart rubbish important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
