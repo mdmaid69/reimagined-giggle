6b1d8e67ebb22de12f558b1c8ab7d12a3a617636
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:38:59 +0000

    near half heavy holdout comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
