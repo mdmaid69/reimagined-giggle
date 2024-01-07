ba1397a9e9ea716efd71579891d5d10d4c51f0c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:11 +0000

    number practical command drop fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
