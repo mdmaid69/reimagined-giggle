7995ff50aa489651231b5a51d0116679db73a1a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:45 +0000

    present sick rubbish loud mother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
