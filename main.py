485481cbc1551267221e86deee2bc3820764a066
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:30 +0000

    gooff giveout sort accidentally call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
