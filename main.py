b1663a3f5ced22d965d87df4fb9416356f4ca6dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:06 +0000

    clearly die point basis now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
