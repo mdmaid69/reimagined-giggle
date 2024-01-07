5e754ebf52a9982df7e3069401dae19c19428fd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:54 +0000

    pump responsible get suit lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
