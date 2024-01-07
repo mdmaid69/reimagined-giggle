bfc810f10ea4620594859eba7ae2b0cc0bc9d385
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:45:01 +0000

    community completely will ring safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def square_number(x):
+        return x**2
