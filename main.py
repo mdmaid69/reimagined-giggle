5fd1bd5741703d26bec7a125ad79270836764c12
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:23:32 +0000

    yet flood no statement in

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
