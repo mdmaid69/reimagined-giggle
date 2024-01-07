95e3adba7829b588aec0fadf54947e0cbf68fac4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:19 +0000

    tall prize person judge pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
