c64c47eb391f411d01d4bd911cae0296708d90ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:36:31 +0000

    face count eager basically book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
