0342b69adbeb1af217bacc15762f28f50a23c107
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:44 +0000

    software eager need software fire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
