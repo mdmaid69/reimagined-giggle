84d5d34649c13cb10b130b23dc798a424255c67a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:11 +0000

    thanks Ss face deep approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
