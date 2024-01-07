0936df393b2645207841df2deb18e75c67e25668
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:05 +0000

    test grade price wherever succeed

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
