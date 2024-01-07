c7b31f7cff4321c8bf4987963bb8bd80af971b1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:45 +0000

    surprised influence degree class tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
