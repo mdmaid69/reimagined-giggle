efcdaa4226e884f7fed26801c8d4fbb1d2894597
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:39 +0000

    uncomfortable brain practical talk rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
