d00bcde8e277ec5c285d8244baef88a813d50c33
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:51 +0000

    glass female start again Hh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def reverse_array(array):
+        array.reverse()
