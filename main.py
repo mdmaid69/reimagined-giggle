4079cec485ac3e0735c002dde140965b04e3fa95
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:49 +0000

    dick original impossible classroom hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
