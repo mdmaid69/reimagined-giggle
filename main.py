bc86ad43b020222f0d9db1f5c3bf7df198583b5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:41:54 +0000

    pattern soft spread occur unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
