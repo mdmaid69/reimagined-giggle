0c46a1b8cc93ddf2a692f6478c18fe7145cbb1ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:45:59 +0000

    onto market tax base environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
