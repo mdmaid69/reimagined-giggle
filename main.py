a505df0b18ccd9a38493b7327b0d4b58194766dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:48 +0000

    difference leg tip desperate ball

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  def convert_to_binary(n):
+        return bin(n)
