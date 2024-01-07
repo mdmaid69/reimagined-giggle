0676987a95cfc74ce21b2c171eb03caf0836d8a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:46 +0000

    open argument funny evidence him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
