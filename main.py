43a211f4264ed483f680aeb7dc2d01c50cb43f97
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:13 +0000

    official typical comeoff subject why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
