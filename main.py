8fc5ec7da8951fa918507f8a1b62e74e4ebae972
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:03 +0000

    name rain loud cry towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
