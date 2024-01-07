f0ebc0e4d1cb2a4203c73a2f07806f6e14e5b35c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:24 +0000

    concentrate change know rubbish agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import tensorflow as tf
+print(tf.__version__)
