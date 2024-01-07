55de26b1cf60a97911027d57a095c7d2ef3520ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:33:26 +0000

    no pretend suckme dangerous fetch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_palindrome(s):
+        return s == s[::-1]
