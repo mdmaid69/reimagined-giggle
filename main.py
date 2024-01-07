128eff268272cfb6c653a89383c3e6f99c2f42f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:30 +0000

    closed trouble name judge live

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import datetime
 def get_days_until_next_year():
         next_year = datetime.date.today().year + 1
         next_new_year = datetime.date(next_year, 1, 1)
         return (next_new_year - datetime.date.today()).days
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
