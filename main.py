237bfc4e4d8ce48c194191ab52695e9950242dee
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:36 +0000

    tooth tonight accident tongue division

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_area_rectangle(l, w):
-        return l * w
+  def convert_to_hex(n):
+        return hex(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
