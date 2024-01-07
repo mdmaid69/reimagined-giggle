8bb851ed9a0df400747d8dc3c48c94cf04727eb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:17:09 +0000

    dress another perhaps stop nerve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+import os
+print(os.getcwd())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
