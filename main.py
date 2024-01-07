bb0f8d61856667f208f08c47d0fcff46039131d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:05:59 +0000

    share situation pocket record force

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
