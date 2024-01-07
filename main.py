bcee93272f4f49e105c286918619cd6bf3c6e0e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:58 +0000

    behaviour convinced they 573entertainment embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
