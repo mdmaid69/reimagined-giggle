2654a610c35f6da2a8925c317969b9c5690a7640
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:46 +0000

    woman disapprove question always hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
