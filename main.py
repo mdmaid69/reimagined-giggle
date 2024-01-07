79007e40322d801b18e703e716966e540888167f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:03:51 +0000

    marry safety term possession typical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_str(array):
+        return str(array)
