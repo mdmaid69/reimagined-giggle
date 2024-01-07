2b51961c52852c499cb3c2682840453092729eaa
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:32 +0000

    concentration separate rest normal choice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
