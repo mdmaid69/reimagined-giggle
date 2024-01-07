6a9bb5fee51129264a469198797190435789defa
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:54 +0000

    movement when concerned rank noone

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-print(time.time())
+print(sum(range(10)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
