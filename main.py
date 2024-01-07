bbe8e71a7c3747991bbfc66fb3c6cb69ba6dfc7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:33 +0000

    opinion story flavour slowly number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import getpass
+def get_username():
+        return getpass.getuser()
+import getpass
+def get_username():
+        return getpass.getuser()
