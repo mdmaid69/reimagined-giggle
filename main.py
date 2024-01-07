abed83d0a36ceb00b7dbeeead29f2b999c1284a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:59 +0000

    address exciting party grateful physical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
