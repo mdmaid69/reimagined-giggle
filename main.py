46648c4dd9b6e3f7c525b1e93984121e5455b1e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:48:27 +0000

    meeting squash good goup lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
