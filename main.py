b9902007965f9d206730504b797b8b305515028b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:59 +0000

    that task happy call rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-print(sys.version)
+def count_elements(lst):
+        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
