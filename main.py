3fd7471006da8d201f223405f94e559ef8a5508f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:54 +0000

    claim contribute he believe might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def set_array_item(array, i, item):
+        array[i] = item
