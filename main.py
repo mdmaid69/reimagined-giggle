5c8f0d43e0e260f82f8fb006b52945fe841e6a31
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:32:33 +0000

    loan chance remark before peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
