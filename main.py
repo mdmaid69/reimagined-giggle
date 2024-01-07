2dd99b700295ab4ca66bc52130e6497cc1a4c510
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:54:11 +0000

    sleep degree human unit shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
