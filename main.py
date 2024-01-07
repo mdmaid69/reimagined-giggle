c9699c7a9838860311438833d32e16c375480ad8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:27 +0000

    conversation who hot shop conversation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_odd(n):
-        return n % 2 != 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
