0ac9c68e44911cd7dfdb4b2b21ee5feeeb140ed7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:57 +0000

    influence giveout closed face every

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
