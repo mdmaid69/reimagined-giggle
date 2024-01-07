29ae1c63eca1c2bd32ad7b23b40c2766021be251
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:15:32 +0000

    poor ago over bread image

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+for i in range(10): print(i)
