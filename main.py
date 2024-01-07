87da3ee674b8d0cf330e1aa6ae19b2c17392a264
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:41 +0000

    special feature indeed size list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
