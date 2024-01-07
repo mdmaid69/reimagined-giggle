ead948872692035f8d1bb27a66bd38d3c37e9410
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:35:03 +0000

    Gg wander leave mass hurry

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
