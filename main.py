ebbc8d823175e0350c108ec0ffbf7b2aa860640f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:07:32 +0000

    strange thick phone build autumn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_elements(lst):
-        return len(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
