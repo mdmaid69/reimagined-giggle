3b8c167f6a8e255d5c68b82e345cd04f79fe8ec9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:50 +0000

    expensive personal comeover mine turndown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
