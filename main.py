0492b5bec559645bf6882c533c0cfb802df91f6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:09 +0000

    typical workupto to comefrom congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_floor(x):
+        return math.floor(x)
