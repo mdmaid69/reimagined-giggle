936eb138c3f4ba8a90ea01a668e21f44b29beee4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:53 +0000

    issue raw juice interview win

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
+def subtract_numbers(x, y):
+        return x - y
