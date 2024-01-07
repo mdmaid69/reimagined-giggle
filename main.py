db78c9cc24520675497a3a88d1691342abce61c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:32:04 +0000

    bill deliver university fire find

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
