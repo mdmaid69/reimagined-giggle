1989eabb69ed99227318c4c396eb39763284a6f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:50:10 +0000

    represent this interruption hope winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def multiply_numbers(x, y):
+        return x * y
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
