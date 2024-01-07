69263f81c63d35477f88df7f1ae8e96b848d3e97
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:19:25 +0000

    shallow himself regional union bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
