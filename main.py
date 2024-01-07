44f05bfd1920cf3d93bd7949e8282682cab8081c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:14 +0000

    tread and breakinto position mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
