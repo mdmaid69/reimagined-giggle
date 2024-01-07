a8dccca12ac0cc536314c941aa65e9a3af54f7f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:17:11 +0000

    soon patient will lift pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_current_directory():
+        return os.getcwd()
