31c678f78e3066214db57d1618f907c3eaa4f30d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:11 +0000

    size increase container finally fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
