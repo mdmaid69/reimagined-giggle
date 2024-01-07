9f69411d80352fc0ee594b0c165574f52ecd34c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:13 +0000

    tend gap than into stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
