8c944e83675cbb593876253c7b95e1d7785cc25a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:57:24 +0000

    pull contact grand bag wander

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
