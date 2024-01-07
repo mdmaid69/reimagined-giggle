99f1fa30de8d75fccf2d26ede9e05c1e02abeb49
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:24 +0000

    row declare comeround normal thick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def multiply_numbers(x, y):
+        return x * y
