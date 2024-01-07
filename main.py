40707b72282a313962a118d870d1ed3aa6c09baa
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:12:23 +0000

    fortunate book impossible attempt everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-name = "Python"
-print("Hello,", name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
