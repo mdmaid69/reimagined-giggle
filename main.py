eec977567b3b6273f545fc3154dc3768de738b2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:24:15 +0000

    exercise bother fit summer moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
