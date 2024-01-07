24f3657f8c1a08e9dd85e8bea2733ddbcbe465b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:07:11 +0000

    set material exchange calmly port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
