fd40d0a8e9c4ad35bacac52ba96bf0cd57c81094
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:40:35 +0000

    national post patient roof process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
