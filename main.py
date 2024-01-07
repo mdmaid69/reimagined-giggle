bc4fb0b6165f7427ca2955c2b15a73374891674c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:26 +0000

    ticket height mail however street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def find_min(lst):
+        return min(lst)
