2b6be8b4949dda01fa6c6b18ccd62b616996f079
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:16 +0000

    uponon maybe information ticket noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
