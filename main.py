09db17067dc4a946128d486b20d01f3a93677b07
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:58:34 +0000

    still trap damp dig true

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print(sum(range(10)))
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def cube_number(x):
+        return x**3
