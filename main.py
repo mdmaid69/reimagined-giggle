c8cb0d9c8515461493e54b2f9db94c285da62efd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:45:41 +0000

    top zero advice may prepare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
 def calculate_profit_margin(revenue, cost):
         return (revenue - cost) / revenue
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
