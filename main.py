2873b1f806c798f1f2b799880eb1a498656c3e08
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:24 +0000

    visit few however chief long

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+for i in range(5):
+        print(i)
+  def add_numbers(x, y):
+        return x + y
