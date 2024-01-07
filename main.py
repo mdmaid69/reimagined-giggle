f445fd25dff98f0c8977b21ad70fbe536fca2af7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:30 +0000

    like arrive opposite spring difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
