318f3ed4f9f49d595b7f19073a4cd86e5c26055b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:26 +0000

    upwards finger flood phone drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
