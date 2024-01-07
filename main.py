428000b9ac30e5d9963c5a3dc5080c90055c6926
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:28:36 +0000

    mail independent only accidental play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
