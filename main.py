759d387ccbcfde63cf48b52ea6ebe86c023f9047
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:47 +0000

    tough custom bury excited attach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
