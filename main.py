178640a108700536d86c93f0677d4b0117f4ad1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:36:43 +0000

    because please herself climb refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
