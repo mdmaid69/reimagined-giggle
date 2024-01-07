db4c3dc5d1f0c74f20d9e71af7fb9cd73b656234
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:33:45 +0000

    phone rise programme occasional loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
