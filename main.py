5bd75387d74caebf97b58110322d6fb2be63d92e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:48:26 +0000

    like path more collect amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import numpy as np
+print(np.array([1, 2, 3]))
