655c20e80ff6a55f9dae24f0a6576e26bd00f3af
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:48:32 +0000

    shop pretty breakthrough die relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
