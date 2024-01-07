6a33102403847e0eb63d33588cb45f1a9d6fed27
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:58 +0000

    provide poison operation Cc could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
