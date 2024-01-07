56a63aa8c73de5c603e0a86c6c2298d1e079a5b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:30 +0000

    zero worse still downstairs anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
