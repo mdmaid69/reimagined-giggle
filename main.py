b06d59041df99cb282c4f9bf17414d6494cd92c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:07:45 +0000

    tip behaviour language escape spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import re
+print(re.match("h.*o", "hello world"))
