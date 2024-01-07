3e63884b859235886649d99cb22fc133167eaa02
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:01:50 +0000

    positive region price bend flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
