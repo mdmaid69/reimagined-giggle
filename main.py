5e6f6552e1ddd53e33de30521e3e1f98324f6542
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:47:42 +0000

    match kind collect report arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
