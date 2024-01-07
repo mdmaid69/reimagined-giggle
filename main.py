042131147a3962570805253178d06aba3f5ec877
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:12:50 +0000

    write love big tense expect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def multiply_numbers(x, y):
+        return x * y
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
