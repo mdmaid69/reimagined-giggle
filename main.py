0cfefb77cb6edfecafc43cc5c5fa20732817f6c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:07 +0000

    head under workout muscle kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
