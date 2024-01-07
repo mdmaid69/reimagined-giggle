45f5b8361e383cb0d36c0e82bb27056ed53e5376
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:49:41 +0000

    separate unkind coat relationship load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def find_max(numbers):
-        return max(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
