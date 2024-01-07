7ef307e803a8c606098a48aa187dda19b42f623c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:07 +0000

    bringback number contain keep advice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def convert_to_hex(n):
+        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
