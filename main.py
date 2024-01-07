4d76cce04fbb55752dfa87ddb7316f782de9f426
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:53 +0000

    violence constantly out supply suppose

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
