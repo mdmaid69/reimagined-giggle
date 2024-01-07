883a187e2d5d4275f94b24cbab9e89187ec43d89
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:40 +0000

    breakout spoil especially flood size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
