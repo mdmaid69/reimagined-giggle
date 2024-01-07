adb05c12e52874f6cf15273b7a1760e912addeb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:14 +0000

    rather behind kind guess regional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def reverse_string(s):
-        return s[::-1]
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
