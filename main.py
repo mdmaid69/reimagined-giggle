b10ebf22171910a6801efb2a0cc82caab99a42cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:55 +0000

    cross Ll please date another

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
