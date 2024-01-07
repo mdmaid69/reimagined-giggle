dbcf34b392ead5cb7abec47318c2f683ed370a67
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:47:20 +0000

    majority off left light base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_unique_words(sentence):
+        return set(sentence.split())
