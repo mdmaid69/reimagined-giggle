9bb2c57192ec6d654f63307795411be84861df37
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:45 +0000

    probably regularly turninto bringback pity

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
