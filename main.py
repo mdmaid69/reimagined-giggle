27a7272edec35bd5e986da432fde6706b44597b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:17 +0000

    scare tidy discovery afternoon flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
