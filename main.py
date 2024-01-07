63dbf57ed164913f3b73cf13007272f23dba4864
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:47 +0000

    begin goup familiar early nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
