5fb10b9f8fd63c7dd36cad9f272cd32315d48e75
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:31 +0000

    568energy circle nerve peaceful careless

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
