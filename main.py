c60a724bb177914833ceb2b4a6f13fd2065213f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:00:09 +0000

    slowly decide blame maximum steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
