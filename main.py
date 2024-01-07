875d0021bead0c18ea6b4f0938575ce021d58a32
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:33 +0000

    suppose entrance laugh our culture

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_max(lst):
-        return max(lst)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
