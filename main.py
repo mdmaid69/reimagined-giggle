37105f559c4fd098d657abadb56eea3350c560a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:14 +0000

    flood instruction neither swallow that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def extend_array(array, iterable):
+        array.extend(iterable)
