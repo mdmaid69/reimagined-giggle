606576bd358212aff7b76e2169f8a0b80c374182
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:46:22 +0000

    obvious consult squash innocent mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-print(math.pi)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
