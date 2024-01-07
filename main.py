9d4379ecd14724a2189b3f9f5d73d8f4594f2e20
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:12:45 +0000

    throughout pot run trick add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
