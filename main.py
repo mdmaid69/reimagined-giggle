b31da2029fa1feb7fa0cbd68057c85344d35dd41
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:17:08 +0000

    cost compete work room enthusiastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
