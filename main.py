b5c1dd447f5c09d080eb560b5940f3f41d29450e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:56:30 +0000

    healthy except keepfrom delicate survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
