08b0979496691457517613fac882ab6cb5dd9937
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:04:38 +0000

    spin exaggerate herself phone season

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
