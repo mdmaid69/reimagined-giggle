68fbdcac83755f56c826317d27e0129fed3f710e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:46 +0000

    takeout estimate heavy equal theirs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_work(force, distance):
-        return force * distance
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
