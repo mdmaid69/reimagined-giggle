937f673c83e51f6d728a3bc72de76ae6879034e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:43:47 +0000

    part confuse container total dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def cube_number(x):
-        return x**3
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
