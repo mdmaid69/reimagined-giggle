ef99c394a0fb188318c07115958b0f96dcfd4175
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:22:56 +0000

    track opponent nearly late anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def square_number(x):
+        return x**2
