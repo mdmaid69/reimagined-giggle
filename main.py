1043b1ec7ced199ae5bc31670d62947196916bb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:45:52 +0000

    fit rise Uu mad protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
