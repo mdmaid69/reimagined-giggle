d2acf9c2b81ab207cbefcbc128370f133b7e61b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:46:53 +0000

    explanation fork lay emotion sore

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
