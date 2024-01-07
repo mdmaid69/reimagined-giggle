d0023d6ea9cc681d56f1b7011c270e8721771957
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:33:35 +0000

    storm home callback calmly frequent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
