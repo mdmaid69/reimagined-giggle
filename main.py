e03492b0e7cbcab91d11d6a60779a1a4c6dd03c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:13 +0000

    bear disagree debt yours burn

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
