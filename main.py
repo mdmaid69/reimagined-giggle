020ef7d855cee9226e2f192b4d56e77d8a8d87ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:50:44 +0000

    advantage ring school how fierce

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
