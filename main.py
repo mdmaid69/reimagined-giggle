c54d7cf5f6717dee17239b25e55c0493f05dfafb
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:15 +0000

    Gg whatever unit pump failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
 import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_str(array):
+        return str(array)
