98d5e0cbf724baf6b38992b2be2d08b06e30e7f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:35:37 +0000

    theirs body if ocean answer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
