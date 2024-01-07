9d7c521a73bb4ef6da008f8fd65bfd40b19dbfbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:29:16 +0000

    organize sky point escape walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
