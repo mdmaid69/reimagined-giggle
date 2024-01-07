3392cdc47d576f5f4af4937264929e37f5183270
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:09 +0000

    progress just call car should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
