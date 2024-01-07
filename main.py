cb0c6f219d41f2c2dda93c7d082886fe14b496a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:59 +0000

    indeed risk level pen probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
