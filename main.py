65dbff8e1cffe8b32e0da34c0009217d327c4d2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:12:44 +0000

    possible issue study platform hurt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
