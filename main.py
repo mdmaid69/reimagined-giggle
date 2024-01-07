876885ebe1486eb9bf6efd49ac72d59a50bb99ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:12 +0000

    average really noisy comeout impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
