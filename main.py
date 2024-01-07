631fafe5465fc85c4f424ff5d77687bc8b446941
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:45 +0000

    female estimate upstairs child emphasis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
