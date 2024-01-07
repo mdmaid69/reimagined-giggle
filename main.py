ee38e47c5d86197df8397559215bb8741018b3f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:00:37 +0000

    choose last flow black wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
