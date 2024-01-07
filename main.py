55abb38a7e1842f6f72ba6dd89fa65cb424c9d77
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:14 +0000

    heart comfortable holdout clue finish

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
