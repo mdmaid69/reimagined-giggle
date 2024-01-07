2d8fbcc84d64cf2a8e2856b761dded1068ce0126
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:31 +0000

    where sick plastic gradually anywayanyhow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
