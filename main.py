f3c9f74ae21e53411b6e891cd35a5285a2888e6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:46 +0000

    of home grateful ashamed entry

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(lst):
-        return max(lst)
-def count_characters(sentence):
-        return len(sentence)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
