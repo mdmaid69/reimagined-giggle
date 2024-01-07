14d9923680dce5832749bbd149fa1612e9e4dade
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:17:39 +0000

    thumb mixture search breathe convenient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+print(os.getcwd())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
