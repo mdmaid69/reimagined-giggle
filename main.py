02063f81fa214e3c9cf47acff54a700dd6a4fdc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:19 +0000

    combination hold slide avoid any

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_max(lst):
+        return max(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
