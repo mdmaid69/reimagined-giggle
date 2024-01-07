69da2ca6dc7bfd2279e1c20828a006562f7aa964
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:15 +0000

    from copy pencil wave used

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
