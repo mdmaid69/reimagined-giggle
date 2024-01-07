ae8a90666967a137b952bb098661e29a672f10d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:55 +0000

    bag arrival meal yes pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
