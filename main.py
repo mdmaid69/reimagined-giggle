ff44f35ea1f5e4a5be9f7f6563893cae013cabcd
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:53:02 +0000

    centre speed even own Cc

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def remove_duplicates(lst):
-        return list(set(lst))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
