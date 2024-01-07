e629c65f6f5e149bc2ef23e3913d1fdee4262661
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:14:05 +0000

    past average identify market flat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
