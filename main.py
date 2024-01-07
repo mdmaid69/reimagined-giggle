dcf161001d7630134fe866963ed8d67ec076c3b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:32:19 +0000

    distance likely active record lonely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
