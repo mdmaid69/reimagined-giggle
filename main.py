cdc01f0d851d9fe2a6a1d72e443fe8b1598d8e1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:50 +0000

    reasonable shop worried serious disappear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
