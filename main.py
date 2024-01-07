651cd529b9a87a281be520533537078f9e562d2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:20 +0000

    571enough better where field everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
