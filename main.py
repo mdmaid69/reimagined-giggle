96328e3d5513587fb132a7213763f7b31a81908e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:31 +0000

    touch drink list square note

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
