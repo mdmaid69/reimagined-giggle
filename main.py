004848b9abb881c1029cc9e5474edabc9c9b7cbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:47:56 +0000

    indeed arm combine offence strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
