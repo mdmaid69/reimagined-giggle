304538f7524c7e9ac58c2e6b2fceb87aae87b58d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:36 +0000

    influence breakfast develop uncomfortable attach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_as_bytearray(array):
+        return bytearray(array)
