ea72c74effc293a27674982348ad47dc921c57a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:26:52 +0000

    Cc long unkind air manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_array_to_list(array):
+        return array.tolist()
