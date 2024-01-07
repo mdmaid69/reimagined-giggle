47445777d83a77d19c53edca69e685bb309c9f14
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:52:03 +0000

    slice sorry interesting Yy base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+print(random.randint(0, 100))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
