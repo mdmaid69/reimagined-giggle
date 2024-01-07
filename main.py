005d525b4fe149083da33a9b4b33d1bfb7ab2eb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:06:11 +0000

    secret call what less they

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_string(s):
+        return s[::-1]
