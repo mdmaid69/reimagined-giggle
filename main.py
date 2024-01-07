4ba19f185bcc0c4a2c548cf7aee4f886a5a8dbc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:09 +0000

    wrap suck personal spend hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
