b6fa174c7fb0a956f20fcb4821381a50ea0a0004
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:59 +0000

    sure swing hold post poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def divide_numbers(x, y):
-        return x / y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def exit_program():
+        sys.exit()
