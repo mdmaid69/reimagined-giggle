d6f00b3d5b82893c731e86b97749d796d03b4927
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:23:14 +0000

    process fault build improvement content

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
