2bd845768e5f3c47c9020cc60012b6a09ae8fb5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:44:08 +0000

    aware pause bother bored beautiful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
