a3db39d34ec6059bcccb82832649cca59f8a9130
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:11 +0000

    note agreement proper near emotion

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
