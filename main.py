f3fbc467666ecc66c7e9be1a25e337aebce6d6d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:01 +0000

    word hear immediate herself perfect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
