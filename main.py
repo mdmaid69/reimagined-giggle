0779968fdb81033a689c8b88d16cf7be6f42c0e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:20:18 +0000

    disturb point unfair down furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
