4885fb9a4300e66805606a677e662486fe97fc57
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:00:17 +0000

    easy muscle dish consist luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
