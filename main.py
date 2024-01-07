92be96578f0d9eb93e229bf86f033680a7dc54eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:09:19 +0000

    understand occur Uu extremely cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def divide_numbers(x, y):
-        return x / y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
