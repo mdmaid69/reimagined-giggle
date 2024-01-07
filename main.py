763e43c95a77588221dd5fd7c63217d2efb1f016
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:20:31 +0000

    load giveout respect contact thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
