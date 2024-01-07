a53ace8d9a9f55d8f32123194b77428351f4d925
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:30:18 +0000

    trousers just sing closely nature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
