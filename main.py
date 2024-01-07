390cf7a8f666d61d7606f711963e15c617762c54
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:53:32 +0000

    immediate importance book wait turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
