f91f1a96630f07152ee27957f4be422bc5d58d4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:22 +0000

    advanced advantage healthy centimetre necessary

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
