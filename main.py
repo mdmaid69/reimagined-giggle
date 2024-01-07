8478333346379c9f7dbb605222008aa365fcbb10
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:44 +0000

    destroy demonstrate term shut today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
