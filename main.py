00b9bcd5328d23b03c4c86fe185cac86577471e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:08 +0000

    block between notice nice keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_max(lst):
+        return max(lst)
