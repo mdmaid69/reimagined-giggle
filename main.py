573e01c6b9fe271c0ca3b03e415927281bc0eb6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:08:14 +0000

    part nearly Hh operate leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
