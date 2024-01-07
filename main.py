23c64362788577ca6a6ec21c66169ab3cdc4124e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:42 +0000

    message form open share many

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(lst):
+        return min(lst)
