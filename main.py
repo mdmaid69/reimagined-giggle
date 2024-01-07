c4a024e941ac73be04e9b1a28ed7e040146ad041
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:09:15 +0000

    block main luck cut goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import platform
+def get_os_info():
+        return platform.uname()
