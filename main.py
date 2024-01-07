38cdd1d788ce468c1cb0cf7cc7cc4b8404d48b63
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:09 +0000

    other whenever callat pull close

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
