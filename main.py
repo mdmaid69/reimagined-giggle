afd2f0c218bec45e0064b9203cd6d6b3f6194dd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:36 +0000

    try realize small deliver address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
