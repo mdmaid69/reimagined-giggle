1e7ec3ec50d06d9fe22230ba6a76b3fddd9414c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:03 +0000

    offence suckme week succeed still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+for i in range(5):
+        print(i)
