4c068339ffc5531210bf5f9f7065ecb37bacfdc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:44:29 +0000

    piece hair note middle trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
