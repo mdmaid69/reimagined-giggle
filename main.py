cebcc34561b7d40479db17de78f9eb8b630c3934
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:01 +0000

    fill score pour sweet each

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
