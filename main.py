22a58e7845432ebc492381a60edc9f4ac59b9b7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:23 +0000

    computer excuse round play game

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
