c11ea9f0dce541da9ea1b870e419a430aeb88dd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:13:07 +0000

    giveaway lost whenever have somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
