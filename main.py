958b7c24e2c47d7a3a87aacb2942f0ce42cb78a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:47:46 +0000

    decision meaning march game hear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_complex(array):
+        return complex(array[0])
+import collections
+def create_queue():
+        return collections.deque()
