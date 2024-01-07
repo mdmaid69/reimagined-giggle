ee80f1e07ddea0b26bfa4f44de8331dae9149097
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:53:08 +0000

    warm system discuss born comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
