28959347938d8e2be0f145e28985b9640672e131
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:32:21 +0000

    argument over church understand imagine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
