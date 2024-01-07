779b7c384fb8e28a16f9cda1b0e6ae87c9eefc2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:08:59 +0000

    medium moment hit sail consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def get_current_working_directory():
+        return os.getcwd()
