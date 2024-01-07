eaeaa3a9ccac5b26c7c2eed5cabdb91010399ab9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:07 +0000

    characteristic introduction tax sign clothes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import heapq
 def create_heap(iterable):
         h = list(iterable)
