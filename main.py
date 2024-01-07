5ebed3bd54168a3f5e0eced2030d418ec911d4db
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:42 +0000

    myself note high relation sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
