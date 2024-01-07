dd8a03a95c69c5e0e422078448e0efd8e0c40d5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:23 +0000

    present earn final there they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
