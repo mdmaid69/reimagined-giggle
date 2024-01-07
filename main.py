1fd6b3ed0abf233b1a593bb892bc319ed688b355
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:23:10 +0000

    else power bury information whom

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
