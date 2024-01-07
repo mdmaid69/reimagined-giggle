c53d2043dcc41fb9f5a459fb8b5806905069a8a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:58 +0000

    close concentration passenger stiff old

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def print_python_version():
+        return sys.version
