4f1f828f7cce3f75e5d25541ecc006681c231878
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:34 +0000

    responsibility fasten remain event combine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_sine(x):
+        return math.sin(x)
