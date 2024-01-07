c33045e32208d7750ce4d5640f6f15b2502e9433
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:31:01 +0000

    Oo software post ability may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sys
-  def get_python_version():
-        return sys.version
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
