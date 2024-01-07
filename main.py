fc9bf1724a45c9d46f8b0e82568751274d803312
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:19:10 +0000

    freedom change alive not impressive

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
