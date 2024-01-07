9969efd0f52e9b88e2c8bc32db717abeadc1dd57
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:18 +0000

    head simple male band comeoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
