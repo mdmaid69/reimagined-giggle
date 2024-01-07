4aab72d472b156798f5e648766052f61f1c46a18
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:18:59 +0000

    hope so suggest into save

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
