6968c36b0424d52b3c9e3edb67ad07aa7f24eb23
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:25 +0000

    goon many teach language great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
