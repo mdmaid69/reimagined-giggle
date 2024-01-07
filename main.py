89ff3c823945da93cb958c27d9231c3567a73663
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:08:13 +0000

    cheque advanced soak spite world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
