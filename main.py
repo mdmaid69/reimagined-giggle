12aa45f27f0f15b0e5c4cb4113d8695e23acd6b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:59 +0000

    extra check not especially city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_stack():
+        return collections.deque()
