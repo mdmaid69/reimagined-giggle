236c3005b91d882687ce80ba77a4216366cba693
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:53:21 +0000

    independent already term on scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_counter():
+        return collections.Counter()
