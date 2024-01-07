0dfee7202f26c05b5228944f09ac09eb4c8f0da0
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:26 +0000

    shine stiff steam firm collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_user_dict():
+        return collections.UserDict()
