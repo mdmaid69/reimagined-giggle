7e074e1880459fa8a6349303bb1eb2ff93075d87
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:58 +0000

    shadow abroad try autumn maintain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
