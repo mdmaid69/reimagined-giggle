205a3b6c4f4e73dd6c80584c5da89a4204850d51
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:02 +0000

    everybody hide plain left here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
