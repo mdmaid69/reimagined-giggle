2e01c2ad5c382cea77eb3fc6403bca04e53aedb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:57 +0000

    we corner live harm old

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
