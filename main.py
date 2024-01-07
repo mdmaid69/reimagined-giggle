64cbe763748fc5d101dd543e09650ae26e1d0038
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:46 +0000

    study light grateful follow bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_volume(length, width, height):
+        return length * width * height
