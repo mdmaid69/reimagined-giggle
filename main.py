c078f23f55db989b6940e38a539f9171c06ebf38
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:52:10 +0000

    cruelty ignore strength plan charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def convert_array_to_list(array):
+        return array.tolist()
