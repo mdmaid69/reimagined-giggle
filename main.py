47f327d724f35796475526fea9dc207721df4321
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:08 +0000

    eat properly seem stare exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
