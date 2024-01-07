ac6aad5e3bcea2ed184a610e8ed96d262765850f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:26 +0000

    pain dry already odd day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
