3f4aca26d982e40f7d5c882af45419b108894f7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:24 +0000

    instrument frequent print service beyond

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
