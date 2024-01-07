ba819f938d294835623435cd96d53c08f1fd1065
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:38 +0000

    advantage giveback lead later determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
