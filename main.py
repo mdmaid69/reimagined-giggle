b9e3f61576dead892917f7db924b3c323c313edc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:26:17 +0000

    normal definite post middle extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
