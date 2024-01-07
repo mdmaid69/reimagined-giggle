2a1456bbcbd49bcefcba570f62af615e6cc6d88a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:17:36 +0000

    blue present trust payment punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
