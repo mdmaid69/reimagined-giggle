0d52284de54830533ddecd8507f8f940ca3760cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:09 +0000

    herself production in allow sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def count_elements(lst):
+        return len(lst)
