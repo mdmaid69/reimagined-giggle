adaa381c3464c96b7b0ccdaed23f5387fd1633d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:49 +0000

    during refer way yesterday practice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import sys
+def exit_program():
+        sys.exit()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
