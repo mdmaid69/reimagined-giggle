7c8b073836288186d8976c80b39fb9a6dcd5a1ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:13 +0000

    within turnout commit city holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
