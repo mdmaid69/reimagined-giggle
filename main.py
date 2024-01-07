2981e07b66c67ab58cb4f719329d4cfb30229a1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:21:11 +0000

    pinch smile shape any comeback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
