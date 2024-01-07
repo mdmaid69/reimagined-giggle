7b77cf29acdfaa332e7585c017cd3542ef854c29
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:13:00 +0000

    brief ours whoever firm flavour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import sys
+  def get_python_version():
+        return sys.version
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
