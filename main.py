650c5a97919f7d66926b7daaeafa112791fe4939
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:02:27 +0000

    chase sell relax wise body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
