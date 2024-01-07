a03cfad28ee500fd155249181634c7ed16dd1f80
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:32 +0000

    contribute also tone enthusiastic trial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
