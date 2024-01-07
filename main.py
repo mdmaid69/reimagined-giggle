bbfa28f6c2be8cd0a989f79b78b0060737b25bf2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:01 +0000

    piece calloff conclusion generally professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+def count_words(sentence):
+        return len(sentence.split())
