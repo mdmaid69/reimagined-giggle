46341b5a66953c4431c698f75349e23368f54843
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:15 +0000

    church light disappointed comeout shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import collections
-def create_stack():
-        return collections.deque()
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
