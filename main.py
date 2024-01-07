80b5fe27b480aa67e91ec08627c6f1cbc817af41
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:01 +0000

    detail steep connection glad violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
