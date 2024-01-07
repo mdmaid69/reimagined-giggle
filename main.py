f39e3a206938bdea46420e00e4a7d52f2e3f87c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:54:28 +0000

    shout school pretend person rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
