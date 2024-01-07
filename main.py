bcc4d3296873bd065b1404666a665c8e94648462
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:15:03 +0000

    split confidence inquire drink rarely

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
