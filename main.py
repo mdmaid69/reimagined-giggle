420a078041ffd81d416834d110c7a29e79eb67e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:35 +0000

    build load cook deliberate object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import sys
-def print_python_version():
-        return sys.version
+import tensorflow as tf
+print(tf.__version__)
+def calculate_work(force, distance):
+        return force * distance
