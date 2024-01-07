1f83601a19d6a786e01b7a780d87e40583661a19
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:44 +0000

    nigga nearly persuade sound smile

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
