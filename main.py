7c7fd1180080892c2f2cd22abf882247cd006aab
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:22:00 +0000

    soon row friendly board formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
