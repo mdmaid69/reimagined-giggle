30c6f6e03c6e30e98c26bf756fed1df85f247cf3
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:35:24 +0000

    material contact book than rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import os
+def remove_directory(path):
+        os.rmdir(path)
