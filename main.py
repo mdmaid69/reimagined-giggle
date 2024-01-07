455d3d8aa8b697f3c2c8e89fa8c3d32b211f94b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:22 +0000

    capital custom sail mother march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
