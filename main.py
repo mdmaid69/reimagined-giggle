9156e80b7e53e169d102f5013a433fa8bfc8c3de
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:04:42 +0000

    through trial official future activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
