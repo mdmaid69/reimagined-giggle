f4123266510fce63824423914e394c982b2abf56
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:52 +0000

    behind negative gather favourite be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
