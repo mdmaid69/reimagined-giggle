e6c7af7752d6a246918c8f7174db330e15ad4e27
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:06:31 +0000

    mystery price frighten anything expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_current_directory():
+        return os.getcwd()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
