cdc9750fc799f0f607a05a1c854f99cc2f6ae790
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:49:25 +0000

    more effect proof address left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
