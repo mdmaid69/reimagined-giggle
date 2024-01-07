2edbbf6efb36d012cf5e0f257fc64c3e7411ac97
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:08 +0000

    turn punish with ahead colour

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_characters(sentence):
-        return len(sentence)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_set(array):
+        return set(array)
