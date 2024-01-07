082431cbe8a7e4d61c554a1e15def52a4d1da09d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:39:41 +0000

    undo comedown correct example black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
