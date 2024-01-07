86c3aed626ba3c6cbf489cd877476fd85d778fa1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:09 +0000

    clean old way callat unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def get_current_working_directory():
+        return os.getcwd()
