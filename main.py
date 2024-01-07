2fc72fa8f2cdc730ffa1150073d3ffba47f6d170
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:25:48 +0000

    business snow tongue national work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_list(lst):
+        return lst[::-1]
