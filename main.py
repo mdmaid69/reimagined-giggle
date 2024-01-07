7edc7c6c68c0bcf262258b8bbd79390605760620
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:21 +0000

    smell stupid recovery but cinema

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_power(work, time):
-        return work / time
+  def sort_list(lst):
+        return sorted(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
