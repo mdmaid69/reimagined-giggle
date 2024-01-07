c0831194833738e9741999b62704bde05f93c3c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:28:29 +0000

    left ill reduction always send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_density(mass, volume):
+        return mass / volume
