d1aaebf4e2ed84cadbe679ad61ca4c1c111f783b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:22:03 +0000

    calm office yes unpleasant arrive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
