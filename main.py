ce759f541eed8a1c933a0769af830c288a0141ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:06 +0000

    holdup reasonable live freedom suppose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
