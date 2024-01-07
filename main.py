81337ac177a4feb70c07619042f805a9b8311925
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:02 +0000

    pretty amount complicated slow luck

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
