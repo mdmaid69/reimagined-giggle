e04d21cc0eeaa24de140dbeeaa8a899f8278aa9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:27:23 +0000

    best try satisfactory summer improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
