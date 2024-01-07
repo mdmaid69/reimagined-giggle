5cf32bf27069e27252e7da2a272705d639b2fe29
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:56:08 +0000

    mix cry definite borrow exploration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
