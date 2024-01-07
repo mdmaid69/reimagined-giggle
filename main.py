1ba69ff1ea286e37312c7cdd818dbbfaa9df91ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:15 +0000

    Bb drink maintain display tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
