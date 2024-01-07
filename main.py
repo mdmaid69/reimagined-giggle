3e7fd4b3a96c14cac72d7b9c7f0a36da932ed385
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:42 +0000

    influence tear invitation present favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
