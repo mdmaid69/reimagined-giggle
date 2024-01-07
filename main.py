23c94e928f6912474d4148b1022f582b3694114d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:11:01 +0000

    bear goaway female match bet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_user_list():
+        return collections.UserList()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
