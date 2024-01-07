ed814a09959d22195be66151c4ae8f535208e3d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:38 +0000

    cost history absolutely satisfactory protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
