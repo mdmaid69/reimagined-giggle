b9a418551382590898f90432a67824e92502fd5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:36 +0000

    hello walk impression favour eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
