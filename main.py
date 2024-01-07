c70fda362748f02ffc3521aa7c3c5fc2f7c81055
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:53 +0000

    all attention check drive far

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-for i in range(5):
-        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
