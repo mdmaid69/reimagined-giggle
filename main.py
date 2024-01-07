5ab5162dcc6801d5adf5d76c03ceb455bee45bb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:41:19 +0000

    through hard add damp practice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
