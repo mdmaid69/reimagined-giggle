2d1d55dfa34d0be59ecd9f525b8bca9350a56a36
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:38 +0000

    negative sorry last already year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
