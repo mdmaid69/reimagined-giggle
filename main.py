a0f362ef491d6817e6a71dbe531df4d94719995f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:43 +0000

    involve behind last compete hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
