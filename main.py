2a79e7f469fe654201efe9a9175a4ad7cffc0c29
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:42 +0000

    contain right list bright however

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
