48d398c7e8d6b88dc460f1139da620f4b7a32a7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:27:49 +0000

    thinkof pull physical takeoff beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
