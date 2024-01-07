b4d23e9e4cc49a8c225caa25cb91fd5271e73cf5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:23:13 +0000

    risk hair crazy match discover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
