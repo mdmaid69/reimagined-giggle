183885a64792e5a17660f6409e3fe171b7567fae
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:18 +0000

    closed odd 567enemy comeback environment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
