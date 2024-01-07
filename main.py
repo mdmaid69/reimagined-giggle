99d8708aa84a96fb17f748d924d412160065a6e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:01 +0000

    surprise dance sign phone steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
