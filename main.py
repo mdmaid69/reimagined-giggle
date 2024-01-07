dd39f97d21d711d739e6117b58936d14fa3f4b05
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:23 +0000

    turnaway behaviour foot top soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import sys
-def print_python_version():
-        print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
