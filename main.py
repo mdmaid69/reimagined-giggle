b8baea8ac6972d8b33f4615d58810571bcd3d920
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:21 +0000

    mail Jj rely because nearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
