89aaf63492f8013caa27f76cda044b26a78722d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:05 +0000

    awake fact used compare afraid

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
