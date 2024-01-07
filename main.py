99f3d5471adb3638b5bb67ac7c84627b1492aab1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:54:08 +0000

    character basis scream dangerous safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def multiply_numbers(x, y):
+        return x * y
