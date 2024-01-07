c966cf718e50dea8e89c6a380ef2061ad7e3d398
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:52:27 +0000

    move strict throughout future feeling

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+print(os.getcwd())
