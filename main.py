b38f46ce15ad59ce33dd96bec13ade414f49c70e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:18 +0000

    hollow tear between objection business

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
