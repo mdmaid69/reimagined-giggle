9ff8a5c073daf985fd320ac41879d85ca4b8a007
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:14:36 +0000

    occasional far dig ready happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
