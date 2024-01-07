46d1a152de7d37763707e59ca43af46fe4a62faf
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:30:13 +0000

    pool sharp also unpleasant pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
