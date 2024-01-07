0f3d998b855f7b83a84e846a9e74bf56a2ba3014
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:53 +0000

    Uu answer flow conclusion score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
