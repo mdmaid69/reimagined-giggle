cecdd1445dfbdce781a9ff2dc50c0af36caa3743
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:44 +0000

    properly think fun famous change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_as_float(array):
+        return float(array[0])
