9d2012d97a14c56f69c7be306f2c044bb7343312
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:13 +0000

    many someone rush arm mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
