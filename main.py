4813b1946505682f878e0cf4a8a1ae894bd36e16
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:15 +0000

    dear cap main distance fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sys
+  def get_python_version():
+        return sys.version
