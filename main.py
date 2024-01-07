62a82d43b1ff4b5d2884201edcecbfe4bc25300a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:39:02 +0000

    worried back wide responsibility excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
