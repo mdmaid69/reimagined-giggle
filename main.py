d26faddcab8d005994cb6bc773254d6aae8f3f46
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:13 +0000

    rather quiet crisis off miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
