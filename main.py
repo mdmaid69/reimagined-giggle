874582e18ac01d8485311f845add7d2f12383069
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:35 +0000

    into guess speed weak anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def add_numbers(x, y):
+        return x + y
+import tensorflow as tf
+print(tf.__version__)
