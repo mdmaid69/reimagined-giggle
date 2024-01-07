8f85a440539137557de76f49c021f10329611096
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:36:26 +0000

    advanced always thinkthrough quick circle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
