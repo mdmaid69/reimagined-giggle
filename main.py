02e993571331b39e47e2635f6408b5720563c17b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:33:19 +0000

    most disapprove expect form complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
