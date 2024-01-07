5ece24187b01cbbd7e41938aecabdf88e4a39e30
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:28:24 +0000

    ring have crowded hard human

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_set(array):
+        return set(array)
