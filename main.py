3d5314f40afb224ebb0ca62aece12d9b4283ec9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:01:33 +0000

    face right special risk car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
