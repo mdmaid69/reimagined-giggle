c0c49b901edb872ce73e22e272a2d2cc620f2053
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:58 +0000

    wire also look who giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
