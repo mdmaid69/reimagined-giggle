30d6436f75885d80c0bc17e013cbaff73c6dd383
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:40 +0000

    shoulder after pin leave smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+for i in range(5):
+        print(i)
