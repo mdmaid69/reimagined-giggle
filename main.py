a3e575e2c4e5562f2c2a94082ac0c4b9da1505a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:17 +0000

    instrument phone base soak plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        return sys.version
