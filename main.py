23395c3794d929ed39cc31630ad9100a9ebbccc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:55 +0000

    opponent port accident tomorrow moon

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def is_odd(n):
-        return n % 2 != 0
-def divide_numbers(x, y):
-        return x / y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
