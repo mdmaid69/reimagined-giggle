8fb86ea0f58d302f4524a89d99f8e436bf0ae970
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:48 +0000

    skill callback cover late godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
