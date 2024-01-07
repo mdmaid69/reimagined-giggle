43053ad31eba8326297e16c0f0964555c0b7b1b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:38 +0000

    provide where let bang combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
