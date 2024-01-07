45033d1145d1f05776abddb68cf78d303e7316a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:47:59 +0000

    blow blind side fail lucky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
