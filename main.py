716bccfc0ac90377a7cdde6339c4c9f26e89b908
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:59 +0000

    aim any artificial note health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
