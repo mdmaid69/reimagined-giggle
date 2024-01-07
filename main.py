96be501bf97f00eb11fec4f83f24ab8563aa15c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:02:08 +0000

    before method long wheel breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
