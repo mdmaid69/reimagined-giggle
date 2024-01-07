39cdef0a923f87da1df5d2c1a393ba376cbb9d04
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:15 +0000

    boring sink question door neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
