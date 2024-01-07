8c7335634651a9b7c937c149517eb2a34161226f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:09:12 +0000

    contents system workout giveaway actual

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
