59750fe884ad0a2ec67460eb2c31735b959740eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:00 +0000

    class fat charge float hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
