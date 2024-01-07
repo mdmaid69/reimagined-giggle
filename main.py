d98b62ab6b7fc44d656de76efd208447e1ce10ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:45 +0000

    command same frequent getup best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
