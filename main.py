70af3ac67b8f994f695793dc440ece8b4b480aa3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:29 +0000

    callout direct wash flat 569enjoy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
