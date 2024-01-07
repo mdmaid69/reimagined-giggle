ecd9651aac6a9ea3e72629a0ed625d3b72cf973d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:34:32 +0000

    mend near taste offer sign

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
