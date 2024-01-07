736b99d1a5a6a726b21414f10f3edf2b84bc076e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:41 +0000

    nothing frame see responsibility someone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
