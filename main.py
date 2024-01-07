de9858171299c6af0175330d20ecededfeb70b6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:55:20 +0000

    suggest distance anxious study tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def convert_to_hex(n):
-        return hex(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
