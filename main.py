f738b52f4aae7fb4e4b01c33f94c8a9c7be20d9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:14:52 +0000

    compete accidentally reaction big shelter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
