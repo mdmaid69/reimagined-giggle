94175224979b9256a4fe1ba9f58230e5e3c2b828
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:48 +0000

    judge progress intelligent record where

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_area_circle(r):
+        return 3.14 * r**2
