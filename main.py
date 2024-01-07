2f728fb5561a28d77aed1fd5d58c071d0ca63ba6
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:58 +0000

    hardly middle air really beyond

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sys
+  def get_python_version():
+        return sys.version
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
