fa18f2313b3f410f4506a35ab54acd1c06a71902
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:02:06 +0000

    furniture waste fill lookup habit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
