1f408beb89c77d3c4778c3c11783a0a3c794c64b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:27:11 +0000

    each balance clean order spoil

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_even(n):
-        return n % 2 == 0
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
