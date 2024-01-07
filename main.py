a961916f102478e7ae8cda6d039444bab850a90d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:03 +0000

    mess moment spring snow during

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
