366b7e746a537a8715a48536544d870c490f35d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:51 +0000

    sell compare inform care willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
