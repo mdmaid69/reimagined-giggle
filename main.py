20a5a2343cf8c81fabd6a0a7ef57d740ae7cd1d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:53:58 +0000

    care unable buy card reply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-for i in range(10): print(i)
+  def split_path(path):
+        return os.path.split(path)
