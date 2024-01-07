e3e5e1cd1aea6af887b0f6a8c669a84c09ca22de
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:08:26 +0000

    underneath sale set turn subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
