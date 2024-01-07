d00e29dacf04fef388624368fc0bdb99c4ff3750
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:23:34 +0000

    young strong separate impossible happen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
