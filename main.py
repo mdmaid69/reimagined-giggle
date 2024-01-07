7b63acf49065d8b1f67b9c6194326c55f1d7d812
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:38:23 +0000

    closely regular account modern today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def convert_to_hex(n):
+        return hex(n)
