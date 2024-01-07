a375aec6ab2266ae9d46c740497debdfe9d52b15
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:08:10 +0000

    study picture giveout disappear chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
