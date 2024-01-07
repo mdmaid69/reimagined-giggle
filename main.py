badc3f474b6bb3a4fb21b076c9967513a1ef10b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:32 +0000

    bother tour miss appeal getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
