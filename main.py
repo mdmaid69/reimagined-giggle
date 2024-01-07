79ffc39c3c5dc1c356699a4ebe4f0063e2a19e65
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:29 +0000

    object half forget over refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
