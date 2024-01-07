1146dc45f45c9452b8262b0064d2c69b1be299e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:33 +0000

    decide involve study walk fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_str(array):
+        return str(array)
