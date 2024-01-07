e6926edf84f239a6ac75bb182b52015c18ff5251
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:27:17 +0000

    eager thumb paper fear neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
