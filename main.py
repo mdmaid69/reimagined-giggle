1162ffce6385c35370c92c1fee5989f2b1102b75
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:24 +0000

    there disagree cry steer human

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def add_numbers(x, y):
-        return x + y
+for i in range(10): print(i)
+def find_min(numbers):
+        return min(numbers)
