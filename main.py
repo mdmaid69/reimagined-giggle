cd7a6a7f16a1cd5ae9b59a125c16bd8c851cd4b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:46 +0000

    pen trouble instruction naturally agree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
