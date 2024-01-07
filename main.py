7292d26dea2d2580bf1bed6d34ce4cdfe00021f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:46 +0000

    clearly petrol of right careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_queue():
-        return collections.deque()
-import sys
-def exit_program():
-        sys.exit()
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
