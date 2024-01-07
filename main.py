8bb02d118eef4c887b71b2983b6def7aeeb44b32
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:27 +0000

    law raw stone fire middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
