e683b75636128c4fe926fa12165c07680b7d8750
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:09 +0000

    food spray flow wind scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
