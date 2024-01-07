cc5f80f2adb8e86a13342e63d9a105dd721f742d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:52:04 +0000

    remove hate crash brush basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
