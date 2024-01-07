ac538e8aecd0ec1032912b6f53a46524f61c2223
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:15:09 +0000

    giveback no check set stop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
