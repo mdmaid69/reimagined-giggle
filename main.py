81b702bf27839fbd964971a3503af4539daaa05e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:21 +0000

    complete poor wet meet computer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
