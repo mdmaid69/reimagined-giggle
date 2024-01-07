40f4d5a0813a6fbdc363279cabdf09ab4b6ffd87
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:59:32 +0000

    spread far stare leave quick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
