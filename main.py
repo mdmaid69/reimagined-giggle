3e323033d6a3bd75fec7639c413fa2b7067434ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:36:50 +0000

    point job takeaway healthy rob

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
