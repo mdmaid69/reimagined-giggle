50e5ceff1961d5b76f6f7fb0ec723e336690fd1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:45:22 +0000

    plenty learn inquire satisfied slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
