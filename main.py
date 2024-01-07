feb37df3e3d28141733f1e8cc40c8066d1300018
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:25 +0000

    consider minor arrangement mass time

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
