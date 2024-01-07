3981eec401c59d34dbf9a98c6b4f75dbff0580d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:06:21 +0000

    last speed persuade original 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
