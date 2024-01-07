fafb8172e5393a1267c0c7d69e3a8f65fec62dab
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:29 +0000

    fire 570enjoyable first whole cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_max(numbers):
+        return max(numbers)
