b0fa75970486071b1a12874a234a07d28731bc51
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:07:54 +0000

    rub bank disappear already software

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
