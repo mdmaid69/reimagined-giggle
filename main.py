79ad75f1ff9063b66ef60ee78217ab1aa0811d28
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:20:57 +0000

    cheek kind before scale excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
