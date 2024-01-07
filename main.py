013e57fee8b64bc6ed73de433dbd6de66412ff3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:18 +0000

    hurry square goback(to) since consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+import tensorflow as tf
+print(tf.__version__)
