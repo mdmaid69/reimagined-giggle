1032da6bdcaf74cb7462b429e313dacdde261716
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:24:11 +0000

    weigh straight rate exist congratulations

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def check_if_array_contains_item(array, item):
+        return item in array
+  def convert_to_binary(n):
+        return bin(n)
