3eeedd1539e818b8e15b775c284e68fa7f878e12
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:35:14 +0000

    press 565end notice brief worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def get_array_index(array, item):
+        return array.index(item)
