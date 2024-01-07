1038d8af62ccf46468ee1f385237a574c3390d10
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:27:59 +0000

    confident design bored disturb company

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
