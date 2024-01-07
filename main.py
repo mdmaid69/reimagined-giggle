0d4db36300dddc8f2553cc0e79532330ceaad5cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:31 +0000

    wise afterwards at smile sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def is_odd(n):
+        return n % 2 != 0
