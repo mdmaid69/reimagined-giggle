aec596eed5f3991d9a66f3eb6293511145070b18
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:57:10 +0000

    release bye control feel charge

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
