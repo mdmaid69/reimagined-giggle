579fdd929a349ea4c45434b355af27d917fe61a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:00 +0000

    long book pencil project prepare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
