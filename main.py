0a72d5058a2a0d4c912a6dae92f845829500ecd3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:05 +0000

    minimum replace schedule convincing touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
