b3514b98491a4c06582e6968c495ac7ff94e74b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:54 +0000

    spring sleep cross serve until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
