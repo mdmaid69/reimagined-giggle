4427fe1c800171299f5e8d60c341bc9212fc2764
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:18 +0000

    trust mouth receive ability thin

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
