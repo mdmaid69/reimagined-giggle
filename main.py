a14c39c5a4aa7283b1edeb47b31a8827430989e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:50 +0000

    advantage more steady line sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
