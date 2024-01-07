35f677d37bcb0c965d9497ca84a088ba72af8263
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:38:05 +0000

    damp show Jj dry far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import getpass
+def get_username():
+        return getpass.getuser()
