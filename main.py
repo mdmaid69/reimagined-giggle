40dba6cf6bac3de036080ee4fcffcdbd65a12d78
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:43 +0000

    slice hurry estimate believe sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
