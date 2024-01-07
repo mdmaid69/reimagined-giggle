48731fe26a1e471c4e17915fde3c9879c32990fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:13 +0000

    convincing division cinema flood hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
