ab05d30f2745c1231387b57608609ed66516fa86
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:14:12 +0000

    science tread joke like church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
