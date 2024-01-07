a52ff49a8a36e5667b341594be5f4ec09887bbb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:24:26 +0000

    influence it mean bringforward board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
