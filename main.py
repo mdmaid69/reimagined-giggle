cf7bc17c618e7f5a6ed14647eeec5075f3f0406f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:36 +0000

    snow listenfor possibility convincing congratulations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
