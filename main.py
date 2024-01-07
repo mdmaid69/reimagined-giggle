cf61bb0d236bbf8b03633bfac56a140765c8b8fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:37 +0000

    vote yours tie price means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
