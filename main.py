61794a07a3adf9117aae457c600ef6b19304bff0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:43 +0000

    best prize season low decision

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
