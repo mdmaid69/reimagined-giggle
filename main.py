56a99c30d8525b1d20acab5ed235390ac72dfe3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:06 +0000

    practise turnover during why control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
