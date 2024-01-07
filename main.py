edf3e9f5687ab05aba2bfa7b8b59167216fa1bf8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:42:52 +0000

    imagination drink breakthrough holdup uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
