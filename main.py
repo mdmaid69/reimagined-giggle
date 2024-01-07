c2f5171f969dd6b512fac96de9b69c1527087fa3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:13:58 +0000

    means myself cry down interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
