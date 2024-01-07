b3531f50bc68acd571142cda11d11a7536c67f72
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:16 +0000

    do know ilovedicks welcome shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
