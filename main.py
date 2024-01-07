32af60ea5b9efe22aa3eeadac0f23b8c5d901cb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:28:56 +0000

    sign lift label behind thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        return sys.version
