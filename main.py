dbd5634f6bbad1f331c828c35205caffd602d198
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:28 +0000

    nose part hold scrap tear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
