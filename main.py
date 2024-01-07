59445beb56413cffe35bddc29fb3d9fb8946361e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:25:56 +0000

    floor giveaway leg mad contact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def multiply_numbers(x, y):
+        return x * y
