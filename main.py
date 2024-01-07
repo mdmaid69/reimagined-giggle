e51f495751bcbbe7d68ea20cebaf94752952a1c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:00 +0000

    warning performance fill regret any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
