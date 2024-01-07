e2754f66067af489432e0793bfa9ab7f294ee026
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:29:18 +0000

    mix education stick consult cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
