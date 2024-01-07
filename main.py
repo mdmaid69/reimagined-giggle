8a20fdd724721c613c4ca58137b56e088d2481ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:01 +0000

    justice attractive surprising bury chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
