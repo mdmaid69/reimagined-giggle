62da318748c49307523cfd0443e1c5bcd8fa243a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:12:15 +0000

    near final week occasional fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def set_array_item(array, i, item):
+        array[i] = item
