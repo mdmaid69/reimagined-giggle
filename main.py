e0f60efe285b7af8118b7b0f86ef219347fb316d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:05 +0000

    scream task war shut brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
