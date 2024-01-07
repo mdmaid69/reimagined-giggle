c7ee044f99c831d5b0f663bf836c1d36cac494ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:23:09 +0000

    related measure experiment fit hit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def count_words(sentence):
+        return len(sentence.split())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
