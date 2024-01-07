fe739e79cbf1ecf9d9ae326ba01b7c1ae5b32db5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:34 +0000

    possession comeup giveout traffic work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
