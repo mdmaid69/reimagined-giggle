8ec5141fcbece6bafe3118d5d7b50447beefd5c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:23:41 +0000

    concentrate discussion support hit control

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
