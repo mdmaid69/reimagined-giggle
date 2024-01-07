6744ab68b44cf5914b2655301e272f05b86956c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:23:46 +0000

    pattern this sensible indeed radio

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
