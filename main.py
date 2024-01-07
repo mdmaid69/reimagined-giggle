6ab1826a159e6eb3b19254f73ccf05c2eea3f487
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:02:21 +0000

    hurt task match fail proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
