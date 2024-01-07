f11e92b77e93530571dd4aad4fb039b942f04180
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:15:29 +0000

    positive keen style out storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def find_max(numbers):
+        return max(numbers)
