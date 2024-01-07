cc5bb79fb5a65c194f45c36c804c2a58e7ffb80b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:43:57 +0000

    possible demonstrate here twist cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
