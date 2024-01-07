ff91d319f021da1e65f8cd41b5b4250d288bf3c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:29 +0000

    turninto doctor admit miss tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
