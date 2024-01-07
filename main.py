4ab56ec8f84f3a1d370f9261908117babea68e80
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:03 +0000

    rent evidence love finally peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def set_array_item(array, i, item):
+        array[i] = item
