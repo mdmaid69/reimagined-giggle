3a760caab1d1b026070f4b131ccd637166cc04eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:43:18 +0000

    depend brown however relax petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_set(array):
+        return set(array)
