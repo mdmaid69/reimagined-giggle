3066e8a3af0dcc3b02e5a5662a53cd9a71867408
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:45 +0000

    wish long climb bottom shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import platform
+def get_os_info():
+        return platform.uname()
