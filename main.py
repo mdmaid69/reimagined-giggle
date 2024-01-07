ebcc2de40f2309e000c0f8264fbf2de54e6d8ca9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:37 +0000

    along voice available common turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
