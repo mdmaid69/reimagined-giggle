3b9d80aeef5955fa3b2cb537a835212b48683996
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:28:56 +0000

    take tend direct create knowledge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
