8d5fd951415605efd13e39d4e3248673242e5f0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:06:32 +0000

    punish objection string Tt mark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_speed(distance, time):
+        return distance / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
