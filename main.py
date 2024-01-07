fec9cc98da8e4434c500576e103b20cd9540a165
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:27:45 +0000

    various rule 566end wet partner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
