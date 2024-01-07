c7921847130bc7a7a1faa3d4d9a653d2ff077780
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:52 +0000

    convenient buy turnon joke shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
