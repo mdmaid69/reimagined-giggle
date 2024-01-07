3bb22ce0219c716d07f9199a1d77bf31c2452935
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:10:07 +0000

    sign glad branch pour plenty

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-import random
-print(random.randint(0, 100))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
