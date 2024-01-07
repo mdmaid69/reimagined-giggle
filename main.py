f1f8561ec389e48ff6bd13c69746fa6ca719e132
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:43 +0000

    commit produce spin several soil

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
