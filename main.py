6c05f0de94c8915491af3d1407015d84084ffbf2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:20 +0000

    demonstrate science strike well offer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
