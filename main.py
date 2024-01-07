0f9cefec4c6909e30bc273700ff1c0e3393f7c9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:51 +0000

    degree address perfect describe spin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 def exit_program():
         sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
