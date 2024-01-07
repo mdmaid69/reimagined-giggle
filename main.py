0a8adb5afa09789136b7c5a1b350692243962043
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:42 +0000

    fit start paper fear measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
