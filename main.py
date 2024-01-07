7b26867676244969928824d432f53c9ba108aa21
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:44 +0000

    any bottom thinkthrough concentration yours

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+print(sys.version)
