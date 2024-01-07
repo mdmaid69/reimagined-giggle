ddd680020c965e1b33ce4aa72751e0aadbcacdc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:11 +0000

    then eye everywhere bridge reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def subtract_numbers(x, y):
+        return x - y
