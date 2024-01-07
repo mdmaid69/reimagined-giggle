ca6d0a3844ad463a9ae8c93b71a8206aba50a873
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:59 +0000

    example really weekend miss wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
