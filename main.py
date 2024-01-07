28b018c8a3637a0373b193f114ad1f9a4a35997f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:07:14 +0000

    transport bright flat geton Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
