d78f5017fbf3573613376b0ec4e63965e32b9a42
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:23:01 +0000

    night long accidentally invitation belong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def subtract_numbers(x, y):
+        return x - y
