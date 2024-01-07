9103d05dfb84d59087352a941b64df7e24acb8c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:22 +0000

    wall recommend simple wrap organization

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_floor(x):
+        return math.floor(x)
