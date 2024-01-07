961c276e090cd9bb1d34f4b91241d3a559c298dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:23 +0000

    attach sing smell wonder disagree

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def cube_number(x):
+        return x**3
