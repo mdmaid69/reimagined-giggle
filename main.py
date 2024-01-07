e443efdb3d55dcd7660c45c44f90557e52737222
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:18:31 +0000

    pollution case professional turnoff eager

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+def calculate_power(work, time):
+        return work / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
