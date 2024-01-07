944adab3a5aa6e96c7ef8fb9179ec3d6aacbe304
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:08 +0000

    offend even 569enjoy special answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
