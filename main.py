ce0ce35606251e7e29065fb047b04157b44ae5c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:10:11 +0000

    centre watch dress miss hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
