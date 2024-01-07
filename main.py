228e773e9ce8cba7269090dbd5654ec66621e62d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:14:00 +0000

    load deliberately somehow frame lost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
