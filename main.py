d8cefcafe71d1de59bb73c81d1eda4cf9c957c41
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:01:00 +0000

    considering control angry test Ww

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
