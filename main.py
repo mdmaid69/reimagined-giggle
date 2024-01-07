6e31e4d04ce4d992929ed8e8a1bad4f260dc36eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:47:34 +0000

    pressure telephone satisfied desperate dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
