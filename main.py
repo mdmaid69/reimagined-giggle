54085562d0f31414110ebf6006cf403846c9660b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:30 +0000

    centre bunch stop drive declaration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
