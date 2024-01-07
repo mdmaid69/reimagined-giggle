18787368a44178e8423ce44a817bfc3c20ab586f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:37:41 +0000

    crazy want narrow certain story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
