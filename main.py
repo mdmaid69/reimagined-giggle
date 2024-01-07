70a214f576afcd35f48e90e8401ea41e5164ec1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:32 +0000

    all spot spite rich politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_current_working_directory():
+        return os.getcwd()
