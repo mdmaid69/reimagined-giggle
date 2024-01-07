551a97746892ef3c95de48ff4b17d7d249df6c0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:33:32 +0000

    boat on cost accommodation pure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def multiply_numbers(x, y):
+        return x * y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
