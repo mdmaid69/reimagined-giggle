c146c54db8ae789676491be20e3bd70b87326d53
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:28 +0000

    noisy admit exhibition injury shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
