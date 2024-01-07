75be50a4acaad3f2c56f1dbe89bbb491ede2bc6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:58 +0000

    supply divide thinkover lookback(on) price

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_work(force, distance):
-        return force * distance
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
