f0a90ab11e803a0ef95e439713a3132ec80f3d0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:45:03 +0000

    refuse shut air tear show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
