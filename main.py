ef3ced73e8627b0a06fd729f37a8dec0ebce34ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:39 +0000

    cost honest sure surprised furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_arc_cosine(x):
+        return math.acos(x)
