dde6ffb6cd8e08e36356a93eb41f0b45c689e803
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:14 +0000

    pair by turnout dance practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
