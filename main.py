8eae10ddb8e6c934c13ddea80ead681c635390d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:42 +0000

    use grow turnoff passage many

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
