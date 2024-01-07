a4f3dc4f552feba7f54fde871202db7eadfee57e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:39 +0000

    edge geton power fire disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
