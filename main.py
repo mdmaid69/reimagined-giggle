7e99b280b85fe9937ad00e1b9e925ac759bcf752
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:51 +0000

    bye mix cook impression he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
