a6f3312b9a8ed3265b673810aab09a005199113f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:29 +0000

    involve empty serious forget road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
