2a6a47f6d8d5f4d40aa049f57c0ba1d3105348b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:58:53 +0000

    open collect smile common centre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-for i in range(10): print(i)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import platform
+def get_os_info():
+        return platform.uname()
