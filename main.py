b7b07f7d7c732ba6986622cf5c67c202f8b23f17
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:24:23 +0000

    produce notice name transparent undo

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+for i in range(5):
+        print(i)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
