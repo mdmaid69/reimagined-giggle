f87eae6e6dc30083027490cad7e721708052b817
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:30 +0000

    manner measure agree strong Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
