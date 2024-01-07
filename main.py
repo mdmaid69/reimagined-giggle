e60ef9c041d34e91cf37a3e4f81e93589cc770f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:43:01 +0000

    command sore success introduction there

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import random
+print(random.randint(0, 100))
