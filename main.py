1adb6cb214d8071febe2cde8947a7702cd05dc13
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:15:29 +0000

    occur platform call joke hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
