7dfbc6c0fc517b14e33e4e47a204f15e55e7fd8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:11:30 +0000

    inquire interview nerve hurt home

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def count_words(sentence):
+        return len(sentence.split())
