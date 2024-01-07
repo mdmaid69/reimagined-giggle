9b4cd6da472b150e23f9a86e39a453559dc68483
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:50 +0000

    release without praise care mouth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
