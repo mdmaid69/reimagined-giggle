ed11d5c18efae9a7609923a6d49b0b7607917779
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:44 +0000

    used share blame impatient say

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def square_number(x):
+        return x**2
