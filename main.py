e321cbaeffad9a891835423d446de66dc3b0e872
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:10:43 +0000

    rub date visit score hold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def find_min(numbers):
+        return min(numbers)
