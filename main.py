6e2d9696a829f229af1469ef2a56fd199f8a553e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:31 +0000

    account sick either left content

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
