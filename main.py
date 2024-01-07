50373a4ed3a1bf38f73c055692ad9ca53c40db1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:52:12 +0000

    idea ahead none mean appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_typecode(array):
+        return array.typecode
