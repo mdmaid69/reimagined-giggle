8ffdf937cdb88ec94b41a1f05a72eac8b2e17026
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:19:50 +0000

    criticism its Ww spread let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def is_odd(n):
+        return n % 2 != 0
