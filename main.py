cea9ac3c8de176c30babd39b03b9349450463e36
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:26:34 +0000

    shape drive height between loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
