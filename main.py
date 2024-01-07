7aa11e138a93140fab9ab120bf572dc380e53691
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:28:45 +0000

    house door confused future brief

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
