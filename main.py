b98d89c6d09005e7e2024ceefd10821bb3eb2abf
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:29 +0000

    bye meaning match quality pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
