fa92b66451ddac4fbe9179b02897bb76f9c2c2b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:03 +0000

    rough different high wander well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
