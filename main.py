5569715e0f814e8e6e279b611e7dd2cb8d34ac74
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:14 +0000

    deaf importance comeover produce name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
