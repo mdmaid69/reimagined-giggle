87b4ee313b0eb49c9ae7fc9e3ccf54d7d0d9604c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:43 +0000

    punish strong secret answer environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
