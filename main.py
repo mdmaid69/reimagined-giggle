796fe29b0d4a29f026c323d6996d54b977be2caa
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:26 +0000

    raw doubt own should group

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_float(array):
+        return float(array[0])
+  import sys
+  def get_python_version():
+        return sys.version
