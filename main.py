d4c0885e3e772754019aa9942a15b286866f2e8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:15 +0000

    basically small middle runover comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
