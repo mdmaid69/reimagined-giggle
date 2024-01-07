9e72a01bc61493ba8ec41c4ae6c8c7776c051b64
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:45:28 +0000

    chief weekend straight awake light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
