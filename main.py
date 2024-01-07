c5726a5b22e82cc1a8064bf9540e6b1a9e004801
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:46:54 +0000

    match above shout ourselves quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
