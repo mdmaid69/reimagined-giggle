dcbaea68155263872d513ba4c0dd49f674851320
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:26:14 +0000

    sing Ww describe pride flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
