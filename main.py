399166ececa95a19f2c1fcb60ec720338f3e5c13
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:36:24 +0000

    smell begin behind quickly lower

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
