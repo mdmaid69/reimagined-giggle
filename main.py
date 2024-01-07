da0708b530a475950a7a76192eac09d03d75cb8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:13:53 +0000

    sort themselves common telephone spray

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
