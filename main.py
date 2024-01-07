8aa9c85d29e5fef7911a6303e73727cce5977183
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:59 +0000

    about soft use load wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_item(array, i):
+        return array[i]
