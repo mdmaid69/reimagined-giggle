6649afc8d5a6d1f648ed7a51b4cb5223034e33ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:18 +0000

    family estimate hold move though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
