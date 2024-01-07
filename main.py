013087d656eab322764c35dc7aacc76111a6af95
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:20:00 +0000

    shock always move what Oo

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
