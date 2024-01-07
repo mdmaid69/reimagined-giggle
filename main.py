f96c4d59a997c8b1a4a4df8cf0083030879851ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:51:17 +0000

    stone software contrast loan roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
