0f803bb23df51b9534865d616012e99ff4c29a3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:30:57 +0000

    fine temperature private thick sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
