1d97a5afb84f42ff1e474d49bde221f3322ad71a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:15 +0000

    high pass relation since work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
+def reverse_list(lst):
+        return lst[::-1]
