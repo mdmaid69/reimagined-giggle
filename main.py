a800c45d054c42d00ee8efd710f6d24ca54c99fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:58:36 +0000

    centre high path home set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
