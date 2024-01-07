b139a57898dbaa3b3d4aa636511cc7e4cf9644d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:54:15 +0000

    tidy trouble fix fit century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
