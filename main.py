2664027d917aa0f415104ebb75ddaf613c181ca8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:54 +0000

    discuss team nearly single thick

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
