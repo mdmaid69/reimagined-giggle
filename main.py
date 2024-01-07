d200ec6b7fb44d1d3cb016e20dcacf78a43f06cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:53:35 +0000

    mend price gap holdon contribute

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_length(array):
+        return len(array)
