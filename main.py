c1f4768093d244b368d69de170163d86dd130cdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:00:38 +0000

    trade actual upwards obvious makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
