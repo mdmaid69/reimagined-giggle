95f0c9cd6b7274c9107df3bf7937a228b8c717f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:25:32 +0000

    change rid point certainly aim

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
