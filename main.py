117083bf417b12891ab8b4e298e76fc73fd46f37
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:56 +0000

    touch clear remove drive dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
