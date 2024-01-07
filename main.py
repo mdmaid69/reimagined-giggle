324fd31c8b39cb0d90bec92fde59e6c25569af28
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:01 +0000

    week lose blame day judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
