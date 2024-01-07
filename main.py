28afe111c988b727a5a08a337bc6734b4699db7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:26 +0000

    underneath transparent demand something occur

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
