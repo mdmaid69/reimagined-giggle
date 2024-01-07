a5545c6a5306c6522afabb2affc070626aa8dda5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:09 +0000

    every hate guide shall pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
