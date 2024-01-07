e43291ef2ba9f0236ced7571262f050a54382910
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:51:26 +0000

    constantly comeback scale floor popular

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
