2efed7559c806c870caa5b20fc35dc9de150ef23
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:33 +0000

    soil violence confused justice present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
