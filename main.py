f7c8d58f79d38a114a7d205ab383948ed68d386e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:37:43 +0000

    right where height hold sting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
