e2e527a665df9ccc9844fa90e76dc8d5e984d5d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:53:35 +0000

    butter sight word repair whole

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+print(sum(range(10)))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
