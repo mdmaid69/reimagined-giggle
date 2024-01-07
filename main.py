e3d7a8d7dc495f3d04ac4f9f96fbe793501609d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:56:22 +0000

    experience cum hope be job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
