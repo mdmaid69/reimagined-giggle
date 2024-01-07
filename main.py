76312dffd23662a2a145d7e8b8bcfa2842c27e2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:23:57 +0000

    connected regret nonsense principle difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_stack():
+        return collections.deque()
