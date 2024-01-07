d13070dc23c9cf33bc4c52592fe177303d3bae80
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:03 +0000

    general common realize switch port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
