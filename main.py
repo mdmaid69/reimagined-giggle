a24625f41d31969761a4d2a55c21136a5da8d5b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:12 +0000

    black once wrap last reserve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
