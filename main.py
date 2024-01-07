5b6dea651256e51292c7b10dc308268d76e9c20d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:27 +0000

    mine shade run lead various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
