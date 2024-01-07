b44fd12e0b15bade3c57bca3f9456284568e7169
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:59:52 +0000

    avoid left love proud shopping

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_max(lst):
-        return max(lst)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
