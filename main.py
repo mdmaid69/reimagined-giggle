26238e50a03793f9beaaaa1cce300c95f790fc71
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:51 +0000

    company familiar school pity communication

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def add_numbers(x, y):
+        return x + y
