dfea5d4aa13a4568b57f695cb8bf6139c0e67e50
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:32 +0000

    earn produce familiar square final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
