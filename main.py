7d709ee99985be30e62e706b77d99a1171407ea8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:00 +0000

    until drop work busy money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
