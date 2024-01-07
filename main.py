59c86cee86494ab9d7994e5dc3564adc24269ab2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:58 +0000

    weekend worse present complete second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
