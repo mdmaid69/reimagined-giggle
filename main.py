eb08ea31b789e580edc2edb42ecbb725de0f6c37
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:27:05 +0000

    vision against rule unit disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
