7e248081d3ec3dab37580b551d04ea0fabafbd32
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:44:05 +0000

    free slight uniform wide mad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_acceleration(speed, time):
+        return speed / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
