f103e6d727566cd7e8b086c5fe25e40cdf7b9a31
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:51 +0000

    anybody press pipe undo prepare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
