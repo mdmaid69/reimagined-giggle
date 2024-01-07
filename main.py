53b33a9f67b82ff57d460026dc7521e8f1e6e65f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:19 +0000

    morning giveback silent turnover permission

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
