d8be4d2d9b75d20696bb1f12dace6febc85af684
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:26 +0000

    beyond wait unfair uncertain who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
