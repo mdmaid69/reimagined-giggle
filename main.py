c31c3e617f172d05f8c27738eddf3bcd38776df2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:56:48 +0000

    weekend order value cause little

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
