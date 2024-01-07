e5c3f6eabc6e163521d7fad4831ae326fa139e91
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:11 +0000

    thought pattern protect mad century

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
