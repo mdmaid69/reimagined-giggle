da6484c0f0d0a8d6c81210768890a1c5bc99e35a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:28:03 +0000

    pity there everybody until pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+print(math.pi)
