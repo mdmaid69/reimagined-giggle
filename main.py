b5d81d577b3a2c79a26f1273cb417c802a86e0d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:40:09 +0000

    final night cruelty mind leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
