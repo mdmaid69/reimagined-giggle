df876db72c22a9e7a5ade2fb438acdd082b870f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:30:35 +0000

    way anywhere better heavy opposite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_stack():
+        return collections.deque()
