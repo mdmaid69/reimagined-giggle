dcb42797e9aa7dfe55b555d3989c94fc890f8de7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:56 +0000

    to after fly luck right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
