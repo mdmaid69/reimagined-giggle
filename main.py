55a5de178854eaffeea3aedc162795cfc48dbe18
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:33 +0000

    best my period even rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
