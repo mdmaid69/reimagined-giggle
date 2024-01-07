730f3d480cedb1baff6da1f602b379583ed352fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:28 +0000

    narrow thin of fall myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
