61922b40b98fe341d72220d020d431260bf0444e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:20 +0000

    objection disagree central later way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
