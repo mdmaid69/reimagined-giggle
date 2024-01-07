546379b4b966f631e4a5da411ef8d85450b0c4b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:34 +0000

    sore relief explanation situation Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
