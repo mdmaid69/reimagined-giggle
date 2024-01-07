4bf27aced92cbdaf65966cd5257dbd0a0ff2db89
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:04 +0000

    insist hurry central study wherever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
