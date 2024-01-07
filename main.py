0a30278f020f1b0cb9cff5daf84b4e6775c592f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:16:18 +0000

    prefer demonstrate uncomfortable further condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
