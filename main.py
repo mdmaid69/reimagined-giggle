86b3f4add383e6f239196c581e6bb24e30d040e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:45 +0000

    its miss scene up explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item(array, i):
-        return array[i]
+for i in range(10): print(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
