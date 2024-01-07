7893f566b0deb12512d9e50d30161d6d92033a85
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:30 +0000

    during door favour I say

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
