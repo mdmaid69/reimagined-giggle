138b4bd9b47594410a51dcde0121f0bf29fc72cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:12:47 +0000

    reliable further blow together receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
