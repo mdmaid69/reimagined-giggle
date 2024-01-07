92c8701bfbe9cea599ea5549e06749abc6321da9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:52 +0000

    mine physical rely intelligent priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
