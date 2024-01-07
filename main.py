dffc25e5f1d6c365bc7d5cf54749e601bff8aa3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:42 +0000

    waste shake hold turndown home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
