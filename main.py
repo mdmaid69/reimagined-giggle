bc1535a7ae2d316984012c77a297c83a4a825fd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:53:53 +0000

    leg ashamed off snow really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
