eafe69b169a27e57daa4e866fc1aec5ded27875b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:25:08 +0000

    decision rush can pretend method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
