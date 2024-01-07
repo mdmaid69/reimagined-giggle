a14ed654b7156f678dab68e5449a799c6725f5b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:31 +0000

    great anyone failure special city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
