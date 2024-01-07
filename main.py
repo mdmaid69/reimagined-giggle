782abe31c55dab1c37f02e81fa19d06c2a95c909
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:53 +0000

    proof sleep actual still sure

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
