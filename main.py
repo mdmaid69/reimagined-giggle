d4418c5e44f1ec5f23ba48c922b2e09f816bfc56
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:12 +0000

    lay dry argument cry disagree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def split_path(path):
+        return os.path.split(path)
