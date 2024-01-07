7e3a4f4e39df34d56dfc727aead6bae6da31a3bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:52 +0000

    risk foot easy dear attach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_list_from_array(array):
+        return array.tolist()
