e7639968bbdfb954caaab88778b791ef76568e14
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:40:11 +0000

    display comeup voice dish exciting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
