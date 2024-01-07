156cc594b9bebbb17a49ed1539e039243b713953
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:27 +0000

    face content season calmly interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
