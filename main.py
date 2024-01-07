a0696d823f566bda77c6e1a000f4e31b8492fed6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:23:16 +0000

    566end thinkover smell demonstrate refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
