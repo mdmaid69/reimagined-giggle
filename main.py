fdad55f34b39295d2968d52ae56e917ded45a879
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:11:37 +0000

    somehow winter boring confuse little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
