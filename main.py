3562b104e00d9b9bccacadef320abd1412889615
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:09:21 +0000

    disagree remark choose imagination chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
