83529b80e7fca631392441961e7ddd609c3a9e3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:53:08 +0000

    bread dig Ii remain rise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
