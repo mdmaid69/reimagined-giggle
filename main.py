d415673997264e0119890325e1eb845b622162a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:38:12 +0000

    straight deep thanks part calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
