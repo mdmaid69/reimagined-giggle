93ac05eb02088e3b3ae6f280da4e131e6b17561e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:42 +0000

    computer tap question head chin

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
