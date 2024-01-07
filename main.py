d3cdee545f0113bddbac318ca03814f652d331f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:05:02 +0000

    judge clear she receive freedom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-print(math.pi)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
