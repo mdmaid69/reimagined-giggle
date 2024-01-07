80399a18db87c1bfc9dceaf304a0fb701a24c75e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:28:17 +0000

    difficulty treat switch shout day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import platform
-def get_python_version():
-        return platform.python_version()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
