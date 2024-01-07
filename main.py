75fa7d46d85240b54aab9d940fa801fea8222ea0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:29:58 +0000

    decision like so swimming certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
