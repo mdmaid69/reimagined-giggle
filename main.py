a5aeb62a5464621e46e0562c120bcb9b822ac854
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:29:49 +0000

    birth treatment pretty run winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
