e909b0b7fedfaf165459a51982673d36a9a6ea8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:59:53 +0000

    challenge team quiet remark bar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
 import array
-def get_array_length(array):
-        return len(array)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
