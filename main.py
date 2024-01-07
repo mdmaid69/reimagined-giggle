bfb3f567ff03fccf8518816a8256c285203fbf6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:02:31 +0000

    community pay debt expression plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-print(random.randint(0, 100))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
