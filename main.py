fdf588715476199cbf44e9fa08f81770fe949f34
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:28 +0000

    imagination space limit almost cost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
