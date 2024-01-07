0de867d4f8e66c9a0f32040fbbf2b44f4fe53044
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:53 +0000

    for feel reduce noone thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
