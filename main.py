c876a326909ee49d7302f1c4ce04193efb350a5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:20:46 +0000

    rank flood unhappy arm achieve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Squared:", [n**2 for n in numbers])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
