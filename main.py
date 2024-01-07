5e83d18bb1f10ff7162f84c53d550486287165a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:21 +0000

    special wild stiff since vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
