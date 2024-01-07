61c25e7744cfd288bb67d6316620315470a5abdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:10:47 +0000

    there fear underneath change then

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+def sort_list(lst):
+        return sorted(lst)
