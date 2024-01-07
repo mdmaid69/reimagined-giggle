36db63cd8a8a432e2933b5bfbfe2bd7f433cfc93
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:28:59 +0000

    whistle they target taste bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
