1f99f6db19872a75633f81a3c427a44a12a6bbe0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:20 +0000

    wave heavy giveout fetch notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import getpass
-def get_username():
-        return getpass.getuser()
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
