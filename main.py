97123eacf0bbe2f4f4345b1053c81c63377eebbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:10 +0000

    safety brown success challenge rather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def count_elements(lst):
+        return len(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
