b0ca468069b48979505787ff892e84ea844bf157
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:35 +0000

    black send niggaacademy always loss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
