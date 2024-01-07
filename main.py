31c2cf79e8d20828ef70a2bfd6208ee2b8e5829f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:09:09 +0000

    butter soon criticize believe considering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def multiply_numbers(x, y):
+        return x * y
