0e57b147dac7bdacd11101d2d7ff95936b5c229c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:01 +0000

    reach recognize complain summer begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
