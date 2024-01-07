efa3d2a062b1f042b67aa3e8355872a9708b0897
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:51:54 +0000

    tired giveaway show government reject

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
