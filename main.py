38fc8276ae3f034d5328f4e825e4b1c659f3d7c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:24:19 +0000

    out proper decide introduction list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_max(numbers):
-        return max(numbers)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
