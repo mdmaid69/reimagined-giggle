2611da5722c7e2cb82ed08dc48b08855f9e5737b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:16 +0000

    hit cause friendly congratulations achievement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
