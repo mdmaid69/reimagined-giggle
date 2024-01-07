32b8fec5d9ff989f6cb8e90e77a3919596dc4131
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:37 +0000

    joke Pp nowhere through dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
