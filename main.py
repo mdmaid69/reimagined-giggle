fad388f8790655f1c97155d2b35d890900718e00
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:08:01 +0000

    ask happy runover exciting art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
