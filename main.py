5712bbca9f94bfc25412a77566eb15ce43d7d157
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:51 +0000

    nearly split arm contrast everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
