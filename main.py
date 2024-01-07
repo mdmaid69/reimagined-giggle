d66c1e2e2f98271aeacfc4a5fb41bb7e7f37e353
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:16 +0000

    valuable guide before fork injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import sys
+def exit_program():
+        sys.exit()
 import itertools
 def flatten(iterable):
         return list(itertools.chain.from_iterable(iterable))
