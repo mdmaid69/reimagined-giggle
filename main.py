b3a1b5fcdbd6d76a718710d3a4d6ab52e47a9783
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:16 +0000

    joke wise wander image beat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
