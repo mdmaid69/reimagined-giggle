ed4f30a63d76fa56b5abb6a5d3408c26c845ed9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:23:36 +0000

    unusual stupid part better discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
