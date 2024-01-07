e8407ea4f541ccc9403a0dddbcb80eb9531c0066
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:57 +0000

    something create better offer them

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
