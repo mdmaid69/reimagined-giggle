caa51487374bae782d37751fcbe49ecb4a06302f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:00:07 +0000

    lost spend excuse proud fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
