fe75c5a703e5f0f07f4205a067ae806672dbb390
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:29 +0000

    rest along advise noisy someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
