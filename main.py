8989ea538f3a503f1f808206bf5e314dfb946b7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:10:04 +0000

    friend his secret uponon pocket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
