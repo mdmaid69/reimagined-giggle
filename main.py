1b2023896a7c92db7b1cbc1f960f68aac7b44ca5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:17 +0000

    almost along sweep crazy when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
