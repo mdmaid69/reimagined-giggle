e014654ed39a603678374a1e933b477f7f865971
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:27 +0000

    alive reject grand delicate pipe

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_current_working_directory():
+        return os.getcwd()
+def greet(name):
+        print(f"Hello, {name}!")
