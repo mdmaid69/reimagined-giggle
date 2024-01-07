5c5f5a8ae2faf894c3e0010b0dc9a138b9cce4e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:09 +0000

    noisy pride much period estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
