153f87511e7cb19e1ff88b9a1c5d0ac6e2d5387b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:22:47 +0000

    bin occur straight female word

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def print_python_version():
+        print(sys.version)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
