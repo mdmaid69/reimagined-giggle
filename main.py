18f5f680e63f278180db74b47e702fd12bd49df7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:55:16 +0000

    together goout relax funny send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
