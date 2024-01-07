cc8a935030fc96ce94e235c1781109e552020dbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:29 +0000

    country unpleasant butter lower read

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
