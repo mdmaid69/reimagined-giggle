75f5351f370438d5650a2f1d59319645024206fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:21 +0000

    sort poor occasional slide on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
