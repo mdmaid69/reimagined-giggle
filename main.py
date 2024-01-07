8e0458c3adf659e0af8ce999d816a840cbff76e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:35:04 +0000

    picture inside common old Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def append_to_array(array, item):
+        array.append(item)
