2361ccabcf2846968baa2e04c5d9b81286056403
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:37:23 +0000

    neat usual sentence minimum sensitive

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
