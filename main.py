82e438fb1258e261bca65040127247d1466f19fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:26:10 +0000

    blame major accommodation pencil lookback(on)

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
