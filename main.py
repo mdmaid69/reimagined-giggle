65b08de7744f7d0a78569fe8074ea11ffa0f36df
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:28 +0000

    almost next market nor space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
