fa95098e6694f7d779654fbdfab0d3f247b44423
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:25:18 +0000

    stupid silent throughout make female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
