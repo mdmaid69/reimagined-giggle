3f7c06f420ae2ef914e6efab6f0c19b48a819c6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:39 +0000

    clue empty workupto shape command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
