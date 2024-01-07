813c3e6ded91cbbdd102e265f4d167cdea0858a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:49 +0000

    judge tragedy cut turnoff development

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
