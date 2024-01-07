bc5ddd06e39ab8678eeb414c9d27fd63a84cc05b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:37:47 +0000

    doubt characteristic nature keepup reach

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
