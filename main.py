f1afae0bf93406aa34a267a6ddc01ad3fc4036db
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:49:33 +0000

    region cruel warn unite with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
