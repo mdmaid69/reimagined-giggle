bea6ac59353d6556f65d7f74a0d101bb2cf7efb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:19:12 +0000

    so furniture chase keep distance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
