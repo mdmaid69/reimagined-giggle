606262effb1f20b8e3c7bd4abdfccdb12681ec4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:42:28 +0000

    like discussion hit scale worse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
