f3a32eb2410d33db02adb81f4b869eaa8bfc9b70
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:07 +0000

    talkinto warn everything smart surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
