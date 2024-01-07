8d6cfd48538ee714b5d6c238289a82460a0eafb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:59 +0000

    behind fight care speech small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
