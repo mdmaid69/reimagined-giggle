52a73f77b80c64aff0026739a57b0fb7bc177cd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:12 +0000

    Itself trick strict appear challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
