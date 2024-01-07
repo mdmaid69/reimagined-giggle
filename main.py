afef4096e2e1f02d46b39e93702f6fd189d75b2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:08:22 +0000

    pass impressive debt runout combination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
