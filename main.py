c27619f560bb0e46fa002cb18289c876fbad5bca
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:41:31 +0000

    skin track breakout comeoff possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
