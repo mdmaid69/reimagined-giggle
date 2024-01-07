db787b2c4e6736d32f2a0466a2f6c19e66698f96
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:46 +0000

    brown soak abroad acceptable total

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from collections import Counter
-print(Counter("hello world"))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
