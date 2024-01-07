cd79ddcfa6b7665fb9b3e011a688278500cce3c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:12:15 +0000

    limit greeting false carry boil

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
