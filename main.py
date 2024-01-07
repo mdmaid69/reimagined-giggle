1762a23412eac3ff6b1e50347913fb55f03fe22c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:50 +0000

    error example delicate weekend protection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
