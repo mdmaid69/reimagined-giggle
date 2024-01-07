c9fd2f498c3467286bfe496919d54b5ffecd3bfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:39 +0000

    want breakin claim lesson whichever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
