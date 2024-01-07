ea76cca48eadb44b43e3baa15c8941450781db29
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:28:01 +0000

    together law bath doctor terrible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
