6679510046b62627d2d7aaf5fcaa21f90c3ce8d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:47 +0000

    rough maybe show place pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_list_from_array(array):
+        return array.tolist()
