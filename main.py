178320b545485f8d95d8d665b549dd6b20761087
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:57:13 +0000

    advanced block several best along

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def add_numbers(x, y):
-        return x + y
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
