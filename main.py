ecdade14cdf32c561a317bff1c7db63a470fe064
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:52:53 +0000

    interfere crime give taste church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
