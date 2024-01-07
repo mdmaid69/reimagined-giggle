44b750127741062d51e3d3a2c7aebcf743e7fcc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:17:30 +0000

    even offence new wait comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
