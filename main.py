26dada04b71b46e6e75fe4ca21ac5b3faf69a6cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:49 +0000

    read actually worst up similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
