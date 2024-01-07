7bd393433bfdf9f0ec21d767379fc0ecf37d3fa0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:31 +0000

    face once stamp attention set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import datetime
-print(datetime.datetime.now())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
