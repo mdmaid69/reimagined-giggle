c1a5b90629241a8b16a986277406d4b0896c9374
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:21 +0000

    long provide address everybody suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
