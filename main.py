2175d6bfb27cf23fceaa92fc019624a8b995f312
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:29:42 +0000

    view born warm argument dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
