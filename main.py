aeafcdb9e9a91e93ac4db22ebeb9b3a78c153db1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:18 +0000

    alive sort edge Ii involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_density(mass, volume):
+        return mass / volume
