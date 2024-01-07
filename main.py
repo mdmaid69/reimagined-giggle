9456962887d433d00d8882f380f9c01f9aaa42d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:02:36 +0000

    extra clear see contact collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
