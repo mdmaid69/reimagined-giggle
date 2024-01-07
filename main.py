795afa1d0af2b1691329f50832d303a218763da2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:42 +0000

    typical visit wild hardly subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def remove_duplicates(lst):
+        return list(set(lst))
