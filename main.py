7f147de14d8098440f9c535942c64d8422d75f41
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:12 +0000

    same process matter pin length

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
