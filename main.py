78116f022927086bd16ce8eae63aaa123c9e5934
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:36 +0000

    steer learn community interview power

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def find_max(lst):
-        return max(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_list(lst):
+        return sorted(lst)
