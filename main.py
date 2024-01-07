14c91d214b12118ed91c26e83db29db8966de967
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:36:59 +0000

    handle future muscle letter instead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
