c11c60190315c780a8d87e9920ab8b3f0a1e8c68
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:07:44 +0000

    dull claim give famous development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
