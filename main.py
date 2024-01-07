ab7fdf106f65525dfdd6d4014398e0b4106af81b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:39:32 +0000

    school lead sign either runinto

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
