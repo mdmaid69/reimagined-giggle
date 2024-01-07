4c278238b36a5290fe78e5ea16a1f577f89aacc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:38:35 +0000

    free spare brief into trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
