fbeb9bd38d0a09cab72718d7653b819f69156fef
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:43:07 +0000

    finish set deal display big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
+        return oct(n)
