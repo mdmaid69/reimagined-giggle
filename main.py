36ef0d6b19fe66177745ea08d519f6d19605dc10
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:42 +0000

    condition rough temperature bye copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
