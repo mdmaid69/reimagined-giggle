cc91dcf3a638851dae19278da84a6c420103a06e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:58 +0000

    use frightened poor worth centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
