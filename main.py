90a92de6896e987737cbc4d3e38489fc9d6e1abc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:07 +0000

    comment everywhere except drive breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
