e98bfaabeb2b710c84753ac9401ec24db85d0bde
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:43 +0000

    strict gather hard film dear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
