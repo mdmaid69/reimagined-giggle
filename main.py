7ed17b6b02b651f8b7b2988713b20112778286bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:40 +0000

    anxious floor dance crime tough

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print(sum(range(10)))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
