301729f42d7cab56388c018af9f9200567de7bf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:58:21 +0000

    after why Pp praise across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
