2af10fc9cc5d742cbbe4f7a66f2100c50f511517
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:07 +0000

    shake exhibition mine curious pressure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
