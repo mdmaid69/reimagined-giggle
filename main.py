990f187b43182fa1247d0f251701277aa4be9a6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:52 +0000

    manner ocean quickly instant quality

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
