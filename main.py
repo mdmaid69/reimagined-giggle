c1d07214406e98122e002f4e249d27bee9a85a2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:29 +0000

    mine floor such game press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
