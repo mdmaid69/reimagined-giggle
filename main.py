d276e951324a66ff146de95f33253d1708989ee0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:40:26 +0000

    mess release drawing sink earth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def add_numbers(a, b):
-        return a + b
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
