84d5710e65cc7bda5bd945b704de51c082921563
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:49:38 +0000

    content classroom let open seat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
