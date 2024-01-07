6ea3776b5729850aad8c8a57e6dd8a63964bbe6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:01:05 +0000

    remark maintain family attractive bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
