a01fe195d60ac7ad2484cb07b50992f383181c9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:11 +0000

    book example base here neither

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
