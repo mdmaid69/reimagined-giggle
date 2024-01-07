07d3086f2b160155848beaad776fc81a102363cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:57:31 +0000

    nonsense priority course wind sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def add_numbers(x, y):
-        return x + y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+def print_python_version():
+        return sys.version
