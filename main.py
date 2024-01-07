8deb76ee3bf7a25e78b0f652eeca8fc069bfa668
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:41 +0000

    freeze crash smooth tip switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
