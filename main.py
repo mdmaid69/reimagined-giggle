579258faa5c6dfe95185dde71de7cddb3e6fbc79
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:37 +0000

    future region big equipment brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
