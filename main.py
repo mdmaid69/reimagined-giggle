f0c6645fe700fdcb90364bdd30ea609cc11c7a91
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:15 +0000

    sad basic possibly responsibility public

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
