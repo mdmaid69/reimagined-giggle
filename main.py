dc8c424a563331baa0e10b6feb71d1a7492f3e2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:33 +0000

    now short round colour decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
