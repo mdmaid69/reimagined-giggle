d6c20f7d997af15d8d020740dbd05457018a3b61
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:35:55 +0000

    thinkof loud trade except father

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
