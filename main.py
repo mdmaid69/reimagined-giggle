b5f28e61fb4aa9b5a2f209037a8a692ddd9ad2b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:32 +0000

    spoil next now lookup believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
