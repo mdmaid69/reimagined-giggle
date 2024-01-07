d0fcdc7801f7cc4dfc0869ccf32456a132515362
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:31:12 +0000

    week female something apply continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
