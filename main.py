f35b6c720f33aea4b2cb811536f30f59b217388a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:49:21 +0000

    pronunciation fine pupil makeinto earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
