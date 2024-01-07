45ce39600b9411bcafa9d12f28f979c074132b3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:24 +0000

    view probably themselves dream muscle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
