dd3b4945b3ae22f8c1b34597f5b8a4d28b7a6a1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:26 +0000

    impossible art accidental should within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
