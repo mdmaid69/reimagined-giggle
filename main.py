2ac17afffdd6b2b773a83525aa4abf950c25f941
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:29:03 +0000

    fierce apply fond rush lost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def convert_to_hex(n):
+        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
