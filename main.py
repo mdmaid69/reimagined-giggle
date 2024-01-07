16dcb1de1e08e61a80c270810252658081c1441d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:03:19 +0000

    square getup first fix favourite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
