b8beaa1410d7ca0f31b39f8776eccc089e5817f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:25 +0000

    call geton expect jump without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
