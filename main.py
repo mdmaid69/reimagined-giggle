36e5ba13329928498da4739df4113f5c2b2fe2b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:21 +0000

    would accommodation excited order thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def sort_list(lst):
+        return sorted(lst)
