ff7e7649077d2228783d8464b768851f6652c116
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:29:36 +0000

    566end bill any pain drop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
