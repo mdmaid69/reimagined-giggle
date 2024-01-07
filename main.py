82dfe29ef5a603a5dedc9635ade812e9912d062b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:36 +0000

    check that benefit usually everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def is_odd(n):
+        return n % 2 != 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
