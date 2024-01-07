e8f20d6730474899931a245e47a4c3d4e1e1adc8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:10:29 +0000

    active there when settle long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
