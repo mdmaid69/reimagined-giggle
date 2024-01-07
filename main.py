6acb5d4259c840c7c86bf97d1e5ec2edc0148187
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:03 +0000

    talkback personal attach track thorough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def convert_to_binary(n):
+        return bin(n)
