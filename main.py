086245ac7e57fae888f0271c0237e5c8f6c0af6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:53:21 +0000

    borrow Uu skirt gap recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
