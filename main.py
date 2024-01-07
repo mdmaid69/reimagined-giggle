bce6a15b73598b41db364a0774753069be01d50d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:04 +0000

    hello law total tape Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_current_working_directory():
+        return os.getcwd()
