70519b7405eb0a589463c53392706cdf7ceaebbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:29:59 +0000

    rest claim moment fault create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
