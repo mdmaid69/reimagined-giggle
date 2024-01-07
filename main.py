aeb9462f3225ac3670f25e8a446a3da5feff05e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:02 +0000

    interesting risk arm flat very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
