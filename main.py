f97586d6dc374c9fe2a8d310bc8ad63a51462616
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:03 +0000

    tour under improvement deliberate ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
