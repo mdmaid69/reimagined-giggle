74fa30e8a86d81352ef6ab249e37421241d43d30
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:27:06 +0000

    wish kill well fairly study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
