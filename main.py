9521da1a346d0269532565e9815769eea513ec89
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:21 +0000

    exchange love print work sense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
