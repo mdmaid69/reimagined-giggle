16447cfc2096bc6caac2812594c7578efee6ad30
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:02:37 +0000

    clean declaration imagine shock allow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
