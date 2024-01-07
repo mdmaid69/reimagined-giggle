257dc4e04f07b57eecbea3e8a92eb554672a8ecc
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:55 +0000

    split smart book like keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
