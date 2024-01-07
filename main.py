e0b83771ff48420cdf9b47288a711b7534133a96
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:38:40 +0000

    appear doubt recommend unless make

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def sort_list(lst):
-        return sorted(lst)
+print(math.pi)
