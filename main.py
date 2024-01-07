94cd46d5d1fa851ca29afbd9d3912fad53a3b09e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:30:55 +0000

    not responsible in communication ourselves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-def calculate_perpetuity(payment, rate):
-        return payment / rate
