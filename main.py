ba6825a773448db18084fc39e4c8d4739142fedb
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:42 +0000

    keen recommend grade worth apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
