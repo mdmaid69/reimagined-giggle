420a9aeb711e4f3f5e50cfb5e4274b8e42c9c6b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:13 +0000

    action paper creature sister underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
