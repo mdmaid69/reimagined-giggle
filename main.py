2a4ef59803d859a18a7e50027408945a8dc0ed08
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:43:13 +0000

    bunch my care impressed purpose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
