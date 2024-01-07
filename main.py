712950136bde8c664a5629ca393cff7754fb95a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:30 +0000

    serve true protect otherwise female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
