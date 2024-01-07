4290c9e05561665789ad8f7af1a5ce8221c57936
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:18:55 +0000

    his future prepare release often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
