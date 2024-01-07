323925157e63b9d8c36f18966807418970e6f23f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:18:53 +0000

    wrong survive right surface call

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def exit_program():
+        sys.exit()
