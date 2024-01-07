7fd8645dbb45686e3a25310426beaf842b6564e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:09 +0000

    hollow turnback cost absolutely money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import json
+print(json.dumps({"name": "John", "age": 30}))
