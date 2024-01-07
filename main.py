c408e8a0f6cf0f14aeb375409bf1d98f500d82ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:07 +0000

    shout rush bottom wash private

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_item(array, i):
+        return array[i]
