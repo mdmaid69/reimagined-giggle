240b0b30d45536928aa8f479b4476e7c0e5ee105
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:27 +0000

    ashamed thumb comedown like silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
