7c8060a0eaf95b5c2b655912e67e65ac5347c478
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:31 +0000

    average world real pin travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
