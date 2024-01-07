bfc3791441f351c2a82ae7b6c1c64b4b5af834ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:08:19 +0000

    more however range spill across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
