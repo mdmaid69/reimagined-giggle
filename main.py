4bf5bb96087eed403cceccd97ebb6687ce3055bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:19:46 +0000

    excuse swallow section exit from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
