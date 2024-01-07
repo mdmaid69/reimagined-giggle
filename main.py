2e8f2241d0d4f705020522e18a3cf5b28b9b6ce3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:56 +0000

    smart close real room nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_unique_words(sentence):
+        return set(sentence.split())
