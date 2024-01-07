69c2390eff1072a5d69299a9f92898e99298b908
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:01 +0000

    unit sea crash under mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
