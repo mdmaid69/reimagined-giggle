685b8c9fef10172fbb81b0e6bf6129de26cd2062
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:48 +0000

    typical limit production share crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
