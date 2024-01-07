dbc9f234ad199cd9fa6ff70fa42194725ccf4c90
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:15:49 +0000

    remain wake emphasis up hardly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
