6593b03d6c720114f84c63bd0de0601edfaf37b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:23 +0000

    dull comein duty mention damage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
