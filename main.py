c9418ac401e29795442767a2c1d659395d258252
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:41:25 +0000

    neck whisper careless suggest die

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
