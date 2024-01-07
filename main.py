3aaf70d8bff4c2fe5525c8675c21727ff6755259
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:15:34 +0000

    command fix slice workon worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-print(datetime.datetime.now())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
