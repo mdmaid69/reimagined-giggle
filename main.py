5363124cab880e093bcd2bdab419b844f8355464
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:23 +0000

    may speed least serve exist

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def greet(name):
-        print(f"Hello, {name}!")
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
