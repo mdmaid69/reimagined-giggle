b3da5818195e857736bfeb25e3912a437db7be31
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:58:05 +0000

    hair whichever culture chief backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_stack():
-        return collections.deque()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
