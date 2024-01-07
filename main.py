9c335b1b3f94eb92f8fc7c3018931fb9f54a0bed
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:59 +0000

    goback(to) second evidence stop clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
