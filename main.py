d5d0adf97b85a89c8506b3a80560a946dc8a7030
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:17:13 +0000

    famous represent paper trial arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
