4b5b97b52ac478fa4a6e7a37631c6a8113b8d827
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:05:26 +0000

    turn solve accident substance everyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
