c68e8573c31b5a77d8849b810e44892f8290e157
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:30 +0000

    wipe admit description callout low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
