85518de13addba4a4e258a6963e0a17ffbf32091
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:22 +0000

    about criticize child defend boil

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
