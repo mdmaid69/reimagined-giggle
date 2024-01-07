f24e068a4ba00fd9bf50f61b6895e971d587a527
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:29 +0000

    back hit instant shelter possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_counter():
+        return collections.Counter()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
