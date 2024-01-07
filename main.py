3ad48b83f3b74070e6fcfc2fb57e46970a6e4c4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:15:43 +0000

    wonder principle send hello winter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
