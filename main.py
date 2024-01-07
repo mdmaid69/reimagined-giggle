7d15eadd3f5aeb43386850f6e9ba5b53285060ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:54 +0000

    makeinto feel physical workout satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_bool(array):
+        return bool(array)
