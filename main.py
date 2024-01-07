509796feae06f49367ca6168db7350fd6118879b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:06:12 +0000

    middle year public problem minute

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_min(numbers):
+        return min(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
