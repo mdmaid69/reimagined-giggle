23a0a82f0c7aaa2b72a44b7fca98827a8718b039
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:59:59 +0000

    damage prevent short summer block

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+name = "Python"
+print("Hello,", name)
