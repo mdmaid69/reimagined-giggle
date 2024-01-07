424c76b347f5dd9828ae297aaba9412f5e498b41
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:57 +0000

    traffic along command second threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
