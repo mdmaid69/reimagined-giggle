af8a58bbdea259783aad1343754e1fc56515c2f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:17:40 +0000

    town loose block makeup takeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
