9eadfc3afd95ef72bce31c85147d0cc884e12e07
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:07:34 +0000

    degree know call autumn nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
