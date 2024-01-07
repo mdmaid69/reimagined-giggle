ab88950225b2b4797c478723abc444d155850e44
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:26 +0000

    maximum another justice open grow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def sort_list(lst):
-        return sorted(lst)
+def get_array_itemsize(array):
+        return array.itemsize
