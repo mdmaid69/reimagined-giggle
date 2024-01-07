1510286acc68c764d2cd207e4b3070a01830664a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:08:29 +0000

    effort reject solve fall broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
