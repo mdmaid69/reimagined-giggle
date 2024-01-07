a09fffeca451b720451216eac1b4f5cf7df2024a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:13:29 +0000

    serious ticket present afford sense

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_python_version():
-        return platform.python_version()
+import time
+def get_time_since_epoch():
+        return time.time()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
