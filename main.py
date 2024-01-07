77a60ba36595d7e6134897432ea682ef7e230a5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:58 +0000

    failure trick least cover earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
+def subtract_numbers(x, y):
+        return x - y
