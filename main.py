352e23bb946bf6a58e818f6df20b81f72c01118e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:30 +0000

    workupto shelter brother trial just

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
