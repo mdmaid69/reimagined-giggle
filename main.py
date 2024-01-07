9bf59293da63e5109a328bcd417977d516f7a838
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:58:04 +0000

    effect mainly lower please regular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
