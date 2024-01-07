14bc7e17f4c7ab2056694399f21293ca8bdf0746
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:28:39 +0000

    pencil excuse side past bridge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
