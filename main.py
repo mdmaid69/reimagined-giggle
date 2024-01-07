a45f98cfedd990ca38e73e445ebed3e039ffe37b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:03:55 +0000

    choice closed acceptable write pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
