caa1a9b0744f070348c0a4035b47b65405202420
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:11:54 +0000

    turnaway ball through between advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
