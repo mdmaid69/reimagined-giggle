bb3d34d4ba4bb8f2708219764c5af106eece444c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:22:33 +0000

    achievement turninto when loud wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
