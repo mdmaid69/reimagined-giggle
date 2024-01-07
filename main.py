c7e1941470952db434a14656be93d434b2f4575a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:11:27 +0000

    replace cause callat estimate recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
