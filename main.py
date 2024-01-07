514b32ba939e88663e264bc1c808a5275f966915
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:52:51 +0000

    wire sudden market good brief

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
