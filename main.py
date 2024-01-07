5884c36d9c0b09b256a7ca0b4380ec077a4f6c95
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:20 +0000

    backwards friend confusing conduct course

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
