645eebac96c72181371c7e282715ef21896e1804
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:00:15 +0000

    culture major treat march clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
