27c6c8e0ca96e7fe154205194c8f30beaa375653
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:55:35 +0000

    takeout word getback runover remark

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_speed(distance, time):
-        return distance / time
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
