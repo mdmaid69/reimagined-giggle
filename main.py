cd01795827a15dd58af8e4d21bedec3843259563
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:25:08 +0000

    himself paper design region expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
