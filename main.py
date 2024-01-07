896027415e2c077b38f65d438493f029c164d5b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:55 +0000

    shout emphasis small call by

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
