bed8be275e384d8fcddc4b1da26775d45225a446
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:03 +0000

    pinch lay carry might constantly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
