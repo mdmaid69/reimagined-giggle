8684aff6a8dcfa1dadbb144864001e8c5a98ea2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:01:34 +0000

    design boat receive sign study

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
