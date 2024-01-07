f49808ca2b571aa292a7c85d5e04b0c923a904b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:24:39 +0000

    away lie control basic book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
