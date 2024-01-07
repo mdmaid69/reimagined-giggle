0146eb8c1871aa4458365a267f7e4c2fb4e3eb4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:44:36 +0000

    holiday related equal base satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def convert_to_binary(n):
+        return bin(n)
