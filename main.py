e0b59e327dd94bcba7715b36f526a735dbb3e32a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:18:34 +0000

    case company pour workon would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def multiply_numbers(x, y):
+        return x * y
