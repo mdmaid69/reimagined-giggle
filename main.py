5e0cc5d4b7eb4f08d11e6d5623ede104a2a240d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:59 +0000

    trousers respect snow shade actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
