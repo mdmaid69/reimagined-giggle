f3adf97fe5ac4e826f7051afc142b044288d84b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:50:54 +0000

    narrow equal suppose building bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
