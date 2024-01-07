78deb46bedacfb92caf4cb13ad2d36d176d0c283
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:51 +0000

    drink turn makeinto positive lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
