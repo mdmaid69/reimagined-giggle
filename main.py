2b4495cdfbeebf05354845c6b3e523f982d12242
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:56:33 +0000

    light spread who bin desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
