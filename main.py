bd8d8bdbdeac857677dfdddc4d13dff8f8fa6ac5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:28:52 +0000

    wing her glad do steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
