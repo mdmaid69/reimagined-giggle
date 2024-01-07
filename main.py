75f9c1dafe35198e23822c6fcbba093ad5c41042
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:36:44 +0000

    temper band mention course swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
