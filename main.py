c2098d4fcc798d69ab7cf1713ae9f4fa82fc0b41
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:48 +0000

    quantity actually bringback official nature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
