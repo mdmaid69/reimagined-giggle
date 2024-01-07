2a7c89cb866849ba729314ea12ba18484475b6a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:43 +0000

    mess favourite allow uncertain last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
