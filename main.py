102a99f2dc37cf059a1f6152985bd7fb437a6aa0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:16:19 +0000

    cross open church question test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area(radius):
+        return 3.14 * radius * radius
