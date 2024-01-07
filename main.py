cc110c3d38a48ac1572b27a3f1591fb00afbf54c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:21:27 +0000

    ought reaction image share reply

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
