3a04bc3a94b1fb5de64e76e7acca2a8088c2401d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:57 +0000

    slide doctor innocent share try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
