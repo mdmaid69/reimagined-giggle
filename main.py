04df7d74c4c6ea304f31ba057fd3e8a5d5f05270
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:23 +0000

    sick keepoff blow bother smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
