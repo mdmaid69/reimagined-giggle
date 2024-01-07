9a8f6a5768f98e10c7c75568e1583057f7ca8dc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:40 +0000

    note drink range Dd estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
