a225d73e2399a129405b309f19c14a5f4424c580
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:05:58 +0000

    compete safety escape pride concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
