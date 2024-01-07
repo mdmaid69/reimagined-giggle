da71e37e47c13eddc903237889ee6b10374192a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:09 +0000

    charge service comefrom information steady

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def find_min(lst):
-        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
