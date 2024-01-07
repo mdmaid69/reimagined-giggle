e0051171c8097b873dc492c79abd80517e8b5ac1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:25 +0000

    holdon line Tt me once

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import datetime
+print(datetime.datetime.now())
