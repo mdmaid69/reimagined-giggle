a22c486fc6e5f4e1a0b8006b72167c57438bdde8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:49 +0000

    smell compete above convince getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
