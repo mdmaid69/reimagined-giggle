13598fd93367f2aba5e669fe1cf5f4941e1f7b6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:27 +0000

    that impressive command hit question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
