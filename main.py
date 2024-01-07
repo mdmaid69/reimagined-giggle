4028eaa0e64b8afd2cdf02ab6bdf75fffd2df114
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:18 +0000

    fill fold case show act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+x = 10
+y = 20
+print("Sum:", x + y)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
