440b80fa101ce03a46e1f3acfbd643599caa43b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:12 +0000

    so wing trade win dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
