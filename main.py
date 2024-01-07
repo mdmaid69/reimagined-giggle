eb645498ecd2c190caa39389fb09cc36150f855c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:44 +0000

    determined generous emphasis basically board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def reverse_list(lst):
-        return lst[::-1]
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_bool(array):
+        return bool(array)
