b8d07541ebbd621a1ec51f19dc8b255d0904ac44
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:57:32 +0000

    question fast proud such improve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import collections
-def create_user_list():
-        return collections.UserList()
+import tensorflow as tf
+print(tf.__version__)
+def subtract_numbers(x, y):
+        return x - y
