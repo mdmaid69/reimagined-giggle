1456ec7141b47f92f1a9470a4840a82dbb10a2f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:37 +0000

    boil price insist touch ago

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def convert_to_octal(n):
+        return oct(n)
