4d5b8e8e09cde1dbe3c41c6cf86438a87a71653b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:34:04 +0000

    shame touch estimate chat mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
