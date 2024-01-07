618a0db1c0ee8bb18ed613a229e7b93327796f88
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:21:12 +0000

    store about exciting development arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bool(array):
+        return bool(array)
