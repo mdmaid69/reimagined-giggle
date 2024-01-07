54417b9b1e5517b565935bce9c95311dce4852b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:51:55 +0000

    read habit sell year geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
