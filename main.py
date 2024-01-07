a454d5771c83393b5812c2c31d0308a05b03fd63
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:21 +0000

    average breakthrough shine skill identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
+import sys
+def print_python_version():
+        print(sys.version)
