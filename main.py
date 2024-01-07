f4ab89c6ea0e72ae5b42307bbb21e509e7c33381
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:05:15 +0000

    notice class either bury kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
