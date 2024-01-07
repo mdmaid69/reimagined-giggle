750ac2ab87482f7180bf2e58ed2de639cba8f591
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:10 +0000

    first call most patient butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def get_array_as_complex(array):
+        return complex(array[0])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
