77caf2cc06b7b07e85ff04f929da37f781c38f62
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:46 +0000

    road burn afford bye substance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def convert_array_to_string(array):
+        return array.tostring()
