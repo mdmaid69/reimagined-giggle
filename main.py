2300d70c00212e9cc0f16a3152e95d2d811ab7f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:35 +0000

    radio content by size lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
