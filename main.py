d83d9c71ef9c910ff298388987e91b4c2f04eea7
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:26:08 +0000

    alone realize cruel set furniture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
