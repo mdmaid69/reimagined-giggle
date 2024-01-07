22641c9181bbe6e04b057057051613f3e92a1860
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:42 +0000

    any care whenever let software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
