9b7a86da0e2696f03e37446c2565308c08a4639f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:54 +0000

    tone Pp meeting shoot variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+print(math.pi)
