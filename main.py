3b2859859132cb898f0d6a98cf366822e3e98e7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:27 +0000

    pencil collect tap cook any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
