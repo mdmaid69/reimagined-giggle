34246bb6e0501332203e78fd862f62e44bee4324
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:24:25 +0000

    rush get count passenger dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
