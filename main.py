16f2b1eaef4501eefbdfe4c810722d3da54c1e67
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:48 +0000

    perhaps square custom into until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
