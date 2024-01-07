aacb1a38416ec0728819ebc38e37f05668bf0322
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:06 +0000

    nose stranger production Vv past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
