e006675ab11f3a2c022cd62159184ccf94db827b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:02 +0000

    stir world disturb shout rough

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
