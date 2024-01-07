16cd2140faca8c357a7997528f7779728fbea0cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:41:35 +0000

    break sit nearly accidentally course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
