b25fdfcff2021980c1e07fa909a9e10b298e475d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:24:58 +0000

    theirs bus lot regional general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
