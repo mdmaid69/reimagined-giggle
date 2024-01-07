513186f70d8d65b1e3213709c7642b5e86212c0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:12 +0000

    behind offer whichever skill soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_list(array):
+        return list(array)
