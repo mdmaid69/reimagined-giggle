0c7ca973ad8b844a18bad5e9edd6bc1183113640
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:26:58 +0000

    thin stay cheap holdon unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
