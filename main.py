f7b6b90d0878556f42607d3664920818bb298a44
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:11 +0000

    mail tip switch direct too

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
