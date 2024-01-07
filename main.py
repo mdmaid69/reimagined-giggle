72787ba1fd34761268a279e2295b91a330b73b5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:11 +0000

    injury drink sometimes person roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
