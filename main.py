a44a3b8903c38ab00cbbc41dbf7d9c703b37e145
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:08:24 +0000

    listenfor principle pretty waste ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
