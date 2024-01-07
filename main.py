9915a7db37b90cbd8cdff4bb299f2abceae84d6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:50 +0000

    work decision on exit Dd

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+print([x**2 for x in range(10)])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
