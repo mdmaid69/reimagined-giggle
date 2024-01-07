278f030cae880a46974ad2471d0818aa5e51a20f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:27 +0000

    chase almost file actually skin

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_ceiling(x):
+        return math.ceil(x)
