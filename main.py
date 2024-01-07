f0cd58b5b94c907c57328bfd4e75142cc7b963e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:22 +0000

    band exact want learn idea

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import platform
-def get_os_info():
-        return platform.uname()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
