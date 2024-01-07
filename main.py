776db8952ee0b6552185e4a8f57db1c0e9d64adb
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:08:10 +0000

    address born guard worth consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
