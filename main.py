ce086f06275cabdf2244740a175f672bf331ac80
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:46:53 +0000

    love whom temperature have work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import numpy as np
-print(np.array([1, 2, 3]))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
