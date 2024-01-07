5ede0a181a32b1f565bcbf80498f18f180a28c2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:41 +0000

    impressed forget learn speak cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
