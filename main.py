d69aeaef01ba7b8ab1d38ef9d9302bc1cb02fdef
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:49 +0000

    march extraordinary glad tongue dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
