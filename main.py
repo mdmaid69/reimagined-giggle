7ad7732b89c6b17b9d9b7a8da425526c401d60a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:08 +0000

    mine or politics respect level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
