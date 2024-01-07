1e28ee7bb2b72c291899d89b4727a1de4e403872
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:10 +0000

    usually thing female week character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
