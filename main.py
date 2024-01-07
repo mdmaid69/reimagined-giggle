542ea6e6af1316c6a5cdda170ce1fe30dbe153af
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:05:25 +0000

    owe thumb will dig wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
