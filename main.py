a2cad25a7f470086f3bbe9d998dc27f421e60d02
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:42 +0000

    favourite mistake classroom exercise exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
