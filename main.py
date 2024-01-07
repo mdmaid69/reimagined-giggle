c6c11b0868ae00150919d1796dea2de7015f3d29
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:33 +0000

    machine hear sweet punish comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def print_python_version():
+        return sys.version
