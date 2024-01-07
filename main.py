176e5c5e67ee3afabd66b96d33e95f80d4826b22
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:53:07 +0000

    behaviour bread beautiful statement miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
