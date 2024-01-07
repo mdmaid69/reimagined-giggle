0bee08671fca993a3e91690b44bed70cb755d73f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:38:55 +0000

    development course relationship fire love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
