6388e223edec8780dbc0aeeaf54fa19e9c48d9ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:32 +0000

    health priority legal impressive colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
