e16dd5fa9e235c3e58a8bf43b91d7242a3346357
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:58 +0000

    word connect train escape measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
