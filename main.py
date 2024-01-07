7625d7fcf52cf6174b1ee83b9268430616e5a4dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:37:17 +0000

    meanwhile object contrast solve extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
