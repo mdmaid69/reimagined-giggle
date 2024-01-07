8b9b39401976c23d7405dcd5c2fa7a9e0ee49595
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:29:39 +0000

    repair ticket yet turnout forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_pressure(force, area):
+        return force / area
