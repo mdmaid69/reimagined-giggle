c14eb2a3bfab46cabcec28fe84b1560e85e5bbd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:15:25 +0000

    small maximum broad feel hide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
