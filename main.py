0d3a2138981964aafcec83f581941ea4a64e83c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:35:10 +0000

    see store opposite lonely fond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
