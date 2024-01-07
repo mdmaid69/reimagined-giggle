7b5306bb316c0ce36f2664f61c7e0aed63c98c8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:58 +0000

    debt minute permission 572enter cinema

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
