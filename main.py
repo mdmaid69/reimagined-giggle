ac350096be5532b3fdab432b735229a4352ab920
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:39:00 +0000

    spring unlucky desperate family slice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
