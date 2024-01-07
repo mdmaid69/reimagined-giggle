004c1ea9dc089cb7069bf8be3477e8b29a001e13
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:39:37 +0000

    but achieve stand measure prove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_even(n):
+        return n % 2 == 0
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
