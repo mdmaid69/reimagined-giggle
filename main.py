159e4cd47bcdce8d76bcc3486252af12ca729f29
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:49:47 +0000

    criticism refuse talkinto order invite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
