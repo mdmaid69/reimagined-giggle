32ac853da3c8d8dc7300824c83d063433b16a1b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:59 +0000

    corner wall building seat discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
