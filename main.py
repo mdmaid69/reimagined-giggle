4f1a98e25f99e4521336cd1d64205b37e5557f52
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:12 +0000

    container action smart oil reason

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
