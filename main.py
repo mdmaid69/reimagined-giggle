772feb86c1d7a3678c28d8d866971d5a04788915
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:17 +0000

    callat Bb bother night question

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def count_elements(lst):
+        return len(lst)
