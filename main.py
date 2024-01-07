3c7e03fdccc2a07c6436c939a783b7bbae633fe6
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:26 +0000

    touch splash tour argument finally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
