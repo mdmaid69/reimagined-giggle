b03118a42e70dcdedba525a3ba6c013c4bea474f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:33 +0000

    mine turnback admire strict strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def remove_from_array(array, item):
+        array.remove(item)
