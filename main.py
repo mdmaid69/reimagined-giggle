eecb788427c5b0f4c32304b873f6bc8a8ee03e54
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:33 +0000

    harm soon share drop respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
