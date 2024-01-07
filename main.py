7550f5539a7c87239ee22c4f3a772423336574ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:51 +0000

    safe smile warn error live

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print(sum(range(10)))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
