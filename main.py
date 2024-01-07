72c72d27cc7ec5eaecf2e187614a2ded6dd7ca25
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:00:45 +0000

    trouble post hope practise pain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
