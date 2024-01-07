228a5ab6b0115597a4b6d4a6e40b6942c79729e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:44 +0000

    effort order idea matter hair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
