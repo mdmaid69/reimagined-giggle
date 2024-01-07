54550f8535c1573e64afea2b68c066054ffa0801
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:56:42 +0000

    people legal best terrible announce

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-for i in range(10): print(i)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def reverse_array(array):
+        array.reverse()
