b2075b40ac51f3cd70883fb37c9d1cb7346f5e16
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:56:39 +0000

    sometimes table finger shadow act

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_speed(distance, time):
+        return distance / time
