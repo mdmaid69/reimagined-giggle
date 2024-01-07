1942ae6699c22ffed2a2469ff64b36db792936b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:51:40 +0000

    in tell risk level room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def get_array_length(array):
+        return len(array)
