e1f16ee8452eeb0920061cee5d34dcee42a3bc9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:03 +0000

    slice shoulder various previous so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def subtract_numbers(x, y):
+        return x - y
