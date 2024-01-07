3fc1d120d2f8fbed5d2e9e130960594f2f13a4eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:33 +0000

    clear reserve comeout pay birthday

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
