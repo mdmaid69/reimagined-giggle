a8561d026a37691fcedbccc43723aae890cc0370
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:49 +0000

    nose us excellent general command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def multiply_numbers(x, y):
+        return x * y
