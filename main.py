c02a52fd12104d002d83286b2e08f7ee18ed48af
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:55 +0000

    warning crazy fierce lookafter last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
