7695c6b222b0e8310a521b3b4ec668a0e4e9a097
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:07 +0000

    stick underneath responsible accidental 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
