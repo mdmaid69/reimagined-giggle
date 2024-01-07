1b7ed447425f5b9a5a781b758ed06ffccf0cbe20
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:05 +0000

    neat just fence proud pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
