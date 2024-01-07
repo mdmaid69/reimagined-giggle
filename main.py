45498c5adc4e77125cfb357b06ac5bb736c1877a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:15:49 +0000

    pay hide impatient state large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
