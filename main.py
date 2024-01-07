45396198daeaa7c48fa9ad6d87f2ec1946144363
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:47 +0000

    follow pattern Bb ocean information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
