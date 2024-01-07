16de63b31edbbe26643515c95d995dec2bb88112
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:49 +0000

    brave attract finish occasional stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
