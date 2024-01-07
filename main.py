be06caad95d38161fb9dc4159b5d9a2d13979fd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:46:41 +0000

    breakinto switch slightly take unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
