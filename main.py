2bae2f5236b97c3e08303654dc0d675519f6ab80
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:38 +0000

    division comeout for food abroad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
