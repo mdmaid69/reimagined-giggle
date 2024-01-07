b800247551ee1da24d0516779ae76ebef6219ec6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:57:21 +0000

    should record sticky exploration breakin

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def get_array_as_complex(array):
+        return complex(array[0])
