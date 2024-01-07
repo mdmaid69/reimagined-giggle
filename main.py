ca5dd19ca2c01cc99da0c7d5634679041f8c0b3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:49 +0000

    bear relative use lead getaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def find_max(lst):
+        return max(lst)
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
