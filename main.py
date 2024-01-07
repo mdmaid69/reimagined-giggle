6827907b4e7329c9da38396359d3a690b5f1bfce
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:14:23 +0000

    charge further serve keepfrom this

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
