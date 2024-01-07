38e211b27c0028ade9175bb9bf742343a8c08b2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:05 +0000

    hair minute turnaway path Tt

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
