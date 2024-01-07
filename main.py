7788fde553416ebfb65df44dd1666edb7138f8cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:09:14 +0000

    pleasure reasonable protest relax bar

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def change_working_directory(path):
+        os.chdir(path)
