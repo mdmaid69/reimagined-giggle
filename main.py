cdbb273f8cc21a7653f364c643428919f9424e21
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:51 +0000

    relief tense everyone inquire battery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_set(array):
+        return set(array)
