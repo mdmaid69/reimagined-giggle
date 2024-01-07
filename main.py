bc99d1cc648a31597a5e0e3e8330385728e0868f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:30:33 +0000

    pool nose voice make past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
