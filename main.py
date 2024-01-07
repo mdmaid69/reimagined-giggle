ab504183d2bdf212828d32c96861ef4dae4c9e2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:29:10 +0000

    needle 565end breakfast winter me

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
