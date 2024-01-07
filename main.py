1f5403c63ae1b233488004432df9aa7343f0e7c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:30:54 +0000

    suggestion past window lot opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_string(array):
+        return array.tostring()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
