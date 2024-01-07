8e28f3c426eb6435fb1548ea97ecfd75fdac6b52
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:25:25 +0000

    match memory convinced soft comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
