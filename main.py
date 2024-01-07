2ba895b76e496a4124457559123ca7e551bfb2d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:08:59 +0000

    cold town pinch little greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+from collections import Counter
+print(Counter("hello world"))
