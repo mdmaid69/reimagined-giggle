7cf977ed7b1b3b977f9caf3cf59c4075b979d45d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:12:06 +0000

    nurse muscle calm suit responsible

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
