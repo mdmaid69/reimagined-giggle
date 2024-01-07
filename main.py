582801a631e44c9925063688b48c78daf09bf181
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:30 +0000

    bottom thumb education possibility cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def convert_to_binary(n):
-        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
