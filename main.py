75d631c6c92d481ae64470e65e9abf8bbeb4d25a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:43:09 +0000

    rare path noise start smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
