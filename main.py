5e6c7b6fd9ba964ad5b0dfa1356af9c03af9c5e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:37:26 +0000

    borrow passenger blame watch disagree

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
