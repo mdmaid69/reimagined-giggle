a4cb1b7bcd59581697dc07bfee2fd15120bdf123
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:17:40 +0000

    hope new sight bridge war

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_acceleration(speed, time):
+        return speed / time
