fa28392f77c9520abdf46dd4d73068ced8430616
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:26:43 +0000

    suggest Nn night guess wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
