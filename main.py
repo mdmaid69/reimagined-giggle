e37b08df9fd0979dd47eb622638b43cee74773d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:59:31 +0000

    doubt powerful piece unfair steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
