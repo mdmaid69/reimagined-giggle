d39ee619146c3079aee8aebff1638903b6458b78
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:59 +0000

    measure learn track put many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
