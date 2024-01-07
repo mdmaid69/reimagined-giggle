8658afebd5ad2db9251817a0f9a24b3fd51e410d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:34:29 +0000

    strict goback(to) worse try trade

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
