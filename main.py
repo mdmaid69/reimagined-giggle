f29a150315df0ad6baad3d4c1d2afcfb75c1e0c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:19 +0000

    cinema valuable snow pronunciation care

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def is_odd(n):
+        return n % 2 != 0
