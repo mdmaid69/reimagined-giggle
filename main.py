c9898e9c746a8c4f0376479d06445c02713a9885
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:27:40 +0000

    crash choose mind seem each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
