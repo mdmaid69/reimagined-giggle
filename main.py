1e4432d585210ed44a1f41f8fb2c9659665c17b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:54:36 +0000

    lonely own whatever tidy speed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
