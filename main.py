84b3ea7a9cb76f06e57b03f25e8114371951ca42
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:30:13 +0000

    kind Nn ride all 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
+  def convert_to_binary(n):
+        return bin(n)
