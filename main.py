96ff3117739d0a78503bad9bf67b9a856397c924
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:51:09 +0000

    exam nothing car send police

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
