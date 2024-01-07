9c85bab55523198b3462b32c5f6bfcdaffe670b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:32 +0000

    introduce reduce gather actual thanks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
