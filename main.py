930be1137515e317c60df702ddaf76826236388e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:01 +0000

    child shut quit base brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def count_characters(sentence):
+        return len(sentence)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
