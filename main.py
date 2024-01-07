f20f780c005899f8ed6a5e20e06881235a80093d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:45:18 +0000

    confusing case error square dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
