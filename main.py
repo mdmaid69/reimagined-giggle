bea6831787cba29f5b4cb5513489759cfe215212
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:35 +0000

    ours leg drink shame both

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def convert_to_octal(n):
+        return oct(n)
+def sort_list(lst):
+        return sorted(lst)
