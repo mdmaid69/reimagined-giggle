1b065c6406e751dab5b12c764fa25f4fe092e012
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:42 +0000

    wave dress summer transparent offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+from collections import Counter
+print(Counter("hello world"))
