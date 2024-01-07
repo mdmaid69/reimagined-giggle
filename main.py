a130c0b3f304e5bdb8cadf9d5b9bb316db893b2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:16:28 +0000

    sticky exactly what arrive set

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
