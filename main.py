e466422bbc2e5ac490e4361af3e3d1bd24ef0fa0
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:57 +0000

    correct workupto study maybe tense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
