63ca42371abcae500af87c756f2cfe1b4cf64bf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:35 +0000

    long practise eat occasional mother

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
