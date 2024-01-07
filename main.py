3bde65c5f7bf24f8606ea81e164431fff06b6279
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:06:25 +0000

    tip shade onto also attempt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
         array.tofile(f)
+def find_min(lst):
+        return min(lst)
