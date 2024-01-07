e5babebdeafa0c00ddb6306dfddf9101b181fdaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:07 +0000

    soft suggestion unhappy continue exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
