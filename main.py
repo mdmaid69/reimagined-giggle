5327d69c887ba7d7858d673e2102a9971545dd7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:20 +0000

    rain mystery straight lock boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def convert_to_hex(n):
+        return hex(n)
