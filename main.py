a039b21c86ee35a0b712ef6f13d1754bc73e0fed
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:15 +0000

    soft bush on no mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
