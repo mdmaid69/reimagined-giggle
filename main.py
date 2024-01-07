22a89fe0d5f4d4a5cbd41ac25a312470d6c918f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:43 +0000

    rob worried but keepup cover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
