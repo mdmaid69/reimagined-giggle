491da3a9786a2aa62500b1ac7aeafeae6a622f64
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:21:06 +0000

    release machine cross scared meal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_priority_queue():
+        return collections.deque()
