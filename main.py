7110eb4ca672f593a1205e859d1eefe8574e9786
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:30:36 +0000

    breakthrough callup harm lock Uu

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+for i in range(10): print(i)
