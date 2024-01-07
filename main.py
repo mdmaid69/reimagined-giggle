8eae20b0b36e10fa200fe37b2758a0635cb0752f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:26:20 +0000

    slip heat rain interruption law

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import os
+def remove_directory(path):
+        os.rmdir(path)
