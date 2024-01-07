6eb68199df23c7378535da7898e1b552b37d572e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:21 +0000

    all immediately lot introduce bet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
