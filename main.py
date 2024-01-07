4031e6bf745426bbf507f68c0198b3359819f168
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:08:08 +0000

    mind born lower row study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
