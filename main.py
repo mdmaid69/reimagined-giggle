b379baf16f31944cab29f63f070cedce32baff80
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:59:44 +0000

    touch she everybody appeal oil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
