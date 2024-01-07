25bae26ac55ef39d9af746ed62bd25ded53791b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:26 +0000

    visit worried soak brush wave

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
