ffdd571cd7c72a0e81c344ac8b28facbf9b5dd4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:35 +0000

    meaning wrap plate opposite behind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
