52f750057dd9d84e9c8e8ed70eacf1842443fc3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:53:07 +0000

    pay callup somehow possibly join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
