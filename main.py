58fa017ddc0676998dc1da62b402d9343cc31650
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:57:55 +0000

    major swallow oil relative people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
