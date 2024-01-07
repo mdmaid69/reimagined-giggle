bf7ff8348b9547f818278def99250f3d0e9772aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:14:42 +0000

    bringback measure practise Cc bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
