bc7451a37c188fd0473e3829e0435cb1d16726a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:43:23 +0000

    wrap risk side wing evidence

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def get_array_as_tuple(array):
+        return tuple(array)
