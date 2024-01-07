36753c8cb6e8197267c527a3ad7f1be308657fea
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:13 +0000

    charge meal convenient big score

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def reverse_list(lst):
+        return lst[::-1]
