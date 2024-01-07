4b8a28864574f4d5db5acd167a525eeeceb4cfe7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:09:16 +0000

    slight closely below impressed justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
