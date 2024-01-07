c981c325445e5a0ecd2e75e496f9c65c06b16027
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:03 +0000

    otherwise callout mysterious kind tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
