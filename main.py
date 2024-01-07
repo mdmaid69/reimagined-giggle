595ca056d9c2af3e2396b2017da88ae2ade2f4c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:50:54 +0000

    evening hour nurse modern deserve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
