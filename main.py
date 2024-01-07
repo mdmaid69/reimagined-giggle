23c043589cfac87d9fd2a2dbde06e71df1a440f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:30:10 +0000

    phone should pain reliable finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
