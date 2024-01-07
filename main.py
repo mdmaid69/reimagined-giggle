8754356542f508ec91be7b18a80e605ab194b17d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:56:35 +0000

    print practise line 572enter somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_set(array):
+        return set(array)
+import time
+def get_current_time():
+        return time.time()
