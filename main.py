dcc82620f01a736c78fba72a2f9e28404fc36252
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:54:59 +0000

    refer statement under regular protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
