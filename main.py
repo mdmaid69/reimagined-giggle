2de62b289325df8a92281996cfdc2ca7a370ebc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:30 +0000

    weekend type degree unlucky speak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import collections
-def create_counter():
-        return collections.Counter()
+def divide_numbers(x, y):
+        return x / y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
