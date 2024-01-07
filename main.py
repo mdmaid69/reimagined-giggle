2e8634eb21172fe76ba6aa45bfb63defa4407a58
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:31:17 +0000

    talk now once process room

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def create_user_string():
+        return collections.UserString()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
