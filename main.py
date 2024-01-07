511cd1a5082943f1ca574f255f3f8a744ff56e69
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:26 +0000

    refer kill character tidy eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_str(array):
+        return str(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
