c10f4e532ed0151ca66a11304f8e7c1e45ca6d70
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:33:43 +0000

    neat cough reply load talkinto

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
