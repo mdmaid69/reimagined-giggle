78c9059a5a2db63082b294d3b27fa6e5380eae6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:45 +0000

    odd pretend strange on he

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
