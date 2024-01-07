872feb030d343133b13b8f3b6839ac90766636f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:44 +0000

    exploration poison development as best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
