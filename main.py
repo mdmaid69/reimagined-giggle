e881c783e7405480512cf57d08834022b27512b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:08 +0000

    point appeal such brown waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
