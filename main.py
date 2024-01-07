9c4b804600f70fd60446d9323c39689cd0f3fd8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:32:32 +0000

    dangerous plant everywhere wave demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_speed(distance, time):
+        return distance / time
