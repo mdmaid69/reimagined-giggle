fd3d439dfd83b3acee6153071dc038e6f5cbda2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:42 +0000

    turndown any keepout patient near

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
