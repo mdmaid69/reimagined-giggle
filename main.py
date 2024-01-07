8216ad3dca1493c4857bcd758d976f3ad04f7f94
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:03 +0000

    they board wake choose emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_area_rectangle(l, w):
+        return l * w
