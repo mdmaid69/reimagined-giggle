e699a3d8e44b86a26032ebb61eeb0d1915bd54bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:08:17 +0000

    count plenty pinch judge fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
