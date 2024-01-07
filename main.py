0fe7c0d8a7362ce6113ac71bb60cf23b7ee4d898
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:57 +0000

    nowhere where serious slow disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
