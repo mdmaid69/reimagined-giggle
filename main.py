bb7b1b3016cc8e9d1e043facd0a56ceda9b4a31c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:06 +0000

    prefer deal flow connection across

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
