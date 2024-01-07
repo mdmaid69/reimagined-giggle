115ddbd0bcf2a4935864abbfbcdc7cd552f8ad98
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:08:12 +0000

    pin reply takeout tax Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
