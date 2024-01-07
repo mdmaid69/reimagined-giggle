fd5f5a4b3b2f4b75e1eb931d5a017c99511ad656
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:00:44 +0000

    cheat care style once rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
