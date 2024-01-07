8f26ed83e73657fa4eb80d0788688a1fb728a5e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:29:45 +0000

    sorry duty dinner closed female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
