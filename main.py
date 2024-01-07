04fbcd002de05ebf82df4adaef72049018d26c29
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:33 +0000

    range floor brush man pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
