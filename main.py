79ebf1db598fea5349ce6339acef511a9e8b8f60
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:57:17 +0000

    scrap healthy advanced day birth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
