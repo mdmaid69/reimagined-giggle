fb73a0ff6b8f47da8bcfd0a34e361185c43d2f18
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:09 +0000

    572enter spread spin rare mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
