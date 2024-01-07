4049c0913e7dfc631e84ad32e5e6aab7e13dfbbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:27 +0000

    organization lookup smart black before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import os
-print(os.getcwd())
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
