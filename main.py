dc86c38fcb05782a2f7cb72f4482d594dbce01a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:00:17 +0000

    else noisy game forever bother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
