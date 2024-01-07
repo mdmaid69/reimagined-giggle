bfd0a42a4bb9f5c63806fbe66837ecfb858281ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:46:39 +0000

    turn birth leave soak cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_acceleration(speed, time):
+        return speed / time
