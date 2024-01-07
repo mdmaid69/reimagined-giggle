f8438506544d81a3cff296c4dc7a2858fe5e69b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:14 +0000

    task everywhere whistle light dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
