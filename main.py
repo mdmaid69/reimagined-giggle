2c7a94f026e74702495b47a109894d1be92af306
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:21 +0000

    regularly fresh beneath influence identify

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_directory_name(path):
+        return os.path.dirname(path)
