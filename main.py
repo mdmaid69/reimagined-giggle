abc5d79aaa54a5d45b99b92801015faeaf814123
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:37:51 +0000

    exactly immediately weather hair safety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
