c26ebbc04aa1d7f3f33003b276902684c874a999
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:26:48 +0000

    move callout home event storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
