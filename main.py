5570bc79ce97ff4319e37a9e0b5a1b3c6325ec41
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:15:11 +0000

    responsibility depend owe formal act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
