45d5716f782f8cc60ca109abd4686aa2dc1c771a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:06:09 +0000

    label nerve town callout Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
