e1e746c7fa6d7dfdbc10542b4e696bde0380e3a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:09:07 +0000

    best tell turndown guard phone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
