ff4d61e6f64bbd6f97ae954af32e5e971af84d26
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:48 +0000

    task excited food match book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
