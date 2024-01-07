e5c73bb416a3a60a6fd4fca713eca780a2d67354
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:23 +0000

    mess spin probably story almost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
