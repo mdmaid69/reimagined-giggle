ce6bfe0a8d0881d050c12f88bf83cf38ca8cb424
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:49 +0000

    afford dinner react 568energy father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import sys
+def exit_program():
+        sys.exit()
