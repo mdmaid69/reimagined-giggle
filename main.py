c29d6178cbf8cb4effd73792f4ca535c97e2a5df
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:54 +0000

    count programme ilovedicks please road

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
