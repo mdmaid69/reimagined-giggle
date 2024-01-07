4b8714f59c7c8dd6337530d3cd50ca18c833582a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:29:16 +0000

    thought also record attract board

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
