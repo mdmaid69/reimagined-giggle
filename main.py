2f10131b282d3a221f6f10345689aba4545cbf19
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:29:39 +0000

    push open stop into material

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
