0883d0a6185ef22e9de6e4fc908621eb74951bba
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:05 +0000

    study journey real custom great

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
