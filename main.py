6a0eb85106722e23183d01e37d20030a245ebfde
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:36:28 +0000

    expression past yours secret container

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_words(sentence):
+        return len(sentence.split())
