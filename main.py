0e9779a99481eefaafe22669e378bcfa10810015
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:01 +0000

    together effort week mood so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
