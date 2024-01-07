14af5130a673aef5d9e772c1acf2103dece7ecd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:41:47 +0000

    dull enthusiastic through breakup nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
