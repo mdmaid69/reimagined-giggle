c8ef7c27fcfe9448983d6b7eb80ab158b6c09397
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:29:56 +0000

    sleep reduce cause other proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
