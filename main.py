46d4456696261732ee1b56e599517124fe8c9e7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:57 +0000

    this advantage foreign offend skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
