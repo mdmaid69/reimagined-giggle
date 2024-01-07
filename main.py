5d4c08f5eae14de856896f91727e66bef29a155d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:37 +0000

    crowded go swallow emphasis maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_palindrome(s):
+        return s == s[::-1]
