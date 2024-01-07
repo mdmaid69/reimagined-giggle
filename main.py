ae2db2a553cbcb05a79f8500b362f4738cc74d84
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:50:57 +0000

    separate back record bread gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
