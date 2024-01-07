c40d38022ffe7a69b8326edb0379dc9cc778b8eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:20 +0000

    our arrangement difficulty front where

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(numbers):
-        return min(numbers)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
