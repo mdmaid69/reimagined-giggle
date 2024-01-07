77df0f42291471ec3345dfb5d47b52658f09445a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:45 +0000

    comeout collect pocket like silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_counter():
+        return collections.Counter()
