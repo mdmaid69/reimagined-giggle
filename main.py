827a21b4f88eb591a116589e482bdfe97907ab5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:41 +0000

    anywhere takeoff hardly formal telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
