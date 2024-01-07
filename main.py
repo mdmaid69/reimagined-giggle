d152419d53b45c2ac86913bb6a5ea764c99a3ca5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:52:43 +0000

    remember pressure may area pay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
