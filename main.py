e8d432c0ae612c0932cbd8d5212b611dfbd4ea8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:26 +0000

    stir settle tall calm snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
