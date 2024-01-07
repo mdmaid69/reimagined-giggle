25591ddbb72faa85f2cde152921438f91889cd15
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:58:12 +0000

    Ll leave ocean sticky kick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
