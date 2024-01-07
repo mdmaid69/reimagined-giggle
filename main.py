d1c2d0a325d4f8a4491331e703d79f5358c9ef78
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:32:30 +0000

    guess already report runaway doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
