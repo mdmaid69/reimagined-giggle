2f47699941fdea51e2ea0a276924a4daaad712ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:15 +0000

    equipment cook order party mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
