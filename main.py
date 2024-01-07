fd329eca830612e452201e128d7c59dbaa317b0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:48:37 +0000

    spin 569enjoy lucky original slice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
