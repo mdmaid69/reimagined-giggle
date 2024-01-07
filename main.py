09b1be05ae7d4580cc54e614bf68f8c0eade76f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:58:41 +0000

    for miss place kick me

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
