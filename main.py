20f385717c451c801645f11217cbd71fa32c5a76
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:54 +0000

    lock polite dry borrow waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
