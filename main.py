a64a4b28e421ba9d9caf4bb84404f8bc3f32729a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:40:58 +0000

    impressive onto finger spring check

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
