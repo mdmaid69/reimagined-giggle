9900523e1637a69e9d4e5b40be4686505919d4d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:21 +0000

    rise upset imagination heart false

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def square_number(x):
-        return x**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
