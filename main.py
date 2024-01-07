3ad1864f59a54bc0cf23dc804c926ed2f0b4a456
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:01:13 +0000

    surprised original tongue board few

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
