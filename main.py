7226876d94d69e0b8c08e5fe2c7c1ffdedeb4055
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:41 +0000

    steer properly completely process confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
