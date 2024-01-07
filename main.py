f0159a3c9fb4df871dd7a7e3bc5ced48d573d8e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:02:58 +0000

    wind of nearly hand solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_user_string():
+        return collections.UserString()
