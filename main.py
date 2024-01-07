e2fa3a2b5a304ec7c29aed77de9b684c6074b8ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:34 +0000

    space create suckme visit please

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def count_elements(lst):
+        return len(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
