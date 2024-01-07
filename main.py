d1eca2fe5d3550e5b4a92eb18b6d7e6438b22035
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:54 +0000

    long damp mouth reduction measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
