342a69c929c995f788ca0c28e7cd843a5a578465
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:08 +0000

    draw pretend sleep protest difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
