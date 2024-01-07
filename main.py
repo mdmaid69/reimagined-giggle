3d9470157cefe9f06d2dabea2a77eca2730f6aea
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:17:41 +0000

    father adjective conclusion mind close

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
