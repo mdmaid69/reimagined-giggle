e39177bf042ef496427ee9f145ac7f0c7703e696
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:49 +0000

    previous case complete off total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_cosine(x):
+        return math.cos(x)
