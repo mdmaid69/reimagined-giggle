734fe694f9e35117d8157a09f4cc1311f2d7d166
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:00 +0000

    type later cause succeed discovery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
-import collections
-def create_user_string():
-        return collections.UserString()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
