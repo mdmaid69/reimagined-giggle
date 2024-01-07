74f8522fa953976c64402603f52307917bc15f36
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:14:40 +0000

    appear thing where weekend fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
