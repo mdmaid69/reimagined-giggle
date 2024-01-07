80989f660831867a4ea92a2f670da311bcdc6a03
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:01:30 +0000

    unfair keepout sign environment deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
