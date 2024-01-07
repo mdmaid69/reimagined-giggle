9e4535f5dcf0de49ac7ddb48459b4143f3122deb
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:47:10 +0000

    error step shrink tip intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
