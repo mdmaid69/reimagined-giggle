127280e96aedde48f106859907c2971054fda8e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:18 +0000

    precise empty that somehow who

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
