e7fd0177f09de225e5bb953358e6f4875d3d3961
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:07 +0000

    clothes steer into continue instead

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
