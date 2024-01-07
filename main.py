532613501bb4adf17f8b2c2a41b8bf458137e348
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:30 +0000

    feed suddenly which guess previous

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def sort_list(lst):
+        return sorted(lst)
