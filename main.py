cd696d99a9f46952491ef748090c254ac9e57af9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:16 +0000

    pause else leave cheek favourite

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
