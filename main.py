52bf133a4833e51a178d388d8128d2c7fecd0062
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:56 +0000

    make spend key example best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import time
+print(time.time())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
