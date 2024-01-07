caf537424b8089a443c7c383bd93bddf13e0e210
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:56 +0000

    worth manner geton window target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_array_to_list(array):
+        return array.tolist()
+import time
+def get_current_time():
+        return time.ctime()
