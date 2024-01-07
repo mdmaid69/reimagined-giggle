1787ccf159a5bcca54e344d26f0e23ebcafe80b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:49:25 +0000

    label try demand shut decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
+import datetime
+def get_today_date():
+        return datetime.date.today()
