7062b04b3d8c271327526e71af0820ef6b52a1ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:50 +0000

    change Hh handle sensible steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
