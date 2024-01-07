022a14b0bf64cf8555ce24bd75f83196a85079d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:13:27 +0000

    sea one stir foreign else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
