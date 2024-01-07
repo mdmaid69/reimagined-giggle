ce514fc11466d8a37f0ede9387ba8a53d79f844a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:21 +0000

    central present weak attitude work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
