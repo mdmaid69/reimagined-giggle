243416b39a488df6f84f950a6ce5b996dda5b9b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:05:49 +0000

    correct memory time rubbish beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
