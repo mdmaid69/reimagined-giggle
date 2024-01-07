950206ef5967019fad3ce7ab64f6201b89a4e032
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:56:54 +0000

    impression expect cut copy trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
