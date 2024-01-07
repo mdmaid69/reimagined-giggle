ecba30ccc6a86e8b35b8eae8bc33cf4fed6b72ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:18 +0000

    immediately fit teach close equipment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
