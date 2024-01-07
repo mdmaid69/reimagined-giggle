b7554b1467e65fbfaf69dfc1035173e00e8c9c54
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:08:06 +0000

    complete length foot terrible until

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_max(numbers):
+        return max(numbers)
