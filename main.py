803e3625c8189671bd40bea88c2aacba9ea7d855
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:29:39 +0000

    opposite pocket wind support phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
