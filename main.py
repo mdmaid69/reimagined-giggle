9ebaa70e9da949195acb9ca0637509155cc684c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:40:46 +0000

    design find object possession clean

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import sys
-print(sys.version)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
