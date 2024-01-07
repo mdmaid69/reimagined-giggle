dd0550091293fe1ab558195a5b302653e1efd4da
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:12 +0000

    environment equal fail good talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
