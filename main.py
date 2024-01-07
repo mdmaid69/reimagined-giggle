a25b5a571813dd70a2cd75774abf52ec4a920656
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:36:37 +0000

    tradition mine justice story tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import http.server
 def start_http_server(port):
         http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
