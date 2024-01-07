15d370747d9bfd0bf8f53cdbc10e9f2a1a67cd17
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:53 +0000

    will up soon into last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
