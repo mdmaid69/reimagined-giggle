617a258e365d347d66ccf78e29566915a74b0005
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:27:34 +0000

    influence impatient steal challenge shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
