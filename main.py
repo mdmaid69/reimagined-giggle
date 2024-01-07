b192f714dfa4c87c6ab1560ab864c9da17d81c2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:58:08 +0000

    this should late club need

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def divide_numbers(x, y):
-        return x / y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+print(os.getcwd())
