800a5afe01c75f9ab596856bbac2fcc08465296a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:33:09 +0000

    outside bet stupid sensitive Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def divide_numbers(x, y):
-        return x / y
+def calculate_tangent(x):
+        return math.tan(x)
+import numpy as np
+print(np.array([1, 2, 3]))
