860423638b4ada3848b949f4aa25bb94c7ff7e21
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:08 +0000

    opposite complete lost 569enjoy means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
