05207e8ca64c2a3c6778445b6b88bf872d261b90
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:19 +0000

    several set base emphasis eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
