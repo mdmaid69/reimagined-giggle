8abca04090763ca8cd740d3a268b4d038a718671
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:30 +0000

    handle interest immediately butter replace

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def sort_numbers(numbers):
+        return sorted(numbers)
