e09441c866fa1708fee4fe8acf5603080cc1f2d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:02:42 +0000

    marry drawing shape container certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
