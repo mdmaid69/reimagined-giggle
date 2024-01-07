306e1d866c81d1f43c007e4f018e52d7ba78b640
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:57 +0000

    he strange stretch somebody production

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 def get_current_working_directory():
         return os.getcwd()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
