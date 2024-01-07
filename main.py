ae93b93ec06454bb46f0c0ea9580b82dc8acc626
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:28 +0000

    doubt stand tie basis value

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
