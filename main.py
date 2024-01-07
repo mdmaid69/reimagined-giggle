3896679b52476fcdd5594c427fbfb14bdc6fe821
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:47:37 +0000

    popular many touch bath authority

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
