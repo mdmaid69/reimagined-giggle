b68d933a551b19bf2cfdfa6d4f0e5db0d12a0b4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:32 +0000

    illness lower whenever will street

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-print(sum(range(10)))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_index(array, item):
+        return array.index(item)
