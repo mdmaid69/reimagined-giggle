7ab3bfdc6afcd8a170f258bf1e80e9306567bd5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:37:50 +0000

    balance relation switch side giveaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
