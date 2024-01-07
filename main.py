4b4fa4ee5f9097b289cbe8642052bcd9535b1eb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:34 +0000

    already offer Jj appearance swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
