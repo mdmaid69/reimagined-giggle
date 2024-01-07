57f0a1665b1114f20a4f890cd0ed1fa63be5a9ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:18:11 +0000

    interrupt hello support division behind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
