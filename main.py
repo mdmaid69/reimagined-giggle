ee870b29b8584c3ebdcd6b5d930a2fd3e68bce9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:38:38 +0000

    responsible fortunately suit willing payment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-print(os.getcwd())
+def find_unique_words(sentence):
+        return set(sentence.split())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
