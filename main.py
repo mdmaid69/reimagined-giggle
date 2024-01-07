fc76c6ac0ce2b93bffba24a1ca5437457be4d5f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:07:01 +0000

    pile apply various direct male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def clear_array(array):
+        array *= 0
