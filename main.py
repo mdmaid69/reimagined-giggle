5987a090570384a2c121c3ffe4be14dd3245f31f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:13:54 +0000

    post include tidy their contact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+print(time.time())
