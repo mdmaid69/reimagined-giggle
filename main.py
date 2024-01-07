0a209dd16792b05e11750ae3e35f06da5030f930
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:59 +0000

    dick herself contact still lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
