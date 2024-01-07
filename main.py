0d24afe64cd9fc38fac22b844f3a5127153ad35e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:57:03 +0000

    stick concerned board point age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
