9e7850c6bfbfa8d8a041aa59d51df72e32bf20e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:05 +0000

    curious comeover partner speak classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def is_even(n):
+        return n % 2 == 0
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
