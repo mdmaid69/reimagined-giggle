c14ca344998be12fe9a0365cd56916ee5bf03b27
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:25 +0000

    too precise 571enough again perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
