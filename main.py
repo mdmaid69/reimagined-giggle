c94d937e5db470f3e6885488b1915cfcad864dc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:56 +0000

    here slip yourself possession stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def divide_numbers(x, y):
+        return x / y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
