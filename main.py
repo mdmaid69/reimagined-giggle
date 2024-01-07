737e86ae6cb246303ca3f4999cdcc8fc7cfed3c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:02 +0000

    pattern live deep story knock

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
