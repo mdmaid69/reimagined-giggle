fbe077731447e20b8628043b49916c838fa260fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:48:09 +0000

    branch instruction attach gas smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
