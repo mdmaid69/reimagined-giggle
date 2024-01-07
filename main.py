a482398518aa5b4bdb91dbc584f0a1ab4eaac925
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:30:54 +0000

    fold object possibly Ii fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
