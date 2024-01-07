a5ce7dff747879ddbd1e48be5f864f9bc1a6a403
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:17:37 +0000

    upwards message someone side behind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
