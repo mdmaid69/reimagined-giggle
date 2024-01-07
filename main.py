ca2494b5b75d5a448ef7c54457a96a9ffe40ae72
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:50 +0000

    beneath often interview soft crash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def sort_list(lst):
+        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
