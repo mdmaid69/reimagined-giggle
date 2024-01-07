3f1021bfdb2727aa9a0571b42c7c5e54068f5713
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:06:59 +0000

    breakfast glass visit male talk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_typecode(array):
+        return array.typecode
