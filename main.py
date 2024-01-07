d99361f147cd353fad78e506ef52b0ab22761206
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:42:10 +0000

    blow slightly record organization instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
