e293c81f0bf41f364a72dcc0b94f632b434b2b82
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:28 +0000

    help middle face illness warning

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
