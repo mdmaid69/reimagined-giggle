7360e4e9bfbe3e0331eecda834caff0b1409d847
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:58 +0000

    stretch safe unkind instant along

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
