6b650b33b76bab5564364b685f645cc689c76c5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:35 +0000

    it you joke success fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def convert_to_hex(n):
+        return hex(n)
