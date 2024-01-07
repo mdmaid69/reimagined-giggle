85c11920df27d229f6441464d6bc0cfa208d9f71
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:07:22 +0000

    across error before unfortunate crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def cube_number(x):
+        return x**3
