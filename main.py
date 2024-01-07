76712e3a46cf060b60af7cd3f9c969395c2a64b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:01 +0000

    ordinary broad must instant cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_index(array, item):
+        return array.index(item)
