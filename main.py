3fb0ef4d42c2a18d491d92491b9340c751e89204
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:10:33 +0000

    dry giveout bye middle picture

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_density(mass, volume):
+        return mass / volume
