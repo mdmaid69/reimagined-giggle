77d9a303e2c433c30a5f1013d14c7a05f8b284f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:27:14 +0000

    handle steam contrast word bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
