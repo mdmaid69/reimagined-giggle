cea5c9b3657f626a6dfff32bd6f1738c6029128f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:47:59 +0000

    stamp check help reserve swimming

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+print(sum(range(10)))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
