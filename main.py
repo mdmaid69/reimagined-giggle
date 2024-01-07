a9333bae1099ae93cb1a9cde8bd648477adccff5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:38:54 +0000

    possession front believe listenfor fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def cube_number(x):
-        return x**3
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
