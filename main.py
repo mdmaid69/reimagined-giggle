5bad8fb0b311884806ab158b6623c84f1281cbb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:43 +0000

    check entrance curious ear morning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-print([x**2 for x in range(10)])
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
