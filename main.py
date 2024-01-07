4c55861a3a0df96c0b84fa4f4786208b9ad2f3ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:04 +0000

    comefrom describe wash in exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
