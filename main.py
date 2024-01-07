7ac1a569ac916d78188637bfb342b85158d7456e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:27 +0000

    worry swim intend difficulty comeround

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+print(math.pi)
