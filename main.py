8df3e49c8a990b9c1690b5740febdf94caafcdc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:07 +0000

    exact refer embarrassing where increase

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def cube_number(x):
+        return x**3
+def find_max(numbers):
+        return max(numbers)
