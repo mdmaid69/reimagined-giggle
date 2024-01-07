f89a2230748ad2411fa28c3e23a729901230ed83
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:06 +0000

    view distance hard sink herself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
