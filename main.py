76a33001c3fc95c6e04f261080628504bca47cfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:44:13 +0000

    downstairs out statement single measure

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
