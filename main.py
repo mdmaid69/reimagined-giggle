fb70df6872e6529d7aa340241f41df3af73f007a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:17:56 +0000

    exhibition him grow mention start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_str(array):
+        return str(array)
