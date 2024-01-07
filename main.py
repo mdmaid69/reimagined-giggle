7a444dc38a309590b57545c957c2b991ea0ba7c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:34:23 +0000

    solve fond later write takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_exponential(x):
+        return math.exp(x)
