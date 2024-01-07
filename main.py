c5afd0fa94f0cad70695640315d82b1b70648e84
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:10 +0000

    centre fast blame convinced gift

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
