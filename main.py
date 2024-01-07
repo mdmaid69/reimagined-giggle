12057c8e348e60d235f5000e61cc94a6aa2360fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:06 +0000

    proof turnback person protect suit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+import re
+print(re.match("h.*o", "hello world"))
