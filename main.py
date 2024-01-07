e6c9b85f40b7b619fe89cc0e593065d17ecb48b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:27 +0000

    spray patient spring genuine final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
