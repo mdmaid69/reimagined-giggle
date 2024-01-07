08ab01767205fb8a95935e6ce75944256a8cdaf4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:30 +0000

    final present complete production male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
