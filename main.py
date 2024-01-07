456dee106c21007bf30ad9d98e53e574d0e07ebf
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:56:26 +0000

    have guess cap pool education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
