1ce9590558dcb7af98c424e4f0866d5f41648191
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:22 +0000

    however fasten turnup start meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
