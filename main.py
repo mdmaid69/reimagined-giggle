7d9454c3bfda11ec80359073505902965888d685
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:33:59 +0000

    live zero confident demonstrate active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_tangent(x):
+        return math.tan(x)
