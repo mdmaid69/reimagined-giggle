6b89a5f78fc89276b1a4f5d90e02e5ee881b4f44
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:17 +0000

    stone breakfast rely draw satisfied

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
