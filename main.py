0b0f528dfd42940c85b62fb39e89824681a2ef59
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:14 +0000

    contents quit active building old

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
