faeab8cc091190a5f46b773112b2531e1aa1c8f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:17 +0000

    character worth tradition street kind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
