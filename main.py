0ed81d37a6cd3b795aa84ec836b29f5c122d7471
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:06 +0000

    female welcome suggestion pull strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
