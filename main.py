e23ee049beac04d01aee3b9ffe901b8b0a245ed6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:16:16 +0000

    put sticky gather of increase

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def sort_list(lst):
+        return sorted(lst)
