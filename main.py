42cc2c7389974568cd3eddfa854d6ff8dca5d0c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:53 +0000

    rather brother various talk chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
