90023818e79e480389f7a9792a54d606ca5854de
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:52 +0000

    live cry swim declaration tired

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
