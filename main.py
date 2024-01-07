270b6a14cdcc9e30387467f9813243daa0193a3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:57 +0000

    passenger regularly show shall advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def convert_to_hex(n):
+        return hex(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
