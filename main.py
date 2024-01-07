90defa20135a769e1bf4d21a27df958347f929c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:47 +0000

    desperate silent print motion pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_set(array):
+        return set(array)
