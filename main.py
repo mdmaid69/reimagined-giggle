312096063410bef751ccc0f1373bdc290a63587b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:01:30 +0000

    treat dish cruel top voice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
