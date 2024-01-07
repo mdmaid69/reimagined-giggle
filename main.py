6607a962fd79cf4236e4f20fb8a649ca93e7854f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:48 +0000

    make under soon confident condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
