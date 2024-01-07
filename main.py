294a88d8b0852ab17efcb83903a29bf0cdede247
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:20 +0000

    minimum stick report get fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
