c0864598d60f5e660add3966d346539ad9e24880
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:23 +0000

    mistake so hold strange usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
