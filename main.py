7d6ad5bf4c1e8a7d08e7a60c7bec08f3f6b7c789
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:28:40 +0000

    give lesson minor call performance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def sort_list(lst):
+        return sorted(lst)
