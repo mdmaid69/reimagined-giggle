5ab78fd95076fc96daaf9a5cf172b156dfbb4883
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:59 +0000

    lesson tough basically nigga move

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+print("Hello, world!")
