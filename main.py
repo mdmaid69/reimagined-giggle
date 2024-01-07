aabd15ce7a3e30ce39f9be78fbe50be50e0cd537
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:35 +0000

    to listenfor door effect set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
