bdecc45997659193787ea658d956cc00e1db6226
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:04 +0000

    term sense afraid accordingto hand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+text = "Hello, world!"
+print("Characters:", len(text))
+from collections import Counter
+print(Counter("hello world"))
