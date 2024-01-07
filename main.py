cac314c299f9f6b77afd57f6405ca5b00c17b24b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:26 +0000

    like winter coffee splash reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
