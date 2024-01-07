d4b05da5a788891bd77449bc2f3af6f46ed2586b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:41 +0000

    meaning safety action suppose know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   def square_number(x):
         return x**2
