bed91ec5703fb974dbd86bc5ab2c75d23232975c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:41 +0000

    chance nonsense walk near polite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
