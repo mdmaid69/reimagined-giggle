8c98488d08fbefb9eb220298697467089ff27ad9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:32 +0000

    choose precise transparent suddenly guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
