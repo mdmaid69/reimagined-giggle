b3c70e218e617e468e078d249bb9fdb84a6099ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:53 +0000

    point finish turnover mark treatment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
