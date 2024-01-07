f6ca47dbd65c40750ca97abf9a411c2e42ce8f65
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:57:10 +0000

    himself class thankyou prize constantly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
