1be8e10dc921d60d533feb936d34125746266ff0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:56 +0000

    reaction cover party conduct correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import math
 def calculate_remainder(x, y):
         return math.remainder(x, y)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
