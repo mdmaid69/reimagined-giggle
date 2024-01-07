ed9fc8ef9a223f31658ca23fe6a27bdb676c126c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:12:48 +0000

    one smooth opponent wind judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
