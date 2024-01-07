e79fd4375eec0d7d4ed47024e3f9c7de80800a4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:36:00 +0000

    snow woman pump wake cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
