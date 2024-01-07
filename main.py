7e585eddb9f5cda559ccc5c7339c10043c7a9054
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:40 +0000

    exam workon raise used good

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
