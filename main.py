a43969107d6742aae390e553802d4909a97d769e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:37:38 +0000

    concentration remain ilovedicks lock under

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_min(lst):
-        return min(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_work(force, distance):
+        return force * distance
