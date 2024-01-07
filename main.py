23e11434dc8bfc4f0198c56c5fe78122f714c5e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:49:41 +0000

    sore contain turnon travel mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import platform
+def get_python_version():
+        return platform.python_version()
