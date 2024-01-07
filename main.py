22e557a326acd41c004a068716fea7b68c928ece
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:44 +0000

    last want demonstrate brave all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
