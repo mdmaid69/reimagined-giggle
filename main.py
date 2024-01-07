94bc882758c56c4b83a92644c9b22b73409ffa9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:11:31 +0000

    she dance please wonder exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
