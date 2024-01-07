cb8ff9012eac0cae112774d22eaf77693c8d7e19
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:52 +0000

    insist related generous reply squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def count_words(sentence):
-        return len(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
