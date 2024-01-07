d2e094a162551368a9d9b778e3733bbb21e39fb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:24 +0000

    kind hair precise offer public

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
