2c7414b00d76173d11488a7940be13df75dcf1a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:54:27 +0000

    hungry culture size expensive card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
