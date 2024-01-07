bd14123ac64eb9d2cf0aedfcc69889f01d312d21
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:02:23 +0000

    open belief pretend shrink lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
