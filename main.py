f83c0544d85268b0b539ec82de3f053bcc26d819
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:20 +0000

    fetch climb personal bin declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
