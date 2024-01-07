f7d7178584107c0398ee81250cd21b2ac94444fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:34 +0000

    crowded brave reaction fond correction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
