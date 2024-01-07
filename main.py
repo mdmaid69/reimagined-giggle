00d6127f0df75e298caf85b0d3692a9437e97084
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:37:13 +0000

    boat squeeze very spill various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_queue():
-        return collections.deque()
+  def split_path(path):
+        return os.path.split(path)
