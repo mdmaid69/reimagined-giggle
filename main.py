ae8bd888b3931de05972d7287dc40f6cd8e714f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:10:28 +0000

    score doubt story hers total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
