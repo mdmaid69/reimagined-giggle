c509bf93407aba573bc04f310bc44cbf74ff706c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:28:13 +0000

    grow provide mean strange bringback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
