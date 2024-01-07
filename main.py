a8553232b8f6753ae778ad768a75342d2d1bd2d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:33 +0000

    family kill find getup fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
