4222e07d237e509095a28b5abb9ef90659921977
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:43 +0000

    before getup flat arrive around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def remove_duplicates(lst):
+        return list(set(lst))
