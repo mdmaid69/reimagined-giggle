7e3216b724b3a276592e45acd646e84639765aa9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:08 +0000

    nurse collection lie goout process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
