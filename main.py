85efe7b3b65cc74687309dc0b9500d3f80a939ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:21:37 +0000

    bottom hide start deliberately mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
