e5ec5c03a4f2dd04d072e880666a6a0463535f58
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:49 +0000

    free upstairs happen until beyond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
