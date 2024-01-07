c23338a7b9d9341398ed19f439b9feb20681c33e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:32:50 +0000

    convenient else maybe complain total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
