b1b84214e94816205b22f1c9e70eb6f566daaadc
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:44 +0000

    contrast emphasize cut unit already

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-print(os.getcwd())
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
