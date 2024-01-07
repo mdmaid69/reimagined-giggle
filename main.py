5d40e8bf9da45ff808bdfda3d69afba3852deb4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:17 +0000

    cool confident apologizeapologise judge creature

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
+def sort_list(lst):
+        return sorted(lst)
