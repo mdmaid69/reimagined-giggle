cdcfe7242dfc4be7fa370029665be23bf4ce0f80
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:50 +0000

    minute 569enjoy tax minute previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
