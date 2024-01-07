6c7b0ac407cb5f7526e82760eeef30e6f516b7a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:46:58 +0000

    float comment strike he dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
