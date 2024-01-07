01fc9ffef3e8d2f0761c986a628e9cf043d917fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:35 +0000

    so never typical interested order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
