6a97847ff85818fcc6a379d3fda7cd7a943dae5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:09 +0000

    Vv future rescue away personal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def find_max(numbers):
-        return max(numbers)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
