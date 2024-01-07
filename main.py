a7544b8acddaf3fa30df0edfef9a0515f67e790b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:46 +0000

    glad science perfect clear tip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import getpass
+def get_username():
+        return getpass.getuser()
