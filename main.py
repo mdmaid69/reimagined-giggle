c73aed9c1156c625600d5608a80c1f585fb37de9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:10 +0000

    physical rain far fear goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Characters:", len(text))
+def find_max(lst):
+        return max(lst)
