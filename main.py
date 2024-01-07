c6607fecb8e28b07ed77b1dcb51f4f7dbe2657c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:27 +0000

    funny actual material view style

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def square_number(x):
-        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
