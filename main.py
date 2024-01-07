874bcc09125578c5ae8e180a532daebcb031d3fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:34:00 +0000

    relative accidentally side kick cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
