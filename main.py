18aae7600e746c283056558002934feb834d3ecb
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:00 +0000

    deliberately country display meanwhile appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
