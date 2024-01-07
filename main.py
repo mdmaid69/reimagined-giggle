01279e9c5db51a841385118550aab9b18dfa685f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:19 +0000

    pair replace pride likely give

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_set(array):
+        return set(array)
