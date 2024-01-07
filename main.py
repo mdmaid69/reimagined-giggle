e533c50b9d39eae740005ef33965ee227d3f7222
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:17:00 +0000

    promise drive kick popular fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
