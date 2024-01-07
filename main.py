832c5e7b1882134466a5ef79e904c950918f8fff
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:51:01 +0000

    describe flat squash rubbish gooff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
