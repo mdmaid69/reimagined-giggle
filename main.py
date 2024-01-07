26033a7ba012fef12dfa2eecb127fae10a0a2be0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:32:35 +0000

    detail fetch reduce sentence nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
