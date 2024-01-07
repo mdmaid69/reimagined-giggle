65d4445168ecd560c49d9187493c0e7771c2baad
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:54:05 +0000

    report straight gas familiar exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
