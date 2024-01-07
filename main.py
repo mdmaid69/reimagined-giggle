824800ad02189ad9c358f45fff161293c45fcf10
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:55:57 +0000

    stupid area copy top against

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
