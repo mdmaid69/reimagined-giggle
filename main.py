635f3e941a9b778475f506ab565bc7fe9934b0e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:08:36 +0000

    contact around hit lookback(on) attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
