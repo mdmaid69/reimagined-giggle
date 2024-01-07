1ebfb8724e42a2795b250609b3fefa6d5d68d671
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:29 +0000

    declare confused art brain post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
