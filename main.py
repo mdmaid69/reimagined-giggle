fb5460090115830325119084b169135f5aaa6332
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:22 +0000

    turninto sentence confusing frightened breakinto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
