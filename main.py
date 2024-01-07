a4e8aee1fd4acea765d53e0db74690b999986e0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:01 +0000

    post small quality each towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def remove_directory(path):
+        os.rmdir(path)
