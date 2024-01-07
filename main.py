413dfdc051a2fd38548d9d03ab5a6fddf272f1b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:59 +0000

    excuse admit sea apply pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
