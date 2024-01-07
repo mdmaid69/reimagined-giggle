0ab3df3861d9ad6784776f75ed5564a4138a0f64
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:13:38 +0000

    shut risk thankyou battery organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(a, b):
-        return a + b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
