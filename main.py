fa611ca879c4779bd255b29e4a023ae3c1563856
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:12:37 +0000

    beginning basic story forget compare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
