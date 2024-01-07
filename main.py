e939bfcf45fe09cb492e05845582bb91bf9f4603
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:38:54 +0000

    bread runover mark satisfied create

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import datetime
+print(datetime.datetime.now())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
