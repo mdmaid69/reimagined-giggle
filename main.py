fdd92cefb1202d21a534ac84ffd2d82d55f8cb89
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:17 +0000

    region trade say basically thinkof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
