e389b13fe8d97fae93270a6e720ab0b9d83a95f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:45:46 +0000

    before more use slip cry

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
