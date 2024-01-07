2c78482a0c4ecfb1fec930914f0894e31f760d5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:48:36 +0000

    anxious send balance on care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
