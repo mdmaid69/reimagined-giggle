ab29111e9c56a53fb90baad4a286500fa4049ce8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:58:43 +0000

    bad upstairs lift case insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
