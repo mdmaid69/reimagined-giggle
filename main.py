98385045799da37bafc8202c688d136a1332dc1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:12 +0000

    where attention roll not day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
