7fe9584d4d10dc2132790e6a18d92e8595e7c202
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:25 +0000

    power union like autumn shock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
