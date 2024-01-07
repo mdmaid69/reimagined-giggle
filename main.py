534e1271ae091c66b6616e9ff8ea0db21ddc2ada
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:30 +0000

    sore settle habit sign only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
