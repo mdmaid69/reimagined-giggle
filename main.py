eacc35bd616dc698f9893d14c4a41abd82eb7c4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:01:44 +0000

    by count under under ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
