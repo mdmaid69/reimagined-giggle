1f0aaa83ee14789ee12f45ab6d0ebb963df2e8fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:01 +0000

    wonder estimate damage shadow no

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
