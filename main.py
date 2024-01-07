af0ae5fdafc63226f2b08ddc14270c8af58af06a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:27 +0000

    fence thinkover look grateful stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
