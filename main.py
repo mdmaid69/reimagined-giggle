669cb287656f671bf5b3c716d554af8f497da74b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:10 +0000

    thought Ee closed challenge pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
