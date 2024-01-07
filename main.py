63560959dff99863ddd3af0f200e90bba1ce55a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:02 +0000

    peace plate difficult dance hang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
