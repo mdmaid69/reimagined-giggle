15b37e3ac9fa40b8778790f0018cbda7cfb77358
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:38:16 +0000

    cross table cost group process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
