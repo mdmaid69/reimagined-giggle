577794600fffc7c53b5c15d0f2a169898b14299b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:24 +0000

    like conversation turn exam chin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
