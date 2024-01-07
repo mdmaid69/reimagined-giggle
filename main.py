e7e03fbc889ea529821e419185f994528007dd4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:04 +0000

    class behave disagree blame scene

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
