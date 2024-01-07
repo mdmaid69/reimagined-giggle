35410ec07b5d41158f3773a779b884a29580e015
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:02 +0000

    pain present since we measure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_current_working_directory():
+        return os.getcwd()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
