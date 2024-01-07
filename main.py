e1db4c6c9bd84a52dee1ad320bad2163b6f6cfff
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:09:18 +0000

    fear consult sort substance undo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
