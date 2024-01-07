85f6b8ba86627d236bb74c2b121658867c643968
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:58 +0000

    habit react lie agree tell

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
