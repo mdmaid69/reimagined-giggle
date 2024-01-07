d594f14f41ac97231819c1a4e0be99791f537d21
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:59:32 +0000

    steam aware shut offer image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
