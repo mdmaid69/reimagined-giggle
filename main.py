f5d4ba3e623306ba759e262ad7c3f51c898440ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:58 +0000

    kind quarter front back suggest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
