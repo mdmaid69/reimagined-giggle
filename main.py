8565e78c0d0866aca5301c4354263ed4a300e6b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:32:12 +0000

    pity obtain include thought indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
