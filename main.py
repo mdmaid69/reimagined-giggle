10b1b16f52984275c67fbc2bd795a5f40f7b89f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:07 +0000

    answer purpose swimming excellent appeal

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
