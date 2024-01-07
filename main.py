d34a2291533d2309ea3fbc13e1ba95eefdc32e1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:56:56 +0000

    attempt criticism accommodation country lie

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
