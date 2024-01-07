1ec895ade4939b12841325415a8e6757cfd75be2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:22:46 +0000

    negative too professional anywayanyhow side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
