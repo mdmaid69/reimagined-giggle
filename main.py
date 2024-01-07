ee507567f64360fc64e165dfe817746dd5a56d25
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:47:05 +0000

    thinkof repeat contain really willing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def clear_array(array):
-        array *= 0
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
