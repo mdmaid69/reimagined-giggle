7e4751c4ca2548a886b5c1c5618399acb4b680b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:26:31 +0000

    scrap close wing over plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
