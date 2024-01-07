55815f2c05022a3506081d6dc26a8c100647f277
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:18:34 +0000

    ashamed once recognize wing cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def remove_duplicates(lst):
+        return list(set(lst))
