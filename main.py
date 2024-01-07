f498700c7990524c3c3fbaf9d4bae03ee48b7057
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:54 +0000

    store sale crazy would about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def reverse_list(lst):
+        return lst[::-1]
