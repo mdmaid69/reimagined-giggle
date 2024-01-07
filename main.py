d90e566bd49bfbc7be152d38d1449fc28ee0b028
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:42 +0000

    sale not also rescue emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
