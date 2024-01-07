619740cbc9684355ff82daa38b8cb0c8a6442b4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:52:11 +0000

    voice replace tired pretty sister

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
