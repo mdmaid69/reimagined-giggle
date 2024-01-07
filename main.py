fe01c8c54eeb38a035a939e4c7413ffcb614f9fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:45 +0000

    well few dead least government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
