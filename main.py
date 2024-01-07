7f46be745fb351795e3ceb2a24038381b92b7e7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:02:55 +0000

    split there lookback(on) Oo sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
