c1dabb81cec5b90374ec14d4ab7b179a5885bf9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:41 +0000

    bottom gooff Jj basic foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
