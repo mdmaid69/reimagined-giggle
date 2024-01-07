ad0447774e772f31ab16a6b849cbd331653a3b9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:32 +0000

    challenge treatment comein various afraid

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def count_words(sentence):
+        return len(sentence.split())
+print(sum(range(10)))
