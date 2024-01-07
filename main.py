50ef0040155b6abe6cf89b6a6e97aa2fe495d878
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:04:30 +0000

    wherever talkinto bringup song exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
