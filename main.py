3ae1138a000443a27398fcb4bc110780dd3d3ca9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:38 +0000

    wing show dark ball accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
