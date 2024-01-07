8234c90ee8743f84d9f969b1500874d5cce23c77
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:50:18 +0000

    message spin treat 565end machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
