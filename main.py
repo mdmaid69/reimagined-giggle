205cd367bcd3ed98b084e96440648c5ffaf7f004
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:08:46 +0000

    worth miss before apply cost

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
