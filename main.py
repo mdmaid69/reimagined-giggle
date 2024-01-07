7115ccce6dde9ae17774cae894935b94c1e93a24
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:25:25 +0000

    deliver model getaway watch collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import matplotlib.pyplot as plt
   def plot_scatter_graph(x, y):
         plt.scatter(x, y)
