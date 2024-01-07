3265402781bf0a15861e6d6228b54d2db4b5a8ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:11 +0000

    unfortunate slight takeover action bunch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
