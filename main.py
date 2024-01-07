170b5e72d2ab4d704cc80a0af95a2911df738657
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:38:08 +0000

    each become represent food serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_length(array):
+        return len(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
