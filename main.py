94e957688d4af75c2b9ead87eba0a15bebf054f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:25 +0000

    passage worse excuse whatever once

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import math
-print(math.pi)
-import re
-print(re.match("h.*o", "hello world"))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
