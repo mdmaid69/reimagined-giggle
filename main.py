713de80ed657806165aa280f39a3c68adccb205d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:10 +0000

    amount laugh protect many supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
