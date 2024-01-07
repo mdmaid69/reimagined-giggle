bc235adce1b60c6d4ed476ae88e35f322ec24355
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:57:29 +0000

    exchange pay recent recent nowhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
