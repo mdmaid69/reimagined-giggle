90f19fcc02331278bc908e1492e3d1e5ace85608
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:07 +0000

    choice increase correct smart goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
