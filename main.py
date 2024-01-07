05ad0e50f3ddeae2a96f23cd0e111f8da3adc4a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:44:21 +0000

    press medium bend evening risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
