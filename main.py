1da37ba03e2a3524a8d672a20b23c3b3c5a89da0
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:13 +0000

    Mm trust flat it say

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+print(math.pi)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
