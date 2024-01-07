5ba2630322a0b148ed54ed23304ed3d625636807
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:37 +0000

    sky action unpleasant person accidentally

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
