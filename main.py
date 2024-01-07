e76b795ba3c7f477747b3f7a70edd85343d4efce
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:29 +0000

    car learn market main improve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
