19100d99a7e4266b6b739db26bb4a774bb4e4e0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:31:46 +0000

    example loose responsibility remark shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
