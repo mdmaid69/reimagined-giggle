d7aff4e999ef406bf686b6eb04f690dba591ca85
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:30:52 +0000

    inform punishment hear earn drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
