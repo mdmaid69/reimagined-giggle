360d888145400c4e49e0d869265b5a102f9b56e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:56 +0000

    unkind there workout awake past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
