d1aa7bd86762d4d6941912b48fe75415baa540c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:04:22 +0000

    achievement break notice up unlikely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
