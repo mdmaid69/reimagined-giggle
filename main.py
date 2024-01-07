c802a1fb3a87592fe2525878fe08a584d044ffd3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:35 +0000

    support still whistle average not

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_index(array, item):
+        return array.index(item)
