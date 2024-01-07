c860f542951bebc2928dca66e29e0bd1e2f00f6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:08:01 +0000

    hide example length intend noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
