1461018781fc8612ce9524767fc80ed5d88bb8c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:09:02 +0000

    possession only address shallow thought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_pressure(force, area):
-        return force / area
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
