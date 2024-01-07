89b5d94ad6df6187db4e4644986b00c50b95b08e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:43 +0000

    dry possibly cut travel piece

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
