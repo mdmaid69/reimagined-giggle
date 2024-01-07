08da8435441eff2d6afa769c8ba98fd196951850
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:29 +0000

    month exist great value nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
