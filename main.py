adbdf5e48595c694cfb3f9a7a3503bf5aa065076
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:21:51 +0000

    government bush relax wealth ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
