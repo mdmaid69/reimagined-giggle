3be406c9baa22bb42112aab3a7881dd547a14bce
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:57 +0000

    transparent breathe why visit lie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
