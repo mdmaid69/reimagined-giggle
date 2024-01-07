efd399582d33caeb6228d7945f0a8c4c08cd2a9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:18 +0000

    bread company temper image there

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+from collections import Counter
+print(Counter("hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
