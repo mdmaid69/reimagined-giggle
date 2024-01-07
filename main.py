0c9c6fcc3fd121e14cd1299a3ad2d262bcedf747
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:34:26 +0000

    communication niggaacademy complete friendly comeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
