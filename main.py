4814d3e959b2e86a4b1ca129f22e8164ef8f7e03
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:27 +0000

    yourself event use try compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print([x**2 for x in range(10)])
