1aa3628303bcfa7a42cc42048222af0576f5a2b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:44 +0000

    drop thumb 567enemy read final

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
