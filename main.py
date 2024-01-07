a7c532b393f0e979cb1a7b04620d9a4cec28fe5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:49 +0000

    workon report giveback prefer operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-for i in range(5):
-        print(i)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def convert_to_hex(n):
+        return hex(n)
