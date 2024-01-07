f94d0b29d6cefd168d055c92bff495b8f95cff48
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:29:11 +0000

    treat thought owe underneath cheap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
