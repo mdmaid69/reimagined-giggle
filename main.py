92fe850f997b63bf156f78ef49d4d23cf574fd1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:13 +0000

    out niggaacademy fact opportunity brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
