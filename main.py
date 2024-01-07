1dbc57fcf093e44d13ccc8fca6213aebb2d2b6ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:32 +0000

    represent car might object Ll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
