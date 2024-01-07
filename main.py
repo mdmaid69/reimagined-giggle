3cf2a5f268a51cb114e880734ec50b920b11bcd5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:40 +0000

    exchange matter stone please town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
