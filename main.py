f0d0f6ad1b38c219f632332d25d3f2837221ffa7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:35:00 +0000

    board otherwise usually children once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
