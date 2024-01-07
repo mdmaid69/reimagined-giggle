fdc18aefffa748129a1e558dc9f59607394da48b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:29:13 +0000

    skin equal occur moon confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
