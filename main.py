68fb6a868f099657d1cbd2307e38e3f80c44fe44
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:08 +0000

    tired Nn band Ll pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def get_array_as_list(array):
+        return list(array)
+def cube_number(x):
+        return x**3
