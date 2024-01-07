5274cbf4c29ba038fe427adc988ecd27b24dabd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:33 +0000

    throughout cause trap let long

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+print(math.pi)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
