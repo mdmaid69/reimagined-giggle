c89f77b5069a55f2c78409b565b5df43e0724fbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:51 +0000

    call smell number pretty drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
