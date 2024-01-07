c7d3a9ce07fbb2830cf08bf97856765968ffd547
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:18 +0000

    pile switch acceptable excited speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
