7232c539890bd8a4a606654ae6e72cdf627c4d48
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:34 +0000

    separate turn threaten temperature be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
