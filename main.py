34aaafe20e040e25be16dbf45ee8cedb476b8df7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:59:27 +0000

    entry grateful business accident whisper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
