356ec1a435184de4829bb735088c1369c19daa17
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:59 +0000

    discussion class strike compete word

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
