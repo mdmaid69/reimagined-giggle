0ff843ddb141706d2d6996c65945db31c69e417a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:37 +0000

    track rob tie measure comeround

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import sys
+print(sys.version)
