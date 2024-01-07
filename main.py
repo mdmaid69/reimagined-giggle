d6072e996a8c55d64836629248c500dde90f7012
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:34 +0000

    choose spray high will action

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
