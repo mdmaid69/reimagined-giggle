6e7d99218b8abbd0a316c2031cae775e00ecd1cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:33:17 +0000

    tear nothing consider push dare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
