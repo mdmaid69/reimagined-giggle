31643c97dcb1aff49c642c05d74c7a7e278b0928
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:14:47 +0000

    block press high miss cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
