fae13a4f9b925eb209542019ab87083ae8385843
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:22 +0000

    announce relation relative scale safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
