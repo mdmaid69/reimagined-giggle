4285b651950755c53844ed4b3ad1ec7f5161ea4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:48:59 +0000

    choice name score respect late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
