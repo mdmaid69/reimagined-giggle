375180a5ad793b3f40c2a5346649486bb96ee1a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:37:26 +0000

    listen anybody sharp influence downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
