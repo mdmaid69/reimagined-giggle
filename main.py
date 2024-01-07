4396f24bca0a64baed8f97613308df0b2721c0c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:31:41 +0000

    gift impression describe comein for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
