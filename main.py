9ecfa153eb62e5947abdecbb4e6fccd5eb4bfc93
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:12 +0000

    fix progress understand year justice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
