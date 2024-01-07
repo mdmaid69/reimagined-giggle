774fa79c0c1144685bc9ff45c9b597dd603cba04
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:10 +0000

    depend giveup mess discovery whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
