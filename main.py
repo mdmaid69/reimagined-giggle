1031770c79d37406186867c7198671b4f3549bd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:33:00 +0000

    free post roll guess wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
