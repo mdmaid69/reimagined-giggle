606376ddaee3f6241f86829e2dfefaac53b41f8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:57:15 +0000

    pipe poor place since cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
