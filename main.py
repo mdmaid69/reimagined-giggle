1a66c7dbc33e043de139498f53b82bf4dedf1df5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:23:32 +0000

    geton press complaint suppose miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
