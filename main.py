b6b333a6d64d6897b76019d5ad34abadc90a2487
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:35 +0000

    through inform impression sink achieve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
