e62f5a2eff759259dcb6a9238809f322231b1141
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:23:22 +0000

    character environment organize gate see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
