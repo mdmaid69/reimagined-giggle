8930c134477c9fb1f1d0525c87a1c53a4def6fc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:19 +0000

    promise manage many effort cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
