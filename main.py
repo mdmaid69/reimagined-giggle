c942ef52bff4a770db4237c9414a696de741753a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:04 +0000

    practise habit attach land impressed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
