c0b591149245f99f86a3fae069baad19d0efa705
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:40 +0000

    nigga crime scare friend convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import platform
+def get_os_info():
+        return platform.uname()
