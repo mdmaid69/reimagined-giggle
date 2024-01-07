cbdde7e2c64262906e5d9b7512779bc23630b700
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:29 +0000

    achievement exercise influence keepup tough

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
