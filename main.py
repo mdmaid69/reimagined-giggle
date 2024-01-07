6ec2ff38bbec15e843469db2dad39332275bae38
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:16:57 +0000

    circumstance error club transport land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
