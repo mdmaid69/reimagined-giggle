356414de3c0e2db45d40940d178349cadf4a1902
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:42 +0000

    offend field tie whom turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
