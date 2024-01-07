bb4d2ad618e94fee3018de45753c93cef9a5f049
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:09 +0000

    we rain fast mean record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
