e47a4486aa89fd310a154c53089637dfde979323
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:29:10 +0000

    bus forever minute pause connection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
