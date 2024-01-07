8e9513ffbd9005053dbd806eba1f434c1e6ea6a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:31 +0000

    feeling add talkoutof kick nearly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
