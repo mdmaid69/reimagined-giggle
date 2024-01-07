8fb96e9a79ac7d860022b4c9a0c7affec53dae02
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:40 +0000

    fashion pity runover beat entry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
