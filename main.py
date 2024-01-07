af0a1dc54c58c109d3729c80d0156643e8d5d699
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:29:02 +0000

    unless interview anywhere right call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_logarithm(base, x):
+        return math.log(x, base)
