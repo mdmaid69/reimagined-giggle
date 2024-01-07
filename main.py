3512d31966ec41c5f79748f2ae8dfdb27caee0ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:45 +0000

    week direction colour word dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
