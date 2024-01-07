6171ac615358ee1b550077ea289fc6a39fb3171d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:14:06 +0000

    rubbish secret taste usual communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def multiply_numbers(x, y):
+        return x * y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
