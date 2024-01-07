cdb781b368498c8000ff93c78d552cd06ef20e5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:13 +0000

    trust dull prepare love upstairs

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-from collections import Counter
-print(Counter("hello world"))
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
