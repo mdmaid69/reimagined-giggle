5045306bf422292c3af27ce718221e2db8134353
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:07 +0000

    turnon passage direct comeout ill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
