f40197d8c9622aa14fe129da498e7c7e1de37b0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:16 +0000

    realize it tax load better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
