f8cb1cadb9ab6349ff25d3a50133279c95ef84c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:08:32 +0000

    hurry hard sit without thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
