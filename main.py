2f0fa1e1922ca055466db2db7e37188fa48a28e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:28 +0000

    nerve track certainly after scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
