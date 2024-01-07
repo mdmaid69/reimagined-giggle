f786037ca6d75d8a06b763e41e01a4c937dda7a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:20:56 +0000

    rule high should spread reject

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def subtract_numbers(x, y):
+        return x - y
