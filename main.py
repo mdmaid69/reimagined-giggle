65826ec6e109176990fd0a2c9c4649d17e631aab
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:57 +0000

    dangerous smell can negative light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
