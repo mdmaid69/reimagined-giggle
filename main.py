9aedcf7698b87641f4077f3b8dfdd11d5e034202
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:07 +0000

    belong information remain among plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
