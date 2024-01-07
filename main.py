34ba9ec8b3b969d9c6920d04894d8ceda2d7279f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:34 +0000

    dinner calloff lend flow convinced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def count_characters(sentence):
+        return len(sentence)
