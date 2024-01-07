e1bb2215e3318ecfcf19ac6f623fcbf6bdd5abe4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:23:30 +0000

    determined sing active clean crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
