41602bd57b7649037aa277adda1f9f835e95a6d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:46 +0000

    bringdown come appearance below team

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
