901346be41d7b3dcb796632cb3135502f4c08dfb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:55 +0000

    scare correct deaf guess behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_str(array):
+        return str(array)
