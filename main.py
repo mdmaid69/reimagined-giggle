ce236a86e8b86fea4f0cee89b091a8b5c4ef8362
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:44 +0000

    evening bank voice deaf agreement

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-print("Hello, world!")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
