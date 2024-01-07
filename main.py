c77d4456308c118956ab1b91d6e1e36263eb6b4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:15:09 +0000

    friend succeed add callout equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def convert_to_octal(n):
+        return oct(n)
