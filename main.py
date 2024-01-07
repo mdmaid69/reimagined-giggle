d067468612299fea72c9c9c25b6c4f15c2c86326
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:08:30 +0000

    hang remove recommend tape principle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import time
+print(time.time())
