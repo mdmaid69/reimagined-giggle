eb570828f59fadaecf8599e411bbb1445513ac38
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:49 +0000

    class married runover land breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def reverse_list(lst):
+        return lst[::-1]
