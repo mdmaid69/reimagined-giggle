1c95f7fb4964c425752d59445d2b437af15f268d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:36:41 +0000

    want hair tone zero store

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+for i in range(5):
+        print(i)
