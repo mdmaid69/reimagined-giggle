50fb07db35e48bc0f85d48c4550ab7ae7580d8a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:19 +0000

    watch home hear gentle us

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
