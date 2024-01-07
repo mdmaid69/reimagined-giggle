86475fa9d86ca43f8be1bfbbf3a805d9a8e32130
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:08 +0000

    getoff unfair proper single post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
