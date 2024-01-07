29a422247c151075f3b213e5a6fcf547110a138b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:18 +0000

    pronunciation smell rob offence about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
