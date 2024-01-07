59b4713f0b2dbe7743a09a9c638f813cc3ddf0ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:20:30 +0000

    far yours rest accept centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
