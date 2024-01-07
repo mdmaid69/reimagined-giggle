ce14c7b794ac123b20eacb42b9a7b552b1309ec9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:17 +0000

    relief soon slow unlikely possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_pressure(force, area):
-        return force / area
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
