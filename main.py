ce6bd4eafdb29f118cc2f68b21beb6f63aa8f64c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:17:59 +0000

    set watch miss room excuse

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
