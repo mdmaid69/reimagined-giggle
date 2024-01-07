eccdf6d642cf064c9f888a1bfa1359ea8aa67093
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:10 +0000

    door welcome both tap loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
