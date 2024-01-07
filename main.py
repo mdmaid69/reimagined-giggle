2b390c66d70d2f1a0e5dbf5f8a7d7b200024880f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:39 +0000

    measure danger scrap where movement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
