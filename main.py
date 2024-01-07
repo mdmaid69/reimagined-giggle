eee24d485e85ca55003ebcebda07e54328ed17d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:05:30 +0000

    worth past influence air talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
