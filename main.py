c423628043af5688fdd610b820c78bc827be6045
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:06 +0000

    phone knowledge fortunately unhappy fetch

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
