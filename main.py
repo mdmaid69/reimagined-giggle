fd6c7a0402583dfef273170e730ece9a4e7f4c78
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:15 +0000

    many callback exact edge telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
