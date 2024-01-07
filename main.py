59299e75d44138efc6d81f7558266862b827c679
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:01:28 +0000

    cheek house reduce scheme 564encourage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
