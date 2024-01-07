e44e2283723311bd73e8b1189a58a286208512f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:59 +0000

    quantity control responsibility getaway interest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
