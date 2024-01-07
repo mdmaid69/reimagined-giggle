bf4338a2d52c6e5c5b80943dcfb2120da54995a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:48 +0000

    report bridge noisy unable sport

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-print(os.getcwd())
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
