25a82a4a9880c4e9e24f14aa136cd492228a485e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:04 +0000

    speech murder occur attitude smart

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+for i in range(10): print(i)
+import json
+print(json.dumps({"name": "John", "age": 30}))
