689adaaed5bbc093796187240506c7df209500f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:58 +0000

    trap police gap plenty pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
