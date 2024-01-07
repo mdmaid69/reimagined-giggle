c90481bb2036a387376687f70e10b60aaf92e67d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:34 +0000

    without lean right turnaway main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_length(array):
+        return len(array)
