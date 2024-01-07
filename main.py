ca75debd9475981cc3e53ee9f5ad01d32444094a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:27 +0000

    goback(to) collection willing gift hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
