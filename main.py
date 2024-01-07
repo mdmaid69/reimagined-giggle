c00ddd09f0958ca4512e93498b80f9315bea1e08
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:14 +0000

    next share tear accidentally forever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
