0fce87ae1030abfbd82073c6e0228ff86402fbcc
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:54 +0000

    manner geton arrange use lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
