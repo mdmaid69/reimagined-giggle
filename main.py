53819e0285cedb66a9beb688de19fadcf24d4298
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:21 +0000

    live weekend cap everything by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
