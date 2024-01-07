c813db27f24f642c3c6ad510b4c6cd05b4f2fa76
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:59:11 +0000

    formal wing kick whether control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
 def calculate_hyperbolic_arc_tangent(x):
         return math.atanh(x)
-import math
-def calculate_floor(x):
-        return math.floor(x)
