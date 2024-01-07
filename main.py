94aa616351042c324ca9bccb7837c4ca7f542fc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:58:59 +0000

    dry conversation dinner pull pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
