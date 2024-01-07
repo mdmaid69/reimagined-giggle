1ef23bf33c947fafe0bcc013f614a2a99fcf13a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:09 +0000

    Itself capital accordingto smart goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def is_palindrome(s):
+        return s == s[::-1]
