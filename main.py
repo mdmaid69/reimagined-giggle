d91e4bc28fdf115a3ce23e5f74a777f7d6dc492a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:47:09 +0000

    rent everyone fix post secret

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
