41612858ef486ccdbc1e5d0e6b9f192d7dc78984
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:03 +0000

    fact keen far question gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_elements(lst):
+        return len(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
