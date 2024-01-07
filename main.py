d9ec6be419842a2b220c829ea6ddc587f9612b4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:11 +0000

    thick lift none attempt callup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
