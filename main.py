a3b202bf795fb0d8f37b0f35a12d4f8f97e407d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:37:11 +0000

    dinner cross country start permission

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
