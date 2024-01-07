0178f8afd3deb99750efa1940ffdc1d251f9c26f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:54 +0000

    force with loan risk production

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
