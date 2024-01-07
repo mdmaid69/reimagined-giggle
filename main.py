a99c20e9e8163caed92b6b292f3b3100c5911518
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:14:02 +0000

    region convincing circle joke safe

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
