4ee7a3a92b3c5716401f39f4f614cbaaf3192c03
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:36:53 +0000

    rate point convinced declaration careful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+print("Hello, world!")
