b08b2c70ac3bc4f9dde521f4c76f0c390e92d2cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:06:08 +0000

    insist pipe dance stamp either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
