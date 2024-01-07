b613276769957f3434ace1138169ba8948276a49
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:45 +0000

    estimate tense difference popular live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def find_min(numbers):
+        return min(numbers)
