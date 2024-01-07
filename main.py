edfe59acc3dcbc3316bc89a990c063bb5a3ec58b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:41:17 +0000

    runaway bet female flat demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
