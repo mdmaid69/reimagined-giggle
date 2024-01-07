e3e63886421c93145820105de362239dc395ea88
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:29 +0000

    rob middle indeed gradually tooth

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
