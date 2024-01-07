b27c8df950e0eef881c4ade97fc33ca3bbcc2fb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:19 +0000

    about pay scream anybody reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_index(array, item):
+        return array.index(item)
