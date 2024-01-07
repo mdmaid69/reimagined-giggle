3df7e3b844f90c71231b38d4620e9a9d782f2c36
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:05:29 +0000

    doctor top blame loss pretty

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_density(mass, volume):
+        return mass / volume
