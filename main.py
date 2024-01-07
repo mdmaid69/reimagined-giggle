5a8fc18dbf38d642e0ad0e4b77d087fa91270333
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:53 +0000

    prevent famous cool sight completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
