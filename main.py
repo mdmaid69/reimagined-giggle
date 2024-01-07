9fb89edba2e737ae8f4d3e002c3ff891f31c5abc
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:14 +0000

    case nice hungry scheme plant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
