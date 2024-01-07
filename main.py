fa8aaf1352136be814a2b3fc0b71125da1734382
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:23:07 +0000

    impressed about now Itself aim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_min(lst):
-        return min(lst)
+import random
+print(random.randint(0, 100))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
