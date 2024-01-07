87e121cfc11ef715c6d08ed8089fb900df66b124
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:13 +0000

    glad embarrassed plate during thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
