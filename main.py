c3c9c09f7a526f92944ad1829143b2ce9b55682c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:53:55 +0000

    speed forget ill but aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def find_union(list1, list2):
+        return set(list1) | set(list2)
