687666002e699ee8450749c7c78c5d057cf24205
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:48 +0000

    by moon shout unpleasant clue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
