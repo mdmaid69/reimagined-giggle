edd11132cd21f2cd86267f896e705dded65fcd12
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:35 +0000

    freeze fact theirs fail owe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
