521634e3b26b9e9cb166b606a61553f9c2937063
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:01:46 +0000

    tell fit impression police have

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
