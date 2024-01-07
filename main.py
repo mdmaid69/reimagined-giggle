4e3ee67e5c38ed2fef57c5275064a9adacc99a62
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:44 +0000

    bin rid about always skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
