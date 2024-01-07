b66148716a5eeebaeefc0eaffefa2b3832beb4ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:05:22 +0000

    contact bread report dream achieve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
