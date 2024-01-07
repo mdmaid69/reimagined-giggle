4951304a6c681d3701190b636a379ed10bd7cf69
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:26:50 +0000

    squash on comeover anywhere whatever

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
