636ace7a68eabade9fd62aa1403b7fe6698842b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:09:00 +0000

    difficult giveout whichever decision attention

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
