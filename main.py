6c5a37a7e0a4a2263638deaf002199a604a0ea08
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:34:54 +0000

    breakinto getoff minimum live mix

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def convert_to_hex(n):
+        return hex(n)
