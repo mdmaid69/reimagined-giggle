809128996a4c6ed98a0c211ff20d1e5de33ea483
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:31:36 +0000

    rescue themselves behind behind wrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+print(sum(range(10)))
