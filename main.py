9ab160e9b4c86d847f0e999ae2b1f69e76891e4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:41 +0000

    fail prepare shoot expensive arrive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
