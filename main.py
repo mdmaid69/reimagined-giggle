f8e51f995791fdd36be5a2fcea3b87774e4c66a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:24 +0000

    division visit decide common like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
