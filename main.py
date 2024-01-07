a746d48af9fc1e73711f9b04c03277c4765ce347
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:28:27 +0000

    fasten cough ocean underneath estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_list(lst):
-        return sorted(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
