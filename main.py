f763a70be24975513e7d533ed74837eb2c39dc66
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:55 +0000

    introduction thin combination permission breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import sqlite3
   def close_database_connection(connection):
         connection.close()
