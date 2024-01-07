1d9a77ab177768242eae38831d49f1cba1ea5928
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:50 +0000

    corner journey close majority prevent

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-def convert_to_octal(n):
-        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
