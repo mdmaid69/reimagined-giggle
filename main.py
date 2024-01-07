e8020134a5e1ce5c6a712d3355609e6ab3068066
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:07 +0000

    will solve ask hole limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def add_numbers(a, b):
+        return a + b
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
