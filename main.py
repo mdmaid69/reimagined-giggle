d8c46bfa07f92e205e9338a8512f666cff30f85f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:37:01 +0000

    speak science love process interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
