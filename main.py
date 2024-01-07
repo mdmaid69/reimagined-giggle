5571fe69c6d8e80020806eaaa4a84c126599cd22
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:56:01 +0000

    eager still child Oo dance

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
