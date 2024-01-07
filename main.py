6a000cf1ff3756759db5704bc2a314eed1a12fed
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:52:06 +0000

    meal free shine mysterious him

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
