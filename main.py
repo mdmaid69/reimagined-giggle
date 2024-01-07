6ea3736629b9c76af625ba4f0ca414d4cf0a8ce0
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:26 +0000

    Pp nice push tend temperature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
