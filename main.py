7f1fb13c87ba432b4350700cac38738d6773f301
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:49 +0000

    set happen needle long least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def multiply_numbers(x, y):
+        return x * y
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
