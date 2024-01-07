885921955ae4967c198abfff166ed41d5df6c92d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:49:11 +0000

    muscle fire have fun thumb

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
