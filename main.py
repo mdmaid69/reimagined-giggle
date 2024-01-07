1f567e842d1dcccd08009da76f50430f00aa6842
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:23 +0000

    degree foot polite order mix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_sine(x):
+        return math.sin(x)
