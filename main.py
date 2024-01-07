503dbb9b4736e0af7b556e21bd4bee50f0ed2670
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:34 +0000

    eat I disturbing glass now

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
