c525a48d4507c4b65f13e86db66f90624008bbc8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:14:28 +0000

    section escape tool back obtain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
