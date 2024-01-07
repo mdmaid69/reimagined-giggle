0df93e02b08af8bb9dbba4cc7ecd4b482f936359
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:01 +0000

    nerve gift undo against train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def convert_to_hex(n):
+        return hex(n)
