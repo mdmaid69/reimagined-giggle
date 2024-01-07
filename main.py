1602332eb83c94de24d35752a7440cacfcf08069
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:33:07 +0000

    speech flood deaf band Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def divide_numbers(x, y):
+        return x / y
