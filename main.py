20fb90bf6eb0dce56e550f8afb835272240b6b4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:53:50 +0000

    note beginning water friend spend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+for i in range(10): print(i)
