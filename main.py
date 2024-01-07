669d01fc2884f8bb82bdb7d424635d9bc0dc1f4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:26:24 +0000

    operation tap takeoff once instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_working_directory():
         return os.getcwd()
-import collections
-def create_user_string():
-        return collections.UserString()
