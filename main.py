dd0a9031ef7edb3635fe1e601d8dbadeccbc608d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:12 +0000

    runaway behaviour round visit measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
