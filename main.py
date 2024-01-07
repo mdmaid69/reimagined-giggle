52539534132abc5ffe377f07d489eddfd407c540
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:08:29 +0000

    skin comein own fault try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
