a0d724e204b1a94e713e8fed1465a07969ef6758
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:23:02 +0000

    exchange close rush swim top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
