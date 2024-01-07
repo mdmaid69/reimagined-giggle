2837a35843526c62cc489af75779b1f1f642f95e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:52 +0000

    exaggerate tight defeat search sometimes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
