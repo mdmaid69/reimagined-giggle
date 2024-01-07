0d2335d35cba4cd6a9d2a201c14a167342cd999b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:34:35 +0000

    impression whom letter friendly bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
