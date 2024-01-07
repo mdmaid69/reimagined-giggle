22fb4be2117f3dbefb9992a45c84ae288da3c3aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:11:30 +0000

    any rare valuable switch contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
