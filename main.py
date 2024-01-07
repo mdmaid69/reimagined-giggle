f579e241fdefeaf3e64c868479c4be4fe7ce7454
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:13:39 +0000

    division travel sick sensitive Tt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
