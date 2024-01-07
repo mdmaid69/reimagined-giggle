4fe324df01a9a12a8423d0a524cde027f7fb5fd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:36 +0000

    include late taste bend risk

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
