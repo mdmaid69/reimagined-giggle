768473bf003c293d4d8eca0cf4830a6d222b4254
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:05 +0000

    scrap talk bringforward compare punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
