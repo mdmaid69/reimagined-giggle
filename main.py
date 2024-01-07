2ba61112957f37045db82d22b542fbd2336121c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:37:55 +0000

    congratulate touch substance telephone plenty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
