fc731cd46eadea714714c22b7a8b5f1bea75c81a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:44 +0000

    though squeeze Ff start behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_acceleration(speed, time):
+        return speed / time
