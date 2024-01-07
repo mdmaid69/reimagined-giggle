156323175e2286f93655b651c17b394494679ac9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:32:52 +0000

    contain buy afford objection petrol

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
