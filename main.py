52b9a59f37533700136a0eaf91a0b2e88d9b5ef4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:13:46 +0000

    bang during wet characteristic friend

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
