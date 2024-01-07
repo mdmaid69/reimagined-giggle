9b149759200164da2e43f133d3607b3fd451b576
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:03 +0000

    pay occur sound flat 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
