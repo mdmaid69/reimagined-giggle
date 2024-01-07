2cdae67bcf140d010cfe7941c199f6f1d21ae830
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:09:33 +0000

    rush bus exaggerate lookback(on) news

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
