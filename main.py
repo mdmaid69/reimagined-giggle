473744d9c87c9afeb97adc29230bc03069208a0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:55:23 +0000

    want bang trouble round frequent

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
