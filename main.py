a920a360739c8954b925d608b6f93a6b58f787ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:18:09 +0000

    ago slice cruelty damage measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
