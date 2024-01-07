a2bffbc386d34eb149553d95e89dedc643b56dfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:47 +0000

    meeting unusual but flat breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
