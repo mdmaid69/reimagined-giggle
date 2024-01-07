87ca2e748cdc02580782741a25936eed5a451da4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:17:25 +0000

    behaviour value rough actually urgent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
