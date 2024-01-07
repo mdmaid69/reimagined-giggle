c8deca39a71479ab495db0c4e1290bc9738885cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:20 +0000

    rent scheme mystery limit tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
