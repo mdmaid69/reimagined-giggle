19bdd8a58d9cb3b92618387ff0c832cd6d65486d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:45:20 +0000

    love defend pretty through vanish

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def reverse_list(lst):
-        return lst[::-1]
+import time
+print(time.time())
+import array
+def get_array_item(array, i):
+        return array[i]
