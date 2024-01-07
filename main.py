8d1bf3c310d32f840c50167aea717c43774a89c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:07 +0000

    space visit how regard behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
