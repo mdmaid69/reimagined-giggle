06398fba8edc7d9155a6bdd0aba890fecc975fe4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:28 +0000

    string lost may pretty sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+print(datetime.datetime.now())
