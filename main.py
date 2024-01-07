268324315bac0cc76c33c02a5912d5f807f49604
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:18 +0000

    flow away favourite flight square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
