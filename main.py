74ae3dc26817e363cb485f360a5f62a782725f81
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:20:19 +0000

    beside other closed want roof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_length(array):
-        return len(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def is_even(n):
+        return n % 2 == 0
