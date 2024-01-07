e061077ed73229b00eb8cc2f0d9c1e02369197df
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:20 +0000

    wait love material seat declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
