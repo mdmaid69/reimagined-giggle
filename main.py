6512d7a067d6aa5343eeaa2abb7305bc642d722d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:58:01 +0000

    crisis offer sudden responsible we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
