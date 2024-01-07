4eb616d68b101c32434c3cbfeb67d30b43618257
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:57:13 +0000

    steady easy pleasure behind Dd

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
