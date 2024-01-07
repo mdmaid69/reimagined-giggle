43d056ff14cf1b6e29f050523d6ebce0bf54c0fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:03 +0000

    noisy towards afraid combination want

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-name = "Python"
-print("Hello,", name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
