5a816342f094abc080823df6ef641890ff024ed5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:04 +0000

    street float watch shall feed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
+def find_min(lst):
+        return min(lst)
