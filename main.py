4f4ee3037f0038c801c1119fa83be76f50d3d25e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:40:25 +0000

    179bite hope block mind prefer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
