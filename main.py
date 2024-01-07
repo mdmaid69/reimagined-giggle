1456fd53a19006f7ca9f76b699aa7ae9e5b7ad76
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:47:24 +0000

    lookup react sort except invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
