44f69045f2a7566dd50259c744455d809cd9081d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:08 +0000

    operate comment contain city position

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+print(sum(range(10)))
