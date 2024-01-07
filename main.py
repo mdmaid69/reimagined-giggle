b745538fe4eeb4629c5b201b4cd836dbed5f1c57
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:09 +0000

    reply bag everything total push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def sort_list(lst):
+        return sorted(lst)
