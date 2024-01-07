49702fa0af8ec7782a930c3db188db91e680278b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:39 +0000

    throw emphasis pause wash wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
