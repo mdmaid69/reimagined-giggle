87e855fe31df65f96e95cf5749132ec0493929de
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:04:24 +0000

    list practice cover community imagine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_bool(array):
+        return bool(array)
