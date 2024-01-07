96a4630f4187b213152f5b9d72f86e72cfe3fbc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:26 +0000

    open demand finally schedule wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
