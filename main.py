b2f10243008a16e0cd5dc278ebaf6b0465d2902a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:20 +0000

    suppose legal change read press

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
