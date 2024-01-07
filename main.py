1e681713f7dc39d169a2537492eec76eddf87d24
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:47:40 +0000

    half convinced convenient branch miss

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_list(array):
+        return list(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
