373240c6458852efd0ed203582498f03a141d424
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:18 +0000

    card mouth comeover carry ticket

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
