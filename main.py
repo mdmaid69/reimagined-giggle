32f57652700574d72712b06e9ab94f7945464f0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:22 +0000

    effect kind hers ilovedicks develop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
