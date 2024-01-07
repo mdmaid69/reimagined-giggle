a76ec2f7a82a5e91dd2af8b97b5f2d564f7715eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:52 +0000

    scared comeout error cover cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
