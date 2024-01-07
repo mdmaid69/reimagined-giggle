5a595c8b5ee9baccc6e7bc40d0e29fa228b050f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:03 +0000

    deliberately importance sign curious honest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
