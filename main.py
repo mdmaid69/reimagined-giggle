5e7b388e6db770bcc78328b27dea06ca3702930b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:33:17 +0000

    plan extremely step worst cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def reverse_array(array):
+        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
