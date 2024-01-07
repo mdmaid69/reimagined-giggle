afdceb9f66720abb650f91a90010ce471b84a917
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:22 +0000

    different not praise underneath accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def reverse_list(lst):
+        return lst[::-1]
