4b9e5ac161d414367c89770efaad0870cee8fef8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:07 +0000

    opportunity fire somewhere him attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_min(numbers):
-        return min(numbers)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
