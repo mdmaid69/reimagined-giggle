fe2aa975b3a352d133ad51a07aa2919ff7a1f587
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:16:20 +0000

    comment find miss turnover sail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
