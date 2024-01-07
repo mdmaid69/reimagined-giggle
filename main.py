5ebafb32554723b908349f6ad0f93a1e9a6cc28c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:20 +0000

    fly spin anxious care experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def convert_to_binary(n):
+        return bin(n)
