076c8a59a9e79965cfb006562eba073199f271ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:53:02 +0000

    knowledge correction side mail busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
