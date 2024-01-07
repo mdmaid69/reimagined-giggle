a2f51e06e77615e8204818ceebd72063fdb703ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:45 +0000

    objection stare practice smart attract

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
