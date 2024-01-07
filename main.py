f972c0da0e1ef2e9826f88fa1eb0e01d8b726685
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:45:10 +0000

    few yours apparently visit probably

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
