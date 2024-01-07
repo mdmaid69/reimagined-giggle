bd268bff72fd5e1513f459acb9c1e13bcfcee4e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:18:55 +0000

    runout let finally shoot go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
