42066eed644bd7105316ddde5f5d4f5624df4a46
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:10:59 +0000

    flat exactly preparation possession phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
