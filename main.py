bd973ef4d8439e78d5402b682019a6e0970a59b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:21:08 +0000

    price clean whichever summer pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def count_elements(lst):
+        return len(lst)
