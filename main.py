20f69de65e3510d475cc55d2c60532e426dce89d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:12 +0000

    meet yourself public original squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
