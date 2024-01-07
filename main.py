4b3e7e78a46ca6488b63c5d8f8467ff37daa2dda
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:33 +0000

    character deep pass firm confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+text = "Hello, world!"
+print("Characters:", len(text))
