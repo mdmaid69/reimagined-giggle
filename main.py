2627c6bf2446e621acbdb75911e9f94912aa874a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:23 +0000

    broad innocent knock before print

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
