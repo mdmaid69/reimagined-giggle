da32213ea7d4a5e41809835955439a5aa6b796b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:26 +0000

    sure powerful no success sick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
