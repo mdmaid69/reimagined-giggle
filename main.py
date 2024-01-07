04848c8786bf6a18256b68448823a1128069b9d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:40 +0000

    slip weak through impressed majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
