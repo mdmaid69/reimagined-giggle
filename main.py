565059efd2e11b0e415ab83f35c1d85526d7425e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:19 +0000

    separate past strong level bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
