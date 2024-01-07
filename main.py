96df4825ae3c488769d4f77fef735ccd82cdc78d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:07:14 +0000

    almost party guide match limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
