c258baad12569fa8d0c40ae79ca59bb980694b05
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:10:14 +0000

    union paper clean another surprise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
