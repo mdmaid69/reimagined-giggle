49363a3cf427aa6f09f8234dd061575096d35927
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:34 +0000

    deliver friend add black protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
