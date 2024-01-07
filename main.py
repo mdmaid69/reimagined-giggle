35d91315e674ba3539210334ad888648f7d751e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:53 +0000

    interesting sleep total storm save

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
