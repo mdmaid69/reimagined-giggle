ac12b74abfb7db319a7cf9124a536262753ec490
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:33:10 +0000

    interview independent sentence trial share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
