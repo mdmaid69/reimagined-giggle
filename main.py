028d3a00ce7fb00bc1fd75ed4f5aaa92d247b64a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:06:15 +0000

    whoever advantage reserve system area

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
