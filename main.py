47ec2a2b086ab07a45d170a42526c2f9c7ea953e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:20 +0000

    furniture contribute brave rent week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
