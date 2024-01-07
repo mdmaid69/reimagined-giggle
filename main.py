ff5f6b5d9059c812785f06473c4730c9609f77de
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:23 +0000

    please sing able ride meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
 def calculate_greatest_common_divisor(a, b):
         return math.gcd(a, b)
-  def convert_to_octal(n):
-        return oct(n)
