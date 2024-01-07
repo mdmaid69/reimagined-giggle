e05a30392516c18c085891ca90ae5a8e908534df
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:27 +0000

    tip complaint doubt watch division

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def is_even(n):
-        return n % 2 == 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
