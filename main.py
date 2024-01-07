806fb648b728f63f5060f74e444a3ba6a0d66026
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:24 +0000

    oil worst in birth do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
