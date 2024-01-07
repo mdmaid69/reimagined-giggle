fee7243c4a5a4ad2e41baea9a49b7fa59dce1c4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:52 +0000

    account improve towards perhaps can

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
