d70ffb72e9f6d0c1524dbbaac5cb0701e4c3321a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:00:28 +0000

    unusual mainly later convenient pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
