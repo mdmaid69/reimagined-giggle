9d074bfd1548ad564b11f7c0ddd5b67913d7b74b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:50:52 +0000

    empty flood project substance equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
