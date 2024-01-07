1b4cfa7ce961dcbd9a6b3d859c1383c2dbb61d3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:18 +0000

    discuss marry whisper smile straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
