1c99d90edb6b6136f1dc533af15953f99686f5c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:29:31 +0000

    official border bottom illness exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
