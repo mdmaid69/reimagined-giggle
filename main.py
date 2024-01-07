4164368cfba6f1d30dfefbebf83b5c0d00c69e9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:33:50 +0000

    far Tt wipe until generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
