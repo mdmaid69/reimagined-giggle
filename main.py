50a58fa789b0a72689d3e7952ab3daff9c51fe1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:30:24 +0000

    lift extraordinary promise adjective 571enough

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
