57774d45b14edc79a932029bfbfd5518408d9248
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:35:50 +0000

    equipment free wind meet shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
