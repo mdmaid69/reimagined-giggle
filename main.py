e0b793e10e870fc863d691d66db5782b4aa329c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:21 +0000

    tell there chin rescue equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
