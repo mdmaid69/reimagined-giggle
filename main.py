be0fd0b540bb20b40e60c652debcc788e5ede45d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:16:11 +0000

    what priority precise steady very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def reverse_list(lst):
+        return lst[::-1]
