02e4651d1594f1d02c9c64698a8cb73e9f8879ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:15 +0000

    small now accept frightened explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def exit_program():
+        sys.exit()
