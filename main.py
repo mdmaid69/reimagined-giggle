e159877af3ac3c668384b7abd88e013d35243c81
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:36:54 +0000

    impossible total hole wave shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
