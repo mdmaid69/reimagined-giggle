2949ecb83f586dc5a990be05ad612ddac31ed7bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:16:21 +0000

    battery approval acceptable fashion only

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
