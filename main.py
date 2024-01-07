740a5199184d933e170340bac0dd6937b9e9f1e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:54 +0000

    approval proper either improve left

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
