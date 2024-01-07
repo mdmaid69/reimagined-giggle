e47a8aee408ec0f3fbbe6909ca8912ee76c80fc5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:38:54 +0000

    suffer match in anything direct

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def subtract_numbers(x, y):
-        return x - y
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
