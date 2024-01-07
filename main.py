f6a45071fd4a95e42854ecf1432a4114b942f079
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:40 +0000

    spend definite Mm thought by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
