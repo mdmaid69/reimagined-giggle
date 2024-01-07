257187fbe5564a845a62516daee7d8c95ab971fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:00 +0000

    getaway left keepaway hang frequent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
