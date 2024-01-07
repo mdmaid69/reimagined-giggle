88f9b4e6c35c8341a61227e1e7197670b3bd5a29
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:18:20 +0000

    his rare give agree protest

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def convert_to_hex(n):
+        return hex(n)
