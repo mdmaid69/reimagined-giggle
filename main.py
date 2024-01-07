645571e50b7754c87f4b988256c4c9f73f97faae
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:45:22 +0000

    else pencil bringback turnup which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
