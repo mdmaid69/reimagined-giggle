ae732edcd464276286e9991c6526eb4268e2e591
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:44 +0000

    callat steam thinkover once chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+import tensorflow as tf
+print(tf.__version__)
 import array
-def clear_array(array):
-        array *= 0
+def get_array_itemsize(array):
+        return array.itemsize
