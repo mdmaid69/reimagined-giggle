1b5139c08542d75a0acd82a7c2de11f03b3242d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:21 +0000

    pair deliver track lookafter show

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
