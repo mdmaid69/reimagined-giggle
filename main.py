a4035355481365080d83dd0879657efc3234bf92
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:01 +0000

    failure plan behaviour calmly rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
