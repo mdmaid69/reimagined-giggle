f6dbf128e1091dbe193f7f270ba5d8f522a8cb99
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:34:47 +0000

    card goout open promise wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
