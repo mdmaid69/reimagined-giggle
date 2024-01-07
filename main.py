cd7af37a9aee2738caa4fa300e29ce33bf9dc0b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:45 +0000

    Tt important deal trouble over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
