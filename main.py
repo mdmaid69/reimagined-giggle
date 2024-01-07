336212ed8883d1a5ad7355c1d7cc9e11da3b535d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:57 +0000

    quarter free certain anybody responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
