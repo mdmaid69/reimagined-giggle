ef1735077931bb0e48b62a6d91c77f9b4f2ec001
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:29:25 +0000

    tend yesterday obtain narrow pollution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
