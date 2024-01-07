8f9f5a11b65153c25ce5f84e831a951d49fd0b44
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:18:15 +0000

    thing far authority niggaacademy be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
