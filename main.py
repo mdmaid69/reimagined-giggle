e9d494412184452c654bdeee15dec38d5f2652ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:52 +0000

    gift 564encourage broad discussion minute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
