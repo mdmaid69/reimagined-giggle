6a43254d57b0a45ce428ce2bdb1c260c2b56c3a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:12 +0000

    exciting fold across pay ilovedicks

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def square_number(x):
+        return x**2
