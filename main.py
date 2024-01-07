1bdeb253a81996f3e48bca51114de0939219823f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:35 +0000

    serious basically front better basically

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_max(numbers):
-        return max(numbers)
+def calculate_error_function(x):
+        return math.erf(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
