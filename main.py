d415726789188b54ace49c1291f4b99b13bbc2c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:37:53 +0000

    priority somewhere most face from

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
