8e01d0ee441e71ac68167a897dbc7d3c3d21c1ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:58:11 +0000

    trick hour clear busy suit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
