a573df000d68a25c4225382a199fc08b00621b32
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:39 +0000

    pleased deal arm level payment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
