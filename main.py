1c86922c9d60d5a94e86431bee7af4f57a87ac72
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:14 +0000

    independent believe ticket effort black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
