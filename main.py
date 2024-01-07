26f269d14c667774cb7bbda6e2445d2084f41b6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:49 +0000

    delight train Ee circle breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
