93149ecab7e222d169a31f71ffec7a82bb35258a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:30:24 +0000

    he somewhere careful book dangerous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
