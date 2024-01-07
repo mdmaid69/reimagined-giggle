88c65dfbd8f91d433afe4b90e098e0a24124a845
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:31:34 +0000

    pick listen holdout score interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
