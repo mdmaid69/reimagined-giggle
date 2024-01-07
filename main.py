8c0245fffe25c680e5b0d7f1b8a23e784e30eee1
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:37 +0000

    platform worried begin face noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
