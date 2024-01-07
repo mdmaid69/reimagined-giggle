9ca5f67e65be2f583844845a6690edd104e2a943
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:00 +0000

    answer balance border wild age

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
