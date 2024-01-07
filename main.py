325078f8f2b4e4bf6a29546fc64e2542a00612ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:37 +0000

    history season goback(to) rich consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
