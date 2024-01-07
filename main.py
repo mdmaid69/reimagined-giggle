ebac57b926a9521fd49f878f8d2d2cca3257258f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:34 +0000

    turn protest sleep sister phone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
