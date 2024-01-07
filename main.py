0f83ab4cedb6240f4533ccb8a1e5618cb27181c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:18:15 +0000

    admit university tense bottom centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
