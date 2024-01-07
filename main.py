1a1cc958f256a26c63ae7711a6cd5d2bacf6960a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:50 +0000

    office whoever into official follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
