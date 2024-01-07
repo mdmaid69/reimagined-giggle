305d669ce0ee03400d3d80ecd3e52d68f2ec46ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:41:34 +0000

    yesterday Nn innocent in refer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+from collections import Counter
+print(Counter("hello world"))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
