3dce4de61a8fc0ff7c7c1f894649ecc450437604
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:51 +0000

    forgive further visit hello destroy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
