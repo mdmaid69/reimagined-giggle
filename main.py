0dfbdfa4b50a8b8dc3d34a22d9b0ebcea4efc442
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:28 +0000

    commit tonight tomorrow heart ball

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def add_numbers(x, y):
-        return x + y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
