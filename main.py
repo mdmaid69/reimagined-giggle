8f1eb2cd19b30d165d6e5143b79cfc2849db60f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:22:33 +0000

    protest nice satisfied chief coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
