59009a2fa80793bf84131da263f4d2fda07b7499
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:04:43 +0000

    breakfast copy along point moral

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
