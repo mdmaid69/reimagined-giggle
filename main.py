0cda74d890aa781bf7908667ca7cdfd3eec750ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:29:54 +0000

    only comeover bright her rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import sys
-def exit_program():
-        sys.exit()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
