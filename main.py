ebfa3fc5376bcecab347d26388f04e804dd752a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:47 +0000

    advanced begin sudden deal intend

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
