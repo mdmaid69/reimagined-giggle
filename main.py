a738e3159eced52ced2dc5ae8595cb0f711acd46
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:08 +0000

    school means unfair question scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
