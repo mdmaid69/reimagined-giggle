09dff013d97f07503ab9c95cab3d952275890c3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:38 +0000

    cook head there look roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
