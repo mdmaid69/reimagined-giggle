3127eb0441d2776d0bb7aeda607ec62bf4c94e19
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:56 +0000

    advanced pair expensive total loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
