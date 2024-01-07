a7d743400bb5cccc9709f5b3a33e7269be5fcca1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:07:26 +0000

    corner arrive period everywhere false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
