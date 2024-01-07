40bebdc96c895fb3cc90e9689febe74bb24d8e8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:52:23 +0000

    worth air fork bye underneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
