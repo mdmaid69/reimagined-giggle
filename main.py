b952d8deb895f2f4d0c7de9028bd2cd83dab1f4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:59:08 +0000

    surprising only talkoutof lookaround crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
