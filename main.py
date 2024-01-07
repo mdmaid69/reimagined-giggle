394d782caafcf347842c2b95c44f75e1906639fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:51:11 +0000

    regard prove development adjective typical

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
