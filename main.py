84f2774ef3d9ccb0b5b6fe25b5f2a24ec1fe1f57
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:04:39 +0000

    raw explanation proud bang ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
