68394afd14edc7e6aad88872f2e400afaca937d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:52 +0000

    main prize by card score

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def convert_to_octal(n):
+        return oct(n)
