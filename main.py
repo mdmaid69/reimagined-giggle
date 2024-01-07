c163faf1e8472e31e4d02ac13666201180747e58
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:14:50 +0000

    discussion takeback upstairs dress excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
