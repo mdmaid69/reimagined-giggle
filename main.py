1645cebc937c52062aa69269966453cc13a30032
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:35 +0000

    love bank talk hard burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def append_to_array(array, item):
+        array.append(item)
