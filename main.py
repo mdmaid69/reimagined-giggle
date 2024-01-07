3a0e2a3205922c596d7d59ea305280eb61dc7e0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:38 +0000

    family woman heat include connect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
