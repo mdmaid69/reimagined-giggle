aa3db1dab41461ebbd92fbca91e7c12944975697
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:06:19 +0000

    hollow congratulate save plant dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
