719f355f9eae4a1ba6bdfd53e0cb5b0c2bd2bc47
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:22 +0000

    importance time big keepaway sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
