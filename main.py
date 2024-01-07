5f50c3ed0e9c359bd13c882cf3f40b7e16bf9bbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:40:50 +0000

    slightly own winter general maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
