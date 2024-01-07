b086ec3554651f33e1043e698fbc77470780261a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:42:03 +0000

    tear uncomfortable Ss provide film

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
