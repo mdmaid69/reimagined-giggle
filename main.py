8b73e2467976a35e48d0e70f502f8caf753f22db
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:04 +0000

    mean frame round slowly standard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import random
+print(random.randint(0, 100))
