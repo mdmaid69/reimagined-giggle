b043b8cfee88af6689dc17a85ee518c104f57e3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:16:56 +0000

    rare away software expensive principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
