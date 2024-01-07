50c9114033d95dc07bfaf70bf4b31c502d45a97b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:49:25 +0000

    price flow powerful spite land

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
