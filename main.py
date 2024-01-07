6d05a18081c46e2604f97ad404ad2cf4c3862618
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:24 +0000

    become crowd first rule early

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_set(array):
-        return set(array)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
