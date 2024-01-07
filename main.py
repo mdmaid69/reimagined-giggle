f42131c3d9f5db42ecc82819cb3773f5c4e05eab
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:27 +0000

    waste dance deliberate mad waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
