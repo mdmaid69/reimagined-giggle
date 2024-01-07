c526508f33e4c73c43bc629465e3ddc90647f39b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:36:33 +0000

    noisy middle danger cost other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_list(array):
+        return list(array)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
