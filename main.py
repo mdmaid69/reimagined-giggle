1f61aa748fa23d456784e944feb6c47b82773148
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:51:13 +0000

    almost emphasis avoid no strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
