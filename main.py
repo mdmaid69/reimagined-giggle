3ea9a9201c72430bf18721befc578db462df09fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:34:05 +0000

    suggestion gap maybe speed trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def multiply_numbers(x, y):
+        return x * y
