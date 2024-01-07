1fa3fd845b700f8e668484591c545aa01690bb70
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:53 +0000

    ever identify instead there performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def append_to_array(array, item):
-        array.append(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
