f9d1356e24a465ac346e5a521d03c53f1bb0372f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:47:48 +0000

    protect nervous round past train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def pop_from_array(array, i=-1):
+        return array.pop(i)
