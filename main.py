e40e31000fea800479d490b0696e20120abc4291
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:11 +0000

    usually point table wash never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
