ec155b5cdd2f351f2d407801ddc5b6d620d8d646
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:29:41 +0000

    rule strange wind Bb ring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
