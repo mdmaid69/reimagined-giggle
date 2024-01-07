7b07c7cfa93909d2dd6642615bf1a38307371585
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:55:22 +0000

    ear Pp pair towards pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
