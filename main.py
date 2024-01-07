510268e5d956bd6d6b5ff165195c479b6b34a877
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:34 +0000

    brush attention terrible performance chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
