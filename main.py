f38ce1630a4cec4ac19793512f4c50c747eb51db
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:41 +0000

    grade book study cross demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
