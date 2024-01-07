a011a6212ed57588d556d7c9bce354311fe3738d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:37:51 +0000

    plan stick act hide excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
