6ddb7ab9058d5a9e2c102a25f9f37a79f7a9d705
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:53:12 +0000

    collect children cruelty visit goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
