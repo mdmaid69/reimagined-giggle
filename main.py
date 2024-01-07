da8d22a66cd0b7475016eb2d7d8780b01afdfec4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:18 +0000

    second Zz even eager next

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
 import array
-def get_array_as_list(array):
-        return list(array)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
