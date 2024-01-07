aec087938186fdcb93b9b6a2109aa38e0fd8e844
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:23 +0000

    difficult however pipe wait mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_file(file_name):
+        os.remove(file_name)
