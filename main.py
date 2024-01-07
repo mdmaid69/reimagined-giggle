2d0d9a56ec855292a3ab09c9ce262302f574a7fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:17:49 +0000

    sport quickly book report perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
