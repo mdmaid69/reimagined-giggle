3b00de45d715d51174aee5e02727eeb5d98345f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:20 +0000

    dress trick fetch reaction pick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
