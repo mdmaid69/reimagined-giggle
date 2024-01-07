2c01d73750afc148f4ce2f4a38b16deaaf5df238
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:03 +0000

    song pass send friend available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
