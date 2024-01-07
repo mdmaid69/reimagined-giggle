ded70ab323aea28551ff83c2f28443b8341a3c76
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:24:24 +0000

    dress deserve crowded soon use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
