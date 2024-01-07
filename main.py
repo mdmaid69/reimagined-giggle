221e8ce635ca49013528a8e64957010a0ef98f17
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:36:20 +0000

    gooff quickly nor plan save

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
