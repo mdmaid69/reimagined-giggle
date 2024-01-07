f2f013521b4e988051fc5871d5219905b12bc322
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:34 +0000

    positive compete turn impatient head

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import tensorflow as tf
+print(tf.__version__)
+  def convert_to_binary(n):
+        return bin(n)
