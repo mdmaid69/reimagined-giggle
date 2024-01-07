ef2e56a7c7fc68f5b7d93e0065e3c11a7834051e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:49:02 +0000

    control risk science survive whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
