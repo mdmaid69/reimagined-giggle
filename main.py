f75866ecde03ee28cb22b3c3149a0bfd351a2857
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:09:03 +0000

    herself organize slight car burst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
