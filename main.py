a4a224c8a39fb1d9940adcf513dcf9ac59e8323c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:26:21 +0000

    danger whether dry surprised right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
