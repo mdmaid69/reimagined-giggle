53322de3a3be7414e3c21f349c1052add0a27a02
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:59:56 +0000

    strength affect want escape brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
