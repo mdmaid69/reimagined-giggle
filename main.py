d595634d165ee474363158ae658971fa048b4a15
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:42:34 +0000

    shut perfect store test healthy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
