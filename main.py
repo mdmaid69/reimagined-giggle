80d949e91f5487fa36b02b717b7735842d5891b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:48 +0000

    even direct phone clearly Bb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def square_number(x):
+        return x**2
