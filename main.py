a586829e1da3f7fc6a09fa08c28e4d59b1323cb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:23:22 +0000

    improvement bill accident talkback trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
