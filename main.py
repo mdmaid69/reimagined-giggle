e8f346647e924b5c990a6959008d93f5e4b6f606
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:38 +0000

    its fear alone organize further

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
