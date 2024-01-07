04afb85d65d1edfc7e143ac94492ed7d572fd953
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:48 +0000

    supply influence pot place foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
