aa6943dac0753c7fa9466f547f0fe4dca35da7ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:16:27 +0000

    television day bringback its undo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
