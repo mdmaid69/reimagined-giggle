f3b5d20f0a68e104af200e22a316a0299f453fcd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:07:29 +0000

    offence wire reply cost brush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
