555a2eded8e48ad312ef14a4caed1d52aa9fdcf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:01 +0000

    necessary listen land sight above

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
