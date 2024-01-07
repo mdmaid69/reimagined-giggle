e686c09cf54e9e12c3fed8649df60fe406650f66
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:55:16 +0000

    word need song than thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+for i in range(5):
+        print(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
