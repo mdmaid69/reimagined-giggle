868815a3b26d4516337d62d1a56b22870a2b93de
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:18 +0000

    whistle similar consist mind exercise

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_as_set(array):
+        return set(array)
+for i in range(10): print(i)
