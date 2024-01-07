80b0919dcb0e0768e1a88740eb03699befe91d9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:55 +0000

    couple lift mean high hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
