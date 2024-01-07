5f838b9e12972458435ba25281e91b01bb2af681
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:06:44 +0000

    border will concern godown teach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
