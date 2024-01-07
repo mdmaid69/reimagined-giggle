7f2a19b039882f0e5ec12cfb5b8ab6ce154e00ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:41:38 +0000

    rest occur with business spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
