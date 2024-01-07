f5cda1f33ed223e1882131f470475243eceab1dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:39:04 +0000

    report behaviour address now positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
