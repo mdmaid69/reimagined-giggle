4daaf367b19ef20c338e33042497f9a03196844c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:56 +0000

    offer connect top watch car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
