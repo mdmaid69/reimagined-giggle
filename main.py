7ceaacedde44c254dc241e86af1bd4e707200779
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:11:51 +0000

    accidentally advanced promise funny silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
