f21ebb702e64b476a34ee394bd4093f16bbb56a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:32 +0000

    serve adjective tense perfect importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
