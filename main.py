627d27914dbc2182d6e57da2e2b2d6c6e4dea37e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:27:50 +0000

    quick often hardly job regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import collections
-def create_counter():
-        return collections.Counter()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
