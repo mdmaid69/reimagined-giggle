ad337cbfe9b4000e55eb57f0f67d53f2e16a8afb
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:24 +0000

    message man negative high clearly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
