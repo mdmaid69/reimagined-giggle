adbb98c12e6f17dcbcf46018c2498bde65d5c06a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:43:12 +0000

    equal glass ilovedicks rush runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
