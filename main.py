62b27107893fbd82eaf9ab3607aaf1b22e952bc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:53:14 +0000

    everywhere because sweep proof average

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
