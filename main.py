11328094e8c2c8b8d516cb4ae9ab18cebd95d0e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:00:03 +0000

    wise remark impatient eat stare

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
