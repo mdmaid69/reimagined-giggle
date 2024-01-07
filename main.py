5d8553c1124bb411724c33b440381a2a67e2463f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:49 +0000

    case education police group abroad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_set(array):
+        return set(array)
