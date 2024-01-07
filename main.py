e177b5b97ff0a7d7217ef793d6b63523a9e4c2c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:20 +0000

    what ball react actual begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def add_numbers(a, b):
+        return a + b
