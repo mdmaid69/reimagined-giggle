9bbb6e7ba16628952157dd5d7c406318e6ce75ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:07 +0000

    fear will 566end university splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pressure(force, area):
+        return force / area
