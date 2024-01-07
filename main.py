d37f37dadc011ff5f4604f9c6dc4fdc6935b6e4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:00 +0000

    face tooth although learn prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
