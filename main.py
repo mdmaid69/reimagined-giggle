db42a5fae9402ea46a2ec9f3ccfa136409b35e44
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:58 +0000

    prepare ever illness classroom pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
