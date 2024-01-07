a9f2d9b07947a9df4f20866c72a5869b22b2ec10
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:13 +0000

    getout love Bb squeeze only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_int(array):
+        return int(array[0])
