7279efcf4a698b7ecf3ab40bfc1ead741665fa53
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:34:11 +0000

    spend work its or positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
