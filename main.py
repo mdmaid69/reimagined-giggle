3867c8220c76af8b164db182741570eff7535d94
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:08 +0000

    honest turnaway answer contents solve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
