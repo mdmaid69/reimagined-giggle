ba831bff971fb1a3bfc1945e8d28162fbb5ec411
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:52 +0000

    threaten rare swim occasional awake

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
