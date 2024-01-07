414d82ec7e91dab226482e96ef7352bb0b9abcff
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:07:59 +0000

    true bang join space free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
