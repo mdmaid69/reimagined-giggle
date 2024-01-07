8e6fab137a7f978524acbbedfd237c90797e7e0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:25:42 +0000

    Ii classroom circumstance off trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def exit_program():
+        sys.exit()
