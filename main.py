cc7f2ff607db8c0b98806c9917819982ad3fd0e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:25 +0000

    vision speech goup dig neck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
