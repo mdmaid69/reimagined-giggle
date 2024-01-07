604236d1cbf06f68857a5b5eb7f95210558065ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:47 +0000

    service combination breath recognize part

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
