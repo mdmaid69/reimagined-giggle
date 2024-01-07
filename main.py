efec83f8fc39c97a8f7a03725dc646a0e3fdec57
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:55:35 +0000

    love everywhere position Bb this

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
