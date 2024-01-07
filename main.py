2e30bae8e8f74b2b1c622ee25f89144b80049980
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:36 +0000

    love no shape cheque odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
