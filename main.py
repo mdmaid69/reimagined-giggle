19ff0181da9a5ae9f7bd5ad5bbe0139c82bd5881
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:49:42 +0000

    convinced capital correct ourselves quality

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_bytes(array):
+        return bytes(array)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
