6f48eed553e4b1888f5d89cded6ea4946d7cafbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:35:53 +0000

    phone progress dry operate like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+print(math.pi)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
