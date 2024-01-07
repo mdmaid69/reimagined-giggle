38bd465102bd47ab80d1b8ac7cf64aadaba200bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:18 +0000

    its connect worth like bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sine(x):
+        return math.sin(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
