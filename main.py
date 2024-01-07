8eb8cc946530ec1cc9aa8617bb10072f840dc3a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:00 +0000

    declaration deal measure help lucky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
