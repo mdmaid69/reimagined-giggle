38afc22a32f7f46886eb1aaf2f95a809250f1308
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:50:04 +0000

    now land design less Ww

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
