5d4c514b8738ea2d9fef499f1717ade4abe9d99b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:02:25 +0000

    rich us fight charge meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
