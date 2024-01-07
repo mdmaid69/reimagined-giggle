a5bd497eaf7a3af706c376c10a4d761a693834b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:38 +0000

    plenty decision food Oo concentration

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
