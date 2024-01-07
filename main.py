67f018645720dfc0f2a209ecd804384d1ae17312
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:28 +0000

    loss float refuse rush action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_directory_name(path):
+        return os.path.dirname(path)
