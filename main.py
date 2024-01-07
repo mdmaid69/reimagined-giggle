96825dd551e8f9ed9883a35def99966a7ca73880
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:30 +0000

    trade section grow crisis fond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
