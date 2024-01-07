98e591761991226dc3d2d1e4d167e8f756fa6a4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:11 +0000

    intelligent dry pleasant somebody beat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
