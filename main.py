2d77e1098e51caa01cd37c7727e386911fe16e35
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:43 +0000

    press influence beat duty scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def reverse_list(lst):
-        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
