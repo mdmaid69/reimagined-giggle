b1e46b9ebb942f3689cb2bf34db47e22d81a6814
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:00 +0000

    fun supply name willing love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import sys
-def print_python_version():
-        return sys.version
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
