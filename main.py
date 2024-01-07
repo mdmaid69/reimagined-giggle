222753839a60f77bb8fec17023676e4d5bb2074b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:15 +0000

    complicated down happen emphasis time

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
