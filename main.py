96d745e970f0d8c4eafdbf6fe36dfdf4c34b5598
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:46:50 +0000

    show suffer up question not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
