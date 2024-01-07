dfd18f5ced719a32fcca61f81aa9ac7ab5667f9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:04 +0000

    chief abroad crowded allow prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
