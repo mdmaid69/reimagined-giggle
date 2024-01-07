b679224ae7136e90415636f198f588b76023cc8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:02:37 +0000

    hear except always breakin laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
