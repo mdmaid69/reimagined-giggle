53ca2ed922bbbc230d8ac8fd62a5a54aca82cb47
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:54:20 +0000

    after bang everybody he plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import time
+print(time.time())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
