c98c01bd37aa275a271a6300cb71fbebbc768ac7
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:23:25 +0000

    cope callout comfortable proper corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
