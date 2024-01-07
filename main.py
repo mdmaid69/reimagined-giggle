1aaf5d48ef63240443020f024e2631cc8f3f2126
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:17 +0000

    operation passenger wild before takeout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
