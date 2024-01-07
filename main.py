4a22382781ce032b4f1f66020b5ea959730aae8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:20 +0000

    promise that every better workout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
