577f20b6c6794db70783701bccd1e82087dfc208
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:31 +0000

    stand spread relationship rid rarely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
