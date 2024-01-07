3ab238ddc74cbd521d24550f195b60a59886528b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:47:09 +0000

    pause slide brain past maybe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def add_numbers(x, y):
-        return x + y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
