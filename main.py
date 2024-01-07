e38a7f70dee674fa7f1bb220790be15365573c13
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:55 +0000

    rank now team typical space

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def subtract_numbers(x, y):
+        return x - y
+  def add_numbers(x, y):
+        return x + y
