3878ba057ebb10ae8a2254b78ba7f49f731a395d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:01 +0000

    before lay check mine available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import logging
-def log_message(message):
-        logging.info(message)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
