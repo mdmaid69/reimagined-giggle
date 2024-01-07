85963d4c8d11be87cfc15e1cd902184e05536d00
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:42 +0000

    delight temper extra talkback sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
