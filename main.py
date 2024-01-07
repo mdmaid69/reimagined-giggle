cc890ce8e72b12f1838cb0250fa50e3915888540
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:17 +0000

    569enjoy prize unusual impossible design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
