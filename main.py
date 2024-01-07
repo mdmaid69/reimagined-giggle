d37c3b5acfca9cd010dd87e19a705dacda8d9b42
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:24:41 +0000

    her congratulate brown quiet giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
