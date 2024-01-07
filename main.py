7678c91a8ba7fc164fecd8476cb4f630402d8fc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:08 +0000

    tough practise task hope shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
