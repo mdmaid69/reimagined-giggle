d817b9987c6798a8a4e8679a9f5977bcd5dcda4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:03 +0000

    area if mad noisy contribute

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+print(sum(range(10)))
