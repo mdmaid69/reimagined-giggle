73d664ddb6163ec29803e0058979d053b458058f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:21:57 +0000

    Ff pay that joke increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_username():
-        return getpass.getuser()
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
