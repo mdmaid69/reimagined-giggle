a080f587b2a787bd019e29cecda52054c4f20dfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:38 +0000

    lost no smooth contain develop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+import collections
+def create_user_list():
+        return collections.UserList()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
