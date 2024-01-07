6ecba341784b99c1b5e787f8a74889b020348409
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:02 +0000

    word experience late correct approval

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
