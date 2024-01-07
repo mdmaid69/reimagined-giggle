f19cdc028d7e3e16734d6ec6862a9686af025d44
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:07 +0000

    wipe separate they soil police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_max(lst):
+        return max(lst)
