fb00f1d0247b58e2f73225370c32b61677d40adf
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:43:56 +0000

    suck difference either acceptable Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
