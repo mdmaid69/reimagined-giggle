761548a1e263f9accb5a68e1cc3191069282abef
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:03 +0000

    pipe exit female knowledge lost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
