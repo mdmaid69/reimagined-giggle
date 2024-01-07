f91a1cfe0870266719e66d3f51b947ea1de74651
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:49:39 +0000

    view afford wing hungry first

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def print_python_version():
+        print(sys.version)
