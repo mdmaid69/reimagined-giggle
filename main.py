4a5e64499f9d8d8d158d5cdb76efe82b00ffe510
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:29 +0000

    month before comfortable choice crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
