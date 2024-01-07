9b408138b6b38240587a2cf1580be87e9bfd9daa
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:12 +0000

    name how usual afterwards 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
