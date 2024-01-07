2db73671895c7c076f0c90c80c9d3f9d41be61f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:40:52 +0000

    house proud regard until workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def subtract_numbers(x, y):
-        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
