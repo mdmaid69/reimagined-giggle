37bbeadce47190f29795a38b0e78b7771909e1c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:29:00 +0000

    content promise thankyou score pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
