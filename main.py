bdec53c41b4c356ba21cb5782be4906b2cf1900c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:15:39 +0000

    exchange definite into cross juice

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_acceleration(speed, time):
+        return speed / time
+def is_odd(n):
+        return n % 2 != 0
