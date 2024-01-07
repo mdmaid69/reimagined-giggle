6daacbdb6ec693c968ed6094e0ab91980d1091ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:32:40 +0000

    flow breakin debt wheel spin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
