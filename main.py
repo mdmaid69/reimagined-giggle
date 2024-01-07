c0c8dcf1f39bee2ea543a8e97256759e1781da51
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:16:14 +0000

    row embarrassing both takeoff whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def convert_to_binary(n):
+        return bin(n)
