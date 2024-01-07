f23fd2b0bc0aa303d2f78f94e1d7e6c14fe3b2df
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:15 +0000

    send benefit send crisis any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
