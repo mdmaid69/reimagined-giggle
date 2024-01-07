e43abb75ac375524fa0750bc28e703f141e13878
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:14:47 +0000

    breakin guilty prevent accommodation niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
