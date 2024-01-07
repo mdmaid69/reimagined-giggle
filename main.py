6986b84980ae287464fc63c92ae63d5c90594bc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:51 +0000

    mail embarrassed discuss day lie

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def reverse_array(array):
+        array.reverse()
