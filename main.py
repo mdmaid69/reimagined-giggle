62a85eeaf17f15d8c4a3477125e9fa832c32582c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:47 +0000

    centre pretend born traffic majority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
