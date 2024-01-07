0ad5de5b5f598b58f2d459cfe6518eedb9b2db06
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:01:29 +0000

    which keen divide thanks my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
