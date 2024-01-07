3597347f27aff3c748e75d55e67ad1ba1b157d48
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:36 +0000

    coat failure public defeat sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
