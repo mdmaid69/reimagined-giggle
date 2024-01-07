fa7cf848fba33ac7ddea689a8e6235df08df86b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:29:57 +0000

    stamp unite gather far fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
