e0a410444da7b5feef41b5003505e9cc1be93d96
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:38:14 +0000

    from waste position performance about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
