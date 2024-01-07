f986ce3c4125ce888ac6089faadbeb8753bcef37
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:00:59 +0000

    lookaround greeting roof drawing example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
