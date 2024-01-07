27c3387c48f1ee1726b87b34227f7d2b1cdaffdd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:26 +0000

    principle heavy Dd rise fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
