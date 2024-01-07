ecf6e8e8a28b8a7a29159f7b3a5e8296301d77b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:38 +0000

    appear state control face mine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
