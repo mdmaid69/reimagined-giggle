0479ec6a701f92aa5bcb26948f4b74cf300b4ab6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:52:30 +0000

    let sink close promise advise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
