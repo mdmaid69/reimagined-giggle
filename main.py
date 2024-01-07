16788f7fda11125a672005e97a982a2920fd4d22
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:00:57 +0000

    service team city bad pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
