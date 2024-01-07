cce8af66e62e99fea05df2d9feb5467eab3b43a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:16:07 +0000

    hurry confusing plenty height speech

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def remove_duplicates(lst):
+        return list(set(lst))
+def count_elements(lst):
+        return len(lst)
