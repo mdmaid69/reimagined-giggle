04b3f5e42bd4478e7669e4900be7fde32fa21a56
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:38:56 +0000

    connection judge press official name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
