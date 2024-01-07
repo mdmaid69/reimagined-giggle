6adbb4f0e45a5c838a81bab5ccfe472e57f1ddf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:15 +0000

    measure whole drop basic measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
