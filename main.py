5168ba9bffb22da629a60219433c98d9df28a9e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:47 +0000

    telephone giveaway exercise long comeback

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
