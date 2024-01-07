86680faa24993464d2c105ed5f77abf7b1de0801
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:26 +0000

    past embarrassed number object claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+import sys
+def print_python_version():
+        return sys.version
