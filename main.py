851984ed4e2f66f95493f55614f65a5233b794aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:37:48 +0000

    weak fashion follow teach snow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
