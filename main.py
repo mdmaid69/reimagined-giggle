1353d213cac037e5ae3bb79b779fd80392cec304
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:29 +0000

    system must prevent friendly reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
