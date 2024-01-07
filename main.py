cba4e2541f21394b106797b3ee15b687dca2dee9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:56:53 +0000

    rush other effect demonstrate bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
