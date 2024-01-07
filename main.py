67acd5490d34df7cf99a2064513dac9917bbb198
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:37 +0000

    arrange shade pity after sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def calculate_pressure(force, area):
+        return force / area
 import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
