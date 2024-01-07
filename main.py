35e5ef2b88fd77ea240adff4476721534d67ae07
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:28 +0000

    style proper mother trip question

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
