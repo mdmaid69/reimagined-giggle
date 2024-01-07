3489d0a4cc79aabdec4c4dee1be2d730cbd0b212
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:57:31 +0000

    burst guess embarrassing world she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
