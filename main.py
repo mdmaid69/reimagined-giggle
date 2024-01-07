87b7684c76587a418cbb18c3171e7cbfc63dc3e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:20:00 +0000

    dish free distance turnback wonder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def reverse_array(array):
+        array.reverse()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
