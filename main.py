0f36685a540203858569d1870a9f4b95c63e5a4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:45 +0000

    suffer as thing justice except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
