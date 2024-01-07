1d6a54aaeafb33f9b16e1ffdbb03e5aca078f00a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:52 +0000

    key rarely mean cross again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
