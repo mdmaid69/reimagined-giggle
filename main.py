f611c7fab10a65c2b6104342edafc208d0a23bd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:06 +0000

    bang lot straight bright miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
