cd9126a2e4526e3ca01b76f749ef096f44516cdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:18 +0000

    contents pleasure cap limit Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
