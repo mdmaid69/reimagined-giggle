349a9360aa98b7d1e6b5fde9acfdd06659d842d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:26:55 +0000

    hang characteristic needle aware limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
