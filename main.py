405758637bd9b273f7c3e70af3b30ad124293b84
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:51:31 +0000

    extraordinary close fact hard land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
