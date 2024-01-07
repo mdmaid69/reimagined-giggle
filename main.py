5bb355e63073ebeb32554edf0a687013550d7a3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:51 +0000

    cool strike decide declaration twist

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
