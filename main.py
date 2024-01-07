35b27e397e42e9c5485be07fce49f93caf8311f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:05:41 +0000

    variety worst behave shame excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
