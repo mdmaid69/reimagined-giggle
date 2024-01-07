ffe1d531e19f54c0ce7983fe58fccabacadda0ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:00:22 +0000

    sorry workupto cope reliable clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
