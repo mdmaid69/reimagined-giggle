fe178b6d00afa9475f65fcb70640075c979568df
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:34 +0000

    kind whether deaf fashion either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
