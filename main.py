c26a11f18dc0859ef3f6b411c8913d4c6a524f3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:09:13 +0000

    television keepoff at Ee include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_error_function(x):
+        return math.erf(x)
