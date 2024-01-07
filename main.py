814dcd718bdb9face12ab66e739e20910943e643
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:51:02 +0000

    thin consult book pleasure every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
