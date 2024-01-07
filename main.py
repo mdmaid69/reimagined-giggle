f5d1e8cef55d397cac7a1c25532cd0479354c4b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:53 +0000

    several perform description chase capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
