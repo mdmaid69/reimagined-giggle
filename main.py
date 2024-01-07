26d87b8a6470f3ffe0c03980d7cf75fbc91c917f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:01:48 +0000

    miss pain fold swim position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_user_dict():
-        return collections.UserDict()
-import sys
-def exit_program():
-        sys.exit()
+def create_user_string():
+        return collections.UserString()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
