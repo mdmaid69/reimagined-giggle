d51766277f10eba94f8d2df11af7caafca37a34c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:03:36 +0000

    sport behaviour yet any mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
