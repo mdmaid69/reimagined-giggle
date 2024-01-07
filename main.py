c0453e990040e2fb7ddd50c380260c60508ede27
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:30 +0000

    within use cough pupil basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
