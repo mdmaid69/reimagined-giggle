503657d344bf78c10ed880a4bf97fef331a7c456
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:44 +0000

    century true Ww press breathe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+for i in range(5):
+        print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
