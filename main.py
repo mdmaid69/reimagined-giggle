c22e5f5514925cdcf421bce44c9bd18f9881758c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:05 +0000

    limit coffee compare power earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
