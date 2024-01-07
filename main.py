e57cab415514246814a609b7ebc06a4629504e0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:40 +0000

    fork surface minimum sensitive active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
