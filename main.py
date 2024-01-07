672a8eb8256429984369d5ec4650d102844e0c30
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:17 +0000

    culture turn bit pronounce shut

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
