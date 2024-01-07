72cc6677a7d717a1ccbe072177ec525339ac6215
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:40 +0000

    influence near float apply remove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
