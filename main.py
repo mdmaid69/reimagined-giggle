a915199f7a94954eaeb785f51ad757a048083741
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:36 +0000

    lunch be whistle mysterious create

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
