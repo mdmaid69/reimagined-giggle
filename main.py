a705fd8134aadc230baeabd620893edee6587e62
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:13:24 +0000

    train whole depend post terrible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
