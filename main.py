ec346bbc0ddfec2f6235f532d6eff11d4b13b58a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:02:48 +0000

    broad fit means moral characteristic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
