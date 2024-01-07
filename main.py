569396065316cb20a74c6578ddf1797695cdcd83
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:20 +0000

    idea 566end comein moment team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
