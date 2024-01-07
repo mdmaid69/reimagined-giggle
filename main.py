8e040cae0a5a8e040d59197322adf9d42ab230c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:29 +0000

    comment holdout confused neat consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
