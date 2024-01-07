ff1d80d54853ebbb883fde2704a1e6f9882dc757
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:14:44 +0000

    runout suckme loose wind be

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
