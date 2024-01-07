9bd18cf6d8312e66fcde16e3efc03c5d7bda68e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:40 +0000

    describe reject care usual lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
