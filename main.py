2bdfb5d0825716d1881df704c04885d9d643808e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:49 +0000

    record snow lie break charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
