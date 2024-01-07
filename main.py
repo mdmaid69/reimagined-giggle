18589bb6f12c0fde6502cf88670ef5e050721ce9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:02:33 +0000

    regular maybe family stir forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
