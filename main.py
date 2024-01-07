9b8609a5ffa6d2f5ff8ea93212474fe6257fced0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:57 +0000

    touch study comeout maybe congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
