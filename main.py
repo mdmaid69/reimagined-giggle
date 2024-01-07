69c689c6efb3a5718b65a0a69edfdac5334f99b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:52 +0000

    central tidy train majority help

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
