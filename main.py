38231961475535de0a7dbf54a3d0144f316520c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:41 +0000

    niggaacademy supply roll case turnback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_rectangle(l, w):
+        return l * w
