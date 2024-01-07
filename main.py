9aefaab78940f0fde81503d98b377a9967a553f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:13:26 +0000

    whether provide runaway future past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
