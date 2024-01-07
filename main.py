9d9a0329d723108edb4d5b0dda58a930f4f547b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:46:17 +0000

    food spend aim press smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
