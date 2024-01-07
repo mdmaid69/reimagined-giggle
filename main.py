640d1691a1a8a0a1971f5be3fde5f89ef048ee1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:10 +0000

    dance criticism tomorrow Pp emphasis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_dict():
+        return collections.UserDict()
