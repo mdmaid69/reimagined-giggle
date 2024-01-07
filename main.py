52fe0f4e6229510469e0e606d7276c72903851f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:00 +0000

    comeover silent violence medium board

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
