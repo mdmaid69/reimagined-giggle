b682826a823e6e145c783f2b047de99a53b48baf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:46 +0000

    close throughout sell or much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
