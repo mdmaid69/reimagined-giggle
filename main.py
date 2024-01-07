4e604bfbe416c8484ff6ca1635612ac3f98b31aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:22 +0000

    regard us production available legal

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
