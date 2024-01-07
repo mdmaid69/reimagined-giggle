d0e5850175476dd6d67c5a00c7b12836648ce1e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:38:53 +0000

    spite comein dig development match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_current_time():
-        return time.time()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_repr(array):
+        return repr(array)
