15746390ab2db28b45bcf0557c1d9798dfc839e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:02:23 +0000

    street welcome apart welcome rent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
