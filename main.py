fc531ede265dbd9f916faf3151c3d53b45160746
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:14:48 +0000

    design mind autumn throughout out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def reverse_string(s):
+        return s[::-1]
