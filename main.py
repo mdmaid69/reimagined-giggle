50ef80c7390a3763a67d569b5f49672ac732c537
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:26 +0000

    fall sad month mail chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_max(numbers):
+        return max(numbers)
