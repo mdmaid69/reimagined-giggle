42d0cc0599e077131849e1ba405bb7ae2e89cfac
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:26:25 +0000

    when skin achieve small sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
