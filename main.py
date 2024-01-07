6f08c386255ac8e9a3f6b905def9ff479f58bab1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:39 +0000

    564encourage left advice light fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
