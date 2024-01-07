7415c3de70e88b1b4a54ac0fb211a93b2d8dedf4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:56 +0000

    genuine finally but especially flight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
