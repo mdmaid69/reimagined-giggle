cd6e9459773c34570bef4a9305c43d897bfb5540
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:05:01 +0000

    stare history week mind call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
