8beca44af2c37fb0fab312b8ed114ef991499965
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:58 +0000

    avoid spend cope wide fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
