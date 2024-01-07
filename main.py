07df6a2b494ea4382f3718316dd4b3f4101b5ac2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:18:03 +0000

    fortunately meeting degree surprising unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
