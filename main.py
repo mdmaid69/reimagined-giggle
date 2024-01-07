9ca838f09fb12b1c89297fa59ddf0b1c5f51c360
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:32 +0000

    do bringup as there order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
