4fef3e6aaa6b283daff3318915e19d1a6c9f824c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:23:58 +0000

    small explain announce relative comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
