7f65729ba410c8710d668c2b819edbe1a46b24b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:18 +0000

    organize pride coffee as crazy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
