559c5f93a636a3ce7414f890d91d3af3ed44cc6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:16 +0000

    influence attempt challenge pot guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def set_array_item(array, i, item):
+        array[i] = item
