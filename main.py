b022d46ae27988a3ce81c4cbcb6c9dd39aeec698
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:00 +0000

    dry welcome separate prepare familiar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
