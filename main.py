4b2b0a831b93f03125c8a870dcf919f0c893963f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:19 +0000

    full thorough increase price easy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
