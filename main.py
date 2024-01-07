66afbea59613bb9b51aea78e632b0f7584719071
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:00 +0000

    little common bringforward rule future

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def count_words(sentence):
-        return len(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
