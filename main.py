6d0ee5d1b05cef876405233ac0bc046e24edb6ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:23:23 +0000

    suggestion since confuse defend raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
