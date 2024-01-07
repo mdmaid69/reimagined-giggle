137172e24e4898c3254b407afd0819b13cb455ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:18 +0000

    trust believe beyond hold share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
