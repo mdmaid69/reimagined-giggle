93f39fc74722771b542f2aae6bf0f9a4c6719a5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:31 +0000

    little breakup bringdown fight little

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
