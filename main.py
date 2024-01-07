ada30cdd420eaa8fef2b45052fac6ee69a4ccd43
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:20:29 +0000

    ago scrap cross swallow comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
