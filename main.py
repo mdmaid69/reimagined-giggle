f863291bc51a3ae4f86f0b87aeca2753d9396940
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:17:07 +0000

    yesterday colour length bread deal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
