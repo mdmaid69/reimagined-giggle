b428020311f75fb3b0c4c8c88eae288c3ca9abd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:55 +0000

    breakup apparently interesting understand stare

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
