6f8641d775d5892e9bcd68473f359628fbe449cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:57 +0000

    burn influence art pour watch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def count_characters(sentence):
+        return len(sentence)
