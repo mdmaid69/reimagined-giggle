216a696c6dc91aa19f66291d7595e4f7fbe5ab55
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:13:45 +0000

    interested neither watch sudden yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def convert_to_octal(n):
-        return oct(n)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
