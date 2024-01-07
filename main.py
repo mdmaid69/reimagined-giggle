129fef65d1eb7cad848180b7c152c9cb5564a2f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:30:25 +0000

    support sure passenger gradually mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_min(lst):
-        return min(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
