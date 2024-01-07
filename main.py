4a47a0296552c07ce7b9a4f00a2543759440ed3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:17 +0000

    Kk undo popular touch left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
