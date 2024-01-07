f485c3e4224f15019bc1e732427d3f0f3d5a9089
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:31 +0000

    upwards convince worse communication wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
