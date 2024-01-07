545f9760e8ebacc246ba4cd73bb368aa2330035e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:34 +0000

    clear guess emphasis fear slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
