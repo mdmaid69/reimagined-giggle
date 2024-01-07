25d7d34cc55c6a467997c29f2c808c94ad344707
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:21 +0000

    tape young stare slide concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import platform
+def get_python_version():
+        return platform.python_version()
