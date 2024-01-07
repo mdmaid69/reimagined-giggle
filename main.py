49b60378b90c2844be85ab7dd7d8e67d8357ceb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:10:24 +0000

    key move add before breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
