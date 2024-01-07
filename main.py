b2ca3bd9e2baf2c79d3d5f8a1adfa22da9371328
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:10:50 +0000

    control speech miss foreign another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
