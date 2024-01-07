f088b06fec4358a75e0d6022742c239c654ee09d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:33 +0000

    pot foreign circle true damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
