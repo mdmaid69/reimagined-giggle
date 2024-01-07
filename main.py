f4084017c7877858300f70d42f90cac18c6ed12f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:30 +0000

    settle live put probably beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
