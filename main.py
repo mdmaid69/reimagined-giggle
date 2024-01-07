cbb13505b85c528b8c649f541c05712585a7dc31
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:56 +0000

    difficulty long next dress influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
