410772ca29cba346d30c3db7ca6b83ab86f15b10
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:37:28 +0000

    name wake identify count limit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_pressure(force, area):
+        return force / area
+def calculate_volume(length, width, height):
+        return length * width * height
