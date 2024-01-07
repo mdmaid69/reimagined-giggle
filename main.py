da0304812e1992cc446caeb5b005095d87afd365
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:34 +0000

    calm solve understand sentence improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
