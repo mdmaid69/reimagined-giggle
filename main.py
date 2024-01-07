ea4367bdfe757c60e9a617a5726212d6e24d8268
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:44:51 +0000

    add reply meaning lonely discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
