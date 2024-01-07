f6f151d98b9ba8df21fb773434965d51dcd08f50
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:45:47 +0000

    gradually hungry idea aim slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
