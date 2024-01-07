833e9f94b4fc2752f62c28e4a57e46c8e3d48177
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:43 +0000

    any ahead needle attract finally

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
