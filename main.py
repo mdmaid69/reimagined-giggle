df0da8fcd0ab111c530c6b3010a8340b29a19df3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:35 +0000

    anybody desperate not space unite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
