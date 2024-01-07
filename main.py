f5fab468d4be14686486fc207d47b39ea2e5c30b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:12 +0000

    perhaps us anybody march fill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
