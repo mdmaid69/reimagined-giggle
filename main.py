fac1c27cdfd37a3c31a88fa7bd1c4219b0c64cf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:32 +0000

    tradition almost smell part concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
