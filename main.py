79ba26483497d9e6a7c0362847d924638cf5a221
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:34 +0000

    before thumb concerning getback forever

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
