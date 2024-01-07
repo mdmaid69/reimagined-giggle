899f0365627e31cbfb093c21acbfb1ffc97a3abf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:11:20 +0000

    along he mail empty bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
