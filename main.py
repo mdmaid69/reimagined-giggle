e87f58c0e9b1267c5389d8092f50bf993dae16b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:02 +0000

    sensitive height nigga crime warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
