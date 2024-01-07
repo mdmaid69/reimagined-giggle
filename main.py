7dfd6f546227d095d7cd3add51035c20e56af755
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:06:46 +0000

    company respect class Hh address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_speed(distance, time):
+        return distance / time
