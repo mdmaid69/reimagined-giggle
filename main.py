d24c238045896bd723c319e7b1acfab31c98c08f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:15 +0000

    thankyou bag rule rain certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
