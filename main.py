c85e490c2004f0ac9c5c480f5ddbee9ef026517e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:48 +0000

    pick guard include defeat spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
