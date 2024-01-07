409690c656702651ae997f3b08f8ab89c1992bf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:14 +0000

    knowledge appearance room conduct talkinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
