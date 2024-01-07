5068c5691cccf655985eee56d263c4feebf852d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:06 +0000

    temperature worst luck breakfast rubbish

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
