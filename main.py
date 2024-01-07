40193b7bd5b86725952ddc00c74f6f713827ce47
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:54:35 +0000

    underneath lookup about strong down

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
