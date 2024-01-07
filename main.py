81a957e063fac22bd8091b87329b300e5b15f2a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:30:46 +0000

    change happen considering ocean my

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
