af72d67e485216f43f99209fc4195ea35d808682
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:49:13 +0000

    help spill rain clean scare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
