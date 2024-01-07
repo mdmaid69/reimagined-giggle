4d8c45d351142694c82d98ca6f9a6807007e458d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:48 +0000

    want smile hope Jj gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_even(n):
+        return n % 2 == 0
