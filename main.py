e192637b0c0b746181e3d98788df00783ced070b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:49 +0000

    laugh file bill interesting to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
