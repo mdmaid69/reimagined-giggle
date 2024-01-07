49e4ee49b755574778e90607a210f33716c02c25
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:50 +0000

    wind poor mood nerve majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
