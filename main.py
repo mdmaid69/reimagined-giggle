f813985c03fe5d280b3dcd2d9d01ebe2c320e597
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:01:59 +0000

    child stir scare juice community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
