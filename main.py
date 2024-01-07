f36a97b26e637c22b2b6fe52658c046d1f202798
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:16:16 +0000

    board contact slice open identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
