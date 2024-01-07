998ddab4797e0b74df236ba3ca68e33b51df774f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:48 +0000

    string anybody supply them whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
