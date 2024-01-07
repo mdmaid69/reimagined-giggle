c6538f171ac90892dd6ca87fdb7c97986aa7a9bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:43:12 +0000

    university future near feel seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
