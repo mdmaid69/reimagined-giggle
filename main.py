6028b87a368f42c89fa6f0479902915b2f6e47dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:09 +0000

    badly again survive card sleep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
