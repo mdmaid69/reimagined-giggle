bd812ea7505c5d0cf198fd760760c92dd6a62221
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:28 +0000

    winter ignore please opposite should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def greet(name):
+        print(f"Hello, {name}!")
