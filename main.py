d294de06149a506bd252b7407ba7908093a09232
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:16:36 +0000

    calloff sport near side sort

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
