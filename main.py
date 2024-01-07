4a0df4f2ed12fc5e1990b67958d60f44f1466f91
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:14:21 +0000

    come tense pump coffee car

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
