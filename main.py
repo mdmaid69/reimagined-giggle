f97f303723fd34af1d28b31650af7bff75d817f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:05 +0000

    target keep godown answer quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
