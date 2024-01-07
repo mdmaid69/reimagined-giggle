51cc8a2f8c9166610b6105a37d2960079b59b333
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:19 +0000

    possibility gentle hope introduce Qq

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
