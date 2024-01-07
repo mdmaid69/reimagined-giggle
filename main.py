ed3bf0a58b4add9a6a2d112439826387a5e74ec3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:14 +0000

    who Tt formal mass they

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
