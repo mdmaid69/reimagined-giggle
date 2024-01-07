f2dc84973af7232767420cb7284e5d55b7c33105
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:00 +0000

    unpleasant base popular represent detail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
