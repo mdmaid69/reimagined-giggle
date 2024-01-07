b113f9406672ca722291cfa542565e464561d9cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:03 +0000

    central central you central doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
