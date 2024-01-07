7a1a3b11a937b56ea5caabe9720b3b76a99d2326
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:28 +0000

    ocean comeback distant guilty party

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def get_current_time():
+        return time.time()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
