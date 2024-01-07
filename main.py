7ec828cc6f95ba4f98365bc66c4906e8f105aa8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:00:58 +0000

    succeed best display what part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
