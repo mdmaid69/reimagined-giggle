39c2d1e95381b61e3eb7d3051e6d0f662cbef42f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:02 +0000

    both alone favourite 572enter previous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
