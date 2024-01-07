b3418efe1e10492211f2739f0515514624f42342
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:51 +0000

    surprise lift split world absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
