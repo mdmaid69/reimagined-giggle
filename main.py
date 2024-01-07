a105bfe9ca51d9f7af2ede8a34c699a09037f12e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:15 +0000

    patient round file sting with

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+print("Hello, world!")
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
