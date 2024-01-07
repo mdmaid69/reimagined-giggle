4b429e592bec024457b88e9a9ccfc73c89cb72b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:26 +0000

    giveup information send machine collection

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
