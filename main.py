e57805b56487b54f34cf18641ae8a1ac53db9493
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:26:18 +0000

    description away live partner 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_pressure(force, area):
-        return force / area
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
