c7961d2f679ccedcd8066bbd689db475a1836131
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:58 +0000

    bother as near several press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
