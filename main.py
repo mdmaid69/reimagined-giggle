221cf7ea3830b6c8615a4acb87cd166f71b2bc32
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:30:22 +0000

    mend Ff agree private inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
