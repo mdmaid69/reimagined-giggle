49ce428b76259fab99514bb3de653aba302887d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:31:32 +0000

    which me fill determined deliver

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
