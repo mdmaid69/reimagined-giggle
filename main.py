dcd9299c02196a19a07bd82186157a93c5558524
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:49:02 +0000

    grateful wear habit birthday ourselves

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
