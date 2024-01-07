4ab472f8a94e0024ced31b583a3aa81302b0c38a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:31 +0000

    knowledge water exist Tt model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
