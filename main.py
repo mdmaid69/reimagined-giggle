5fbed19fa7beb6d7b05ce96c60370e18f0dd9e4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:08:50 +0000

    sell least tear hurry line

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
