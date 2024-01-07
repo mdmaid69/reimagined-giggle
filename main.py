26005c431d13f48d8a0a77386bf90e0a4a5a9d7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:29 +0000

    brush sky wear straight move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
