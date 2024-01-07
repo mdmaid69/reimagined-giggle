90e02c120a1b5eb57105e8af4246d4c5f834d59c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:45:54 +0000

    near 569enjoy false uponon comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_work(force, distance):
+        return force * distance
