1d750f9020d045486b1a31bb8f1ef86125ebb1b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:03:40 +0000

    learn like lower comeback combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_work(force, distance):
+        return force * distance
