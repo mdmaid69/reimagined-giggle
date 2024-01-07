e2c470be04b64309b207a99a86c51fa0c74d9f7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:51 +0000

    later holiday back only round

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
