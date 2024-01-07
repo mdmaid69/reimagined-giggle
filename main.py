4da83b7ed27edb014165cad4bfc8db2b62121b76
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:28 +0000

    moment recently company spite support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
