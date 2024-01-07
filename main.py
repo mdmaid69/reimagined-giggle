ee01577bccf2b65286304434efb0da7363c8a389
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:48 +0000

    564encourage necessary somehow fence another

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
