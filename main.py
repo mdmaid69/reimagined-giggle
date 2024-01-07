687d4101c42014ce2ab6abec4a24ad537f60e734
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:41 +0000

    Uu opinion rescue ours well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
