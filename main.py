9cab9ac7dda5446a53a25a077e8d2e11659d9389
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:12:06 +0000

    your sport taste trial crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
