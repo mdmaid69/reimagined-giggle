313a6af3747afad2ba778ac99f1e5bf785c753b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:31:38 +0000

    turnaway immediately chin but speed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-print(sum(range(10)))
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
