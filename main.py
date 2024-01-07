f965295fe0856187e41e64f8c8412ec4a462a892
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:12:19 +0000

    comein Ee rent opposite possession

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
