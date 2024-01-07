b43ddc0f5f075d100544560e0847b7a909ba420a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:05:02 +0000

    music relief responsible sweet shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
