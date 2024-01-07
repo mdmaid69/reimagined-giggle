36e6e603a5ccf5790b2aea57e5565170a09526d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:26:56 +0000

    almost kill quit broad connect

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
