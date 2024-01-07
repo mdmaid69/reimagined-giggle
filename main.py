4d38ff2718e332bc69943d1d9dc6993fbdd01d33
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:22:11 +0000

    many talkinto badly lie immediate

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
