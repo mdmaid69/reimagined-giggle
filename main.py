a869360c7a13007079d7a39b623c0c234634c69b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:01 +0000

    world perform minute fork harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_max(lst):
-        return max(lst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
