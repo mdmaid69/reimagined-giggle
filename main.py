f723fc4109105c54ad901d41dabeba19eeac40e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:02 +0000

    snow run fill argument professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import time
-print(time.time())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
