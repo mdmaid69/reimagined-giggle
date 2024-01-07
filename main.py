6e25801d675bfd7572ac10f68dfa0c2a33a79f56
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:32:09 +0000

    uniform agree like last prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+def find_max(lst):
+        return max(lst)
