ec11b71f4b663f7aef40cfd77200db474ef12cdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:43 +0000

    goup temperature sign fault shrink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
