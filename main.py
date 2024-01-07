15573466d3aa643381c35d2cc3439cc1ef88979f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:11:04 +0000

    geton excellent contrast state silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
