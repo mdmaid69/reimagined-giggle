7d5d964fc1be342129d607f26cb90fabc7494af2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:06 +0000

    central wave spray patient godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def clear_array(array):
+        array *= 0
+  def convert_to_binary(n):
+        return bin(n)
