b714fd7e8694f891af9d64e2ea1349fddac18e7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:26:51 +0000

    apparently with hospital die failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
