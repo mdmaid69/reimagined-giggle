d9f8502b272701e914007467a0014b1afff7e94f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:09:36 +0000

    jump brave drop protest phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
