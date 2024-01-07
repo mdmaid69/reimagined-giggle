37c13ec348fc984f52460a3e648e65f731bf10ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:19:05 +0000

    related shoulder possibility pleasant best

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
