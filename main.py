6c8dc29fb4b49a3d475d00fae61d6b73d8f77e60
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:32 +0000

    shock remark modern imagine record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
