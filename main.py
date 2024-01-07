18d9980ed0e3c18a742266ca99388f0c9d568d7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:45 +0000

    crazy authority serious embarrassing second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
