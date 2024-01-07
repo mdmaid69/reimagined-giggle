44b150d6dd26c7c7474781a3720d14a5eb08ada4
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:27:09 +0000

    separate side turnoff definite snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
