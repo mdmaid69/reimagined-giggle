918320a17e71831d195f7a5da2d92201d6d20aa1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:04:23 +0000

    impressive heavy people likely survive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
