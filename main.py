587d55e55bf3ac51e138ba23c368bd3ab32163f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:39:30 +0000

    claim possible breakout operation false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
