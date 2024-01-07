404d2235431cdec892492d7f3e44ac9b5adb65ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:45:51 +0000

    between personal handle control punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
