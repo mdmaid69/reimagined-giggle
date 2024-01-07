858057e333282abf360778d82eaab5e30ebd91c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:49:23 +0000

    division chief breakout exact before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
