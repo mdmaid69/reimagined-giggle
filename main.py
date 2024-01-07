15f2875c37985d0b91d7872c3d045caad10f3b07
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:53 +0000

    switch touch Ll knowledge solve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
