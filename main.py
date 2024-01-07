4c007ab4661a8c07f0a5dcd9785f0abfc1ebc66a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:45:22 +0000

    immediate too edge load talkinto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def add_numbers(a, b):
-        return a + b
+def calculate_work(force, distance):
+        return force * distance
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
