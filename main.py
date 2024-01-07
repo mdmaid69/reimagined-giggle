94e671a9056cbd5aeaf2c5a7f74d639e95cf6f7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:21 +0000

    lost usual boring snow level

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
