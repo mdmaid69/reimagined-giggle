4b20f49aaf6acbee4e8ea1097b5e0d616a05c7fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:17 +0000

    arrival lookup influence tread cool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
