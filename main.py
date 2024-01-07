7c61acdbea0942c997a6894e6f813f0032420f73
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:14:50 +0000

    row original broad shout win

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
