70a64ad5ad2f840e0904d2482d492f104e1370e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:18:24 +0000

    regularly flood substance moon Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
