3c342a2f5c1a33f1e0359927dd456371b8156d38
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:07 +0000

    difference tonight direct prevent table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
