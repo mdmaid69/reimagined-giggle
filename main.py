f3cb68e72e2747646624bcec6796f7fd582d1a91
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:31:41 +0000

    hers myself noone makeup black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def print_python_version():
-        print(sys.version)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
