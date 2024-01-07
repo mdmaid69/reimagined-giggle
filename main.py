6d23112153505b17c14ec398a5c0642efe43977d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:28:18 +0000

    convinced thorough any through country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
