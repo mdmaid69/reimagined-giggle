d9a9dd983a657dae90241fe1666a4599d082128a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:39:58 +0000

    common underneath vote minimum visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
