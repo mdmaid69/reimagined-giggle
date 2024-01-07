9fb6c2523e8418c085477f72554df376448dbb5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:24 +0000

    suit excited ignore attach beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
