2e4e58925efaca536f7798aea7877331284d5da6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:20:54 +0000

    tip tragedy mind workupto soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
