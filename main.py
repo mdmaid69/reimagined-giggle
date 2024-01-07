74927245a20c67e8e53965bad977c135e65eb3e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:19:56 +0000

    quickly corner suddenly describe fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+import datetime
+print(datetime.datetime.now())
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
