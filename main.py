c06a4c96c1af215523e15d6ae84ff9222faa5afa
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:46:22 +0000

    word speak anywayanyhow laugh trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_tangent(x):
+        return math.tan(x)
