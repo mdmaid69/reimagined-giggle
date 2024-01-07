3095f3a3fe736768c17ff3a97908c64f29713b7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:48 +0000

    platform react talk apply view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
