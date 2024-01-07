e77ed4bbaeeec0d8821428ad1bbd298894834f3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:01 +0000

    remember she tidy surprising professional

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
