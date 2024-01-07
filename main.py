7d2fc68355e5aaaba2e7b5dbc2807914736c355f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:47 +0000

    honest careful year face child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
