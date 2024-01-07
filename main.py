7623eb3cbbc533f99909526c642a74f7949f6f0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:47:39 +0000

    deaf swim instruction message too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
