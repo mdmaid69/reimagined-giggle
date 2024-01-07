da3dd119d2ef5f1fd255f34a8c909d19dde2fc33
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:32 +0000

    yes hit left public dance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def multiply_numbers(x, y):
+        return x * y
