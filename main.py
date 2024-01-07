a6259ef6cf1d947ad2ea7f366b12021c167382e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:18:36 +0000

    trap cry getoff wise able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def divide_numbers(x, y):
+        return x / y
