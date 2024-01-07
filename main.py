4420573a98248725f9d48d3c2cead45045be6b27
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:59 +0000

    behind female study learn from

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+print(random.randint(0, 100))
