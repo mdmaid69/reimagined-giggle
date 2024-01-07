f89ce231a9baf9d880ae8ee362ad56d415f8a57f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:02:36 +0000

    lookup day accidental fail tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
