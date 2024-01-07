a6a5e6a03e674b45ccf51cb9ef557d7f56410f73
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:08:50 +0000

    cry introduce purpose tidy border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
