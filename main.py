5f6bb8043db59ce4375cef30895f9150453abf20
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:33 +0000

    towards recognize train secret concern

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
