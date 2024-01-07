fa1abc153e75495746938b40fd09ea7addad626b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:43 +0000

    cap deserve properly purpose shop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
