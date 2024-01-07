9eaec4d2f62cf1502a9f8870a8757153b53a8353
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:18:38 +0000

    division accident gift false characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
