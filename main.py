7df67bc2eee1f1d48c90556c9e21dc98421f6b52
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:58:51 +0000

    whole lock wish shop relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_dict():
+        return collections.UserDict()
