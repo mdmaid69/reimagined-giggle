1594552021cdfae4bc8e67f53372b82183132d63
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:50:24 +0000

    confidence string flow piece minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
