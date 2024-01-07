7d11aa2acf34077e565904347fcc40b4a3c52393
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:30:09 +0000

    read collection Uu thinkof sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
