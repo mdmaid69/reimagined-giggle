d202ab1be1739d55863b16ef1f1c25f708114c2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:58:41 +0000

    view trousers load concern room

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_words(sentence):
-        return len(sentence.split())
+def greet(name):
+        print(f"Hello, {name}!")
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
