8f5a4ea4ec00b3e98a5d2fbc0ab8d5680daea3d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:24 +0000

    excellent fear form pull likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
