3b40e4d9909fe32f2c578a836ce22e7efe0c137d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:53:01 +0000

    silent prevent tool throw proper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
