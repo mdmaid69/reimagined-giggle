bb907802e1ce9f6b125d5dc8cd4f8c3824e184c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:23:01 +0000

    bottom runout getup bend play

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
