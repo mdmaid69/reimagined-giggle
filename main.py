e92c8a2429b3d61dd3ed8fd200f47524bc9790a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:19:33 +0000

    over shop mail himself pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
