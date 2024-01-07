056a63aa4d743dabf009e112e52dcaa04534115e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:36:22 +0000

    back mean tread ask fill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
