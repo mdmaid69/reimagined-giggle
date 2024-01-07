1c8d2a37f23689683086394d8eaf6ba77b5d414e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:45 +0000

    plenty block sell truth accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
