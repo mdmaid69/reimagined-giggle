da5c68e711ddbb6c65e369668e0f0b70cc19ce0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:45 +0000

    whether deliver along bringback hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
