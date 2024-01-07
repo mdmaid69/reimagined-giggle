06c3641faffdbf71f8771f4ef566da9feb19d347
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:06:11 +0000

    formal connect reach crash press

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
