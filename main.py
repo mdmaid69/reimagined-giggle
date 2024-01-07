74ecc060fb37e91150d6428991af7e6555eaa901
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:15:01 +0000

    position badly comefrom injury joke

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
