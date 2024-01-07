b9bd2f92325cece248fdbb518ec7578a9765d99f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:12:11 +0000

    telephone bend all fly rather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_even(n):
-        return n % 2 == 0
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
