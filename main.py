13d940764e1e66d230971faf0a0ac63649575ba4
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:30:46 +0000

    round whether hold separate Mm

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def count_elements(lst):
-        return len(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
