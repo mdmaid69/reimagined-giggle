5f934dc0acd7a63a1b82138b213bc19d570e836c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:38:51 +0000

    favourite interested wave wash bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
