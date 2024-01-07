b451eb15d7f6e31f84aec3faf36aa02995120b89
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:52:27 +0000

    married touch convinced belong means

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
