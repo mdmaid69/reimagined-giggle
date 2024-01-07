4950794584f13701e4221b9fab7cc28ccedbf7e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:13:07 +0000

    lookaround last holdout catch break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
