dbfd2abf2e31d1feee039c2f14e629434232a627
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:30 +0000

    laugh only exit split waste

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
