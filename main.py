26f1eb643c145a9285553aa7db9e24f940a58dfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:57 +0000

    finger direct top set official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
