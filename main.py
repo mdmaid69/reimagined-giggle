e1be92cbafdaff5f62f1ecfd9726a2e616088707
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:47:40 +0000

    wonder kick believe continue always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
