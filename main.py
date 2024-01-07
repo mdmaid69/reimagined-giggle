fcb7cc50cac0514cbc521328d617866261cb2d8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:05 +0000

    argue reduction pity miss office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
