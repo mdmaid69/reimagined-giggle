ba9c71153eea060ea0856fb588ecc8a923df1411
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:18:17 +0000

    welcome achieve lookafter moment pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
