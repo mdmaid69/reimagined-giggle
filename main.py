5fe4bfad165d2bb195bde321430b62ab6cb7fdbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:04:32 +0000

    content trip release shine failure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
