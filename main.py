feb20819ffabf17e0569a63dddb4926c3a60ea21
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:00:28 +0000

    onto situation black image shoot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
