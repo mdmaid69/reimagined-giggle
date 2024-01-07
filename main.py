213dee1a84af5d168a3e35c9b9d58753ba80658d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:50 +0000

    bite ours dull listenfor practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_acceleration(speed, time):
+        return speed / time
