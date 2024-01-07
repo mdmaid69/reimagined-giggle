de7bafcac34b53c6649bb5f7c30fdb9816db0757
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:38:19 +0000

    ill thick blame appearance contain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
