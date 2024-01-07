50055a75a8d863a051e43e7ce34fc13118ebdcb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:22 +0000

    finger mysterious base unit scare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_tangent(x):
+        return math.tan(x)
