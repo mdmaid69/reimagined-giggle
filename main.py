dc9dc33e30c501b7a42fd110cc0df58866292ff5
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:10:21 +0000

    design generous world beneath temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
