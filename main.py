27c0bdb4b48ee8f4bf75d69694c611a42038aeff
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:38 +0000

    probably spill progress ourselves tradition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
