a2c93741bbaad6a93b153e15e5ce3f77910cbe84
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:41 +0000

    contrast worried open much rely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(10): print(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
