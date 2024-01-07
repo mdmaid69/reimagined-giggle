9783be29a93ae489c92ce5020d8543f6b8c7976f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:18 +0000

    protest runinto mixture just inquire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 def calculate_return_on_assets(net_income, total_assets):
         return net_income / total_assets
