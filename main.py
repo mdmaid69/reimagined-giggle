f500816e3aefbac66c74d4ebf1ee1691f39d0cb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:45 +0000

    wing exactly either sorry subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
