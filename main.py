28b9e1607c06bdc69b42b08d13c1cdf3eb29b0e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:08 +0000

    rush claim pipe description whole

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
