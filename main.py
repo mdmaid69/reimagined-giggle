90d7a641a785702af8c74126276c275af2174670
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:38:55 +0000

    apologizeapologise contrast survive naturally slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
