39acbf232b2f8878bf4834b6e7ecb4ecc5451c5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:16:49 +0000

    ourselves own negative schedule reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
