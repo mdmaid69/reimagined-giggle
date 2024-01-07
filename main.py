2ba8f06b74b03b22fa9e37ddaf087635a1b2f19b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:41 +0000

    slice air protection material dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
