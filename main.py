a892ae6bac6010001d0bcec1a816146552c02895
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:49:58 +0000

    shallow say skill carry Ee

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
