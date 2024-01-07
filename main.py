89b6555258731a8b569b7ddd987bb082711896a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:56 +0000

    appeal hide radio makeup vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_frozenset(array):
+        return frozenset(array)
