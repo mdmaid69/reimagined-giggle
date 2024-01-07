8c7b76e3c9938a50cedc22c1fb5df519685aa257
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:53 +0000

    though take twist improvement reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_int(array):
+        return int(array[0])
