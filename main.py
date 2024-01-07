2b29fdc18b96ff6c6b891b6817fa86acfc21b686
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:44 +0000

    beneath pile takeback period bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
