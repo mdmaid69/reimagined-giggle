752d7c4ba839090fbf71246f0f500b66f4258d4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:54:06 +0000

    add pair wide valuable its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
