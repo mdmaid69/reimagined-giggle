113d2af544c37c74a9ecaf2f4e323ab5bbf6a183
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:09 +0000

    why getoff protect thin damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_list():
+        return collections.UserList()
+def count_elements(lst):
+        return len(lst)
