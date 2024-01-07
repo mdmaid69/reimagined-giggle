766744cce8d57ed81ca9b291cac25b863ce5b033
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:07:51 +0000

    his soak tour calmly unit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def subtract_numbers(x, y):
+        return x - y
