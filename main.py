74bb05339647a2f8893dc4ace9464cdf73721e8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:52 +0000

    female centre equal minority standard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
