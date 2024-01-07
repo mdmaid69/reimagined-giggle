840203bf7aaacc5251f6a814b93367b22cba07f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:29:44 +0000

    shame autumn soil label union

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
