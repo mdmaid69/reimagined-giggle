2be97436999428a00d68277181021d0b20578b01
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:56 +0000

    put even song nature keen

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
