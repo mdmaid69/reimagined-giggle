0daceeb67440a29f1f36375a736a622d1575fd9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:51 +0000

    urgent back city used Zz

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_words(sentence):
+        return len(sentence.split())
