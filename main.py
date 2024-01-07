8e4a9bc3304a1bcb2849e9415bedf83d6e90826f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:50 +0000

    shout laugh figure no cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
