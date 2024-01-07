c252932f2c56b13219a11df45eab6a8ad068a090
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:39 +0000

    smell characteristic for fun so

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
