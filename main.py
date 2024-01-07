c73a9de5a9e6b7aaed67ba278e957410bbcafc46
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:51:26 +0000

    load either limit flat down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
