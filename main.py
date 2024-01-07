8c462180d1c4d602dfa4f5b8e57e430938fb8172
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:18:56 +0000

    sensible nothing forgive advice result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
