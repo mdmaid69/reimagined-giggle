6060baad3e2e209ba3776fb8f0b0cd070a522de0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:56 +0000

    spring path less one ashamed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
