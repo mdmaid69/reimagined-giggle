f8c2a6574faa206c08e4e3eb5118de222bb423ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:57:55 +0000

    expression reserve beyond field exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def reverse_array(array):
+        array.reverse()
