ccee60fd45e794bc55bb18d8166e81302b5881bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:24 +0000

    occur frame then flavour time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
