6c0e7c070e4d3d0be2c21bf63cebf379d486214c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:41 +0000

    skirt clothes second exam she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
