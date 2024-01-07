d46a3e9e0efe4cbff6286eb7c5635b4e977dec40
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:17:05 +0000

    apart conversation word public edge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
