3fbffec4885bc233e9937287bb80f6b49bdc095d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:19:48 +0000

    connection blind approve notice maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_bool(array):
+        return bool(array)
