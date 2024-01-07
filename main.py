a95c8e064d50672d2fc817e2da5bd327e000f972
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:51 +0000

    avoid frighten get compare level

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
