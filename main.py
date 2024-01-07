bbc30a4da7e0dcc4065b06b56c27ea113ee0810e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:05 +0000

    bet spoil cross bottom appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
