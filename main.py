633d01683ef6a504ebe71eda0d76b95568ab4d2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:09:28 +0000

    male prevent continue kind shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
