5d657d2c79315f0537a7659e577adc0358dbd1ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:08 +0000

    sleep form display injury ever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def change_working_directory(path):
+        os.chdir(path)
