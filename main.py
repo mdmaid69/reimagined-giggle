679ab7f8f3933d1c0e0559db66eb2b9a6a6eb935
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:22 +0000

    shoot train soak act open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
