c1e814ad20fceccd0401a105e4f6479275c3a919
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:20 +0000

    goon play load raise habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import time
-def get_current_time():
-        return time.ctime()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
