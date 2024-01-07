fa8ac29b745e0c46caa007a8a0d7819aab18a845
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:22 +0000

    trick set support along keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_queue():
+        return collections.deque()
