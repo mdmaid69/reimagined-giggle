567dbf1135d9fc0ac5366c7639373cb636bb12bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:03 +0000

    mine appearance advise hurry woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
