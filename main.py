902c795d472a3203adffd7766ca4f6179c6a7900
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:16 +0000

    care owe arm study past

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
