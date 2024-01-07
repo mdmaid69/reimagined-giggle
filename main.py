590e251c25f131009253a9107945cb6e9c1008db
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:11 +0000

    pot run form ability build

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
