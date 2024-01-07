4ec9f1bd6d959a863bb6a75b00f1500b5cc623cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:07:42 +0000

    react loan flat away finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
