bb0c23f1857ca61339ea379fe78a3d95b94c62c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:21:16 +0000

    then range cool also blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
