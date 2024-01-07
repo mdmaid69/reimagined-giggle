e72b26adbcde4bf446256ba5055d394492a7dd29
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:13 +0000

    way past throughout compete clean

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def is_palindrome(s):
-        return s == s[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
