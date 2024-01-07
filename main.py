914b105e73998dcc4b793ed63b95b792e40b0b03
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:29:33 +0000

    whatever only police earn perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
