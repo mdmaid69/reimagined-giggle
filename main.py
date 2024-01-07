3207fc63781681fd478c337ef6fdfd04611d2cff
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:34 +0000

    lock crash expect temper careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
