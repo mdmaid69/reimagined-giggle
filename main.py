684e7bfbf48ca42d752c288be140429dc8ccf105
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:58 +0000

    scream well contrast bother belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_list(array):
+        return list(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
