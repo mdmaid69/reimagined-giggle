e0fdbeb6a998b7d20acc1f015fb2777799766f86
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:23 +0000

    home cheap Dd miss everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
