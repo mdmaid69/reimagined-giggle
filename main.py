6bb067ff98678dad921e04a39da8aab59f48a7ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:20 +0000

    experience dream show production ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
