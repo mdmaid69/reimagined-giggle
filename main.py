4b21c1a868b6e245c6240494e7cd5d0248d53c8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:06 +0000

    storm undo centimetre turnoff collect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
