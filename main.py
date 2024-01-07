1fdf4cbb318e6197cea2c4bab90ddafabb4596ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:33 +0000

    comment dish intelligent crowded case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
