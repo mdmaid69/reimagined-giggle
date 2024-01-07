3dcd243b21f24a0de4c834f9a03273e9ced2038c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:48 +0000

    pronounce extra hang failure unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
