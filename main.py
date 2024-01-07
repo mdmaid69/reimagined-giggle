6e461baf8939bdfee3906c890edef4e65d79ec80
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:21:04 +0000

    earn splash who attempt doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
