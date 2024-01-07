1a5e91fb046670ef0df8b51cf9edc807d31e3081
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:50 +0000

    reasonable bye for which Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
