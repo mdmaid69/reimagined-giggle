43a4cd67e2030acd380d16bfa7cfe7c116129545
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:54:54 +0000

    lookback(on) air entrance unless answer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
