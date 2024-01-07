511905584ca1cf0d42757f4eb00f7908526c81f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:14 +0000

    shame bin name read trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def square_number(x):
+        return x**2
