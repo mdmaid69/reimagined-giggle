9c8b35aba3094c1a8b636fac997c63785aa77f4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:18 +0000

    advanced press desperate original trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
