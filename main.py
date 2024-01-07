53b2cdb7e20d6a6dc2b60c6939ddd22462d43cdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:57 +0000

    remark runinto go grand translate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
