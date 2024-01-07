a81221722993441a523f74e5e8f59d9f667b9648
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:56 +0000

    whatever Yy along organize mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_list_from_array(array):
+        return array.tolist()
