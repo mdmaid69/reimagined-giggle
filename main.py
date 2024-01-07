0f086f68068d0d34f1b240db1d1408dc3510e140
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:24 +0000

    reliable independent face interruption attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
