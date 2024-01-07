f9e72ef357f382e4594513c5a423433e3de83371
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:25:16 +0000

    interesting replace Ii slip forget

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
