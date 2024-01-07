0b7a2fc79cb0b3979e320911b8e10b4f4f39e962
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:16 +0000

    callat apply size hit convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
