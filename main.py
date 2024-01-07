8099c08c0b07b3ed12b919f455dfaa305eafdbbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:19 +0000

    course translate church your expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-print(time.time())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
