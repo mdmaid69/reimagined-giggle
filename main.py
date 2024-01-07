db5d8cfbf6a8ea38f00c70721e7df7683252f116
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:38 +0000

    even wake accident exam early

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-def count_elements(lst):
-        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
