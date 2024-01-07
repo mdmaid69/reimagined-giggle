6484791b3d700020523ff3e525ee415e2243638f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:49 +0000

    estimate fire impressed swallow accidental

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
