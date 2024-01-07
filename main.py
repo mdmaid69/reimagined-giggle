a764b356ec1e1d5b75e310c446dc6819fbc81d92
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:09 +0000

    male reject land prevent bus

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
