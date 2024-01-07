d9270faaec42ad5b520112b0e2666657686c73cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:32 +0000

    concerned memory conversation supply by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
