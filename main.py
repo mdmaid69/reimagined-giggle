32309aab707822516602d57773bd39eda089c07c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:40 +0000

    respect most undo justice picture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
