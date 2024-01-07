97f7ac0a9162aa6f083adf3b108887a52b30aa68
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:37 +0000

    public ticket word fast length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
