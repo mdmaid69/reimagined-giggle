b848d992a007c4e9cd7ff07c0500280c4bf1a747
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:54:08 +0000

    reserve please about place I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
