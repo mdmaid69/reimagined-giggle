96b4d697c9088d7f162d5843d309f7b13167e448
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:27:26 +0000

    type worst acceptable win process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+print([x**2 for x in range(10)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
