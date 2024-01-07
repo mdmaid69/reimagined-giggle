0dce2df15c45e5a2f51b7b3ff432769d39b7ac19
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:47:49 +0000

    polite fault fit creature murder

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import tensorflow as tf
+print(tf.__version__)
