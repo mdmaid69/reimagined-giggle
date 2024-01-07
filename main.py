fdaf25a4ab0c3fc0976aa46522cd1c448af53fc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:41 +0000

    runaway production bit important its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
