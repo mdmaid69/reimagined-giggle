7966a241b75ebe9a5208018293d82b5814bf6c7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:33 +0000

    goback(to) comeoff fear half possibly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
