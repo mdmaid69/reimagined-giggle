e920f5ef2440dee5812426b7919d2c61db81303a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:34:40 +0000

    disagree drop score list explanation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
