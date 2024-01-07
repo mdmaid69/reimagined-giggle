5d655acb23d50b5fa3463db02bccdbd9dc553e52
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:07 +0000

    except private habit clothes afternoon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sys
-def exit_program():
-        sys.exit()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
