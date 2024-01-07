fe66471eb1aaab51c7d12508f96ebbedac26bbda
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:09 +0000

    please disturb centre must adjective

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
