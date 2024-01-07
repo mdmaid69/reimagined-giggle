6a7dc26a83e068184c951c78c3b63fccf71200ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:05:49 +0000

    bag exhibition produce soil whichever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import re
+print(re.match("h.*o", "hello world"))
