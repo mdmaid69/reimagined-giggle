82b43579d58af9f30b3a56b56e684d8809e719ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:38 +0000

    dead pour right moment workupto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
