017ec7c95abbb5827c1225ed39473485ae1c55e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:10 +0000

    passenger bag tape whisper married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
