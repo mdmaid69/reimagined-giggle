4a1c09ae67ffa49f8127b973c7241a067a68c42c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:12:31 +0000

    closed they recent 179bite meaning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
