0ad04789831eaf0ec9d3490ea66590e08d35eca4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:44:03 +0000

    concerned new cut off sit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
