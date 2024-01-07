7ff8bddae7a691e020d50c2fb0fbad5bf107650d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:52:37 +0000

    clue type track takeaway exercise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
