c8ec9219b4aacdeef8920fc5c5c54f543c765b3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:16:59 +0000

    reach transparent drop honest compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_current_directory():
+        return os.getcwd()
+  def remove_duplicates(lst):
+        return list(set(lst))
