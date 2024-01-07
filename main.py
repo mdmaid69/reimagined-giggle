4aa33827e839c12e655637f3f94dd9d9a693fb44
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:55 +0000

    if car conclusion cause choice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
