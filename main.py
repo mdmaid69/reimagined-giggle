c32ca4ac45db9aea07346e44f34b4f7bd212d610
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:26 +0000

    wild true trap achieve building

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+  def convert_to_octal(n):
+        return oct(n)
