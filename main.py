d8225ea8b5813bf3e42572b4c6c1e474e3fda6b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:52:43 +0000

    like stretch translate float level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
