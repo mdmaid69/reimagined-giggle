340c1b91ecd902fc04dfe208aa41d0251e6ae347
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:37:12 +0000

    that unless turnout nice finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
