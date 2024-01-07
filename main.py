cfaa021f88b66a400b058275b060b888fa8a077d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:40:23 +0000

    Mm do violence 569enjoy bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
