8a8b44603db517d848c33dc35e6db89857a64924
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:58:24 +0000

    attention me snow mixture power

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
