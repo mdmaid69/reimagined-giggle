0012aa110d509aa737c7365a9b72d66a1f5f4601
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:53 +0000

    easy dance culture exciting confuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
