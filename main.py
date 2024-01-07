fab8a71ea9045d8ac94bf820ba4e8886b37435cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:09 +0000

    clothes leg what cheap better

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
