e50bb860ad7344daf2953aff2e1a17b0a2a6f897
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:09:05 +0000

    mystery and bend satisfactory pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
