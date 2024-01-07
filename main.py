c597829809e8067be0d3db657c2801accb08725a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:07 +0000

    hold attention division jump runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
