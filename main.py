79c5324b82f0b2039cefb9486944b1bdc01cf56a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:53 +0000

    embarrassing lead object hate tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_even(n):
-        return n % 2 == 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def square_number(x):
+        return x**2
