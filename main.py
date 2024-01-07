a1270b1b7dc26f87fe1d6a931775c0500e45e9ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:19:26 +0000

    regional remain strike contrast recognize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+print(sum(range(10)))
