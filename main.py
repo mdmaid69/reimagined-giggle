78c82e325afbda3311313f146d6b25d91253ae0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:29:31 +0000

    board later content thirsty our

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
