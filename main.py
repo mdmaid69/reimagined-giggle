8e7c1976ae4cf9aabea571c24dca782e8d3063c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:26 +0000

    below whoever thorough so complain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  def reverse_list(lst):
+        return lst[::-1]
