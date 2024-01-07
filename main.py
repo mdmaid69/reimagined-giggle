250d324d5faafe9b5305da139d4bd7c07265d51c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:19 +0000

    along father possession future protest

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
