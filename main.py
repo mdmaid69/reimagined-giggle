e79f18a44fdca8cedd77541596aecdc1035751f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:46 +0000

    teach sight let wing clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+x = 10
+y = 20
+print("Sum:", x + y)
