09be01bc3a5235a30a07f495bc3cc854fc9d2861
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:10 +0000

    mistake common pick much appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
