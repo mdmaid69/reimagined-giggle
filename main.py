2c33c831fe61b886082e34b9ad885d2f3cd7b281
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:37:51 +0000

    under basis height guess police

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import re
+print(re.match("h.*o", "hello world"))
+def is_even(n):
+        return n % 2 == 0
