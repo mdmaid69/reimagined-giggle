85a9024d7f4c63120dde7f2f339e28a22efdde83
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:03:28 +0000

    sound leg force class rise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
