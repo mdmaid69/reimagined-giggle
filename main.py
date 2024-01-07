408fb0fe3dfda9aa6d9717b190ba8a3ae7d477fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:23:45 +0000

    keepaway runinto urgent example finger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
