d3aa874f78c48deae7a766dad7939517d8d9465f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:25 +0000

    tear content use stay exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
