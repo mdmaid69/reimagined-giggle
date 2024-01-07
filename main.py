fe6ddac6ebcfcf89016b261610684b9c300683f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:20 +0000

    but him flow fire mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
