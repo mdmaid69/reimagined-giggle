d59eb9165bc90683c8d9add89b8ba868f96c2247
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:23 +0000

    niggaacademy bin punishment flat doubt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
