fb7b7dcbacff280d517fdec1208fd0c0e082c6a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:49 +0000

    little runaway comedown forever shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
