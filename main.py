9d04c42893ca6314b147819c489160f74e0d4e0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:56 +0000

    stick battery we walk OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def find_max(lst):
+        return max(lst)
