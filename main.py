01e97a7da81d34d88718c5ce871e2de3c6ee5768
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:44 +0000

    study blind star event occasion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
