bc0d0f2afe850fa58d6fd2c251eb20cd54c8d10a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:01 +0000

    on pure put shape opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
