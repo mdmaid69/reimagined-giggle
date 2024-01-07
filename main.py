ae74a4fe51c4a5952592bfc6be99452a7f908c2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:14 +0000

    improve act closed state anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
