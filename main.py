071755ab0435e51fb82e7cd6e5c8ac6e10449e7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:41 +0000

    manner violence generous keen street

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
