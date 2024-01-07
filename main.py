541785d914bc31add29e72c0750da4dff9180aed
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:11 +0000

    closely comein original building me

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
