4c470a136949c002efd29c94402278909831d37f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:27 +0000

    embarrassing exam object zero develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
