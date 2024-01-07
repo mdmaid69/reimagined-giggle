abd0ab7dc134b3fc4b2cdd5245d52aae6b1593b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:16 +0000

    born open nervous help 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(lst):
+        return max(lst)
