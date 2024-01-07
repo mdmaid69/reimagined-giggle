358a16da426d636671875326cca9f18e7d52de51
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:31 +0000

    pity bus score person official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_array_as_str(array):
+        return str(array)
