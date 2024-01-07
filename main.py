13c97ad486a3381de66da7d4e335b60a303baac2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:06 +0000

    top wherever big runinto godown

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def add_numbers(x, y):
-        return x + y
-def find_unique_words(sentence):
-        return set(sentence.split())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
