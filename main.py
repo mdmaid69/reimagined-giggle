42fb6cf1ba2d6dc7a76aefd844a6d464ba87a500
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:22 +0000

    destroy reduce concentrate minimum least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def is_even(n):
-        return n % 2 == 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
