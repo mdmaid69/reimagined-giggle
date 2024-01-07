655c5d1e6bcb685e980bae073b71b7c58bcb890b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:36 +0000

    Oo floor busy rule angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
