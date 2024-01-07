36b58a8197595f239e78a0b19b778bb2f78ff848
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:20 +0000

    street climb minority flow Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+print(json.dumps({"name": "John", "age": 30}))
