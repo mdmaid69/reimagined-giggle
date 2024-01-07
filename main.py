f97869a069e1c9ad9fc4fa69f55f83cf9f20aa38
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:08 +0000

    another print dig only quiet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
