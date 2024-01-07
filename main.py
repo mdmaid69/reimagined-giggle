b7f5f9fbec7352b833c79ba42bc065ab540b968d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:20:11 +0000

    none dinner sure truth surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
