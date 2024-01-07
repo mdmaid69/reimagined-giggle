b338318c1d4a9aeaf215d192dd26234b010d824e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:26 +0000

    follow office leave organization beautiful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def convert_to_binary(n):
+        return bin(n)
