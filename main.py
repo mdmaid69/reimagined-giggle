5825429ed4839590a88146c31501b2d4c18e9fb9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:13 +0000

    sound what combination wire skill

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
