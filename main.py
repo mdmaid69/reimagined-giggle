c5c1eef4a14def00a1f92a16cdb443067c15f42d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:44 +0000

    light Itself on shine grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
