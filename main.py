b4c91bd8c9a1fdac94d9a6feb7b5f9807836816f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:01 +0000

    coat black workupto instead join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
