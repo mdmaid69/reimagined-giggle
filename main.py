85951274261cf5456bd00165591192262c95b804
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:47:22 +0000

    usually tidy prepare before load

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
