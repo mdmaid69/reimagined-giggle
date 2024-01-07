2cfee1cac9b333cb30f9adf59cf853f1deb1c488
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:16 +0000

    everybody discovery taste impression sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_min(lst):
+        return min(lst)
