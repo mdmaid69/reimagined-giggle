1e55c03c6cdfe3fa22f59a0deafd9ac0f56950fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:15 +0000

    card rush bored close excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
