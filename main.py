fda38b0761f636a6045ea49360db9af1da5fa7ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:46 +0000

    not damage section continue community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_min(numbers):
-        return min(numbers)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
