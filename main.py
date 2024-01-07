6edd98e172bdc6ea295203617120fe12cc558ec7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:12 +0000

    into earn criticism adjective feel

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
