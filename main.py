e4499a6124d4d2ffcedd90488a0735cec3190df2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:01:00 +0000

    party bang unlikely play fork

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
