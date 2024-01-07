85e13bb2ca7f417cc0c0d84fa8b96fd89f55a752
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:55 +0000

    willing fall course deliberate deep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
