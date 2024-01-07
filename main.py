8fb46c3844e882ed5815f6e8b9a596f88544d65f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:28 +0000

    surprise plant bringforward danger hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
