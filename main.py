8f133081121c9ada10e541cd395b4e72600f4dee
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:40 +0000

    pencil reply right round about

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
