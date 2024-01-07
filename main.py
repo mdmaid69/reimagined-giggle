5f53304ace9ac3e2a47569612df717acc4f2b509
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:12:34 +0000

    task cough trip reserve acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
