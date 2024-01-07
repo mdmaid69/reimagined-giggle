0b305495e4cb21556c755ba771f37449d97b6367
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:54:05 +0000

    impressed closely injury fierce minor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def multiply_numbers(x, y):
+        return x * y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
