07ef3a679c186037d6915c1b20f070f5709e642d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:33:45 +0000

    delicate count exact exciting bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import time
-def get_time_since_epoch():
-        return time.time()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
