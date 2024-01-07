7ebc03d4a0ad2215e67468a390580db42c958a18
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:36 +0000

    organization record impressed similar aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
