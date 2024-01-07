6aa3057e5a0e8168576dc802d699edeff0cbd450
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:15:44 +0000

    question bringback result raise badly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def check_if_array_contains_item(array, item):
+        return item in array
