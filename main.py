4987ad55cb526c9b3332650abbd218e5acd3ce4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:00 +0000

    wave teach confident telephone over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
