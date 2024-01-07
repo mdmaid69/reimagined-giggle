46dab863c241970d3142a2673843e547897abb99
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:02 +0000

    rare boat but shock shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
