964c82a42d2d7993ca4214c04529ac61ba7ed843
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:37 +0000

    present positive group custom pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
