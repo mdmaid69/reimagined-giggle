2edad9f131287556e9c4ea696d43647abd0c9724
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:59 +0000

    congratulate whenever unable claim shoot

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
