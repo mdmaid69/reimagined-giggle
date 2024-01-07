c76af1416af30b7fc67247629ab7249b7d114c3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:39 +0000

    forget become whoever instrument like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
