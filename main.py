d7f453a226f839d99133bc98f872651ac7c7e024
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:21 +0000

    share gift war switch quiet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_min(numbers):
+        return min(numbers)
