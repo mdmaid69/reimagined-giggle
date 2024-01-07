e3ca72743f9e622c7c0f99fdf819ea704046f5bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:35:44 +0000

    tooth friend block tidy sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
