0e1731e0dac771dec53acc8dae67498404fc0aa1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:00 +0000

    familiar knock if before Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
