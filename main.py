ec2a4aa573b1729b75ebc78d0ab607ffb734ea70
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:00:11 +0000

    responsible attractive limit bury which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-print(sum(range(10)))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
