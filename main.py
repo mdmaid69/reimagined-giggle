fc04462b9a05d7742ee304b004116d0c91f47c15
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:53:09 +0000

    crowded where live upstairs artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
