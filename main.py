461ff8945c0b8c900b7786af772f06841d94f5cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:07:08 +0000

    anxious spill shut lend listenfor

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def is_palindrome(s):
+        return s == s[::-1]
