5eee9143c5187a92aeed64aae2db395c2b14d355
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:16:21 +0000

    memory callout cross let might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
