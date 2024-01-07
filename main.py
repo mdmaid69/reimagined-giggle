cff9d5757b88b7b522d7e1fd7f684b00c8fb707a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:17 +0000

    once friendly without stretch communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
