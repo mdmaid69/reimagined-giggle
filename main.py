996526a4f51b0ad68ec8b92a414b44e758c226c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:56:16 +0000

    date comfortable curious less difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
