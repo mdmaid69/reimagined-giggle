3d6025b98a7e20bfb4228809cee48301817ba358
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:07 +0000

    street shade slip instrument confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import sys
+print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
