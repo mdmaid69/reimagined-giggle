5d02ef65562d82fb5f4b5a7344239b6793733f24
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:40 +0000

    affect comeoff doctor goback(to) left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
