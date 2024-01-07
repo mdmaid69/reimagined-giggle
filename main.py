0c14163008e1bde10fac30b4c289c641637b94a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:00:05 +0000

    change cook lookaround also identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
