0e220011f640ab01cd31b85bfed840dd634162f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:42 +0000

    fairly it test correct Ss

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
