d2668d77a638061e3576ad08a263d688ad72f636
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:38:28 +0000

    seem back ball object rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import sys
+def exit_program():
+        sys.exit()
