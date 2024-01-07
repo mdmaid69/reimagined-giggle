aad662375aa63e2c81d2ef2aad7a9e4c3ab1a588
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:49:45 +0000

    train wheel hope plate short

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-print(math.pi)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def exit_program():
+        sys.exit()
