1f129f2f36d256858dae7021d20e357fb876b9cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:58 +0000

    pretty through suggest opinion both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
