fcd371eb762f20377a85338b9ae9dd433866c1ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:31 +0000

    switch scene border system noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
