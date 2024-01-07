79a0e8a939450fde0568d9e6d64db8bc6b20afb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:35 +0000

    healthy neither old mistake light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import os
+print(os.getcwd())
