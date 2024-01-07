264a8ea7c8306ca0c760c3139da3f94aabdaa28d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:51 +0000

    bite official combination 572enter godown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
