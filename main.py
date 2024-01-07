c3a3d7417db8d6497b6d276ce26fe591aec10653
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:37:58 +0000

    tip off Mm cut fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
