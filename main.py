d3b7e4df9e7be2915bf0b57f6e0e425b2d14bd80
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:22 +0000

    collect otherwise male pay pretty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
