0559a7e2fcc9583633d5cb9564488865de68bce7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:36 +0000

    official fall mind never but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
