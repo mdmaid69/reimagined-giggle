48666b6d03c11d70b41345fc401242f88472837f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:21 +0000

    unkind act increase sister fold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
