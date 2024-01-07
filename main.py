72e888747d0f29ed70fc2f05054f3fb599c8b4c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:41:01 +0000

    target trick turnout set yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
