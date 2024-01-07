a93f66977720d5e53c0099f1186b13f2349d5021
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:22 +0000

    thing reach discussion underneath bored

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
