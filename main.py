ab558f7a49a593d66c3583cb32ec3030f19c910d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:44:09 +0000

    holdon thanks callup stop place

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
