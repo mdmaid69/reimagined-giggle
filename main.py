b7702ea39d08b397bbe788b41d4d0b6b4c934ffd
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:30 +0000

    wild chance whether speed book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def reverse_string(s):
-        return s[::-1]
+  def split_path(path):
+        return os.path.split(path)
