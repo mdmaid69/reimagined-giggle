3cf011bc4180bbbf044648028c3d0a82e00128f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:21 +0000

    married determined spin spite important

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
