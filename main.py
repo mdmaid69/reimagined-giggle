40702a0bb2ce3b05c86aa9e1c6b8686438000827
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:15 +0000

    harm callat character at fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
