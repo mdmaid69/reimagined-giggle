2f396053b13453bc57e70c037f12c68b145465c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:10:11 +0000

    weekend finally right exactly aim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
