4463e145aef1352db8e9908d149fb4e378655746
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:37:40 +0000

    shame free tap seem party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
+import tensorflow as tf
+print(tf.__version__)
