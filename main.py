ea0a30a4b62472fe7095602dc086ab038685ec2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:41:42 +0000

    wrong complain guide acceptable forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
