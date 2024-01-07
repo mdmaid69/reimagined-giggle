e2eba8dc39f77d2838d49f48800320846411e4cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:56 +0000

    join workon operation tap target

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def convert_to_octal(n):
+        return oct(n)
+def convert_to_binary(n):
+        return bin(n)
