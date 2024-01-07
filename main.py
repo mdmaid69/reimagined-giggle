dff66c0d05f702d70851b2cc00e797b222926fdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:11:28 +0000

    zero switch result getup exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_str(array):
+        return str(array)
