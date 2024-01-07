a8bbe8818c83f5724f7b2177e8b14fa7b31d3d29
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:14 +0000

    spend no sure raw memory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
