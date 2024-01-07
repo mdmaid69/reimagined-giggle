13d74369bd72ec29678e57afeb2f80894a042d43
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:53:22 +0000

    talkback lie beginning ground excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_set(array):
+        return set(array)
+import getpass
+def get_username():
+        return getpass.getuser()
