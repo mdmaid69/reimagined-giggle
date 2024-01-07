23ead1a0a4df8a9fac4c7bf151fa5def9c487074
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:29 +0000

    work petrol fasten whisper turnout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
