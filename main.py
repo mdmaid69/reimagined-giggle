8ea83467f8f3ee7cc02444214fb937bf35fcd3da
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:02:34 +0000

    pollution mistake shoot school beautiful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def reverse_array(array):
-        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
