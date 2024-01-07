643c76b37c0ada073e2b68b07342466f6ce2c78d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:08:33 +0000

    travel tooth extraordinary 564encourage remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
