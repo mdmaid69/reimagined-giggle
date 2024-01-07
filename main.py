e8e6c78136ff864425ac662c7501502af4060e10
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:16 +0000

    line copy general both on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
