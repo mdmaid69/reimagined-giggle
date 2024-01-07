4adbffc07d87b1822e66a96445fa962b7ba63187
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:05:00 +0000

    university suggest absolutely memory recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
