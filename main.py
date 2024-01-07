6c6c4b0b94dd1fd0849ddc844c1f26f91c4ff892
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:16 +0000

    regularly interest opportunity new dinner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
