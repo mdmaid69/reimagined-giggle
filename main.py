6c41bb9c810c2bef1b29e39315df8d3cead5dd2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:17:35 +0000

    pool birthday suggestion cut though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
