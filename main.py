e5305eaca03a21c56b921f771df97377a4bf85f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:30:31 +0000

    bottom interested scheme Itself difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
