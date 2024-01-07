67a9c4ec006426300fd557e429307f2cd45cd40b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:14 +0000

    challenge beginning top mistake disapprove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
