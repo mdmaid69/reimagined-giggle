4624592716d9daa274400d717627516044290442
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:58:21 +0000

    left attract tune stretch himself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
