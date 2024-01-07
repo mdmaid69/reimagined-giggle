c6634f2a91b12939e5da41f0d878a5233f801dcb
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:19 +0000

    what crowded warn suck begin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
