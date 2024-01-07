83b2e7ac4d3f3aa8720f3e6fd0bb03df32659d3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:40:27 +0000

    card development praise demand settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
