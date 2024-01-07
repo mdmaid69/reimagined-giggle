a7eae33d8642ba240d549fb370782d92d476f352
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:46 +0000

    perhaps few young come Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
