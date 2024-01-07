006858b6e960f5f863098eb95d21a1cd5b956c51
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:15:11 +0000

    together trouble carry tongue freeze

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+print([x**2 for x in range(10)])
