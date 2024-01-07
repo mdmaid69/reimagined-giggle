12296428fab95c4b4d297c9f1875f09fc7470106
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:47:37 +0000

    eat shout think flight count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
