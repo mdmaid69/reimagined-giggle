5e69e6dc6f6a4d06f357973e5220d534804845bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:37 +0000

    somebody feeling through creature train

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
