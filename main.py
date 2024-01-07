9ed8160975434776810afccacab8a1a98bd48b95
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:15 +0000

    period proof tough congratulations side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
