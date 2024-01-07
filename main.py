1d7e11ced761bc7fd9d87e83c4390f43e0b20e06
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:45 +0000

    prefer risk off among absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
