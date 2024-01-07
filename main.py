027954c093179a60f19c41c358322ab77e52aa71
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:31 +0000

    out face declaration heart standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def sort_list(lst):
-        return sorted(lst)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
