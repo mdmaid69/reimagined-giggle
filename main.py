834459662791a42d669dae10fb94a7f00fe36447
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:28 +0000

    raw arrange tip event evening

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
