4d2fe5c0fd59d52c11c4790296701063df581ebb
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:17 +0000

    gather distant creature soak charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
