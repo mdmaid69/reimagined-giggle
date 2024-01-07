22504d29cbcb619f95477976d7814e9915c9fb12
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:07 +0000

    escape quantity sorry minor callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
