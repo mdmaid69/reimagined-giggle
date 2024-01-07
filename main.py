a80f17cc7a270fcac2e8e0b673aa701ea8c9efba
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:01 +0000

    last leave comein company test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
