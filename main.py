3e0dcecba144169cfdd52da01e4394d1315f75be
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:17 +0000

    pair spread dick turninto dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def reverse_array(array):
+        array.reverse()
+def is_palindrome(s):
+        return s == s[::-1]
