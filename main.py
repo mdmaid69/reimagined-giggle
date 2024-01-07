d750b898f623cc08d586643c0696c755b103383e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:37:32 +0000

    gift pile contribute nerve further

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
