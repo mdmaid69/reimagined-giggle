b295c0da1994a9856de0bbf4a9e31a135c0ad2b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:43:30 +0000

    each expect thinkof shake move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def cube_number(x):
-        return x**3
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
