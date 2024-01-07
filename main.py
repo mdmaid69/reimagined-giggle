d0d1bf01ba80d9a934d8a601f1e549b1e55cda81
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:17 +0000

    height short narrow division runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
