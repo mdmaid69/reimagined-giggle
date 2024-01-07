47b493a008dc8a449120c3e285a75850101d9c71
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:06 +0000

    exit bush shut badly wipe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
