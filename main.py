ae0c0ad1039e3f94200b12c5ed362997a34ae1b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:36 +0000

    always spray that ever drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
