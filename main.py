acaa57e4aaf70c89815ad0c561394b916a4435e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:22 +0000

    funny skill happy available shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
