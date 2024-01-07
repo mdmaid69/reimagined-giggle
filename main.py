6287ad1fedcab92d21bda8b93ebe8905091c0db8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:06 +0000

    price succeed in own future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
