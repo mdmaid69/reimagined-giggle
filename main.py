0bc06be532e4471d4ea2a7201eb2ec42d2a50b6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:57:10 +0000

    communicate grow remember train corner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
