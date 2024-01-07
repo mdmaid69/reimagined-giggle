6968e01de1e7342c3eb1ed328433a1b56e2b08f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:26:16 +0000

    outside honest another direction confuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
