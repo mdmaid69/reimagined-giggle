58a212d4201b4303630f076eb55f71894c12576e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:10:50 +0000

    maybe sea present attitude bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
