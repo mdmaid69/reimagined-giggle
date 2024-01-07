9cb83f7e31dc8f3f415ddeff68ae8eb9396f0186
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:30:11 +0000

    course extraordinary lock honest worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
