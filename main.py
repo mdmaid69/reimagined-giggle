548fcb7f3bf7bbc7f3f57c8609bd50e5eb680479
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:14 +0000

    worth know fix view rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
