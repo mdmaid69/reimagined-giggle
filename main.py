2cdee89cd06085340c590560417d2fefd026fbf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:58:18 +0000

    deliver seem talkback shall suggest

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
