a28a59c56e70e2c76d4257d8c572767351b93cc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:09:43 +0000

    scrap down report in detail

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
