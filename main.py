1529db7a389b4749cc8204cfbdddcb7bc01c7bcf
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:42 +0000

    contact wild serve sell tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
