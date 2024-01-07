d00fe543259f242dc21e7040dc506ccc946ab261
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:11 +0000

    indeed relation before breakin mean

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
