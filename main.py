0785486f34cdd2112a7dcb5ec32ee3560ebaaf95
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:14 +0000

    ride violence walk stare pronounce

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_acceleration(speed, time):
-        return speed / time
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
