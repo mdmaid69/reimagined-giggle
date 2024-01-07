7e579b43e73d049af924db041d2f33b696b1956a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:02 +0000

    as long shallow knowledge free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
