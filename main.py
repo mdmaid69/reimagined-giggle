1cb3180b8846b3b7cbdb2ee07e83525b312d9776
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:25 +0000

    guide face card mail perform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
