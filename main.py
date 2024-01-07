5a76340da28b0bc4cc9c048b0ed7485bb9e1b877
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:26 +0000

    equal high respect pinch identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def square_number(x):
-        return x**2
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
