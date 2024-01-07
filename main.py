495073d6c369170aa4fd13d1b65b2d16ab9a2c7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:17 +0000

    pure comein bottom Oo dangerous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
