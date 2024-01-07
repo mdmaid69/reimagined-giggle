6b0dfcb9f871a46db026e3cef120bca1907ab787
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:16 +0000

    somebody fly beat wish downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
