edb1057ee1302ef6c4bca77cda034a48f3a99d06
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:38:32 +0000

    give occasion Jj year goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
