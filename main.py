d0bc9ff6bae43284a217c32bb5581452f9db4a36
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:59 +0000

    week scare issue punishment takeover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def remove_duplicates(lst):
+        return list(set(lst))
