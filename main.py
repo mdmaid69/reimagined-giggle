c59c8d50f4bf4109298194670b76b3dcd33a03b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:30:26 +0000

    perfect patient joke moral test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def get_array_as_int(array):
+        return int(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
