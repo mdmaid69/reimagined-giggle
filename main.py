769558dc096423e0ac5882a281df146568037b4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:12:16 +0000

    everyone preparation brush where quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
