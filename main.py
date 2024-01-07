bc93af0b07ee91d77a9aece998690e95ddbcea41
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:06:30 +0000

    will add score whoever under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
