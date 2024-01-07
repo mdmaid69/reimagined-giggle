394c5d40b1e8937355b8e33e1aa00c8f40e9300b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:53:21 +0000

    party fun crisis success holdup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
