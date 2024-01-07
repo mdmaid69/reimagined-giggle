70681ded0dc7b77c89e71726f961f92c98cd6954
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:07:20 +0000

    weigh tomorrow old plan wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
