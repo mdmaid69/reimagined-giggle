d84a5dd728775fde0d7095e4d8040957c1a862ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:35:23 +0000

    cool black go contact describe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
+import array
+def get_array_as_set(array):
+        return set(array)
