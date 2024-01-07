55fd5d28c6ca330305893328884a5feb7f1a6afd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:46:03 +0000

    circle mine closed interview trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
