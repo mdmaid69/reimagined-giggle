ce4f1d3a782d9ac1b5e1a34f5c367f22361087b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:09:14 +0000

    land several Pp confident birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
