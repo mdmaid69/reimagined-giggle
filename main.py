ea27df17a7b573218a5493f4841164707ba8c8f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:11:03 +0000

    pair refer top apart account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
