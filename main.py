f51f4ffab5a6c3d449cf5c8a2d5200187e2d9fc0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:36 +0000

    disturbing blue disturbing weather connect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
