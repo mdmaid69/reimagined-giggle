ed6ca951a8b229455561220ea786565cf1ddd11c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:58 +0000

    loan sound wind just valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
