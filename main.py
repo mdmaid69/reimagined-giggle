a3980de5e82da3eea4944312c280e2b77123cd84
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:30 +0000

    holdon ago boat children crazy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import datetime
-print(datetime.datetime.now())
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def divide_numbers(x, y):
+        return x / y
