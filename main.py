cfef825a6998e7fe70d305bb79fe82f05ae2deed
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:39:29 +0000

    sound confusing ought strange plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
