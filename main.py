e3d0c7ec99a41aa074376ab8532164c2dc297957
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:45 +0000

    Dd boat century polite my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
