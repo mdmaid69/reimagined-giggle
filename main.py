ec1de11673f4ff03e55066b9e76e37bd26cab7cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:20 +0000

    substance Ww allow mean total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
