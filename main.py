9c02d9ad04786ab13514284485303fc1774f13d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:01 +0000

    so swimming pain physical precise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
 import array
-def get_array_as_list(array):
-        return list(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
