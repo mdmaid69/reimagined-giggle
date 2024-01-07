7eee0dd73bf5a38aee2c70b8cfe827a1f664a671
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:24 +0000

    moral pure negative influence argument

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
