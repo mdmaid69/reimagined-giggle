d79c959da97634344fefb6422bf1596ad98c1633
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:21:08 +0000

    minimum hungry creature interrupt 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
