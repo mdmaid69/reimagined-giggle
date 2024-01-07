5abd4e430f4ba414c9d9feceb65a2462edd73ef8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:21:04 +0000

    either rent nearly up bringup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
