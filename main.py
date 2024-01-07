0df419f4205c7f316d2472ae33fa7db5dec82498
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:43:32 +0000

    appeal choose attitude upset defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
