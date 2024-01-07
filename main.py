a7aef8a6d37cf6c77de098a6db9af7fe5859a40b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:38 +0000

    criticize splash murder computer back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
