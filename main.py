e80dec682ddff7ef2de749c39e62fcea37d79386
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:13 +0000

    gift turndown rush buy progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_area_circle(r):
+        return 3.14 * r**2
