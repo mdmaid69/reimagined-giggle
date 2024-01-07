46314f56217413527f37eb61ac9cfd53cea5acb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:26:59 +0000

    basic old compare course opportunity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+print([x**2 for x in range(10)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
