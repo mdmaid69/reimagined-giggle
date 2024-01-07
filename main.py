870ed194ebfe545e07e616458f5856c028417c1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:51:35 +0000

    contents finish sort on normal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
