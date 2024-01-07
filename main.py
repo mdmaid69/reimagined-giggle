dc43701966344ff93142daa240f46abd500c727b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:44 +0000

    Mm copy around willing curious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
