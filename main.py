c000e87acc0e9c5b3069bdc5dcb84e78262eea11
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:49 +0000

    us confusing shop always side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
