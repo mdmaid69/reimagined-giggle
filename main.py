986d4a2bb53111d2781076c3ec200a84ca612cd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:02 +0000

    accident bad print talk equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
