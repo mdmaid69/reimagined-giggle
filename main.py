ca8ac151d6f0a6ef5ea43f0254d919f2b2a6731f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:05:08 +0000

    bush left along instant national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
