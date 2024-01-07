4a2e7b997460434eb0ed9a6467870f98c3627016
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:27 +0000

    content command single trust face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
