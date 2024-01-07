a768f00f397d07dd1b422b7adf161a3f9eeda907
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:35 +0000

    swim injury taste water wait

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def append_to_array(array, item):
+        array.append(item)
