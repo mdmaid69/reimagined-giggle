56ac51a1725bbf8556830cb936fc04f525455c01
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:55:25 +0000

    ability always claim lunch refuse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_power(work, time):
+        return work / time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
