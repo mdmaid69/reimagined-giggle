4805f48f431809c656fa3957b930ef6a76b69481
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:36:57 +0000

    through wide name relationship important

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def sort_numbers(numbers):
-        return sorted(numbers)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
