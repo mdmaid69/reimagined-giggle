42b721991d7822c4e89675648634cc2e4afb32aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:36:58 +0000

    against handle pull mark claim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
