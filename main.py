c840d407fe0a3c4d504799233e2bac4939a265c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:27 +0000

    tall to taste corner welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
