0f10f790ede115d74d4ef104fb044e60c13cffe1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:03:46 +0000

    juice later intelligent limit pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
