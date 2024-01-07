0fe049782ea221c51e7f70f4d38a073d74cb1b9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:32:47 +0000

    inform treat consult count equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
