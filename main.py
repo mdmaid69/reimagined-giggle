8118535c2843e64430f9dd3e85de130aec86b4fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:42 +0000

    somehow funny intelligent survive listen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_str(array):
+        return str(array)
