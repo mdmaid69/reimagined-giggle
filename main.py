85a5e37ef1e6a55479f43bb7ce22e38aa6cb7351
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:25:19 +0000

    land exploration give famous wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
