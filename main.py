c615937d099788f792224845437fcaddd219fe38
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:08 +0000

    broad himself generally through birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_file(file_name):
+        os.remove(file_name)
