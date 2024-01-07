ec465f48ca7ca4f52ca0cc333ca238d94fb33a8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:30 +0000

    by married Nn any near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_floor(x):
+        return math.floor(x)
