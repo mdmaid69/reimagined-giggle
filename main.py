8897d9beb4e14a5092eca61d59e1d75eed99c97a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:32 +0000

    declaration peaceful beat trade goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
