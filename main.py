60c333b1e73359fda3ac1a11d739937a6c1dc71b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:28:34 +0000

    disappear onto water try Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_length(array):
+        return len(array)
