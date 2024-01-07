7b9de013a8986ccffbbe7ff034dbb1680953eb40
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:04 +0000

    knowledge centimetre geton quantity brown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def append_to_array(array, item):
+        array.append(item)
