aa72ad06c2bdeb3e0cf01043844ba6a3d2364da4
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:10:52 +0000

    than callout whom minute rule

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
