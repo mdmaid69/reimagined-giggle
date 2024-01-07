2c78a64624e597f1cc5b785128533d4375ae507f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:27 +0000

    tie believe no back Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
