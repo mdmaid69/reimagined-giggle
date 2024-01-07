e20c03f610748c8471550b6367c7cfe34079b663
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:32:00 +0000

    second payment show nonsense check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
