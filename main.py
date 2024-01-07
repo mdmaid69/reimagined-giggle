e79e45523f5ef749423561ee588053d824402f6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:09:27 +0000

    soft letter matter means contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
