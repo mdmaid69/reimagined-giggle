e6200cb94a78ef433d16d73d2dc9465a3b96fcf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:59 +0000

    fortunately good weekend least promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
