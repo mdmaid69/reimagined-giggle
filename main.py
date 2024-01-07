a394441aa12f25eb33fa5d76b59abd7ca37f23ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:33 +0000

    factory past surprised scrap emphasis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def find_max(numbers):
-        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
