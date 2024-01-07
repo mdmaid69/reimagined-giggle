2fa013de5443217ef655eb20edb106d2483f220e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:10 +0000

    wild tired bound Itself opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_volume(length, width, height):
+        return length * width * height
