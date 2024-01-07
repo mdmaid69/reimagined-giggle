de7a14a2870974445f35b6bd312795d2a94207d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:20 +0000

    work ever our friend Vv

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item(array, i):
+        return array[i]
