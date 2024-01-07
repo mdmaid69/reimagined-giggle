519bb90c35ad319ac42277afcd5577d74d87e131
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:10 +0000

    legal reserve 566end tense official

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_min(numbers):
+        return min(numbers)
