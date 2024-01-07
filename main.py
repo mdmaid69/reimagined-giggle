cc66593a4155526cd46315019e322fb72bde43a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:15:53 +0000

    likely forward further behave hit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
