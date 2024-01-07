5abbe2f041ba2985b3e4ab22a845bf243752a512
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:31:26 +0000

    school way within confusing rare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
