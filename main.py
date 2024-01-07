7583c8541c787ed9d5ac2a59ba4b5c5a45883f6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:25:19 +0000

    plain damage winter pick congratulations

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_power(work, time):
+        return work / time
+import re
+print(re.match("h.*o", "hello world"))
