4608814023bb0eb6087c52f1ef0bbf8070867a7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:07 +0000

    let exhibition refuse importance attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
