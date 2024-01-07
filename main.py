11746b657e9af7a9c973bfa54e5ccdbea6712408
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:45 +0000

    switch terrible expect it consist

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
