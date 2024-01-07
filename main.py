5d088dcbccc55cabad88e51797f1bf7fdc0e7b6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:36 +0000

    copy everywhere something city occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
