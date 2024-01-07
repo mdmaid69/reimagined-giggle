8231b8127a58ce046167b069b37bcc11bd9a6332
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:44 +0000

    need up machine how keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
