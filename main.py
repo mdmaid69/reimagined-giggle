49be20017af445165cf9c219e1a58a87d9d7cbac
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:50 +0000

    performance interview switch market young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.ctime()
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
