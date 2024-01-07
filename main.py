ae1eff720b3cc1af5643eb67eba00d6b05f71939
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:50 +0000

    human 565end lesson comein delight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
