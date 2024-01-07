de62f4cbdb2ebdf73dd15b360b55fe5ab23576c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:58 +0000

    coffee under future round mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
