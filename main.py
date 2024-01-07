1234b450c4865ff75ed683fe2ca90d282038b499
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:02 +0000

    571enough practical proud subject turnaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
