dc623e7af727853ad2bd9007b1f9589b428e629c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:25:36 +0000

    tall ability dream straight myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
