2edb7d4c11a6abac66c84c759b3578dfa707beae
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:19:27 +0000

    obtain cap accept Ee finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
