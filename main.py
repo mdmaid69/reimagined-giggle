512ef4545ac314d3a125e8bc4264b29595819c69
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:59:33 +0000

    town chin hard future win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-def exit_program():
-        sys.exit()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def print_python_version():
+        return sys.version
+def reverse_list(lst):
+        return lst[::-1]
