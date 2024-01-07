8dc8e035b2d58a49056b6565cf37f62858951fc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:29 +0000

    second workout choice copy always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
