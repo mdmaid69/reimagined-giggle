2586847678d74e8ff9b88740006be64c8a462db9
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:48:45 +0000

    float use music shall challenge

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
