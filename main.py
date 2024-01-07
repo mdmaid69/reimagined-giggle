e55548eea6f24fcad78be78363792fefe2461ae9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:06 +0000

    birth complete taste even claim

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print([x**2 for x in range(10)])
