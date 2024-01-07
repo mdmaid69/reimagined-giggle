4f88fb4cb94055e50b784bb9bb9171f769028865
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:04 +0000

    keen wrap greeting try few

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_bool(array):
+        return bool(array)
