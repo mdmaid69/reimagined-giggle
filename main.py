7cb046705021f24bfd1e59ec352e5221908e7d59
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:32 +0000

    shopping importance violent loose impressive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
