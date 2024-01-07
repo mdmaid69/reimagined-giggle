5704d7a40850bf3e68677b955559b5238e833a09
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:39:26 +0000

    could do office deep noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_as_frozenset(array):
+        return frozenset(array)
