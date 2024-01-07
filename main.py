1446ee72172cd4a0f5ddbd50ce040760bea05220
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:21:34 +0000

    thing absolutely meet experiment dream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
