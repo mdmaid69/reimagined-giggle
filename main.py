4add4373ba7202e83bce4695bcd5004f73824543
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:14 +0000

    just realize side period order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
