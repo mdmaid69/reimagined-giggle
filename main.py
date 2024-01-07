90b5a468aef01d38743cf0ea9533217b07360f0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:15:33 +0000

    unkind attempt keepoff cut comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
