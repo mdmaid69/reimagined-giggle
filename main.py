ae78778a9f4875669a09ef8926f2454adc2de69f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:13 +0000

    turnout club greeting idea cost

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_int(array):
+        return int(array[0])
