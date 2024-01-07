f84a8ec578c23fa03f547490e8c437f67cbd1af0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:23 +0000

    relax have tragedy price surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
