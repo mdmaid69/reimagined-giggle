f52b0bc207e160707ce102fd05f06050aa0718f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:28 +0000

    whatever calloff steep extremely after

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
