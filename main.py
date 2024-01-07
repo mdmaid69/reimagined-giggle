1b85e9ce63f17f239f7746c16fe9f81ac579ca2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:45:01 +0000

    least game read tonight owe

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
