bfaf9bf7f955aab026c5679b5af82ee965fe6f30
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:19:17 +0000

    again cry eager interest shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
