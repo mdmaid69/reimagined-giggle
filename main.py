03e87367dd1d807409d50165f645d13504a2c9a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:29:15 +0000

    turninto generally national broad near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
