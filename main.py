15d3c9392fbcd47a7ae012bcda8cd3369c17ba1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:33 +0000

    meanwhile closely simple border wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
