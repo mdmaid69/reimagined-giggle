9e6a03274746e53fa849080676cf11349604aeb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:02 +0000

    introduction regard reduce telephone press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
