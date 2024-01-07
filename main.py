e13885f58fd11f99a4ac8de236180478e4349e31
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:11 +0000

    urgent its Hh term activity

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+  def convert_to_octal(n):
+        return oct(n)
