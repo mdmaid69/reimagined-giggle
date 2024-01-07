2ddc1804df16934f422f22903a33512367f4f18a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:47 +0000

    rest crisis something murder past

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
