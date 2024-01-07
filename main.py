2f1c01d805c3046cf1b6f1013bd2f492d0757e53
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:23:03 +0000

    evidence runaway burst Ff runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
