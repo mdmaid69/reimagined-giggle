3283a2d49b7f17c1dfdc4ee866723fe4f54c83fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:33 +0000

    wealth off introduce major nonsense

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-print(sum(range(10)))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def count_words(sentence):
+        return len(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
