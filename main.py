74472b9aa7341315918917ac048d6cf85c268cdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:33 +0000

    building effort finally church 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
