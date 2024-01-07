fdfda4219ee982d8a3c3a6e9c4e8f4c7b14e0e5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:03:55 +0000

    cheek pleasant unlucky area pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
