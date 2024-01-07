7043676b8a0ec8ecc6507b4706268a37c7c8657a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:03:06 +0000

    demonstrate appeal backwards tough but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_even(n):
+        return n % 2 == 0
