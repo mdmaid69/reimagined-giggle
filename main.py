85ccf3ec218163d69c06ee5029df3cc1e4c59699
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:27 +0000

    car central satisfied height easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
