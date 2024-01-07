e946149f6d56b2675d1d322804e309693f575ddc
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:35 +0000

    else change urgent 179bite immediate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
