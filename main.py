60a5e7441bd093bff999fdc47bbb7bf421116625
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:22 +0000

    design thought admire operate cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
