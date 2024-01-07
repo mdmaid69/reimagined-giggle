a210983fc77f6c342bb1d18a3d266bd480bcc4c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:15 +0000

    gather Tt his extremely needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
