54467d2916624af0186c001a8832b664d1fce59e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:26:22 +0000

    rare school downstairs polite spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def square_number(x):
+        return x**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
