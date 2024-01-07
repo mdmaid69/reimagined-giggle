b257d5e4ed92cd5b88faefe78c59975adbf3e3f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:05 +0000

    tap pull message reaction maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
