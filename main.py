390b1a1b8df282b7c60ab3cef2ea1cac5134ae45
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:57 +0000

    practical hurry children spare machine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
