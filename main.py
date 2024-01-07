6cecf984bed01b3634470fe00dad27fb98a656df
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:50:14 +0000

    memory plant shoot wrong introduction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
