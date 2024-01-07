187924d41f9ac8f4636da5449f128d7725a3bf62
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:52 +0000

    quality tour form pull culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_base_name(path):
+        return os.path.basename(path)
