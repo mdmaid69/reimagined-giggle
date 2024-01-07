1d14dc0c52ea3efe42041b72861613376045b302
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:10 +0000

    sound plain store tool generous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
