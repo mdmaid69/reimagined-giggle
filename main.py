d32319858c62ee5d30269fe68e242f09a3e567b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:55 +0000

    tall wild often exchange embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
