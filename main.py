28e5901f7be526e7960efe7a740a866b0c4250e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:47 +0000

    plan somebody attention fast month

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
