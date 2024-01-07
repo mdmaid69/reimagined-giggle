587b464011182dbee26199da75d1049a3d8f6c15
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:41 +0000

    complete rarely move rid short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
