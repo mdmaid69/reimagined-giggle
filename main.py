e55946c29709ed3cacdd6922a31e268bf68501bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:11 +0000

    ever lot sticky behave well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_int(array):
+        return int(array[0])
