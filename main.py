fbafa3d47c92db5b0a2d4ee1ef25bc71b8dc7efd
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:49:11 +0000

    early lookafter could criticism complete

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
