b5f6507e0450b370b7ee45ce8d20795c5e8a9f47
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:15:13 +0000

    ever opposite apologizeapologise guess cover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
