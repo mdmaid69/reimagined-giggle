e8ac7bd9c48f66a99b845a24b5bf6641f5f24a20
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:55:06 +0000

    shade rain miss official surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def add_numbers(a, b):
+        return a + b
