eb3a03df688dc26e3aca98b5ff6fd0053d3aee85
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:34 +0000

    definite inquire quickly memory tip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+print(sum(range(10)))
