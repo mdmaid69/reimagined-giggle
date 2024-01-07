38b0a495a8c6881e0051368f99fa11d14eed349d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:30 +0000

    brain bad comment skirt minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
