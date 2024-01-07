70b4656a77ba895f8c6bec8329d13dce4d32e398
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:54 +0000

    base ordinary why pride affect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
