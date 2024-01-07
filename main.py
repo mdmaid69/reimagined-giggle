ef37e3584e1b0c1fb3557864b903cf790eb90a4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:17 +0000

    wide close ago cause into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
