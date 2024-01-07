6a6bd8af6f340a93d75121417eec6730e0d54421
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:46 +0000

    sea actually criticism persuade blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
