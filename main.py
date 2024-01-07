4603bab83bce785b363a96498afd6b433a38330f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:50:16 +0000

    switch point boat custom or

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
