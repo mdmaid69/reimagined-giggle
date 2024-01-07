5b9f06d046e73173871d96bdcd9d6de64c9128d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:40 +0000

    leave innocent before prefer exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
