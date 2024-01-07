375beb667453626e1a299918253ed81294b6a2ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:24 +0000

    eager protect lean dish table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
