6110d0c81f5f303220aa02e37584c7b999d3e74b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:31 +0000

    extra before clever quit step

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_speed(distance, time):
+        return distance / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
