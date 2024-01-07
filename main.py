c8a8517373c3c528559c41535d5618576722e796
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:38 +0000

    tip past willing manage shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_list(array):
-        return list(array)
+  def reverse_list(lst):
+        return lst[::-1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
