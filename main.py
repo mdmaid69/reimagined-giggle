03c312fa26bab2afe2d8430bba817bc955f3c5e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:12 +0000

    proper leave combination arm autumn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
