804f5cae7407ae691beea2c9b92321b3554d82e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:11:40 +0000

    sharp sport get however little

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
+  def convert_to_binary(n):
         return bin(n)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
