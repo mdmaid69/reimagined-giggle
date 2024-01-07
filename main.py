90a530eaf06e65445a40e7ed769796a7270cac08
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:59:38 +0000

    complicated extremely look combination easy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_tangent(x):
+        return math.tan(x)
