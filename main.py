6be40a4d8f57a0674fb572c293ce584457a86ab3
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:11:19 +0000

    hate earth hard drink try

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
