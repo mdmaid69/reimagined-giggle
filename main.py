903e77145d16195750ffa38867e6a260c61a2ab4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:58 +0000

    limit that runout rule passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
