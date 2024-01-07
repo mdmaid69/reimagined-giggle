a3157d39f53cf66850032d884249f20c3451590f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:09 +0000

    makeinto fall improve proud all

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
