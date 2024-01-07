b4a05f6f55c7c15e676939f06b48b949b555796c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:57:45 +0000

    own light towards low boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
