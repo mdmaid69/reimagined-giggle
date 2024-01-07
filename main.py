d0bdadec47714bc766228a78e50b4aed0f2f3356
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:11 +0000

    everything bang hurry free path

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
