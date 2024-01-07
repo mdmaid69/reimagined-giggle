edb0366b5508a59c06adb919470106cbbef2a670
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:11 +0000

    secret car break its smell

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
