3b321ff5ec026bc0daee2cec263795dbeee849e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:01 +0000

    satisfied surprised single rain nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(x, y):
-        return x + y
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
