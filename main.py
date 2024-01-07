a5189503b7ba18b5b81124d31b88638b573007ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:26 +0000

    account reject keepfrom kick back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
