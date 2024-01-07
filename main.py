1eaf56638fc4abbd61c0b7a06e2f773878401316
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:25:48 +0000

    dangerous hard region rush lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def remove_duplicates(lst):
-        return list(set(lst))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
