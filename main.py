550005b3d913446f9c5f57d547148c26c734b6d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:09 +0000

    regular appearance pronounce common just

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
