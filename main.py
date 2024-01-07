356565a076fbc30ae0794846861ae23cf61184f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:04:47 +0000

    cry moment gift prefer small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
