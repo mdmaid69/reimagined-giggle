a07f7d8bf7d6b05b9d297587deae18a038a0b559
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:49 +0000

    comment war rush angry hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
