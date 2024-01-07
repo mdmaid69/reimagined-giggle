38d957b829bd7829556fe52c8d6b7a82cd0c39b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:35 +0000

    fun callout find school treat

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(10): print(i)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
