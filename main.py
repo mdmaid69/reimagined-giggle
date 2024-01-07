3ddd8b1f2d82fe823b45a6eb6cc137e9026c0038
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:27 +0000

    abroad difference dream lookaround anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_floor(x):
+        return math.floor(x)
