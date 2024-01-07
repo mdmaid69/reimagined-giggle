b3360176d059e00bbf80d564d88f329b452fbfa0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:44:28 +0000

    long equal physical sail do

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
