786b41269ffff33df67ecd35e64ab1c19271e583
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:06:18 +0000

    event environment seem air offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-print(datetime.datetime.now())
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
