6566d6dca359820c7bbdcc624107d33d87be7b14
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:13:35 +0000

    rely comein cheat sharp side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_item(array, i):
+        return array[i]
