8eba2547adfbf45bf13cb1a4f180b481280d8813
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:10 +0000

    572enter night related question means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_min(numbers):
+        return min(numbers)
