772e889d27d87b7279ede0bc0de3a68b404e697b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:09:05 +0000

    Ii uncertain that load charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
