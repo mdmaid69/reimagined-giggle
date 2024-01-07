5d6a82ed7eafced3ac49c2d955407b14e2282d4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:18 +0000

    now thick family entrance expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
