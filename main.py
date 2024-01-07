e61a37f6d2d137aefc8084a4e2ee9a54646284d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:44 +0000

    slip flight journey outside pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
