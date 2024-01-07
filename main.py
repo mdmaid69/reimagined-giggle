37707a5dfabf0c13cfcae274496697deb80a6a31
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:39 +0000

    translate close fairly dig interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
