86b9e2d1b1b940aeeda6bd2d88807ba94b95b025
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:41 +0000

    communication negative pull refuse unkind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
