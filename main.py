68141e4b29ec7993083f01b927e12149c71853f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:30 +0000

    wait drawing undo colour calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def clear_array(array):
+        array *= 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
