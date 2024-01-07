44c8867773a51b97259117c308048b8d7b4a4dd3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:32 +0000

    keepoff past other holdout pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
