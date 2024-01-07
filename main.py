17d5a3f0eb94e468185781c82d1937c958e1493e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:00 +0000

    interested precise last another callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
