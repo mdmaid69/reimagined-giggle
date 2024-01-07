77e1c89a9dd390caf52ea2a22169f65cafa01d8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:14:54 +0000

    side community contrast hit thinkover

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_set(array):
+        return set(array)
