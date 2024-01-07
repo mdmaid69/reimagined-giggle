0ff9c37d66c187fc5e430531ddda12d8a2c0358b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:11 +0000

    eye copy smell necessary whoever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
