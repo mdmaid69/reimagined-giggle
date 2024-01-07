c6ceb3c41cf9a03073414916b94e688f5f12bf19
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:46:27 +0000

    scene work classroom indeed mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
