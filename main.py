414f253ed0fcf8a4b1042452d3839fa6fb64dcd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:35 +0000

    correct kick injury deep tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
