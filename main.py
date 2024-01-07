c3e324356f093f8e2364b42d5f52598fa50036d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:03 +0000

    each empty tie scream soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
