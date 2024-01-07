3bf1101f131fe97c4246d9f84c48819b6168d326
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:11:52 +0000

    block experiment genuine worry hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
