3bfebc7be8a8b96e83e10ec4901013d82e286b54
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:19 +0000

    history clear field after division

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_float(array):
+        return float(array[0])
