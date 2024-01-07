0d57ce574b1b187e967c31ff3a0793c62676cda4
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:30 +0000

    sad control maybe relative tonight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
