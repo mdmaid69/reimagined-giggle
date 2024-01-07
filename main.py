758fe565a786c85d43366eb26b58c8cf8c9a7a27
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:11 +0000

    write where exchange unfortunate confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
