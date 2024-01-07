b994b4fa2cdb4e645a7d7661bd012923e95349fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:34 +0000

    glad combination runinto Mm find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
