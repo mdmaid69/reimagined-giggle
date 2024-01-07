b570c482985d1d85f63e2ffbc95169b15d39e116
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:57:21 +0000

    surface Qq couple hurt Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
