85e87e6587d417109de8eb1a8cc9d31e6038ac86
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:33 +0000

    several thing lookup sleep clever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
