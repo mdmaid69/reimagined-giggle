46d88815fbe01ac5cecc861eeabb2f47d73195d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:04 +0000

    speed receive 573entertainment transparent cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
