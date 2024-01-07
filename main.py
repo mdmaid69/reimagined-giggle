d540e3dc6643dd13725029968787788d8914a1e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:44 +0000

    than Cc formal disagree money

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import collections
+def create_user_dict():
+        return collections.UserDict()
