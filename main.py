ade764c3051ec4962e21e008fd3226cff5151c7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:28:07 +0000

    reply stretch treatment worst ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import math
 def calculate_neper_number_to_power_x(x):
         return math.exp(x)
