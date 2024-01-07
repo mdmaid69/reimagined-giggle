6acfa8ac605c28e0f2e2c4d6676a7983412c4112
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:38 +0000

    workupto young interesting steam exit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def count_words(sentence):
+        return len(sentence.split())
+def find_min(lst):
+        return min(lst)
