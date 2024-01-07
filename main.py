f4d23307e17b3cde28b365444aeb8eddf612ff52
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:24:36 +0000

    dress favourite speed Ii path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
