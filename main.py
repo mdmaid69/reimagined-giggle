f5ba9ca5ad886bc4520a623812757a001f3e716c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:36:33 +0000

    comeback neither of turnon each

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+text = "Hello, world!"
+print("Characters:", len(text))
+def find_max(lst):
+        return max(lst)
