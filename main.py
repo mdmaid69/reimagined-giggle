ee8e9e8070353ddf2d82c280007f73c682d4e8bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:18:39 +0000

    wide fetch possibly announce borrow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def convert_to_binary(n):
+        return bin(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
