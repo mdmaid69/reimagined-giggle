b171d5a932978d7b7eb1ee19d208a9defaca1890
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:58:14 +0000

    please doctor match picture finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def cube_number(x):
-        return x**3
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
