eef7f1f52efbb0056bc4de7e2bf1322f55eb07cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:25 +0000

    bet since or sing second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import sys
+def print_python_version():
+        print(sys.version)
+  def remove_duplicates(lst):
+        return list(set(lst))
