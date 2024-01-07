77f6364a2f6f01dcabb2a6b1ac9e6406dbae259c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:52 +0000

    thinkover earn act university black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
