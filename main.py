f7301ea8670b35b82af8bea21e5760b9295a6987
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:28 +0000

    likely smooth ride statement comeover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def convert_to_binary(n):
-        return bin(n)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
