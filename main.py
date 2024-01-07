20287274c8f8ed0ec96e04a589d1c5021b40647d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:09:55 +0000

    bend force floor wave music

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
