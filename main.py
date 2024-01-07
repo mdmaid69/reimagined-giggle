b70a681a85074a4f4693db0044ab60ad136981f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:42 +0000

    appeal whom female turnup sore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_list(array):
+        return list(array)
