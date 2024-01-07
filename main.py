7f32f7560b7edac5476a54c858e639032afe45f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:43:10 +0000

    cost no somehow pollution joke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import time
+print(time.time())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
