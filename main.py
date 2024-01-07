4efa8431567e908c6f9b9f0050a335b539e47dbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:59:00 +0000

    major middle great happy turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
