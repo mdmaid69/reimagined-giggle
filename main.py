9ceb3dd164dec643d6b08daf2d5da347e6d21770
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:37 +0000

    naturally sudden spring office your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
