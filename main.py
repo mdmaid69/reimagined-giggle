140b81bb191dd4368893a4ca20035f77b8a3adf3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:59:29 +0000

    exam image air sure best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
