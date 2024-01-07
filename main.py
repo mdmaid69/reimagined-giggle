07a590b273ad2bfd87c27524de3f8c0218bfb75a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:02:39 +0000

    Ss use Bb while stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def clear_array(array):
+        array *= 0
