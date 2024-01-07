dbd4f80d757893cbec37e2e34189b70c7714733f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:27 +0000

    ignore rich breakdown collection card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
