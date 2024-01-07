add1207f6c5d5a3f8b8f2c6107627f63fa6250a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:53:17 +0000

    possible name remain fast course

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
