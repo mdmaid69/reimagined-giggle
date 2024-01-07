822ab705f5e8a27a9e9f10f6ebb769a9e24fd2f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:53 +0000

    record interview difficult sing identify

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
