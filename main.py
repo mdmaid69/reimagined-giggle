bd08b8c3057ce0f55487f4f2bdc3689b16f28760
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:35:51 +0000

    imagine either Ii suppose by

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
