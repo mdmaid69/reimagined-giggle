fa015940c1121f33e4a5cf10b4a2fac69234743c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:55:57 +0000

    emphasize car peaceful especially shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
