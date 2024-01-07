3c672cb5b97900e365cfcbd8c91ad0c863494e9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:14:57 +0000

    man state reduction nearly means

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print(sum(range(10)))
+  def get_base_name(path):
+        return os.path.basename(path)
