ca6f7ffbe28ef71dd30d41339199cda989e46549
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:03 +0000

    father minority body shine aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
