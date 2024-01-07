591c3b6be1a7846489544016b53fc5c53fabeb35
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:31 +0000

    store distance understand storm getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
