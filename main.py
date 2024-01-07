0b297432b1862ae1a9f9cfc33779714f36149dfb
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:40 +0000

    flat stage week and himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
