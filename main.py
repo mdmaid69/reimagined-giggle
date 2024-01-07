ea3f053ce51f408e80a10feb4b9e51e0a661d3af
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:58:35 +0000

    undo shrink tax 568energy quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import time
+print(time.time())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
