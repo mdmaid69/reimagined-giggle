4f3e1c0ad5946264ad447c11f259005397f224d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:57 +0000

    know wet surface modern side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
