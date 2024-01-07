cc208cdccd389b2f342e8820965b608c8e63d41e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:02:19 +0000

    tip nearly thirsty upwards commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_max(numbers):
+        return max(numbers)
