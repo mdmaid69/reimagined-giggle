ac824a3cebe0bb6a0259c7f4b2e2d8ad269390b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:11 +0000

    part jump alive spare near

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
