47f3579c3bfe162b769ab93d4c39dd24a529ae12
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:30:53 +0000

    entrance check taste beyond pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
