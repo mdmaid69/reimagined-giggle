e760d90b11896cb626e73271f5697a06540af4ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:21:36 +0000

    fairly suggestion act whom entrance

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print("Hello, world!")
