67477b02a0c19c5ebd413b6ff12cae5743a40a60
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:09:43 +0000

    pressure twist formal top push

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
