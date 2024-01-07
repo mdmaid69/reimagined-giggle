b9bbb4f6095c0c54206331541bf6ac92de81fc91
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:42:37 +0000

    forever clean period last trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
