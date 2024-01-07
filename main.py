1e8e53797f3fae0b16bcf825dc63ee148be95848
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:03:18 +0000

    pain under regret waste Ll

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def count_elements(lst):
-        return len(lst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
