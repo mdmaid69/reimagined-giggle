1981cf1fdb10a8af8bf7d6d70afe84cf6ec84b47
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:37:15 +0000

    juice deal me during port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
