ef0bcc4de4da2f8c26d0c57109a1eff1b3e2b2d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:40:51 +0000

    blow press light ocean battery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
