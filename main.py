d415dfa271400432f19524e70bcad714125069dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:30 +0000

    fear after spend thank suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
