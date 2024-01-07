8b9d3c3c36a52f39276d4feefee18630137874f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:19 +0000

    exploration temperature grateful remark gas

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
