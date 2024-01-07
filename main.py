6ebf2d2913ec88f00d019cd5d14bfd3908cab721
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:17 +0000

    mixture square intelligent answer takeaway

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
