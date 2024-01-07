c84235a8e74a4ca6644158d4f82c76f00f7f226c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:46:03 +0000

    pen Zz rescue war uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
