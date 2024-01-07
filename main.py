a757980343840221840646488f07d9d70f37ce5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:38 +0000

    size pressure would flight possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
