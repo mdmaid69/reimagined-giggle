a6e21792000bb975159ee76ce70737349b5fc87e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:40:49 +0000

    complicated satisfactory unusual forget clearly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_set(array):
+        return set(array)
