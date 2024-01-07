0e55379afe34937b52d493215412fbd4e2769359
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:52 +0000

    slow sometimes scream trip challenge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
