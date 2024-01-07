9e329a8acab02ee83e0733b8b80897b6aad88b57
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:59 +0000

    respect certain direct successful commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_item(array, i):
+        return array[i]
