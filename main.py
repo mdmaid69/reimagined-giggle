c5098a0e08b20384343c069d30107fd990c159dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:23 +0000

    among judge change judge dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
