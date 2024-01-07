20aa664475965d29889ff39107a0eb4d8eaa7a2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:29:19 +0000

    fail round late cruelty weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
