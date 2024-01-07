86b92ca36f52766efae1bfce6d4a5783a7b7212f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:45:18 +0000

    comeup street power tough in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
