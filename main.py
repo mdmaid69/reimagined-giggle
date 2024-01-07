094e5671f65e1b0c812f01e991248570b197cd0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:57 +0000

    while attach clear success drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
