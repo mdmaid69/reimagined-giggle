3e8c2ced09f196621010fe8bb2af2cc00090360d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:51:09 +0000

    unit spend born value tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
