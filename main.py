449bc7d3a62ef24e3b15085d565130b8d3ee412e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:24 +0000

    prefer worst nature delicate her

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
