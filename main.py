9f46c73a98684cc879b24e11fe281da168020100
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:25:27 +0000

    mention defend chin exciting wake

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
