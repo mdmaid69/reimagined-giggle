5843e3338c4fb6c572286c98de0eb3ac22afae1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:26 +0000

    man attach conduct keepoff programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_priority_queue():
+        return collections.deque()
