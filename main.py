abe0ce80adef86fffbc5426e9bf6c8cc282b7b74
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:05 +0000

    would everything material tomorrow flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def split_path(path):
+        return os.path.split(path)
