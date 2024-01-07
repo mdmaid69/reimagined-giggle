21981b4a4b30ba66592a4c6d0d871e8c5d5e594b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:13 +0000

    concentrate exploration think workout mouth

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_work(force, distance):
+        return force * distance
