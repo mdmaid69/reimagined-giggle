1e443ed87fb6f433c78e450dd20bf0627304e5e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:46 +0000

    fast survive appear several head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
