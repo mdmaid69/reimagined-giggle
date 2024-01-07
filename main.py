d1534cc7f72eba029eb93acb28d2c1346da668f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:39 +0000

    similar hit sick ilovedicks stranger

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
