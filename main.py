fd081f56531f971d24bda37ca4ca00ad984045ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:33 +0000

    goup OKorokay proud welcome mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
