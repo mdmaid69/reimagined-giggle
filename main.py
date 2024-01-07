2c9cf2a69ec77985e9ee981d0402575ef822a1f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:29:28 +0000

    market string slowly scream hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
