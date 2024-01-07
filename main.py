aba7477c3d2114c200e5f935036015fd6eb778b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:15 +0000

    section lend song lead manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
