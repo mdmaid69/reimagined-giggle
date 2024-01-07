006de69406b62c4995b14c94e9dfc72a5129e9cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:43:29 +0000

    whether on card model bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
