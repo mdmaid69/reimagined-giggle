14cd1e24018767e61592dca77bf65b2654ba8f20
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:43:23 +0000

    survive breakthrough conclusion sure breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
