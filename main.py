a0220ebb91520950e4d28fa9082747c4cc8b6854
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:03:15 +0000

    heavy send point heat dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
