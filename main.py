8f71e773ee2a12d4a5a39a25468043bc33f6d6f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:39 +0000

    with invite trap pipe power

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def cube_number(x):
-        return x**3
+def convert_array_to_list(array):
+        return array.tolist()
