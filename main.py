9ab27cfa88f9ed737d5d22a6140682bd85f6b90f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:37 +0000

    narrow dare squash recovery fast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_pressure(force, area):
+        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
