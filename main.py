60bd35766ddfaee510f02b17ece0cf4f6e5dd86d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:56 +0000

    during before whom embarrassing upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
