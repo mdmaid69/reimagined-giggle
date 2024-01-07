97603f73a73c11d37852521315aa5783fe46f189
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:40 +0000

    correct pain dick chase accept

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+print("Hello, world!")
