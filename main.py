1b8af28d5af125646631df29c11145dbdd0a00e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:57:05 +0000

    still operate wide their cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
