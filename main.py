9556526de0a77cb46ced3f343b80ebd92381a2fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:02:49 +0000

    tragedy may regular form afternoon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import tensorflow as tf
+print(tf.__version__)
