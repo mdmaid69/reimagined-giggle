f99cdb0c0c4f0f085e1e31ea85fe604f2caaa397
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:49 +0000

    frightened ring 567enemy read long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
