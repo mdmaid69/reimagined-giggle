b482f0eb3254b364bb8db751f3f4041d4be408ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:29:41 +0000

    somehow willing accidentally cruel lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import sys
-print(sys.version)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
