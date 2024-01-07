fcdedec32ad54ec3ee200dd27ebc939e4c2d06d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:15:22 +0000

    shop straight passenger risk difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
