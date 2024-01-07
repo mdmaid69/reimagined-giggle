8492dcf9be4c3b5354f7663ecf41652bdfcb26cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:05:19 +0000

    Pp appeal introduction eye blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+def exit_program():
+        sys.exit()
