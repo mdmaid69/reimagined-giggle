2dcd61957489835ee4901267b897a011bb88e3da
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:42 +0000

    Itself unlucky definite change opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
