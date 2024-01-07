2aedbb6cb634eae71a0163c7cd14f794341f113e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:45 +0000

    body operation polite once pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
