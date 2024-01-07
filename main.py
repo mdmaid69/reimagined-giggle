7b15908a711d8fca6b083a66f0b7d1dbc644900c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:11 +0000

    after mad detail off operate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
