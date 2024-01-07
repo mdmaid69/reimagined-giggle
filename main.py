27f4b526f24feb0f125074974d0b3a4e8758d963
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:40:26 +0000

    nervous theirs run last rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
