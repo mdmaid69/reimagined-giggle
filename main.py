43b0f9596d5b013208d3d81fcdc921dce0d2ca13
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:30 +0000

    invitation badly pool chase famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
