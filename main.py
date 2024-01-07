5f2c596029253a6df500f5ea54bbff2fa012fd64
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:13 +0000

    bringdown nearly bear though spend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
