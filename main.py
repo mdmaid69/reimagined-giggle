564198f1cf228bcc187a1eae9ddb055a3081cd63
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:29 +0000

    start decide knock shake then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
