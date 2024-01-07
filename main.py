a8d47d3660c9062e2c486ad904970cd5f03f0532
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:06 +0000

    law flow since soil interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+def calculate_density(mass, volume):
+        return mass / volume
+i = 0
+while i < 5:
+        print(i)
+        i += 1
