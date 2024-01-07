a99faa3f06171614c95d53fe97ff31c1c4cd0a12
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:59 +0000

    debt bit present onto shake

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
