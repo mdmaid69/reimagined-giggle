e1e4baccf5b7c932e7b77134f20006173190be2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:33 +0000

    mistake conclusion issue innocent buy

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+print("Hello, world!")
