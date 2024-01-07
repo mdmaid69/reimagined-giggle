7780a4f754b1ebb22288a76245729c89ff306bab
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:47 +0000

    record shake finger ahead average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
