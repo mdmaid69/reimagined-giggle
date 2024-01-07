d279fff6f8658b4b84b3c1ae5544f8523ede0c26
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:24:29 +0000

    correct task survive poor surprising

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import os
 print(os.getcwd())
