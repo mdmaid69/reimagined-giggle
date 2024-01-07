cf104bb3c91372486b3f32267b46cc73ae02a6af
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:47 +0000

    Ii independent city brother base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
