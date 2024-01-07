5973da16b97749a42eb40bbcf8c5617ba238a7a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:16 +0000

    count gas if it standard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
