1690c9f5956d8b1faf09f20067244d170991b809
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:19 +0000

    score any several dream level

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
