0c7f09f831ac2aa03fc96639a2e7bdff80d5b1a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:22:41 +0000

    little dark city partner swallow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
