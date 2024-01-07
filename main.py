eaa165656d72799f38cdbceb2a8985d5575d2ba1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:10:56 +0000

    long at doubt truth record

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
