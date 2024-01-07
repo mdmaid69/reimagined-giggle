4a758314818b083052eb26a0ba2b1f9f99f78ddd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:51:56 +0000

    custom new slowly rest process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
