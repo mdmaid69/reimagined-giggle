a196969fb7cee01d4641301b19e6b34ecd7ba7e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:20 +0000

    film still quiet affect indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
