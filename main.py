c4e40866099f0087439c7ad5d256dab92530d211
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:49 +0000

    phone 567enemy Kk herself poor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
