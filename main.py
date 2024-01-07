ffd327fdeff2f9d7dfde4c9f35b2911a3da64d91
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:35:48 +0000

    avoid year fall nowhere gas

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
