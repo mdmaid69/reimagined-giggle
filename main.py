edf4841bc3c5cccdf84114e3ec27bc1ad54f0bca
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:21 +0000

    block hide Uu goback(to) him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
