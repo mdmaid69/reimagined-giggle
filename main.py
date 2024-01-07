db8b77907abeed216960e0a432d8461a79e58672
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:43:22 +0000

    dance knock exam taste oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
