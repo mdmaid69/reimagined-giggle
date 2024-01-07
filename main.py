b8f7042473fa930912d258fdf367924e8647ce72
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:25:23 +0000

    reserve born workout type area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-print(math.pi)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
