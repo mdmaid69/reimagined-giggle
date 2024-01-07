4d03f47cfdd9904651092789144598ea8a2f66bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:13 +0000

    preparation borrow bringback tense my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
