4e50837318f5795713107abedd8e21858af78f3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:34:29 +0000

    recently keen yesterday strike young

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
