359fa6dc280f6be66efb8402102e765c890da495
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:57:39 +0000

    dig may perfect grade like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
