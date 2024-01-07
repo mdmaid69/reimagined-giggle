59d45490bcfad3253ca9cf3f02a129c27a3c3a98
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:54 +0000

    then acceptable bin old basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
