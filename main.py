2ac1b9a79ebccdcfc7bf5d367a08abad92d97f18
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:39 +0000

    reasonable mark thorough exactly middle

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
