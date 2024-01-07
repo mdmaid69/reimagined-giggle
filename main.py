ce3ac99fd99772864db661838d1f4debf2a06e08
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:49:27 +0000

    closed risk wonder special plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_as_str(array):
+        return str(array)
