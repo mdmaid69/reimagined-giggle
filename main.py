264a7e579bb9ee063d235d03a613545dd90b8443
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:49 +0000

    takeout grand attractive patience of

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def reverse_list(lst):
-        return lst[::-1]
+def square_number(x):
+        return x**2
+def count_elements(lst):
+        return len(lst)
