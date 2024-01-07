57ab07a4124734fb2e176c74386c2e82645059ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:48:08 +0000

    remark invite dream turnon never

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
