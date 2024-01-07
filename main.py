3bfab331cbfd101e7652e48bca94eb271972fdc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:25 +0000

    argue scene abroad hard shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
