dc2473a14d1769a3d00d7a3845664db1ec653f21
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:21 +0000

    wash command report tall shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
