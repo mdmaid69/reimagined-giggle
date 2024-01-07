2b024f409915216984f7b579a83b16a5c605b1b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:37 +0000

    fun large burn innocent kick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
