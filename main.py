12bc3a332efd8366f0a8849bff5438bb66999b76
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:51:20 +0000

    various start fortunately sudden lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
