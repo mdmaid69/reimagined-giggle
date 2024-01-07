9d66488b93eaa608287a9c02a28d3abdaf57c549
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:25:47 +0000

    allow sure drawing luck available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def set_array_item(array, i, item):
+        array[i] = item
