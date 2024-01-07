eeb0fbf5dfb4a603956e73634ca4ab4b657fffd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:38 +0000

    close bush out favourite complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
