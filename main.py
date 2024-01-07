f63f69212dd19e744f5713c8051843773ff93952
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:50:40 +0000

    professional question Ss with dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def convert_to_binary(n):
-        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
