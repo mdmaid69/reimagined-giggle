ccc48beffcb46971592b8fd37c541c145b1405e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:17 +0000

    collect load grade shut offence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
