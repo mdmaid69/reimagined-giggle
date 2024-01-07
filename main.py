b82dcb08f0b9eb6be13bc4bf723f39c9a45e8751
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:56:37 +0000

    matter movement reject anywhere letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
