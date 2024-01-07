f055b2cb08c9480eb9b62be3950bbedac51dc09d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:13 +0000

    future trousers official never fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
