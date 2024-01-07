8e0454ba2e2050e2133a8eb00952be5fe316b95d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:44 +0000

    shelter beyond bringdown square limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
