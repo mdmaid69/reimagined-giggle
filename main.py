0cdab4b48afbe04375238b17901072fd05f913f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:55:40 +0000

    566end market afternoon before follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
