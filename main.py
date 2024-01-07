426828705e04e6bd898b11cb3ef6a62445361e84
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:46 +0000

    lookback(on) custom Bb undo temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def add_numbers(a, b):
+        return a + b
