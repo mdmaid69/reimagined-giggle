5780077a61ec6d28d7214a570bec5e920c43b185
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:22:41 +0000

    joke grow appeal break gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
