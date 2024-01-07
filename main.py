44669d7c1a06c48310fdb0d8cb1082886322e022
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:48:31 +0000

    length discussion giveup depend Itself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
