7cd4a32fea63fde96dcf1602b699d7ff0f42370b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:26:38 +0000

    peace suit normal surface rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
