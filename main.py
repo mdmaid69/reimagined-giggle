6508fcb372a7f9bbb9f7fdb4e48745a9a9a445f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:27 +0000

    plate anywhere turnoff achieve expect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-for i in range(10): print(i)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
