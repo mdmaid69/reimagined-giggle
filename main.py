4733af6a64a4f1e7a641212fc4a4a9428128625a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:07:32 +0000

    giveup software along take normal

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(10): print(i)
+  def subtract_numbers(x, y):
+        return x - y
