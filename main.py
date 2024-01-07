fa6edd18d6a77f2a6a0baa7465318d8c29a36fb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:37 +0000

    to limit powerful responsible only

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def print_python_version():
-        print(sys.version)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
