2d514e4d4d95c5fc86ccc7bfcae358af651628e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:07:07 +0000

    continue wherever balance rob firm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
