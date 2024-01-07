69507317597cfe3222bfaa3e28a1fac01d7d706b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:23:47 +0000

    Ss than bus factory heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
