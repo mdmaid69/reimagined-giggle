bee3d3d06ccf2a69880f50041bf74a357cb741bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:43 +0000

    cope nature activity terrible soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
