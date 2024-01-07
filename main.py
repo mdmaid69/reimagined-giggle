621b7bf182435d1aacb4b7037a1ebadfc125f071
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:18:32 +0000

    watch dinner satisfied share long

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
