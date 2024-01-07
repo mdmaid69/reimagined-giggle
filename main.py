0539b851fe629a6aa81cbe68511da28dd4ef17df
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:23 +0000

    pump you so heat development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
