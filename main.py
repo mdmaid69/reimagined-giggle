52fa63823e32649265af848ea1c8d02d1464e39d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:12 +0000

    circle city emotion table insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
