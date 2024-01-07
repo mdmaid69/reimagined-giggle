9e3be0a09ca352667c34e9ce49ea17153ca5df8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:23:56 +0000

    hide support original feature already

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
