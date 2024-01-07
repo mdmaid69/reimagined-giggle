94c8b633bb6dddbbb609a19420cb288031a29d95
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:59:18 +0000

    traffic little closely break vote

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
