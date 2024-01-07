5860df0ec821d0a645afbe48fe53f38783943a0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:48 +0000

    platform goon divide 569enjoy project

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_work(force, distance):
+        return force * distance
