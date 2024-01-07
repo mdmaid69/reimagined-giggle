e9ba7d687eb953ac266071a014d5bab5751e252f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:54 +0000

    still prove slight drink sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def square_number(x):
+        return x**2
