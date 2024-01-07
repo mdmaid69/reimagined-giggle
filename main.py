67ae1c6d858df0562210e4c9541fbe578784e55c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:39:34 +0000

    family cruel bury so strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import sys
+  def get_python_version():
+        return sys.version
+  def convert_to_octal(n):
+        return oct(n)
