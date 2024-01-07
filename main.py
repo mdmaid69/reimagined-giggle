d603e0e870d0446d16321cfe9f64e5a943e01ee0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:07 +0000

    work tear reliable peace yet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
