3556efe50ae994868ced0fa8eb329fe618343369
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:19:27 +0000

    print name conclusion paper port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
