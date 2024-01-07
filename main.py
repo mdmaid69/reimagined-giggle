2a8b25127410fb2c35479a657db0e34c7200bf6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:05:28 +0000

    thorough hour climb bring broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
