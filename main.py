9353839751a9e9d7087f8f6889a367c89d527a1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:48:47 +0000

    correction ashamed imagine same total

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+x = 10
+y = 20
+print("Sum:", x + y)
