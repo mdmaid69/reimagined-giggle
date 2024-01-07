9750d2a80e5e936ee10033249b64406ffff1ac75
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:26 +0000

    thinkover be ground to house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
