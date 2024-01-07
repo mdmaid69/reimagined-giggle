8ce0f4a0b333fb0863394d8a6926b4005727ff68
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:10 +0000

    walk sort your hello line

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
