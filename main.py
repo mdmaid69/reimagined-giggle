00ece20314cd41080499e7a2d85b82f4aab840ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:21:04 +0000

    himself food blame trap convince

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+for i in range(10): print(i)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
