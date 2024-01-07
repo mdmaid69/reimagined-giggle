ebf861037cbde82683e9339b9f51d34efbb93e44
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:32:25 +0000

    programme offer right give nonsense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def get_list_from_array(array):
+        return array.tolist()
