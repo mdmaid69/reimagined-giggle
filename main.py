3f678d5be0f7b437da28d2fb7512a8a7575bc148
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:36 +0000

    most occasion none basically swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
