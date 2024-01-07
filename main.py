9c02f07deba0c455146e43b6cf551b944f8350cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:36 +0000

    intelligent spot ring spread pass

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
