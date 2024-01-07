80fd27c2825a260e1b1a61df9bb0dac06b0af3aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:51:07 +0000

    relax answer attractive content information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
