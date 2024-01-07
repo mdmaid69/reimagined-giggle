98d73d329493736eeaa298fd12c632bafb82e3bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:33:09 +0000

    after instruction keepaway flavour bottom

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
