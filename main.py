8e21e9a014a413b246c93080afc6c6ad63117624
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:31:26 +0000

    check comfort middle no pleased

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
