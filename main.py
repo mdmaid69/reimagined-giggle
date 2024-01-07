7ccbcbaf07ac5fb659bb3ccd5f9aa2f0c40e20a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:09:45 +0000

    chance break either commit own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
