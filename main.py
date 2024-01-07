39f0ae34ebab3f75ec648cf3be1ec13652d814ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:36:57 +0000

    emergency report easy about demand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
