879dd8837bb32b0b03847b01387aac55db2dc995
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:03 +0000

    hurt activity enthusiastic immediate cheap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
