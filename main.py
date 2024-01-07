93cef5677c97163ade821e61416c5dd2dbc8dccd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:29 +0000

    takeoff run minimum emergency angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
