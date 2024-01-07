847dfce005e8943c08ed83abef86e42882bc4f23
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:53 +0000

    relative attention pool copy suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+print(os.getcwd())
