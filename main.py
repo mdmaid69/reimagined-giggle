b4bddeea81409918fc2ef994fdd5a9cf6c9ed2ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:48 +0000

    quick shelter target dance central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
