26fb5d88f7b175b1014912bf358a2ddc2f68404e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:18:32 +0000

    blame interview eat rarely peaceful

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_length(array):
-        return len(array)
+import sys
+def exit_program():
+        sys.exit()
+import os
+def change_working_directory(path):
+        os.chdir(path)
