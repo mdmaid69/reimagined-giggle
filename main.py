e82b7b75b7025049eb7f657271b1cefbb88bcd35
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:14 +0000

    payment hear crazy storm bury

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print("Hello, world!")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
