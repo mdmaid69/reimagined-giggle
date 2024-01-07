7c5da1a408e1d078c97898ffa255cca9f8025d0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:26 +0000

    wave another thinkover shoot treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
