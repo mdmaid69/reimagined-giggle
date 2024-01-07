4dd26c15e9184a6b92af9627d1d140c4316e0a13
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:59 +0000

    introduce sick plant key start

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_max(numbers):
+        return max(numbers)
