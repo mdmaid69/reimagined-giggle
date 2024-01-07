aad9b65ca976fe00fdbf495041305cda7a5af798
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:41 +0000

    upset over job brush refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
