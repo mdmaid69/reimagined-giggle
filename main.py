b724e63d51d2f5a45fcca900bb4736b8c09127e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:35 +0000

    speak Kk tooth mind bank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
