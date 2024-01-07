b3cad0a3f9725fbac93cdd98279aec40f968d728
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:07:07 +0000

    disgusting perfect region geton realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
