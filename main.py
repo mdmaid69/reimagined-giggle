1490ade4602c0880974b470ab51c2e4b2eeefcd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:53 +0000

    in comfortable straight tune want

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
