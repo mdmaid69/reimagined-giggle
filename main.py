48ed919419fedd463aa5df7b34017e080aa276d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:46:23 +0000

    public scare statement wish provide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
