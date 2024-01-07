16197275644ab67a6e83769f0334c354814c213d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:34 +0000

    family dry bury bringup shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
