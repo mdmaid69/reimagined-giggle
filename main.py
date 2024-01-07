c15c0a2f7adcdf912db8bd1d6c2cbb2b63bdf846
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:05 +0000

    benefit petrol keepaway tour tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
