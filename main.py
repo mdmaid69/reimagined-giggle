15365d5df8b1e2a499c0b27120a91d723c96ee79
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:57 +0000

    no opposite mixture day Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
