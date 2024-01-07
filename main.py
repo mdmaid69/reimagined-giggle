1fd65c7c5d5dbea2aca5241b6f1c68495acfd317
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:44 +0000

    passenger character squash regret 565end

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
