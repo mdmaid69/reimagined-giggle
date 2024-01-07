72f9e3b52b13c4a503caa9a58e69b4858031992f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:47:16 +0000

    better uncomfortable reply balance variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
