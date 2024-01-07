9cf6d4ad1e5d599fd55bdd348feb9e24d5abb7bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:13 +0000

    each report matter feed smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def remove_directory(path):
+        os.rmdir(path)
