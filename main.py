7b94270aecc569fcb335180d4cd87e91257029c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:26 +0000

    rescue dream about worse regional

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
