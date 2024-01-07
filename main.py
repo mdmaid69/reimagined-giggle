952570174e242dcaf93da15088529af56e7b6d65
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:06 +0000

    slide just more spring opposite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
