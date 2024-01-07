cf8c63a0fa3990a31c984bb300b3d897a5d6ef3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:20:46 +0000

    keepup proof process correction afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
