783cf9191ab34542d3d6320a5dcffe6d3c7fbe71
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:04 +0000

    talk street unite moment perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
