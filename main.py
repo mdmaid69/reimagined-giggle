86bced9466b6f9d8caaa9631f5c0ef1c709e24ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:47 +0000

    rent yet stand touch heart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
