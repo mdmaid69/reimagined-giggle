65b0865e8e9de1a6247132cb2115775559b1ad93
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:54:07 +0000

    workout understand lead perform opportunity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def reverse_list(lst):
+        return lst[::-1]
