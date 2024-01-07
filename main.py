6ef8d1e1489bc385d2f6ea6d5041a50f55fca9f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:25:12 +0000

    well run season measure bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
