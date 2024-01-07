ebf4a98e85648c3ce5a9d9ee67f061ed6cb547b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:40 +0000

    winter even tall hardly each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_directory():
+        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
