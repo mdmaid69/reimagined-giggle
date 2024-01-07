7085cf9765dde25dbe55e2bf017f09a61e9a586d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:12 +0000

    strike interfere twist another rate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
