0a9bb87734b0e357e5daea062dd29712c1427f9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:23 +0000

    position contrast tool book thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
