36b13ccb76682ab0926e903aa4d160b66c65b97e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:43 +0000

    apologizeapologise commit game health smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
