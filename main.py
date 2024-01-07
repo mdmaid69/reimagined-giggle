29d3da1e94e4217d62d004d628ef120b7be15140
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:19 +0000

    coffee deal collect close tonight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
