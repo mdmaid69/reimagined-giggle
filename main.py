51c6a417efcedd71b04519b44a861106c100d41b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:38:42 +0000

    official fear just centre basic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def subtract_numbers(x, y):
+        return x - y
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
