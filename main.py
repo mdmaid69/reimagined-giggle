a293736abddda1c110e0b553a32139f18f95ccc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:35:56 +0000

    priority introduction muscle glad wealth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def sort_list(lst):
-        return sorted(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
