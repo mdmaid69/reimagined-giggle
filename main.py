e14b4425d67c0011ddf373b9c3fa60300d325d8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:43 +0000

    crowded permission family Jj people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
