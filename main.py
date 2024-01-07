e1920c31562040e4f0155c9ddb0f6fc2b00e1f83
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:45:13 +0000

    term earn mass only far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def remove_duplicates(lst):
+        return list(set(lst))
