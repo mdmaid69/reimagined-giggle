0a19018af1e6e4f7eb3d6ec478400beeddf2e86e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:45:04 +0000

    abroad small roof scare record

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
