d948143141ce177314e52818bef7bd6ab4e95b1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:35 +0000

    refer store blue interesting beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import time
+def get_current_time():
+        return time.ctime()
