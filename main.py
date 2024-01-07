8a39d305610260addb792abae4f1fdf564fc7b0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:57:24 +0000

    impatient path wish street entrance

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
