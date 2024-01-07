b0d9ccb92b977c8e6f22fbd3bef21619d9f16197
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:17:38 +0000

    hurry cost teach could black

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
