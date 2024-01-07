117d58d71b929afc94ecf281378c9d0b346579b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:54 +0000

    love runinto make order scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
