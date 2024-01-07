f498ad1be2d89b588aa233364a74677d0eab23eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:20 +0000

    full battery business invite there

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
