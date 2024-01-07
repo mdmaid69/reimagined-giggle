8df336cf507eab3d2adab2920db4cbfdfcd8c545
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:28 +0000

    official back busy confidence loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def clear_array(array):
+        array *= 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
