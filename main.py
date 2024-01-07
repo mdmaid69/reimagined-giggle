97221ecc3de5630a8bce29f63470b860d7494f55
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:41 +0000

    loud sink class stiff get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
