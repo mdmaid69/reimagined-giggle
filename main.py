8ce6ab980c721b115d815fff49188ea671f7516d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:40 +0000

    right impression twist nurse about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def get_array_as_set(array):
+        return set(array)
+def count_characters(sentence):
+        return len(sentence)
