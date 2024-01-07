84f18a16e9781906d8bde071b66a573bd44eae82
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:19:48 +0000

    last try flow rob own

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
