49810cc5f5fca27b52f7da0f868b4501a62e145d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:13:08 +0000

    production mistake separate consist week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
