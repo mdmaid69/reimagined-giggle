df336f9ad6439d9e63599ecee2b1e99bde2903ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:45 +0000

    record uncomfortable solve grand feeling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
