63f74563df7dde88ff9081cd3c63561ae48ff2df
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:28 +0000

    cum twist contribute advise know

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
