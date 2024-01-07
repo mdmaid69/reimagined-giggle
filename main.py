b48c677fa7f418dff8a7e972e1369d1724ada195
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:15:18 +0000

    whenever favour dress lock roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
