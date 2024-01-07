be752fa7cef32ca440cbc5551ce5cb098a411093
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:26 +0000

    bear success point wear absolutely

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_elements(lst):
+        return len(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
