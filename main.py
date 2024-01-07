0611b26ebc3c72834483782cdc3a242aa710ac7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:03:45 +0000

    sore physical takeaway delicate willing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+print([x**2 for x in range(10)])
