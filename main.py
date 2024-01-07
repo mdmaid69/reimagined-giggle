15518ec520c40b1760eb991d8b1aeb8eaff9dde7
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:21:03 +0000

    container exit sudden control in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
