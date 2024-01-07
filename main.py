0dd74b85875f6ffc4b8d1d67cbcd71b5297e1eef
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:26 +0000

    apply price surface situation Ss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item(array, i):
-        return array[i]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
