a756b636c71c3a793e853c655bd2cb4f29ce4d7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:18:29 +0000

    under of discuss wash related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
