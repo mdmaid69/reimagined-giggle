51595013a8b9528f7f5a26869bf353f813d0997b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:52 +0000

    event regular number under bottom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
