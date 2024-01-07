a37c4d7200ff10784f0050105596245a404cf8c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:00:43 +0000

    571enough peace within may important

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
