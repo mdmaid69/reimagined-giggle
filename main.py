6e4d22c2f82a030847f98a1bfa59120b5e553297
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:29:16 +0000

    heat strike recovery temperature reply

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def add_numbers(a, b):
-        return a + b
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
