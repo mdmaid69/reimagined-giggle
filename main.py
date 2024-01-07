4ba6b25d9668be9fa426019cf8b29e06a6a3957b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:01:47 +0000

    black Ww meeting mend watch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-print("Hello, world!")
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
