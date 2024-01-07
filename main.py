672bc5ef1ae2d1da79f9118a4fde360fda82d28f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:40:22 +0000

    getback check shall stick tough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import tensorflow as tf
+print(tf.__version__)
+def add_numbers(x, y):
+        return x + y
