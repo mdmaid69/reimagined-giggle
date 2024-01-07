ba1868d7d361b0da33c2255c2d96008985a7581d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:33:32 +0000

    bringup 567enemy platform empty watch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+print("Hello, world!")
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
