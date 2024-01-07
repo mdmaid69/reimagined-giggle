3d653ff4f426ba2dc931d3ab4f1e83236955023b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:19:38 +0000

    set shame deaf criticize past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
