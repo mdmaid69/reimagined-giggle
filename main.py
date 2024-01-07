7b49858f5db8d21c25324219a64fcbb1a466d4aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:41 +0000

    leg either please move unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
