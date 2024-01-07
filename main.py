b83056ee3293341922fab198448f342fc29b6843
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:34 +0000

    ourselves cruel instead holdon arrangement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import logging
+def log_message(message):
+        logging.info(message)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
