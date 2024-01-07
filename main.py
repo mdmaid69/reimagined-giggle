7063118acb6e3c089a2f39d21e71d52aca20a979
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:10 +0000

    frequent breakthrough accept translate development

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
