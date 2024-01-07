3295418dec57b75875881955da34c022c68334b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:54 +0000

    deserve recommend respect model call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
