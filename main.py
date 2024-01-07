326f4b54a73b83e406ec5ae70764f04f5cbdbfee
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:32:04 +0000

    woman exact whisper period lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
