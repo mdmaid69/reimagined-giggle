72ae242d7072531b15c9a134d8746fb0905640ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:08:48 +0000

    equal part run dick shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
