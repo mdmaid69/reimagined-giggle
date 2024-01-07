b01b368ed968758cad72e75fccf44815ad183288
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:11:29 +0000

    this fold slow punishment Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
