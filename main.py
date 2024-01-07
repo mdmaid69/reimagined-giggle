81cad778758a88c649a303e72d6adb5e179a4d53
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:00 +0000

    pretend pretend healthy delicate table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
