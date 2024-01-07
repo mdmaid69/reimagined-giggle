68cb3c294b0709c56ea4426b8d6ad950a8bae2b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:15 +0000

    pot eager example tidy needle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
