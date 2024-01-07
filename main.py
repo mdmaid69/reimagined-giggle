3f63fc624e152d03d63d6c2a19cfeec37a15f69e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:54:17 +0000

    nose than pressure meaning poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
