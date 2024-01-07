99101657afe771a212b3bd67aa58410eb41d0dfa
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:45 +0000

    positive cover gooff complain dream

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def change_working_directory(path):
+        os.chdir(path)
