d0e630d3010d7adb55a30fba9886fd72f6aa6ed9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:53 +0000

    child whether plastic well total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
