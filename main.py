deed0c6f44d33377fc616f4fda66a8e1dc2259e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:09 +0000

    float waste at connect lunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_pressure(force, area):
-        return force / area
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
