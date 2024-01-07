60f1138d27c221dfdcacb9c06d1a9fba101f4484
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:18:47 +0000

    rare press Zz frightened raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
