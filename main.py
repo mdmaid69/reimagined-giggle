5474e00b6c8ad38192c6bc108bacdfa84dfa3347
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:18 +0000

    here winter regular goon dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def reverse_list(lst):
+        return lst[::-1]
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
