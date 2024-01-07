2d0a1f6050c0ff590deb20f6e8b19d99f074642e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:23:40 +0000

    range fine miss effort brave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def subtract_numbers(x, y):
+        return x - y
