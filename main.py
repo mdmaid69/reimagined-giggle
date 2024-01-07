28742079d25af0efa57628000e6820478a94f894
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:33 +0000

    smoke seem Tt worried learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
