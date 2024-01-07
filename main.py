39cd52453ec40d210d0dcccb783dcb7a8636fc26
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:22 +0000

    roll leave complete high visit

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print([x**2 for x in range(10)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def add_numbers(a, b):
+        return a + b
