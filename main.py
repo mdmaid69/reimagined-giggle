2a7d60b04d2eead54a6981f155a2ab2e21817a87
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:33 +0000

    nice hang way news welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+  def sort_list(lst):
+        return sorted(lst)
