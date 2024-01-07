8a441719ff64c8b620d8967f3320a9c0a3106f8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:15 +0000

    production before snow heat mood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def square_number(x):
+        return x**2
