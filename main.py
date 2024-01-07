d30f988b3dcd9a4c1471509968fdf5a9b04ef6b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:57:40 +0000

    throw upwards congratulations common tread

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
