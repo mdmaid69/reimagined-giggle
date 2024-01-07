a388779299293919376f797e1be798eacac41e35
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:06:37 +0000

    uncertain some although nothing uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
