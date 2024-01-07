1e4954586c72a955e0557fd5d71483e86a6393a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:44 +0000

    except complicated base contain bored

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_max(lst):
+        return max(lst)
+def count_characters(sentence):
+        return len(sentence)
