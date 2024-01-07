096ce8824db03a3c9888f0da0ea35ef51e8cf02f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:56:05 +0000

    straight difficulty some through score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def reverse_array(array):
-        array.reverse()
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
