0886bacd9b1b7f3a33498c88b02e56032cf6334e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:36 +0000

    love join breathe either blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_sign(x):
+        return math.copysign(1, x)
