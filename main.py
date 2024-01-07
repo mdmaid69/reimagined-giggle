44479c0219a315d0cc8381db7e9660f405e39333
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:43:05 +0000

    afternoon meet laugh succeed towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
