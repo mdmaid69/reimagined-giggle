73654f09204397deb912a4be31052357876031bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:23:05 +0000

    same dance Mm foreign disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
