467e235615a339ffcd9b95c3ab71a7b4ea02f736
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:23 +0000

    public home general prepare during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
