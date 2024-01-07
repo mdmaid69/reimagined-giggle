abfb5ab7c2ed775996a82ea506f39f93d838b337
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:33:52 +0000

    rough letter dry violence makeinto

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
