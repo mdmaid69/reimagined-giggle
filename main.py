a6dc07c315b1b2c37d857e35ddcf78d8beaa471f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:44:01 +0000

    broken brother conduct tidy transport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
