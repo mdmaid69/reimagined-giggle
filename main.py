52664d5aecec8c159298d71c0381579b465e5088
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:47 +0000

    finish nowhere bear punish late

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
