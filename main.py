12c92c29d2f38a77c1aef4507b8660216d2f5278
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:11 +0000

    heart country rather whether occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
