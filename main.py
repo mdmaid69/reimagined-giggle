a68a31e525cc4b439c7460d58e29ec3f8fbd5cb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:53:19 +0000

    path read guide rush around

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def count_elements(lst):
+        return len(lst)
+import re
+print(re.match("h.*o", "hello world"))
