ceedd64f7e08e5771a5ee666863a6adde941cb17
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:22:39 +0000

    dream favour scale public swimming

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+print([x**2 for x in range(10)])
