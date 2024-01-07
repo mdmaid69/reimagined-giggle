3f7f1974c0fcf222666504cd1b8154315c888c92
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:38:24 +0000

    confidence ago recovery we success

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
