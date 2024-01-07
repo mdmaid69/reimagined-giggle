35e952a3b1e187366bfca321c837d09b99e8a6cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:40:23 +0000

    half doubt answer union Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_str(array):
+        return str(array)
