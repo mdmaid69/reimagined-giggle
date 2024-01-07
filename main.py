fddcbcf91790dfc801b5bee977e6a66eed660ae9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:29 +0000

    confuse die myself repeat probably

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_counter():
-        return collections.Counter()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_rectangle(l, w):
+        return l * w
