62c89660c38c2164feb2330848488bd67c46cb23
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:22 +0000

    sound whether roof so thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_int(array):
+        return int(array[0])
