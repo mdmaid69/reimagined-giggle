fcb4b7212ee499ba838e650d084502af27fd47e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:10:55 +0000

    build small new care exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
