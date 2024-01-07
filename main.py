d98e9735e4f19e9d58937291d747120ea778a508
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:45 +0000

    tax week really detail major

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
