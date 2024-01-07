e54ab8a8562cc710bc8f45ac8024725a6c512b18
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:55:45 +0000

    positive remember frame yet popular

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-print(math.pi)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
