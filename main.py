a07da710aaee95281ff4d2a130cfc3d18ae04d06
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:06 +0000

    surface lock good balance post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
