0d4d2cf7274ce7d17cbcac0a094c943a6aad1cd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:35:09 +0000

    advise mistake leave interview bite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
