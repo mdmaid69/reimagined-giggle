cba5ff68ad72dc7be9af60cc8acf3d10228c0d25
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:22:43 +0000

    worry free stage fire journey

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
