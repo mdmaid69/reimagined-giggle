e28e6413bf9aad7507ea3e0b0d685af503498656
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:56:20 +0000

    strict display declaration convinced owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
