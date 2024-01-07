0804c5520302fe08c1601df0a948d6950214968a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:04:03 +0000

    stick able combination success wheel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
