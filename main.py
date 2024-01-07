8c2525851166fdd479fe09b06b01b25ac3a327e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:26:29 +0000

    offend tear excuse machine phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
