d03511a2d156ccef5f866be8c1022928aefaa529
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:25 +0000

    stone gap amount reply minority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def remove_duplicates(lst):
+        return list(set(lst))
