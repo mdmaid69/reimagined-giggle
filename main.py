2c29138a59dba01ca044b9b5b483dcc4e037dd1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:54 +0000

    tooth welcome bridge behaviour ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
