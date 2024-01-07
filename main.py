4708b043f7857c9f9a1d90f5f121fef2dadbbfc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:21 +0000

    stand answer far problem settle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
