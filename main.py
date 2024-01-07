49b9d3801bf49f2af666f0d6346d176ce31eca8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:43 +0000

    occasion then well weigh wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
