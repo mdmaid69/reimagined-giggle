4322706fa573a6e6b9542ba024f00e4966eac09b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:15 +0000

    careful indeed treatment unfortunate lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
