c4861120450afee4f1fa2a3e234e3639ed896327
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:09 +0000

    place turn earn raise performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def remove_duplicates(lst):
+        return list(set(lst))
