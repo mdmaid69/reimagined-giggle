053cc1c217db5e523012c32cfc0ebdd8a4397a71
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:41:54 +0000

    surprising tape behaviour reduce excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
