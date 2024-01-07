4b92891dc0db1265fef51c60ed4a1a8f0c2a934c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:55:28 +0000

    brush unless straight purpose minimum

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
