bb595a4145c306c61407eee5a8227acaeb1c07f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:19:53 +0000

    heat if material education get

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_to_hex(n):
+        return hex(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
