3c4515e98639b56238c91438b59b8ca6585e4cbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:08 +0000

    else fast month long deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
