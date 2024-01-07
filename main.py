1a1b6ef7ddca39c57847dbad097f1d4c1be5285a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:30 +0000

    home eat comfort question statement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
