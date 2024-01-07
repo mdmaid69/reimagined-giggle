cce61c990b92e035bd909f61ecb6b56356ef5016
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:21:30 +0000

    turnout direct invite satisfied smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
