403ffcbcab0c3cdb763484e10da934223cc7314f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:05 +0000

    pretty suit thing himself else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def is_palindrome(s):
+        return s == s[::-1]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
