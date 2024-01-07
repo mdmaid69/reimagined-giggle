d4abe2e6e1a6bb9f60746e95ce46ba4899b0ae97
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:42:33 +0000

    call on doubt kind repair

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(10): print(i)
