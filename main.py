dd2e672b1a2ad0418e4d05330923a4c1fbe37b66
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:24 +0000

    relative project fond mother suppose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
