05aad1b8d3a6dbf42881f1a2355324acd30ea4e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:27:24 +0000

    belief regularly powerful unpleasant wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
