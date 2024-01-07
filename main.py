fdb41b4d667cb0cc41cb1694481e2f791365e9b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:24:28 +0000

    spot comeoff keepup neither responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def set_array_item(array, i, item):
+        array[i] = item
