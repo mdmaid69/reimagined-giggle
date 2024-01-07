1b665a61ce433a39b6905b6dcbfc599f7312a118
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:46:20 +0000

    cheap slip share smooth speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
