78d38ae614db393fff3172749bd779277f7b868c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:19 +0000

    scare small dick joke sore

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
