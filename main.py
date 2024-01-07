8e32d8460855b398ba33b9848a790a1be379ef49
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:52 +0000

    cross trip pen tidy properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
