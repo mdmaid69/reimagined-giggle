7d006d5c90c8a571bc3692d1ce26b008f3b42b8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:54 +0000

    record lead comein respect willing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
