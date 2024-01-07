c041ef2367a3d831209e57589b80dedb75c742b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:20 +0000

    sort not extraordinary fire large

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
