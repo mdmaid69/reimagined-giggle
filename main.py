ead015edfbd5657db9dd65c62029b381a4b24ae6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:20:36 +0000

    comedown rely buy lookaround sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_memoryview(array):
+        return memoryview(array)
