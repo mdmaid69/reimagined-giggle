b0e2b19a421fc3036412146f0e4eb555ff24f3c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:29 +0000

    over rare protest healthy afford

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
