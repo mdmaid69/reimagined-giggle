f76fdde8371b722cf83e751fdfeb52d1f5758ff2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:41:25 +0000

    life organization prize Jj check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
