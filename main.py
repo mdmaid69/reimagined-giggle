4db93d15200f65f4cadf2a27942982855eb6fc4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:52 +0000

    along related successful hard centimetre

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
