fa5654d0ea89301e3946af75ab5b0b20e0917da8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:18:28 +0000

    bread inquire advice cough talkback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
