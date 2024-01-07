ca255a1f16eb3f9b9c8358de684d80076ae71fa8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:03:05 +0000

    lunch scrape responsible pain eye

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
