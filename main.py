48f7ee5c8149ce855e286e6ca00aa5b5488d034c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:24:23 +0000

    above callup wire issue future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
