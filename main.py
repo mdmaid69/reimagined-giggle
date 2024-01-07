86b474689584c65cf7a77af84901046a79859335
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:22 +0000

    friendly statement suppose best right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def convert_to_octal(n):
+        return oct(n)
