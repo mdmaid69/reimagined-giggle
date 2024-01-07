33aa1a37625366f79b0cd61630606b652d5ca19c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:17 +0000

    forget even somebody look black

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+def count_elements(lst):
+        return len(lst)
+  def reverse_list(lst):
+        return lst[::-1]
