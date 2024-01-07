609b126af76ee3e66bbf0186dc4eff0f7298d1f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:29:49 +0000

    move uniform offer fat sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
