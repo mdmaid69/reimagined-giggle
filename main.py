e2dde14ddc7980663d4ee9659b56aa4be92ed1df
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:46:30 +0000

    calmly go exact clear argue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
