f4bd2e080650d1300c080e492b18c6607b4f67ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:40:46 +0000

    suppose eye thinkover course normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def reverse_list(lst):
+        return lst[::-1]
