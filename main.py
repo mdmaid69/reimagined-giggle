d87b8420c2f267b27ca65bf9bfcfc5628ea6fe93
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:02:12 +0000

    left Oo set grand every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
