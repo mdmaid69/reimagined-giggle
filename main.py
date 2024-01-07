9a7f6efd564054ac04a5e57c09a0967e85926188
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:27:31 +0000

    steer however even keepout reason

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
