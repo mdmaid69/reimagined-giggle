a169b7873340e0185fd400a2d0c569a8d85b73fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:32:17 +0000

    ring hate odd start comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
