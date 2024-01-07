386d9926efaa20a1ec5ee43f0c29eba9077a288e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:45:37 +0000

    exhibition discover office full cinema

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
