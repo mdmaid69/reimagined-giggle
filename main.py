6ae5b671e646e170e4c04e2fc800f8fea617b4e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:52 +0000

    contribute bother remark satisfied Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
