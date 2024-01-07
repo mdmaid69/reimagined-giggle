c3709f2a18dc2bcee0dfb225c213e94a08682426
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:24 +0000

    heart noisy urgent spend pile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
