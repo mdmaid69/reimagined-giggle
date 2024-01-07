cf77b70721960a12ed2038be87ee605750e4a48a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:11 +0000

    question crash foot common basis

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
