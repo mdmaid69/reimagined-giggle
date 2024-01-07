2cce3840772a91546c3722d0ccc7b92c8ca0bd82
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:04:13 +0000

    scale Uu expensive contain hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
