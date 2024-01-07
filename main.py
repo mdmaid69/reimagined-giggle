bf921216d77ff24accc5540611f8a4ee1c2d04d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:01 +0000

    mad justice feel bringdown check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
