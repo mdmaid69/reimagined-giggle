2b53652253ca345dd61cc0e76f1c63c86883b096
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:20:15 +0000

    game Ss pity lookaround scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
