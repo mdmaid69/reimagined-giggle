335d2bca385e68698159081c4232cb94d439b437
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:10 +0000

    disapprove on debt copy consist

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-print(sum(range(10)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
