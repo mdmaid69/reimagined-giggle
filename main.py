8ca53a6092b32aa87f3ccedadd85fa443b566982
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:16:12 +0000

    today that happy material alive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
