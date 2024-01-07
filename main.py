66b143e41d798bd28cb014f89c8ca11b1ecc2394
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:22 +0000

    train anywhere Ll criticize meet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def square_number(x):
-        return x**2
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
