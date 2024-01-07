121d6e51d336e9b7646eb9a00400ae3e08926d66
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:50 +0000

    shake soil behave suffer interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
