4f67195d1ddb0e08430da9ce17e492684d338dde
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:49 +0000

    limit have contents sting able

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_hex(n):
+        return hex(n)
+  def is_odd(n):
+        return n % 2 != 0
