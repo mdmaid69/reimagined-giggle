25b45cc092ad9c36cd57e1e5ca4d293980171abb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:13:19 +0000

    mother amount here translate regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def remove_directory(path):
+        os.rmdir(path)
