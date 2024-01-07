68286dc57b7b5073cc06ecd237dfd8b26596a9e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:39 +0000

    colour decide shock tidy persuade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
