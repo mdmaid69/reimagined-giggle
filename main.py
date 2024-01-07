37c37458a456b0887741d93d5198ce4283d243b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:15:26 +0000

    properly length distance mine left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+print(math.pi)
