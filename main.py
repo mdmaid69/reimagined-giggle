e4929d768218a1e392e043b2c07ea95ee82bbc67
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:02 +0000

    write threaten march giveaway she

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
