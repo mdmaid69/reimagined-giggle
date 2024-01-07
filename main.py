0a7ebd23e571bf30e18cd7e4d8e6e222c948bc70
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:28:03 +0000

    disagree lookback(on) hardly transport not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
