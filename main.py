5883749f859f631435ba3d08206df057d315b61b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:15 +0000

    moon turnup risk 567enemy afterwards

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 import array
-def get_array_from_file(filename, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.frombytes(bytes)
         return a
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
