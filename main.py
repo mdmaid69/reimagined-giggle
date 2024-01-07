53e3238e2cd34baeaf2ba3106145d7cd5e8d3d66
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:33:22 +0000

    close copy by active eager

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
