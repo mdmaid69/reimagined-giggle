f4e1cf1a3b764975704783635af75e06fddd519c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:35 +0000

    pronunciation job nowhere word flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_queue():
+        return collections.deque()
+import os
+def remove_directory(path):
+        os.rmdir(path)
