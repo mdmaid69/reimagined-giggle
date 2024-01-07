7ad4331b8c1de1d8b87fe38a230b7cbe3043d034
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:45:58 +0000

    frame number objection temperature behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
