31770a63e2fa941207ddc09380a4a3c71c2fbfa3
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:09:51 +0000

    tip murder bring with cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
