11496712134f239d0ca56f344c08657e0969e3e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:59:31 +0000

    communication bridge space act suck

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+print(sum(range(10)))
