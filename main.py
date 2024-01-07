5bdf13637d56edb3a221e7c81ac9d3a3ea3729f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:35:08 +0000

    circumstance interruption home type pattern

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
