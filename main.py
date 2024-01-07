df15e02d88eea2202a243ac9d69059943369f7a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:37 +0000

    man modern holdout emotion past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
