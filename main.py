879e3585ef272358247dc163d9bc78acfee558c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:38 +0000

    pay money century characteristic fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
