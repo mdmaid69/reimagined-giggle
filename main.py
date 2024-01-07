93936cccbc208b9a77d95ea32cc46339d584ff02
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:52 +0000

    birthday degree skill lock excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
