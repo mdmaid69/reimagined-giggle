f588fe08808065b510c2cc6a70ac37048d958403
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:31:02 +0000

    pay mix dream relationship friendly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
