e845f4b1b4b6e46fb35f8e955df10715948c8198
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:01:27 +0000

    charge do now attractive value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
