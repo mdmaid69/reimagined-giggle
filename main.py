3209e8fdcbb600160aee113ecd7b9e32fda432f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:17:20 +0000

    ever swing wheel dare achieve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
