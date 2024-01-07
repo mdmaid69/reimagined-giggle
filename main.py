4fcc1753e3790782e13fcf2b7ae3687475a63501
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:59 +0000

    question whole knock test each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import collections
+def create_user_list():
+        return collections.UserList()
