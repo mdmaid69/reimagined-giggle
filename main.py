574f58d4175006c72f00081d1be9fce865afb98b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:41:16 +0000

    turnup age church tomorrow convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
