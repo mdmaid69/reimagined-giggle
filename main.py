1a8d5f10355fb444f1bbe91a116d86ea37ff1177
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:01:30 +0000

    understand impressed minute absolutely contrast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
