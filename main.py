cf6a7c04bd9f31dc12e9b76fe04c482f10c87f2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:32 +0000

    fail wealth original mood Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
