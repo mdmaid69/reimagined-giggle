fef11408da76234fa7e72f7cd835e00d23ecac0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:16:10 +0000

    best repair single this neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
