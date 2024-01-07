93c57647c7395124c9530222bb8d31aa4e7c7efb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:40:12 +0000

    warm thumb keepup cut comeout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 numbers = [1, 2, 3, 4, 5]
 print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
