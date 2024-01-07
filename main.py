61466630bc6d7aa4af2dd6b459b646fb2728770d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:45 +0000

    sense bored talk language tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def clear_array(array):
+        array *= 0
+print(sum(range(10)))
