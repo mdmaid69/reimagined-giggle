f122b35384331fc4412d3353544187c783e07af2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:41:08 +0000

    gather similar makeup improve past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
