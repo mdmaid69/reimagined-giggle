fac2e8b12537b676fbc687ff9155733e3f4c72f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:09 +0000

    discuss concerned split report damage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
