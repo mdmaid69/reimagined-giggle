5bf952b0009a272d3fcf942bb073bd49e84aae18
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:59:09 +0000

    language workout regularly dangerous like

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-def print_python_version():
-        return sys.version
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
