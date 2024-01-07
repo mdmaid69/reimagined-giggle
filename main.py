fb736746aa023b1cc7e50e834c0063f7523db0b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:28 +0000

    pretty pass extraordinary freeze reliable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
