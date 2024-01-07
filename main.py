48c41f1ab40b6ba89c780ba3ef4f2fd63d55e3b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:32 +0000

    she build reduce afraid closely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(lst):
+        return sum(lst) / len(lst)
