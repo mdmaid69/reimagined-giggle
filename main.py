b0b6627d19ac7c2196a2a02d9379f7b2fd8b3461
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:44 +0000

    decision remain force form far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
