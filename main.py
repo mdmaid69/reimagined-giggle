5cfaf947a299a7b610f6c313f07afe47cdf2ab94
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:47 +0000

    neither would advise uponon dance

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
