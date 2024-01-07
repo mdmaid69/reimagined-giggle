644fc1e310b20339b3baf5cee9247e00bac7c322
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:00 +0000

    soon limit left many concerning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
