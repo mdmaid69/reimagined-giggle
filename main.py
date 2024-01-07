d144abddcd09a8bb67d5139c5a539faf97c0e7c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:07 +0000

    risk advanced downstairs summer another

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
