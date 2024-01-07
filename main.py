010c5443e05b966753fb4ba4c726bc66c5e99b1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:44 +0000

    improve share permission later environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+print(time.time())
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
