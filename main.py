f3b7dc99adc62eb2eded85ee50ba7b7157ec6243
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:47:27 +0000

    than container period enthusiastic sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
