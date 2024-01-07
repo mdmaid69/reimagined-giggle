6f2565fad8ddc21321fe16dfd446ecb548b92b48
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:56 +0000

    kind correct pipe own keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def is_even(n):
+        return n % 2 == 0
