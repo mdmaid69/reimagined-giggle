ff35e9a3d1bfdc02e9c2c08131729abe6ba1c531
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:03:27 +0000

    brain complicated dick communicate Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
