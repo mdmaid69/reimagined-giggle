8a11d2833e4691c821acbc4ea8ba62270a809a32
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:16 +0000

    tomorrow respect team comeout mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
