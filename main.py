7d5f4a02f0a07738060cf0bd77c6bbdd1824f6c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:28:35 +0000

    taste practice apparently talk preparation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
