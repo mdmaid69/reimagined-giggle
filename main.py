328024dd05b75cb89db7529bc80b61f9c4db08f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:57 +0000

    hour last promise dance under

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
