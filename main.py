fc9ce5e58e95c76f5f17cab7c1e68113215ebc64
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:28:54 +0000

    makeinto event experience solve more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
