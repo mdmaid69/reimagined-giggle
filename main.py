62a6401ad0c22f4255be86c366dc2ac28c16facc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:19 +0000

    Ee freedom care each injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
