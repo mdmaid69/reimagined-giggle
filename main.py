06f1a9edced12246a971cf4ef46c4ecfeccd9b0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:14 +0000

    earth split splash central dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def roll_die():
+        return random.randint(1, 6)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
