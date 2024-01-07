cb38211953c330d71fbc9f72a2072e264e14896c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:57:19 +0000

    571enough prevent bang transparent kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
