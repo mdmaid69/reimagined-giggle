ae4ce205d3ee097c8c613f1a7e93d2dd41430607
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:22 +0000

    hardly imagine model burst getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
