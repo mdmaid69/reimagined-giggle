75ea255deddf111c78939bbc1ebf968c2f3a684c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:10 +0000

    future high joke scheme shake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
