6b81f080801c099205ac4af7df25205f29567843
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:21 +0000

    score born past spill direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import os
+def remove_directory(path):
+        os.rmdir(path)
