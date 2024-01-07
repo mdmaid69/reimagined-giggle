32c45e56746cfa4d020110c76f03993e3bc7c082
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:26:13 +0000

    challenge look foreign modern runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
