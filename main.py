31753758d19d6f9157fd7ed752ae43f59c36d65d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:40 +0000

    muscle 568energy you blame rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
