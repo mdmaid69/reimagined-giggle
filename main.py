fd798e77b2a6eaaec9452ab35b0984286184bd5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:35:15 +0000

    make lay colour sensitive search

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
