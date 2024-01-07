647f265d9587936fe1373e2826b0b7f97ce50c98
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:10 +0000

    concerning Vv well special seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
