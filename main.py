99e1901d21b074a58eec82155af2783fed3eeb88
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:19 +0000

    negative task show lesson slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_acceleration(speed, time):
+        return speed / time
