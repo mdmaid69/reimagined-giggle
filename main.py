092c5198919fff2ece3f125312e935a791ad9572
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:38:44 +0000

    behave improvement glad back window

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_pressure(force, area):
-        return force / area
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
