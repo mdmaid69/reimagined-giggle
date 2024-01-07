f54f793f37559df2ea603c287ce937f57af02448
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:15:45 +0000

    aim meanwhile way uponon safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
