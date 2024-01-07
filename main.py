5537a94d7d31db92448975ff792942ff8ac53824
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:04 +0000

    distance gradually lay speak care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
