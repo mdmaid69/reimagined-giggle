54457ddd91165f0a0f2868034f3e2efea3e673ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:45 +0000

    error joke young protect match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
