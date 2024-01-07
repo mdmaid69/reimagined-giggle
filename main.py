e8f5bdabb0048b5f6c1a4578e7e2b8bd561cd8f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:53:45 +0000

    attractive decision forward their number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_acceleration(speed, time):
-        return speed / time
+for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
