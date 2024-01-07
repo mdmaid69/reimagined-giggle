d4aaa831364395d423caf3a219ab823750e9f5cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:36 +0000

    pollution previous path high comeup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import getpass
-def get_username():
-        return getpass.getuser()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
