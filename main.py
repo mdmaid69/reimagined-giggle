c868bc7934d84f690fb13a50a521b608af808e68
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:01:53 +0000

    side his hard exist so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
