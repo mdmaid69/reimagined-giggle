febec9b81d6fab91643e1e2c59d0a6217f45a555
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:11:57 +0000

    style spite design stranger murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def add_numbers(x, y):
-        return x + y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
