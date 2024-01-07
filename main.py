a3aec360362100099fd188509ddd187b8cf9d2b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:34 +0000

    tonight hurry only handle control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def add_numbers(x, y):
+        return x + y
