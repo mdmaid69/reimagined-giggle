c8a66c12de924c24ebf2585d93c6dffccf17b0c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:00:01 +0000

    ready raise event estimate sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
