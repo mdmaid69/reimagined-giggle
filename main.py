e9c846a7ff7c0b881125404d12815c8978273c3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:29:54 +0000

    doubt OKorokay as check getaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
