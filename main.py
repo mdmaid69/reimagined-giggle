1233d107516f6eb2c45f4fd3764b5c33fee91d26
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:49:07 +0000

    birthday exist gate total admit

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def clear_array(array):
+        array *= 0
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
