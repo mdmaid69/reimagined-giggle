eea9ff85b306891ee4efcfb2e748358f5c257ef9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:01:27 +0000

    tend union 566end time jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
