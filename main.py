0dc47b55b377c95e3dafb1a6b1d8da8232a88b73
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:31:35 +0000

    Itself brief middle broken line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
