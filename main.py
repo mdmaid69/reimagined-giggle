2f05ee95de7f27a3547acb2536fafc548bbc2158
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:38 +0000

    mine spring sound shock wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
