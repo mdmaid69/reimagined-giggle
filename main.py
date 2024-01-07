de0fb662e4f4bd52423d704af53f0b52fb10ab1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:35 +0000

    distant comment opinion reach copy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
