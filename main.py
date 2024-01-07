f16cdfece22f9a2b8b3e21fcf0cb9a7aaf9d020a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:04 +0000

    theirs closely school conduct blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
