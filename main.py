4c0f680e60d0c5e5649d35744940637a091dc875
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:53 +0000

    emphasis 573entertainment mystery poison Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_typecode(array):
+        return array.typecode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
