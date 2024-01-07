c39e9c7f1d6b10daaa8909179715bda00376e0fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:19 +0000

    recover word report boil custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
