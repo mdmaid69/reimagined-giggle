6f48d3025982c7718497925785cf27624ba87a87
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:16:11 +0000

    nowhere life tight beautiful check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
