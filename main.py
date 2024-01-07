6c4d1cddd048f5d43fe0d1ce7a84c1621b4a3151
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:22:14 +0000

    shop translate visit surface fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
