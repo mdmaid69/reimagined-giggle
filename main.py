3049760209ecb478cfa89884e3c4cdf02c842b8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:03:34 +0000

    eye stir soak borrow kind

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print(sum(range(10)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
