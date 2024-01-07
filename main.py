ac833473db031032d740a98bc294bd597d2396bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:01:51 +0000

    check wide course wide apart

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
