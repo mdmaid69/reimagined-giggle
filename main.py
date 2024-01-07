2fd43c6f5cf745f0474569c98df956274d65a6b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:45 +0000

    later world offer simple indeed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
