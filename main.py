a769a0183235bc52a84a27c7c19550cfdc1a221d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:29:46 +0000

    further alive birth hold organize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
