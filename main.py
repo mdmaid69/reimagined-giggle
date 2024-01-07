eadcf667efa82f1fb9ba5353440e07535468ab36
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:19:46 +0000

    rush pity hope lunch free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
