ab44ef75a2584b68b9270fc00f4b2f8e014495b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:06 +0000

    seriously street sign next legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_str(array):
+        return str(array)
