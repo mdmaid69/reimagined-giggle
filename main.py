6c904c2bf834492f4af2882fcfd72755a04a9b70
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:14:14 +0000

    check Ii report age exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
