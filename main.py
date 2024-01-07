d5209ef75e3db43408545716ef687fae59657688
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:04:22 +0000

    number compete height play good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
