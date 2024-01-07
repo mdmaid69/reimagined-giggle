313fb86bd994521f4c9c35b9472fbf320e1e374b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:28:30 +0000

    state generous wide hurry number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
