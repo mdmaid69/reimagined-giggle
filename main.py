f29af43576f5787c8bda03fddee89f06747ee018
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:33 +0000

    steer line lunch lucky fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_cosine(x):
+        return math.cos(x)
