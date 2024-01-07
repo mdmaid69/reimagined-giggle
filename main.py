a2694323e91955b637590d9a35ef432c6d2d7fe2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:37 +0000

    sleep lean dull lower considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
