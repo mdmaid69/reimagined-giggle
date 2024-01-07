0e2a0087df0d63d44dd293ea6c6a38e69c018931
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:00 +0000

    freeze noone minority hour pause

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+print(sum(range(10)))
