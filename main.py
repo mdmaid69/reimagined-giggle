cda7d9cd0515dc2406cdc1d55b7dbcedf21dc21c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:59:33 +0000

    event contrast coffee journey pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
