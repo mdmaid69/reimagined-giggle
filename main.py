43bb533e0e0f4b9c28a1ed4c9eadedafb0d0c60f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:00 +0000

    regional except recognize attach exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def convert_array_to_list(array):
+        return array.tolist()
