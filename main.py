b5c43eed4dc671c1bea57242f187fa8d22c51541
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:54:18 +0000

    sick week minimum material worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def clear_array(array):
-        array *= 0
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
