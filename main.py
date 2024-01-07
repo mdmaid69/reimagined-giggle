b8102824e49348117c10921a84179e991ffc3d25
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:08:42 +0000

    tell work drop lost show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
