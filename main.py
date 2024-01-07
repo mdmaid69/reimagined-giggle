6986cddb7b182af57ca7ac0b2d6f68569e59c96c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:15 +0000

    steep no tense day few

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
