7d0ab53e6760b265ed2374b1239a6c4424d2a87d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:27:45 +0000

    might personal field wherever choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
