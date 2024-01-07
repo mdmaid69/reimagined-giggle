e6e7b2ded323c2e340349df1ddb569de584c4dfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:18 +0000

    contact yes want buy range

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+print("Hello, world!")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
