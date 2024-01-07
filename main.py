ab3d46abc78d809cc87c908043c89949cbb1dc5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:00 +0000

    respect middle completely load house

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_user_list():
+        return collections.UserList()
