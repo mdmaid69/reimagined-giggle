577b392bee1b99266837ca6549662f95f1348d86
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:47 +0000

    bus lead centimetre them body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
