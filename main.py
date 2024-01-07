ca58d32c4898a30824ad97a49ff7553dc99979c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:40 +0000

    swing share unfair spite precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
