c74beb9c8f0985429fddd31b7eeebd7c86c2e64a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:28:21 +0000

    sensitive artificial goout ring up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
