a5d12888a1f3d99aa9593d1f27ec0170f6ccb043
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:46:30 +0000

    steady string calmly else freeze

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
