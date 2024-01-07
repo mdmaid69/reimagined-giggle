ff62523d713338c8ec0c81b192c5aea73bdc3041
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:47 +0000

    alive formal attract range heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
