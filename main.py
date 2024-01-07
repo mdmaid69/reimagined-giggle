4889ca2ebb3c752685055a2fd9508c8e0c914a63
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:17 +0000

    accidentally wash slip left last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_power(work, time):
+        return work / time
