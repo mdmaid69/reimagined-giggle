bcc8f04f02b8974217c919e7021aad69ec74c76c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:57 +0000

    mainly lot limit true size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
