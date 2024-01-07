1fb98f181d04a08d9bfdbd3121d852b98aa69dc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:19:59 +0000

    none instead usual apologizeapologise use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+for i in range(5):
+        print(i)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
