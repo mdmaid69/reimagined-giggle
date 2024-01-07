d9e2a319a77a9d4bcd4d8e9b7ffce2524c3abc37
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:23 +0000

    warm alive favour benefit pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
