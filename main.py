1d7072e6dd1de83d8128f350ac63e19006994868
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:50 +0000

    record hear importance noise underneath

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
