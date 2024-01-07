c5f8861acd787da9ef9aa65c528ee50a8d537242
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:51 +0000

    money violent simple confusing mysterious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
