0204d1eb15f09b595c310d0f4ebca1370960fe5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:31 +0000

    long culture loose mark official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
