6b7d381db91c6f99b44f20b1dabac551bb830f23
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:45 +0000

    answer either service simple central

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
