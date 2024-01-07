f8a1db1287684d3600c995dea215b301936d6d80
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:59:22 +0000

    bother mine stay difficult ticket

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-print("Hello, world!")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
