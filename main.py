951c7d8977f367bfb4ac2be64a23ede9f59b2781
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:10 +0000

    suck disagree piece answer indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
