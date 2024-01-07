f78faf8a4a0e19d8a8a5b2cbfa0ed6116bef63b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:45 +0000

    exist afternoon turnback art wheel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_power(work, time):
-        return work / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
