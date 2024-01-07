a81c9cef1f296c791b0ab99dc3a6f2f69c98731f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:26 +0000

    upset cheque difference choose inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
