71c82e9839441bc59edfecbda3815725d6bd778f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:16 +0000

    both feel state after lean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
