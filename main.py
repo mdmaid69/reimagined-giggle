f46538442604dc86ec519d431e421bfad40a1df0
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:11 +0000

    mark total comeround talkback closed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+print([x**2 for x in range(10)])
