6281dd0b61aa7427f4c51ca876efd539473359f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:38:23 +0000

    laugh disagree active exploration strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
+def find_min(lst):
+        return min(lst)
