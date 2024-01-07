24278c64320fe1fd70a2dccce31dc3b0ac1efe6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:33:19 +0000

    service instruction piece turnon claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
