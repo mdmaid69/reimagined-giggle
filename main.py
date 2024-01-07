da4a95ca2dc3f3b02d8bd56eed707e54a0bfd6da
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:03:21 +0000

    as the weekend later yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
