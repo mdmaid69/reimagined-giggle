379bf41763fd13fa4d26c63a5a639706e52a5857
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:28:57 +0000

    formal wall snow creature blind

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
