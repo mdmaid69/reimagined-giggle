ffcd4f7e6eed635d74083f8fb9a43f887e259b24
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:25 +0000

    rather shop taste circumstance suit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
