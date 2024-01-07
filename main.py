57ab7ff5c74ec172d89bd9e959285066b024d407
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:18:22 +0000

    crash appeal track image back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def square_number(x):
+        return x**2
