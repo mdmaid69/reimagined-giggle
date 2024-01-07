dfe1694fc7779d3fe95f788668ba07b398417f1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:16 +0000

    water anywayanyhow mysterious finally inside

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
