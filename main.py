fc7b920a56f11083d35725bfb87db03cb9c21f98
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:27:15 +0000

    long plan where hand move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
