ecb12d8fa58a061bb7674613b07ed11adbe1660a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:26 +0000

    breakup affect meanwhile us future

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
