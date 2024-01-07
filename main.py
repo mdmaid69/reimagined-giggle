718cee7e4dbe971d21615752528c6238190824bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:56:38 +0000

    problem clever stone either crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_factorial(n):
+        return math.factorial(n)
