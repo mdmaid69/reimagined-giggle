102bc108eedb1db7067f0ae9f28553117f837174
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:33:53 +0000

    scream blow list reduction switch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
