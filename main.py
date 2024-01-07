2bee44fb8c5f739b2cbb68b215f30fac370afc9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:07 +0000

    deserve bunch attractive hope combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
