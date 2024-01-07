02406b9d25499bf3fd5e2ba95b526460df70e13a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:45:29 +0000

    clever increase surprising keepaway calmly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
