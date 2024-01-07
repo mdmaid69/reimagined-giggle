5d38d05c9f1f3300f347fd035f54d02109713e03
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:52 +0000

    fasten concentrate chance attempt discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
