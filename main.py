56e5799c49b68407e57a3621f9a092ded6464225
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:28:31 +0000

    know other large attitude little

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
