5982179704953e8616a69a45d6a22ec4a9dfc4b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:47:13 +0000

    major body cause blind meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
