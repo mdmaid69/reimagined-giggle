b8253fcd6e20d3ef108672b8ec785bedd5b89910
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:29 +0000

    pleasure favourite discuss calm disagree

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
