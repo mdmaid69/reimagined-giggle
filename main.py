4cf0f5d7b542dba19da57d63938939c30df95199
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:34 +0000

    sudden threaten pressure waste game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
