0b10d368bdbdc98b371f09735f317069b3476103
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:08 +0000

    Oo drink pretty soft clue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_speed(distance, time):
-        return distance / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
