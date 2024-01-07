0f50ac7e20467a2ec7e7635e4f2bd7c6c06a9dab
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:45 +0000

    have seat shop discussion shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
