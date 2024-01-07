dee8ab6197b238fce4f013a5ac8695c8c3970648
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:54 +0000

    tip knowledge waste keep eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
