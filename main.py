fd21150a222225107ce4b4c9920709b0a85da29f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:45:14 +0000

    bush discover right off perform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
