7d54e3026e6e78725ae08a1ac9210656f2f902e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:43:37 +0000

    relationship attention Oo ilovedicks even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
