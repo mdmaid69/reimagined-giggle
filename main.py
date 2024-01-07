85ee957aaecda4627cd981832007db7de4917ae2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:28 +0000

    pot fix tap compare argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
