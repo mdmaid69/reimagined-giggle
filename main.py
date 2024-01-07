cf0efe85a1b2e6f776519e56a727f6acb6c00039
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:46:12 +0000

    force thinkover scream tour cry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
