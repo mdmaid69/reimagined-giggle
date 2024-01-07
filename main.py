7641a8a279bb510709d96c8a748df624a39ab16d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:04:46 +0000

    makeup quiet formal tax often

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
