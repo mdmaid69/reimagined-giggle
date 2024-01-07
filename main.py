44f6fbc2a148670031361108c3ff370a64751234
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:37:36 +0000

    note concern large quickly soon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_min(lst):
+        return min(lst)
