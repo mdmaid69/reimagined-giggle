14602574d71c5d3fba5d446f19515b8d423b8735
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:07:53 +0000

    try juice sentence scream start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
