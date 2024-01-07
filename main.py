5c8bbfd1d25dadc43eda956d8d1b593afe81affa
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:53:47 +0000

    block dress shoulder declare guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
