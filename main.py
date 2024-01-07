668d972b4958e9fcf6757fdd0011cddca72f913f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:42 +0000

    deal appearance curious post Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
