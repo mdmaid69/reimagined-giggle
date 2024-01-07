77a82e05fd738c88c53c0a4d5da9b9991b7497ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:32:38 +0000

    solve contain Ll opponent sort

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
