26415f0eff5c9d10db8747fc4636fbe9caac2873
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:55:58 +0000

    forward disappear dry tomorrow practical

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
