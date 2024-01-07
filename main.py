dec480ac8ceffd587ea5a3b9ef2a805736af83f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:45 +0000

    sell admire pass getout beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
