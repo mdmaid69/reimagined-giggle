af3b0b13ba99c3e3a95f5fec239ff486462b4f04
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:37 +0000

    piece upset turnback noise neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
