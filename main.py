bf32add57054aa0b4d2294423aea20c44abf9f56
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:05 +0000

    game talk develop warm practise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
