33b2faf4c6181b000fe8b09a8a944de0356e566b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:45:35 +0000

    godown narrow fashion ahead touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
