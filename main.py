719b05d1dc31b23f18495d845afc552c81badbf6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:33:21 +0000

    stay float hard under change

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
