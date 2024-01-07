036bc6ec9782723237c11679e7e21d28ec1ce11f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:49 +0000

    deal clothes please feeling fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
