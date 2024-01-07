b6b773c2f1373047b3e4c7ad05c6de1d9b98b238
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:40 +0000

    priority period feel turnup turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
