e3a67c706c3a959b5e60115cf8155278ba076325
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:07 +0000

    behind food child of happen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
