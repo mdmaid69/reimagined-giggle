4383f6914749e8cb86e49cfe37899f89fb272c47
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:44:46 +0000

    hard finish near company fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
