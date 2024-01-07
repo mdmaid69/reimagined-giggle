f391792122647b28a50d0619bf3270136306fae5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:00:20 +0000

    precise start fire snow identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
