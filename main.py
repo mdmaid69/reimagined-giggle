9d5e8bf5266feeae4dd6df833f7b567c08e2f5fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:34 +0000

    beyond supply port provide pair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
