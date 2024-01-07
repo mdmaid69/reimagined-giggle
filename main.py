06c6ac76a906e8885d9d30bc38238fcbcf868f1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:07 +0000

    last wealth stick waste sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
