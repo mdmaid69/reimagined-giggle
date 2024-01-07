d4ba980ff12be7883763bff7997d7c0deae528ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:10 +0000

    walk remark left very reply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def clear_array(array):
+        array *= 0
+def count_words(sentence):
+        return len(sentence.split())
