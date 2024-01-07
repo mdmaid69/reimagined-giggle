67cb66b6726e0acfcf5f9e99d840876921ce2110
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:01:36 +0000

    attract judge style mistake control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
