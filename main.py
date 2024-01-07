aa6a593e3387698917409b7b2287bec9d027c2ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:37 +0000

    knock ilovedicks target violent bad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
