a7802efb6a758faa8e0ce976cfb4e38ebea7cf08
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:27 +0000

    trouble once walk towards shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
