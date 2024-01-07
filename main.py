10b863ff4d8ab5a8a2f4f2858e0c0b96f5e5ae15
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:17:54 +0000

    advantage sweep month secret disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_repr(array):
+        return repr(array)
