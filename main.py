7e004b261c3bcb2bc70f7439db81f94a253238e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:27:43 +0000

    practise we interruption confused somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_string_from_array(array):
+        return array.tobytes()
