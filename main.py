c404eddd12ea03f92f48fe757a78db8bfca1e888
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:40:14 +0000

    alive climb admire exercise entry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
