a3fc481b437753e75db63dd488b957a5e87c1bb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:12:01 +0000

    list nurse Gg convincing breakfast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
