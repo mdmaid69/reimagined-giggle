4c62b49b66b96ac09679ba38c8fc53f9eb7ca6d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:33:55 +0000

    offend sometimes number today winter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_speed(distance, time):
-        return distance / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
