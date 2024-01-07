644967e9c25975c7f53a61faa7cd6f37873ab7a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:10 +0000

    combine usual modern take sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+import datetime
+def get_current_date():
+        return datetime.date.today()
