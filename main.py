fc5e29d37140a33c90057e1b1141e4196554ed7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:11:56 +0000

    style check brush available solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
 import sys
-print(sys.version)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def add_to_python_path(path):
+        sys.path.append(path)
