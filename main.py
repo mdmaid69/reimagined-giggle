6423c9809b213cd0fca911aea1c1d2bf20632199
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:04:07 +0000

    gooff wait similar instead persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
