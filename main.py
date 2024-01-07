bcb83983de448cfadd1a443e11cab5b23fbe3c21
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:01:38 +0000

    ought feed wise squash record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
