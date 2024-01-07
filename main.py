6280c174d1ea76c47912c3c3934683c50629de7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:37 +0000

    issue promise move within union

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
