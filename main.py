3a0644bd5afa64e5b365d56d5192c00c486a1899
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:54 +0000

    border replace handle basically telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
