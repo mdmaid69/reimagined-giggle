d8a7235a731c85eace87f0c5a786db52b54f8ddd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:49 +0000

    total somehow dear shoulder fast

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def convert_to_binary(n):
+        return bin(n)
