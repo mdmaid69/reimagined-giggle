8454a13777c14cf0c0c5aeee32f3dc6793495c0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:46:54 +0000

    under flood justice male have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
