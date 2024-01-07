ec2daff58cc8bb1130c65897b3234dcc52ea1242
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:02:48 +0000

    sentence everywhere drop upwards lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
