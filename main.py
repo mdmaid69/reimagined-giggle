882420996b3c8aef78941e47ff529cbf470f5eb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:14 +0000

    apologizeapologise with rule search further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
