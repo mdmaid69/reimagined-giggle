04bf1dc8a5de169fab1193c5e2c720c5b6fd644f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:59 +0000

    zero glass inform wind square

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
