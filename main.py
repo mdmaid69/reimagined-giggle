63b4bbcf4009dbd0369b70791dd53e030835cd07
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:55:27 +0000

    interfere each dream several order

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_bytes_from_array(array):
+        return array.tobytes()
