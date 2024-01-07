702b8395b6611f705868202d216c331fc72568c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:15 +0000

    plan issue label influence bunch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
