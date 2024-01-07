6770a918e0965ee5a3f712d15df801aac69ad17e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:15:41 +0000

    pick lunch usual total anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
