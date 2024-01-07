e5efee3f66416b8e963783fb96d3f3998ba17784
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:57 +0000

    underneath temperature getback part command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
