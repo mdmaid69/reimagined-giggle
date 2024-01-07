15a13174edeb71c41653b45ae23f6c41b454f032
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:50:27 +0000

    guess board pretty turnup over

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def generate_random_number(start, end):
+        return random.randint(start, end)
