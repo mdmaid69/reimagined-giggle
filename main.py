add22e0a32f64d942c6e0a3435fb7b3bf49f963a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:20 +0000

    back easy leg difficult getaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-print(math.pi)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
