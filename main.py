c4a1c27d4e44d01e173d99cb77c2e93a3a590a51
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:14:33 +0000

    temper become single understand copy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
