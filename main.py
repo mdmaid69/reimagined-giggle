a8b3e6003991404094668631532d8f5c9250cc0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:05 +0000

    announce shine callat talkinto miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
