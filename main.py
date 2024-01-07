c6ea48f9f387fdc41e2d1ada8d0a58461cfd7e27
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:11:38 +0000

    colour difficult accident through cost

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-print(sum(range(10)))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_to_binary(n):
+        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
