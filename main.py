c4208b1c4a580068d0af6e2f24749f9c5b3fdc7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:48:27 +0000

    common creature keen branch calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
