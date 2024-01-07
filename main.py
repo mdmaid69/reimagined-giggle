d3a0a217492270ed041bec6bdcd26e13ed5fadd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:59 +0000

    there concentrate human late half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
