7568eabbc960af7166a457c3fe70d25b1816774a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:15 +0000

    before hard holdout male plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
