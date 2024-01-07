9bc5ebde36c22ff09d43db7d18d527a606fb5141
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:46 +0000

    shut once regular which discover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def convert_to_hex(n):
+        return hex(n)
+  def convert_to_octal(n):
+        return oct(n)
