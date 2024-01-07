6d45c8873037d32fec775b701b8caa3586cb8e2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:56 +0000

    rid pair mail situation shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
