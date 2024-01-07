4891dc92475b02371924679e25d5d75a55fb8c75
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:56:27 +0000

    dig than search fear they

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_unique_words(sentence):
+        return set(sentence.split())
