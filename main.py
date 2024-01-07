66cfb6c848df052534c38f757f1c88009eb02530
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:32 +0000

    separate settle left but ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import datetime
+def get_current_date():
+        return datetime.date.today()
