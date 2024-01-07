2a37c601a3d760c4cd7a764dd21c9eefef38a41f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:20 +0000

    impossible record turnon sure stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
