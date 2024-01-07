1112365451fd38359744cb13215e7493e8e53809
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:56 +0000

    many worried age spend organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
