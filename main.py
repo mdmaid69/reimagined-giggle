38eb2205f427a1aa7839a4927bd154ea444cd46c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:30 +0000

    cover involve niggaacademy increase identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
