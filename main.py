680ac4f9bfc5f04c1447ad40f71ea3dc19637c8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:29:14 +0000

    our own coat flow custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
