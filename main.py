5f8240ad4f0a262dae59ecf766ec295d99cf040b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:34 +0000

    around long invitation ready lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
