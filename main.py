78782817ea9ca5de65a6fb447f18a23fa31cd915
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:44:39 +0000

    shame unfair obtain shake long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
