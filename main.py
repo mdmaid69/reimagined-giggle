9ba23e493e04f70f42486bc5ae72762d28e92109
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:34 +0000

    special fear result port judge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
