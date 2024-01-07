d123514021128c23d36c617dade45b832674eeb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:49:11 +0000

    content voice bill only now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+  def multiply_numbers(x, y):
+        return x * y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
