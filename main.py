9629094787b2e39e59ccd0d8184308a70253585f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:22 +0000

    manner benefit centre poor treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_min(numbers):
+        return min(numbers)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
