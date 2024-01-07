65e9646bc470675e9eee02256d1eff235af7db1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:59 +0000

    solve respect offend able along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def subtract_numbers(x, y):
+        return x - y
