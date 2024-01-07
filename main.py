58bd6129be319871a13d8e7bf7517a12f28d0f4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:47 +0000

    definite breathe fast describe how

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
