965ec808dff69316841fb97474bc6fb062695558
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:04 +0000

    argue sort ask equal chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_current_time():
-        return time.time()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
