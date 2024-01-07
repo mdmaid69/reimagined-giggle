684791e8daf4b962ed2e00e8e5c2127bc4ef4122
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:50 +0000

    flight rest exist unit limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
