b3169d2d3ece7a759ca71d28ca0d92dda7991ce9
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:09 +0000

    spoil march suit drink move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
