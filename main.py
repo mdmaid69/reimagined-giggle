83b5854b811274aac58b1538eb1ffa3d7ba1f8db
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:06:07 +0000

    never repeat final valuable left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_length(array):
+        return len(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
