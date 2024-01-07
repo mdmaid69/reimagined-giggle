e90d973ef05fc72df71ff122a00494b90eb239f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:35 +0000

    unit section amount likely skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
