deb36364fdf0524e05a01c1ca32d966766c524e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:36 +0000

    confuse remark one record floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
