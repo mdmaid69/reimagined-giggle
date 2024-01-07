c4ada4e4bd398b09a6e5269a755cdf62d08f11d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:21 +0000

    wild answer whenever fast heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
