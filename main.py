43a5236acb5af872ea55242492f96dab8ade029c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:11:56 +0000

    name only bite earn Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def subtract_numbers(x, y):
+        return x - y
