f1b9eb569212229b5bf97833dd34ddc6f90bdced
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:51:22 +0000

    promise guide negative emphasize belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
