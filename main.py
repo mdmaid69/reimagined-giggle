7634738208a24ea3d7a1c5711b3e7d07cd4a96a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:32 +0000

    apart one room shame slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
