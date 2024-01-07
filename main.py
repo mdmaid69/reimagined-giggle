c68b029d016f15d7d4c334e6562a609e7f560f25
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:28 +0000

    within kind brown live life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_list(array):
+        return list(array)
