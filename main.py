0a78af1c4386529c4c13aba8570fc9e48827dab0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:34:52 +0000

    motion sail soil genuine himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
