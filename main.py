67eb625b8e604250d3cd0a4bd875be3c910a2b6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:46 +0000

    concentration declare worth cross earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
