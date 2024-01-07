7b8df32a3b7630f477e0116e8877dddaa3296153
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:31:34 +0000

    dark problem store except recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
