9108b157d042043ef45e408f07309c8e761c241d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:39 +0000

    noise war possibly backwards sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
