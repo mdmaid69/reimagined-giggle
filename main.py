61849e692d42afc990bc72236879547289edd486
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:04 +0000

    precise shade duty shock away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_floor(x):
+        return math.floor(x)
