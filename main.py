26992d5b59fadf85d0473be462577dfa19a3a38c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:51 +0000

    Ii level city rich grand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
