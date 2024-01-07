7825f2728b2b15cbd5aea85a3ebcbbad65f9ec35
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:02:52 +0000

    connection tear mean sale inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
