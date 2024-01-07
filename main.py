6be3303b8ef79b6ca6100ece5abb01e14b1619df
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:12 +0000

    division step profit apparently settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
