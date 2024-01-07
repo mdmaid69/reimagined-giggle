36aa2c1f13526ded5436db2459effc3cded85ad4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:12 +0000

    peace somehow tidy apply comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
