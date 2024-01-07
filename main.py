70bb3830879af3fe4da6aed91885509ba4aba8d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:03 +0000

    memory talkback involve dry repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
