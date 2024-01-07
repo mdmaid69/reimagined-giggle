62bffc408f281c547c03534962dcdfb8fc045df0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:01 +0000

    and 569enjoy bread clear cope

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import heapq
 def create_heap(iterable):
         h = list(iterable)
         heapq.heapify(h)
         return h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
