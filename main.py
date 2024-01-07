a6518e94fe22d85ee5d1f5c140ccdc87cec943a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:35:33 +0000

    whistle reply onto responsible stupid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
