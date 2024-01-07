046c4fe246c8e23964bf7f050a5cb8e49b419958
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:52:40 +0000

    plenty actual among perform violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def convert_to_hex(n):
+        return hex(n)
