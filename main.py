97a124d6ca5bcd0afea76570e6bd13707c6828f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:44:03 +0000

    pipe rough support oil prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
