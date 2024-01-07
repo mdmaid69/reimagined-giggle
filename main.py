a027bdb8e43f8f85f026d32803e320a316f931b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:28 +0000

    high sign cruelty minor ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
