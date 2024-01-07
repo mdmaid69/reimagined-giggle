cf1deb94399f97e3a717ad81d1b5f3abc8de6a07
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:49:43 +0000

    past spite trust pronunciation record

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
