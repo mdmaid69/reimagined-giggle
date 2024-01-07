59296080591f5819d76efb7a4adfbd2bc5703b5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:37:10 +0000

    scare concentrate mysterious weak mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
