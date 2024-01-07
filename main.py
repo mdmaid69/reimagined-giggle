57110d2a3e321c79543896ff31e5118c6b124d4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:17 +0000

    sit welcome defend position floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_speed(distance, time):
+        return distance / time
