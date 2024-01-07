4616020550422a914ab34d23f35ff6f0ca3f7ecc
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:57 +0000

    exist development scare aware worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import re
+print(re.match("h.*o", "hello world"))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
