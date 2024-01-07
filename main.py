6d439d52ba761b43b9a9a5e5ad2bdae691d24125
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:51:55 +0000

    usually partner invite clever plain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_error_function(x):
+        return math.erf(x)
