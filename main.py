2251fff95045b8cf93b329830e942ebae07cdf17
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:30:04 +0000

    worse side fail race programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_unique_words(sentence):
+        return set(sentence.split())
