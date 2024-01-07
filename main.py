7c1b41cbe68bb06aaf26e83c4a62ebc66d8ac9b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:46 +0000

    game little gas low into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def convert_array_to_bytes(array):
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_string_from_array(array):
         return array.tobytes()
