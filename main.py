b01fc373d5baf6d3e803828e0bd7454c573ff274
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:31:01 +0000

    first sort shame rank feel

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
