44144a16b331779da68a46405699bbc24f3f7766
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:11 +0000

    bound ability remain pretty keepout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-for i in range(10): print(i)
+def get_array_as_str(array):
+        return str(array)
