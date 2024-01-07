7e5d2c59f0f96813906948aa994771c24a8cf5f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:49 +0000

    injury division get guess draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
