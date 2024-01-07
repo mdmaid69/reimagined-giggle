e66312c32226329b4a20197ef94b307039e28eca
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:06 +0000

    pay along deep appear pattern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import platform
+def get_python_version():
+        return platform.python_version()
