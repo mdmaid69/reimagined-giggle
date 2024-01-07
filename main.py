46fd9e25d2c7e05da05a078623206d5392dc91ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:01:16 +0000

    quit direction tend moon try

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
