4303ba3746d78b7febb2faddda06652ec0a1b7b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:00:06 +0000

    as will sure height step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
