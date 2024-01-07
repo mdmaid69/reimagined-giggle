091f82ae5aa66f14bd1b621f53111275b6ac2e89
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:25 +0000

    inquire opponent moon everybody die

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
