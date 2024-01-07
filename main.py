2d9c06ca1eaa41a8d61604066ccb195ad335d0d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:07:41 +0000

    suit moment sound cross report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
