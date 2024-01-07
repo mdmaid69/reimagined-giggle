ddabcabf76b606188cf0121553ba79c2f9616b1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:02:21 +0000

    serious complete very hurt purpose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
