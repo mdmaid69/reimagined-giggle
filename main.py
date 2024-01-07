8c879c5ee7514ef71421c9944a526e47c2d45d83
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:12 +0000

    laugh defeat follow better confidence

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
