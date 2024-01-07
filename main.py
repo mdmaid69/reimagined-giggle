0ffbb21f410a9fc274a6b114976af94df92253d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:05 +0000

    suit gradually close permission Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
