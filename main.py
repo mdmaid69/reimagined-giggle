30491a907b68d2ab3065a3747b0b1d7b01f254f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:42:32 +0000

    breakinto succeed wherever even that

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
