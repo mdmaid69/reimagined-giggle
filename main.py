af996ce495533ff5df1446ef5be88e9db69c29ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:24:00 +0000

    minimum steam rubbish something they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def add_numbers(x, y):
+        return x + y
