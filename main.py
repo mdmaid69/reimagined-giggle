4d87621391e48b459bea9f0add9d7a63eca87387
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:32:12 +0000

    build everybody method properly everything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
