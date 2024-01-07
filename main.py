7d7b19e3c258f27f7582a7668f708c499b3966ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:09:31 +0000

    name constantly seat fact autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
