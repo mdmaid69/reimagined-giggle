e861859d620fb7468f724a095c76b8135a54aa6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:13:30 +0000

    switch state patience base appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
