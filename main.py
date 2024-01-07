db2079f0086e890ee11d72d6cfd977dc3ac94c47
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:16 +0000

    tend convenient full hate convenient

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import random
+print(random.randint(0, 100))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
