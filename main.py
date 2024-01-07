0a701f96f111830adadc09cac400fc279808c31a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:09 +0000

    sore spot now so since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
