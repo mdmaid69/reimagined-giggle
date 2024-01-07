60ebf9468ee2265eb53a22d6093c406cd7ba051e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:04:19 +0000

    away firm vision myself reasonable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
