3b335b1457049299349a08fb2d117e40d884e7c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:42 +0000

    past type phone plan admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
