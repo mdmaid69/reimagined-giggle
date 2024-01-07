59eb7cbe29da2f7abf473d7e5f31fcb94dde2c1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:17:25 +0000

    care future weak along port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
