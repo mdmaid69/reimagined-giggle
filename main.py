5bc6aa9906878ce580608a247731fd3efd83d90f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:20:03 +0000

    trip cross lookafter act hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
