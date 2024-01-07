f961792d134d4d8eace5d4f23865e9636f9ff3f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:36 +0000

    slice pleased she contrast stop

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
