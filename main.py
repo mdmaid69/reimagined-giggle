22838288929cbe9f443083be51406f1bfeb4d67f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:58:47 +0000

    reach tape charge model suggest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
