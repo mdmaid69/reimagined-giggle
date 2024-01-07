0d0c4ca1199f5189f5de11a2403772621bcb13c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:21:49 +0000

    pinch platform single culture punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
