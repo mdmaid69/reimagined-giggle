49945dd798a47b75d0e1039bec4e0c996d58fcf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:54 +0000

    politics ocean reduce free quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
