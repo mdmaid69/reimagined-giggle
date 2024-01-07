8aa43bb9064b502c39c794785e230bf954461ef0
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:08 +0000

    lie bar confuse affect unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
