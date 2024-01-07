7dc16a900495147792dce2c69e2e4a69b834bb96
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:01:04 +0000

    live to stamp sign worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
