8bd1b3d3a8ba34c1e47cb01328f9861daa502b3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:16 +0000

    lost suffer but land dream

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
