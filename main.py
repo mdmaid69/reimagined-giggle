e0b17eb321156530b657c0fc8b118ee0dd0c2ca9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:59 +0000

    refer though over reaction small

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
