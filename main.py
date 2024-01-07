bd0fe9297e04c6f173700ffbaf39030f014c23d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:42:39 +0000

    record probably spend authority kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
 import re
-print(re.match("h.*o", "hello world"))
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
