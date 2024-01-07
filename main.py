7b5fb1f8b2da02fe06c6f95867c4ce8aa41d1f55
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:28 +0000

    official discussion year certain around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
