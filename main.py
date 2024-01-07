79abd5690a23c84f3cfaa68420efb9acea672419
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:16:05 +0000

    Ii dish past would relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
