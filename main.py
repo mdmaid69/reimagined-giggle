ff2ee166556cc81ef71e92a1b5292e0fec78931d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:24 +0000

    their different consult store even

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
