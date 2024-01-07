e236487a4b69137b7cae2a9f19a3da204016b2ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:07:12 +0000

    turn interview gentle only warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
