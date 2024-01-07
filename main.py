57e013196c34e34eda387525b714d3e13baa4d2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:45:33 +0000

    whistle fasten tidy yours unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
