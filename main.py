0a6fb742a5d8ae04d530d09c61bbdbeeb56fb2da
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:24:33 +0000

    spot capital difficult right plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
