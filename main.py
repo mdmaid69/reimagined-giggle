72dbf0ee199691d799ea4fa29218dafda626b241
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:15:04 +0000

    cap bend average stare cover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
