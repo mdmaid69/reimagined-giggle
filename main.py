8aed8f990b3a1683cea1b8e3a65f89fa9ad18595
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:19 +0000

    character consist come desperate computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
