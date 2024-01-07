6f3b540f8ca57766f47ebc1000338618757775e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:31 +0000

    tip short town exam on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
