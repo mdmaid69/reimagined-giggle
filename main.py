c3e96643a079ccfb0415e0589db487b44d96d386
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:59 +0000

    left however club consider play

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
