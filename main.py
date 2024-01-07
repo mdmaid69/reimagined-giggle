3f54d8f673b35dda39bc102cfdfb8bf681b3f0f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:37 +0000

    less manner region waste excuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
