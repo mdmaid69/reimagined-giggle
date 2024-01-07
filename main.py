7d3029ea08d97a8bc463319b77b3650e578221f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:25 +0000

    city block authority thank die

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
