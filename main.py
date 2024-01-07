f7cf9f09a1c941e73ada89015fdc934d438521fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:48 +0000

    cost godown remark intelligent insist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_work(force, distance):
+        return force * distance
