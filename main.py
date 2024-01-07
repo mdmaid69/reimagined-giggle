796572a7fdc11d5aaecfbe4ed7b0cb9fd56a1b46
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:47 +0000

    Ee strike often distance separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_pattern(pattern, string):
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
