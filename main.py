0157e4f9ff981b7108d814cc236c09c77ff1fd65
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:22 +0000

    second on use major swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
