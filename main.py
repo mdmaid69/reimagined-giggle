2e4dad9c02edc162176dec561625d2ac90587ba4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:01:08 +0000

    last fear curious bush can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import random
+print(random.randint(0, 100))
