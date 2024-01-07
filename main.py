08bca8eed05e3fa3328c5b049680a6062e20aa7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:30:52 +0000

    memory silent of want serious

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
