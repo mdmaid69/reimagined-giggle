6c9a343e47e70e83cf55ca07e33438b4f68f9867
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:59 +0000

    rush would Qq new low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def square_number(x):
-        return x**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sys
+print(sys.version)
