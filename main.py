519a7e1537fabb703c64a2cee8fe5d951af5c6b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:18 +0000

    yourself depend walk lookaround impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
