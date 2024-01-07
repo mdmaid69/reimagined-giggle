df92e2bb81c5e249bf0da04dee3ed470b40faf3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:47:31 +0000

    stop behaviour may window responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
