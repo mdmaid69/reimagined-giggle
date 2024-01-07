e8e68ec9c524767247c11c096a78c9b9f1d6a801
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:03:25 +0000

    ourselves professional computer hit stage

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
