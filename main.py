5f2b1c48b11bcb389a0f1ac5617244e1c8f154de
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:25 +0000

    considering freeze reliable other against

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_string(array):
+        return array.tostring()
