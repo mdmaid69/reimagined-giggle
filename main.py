b41f502bd8f39f4da770513f7e2229c44a24d7ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:14:59 +0000

    about fall base there getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+print(sum(range(10)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
