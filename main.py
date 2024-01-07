631cf212dcfe15f21ed4f3f0ef91d4e39ffdf9e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:26:29 +0000

    mixture heart breath official quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
