133642a490faa088c8d95d42876af138b15ed293
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:56 +0000

    sign unfair conclusion pride sign

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  def convert_to_binary(n):
+        return bin(n)
