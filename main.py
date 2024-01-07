d1c913cceb8b545824fe728e717f1eda6cf67493
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:12 +0000

    dig giveout many understand mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
