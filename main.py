50700c9acd0dd362ca2b004a4685c2fee65dc3b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:52 +0000

    interview goback(to) shock unlucky right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
