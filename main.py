c71be68468b2bf4e51ccfb6a48470758be301ce9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:20:46 +0000

    goodbye 571enough confuse class note

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
