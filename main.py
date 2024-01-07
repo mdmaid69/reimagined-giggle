9f99c84e2d761de06dd6f633a9fc9bd759fdc905
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:54 +0000

    bringdown present convince lookafter influence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def convert_to_hex(n):
-        return hex(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
