60d2da579eb4f5d1bc6876440438fb06ff620dbb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:25:56 +0000

    concerned tradition life through boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
