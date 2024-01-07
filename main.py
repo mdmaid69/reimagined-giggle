a535e44379e44857a4c66d9f52e4c44255fddc59
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:38 +0000

    waste honest little interest have

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
