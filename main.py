1134eac8339a3f392bb1f6d611ee9922aab05e16
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:35 +0000

    entrance fail flat Uu dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
