1997d7de6f2deb967dbf0e92ccc6395afe8320da
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:22 +0000

    godown high anxious lonely second

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_max(lst):
+        return max(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
