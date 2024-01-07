aad5758c12c721be8a58edd93f822f98dd98d4b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:44 +0000

    congratulate bottom voice this however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
