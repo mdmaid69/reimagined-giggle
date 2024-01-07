d698bf2501e40762d96ed8202ce3e0f15421ef47
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:07:26 +0000

    free opposite watch time more

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
