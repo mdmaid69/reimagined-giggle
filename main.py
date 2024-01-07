54590556c4d51545138340293df25576b62c2203
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:26 +0000

    arm chat bus nervous crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
