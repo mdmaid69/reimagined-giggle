55ccb02bfe8968f4f957701fe1b5e88e210ba9ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:48:19 +0000

    to unpleasant meeting mystery exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
