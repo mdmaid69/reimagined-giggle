f79750a23ba36eea0011398dd99c74fb992c58cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:22:43 +0000

    know who variety catch until

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+print(sum(range(10)))
