6d27a7e5d4c512dbbdc08c1f2c40dc10e7e8d956
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:07 +0000

    exhibition bringup disappear live lucky

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
