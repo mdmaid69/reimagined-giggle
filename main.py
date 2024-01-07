cd678470d3792b5f4799e979b9e2227368926e7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:23 +0000

    always dull city extraordinary hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
