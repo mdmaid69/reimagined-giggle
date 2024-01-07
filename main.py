8d07066e571094243a46468e23893c4092a900d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:23 +0000

    medium tonight holdout far lucky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+print(os.getcwd())
