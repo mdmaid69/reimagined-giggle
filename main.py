1bde80dabc57afe4a50e4339acd13e71af2f8bf2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:13 +0000

    partner emphasis read clearly maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
