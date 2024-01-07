5f37f8c002acbaa263948c46c58487eeae93cb38
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:10:16 +0000

    schedule introduction congratulate contrast blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
