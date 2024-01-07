d96c80345a3ba6cedf48fe2a0dca6f7d68dc4cc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:45:35 +0000

    afraid habit while pump shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
