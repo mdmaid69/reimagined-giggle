ce8b373792a6b8c835f00f34031883b331baeba0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:55:17 +0000

    bar runover open waste Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
