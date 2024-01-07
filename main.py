4eff1ccd9d0d308eb0b4bfe0b14acdb36bf4f2b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:18 +0000

    cost swimming period bury after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
