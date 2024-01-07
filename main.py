01ea4c7946508f91da4d265d0aca28fda0002d40
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:17 +0000

    muscle show repeat turnaway decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def find_min(numbers):
-        return min(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
