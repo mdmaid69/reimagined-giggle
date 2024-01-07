dbfccef707fb59007e50f55ea7da3ef79c7bfbd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:20 +0000

    unless goaway 573entertainment refer confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
