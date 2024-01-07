adfd14278f02732225dac7d1307ca14cd8a637d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:26 +0000

    great spoil suit use slice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
