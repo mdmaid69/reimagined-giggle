024a5d7fa1527ad53ee8ab47283444dea7c620a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:51 +0000

    dear her that bottom offence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
