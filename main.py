14e5a94f9b75433ca8dcdfdaa811d0c26366d05d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:57 +0000

    afterwards difference place operate tip

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def divide_numbers(x, y):
-        return x / y
+  def square_number(x):
+        return x**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
