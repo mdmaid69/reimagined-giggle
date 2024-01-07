634e04377a13701274aec477b410c2778efef444
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:38:50 +0000

    be help correct thing cost

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
