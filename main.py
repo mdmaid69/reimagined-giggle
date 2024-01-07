0a170f041f0622b629a430cabb928260105ebd31
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:55 +0000

    actual Oo hour curious better

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
