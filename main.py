3444903167240c49a841c3f09fe016c03b371e89
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:48 +0000

    arrival be communicate embarrassing breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
