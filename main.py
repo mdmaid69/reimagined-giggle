2e8dafdc452e5a12116b227bfdb68528e6c82c86
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:59 +0000

    persuade slip order pile she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
