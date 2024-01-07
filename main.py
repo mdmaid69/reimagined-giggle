1c29ee3e1d2190388f034f54ceb511c006dac213
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:35:01 +0000

    unusual flat shallow noise thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
