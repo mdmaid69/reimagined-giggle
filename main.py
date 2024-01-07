0b67c1c307c928f836ed7dbebe22f1c788903e93
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:47 +0000

    message wheel cry back ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_user_list():
+        return collections.UserList()
