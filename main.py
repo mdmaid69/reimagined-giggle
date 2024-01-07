3b5110111ce20427793a8a20372ac5c1a6d6573a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:19:03 +0000

    back matter push open worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        print(sys.version)
