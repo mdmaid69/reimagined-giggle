98d91d495c956b6fec57fc71e867538a00e669f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:20 +0000

    colour voice doubt takeoff breath

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
