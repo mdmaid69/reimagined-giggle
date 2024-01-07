36e1c57a172fc9e67d38270bea689eccbdf1d8f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:58:05 +0000

    narrow strike present accident importance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
