d45826d38655cf3b28c214ac453e27aaf5d57443
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:51:00 +0000

    centre forward fetch instrument support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
