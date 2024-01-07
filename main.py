1e6749274ad09a10f32b9ef960d8d8934b01f95a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:53 +0000

    so measure early pity duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
