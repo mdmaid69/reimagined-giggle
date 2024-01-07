784e6cae8eb21444fbb53522691c785d697efe87
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:55 +0000

    shoot lookaround destroy run takeoff

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
