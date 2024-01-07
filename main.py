33d7d225fbaa3560f02eb44b2d7048a51f088b1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:58 +0000

    Ll giveup maintain exhibition bin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
