68816b1c9b5f8d3c24e1d91dd6fc847024ddad69
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:04:54 +0000

    time this several speech declare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def find_min(numbers):
+        return min(numbers)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
