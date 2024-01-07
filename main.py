c2d6f6e1bf1d312f5c4115baa31dad73ce3c2600
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:01:02 +0000

    loss popular like during lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def square_number(x):
+        return x**2
