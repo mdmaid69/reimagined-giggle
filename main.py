beba4fa844f7c5f849f5dc1c831b65341f5a1b01
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:04 +0000

    war wave recognize course circumstance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
