388b5d656b5963695d86861416a340cb1de66716
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:49 +0000

    exchange minor off along line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+def print_python_version():
+        print(sys.version)
