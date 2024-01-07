1bbd5eda2e5fe158592dedfae149a49f02133d3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:40 +0000

    compete boat standard watch announce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
