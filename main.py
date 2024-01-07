3bfa3893f798cdd045a657b0ccf412b1d232ae75
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:36:12 +0000

    knowledge hard idea collect plastic

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-print(math.pi)
+import collections
+def create_user_string():
+        return collections.UserString()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
