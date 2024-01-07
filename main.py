b0460f5a249c8a29a36eaab391cae28a5b19dfbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:17 +0000

    load opinion belong relative push

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
