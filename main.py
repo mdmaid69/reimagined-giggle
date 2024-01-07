352ff4944a6bf003b8a3de955a411dd5e3997ed2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:01 +0000

    split weigh actual match clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+for i in range(5):
+        print(i)
+import collections
+def create_user_list():
+        return collections.UserList()
