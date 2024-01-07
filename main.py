6cdf0c67013c924c272445bdf99708921b23bb49
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:52 +0000

    circle sort disturb float finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
