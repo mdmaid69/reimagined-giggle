9efe208aa4c5ceffa009fe8f3336c9d60531c2d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:15:27 +0000

    goon pronunciation deserve succeed poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
