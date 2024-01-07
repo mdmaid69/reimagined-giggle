c33c0a3b140c1241de7044e30ea79ad2507ce2d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:38:59 +0000

    pollution better peaceful mix beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
