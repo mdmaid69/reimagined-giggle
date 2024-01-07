7b4a7f2dd2ec1fe4481806e810173e4c615d036b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:34:50 +0000

    container he hurry question edge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_set(array):
-        return set(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
