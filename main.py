f3679fc2822eb970e3039b2d2de8b0c5ac7bf9f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:30 +0000

    father violence tired opinion eye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
