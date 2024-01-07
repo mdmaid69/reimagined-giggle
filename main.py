de23dc98ee12162f2463fdb2517f77ccd25448e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:39 +0000

    mass stamp splash turnout OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_list_from_array(array):
+        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
