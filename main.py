ba3b33667eab383a2fcc40bde3f2eff7a7ca4dc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:06:23 +0000

    Cc Ii charge Uu reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
