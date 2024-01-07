62be964a81eaf4f36c842e427b46c7ba19e5cac8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:37:39 +0000

    voice peaceful whom politics sky

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
