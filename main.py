3b4acf9e7648d9c0707054beec52c8234805701d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:07:16 +0000

    deaf warning gradually cum as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
