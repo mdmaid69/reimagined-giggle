84af21a295311eec38caf12b3500db483699458d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:53 +0000

    hurry second what lean plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
