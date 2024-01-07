4f79d306b572d994a6f64fa8b6369bba35540c5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:16 +0000

    exchange Uu pattern keepout female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_work(force, distance):
-        return force * distance
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
