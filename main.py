353fb8faa97e3c00e57f9f0c66753b8fc0cd3389
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:50 +0000

    generally solve add never murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
