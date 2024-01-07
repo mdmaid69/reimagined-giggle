8c2630a5bad2b304d8ee3346fb65acf2584f8743
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:42:36 +0000

    hurt cruelty relation throw turndown

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+print("Hello, world!")
