3e14da752626ce208ddd4b80d6b71dfdcb0c5d20
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:32:27 +0000

    refuse brush content protect star

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import sys
+  def get_python_version():
+        return sys.version
