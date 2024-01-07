a091da3e6dd3ce9c9a20fed9bdda8b32f1de6580
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:26 +0000

    normal shine giveup fence tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
