91a9864cec5fa3bf2395bb49d63bfc755122b166
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:45 +0000

    beat disagree flat water constantly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
