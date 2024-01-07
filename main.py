2352a2091917c4ee015c0975041f14cc44de251e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:09 +0000

    runover thirsty dig hair miss

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
