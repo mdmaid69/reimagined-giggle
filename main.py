d9a89eeb9d06296dc3a6cb0501d03acf5e81fe87
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:46 +0000

    dress alive tap rich bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
