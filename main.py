ced28ace0e176a85b6eb9730c920a2d23be3c2bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:57:12 +0000

    fasten expensive tear both comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def get_current_working_directory():
+        return os.getcwd()
