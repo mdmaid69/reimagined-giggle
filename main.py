77c93b263c95809f5b8706eada7826dab3dd0cc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:39 +0000

    crisis up depend sometimes imagination

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
