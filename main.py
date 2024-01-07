7f4268dea031c6808fe3567a1faedca7942e5212
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:13 +0000

    pen word responsibility sorry switch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
