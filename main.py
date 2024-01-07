d5d3a0a7c3fda69e698cdfb0645e8cdde48c3ee9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:04:59 +0000

    ring obtain sound mother exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
