08583fc1b5284a30a6da3074f36f5176183f13a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:30:02 +0000

    feeling drawing care people store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
