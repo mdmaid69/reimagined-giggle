3f98631e2f031693b201e61980a1908d8a80fb56
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:40 +0000

    realize mail mark moment possibly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cosine(x):
+        return math.cos(x)
