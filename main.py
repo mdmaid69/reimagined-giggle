eb1efdc9051fe8413a61cfa43d6dd5bc868bc8d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:42 +0000

    connection keepaway bar track great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
