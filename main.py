4b51e03eef654867604d55b3e609c71b8086af1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:12:59 +0000

    clear sensitive flow up education

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
