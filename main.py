142487ec280f419750fe4d8e2165fab6b928f12c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:26 +0000

    cost curious punish sister declare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
