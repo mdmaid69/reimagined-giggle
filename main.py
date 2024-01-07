b0705fcdb6d074ec81ed6a4b80302a61c70901d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:46 +0000

    history tend kick worry head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
