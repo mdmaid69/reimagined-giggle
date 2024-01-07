aab12c570e21e3425273555bcaa9e8bc5fd85b6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:14 +0000

    test belong course autumn phone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
