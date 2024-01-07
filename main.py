b1087b7e7970b8a0c11d3c6ec6becc88c4d93f5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:07 +0000

    mind gap pay calloff over

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
