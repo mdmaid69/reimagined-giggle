91a2b2a1a2ab2c46d1aab5de03d19252721e2338
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:34:27 +0000

    certainly close attractive aim far

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
