0ed0f780567b37303aa2b882a5c46de9f6e7504b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:16:17 +0000

    rain inquire ordinary build favour

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
