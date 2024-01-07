f1e50bd6b19dfa8b5f9e40f514cf2478865487cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:49:43 +0000

    display search post help push

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
