8d0969311fd61f08c499a3a1788fb549fe4d13f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:36:25 +0000

    chat own grade bringback visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def delete_file(file_name):
+        os.remove(file_name)
