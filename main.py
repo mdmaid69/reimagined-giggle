a355964e5dc38435a586cc649743ddf045e6d2df
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:17 +0000

    stranger involve peace borrow family

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
