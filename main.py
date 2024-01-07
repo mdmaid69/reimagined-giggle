0870770b19ec1d51f69a31f9e6196c574b96c5c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:43 +0000

    skin where across injury to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sys
+def exit_program():
+        sys.exit()
