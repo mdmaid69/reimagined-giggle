f75262b927b07a18440c0b882732140f1e0c33b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:27 +0000

    let familiar defeat system no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
