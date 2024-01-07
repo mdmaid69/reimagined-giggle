038a2dbb4fb57264042074499739a2679d4709ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:35:06 +0000

    gap love week callup switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import getpass
-def get_username():
-        return getpass.getuser()
+def is_even(n):
+        return n % 2 == 0
+def calculate_density(mass, volume):
+        return mass / volume
