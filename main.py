de6f12f388d136a186e57f124101854741572041
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:47 +0000

    steep scrap then deliberately grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
