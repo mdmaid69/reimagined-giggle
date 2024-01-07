d77a651a46956b8490f28a74948d212cd810b752
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:53 +0000

    table reserve plenty use spare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_volume(length, width, height):
-        return length * width * height
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
