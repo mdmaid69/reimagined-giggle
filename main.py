49126e64ea24c55e8dcfe669fd9a29e6d602f9b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:03:34 +0000

    top otherwise rain apart comeup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
