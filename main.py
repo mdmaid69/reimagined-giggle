686469128efc09a271438cde91b3c5325d61824a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:36 +0000

    border rarely same happen body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
