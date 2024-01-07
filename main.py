23f801a24990b99886c7c22f2434e131b2d6fb86
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:00:06 +0000

    art equal joke sea chance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
