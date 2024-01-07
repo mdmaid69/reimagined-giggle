64433462a57c2174671015d0631547e4d279ec0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:55:55 +0000

    comfort same lose secret substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def print_python_version():
+        print(sys.version)
