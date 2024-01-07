b685fa573c3fdd3db23b08ce1c8fba97c62fbe53
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:26 +0000

    copy date rush fold introduce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
