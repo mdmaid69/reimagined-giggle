719f3c9b4c2aa0d1baca1a8ca759bd31e9f85bfc
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:22:53 +0000

    happen zero close introduction exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_list(array):
+        return array.tolist()
