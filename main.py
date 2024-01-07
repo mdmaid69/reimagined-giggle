888d6b28ba4c06ba165c9c11a822583459f88ec8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:07 +0000

    separate rush murder imagine better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
