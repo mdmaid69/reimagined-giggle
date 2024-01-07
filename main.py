bbe6befa8458e0001dc1d9306f2fc64fe1b412c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:59 +0000

    apparently lookaround side once boring

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import threading
 def create_thread(target):
         thread = threading.Thread(target=target)
         thread.start()
         return thread
-name = "Python"
-print("Hello,", name)
