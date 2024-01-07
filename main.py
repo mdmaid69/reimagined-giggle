98a8d6a2e0f8421cf04b8eb2bf4165981979ce58
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:43 +0000

    price 568energy separate abroad reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
