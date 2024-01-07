6876b9d27ddb614715a5fdc60fbb960c3b55322d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:17:33 +0000

    impression be sting best undo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def is_even(n):
-        return n % 2 == 0
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
