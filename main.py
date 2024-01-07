a548027de95a592259cfc7f8d1bbb5576868dada
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:21:25 +0000

    clean luck union solve concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
+import collections
+def create_user_string():
+        return collections.UserString()
