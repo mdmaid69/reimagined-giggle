acc1a3393350dc2a00530e6271bcf31873e099a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:12:19 +0000

    brief giveup fast on climb

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
