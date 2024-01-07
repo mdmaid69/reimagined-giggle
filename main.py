ef0f69a0ff9dbb8d838fc338299a1d17758c6d7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:04 +0000

    before news bet swimming down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def is_even(n):
-        return n % 2 == 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
