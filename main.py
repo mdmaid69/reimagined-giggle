fcb12e729a5059d422f00350eff811a19273c6fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:52 +0000

    tend between in discuss length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
