2318cabd10cf69741838c27841dd307e99ce1ce9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:37 +0000

    broken exchange success pay rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
