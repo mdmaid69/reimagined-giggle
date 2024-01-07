c5737705636e9992002374a205f80bcaa98a3452
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:41:33 +0000

    slip appear bury description boat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
