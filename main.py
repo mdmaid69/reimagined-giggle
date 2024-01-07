88ec70a54f4d9efb5ce6efc1683184151608f87a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:53 +0000

    trust passage flavour instead interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_speed(distance, time):
+        return distance / time
+import sys
+def print_python_version():
+        print(sys.version)
