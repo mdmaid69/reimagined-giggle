017d0c5ecf41603775881191c343f809df844b48
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:30:46 +0000

    mix person narrow far feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
