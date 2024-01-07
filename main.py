922fece6ee1530acb0f18d2723d16a936f568da6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:04:24 +0000

    point unlikely class lost special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
