65ef2be3efaada110dc4714881d481e72c4caa6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:21:54 +0000

    good better mark accidental worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def reverse_string(s):
+        return s[::-1]
