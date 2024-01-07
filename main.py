9126445d60bc8da860c04a7530738066fbb757e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:17:04 +0000

    available it score healthy aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
