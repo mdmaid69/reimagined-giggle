29de4fa51c9c95155cef6c028fc2fdb37c00d461
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:43:24 +0000

    fear evening idea his action

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
