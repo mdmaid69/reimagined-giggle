3ace28b4104c4d550f9e56782581e25f56dd4dc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:38 +0000

    separate slice want regret board

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def is_even(n):
-        return n % 2 == 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
