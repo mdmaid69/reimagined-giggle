5b1766f5ee304705c85be1296ff8766be5c85778
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:14 +0000

    common safe contrast mood violent

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
