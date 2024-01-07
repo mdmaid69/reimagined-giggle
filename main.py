1ea4e1ec35b07ff97386ad4137cdeb758abb1502
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:09 +0000

    pool genuine deep attach definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def clear_array(array):
+        array *= 0
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
