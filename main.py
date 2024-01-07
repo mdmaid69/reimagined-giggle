4e3da379ebe1ae8d13dea94af1c43d828bdbf3bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:16:40 +0000

    tight whichever the coffee continue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-for i in range(10): print(i)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
