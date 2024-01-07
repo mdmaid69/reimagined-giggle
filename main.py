9e77d226696aefcc11dfb8e7e6e249ebec71c430
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:00 +0000

    save write class achievement rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
