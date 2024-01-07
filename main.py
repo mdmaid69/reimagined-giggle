66f519389b2f78b40112037e67d955fa84d76e46
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:37 +0000

    which even special appeal medium

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def count_elements(lst):
+        return len(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
