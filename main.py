d556ccc82aef214063c21f78ec0d4ab518f8722e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:44 +0000

    cap cum normal talkinto willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
