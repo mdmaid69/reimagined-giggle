e2856b42b795b9d1e785a29f9a865bc234be3a0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:18:57 +0000

    advice please nigga congratulations especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
