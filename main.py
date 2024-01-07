8e0d080abc082c652b5b2c81946e384eea2884fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:56 +0000

    spot debt worry takeback pupil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
