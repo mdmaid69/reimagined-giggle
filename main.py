1e7ed2150d305b22265db1f7d03d71b6ffae3162
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:37:43 +0000

    ring body innocent accident shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
