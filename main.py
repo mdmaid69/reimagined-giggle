b98d63cd47cc0e4057267a22c81e794f34e9816e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:55:02 +0000

    indeed train marry lend thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def find_unique_words(sentence):
+        return set(sentence.split())
