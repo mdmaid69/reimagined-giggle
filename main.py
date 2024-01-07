7b89e7c10c47fa0c7dafb0b3c9d2346781925c41
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:15 +0000

    comment will hurt organize invitation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
