e8b01e9eb12e687e961a70e97104f7cfaa1434dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:35:10 +0000

    squash music road pool although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Uppercase:", text.upper())
