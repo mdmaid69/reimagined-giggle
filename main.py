e712fbf90ca9f14aefab22fecd1c2ed141f4b26b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:55 +0000

    warn mine entrance strict matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import collections
+def create_user_dict():
+        return collections.UserDict()
