49a2a72a4fee2e208a61fd8630ff9daa9c81590e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:06:23 +0000

    ourselves head everybody pleased children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import datetime
+def get_current_date():
+        return datetime.date.today()
