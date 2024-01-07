dadfbfffbf32af79d9b518dd371180854b4458f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:15 +0000

    gate better complete size blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
