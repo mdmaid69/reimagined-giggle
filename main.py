a3df00e95b90c04d866af84880b6ae8a4889a4e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:46:45 +0000

    direction alone height smell repeat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def list_files_in_directory(path):
+        return os.listdir(path)
