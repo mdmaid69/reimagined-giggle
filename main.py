0d06056e7a1a97323baa040a8a832ee6f6ae9c10
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:28 +0000

    bridge waste away pencil mine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
