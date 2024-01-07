2adb77d3f4b6af1b7ae4f73e9153efa511f5e78a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:30 +0000

    rank deal run just big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def count_elements(lst):
+        return len(lst)
