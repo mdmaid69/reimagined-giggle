868ae1b774eaf74931e494743f2d8167de0e66c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:29 +0000

    570enjoyable die lead grateful stir

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
