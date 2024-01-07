bca94b143f32dd772c959e115dce14eb4f38f87f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:16:25 +0000

    gradually worse total agreement train

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_base_name(path):
+        return os.path.basename(path)
