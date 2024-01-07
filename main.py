77182e070eba519ca8935dc0ac9da543be2c7e9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:14:54 +0000

    poor nor willing cross practice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
