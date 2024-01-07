232ffc28c2d05c0bae63e3d3f02cdb9c09f440a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:44:03 +0000

    politics trust turndown relationship gate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def count_characters(sentence):
+        return len(sentence)
