e52fcb486e50d14f8f8c844fc41c7c0d56f4cdce
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:38 +0000

    habit create dangerous measure apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
