cc204153e1875c23f7f2d65a0330bb5a257f339d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:59:46 +0000

    thought test clean demonstrate sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
