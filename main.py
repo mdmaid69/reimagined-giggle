5bc9dbe4f0a01c803520f83e8323c51b2cb20179
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:43 +0000

    sorry ride replace later anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+print(time.time())
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
