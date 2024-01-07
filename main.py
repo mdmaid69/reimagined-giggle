5c339a1fbc6005e95a46b14ae9931416bf97d679
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:56:41 +0000

    colour turnup arrange cope embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_cosine(x):
+        return math.cos(x)
