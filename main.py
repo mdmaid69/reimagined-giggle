0928ff12caa0abb7b49b0996ab8873556a69d1ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:08 +0000

    familiar fly commit watch visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
