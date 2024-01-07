346feb4493480153633007c6857e6c03f61108f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:58:59 +0000

    open pleased breakout right interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
