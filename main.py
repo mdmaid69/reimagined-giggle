5c25f85aba3935e33b1308efab74f51283e696da
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:22 +0000

    street message dry joke improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
