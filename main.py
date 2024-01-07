53f3890ebd3bd39ab90b89297be6e308e023dcc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:22 +0000

    earn yet receive belong dry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
