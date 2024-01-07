c812bdbf55edf809bb64672b1b45921ba077cc87
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:01 +0000

    pretend equal small experiment turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
