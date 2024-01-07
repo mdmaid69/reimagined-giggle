53d477985638b8d2447e0f55a757880224ad62b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:28:53 +0000

    object everywhere mix hurry man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
