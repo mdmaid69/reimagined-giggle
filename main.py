fa94d533803f22b205dbb70f1d0562b6463e6267
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:59 +0000

    recently Ss how feel takeout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import collections
-def create_queue():
-        return collections.deque()
+def find_max(numbers):
+        return max(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
