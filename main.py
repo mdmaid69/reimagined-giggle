b9e8e83751824d35c4af76f8f99b3aa68d09fba4
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:49 +0000

    violent fear myself under stay

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(10): print(i)
