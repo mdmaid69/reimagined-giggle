861bb74977bec194687cb942c8a80127f888740f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:07:05 +0000

    power like wish hang meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_current_directory():
+        return os.getcwd()
