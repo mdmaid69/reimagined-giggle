35cc3312cba6575320864f898f47d642e9f13f3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:39:58 +0000

    total name difference storm anxious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def reverse_string(s):
+        return s[::-1]
