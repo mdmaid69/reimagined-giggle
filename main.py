2301e73bf7be040e34922e7243a812d4189281a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:06:09 +0000

    peaceful left suck discover tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
