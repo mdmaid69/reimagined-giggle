3a74fd23f4b390daa80df7886e466eab1df24e5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:06:25 +0000

    stretch book sister odd note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
