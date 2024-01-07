95345c1f33509c4fafc186321d7a6935d71872d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:37:07 +0000

    callat but blame trial partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
