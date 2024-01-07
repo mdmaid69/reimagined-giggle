028f380d71b7dab601dec238a17a0cc3f28e488b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:24:10 +0000

    weather watch tax only plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
