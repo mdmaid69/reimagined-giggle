17395bc69f6efe18996d9df6b490816a993f904b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:02 +0000

    Rr common 566end unite hollow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
