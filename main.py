939508efa51a5363569811206873a28937618162
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:49 +0000

    which interview loss up deliberately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
