69e13b1ec9ca3e25fc3396aa95c8d1f00bd26c05
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:03 +0000

    music grand traffic father this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
