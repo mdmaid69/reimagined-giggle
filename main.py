4eb1b1481905fc0fcd14c04cad013481bd72d1dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:47 +0000

    lookback(on) damage curious edge listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def multiply_numbers(x, y):
+        return x * y
