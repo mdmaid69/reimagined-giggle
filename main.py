90838d04a226396f5b891fa787d29d83bf198488
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:50 +0000

    ilovedicks well warning border rest

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import random
+print(random.randint(0, 100))
