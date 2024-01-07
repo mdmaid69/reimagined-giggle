0b51ec407e8d26bbcaa2a5ae5d8e2a37e6a696a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:42:36 +0000

    little community refer bringdown proof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
