c6b7410b174a30d4256b47fbcf41ae36732f098f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:53 +0000

    Ss unless help house business

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
