6f33f7056547660d0d34d435aa07227aa6a4ca6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:35 +0000

    climb scheme say tune sister

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
