af2aa673d5ccfabf18a08ba8eb50286f736c80af
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:35 +0000

    guide line tough used large

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def calculate_area_rectangle(l, w):
+        return l * w
