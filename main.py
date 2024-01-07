1834094f544da321730ded33f2d500c7e8d0a69e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:04 +0000

    out mood amount relative service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_work(force, distance):
+        return force * distance
