dfeb21f161bebe25547779b6b38286308604b6b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:27:40 +0000

    strong violent reason idea steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
