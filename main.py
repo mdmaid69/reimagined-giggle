2d86b5c8c8eb0d6b495ee864352f98f5bb35337e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:41:15 +0000

    trade sign excited exchange indeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
