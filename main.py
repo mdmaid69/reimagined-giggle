f8a03608b86e9d050046a4f01a367152108d0441
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:07:55 +0000

    plastic range school challenge when

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+print("Powers of 2:", [2**x for x in range(n)])
