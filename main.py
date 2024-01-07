cb11b2f3641db208e1ce3fd2f68322a02083899a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:11:57 +0000

    himself care and put noisy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def cube_number(x):
-        return x**3
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
