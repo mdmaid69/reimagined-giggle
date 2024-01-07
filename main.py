f053b5cc3a592e897c8c3b4f3a42927e34a85ece
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:52:01 +0000

    fit cap else Kk to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
