39821b07b582a954bf7248f7e77051b4c2bd714d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:19:07 +0000

    chance introduce arrive shock this

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
