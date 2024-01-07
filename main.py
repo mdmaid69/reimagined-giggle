eafa6d9776bc20583e7dc9ec4838a6e7dc074d87
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:24 +0000

    runover radio Vv day order

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
