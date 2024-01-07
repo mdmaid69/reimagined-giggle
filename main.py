95634b12bded1e466f6b0d2b87b1365b361ecac0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:23:56 +0000

    whichever tip system nurse tax

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
