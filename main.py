d04860e3c667e0b87b5081c45fd2ba15b2e9a574
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:18:09 +0000

    necessary fault sometimes story poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
