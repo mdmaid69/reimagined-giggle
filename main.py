8c86c070e60f0face2dfd1ccfcae1028804acaa3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:24 +0000

    damp up cheque problem difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def split_path(path):
+        return os.path.split(path)
