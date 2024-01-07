f34af6d8b52d27f5df45ca76fdd0f7526215e48a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:30:54 +0000

    whole pollution test under stranger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
