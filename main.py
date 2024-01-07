a05316df5170de310c9178e436accca63cec10f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:01 +0000

    shock interest soon take home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
