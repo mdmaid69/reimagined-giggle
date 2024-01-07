7de7dfa78e9ac9055eaaaa19e9ff4c45c9432427
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:26 +0000

    go physical miss notice as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
