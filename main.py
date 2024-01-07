ebe0d9c60f29fe6bd9fe6651984c48c8f785e831
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:11 +0000

    call switch provide sell 572enter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
