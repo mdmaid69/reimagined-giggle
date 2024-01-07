f0dbc44de4e6354feb180311cc3c5c028bb5e715
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:40 +0000

    Cc we eager offend legal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
