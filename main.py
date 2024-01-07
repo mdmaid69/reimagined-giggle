1ac0929526cdbe9a66eaeac98dbb22c49f625ad7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:54:27 +0000

    quick sight congratulations mouth dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
