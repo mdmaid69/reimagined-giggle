5d9e7301b094a1cc4146242a78864bda08277b63
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:00:37 +0000

    regret food note giveback silent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_power(work, time):
-        return work / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
