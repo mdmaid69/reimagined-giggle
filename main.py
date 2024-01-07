248e63070fcad8cced4e74eb003e2f3eb4473628
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:37 +0000

    could now regularly juice skill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def reverse_list(lst):
+        return lst[::-1]
