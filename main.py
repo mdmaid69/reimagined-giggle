6d4b2c9d3b75746ab7f0f5f047ad249448e4ae20
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:48:54 +0000

    upstairs club success exchange valuable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def find_max(numbers):
-        return max(numbers)
+import tensorflow as tf
+print(tf.__version__)
+def sort_list(lst):
+        return sorted(lst)
