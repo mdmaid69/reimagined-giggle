0f5265cae53de4cb6b4870c517b6b7bed3ff44b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:44 +0000

    awake roll my for especially

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
