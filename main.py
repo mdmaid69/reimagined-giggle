755a31ac0abdedd8501da7904cb48caa5a0a77af
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:45:46 +0000

    point change them last consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_index(array, item):
+        return array.index(item)
