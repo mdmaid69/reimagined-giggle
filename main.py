f194a775d44ed3eb0923a6782284aad9739ec8bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:56 +0000

    exercise of off grow open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
