6f670cc6d2f760af86120622c2254f5d2fdb9b40
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:50 +0000

    callup attempt thank may affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
