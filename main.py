7d61ac504ead847e3f8880801535002602465d5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:43 +0000

    accident moral everywhere control care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
