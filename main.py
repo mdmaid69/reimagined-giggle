185075b16ea686db7640e50efc0e279b1f49d9b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:13 +0000

    lookafter cruelty heat completely wide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_set(array):
-        return set(array)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
