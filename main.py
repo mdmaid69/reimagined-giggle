8d64f2a28d0102f8a5deb1add9384f293020207f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:26:42 +0000

    content inquire piece born tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
