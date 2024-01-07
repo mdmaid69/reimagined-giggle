fcf666cc92bb1f22bcdd9c0f3bddf76b8a98213a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:07:04 +0000

    car protest every lookafter comment

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
