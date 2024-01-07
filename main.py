04ef37fc663869a21468e3e417924b6749e061ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:37:51 +0000

    normal male injury left fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
