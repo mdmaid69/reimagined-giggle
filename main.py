7285290de2492348843d3fe5bde182708fc2687c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:17:24 +0000

    exam apart cover proper indeed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
