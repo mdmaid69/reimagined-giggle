2bd39c0c573c30df40facc5b66f17ae76173440f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:31:38 +0000

    lock play guilty fetch birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
