d8cf1407aef719a28400b601d4e98fee05d5b506
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:03 +0000

    base vision recently good slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
