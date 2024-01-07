76e2df3e5b13d8fad6819d2ab08317bf91dd7fec
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:06 +0000

    claim night pencil ticket squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
