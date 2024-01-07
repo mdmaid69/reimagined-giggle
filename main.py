aa002c507273ea8c7cc000606a668d7670eb878f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:58:40 +0000

    steep impressed behind peace callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
