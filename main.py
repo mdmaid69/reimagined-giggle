dc023eebd7b1802580594fc432d36eed5bb726df
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:45 +0000

    accommodation ago post piece tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
