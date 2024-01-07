2eb9d940eccf0dfaa1b1fd6f79ba24bcbeb21a8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:37 +0000

    getout arrive see brush each

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-name = "Python"
-print("Hello,", name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
