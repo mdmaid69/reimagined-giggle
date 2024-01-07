c451d821463d931cf048313a778379f3ee756b33
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:52 +0000

    young weigh appeal up right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
