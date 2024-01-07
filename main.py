22d44435a1c9ab351a0ad0620e671cc2d93eb86e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:21:20 +0000

    funny our dry school survive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_work(force, distance):
+        return force * distance
