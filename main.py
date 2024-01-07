fa26e3d789ea2777cb19ff5c1d055573413727ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:03:14 +0000

    bright pencil to blow trap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
