226c9274b90dfaa33a68f36405264407565a6957
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:08:46 +0000

    turn destroy aim maybe ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
