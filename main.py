c69e887b1b43b7f23a59f862415aff37f7df3acb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:26 +0000

    card quantity far born late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
