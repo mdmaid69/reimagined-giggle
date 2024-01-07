84d49a5ae40372344b058ad355d0798221689b7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:23 +0000

    watch measure close present dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
