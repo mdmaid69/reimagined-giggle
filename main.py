f0aa5361d16cc83b001f1ff50907e080547649f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:34 +0000

    566end cost service school hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def log_message(message):
+        logging.info(message)
