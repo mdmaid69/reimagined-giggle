2ee6190c5fa267d1bf458f395e779bbd19b0f39e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:37:17 +0000

    order advanced judge wait lead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
