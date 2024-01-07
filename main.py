28bffc651f4604fbab9bfebd6448cb2d37034541
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:18 +0000

    wash Vv deliberate past fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_unicode(array):
+        return array.tounicode()
