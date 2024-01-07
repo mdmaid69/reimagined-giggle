cbd3f0c6adf8618c87a0e9c07bfd381c70190edf
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:24 +0000

    meal let minute arrive around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
