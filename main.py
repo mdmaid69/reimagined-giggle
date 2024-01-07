3692bcd475d26824ae0397aa849fc87adb25accb
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:01 +0000

    one embarrassing wide blame square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
