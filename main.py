bf3f5f358a32b15142a7e9c950c43f9cdc6c6a32
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:16:02 +0000

    spare geton question continue frame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def count_characters(sentence):
+        return len(sentence)
