892e72dc26315d50275d5ebc3ddf91d76ecedc79
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:55 +0000

    keep whichever break foreign wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
