d5c9554704fef43131042a9293ed9500ce00bb27
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:42 +0000

    market with positive squash project

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
