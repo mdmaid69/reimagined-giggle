45c8335a7c42d737c93f9ce161dd2d6ad058c830
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:17:42 +0000

    day combine ignore position noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
