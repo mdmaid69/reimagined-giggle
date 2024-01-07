ae9357fb4a93263d8097606f0e3b3851adfef552
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:16 +0000

    best board education recently flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def convert_to_hex(n):
-        return hex(n)
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
