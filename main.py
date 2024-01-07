25913bab547507ec18f551e93824f026d898dfaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:20 +0000

    father quality early comedown male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
