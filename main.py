25fffac03f55b20aa9903d707be4f062dcb9327e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:50:51 +0000

    formal development criticize band purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
