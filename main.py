7041412ef5ae0143bd2764620da37686788cc379
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:34 +0000

    cheque every type late another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
