8227b787f3185c26f3a54f148ff04e6a47c03c4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:06 +0000

    regard education work smile dare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
