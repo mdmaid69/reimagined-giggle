54226f4a9ec36eb24d4f2c7ac30820fd9721a8c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:24 +0000

    release know and reserve embarrassing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
