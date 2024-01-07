42a85fd65f264d7f45f013d9ce3bd9eac0882d94
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:48 +0000

    admire march surprised fall identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
