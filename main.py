43e3c55fd179f0277b091c38629c9e5cf6155dc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:22:10 +0000

    lay lean belong fasten inquire

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-  def add_numbers(x, y):
-        return x + y
+import re
+print(re.match("h.*o", "hello world"))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
