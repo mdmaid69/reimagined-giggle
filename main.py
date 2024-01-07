a449c97470f977e562a6d1eac33f02cbb9f76cb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:18:13 +0000

    pleasure shadow 566end worth hit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_unique_words(sentence):
+        return set(sentence.split())
