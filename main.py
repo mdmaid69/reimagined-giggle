b0c5e66038461bb2f2699e82ca4490221b3a1b0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:09 +0000

    excuse fire brown mood switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
