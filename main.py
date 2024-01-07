3bf57e0f9096fb383f5ba29c6b4bc929547184eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:35 +0000

    turnaway middle job connect lot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
