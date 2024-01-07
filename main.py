eb862e437681000bd739ad5a51c6073fe75c65c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:43 +0000

    she history turnup have loud

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
