3b27ca24a36a0a70387f152678b8d6d01e26a27b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:21:08 +0000

    negative wake still activity possibility

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def reverse_list(lst):
+        return lst[::-1]
