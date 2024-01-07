8cf30296c286f1ef000aea44dc38bd930d3cc2c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:43:22 +0000

    smell angry wrap language fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_python_version():
+        return platform.python_version()
