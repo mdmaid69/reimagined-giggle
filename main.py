6519513c1d5e148fb47a529c43484bc5c508b440
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:41 +0000

    offence dare tired introduction funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
