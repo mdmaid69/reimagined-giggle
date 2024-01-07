9b86dc6ffbd9e3367d3d8430c1373a0b27b1a4d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:21:30 +0000

    meaning health battery thumb voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
