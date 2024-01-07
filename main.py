bdbf2a06092dbe1fa7335c8f03a3b67e7244c719
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:26 +0000

    practise confuse direct turnup bringback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
