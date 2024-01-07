9057d7cfeacb50578af66f3eb6dbc4468fd7669a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:20 +0000

    convenient mark reason cry hit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
