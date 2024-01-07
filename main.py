afd45d9479e69c5259c36391cae310ef996e3ce3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:26:47 +0000

    appeal comeround generous mother hold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
