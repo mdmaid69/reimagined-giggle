40679ec00d89fd514a269246ca6baad44f6cdc96
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:37:16 +0000

    open man hard neither area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
