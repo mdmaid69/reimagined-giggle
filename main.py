55bb2e79c3b2567352b85f6c61ac5b863bb4602a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:28:30 +0000

    soil neither much dig ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
