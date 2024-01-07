06bfd502758b32431668a3aff86fc0d91acd79c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:08:23 +0000

    shopping lock complicated disturbing suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
