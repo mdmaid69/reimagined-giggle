f7e953d9674f9358afff514cce000f6ed4e85c2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:26 +0000

    accommodation sort no extra number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
