b3b4b088784400a05283fbfe6c2aeef6be0a9773
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:54 +0000

    row show boring from ring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
