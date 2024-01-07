d30aab74e2f9d78a6e1ff9900572f9a7f5ae2d40
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:11:04 +0000

    surprised slowly only deliberate centre

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
