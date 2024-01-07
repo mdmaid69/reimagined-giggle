133ca6487278b4931680e7cc7e2615283d411b39
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:57 +0000

    theirs example takeover wealth pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import sys
+print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
