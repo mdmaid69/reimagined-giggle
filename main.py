0795293145caea05a7f4b19dc7771ac8d5141015
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:11:54 +0000

    benefit unfortunate ought someone opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_density(mass, volume):
+        return mass / volume
