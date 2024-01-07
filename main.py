63c30c92d42b29a8b61a7a4937ab96d8a087f8cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:27 +0000

    own rare travel tap badly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
