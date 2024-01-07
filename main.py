5874faa6e43c81e86ef894294468529cc1030e09
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:07 +0000

    answer speed approve store justice

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
