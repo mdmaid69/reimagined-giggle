372581a7040e7d74aaa564fd4dc3c392562134a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:00:45 +0000

    turnaway move basic goout one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_arc_tangent(x):
+        return math.atan(x)
