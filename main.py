4a98f19fe07efbb47898615b47fc13f9b66a3ad3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:44 +0000

    common valuable represent perform anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
