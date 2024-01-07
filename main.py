7d8823a503fcab2d61d53fb05da0ad17cf530573
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:07 +0000

    impressive because murder keepfrom delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
