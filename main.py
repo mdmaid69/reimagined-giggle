c229f2342c828293b7f6e91854d85a6fdc045f13
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:36 +0000

    fix straight train rule prove

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def cube_number(x):
+        return x**3
