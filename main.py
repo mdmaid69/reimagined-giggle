35e25b278debb809872e99c39b498918481fd834
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:29 +0000

    skirt above accept familiar conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
