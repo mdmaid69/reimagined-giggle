408dad4826384bf63098098b3a8a5db702055d28
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:17:13 +0000

    badly sometimes Ll example weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
