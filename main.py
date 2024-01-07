1915cf7bd7757dde971c83b7a7a72418872d9b67
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:18:34 +0000

    protection tone agree rob want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
