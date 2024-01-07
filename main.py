8d38894d9c9b4f7cf6f4e39dd0f7fb0af6721d35
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:33:51 +0000

    fast tired impossible decide quantity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_typecode(array):
+        return array.typecode
