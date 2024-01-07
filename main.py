56796bfb3d77d53abed64d25267191ace8247d38
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:11 +0000

    identify giveout top pair custom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-for i in range(10): print(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
