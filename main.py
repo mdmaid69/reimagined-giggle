dd4dd5540a81ef5e360353301bb6bac0a42ea286
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:12:29 +0000

    find second suppose black up

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Hello, world!")
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
