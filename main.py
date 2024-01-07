b0c73c40a3b41ef11d7a9e6911952c84a64b0195
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:33:29 +0000

    569enjoy waste power himself declaration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
