77dbf96507abc6ee852341e059dc73ea6ffdf72a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:33:05 +0000

    achieve whenever bang fence deaf

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
