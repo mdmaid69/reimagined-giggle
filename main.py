ed9c5f313ec50ce0dd1c4bbb74f439ec23ab4377
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:00:13 +0000

    doctor genuine recommend defeat entry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+for i in range(10): print(i)
