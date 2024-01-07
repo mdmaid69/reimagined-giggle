148f330a70d5ee473071eae43a86aa208ebdb3e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:05 +0000

    hurt admire night keen balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
