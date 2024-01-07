c05da36c7e99f3fa1b61e2cc19e9c467b2bf8b27
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:26 +0000

    deliver unfair please habit loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
