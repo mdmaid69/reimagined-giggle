7c87aecc76c2b83db3d3174257e1183ffc38362d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:24:17 +0000

    opponent tie frequent steam care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
