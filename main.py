46a54a60149ede5b77e1c8dd0d4fc4eef9957c86
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:49:00 +0000

    near mend pool check friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_pressure(force, area):
+        return force / area
