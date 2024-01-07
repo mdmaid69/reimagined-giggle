5d668c6a3f0c3f027c65362b8537d40fe0c97de9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:57 +0000

    cold edge bright any print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
