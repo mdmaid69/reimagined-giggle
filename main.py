de136163df41c9adde0e0454412230f46bc16ad5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:22 +0000

    Qq fierce ride closely trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
