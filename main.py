53bb095cc06c006ae781a842f02555bca7e784eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:17:09 +0000

    stare not perfect our crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
