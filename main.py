efd72183eed9cb90aba2b2f5eb2cadeefa0c9e11
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:36 +0000

    machine she pressure head pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
