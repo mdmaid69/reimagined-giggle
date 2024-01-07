03d529d7bf66f0f64c7fd7d0f1b4c4ecc98ed1ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:06:34 +0000

    music game trick everything away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
