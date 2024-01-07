2a9c30859c72d6305bccdda44f65eddc78b33a8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:41 +0000

    variety OKorokay age even keepaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
