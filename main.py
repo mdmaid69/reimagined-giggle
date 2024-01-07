61570f5c5fe143a9f7537fd3b408e5ab37daa455
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:38 +0000

    instant proud while exit actually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
