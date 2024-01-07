c31f5efb24b179f972cbbc80cdeb4ce5aab48ad4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:17:09 +0000

    butter model use wrap over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
