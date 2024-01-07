e937ec3ad1ac1d73007245c2a21a94ca4cfb3d82
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:02:46 +0000

    holdon squash polite taste build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
