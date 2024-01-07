a8757df9e1cff5c40160dfc63eee34e76ee9b598
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:05 +0000

    mix interest most imagination same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
