eb78012c272a97291a59ececb655a4cb114a7163
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:24 +0000

    admit sit strike break how

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
