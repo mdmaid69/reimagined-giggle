077f1127bdd3000df212520cd580c3849556126f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:48 +0000

    neat politics ordinary mixture file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
