ec55aa06b45733ad4df383305d6b59436c0c496b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:43 +0000

    connect earn quiet spray walk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import collections
+def create_user_list():
+        return collections.UserList()
