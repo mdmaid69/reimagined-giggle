492ad851c3e5d4cc0ebb03d64d7d205a65a88349
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:29 +0000

    thinkthrough busy square instead zero

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+for i in range(5):
+        print(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
