d08a2b3ac04e924ab6d6bbcbda3f3256669559f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:55 +0000

    ear sound partner sale dish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
