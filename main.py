1c45cb15d0b7127307c43b164669cff82dd2ea15
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:28 +0000

    join comeback smoke ours wish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import array
-def clear_array(array):
-        array *= 0
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
