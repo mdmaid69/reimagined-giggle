0f2ed62a582c4519771ed160ea2c3b4993698121
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:58:33 +0000

    scheme powerful wipe shoot bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
