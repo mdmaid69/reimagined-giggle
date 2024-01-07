f516ab7545b0e98c00912c304f437b0aff314f4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:31:17 +0000

    wet set match violent part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
