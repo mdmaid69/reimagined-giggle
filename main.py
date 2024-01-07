799e2bf1789472db2c8e51c72ec3c6fa70c0cd36
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:32 +0000

    confident scale complete computer battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def reverse_array(array):
-        array.reverse()
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
