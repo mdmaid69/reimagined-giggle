f12c66d1bfdf89dd29b2d8b6ed2143b722bcb41c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:57:02 +0000

    ahead break sentence promise detail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-print(os.getcwd())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def convert_to_octal(n):
+        return oct(n)
