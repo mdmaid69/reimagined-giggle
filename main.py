9d6c7f3c6d1b0f66192865f9dbf04ec069c2b959
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:33 +0000

    excellent pay permission brief callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
