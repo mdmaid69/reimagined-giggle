72f38a64cb9c5965d0e20810d1f90ebcf91fe822
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:25 +0000

    discuss pause see grand interruption

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
