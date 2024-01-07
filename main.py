783f1a12510efd670cbb3a5dd26ec0577e23b45f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:29:33 +0000

    nice sale apparently myself woman

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
