2895e38d10db9f92c77c2a1be7997db51078b72d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:48 +0000

    destroy nice persuade represent search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
