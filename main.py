c7b68f81d9bc67f3480c1f4e676c3bdf06d1ddd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:40 +0000

    because keepout turnoff half neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
