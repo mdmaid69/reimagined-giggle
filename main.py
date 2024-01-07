eaa84554dccb4fd1a350dc15939499c8d868b1c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:24:07 +0000

    tread touch reaction breath smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def get_time_since_epoch():
+        return time.time()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
