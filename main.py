57e24b529dd122290b171e3bf1d449a750ffca4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:24 +0000

    false interested reasonable ago she

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_binary(n):
-        return bin(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
