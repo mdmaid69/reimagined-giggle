37b007a997acf32549c58ee614ddc52354994c59
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:41 +0000

    friend begin cheat smell goout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
