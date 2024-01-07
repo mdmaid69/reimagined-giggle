8f6b5f1d9cff43a85f874a227769f127b7298729
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:55:10 +0000

    trouble wave paper average runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
