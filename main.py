2a8aea34b7d74944aeb5460e4f2676bfb15fc043
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:56:26 +0000

    inside tax speed save further

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
