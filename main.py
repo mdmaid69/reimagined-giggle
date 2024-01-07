336852ab2bab83fafb66a0db020dc0ef3ef3e17b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:14 +0000

    transparent search smell mess around

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
