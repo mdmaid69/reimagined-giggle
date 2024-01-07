03d7c2598d3d6c7c810aebd90e6557daa0d2a89a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:03:30 +0000

    keepup connected around level up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
