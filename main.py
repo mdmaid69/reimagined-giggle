e718d8455591e91d7ac6fa488027bf6b08ada8f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:55:29 +0000

    familiar attract hurry suppose deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
