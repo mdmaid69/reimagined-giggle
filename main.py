bf2b03e8224b70539eb87a76adb5f94df9b060f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:14:57 +0000

    once square face bringforward vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
