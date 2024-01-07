716dc0f26e7615b00b74bf4b7b835c75c2fb9ec9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:12 +0000

    base surface nor ignore deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
