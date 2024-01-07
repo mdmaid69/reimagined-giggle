dc57774718939e88618e9aa279a296852e31ecd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:16:25 +0000

    suckme rich especially trick interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
