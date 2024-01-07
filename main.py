15c8a9ef459a6dd76a9f4e73696efb145ac13a30
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:28 +0000

    detail phone object up crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
