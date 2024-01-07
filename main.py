6aa76b4c3c391dc7ca949a99c209d36992fe8be1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:12:44 +0000

    power waste second state supply

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
