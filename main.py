4b3ef0959dcdbbfe104b9e98833a9d67c70d29d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:02:40 +0000

    argue future claim block floor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
