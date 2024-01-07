282577578510a566659fb330b995a1376705d7ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:46:45 +0000

    load dig recommend tell design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_pressure(force, area):
-        return force / area
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
