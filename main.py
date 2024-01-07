afea0d806f7cfe817998b28865a40a5b70bebcdd
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:03 +0000

    maybe contrast uncertain wash Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
