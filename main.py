c76b59b9548e2fadd815980f25bbb7a2515faaea
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:48 +0000

    idea attractive habit shape boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
