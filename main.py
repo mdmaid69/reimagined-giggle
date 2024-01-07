2705e95712e4da9755c61c6636f5294ea45aa8d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:45 +0000

    surface substance once getaway spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
