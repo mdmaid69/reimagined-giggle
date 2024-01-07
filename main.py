e9f37adea269c13ba1b85848e05e0d7e4c461266
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:42:02 +0000

    shout war long gas admire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_current_directory():
+        return os.getcwd()
