3faeb95f749c7e80964e26f017629f2d21215e26
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:44 +0000

    Cc bath dry buy wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sys
-def print_python_version():
-        print(sys.version)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import time
+print(time.time())
