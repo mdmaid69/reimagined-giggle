ec432bda306441428726219195eb6b177e2aa739
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:22:56 +0000

    marry write ashamed weigh modern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
