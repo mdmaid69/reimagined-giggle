fb763089c67ffb74e6b68730e44f2efc8d8f193b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:36:10 +0000

    invitation past interruption cinema politics

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def count_elements(lst):
-        return len(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
