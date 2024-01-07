c43d55771902bccaa361f059bead2eb359bf416f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:33 +0000

    exercise hold patience create sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
