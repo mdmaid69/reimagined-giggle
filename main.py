20cf3d5800596f7c655481100a2c70d7d768e640
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:09:14 +0000

    passenger many group needle ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
