cb7603f6373999edc23b34d7cd1252e9b3581152
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:39 +0000

    probably union back slow physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
