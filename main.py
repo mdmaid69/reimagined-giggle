c40710c896437ff223a2bb5cce1cb16b4a9b73e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:12 +0000

    poison contrast near official shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
