bca2f3ff238e7263c73f5c3a0ceba9a9faca0d87
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:10 +0000

    goon who major us mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
