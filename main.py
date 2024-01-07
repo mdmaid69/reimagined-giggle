98694b02ad96314e436e071e13eaba8c4d1d816f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:52 +0000

    breakin or for join swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
