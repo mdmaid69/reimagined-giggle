c7fa84027915a3fba462e17b816de267de3a469c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:56:12 +0000

    more Kk takeout replace soft

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def split_path(path):
+        return os.path.split(path)
