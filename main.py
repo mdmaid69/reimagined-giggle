4cd4e218ff81fa4c8450b94783a5da671ce8cd00
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:24:09 +0000

    sort file mistake over happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
