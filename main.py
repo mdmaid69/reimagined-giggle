688031787e17bbe2651cd0826311bfde65e68d9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:51 +0000

    comment intend characteristic development there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
