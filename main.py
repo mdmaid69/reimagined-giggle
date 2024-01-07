f9a3aefe9d59b337f78b58673f8acdbc3542ed08
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:35 +0000

    about immediate proud balance oil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_pressure(force, area):
         return force / area
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
