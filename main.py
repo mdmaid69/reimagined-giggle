544d8bb41233d5d463698c142f27ae935dd93429
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:48:02 +0000

    rise plenty whistle party eager

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
