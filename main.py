f4acce6796bcf8d54cf36170a42c52b69d7e2433
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:28:21 +0000

    calm honest put ring report

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ def create_thread(target):
         thread = threading.Thread(target=target)
         thread.start()
         return thread
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
