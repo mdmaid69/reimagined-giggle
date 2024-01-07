a423d219ba8725e941cc01bf19e14b1abc66adf0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:49:54 +0000

    divide personal capital tune obtain

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
