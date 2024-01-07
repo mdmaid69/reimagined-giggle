167f5431eb3699af3471467aac83e8a3355ea288
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:25:39 +0000

    direction thin create nurse child

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def remove_from_array(array, item):
+        array.remove(item)
