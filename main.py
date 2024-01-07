45d763e1e6aeaba8406510ec2a71ad1c81c4ad3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:49 +0000

    realize join stop wind aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
