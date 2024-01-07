b70c42a7ce3486415ab3f7f352d6e4bd33e4a0c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:15 +0000

    loan Yy such thinkover dig

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import numpy as np
   def calculate_median(arr):
         return np.median(arr)
