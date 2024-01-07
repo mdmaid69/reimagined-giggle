02cb2cbf63d79d7dac3ec50b082f60fa7e17c0ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:14 +0000

    rob memory might vanish 571enough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
