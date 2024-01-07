b5cbe7d3cc1a1120a4bcf2b9d561563c4d7bac80
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:37 +0000

    uncertain safe open contrast with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
