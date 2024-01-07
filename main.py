14b564b44d3327c9ac4483acb400d7c1717bd848
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:29:28 +0000

    worried last slice conclusion perform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+print(os.getcwd())
