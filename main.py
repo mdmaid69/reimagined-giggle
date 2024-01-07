f12f696cde9486bf8883fae9d0cdef544b241c28
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:08 +0000

    port concerned since fail fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
