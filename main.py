b84166f9cffdd63ae8b6c24fe73e21642e0b22d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:09 +0000

    patient past pin probably funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
