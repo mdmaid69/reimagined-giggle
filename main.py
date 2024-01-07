ada44cde672f6129cd56869bd7bbecec4ad7bc56
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:48:52 +0000

    trial Pp lesson port shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
