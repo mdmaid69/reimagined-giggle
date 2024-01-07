1e663e2bfd484883aa6b1ec503ce1694480e04c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:03 +0000

    glad interview moon later go

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_float(array):
+        return float(array[0])
