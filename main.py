ed3fee0fe466c4929deaf735148cb1d5f1757fa5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:48 +0000

    either attract exciting share bite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def clear_array(array):
+        array *= 0
