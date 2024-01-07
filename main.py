6424563354cffd43bef40eabd2c16bfd7eddea5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:33 +0000

    foreign anything persuade run everything

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
