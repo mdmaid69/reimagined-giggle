7390f082374e7db15064263f0fe8d2b73e874077
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:51 +0000

    lower foreign his sharp contrast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
