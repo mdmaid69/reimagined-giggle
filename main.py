ceb37e131cf1985878adb91b5d6b4618b474c7b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:41:29 +0000

    reply ashamed several collect official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
