119e6d2c72144a642684908af02bd2f346bb097e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:28 +0000

    breakinto shoulder cross adjective hold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
