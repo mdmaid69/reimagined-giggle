4155d1a925934014ab2835a06f6cc5cfcd20709a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:30 +0000

    thumb central birth aim boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
