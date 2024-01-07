67728e220e4c1b59030e06b14fe6c60ac2dcffc0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:11:47 +0000

    embarrassing hot soft appear agreement

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
