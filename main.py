b5331737088ff5886720bcb0ebc44136190e3814
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:47 +0000

    sit concentrate suddenly false suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
