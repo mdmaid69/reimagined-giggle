a073077f14d5cf04d2f7c91fa8bc9dc543ddd62a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:11 +0000

    view guilty judge pattern smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
