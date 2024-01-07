b43c3bc86ffa6a7fcd2d1e5a2fb8f71ee8d7e51a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:08 +0000

    reserve pronunciation reliable fat frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
