2e941040dcc46c5e1922ef4ac142d940c75025f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:05:44 +0000

    lift sink either centimetre dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
