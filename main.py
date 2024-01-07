fcb04a432a9e87e500e6f98b8c7672c9d3acc34e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:49 +0000

    slide brave ever sting 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
