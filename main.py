68a971e742b9bc89c0d6a686fcc95bd27fd7474a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:02:02 +0000

    below behind certainly occasion birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
