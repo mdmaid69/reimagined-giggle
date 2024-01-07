adcf6bb8a62f4ef6c9e1e151cf04a36fb20037af
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:34:25 +0000

    principle talkinto tooth close total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
