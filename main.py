5e16883afdfecaa33bc9bb9e58d584f7ab1e64e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:22 +0000

    stage can justice desperate shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
