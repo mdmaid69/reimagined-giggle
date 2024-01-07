6c10077e025ddcafdab62276525bfe3c8e102a83
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:51:57 +0000

    turninto number war go people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
