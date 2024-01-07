909dbbd1072e592c3fd5d099eacd71176e4bb913
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:41 +0000

    geton must transparent tip attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+def print_python_version():
+        return sys.version
