e4506b7f3ae7ce151ec422bc7b0a8ebb22512ee0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:11 +0000

    short upwards mistake large understand

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
