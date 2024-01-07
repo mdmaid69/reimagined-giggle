17e467bfaefd7dc2e941c3e177339a27b78e84e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:03 +0000

    supply she record disapprove anything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
