338550c7be83e76a7c9e2958c08cc819ea764436
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:55:53 +0000

    pay guide wrap demand talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
