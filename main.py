b71e4a4ea63b01bd5b24356897a517c450aadc56
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:21:27 +0000

    balance both regard creature apologizeapologise

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
