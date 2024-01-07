caa82c62ae0441540c395b1f89babea8a129222a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:33:00 +0000

    position Oo temperature mass back

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
