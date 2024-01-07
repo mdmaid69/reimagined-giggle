1b9ce6b7159ae39fa8fc126bb5a94ca91443ec0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:31:47 +0000

    constantly off interview unable tend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def divide_numbers(x, y):
+        return x / y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
