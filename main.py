b135765433a133482de4cc181afec5129e13129a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:47:23 +0000

    frame worst young intelligent detail

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
