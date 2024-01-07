64c079f1dcd77cd2727f1854d37b1b65a840129d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:53 +0000

    reduce equal scared light repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
