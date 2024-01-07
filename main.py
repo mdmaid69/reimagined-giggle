c27eeeb1e56547f7f48e7843f548c3858abc5975
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:07 +0000

    neither beside they miss development

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
