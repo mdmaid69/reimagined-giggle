f13497090b4eaf1e33258970b708e88d4c0681b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:25 +0000

    road prevent impossible comeround reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
