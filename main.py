a9383be145ee4325aa4af687572d28abb3f10aa7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:48 +0000

    smile it care man before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
