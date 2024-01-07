e4ab73b48db0d65ad7a2e5500a7646907a307cc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:48:41 +0000

    attach very sore stay maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
