d553ba57932c5991832119ae3c57ffac8c001265
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:27:12 +0000

    sleep urgent rob wide platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
