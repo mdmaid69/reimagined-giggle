d1b754ca75e4b6a8883f57d3100aa2145d2b5a0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:35:30 +0000

    here very fire special vote

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
