c35595f732cfba9c76285d65471aade39478c75d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:37 +0000

    sea bet cover lie ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
