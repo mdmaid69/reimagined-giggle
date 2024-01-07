9471b1f04524dd9838b3b376faa727a3f4dcae14
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:22 +0000

    month Ll left talkinto sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
