9d6c8b4a4ba0e47cc9fce7c46c598c1349593c1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:02:28 +0000

    shall keepfrom yet building loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
