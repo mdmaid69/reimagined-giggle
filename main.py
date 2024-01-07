dbb480868442d8a265db4524b42c40f5fe0de6a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:08 +0000

    wear sure properly gas shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
