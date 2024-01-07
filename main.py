ffc56634d087e9dbb20dd52960012d62b445f3fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:07:39 +0000

    emphasis tune peaceful lookaround go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
