bc1225dad53a55d158d8da81679121dd4378a6fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:17 +0000

    book plan moral alive comfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_floor(x):
+        return math.floor(x)
