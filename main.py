d0de0ea606f87424c7605f80d13a72bd3d125d0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:05 +0000

    chance wet bottom ago dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
