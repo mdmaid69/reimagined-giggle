641ef21f48f9fd00b738376fd13a1a737aa63311
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:04:59 +0000

    opinion tune Ll would conversation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
