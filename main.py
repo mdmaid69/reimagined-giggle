8a3105e264382a7c7c083c8ee286c7b6d925ba22
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:31:31 +0000

    wind left earn corner upstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
