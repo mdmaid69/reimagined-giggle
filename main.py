ae847cb9d1c902a73165fb2126c1073d1260ade7
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:54 +0000

    destroy polite cope build absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
