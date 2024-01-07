f896b8691f84b3bc1bfc8c0533275355e231c07f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:17:37 +0000

    port please remark need lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import tensorflow as tf
+print(tf.__version__)
