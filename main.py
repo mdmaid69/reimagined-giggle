2d57c8ccc0a6a89b9942e3aec6b7f4f8b9552ab2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:45 +0000

    Oo bush gas school congratulate

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
