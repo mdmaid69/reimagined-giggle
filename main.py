eaaae3ee39307d103974cc51168b8b668918697b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:26:20 +0000

    less hello cost somebody free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
