5cf4495e4ab27b02ad6fbbe16788fa1888f99a0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:44 +0000

    test happen shop wander office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
