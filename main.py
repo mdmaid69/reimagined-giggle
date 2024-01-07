8d3986e7236aa56f3b075b91d31e7951a8604923
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:30 +0000

    science plastic curious lend backwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
