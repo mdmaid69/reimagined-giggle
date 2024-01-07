4382eb75865120dfba9aee0c3268bb299ecb385a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:08 +0000

    calloff thirsty dry careful detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
