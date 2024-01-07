42530ee7b1fee46edb829a84cfd1ef3952099a5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:41:52 +0000

    talk state bill delicate film

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
