b92d98d24c12079ca941a7a63cbfad10b5db4503
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:45:59 +0000

    insist persuade collect arrival comeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+def find_min(numbers):
+        return min(numbers)
