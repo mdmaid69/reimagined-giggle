59d7762a8ee7157623e97c8ea8feb167788ed1d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:27 +0000

    forever practice lookaround turnover solve

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
