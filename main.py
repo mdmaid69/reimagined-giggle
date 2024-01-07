d1198e80210449ebc4d5df108ddaf506cd7093d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:13 +0000

    by conversation period part central

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_speed(distance, time):
+        return distance / time
