97855603b6221f6f7442f9411de465340e6c02d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:27:33 +0000

    interested board scrap bright feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
