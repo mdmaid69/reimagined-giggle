e9c2ff4ceabba3bd9aa40b73a2b2be0c13cf5162
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:47:39 +0000

    fast clue thorough waste ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
