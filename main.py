31a45e5aa6fcba4cd16bb2857ef2bb0f6186ef91
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:15 +0000

    nothing promise come excuse could

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
