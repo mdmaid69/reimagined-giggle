c70202400981da631a64ff6a52b04e8656cff50c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:19 +0000

    food fault Tt call chin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
