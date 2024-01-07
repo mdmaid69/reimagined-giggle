f14826514db626ee0de48871ab07379f50b49d9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:03 +0000

    front string tidy worse payment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_user_list():
-        return collections.UserList()
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
