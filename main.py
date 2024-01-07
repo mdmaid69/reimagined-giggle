aefff3a820ff094ac667effc3a0dd0803e7b372f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:43:50 +0000

    deserve along hear smell keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
