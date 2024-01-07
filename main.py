59261fbdaebf4507888e5a839e567bceffc1d273
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:36 +0000

    future afraid determined comment while

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
