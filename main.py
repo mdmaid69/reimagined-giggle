9641f57ee3b6991025d03c6b91ba62031e95a394
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:15 +0000

    criticize Tt opinion room detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
