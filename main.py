6d93fc7f619c944af0d05ccd144c2e07fef6e88f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:57:24 +0000

    greeting demonstrate spite miss by

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import random
+print(random.randint(0, 100))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
