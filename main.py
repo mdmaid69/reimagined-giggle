4860356b904f5978874bbdcbcff6ef815589a4e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:52 +0000

    young also black expect support

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
