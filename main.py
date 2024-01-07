c1f07ac53f4cc8934d6f57bf97fb8f71a3d160e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:52 +0000

    direct bottom worse almost post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_exponential(x):
+        return math.exp(x)
