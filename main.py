b3018c24405ac4a7d22697e361e07e50b372024c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:48:46 +0000

    grand Vv dick except storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 def greet(name):
         print(f"Hello, {name}!")
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
