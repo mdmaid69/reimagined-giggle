0ac57b21cfe96b7b5f873b32aa37c2d86263394c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:21:08 +0000

    basic stay after themselves sink

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 print([x**2 for x in range(10)])
