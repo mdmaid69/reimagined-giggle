42208783ff66a783a69dddb32374e752599957a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:57 +0000

    criticism nerve mention uniform to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def divide_numbers(x, y):
+        return x / y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
