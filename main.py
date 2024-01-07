05536c70885d675be68c2732af1ccfcaf6bb5c8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:51 +0000

    then national house represent trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
