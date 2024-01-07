c70b714b7a3004019dc5cfa715a02cc13662c2e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:11 +0000

    thinkover note while long sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
