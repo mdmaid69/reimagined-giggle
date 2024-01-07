1d3a9fdfc24c44dd69da0e93bd43f037c2b08140
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:48:36 +0000

    air behaviour tidy holiday aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
