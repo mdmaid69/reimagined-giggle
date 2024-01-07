89d121196eb8f6f3103b35c1052bbea16fe2d07b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:25:42 +0000

    swimming floor tense picture breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
