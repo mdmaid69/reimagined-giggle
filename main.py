3a7057f5e6976c5777b6a287b2a2880b944efe01
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:52 +0000

    Cc Hh holiday physical kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_floor(x):
+        return math.floor(x)
