c0515d8b8219467a4038a99383fc5eb8063c6846
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:49 +0000

    begin uniform twist tall radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_power(work, time):
-        return work / time
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
