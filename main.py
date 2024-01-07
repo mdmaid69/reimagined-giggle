ee89676bce09ec1a1e67c47ae13effb780aa07f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:54 +0000

    remark shock myself begin rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
