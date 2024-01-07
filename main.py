e19cbfb5a59638c1c39e0eaef1b7a219fad2d776
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:16:51 +0000

    field justice appear hurry the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item(array, i):
+        return array[i]
