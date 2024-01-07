019277a0e9ae11c416848b85b8406824a0929a6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:31 +0000

    sleep holdon team school threaten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
