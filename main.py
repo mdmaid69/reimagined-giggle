b1630d86deca732a537571e2265f603b70165f85
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:34 +0000

    stupid brush priority computer ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
