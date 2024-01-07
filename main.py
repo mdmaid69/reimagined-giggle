13dd3751a3a45397d05a5d34a88ae48271b36dee
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:20:18 +0000

    related word shape ring spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
