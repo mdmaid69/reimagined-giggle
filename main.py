4675c58c6576014791b525d815bbcca8159b0ea9
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:43:54 +0000

    confident again confused bridge neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
