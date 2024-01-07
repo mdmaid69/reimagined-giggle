c72fe5e13f72b72b1299f0fd1bdea64a21b6a417
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:14:08 +0000

    tone advice mood Mm modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
