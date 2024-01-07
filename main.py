fdfdb715d651c113f397d88366a5d1304a27d7a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:17:10 +0000

    ago punishment reserve between bringforward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
