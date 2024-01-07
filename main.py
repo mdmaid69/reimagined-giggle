f4fa8f2922a07988a76454289811becbeb7b6b44
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:37 +0000

    waste press offer address face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
