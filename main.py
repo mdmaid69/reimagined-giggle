f9903ae75643f806cf2bc49d7b252c0e3c6c624b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:03:38 +0000

    me whether short risk vanish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
