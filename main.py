041cc52b8396e6106425691f87f9e6a799da94c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:07 +0000

    occasion visit town wet flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
