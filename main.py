fbc5cdeb2bb6d5fece6e8c67cedf9e91ee7c9c32
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:28 +0000

    worth plant book wealth favour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
