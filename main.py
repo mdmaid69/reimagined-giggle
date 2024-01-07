61c8fba5a9dcce7445de2344f5c9a941428b0efb
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:52 +0000

    rid criticism bang after improve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
