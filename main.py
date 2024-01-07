7de59dab4c68abb4119e2d8590aec7223b0af383
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:49:05 +0000

    relation exciting bear wall 565end

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
