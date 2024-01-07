eb5d0035d408a7bf84e41e8a2e4e97119090ce7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:03:23 +0000

    question makeinto dry road talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
