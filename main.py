2a82eb5983f49860c7467737b00fa05b6d41fee9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:11 +0000

    limit world room swimming principle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
