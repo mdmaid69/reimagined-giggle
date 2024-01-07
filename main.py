a46b14db3e5135290dfe1a6d56b86b27439170ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:34:42 +0000

    dear breakin across far callat

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def multiply_numbers(x, y):
+        return x * y
