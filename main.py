cee00cbf359d68bf6fead9c0d5188333b4c82f63
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:56 +0000

    disappear him band open onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
