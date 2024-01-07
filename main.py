34a69c1f05fa475e692e2f9e3d38aec91bb2d10e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:17:02 +0000

    before properly arm wing possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-print(sys.version)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
