03a46a615fd62bb9214249df46b45633f087ee91
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:09 +0000

    ever sink its set event

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def square_number(x):
-        return x**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
