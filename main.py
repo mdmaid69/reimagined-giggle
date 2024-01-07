370ae837976509981341fff8566be733cd0ba725
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:36:01 +0000

    ocean unable plate birthday government

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
