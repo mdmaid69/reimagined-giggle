d715307569fc414353da5a23d21a10e3fc4d2527
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:04:41 +0000

    choose world somebody argue bunch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
