344fc9d07fc02a145e9cdf853a711e73b8204a90
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:27 +0000

    family closely history appearance bet

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import random
-print(random.randint(0, 100))
-import sys
-print(sys.version)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
