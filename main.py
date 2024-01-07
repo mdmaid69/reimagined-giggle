611f5435a45911f76be984f5da898fe7614717d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:45:50 +0000

    room use sign wide nature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
