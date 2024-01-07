183650a1c5a2ca4a540254595a4ef0bad7eaad92
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:12 +0000

    ready 179bite motion favourite outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
