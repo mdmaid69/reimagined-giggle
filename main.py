0deb388ac6863a55091da03a896485eca17f4b72
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:36 +0000

    interview promise realize destroy except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def get_array_as_bytes(array):
+        return bytes(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
