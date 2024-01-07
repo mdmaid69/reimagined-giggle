f0439fa469e24ac5dd6dfb57d8daef7a206e4007
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:27 +0000

    post platform else way pay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
