876261d2654a1b9af1acedd9c5d33349c0ddd369
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:13:08 +0000

    change complain brush impressed himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
