4172fef6bef26d0e3ab164681b8d470d046e359b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:54:57 +0000

    condition stretch Itself Yy chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
