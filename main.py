60c248bad7ed8b0af08b5dee5bdf6cc31f8893db
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:44:36 +0000

    circumstance around union slice motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
