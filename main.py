525dcc99387c1c1fb759500feb8b9fdf0b5af41d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:41:40 +0000

    remark evidence cause use loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def is_odd(n):
+        return n % 2 != 0
