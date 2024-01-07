fd9354711835cf2069cb5bbb66d142c6624964c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:31:34 +0000

    go point modern kill great

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
