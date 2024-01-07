b714217ea568e40eb4cc3484ac2b2dfb355a79e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:23 +0000

    rather error big Ll national

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
