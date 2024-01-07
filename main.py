c52d57464af0e7871a738ddf4349ae28bf78f915
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:21:02 +0000

    tie towards rarely considering belong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
