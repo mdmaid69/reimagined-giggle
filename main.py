2be9224de890c0458e5d08fb9d1982fcd98431dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:35 +0000

    group excellent own fast people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
