cb83abd230f694c7e1a1327b81fd9a8f1d09d7e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:13:50 +0000

    interview pipe soft building discovery

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def add_numbers(x, y):
-        return x + y
+import collections
+def create_queue():
+        return collections.deque()
+from collections import Counter
+print(Counter("hello world"))
