3891300bb45221f75f0e5d284cddb52311a6d415
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:41:32 +0000

    nerve group damp belong success

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
