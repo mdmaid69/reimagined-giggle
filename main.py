4a72f930fe2e7eec7acde856391bf8aef45def40
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:21 +0000

    region settle busy Ff no

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_base_name(path):
+        return os.path.basename(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
