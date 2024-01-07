88c72bab6a6e377a03a71f78b1eef6b4875dcc17
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:58:51 +0000

    task lookafter opposite confusing anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
