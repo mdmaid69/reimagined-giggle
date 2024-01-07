c64beaadb899bd8a083db414bf51356582eaaa3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:45 +0000

    reach affect cut flood autumn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
