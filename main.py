23e692f06ea0921645cf1271618f36833431054f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:35 +0000

    poor interrupt regional secret crisis

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def clear_array(array):
+        array *= 0
