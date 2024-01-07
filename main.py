672f4da34f3dee25a23a22dcd21948243e05c9d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:53:37 +0000

    sister callout extraordinary inside obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def is_even(n):
-        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+print(time.time())
