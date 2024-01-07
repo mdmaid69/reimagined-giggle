e0ca84cbb6dd7e0ab2daa0655379519d033c4a3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:05 +0000

    keepaway explanation care breakfast anywhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
