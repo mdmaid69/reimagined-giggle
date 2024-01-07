748cec9294c62b30e9ca94b9530158a184bc2adc
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:06:28 +0000

    lookup copy broad lookup too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_buffer_info(array):
+        return array.buffer_info()
