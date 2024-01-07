a7ee4574017653bd60433a5f0c90a29acb5e7cd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:00:52 +0000

    name callat cause Itself swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
