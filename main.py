d54dfc4ed249c15b77bb53481faf84cec70849af
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:12 +0000

    operation pair offer ability train

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
