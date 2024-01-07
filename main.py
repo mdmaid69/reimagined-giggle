0c606efb28249734fcbfd1f0bcdbe242000d3d28
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:17 +0000

    giveout refuse change ours gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import time
+print(time.time())
