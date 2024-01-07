81670f427aedef44e4b653d71dd668f27ae9eb24
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:35 +0000

    join develop some to careless

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
