152469d931a148fb5061f925029a1594cd1771b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:31 +0000

    language familiar broken evidence middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
