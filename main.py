a4b5b6f72fcbaf28e4a0808cfb54200815c21c2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:07 +0000

    must wear tense ready violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def multiply_numbers(x, y):
+        return x * y
