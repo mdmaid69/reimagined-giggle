abbdced5a156c1a1f70085de586cb96bd3bbb1e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:07:41 +0000

    debt behind payment crowded land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
