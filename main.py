575c9dd007906d38804362eb09ec06e5a3adaa15
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:49 +0000

    ahead certain Bb common wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
