6c461aa6eb576f03cc28df36438a05af001f3b81
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:29 +0000

    stamp question bringback spite just

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
