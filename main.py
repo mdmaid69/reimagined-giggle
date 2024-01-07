0383aa2f140f8f46a1f6914278ba9fabe7d1007c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:18:30 +0000

    sport fortunate justice traffic load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_array_as_int(array):
+        return int(array[0])
