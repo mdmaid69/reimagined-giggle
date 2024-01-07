87c5b802e8fe4b832afa2f7f991edb8a5091edd3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:05 +0000

    impatient complete most classroom release

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
