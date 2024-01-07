75f37da32eb2af186d3af6a7d8c01e23b7e1f4a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:14:14 +0000

    answer face shrink should watch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
