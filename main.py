6d2f994fb84f9dfbc415fdccc74ae52738822abe
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:55:00 +0000

    runinto undo recognize programme direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
