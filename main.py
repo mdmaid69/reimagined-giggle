b0ec9b438fd8a410d1ea327a4c1ec48c50d0729b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:14:33 +0000

    level trip sore burst share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
