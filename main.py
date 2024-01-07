7d054f8412c7a11e8ce7fc99cc8b019c5951f8d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:31 +0000

    tired forever ground mark mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
