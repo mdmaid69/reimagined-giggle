3ad93826b5bdb6758abe9d36e1578c014f15e5f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:38 +0000

    people arm scare cheat straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
