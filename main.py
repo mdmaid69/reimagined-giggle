e779ac506144c131c8ae90a0586ac71ab7e3fa7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:26:40 +0000

    make break towards vanish best

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+for i in range(10): print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
