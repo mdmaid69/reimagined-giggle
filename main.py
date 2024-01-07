cea29d93996a769a098bd137b1fe247568e37a65
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:35 +0000

    short shop fast miss familiar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
