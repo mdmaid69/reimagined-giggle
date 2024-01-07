4de794426f80040adf2e0aa1d89cf6668f7b18fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:17 +0000

    snow succeed concerned solve more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sys
+def exit_program():
+        sys.exit()
+x = 10
+y = 20
+print("Sum:", x + y)
