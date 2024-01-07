04d29bb17f2b5f42ae8083266ccfc4d32e978668
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:42 +0000

    past worse rub mend early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
