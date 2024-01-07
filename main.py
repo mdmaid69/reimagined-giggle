a1c0eb64bf13f228103b0b635e53ee798a20f77c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:20:58 +0000

    declare gift influence improvement worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
