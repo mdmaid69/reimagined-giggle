fd62a889090f77b26945385495166698b8d7f320
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:06 +0000

    thinkthrough course flight behaviour maybe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
