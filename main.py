9d9bde76509f12aad0b09943c992cf643f96c35e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:45 +0000

    poison getback watch protest sudden

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
