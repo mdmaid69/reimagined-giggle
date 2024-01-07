63434295ffabad5d5512d3017f6aa61136ecf586
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:04 +0000

    minority circumstance half art fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
