62b8062334ba67272860b7bacfbe24597d6d24b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:15:01 +0000

    school material sudden government praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
