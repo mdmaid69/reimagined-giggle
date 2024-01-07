870dd35f5e97f7221827e5f406f173596cc70003
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:19:12 +0000

    today compare grateful bite best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
