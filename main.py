698154ac08d6eb6a6731a6d3d7a7cfb2c06a44b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:06 +0000

    possibility normal fair underneath strike

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
