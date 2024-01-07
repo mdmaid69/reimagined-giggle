5033e61b34d769fa786fa28fe4859ed2fcd8e19e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:27:09 +0000

    spill phone wind run scrap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
