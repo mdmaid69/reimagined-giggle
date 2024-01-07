fe6a6a85515547263d94e8a5df6ab63b6fd93320
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:26:26 +0000

    protest Qq tooth shake arrive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
