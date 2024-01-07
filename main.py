fa1010af56d5db4bbea54c58fb96c66816e2e442
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:58:54 +0000

    measure steady bit circle fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(lst):
-        return max(lst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
