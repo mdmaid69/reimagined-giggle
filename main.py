6ae6c963499cc621b0a3f59b7551b53e35d7e6df
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:52 +0000

    everywhere sit about 570enjoyable load

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
