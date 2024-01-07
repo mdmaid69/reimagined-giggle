91d7b2ed1234dbcc7f1eb97ec751f8a55ade55ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:01 +0000

    trouble move home storm safe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
