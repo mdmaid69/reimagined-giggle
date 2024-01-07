ba5a91f2b6c6b30e2bd2d1e40468fb58a1f4a164
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:43:21 +0000

    turnout callback entrance narrow body

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-for i in range(5):
-        print(i)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
