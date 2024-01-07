02835a3eec929700ccaffdb32093b1db2cf6bd11
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:15:35 +0000

    set swimming wonder comedown exam

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
