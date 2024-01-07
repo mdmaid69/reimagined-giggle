9f64bfede7024c19bf2ad1a3f5aca72c561d06a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:48 +0000

    priority trade basic stretch agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
