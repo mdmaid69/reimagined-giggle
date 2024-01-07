59538b2387ef8e3e1fd941b8cb8cc95845f91283
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:51:24 +0000

    step sting care unfair want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_current_time():
-        return time.time()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
