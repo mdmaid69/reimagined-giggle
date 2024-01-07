32488d524625a6e369903f57857934bb7a16fbde
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:15 +0000

    pressure quick hello plenty fun

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
