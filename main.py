460be01cdd10212bfa5cc468647a6a6237e61a02
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:28:44 +0000

    false quit comeround so allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+import numpy as np
+print(np.array([1, 2, 3]))
