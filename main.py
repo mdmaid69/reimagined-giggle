f809c2d8a07cb36f4f52d4e5880b7c83845fbd1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:11:12 +0000

    balance fear thinkthrough order Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
