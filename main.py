04aece25ae7d0a7ce75664814e3c1f655bdcfa66
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:20 +0000

    attention criticize children pain blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
