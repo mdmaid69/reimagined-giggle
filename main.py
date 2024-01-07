74f81b1184f176f41524aaf25d043b6185e975a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:59 +0000

    month unusual copy much brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_bool(array):
+        return bool(array)
