6cbb445d28fa816639275ffc91542543a35b6cb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:11:01 +0000

    exam getout window fair form

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
