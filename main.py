bb20ab40649ce1c1e6a908e880b85a9fb1cfdfdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:01 +0000

    lucky measure in everybody sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
