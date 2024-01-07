ac6170569a2025fd69e4d4e3d38a3a75d031ad0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:57 +0000

    less side keepout think search

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
