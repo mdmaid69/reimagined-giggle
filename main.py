a3736caeb2eff45c196358e79ea45fbabc61dce6
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:11 +0000

    runover turndown main sad plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
