7ee2df9e41743a41fb3e6b34022867ab08a425ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:16 +0000

    drive can ring splash trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
