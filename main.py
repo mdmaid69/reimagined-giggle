f903141dc40640a0ffbf8c383f5edde544120fa5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:57 +0000

    active part further clothes neat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
