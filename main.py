ca8e107d725f30de048a820d57a14c866b3236b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:47 +0000

    excited heat runout mother Vv

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
