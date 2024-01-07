a9918aac044a308ce7350e4b73930cd7cac66b7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:07:35 +0000

    touch record surface lot attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
