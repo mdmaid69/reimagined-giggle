d3d726256a4ca17e242021ab82c02775a4b34dad
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:23 +0000

    discovery family production discovery noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
