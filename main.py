8752714b5d028837674d8c1cd916cf19ffd50a64
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:59 +0000

    always protest extra stamp towards

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import random
-print(random.randint(0, 100))
+print([x**2 for x in range(10)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
