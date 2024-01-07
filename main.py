dbc0b03ea87f083f5c7289137f67588c4b8befde
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:25:09 +0000

    develop punish capital make yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
