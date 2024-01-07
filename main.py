0f0f0f759e903460e4d67379adb370e937041ca7
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:19:06 +0000

    instrument care chat slice attitude

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
