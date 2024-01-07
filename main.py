42416fce36b568951394e261afa066136f69a4a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:21 +0000

    in score search decide nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
