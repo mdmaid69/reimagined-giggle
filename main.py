262ac96abdd6300c0c61026a051ac5bb0b7cc67e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:52 +0000

    string now right roof among

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import time
-print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
