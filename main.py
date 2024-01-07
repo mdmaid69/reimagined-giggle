1193363699ecbfbadd18164b7a583420fffb5246
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:22:27 +0000

    dry complicated balance steady neither

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
