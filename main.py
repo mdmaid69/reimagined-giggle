9feec27659a7e3a16365d64ab56312e1f523f4a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:14:09 +0000

    hurt turnaway autumn trouble complicated

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def subtract_numbers(x, y):
-        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
