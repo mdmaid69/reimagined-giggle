75fb8418eabd03f748a44268034d489f394afcd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:23 +0000

    room interesting sell hurry else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
