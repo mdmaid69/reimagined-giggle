0b909736b31967c86407ad9911bd2c65dfc80e59
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:30 +0000

    too capital light exchange congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
