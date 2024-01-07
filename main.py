025c9f9e680d1bbf9e14a55bc128e28f487cbb6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:29:06 +0000

    service change dangerous recently worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def count_characters(sentence):
+        return len(sentence)
