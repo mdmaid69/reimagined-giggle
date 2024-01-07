fc48e5d359bb28f470b3ecbcf59b129a03db56d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:30 +0000

    maybe regular share picture insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def reverse_list(lst):
-        return lst[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
