4dc12b96209ca1adb207f616e3bef15b4c34e124
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:59:49 +0000

    vanish that congratulations disapprove escape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
