01ec2188cd2676e013ea90392797d8ae8fd1c175
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:53:50 +0000

    lesson scale breakfast bright fence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
