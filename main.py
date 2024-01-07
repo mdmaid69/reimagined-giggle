bfcced651f374448618e057df308dadf5e7eefae
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:27:53 +0000

    regard zero present should tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
