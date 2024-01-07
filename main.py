9856fe947cec1948d2b6eb54a36c66d3a5e9fc4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:15 +0000

    Nn dress only key eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
