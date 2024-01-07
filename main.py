ff03a1274d5cc86690830dcbab5c7f5acb566313
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:08 +0000

    fortunate nowhere proper tooth thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
