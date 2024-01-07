ebcb2f6acaf6882437bec4efd1841285caa308f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:04 +0000

    tape remark correct best set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+name = "Python"
+print("Hello,", name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
