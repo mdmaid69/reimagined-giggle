057fba7bd2e7cc25e76ef5f01315dd7e04bf0cd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:06:36 +0000

    flow surface surprised bread peace

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_length(array):
-        return len(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
