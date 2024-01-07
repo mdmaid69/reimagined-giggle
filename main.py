d98d3c02e72ed9f7bb986427d953ff3d33bed220
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:03 +0000

    Qq majority tend influence safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
