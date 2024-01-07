7f0e4b4f1b2aa27eae574bd7cf628bb58c37dbcf
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:56 +0000

    occur exam fun date mouth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
