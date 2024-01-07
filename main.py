5af336c538ff38c3910ad7366b5c5946b60dc815
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:36:27 +0000

    thorough means drive morning project

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
