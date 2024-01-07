b4465f61280c0a582c7e0275700965b23f3e49cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:31 +0000

    middle pot ear fortunately dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+from collections import Counter
+print(Counter("hello world"))
+def find_min(numbers):
+        return min(numbers)
