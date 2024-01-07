f5bc26937e55bead6303f5763a8810577b901d59
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:45:27 +0000

    bang group behaviour breathe now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import platform
+def get_os_info():
+        return platform.uname()
