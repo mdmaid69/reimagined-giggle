8f33191c82c2d289a8e1e6b52798933a544668a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:22 +0000

    port from never to colour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
