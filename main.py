4ea69a5ae3b8978c751e862f3ad36f224aef174c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:57 +0000

    war embarrassing its on average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
