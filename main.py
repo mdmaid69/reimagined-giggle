d27b6ca9dcfd8dd65047a72dfcfa9195b57c6e2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:48 +0000

    sign rush store juice uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-print(sys.version)
+def get_array_as_memoryview(array):
+        return memoryview(array)
