c6e9bb70cd452bf6d2013cac33840aee34079055
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:31 +0000

    cross natural create odd long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
