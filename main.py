940185881fbba4d1dc262a73e59b44605942209b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:06:15 +0000

    depend callback drawing breakfast personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+print(datetime.datetime.now())
