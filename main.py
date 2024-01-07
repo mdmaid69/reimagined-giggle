3894d96d18473f595c0b949e8b6e1a0d54db042b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:28:10 +0000

    recovery task pay geton angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def greet(name):
+        print(f"Hello, {name}!")
