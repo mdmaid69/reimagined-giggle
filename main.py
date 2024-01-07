615ab64d99c1f4fccb852b9f7a7abe9f5635b5b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:07 +0000

    climb likely hungry flavour recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
