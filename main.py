ceba2baf80839136b0436082a1554fcd1ea75933
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:11:53 +0000

    design nerve rain yesterday favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
