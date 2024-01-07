5a88a43605a57b357ce22822198b3d3ba8947bfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:48 +0000

    embarrassing mine practical estimate mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
