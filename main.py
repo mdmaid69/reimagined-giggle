dc2d0dd36c4cdc29c50c0dabcc134cef25434538
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:32 +0000

    rarely calmly ability goon early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
