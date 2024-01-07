e1b86e33655054d18cdb06f477b5dcd23b48b7dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:53 +0000

    179bite afford shop ball forward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
