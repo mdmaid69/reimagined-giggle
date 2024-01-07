ae01e7383b4810bc84ae7d1cf70ae9c549c52e01
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:37 +0000

    stranger weak narrow except boring

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
