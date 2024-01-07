4f02d2bc8357f33a1787bd481bc5fbefc09f8edc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:24:09 +0000

    lookup yours anywhere charge grand

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_set(array):
+        return set(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
