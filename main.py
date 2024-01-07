74c7d636540238a832e0a1603a2f77f75b87725a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:23:30 +0000

    anyone opportunity possible build Nn

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
