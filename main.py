bd34b4e6969d670bdd5ef2bad180c300cfda31ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:41 +0000

    advice trip smell clean pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def reverse_string(s):
+        return s[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
