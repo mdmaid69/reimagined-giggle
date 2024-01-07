b6f3565ff79471b0f30087d29a2b3e86ef812008
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:02 +0000

    actual tap feed later difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
