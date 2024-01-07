622b5c088ce2f462ee38d8143b6cd17b5a39a757
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:02:31 +0000

    error tend rub explanation some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def sort_list(lst):
+        return sorted(lst)
