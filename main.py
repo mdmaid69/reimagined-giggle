c5d61230d6df2e18216ad416e8bf90a1606bde8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:50:35 +0000

    smell recovery born conversation approve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+from collections import Counter
+print(Counter("hello world"))
+def reverse_string(s):
+        return s[::-1]
