0164651a36aa5cc43545d6a7b1068f43c9163b56
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:07 +0000

    this urgent hurry great odd

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
