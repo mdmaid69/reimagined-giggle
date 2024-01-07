0701a15759aff86d9074cda225ce5b7df9f5cbdd
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:57 +0000

    seat grade our spring tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
