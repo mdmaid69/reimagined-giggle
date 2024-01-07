df17faf974f812be32bb38286fe55f971147a2c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:28 +0000

    minute wrong time appear nerve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
