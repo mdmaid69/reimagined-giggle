d34308424543a68b9b09c04a08654dc59b0f64fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:13:41 +0000

    opponent born hit nose along

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
