3ed3ed448f8b8d2d9399bfc11c8e5a8b6b14182b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:47:25 +0000

    bang behind exercise sometimes innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
