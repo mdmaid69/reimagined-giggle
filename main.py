a8563d808809a99eb68fc89f3e7f87135589b2a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:59 +0000

    scared then injury congratulate twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
