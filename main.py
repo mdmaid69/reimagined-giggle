2c4a37ecd8145fd4891edc5ebb4e8833ddd66bf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:39:02 +0000

    never by exploration cinema good

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
