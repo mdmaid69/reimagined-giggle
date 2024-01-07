49d1d983dae35927d1d206b37e3918b6ec8b34ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:22 +0000

    real record nonsense family basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
