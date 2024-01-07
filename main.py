4a8b690ba8ce315bc5060a576beb54264de88cc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:55:58 +0000

    situation needle shame suffer attract

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_area_rectangle(l, w):
+        return l * w
