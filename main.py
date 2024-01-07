53adeecd63010e76622c43b57e3131aba19a530f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:02 +0000

    dress measure human touch excited

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
