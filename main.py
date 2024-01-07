06e6e0788d03b3cd1d000ecf6fbe71bceda04d4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:48:38 +0000

    just health build criticism public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
