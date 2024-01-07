7646f777ea89d061ba698efeb67618fbd4c952c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:29 +0000

    safety die discover car speech

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_work(force, distance):
+        return force * distance
