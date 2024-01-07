62177c9a0fbf5b38a6568dedec37ed0594d5dbf4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:02 +0000

    sound word avoid trouble quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
