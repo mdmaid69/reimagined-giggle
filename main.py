7c9d881641d2bfc69b3ef1e70e1c9fb082908a0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:22:26 +0000

    country traffic tough well lookup

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-for i in range(10): print(i)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
