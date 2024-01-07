8dc2662ff842f1b8a9186cda676aeb02fd0ebf45
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:55 +0000

    settle settle talkback should powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
