23163801640b2c0436d09e4f2379c5925276cd19
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:26 +0000

    inside 568energy 568energy direction frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
