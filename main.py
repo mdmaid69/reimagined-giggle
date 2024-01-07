94567952f1eba96f7a8c567cebba2cbf822112c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:00:04 +0000

    equipment shop remember arrange still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
