558104b6734c990b46c2ac0d6bd77e8b6cab1e77
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:58:16 +0000

    steep comment hot transparent shout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
