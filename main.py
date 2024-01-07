c5688891858cf1fbe3b6cddd5c17ac1e61dd81bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:19:50 +0000

    fortunately bush 179bite cost left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
