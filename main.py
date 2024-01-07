fa18d1b5de2fc159237d1c72243c08548f1d7cb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:37:35 +0000

    manage present decision relationship collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_item(array, i):
+        return array[i]
