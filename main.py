f570088e0f11fb56180ad8512bec3571625b8b08
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:59 +0000

    innocent now by follow pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def split_path(path):
+        return os.path.split(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
