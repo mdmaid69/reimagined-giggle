b447e6ecd9c927695af97dcc195b255d0ec1a82a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:34 +0000

    uncomfortable give after welcome Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
