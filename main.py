e5d6de7b73a3458a67773122dab88fb212efd571
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:48 +0000

    century plain typical for club

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
