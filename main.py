2b1af777e3ad2df4da3f0b1879547362180dc911
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:01:07 +0000

    wise runover sometimes serious closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
