27d5c6f7f952875222047bea9732ad72cb235e3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:36 +0000

    square turndown war love sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
