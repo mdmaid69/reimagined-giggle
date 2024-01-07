b8862cc37120d2e70e17ba12501191a7c1247252
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:09 +0000

    from path bottom before round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_string_from_array(array):
+        return array.tobytes()
