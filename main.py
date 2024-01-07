a20aa7a004a1d931778f6c41c879beba09cabbd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:53 +0000

    come official save left connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
