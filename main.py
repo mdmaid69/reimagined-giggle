b8bfd1866086a3bcdbe4d07b4b107e22d0507014
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:01:10 +0000

    guess correction letter next forgive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def is_palindrome(s):
+        return s == s[::-1]
