a57eb776a509914bf82a36924a3bfcef9f9e7312
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:04:13 +0000

    ordinary away rest behind 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_speed(distance, time):
+        return distance / time
