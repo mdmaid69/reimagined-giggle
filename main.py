312096cf38dc942aabe87a46eb345b655abeb97b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:46 +0000

    part turninto somehow rate understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import os
+def change_working_directory(path):
+        os.chdir(path)
