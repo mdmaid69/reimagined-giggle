b1faa91e4d153d9e7740b40980a6b30c5a6d363d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:18:37 +0000

    proof terrible failure shop confusing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  def calculate_area_rectangle(l, w):
-        return l * w
+print([x**2 for x in range(10)])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
