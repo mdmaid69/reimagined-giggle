f65ebb74d4249a8349bb9e1b670312d9f04b48b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:31:35 +0000

    pollution pencil choose deserve turn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
