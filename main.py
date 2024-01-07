7454bb8dc90cc84988ac3c3115c9c972f9cffc51
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:40:09 +0000

    lose gradually open tight disagree

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_to_binary(n):
+        return bin(n)
