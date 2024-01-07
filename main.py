c0b64ed8a3e4d9f53b5a90ffb0b8fc6bb88d7767
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:48:51 +0000

    produce thumb spring police nigga

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
