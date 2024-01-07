edbc25370521af9f2c6bce11c3a4759123dd75ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:51:29 +0000

    sentence beside fasten common the

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
