0bacf7bba264f3c1d87a7d026f3143c9fe8cf527
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:11:53 +0000

    until bringdown thick rent each

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
+print(sum(range(10)))
