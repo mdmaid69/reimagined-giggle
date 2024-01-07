7c33cba4e8fb40d19d3df0714b95dc5afba81515
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:03 +0000

    herself further bend question search

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-for i in range(10): print(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
