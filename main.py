ccfa37727159d9e99fb47b041778a227ed241d21
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:30:24 +0000

    train middle problem eager believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_typecode(array):
+        return array.typecode
