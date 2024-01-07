1b2ba186fddb34876c34f3e7d637f51a0ce8d6c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:18 +0000

    reason pain Ll environment curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
