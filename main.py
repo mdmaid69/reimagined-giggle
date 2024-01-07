535cfbae3bc3183e0c5e5d6a316bdcce86f4134c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:04 +0000

    565end design Ss bang rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
