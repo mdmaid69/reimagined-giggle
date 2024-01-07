4149b206efbff36ffaa95f872ec4edce7a768957
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:48:18 +0000

    risk use whoever company breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_set(array):
+        return set(array)
