3f8cf0c766dfe7fa6354aadbb500ab5f37955e40
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:31:04 +0000

    boil especially speech firm bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
