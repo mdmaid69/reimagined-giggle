4f80778b40dce3d61346e3653d1361fbfd6c2e3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:49:48 +0000

    friendly count tape accept takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
