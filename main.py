96b81f7c07b3408fd3a069d022eaac91f1921bbb
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:24:54 +0000

    land confuse sell also thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def remove_duplicates(lst):
-        return list(set(lst))
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
