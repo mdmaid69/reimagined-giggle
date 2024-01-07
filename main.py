1303fdeacebecf4975a4edfce442858a7d552416
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:52 +0000

    deal dangerous duty little discovery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_power(work, time):
+        return work / time
