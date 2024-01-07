878cbc52843782782427172f939e656cd7cf7936
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:57:14 +0000

    evidence peaceful key instrument ill

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
