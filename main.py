c5cc60a8c47f517780b528ee73e382893010b137
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:07 +0000

    promise town but disapprove defend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def sort_list(lst):
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def sort_list(lst):
         return sorted(lst)
