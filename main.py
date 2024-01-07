129a2951766212460cbfef77d7e420e553422307
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:47:03 +0000

    error reply worry unite herself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
