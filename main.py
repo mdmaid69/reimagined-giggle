d265ebe76534146745f8398bd0ed2206fab8b808
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:46 +0000

    clear smoke tread under welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
