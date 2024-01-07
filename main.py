5e95140e6edfbadc982eee49a4fd72d10d03a718
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:08 +0000

    real worried record vision allow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def is_even(n):
+        return n % 2 == 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
