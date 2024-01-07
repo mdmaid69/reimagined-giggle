be88a4ef1c4717e2016639457d4bce6034a8c0aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:13 +0000

    after prefer manner absolutely surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def square_number(x):
+        return x**2
