afdcd384d928afc132977aa88b3a87cce80f3ac2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:11:34 +0000

    chance report weekend set notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
