f9be56705c69695fc2b1f63644e64d138e189284
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:52:55 +0000

    decision base full slow wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
