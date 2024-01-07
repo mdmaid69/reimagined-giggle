7648764c71f187a0359911f2255782a2359d943a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:28:22 +0000

    better geton Gg raw support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
