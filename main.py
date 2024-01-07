2c6e86c9eb303115151cb8e2616fe6d7c259a438
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:20 +0000

    pump simple protection repair talkback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
