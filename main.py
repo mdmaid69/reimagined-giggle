4cd8fc31570c6a0617ad24debaa8d7233485af26
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:43 +0000

    extraordinary danger blow obvious most

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
