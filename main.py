629dcf62952c04ecf727285a57e085a8d1bf2f6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:45 +0000

    demand ordinary top autumn squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import getpass
+def get_username():
+        return getpass.getuser()
