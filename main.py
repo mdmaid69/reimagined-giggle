ee9aae12aa7106fd8978cbbdb8b82d2cf31bc5a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:29 +0000

    trap future feeling at shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
