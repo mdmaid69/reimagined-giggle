e3a508ed10c958152d48764161426bbea6be957c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:01:09 +0000

    mind study fly catch blame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_elements(lst):
-        return len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
