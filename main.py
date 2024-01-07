0eafb998721064fa304ad8d76b9900b9e0a412fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:25 +0000

    drawing table whatever card pinch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
