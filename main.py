b13083d2ae670523dcc3d46f370a93d4332b70af
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:17:02 +0000

    whom love pencil create rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def count_elements(lst):
+        return len(lst)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
