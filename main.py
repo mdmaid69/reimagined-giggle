938cf0c2ccb555ee275510b3d20f03faa3071725
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:31:12 +0000

    meal last sign situation experiment

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_density(mass, volume):
+        return mass / volume
