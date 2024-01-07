a906c2be752c799a76d9c0bcc662db88eafecba0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:01 +0000

    step fire smell mess turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
 print(os.getcwd())
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
