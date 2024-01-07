0e6d63943677a2f81d46fa085379a8c38d4e1843
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:21:14 +0000

    beyond almost exchange cross motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def square_number(x):
+        return x**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
