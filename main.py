3d19d8d2ca1745866f944020ef7c1479504f2fdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:26 +0000

    serve list chief pure keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
