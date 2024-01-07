bf0d0970b00aadffec9da74cc7469875a8c89c93
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:02 +0000

    start sting university holdup combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_file(file_name):
+        os.remove(file_name)
