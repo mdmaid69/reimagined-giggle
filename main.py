1def47e73bf14714ffa423e28ee78ab1cd35be62
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:51 +0000

    seem argument him disagree for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
