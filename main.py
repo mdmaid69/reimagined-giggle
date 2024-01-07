8116ff6e231009b7cc73a8f9291f083a3fdb06ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:25:53 +0000

    573entertainment however sea callup patience

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def subtract_numbers(x, y):
+        return x - y
