3e2e1d03790e3d18d69abe501d50d67a1e7958d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:30 +0000

    damage expect approve muscle stop

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+print(math.pi)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
