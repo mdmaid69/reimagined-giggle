fda663e4f33319a1838fc0d38c445161597e664b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:41 +0000

    smell country card sky top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
