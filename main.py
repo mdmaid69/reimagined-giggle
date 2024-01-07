4ba1850b9823c8ffa5608bf39c717158be67a470
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:15:47 +0000

    mad match zero measure pollution

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
