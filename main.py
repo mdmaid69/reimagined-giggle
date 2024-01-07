f830908d60ffbed9612a4533d499e78dcecc2750
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:48 +0000

    sail run along family 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
