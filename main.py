2f49865dca1a2f9ecd64bef17849f18a45f9ab36
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:28 +0000

    greeting tape pen Oo note

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
