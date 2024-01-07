6d1e51ad57920fb1998e10ba7947545a409004c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:17 +0000

    past money measure scared fortunately

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
