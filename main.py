4a441bef53b385bd43f1b1dff7cbaf3bff3ed61c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:08 +0000

    appeal comfortable short describe along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
