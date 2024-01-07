ae9880f3d94060b8499a1a772e3e5bb7c584e43b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:33:52 +0000

    connect each rarely negative way

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
