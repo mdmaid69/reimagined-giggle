29c9620b098257bd62cfff6776619cfb5bbfbbfd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:16 +0000

    pain turnon machine fail information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
