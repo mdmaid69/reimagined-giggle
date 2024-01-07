45bdbc8b17beeb623178be349b983108738c310c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:36 +0000

    challenge class consider general my

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def add_numbers(x, y):
+        return x + y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
