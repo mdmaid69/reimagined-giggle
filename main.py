cc7d3dcb41ea09abec1046ecd7ad258bc852f69b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:06:03 +0000

    sky constantly yourself wild interesting

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_odd(n):
+        return n % 2 != 0
