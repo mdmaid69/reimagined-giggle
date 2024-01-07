570584b95a7c3d0b67fe66c2a7adafd524190e4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:47 +0000

    boil sick praise create world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
