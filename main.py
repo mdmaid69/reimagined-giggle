07cbb21fa5f89f5db8d169d0741e032caf34471b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:24 +0000

    range greeting vote measure walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
