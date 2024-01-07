cef35863336513a5d1fdd0054cc30af8a3ff3d0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:22 +0000

    gentle fault song shade actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_current_working_directory():
+        return os.getcwd()
