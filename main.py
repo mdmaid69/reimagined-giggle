4e779fc9ae309cce84c7276303dc39b943d8f8c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:40:50 +0000

    profit lookaround immediate give criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
