e6eb471af99a76b44c4f66738216efc8ae2b9eeb
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:58 +0000

    pen hope run police tidy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
