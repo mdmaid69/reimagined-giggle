e58cd878380913f484c9921dda415f650eeedf5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:31 +0000

    work take yes open train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
