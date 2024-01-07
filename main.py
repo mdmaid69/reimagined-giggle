451c8b1831b861bce841044f0bdbbc18437537b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:34:11 +0000

    fire sure close goon grateful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
