212b6b303de22ee278f668aaa7e2f4f734140d3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:16 +0000

    position crash ask prevent attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def is_even(n):
+        return n % 2 == 0
