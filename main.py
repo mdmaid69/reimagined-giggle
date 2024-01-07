3bbc71b1785c8b9ea868120e2c129d2e7b2e8dbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:02 +0000

    welcome less sail pressure tread

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
