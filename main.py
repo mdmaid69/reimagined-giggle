13831f2be76ff30ad681f233e0a7370b42e76111
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:09 +0000

    reduce breathe area clean view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
