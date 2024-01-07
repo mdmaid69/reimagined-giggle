972978ffe39b762cdb66db503c2c0f73dab26abc
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:13:45 +0000

    cum bet ball wire blue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
