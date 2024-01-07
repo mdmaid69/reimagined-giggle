2c8114b0fa71ac3375e1ff566a6c9aca5cf7f79d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:15:29 +0000

    stop one score war flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
