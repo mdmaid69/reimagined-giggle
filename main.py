4e37daedaaa822f89be0f8dcfc3d8780689c5d0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:17:43 +0000

    beside handle dish record difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
 def get_array_typecode(array):
         return array.typecode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
