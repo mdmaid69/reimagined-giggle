0c46b8da6629213cfea7f94a7e01848b3ee9d087
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:27:44 +0000

    count hand sting tidy transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
