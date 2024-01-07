bf50bc5fd857f078c261fdcc5882ace2a7099e86
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:46 +0000

    middle declare comeoff choose weigh

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_volume(length, width, height):
+        return length * width * height
