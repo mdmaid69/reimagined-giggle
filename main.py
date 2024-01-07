0a3817783703ed2b8b020c420f8ef61a5b7f0115
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:59:49 +0000

    repeat know crowd defeat start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def square_number(x):
+        return x**2
+import sys
+def exit_program():
+        sys.exit()
