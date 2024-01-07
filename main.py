0a3bae3fcab06b8c07f3ce4ed67b0a8d368e4a92
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:48 +0000

    fortunate rob flood declare ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
