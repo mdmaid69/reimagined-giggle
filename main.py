a2c9df6201da895ccd4ed3a432699e70d1090f4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:56 +0000

    close country yesterday cope bad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_min(numbers):
+        return min(numbers)
+def find_min(numbers):
+        return min(numbers)
