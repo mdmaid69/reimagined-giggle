7af9490ed2169f470e7c1f020b4c5214173d83e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:10 +0000

    he murder round equal murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import collections
+def create_counter():
+        return collections.Counter()
