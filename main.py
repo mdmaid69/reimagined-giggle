84694b2ae8bdeaebcba1907ef676779d26a59d80
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:47:20 +0000

    else no lucky car schedule

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import sys
+def print_python_version():
+        return sys.version
