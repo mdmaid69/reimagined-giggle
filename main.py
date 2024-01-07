17369673f064fd55a5482d1ea4f55157ea1111c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:47:05 +0000

    himself news dish under comeoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
