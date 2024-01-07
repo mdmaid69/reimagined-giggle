1fa4d63d03b5ad1d6725973f80a06a3f2273648d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:15 +0000

    damp cap strength difference turnup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
