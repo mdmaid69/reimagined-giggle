7b778ab63831c8c2295d5f8477f5d015caf5bf1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:10:37 +0000

    tonight recovery scale Ii case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
