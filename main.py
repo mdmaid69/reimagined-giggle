9c21140be81adc3210332cf794f8fbebcfa7bad5
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:17:40 +0000

    shine goodbye telephone takeover choice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
