c52a8324867a042d8997057a28850dd4cf47ca34
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:26:00 +0000

    happen most outside dance throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
