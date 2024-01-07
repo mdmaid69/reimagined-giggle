8611a381b9d2b6fd9a1a783a6e90aeb93608a1fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:45:09 +0000

    contact sing different personal callout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
