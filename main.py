2e0551ad7ed158ede3f8dcb84c931a9e02543f3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:27 +0000

    to pile occur them gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
