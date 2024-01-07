f3a04ca7ee767273a36efdd0e602fac0e989ed83
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:40:14 +0000

    dark sea fairly careful Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_pressure(force, area):
-        return force / area
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_unique_words(sentence):
+        return set(sentence.split())
