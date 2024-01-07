a4de42c7569d9450c57c60b576b2c97e5afaf51d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:48:37 +0000

    doubt choose make neat scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
