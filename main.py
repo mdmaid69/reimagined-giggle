d6f3dc392c2e91c20f22d3af143020bee9c0fc09
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:54:24 +0000

    sink anywayanyhow gift 571enough wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
