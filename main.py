16ab105349f5c2e5d14c3757968eb10ee63022b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:25 +0000

    exchange image drop beside better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
