b56d702bf9b2c33d7f88a7d2289da08a7ffc3c93
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:50:51 +0000

    belong immediately urgent importance mention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
