bdf7dcba0da843fc4d59507479673dc8f704bd6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:44:31 +0000

    force calloff nice thirsty politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
