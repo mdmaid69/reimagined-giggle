d26638e75781903c0e66ff15af18f917956ef552
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:51:43 +0000

    bringdown exist create time delicate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_density(mass, volume):
+        return mass / volume
