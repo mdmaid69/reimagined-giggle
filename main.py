b35713a6bebcaf82bea47baaa478f286cad044ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:44:22 +0000

    moment actual obvious picture create

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
