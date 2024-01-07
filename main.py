f188fcd83e8c71b2c80d29d4505d1df247eaab0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:07 +0000

    hold circumstance occasion measure guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
