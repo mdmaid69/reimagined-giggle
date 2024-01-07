814b043d30a1092bea11da5b520bdf2ab842f39a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:55 +0000

    exist me exact cruel admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
