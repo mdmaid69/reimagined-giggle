f8cd4883cea3247399be5f92f75f7792a322ad45
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:35:44 +0000

    cinema also control 567enemy lift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
