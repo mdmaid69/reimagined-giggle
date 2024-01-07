462e359691fd3311c7e19edf5155cc41e89d7f38
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:32 +0000

    listen approval entry we Ww

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
