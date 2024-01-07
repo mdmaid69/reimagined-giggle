ac576877de9cd2a444d26246bf430c56a31f926a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:01:17 +0000

    summer responsibility worried easy shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import sys
-def print_python_version():
-        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
