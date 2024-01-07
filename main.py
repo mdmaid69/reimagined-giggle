572d8be0eea954e33005b8fa9280a56551212588
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:09 +0000

    mainly not satisfied it bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
