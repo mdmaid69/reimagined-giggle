fec87c574b8150f2d31e77972d01b1c4597f3a5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:49:07 +0000

    begin born noisy silent war

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
