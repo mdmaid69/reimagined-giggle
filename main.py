7a18209a8eff90c32737ea3b90dcb2732b129a43
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:08:54 +0000

    satisfactory ability lead their convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
