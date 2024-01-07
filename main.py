c312a92d4b5ffa2d59adf4b7526400b5a7320366
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:00:34 +0000

    better cry congratulate yourself handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_max(lst):
-        return max(lst)
+def sort_list(lst):
+        return sorted(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
