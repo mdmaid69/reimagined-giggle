94f3eca770a1480fc49ac487706aa7f0dcf95e3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:11 +0000

    century plate I past slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
