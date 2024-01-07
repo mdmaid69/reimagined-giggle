52719e18af0208afe677a42e519460d7144e8ec6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:49 +0000

    turnon take spare enthusiastic politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
