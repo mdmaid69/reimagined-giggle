3fc367b11c15ec623bfc168fdbb084c2bd2a5739
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:52:04 +0000

    impressed stone exit interesting office

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
