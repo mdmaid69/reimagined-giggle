44ef4a9e48c10235662721eb11f4c77d5567f0fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:22:20 +0000

    funny steer loss either disagree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-for i in range(10): print(i)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
