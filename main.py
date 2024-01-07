b8eb0928f6d2e764e5b1ed6e00c18463c6cf29bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:07:41 +0000

    eat sad responsible one land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
