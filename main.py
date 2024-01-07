96dad291938e755c2ebd887032b021f9819bf2f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:11 +0000

    flavour also soak much apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
