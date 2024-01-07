ba128d94fd3a76aad0e4bb39e78b99ea9a720c25
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:06:47 +0000

    onto afternoon regularly Gg than

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
