cc1d301e1bec47b0ad98f09c2c9266ec1c5bcd17
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:49 +0000

    best open back our but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
