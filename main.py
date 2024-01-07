797edbd4c5c998aab29d56c45ca55edbaa97f29b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:15:59 +0000

    later quick shut hang next

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_min(lst):
+        return min(lst)
