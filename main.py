af28b776a11ddd9bfdbf32f1929d14e3ba8875a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:47 +0000

    attach new thought jump determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_user_string():
+        return collections.UserString()
