2a7f81f9b09bb34f7d22d5a1be3f3784bb5ba52e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:20 +0000

    note during winter just callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def split_path(path):
+        return os.path.split(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
