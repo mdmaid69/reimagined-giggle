610e59ae776999c6688cf008ee7900220478bd6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:46 +0000

    recognize through platform produce either

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+print(time.time())
+  def convert_to_octal(n):
+        return oct(n)
