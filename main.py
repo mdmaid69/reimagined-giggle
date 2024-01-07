ca786c2c7704c34fe1f0d951acfe3b3ebea21d91
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:55 +0000

    miss suit force immediate lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
