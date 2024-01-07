561768125f336f7cfdc24e2b2c2e14a9f0d961c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:21:23 +0000

    advantage content write scene undo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_min(numbers):
-        return min(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
