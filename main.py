15dc7f562704bb7edadfb71c6afa5adb6c4c1f9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:37 +0000

    straight set closed advice reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
