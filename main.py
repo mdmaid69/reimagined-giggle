a199a0a1a8a973914a95d8926f614b6917edc7ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:36:41 +0000

    act sail smell concern expression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import platform
+def get_python_version():
+        return platform.python_version()
