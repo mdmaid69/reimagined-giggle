c36d0ccd8229aa135a4f326fc95fc4a7b26f9975
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:13:25 +0000

    hungry interesting ask pinch please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
