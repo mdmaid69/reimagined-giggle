1160f667ea9e33ae690151c72808ccbb5a3742e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:17 +0000

    opposite group big back except

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+print(os.getcwd())
