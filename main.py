822d6f9288705ec9065d96ac21f11b4dd42473a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:16 +0000

    quiet rather apart urgent practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
