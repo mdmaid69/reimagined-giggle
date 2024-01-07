373f14260fdfc2f700e03d522df78c4399b9e517
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:47 +0000

    impressed forever silent deaf mean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
