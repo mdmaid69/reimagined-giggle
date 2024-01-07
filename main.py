00267c8b90183805d6f3f3d4e4b700993a153234
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:51 +0000

    principle little female direct apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+name = "Python"
+print("Hello,", name)
+import platform
+def get_python_version():
+        return platform.python_version()
