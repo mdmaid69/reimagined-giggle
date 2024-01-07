d290c72994ac91bd95cbeea721a7ff4617322b06
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:40:18 +0000

    buy comeback Pp dream Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
