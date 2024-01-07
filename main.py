d96f52bac8730d66071818ac7519ed134270e119
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:44 +0000

    workupto last live fire late

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
+def calculate_work(force, distance):
+        return force * distance
