5f67b5eb33a9fd8b4b0961e123ed2413c71dd121
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:22 +0000

    once left snow character loud

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
