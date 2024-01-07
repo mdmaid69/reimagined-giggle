37085192fc1a87db01e6ba32f3b086627b01511d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:15 +0000

    walk grade sentence under possibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
