7b47872edb0529a37c5b52c93704c7b448d84528
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:31:59 +0000

    responsible opposite oil along benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
