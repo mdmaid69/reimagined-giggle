3f07957dd0f160b1e6aa0ef82ed262bf44a5b52d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:40 +0000

    aim fill considering abroad sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
