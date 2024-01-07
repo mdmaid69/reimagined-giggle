ec304818d16254c253dcce51a68cdf10e75ee469
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:05 +0000

    criticize butter factory criticism plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
