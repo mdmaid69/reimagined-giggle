5a1bf423afa83a215f97f87cb9193a14aa71b1a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:56:48 +0000

    treatment gap run murder sign

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
