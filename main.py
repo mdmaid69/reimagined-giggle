fc1e9e0315655dcce35a3c93835d585a65a06844
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:06 +0000

    follow closely collect boring thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
