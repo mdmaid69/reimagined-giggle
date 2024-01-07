8dfc23c6abca81fb0251cb86d9146529345fe9ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:22 +0000

    blame system rain thing path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
