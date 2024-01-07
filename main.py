99d8264e6fdc122876bc813f4ac1470ab11ffd07
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:28 +0000

    precise middle come bridge increase

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
