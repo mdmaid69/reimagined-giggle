0ba61d2744d344b1ce1033b3dcbdca6be2e2656e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:40 +0000

    answer whichever dark come wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_min(numbers):
+        return min(numbers)
