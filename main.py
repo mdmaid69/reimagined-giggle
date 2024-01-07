587c4d0973eea5cc7b9ffadcf578583547acf9a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:58:43 +0000

    consist into Bb drink habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
