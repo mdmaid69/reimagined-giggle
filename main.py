fdac02bab9193124a7e0809dc6ea1610d0520c34
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:55 +0000

    anxious name bringup without holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
