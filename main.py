6482aee6c7508ba291a7fe8e8c5b8cd4fdd569f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:49:20 +0000

    tragedy serious lean tall pay

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
