15e70c3d74e137dbb63b327a1690fcb8c5874213
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:50:37 +0000

    please relative share fetch suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
