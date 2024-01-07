bd2114326b99ebe2b41951ff1cbd0b6747d554d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:07:51 +0000

    careful skill partner waste by

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_area_circle(r):
+        return 3.14 * r**2
