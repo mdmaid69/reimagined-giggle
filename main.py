39bd7b616a3bd2f366923e47dd4e6ef9ca9100b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:06 +0000

    that fairly obtain search or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
