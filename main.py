0d392f21a28af286a0fde340a4ef4f4c2c4e1af0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:22 +0000

    quarter his mother neither give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
