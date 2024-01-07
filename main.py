aaab9c52a58895f67f87a505ef586f29d96f122a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:56:07 +0000

    visit between everywhere weekend workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
