951b3ac5109d4b49241dd5a9db591421b7b4991a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:30:41 +0000

    force clearly more tread include

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def reverse_string(s):
+        return s[::-1]
