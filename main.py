3709e67c5a429ee611ea33aebec38db11c0a0ac3
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:25 +0000

    come mixture offend Pp rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
