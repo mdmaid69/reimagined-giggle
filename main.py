d741e0a149985b758f9a14d6f4e6a30d371a84f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:49:35 +0000

    plenty medium opposite OKorokay snow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
