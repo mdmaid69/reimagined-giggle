12bbf4b33549aa12e2069397e1383a68e883ed17
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:04 +0000

    boring minimum back probably organize

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
