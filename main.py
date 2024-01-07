4091d9331a4835d4ba75f8dec1c357f7de2c8a18
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:39:38 +0000

    sort unfortunate anywayanyhow grow 566end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
