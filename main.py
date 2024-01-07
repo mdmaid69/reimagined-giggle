fa5d8347e1eeb328a501900742c8e571c0421e9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:34:35 +0000

    mix smart turninto shout talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
