9e9ff9c31bbdb6d1f0d8f39e6dd9af4a1396db4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:03 +0000

    dangerous allow young science average

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
