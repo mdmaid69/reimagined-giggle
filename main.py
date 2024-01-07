15e64093f57e5e386202e40396f05cb43df7d323
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:55 +0000

    stone sleep war desperate born

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def square_number(x):
+        return x**2
