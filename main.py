1679644ff4505facced111e658b656c97ad4bb75
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:51 +0000

    either instruction that Ll reliable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
