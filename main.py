f734fc91b496d9c45c46c38705b78c32d344a640
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:57:09 +0000

    move late watch strange slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
