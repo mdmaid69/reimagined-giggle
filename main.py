1ca9dc76a8e11ca594d36cc1bb75fe543958cb52
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:28 +0000

    certainly adjective half figure degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
