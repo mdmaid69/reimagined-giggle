0b19f15934f37aea4446319656b967730b12fe3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:29:39 +0000

    takeaway formal raise who ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import sys
+print(sys.version)
