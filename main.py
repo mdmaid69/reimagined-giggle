0146acfc02e066dae7e8b092a4f1f054ef6e4477
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:33:13 +0000

    comefrom worst birthday film worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def remove_duplicates(lst):
+        return list(set(lst))
