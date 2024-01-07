3ca0e95a0de58c62a259827a26da28bc3541fca8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:31 +0000

    exam pinch star please legal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-print(os.getcwd())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
