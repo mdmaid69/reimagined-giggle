2a60a2e8cd5a437c3fdb7e3197a7624821a62c68
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:14:54 +0000

    miss distant schedule protect left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def exit_program():
-        sys.exit()
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
