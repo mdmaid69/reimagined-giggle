9adacc2a507cf6443e2bfbcff79936b0b9edf2e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:55 +0000

    wash describe believe suggest raise

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
