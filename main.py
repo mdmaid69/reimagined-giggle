5c2fe74201c4cdd6ccb2d8e794121c991a007287
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:54 +0000

    sort fact his introduce ride

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(10): print(i)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def greet(name):
+        print(f"Hello, {name}!")
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
