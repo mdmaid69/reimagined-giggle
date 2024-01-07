4a3932fdef8f48a8f852bf85a1750a967f9a84d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:35:06 +0000

    bath free introduce bringdown community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import os
-print(os.getcwd())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
