eaba73244f60062cd8a7b4d1d49f06af52ad2d55
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:38 +0000

    about half close permission arrange

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
