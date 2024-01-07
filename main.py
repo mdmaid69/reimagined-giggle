d79099a85c0136f40bd833636a8cfa4a148e9381
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:52:50 +0000

    properly getoff scheme pure rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
