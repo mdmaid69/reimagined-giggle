aeba171757489e3a8498e3e6fbf6a4da22348f51
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:29 +0000

    generally sticky study book confidence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
