1feb6e89e342a39bcaf2c7a263e37874b9919a6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:54 +0000

    star active afraid body contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
