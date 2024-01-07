c2cd2d0dad6496345c006a3b6efba0a8f2cc59ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:31 +0000

    cook discussion ever score clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
