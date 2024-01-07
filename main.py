e458cf227df0f5b088c87b2659a5d0fc1717a629
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:59 +0000

    water late purpose check leave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
