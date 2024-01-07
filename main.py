5cf36d5501c4c8d03ebda1495747c64257548465
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:09 +0000

    settle keepfrom fortunate appearance Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
