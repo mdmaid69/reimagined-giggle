67419ec1978c6facdccf2a88f5553ed8e51dd3aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:46 +0000

    old by Cc obvious blue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
