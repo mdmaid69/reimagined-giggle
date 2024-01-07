dbeb9d1fdd4b3b3a54ec5b779b28e286fc9fe2d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:34 +0000

    service wind present share distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def sort_list(lst):
+        return sorted(lst)
