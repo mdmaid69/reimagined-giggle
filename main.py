bc902f1e96e59dc0b999e2b10ec33e563adf5ed1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:41:23 +0000

    interfere pass lay fasten look

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_octal(n):
+        return oct(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
