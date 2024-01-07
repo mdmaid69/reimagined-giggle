bed496c8ce11fa94a1560f0d284cf5ee573a7834
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:30 +0000

    maximum post range either instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
