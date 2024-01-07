481e1508ffdeead01fb421750fb3e95018209fe6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:30 +0000

    healthy loose trousers warm matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
