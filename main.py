430c1af9945558aa53421db74eca1bafacc9fb93
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:32:57 +0000

    nose wait pick example myself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
