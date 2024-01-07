a43e2c2e8e246b9965ad326933704b02371cbbbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:00 +0000

    somebody regret properly reason best

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
