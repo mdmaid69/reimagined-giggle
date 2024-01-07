be7eb638fe6bce5a4081c3080f6922ab6e3ee452
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:25:12 +0000

    little intend they across since

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def clear_array(array):
-        array *= 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_odd(n):
+        return n % 2 != 0
