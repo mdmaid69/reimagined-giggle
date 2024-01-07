ecc297c8f7934fd64ff7995bdf24de711da44461
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:00:12 +0000

    reason use round basically defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
