15eb9dc64aca3afbdb4b22e2e0332a290d2e98c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:59:54 +0000

    petrol bad upstairs song fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
