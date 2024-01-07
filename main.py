5ed4ae53fd4e6f3e44073c2952c5b269db015218
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:06:56 +0000

    blue land fetch promise to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
