fadc6e3d721c2df6d3e2bde629c33b69d25ed7ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:18 +0000

    welcome church hard object getout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
