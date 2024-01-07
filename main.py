e77d5580d28a1de2b3a1df60f0711c5773a980e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:16:04 +0000

    impressed report country throughout arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
