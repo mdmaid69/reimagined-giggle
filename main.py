1225ad399a7b98c41bf56b0b3e678dd24738fd5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:40:53 +0000

    design insist trick opposite sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
