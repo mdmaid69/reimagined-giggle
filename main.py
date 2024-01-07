bcff5be426a78cc9251018d69f219e4cb607ae4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:26 +0000

    tune movement continue possible blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
