bf9dbb01bc6b322ea7c52627e16db393cd1df496
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:25:29 +0000

    callup Yy travel finger clear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
