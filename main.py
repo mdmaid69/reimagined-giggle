0a957bd207f95878e34531ae5e7e5884144b6d13
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:45:03 +0000

    married support common pronounce look

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
