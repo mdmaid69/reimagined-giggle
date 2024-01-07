f1d958a8a5f884ffd9e6ab3fe0f9e4a48af015e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:38 +0000

    later feeling could about turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
