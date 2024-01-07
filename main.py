e353bcae57a7a64dc9c2fe43e5ccf32992571b6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:11 +0000

    nose mix evening trip separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
