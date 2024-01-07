da54cf3465e713a56e7d8347e931fefd660c425d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:23 +0000

    power prepare money pour public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
