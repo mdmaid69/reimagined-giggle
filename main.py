70cca59598f703984d9ebc92fa939dc9f7baf9db
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:49:53 +0000

    advanced broad shoot national soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
