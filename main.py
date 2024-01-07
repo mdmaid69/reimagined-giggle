d0b6a2ddf3227b077ef1e15a8f769ce7c99a132d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:37 +0000

    start split intend thanks area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
