fe73a97f8861992f6d7a081436f59688d7b51bc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:08:42 +0000

    refer soon confusing circle along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
