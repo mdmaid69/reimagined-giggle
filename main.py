d66b52a9e32d70580928fe6dc4d8e9c077b25755
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:34 +0000

    secret Tt address dinner lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
