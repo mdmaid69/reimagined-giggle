e3b58ea8bf6096d8444bb57297ca87e527526b58
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:16 +0000

    climb organization weekend excuse weak

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+import threading
 
-{body}")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
