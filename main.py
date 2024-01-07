871fcd5b60bcc2bcfe3920e76db7b7f38fa4336b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:42:15 +0000

    up against call then instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
