3801050052fa1c2bbc5514cb459c8c1553c4d64e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:47:47 +0000

    influence tread and memory doubt

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
