ace03bcc61db1bf3dbe6ad219647874acd8e6892
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:46 +0000

    situation goodbye responsibility slide original

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def get_array_as_set(array):
-        return set(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
