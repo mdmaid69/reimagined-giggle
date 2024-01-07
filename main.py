27dcd9fba84ecba313915d2fe7de6804b19727c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:40:51 +0000

    twist content they capital pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
