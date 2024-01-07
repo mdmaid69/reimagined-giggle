ec97c6f38a8210170d1572e01df00c47e75e4282
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:23 +0000

    sea smell match usually bringback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
