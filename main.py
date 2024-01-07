e952d4cb73f209e9f23b3697218c70a2de166cfd
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:52:12 +0000

    another tough challenge anywhere pity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
