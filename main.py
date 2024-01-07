abda4cc713c611bc8e16648743d8ad6cfcad19d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:43 +0000

    bill exciting watch unpleasant 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-for i in range(5):
-        print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
