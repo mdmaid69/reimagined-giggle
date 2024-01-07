3ca63d752d63e8b69f616f26c0bb5222c1f5032d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:25:04 +0000

    shut cheque violence tooth trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
