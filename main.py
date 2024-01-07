1a2e7a126055445c0a8808e104a5b92d8cb58f42
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:23:42 +0000

    team anywayanyhow major divide so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def subtract_numbers(x, y):
-        return x - y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
