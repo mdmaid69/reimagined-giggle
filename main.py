7ae9baa32fa911cebb266fb424a65097ac3cf47f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:34 +0000

    urgent kill equal welcome miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
