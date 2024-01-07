2630e995c87b20f8704520434ed02f9d0af56f25
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:02:20 +0000

    exchange sound state until out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
