7be639c1db5080fc85b69ddc1061b13b3431f94d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:20:04 +0000

    let now shade possible turn

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
+def add_numbers(a, b):
+        return a + b
