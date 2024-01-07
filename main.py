8cd48bff8ea5e8396cc2d8d0e6d30af45d68b0ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:05:31 +0000

    round something though feature expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def cube_number(x):
+        return x**3
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
