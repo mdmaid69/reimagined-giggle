5719784eba2392c2223122e304e39935f120d3ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:39 +0000

    Gg record convinced phone public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
