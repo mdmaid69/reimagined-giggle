c20e27c1f54cf933a1acc48a5b3812aa41833d64
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:27 +0000

    beside such fast thinkof black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_typecode(array):
+        return array.typecode
