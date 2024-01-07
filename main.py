2ff5586876f50fc6b215f0fb3dfbc577ef8d3270
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:27 +0000

    fashion weak window word tight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  def cube_number(x):
-        return x**3
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
