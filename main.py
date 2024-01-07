636c7e2fdb552e453ff5b396677a8fea2b938958
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:59:54 +0000

    Mm zero spring check land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
