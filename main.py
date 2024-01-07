c59725f41e261150d2be24bfa9c04efaab96db17
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:54 +0000

    criticize reduction nose until production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import random
+print(random.randint(0, 100))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_string_from_array(array):
+        return array.tobytes()
