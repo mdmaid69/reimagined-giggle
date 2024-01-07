1592b3b4cbdfa6327f7323617b44f717a97664bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:06:54 +0000

    illness tool production foreign since

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 def calculate_pe_ratio(price_per_share, eps):
         return price_per_share / eps
