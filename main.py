6878488d0ac3a50b6dab194310bc99aca231e4e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:18:37 +0000

    swim gas birthday shelter of

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_volume(length, width, height):
+        return length * width * height
