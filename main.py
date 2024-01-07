1920438910b7f854bb86758804175226a18a0c6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:46:17 +0000

    through chin cheque stop embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
