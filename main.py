eb3a6f5372bb4a87e5dd99d228afca5afdb8c7bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:33:23 +0000

    pretend calm there straight brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
