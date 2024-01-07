cd33fbce46559ce45706a0404153b7138acb6277
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:55:06 +0000

    doctor few estimate extraordinary movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
