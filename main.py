8df732ae660cfbbacc1093efc7f2a256e9a446d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:45 +0000

    injury model its old thing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
