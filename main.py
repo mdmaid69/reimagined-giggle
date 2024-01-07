8938de49a41e7197c3f08e7efdb1ea3c1ab18bde
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:37 +0000

    talkoutof address recognize degree soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
