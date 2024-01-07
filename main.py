e97531bf66a7eed73bbcccd1f7e10e7989eda547
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:16 +0000

    fast star fault basic film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_min(numbers):
+        return min(numbers)
