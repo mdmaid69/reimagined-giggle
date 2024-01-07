e2fd9b5a4620bb40d794d77f9c22cdc5d4727b25
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:08:22 +0000

    dance hurry ashamed gather list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import sys
-print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
