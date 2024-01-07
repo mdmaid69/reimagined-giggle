c490f221b136bef087479ed60c290cd1dba2687b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:58:21 +0000

    exercise side fill fight cross

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_power(work, time):
+        return work / time
