9fd5e9d0b2f8360f2a0a095a799060df194a7d37
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:42 +0000

    so switch minority clothes change

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
