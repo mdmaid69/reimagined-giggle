ee329d3966ba3178cdc357b3d4b0027d6f45ade1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:05:36 +0000

    passenger production reduce demand type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
