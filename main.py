5f12de1f8cefd811e6cadbd61266509dbf41a337
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:22:30 +0000

    party ask forever degree empty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def cube_number(x):
+        return x**3
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
