9c0ba5f3cc849e7bcc75db44fcaa3e396ff90a9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:47:23 +0000

    565end getaway cold silent improve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_max(numbers):
-        return max(numbers)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
