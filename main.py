7f5904fa7e9c05837cc3e13fb94520b66fb19643
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:00 +0000

    turnback anywayanyhow speed extra way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
