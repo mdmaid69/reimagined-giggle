3502e9fce0ff96108e6437ef394d9cb82d15eff6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:29 +0000

    shallow blame pen comeup world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_str(array):
+        return str(array)
