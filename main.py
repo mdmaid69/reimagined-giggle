c519871b5616ff305f0f750f98dba5446e1e472a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:33:40 +0000

    opponent tie pour radio Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def find_min(numbers):
+        return min(numbers)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
