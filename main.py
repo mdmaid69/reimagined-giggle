6ad2bf26ba9a36dd3afe5ed642e4feed5eccc037
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:50 +0000

    whistle game comeup young deal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
