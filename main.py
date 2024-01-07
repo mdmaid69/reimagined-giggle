f90bb8b6dc79d14cfe410b74cae1594acf3624df
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:01 +0000

    corner medium hand turnback apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
