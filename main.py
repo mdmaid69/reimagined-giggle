339c35471f15bd3b00379f17a30219cc8337c038
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:37 +0000

    report alive bet skill section

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
