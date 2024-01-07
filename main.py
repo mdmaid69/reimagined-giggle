dc63fa2439abbee1eed1c8ecafb61d934fead620
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:15:26 +0000

    road crowded inside curious path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
