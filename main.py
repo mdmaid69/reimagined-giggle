a54e76d76a7232b8fe9d70550044a18c7147104c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:06 +0000

    use price sky free machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
