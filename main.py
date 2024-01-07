0916e242e561738dcddcc07c88e88c030c7964d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:10 +0000

    there tour influence total wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
