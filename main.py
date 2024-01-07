d455b526a54b017b2de49403d41d628532e34fe9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:43 +0000

    makeup wave waste message go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def convert_to_octal(n):
+        return oct(n)
