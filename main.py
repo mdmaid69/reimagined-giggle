5ebadf800747bcdd0597892449e88c092e4cf9a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:47 +0000

    exactly trap relief praise whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def is_odd(n):
-        return n % 2 != 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+print(re.match("h.*o", "hello world"))
