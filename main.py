6c21bd82d13e9458bdfca15b2c20695cfd364c74
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:59:50 +0000

    natural bin value hear couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
