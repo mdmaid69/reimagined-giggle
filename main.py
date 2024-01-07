5fed59a36e829d12e793968e0081aa299a07adb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:41:18 +0000

    slide bus commit fact shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
