81757ea8fc116b4b1b2456470bdfde6660a1bd6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:53 +0000

    climb include greeting object protest

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
