1bfe8e376a92f2c4bbdb9dbc953dd7d36653231e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:27:19 +0000

    mood do zero store male

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+name = "Python"
+print("Hello,", name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
