33180beeed4a3fd9ef69bbcdddfc1122236e4874
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:02:06 +0000

    chief afraid address reduction beautiful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(lst):
+        return max(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
