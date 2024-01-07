932f51d3930f4de91d2ec3cbbca2be0931efb0dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:06 +0000

    weak worth risk now equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
