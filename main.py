108fa63c5ef738da1bd58414387a34afa25e1728
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:45 +0000

    confused since Ss character influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
