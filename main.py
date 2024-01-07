a9a76b605074ee6ef48e0df02bc11687b217a360
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:56:16 +0000

    in corner when it arrive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-print([x**2 for x in range(10)])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
