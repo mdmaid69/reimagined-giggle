8d4140188440acec2c3cd7dfc98d632c5ba602a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:20 +0000

    spite receive behind house rough

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
