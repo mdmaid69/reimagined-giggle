f7f95f15a9641acddfe53e9ca65acd4b2cd7a5b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:36:04 +0000

    cover mean relative average leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def split_path(path):
+        return os.path.split(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
