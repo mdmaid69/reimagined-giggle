7b41f4de5d6ba6e920a1ec8ebae106b2a605ca52
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:17:47 +0000

    bit spread pot here kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_list_from_array(array):
+        return array.tolist()
