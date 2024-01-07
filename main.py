76c887cdcc6a938deb84dac5498bdae343af8abe
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:34 +0000

    successful week process point lend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
