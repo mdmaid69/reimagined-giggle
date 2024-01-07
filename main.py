486db46b4bef49ad4209ce6655c76686766e93b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:04:43 +0000

    capital shop discover legal change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
