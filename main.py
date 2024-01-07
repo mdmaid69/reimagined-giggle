dc60cabf932730fe5ba90e6aab040a3a409a7a38
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:29 +0000

    definite body talkoutof fast hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-name = "Python"
-print("Hello,", name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
