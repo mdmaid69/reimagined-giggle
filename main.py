4ae495033db46b5daa45b0bbc8b665759f29e423
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:20:18 +0000

    march study base business accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
