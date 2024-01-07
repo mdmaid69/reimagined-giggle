377b2f9a7b7b01b996ed2f6e09bc701b3e7e29f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:24 +0000

    share pride acceptable near organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
