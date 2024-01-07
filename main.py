17e51e7c6041bb1993a7b2a2be91ba63d071ee9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:31 +0000

    attach anywhere free begin female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
