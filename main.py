b4129963f60b20062f36ca08e7ead63ffe2f9d76
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:32 +0000

    control mouth make suckme message

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
