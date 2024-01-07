18c2b1e0fce72928d85338185ae4f65bf1ab2277
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:44:36 +0000

    true roll classroom thank community

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
