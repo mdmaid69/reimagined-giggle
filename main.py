4fcd8f999d7558b0b79355daf4927f15edac3220
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:37:45 +0000

    little neat flow separate distance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
