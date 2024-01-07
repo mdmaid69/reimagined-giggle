ca8ad09edc2e17d823412b1b70dfcc9284cf9398
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:13:12 +0000

    cause tax description ill write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
