c195d104cef77d3d5fd78e4844c68e435ea81937
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:31 +0000

    experiment acceptable congratulations later rescue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
