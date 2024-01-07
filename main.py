a4d21ea1aa0b0c0b7107d606d041dc4897571004
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:29:11 +0000

    gas crowded constantly obtain I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
