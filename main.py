fa044250ffcc6626ce5b12d0ceb63f5b4313d245
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:20:03 +0000

    lend proper anybody although gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
