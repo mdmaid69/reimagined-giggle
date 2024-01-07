f95669789ddaef073365eefd498c93d717450b44
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:48 +0000

    bye rest number aim bored

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
