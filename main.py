7b9753c09228ca0145fee239c0ff69dccf6674f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:25:46 +0000

    forget turnaway art general above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
