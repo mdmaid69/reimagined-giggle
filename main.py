61f04e46c52bb189dda1a123b59f0555ef4a078b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:31 +0000

    butter place separate mouth trick

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
