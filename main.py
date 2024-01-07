08c3b0c6ea2323a3503ad8470276169db9e064f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:08 +0000

    best contents beginning hello mail

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
