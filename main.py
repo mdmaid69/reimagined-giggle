cd24c57df3939bf8ac8fcc4c6747a1b07b295913
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:06:01 +0000

    score popular offend concern temperature

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
