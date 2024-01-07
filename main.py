401b5d2f98c54feef0ba67870f5a1805ad3bf5bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:04 +0000

    holdup today yes open behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
