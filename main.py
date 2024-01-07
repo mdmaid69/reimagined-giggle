e10afbf59432323bddf029d6453523fcb8b8374f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:09 +0000

    attitude shut term promise definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
