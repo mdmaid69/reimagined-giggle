9fb3ecb7f9566bc0cb48c6e0165609a3de846bfd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:23:19 +0000

    aim manage goodbye smart care

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
