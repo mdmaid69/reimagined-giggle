c2aabeb54a45851e9687e2c917db225a135a286c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:23 +0000

    hope rare impressed excellent drop

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_min(numbers):
-        return min(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cosine(x):
+        return math.cos(x)
