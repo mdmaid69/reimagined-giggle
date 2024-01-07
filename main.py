1edb5b9cd5cdc3176268881027bad472b2d89943
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:46 +0000

    walk trade win arrive concentrate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(10): print(i)
+def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
