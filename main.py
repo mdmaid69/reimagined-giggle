29389eef81d3d8711082bb11423f3f9df959b101
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:58:19 +0000

    half key ill some shine

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  def subtract_numbers(x, y):
-        return x - y
+def get_array_as_float(array):
+        return float(array[0])
