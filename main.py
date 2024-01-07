b1547a93bfec94940d52598eaee4bfbf64ed213f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:49:14 +0000

    spin open bye backwards century

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
