2c6642b91fb3c13c00a3bfc6bf48c0090dc891ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:37 +0000

    work journey swallow exercise confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
