ad917a3c79aa3d6a10416fa50f9246532a3c746e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:12:50 +0000

    whistle study progress likely once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
