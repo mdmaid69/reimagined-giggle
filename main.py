fa0c8bba8be7338755320fa22bd35b98647e0507
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:06 +0000

    single outside borrow bottom people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
