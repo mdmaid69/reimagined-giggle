9e384e961f889b000227f3818084124dd8dbf600
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:14:19 +0000

    fence impression give sense uniform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
