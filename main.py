ecb03114f45feaf2553125c3e023eb2240eda034
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:40:54 +0000

    consider coat matter breakthrough pride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def multiply_numbers(x, y):
-        return x * y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
