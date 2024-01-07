49718bc28896b3a99609a8f315bf6c72af3db598
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:38 +0000

    bit somehow neither active behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
