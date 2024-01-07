dac6f355dc553b4c0c0b4cd5faa2b85ef6e2a3c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:18 +0000

    past bored convenient spoil soft

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def convert_to_octal(n):
-        return oct(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
