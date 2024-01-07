da186b214ce1e657bd441b4e41192a561163e7c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:15:47 +0000

    beside stir pronounce scream dinner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
