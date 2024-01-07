e1ad8ab7138ed08c091d249fbc72fea7ae4821d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:36:19 +0000

    substance contribute develop practice nervous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_tangent(x):
+        return math.tan(x)
