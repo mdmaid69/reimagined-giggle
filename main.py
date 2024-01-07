fae52bd55d1a8398f069e22e742a68e95ef525ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:48:14 +0000

    570enjoyable rise broad downstairs represent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def cube_number(x):
-        return x**3
+name = "Python"
+print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
