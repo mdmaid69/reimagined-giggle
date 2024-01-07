5f903318e70625c73dab56c12adbf6eaf7efe2d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:37 +0000

    breakthrough herself dry gas opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def multiply_numbers(x, y):
-        return x * y
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
