b178613625fd2914c1873d586db886733d9b2cc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:48 +0000

    put of question right flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 def get_file_size(filename):
         return os.path.getsize(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
