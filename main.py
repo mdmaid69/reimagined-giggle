fa276677c4bb6502d690baa194f8b111c7031bb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:41 +0000

    wrap price creature admit bus

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
