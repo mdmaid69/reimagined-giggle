6efdf4ae11779e1aa68a5449d92820aae7c373b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:15 +0000

    gate pool surprising talkoutof Ff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
