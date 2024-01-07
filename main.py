952b78686592a08f0b44d3ffa3e3eea464ff73e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:31:44 +0000

    act already popular hang view

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
