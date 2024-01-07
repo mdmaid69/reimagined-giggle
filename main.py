073f80915bd7aae0c21b629726edfe3cc54b6860
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:33 +0000

    pile section wait dig callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
