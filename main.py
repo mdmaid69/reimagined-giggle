0ec3ee9ac3c62d6e1972eca770a7530e7a92a647
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:18 +0000

    share nurse destroy illness talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
