352469493360fea5897d74c8543005d9ea5a8d89
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:33 +0000

    influence sky skirt late base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
