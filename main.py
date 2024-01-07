cc513140c6ea8cbf92469376ce396c3ed8007be6
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:39 +0000

    unfortunate project if necessary less

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
