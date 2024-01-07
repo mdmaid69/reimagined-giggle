db2ba3b2e24012345a95882851044ff0944beae3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:13 +0000

    tough actually degree world gift

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
