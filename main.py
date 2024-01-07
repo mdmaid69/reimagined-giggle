7638b86bc44136b48d05535b44210bfbc3709dde
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:22 +0000

    tragedy rate freeze comment dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
