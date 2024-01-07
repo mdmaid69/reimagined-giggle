ceda6140328237d303d7af0ede53dbb39d26d3bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:47 +0000

    high healthy turnup sound drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
