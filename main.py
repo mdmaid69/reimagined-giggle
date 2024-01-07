711f9523b5f8bc78323ac6dfae79184882077829
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:31 +0000

    either interrupt treat space left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
