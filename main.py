ffec8c9eaf560dc4aeae2f8b566e086a1f78ee00
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:00 +0000

    attempt subject sister rent call

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def reverse_list(lst):
-        return lst[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
