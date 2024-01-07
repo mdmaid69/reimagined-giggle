be11bc1d7d266c885ca80c79f5e7be359c277e0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:10:46 +0000

    scrap hope extremely snow task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
