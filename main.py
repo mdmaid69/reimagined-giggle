60a1d7006b6055cdde5eabbbff6d27d1a9dc96e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:00:14 +0000

    fear kind delicate appearance stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def subtract_numbers(x, y):
+        return x - y
