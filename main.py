b0ad0f1b49dc771814f95212dc9affec33da229b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:44 +0000

    holdout statement under responsibility accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
