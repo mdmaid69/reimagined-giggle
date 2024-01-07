a7cc4b5cd4090cb9b86df08e9d33764828e7873d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:10:51 +0000

    fun autumn distant pollution Hh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
