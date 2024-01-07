199dfecefca5c03aef54c8a0a965cbbf96b4dad8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:23 +0000

    walk block year pump pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
