c1851a912bdc82a791829c6731fe77697f91395e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:40:49 +0000

    afterwards pretty star blue moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
