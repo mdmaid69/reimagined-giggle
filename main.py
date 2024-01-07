3195f0bc2f61d15d41eea904c18e9208b7d6b71f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:08:53 +0000

    keepup fork choice firm act

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
