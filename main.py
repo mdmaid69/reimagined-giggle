a08d61eee7e241e3de36e6b2aa54769cc7f39e6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:21 +0000

    sport geton takeout cruel row

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
