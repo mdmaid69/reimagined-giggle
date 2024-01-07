0e7816d739c7ab3f14fdcb4cec7e3bb65abc38a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:26:24 +0000

    or wide blow mean guilty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
