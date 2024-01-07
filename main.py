46a06052df605e88e1d4035e1b2d739353920d68
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:55 +0000

    importance record wrap company takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
