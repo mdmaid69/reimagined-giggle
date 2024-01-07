cb7db200a5e8c5e8c3217f482e34f436d14079f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:08 +0000

    operate mail identify lookup perform

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
