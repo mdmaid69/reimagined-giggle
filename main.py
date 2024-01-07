0d99e0006d9283aca9394c4968e53931302ee369
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:36:21 +0000

    busy wind Itself plant regret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
