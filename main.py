a13d26304d4b633c1b7755820feaf5bc9ca37be7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:13 +0000

    in confidence act pile breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
