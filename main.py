83c43bc09a4a55ab98c7b29f36c2e178e4762ad7
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:32 +0000

    factory pretty limit fact protest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
