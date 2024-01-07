372338b0bdb815477bdb25a5e75d0ee534131d44
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:06:36 +0000

    half scared follow perfect declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
