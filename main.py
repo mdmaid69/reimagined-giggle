ecd98bdb3b175bac8e16450033777413705128ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:23 +0000

    our answer afternoon deliberate degree

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
