678b5bb25e67c70d55e9ad67408f315e8d91029c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:13:18 +0000

    negative Jj calmly Qq amount

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
