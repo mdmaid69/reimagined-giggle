fee28634cf03235ddc8fd8b22afcd39949c52975
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:25 +0000

    know trick push concern splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
