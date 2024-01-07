6a18811c79e13e17772e4ce1cb75e1e4d8893326
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:11 +0000

    custom behind damp phone earth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def find_max(numbers):
+        return max(numbers)
 import math
-print(math.pi)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
