326b53faf66d5a4a3800158c1c2f975b9a15c531
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:07:23 +0000

    shout chase Yy on force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
