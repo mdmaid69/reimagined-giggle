c8aedfd9ce8940b7b41e7a144339a956e6e7a4ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:27 +0000

    relation never use through large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
