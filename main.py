c86e44cdb2859fda6f453d8bc5a9207bb76551cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:50 +0000

    top lookafter pile approval slightly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+print(math.pi)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
