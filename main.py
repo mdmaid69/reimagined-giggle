07a852f68105eeb34dd5a323e4ec43e35bad03e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:57 +0000

    plenty tune middle correct far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_python_version():
-        return platform.python_version()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
