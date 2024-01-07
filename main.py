e040a95838646830a3524f44d306c6e8be7ddfe6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:16 +0000

    shop try common tell lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
