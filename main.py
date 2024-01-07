9f02019341d64569c591b4ddafd697ed454e4065
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:28 +0000

    protection that its perform about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
