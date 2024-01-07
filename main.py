db23268c16ca442df0c14537eddc610018d8f74c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:41:51 +0000

    slowly owe fast ill spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
