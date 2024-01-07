820ad09380f5ea9ffdfe894705e0a5d5603228ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:01:25 +0000

    week here as ticket fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_sine(x):
+        return math.sin(x)
