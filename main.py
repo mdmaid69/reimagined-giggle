89b73f67bebe0beb57c820b0092183784a00ca1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:03:17 +0000

    group fire swallow comefrom smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import time
+print(time.time())
