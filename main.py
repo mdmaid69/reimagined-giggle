16114aec5b356ef4bd5f10ab9f2a55a8e6b1cf88
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:50:03 +0000

    cheat this welcome light list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import random
-print(random.randint(0, 100))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
