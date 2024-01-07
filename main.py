9e8c27774f329d55459fd60a8dd505d8f23cd042
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:24 +0000

    mix intelligent crime occur best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
