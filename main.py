a9651a40cca0eb6166bac25b963e3727cee790ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:41 +0000

    upwards opposite excuse nature floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_rectangle(l, w):
-        return l * w
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
