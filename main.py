9cb436c1fdefeb03cbdfff51257df1a82b5db781
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:23:40 +0000

    silent complete please draw so

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+for i in range(10): print(i)
