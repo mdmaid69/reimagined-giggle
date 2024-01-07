ba9cfa44c706fbb895880a48ac0e4b03b2cadb0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:40 +0000

    Pp major male clue attach

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
