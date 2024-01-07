51db0bcf1950f6e0cbab62e82822c30098b327a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:07 +0000

    pot search touch slice challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
