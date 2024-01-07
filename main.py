f95922c3a20f28eb2ea738886f60e52b503d50c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:09:20 +0000

    rubbish comment occasional shout ear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
