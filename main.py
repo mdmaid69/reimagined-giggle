a068144780cba9df8cfe3e511b6a0cc54c24da79
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:32:59 +0000

    gate weigh only hope right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
