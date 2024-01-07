9c0f10256e007658889694cdfb176d9f780cfef9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:07:22 +0000

    ignore turnon pinch well write

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
