c462dcc47e336442dba1586a55ed61ea7b2dedf8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:04 +0000

    stiff weigh backwards deliberately fast

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
