2f30a9554ce969b94dc23f2abe2bd773230c9209
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:34 +0000

    nurse makeinto deep matter eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
