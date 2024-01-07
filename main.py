ceef956ba6507a315d2f089c871550b7080167a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:51:11 +0000

    favourite hurt act urgent escape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_power(work, time):
+        return work / time
