c9bff1f1b021663c6375349c1df19571f938cad4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:22 +0000

    travel pupil listenfor modern slow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
