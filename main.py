14dc33d4ebd31c947fd183ee96df0ea5187eaa0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:04:34 +0000

    cause suddenly exploration punish then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
