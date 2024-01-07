bb0463c7f729a930d584416133e1427cf254cd72
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:48:55 +0000

    size rain sometimes detail pupil

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_floor(x):
+        return math.floor(x)
