8336fe400b6e40c1a4b86d52e32c15c0cbc50844
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:12:45 +0000

    about peace attract switch near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
