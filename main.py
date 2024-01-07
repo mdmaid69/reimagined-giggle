2686f1a2810114cc07ec0934b8b8596228e3bc93
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:37 +0000

    Qq under cheque guess shame

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
