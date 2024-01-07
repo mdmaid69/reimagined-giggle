19302bdfac2337be8e7ce070a52a77ac72aa2914
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:31 +0000

    pollution doubt marry familiar save

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
