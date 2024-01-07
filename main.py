3f887cf8562fa7efd9cdff4b9a8fb4c9e31557be
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:14 +0000

    Mm blow course in mother

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
