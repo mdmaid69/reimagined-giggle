a9c5de91015995f20eabb786142f304a6f8d641f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:08 +0000

    load risk attitude workupto solve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-print(math.pi)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
