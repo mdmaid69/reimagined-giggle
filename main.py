b9d39ad2f88042463e29043a54ae47d13eaec86b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:29 +0000

    clean brush upwards gather occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
