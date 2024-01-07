6cce0b13713254eac59056f7a4d4a44e510a55b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:29 +0000

    remove sky beautiful idea complete

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
