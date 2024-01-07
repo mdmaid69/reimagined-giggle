c04311eac31f2528daea174d78a6606280992a1c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:22:54 +0000

    interested nose autumn art ocean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+print(random.randint(0, 100))
