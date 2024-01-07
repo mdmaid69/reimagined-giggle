ec6345e1ada268c3e980484b114a2f8ae9175441
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:10:06 +0000

    your estimate pencil if safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
