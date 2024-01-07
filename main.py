4afd5022e79963f4a55c3c8368bd9824d65f1885
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:57 +0000

    beneath wide develop Kk sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
