ef5265164cce1b8a97b41fa41dd7f1738b8c8fa9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:32 +0000

    shine none sure usually hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
