b98584081f3f2cbc4d27630f7a12c215ab913aa2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:56:01 +0000

    experience level limit escape pot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def subtract_numbers(x, y):
-        return x - y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
