1cb486638ccf826bcb6365dc72804fd48c912aaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:04 +0000

    patience Zz spill successful back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_power(work, time):
+        return work / time
