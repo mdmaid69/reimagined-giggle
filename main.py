c7549d439c8611b62163256894ddd7280ca3d537
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:56 +0000

    soft walk thinkof mixture brave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
