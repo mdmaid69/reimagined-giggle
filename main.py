76a0f9321abe08f505cb28d9bf55465ea5757f23
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:04 +0000

    ball close firm harm back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
