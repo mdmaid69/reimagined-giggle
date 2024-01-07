d65b7a923ddb9ae13a9c078bf628874ead03dcc5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:34:20 +0000

    concentration on perform raise safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
