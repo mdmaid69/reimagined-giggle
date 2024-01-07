73cade57a6236eca6c24be402bfe0e7f777d2448
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:56 +0000

    effect behind better present method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+print(os.getcwd())
