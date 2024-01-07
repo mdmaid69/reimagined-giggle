94b5e6043aa2e01fce1c6a37a30dc7b846216eb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:08 +0000

    surprise anywayanyhow error clean firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
