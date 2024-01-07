c528d9634d75bd62c8ce2fadee03a99b2a57d1e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:39 +0000

    behind lay bar wake action

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def extend_array(array, iterable):
+        array.extend(iterable)
