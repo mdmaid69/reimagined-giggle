9081e46c69736b87e7fd9e45fdf5a598383f441e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:59:36 +0000

    meeting care history smell exercise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_min(lst):
+        return min(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
