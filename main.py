27be06ee5fd986169bbca91e6f4248969f1f41ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:02 +0000

    accordingto majority weekend sport belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
