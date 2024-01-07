a4dc85322edbb1d06b91cfd01db1153aeb37ae09
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:18 +0000

    defeat oil scrap satisfactory handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
