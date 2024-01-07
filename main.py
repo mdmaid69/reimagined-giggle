cd7f9e80726888a1232e9817e019e55934499e87
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:09 +0000

    market remove wide nor century

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
