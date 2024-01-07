21080ddc94db5db609997ed6940c3f235d7eb031
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:19 +0000

    control comeround march joke stretch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
