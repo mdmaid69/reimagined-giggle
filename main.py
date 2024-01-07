b20d0be70a48da7f6724b7a0945c48cea84bc928
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:20:03 +0000

    regret consult record run help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
