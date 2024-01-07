17f7f49eb59d9fcb1f0b96fef49ddf34fd5352ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:34 +0000

    bringforward word feed love lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_as_list(array):
+        return list(array)
