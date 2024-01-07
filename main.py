1eb4f22860729bfc94bf8869ad49880175e9d527
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:35:25 +0000

    phone Uu comment lock lead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
