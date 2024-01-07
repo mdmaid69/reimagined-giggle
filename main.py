1ab6cbd0134b66856c8bfb6cb0d58201ae53db24
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:42 +0000

    stir switch recent hate borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import numpy as np
+print(np.array([1, 2, 3]))
