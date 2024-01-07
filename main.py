65de4b21761a67bd3c0509fde1daecea21225fe5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:51:06 +0000

    fold although finish money let

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
