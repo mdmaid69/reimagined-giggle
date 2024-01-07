9dcb7474f4d3af95c2933dedd1dd0088b4f405ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:37:11 +0000

    accidentally heart union trial extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
