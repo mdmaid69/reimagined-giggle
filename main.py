7687addb62671f68be3638dd737b45890841bee9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:38:34 +0000

    hair load situation able offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
