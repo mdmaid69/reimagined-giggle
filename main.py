4b4535660980a48f20878fd03bed7c7b2c489506
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:09 +0000

    570enjoyable Ll twist always out

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-print(math.pi)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
