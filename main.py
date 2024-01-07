8bcf6f4f2443a0eff229887344ea5494a12ee42f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:13 +0000

    discuss uncertain consist cover recent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
