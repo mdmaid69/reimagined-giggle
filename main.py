b0c02f0e46906988dd055fe792688de37644bc9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:38:32 +0000

    final ground figure soak patience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
