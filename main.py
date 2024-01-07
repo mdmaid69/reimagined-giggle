d67579caa4c71bf9ea1be5192dbfd1e048cc1ad4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:42 +0000

    always attract Itself air often

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
