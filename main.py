f1538b52b63cc1898ea5dd507954dc5cae5464e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:16 +0000

    far friend space contrast maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
