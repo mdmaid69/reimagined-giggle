642ca49beb2d5bb6b8cc428360948cf95fdf0d64
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:12:53 +0000

    seem difficult card moral heart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
