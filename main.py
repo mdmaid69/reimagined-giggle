f049190c5e1307e3d0d8573b160e7970ba0f4b6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:41:04 +0000

    frightened error how name question

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
