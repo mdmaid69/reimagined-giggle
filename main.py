ab36450dde089a08abdba21ab6daf6dc456ee7c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:13:37 +0000

    afraid score clever interesting complaint

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
