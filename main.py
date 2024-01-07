f3ec9e58fc4d8c36ec0afcaf5a56212e7a3781c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:18 +0000

    concerned talk comein matter getback

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-print("Hello, world!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
