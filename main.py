4f34dc525734da1fbb9703167a7cd880cc8cda00
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:34:54 +0000

    survive office leg mean by

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
