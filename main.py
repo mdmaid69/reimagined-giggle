31569390d377eacc9f941c2dcedb0f8a7d81d510
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:46 +0000

    breakthrough announce unusual border meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
