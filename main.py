1350577bb057f74929f5312bd30927099832a644
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:24:56 +0000

    dry kind later challenge bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
