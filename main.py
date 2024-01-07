568f16fbef308339f5a27f86f71d3e7a955d4e47
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:31 +0000

    example her them blame really

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
