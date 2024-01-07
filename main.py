702153ee7e83f1c396ef08e0a0a346ab338eaa44
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:16 +0000

    personal bound geton mean wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
