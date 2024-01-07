bda1573f9f19f03e83226cc97572a7419c7ce52b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:39 +0000

    lead increase before name exit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
