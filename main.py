2b57bbd72d3d2fc6f0b1418241c70de6a6982e64
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:10 +0000

    stir recent attention thorough run

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
