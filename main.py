2da35e67623bdf8c0f4f1794cc14b5eedf146039
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:56 +0000

    succeed production test may owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
