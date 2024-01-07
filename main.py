d0af9befbdb6b7ebc4063f9a16c08418cc86f348
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:49 +0000

    area bend problem me getoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
