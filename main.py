11d1847d4ba4daed3dd25ed0aaf278278f991b73
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:55 +0000

    takeoff judge tonight push operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def log_message(message):
-        logging.info(message)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
