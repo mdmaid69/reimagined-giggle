f2ebe95fb0a43b3e414de463884f1a4455c9d1ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:29:34 +0000

    precise that size low later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
