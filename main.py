e94b08232931a0b5bc32b8dd298587a31fef1f11
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:15 +0000

    rarely willing steer steer ready

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
