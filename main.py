16c97efbfc5f45f2872206227c19773e85b0fa5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:52 +0000

    too still excuse train far

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
