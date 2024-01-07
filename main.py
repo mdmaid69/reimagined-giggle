14565822c4fb7aede19c8fc2da2ce7d3a7180cb9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:02 +0000

    willing rob herself nose view

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
