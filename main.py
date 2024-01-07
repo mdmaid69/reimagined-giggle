cd3878d89863800b7fe0a0c055c0bbd591d749ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:16 +0000

    holdup difference temper goback(to) real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
