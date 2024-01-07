c6f8dc3b52cffb0211f2f2ecf1ca4066d1e05e18
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:06:39 +0000

    switch very rush bringforward clean

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
