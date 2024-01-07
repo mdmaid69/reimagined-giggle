a4b30a7176de6ebdcf498aa3cfa80213ae228625
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:07 +0000

    scrape truth try work store

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def square_number(x):
+        return x**2
