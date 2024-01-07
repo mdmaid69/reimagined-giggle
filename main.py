4ea52655ec5cf340a442d7b8908e3e0c7f1ef6bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:24 +0000

    other 566end repair tap spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def split_path(path):
+        return os.path.split(path)
+def calculate_work(force, distance):
+        return force * distance
