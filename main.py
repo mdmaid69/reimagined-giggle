d0c242f6633189de77d04500c4917a776fb659cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:13:06 +0000

    visit thorough closely offer Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def remove_from_array(array, item):
+        array.remove(item)
