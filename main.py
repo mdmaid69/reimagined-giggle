22e3020a7d9a50384961fd44d7b03d37278f7e77
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:07 +0000

    impatient callat holdup favourite injury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_numbers(numbers):
+        return sorted(numbers)
