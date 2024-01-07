bfd8ca76c89ae414ef014780427a96905adc9972
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:42:30 +0000

    female classroom measure crazy contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def multiply_numbers(x, y):
+        return x * y
