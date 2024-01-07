21af4537d8502a2c3596a729437c2a6ca60be903
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:36:15 +0000

    smile talkoutof improvement somewhere wheel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import sys
+print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
