ca5b8faa839d437b9b8be67dc2c11d99bb74214f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:44 +0000

    supply valuable habit everywhere Yy

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
