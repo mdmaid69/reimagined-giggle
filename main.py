5055f09a54e43da736e80a5609210b2e577bc191
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:27 +0000

    deliver sensible block invite plan

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
