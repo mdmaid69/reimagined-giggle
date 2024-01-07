677c0fa732fb793958931ddd092aec065e15de11
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:18:40 +0000

    shout study pure car well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
