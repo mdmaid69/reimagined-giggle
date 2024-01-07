f8d61c7cf210a0764ef1603faa84c20c61cbd7f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:45 +0000

    side exact chin wide say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
