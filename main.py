e8d3e9b86360b05e6164da46dbe23ada70946abe
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:55 +0000

    they bringforward wonder doubt tax

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_speed(distance, time):
-        return distance / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def multiply_numbers(x, y):
+        return x * y
