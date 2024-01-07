3546db677d27cbbd3c2b62229ad068341ef33205
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:52 +0000

    swallow whether present unfair entrance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
