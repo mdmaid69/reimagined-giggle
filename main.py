05e2397da9c4ab9e2c02d72300eeb49f729d4715
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:14 +0000

    their talkinto more similar hollow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_acceleration(speed, time):
+        return speed / time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
