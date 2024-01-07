889b1ce58961b992828218a8ef79f707c6bd04f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:36 +0000

    false Mm warn advice trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
