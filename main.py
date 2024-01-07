e1e5e13395ceca489bd6b1ce2b5f92c344398bff
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:15:17 +0000

    health bound damage talkoutof square

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
