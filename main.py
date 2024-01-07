22d7c40f85899fe9148c7db3aef47ada35dfca30
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:15 +0000

    comment free dinner cheque period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
