1e058ab71e688092f00e46c9e1aed5d64bec8839
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:10:14 +0000

    hope means the fun force

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
