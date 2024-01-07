d0ec5890f4a7cb39da08a2b69ba57644774fa1fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:44:38 +0000

    safety rob clean unlucky sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
