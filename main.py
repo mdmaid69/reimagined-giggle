a0f9357cf2276c11f1ac32106f8202f6356944b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:47:17 +0000

    maximum dress although damp strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
