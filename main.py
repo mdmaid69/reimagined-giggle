0250206e8cfea17393fbe0bcf0367fc617692a96
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:45 +0000

    offer allow feature close steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
