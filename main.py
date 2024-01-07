182b7aef5d043ee1428ba7321dc0152816a6f8bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:28:40 +0000

    suffer opposite honest mess live

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
