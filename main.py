95377b87f8ebafa8ba096391c62e6c92522742ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:50:40 +0000

    demonstrate children pile influence stand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def cube_number(x):
-        return x**3
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
