59f6622bb2ffd14fdde19953ca6e9ed12bce3acf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:21:44 +0000

    consist wait splash serious least

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
