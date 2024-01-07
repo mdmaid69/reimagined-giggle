0e1ddc20cb45dccabe593bd572d9111033e491ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:52 +0000

    apart narrow base face Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
