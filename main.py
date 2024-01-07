c06924d34c4ec68247709a6db29a5ae0f7c91252
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:47:48 +0000

    strike goon miss scream duty

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_directory_name(path):
+        return os.path.dirname(path)
