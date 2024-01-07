4cf8e2ee148f1baf393efa41b848c856335bdf41
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:16:46 +0000

    hold beginning table wide arrive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
