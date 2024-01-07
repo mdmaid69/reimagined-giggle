2dc24229bf39e29fd665ad274f513ca4c3fbd3e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:32:11 +0000

    smoke support complaint muscle check

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def find_max(lst):
-        return max(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
