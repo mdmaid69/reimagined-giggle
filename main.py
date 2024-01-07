7a399e77eecdabece304edd2d8744c9d1f691a82
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:47 +0000

    excited bin still related record

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
