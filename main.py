93d1d23bcce5a841a2c2d6b9d5f93c09c43d83b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:55 +0000

    tape valuable normal mad goout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
