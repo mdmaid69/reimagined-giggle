92da5fc0ee4badf799c6321a8275f91929443913
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:55 +0000

    declaration remember new comment sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def clear_array(array):
-        array *= 0
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
