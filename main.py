83e81e0f9a42e710e251ee940153618809d2872f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:59 +0000

    throw successful track appear pay

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def cube_number(x):
+        return x**3
