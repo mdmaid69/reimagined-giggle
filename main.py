b3ce4bf88f91a4143364efe99d94eee4f361f2f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:32 +0000

    foreign performance murder Kk basis

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
