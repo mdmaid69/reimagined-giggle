9665fca29890f50271b17ab925d0638b66989caa
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:48:57 +0000

    tragedy comeround Zz visit shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
