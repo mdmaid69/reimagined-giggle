81f9e15a30ab9861432e849193e4d11535d62b86
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:07:58 +0000

    impatient tongue apply short takeout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
