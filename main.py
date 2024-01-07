0154a18577faa63a1cbc636dcdc816ade006d07f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:16:08 +0000

    note information OKorokay tear will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
