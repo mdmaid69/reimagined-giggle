0e5cd726ea4b3754ae4ee10febd34cbf696fc90b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:01 +0000

    share organize group keepup 566end

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_float(array):
+        return float(array[0])
