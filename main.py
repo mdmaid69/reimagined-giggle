145aed559da50a5c1bebcc2c7fc927966640913d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:19 +0000

    period wake relation offer conduct

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
