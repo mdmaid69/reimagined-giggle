922ff41fadb7eb04113df1ee6a6f7f1c2119feb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:59 +0000

    hard artificial secret cheap within

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_min(numbers):
-        return min(numbers)
+print("Cube numbers:", [x**3 for x in range(n)])
