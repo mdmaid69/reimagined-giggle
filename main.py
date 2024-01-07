aa7cb9b5f3f1bde1d4bd7103887c2ed0ff928ecb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:30 +0000

    react Hh track stamp bringdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
