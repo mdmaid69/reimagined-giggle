9023d6c2724870018c9e034e2eeaa4aa17e2b5e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:31 +0000

    angry attempt clear constantly please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
