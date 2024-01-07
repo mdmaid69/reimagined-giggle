f85fea85006fede478f62dfe18666d84fbe1fc47
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:42 +0000

    hollow various your deal press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
