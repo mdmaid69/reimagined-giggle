cf1e30522365dbe19e8e54421eacae98aa0b29c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:04 +0000

    operate thank ago fat skin

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def convert_to_octal(n):
+        return oct(n)
