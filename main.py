18a21863731275b33ff8790f135095a66123b6f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:29 +0000

    combination next obtain regular before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+for i in range(5):
+        print(i)
