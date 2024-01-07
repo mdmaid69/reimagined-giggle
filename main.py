f75f45960f4519d211ec02ce415429d861e63227
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:57 +0000

    game quick see this top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_date():
+        return datetime.date.today()
