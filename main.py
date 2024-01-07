f40de48650f3339855c89b15653fa99898e2a791
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:57:45 +0000

    concentrate list connection roof down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
