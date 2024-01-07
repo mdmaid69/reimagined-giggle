ab479db79d7920154beb57e03184f796a85b120d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:25:30 +0000

    turnout underneath reaction contact meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
