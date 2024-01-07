60df4c0a180ae6c6c0e0929745e7ac46ea68665a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:59 +0000

    length name out dear Yy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
