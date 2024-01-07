aeb5cc7fc5748c9d364c18f050a3352ea0960e2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:53:21 +0000

    square getout win among situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import re
-print(re.match("h.*o", "hello world"))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
