e5967991d9806c779c8c88a903ab8d61cb4f17d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:45:56 +0000

    only above 569enjoy doubt correct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
