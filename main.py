d9879162327bce9079583e6dafef6e1444f267d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:57:48 +0000

    shame remember invitation middle past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_queue():
-        return collections.deque()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
