18d3127ab19e8c4b47f772a1c85dd53465e43246
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:20:11 +0000

    aim feel remember name recognize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_error_function(x):
+        return math.erf(x)
