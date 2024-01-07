e9d81266f4e3915c3206cf7ce8ebeae5c9d8603d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:05 +0000

    able vision noise neck answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
