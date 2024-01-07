b8f63fb7d8cc3a49e1b79266c42588da0f15c5d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:50:55 +0000

    plain Gg share strict as

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
