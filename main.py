dc30c543c322feb297f8802fdb4ce49cbd4a89f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:03 +0000

    habit check natural tour home

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
