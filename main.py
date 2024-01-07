386e8890a731466f8a4d12d9224195a06055f08d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:08 +0000

    clothes great letter holdout depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
