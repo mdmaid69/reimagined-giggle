481e0b93ff5316ede1a1c6855b60a600fc044340
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:38 +0000

    top make genuine early frightened

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Hello, world!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
