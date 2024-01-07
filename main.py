4b1eb588ca8b8c276ce75c1f4fbcd52925a7f527
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:27:08 +0000

    passenger either grow fun soft

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
