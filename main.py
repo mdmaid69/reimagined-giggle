c29a696cd9433b62531b685e4420fb0cac32d7ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:05:23 +0000

    impressed error beginning effect getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
