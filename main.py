1af7551f142f0a3fd3c6a423740f9ec99e3d9a07
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:54 +0000

    bread fight even decide takeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_item(array, i):
+        return array[i]
