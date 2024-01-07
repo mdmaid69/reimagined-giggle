19cdcd7ae007a8cd36c6d8a146c90bd88ad831ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:23 +0000

    target worry smile sudden cheap

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
