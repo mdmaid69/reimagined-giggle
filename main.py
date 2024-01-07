c5da0049e562023e68045bb6f317f6399b66f0fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:50 +0000

    previous combine handle crime term

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
