f22b62797286793b8a470c8cf533074432c68ff4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:09 +0000

    field fresh means cheek realize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+print(random.randint(0, 100))
