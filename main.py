07109b868bf20c00dca7c4f1c7b6d63f94fd4500
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:01 +0000

    runover cost organization short strong

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
