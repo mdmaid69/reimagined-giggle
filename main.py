785db67f81ed5522bf3af55b1ec897087ec3e2c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:31:40 +0000

    marry importance while may difficulty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def convert_to_octal(n):
+        return oct(n)
