2b3a9ee0b047c91c378e3c2bbbdab40c91dbc8aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:22 +0000

    complain key niggaacademy allow over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
