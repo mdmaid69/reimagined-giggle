8c1c5f741e9abaa7da172cc245251a01a1da0b85
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:23 +0000

    grand statement contribute any land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
