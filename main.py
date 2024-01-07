5ec59758c39faa98bad91781240b3a34e2b8c89f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:06 +0000

    undo ourselves straight pocket interesting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def reverse_array(array):
+        array.reverse()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
