5230b9413c771a575efac5a37a7cd674a7987928
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:13 +0000

    somewhere pretty benefit man principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import collections
+def create_user_dict():
+        return collections.UserDict()
