28420fe4e0ed7d774ad4a683bb10d411b9c5eec2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:38:20 +0000

    who cook satisfactory mend comfort

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
