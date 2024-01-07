e992bcae7ea5c3a664bc97561dcfeedb1e480130
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:46 +0000

    comeup blind group loud light

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print(sum(range(10)))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
