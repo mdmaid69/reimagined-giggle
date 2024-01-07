a4262f3ee58fdde3e06c759a771449f1d7980af7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:13 +0000

    organization picture star goup goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
