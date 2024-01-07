583492802cc2461e4995b4e5123543bfcc3a521f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:24 +0000

    gate picture inquire emotion get

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
