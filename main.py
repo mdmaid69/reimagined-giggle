39cdf15e05821b5dbc6b56bfdec345612821c2fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:09:05 +0000

    guard exam appear put air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_pressure(force, area):
-        return force / area
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
