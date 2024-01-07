32a727a76edd86c18a39068fcdbf5e9b9bb8a72a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:12:14 +0000

    wonder since mix dress best

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
