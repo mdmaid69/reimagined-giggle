184fbab1b9cc7ec4df88528a9d9825771f0c49be
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:01 +0000

    formal connected concentrate division hardly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
