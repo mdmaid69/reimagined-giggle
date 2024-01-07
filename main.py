3a3951eadc7c190a5be5599d5efea15423a212fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:16:34 +0000

    mixture party hour command tooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(5):
-        print(i)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
