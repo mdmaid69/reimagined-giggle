9a957c1db89bd71e7f568d6123b5cb3cac07f6eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:20 +0000

    turnover ball we quarter power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
