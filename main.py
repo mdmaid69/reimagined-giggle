ef750a11e963c15f715562d324d09893df1f7c4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:28:38 +0000

    about rough badly dull play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
