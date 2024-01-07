f0bda49c59973df3374a91d85bc75f21cdadc6d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:49 +0000

    arrangement suffer right statement battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_list():
-        return collections.UserList()
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_as_float(array):
+        return float(array[0])
