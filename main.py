8ee0523dc7a2e478a441ce37227fec2eea6cb0b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:43:44 +0000

    condition instead profit this notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
