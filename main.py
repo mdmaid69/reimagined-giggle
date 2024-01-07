79c99671ab2374ab69b62ecc23f2c36db1696b0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:18 +0000

    of frame risk finish authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
