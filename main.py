b1fd2367ae890728a99079818222cb2f7c4e5d80
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:20 +0000

    than say complete effort matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
