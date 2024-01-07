4d74e97ad24536311ae49f6547e21c5155d1ff35
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:02:32 +0000

    turnover behaviour twist escape high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def reverse_array(array):
+        array.reverse()
