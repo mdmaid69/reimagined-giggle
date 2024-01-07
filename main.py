947746b0061038007fe2afe124f3b62d12ed1a0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:18 +0000

    group reliable favour brief pure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
