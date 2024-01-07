3a261d8bba9e5bbedddc3d808c5c7d27a9ba5540
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:58 +0000

    hurt fast suddenly build apparently

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
