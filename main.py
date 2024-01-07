ed407e846b5d40bcab7cdb34fa2ea1acf834231d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:06 +0000

    smooth ever damp trip voice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
