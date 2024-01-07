e23b732ce783f5e85459317aa9a689180e73080a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:47 +0000

    different same night embarrassed finish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
