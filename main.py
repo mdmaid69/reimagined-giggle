50f62b3d5c4e42e0fbf3842b4d8fbbe60a0e2471
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:03 +0000

    distance age fault top tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
