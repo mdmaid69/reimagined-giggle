05ea1b31bf481e7f2e475c9f9a326784599ccaec
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:58:40 +0000

    left fix comeup escape note

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
