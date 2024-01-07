6cbbf951a5c7b80c6a21434c21e3db6d8b7398bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:23:46 +0000

    follow shine conversation score that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
