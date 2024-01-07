dcb15ee372c25d426b1630cab7bd8db69dc211be
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:48:28 +0000

    whole can professional word emphasis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_palindrome(s):
+        return s == s[::-1]
