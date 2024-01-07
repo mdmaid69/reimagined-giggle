66374d10914ac3a9ab7e64b7f61678952bcfa258
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:47:08 +0000

    opportunity broken prove comein furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
