1b33cdcf5179598416d86e15128ea9c51971566f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:06:27 +0000

    question punish free responsible top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
