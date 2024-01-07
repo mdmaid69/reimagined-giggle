d738f44e0d5e4dac2b88b96219c0017f4afeac3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:20:33 +0000

    hurry change age mainly switch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
