db08d9c3d883cdc99f51b89a55015dda2b7bcd5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:08 +0000

    escape listenfor too listen clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_index(array, item):
+        return array.index(item)
