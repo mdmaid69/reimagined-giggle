7881bd4492ebab8446d06d6f1d84665bde269928
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:08 +0000

    later scare more normal sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
