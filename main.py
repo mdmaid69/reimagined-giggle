9d424bd362b29c8e6123951d6eff2526c9370470
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:09 +0000

    hers disapprove break experiment getback

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
