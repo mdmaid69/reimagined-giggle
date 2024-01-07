5af291fd92624a3614f123df864bfcb9037bcec1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:56:16 +0000

    should cry brush decision excellent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
