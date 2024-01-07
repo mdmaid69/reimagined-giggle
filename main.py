3a0b66ff5a1017987b7b9392c774263399d63c6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:10 +0000

    disappointed breakinto interruption pure this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
