40fe5f4b1b9b5c6059515540fd84eea3423d2c4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:53 +0000

    instruction country ring sort to

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
