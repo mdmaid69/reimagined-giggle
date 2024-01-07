90f6d65fb36066e175889093a75b4634b71248c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:16 +0000

    alive blame suckme raw thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
