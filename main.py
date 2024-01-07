7c2ce43364dcd5df117fa2c581c1ac83b3496a98
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:41 +0000

    case count clearly sign mess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
