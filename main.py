ed9f19b9d735ee576f8ea4e0c433c03380921378
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:56 +0000

    hour big life true happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
