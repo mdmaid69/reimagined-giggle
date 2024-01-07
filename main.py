607b6b3e59ef1dd89f7e1868a64867bb53548bf8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:56 +0000

    coat cover imagination crowd undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_string(s):
-        return s[::-1]
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
