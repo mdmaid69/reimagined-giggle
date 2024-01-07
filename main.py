135643de2066bee20acdd28baea9aa46d0cf0634
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:31 +0000

    over explanation poor possession innocent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
