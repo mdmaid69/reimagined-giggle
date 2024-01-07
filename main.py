2b65ce8b6d820380241c464da9339bd0333acc8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:16:33 +0000

    chief backwards confident backwards fortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
