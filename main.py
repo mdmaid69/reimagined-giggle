d51e8889492900857f830db36176ca810efc2bfd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:53 +0000

    classroom knock later large second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
