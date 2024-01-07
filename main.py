9421190b501a6a741b8591e0ced2ff41c1568a6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:43 +0000

    view few too simple soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
