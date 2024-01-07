f51c52ca603ed7c6fd1dd773d8392e7a5fa377f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:04 +0000

    minute imagination television but lonely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
