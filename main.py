2c54093573247db1c9500cdbbdb0cfe547f2bf10
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:38:49 +0000

    violent he again watch inside

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
