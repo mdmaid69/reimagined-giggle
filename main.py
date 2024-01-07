bfbd8f971239acea8e755246f6836b8900028bf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:11:46 +0000

    rule tough Rr loose right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_cosine(x):
+        return math.cos(x)
