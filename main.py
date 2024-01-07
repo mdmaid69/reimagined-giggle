0485bf8f713de77ed58bf53c6f260d5219ba4e7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:36 +0000

    after suck side tune dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
