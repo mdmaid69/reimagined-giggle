bdff14ea6ad2871da70a5fce671e272378ca83e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:44 +0000

    expensive tie pause comedown plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
