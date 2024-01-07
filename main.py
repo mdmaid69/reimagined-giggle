293d72966083a397dd5f94818d099d6fe539b217
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:27:23 +0000

    anything combine exactly cruelty bright

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
