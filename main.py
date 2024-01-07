9667b7a481c2d67f7dd47a7e4762f4c4ace52e85
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:54 +0000

    period relationship light question pleased

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+text = "Hello, world!"
+print("Characters:", len(text))
 import random
 def shuffle_list(my_list):
         random.shuffle(my_list)
         return my_list
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
