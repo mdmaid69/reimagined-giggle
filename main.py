ccea677f90880b2e6879e7496889dab0687553f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:08 +0000

    team study mark immediate few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
