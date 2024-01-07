038b1018182765a8bdc0595da5ecfc16fc92e7f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:03 +0000

    mistake expensive everyone freedom afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
