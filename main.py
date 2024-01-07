fe1f633feaf9dc2c238fafe16547419621cc4f0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:57 +0000

    general me walk block really

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def is_odd(n):
+        return n % 2 != 0
