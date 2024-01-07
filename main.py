3c1314be8f8d2808ac2e00b7c9cce9214eea8955
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:34:01 +0000

    within thinkof indeed welcome thinkover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
