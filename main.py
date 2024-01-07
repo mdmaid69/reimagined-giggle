729bddd5d4b79712e90e080fe8a85529b3c2a24a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:01 +0000

    equipment narrow key next over

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
