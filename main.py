1287f2fcba27e1f90face8aeb9c13946407db01c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:51 +0000

    remain announce spray worst turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
