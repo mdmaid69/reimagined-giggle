078345155b7b1f6dd481dd8e9b76a42ec59cb18b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:57 +0000

    offence expect conduct admire set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_length(array):
+        return len(array)
