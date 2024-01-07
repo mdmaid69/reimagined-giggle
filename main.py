115dea3dfbc862d37a40815fe12bdab15a4c0bd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:26 +0000

    post by attitude distance group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-name = "Python"
-print("Hello,", name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
