5de4a4a6de49e56ee58037a6d7fbe4b1f564685f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:35:05 +0000

    main bet fear foreign move

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
