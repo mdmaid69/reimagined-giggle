d5ccea84d6a7a3ef4d721cee2ad5c395e88019c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:48:24 +0000

    discussion waste warn injury history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
