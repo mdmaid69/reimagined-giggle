f8c0dcea10105a9c3c9516c20e9f7593e89e0dd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:38 +0000

    control frighten unable show welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
 x = 10
 y = 20
 print("Sum:", x + y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
