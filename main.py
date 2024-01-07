4e65e2053f0917be5a12cb490c1f1857fba5079a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:02:00 +0000

    away real from official bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
