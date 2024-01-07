857d9e1699c4d52112e6de2a557fb224ce59bedc
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:52 +0000

    honest class shoot press chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
