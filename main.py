0fb8557b41c8b2d5f3b33666dbae2f8d8381b08a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:41 +0000

    568energy criticism smile desperate decision

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
