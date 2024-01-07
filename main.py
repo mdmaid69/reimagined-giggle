3c790ad47191e48374da50c7789981bd211a7c6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:58:55 +0000

    cap rough risk type close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
