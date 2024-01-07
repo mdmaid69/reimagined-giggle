ac031d932b02bdf498f2eb5ab8852db000094f46
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:49 +0000

    possibly satisfactory comfort group lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def convert_to_octal(n):
+        return oct(n)
