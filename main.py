af188c6b25f98fe08167cf893122b37a3d0b37c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:44 +0000

    clue arrival abroad disturbing tall

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
