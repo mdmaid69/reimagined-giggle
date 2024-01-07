a3961625f1c9d3a6937a796381f9a218545dccaa
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:50:11 +0000

    trip project for 567enemy react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def sort_list(lst):
+        return sorted(lst)
