10e1ffd1687d1defcf9bcdd6d2564695c7933424
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:50 +0000

    want empty cruel better mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
