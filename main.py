9cd4f2419632c9021820c026065d37fc844fe679
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:22 +0000

    reply school ability radio situation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
