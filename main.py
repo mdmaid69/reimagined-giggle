4a5031aab16208f99705bf85fd978b45638ebe3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:50 +0000

    person factory pronounce memory guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
