52b07ed2e4b95d788799779e79019eb306dd7ce0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:21 +0000

    store characteristic after exchange port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
