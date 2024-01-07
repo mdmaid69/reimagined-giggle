b498869eb0c11f29c977bea75c78b1e0fd617340
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:55:16 +0000

    any seat chief impatient principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
