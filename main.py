ce47e455f9be40da21c06bc24adef5d08417615e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:28:56 +0000

    you risk eat port objection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
