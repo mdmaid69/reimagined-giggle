4b4bfbded5332ccb79cb9aec9ceb68f4075c0281
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:17 +0000

    needle over trousers bush anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_max(lst):
+        return max(lst)
