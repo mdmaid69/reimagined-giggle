335bc0a90b608a2f8025c68ed055efa88f648a86
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:15:55 +0000

    report discussion quickly can pronounce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
