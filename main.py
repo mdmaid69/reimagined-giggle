8d333b1ed13c4838574b9f01298c6815885b7f15
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:03:48 +0000

    near thorough instead read receive

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
