ff8281d331cbfbbe97827c08c35015a40a6363b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:24:04 +0000

    talkoutof goup great official art

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
