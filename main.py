f6601297ab889ac6a3f863db38df505185820319
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:59 +0000

    until own floor near expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
