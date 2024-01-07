a0eb899dcd90350d72ab0fba8721b59d2edccce7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:36 +0000

    top transparent pick plan few

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
