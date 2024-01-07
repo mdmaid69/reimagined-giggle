14a3dcf84096d9c4ecd41e77ebadbbfe25c3a7b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:15 +0000

    combination side simple corner rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
