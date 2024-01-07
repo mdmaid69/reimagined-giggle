a8f01c60cc13a8fea1cd9350af053ad6669636ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:13:20 +0000

    jump prefer lost telephone exaggerate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_power(work, time):
+        return work / time
