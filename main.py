c79fcfcc1a0aad0fcdef8e0454f4cb4037ff0867
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:54 +0000

    think over getoff mind holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_min(lst):
-        return min(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
