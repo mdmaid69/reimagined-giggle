be14a1501932566845e76ae5cc574b2aadee3c97
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:32 +0000

    tight far rule break connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
