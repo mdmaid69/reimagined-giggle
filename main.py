dce92e27e3b80776172800b82c3fc39a20fcebf8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:00 +0000

    quality lower shadow satisfied process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import time
+print(time.time())
