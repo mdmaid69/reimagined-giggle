08942f181174222ce6e57cf2498b6f75f3e5fda4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:32 +0000

    emphasize needle high bye advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
