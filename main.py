3e4bc0f1bb9f187bc9b2d0866cb9fdc2642fb6e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:45 +0000

    taste comedown detail in contain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
