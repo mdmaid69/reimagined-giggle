aacf0473522259e7938fd191168bf20d4be6f7d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:10 +0000

    replace see fierce pay imagination

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def calculate_area_rectangle(l, w):
+        return l * w
