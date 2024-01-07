fb810e819ecb75690d3c8c5b285812ba5286a883
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:58 +0000

    through phone noisy regret contact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
