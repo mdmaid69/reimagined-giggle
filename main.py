33785a28e82276208068d6ba9a0747f19e271e72
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:22:54 +0000

    by spring track bang label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
