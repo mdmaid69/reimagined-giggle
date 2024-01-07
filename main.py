35a7714a3b1fd50cda059ccae03ce473541dc53f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:01 +0000

    health spin goaway middle form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
