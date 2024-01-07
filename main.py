5c0ea7846361b7e8780179f8aefd22e60efaae68
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:05 +0000

    excuse wheel judge away wherever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
