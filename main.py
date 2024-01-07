58f17c7f880e90ec471b3c16b9f7e43b255aa42c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:44 +0000

    refer violence unite luck rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
