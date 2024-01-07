0a2b30a806db48bbad3b4ca8d0b81fbf26a0e6a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:42 +0000

    worried demonstrate comeround breakin Oo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
