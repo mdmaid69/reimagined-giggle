3a111d09eb7a6818d01896870630df05c0dc0747
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:05 +0000

    finally nurse natural quantity they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
