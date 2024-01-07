4271c620d7eb112cca45565dd7bbb29970e78db3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:52:34 +0000

    release other available account school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
