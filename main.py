f371fedabe26ad9d95556e531b4edfdcf04185fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:07:15 +0000

    degree guilty circle deliver promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
