cd00abe18c32fb4fcef22f24fc1e27ed673905a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:19 +0000

    please sight correction tight without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
