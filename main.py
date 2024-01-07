ba35147a4b92f8b3c5e516fef4b2429fde61e29a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:15:26 +0000

    scene proof we rich embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
