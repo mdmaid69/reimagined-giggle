04d7145e9aeb22a1418c8ce2e1db5b5c9e9c1008
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:52:34 +0000

    moment satisfactory foot passage Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
