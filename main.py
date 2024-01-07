1ea67ec05a6cf9253a9a0bc137d988ffcfdf07f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:07 +0000

    name regard every perfect enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
