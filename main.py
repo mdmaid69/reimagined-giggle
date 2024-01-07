6691f376fd65cdf5745e0e70dfe75e5d4158e1f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:44 +0000

    general display towards much holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def find_unique_words(sentence):
-        return set(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
