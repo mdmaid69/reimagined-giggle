532dcbed8793d9808966350a15abb0b80cb59b4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:48:29 +0000

    past 565end Ll hand chase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
