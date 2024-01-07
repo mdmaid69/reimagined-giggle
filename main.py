42f2a526935edaa7817dee86843c1accbe5caff9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:44 +0000

    before return tip interest meeting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def count_elements(lst):
-        return len(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
