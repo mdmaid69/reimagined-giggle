09ec0f2aea6d9b2eb987eead86abadf339abd9e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:54:37 +0000

    sharp check yours think shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
