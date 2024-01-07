8b917ec221d35335b412487266e16dd675d9f20f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:19 +0000

    short situation open contrast strike

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
