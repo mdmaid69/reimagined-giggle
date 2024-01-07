9758e09da411195aadadc40764647ad9b90e351b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:25 +0000

    history for on professional none

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
