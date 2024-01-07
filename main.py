9587efb3ff6c8f4b7d835b55147ece0cbe3b4435
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:07 +0000

    Vv human lower suddenly room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
