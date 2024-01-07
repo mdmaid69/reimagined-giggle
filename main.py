152bc596fe994a8dee92209f8e9b47bf7b068da2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:46:24 +0000

    turn Pp remember proper sky

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
