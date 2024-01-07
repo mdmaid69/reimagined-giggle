61d5ce548f20032ed75f8fd0de8979a1d550bdb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:08 +0000

    mad war talkinto represent limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
