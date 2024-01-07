cad164d48c7fcff77c1f2e276ce6a2a6b40bf623
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:16 +0000

    truth along clear other within

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_contains_item(array, item):
+        return item in array
