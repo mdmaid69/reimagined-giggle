3282caefa0757e0a3e85052665ec8bb6536d5d1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:12:40 +0000

    evening direction of wash angry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
