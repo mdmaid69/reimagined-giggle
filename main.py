795795012dfd7177e0df9b2852d3fe2fe2894ed8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:29:37 +0000

    prepare so flat poison evening

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
