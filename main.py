f447f96769536c5f1ca7f5383edfd4b68014247a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:20:37 +0000

    off quickly distance reach rush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
