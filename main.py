4be95eb8a7267ccdf057ae36da391984097e5da3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:42 +0000

    peace makeup keepoff factory character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
