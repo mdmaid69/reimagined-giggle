9294a63bfd5b156a8fa63ce4ac854497fdeba708
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:55:25 +0000

    value responsibility wander reject keepup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
