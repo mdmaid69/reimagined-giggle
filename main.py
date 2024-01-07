5fa671f5778c2f326038b1649f2e6f44c25a5dd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:51 +0000

    admire bet language song doubt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def divide_numbers(x, y):
-        return x / y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
