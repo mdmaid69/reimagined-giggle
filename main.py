745640310c031f539880ba848f5c24a67ca03207
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:24:44 +0000

    urgent wheel this its apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
