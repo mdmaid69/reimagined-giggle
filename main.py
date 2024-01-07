551aa825d92ff8be90c383a4eb00fcd77b431ae9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:37 +0000

    hit before book check appeal

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
