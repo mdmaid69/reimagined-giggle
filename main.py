2241a789387e7ad76c8914da288df0ac83153a8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:34 +0000

    here centre ability date from

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
