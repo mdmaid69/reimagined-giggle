98b29f6c2bff5adbbddd7a5068680786d04600e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:58 +0000

    imagination cope protest turnup bridge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def subtract_numbers(x, y):
+        return x - y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
