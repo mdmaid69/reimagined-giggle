8f16f219e589ced525db8e8dcaee54446326336d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:57:32 +0000

    cut wire shame nothing prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
