023d7a0079b3f76481714dd2bed482ce0978d661
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:32 +0000

    simple reasonable exhibition weekend themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_power(work, time):
+        return work / time
