0431c00231432f160a554bed032dcdfc5b64c77b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:13 +0000

    bus run offer 571enough 566end

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+for i in range(5):
+        print(i)
+  def reverse_list(lst):
+        return lst[::-1]
