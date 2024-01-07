5c5ca9dbe9cdbf390ec4dc9a4c5de2ec8609a0fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:04 +0000

    television smoke inside drop regular

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
