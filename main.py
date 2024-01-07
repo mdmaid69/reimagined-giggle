4c752816acee45429d38615a7ddefa8e81b47052
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:37:24 +0000

    quit save sensitive represent vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
