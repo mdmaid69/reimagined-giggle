0c9244db9fd6bea43ed4cd4e1504bc1c5986c0cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:56:26 +0000

    dead shine great become before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
