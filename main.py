026a4b0b7cf6f3a1d8af4ac21eb49ce16ef77543
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:17:55 +0000

    count part way set soak

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_floor(x):
+        return math.floor(x)
