43ef1fcbc1a7f46b6263128042c67cd21b8283da
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:46:21 +0000

    match statement expensive might arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import collections
+def create_user_dict():
+        return collections.UserDict()
