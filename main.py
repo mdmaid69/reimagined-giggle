8368bf0a54196fd1102e9689b2235f98abf646a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:03:26 +0000

    nice guess past contribute payment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
