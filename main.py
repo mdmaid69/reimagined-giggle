e0f506d0ba5d55a0c5720793cc2ae239bd04b3a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:56 +0000

    many goon matter politics proper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
