de3f7820bcf842e52828ec658a8e0212855ea0f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:37 +0000

    solve regular direction worth exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
