b5705ea7f47c3bba89bde6fe5e45be423c025bb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:44 +0000

    tall actually Pp influence much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_area_rectangle(l, w):
+        return l * w
