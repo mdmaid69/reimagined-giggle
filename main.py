b2eb81bdcf19fa63f5cc8c8ea4dc95fbe05485fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:15 +0000

    respect kick advanced tidy 565end

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
