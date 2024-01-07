d68ec8a0d461fa5150275b50ac3927ddc0b2fdcd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:45:35 +0000

    name string stop switch comfortable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
