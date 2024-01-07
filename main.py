9a6e0abfe67cae87f229595cc28074453d80dac7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:28:07 +0000

    more work closed confidence unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
