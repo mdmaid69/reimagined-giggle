e91799bbc7eb0d4f318e0b7a98b9329d007884e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:15 +0000

    slide cool mistake mess well

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
