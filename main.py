5be8672fb72b0f2b06b0a6b752bfb85c862fa0cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:33:07 +0000

    comefrom practical previous fashion year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
