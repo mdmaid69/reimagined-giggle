0c7eef934fb3becc6123f30ec7807798be3c3967
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:50 +0000

    create pay news careless takeback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def add_numbers(x, y):
+        return x + y
