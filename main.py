d84535758fbc5e984918fcd506feed87a4ea98aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:52:53 +0000

    century up funny trick waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
