148433eff7f0464fdf1d7f4f762320b59c36164d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:56:37 +0000

    fasten transparent discover far under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
