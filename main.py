6a89a7a811b06e2ee6c0223c63c136e64c67a82b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:08:18 +0000

    somehow record contribute care round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
