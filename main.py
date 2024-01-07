a27aa1bebb6739f04698a65b4a2dd1dffb3e2f8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:55 +0000

    turnup live wet store scream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
