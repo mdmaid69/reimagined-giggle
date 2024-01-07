69821defee6bd6a59f4e0210e84331eb4f78f32c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:31:34 +0000

    embarrassed illness supply light coat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
