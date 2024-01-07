3ccdef44c8d93f507c913d73f2f02ff6a4c76d31
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:46:35 +0000

    attention child spend foreign exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
