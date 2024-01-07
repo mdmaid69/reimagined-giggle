20ba1879dfe2cdaf30a7b10088e4c24af44566b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:01:13 +0000

    action person pull distance pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
