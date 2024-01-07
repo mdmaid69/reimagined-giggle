1cc001f257fb63b9642dd4f4ff66285916d1c886
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:31:08 +0000

    class container warn turninto drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
