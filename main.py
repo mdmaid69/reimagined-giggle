4741c2bd7efd95b63d549c5ed41d0bbcabe73ddb
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:18 +0000

    buy familiar doctor like capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
