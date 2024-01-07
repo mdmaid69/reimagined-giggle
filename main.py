2287e054f4d8fea6e774c6edb77e128fb4ab8f51
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:07 +0000

    react object 567enemy home permission

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
