27968a0e4e74eb8b2c30fdce3fca5236b6cb9441
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:54 +0000

    doubt smoke escape travel book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def count_elements(lst):
+        return len(lst)
+import array
+def clear_array(array):
+        array *= 0
