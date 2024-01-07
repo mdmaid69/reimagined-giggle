035edd84c3862eff7ce464bcd0b023a034a87361
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:12 +0000

    shade peaceful draw spoil listen

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
