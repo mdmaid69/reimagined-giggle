7ce14fd6e4a194e710f6ebb0a6c052ea6497e158
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:28 +0000

    degree getaway after this image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import sys
+print(sys.version)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
