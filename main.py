4bade6747f60be99a4b53723ae20364482bf8491
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:22:47 +0000

    impossible accordingto system cook workupto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
