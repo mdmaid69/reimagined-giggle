e939b75008f028498c517feb0e7c4e848361560d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:17 +0000

    separate trial complete wonder safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_even(n):
-        return n % 2 == 0
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
