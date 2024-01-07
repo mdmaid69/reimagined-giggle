a3ac80a01cbf1d06c7078250e78287c0dc3057db
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:51:49 +0000

    slide much scene duty whom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
