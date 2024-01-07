06c40368d91112f69b522e4ffb7118ff3b003b9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:14:16 +0000

    only find lead arrive certainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def get_current_working_directory():
+        return os.getcwd()
