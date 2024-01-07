280919965fa50893ce12190e380f90a0614f8906
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:57:05 +0000

    turnover otherwise middle trick 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
