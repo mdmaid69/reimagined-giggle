65c2ca1624820a913efa9f56cc435f5b83a3a511
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:32 +0000

    result workupto hello school furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
