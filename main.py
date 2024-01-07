59d495354c36c7ee6c37302608151b39067cf681
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:14 +0000

    opportunity pause apparently whistle proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
