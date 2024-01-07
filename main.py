e599880709d79195d6c7830dc9eee53e13df9cdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:31:27 +0000

    vision trick round word callup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
