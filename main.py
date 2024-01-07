99a46cdbd07eb7b49d8f997abf45ad704b415ff8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:03 +0000

    unless special must goon tired

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
