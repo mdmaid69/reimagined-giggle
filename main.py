3c02cf4444854afa3794b85219d62f0490ce5365
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:01:19 +0000

    great distance furniture skin trust

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+print(sum(range(10)))
