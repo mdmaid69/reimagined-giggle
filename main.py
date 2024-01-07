1afc01bdf4ea5438b94bc33de1334068bd46e988
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:34 +0000

    amount careless among one land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
