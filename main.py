21711b214148fe4b7c3a33a2c315cc9d9b3cf5a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:08 +0000

    future note fortunately Ii waste

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Characters:", len(text))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
