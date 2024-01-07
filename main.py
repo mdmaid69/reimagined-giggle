6068765f71db01beffed2ea3d93e243e73d60691
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:15:17 +0000

    walk want steam look ability

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
