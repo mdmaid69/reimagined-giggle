0a4e4aeaabb90fd3d310cd1f7cdcbc815a19ad6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:13:10 +0000

    replace pass left before regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_density(mass, volume):
+        return mass / volume
