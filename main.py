77afbb826390833b997d5ab3b22347649867468f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:32 +0000

    measure meanwhile worry square contrast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def sort_list(lst):
+        return sorted(lst)
