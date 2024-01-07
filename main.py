b171ef0c2b878b437dd3f4933661af72be76c408
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:33 +0000

    cross use slight physical fight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+print("Hello, world!")
