2e4398910aa7fadcfb4be15081346caf64142a77
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:55 +0000

    now study 573entertainment fear trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
