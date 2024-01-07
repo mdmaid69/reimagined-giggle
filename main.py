2c119c6f39557da8273fe0455eee951dd114f43c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:27 +0000

    hurry age please gradually wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
