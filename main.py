94520cd561e1b970dc766c304c94b74c08642d7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:50:58 +0000

    difficulty comfortable character nor question

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
