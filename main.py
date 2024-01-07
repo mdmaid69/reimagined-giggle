fef9c65970535bb68881e407774ee58475c217a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:03 +0000

    shallow difficult then go some

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
