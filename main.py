17be5dcd17e0549d483fa299f2a0f0b455230d34
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:16 +0000

    nor fortunate drive think note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_directory_name(path):
+        return os.path.dirname(path)
