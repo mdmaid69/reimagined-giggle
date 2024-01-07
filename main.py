75c9f72bc83e564f1897fdfed5526e6a7397697a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:54:47 +0000

    force profit movement feed care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
