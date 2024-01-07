b8eb218350a160bc1dc85354a351ca428c085aad
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:56 +0000

    steal permission relax callat light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
