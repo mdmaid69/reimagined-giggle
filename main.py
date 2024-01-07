acbc90c2c7543ae07fe68816b0512e1bdbd6173b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:20:01 +0000

    such mixture negative hungry freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
