492b4b6c34795cd6965aecd8f7d144815ec63671
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:51 +0000

    physical strange exact offend 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
