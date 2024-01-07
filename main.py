6eb9d38f5d242337237dfc25486740bd4d6f75ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:34 +0000

    decide touch high dick pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def square_number(x):
+        return x**2
