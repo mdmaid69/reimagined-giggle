3b885100fbde0949208ac065eb7c5c90563bc721
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:43:57 +0000

    let stand past ever form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_list(array):
+        return list(array)
