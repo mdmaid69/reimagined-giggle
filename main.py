1a47b51efa23a797b30bd307c54dc19c2107ff36
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:42:10 +0000

    black compete motion spring remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+def log_message(message):
+        logging.info(message)
