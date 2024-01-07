6e45c45d2e8054abe473433e084a0ca3e92c21ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:20 +0000

    part description drawing left crash

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_pressure(force, area):
+        return force / area
