77e7fa739cd4cf5297e82cf3228155d37ab62985
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:29:20 +0000

    traffic exaggerate wander bite smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
