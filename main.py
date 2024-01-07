ce23e5eb125e4cb780906e4a1953d10f384d24cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:23 +0000

    snow full getback bet smooth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
