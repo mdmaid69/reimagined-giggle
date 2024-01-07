3653d44c628732160fa3c6560adf6307f41b3099
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:10:48 +0000

    trade exactly uponon towards meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def find_max(lst):
+        return max(lst)
