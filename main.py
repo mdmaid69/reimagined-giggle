049cf5a72fe4f4c3b264ebb8b21096afbdb979d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:35 +0000

    he takeaway repeat wing best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
