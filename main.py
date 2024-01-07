84c03fc54924388cde7865174adb82c35c63dfd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:49 +0000

    better idea plant space find

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import platform
-def get_python_version():
-        return platform.python_version()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
