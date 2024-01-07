1e5c69a4b74da22807abc7f1a00b72b75dc26610
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:32 +0000

    unlucky name minute style decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
