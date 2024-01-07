fe208bd06fa7c2df32ecaf92256b8b623e802126
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:50:24 +0000

    sharp shake act help school

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
