7a3443f8c4a0e25b024d831ec81c361bf1e3a774
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:39:57 +0000

    something steady score chief hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
