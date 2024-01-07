29a73d2dd2d7358c6c6bd326ce39d3bcab7d3767
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:48 +0000

    slightly too poor wherever medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
