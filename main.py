078b75340410b1f637f77f591ad676ebce4fe0e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:00:47 +0000

    contents burst produce repeat book

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
