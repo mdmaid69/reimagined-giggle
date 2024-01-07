effeea53d0fd03c2659a30656aed35d3d592ff85
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:47:56 +0000

    Ww guess journey along feel

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
