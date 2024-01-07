58f776c5a7dde126e7cf828a63d367e2fcbc2289
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:20 +0000

    564encourage health land somehow listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
