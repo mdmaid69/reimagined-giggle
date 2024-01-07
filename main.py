edba1292bda28628ed56de26764e45cf40ee901c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:51:52 +0000

    evening half wipe shopping unless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_length(array):
-        return len(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
