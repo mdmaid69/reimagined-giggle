53b42e6ff3a5ec6c4419a77155d5648a5477b7a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:08 +0000

    very small reject invite wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import sys
-def print_python_version():
-        return sys.version
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
