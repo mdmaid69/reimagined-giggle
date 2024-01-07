990d00f56225d238b4fdf836651f01fb4fca87b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:06 +0000

    stare real early sister Cc

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
