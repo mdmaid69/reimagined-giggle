bfa2892aec021cb11124c823990bce66eda12802
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:19 +0000

    calloff boil bound Nn myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
