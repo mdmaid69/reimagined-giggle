853eb9b6fc724c888a0283c2b5240f35c8154129
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:35:32 +0000

    uncertain accommodation connected continue day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
