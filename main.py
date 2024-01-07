d65af4fd4eed5b7e6deeeae54ca601467df4c8d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:51 +0000

    share call least health mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
