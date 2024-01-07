6b2f363d5378b29fcef3640310ac92776af6a960
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:05 +0000

    567enemy smile search wear base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
