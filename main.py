4b18c1f100adfe5f81451e0ed727adb77072408b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:30 +0000

    point rush tall row image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
