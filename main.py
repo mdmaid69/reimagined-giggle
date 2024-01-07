eebe5ae9cd78947c6f1d7152e406baeac896c172
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:39:50 +0000

    heavy meanwhile shout welcome kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
