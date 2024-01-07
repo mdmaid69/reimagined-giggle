240385c9b1e8bd27cf8e6ee53ad34b13dde289ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:47 +0000

    general border spot congratulate when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
