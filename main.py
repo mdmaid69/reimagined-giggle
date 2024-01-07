6ce231c7ea845f072b547b0fd386360720f07b9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:53 +0000

    information transport fast run thing

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
