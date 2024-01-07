75814b5f889eb93d6145ec255cfd63924bdbc4b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:00 +0000

    workon less call dig anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
