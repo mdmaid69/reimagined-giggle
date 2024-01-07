2f7bb7c1add15ce6aee8b15823fb5723fff2494c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:23 +0000

    turnup about genuine story freeze

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
