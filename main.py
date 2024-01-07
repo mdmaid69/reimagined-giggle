c89d06c4a2ff1aaeab823909d30e3ddbbad5de85
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:08 +0000

    market roll emphasis argument suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
