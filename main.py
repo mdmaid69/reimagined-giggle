bb7efc02a85054695619a1636f3d33b1622a5c15
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:10 +0000

    freeze life juice complain disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
