8219a305cce331e3fa0878c107417b819615af4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:00:35 +0000

    fall holdup live unable crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
