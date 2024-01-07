bdde14f9e4bfafdee25bb4cd5c8298e5f3ab554f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:23 +0000

    shout clear pretend wild sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
