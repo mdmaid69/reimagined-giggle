6a14710098619d6b033c37a04a1354cd2f0dd222
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:36:55 +0000

    complete argument pot go train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
