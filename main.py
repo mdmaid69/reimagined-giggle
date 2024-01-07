30fb836619066bf5d420048ce7ba694eab02e756
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:17 +0000

    promise unite positive lesson Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
