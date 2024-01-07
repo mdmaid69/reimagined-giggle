ee59c27c85cd953288729719c981646e8179ed9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:50 +0000

    want minimum likely recommend easy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def divide_numbers(x, y):
-        return x / y
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_as_set(array):
+        return set(array)
