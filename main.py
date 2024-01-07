bee1d85fafc48602ee81ab05a62ee3d371ae2830
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:43 +0000

    prove turnback over spring minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
