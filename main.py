d14f7e909796fdc9843f42a3ff66a16f18c47b9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:58:44 +0000

    maintain friend Ww shape breakinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def subtract_numbers(x, y):
+        return x - y
