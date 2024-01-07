6f54fac916848d4448e274fe75ade17d68ffd321
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:46:06 +0000

    uncomfortable ride number beside make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
