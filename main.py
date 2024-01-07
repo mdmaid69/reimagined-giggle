a48ad9b226d00d34da97a392f0bf422b375bc353
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:24 +0000

    rather couple expression delicate its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
