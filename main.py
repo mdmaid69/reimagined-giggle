502c6e73bf4a62ae5c3f48c589ac7cd165e0f275
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:58 +0000

    throughout glass must whatever muscle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_density(mass, volume):
-        return mass / volume
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
