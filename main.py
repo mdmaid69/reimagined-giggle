c60e5cd66f3906163d142e230fcc3b3ae585f598
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:59 +0000

    trip lay actually shoot chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
