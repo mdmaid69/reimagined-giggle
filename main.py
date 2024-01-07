5d528c93d30b8a33770398872e19f253d8bf851e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:48:15 +0000

    concentrate false discuss care attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_work(force, distance):
+        return force * distance
