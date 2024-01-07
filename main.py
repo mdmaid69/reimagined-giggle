356703d99ba7655e05d2a4082ea18f80570d3490
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:10:47 +0000

    contribute nose patient splash test

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
