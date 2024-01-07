b559301e2202aa9f20cfe8e7e088329378417374
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:35 +0000

    fall waste happy Qq head

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
