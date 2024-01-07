6a2c592b77395fbae72a4f13e7a45cc55dbb935e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:26 +0000

    fun match interesting beginning medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def subtract_numbers(x, y):
+        return x - y
