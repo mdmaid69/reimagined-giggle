e851a5f0445fa20b4988b196c53ecb30afd892d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:58:06 +0000

    admire confidence swallow outside fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
