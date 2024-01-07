2477d87c22c2cdc3085cd566fc7589dec3ad7b09
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:28:05 +0000

    average lift demand remain so

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_pressure(force, area):
-        return force / area
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
