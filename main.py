891b2165d9bb1a3463ba66e0d598b50eea89f750
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:14 +0000

    kill somebody breakthrough of half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
