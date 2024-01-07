e37b91fa8767526ce8f5fcf9eac7e79415c8eeb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:18 +0000

    quality fun charge edge face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
