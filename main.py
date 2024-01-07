19a2400eb9b97c5cce4baa5ac787812355088a0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:55 +0000

    tie difference acceptable autumn Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
