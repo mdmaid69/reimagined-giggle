b636734266d18b484361582b36a5a54d5a329fa0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:31:09 +0000

    practice round free big situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
