b61968f906b9a5b0d5caa23669da9c3a1cd4e8fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:44 +0000

    common healthy limit now cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
