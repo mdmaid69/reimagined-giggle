7fcf738e93ca1f11ee72559b0ba5165c4315879c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:50:25 +0000

    unfortunate careless colour politics free

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-def read_csv_file(filename):
+def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
