ad4574a20e21cf2aaf95f9d6c3056b2f977c459c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:07:10 +0000

    urgent mess pride arrange round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_float(array):
+        return float(array[0])
+import collections
+def create_user_dict():
+        return collections.UserDict()
