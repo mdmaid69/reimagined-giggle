ca0f282235e611093e5b8537b76cfa5b176cc24e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:39:47 +0000

    ahead famous Kk discovery heat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
 def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
