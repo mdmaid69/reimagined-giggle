f84dfd0a295461cb3bccad35a71b68ac7b43a389
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:34 +0000

    in sink ball in calm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def remove_duplicates(lst):
+        return list(set(lst))
