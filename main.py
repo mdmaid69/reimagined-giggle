80bd6aa00e90c2b2fea38c17db938d1b8c00187c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:15:38 +0000

    easy record discovery my good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
