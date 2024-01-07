d59d6315d9633d890ca1078cae2b3c4366e7621d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:03 +0000

    fairly female harm port wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
