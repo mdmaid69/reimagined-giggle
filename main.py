4759a6173a9ffdf53bf3b7bf36bc96eac10a3575
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:27:58 +0000

    gate vanish meeting drink serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_speed(distance, time):
+        return distance / time
