f0b5e8dc7233a8dc640821734433c91413d85b90
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:26:35 +0000

    fact introduction wherever wish improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
