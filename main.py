118368b817f93353f8699c18d24a3127dca94850
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:37 +0000

    nice single bus sting smile

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+import collections
+def create_queue():
+        return collections.deque()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
