94331ad44f463284795c2f5634808215dbe6683a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:24:27 +0000

    lower they load doubt take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
