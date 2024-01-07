cd6d4f15ea051abcfb3f0969fe2b307448e191a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:36:10 +0000

    rather some image through doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_min(lst):
+        return min(lst)
+  import sys
+  def get_python_version():
+        return sys.version
