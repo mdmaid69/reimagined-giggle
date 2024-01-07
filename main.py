e15a6654a43e11166eeb6b27907f474a5a48d5b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:37:54 +0000

    excellent awake switch task university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def sort_list(lst):
-        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
