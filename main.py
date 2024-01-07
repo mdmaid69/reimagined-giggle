82b8996c1f7e5953aab793563f3810a4ddbe0e24
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:24:21 +0000

    collect close eat measure man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
