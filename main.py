f51709b87f0f2dfc2b07163a64bfa7cf6491021f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:11 +0000

    friendly I correct new conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
