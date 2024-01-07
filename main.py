8b0b699b656185acac032deb3fd6ac6e2cd66a26
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:39:54 +0000

    stare practical free cause pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def multiply_numbers(x, y):
+        return x * y
