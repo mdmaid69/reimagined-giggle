cb018739e3189c7423c8074f083cfd31d6ff0c26
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:56 +0000

    shadow getback change safety by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
