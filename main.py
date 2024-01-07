55f9b70cbb10291a7b67fad15aa0bea5b416d36b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:49 +0000

    damage city sound sale wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
