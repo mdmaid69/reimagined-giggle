0cf364ad5e03fcda71f5f2ed6c84b0a0194aeaab
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:45 +0000

    deal there limit guess frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
