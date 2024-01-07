4596fb09b741ce458bcf962bf9ec35ab221090b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:12:50 +0000

    breakfast bound cry home Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
