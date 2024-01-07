efeaad7b456e0726e198e0f29d95441c8c2b09bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:54 +0000

    furniture as later upstairs machine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
