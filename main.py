f2e3e8a0fa65ad3bd98c6cd3e5177e3abdad4ed0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:26 +0000

    accordingto regularly inside pronounce language

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def clear_array(array):
+        array *= 0
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
