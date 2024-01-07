a62170cb6a663bbd44b98094a0d6a6e507ebd74f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:35 +0000

    cruel late trick Qq final

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
