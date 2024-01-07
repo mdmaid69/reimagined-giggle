c3b3419a2eb50261a241a8c12458ec9d3651f517
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:16:57 +0000

    ordinary mention write typical sign

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import tensorflow as tf
+print(tf.__version__)
