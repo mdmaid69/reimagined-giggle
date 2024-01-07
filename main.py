011e7e304e05b23d4bfdf18e6634062f74a7ac54
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:37 +0000

    worse clearly tap its high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
