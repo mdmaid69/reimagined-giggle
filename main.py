752628e86f67ee1339328db22b420457a514043c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:16 +0000

    famous ordinary true situation guard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def reverse_list(lst):
+        return lst[::-1]
+import collections
+def create_priority_queue():
+        return collections.deque()
