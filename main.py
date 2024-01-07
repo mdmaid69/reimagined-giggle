49f36bec4df359add50ddd8acd1827212d062c1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:53:32 +0000

    life him disagree exchange strike

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_power(work, time):
-        return work / time
+  def cube_number(x):
+        return x**3
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
