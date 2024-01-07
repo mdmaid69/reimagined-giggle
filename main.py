5f51d44090203de668664602691221e483150b1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:33 +0000

    getoff recommend shape break interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
