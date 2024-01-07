836f170348c32b25d9cde82019971431950d83c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:51:00 +0000

    rid keepaway future shame official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
