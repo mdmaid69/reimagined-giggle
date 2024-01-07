cee3147985e6f9b83f73b9406a30c537fb4ebe72
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:33:43 +0000

    innocent new serve many loss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
