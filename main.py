8aa34bef415f04bb361f8c0223d1f4a8102e30ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:06:41 +0000

    switch press trial tradition pull

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   def convert_to_hex(n):
         return hex(n)
+import tensorflow as tf
+print(tf.__version__)
