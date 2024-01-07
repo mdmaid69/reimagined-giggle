72791ca3451fecd8ecea4899651b53058a2a0c37
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:03:17 +0000

    can ahead nervous every exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
