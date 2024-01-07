b61f488cbe9061509aaabdba5ee9e904886d934a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:09 +0000

    autumn excuse sweet book Tt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   def is_even(n):
         return n % 2 == 0
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
