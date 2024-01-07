80480af512d1259ff1dfc1094f0ec98298318842
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:25:18 +0000

    hair dress complain floor foreign

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def calculate_perimeter_rectangle(l, w):
+def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
