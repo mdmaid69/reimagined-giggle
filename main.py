69d5849ab794c9894d18404f7fa3b2cc80669710
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:32 +0000

    laugh aim cover safety bite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
