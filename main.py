69d81f56be7ea3380b7216587c8034df5932a0de
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:36:45 +0000

    treat promise judge guard anxious

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
