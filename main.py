c3d52c22e588587250712ed9c29dd9ccda374395
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:52:31 +0000

    shop lookafter consist question anything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
