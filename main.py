37899cbc1f68c3f0e1d0cbacd76a31d2310d5ebd
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:04:18 +0000

    build up temper tongue motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
