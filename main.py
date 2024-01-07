4796d440e8b7bf9c8c94f2746dcad69a85690326
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:07 +0000

    if reply square brush breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
