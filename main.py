3a76b159ca135048ac832e3549c06709b53e14d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:24:47 +0000

    fine show release card comeover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
