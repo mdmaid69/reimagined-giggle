0910fb93be32ef5476d1fc2c5ac78c7751d53f14
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:23:29 +0000

    similar finally goodbye rather sting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
