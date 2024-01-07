2816a17ef97d29019a078f6e8bd2aa10b5e0b007
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:37:55 +0000

    regularly interested why cost mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def remove_duplicates(lst):
+        return list(set(lst))
