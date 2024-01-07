bba9121586860ec0f6bde9ba36656bd07e2df2f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:46 +0000

    566end miss mind afterwards various

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
