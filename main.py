3c3d5c0959fdefdb6af423f26b6b02a333e3c1cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:51 +0000

    protest 572enter spot difference geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
