d1453c7e5ddfda3d8ce14dedb58a81cce3b1d810
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:51:54 +0000

    Pp practice present giveup mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-x = 10
-y = 20
-print("Sum:", x + y)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
