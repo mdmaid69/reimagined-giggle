3a467ba85807d05de6afe390619c476b50f9daee
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:51:23 +0000

    confidence again kind help mistake

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import random
+print(random.randint(0, 100))
