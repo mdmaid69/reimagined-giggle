6c351d6567d7f49b2308eda41711843163e42474
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:57 +0000

    target crowd include view more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
