566610eb842136abe97ba1d512fee63db60df456
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:36 +0000

    bye equipment fair underneath vision

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
