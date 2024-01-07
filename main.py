c30c7e56c5149f2b484611e20d1428484bcb0d5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:38:37 +0000

    slide only detail owe exactly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
