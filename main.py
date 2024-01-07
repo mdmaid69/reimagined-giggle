17e83a7c401f82d0a5a4e1fbc97cbd2b362d3555
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:56:49 +0000

    rush combination style unite disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import datetime
+def get_today_date():
+        return datetime.date.today()
