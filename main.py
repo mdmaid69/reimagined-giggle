8e5f3bc9d985aafbde98f8976f74e442aa9358ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:47:39 +0000

    runinto experience single future 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
