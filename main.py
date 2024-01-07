3dfdbff5d02ec75d7eeb54fe85c1c90cf1f7a78b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:32 +0000

    protest clever copy wherever hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
