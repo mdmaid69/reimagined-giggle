f9b5ace7f6ffc8723e788a8679acf0e4bf17f252
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:30 +0000

    attitude reduction talk once arm

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
