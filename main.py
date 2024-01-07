906d2495dbdb7d1b5c46933032be9085a4627f25
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:27 +0000

    bet point chance warn murder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import sys
+print(sys.version)
