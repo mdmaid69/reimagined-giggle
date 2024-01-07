ce6089f08bf93a808cab60cd28bc3a7ee544fc25
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:49 +0000

    may experience dry image shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
