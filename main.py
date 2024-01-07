04133992cfcf751b73a7c375ea339280d8264fd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:05:05 +0000

    shoot lookaround carry noone date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
