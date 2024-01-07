ff1c4b93090448c774b1bd9ddd4597cbbdacb875
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:08:15 +0000

    even takeoff temper awake nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
