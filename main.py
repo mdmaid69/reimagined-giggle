9269647d566139d26e4b17f78401eed5569765fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:13 +0000

    moment man water pronunciation bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+print(datetime.datetime.now())
