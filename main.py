18ed99d14ae4b1a9aff8c78ba89f1988d13ddde4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:32 +0000

    trust software foreign still debt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
