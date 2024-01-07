7e0a4970980d6b582dfc2177812deb3d0bc51cb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:57 +0000

    follow 566end computer peace interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def subtract_numbers(x, y):
+        return x - y
