125c269751fc1d2d0c20c602b672c403fba6dbf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:55:23 +0000

    way bet might service note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
