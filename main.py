2bc3c705491272d0c08e652474b2502127a8e269
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:45:23 +0000

    exploration result move standard along

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
