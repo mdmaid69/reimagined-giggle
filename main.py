e3a438f9e06aa0cc173aa344e4bfee14424ba527
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:58:06 +0000

    plain blow dinner congratulations support

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
