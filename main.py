49b19c3991e7bb03d75cd2347fc5c6ad210eb4cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:50 +0000

    mysterious by bus we beat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_slice(array, i, j):
+        return array[i:j]
+for i in range(10): print(i)
