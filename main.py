6028bb7e49553dd31e0739628191d63884bb45b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:23:46 +0000

    adjective emotion temperature 573entertainment within

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
