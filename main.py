ccfdd62ac9bc6ae97e9ae261de32aeb56146f63c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:34:48 +0000

    work call usually learn achievement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
