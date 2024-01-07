7514b58c13e1da563afaf9c76d6c55a54b553132
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:15:19 +0000

    only hers damage journey turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def append_to_array(array, item):
+        array.append(item)
