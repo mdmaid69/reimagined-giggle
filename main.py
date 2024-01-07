e6a4bdd2411c362f20dd285dd4838221b8391d5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:35 +0000

    mail will go stupid OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
