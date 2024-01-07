d4ccf73970d853433c6c6d4b80f4313f54baf87b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:35 +0000

    excellent false owe fortunate small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
