13e1118c6f54c51b9ee9122d4380c4fac16c3f8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:31 +0000

    deliberately sting emotion disappointed then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
