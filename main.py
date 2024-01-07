395192fbc6ee8bcad7994627e2ebe7b9fbc09da0
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:42:25 +0000

    drink pour callat knock shelter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
