36ec937d01c7bc01b10fd1bd448b18a1f6f4b327
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:07 +0000

    heat heavy married ask sweet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def find_min(lst):
-        return min(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
