4dba9911f525b38d85c6fe42d1afd5851b1a6f3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:05 +0000

    project never genuine difference please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
