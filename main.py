7a25ee0b01dc09d96bfe4031b09868004e7f55bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:03 +0000

    clear week objection eager depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
