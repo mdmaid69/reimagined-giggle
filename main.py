fe6997fcb99c8b870ed74433eb64c779db9d25fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:03 +0000

    joke connect means in discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
