7bebbb13416465bee30a3b2fe1a0857ceefedb9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:47 +0000

    temper border train mail square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
