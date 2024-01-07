1d8e4888580e7c90443aa683b9504bc2645cd2a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:24 +0000

    border terrible climb advice suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
