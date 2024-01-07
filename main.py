e9bf7345da8a4d6ef6806d7b1039701de6b16296
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:19:15 +0000

    backwards dig shade quantity throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
