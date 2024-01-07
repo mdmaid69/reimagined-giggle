8373dbde24caeb1766061be518db04878122710e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:23 +0000

    best discover culture excellent visit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
