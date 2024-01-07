9cd860960e70cfa38c4dd541498bc6046f39eeaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:54:33 +0000

    more direct maintain term smell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
