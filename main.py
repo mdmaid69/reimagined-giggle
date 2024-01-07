c151bd01e59c37f34092fd922c9673393f42a8a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:50 +0000

    swing story ticket turnback patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
