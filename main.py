8beeef2ca655859fa7da720c92e35322ce538544
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:12:17 +0000

    wise chance basis cheap whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sine(x):
+        return math.sin(x)
