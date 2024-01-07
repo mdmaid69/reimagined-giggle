09e7924f6954173583383d2140e686357c04932e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:10:20 +0000

    after tour needle shape suck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
