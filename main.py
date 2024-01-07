0ae759d5e19552eb371f69c9daad159b3ac3181b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:14:00 +0000

    minimum lonely waste method Kk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def convert_to_octal(n):
+        return oct(n)
