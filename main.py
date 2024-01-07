a73fb04b143c08420de4409010fa3d4f718a79c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:47 +0000

    name sale reliable typical track

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
