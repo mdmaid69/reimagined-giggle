0bdf95f1394d4a370063185727380cfc5149f57c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:41 +0000

    comeout slow country reply physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_repr(array):
+        return repr(array)
