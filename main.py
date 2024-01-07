f007dee5942d94cc6e52f64bed383b7ff183af1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:14:29 +0000

    commit raise try thanks bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
