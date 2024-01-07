bbc8504e5be7b46b0dbb361e50c1f7a0666164a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:33:00 +0000

    tread like towards difference class

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
