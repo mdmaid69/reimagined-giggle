67ec52d8391397dee757318c107e71c99a6b4ccc
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:16:30 +0000

    unit brush makeup turninto exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
