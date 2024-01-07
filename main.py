89cb3d261a8b46bc3eac0594fe5d9100777c7fd5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:57:19 +0000

    great date turn fasten keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_item(array, i):
+        return array[i]
