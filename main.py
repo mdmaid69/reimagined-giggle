86d28821c48b534c23ba661c8771317d6b42b379
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:16:03 +0000

    left jump married hungry war

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
