69187f4d765e855aaf1d4638ef446363311a6b33
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:32:21 +0000

    fly last single still nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
