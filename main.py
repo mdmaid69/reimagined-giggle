17fc3dd9d7ed95c7b6c4a5df92ec128b548be49c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:29:16 +0000

    spend necessary charge damage justice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def count_elements(lst):
+        return len(lst)
