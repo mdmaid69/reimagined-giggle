8f0e1f6aa91ae699921cc49a2e3338f958c65f78
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:38 +0000

    accordingto 568energy breath behaviour beyond

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def get_array_as_tuple(array):
+        return tuple(array)
