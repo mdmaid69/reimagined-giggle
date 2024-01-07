cbd20e830397ecf0bef697ce12e202c85cef499c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:55:49 +0000

    but wash yourself house sink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
