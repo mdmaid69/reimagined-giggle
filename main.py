3bd027ec7e2f644f31be67f08a6575e6608d6b22
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:15 +0000

    how mass urgent shut talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
