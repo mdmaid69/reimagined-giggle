47ebdff19c9c99274045f2b54221fa76277c704e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:51 +0000

    path along allow pleased watch

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
