a91f10c0951e4e307a92dcbe6f1962cd1632461d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:18:20 +0000

    set lock let on be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def clear_array(array):
+        array *= 0
