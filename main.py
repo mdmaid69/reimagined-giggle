c202d90b6cfab554c2a22d68c7166e302e4b32f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:29:34 +0000

    apologizeapologise tear every every crowded

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
