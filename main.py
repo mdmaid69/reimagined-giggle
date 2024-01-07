1b90d60bbf683f12e633c2f3bc2f350dbc78ede9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:50 +0000

    wet your land breakinto march

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
