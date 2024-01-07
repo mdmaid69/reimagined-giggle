43461668918df039194bc9691e2b37a625754fcd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:42 +0000

    cover score work before other

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
