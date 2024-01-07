9909d5f528337f28d8b6b5ecbb16d0f12dbfb22b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:53:57 +0000

    wide far OKorokay side extra

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
