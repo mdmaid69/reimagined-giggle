06441ea0c6efb26db5a8cef6227851750d06c863
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:20 +0000

    disappear anywayanyhow vote dig recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
