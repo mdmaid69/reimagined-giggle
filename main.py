6c80ffa0280e37473df55bc27fdb60147841ca18
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:46 +0000

    delicate window test attractive their

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tensorflow as tf
-print(tf.__version__)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def convert_array_to_list(array):
+        return array.tolist()
