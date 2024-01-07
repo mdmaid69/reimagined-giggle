74d53ff33c3cbf70c3e0c5faaa126fa38414069d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:23 +0000

    mark course the right boil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
