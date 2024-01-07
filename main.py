30007fadc597d8c714aca5708ea6214e81312f72
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:06:17 +0000

    earth exactly pressure hurry hospital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
