d51b2dbc8cd9e8927f2a4ea4fa82c9f02cac903b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:15:25 +0000

    now worth soon swim choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
