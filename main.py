bead0e41bc38d4e85ee612520255e1137c859d64
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:24:17 +0000

    realize range door brush breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
