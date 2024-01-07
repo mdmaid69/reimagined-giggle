2c0a4af8fd323aea6fd2f8bd8654237b93667c9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:38:58 +0000

    cause spin nowhere pretend across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
   def split_path(path):
         return os.path.split(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
