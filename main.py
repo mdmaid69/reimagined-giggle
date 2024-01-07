bcb74bbd7cc6bd187ca2fa95405f211541b7be41
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:59 +0000

    formal gooff become realize label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
