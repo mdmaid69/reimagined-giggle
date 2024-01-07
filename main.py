fe23c53caada9ea5deef93818a2d6acf160a5221
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:59:24 +0000

    rid shock dream geton on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def is_odd(n):
-        return n % 2 != 0
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
