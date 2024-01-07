2e2320cc3db3ed293c16bb035ddbb3f31dcb9521
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:03 +0000

    dig the anywayanyhow appearance straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
