9645c3f2457bc39698785d35e1c1ef7377417f19
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:09 +0000

    task refer deep guilty trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
