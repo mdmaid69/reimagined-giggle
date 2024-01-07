ffecb366a9d87416cabecde8f8e263528280c435
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:19:15 +0000

    steady upset write ought step

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
