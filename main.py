de7a8aeeffefc6ce98760a5002596a009621a43a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:14 +0000

    hit formal very brother importance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import time
-def get_current_time():
-        return time.ctime()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_floor(x):
+        return math.floor(x)
