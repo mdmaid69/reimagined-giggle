8a184f183d25893bf223e27056290c91b9550a3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:29 +0000

    himself explain breakin firm relationship

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+print(random.randint(0, 100))
