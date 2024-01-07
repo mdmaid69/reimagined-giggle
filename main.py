f1ad704cca6ec90fdfbc99023e094bfa35163af9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:05 +0000

    expect neither bath acceptable sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
