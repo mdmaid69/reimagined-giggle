15f820194e93a61ba9aff0fa876a916693d7ddd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:53:44 +0000

    petrol nerve swimming lookup new

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
