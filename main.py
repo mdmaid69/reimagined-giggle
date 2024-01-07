a3d4e137ab10606d49fbe2a928b2ccb8aca42477
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:17:18 +0000

    stupid bunch drawing splash passenger

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import sys
-def exit_program():
-        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
