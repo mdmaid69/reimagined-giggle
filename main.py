4cbffc04e1f38dd03e02f1c8fce98ecb7672ead2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:57 +0000

    general splash exactly vanish maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
