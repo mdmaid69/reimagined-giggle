22616dc05bd1308103a638a5a12bba4315c530c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:05 +0000

    admit noisy apologizeapologise glass top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
