b92cb223b2dabb072a246b1923b641b9b56d7b60
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:21 +0000

    without discussion embarrassed lookback(on) by

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
