8d166a4a2d841e43eada5294cb2b1c3b16e3eb21
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:33:57 +0000

    grow twist best tragedy stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
