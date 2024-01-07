d9d6885cd595f5807c7faad2c1eca263e10923ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:21:59 +0000

    it early matter night left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
