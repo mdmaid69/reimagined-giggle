bbc738f2061adaa675060737c2738af905e5bb9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:33 +0000

    flow temperature autumn confusing yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def add_numbers(x, y):
-        return x + y
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
