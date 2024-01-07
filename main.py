6ccd127420bec8e80a2bf9df89ef33f677a2f089
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:53 +0000

    consult soak ability track test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
