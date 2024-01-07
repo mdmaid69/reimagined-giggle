e4ea3f9ba10654de10d83161ef97ad71cc5dee58
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:40:21 +0000

    church culture experiment rise connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
