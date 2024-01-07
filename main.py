0f5f599d85d2064c1891dd13e5eaf69a4e438e70
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:14 +0000

    comeback soil challenge special godown

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
