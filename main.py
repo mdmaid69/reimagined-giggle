0e51da68de39f4692806f46a3ba957e6431f67d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:29 +0000

    dig hide shock trick guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
