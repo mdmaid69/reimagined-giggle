afdaf86634e707734916ae1468241b29e80c1d0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:53 +0000

    top mad correct giveaway front

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
