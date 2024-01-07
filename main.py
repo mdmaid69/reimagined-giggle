24c7e79efc83a2949558cd9561249c64e1f206c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:50 +0000

    profit life afterwards transport environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
