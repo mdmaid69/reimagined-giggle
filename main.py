78653ef29f8e8016f37b5bca1bd0f03e227120ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:45 +0000

    borrow crash rather keepout rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
