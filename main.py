f4270006737508977dca091c478472b4e0043a04
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:44 +0000

    well charge ill future upset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def add_numbers(x, y):
+        return x + y
