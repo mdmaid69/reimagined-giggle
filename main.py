1781c5947ff1e6c1f156fe218a5d71d2be2ffef1
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:07:40 +0000

    570enjoyable rare ordinary payment ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
