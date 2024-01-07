5dc54c6bb45c9d21b5484f51024ba251ac42b3a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:48:27 +0000

    comein long straight way chat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
