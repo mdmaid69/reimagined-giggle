30e3131bda7db68b30a2e60aca415313b1f3f71a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:36 +0000

    holiday wash mind behind kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
