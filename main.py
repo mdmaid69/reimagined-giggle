ca8da77e230c66392d602913b5e741bbe129f069
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:24 +0000

    lock inquire wander pile straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_list_from_array(array):
+        return array.tolist()
