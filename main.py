10a5f49f654d5983c58b8baec4a9a7b3aa0ada86
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:10:43 +0000

    several door trade force announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
