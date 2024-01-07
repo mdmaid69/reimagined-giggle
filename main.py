6406a9520801b8fb5047438d5ca6ade6a596ff98
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:10:54 +0000

    callup this praise busy contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
