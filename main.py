937601a9cd375d40f8055a30aebdaf4781fae1d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:33 +0000

    basis style beneath vanish demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
