36d2b999495c8d0832999478feb18111a4f44984
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:54 +0000

    area Ee fork such angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
