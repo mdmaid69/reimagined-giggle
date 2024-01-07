3a07b190aded172bd027dd15b8593d91e4dd036a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:05:22 +0000

    impressed unfair study top official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def count_elements(lst):
+        return len(lst)
