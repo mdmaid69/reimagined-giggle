85ce2da23354bb2fe0a1b574a4985d76fca9c3dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:05 +0000

    embarrassing comeout tonight continue breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
