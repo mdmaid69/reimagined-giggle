bc3e7923dde79a899b208d2026fedbacb353ca41
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:16 +0000

    179bite back entry tidy wall

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_power(work, time):
-        return work / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
