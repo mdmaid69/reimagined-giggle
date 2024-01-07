eed48b2cd367744fbecc54c3edd7d8df9b075694
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:52 +0000

    might recognize woman grow giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
