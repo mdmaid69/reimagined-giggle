53d2bc1174b2ffaecaf5e43e10f90687e1a53d07
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:55 +0000

    stone key tell strength service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
