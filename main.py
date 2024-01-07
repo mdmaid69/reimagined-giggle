e46f959e212e937c551ba3b64ffa44042b5789b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:08 +0000

    cost unit great town raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
