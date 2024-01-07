e4f2794a721c122b7b7abdef37074bc17cd75359
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:10 +0000

    bend work cheat beginning across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
