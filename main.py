c140e698364c926bb7e18f7f3c1250d28781b00a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:44 +0000

    power cheat offence central even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def count_elements(lst):
+        return len(lst)
