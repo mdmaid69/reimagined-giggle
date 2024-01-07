7f182d2ddb468f47ab805430db2556b95e23fd32
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:24 +0000

    left increase sad of objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
