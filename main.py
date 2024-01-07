1ff39c9e555d485b6f8da0cd4ddae4e750506f35
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:55 +0000

    mess Itself definite excited key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
