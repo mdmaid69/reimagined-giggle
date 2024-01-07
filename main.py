0aeb4a36b1e691ce089cf5dbc084e2cfdab320ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:05 +0000

    nor head delight after appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
