b235dfff59cd47e830be1879588d79430de3179a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:25:35 +0000

    length must occasional order spread

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_volume(length, width, height):
+        return length * width * height
