f7374365dddd19e978ca8799d372c6543b143da6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:45:59 +0000

    reasonable promise twist underneath less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
