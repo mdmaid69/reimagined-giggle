2b98825e63ab520497923f18ed144d07b9e38ed7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:39 +0000

    sleep store money cheque change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
