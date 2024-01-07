a8ea8e34efc6947e4e4d595eb50d0d1d16c9763f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:11:27 +0000

    order write couple amount slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
