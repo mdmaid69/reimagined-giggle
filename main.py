dcda6040b66ef7cc937bbfd40b55b40cc886209a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:56 +0000

    last pump wipe creature that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
