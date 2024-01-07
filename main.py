36df63d983c6f48bf643318ec43dc5ba6ecbc701
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:30 +0000

    keepout sentence study correct takeaway

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print(sum(range(10)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
