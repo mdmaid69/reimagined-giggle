3f74fc0bad1def6a04b0404f9316ab5326bbd1f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:49:48 +0000

    voice sensible stiff tired when

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def create_stack():
+        return collections.deque()
