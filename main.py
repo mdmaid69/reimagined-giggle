a1708101b02366945c1a9443dfd824e248680438
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:43 +0000

    leave that moral frequent seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
