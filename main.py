8b94c13dc336c03bfc94c7ca407bc643e345f2d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:14 +0000

    bright goon deep exit solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
