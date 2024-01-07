85dcf24eeec535efdb4fdd7d2b3458a48e21936b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:30 +0000

    slice control pass quality turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
