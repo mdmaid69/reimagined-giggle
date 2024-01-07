89c64bff1077d1113ae0363cd87b26e33efc508d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:31:47 +0000

    spite partner hair ball remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
