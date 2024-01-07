5d264479702172702abb3cc5aba3a6e0d37053ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:03 +0000

    polite 567enemy pool range funny

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
