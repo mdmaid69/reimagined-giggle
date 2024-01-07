32cd005107ae7a8677df586695c8afa0807dff14
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:20:59 +0000

    separate chance slip measure disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import os
+print(os.getcwd())
+import array
+def get_array_as_int(array):
+        return int(array[0])
