c30bef98772d6e429a8e27aa8d7df37349a27595
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:37:22 +0000

    Bb history ignore justice mine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
