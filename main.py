7a4f2571837c55cf1004a482a431faf456f05ce8
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:17:06 +0000

    slow perfect blind adjective holdup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
