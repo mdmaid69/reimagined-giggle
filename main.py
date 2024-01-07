2dc4c5cb3c33fd9ac32ece0bd9081515e2f5ab8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:04 +0000

    quantity round education usually complete

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_volume(length, width, height):
-        return length * width * height
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
