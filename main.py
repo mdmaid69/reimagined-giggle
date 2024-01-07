905742965c6ae12e7c49ef10a738e68086cc2f5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:43 +0000

    call mouth guide greeting back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
