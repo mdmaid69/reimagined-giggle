f3b56582842dcad52a6c01c9d0ed967f655fa634
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:30:13 +0000

    centimetre show system proud side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
