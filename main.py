2e98bcbaf7c58679c2dcb7165bafad4fd6d3f3dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:33 +0000

    hope centimetre situation people order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
