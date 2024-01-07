c35112e0a9211175948f8b2fa7d7861b2984561f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:52 +0000

    side consult bright official anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_interest(principal, rate, time):
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
