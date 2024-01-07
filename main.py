20f65c79798fb4986fb9fa328a7e017c56aca786
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:04:49 +0000

    takeoff be suck hope grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
