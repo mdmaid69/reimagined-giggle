de848f6ceb8c7098e2361cb56b50854fd09728c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:11:33 +0000

    afternoon order strong design instrument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
