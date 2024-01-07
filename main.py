ea19c4b9a55d5929ba3451b7916df9eb8702bb06
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:37 +0000

    target carry print limit twist

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
