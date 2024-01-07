a0a4a7cd9d01b4a24735dd9531bea29db678f335
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:03:25 +0000

    off police summer building comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
