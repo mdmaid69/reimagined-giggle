811a07e9049a126b2cf6bcf74cd4c1e921d3f659
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:56:55 +0000

    179bite off any stiff approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
