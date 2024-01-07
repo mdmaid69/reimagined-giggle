8d2a829c9e480f26c323116c7cad964c0cd4718e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:53:05 +0000

    side but health glass tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
