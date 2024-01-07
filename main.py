6eafa80638e4602edeb83bc718c632cc282a6cbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:55 +0000

    many teach human trip list

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_speed(distance, time):
+        return distance / time
+import numpy as np
+print(np.array([1, 2, 3]))
