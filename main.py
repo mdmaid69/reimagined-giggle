c8e25f20fbbbbf895950d158a3ced51399203029
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:01:44 +0000

    display suddenly split bound left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
