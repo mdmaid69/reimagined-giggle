d58a89152f4c2db1e3dc7b33e2b9f84c7cb7b54e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:26:35 +0000

    luck pity size summer trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
