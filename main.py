4c2953d6123a474e298ae872d8367b2c7f6ca3eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:24 +0000

    mine ilovedicks neck explanation influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
