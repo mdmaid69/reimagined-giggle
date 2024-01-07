8ee4d398c71335d8a0e71c3e0623ced8897e67b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:35 +0000

    unable rarely like closely sweep

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
