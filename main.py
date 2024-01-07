eccdafac82da0c788e18bf037d219e2244a83b53
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:54 +0000

    fall I appearance uponon comeback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def reverse_array(array):
-        array.reverse()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
