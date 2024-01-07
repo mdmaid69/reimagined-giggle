eca26425a91868985548f5e58a2a45289c38bdaa
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:02:14 +0000

    snow swim smile disgusting 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
