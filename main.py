ffbac9cca4ba45fbe29fb68e8807ac86d1814dd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:39:51 +0000

    score cut defeat harm splash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-print([x**2 for x in range(10)])
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
