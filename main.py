e9ce3a09ab5bbbd8f82476251009a6ebfe4e57ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:48:00 +0000

    list her square hope wing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
