90f06442b31ba9d44e96e5a534e5bc08d2e93111
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:39:36 +0000

    roll row account exit exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
