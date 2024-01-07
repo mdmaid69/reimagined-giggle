6e0567d9d20a79577576fa51b91963b7f1a82d3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:18:20 +0000

    something right will spoil search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
