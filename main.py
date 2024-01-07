0d1e7eb9feaa085b7e375cdf27069e23c0c8472e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:13 +0000

    awake broken will second gradually

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
