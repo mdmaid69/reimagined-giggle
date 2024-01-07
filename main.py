0f805b946dfc7f3d9a0253f3071ab9778fa2a644
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:19:07 +0000

    press bath behind organization mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
