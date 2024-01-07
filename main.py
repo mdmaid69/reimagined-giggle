39368fc4d6d0fded48b024d5b1fbd0fe58184aef
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:00:11 +0000

    long cold read makeinto obtain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import random
-print(random.randint(0, 100))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
