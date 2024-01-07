ca4004d36652c96fd2215fcbe177d5fa6e24c7d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:30:39 +0000

    please comefrom opposite emphasis regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
