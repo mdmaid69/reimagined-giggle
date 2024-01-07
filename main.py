c2335ca46ac6b230253c6991e38a460f939f46b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:37:46 +0000

    pretend feel protect tend stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
