7c52bcd8296b857db8625964ee2ba081e877bfbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:38 +0000

    Oo software violence bury past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def cube_number(x):
-        return x**3
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
