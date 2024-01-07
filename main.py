cb3009bfc6af5c553206dddfb7763effd2d63eb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:11:36 +0000

    turnover education force further nurse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
