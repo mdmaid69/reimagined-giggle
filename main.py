7c1ba3f33feb00eac65e6164cb92c835cc00da13
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:11 +0000

    sure better order lie convinced

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
