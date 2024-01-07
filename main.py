8483f1a0e6395019f42239a0b015fc81a5d45446
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:11:19 +0000

    week position strike me stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
