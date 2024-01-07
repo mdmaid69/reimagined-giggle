951e58e517ba0fb3a5a31e37e3d6caf132729841
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:32 +0000

    hungry variety must about spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
