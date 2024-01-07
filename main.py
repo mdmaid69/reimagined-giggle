7ac671c6730e83d2dc4915d0affe54400fbb950c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:57 +0000

    neither light suck limit city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
