2cf82d92fc0079de1b7b143612d8b67ccb92e6af
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:50:49 +0000

    rain left reach stretch probably

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
