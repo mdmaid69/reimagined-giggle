8fe88861172a4384ff575699bdefaf63323f7e04
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:41:17 +0000

    fast authority turninto she exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
