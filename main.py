5caa1707e1e3fc869bcaa0f715a68b275428167f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:08 +0000

    fortunate rare load prevent slide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
