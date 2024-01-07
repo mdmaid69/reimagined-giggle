143e1007ce5b7e5d77077898d4496228989d8da3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:02:01 +0000

    contain that influence workon backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
