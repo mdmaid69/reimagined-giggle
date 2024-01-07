5a82c6f6d753a62c860789ec057152727b6895a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:13 +0000

    bunch fill close happen unhappy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_repr(array):
+        return repr(array)
