bfe9eefd2fd6815aeab8a1774d55f1937e395499
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:31:15 +0000

    anyone hurt rarely scrap beat

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-print([x**2 for x in range(10)])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
