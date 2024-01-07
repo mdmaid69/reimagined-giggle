dab7cc3e9ff12fc373d73b5ed0e31b81b72984ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:53 +0000

    water wind appeal push main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
