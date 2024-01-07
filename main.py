dfd9120d91df33ed30664cd3f561a04f74cdf546
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:04:13 +0000

    clear influence gradually minimum blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
