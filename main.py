afffde3ca0c7532469a608b7346b60fb326609b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:47 +0000

    freeze careful team know priority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import collections
+def create_user_string():
+        return collections.UserString()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
