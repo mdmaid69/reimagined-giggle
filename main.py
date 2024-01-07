ba9c6dbe59304d8ea811ff91ecaf5f6833a1bf7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:15:04 +0000

    unit other really board science

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
