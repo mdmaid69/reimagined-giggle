4acdd12f7d292c787723800212552c739a118613
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:20 +0000

    beautiful normal total arrangement wash

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
