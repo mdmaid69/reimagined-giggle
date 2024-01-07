e130836eac6d5188b833ab3d205942614afc28fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:28:54 +0000

    birth wrap quit frequent cross

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
