e092e41160707167c08bd97106b910fd6276d7c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:40:45 +0000

    mixture across question boat desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def subtract_numbers(x, y):
+        return x - y
