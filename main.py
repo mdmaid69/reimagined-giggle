a84161360d362aa12284cc1915b64cfe15cfc75e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:06 +0000

    mother muscle attitude press damage

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
