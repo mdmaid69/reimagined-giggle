a2d14a633e4c0b3b32a8fef0d78856b110d840de
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:03:43 +0000

    bridge 179bite pocket police keepoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
