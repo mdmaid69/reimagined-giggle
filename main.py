fc22812deb4273a448d631b14eec3066cb36f629
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:16:01 +0000

    act pollution snow represent statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
