00e8a75f43e1b38e8a3af28c8d43e17981ec5672
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:19 +0000

    collect door cheap store nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
