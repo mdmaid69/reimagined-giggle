3d8e4410b8787b40ee4648afa68f64a46b168bea
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:06:03 +0000

    interest popular feel ear spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
