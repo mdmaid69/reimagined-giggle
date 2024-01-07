736780388423be47efc625ae60f32e69b65850ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:47 +0000

    love tape force move against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import platform
-def get_os_info():
-        return platform.uname()
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
