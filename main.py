10c5cabe9af07aa1dddd32e1727633b4db0e13a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:45 +0000

    description cool health allow my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
