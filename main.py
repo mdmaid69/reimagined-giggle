d8fc5601b6f34169fbd65b12da86f6d35be5e29b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:16:43 +0000

    win part eye demand compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
