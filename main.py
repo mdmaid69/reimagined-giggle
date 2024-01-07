8a4ac6c4fd0fa3cc05743655300e7394288b3585
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:07:18 +0000

    thirsty avoid control flat summer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
