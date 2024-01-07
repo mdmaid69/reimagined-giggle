cafef02770e2c253e95b567509e2aa8d063f93ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:54:21 +0000

    simple traffic score become intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
