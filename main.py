f95f7945074e290671bf896cc5f740dd0801e731
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:00 +0000

    value sorry last fair thanks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
