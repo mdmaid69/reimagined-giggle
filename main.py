75721dcc338c8f284bb111eb9b3820300077b177
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:56 +0000

    unpleasant definite so through pronunciation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
