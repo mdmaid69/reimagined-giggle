64618f9bdd5733820375a58fc4535b52f356ce51
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:07 +0000

    mess escape song total past

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
