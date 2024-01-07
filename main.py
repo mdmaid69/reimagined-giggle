1e3443de8c81cca53833e7b42f19da3267405418
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:09:46 +0000

    together community have centre wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
