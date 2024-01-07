a8410d87aa6fd8b8af1af078ca98e46161ba34b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:18:09 +0000

    shrink Ff terrible breakinto what

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
