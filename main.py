1486974362396e9a36c514cdf649d1a136f483fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:14:13 +0000

    rely behaviour Ll exactly once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
