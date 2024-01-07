18174e1d5eaf5d058f6ed3428239a63d593ea158
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:15:34 +0000

    calmly underneath legal completely hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-for i in range(10): print(i)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
