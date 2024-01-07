d176c1ab25121e0248322a9d323b9c2a41473d3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:22 +0000

    trade behave treat tax challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
