6779dafcd86ed3e455ce4b1d265c42bca5e87387
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:06:11 +0000

    government ordinary set joke cheap

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import random
+print(random.randint(0, 100))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
