5eb81b59ad1104770cec68727dc20953380d1b89
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:16 +0000

    also protection runout than film

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+print("Hello, world!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
