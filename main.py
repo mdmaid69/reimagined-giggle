06ac030e066f975b2c17c8ae50fbb37c3afbd30b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:20 +0000

    task speak watch healthy day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
