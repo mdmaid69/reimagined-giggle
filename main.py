21aada49bd61a804dff9e3ca4fcc91396e53ab9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:35 +0000

    female dry left ask giveaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        print(sys.version)
