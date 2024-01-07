3a137c24f177c546b3a427f3982b09231b549af4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:19 +0000

    watch tidy state mistake fall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
