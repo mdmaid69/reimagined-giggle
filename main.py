554e8af0b24b364bd4a4c9dc079d8c4ba74aab8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:28 +0000

    push few undo invite if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
