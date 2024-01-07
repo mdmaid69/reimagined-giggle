ec2dc9ed1f04a583e37913c9bc10fa10c33c1cd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:34 +0000

    shock arrangement extremely rubbish distance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
