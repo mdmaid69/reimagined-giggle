5557a5d8c6ed3698ae38b6c3f70d70f6b0c1c105
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:23:41 +0000

    moment prefer public lost grand

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import tensorflow as tf
+print(tf.__version__)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
