6c1707ae4744043e1bf626a499109f37900205c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:57 +0000

    shut female thanks its still

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
