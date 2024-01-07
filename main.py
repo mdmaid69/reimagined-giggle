f5ebd381113c4e499ac5683d50cb5bd437d21bb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:03:25 +0000

    exciting along send quantity describe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def clear_array(array):
+        array *= 0
