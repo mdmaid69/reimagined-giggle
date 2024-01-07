dddb3e4fed032e355cf14e1eb56076b79a6b067c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:01 +0000

    mother none tidy pair tread

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
