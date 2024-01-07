94f22fd6eaa68b12d0100bc0199365b79e0a1cb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:29 +0000

    appearance reach fast loose makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
