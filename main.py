40177cdc7043e4a73b3a82e3dda62cee73d6bc47
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:08 +0000

    perhaps extra getaway uponon stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
