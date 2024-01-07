f93fe17bbd325df7ced6f1584bb02c1518f772e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:24:03 +0000

    substance workout production whenever television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
