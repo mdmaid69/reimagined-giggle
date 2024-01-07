9cd27e99a7f28da576850a8958d1f460dfc03d38
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:40 +0000

    compete model constantly sweep occasion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  def count_elements(lst):
+        return len(lst)
