0e02be61c3d812e3cd48f212b1e5b9199bc15647
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:14 +0000

    combination use comment see original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
