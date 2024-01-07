9ab23cc71639f733851d9d9465fc47c6afc06b03
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:23 +0000

    least impatient certainly well beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
