9cd2d067ecb75a50ca80c8e03072b5736237f00f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:29:57 +0000

    anybody produce usual to either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
