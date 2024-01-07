e9cf61207732610693c34dbc47a44878871505c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:02:21 +0000

    new noisy better general butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
