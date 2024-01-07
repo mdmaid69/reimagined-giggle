667724bf368362dffa305dad18b36abf863dc1db
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:55:19 +0000

    comeover print disappear may tidy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def sort_list(lst):
+        return sorted(lst)
+def count_elements(lst):
+        return len(lst)
