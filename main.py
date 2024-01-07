451dd1133c9a683a1119eb8dc9b07f61f2280d58
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:53:26 +0000

    makeinto nothing wave inquire lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
