d150c047aaa5e88e589461e52ee6d05f76ea1da5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:38 +0000

    key occasional vanish anything turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
