e8efc98905bdf30e6841b08d08e9600f6c6d12c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:55:35 +0000

    old taste pull bridge entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def rename_file(old_name, new_name):
         os.rename(old_name, new_name)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
