8c700b26588dc781047ffc3e0836cf45be088ba2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:47:30 +0000

    down path imagination maintain skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
