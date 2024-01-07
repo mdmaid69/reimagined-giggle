fc851df8049f5a18cc8d64257ae949e2df110d19
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:55:47 +0000

    square information train other education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+        return os.path.getsize(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
