485142933537284e2f7c5e525ec07a6acb28761c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:55 +0000

    free unkind can image shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
