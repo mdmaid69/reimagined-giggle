251b2507ed3a776651e4f7c4a4853f92cd6f3b6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:11 +0000

    comeback judge welcome pencil fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
