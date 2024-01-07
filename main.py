f660ced01e64c49203a1072e72c1f963aa132a86
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:03 +0000

    rest weekend bury further base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
