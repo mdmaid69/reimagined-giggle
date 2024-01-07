8637715ac28e835ff0e0017c4feea004dc317ec6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:35 +0000

    solve share Jj damage close

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
