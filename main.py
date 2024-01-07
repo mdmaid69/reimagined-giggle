2a965678f6880e54745353c2abd3821353569f6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:09:34 +0000

    situation male defeat possibly advice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def remove_duplicates(lst):
+        return list(set(lst))
+def cube_number(x):
+        return x**3
