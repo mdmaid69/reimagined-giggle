72adee324e8b67d54be0cbe345eda47b2bbcf681
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:56:51 +0000

    bath wait somewhere behave talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
