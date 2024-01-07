cfe7430a9d5ad2c5b2c312a3e42825f8fd072556
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:40:20 +0000

    best him development interfere left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
