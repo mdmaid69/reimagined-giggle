a161c2387473af7789159170261b0325fba16c19
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:11:01 +0000

    week abroad recognize concerning careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
