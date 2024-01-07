7b0d9366dd525d2aac7d0f908633ecfb87614b00
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:45 +0000

    fact you certain takeback laugh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_today_date():
+        return datetime.date.today()
