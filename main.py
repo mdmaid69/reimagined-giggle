3b1f505a1fd3c2d2226cc6a43d368da4bb17d15e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:09 +0000

    machine than act gas welcome

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
