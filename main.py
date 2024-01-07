8c0a234c46fb79d76430c8d7125b0254e34fc907
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:57:26 +0000

    high rush sport her plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
