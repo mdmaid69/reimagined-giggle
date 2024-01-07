f544fd748b9adc99948c50728c8eace21e7537aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:44 +0000

    dark gift real lower straight

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def sort_list(lst):
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def sort_list(lst):
         return sorted(lst)
