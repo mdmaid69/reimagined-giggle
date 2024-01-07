43f28658b4d4cc7b8fd1cf173eeedb4957bde37b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:06:07 +0000

    limit expensive each main another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
