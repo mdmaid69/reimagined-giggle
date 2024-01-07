21a4b6f726dae94dd4ed22c21876a03c7228d1f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:31 +0000

    rarely race lookafter target appear

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
