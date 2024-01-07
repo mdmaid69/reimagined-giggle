81d4baa34fff10a3d023a33d33a9669dcf8ea140
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:06 +0000

    hers capital no pipe guess

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def multiply_numbers(x, y):
+        return x * y
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
