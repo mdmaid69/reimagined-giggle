b53f1d4ec3f6f22793945d1f9d3bed22bb687dd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:19 +0000

    matter declare pause instant increase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
