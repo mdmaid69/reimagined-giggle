b97ecff07d9c08e141c0be250d44c9b829d7092b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:59 +0000

    order ability declaration mine lookup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
