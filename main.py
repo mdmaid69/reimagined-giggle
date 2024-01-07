987f88eaa17df9702b4bb974cef54501f0c31487
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:28:25 +0000

    exciting left 569enjoy fine fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def count_elements(lst):
+        return len(lst)
