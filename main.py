d5224c90e16dcdae8643c36dbbe185085cd2be15
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:57 +0000

    disgusting confidence occasion hurt dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
