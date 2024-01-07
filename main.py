7dc41f48ceec2b798c99bf7838d40241245209bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:39 +0000

    give receive phone prove edge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
