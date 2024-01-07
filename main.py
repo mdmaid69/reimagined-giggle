6e13e897465b2fa5f0a133c1e7e06138b4003b9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:54 +0000

    blame impression guess mess pot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
