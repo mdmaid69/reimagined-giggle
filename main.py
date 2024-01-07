859d5f0f26e849b97a4b8d620842fc1b70bb094c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:56:39 +0000

    together nervous young hide drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sys
+def exit_program():
+        sys.exit()
