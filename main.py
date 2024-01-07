da0a373cf7e89bfc55321981a0ad1b291852577a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:14:16 +0000

    splash wish pick relax shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
