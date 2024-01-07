adb9b1d243197eb7869e264eb97e15c1cda54b1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:42 +0000

    track explanation special sort copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
