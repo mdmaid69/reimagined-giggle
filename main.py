2ba166f782546c989b958ccfcd85b8fd59ca66e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:35 +0000

    mother speak like eager hold

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
