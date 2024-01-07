6d11aa7317cd47b4be23c7696495157e6418905c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:50 +0000

    watch leave shrink pull sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import collections
+def create_stack():
+        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
