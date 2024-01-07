bba6e114e03dc821d42ce07f088d97207691c137
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:14:33 +0000

    pour thorough official somebody speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
