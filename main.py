34deb1ae39bdf5520f5712393c5144b599477504
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:42 +0000

    strike greeting influence direct godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
