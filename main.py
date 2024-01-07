5c8f35c10a29c7e89e54f32ff3d6e29d9f43f006
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:54 +0000

    side tooth stamp 179bite spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
