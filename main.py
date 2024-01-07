f25e8111e6bd2788c32f6fe06c06f6584253090a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:55 +0000

    drop care continue tie judge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
