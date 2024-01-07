30fb96d3ff40fdeda1dedbf404d49abf517a17a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:47:09 +0000

    influence throw black scene travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
