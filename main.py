590b3cf069afc5d1078bf0ac445f4c401b3ea98e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:23 +0000

    dream development goup subject rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def find_min(numbers):
+        return min(numbers)
