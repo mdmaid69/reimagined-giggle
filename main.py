7020b6710cfd954eb1f4af3d83146e6e2d03b7dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:40 +0000

    warm arrival ocean cope pinch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
