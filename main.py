5919db60e99c06629508b3c0c19ecce663ff417a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:26 +0000

    fire afternoon compare ring shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
