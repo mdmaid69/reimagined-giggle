b4219f347d60873f856266427a8f43f04f8a0b3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:47 +0000

    surprise explain carry science nerve

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def divide_numbers(x, y):
-        return x / y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
