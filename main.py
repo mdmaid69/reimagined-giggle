c42f3ccb5d5624cb9e942b0c903e52d88ed24c31
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:02:09 +0000

    good takeout Nn trick favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
