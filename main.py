3ebd346b886534984f93bb04e215e083377161bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:26:58 +0000

    back act over when hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def reverse_list(lst):
+        return lst[::-1]
