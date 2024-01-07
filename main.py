bb0a1e19b2b5ea0ba6537c4b43d1e5cffee0f599
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:55 +0000

    left which form mine slice

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
