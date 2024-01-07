54e86a28aaf84628c268839529b9b9f73fd414f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:19 +0000

    worried away refer realize bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import random
+print(random.randint(0, 100))
