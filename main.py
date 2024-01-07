2f3baf8ef2f346ac60989529bd989b2bd1add4ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:58:49 +0000

    somehow suit put embarrassed schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
