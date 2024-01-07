a07983b70b3df2440c2de97f89683c9224ca3db4
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:12:33 +0000

    plan Ll act morning stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def multiply_numbers(x, y):
+        return x * y
