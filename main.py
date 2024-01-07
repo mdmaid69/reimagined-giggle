bc804398deaf7fe33a5e614ad49eb2e2d567f76c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:37 +0000

    their each history healthy pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Characters:", len(text))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
