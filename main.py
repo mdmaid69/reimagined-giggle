edeb2460741ce823e971ac8b47fa10ee21299aa9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:24:18 +0000

    club Oo food Rr seem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def is_even(n):
+        return n % 2 == 0
