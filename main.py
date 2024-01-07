416a296836109d6fe9b930f4b4a1a72b4e3558cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:59 +0000

    someone start around ring float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import os
 def list_files_in_directory(path):
         return os.listdir(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
