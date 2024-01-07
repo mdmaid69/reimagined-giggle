6202e2b9aece86940d20636645ec91d9eda14bbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:49:38 +0000

    wish education age shallow pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
