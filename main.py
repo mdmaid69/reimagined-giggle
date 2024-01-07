32858c7725ba17b0a730cb5fae5d04dc60587b0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:47:25 +0000

    attach thick pause safe convinced

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_cosine(x):
+        return math.cos(x)
