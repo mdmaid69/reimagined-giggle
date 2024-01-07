ed2fd6f3707f668112697b835a48811ca18511b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:24 +0000

    average unable club quiet connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
   def get_file_access_time(file_name):
         return os.path.getatime(file_name)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
