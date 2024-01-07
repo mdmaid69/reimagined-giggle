040afbd8312b67c1304c19e6a8bfc4e1d876126f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:55:12 +0000

    switch lead apply age believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
