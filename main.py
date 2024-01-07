09c71f3657fae31c16e0845661a972f3c84c409a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:15:13 +0000

    responsible goon health bread on

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
