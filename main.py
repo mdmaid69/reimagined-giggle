0fbf1e9bd2d32bae0bf579c5291216f9f859d912
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:28 +0000

    press meal artificial task typical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
