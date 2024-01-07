1a129a4aa77038915479b5e841408785c1f94d70
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:39 +0000

    interested branch physical warning fence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
