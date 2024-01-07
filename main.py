54e76a0b1a3d6e91558b396feca98ded9d3991f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:10 +0000

    reduction sort round wing broad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
