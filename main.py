b86fe7f74383de191ce0d814cc2d33fbadb2ddb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:27:35 +0000

    water luck date pick press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
