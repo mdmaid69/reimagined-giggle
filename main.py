2eef214555e3170114b25c677df39812f5a9f38e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:25 +0000

    mine unusual hang form of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
