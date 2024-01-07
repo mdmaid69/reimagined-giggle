31b4af7414ee8b6faf4d1b8475c11946adbbeec1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:43 +0000

    after far politics female law

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
