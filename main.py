a6c9715710446a72582599e4cb1c4eaad10b7ae8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:20 +0000

    brush flat someone switch allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
