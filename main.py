d3ebec66bc359377d76d78a93a91b0b556de8a82
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:27 +0000

    achieve may run grade brown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
