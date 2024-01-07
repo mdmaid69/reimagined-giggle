e283260bdf20d9b16976649a83b464fd6e70e6c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:38:34 +0000

    past niggaacademy love cheap plate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
