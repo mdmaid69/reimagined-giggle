79f6add925f7dae002708eda7f4d5d7381122209
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:27:10 +0000

    make favour today job problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def count_elements(lst):
+        return len(lst)
