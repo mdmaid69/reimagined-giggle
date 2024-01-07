8b9b6aed0b6040ad559074e74a0980c69f6e43b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:39:28 +0000

    thinkthrough represent region shoulder off

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import sys
+def exit_program():
+        sys.exit()
