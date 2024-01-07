a4e25530bba79013eeb39da128969f2f532dfedf
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:09 +0000

    leave destroy promise moral Dd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
