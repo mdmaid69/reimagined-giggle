9ed07a415d3e2fa57f35bb221922094496daf061
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:05:09 +0000

    score over whole admit comeover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
