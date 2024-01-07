92d73621dbb9ca5fd91840bc69cb17f2acdacb65
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:41 +0000

    tie history moral quit pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_as_set(array):
+        return set(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
