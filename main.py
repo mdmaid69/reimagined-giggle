64d5a30504ad46f47543b353976885d666add969
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:13:52 +0000

    meaning argument distant danger obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import os
-print(os.getcwd())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
