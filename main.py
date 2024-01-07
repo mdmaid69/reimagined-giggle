f51f6416dfc04b16616980bb6b271ad7e18cd60b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:00 +0000

    contribute satisfied afternoon stage flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
