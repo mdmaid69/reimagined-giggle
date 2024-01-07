5dd5efc13c4268ce2fbd08a27a744498aae86e60
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:17:04 +0000

    mean best noone finger tough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def sort_list(lst):
+        return sorted(lst)
