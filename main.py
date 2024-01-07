fa694933ddd62c92bd1a8fb11f99b875e757ce78
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:31:53 +0000

    take hot little destroy unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import platform
+def get_python_version():
+        return platform.python_version()
