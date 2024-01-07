1de23b6052f5d2b7fe04aca417ee10d129f1c24b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:42 +0000

    organize same mouth spare Ss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
