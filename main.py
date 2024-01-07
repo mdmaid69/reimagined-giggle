e62614a43d78b109e52af4bd4a85c4a28729adff
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:30 +0000

    upset really willing replace himself

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def divide_numbers(x, y):
-        return x / y
+print("Hello, world!")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
