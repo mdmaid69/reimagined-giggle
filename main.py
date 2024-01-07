362fed20ed499b969e723abb9b29aebdea2b8c63
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:02 +0000

    still port unable wide beautiful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def reverse_array(array):
+        array.reverse()
