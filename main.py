18bd4a3ca2a47b5f3b982a574eb4b0c0501ab91e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:42 +0000

    custom ordinary of because fight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
