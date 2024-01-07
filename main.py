67f932bbb69103d3039a2696db261b8732362301
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:39 +0000

    solve occasional sense unfair force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
