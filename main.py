559017fc9154dbe10e6fd04281cf27c90bf1889b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:50 +0000

    justice eat follow away switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
