ef056a458ebb317b96bccdb1943ffd72bfb9bb84
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:12 +0000

    straight key admire tax offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
