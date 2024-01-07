406eee1119b1567ab699a44f0260f3f724887bc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:50 +0000

    loose simple my twist area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
