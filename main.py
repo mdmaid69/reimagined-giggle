0c52c83eb81e99c4c354c5f8fbe9c3b6a04ef10e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:37 +0000

    recently attach pocket impatient demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
