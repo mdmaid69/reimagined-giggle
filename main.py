264afb4c132ecb09abafe0cdc5f2f261763d393f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:25:57 +0000

    store description unhappy concern study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
