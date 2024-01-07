34ee27868d21c879bbe0870dda0820c809b82989
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:17 +0000

    whenever play big few up

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
