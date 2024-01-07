18a2a6fc3e144353a28519b5f93cc4d045800d14
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:08:02 +0000

    will break police inside land

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
