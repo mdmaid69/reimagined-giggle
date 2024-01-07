a0bac0ec68efeb517e6d7d721d34d8e128f7cea4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:59:50 +0000

    terrible bang Kk sink season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_max(lst):
+        return max(lst)
