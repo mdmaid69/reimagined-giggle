c051d8701793ae6411e7d6c18fdb811ae1d3c27f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:31 +0000

    contribute famous moon call heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_volume(length, width, height):
-        return length * width * height
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
