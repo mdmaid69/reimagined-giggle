7d9243c2995c3b1af05a3d0c6caa6831a2050828
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:37 +0000

    tragedy formal gradually form collect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
