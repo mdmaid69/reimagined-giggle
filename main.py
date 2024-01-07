1d9287497a897c7eee4352577ab5ca4ac6c43d1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:23:15 +0000

    rain immediate comeoff cry example

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+print(sum(range(10)))
