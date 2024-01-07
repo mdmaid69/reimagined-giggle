f70004fa428035d361d783ce159d41bb707a027b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:39:20 +0000

    wire press distant call reaction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
