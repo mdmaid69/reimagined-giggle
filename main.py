b56947a6489c12accb10f8498d33314ba95d729c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:51:48 +0000

    direct person would practice block

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
