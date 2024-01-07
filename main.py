65f4aa361ff2cc142c6d11c06e9efeb7db9e7998
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:51:00 +0000

    danger day tidy hard close

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
