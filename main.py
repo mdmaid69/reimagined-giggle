316ccd3078b5e91b49748b609bcb6077e9f3b36d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:30 +0000

    necessary before Ss help once

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import os
+print(os.getcwd())
