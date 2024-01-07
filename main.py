7aa8b1a7d8a408078ba55b7ce18c1ddc171cf4af
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:18 +0000

    dish afraid hard 570enjoyable such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
