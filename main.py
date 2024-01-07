da0e26b57068c81569415157fbfe91e1f3dd49e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:50 +0000

    why second transparent ordinary brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
