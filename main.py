c51267473d40fd36e05b2726869b0aa4b2d64a63
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:15:08 +0000

    wet prefer begin sore combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
