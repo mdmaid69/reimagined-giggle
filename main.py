1c2049b98554f77671c7ecfd9ed13b062191f8d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:56:07 +0000

    in spring practise afraid disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
