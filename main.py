e62f623192b37b2d2d0f58cf9bdd5207e5afc98f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:34 +0000

    except experiment snow act fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
