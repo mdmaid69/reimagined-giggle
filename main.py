bb8b65287d6ebdefaeca7a3c113d1310e41ad132
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:33 +0000

    keen memory rely Dd introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
