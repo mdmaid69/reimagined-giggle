fd4212695d8f53ad89d3362d0d8954ccab9d1844
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:46 +0000

    respect preparation responsible safety themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
