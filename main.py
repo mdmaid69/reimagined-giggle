9efd5dd23f6544951c5cc0affd36c0d4f151d34a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:03 +0000

    Uu occasion keepoff nervous win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
