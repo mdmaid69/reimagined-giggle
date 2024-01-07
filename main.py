ad628595269a73605b0475522be4044824d895f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:17 +0000

    commit push spray answer evidence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def convert_to_octal(n):
+        return oct(n)
