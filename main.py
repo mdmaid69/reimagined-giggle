8dfb15f105b344a3e28c6477f1bd0fc577264446
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:44:22 +0000

    crowd suddenly thorough keen rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def calculate_area_circle(r):
+        return 3.14 * r**2
