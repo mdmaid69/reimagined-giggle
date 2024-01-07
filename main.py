f10a39b429a0716a1f82541d4cc225ea9114ee88
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:20 +0000

    demand go getout available common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
