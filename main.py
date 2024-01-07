495c96ad13142dbc107e92c024541806d025b832
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:04:34 +0000

    raw will maybe ashamed label

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import logging
+def log_message(message):
+        logging.info(message)
