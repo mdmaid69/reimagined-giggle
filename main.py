7dda7dd521083e8cf906b23fd093041b7cabd02c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:35 +0000

    hair possibly result left team

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
