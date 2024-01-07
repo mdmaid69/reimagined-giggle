cca1db00a01de7ba3dde998c1b3ff240b8950da3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:44:33 +0000

    black connect when blame anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
