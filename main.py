12f9604e1491129b7eaa0883a07ec95729deb94e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:23:29 +0000

    grand underneath please genuine argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import sys
+def exit_program():
+        sys.exit()
