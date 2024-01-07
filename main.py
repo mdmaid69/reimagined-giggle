90f7b1e6098f8242ee7a12e44e6a6d406d323fe1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:35 +0000

    little joke temperature later upwards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
