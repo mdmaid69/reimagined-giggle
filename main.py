f74256cb0f486396be8e20d1e32fa29ffc7bc734
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:57:00 +0000

    result circle repeat clearly fire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
