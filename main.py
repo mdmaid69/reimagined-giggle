804a16294a28719c36a4eb216c04c1d14ea2ebc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:33:38 +0000

    seat why error Cc workout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
