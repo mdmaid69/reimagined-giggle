f46d28d350874ffb55b0d276c91fbd4e1015eea4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:25:37 +0000

    not pour involve surface cut

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
