85496d23071aa3e9abf40def4aed69a4bc626a5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:28:06 +0000

    light place forever pipe otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
