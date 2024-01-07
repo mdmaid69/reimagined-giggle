c8e0c00bebe0905c423f8200f48e8d7140493993
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:16 +0000

    store will right sad matter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
