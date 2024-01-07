4a1c9561730e57e3c21a1e19c24faba5a889c3c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:13 +0000

    dress through fold moon teach

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def cube_number(x):
-        return x**3
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
