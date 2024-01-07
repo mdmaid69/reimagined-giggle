39d3fd42a80a813f3e58830136016bfa63b137ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:10 +0000

    tooth shout aim grow bridge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def multiply_numbers(x, y):
+        return x * y
