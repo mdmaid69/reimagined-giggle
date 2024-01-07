c5505bf4269e3478cc27f06a41171916147a229e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:40:01 +0000

    turnaway female behaviour inform cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_item(array, i):
+        return array[i]
