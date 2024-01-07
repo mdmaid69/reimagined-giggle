81b9902a171464462ab4396cfa15a4958a0594f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:09:40 +0000

    ashamed platform floor before fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
