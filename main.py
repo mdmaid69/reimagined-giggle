1c8f5c6ef2ba1bc1919421697d5644209e0124ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:53 +0000

    turn fail once desperate boil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
