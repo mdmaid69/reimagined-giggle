8344447b3dbd5746786f662bc2f509545de85291
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:14 +0000

    offence tomorrow telephone worth cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def reverse_array(array):
+        array.reverse()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
