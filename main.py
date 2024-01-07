48e88f1abcc16398c290fb971f1397707392fe4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:47:25 +0000

    getoff store smoke exit gooff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_array_as_int(array):
+        return int(array[0])
