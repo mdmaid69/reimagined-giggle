ab82403ef4c1b4a744ce6c5d81daae756ba0a908
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:01:04 +0000

    down deliberately gentle write vote

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
