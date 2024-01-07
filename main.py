b9285e414bbd2d687341efead2f0d95315973c39
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:09 +0000

    pride respect not wipe fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
