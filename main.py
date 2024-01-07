b9d76daca31bb0d0b7664099301c034dfa950227
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:34 +0000

    work estimate male runinto sister

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
