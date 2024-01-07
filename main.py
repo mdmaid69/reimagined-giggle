b152f2eed24f8c3c1081bbf06673cb47f652855c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:35 +0000

    giveout winter 565end occasional command

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
