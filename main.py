c9afeed8e37049a0c2e750c1784b227f378a28b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:00:08 +0000

    let head provide exploration neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
