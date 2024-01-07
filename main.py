20a8ccd92acbda2d89a94e6a844f64640ea8bdd3
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:08:04 +0000

    awake myself relative shrink bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
