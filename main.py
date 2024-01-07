42cec067f0ab1e9d2864abe1f70bf0db85c8746e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:46:51 +0000

    slice send quiet thick careless

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
