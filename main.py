1425a67e7f012510a87cd4f98ce7d476527ba9d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:50:12 +0000

    lead rain sell face note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
