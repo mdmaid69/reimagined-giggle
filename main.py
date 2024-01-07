714527e9456bf04d59a4c48cf289b57d2fe3342b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:27 +0000

    mad dish artificial moon equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
