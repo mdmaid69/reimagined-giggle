3067a87637da8861355271115ba44e293d25541b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:35:31 +0000

    theirs safe responsibility pencil visit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
