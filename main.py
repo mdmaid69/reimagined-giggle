64429014740a42b0f932e28afd4a12b6922eeef0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:14:21 +0000

    national realize gooff false sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_user_dict():
-        return collections.UserDict()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
