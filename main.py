44a4eecb5281fe91c42ba2b581551f4392e33ab6
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:38 +0000

    firm sensible fire goout its

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
