9c9ee567fb4185c84abfa7a9a45ca96a5b7ad4a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:24:56 +0000

    appeal rid opponent present fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
