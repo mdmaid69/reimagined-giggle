b7985f8fc54f17698b09e34102e6a1f69427a3a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:40 +0000

    begin payment problem event cry

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+for i in range(10): print(i)
