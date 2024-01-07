8a8dd7588690acf0b4fff7029ceba0c8440b04f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:08 +0000

    kill finally why bright trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
