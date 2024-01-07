32779fbfcc72f4264203bf0cf1f179a4c6e0e1f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:34:47 +0000

    skin Itself push cut organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
