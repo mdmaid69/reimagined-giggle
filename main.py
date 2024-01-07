d4462f1485f5ac6246b34d7affcc58f0f66db8bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:43 +0000

    key mad relief mother rain

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
