3054375069b1529d1039f8508731a2c20b17ee6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:01 +0000

    underneath sad coffee success decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
