38ef040bdf17441c602919547457c553e832de69
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:04:26 +0000

    cold bang container news discover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print(sum(range(10)))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
