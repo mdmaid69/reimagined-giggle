dbc338ccd50a8f7ff1d52ac49ad2ec49e7bc27d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:36:28 +0000

    will copy situation punish pride

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_item(array, i):
+        return array[i]
