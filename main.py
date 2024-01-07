968d28a204ad203f2837c74e8e413a7794226e86
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:24 +0000

    old treat advanced lonely getup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
