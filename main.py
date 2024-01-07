0049ed3b623c43e53fa17d0903d1c3d6c3dec506
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:27:54 +0000

    close mix separate herself bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
