5383f92a3e0f9f0d8b0aed6e535073c190dee467
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:52:10 +0000

    pretty tax dinner during rank

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
