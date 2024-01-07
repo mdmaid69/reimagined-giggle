5075e283d945372bafa49305aa9bd82a8d529999
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:47:11 +0000

    available team could doctor smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
