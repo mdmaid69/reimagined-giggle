bc69a2a97b2a7750333d734dd141dc2c911bba3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:15:49 +0000

    brown Yy month delight needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
