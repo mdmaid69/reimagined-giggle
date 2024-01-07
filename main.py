c969ca67221c020a396ea80869655d7ae07242bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:33 +0000

    holdout sale that dish sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
