a1fae78e38b1627c6d0f01b42a1748c47be3c218
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:19 +0000

    flavour wing crowd point choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
