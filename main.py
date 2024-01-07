c7c1f7f8dc3c6d969aedc3349c8e711bdce2c4f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:43 +0000

    snow situation commit danger gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
