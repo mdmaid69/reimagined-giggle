f0c2bd114affc8e6d50119da74013346d28203b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:27:46 +0000

    excuse speech police talkback plate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
