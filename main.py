28e51ee9426fc2437d6d8913676cf6213fc4bb96
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:54:57 +0000

    worth reach land occur describe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
