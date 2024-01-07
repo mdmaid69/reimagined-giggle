85974cff1a78c67f114c706a33dc8c7259e93bba
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:16 +0000

    pile port snow workon calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
