a697ccccc4ba00f673df52fa5e2c82215b94d0f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:27:25 +0000

    laugh record protect method container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def square_number(x):
-        return x**2
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
