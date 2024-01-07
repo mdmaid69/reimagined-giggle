50278a9a25c97cee27ec8dffec9e8d5d139d093d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:38 +0000

    571enough opposite treat child snow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
