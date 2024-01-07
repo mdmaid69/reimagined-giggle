3ae2c1a6ebbd7a9e98806ef118e495ca9382f451
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:41:57 +0000

    he ball comfortable everything knock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
