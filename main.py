674d370d71a5484be1b1c87dae6a80c4ddb771fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:28 +0000

    drink sick last example familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def reverse_list(lst):
+        return lst[::-1]
