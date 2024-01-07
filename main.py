a82cbec80ef358bf8a2bc04ac5f6b8f6ed9098e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:53:50 +0000

    steady tragedy trust conclusion rescue

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import math
 def calculate_permutations(n, k):
         return math.perm(n, k)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def extend_array(array, iterable):
+        array.extend(iterable)
