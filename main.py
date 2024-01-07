01622eb615e2cdd16dd61b64f4a916f881d2d587
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:00 +0000

    man after Pp reduce boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
