9d51666c957ff431a99217be6ab0614820d1fd53
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:46 +0000

    Pp ring threaten manner lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def get_array_itemsize(array):
+        return array.itemsize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
