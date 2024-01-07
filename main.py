777fc9ebfc405daa4406c7c23018a184b49c735a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:41 +0000

    deserve branch field damp within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
