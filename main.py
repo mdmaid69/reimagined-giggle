b97bcdfdd13c27d79825d1e34f316aa01f2f2320
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:51 +0000

    below backwards television comeround doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
