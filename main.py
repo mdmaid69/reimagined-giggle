40156f5a842604fb025c299de7a8c86cf2bb6489
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:22:53 +0000

    pile history politics they action

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+print("Hello, world!")
+def count_characters(sentence):
+        return len(sentence)
