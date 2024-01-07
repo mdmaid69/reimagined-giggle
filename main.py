89284b29f190e7ba2803851a8316f149d134d985
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:20 +0000

    persuade cut expect read battery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
