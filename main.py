c0fd84afc3797b458beef22c51a947a67a95adfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:01 +0000

    artificial quantity find Ll means

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def square_number(x):
+        return x**2
+  def sort_list(lst):
+        return sorted(lst)
