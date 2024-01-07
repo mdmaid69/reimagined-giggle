ea2577940ff333c22639895e4556cccf4d4d8c35
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:24:40 +0000

    reaction else extraordinary raw manage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_work(force, distance):
+        return force * distance
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
