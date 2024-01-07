56e6d9a9c08a6b900f2c329239a06b1ee379ff03
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:52 +0000

    rate correct defend sale kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def roll_die():
+        return random.randint(1, 6)
