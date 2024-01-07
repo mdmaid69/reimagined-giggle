bb75d3c39f5ee3f9c08ed25dcd1e8d57e1b1a6b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:34 +0000

    usually stick relation miss important

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
