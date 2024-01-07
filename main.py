e76f6497fc4a2bb3450360646ea6b26eb28a8b37
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:25 +0000

    side hair school attitude police

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
