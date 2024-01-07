84c0551012cb588769c373e0b62e181f79fa8d34
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:03 +0000

    male shop of they amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  def convert_to_octal(n):
+        return oct(n)
