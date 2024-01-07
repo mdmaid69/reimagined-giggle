04fcba768bccafb4787e5acb1e550dad32ece6ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:48:17 +0000

    attempt tall label central cross

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def get_array_as_int(array):
+        return int(array[0])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
