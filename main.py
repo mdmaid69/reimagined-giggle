ce94562c1db05f7f9ee0cab3fd27bb04ace73191
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:21:29 +0000

    plastic condition community 569enjoy language

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
