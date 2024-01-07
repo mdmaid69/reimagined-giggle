859822281d4bb45be1fd70a453280f09f8f07930
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:21:47 +0000

    legal strength fork comeover comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
