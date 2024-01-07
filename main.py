5af4044fc117be2cead92fd54cc4cf5b4114bfdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:24:15 +0000

    urgent means pipe general duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_volume(length, width, height):
+        return length * width * height
