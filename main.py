088b5c4b43c352df1116072f56bf71636c72976c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:46 +0000

    unite small furniture amount beginning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
