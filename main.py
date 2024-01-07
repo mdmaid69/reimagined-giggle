cc7e395216d2b9c3fcd000bce534cfc9e7116393
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:45:16 +0000

    turninto purpose long do yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
