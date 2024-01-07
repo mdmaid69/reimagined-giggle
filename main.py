ed5bb7fafe20783f23d96a1ac4c56dcbfce481fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:48:58 +0000

    moral door announce wait positive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
