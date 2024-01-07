5ba441c2b92aafbdd1a13600e2edee33271bb3d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:33 +0000

    confidence attract react activity whatever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
