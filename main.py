c8377168031a657a8000095275a3929a4cb68b86
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:29:17 +0000

    object wonder pay class rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
+import time
+print(time.time())
