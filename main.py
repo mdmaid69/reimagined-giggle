891939a313c08a33bb74e287fa3364df54cd4138
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:34:13 +0000

    guess share tonight through whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
