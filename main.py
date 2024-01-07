66ab52411d69e8ae056df981de12969c721ea354
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:57 +0000

    intend shout tidy story move

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
