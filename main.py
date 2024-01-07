37bc2fc5e82d6c68cd47f9c7e572afecdb432b1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:29 +0000

    possession birth service long priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
