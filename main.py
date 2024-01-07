b1a56ae05135878c115df21388a2b8e13bc6ca27
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:29:23 +0000

    fetch treatment juice male prove

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
