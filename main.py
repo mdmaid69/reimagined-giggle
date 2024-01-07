26d18e2d336e4b74202478a4587d3d77ef819956
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:06:31 +0000

    sound that season Kk leave

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
