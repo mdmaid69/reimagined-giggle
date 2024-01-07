8fbb1639e4e2761c418b29a4c9c65dd9739a7b8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:54 +0000

    empty passenger matter back better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
