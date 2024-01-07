682ebef47b93213bb39dffc7001fdff3c67e1c89
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:59 +0000

    something programme objection have trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import random
-print(random.randint(0, 100))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
