13430c5bf23e1d1ae6917cbd917e0f7f838419c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:56:56 +0000

    false blue teach string deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
