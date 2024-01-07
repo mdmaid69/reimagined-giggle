125f6a258072f7dbc1168623e0bb60531ab8c415
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:35:18 +0000

    wait hurry couple develop odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
