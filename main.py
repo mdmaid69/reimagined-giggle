dbef99eb28f78da9636cadaaa167557975723f49
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:21 +0000

    medium heat cum afraid mind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
