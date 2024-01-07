f7bafec4ddd197bf77e459eaf61a51e7b5ca4e80
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:36 +0000

    popular than century tear beyond

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def convert_to_octal(n):
+        return oct(n)
