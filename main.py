1fd4009312bf205d37ebffb37fc15111c8e83b5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:03:57 +0000

    gooff embarrassing beyond 565end quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
