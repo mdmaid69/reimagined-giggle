5843dcef0ef313a5825da69ea20a336545eb554d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:48:56 +0000

    break stone value reject rise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+print(sum(range(10)))
