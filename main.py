4dc2c8699c593b11cb062412abfd15bd1887d55f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:57 +0000

    achieve 564encourage store have stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
