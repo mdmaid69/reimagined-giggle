cf26323fbb3ca8ed9a1ebb1b8da5543ad29dfc85
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:13 +0000

    no ready fasten seat body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
+import time
+def get_time_since_epoch():
+        return time.time()
