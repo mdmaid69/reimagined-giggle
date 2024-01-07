7bd50ee28964d13b00d83b2ae9eb19081a3c76e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:43:41 +0000

    die strength foreign container fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-x = 10
-y = 20
-print("Sum:", x + y)
+import tensorflow as tf
+print(tf.__version__)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
