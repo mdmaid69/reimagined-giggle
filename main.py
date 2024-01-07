58f24e7a696023527b4d858ccfd44b3412f92f14
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:01:34 +0000

    so outside brown blind slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print(math.pi)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
