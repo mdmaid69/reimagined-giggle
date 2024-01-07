2d1efdaf9f52d5bffc148ebec4bbad539ac2ce2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:27:14 +0000

    interest force back sweet Nn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
