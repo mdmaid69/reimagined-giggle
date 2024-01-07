201b7590de173f7d88d11395244922595f7614d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:38 +0000

    maximum risk lookback(on) contact upwards

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def subtract_numbers(x, y):
-        return x - y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
