92f7791066442c1d32d936ff1edd4e1ef72882cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:30 +0000

    smooth top so ill giveup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
