27859e5a44b7744e97d026b23ae1f5be8b396dfc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:42 +0000

    partner among run famous large

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
