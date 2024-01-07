ae5b3a113013ae0b288cfd3f4ca2126989af39d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:25:02 +0000

    mysterious worth holdup advice pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-print(random.randint(0, 100))
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_max(lst):
+        return max(lst)
