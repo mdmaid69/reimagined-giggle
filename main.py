21f503417a93a0874bc1e4a21d1cd3546cdc728e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:44 +0000

    run reserve neither clearly hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
