c3f569f752dfd55fb009d2542fb0a5e3dba9d83a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:58:20 +0000

    increase fold detail exactly little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_str(array):
-        return str(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import datetime
+def get_current_date():
+        return datetime.date.today()
