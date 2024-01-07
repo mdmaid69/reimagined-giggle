c02721ef19d86d04d329b3148ad18cfd6acde3a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:35:26 +0000

    invite kill before typical injury

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
