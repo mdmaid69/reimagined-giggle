0ec1e1872675cc9615cba6f838d34f410588c7bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:28 +0000

    brave female healthy ordinary slow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
