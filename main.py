a4da76756edb90ccc3880b8787cc05b21360805f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:02 +0000

    long support minor speak but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
