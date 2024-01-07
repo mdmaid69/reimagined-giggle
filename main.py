c74bed154429c0ccda65972189311dd6e171287d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:23 +0000

    own whom interview file bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
