f2c3912df3f9da3b3bdd7120e6009ae5d68eab9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:07:17 +0000

    expression touch look play old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
