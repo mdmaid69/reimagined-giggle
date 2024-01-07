c8f1d476e8699213b970eec8f760e1de38f10dde
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:18 +0000

    along suck apparently pause together

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import numpy as np
+print(np.array([1, 2, 3]))
+for i in range(5):
+        print(i)
