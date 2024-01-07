7be0a56f2ed84b502b4b1b0d5c75d863026da9b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:16 +0000

    section one although so yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def subtract_numbers(x, y):
+        return x - y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
