fc24699ecb6c5b25016c4bfe342aee04a89a0e3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:01:05 +0000

    breakup success traffic raw method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
