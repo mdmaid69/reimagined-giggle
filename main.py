49569053a084f8844cccae1948b7588030149f93
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:23:04 +0000

    violence rescue runout impatient slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
