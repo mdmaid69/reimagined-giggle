1942e71d534d86343c77eab04a359c2707292948
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:09 +0000

    sweet brush neither smooth shop

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
