4fe057be0524ccb9e0b064716bcf64c6a2fb5874
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:19:43 +0000

    angry science takeout everywhere whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
