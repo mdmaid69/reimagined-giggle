eb34f51430c10c367dd8ff9e899ada45119c94b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:52:12 +0000

    too test answer because region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
