3dc58f7e3de60e1e78583d0753d87cb72afd659a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:33 +0000

    open last just grow real

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print([x**2 for x in range(10)])
