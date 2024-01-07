fb7021d90dddde1fb6a85a415bd8f1b8015b713b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:00:16 +0000

    taste private sing build sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import sys
+def exit_program():
+        sys.exit()
