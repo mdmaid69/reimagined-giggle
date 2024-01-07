0b39a1d694419d234567df07e6a4b334450e2399
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:25:16 +0000

    dare second organization write ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
