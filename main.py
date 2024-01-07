f3d93ff5d761f7ef6d677ca974695e1eef70b88d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:56 +0000

    practical interview science store world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
