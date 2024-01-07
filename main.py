52066131c5681ef0cf254d06fecbae80d0e7928a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:32 +0000

    with afternoon string convince impression

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
