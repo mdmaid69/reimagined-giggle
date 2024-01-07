64e716ffbc84883edb9543d5dea671b0249fbe48
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:18 +0000

    plate loose valuable expect think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
