e5ca1cbac65ce23ea2ee67de914348acd2899251
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:44:42 +0000

    state late story book square

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def reverse_list(lst):
+        return lst[::-1]
+def reverse_string(s):
+        return s[::-1]
