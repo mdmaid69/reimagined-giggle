adf390ee9d99b0303354f2c74953fcf1e6f7bec4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:56:43 +0000

    morning long programme upstairs shrink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
