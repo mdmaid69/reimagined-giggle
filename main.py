b9481b1ce744ce382cdf6eeee8966905b5f82e7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:10 +0000

    past figure moon borrow light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
