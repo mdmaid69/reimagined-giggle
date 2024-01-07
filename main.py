49c6a8575bb17b1b0c8c6d723aa4d39ee0e77676
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:26 +0000

    read weekend healthy everything stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
