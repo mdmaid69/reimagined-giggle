db1f868b815e2363d53eb58295fc93ea3b2f09fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:43:32 +0000

    floor tidy welcome language goodbye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
