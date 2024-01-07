6050bb08d92d6334626a1171bb16fa9328fb3757
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:43:26 +0000

    scare protest determined slow treat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
