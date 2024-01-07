932ed737b55a5768f651c3ee450aba61013d4401
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:03 +0000

    risk ought truth read willing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
