2222ae188060e8c176b9df5642e18f2d96b2f589
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:53 +0000

    side miss win goback(to) beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+name = "Python"
+print("Hello,", name)
