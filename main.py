bbf839b72fda11a25bb2d89586db830a4a06b881
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:01:10 +0000

    prove offer stretch brave gas

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import math
+print(math.pi)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import random
-print(random.randint(0, 100))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
