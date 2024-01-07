4d0081cb5f0b431d43f8b584ea844a121c1b1c24
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:15 +0000

    believe thank constantly connected wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def add_numbers(x, y):
-        return x + y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
