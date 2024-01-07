dd286cfee72c00633f2c859ff7fdf18f1767b34d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:45 +0000

    shop string safe boring disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
