02f3f8fef4844646bfe26ab7297855dc71ade153
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:13 +0000

    achieve rub vision important busy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
