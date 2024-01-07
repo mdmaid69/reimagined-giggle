54ebed7d0945f995c533ff7461e395ba08a78f7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:28:38 +0000

    imagine standard part pride positive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_volume(length, width, height):
+        return length * width * height
+import tensorflow as tf
+print(tf.__version__)
