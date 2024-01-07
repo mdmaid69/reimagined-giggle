d0c8dbf3c06ef0eed24100adf4355241852d44b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:44 +0000

    choose innocent wind thank suddenly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_unique_words(sentence):
+        return set(sentence.split())
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
