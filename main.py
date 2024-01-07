a7f2ed1ac23b87f94942347dcbf50b0f4832dd45
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:11 +0000

    rare book shop imagination sit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def square_number(x):
-        return x**2
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
