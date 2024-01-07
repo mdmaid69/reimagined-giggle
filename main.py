3286a41835a4044b6f2543e33df392f257062f60
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:32 +0000

    bored perfect nothing where object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
