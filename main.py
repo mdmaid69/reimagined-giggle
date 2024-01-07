e3eec952ba91bd370819e7d21b4c3aa8f41cf81b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:30:18 +0000

    any character rich reason everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
