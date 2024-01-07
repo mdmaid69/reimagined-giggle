b9096a6b85fe887148fb85504eaf9416c65a3b2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:00:00 +0000

    careful either cruel part hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
