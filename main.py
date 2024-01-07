952def5fb9a6b4fe13ee05053936aefa324d219f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:21 +0000

    performance holdup furniture perfect height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-print(math.pi)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
