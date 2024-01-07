4e373639cea508e052fc57db5992b2a57d7fb95d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:49:42 +0000

    that day pass combination string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
