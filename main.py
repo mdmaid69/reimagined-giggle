f0bd35b63ef6a304a041ba2b67ebb83821d70845
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:57:20 +0000

    179bite rain night impatient him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_floor(x):
+        return math.floor(x)
