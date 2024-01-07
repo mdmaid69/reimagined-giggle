9e14cc150c65e5d8ea0a57b74c084f351aada105
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:47 +0000

    defend custom wear show win

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
