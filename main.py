4bc057b3256b52549725e67319e0f2eaab8321ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:24:17 +0000

    interview hurry wing most exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
