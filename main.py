2294d29a966eb6d81a66239ccba6f98489df62ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:49 +0000

    sort behind succeed direction lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
