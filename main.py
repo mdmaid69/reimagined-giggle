bebde6d0467dc71c5d47913764b0f546c44d856d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:32 +0000

    statement tip use song plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
