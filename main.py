ad8695a476cbe88e064b11a9c131816ff73e9b7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:56 +0000

    suit happy trap Jj plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
