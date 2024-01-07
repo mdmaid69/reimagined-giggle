67d0677b4d839f4e2f930a30ee73e6628a834051
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:43:28 +0000

    lucky spill Ii place switch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def cube_number(x):
+        return x**3
