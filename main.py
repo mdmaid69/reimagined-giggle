ca8bd8e20e64c6207bb2416ec60fa3af2ef4d951
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:47 +0000

    all theirs wave cost level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
