41a63bbf8794ce36f8ef4fad8d1b9b5b519b6e64
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:09 +0000

    talkback job likely make tired

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
