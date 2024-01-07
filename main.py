2739d3a7d3f260049c5ec2ce0ac86157178e5496
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:03 +0000

    normal tight sudden activity force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
