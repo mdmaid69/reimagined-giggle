cad7ceacc764fc3706dc74849c7f40d8c52c8092
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:28 +0000

    worst fashion rich whole swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
