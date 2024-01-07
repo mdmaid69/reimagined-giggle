f0365e163834cddd2c88d0181fcc3142101df3a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:08 +0000

    absolutely worse miss ticket personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
