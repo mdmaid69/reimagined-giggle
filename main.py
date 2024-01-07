1e1fca9b12d46216254976b0719e102b784ad456
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:10 +0000

    same learn what spin lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
