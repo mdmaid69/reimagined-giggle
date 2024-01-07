fe8d1f159cc5d67062e72a4236462a75ae99c882
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:05 +0000

    butter value dry tell immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
