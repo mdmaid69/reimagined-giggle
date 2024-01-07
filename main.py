9472ce85d1decfe7d5629f09a49fde2782844282
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:17 +0000

    suddenly branch branch along runover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
