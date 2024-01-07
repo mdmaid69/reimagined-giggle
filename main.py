e2dd81762e88a2e548247eaf070cfadbd81a0728
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:50 +0000

    ago lookaround any truth draw

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
