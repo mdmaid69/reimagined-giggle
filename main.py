e8722a697068e1e12eaf1f3f46f6094f8eb05391
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:10 +0000

    hurry sting attract nose later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-def cube_number(x):
-        return x**3
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
