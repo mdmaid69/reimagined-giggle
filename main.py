897bbe50cd1de5a934ced608aacdecac78ac9b23
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:24 +0000

    sound total this wall unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
