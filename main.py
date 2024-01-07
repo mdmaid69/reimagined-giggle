f7a3336d7bda8d24f3eb7792f683f89158d368a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:32 +0000

    complete again steal welcome busy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
