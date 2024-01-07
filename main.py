9633a215c157de096c277cbc8ec2f4162038de6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:56 +0000

    shape needle emergency lock violence

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_power(work, time):
+        return work / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
