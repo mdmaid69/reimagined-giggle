acfe527131d27122d9e0e115d2f5b3036047b5e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:45:24 +0000

    pretty minimum bend prove rain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
