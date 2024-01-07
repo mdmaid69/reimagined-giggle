5271c13db0aa41424cf3f2624a43198104323fb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:09:07 +0000

    bring middle priority quick drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
