1014b7ec860c544290505b9223ed7bf191c50e17
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:59 +0000

    under rate more comein arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
