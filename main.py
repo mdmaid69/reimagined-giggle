8ac92296a062747a86485a97592136b039a089f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:09 +0000

    contrast service approve tall weigh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
