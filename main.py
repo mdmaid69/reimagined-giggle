528bb5836ffc24f623674f953a1a60b85f6fa84e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:49:07 +0000

    murder spread suffer just geton

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import tensorflow as tf
+print(tf.__version__)
