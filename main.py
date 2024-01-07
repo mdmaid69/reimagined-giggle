65cb4cfdb928c43c5633387ba24fbc907257b20a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:16 +0000

    quiet rather drop warn choice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
