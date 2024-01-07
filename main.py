deab2ad32ec5ea7da279e02f05d5cf80b3da1511
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:27:14 +0000

    present serious length who considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
