401a703093f43e1ae7baafb53ca813dfff5e88d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:13 +0000

    threaten important spring interruption lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
