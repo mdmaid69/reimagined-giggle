16e5a878b732d6dd8af8278d4debecb850eeb229
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:50 +0000

    begin disturbing bury grand upset

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
