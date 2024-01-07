474040e7baeefc1ae70ba460e42a0fae8747b89e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:56:59 +0000

    bit exercise during handle dance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
