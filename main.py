f479a1e4b27557335721b1ba28bd1a345016e371
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:34 +0000

    ever noisy fair beat hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import datetime
+print(datetime.datetime.now())
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
