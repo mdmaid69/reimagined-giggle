f8b02234d6d43fd511fc0310178a5212e8d3823b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:14 +0000

    doubt cheap wide answer skin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
