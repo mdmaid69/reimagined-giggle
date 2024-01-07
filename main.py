7c388007e254a256e3ec138585ff3f5196f013d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:56 +0000

    hole wall male match supply

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
