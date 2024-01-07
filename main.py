6e2ecb9cc958e5dbf59f06aac6a2bb36f28adfaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:28:20 +0000

    arrival finger live cry phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+print("Hello, world!")
