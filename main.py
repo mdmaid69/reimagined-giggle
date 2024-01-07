5d68b5fd68ea8e93405b86cf62db87843b1e10d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:05 +0000

    wire whoever continue phone capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
