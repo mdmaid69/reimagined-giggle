5ac2383eb9e249fe0a24a44d2cb1eef965844645
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:56:28 +0000

    closely mass leave confuse smile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
