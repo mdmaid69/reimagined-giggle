ab24a84f0b789261e8d663952209eddeb2fea845
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:24 +0000

    gate giveaway health takeback consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
