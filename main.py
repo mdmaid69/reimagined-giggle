7ebfc243d4b2094bce5ac0f8c955955955081023
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:21 +0000

    fact everywhere joke sudden knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def change_working_directory(path):
+        os.chdir(path)
