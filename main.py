f45b38b47a0f1125138521d96a95aa03a0c8f8e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:11:36 +0000

    crime minor suggest quickly secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
