62573674266c2353e0a24ce5c5c87fd27ec81bd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:16:25 +0000

    swimming transparent hair decide tie

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_density(mass, volume):
+        return mass / volume
