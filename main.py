c567781432691ad2aecd8252031c03794ea5fc36
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:57:07 +0000

    benefit conclusion task burn middle

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import tensorflow as tf
-print(tf.__version__)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_area(radius):
+        return 3.14 * radius * radius
