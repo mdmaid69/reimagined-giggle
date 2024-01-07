7d141be976d0b5842e8220df981e1560e0e00abc
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:43:29 +0000

    promise pocket sleep wherever art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_density(mass, volume):
+        return mass / volume
