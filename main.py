02e8c29a3b14b89ecaf10874a495abaa75f09b47
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:31:38 +0000

    joke replace vision weak after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
