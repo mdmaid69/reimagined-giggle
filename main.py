a1ae574d3bf4cb411e30fb1c7b7fb61d612d7bb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:45 +0000

    temperature language frightened goback(to) machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
