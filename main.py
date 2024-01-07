7045bfdd8df02a5c775416869b3739fa9b21dd84
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:25:56 +0000

    quarter consider brush dull opinion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
