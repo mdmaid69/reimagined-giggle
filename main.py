e699738e632e8a3fe76e32d4ff6de3a0a8d7c166
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:36:45 +0000

    side however pile order holdon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
