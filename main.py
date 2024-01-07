0f8683305815a18b5e121e66bcf6b57b838231c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:33 +0000

    taste flight wind emotion star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
