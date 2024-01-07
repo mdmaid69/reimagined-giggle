9f4abed8c4437fe4d745b75757cb8cbddb878873
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:30 +0000

    pity rub invite big comefrom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
