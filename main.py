b3876162a050108df6421bc047a6debe51978703
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:43 +0000

    congratulations winter form funny off

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
