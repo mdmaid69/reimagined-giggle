bf2f80f7222093bcf3841836e3013cb54c9be9b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:37:59 +0000

    bored with flow complaint character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
