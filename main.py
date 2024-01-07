fc5f632bd4333f48c27992faabe4b9fe32703bee
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:36 +0000

    know goback(to) wall comefrom inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
