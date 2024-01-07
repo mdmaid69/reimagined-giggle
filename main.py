5c244fd60860f4909f7a7ecf2366a72677643c2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:02:01 +0000

    day rate fairly try result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
