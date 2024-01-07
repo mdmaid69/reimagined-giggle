3b65b08c1f86e8ec40b02799c8d69e79b49133de
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:33 +0000

    stretch unite minor poison damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
