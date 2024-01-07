bfcd014462c14605b2432bf646b96b42843d6f4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:31 +0000

    mix later detail off age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
