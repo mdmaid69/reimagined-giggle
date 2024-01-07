9075551f0b4a9e40ecfbc00bd5ded89c29dd73de
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:52 +0000

    explain turnon excuse near attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
