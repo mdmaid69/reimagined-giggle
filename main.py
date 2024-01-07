4324a943a7039314133374caf382f339429499c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:22 +0000

    hit sleep match profit produce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def convert_to_binary(n):
+        return bin(n)
