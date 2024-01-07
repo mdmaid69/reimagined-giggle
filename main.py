fd63012eafe2bbbb7b0502e1b430e32d25cfd316
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:23:24 +0000

    dance priority tap 569enjoy top

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
