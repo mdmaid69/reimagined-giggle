0686c7690ae2bad90ff9bd5246deb0b792b65a1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:24 +0000

    below foot positive never discovery

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
