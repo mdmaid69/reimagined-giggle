4e5074b01c61b1607af6fe7cf630a50bf5312367
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:27:31 +0000

    ticket off fond remember expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
