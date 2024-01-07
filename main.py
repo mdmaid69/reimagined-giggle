586e55185a155ca0dc4e24cd426a21b1cbe98f2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:19:41 +0000

    accept model tip odd runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
