eb331b316b82e62b88af6bf4f55a41e3d5a2d734
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:10 +0000

    at Dd prove shoot normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
