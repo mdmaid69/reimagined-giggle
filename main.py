acafceadd47f1c4b1fcb62a68122d5ae1dc6228a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:10 +0000

    hard bang press task closely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
