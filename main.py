02be3acc48fe65c200a8c878f831519d94e862d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:03 +0000

    rank explain rely confidence deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
