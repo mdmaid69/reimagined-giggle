55763635af6e84086774d53d55827df08db7e4ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:27:27 +0000

    genuine basically radio sure public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
