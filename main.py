72ca4e31987c8162c8eb0e92205c23af86b44625
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:19:59 +0000

    week curious through later Ww

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
