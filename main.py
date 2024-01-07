20ff38a058cd85d4d949546941936146543d553d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:57:52 +0000

    swallow ignore true the promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
