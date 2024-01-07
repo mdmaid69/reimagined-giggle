162647a4a66109b9527ff9c0bf614ce3c9beb16e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:51 +0000

    bored advise cheque cut shoulder

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import sys
+def exit_program():
+        sys.exit()
