09523bd7ddcecf6733a58e275f5c9806ee5f21e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:53 +0000

    contrast back recently report report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
