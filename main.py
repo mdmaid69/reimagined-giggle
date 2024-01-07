a3d7739ff1a3be5859aaec2e74e50f32e415664d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:50:19 +0000

    holdup support main breakdown university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
