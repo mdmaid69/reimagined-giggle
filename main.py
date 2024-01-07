7ec5725c0468b21eb965f86c7c212f87e5e7a8ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:45 +0000

    mix bring admit turn just

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
