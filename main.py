d6f06a82440c513727b155df89f9c11222cb1b17
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:55 +0000

    moral cinema should thumb declaration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
