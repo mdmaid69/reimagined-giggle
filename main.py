93515ea272621b82ee1ad33095d794097cc9a878
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:34 +0000

    probably gap kill talkback dig

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
