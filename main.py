9652a0a18bc316628b754b6bc9d9f06d23e6fb19
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:30:52 +0000

    thick probably rate entrance finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
