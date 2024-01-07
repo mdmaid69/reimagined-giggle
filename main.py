4a869aa2bb2c512022fc0bb9558255d46cf9cb49
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:38:05 +0000

    read squash short leg any

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def find_min(lst):
-        return min(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
