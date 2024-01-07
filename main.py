7e1dcba2d7e125c2354f364565675f9e14d3c731
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:21 +0000

    supply beginning provide young total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_working_directory():
+        return os.getcwd()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
