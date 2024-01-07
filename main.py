a630fee7d7ba24e50bb1662910f1c1fbfc2b69c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:56:09 +0000

    stop scream door dance organization

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+def sort_list(lst):
+        return sorted(lst)
