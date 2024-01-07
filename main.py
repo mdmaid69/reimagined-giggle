dc646ac1eca7033ef669da080b507899997aadd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:05:48 +0000

    front part wander experience world

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
