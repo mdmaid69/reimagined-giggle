fb4b9f1b47824e98cb8e558c6cc39ec536a9a870
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:50 +0000

    look uponon attention attitude door

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
