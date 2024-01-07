2326c80e8fb867cf239355c24fd5ac70a0182e57
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:35:53 +0000

    line keen meeting disappointed fond

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
