fe662bf191ada0144ba8e69ae8c235b4f9cec739
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:01:04 +0000

    wear foot top remain message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
