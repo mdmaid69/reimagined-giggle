0c2b9df5578aeeeef89bdf380c43d605fab26d10
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:35 +0000

    lookafter juice kick state turn

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
