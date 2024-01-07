722162b1e9270fbe0ece690eb0c115567ae59c7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:20:10 +0000

    pleased wrap bread hang shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
