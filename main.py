cdfd9320edca8d0ec92b1e069bbd09cf30a161b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:29:03 +0000

    positive tax deaf prefer our

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
