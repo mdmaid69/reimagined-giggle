5c723a8c558ccb5501d6bbe2c1a9a7f1d8b1ab03
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:13:17 +0000

    circle reply well feed safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_power(work, time):
+        return work / time
