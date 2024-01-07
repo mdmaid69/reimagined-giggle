3e733773c8c813f1c9db83f64287039e47d046d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:54:12 +0000

    act impressive afford himself prefer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
