6cd7ed1a77446b23942667df43c45e4bff6f0b36
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:38:02 +0000

    seem laugh bread suppose reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+print(sum(range(10)))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
