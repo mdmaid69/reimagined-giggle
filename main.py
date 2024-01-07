92b9eed7b127feebc3c39f9fd2fba5e3d6455be6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:14 +0000

    callup less clear report capital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def reverse_string(s):
+        return s[::-1]
