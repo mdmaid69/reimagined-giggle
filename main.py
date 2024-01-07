fe8ee9e24d3ed0f639069c5c5682903a13adbe98
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:05:14 +0000

    arrive culture measure business wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
