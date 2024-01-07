263383fbdeb8f63e6fff2b4c832e0a276161e815
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:38:40 +0000

    range terrible deep window bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import datetime
+print(datetime.datetime.now())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
