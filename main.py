03464b30b1ab3da4ac14f93cbfe57ee32ff122e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:57 +0000

    married maybe advanced punish ground

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-print("Hello, world!")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
