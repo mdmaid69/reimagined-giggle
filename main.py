c246dcc28275a57119d19342d3aac79039fd03b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:25:43 +0000

    equal someone cold jump be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
