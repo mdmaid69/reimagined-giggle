3af882f96e04ec91a3ca54ec6ef453d90474c05c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:16:30 +0000

    maximum display determined demand smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
