565018e0206b9ebca9bbe3acf0cea76a441d8686
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:22:05 +0000

    perform control sister affect earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
