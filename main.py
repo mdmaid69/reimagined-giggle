75e9e5adfcbcf0fc89909d13643e977ebca3380b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:51:53 +0000

    your rubbish count lookaround Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
