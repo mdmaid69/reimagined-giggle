f8c8541912a4ee0ed031a6c58368dfc668fc7296
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:19:27 +0000

    deaf relative miss describe reasonable

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import collections
-def create_user_list():
-        return collections.UserList()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
