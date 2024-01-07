ee6b52980bd192c5f73991dcf48ac81532ebab52
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:28:20 +0000

    welcome form waste match step

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def greet(name):
-        print(f"Hello, {name}!")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
