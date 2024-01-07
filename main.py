30c4ba8d4c7b2a8569358fe4b47e0f05d355f53b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:37 +0000

    country mail kick left slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def sort_list(lst):
-        return sorted(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
