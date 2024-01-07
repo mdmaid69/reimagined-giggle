8dea75e4bbfc56075d8cdba4387fdc0172031e5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:59 +0000

    mistake settle runover several crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
