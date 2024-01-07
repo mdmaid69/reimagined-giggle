e3d5c1463d6100dd9bfbdd64c51ebfcede8e9389
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:05 +0000

    prize report lean continue nothing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
