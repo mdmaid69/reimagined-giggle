0d2d24f0b914a80eb1222dd916c69030df3ce663
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:11 +0000

    consider calmly sorry me human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
