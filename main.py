2277be466ad5a7b29dc3b102863c0bbb9f1711c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:32 +0000

    your fall as follow patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
