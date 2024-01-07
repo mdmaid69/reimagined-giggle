6c90ddcec220d65adb590e7f11948c2f34a0e353
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:48 +0000

    stop somebody feature image average

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
