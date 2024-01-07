0c02ed2be9944455dbaa5efdf4b05b5e8f73795e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:17 +0000

    company crisis trust remain sing

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-print([x**2 for x in range(10)])
+def find_max(lst):
+        return max(lst)
+def subtract_numbers(x, y):
+        return x - y
