c342e0b1200b0aa1551a40bede3a4696c30022e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:25 +0000

    search programme pretty step increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
