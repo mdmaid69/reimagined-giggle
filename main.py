304a3a6f181bcb0a5ffa9dde57a6ddb6f1c4184e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:00 +0000

    part measure lookafter bringforward floor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_area_rectangle(l, w):
-        return l * w
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
