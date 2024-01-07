e4d4630765dd00665eaff81c3b906c041cb755ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:58 +0000

    appear turnout calloff wide head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
