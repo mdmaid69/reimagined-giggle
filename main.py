cd7c1473ea243306104e1390956d82af4fe41e94
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:24 +0000

    ours urgent meanwhile callat of

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
