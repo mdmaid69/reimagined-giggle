d9deed63f657f799721fde2570c3a27e8fe4827f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:22 +0000

    noise shout kick action no

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def calculate_power(work, time):
-        return work / time
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
