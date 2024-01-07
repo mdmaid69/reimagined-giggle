1d12c1000a0cdd711d46fd8c6c61a7959423c3f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:10 +0000

    avoid post book consist authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
+def square_number(x):
+        return x**2
