a8288f6a8f0c02b3c642cefc288e9e33f6163d86
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:49 +0000

    train steep me mind discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
