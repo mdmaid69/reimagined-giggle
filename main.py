3d3aef530958de79136b982f1fe38d36afd7a3b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:24:56 +0000

    date model Nn open wish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
