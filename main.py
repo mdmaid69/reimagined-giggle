9d4630dce332d794f290e9a276dcc4cfce00a51b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:31 +0000

    wide foot cheap happy fortunate

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-for i in range(5):
-        print(i)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
