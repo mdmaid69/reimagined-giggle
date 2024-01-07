267566d52d6733e967d43aadcdc3ac1d226d5f83
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:28:15 +0000

    space load whichever problem load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
