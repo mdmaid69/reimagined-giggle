d7990f79b40ca6fe285214cf032207806e7fa439
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:39:11 +0000

    accommodation harm bill confuse hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def remove_directory(path):
+        os.rmdir(path)
