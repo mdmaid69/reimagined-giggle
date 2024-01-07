c2322f589888891bd74222ac6c13587c484a0e45
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:33:24 +0000

    perform dull represent scared nose

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
