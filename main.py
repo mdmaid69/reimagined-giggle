0333332f4d4053ebb3c18793b820ba6d9adbba5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:10 +0000

    blame emphasis delicate dream pleasure

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
