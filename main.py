809e9c6a5dd24affc0f60096561f32d76679c18f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:30 +0000

    instead change once beat left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
