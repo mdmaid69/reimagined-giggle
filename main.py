5e55092136ad2e02c260363ac392805f72356747
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:40:09 +0000

    uncomfortable worry noise back spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-from collections import Counter
-print(Counter("hello world"))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
