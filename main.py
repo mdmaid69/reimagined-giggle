acdba2eea9bfc9a465e455fdea9977cd3230aac6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:01:18 +0000

    ill underneath true day broken

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_unique_words(sentence):
+        return set(sentence.split())
