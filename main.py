e920b487058127848e1636a8c2d54da93240c1c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:44:29 +0000

    do pipe generally close afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
