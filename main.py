a886b73c7ed6364e58a2a589de4be304d4b18aeb
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:05 +0000

    at breakfast this way measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
