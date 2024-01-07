ac6dd22aabad5d31344b680d34f53b9eca0f3d56
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:54 +0000

    machine squeeze hold hear tape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
