1512d08f06e74b9dea8517016e50ec1d3a981a50
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:00 +0000

    cause wealth would hurt attach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
