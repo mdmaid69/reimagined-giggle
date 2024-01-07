a197c7b7793f56b428bced43fe75c2f41be72950
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:24:23 +0000

    fall destroy quiet protect drive

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
