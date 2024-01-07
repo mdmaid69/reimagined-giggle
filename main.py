4ed617c90c54e6e4f85b1dac9b5961427712f192
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:51 +0000

    she common supply age accordingto

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
