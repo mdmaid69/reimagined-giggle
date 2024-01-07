94850874ec97b78d00c94441602edb43a9192a35
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:38:33 +0000

    limit ours contain path before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
