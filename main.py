0efbff8e27e1f1644fb2b6bacf65072e7955eb71
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:44:38 +0000

    price lonely against receive building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
