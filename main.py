37ee9f27de9786c9699dabb65d4827d364dc1ed7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:53 +0000

    poison relax insist fierce first

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
