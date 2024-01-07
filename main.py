d3f20abf334564cacbc9a2cb42c9aef5b88efe18
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:52:53 +0000

    union children part open Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.ctime()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
