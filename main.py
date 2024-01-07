2610f55ed3916691b19b42bb164a2afcfe077266
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:52 +0000

    closed better fasten Ll shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
