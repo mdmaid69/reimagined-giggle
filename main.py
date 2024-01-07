815736a44ac36c8e1da90234d10bb81a008ca94a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:49 +0000

    notice image excuse runinto flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
