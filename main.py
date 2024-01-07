67a35fc8c5975a11649f92e19af7bb42aaa8fb5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:43:11 +0000

    sudden bend congratulations coffee sweet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
