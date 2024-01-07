e5b76c114fa6894a837cf211ac04afbb2574fbe8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:01 +0000

    closely waste Ss on favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from collections import Counter
-print(Counter("hello world"))
+import sys
+print(sys.version)
+import array
+def get_array_index(array, item):
+        return array.index(item)
