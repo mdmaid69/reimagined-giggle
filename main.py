dc44f51a4173270802d23c2d0795d5f78ae0876a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:08 +0000

    picture tie drop woman broken

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
