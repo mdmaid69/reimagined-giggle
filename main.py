5745fe2ac35cfd1bfbed99f45b2d801bdf68a73b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:35 +0000

    table bread work display accidental

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
