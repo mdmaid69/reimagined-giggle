c12196c1db65f25fd7dc81ee8ee7e7cc3aedaf13
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:53:49 +0000

    wherever display drive adjective deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
