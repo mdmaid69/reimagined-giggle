e28ed45f1ad95fd3a80c8f0e89c3d26f3c87c2ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:51 +0000

    bit who pronounce impressed prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
