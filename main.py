716fe2f1e414b3bcc18ad5ce770d705aeb303a2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:38:52 +0000

    suffer serve introduction can find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
