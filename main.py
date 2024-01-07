fff116fde03c30acac36efcb5cc7d8962f765081
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:17:59 +0000

    gift feature necessary post worry

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
