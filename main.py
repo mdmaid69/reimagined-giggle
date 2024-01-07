eb82b2d7b0ffaaca91d1bfb6ce5b327e9cbe5a3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:39 +0000

    tall whenever enthusiastic always convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
