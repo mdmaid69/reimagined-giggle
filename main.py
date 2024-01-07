40e6b542010bdbd00b6811c8736410aefd541c8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:57 +0000

    nor broad deal performance put

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
