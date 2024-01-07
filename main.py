18a08b10dcd1b63f6bc05f541bb7b38feb076123
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:59 +0000

    somebody typical waste improvement active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+print(json.dumps({"name": "John", "age": 30}))
