6e6287783d1bf47954236828e7af93473f10f61c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:00 +0000

    wish muscle upwards list Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
