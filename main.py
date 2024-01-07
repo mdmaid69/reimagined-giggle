8470f76bb1db2459a8baba49fcc30cd6a65abce1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:27:19 +0000

    pair maintain report skin Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
