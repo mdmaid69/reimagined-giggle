0c72ef30303435541a083284aa7039d45c9ebce3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:15 +0000

    law even anybody book programme

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import re
-print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def is_even(n):
+        return n % 2 == 0
