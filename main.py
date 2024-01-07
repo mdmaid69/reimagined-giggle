256214f2c16a8dc03c6d6a70c9c94ab0769908bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:02 +0000

    among pool happen fresh takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
