16013b673540ecab06882cba2e775aad18d8fa3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:38 +0000

    success cinema great fall hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
