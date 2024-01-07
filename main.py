6b4db36e1b3722b01abbf751e62f70ded383f6ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:38:13 +0000

    getup promise depend nature wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
