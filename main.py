d95c9890ef61d20ff3ee90c0d4f743b98b3f325c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:08 +0000

    turn happy mine course trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
