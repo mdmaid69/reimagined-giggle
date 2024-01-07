af9c7e8f6ec24f313b703e6e475aa5840ac1a252
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:53:00 +0000

    interview lend beginning different her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
