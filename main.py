c6f8afc8357a0655f4cad53fc251e214329a286b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:11 +0000

    turnaway tidy feature whoever regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
