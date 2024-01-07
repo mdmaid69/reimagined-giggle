8f7bd5fe15d1524c54830bb64ef34c966b590139
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:21:43 +0000

    disturb instead challenge care female

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
