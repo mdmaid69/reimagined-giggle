913e066a3c26effa7b6f0622aa22d628e3c40f63
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:42 +0000

    part street year connection neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
