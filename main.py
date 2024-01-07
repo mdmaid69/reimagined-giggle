c1358b9fd1c315be77cc2238a59022b1a9ffed66
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:49 +0000

    trip because brief work trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def change_working_directory(path):
+        os.chdir(path)
