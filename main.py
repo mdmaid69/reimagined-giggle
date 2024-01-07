43d9987eb9d8addad850edf3979a3f6c3cb2f083
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:29:21 +0000

    soft benefit attach turnout perhaps

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
