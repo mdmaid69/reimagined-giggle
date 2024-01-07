465935b9ce7cd1199b458cb1b59f736efc1808b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:32 +0000

    degree Zz calmly lookup concern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_itemsize(array):
+        return array.itemsize
