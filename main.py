fc082e44d8b0f3f56ab5d2c5d9a764bb546760b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:48:34 +0000

    sight feeling failure hang criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
