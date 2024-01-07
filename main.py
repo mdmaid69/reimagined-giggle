2ee80c64ba987de3f1474ee68385a7396243adc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:59 +0000

    sense who police soil quantity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   def sort_list(lst):
         return sorted(lst)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
