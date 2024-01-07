5e4a1f38d0a4cbce98ed4a5c8b3d8e3ea19e9376
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:32:04 +0000

    mainly hang deaf performance blame

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def is_even(n):
+        return n % 2 == 0
