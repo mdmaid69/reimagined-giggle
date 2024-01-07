6dabbd80ba718c857eb98250a487addce6004aeb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:57 +0000

    failure lonely destroy custom plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
