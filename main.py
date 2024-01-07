e3e2818a44e3245b51fe9fa689829fdd9a2e0d7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:55:37 +0000

    neither urgent length anywayanyhow equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Hello, world!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
