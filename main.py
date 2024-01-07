f94e4985c456cc6789178df110604a9607676b0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:26 +0000

    run praise bored poison properly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
