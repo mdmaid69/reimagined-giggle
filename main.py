0c7b58d3201e77389c4a5c9c67d7f32cf9eb2d93
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:38 +0000

    wait apologizeapologise glass speak bringdown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
