7eff46bfcd1deadcdccf3cf79a4a540bbe0c6b80
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:47:15 +0000

    evening hello suggestion pleasure say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
