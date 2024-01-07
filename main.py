6d2d4d72082869599537fcf541b1dc238fba0009
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:03 +0000

    fairly dig be field smart

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
