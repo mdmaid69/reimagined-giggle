c64e63de13f99030880a68b50cf242dc65289a6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:33:52 +0000

    sorry closed offer minimum introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
