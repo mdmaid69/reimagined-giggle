77907f141276b613841f279dc7f75afc5c4ecbb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:33 +0000

    turnaway precise punish trick pure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+i = 0
+while i < 5:
+        print(i)
+        i += 1
