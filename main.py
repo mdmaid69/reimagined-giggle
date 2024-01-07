fd26f5c05c6a003a56e379a5b181f99655fece11
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:28:11 +0000

    single fence ring balance replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
