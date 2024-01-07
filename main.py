56bc4db763f4161ec748ddf9fb62ed7fd544eb1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:40:34 +0000

    failure smart eager stretch luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
