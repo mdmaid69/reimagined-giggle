270fcf555443ac6622d27d0a980f0bd0fd3dac6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:47:54 +0000

    shout please statement yourself happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
