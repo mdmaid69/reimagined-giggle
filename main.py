f3a13b344ccd5061ceffedbbf3bfa0fb9d51ca3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:53 +0000

    back advanced quarter reasonable check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
