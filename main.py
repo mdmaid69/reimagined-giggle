01233f58cb2f0cfaa3f67a80a699e517d5570986
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:36 +0000

    keep write artificial sister expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
