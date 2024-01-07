adea85b51be14547d919fc802bf03777212e92a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:09 +0000

    thorough block past classroom strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def subtract_numbers(x, y):
+        return x - y
