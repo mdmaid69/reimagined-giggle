88d1027e5b4a2bfb186f8f8fd4ce13af21651f6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:01:38 +0000

    wherever better begin agreement attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
