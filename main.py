40f3ba4bfb4bf0afae7f67ad267959b49fcffebb
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:50:16 +0000

    manner event handle hospital whom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
