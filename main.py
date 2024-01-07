43b39c2dde12a5392bafd4f9e200b8bba6da5158
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:00:43 +0000

    favour university conclusion offer hope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
