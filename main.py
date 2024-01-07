4db80959e451bfe15ca7e306299749dea2a3b5ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:09 +0000

    meet discuss group relative temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
