18285a5865bb6313e44ea7126d41a9cf5ed1eb60
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:20:41 +0000

    letter hit sit wide along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
