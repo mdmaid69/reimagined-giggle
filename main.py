a76374cd0d4ba44f4269eac0fa6bfbe3e70ad558
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:07 +0000

    error uniform and branch alone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
