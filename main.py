1c2a29996a71a04930cfa6bcec28a3ae207f437e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:27 +0000

    myself yes lose travel lunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
