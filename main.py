338adbdf65c044f4eccf813325a22ca15526ffa5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:34 +0000

    break reply introduction swim realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
