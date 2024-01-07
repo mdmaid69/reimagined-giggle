7e5ca22f4c46535c6d6468020c3b6c0af24d3aa0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:48 +0000

    influence fold start 565end sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
