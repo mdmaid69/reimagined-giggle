c443181972760bd2d175d95643210afaa31d6f3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:21 +0000

    confidence ball light spare throughout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_error_function(x):
+        return math.erf(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
