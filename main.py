e1f891a3bcff8814aa869b0d0e8612a0a2f0f8b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:21:00 +0000

    laugh deserve Dd ago minimum

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 def find_union(list1, list2):
         return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
