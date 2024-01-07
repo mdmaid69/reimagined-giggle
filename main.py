25b2ed62c1da7f2fb013451a19f062a01fa2ff34
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:17:56 +0000

    better switch complete meaning floor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
