9843d516d3bd5192d3f7b37f6d345f68769862c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:30 +0000

    stamp about happen bend ring

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+for i in range(5):
+        print(i)
