c66341a01000b3931bb09fb0bc7fea54a285a2ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:03 +0000

    bush copy dinner belief murder

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_min(numbers):
+        return min(numbers)
