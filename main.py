f1e387cdd129d191395dec362fe779444f40d3c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:16 +0000

    worse mine create only consist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import time
+print(time.time())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
