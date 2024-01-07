5c188b62960a0c60ce28d55e0e6ab53ffc1efbba
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:55:46 +0000

    become beneath draw report luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
