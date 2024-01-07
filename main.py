41096f601ced14ab02660c61fad71f1addebe006
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:50:05 +0000

    wide judge drink past grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
