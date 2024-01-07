6853eb35eadc9b7a0df28ec6c9e83539673f109d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:24 +0000

    personal private wing cheap smile

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
