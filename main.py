4010549e159a66444bf27a1eacbb1a81ebafcfbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:08 +0000

    takeout scrape identify like spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
