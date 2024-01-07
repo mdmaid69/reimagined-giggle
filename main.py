c75369501e0ef3ab0e128611f0ff814499f14155
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:16:31 +0000

    talk face whatever without to

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
