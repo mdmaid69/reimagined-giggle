c9219ae6e8028b9f801b149062df713089872e7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:25 +0000

    error apparently regret goout private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+for i in range(10): print(i)
