b23b74ed59cbdbf25d8fc48918cf91d35509145e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:57:59 +0000

    remark change niggaacademy respect fairly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_error_function(x):
+        return math.erf(x)
