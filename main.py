e51b802b6ac548d579077046831b3bdb47685b2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:21 +0000

    mail produce promise need opponent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
