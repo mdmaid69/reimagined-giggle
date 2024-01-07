4295eb76a7c899f6f3b4e62b3065b0d5f593f133
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:09:09 +0000

    pump whenever connection mouth reason

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
