f409899cf227ca19de9e0f58ccf25a0aef55b226
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:46 +0000

    emphasis message music cry chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
