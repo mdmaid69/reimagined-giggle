117de5350e9b0c15fdb552a581c101323c9351a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:04:01 +0000

    offer minor comfort fire gift

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
