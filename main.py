fef80e1c968a5cc81d91421b1cb125b57999b062
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:23:20 +0000

    suffer bad impatient confident convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
