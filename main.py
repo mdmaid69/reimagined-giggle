78ece5bd91e49523c2e631231cc341a3a6931703
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:19:36 +0000

    used most great Oo suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
