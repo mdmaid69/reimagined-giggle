f8ebc51a09b3cf1f777b0b72a88387f6314d3f0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:59 +0000

    confusing breakup runinto side turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
