ced19e7a3c2c5292db562cba107136ff3376181e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:03:53 +0000

    crowded false mad boil while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_power(work, time):
+        return work / time
