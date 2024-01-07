c65bdead66b4c99b3749cfc9720a08970deae595
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:33:32 +0000

    in throughout much switch throughout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def convert_to_hex(n):
+        return hex(n)
