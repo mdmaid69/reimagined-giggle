3f273fa60ff55d81dc9e314e0ecc08cb6f6c89ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:25 +0000

    genuine switch mixture book worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
