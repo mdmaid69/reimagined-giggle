002947be4d1bf24357d2845510bf2fcc1f109b3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:50 +0000

    sight soon to scrape demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
