4effc1dde5963683217eae794bb8b70ef6d5c14d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:26 +0000

    draw skin process impressed really

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
