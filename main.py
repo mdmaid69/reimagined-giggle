8d73ff25be46d4a8c021d9003f3be0e2cb3f93aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:58 +0000

    invitation ring rain dead scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
