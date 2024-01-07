9e454ad1be32587ccdde31d003519e6016afec44
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:03 +0000

    affect keepaway exciting form outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_work(force, distance):
+        return force * distance
