c56e19159bd767d2ad2b16fb9fc7c136eee3f584
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:33:14 +0000

    hour down lean arrangement conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
 def set_array_item(array, i, item):
         array[i] = item
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
