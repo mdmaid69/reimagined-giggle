02d62716a78b37cd04be71bbf3241363a8dbfa8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:59:58 +0000

    Hh evidence confident work sell

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
