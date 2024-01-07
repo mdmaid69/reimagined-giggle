799c8a1a3ac73fde5cde14ba49085c7d4df6a3a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:37:43 +0000

    favour empty opposite provide appeal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def sort_list(lst):
+        return sorted(lst)
