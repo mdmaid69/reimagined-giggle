68e746224f261c380985ca6f5a84354e7febc49b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:27:04 +0000

    string disapprove duty earth lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
