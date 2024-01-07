67786b25e6b0f13c42a070f7fc2e245776cc1e13
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:51:17 +0000

    depend realize instrument temper piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
