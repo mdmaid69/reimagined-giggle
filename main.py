0711749a47b64072944f48961eadd57a88e64f1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:51 +0000

    shadow goon excuse recognize control

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
