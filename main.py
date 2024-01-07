41715fc6c8b7d4fe4537fb5f39259dbd30dceed2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:27 +0000

    mean broken cover run moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
