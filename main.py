43fb26a3a36e5ad5a2c5b09fe162aa5a529dc19c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:49 +0000

    medium sign intend turn right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
