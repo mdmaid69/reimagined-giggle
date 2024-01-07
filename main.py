31486da77fad260978c1682b1da602d02f5b79f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:30 +0000

    behind recent difficult deaf track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  def convert_to_binary(n):
+        return bin(n)
