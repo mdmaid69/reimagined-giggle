4df4b3b5284405ec364984dbfab60c32a92db020
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:09 +0000

    exchange sit purpose put manner

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
