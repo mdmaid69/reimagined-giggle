77ab18ecda98f853c5da9351848cb580a93e63c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:05:00 +0000

    music immediately shade callout form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+print(math.pi)
