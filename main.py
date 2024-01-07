d0ef8428ee6c43b4ebe5ac59efeeb6d9c169d284
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:55:04 +0000

    anything ready hard nice care

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
