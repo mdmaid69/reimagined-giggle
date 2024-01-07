4fa68604e40e0c6a99afb74868cb19b3a29d1d60
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:12 +0000

    world when early start field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def reverse_array(array):
-        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_user_string():
+        return collections.UserString()
