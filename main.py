c0805f54cf8e48b8ec3cd960966a321e64481c35
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:09 +0000

    regularly cut please piece call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_elements(lst):
-        return len(lst)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
