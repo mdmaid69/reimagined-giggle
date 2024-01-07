3faf5ddc4237c14f6f52886e73e4f6f75c02b46b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:29 +0000

    collection fine true remember look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
