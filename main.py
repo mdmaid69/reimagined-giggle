d29de5b302539ebb6be567a187740d375b75a20b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:18 +0000

    judge deal probably means generous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
