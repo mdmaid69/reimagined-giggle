56396efb927ad5abe924e267b6639fc1600987f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:14:19 +0000

    compare vision call rush ready

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
