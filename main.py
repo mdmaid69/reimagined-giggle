40bae0cc565e978d5be0d761317fab586ba66e29
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:00 +0000

    shock pass pretend another often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
