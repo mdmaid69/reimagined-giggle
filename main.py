0c0b0e2c8d0f9a15618dca6c9004e743a5572cb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:43:52 +0000

    hospital embarrassed Kk pot justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
