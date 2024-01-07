1f73228323b438e663b749c2a7c3cb12b820c33e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:34 +0000

    dish petrol sweep by bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
