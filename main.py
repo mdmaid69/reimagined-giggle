73abc48bf72e31e4c7b89bc04389ba1f73ab95ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:05 +0000

    blame them dream excuse bringforward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
