4a135518ea4d7bd4b4d88ac347517e2faccdcc8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:15:49 +0000

    remove attempt base price workout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
