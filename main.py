54096cfe295ca85f36b5debe3387f70e2c3df0f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:03:46 +0000

    earth hard another describe Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
