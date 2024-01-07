a08b55a50b60c912f93ed0e185353abad649683c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:02:52 +0000

    trust extremely central smoke mix

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import time
+print(time.time())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
