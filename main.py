3639e8a1032acbef771ea5f3faa5c18f27003fd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:41 +0000

    whisper big simple weigh impossible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
