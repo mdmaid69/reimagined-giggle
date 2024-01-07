0c90e4d08f9740b10d015e096491af8f56e1b0bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:39:10 +0000

    even season argue describe everywhere

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
