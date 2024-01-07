0a559102661e6ad9f40958b3f44c485b6e6248cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:31 +0000

    comment have cut describe range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
