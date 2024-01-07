c2053bbb1864fae0c396fd3f792edf7a385758e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:33 +0000

    impatient sensitive load underneath speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
