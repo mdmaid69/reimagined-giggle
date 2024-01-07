fb6c08056e9c3c9393c9a377e52fccd73a6d4733
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:57:44 +0000

    file throughout line container work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
