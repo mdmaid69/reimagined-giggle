a57ef20c48f2718478514bfeceb17e7f0d92abe8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:28 +0000

    blame comeout couple laugh makeup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_string_from_array(array):
+        return array.tobytes()
