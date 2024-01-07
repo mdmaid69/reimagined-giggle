cd97c013c3fe579912bb2f8ac9934b4bb47dc1df
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:10 +0000

    prevent become poison swing aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
