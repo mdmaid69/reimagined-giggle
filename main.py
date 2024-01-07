13dc00c19eb33e9b1f8aa49c96832d3fb42de773
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:38:28 +0000

    date formal because fear rush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+print(os.getcwd())
