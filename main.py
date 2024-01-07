c1eb7edc1a8113446bb512f1a965d55e328e281f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:58 +0000

    behaviour statement recently region succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
