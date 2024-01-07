33fb11eb32a6073dcc61afcac265d1b91385a28a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:31 +0000

    issue shine lot few fight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def add_numbers(x, y):
+        return x + y
