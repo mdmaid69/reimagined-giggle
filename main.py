90d73fef904827dbaddd165a735061717b5149f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:09 +0000

    copy consist community course worse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+for i in range(10): print(i)
