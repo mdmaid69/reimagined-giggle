2c0279a5cb421cbe302ac99ea0c5f917c9640c62
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:16 +0000

    broad best then laugh communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
