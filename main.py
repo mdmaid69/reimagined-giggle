e45831bf00bd8b439b82f47c2f4fb13a26bec60e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:40 +0000

    motion deep the final Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
