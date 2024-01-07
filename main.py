9c90df42b6ddb39fff33250bd9313e5a2f2f4414
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:31 +0000

    push song improve breakup tread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
