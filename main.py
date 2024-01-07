1bcad8ba7412a5d7135c9d2114b5a5ede7b2dd0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:53 +0000

    offer previous beginning play anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import array
+def get_array_as_float(array):
+        return float(array[0])
