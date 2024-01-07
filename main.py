5ed4ed32586f3ed3a4fc19c07fc2964c602a1cf3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:48 +0000

    failure attempt one result makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
