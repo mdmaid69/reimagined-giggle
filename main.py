aff1a39f03d959fdd477680115e23ce9cfe12f04
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:02 +0000

    lose tidy forever limit night

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
