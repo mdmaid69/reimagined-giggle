289a998ed4558d1fd3a8e824597c8922d6ee9c70
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:34 +0000

    deliver danger goon hospital visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_tuple(array):
+        return tuple(array)
