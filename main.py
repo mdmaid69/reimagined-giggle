3f0184b142ad5e5965f2f239df2a603dd8d131fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:06 +0000

    either wide pretty music noone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
