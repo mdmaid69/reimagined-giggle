ca9b0fdceb926f71ede51c0aa0b8a52e595618e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:20 +0000

    test rely win rank test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def reverse_list(lst):
+        return lst[::-1]
