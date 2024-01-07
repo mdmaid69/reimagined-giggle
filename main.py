047a1133558e80a9edc8d5ffb856e62e37f48ae2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:24:48 +0000

    bar suckme key plant get

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
