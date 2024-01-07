5cad0972689a84895b8e1459e052b4b702c71123
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:38 +0000

    attention back cause sing leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
