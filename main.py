81e1677f68cf829b7d34cfdcaa835d8196c19c9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:43 +0000

    room night turndown goaway square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
