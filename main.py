50aaae4b7b7cb3d95ae412febc222e6fc8bf91b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:42 +0000

    effort further circle cough little

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
