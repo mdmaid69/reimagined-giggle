dd47c45cc984e54a54d7fd80ad700253830107c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:33 +0000

    wander impatient treat poor frequent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
