62df9d35043b6dbae24956bea3bd29a8b953bfbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:21:50 +0000

    tough loss either character no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def sort_list(lst):
+        return sorted(lst)
