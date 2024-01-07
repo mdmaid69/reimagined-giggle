ca4b6d6719a594c98689197f7ffbd9d51d2dc198
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:44 +0000

    test turnout jump shop urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
