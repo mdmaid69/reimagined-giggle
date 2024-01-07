551e000b228774371ccb3461a349c542fb5c0b9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:08:14 +0000

    lay ill detail lunch back

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
