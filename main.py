08b1bf6f874b336a2c41d97f7b4cf4d82e4a965f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:00:26 +0000

    sort child close sea something

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+print(re.match("h.*o", "hello world"))
+def add_numbers(x, y):
+        return x + y
