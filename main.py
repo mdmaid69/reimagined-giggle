6bde5f1987b6561507bd9a454109ee79825c5bbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:55 +0000

    blue fast positive expensive runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
