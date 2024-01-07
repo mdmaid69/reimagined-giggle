9468ae652336916f7867b11056e4e433d06667ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:54:51 +0000

    hardly natural kind transport patience

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
