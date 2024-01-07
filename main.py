6fdf7b0c7515983a0ebdbcb3aabd718d54118347
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:48:58 +0000

    convinced light busy such speech

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-print(sum(range(10)))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
