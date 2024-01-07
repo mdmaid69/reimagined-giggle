c5fcc7bb3b8bdef6e7c576f00b4877d5fe490355
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:19 +0000

    slice usual separate simple stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
