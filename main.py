6b145f124db03a3b97a53e6679f9be678143c608
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:46 +0000

    dream in indeed upstairs plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
