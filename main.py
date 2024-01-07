0a1116e453d77012401f0931f20442a2fa556c9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:59:14 +0000

    count reduction even moment do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
