7239ff110ef0800a83c3a554c2e83599087bfaee
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:43 +0000

    stone subject delicate teach physical

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
