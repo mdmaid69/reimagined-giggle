6c23dd9cc1350aec106c29dbbabf3b5b4adb55f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:30 +0000

    himself extremely affect stop gentle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
