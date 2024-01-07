3efc04c18a66e7b2033d6e0a2d79cb7da93f3199
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:49 +0000

    probably round pen likely thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def convert_to_binary(n):
-        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_array_to_list(array):
+        return array.tolist()
