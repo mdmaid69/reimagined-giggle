d9168d3bab1207e1ab4fc737eb54634f86002e96
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:37 +0000

    release anyone meal thinkover unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import getpass
+def get_username():
+        return getpass.getuser()
