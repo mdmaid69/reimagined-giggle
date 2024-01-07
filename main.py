d70e0aced1a4628b51d87cb1628b78092d5958d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:04:20 +0000

    touch apologizeapologise mind positive Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_variance(arr):
+        return np.var(arr)
