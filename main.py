9f8e2afec8432883cad30249bb6c493e47c81464
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:40 +0000

    opposite affect single division declaration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+print(math.pi)
