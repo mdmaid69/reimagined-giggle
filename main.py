f4178f642df23442c844886ca9497ff515221df9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:17 +0000

    possible target scream especially shelter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def multiply_numbers(x, y):
-        return x * y
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
