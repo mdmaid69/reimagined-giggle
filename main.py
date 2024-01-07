9193163c537d1e346f2e38bc69326a78edc4b8b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:25:17 +0000

    prepare travel glass run stone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_bytes_from_array(array):
+        return array.tobytes()
