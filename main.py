03746a2d74707fa17f2fdc0e6fb9ac23c01d4cc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:13 +0000

    certainly benefit minimum freedom firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
