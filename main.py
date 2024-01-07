588520a0dd5b362e2668d747bc5c7f8210223848
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:05:16 +0000

    usually care storm give vote

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
