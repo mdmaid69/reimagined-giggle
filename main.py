a73bc9b53450c7a074be6c94d849a1de5053ff58
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:17 +0000

    middle create shut switch though

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
