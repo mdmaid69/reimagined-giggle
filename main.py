3e636adffaf339185be9cc4e60d4ea0ba310cbf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:45 +0000

    load ill lookafter takeaway breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
