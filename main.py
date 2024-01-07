e9783ce3880681c0217fbca1f6e95951f577a7d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:07:22 +0000

    desperate bottom turnover nowhere turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
