f0edca939ba3669c69c710053668133e59fd62be
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:51:16 +0000

    tongue 572enter war collect careless

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
