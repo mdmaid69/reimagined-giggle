f92e0dbdc9ed75281bf54e68f95a84f1d7111d37
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:03:22 +0000

    dry turninto container insist everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
