e792f6c4adf4b89d12b1f435b7eb4c6bdea8fbe1
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:17:04 +0000

    law over limit lower empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
