e7bf4a843c59fa6138cb0f1bab6a77a1ab2367a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:33 +0000

    wrong lie health afford hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+x = 10
+y = 20
+print("Sum:", x + y)
