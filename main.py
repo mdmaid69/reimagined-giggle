e9d11ed16b91c41b374b16911e3ecc710751d392
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:19 +0000

    people actually size custom determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
