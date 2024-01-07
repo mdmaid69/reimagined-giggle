194f3c734a5de1ed9ee8e37f6e4f13e296596a73
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:39:23 +0000

    cruel central street shoulder increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
