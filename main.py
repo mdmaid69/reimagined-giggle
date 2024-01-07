dd83476c2b0198aee72d480bd15a585ded834334
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:33:43 +0000

    since community cope holdup pass

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
