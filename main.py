b9cf81cbcd411d258b93dc954b8b396de84e5310
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:16:42 +0000

    near effort eager level harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
