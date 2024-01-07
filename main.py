9fa23d40f1c71f704f404215219efcf3926025f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:46 +0000

    away score less complete note

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
