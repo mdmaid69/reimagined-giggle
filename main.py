1e4817e2c61facbd700f188d5300a43a9465e63a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:21 +0000

    bringforward experience disappear opposite Pp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
