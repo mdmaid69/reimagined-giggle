f1d9d056bb9fc98c2fdacd74f2b5bbec30e1f209
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:17:12 +0000

    tragedy food rely memory pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_area(radius):
+        return 3.14 * radius * radius
