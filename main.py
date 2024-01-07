8c5eae5d35376524951d35c63a4fdfb52f66d7bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:11:31 +0000

    sort couple permission safety upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
