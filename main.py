5ddff76389d6686b43c20b31923f8814d769ee66
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:27 +0000

    hide certain satisfactory dull upset

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
