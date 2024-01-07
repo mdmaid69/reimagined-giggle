9f1cfe1b1f353e2fdbdf7dbebd48a342dca52b8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:48:57 +0000

    birthday grade lunch drive pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        print(sys.version)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
