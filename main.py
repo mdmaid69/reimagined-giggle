f5e200f2c50f0aa43279d0696595cd245984c13e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:23:19 +0000

    work equipment try exhibition both

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def count_characters(sentence):
+        return len(sentence)
