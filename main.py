8949d723bbf2ff352d6d30d773d1b2e5414b8961
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:24:42 +0000

    waste part out weak lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
