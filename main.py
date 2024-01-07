da5508097e3ee3898dfa5e6af2e05168d4e7558e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:50 +0000

    favour soon natural behave style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
