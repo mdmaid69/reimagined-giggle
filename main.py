e84b95a1e05b4d09f116b718edfa43adaa1a32d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:03 +0000

    accommodation police description promise make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_density(mass, volume):
+        return mass / volume
