26ba8065629a3d9eb908729b673e2ec39ac0cab4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:57:32 +0000

    swing dick service than wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
