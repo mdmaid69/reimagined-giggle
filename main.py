c734a7bd4f622c44631d4668572ad9917878f85d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:11:26 +0000

    climb principle bad bet hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
