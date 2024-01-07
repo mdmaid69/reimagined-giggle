22434d46bdd1ae90e5970c8e4b55f4fbeb9b6f5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:15:57 +0000

    chin frequent luck pretend create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+print(re.match("h.*o", "hello world"))
