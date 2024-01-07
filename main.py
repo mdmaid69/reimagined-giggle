4168e829d66ab4ec119c1063376c08fef92956f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:46:20 +0000

    transparent meal manage loose behave

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
