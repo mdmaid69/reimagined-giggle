8a57671a55cc3ae30e8425d3d6c4dd57b56b2feb
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:48 +0000

    such check afford quantity factory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
