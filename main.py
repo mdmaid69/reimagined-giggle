58363ca59168219e929607b234567ec486a3cd8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:19 +0000

    follow recover paper jump cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
