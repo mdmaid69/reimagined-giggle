6e26535e455ccc6a332416f16ea78500020ce628
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:16:50 +0000

    butter hers office nearly difficulty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
