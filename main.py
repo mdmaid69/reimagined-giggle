1c2f82a1e0fdf3b9e4928ee3b5512fd0131c0fef
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:06 +0000

    spill luck live towards down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
