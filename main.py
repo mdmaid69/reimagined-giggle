43e6434853e0a3d4e0987a0a2936b8889426567c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:32 +0000

    tend clean joke eager other

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
