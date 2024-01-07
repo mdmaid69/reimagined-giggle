16a136f978b615c7c77febe2a429f8551c04fa12
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:44 +0000

    effort heart nonsense square takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
