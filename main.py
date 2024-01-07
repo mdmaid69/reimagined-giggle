cb7c1f098fafe3f3ae7898d1055a76831e7a1526
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:32 +0000

    ring it meeting long bend

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_power(work, time):
-        return work / time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
