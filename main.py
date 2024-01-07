dbde37e64ebf202c88b1fb5208971915262cbc46
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:49:46 +0000

    rely front heart maximum experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
