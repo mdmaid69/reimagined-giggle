ad8f5005f075db206e097e13ae7869923ee224a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:19:21 +0000

    gap long conduct keen funny

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_power(work, time):
-        return work / time
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
