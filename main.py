56afdc83bbd917572c22cc11595db340b96a18ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:54 +0000

    genuine breathe permission schedule doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
