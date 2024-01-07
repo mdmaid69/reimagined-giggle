a0bc34a4ea10ad59e6aeeeda5523dd52c2bd376a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:14:06 +0000

    pump pay turnon boat Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
