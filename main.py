068a47a3e272bc82d0284fc7e30996103fe77c80
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:51 +0000

    involve reserve seat deaf satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
