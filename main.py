64d74441ecc5e429ce5ebe0ab686ab707b414478
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:44:43 +0000

    dick weigh before steal full

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
