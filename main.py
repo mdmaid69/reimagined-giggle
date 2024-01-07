daca29ae1f01eecd48849ec4105ac855a16902ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:23:19 +0000

    level amount partner change generous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+def subtract_numbers(x, y):
+        return x - y
