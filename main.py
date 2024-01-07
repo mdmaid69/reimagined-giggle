1592ed6e7d0f5f3d6c24251e082f95195fa4eb86
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:56:38 +0000

    advise match should relationship ask

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
