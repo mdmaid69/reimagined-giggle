65eede8c5d28b4ec58dc54f4a526947669584057
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:25:58 +0000

    occasional wish up unfortunate your

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def square_number(x):
+        return x**2
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_item_count(array, item):
+        return array.count(item)
