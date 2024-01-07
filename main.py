3c2f0431181d9e4478cfaf11ff277b8f913c62c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:11 +0000

    pretty fortunately need free simple

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
