cc4df11e979f3c018373815ba0be8648620722ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:21 +0000

    throw so Kk issue lead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
