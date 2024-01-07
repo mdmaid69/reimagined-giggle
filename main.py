5164cbb239d421ce1af6d61d1d0665541fec8ae7
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:05:39 +0000

    their relative argue entrance giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
