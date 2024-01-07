ababd3049b5f68c9baa0b51ad5a72b7a3fa5cac3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:58 +0000

    total her slice arrival shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_index(array, item):
+        return array.index(item)
