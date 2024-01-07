4cc5f722ee1f3380aa9845c85ca79fee55d5658c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:38 +0000

    compare urgent try impressed project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
