6e51bf25780d29a2cd0deb008bc34b241dece05c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:43:02 +0000

    thinkthrough further unlikely tradition Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
