34f151bf4290a8efcf82586d9d813ac48ac39fe9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:50:24 +0000

    enthusiastic pipe prevent practise scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+x = 10
+y = 20
+print("Sum:", x + y)
