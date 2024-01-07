16ca40d0306aa0be294a191a46e26a1f1efe5fa2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:46 +0000

    vanish small 565end great total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
