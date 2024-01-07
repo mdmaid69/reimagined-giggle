1539979f2e118747e672046e1eb1e1552306fdc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:20 +0000

    suck outside takeout comment flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
