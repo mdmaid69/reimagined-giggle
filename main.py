df99481c638fff1688070a1045252c906e4505d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:01:40 +0000

    pocket affect major space platform

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
