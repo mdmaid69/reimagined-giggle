b16c44f01702e45de7b19a6373ce18a58312b78c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:46 +0000

    support correction peace kind march

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def reverse_list(lst):
+        return lst[::-1]
