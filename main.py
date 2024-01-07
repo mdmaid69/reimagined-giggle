437aea1a7f12d462e1d1f0772f732a342566143e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:15 +0000

    turnon see back sorry brain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def reverse_list(lst):
-        return lst[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_binary(n):
+        return bin(n)
