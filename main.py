aa13f2f2d4d9d8f0cdb5f75f53fbdad0a68564b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:16:37 +0000

    Bb trap offence bother estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
