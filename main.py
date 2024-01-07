ee72a6aa5720ec15f147ddb63aa144f9c7840cb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:36:56 +0000

    check solve risk space afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def reverse_array(array):
-        array.reverse()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
