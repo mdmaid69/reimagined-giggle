90599407e62d681de2358ead2d6d8e6db36da35c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:46 +0000

    excuse whistle material official maximum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
