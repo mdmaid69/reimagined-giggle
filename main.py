4ef05a89a42973665f9c26a07684d6d5d0b6f836
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:46 +0000

    ourselves autumn butter sort best

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
