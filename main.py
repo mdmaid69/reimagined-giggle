b2b3f48100325285ad9eddc58a13ac2368eac9bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:28 +0000

    ignore long worth odd level

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
