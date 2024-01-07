1ceb4abd9abe3e677d10b606a48d1a333a40713b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:05 +0000

    stick fly square he world

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+print([x**2 for x in range(10)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
