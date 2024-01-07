c7b71c278495ab9c4bbf3989c57950e8a55cd9ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:45 +0000

    simple destroy group believe sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_set(array):
+        return set(array)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_logarithm(base, x):
+        return math.log(x, base)
