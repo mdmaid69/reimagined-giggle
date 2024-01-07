bcbf5c3c409b59fdcbe16a49c5e8c2760473302c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:57 +0000

    listenfor constantly drink value type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
