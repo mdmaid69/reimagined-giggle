0caa02eb4bd4fed3179d7ccf2eead2f4b4579409
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:27 +0000

    picture below or proud damage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
