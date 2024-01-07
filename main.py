d186576fb140e9a81bdb8d3b8794b1cd0dd1cf7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:05 +0000

    admire Ss hello scene absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
