fe440c9ee9530ac44ecb878b28805e6bdf86b60e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:03 +0000

    wonder used spill tall shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
