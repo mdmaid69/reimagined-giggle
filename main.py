51ca8bb79cf6aa7ba259cfbc591f53287d5e0819
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:21 +0000

    scrape geton brain even crazy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
