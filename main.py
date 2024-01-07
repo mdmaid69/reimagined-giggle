aa97d28ed991b537a4d3ba0a835d05d0d1c8e84a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:37:14 +0000

    contribute possibility children mix feeling

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def count_characters(sentence):
+        return len(sentence)
