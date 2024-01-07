a1688a96c35fc95d589144193446e15d89381be7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:31 +0000

    estimate lean plenty runout far

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
