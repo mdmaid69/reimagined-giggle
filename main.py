6981cad883818e08b6c13310052f8ffc839ed774
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:01:15 +0000

    print it concentrate burn ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
