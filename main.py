f994e89d3ae148fd19d51218bb953fddd4fca2cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:30 +0000

    confused adjective anyone reduce where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
