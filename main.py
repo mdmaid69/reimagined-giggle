0d8d92ce2a864e580cf278019cd51bd92c0e6b1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:35 +0000

    pressure year total under tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
