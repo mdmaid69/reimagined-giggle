909d4674ab6e1ec4364bdf3e44c89c63127214f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:58 +0000

    whistle exit slice press table

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
