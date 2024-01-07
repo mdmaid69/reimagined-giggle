bff490051ddb40b037b8ccca6710e4e91db590f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:36 +0000

    book getout fit offer about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-for i in range(5):
-        print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_queue():
+        return collections.deque()
