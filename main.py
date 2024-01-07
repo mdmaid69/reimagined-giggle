59c10df973811de8334b2aae91b5ae5bf6ee48a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:24 +0000

    callup whichever meet disagree stamp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
