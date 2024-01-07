d8a46b08cf01ccd9b372f86fd50402e8c09b13ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:23 +0000

    search defeat escape price there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
