8d3dd8295047aa7d963542c9738018d7fed681bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:29 +0000

    intelligent sea damage close sharp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
