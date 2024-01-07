2d6739c86539d2952d75d96c7cda65f1faa39b49
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:18:50 +0000

    out detail loud than weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
