08fd68011a5c2704eac010fa86ef7847e9ffff42
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:10:46 +0000

    consist tear clean burst travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
