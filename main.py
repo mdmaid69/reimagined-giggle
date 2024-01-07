08767dfe7c4790210a5d37ad275c191c6f99275a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:51 +0000

    goout shrink still classroom appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
