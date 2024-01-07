f20926a91a6ed22bd2a2eb7727f6ac3c69b33c8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:28 +0000

    deliver emergency hollow steam entry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_palindrome(s):
+        return s == s[::-1]
