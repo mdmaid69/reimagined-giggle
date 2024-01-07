4c19f9c733cf27f5105d825b33d0b2d5fabc4f90
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:55 +0000

    discussion whom major man name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
