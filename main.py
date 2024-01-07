dc134f7081719c1bbc0ae10d322930f30db8d447
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:59 +0000

    freedom open odd excellent protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-print(math.pi)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
