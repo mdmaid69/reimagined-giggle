58ec3ccb5b117d9f5666782c13bf17b5aaf03c42
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:22:08 +0000

    equal fault ask hope off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
