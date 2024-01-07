214b85c80bd3dde981d428b0a4acd98fa3a88702
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:22 +0000

    much admire murder past heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
