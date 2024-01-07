a26112f6ea35b0ff01a46d96bc3d0a43ff18645e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:05 +0000

    priority past Ww modern unkind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+import random
+print(random.randint(0, 100))
