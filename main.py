dc81d4aa9a4e8aeb49046e8ae6915544fbbf5308
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:09:15 +0000

    patient draw fall reasonable pipe

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
