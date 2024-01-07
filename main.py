d7981acf682a381dda04c68587f897613250d04b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:05 +0000

    mail people quit only remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def count_elements(lst):
+        return len(lst)
