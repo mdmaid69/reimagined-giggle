b42f26cd0afd512c049f50a187e74c1c8a19729b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:27:08 +0000

    week custom underneath secret include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
