b27447c4a7009b2d4167c5f20790fef3b276f4c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:05 +0000

    breakinto near bet likely offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
