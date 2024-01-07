bcba8a5a082c7d333e5e29c2b752cff0cdb0e903
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:43 +0000

    raw bank love bin false

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sys
+def print_python_version():
+        return sys.version
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
