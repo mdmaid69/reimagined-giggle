806a53b20b1ca7fb52bccc4185afb25c81836e86
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:24:20 +0000

    force amount flat mainly nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
