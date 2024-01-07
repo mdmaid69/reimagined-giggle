445dccef9ad759959654707a9ec4e6225793c4d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:20:02 +0000

    exam tragedy payment aim change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
