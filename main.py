0ed1b8960076c08ded0a00fba774df47d54eaf28
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:02:14 +0000

    light splash whatever interesting refer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import random
+print(random.randint(0, 100))
