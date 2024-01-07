3f4ff791ace17ee2c669f70fec762d25d10cd8b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:17 +0000

    later likely vote fierce bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
