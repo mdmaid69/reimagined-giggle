9704f4f15bc20d1795391b0dbba3c6d96b2fe7e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:06:19 +0000

    slice water meeting keepoff replace

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
