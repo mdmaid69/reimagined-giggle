194e6393541cdd5dd4e4dd6482bd58ee521be8f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:17:28 +0000

    important what lonely slowly fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
