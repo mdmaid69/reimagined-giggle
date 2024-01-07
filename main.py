84548cac53ee12f041062dfb46a39398806719a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:38 +0000

    distant giveaway passenger comeover close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
