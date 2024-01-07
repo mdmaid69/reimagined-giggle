f4602c961a23bea451c803483b27b919d313a197
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:27 +0000

    lesson create dress guard star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
