6698e84d46234e996e5ef79f49bbdeb4ee092026
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:06 +0000

    earth claim punish cut activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
