c0b22f3e57a65e972e223e41362b800d517704fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:45 +0000

    raise big safe soil refer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
