8a6a9c41d229834d8be72ceafc7c69ca6860c965
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:52:05 +0000

    comment inform foot lunch region

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
