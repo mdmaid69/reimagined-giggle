d3c19b5d6e43ee0783d2b290e1ed276bb2c18d19
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:54:38 +0000

    pencil rough business indeed danger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
