d8727d10dfe04cd26ca1d70cc624b4bcc0aabe43
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:41 +0000

    upset force I achieve sky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
