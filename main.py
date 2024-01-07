14ace3262aca8f7eaf82fe57cbfe56b709764a23
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:27:11 +0000

    teach soak statement upstairs too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
