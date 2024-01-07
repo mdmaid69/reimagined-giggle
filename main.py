4bffbd82da3abd986a2bff6fd12c76523ebf81b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:40:55 +0000

    develop brain pronunciation difference preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
