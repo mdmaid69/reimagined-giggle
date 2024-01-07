fa95855794ca13c596cf69ef05cfebce9ca9381c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:29:44 +0000

    suckme read fence thinkof strange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
