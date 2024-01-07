eeebacbd698d29e03382b113873d213b9897d480
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:17:35 +0000

    pretend hurt innocent carry hurry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
