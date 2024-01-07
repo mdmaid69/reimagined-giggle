78855a47fe925e143709b9e35b1a44926e75d2e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:38 +0000

    child calmly push lose lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import tensorflow as tf
+print(tf.__version__)
