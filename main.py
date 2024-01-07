2283452f901a82feccd9c2490cfa156b7f8461b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:11:41 +0000

    regard wipe after workout seriously

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
