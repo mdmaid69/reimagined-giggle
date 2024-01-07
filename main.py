9ec076c7a41d5b6d523521edc6620f40ade5fdca
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:04 +0000

    consult height sure getback stand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
