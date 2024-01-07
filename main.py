fda1cc3b74ca0a526256c33eecfbeb013327bfeb
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:25 +0000

    in children amount wind surprising

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import random
-def roll_die():
-        return random.randint(1, 6)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
