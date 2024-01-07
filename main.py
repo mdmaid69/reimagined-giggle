fdbc6e430b0048c3480c4955fc4b4ed748f6a5a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:18:17 +0000

    shop square invite improve cruel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def is_palindrome(s):
-        return s == s[::-1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import re
+print(re.match("h.*o", "hello world"))
