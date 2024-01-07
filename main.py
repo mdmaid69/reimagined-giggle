230809566d78c76bc267d3a7be97962e6d21d3ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:58:17 +0000

    trust and pause demonstrate care

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
