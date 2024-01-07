7cd0bed082e16c1eabc4100473929f790db32895
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:25:07 +0000

    pretty view argument crash birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def sort_list(lst):
+        return sorted(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
