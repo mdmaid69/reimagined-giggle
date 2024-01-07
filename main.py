d4b8b59ac753fe22a6080d3f29a98d909f6beba8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:20:19 +0000

    allow grateful keepfrom appeal while

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
