283e0312a9a8828740342978ac7b083d0998758b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:14:33 +0000

    shall surface taste mistake point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item(array, i):
+        return array[i]
