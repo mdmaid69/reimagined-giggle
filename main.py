72f5f34ff6f290cb1be28e6bb6c055438fe840c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:43:14 +0000

    treat pattern development nerve fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
