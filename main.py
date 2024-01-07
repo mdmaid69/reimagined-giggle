c6bd8ebf2d26d028498b58b5a0bb01ad1afd290a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:54 +0000

    period unlucky smart communicate failure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
