5120d0814e904c755e5672df302b8d2c144d63c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:43:07 +0000

    once return home wise blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def square_number(x):
+        return x**2
