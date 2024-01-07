b07fee219473775be6e8807e85945d3a941776d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:10 +0000

    advanced collection giveback wash improve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
