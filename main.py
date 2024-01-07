68f809c13a4a2bc29ad709f823b6ee5d29d5ef4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:49 +0000

    Oo run flavour boring harm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def append_to_array(array, item):
+        array.append(item)
