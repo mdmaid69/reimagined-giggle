f11fc52947d9022d3f1babf26deeb1bee2ee5dbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:52:47 +0000

    confused delight sleep keen standard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
