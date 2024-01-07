60e4e314cbf35e3df8bb5519aaefb066255cf23c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:36 +0000

    even ill kill smoke comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
