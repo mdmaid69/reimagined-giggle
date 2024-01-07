665c9d2658dabd569f3a1f97d78c53c6693d142f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:38 +0000

    attempt accident nothing with climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
