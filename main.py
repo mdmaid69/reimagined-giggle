a1d92cefc7958ffcf3a80eb5d86f0417bcc46679
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:10 +0000

    dear Rr stamp shade meeting

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
