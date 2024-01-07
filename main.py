e6cf5c26b026aaab13c9f39250ad0a9663bb2930
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:59:56 +0000

    message loan light fire dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
