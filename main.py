d1fa58575d6d5ef864a5079df8f1f58bacc08dcc
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:33 +0000

    without file loose right getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
