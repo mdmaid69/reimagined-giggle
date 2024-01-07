0054aa2ba3a779074282e8e169ca27a96648acd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:50 +0000

    die touch reduce cut opposite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
