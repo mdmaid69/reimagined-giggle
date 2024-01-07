d6eabc1bf7bd4dc18eef21206d239b744f51f369
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:54 +0000

    estimate ago fire boat innocent

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import platform
+def get_os_info():
+        return platform.uname()
