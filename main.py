2119fc736982e7ecf51125497d6c23a518607ce1
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:01 +0000

    pride situation centre circumstance worst

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
