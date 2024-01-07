063f2322bc0400ed3d86567905ca0a3d5c9728a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:52:27 +0000

    important produce welcome aim trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
