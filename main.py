57ca179e8d671b5183bf71b82b262a74b88e1859
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:25:18 +0000

    quarter believe sick knowledge language

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+for i in range(10): print(i)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
