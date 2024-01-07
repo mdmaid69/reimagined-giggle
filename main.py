a7daf0fdd52a476c9ce9aee2baf3abf40347cbc5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:36 +0000

    artificial mind state lead rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
