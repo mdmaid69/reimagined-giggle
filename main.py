6bab238713a4ceaac43e7bf566d3b33a9eaaccf8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:30 +0000

    speak rather boat situation mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_string_from_array(array):
+        return array.tobytes()
