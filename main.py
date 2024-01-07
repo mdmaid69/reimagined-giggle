7fc34dbfb16b3bfb6a7c255debfcef96ce242bb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:59 +0000

    main area please release good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
