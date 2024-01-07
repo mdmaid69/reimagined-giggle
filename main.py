0434461c347fa73370ddadab5c033848fe20dff4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:56:13 +0000

    account little fairly lookup breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
