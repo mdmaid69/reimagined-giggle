3244628ec295508f3e6fb47aa5cf9051c576dcb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:40:15 +0000

    regularly turnoff war split along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
