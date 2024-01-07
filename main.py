91a6ddfd4a960d81497e41a3cd7a218dfbe9c7a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:44 +0000

    father spend since tidy part

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print(sum(range(10)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
