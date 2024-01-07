c4457b5404430874c7a2d3a006a37d527cce03ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:34:52 +0000

    ear man refer song rain

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
