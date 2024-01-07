e96c5c625021a4ae5ee78a9c680bd8f84a6d496e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:55:40 +0000

    compete any quickly bar label

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_hex(n):
+        return hex(n)
+  def subtract_numbers(x, y):
+        return x - y
