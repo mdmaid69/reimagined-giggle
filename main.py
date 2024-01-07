5000bbda74d697d31c9f434abbe77bc21abfb793
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:19 +0000

    hit quick niggaacademy support touch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_max(lst):
-        return max(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
