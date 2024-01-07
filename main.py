99db0740e48bf9d3fdaa410be9695f49ea9048f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:45 +0000

    without mean talkoutof identify gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
