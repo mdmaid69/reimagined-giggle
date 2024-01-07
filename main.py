562322a2cda5a9ef297ae9bcd74d50a4cf5bf27b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:20 +0000

    squash 565end land interesting see

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_odd(n):
+        return n % 2 != 0
+def add_numbers(x, y):
+        return x + y
