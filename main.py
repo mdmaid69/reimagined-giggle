47fe408dac1f67ec04ed47e973b7c613b76e7827
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:10 +0000

    various me flat upwards question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_index(array, item):
+        return array.index(item)
