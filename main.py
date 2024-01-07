c90b68d82139f6c888ffd59cf1d00f9fd762d87e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:27 +0000

    believe apply actually means style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
