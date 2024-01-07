0e31bdca20af4a6bed70af038fa3880f18874079
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:44 +0000

    total goaway steal man release

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+print(sys.version)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
