be0743e9b9a3e7fe29b671f7f47b82548f1be0c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:44 +0000

    Pp manage plain pair possession

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
