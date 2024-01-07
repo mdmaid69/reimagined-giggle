352bbcf7548e662cc8b1680b3d2b80afc344326b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:08:57 +0000

    habit return to often from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_float(array):
+        return float(array[0])
