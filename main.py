54c12266603deb200bcf9925867423a33c44a880
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:13 +0000

    cheap race like push drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bytes(array):
+        return bytes(array)
