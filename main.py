3ff2a9d3082c55f4b9d85a2ae0085499abb49761
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:52 +0000

    someone face all result spare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
