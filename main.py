f11cca4e448b92360202d4b9674dc83d4f1f6b24
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:34 +0000

    especially take tool wet contribute

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
 def get_array_as_frozenset(array):
         return frozenset(array)
