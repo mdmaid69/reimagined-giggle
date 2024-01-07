b551e31332681ccbdf43599dcfe7754a090eee6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:09 +0000

    normal runinto model direct blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_acceleration(speed, time):
+        return speed / time
