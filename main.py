36ed6c75d09d725f9fc58bbfa139aa9dd421fe03
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:03 +0000

    touch turnout beneath skill attempt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
