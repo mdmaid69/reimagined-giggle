6fe497d97966dcb97df49ce08094abd4d0edcca7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:19:44 +0000

    surprising gas afraid themselves finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
