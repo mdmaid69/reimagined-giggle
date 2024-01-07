4005a40c4f94e4e0e7ef36f199a4a460e860faff
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:53:27 +0000

    over delicate as responsibility uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
