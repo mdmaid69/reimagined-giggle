198f37a2c6157399bb6ebdfcc0f912111de5664e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:41 +0000

    wrap head complicated gather fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
