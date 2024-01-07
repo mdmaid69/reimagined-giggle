54209faed9dd6d2beaf729a93d709d75cabb98da
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:47:52 +0000

    greeting meaning which upstairs smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
