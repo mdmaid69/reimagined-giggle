5cf63e0c439a7e1053a45332910789daa13fa3a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:07:25 +0000

    voice thin popular nervous roof

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
