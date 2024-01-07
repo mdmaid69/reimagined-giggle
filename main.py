a43ddd1208bbdefa23241431df582a9590237ea3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:09 +0000

    forgive OKorokay nigga off listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
