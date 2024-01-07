c08d7c47c4eced0cd99199dc6999076de0793961
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:52:32 +0000

    lock long either head delight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def find_unique_words(sentence):
-        return set(sentence.split())
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
