da6c7567a8ca23659a0df18bab2a1c201bbc76de
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:36 +0000

    service ill what cool sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
