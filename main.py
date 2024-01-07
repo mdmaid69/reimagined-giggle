ef9c3e3402c82bd6b10a3ab8f474e0d712ebc23c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:12 +0000

    pay thing flavour increase dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
