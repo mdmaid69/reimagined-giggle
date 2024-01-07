80024d071419eb2914259c727a959521ff84f6b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:07 +0000

    rid workout vote ahead bringback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
