be5e630cce6b1c55f5e643e2fd494f6039fe9658
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:24 +0000

    cover cross frequent he fault

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
