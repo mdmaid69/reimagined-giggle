a8cef153129c27577380d9d357b0738f16d567ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:30:24 +0000

    matter sell suit project occasional

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def add_numbers(x, y):
-        return x + y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def find_union(list1, list2):
+        return set(list1) | set(list2)
