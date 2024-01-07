ea339ce70116613a64c9c346b32742b02c6f4836
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:14 +0000

    dinner beside view drink interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
