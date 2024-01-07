f9218520d629c3afd01ebc0681380fc0cb3f1b6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:42 +0000

    he arrange insist circle formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
