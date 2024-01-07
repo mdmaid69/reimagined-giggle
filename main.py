34fc21a1d20986322e886b5f016db01b80de0bb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:22 +0000

    want equal shall thinkof basically

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
