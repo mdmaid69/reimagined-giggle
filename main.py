693e6a25c3a26a2c06700c848adb9ade2fbfae80
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:40:18 +0000

    pure succeed deep holdout natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
