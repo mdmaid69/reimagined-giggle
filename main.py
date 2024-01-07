54cc0f7bbfd3f3c14911b58773aca7fcce735481
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:28 +0000

    chase welcome motion earn can

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def multiply_numbers(x, y):
+        return x * y
