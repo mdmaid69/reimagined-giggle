d0e14fee791f52317b17f11510d0a24f7b417b06
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:35 +0000

    pretty bring swimming mind simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
