d9be93a7c61ab4ee3c5e00f0f1417efbf98166c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:42 +0000

    bang surprised race mood rest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import random
-print(random.randint(0, 100))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
