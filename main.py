63968c1b948ed6c84dc9c8acb01b21b65a78226b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:21 +0000

    bottom swim bye car whichever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
