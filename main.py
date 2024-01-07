daaf3dc9bed0fbaddf9fe4889872172dda6ce1f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:22 +0000

    cap feature climb ever false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
