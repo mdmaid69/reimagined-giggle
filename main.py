703b63a5570d1c12a63e2d972141e6ffab667ba4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:12:36 +0000

    hold rather worry welcome machine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(lst):
+        return max(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
