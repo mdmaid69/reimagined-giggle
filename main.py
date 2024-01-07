31309bca1a1ad155ce02560d4fd425ce2929749c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:47:29 +0000

    welcome lookafter save splash break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
