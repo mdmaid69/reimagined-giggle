a1df52409bbb4c991c79c43ccb1676d7669a313e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:07:58 +0000

    preparation community stay guide 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
 import math
 def calculate_permutations(n, k):
         return math.perm(n, k)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
