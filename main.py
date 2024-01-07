15ac6c4d66c2b1a8ff75b129408a7b2af9ac6268
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:31:27 +0000

    arrangement relative connection class awake

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
