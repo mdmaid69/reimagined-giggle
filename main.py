f9d4a9ef6b419d828c27d533d87f3c293f0d9b21
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:02:16 +0000

    nor area sure hate bank

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def convert_to_hex(n):
-        return hex(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
