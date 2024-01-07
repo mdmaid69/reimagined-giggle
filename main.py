e78ebd9f7cae13afd6d60ff0af8de983aee11b0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:21:06 +0000

    arrangement suit describe comeoff gift

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+print(sum(range(10)))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
