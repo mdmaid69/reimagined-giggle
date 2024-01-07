9cf939d29d30f9fcab276c632ba966c126a048af
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:04:05 +0000

    beyond our branch urgent less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
