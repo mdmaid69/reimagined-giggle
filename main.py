57054e41c2c2b768d7f1b0aaf89d2dfafc94cc60
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:12 +0000

    feeling major violent belong cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
