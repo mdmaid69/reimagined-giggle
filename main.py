de2f24a32db82d47cc8da1bae627e4b94ad916c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:21 +0000

    when so wait as pick

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+print(sum(range(10)))
