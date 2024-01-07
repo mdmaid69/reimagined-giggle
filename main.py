1a6026cf87171120051f36e9befe007eb0f4844f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:18 +0000

    remember frequent dig similar light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
