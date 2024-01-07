a59f84cce993ccf49e59d09a69426ce23e9aca73
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:34:29 +0000

    happen before minority murder maintain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_array(array):
+        array.reverse()
