bd3d01888ab421325048f4dbcc8e2c16c56d7816
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:29 +0000

    switch again tax sudden length

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
