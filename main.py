0646489f5b1100fba54a95efceaec30de73e485c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:26 +0000

    568energy shoot smile Qq mine

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
