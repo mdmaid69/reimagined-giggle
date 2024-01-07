f65facfe9f3db97a363b6fcce2575b602ba84d3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:52 +0000

    concentrate on ring tired understand

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def split_path(path):
-        return os.path.split(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
