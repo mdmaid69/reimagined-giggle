8ce6b1df60a89a8bf5290dc22a65b1ae78003787
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:58 +0000

    storm completely me soil emergency

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
