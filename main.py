eea090b53afa9a88bb06f16751eeb103f0daa5ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:04 +0000

    major size unkind regular meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
