49c67952a73410f38d04f446f90dc1c1c415cda8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:12 +0000

    physical listen majority touch strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import random
+print(random.randint(0, 100))
