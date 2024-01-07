2910536b66b8fddb97f28f6cf87b44bcfb0cca27
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:57:20 +0000

    union wind go goout treatment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
