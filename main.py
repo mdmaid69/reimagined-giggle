b1faaa94eb4d4aceb927892cbdc12315d2d4cc50
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:33:21 +0000

    alone children Rr Itself surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
