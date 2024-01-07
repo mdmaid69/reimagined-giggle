1c7b7013a6013ffe144110ed36b6bb0937db8cb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:49 +0000

    surprise guilty service gradually keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def count_elements(lst):
+        return len(lst)
