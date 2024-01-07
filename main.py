f0a0c4bb1188a46d92cefb888174ce2c362be3ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:52:00 +0000

    message wonder pressure criticism estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
