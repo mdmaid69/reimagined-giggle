f2a720e024d76283b2561fcda51219c4983e2503
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:28 +0000

    deep store achieve wind experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
