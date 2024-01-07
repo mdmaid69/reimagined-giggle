ee696662b9020a1f7bb73b263a1477fe63224717
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:41 +0000

    occasional scrape getup off estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
