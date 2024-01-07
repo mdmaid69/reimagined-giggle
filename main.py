43266cab73495f1f4aefc5a937b8db58bad48ed7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:28 +0000

    walk sure maximum offence pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import time
+print(time.time())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
