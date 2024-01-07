91344f58c2256e0cfafb309401f88297762c8ebe
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:31:57 +0000

    hide sensible worst tour return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
