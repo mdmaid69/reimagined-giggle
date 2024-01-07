ad5f211c14ba2f4b7645aeb2e624b5eb90013eb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:21 +0000

    regret in empty force least

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import platform
+def get_os_info():
+        return platform.uname()
