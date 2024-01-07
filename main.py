b2e5e5d3a55384979fe4bd824542622bd0b226d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:21:15 +0000

    copy lower off than like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_min(numbers):
+        return min(numbers)
