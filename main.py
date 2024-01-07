65737133fb1b7eb6949d695837807a40a88c4e2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:06 +0000

    firm OKorokay refer chief gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_directory():
+        return os.getcwd()
