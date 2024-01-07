2e85763df102c5e416219040fb25df01b9363262
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:03 +0000

    chase somehow just far confidence

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
