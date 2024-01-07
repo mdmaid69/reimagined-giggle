bd710f6582e761e4bc87e8008c01c2800a2dd749
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:53:31 +0000

    clear loan off trick corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def is_odd(n):
-        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
