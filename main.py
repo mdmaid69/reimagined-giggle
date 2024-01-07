2c9659a563abc511354f39d3d3e5707a26b63000
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:46:38 +0000

    chin content physical measure visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
