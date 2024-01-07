1788120b0edab1f2389c5531b1b928933e5c5178
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:18:45 +0000

    scream mix scare blame limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_volume(length, width, height):
-        return length * width * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
