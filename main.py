45f4ac2e271c7fe02935fe5eb1fb6e9f8444bffd
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:33:11 +0000

    rub hello rubbish visit hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
