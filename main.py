d1abbe59f01b0dfa8d492204ebedbab070b007bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:31 +0000

    unpleasant bright better tear mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
