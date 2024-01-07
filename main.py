8236bad22509740793d6d41dbc17ed5a5388c1ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:54 +0000

    future patient corner breakup that

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
