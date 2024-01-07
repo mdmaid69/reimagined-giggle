017d83dbf09d34774d6fab728b982446c9c7410a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:37 +0000

    heavy order flat Dd colour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import tensorflow as tf
+print(tf.__version__)
+def divide_numbers(x, y):
+        return x / y
