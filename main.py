e7adcc5f8a8ae809e8c978135e732f2f9e96c8de
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:35:40 +0000

    rise load runout Itself work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_palindrome(s):
+        return s == s[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
