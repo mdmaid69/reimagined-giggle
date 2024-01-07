acb0afbc6fec2026bb6b0695a354e6ed09a6fa2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:01 +0000

    build gradually sometimes principle precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def square_number(x):
         return x**2
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
