db5e4d5a1261d564469c91e6cde2e2d15e1b3b23
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:04:27 +0000

    convincing respect keepaway Kk discuss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_dict():
+        return collections.UserDict()
