02c5db44a56265637dd28ec7cebc85ce49356d3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:02:14 +0000

    snow future movement judge do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
