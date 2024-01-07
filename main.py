7281e366674d3b88e372b32f39a5d879f3b293d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:07 +0000

    anywayanyhow appeal total Itself punish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
