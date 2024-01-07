cf57c74ed38efec269d4c8de13373eef63c7f7cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:59:08 +0000

    keepoff direct make memory instrument

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_acceleration(speed, time):
-        return speed / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
