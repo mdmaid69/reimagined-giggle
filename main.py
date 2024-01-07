68cd32cf664cd46978db745a9fc7a8266f0592ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:29 +0000

    age computer table drink guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
