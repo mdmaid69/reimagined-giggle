175c05377b45692c67803d2199143625404eda20
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:03 +0000

    whenever whether knowledge pleasure body

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
