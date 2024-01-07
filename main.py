746a07a7412935d78bc75e3a7bb10c4cc195ab42
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:13 +0000

    unite stiff we combination aware

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
