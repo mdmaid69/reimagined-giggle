c487c90c867c24f093e29fc0d35045950cab0d2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:54 +0000

    into join question exam union

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
