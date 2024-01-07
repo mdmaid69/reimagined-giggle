013e0641ed5de067beb0c2906ac3a57f9cef8744
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:47:01 +0000

    pour Ww group machine foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
