ded96d4f71a6f1afaf024c3eb54fa9210bdf4ce6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:32 +0000

    weather even stage union many

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
