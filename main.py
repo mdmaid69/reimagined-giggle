ce96a6d68df82d44418df7282d0c27a11d733121
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:12:28 +0000

    tip near quality nor exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
