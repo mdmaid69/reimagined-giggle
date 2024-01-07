4fac778e4ea1b835d15cf34425f3a7a95e1b217d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:25 +0000

    performance basis finally strike runinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
