61a352bd7b1979dfce45ba3c67153c07d6ed7a86
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:22:18 +0000

    who description fun takeout translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+print(os.getcwd())
+import time
+def get_current_time():
+        return time.time()
