b0b1a50268262d1c4f8323fb7c0284e4be1d34ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:53 +0000

    grade long major moral record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
