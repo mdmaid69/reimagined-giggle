625b40a1399d13ca0d9e1c79bd8079f66bfbb7ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:59:36 +0000

    so bye still shout after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
