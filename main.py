dc3fe595b20a011f758786899158e8b989e67b85
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:33 +0000

    point visit Ee book almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
