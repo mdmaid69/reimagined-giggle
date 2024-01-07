1c7eabd3e859e40a66ce07d14ed9aba53dedf990
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:26 +0000

    march food classroom so dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
