c10579248ffb2ce59727519be1919cabfaf018cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:20 +0000

    impressive trap collect right wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
