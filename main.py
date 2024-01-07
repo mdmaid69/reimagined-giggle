26418d9ef94e14d1dcac26a3d07cad474ae678a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:43 +0000

    cough flow since star believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
