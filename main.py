25119ff9fc1627ae82fb9fc9d9f3f83e0913283a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:50 +0000

    turnup finger towards favour evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
