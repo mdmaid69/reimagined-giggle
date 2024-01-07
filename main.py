8e99fa57be46c174fb009e9d293af0eed90242e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:51:52 +0000

    prefer reliable pencil clean go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
