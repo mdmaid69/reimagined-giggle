5df0b7f065ce1b7149fa0f8ce9d1080853af192c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:59:54 +0000

    slide theirs who phone plate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
