e9d514eea9e901ef1bd1d60e85e77ed6ff919f89
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:30:23 +0000

    gate boat cost shall definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
