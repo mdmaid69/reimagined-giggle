92ec32be84d027367de8864b0552fb981ab43bac
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:08:38 +0000

    break new hello paper declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
