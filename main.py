51ec1917942b112b6e78437130a832499a758467
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:27:21 +0000

    act market thorough hurry reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def sort_numbers(numbers):
+        return sorted(numbers)
