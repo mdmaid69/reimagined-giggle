7060f3afa2b1ae7939691d35ae46d091cbd45b36
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:55:50 +0000

    face rush over comeoff ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
