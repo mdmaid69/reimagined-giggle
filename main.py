339b7e664dc16d2e93865ee4006b0d6f8b96af17
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:37 +0000

    clever chance stir woman anyone

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
