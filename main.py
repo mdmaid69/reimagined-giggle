a89f86072c9af08c159cd07dfe6ccaec23d3e35d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:14:08 +0000

    spoil actual practise occur minute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_density(mass, volume):
+        return mass / volume
