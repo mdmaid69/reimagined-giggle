4c7f0aafbf36f85eec57056572ef54ea514eddcf
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:51:11 +0000

    large wipe politics patient afraid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
