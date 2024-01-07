e8da06bc58a354dbdddfcfc7e7378eb1c9c7c0ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:23 +0000

    step stay bottom afraid direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
