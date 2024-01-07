ece13b0d0fa124f8b12c9ca96827fc2018735c61
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:33:12 +0000

    hardly bringdown train they still

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
