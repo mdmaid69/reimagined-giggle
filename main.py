2f97e5123f67bca4c90305f68699a2f776722d61
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:51 +0000

    result march mass future false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
