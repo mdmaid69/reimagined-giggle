c6df01c72dd5eeadcef89aa5be25a84e7935e88a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:19:08 +0000

    memory wheel rough pure careless

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
