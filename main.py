63898978f08458f33aa5dadb61c81a812b06a165
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:04:30 +0000

    many late tap precise tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
