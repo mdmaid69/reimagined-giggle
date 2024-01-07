c7f173326f4a69e34b707846a18e15a0096c3148
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:04:26 +0000

    practice last before right talk

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
