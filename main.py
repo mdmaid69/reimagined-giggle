c28adec785a88a6dce206f51dee7c91badb1a7a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:00 +0000

    study wish basic division try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+print([x**2 for x in range(10)])
