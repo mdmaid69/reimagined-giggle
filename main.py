fe0e98a9b7842a150f9245020183e3ea055c5251
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:00 +0000

    crash keepout everybody earth home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+print(datetime.datetime.now())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
