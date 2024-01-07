e1b195a0204d705b1065ae825f64deff74b09038
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:14 +0000

    accordingto afraid nowhere suit alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
