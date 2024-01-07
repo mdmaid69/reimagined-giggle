26a25e5e2afb93ee526a7a9ef941de5c3d2f1194
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:41:19 +0000

    description violence pull material lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
