4c63b004488c90555afbeed879df6862ea471708
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:52:19 +0000

    long drive work physical favourite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
