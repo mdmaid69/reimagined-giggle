2a52601ff49b938c4a5a8091be55e22bc25a0bdd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:13:02 +0000

    sister so software one knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
