c52e234ab30749206c4d01990150af6b4a9db441
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:25 +0000

    over prize 566end trade nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
