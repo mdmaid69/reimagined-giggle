56e4eea9d43642a5a110487d2f69f4a414a6404d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:10 +0000

    near Mm strange slice estimate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
