3c622b216584dd77546ebda6cb1ff09ee52400d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:46:34 +0000

    attitude vanish suit fault language

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+for i in range(5):
+        print(i)
