93c4ad8cc71b0eecea49d1fd445c365a1652bc4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:52 +0000

    else pressure past declaration turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
