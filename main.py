aac94b2d4529cb70a6687a30a3bdb11bcacd7033
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:26:09 +0000

    immediate quick Hh oil eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
