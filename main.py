8b8ad66f6c79341fb22ac6785ca632b0d26662f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:00 +0000

    freeze example like action discovery

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
