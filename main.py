e2dc792ea1b54899c6d62de069e930aabe090383
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:19 +0000

    very exam in belong soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
