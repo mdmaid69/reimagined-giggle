b9d5f2c880f319846c239ad43adefbe5c7876387
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:23:11 +0000

    smooth excuse poor sale thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def divide_numbers(x, y):
-        return x / y
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
