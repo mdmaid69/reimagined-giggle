2e78678cae5d0a7e1980647c20e4e04df363081a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:57 +0000

    maybe apologizeapologise wish trade likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def sort_list(lst):
+        return sorted(lst)
