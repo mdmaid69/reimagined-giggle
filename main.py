0a048827fe6223ae6f66c97bcd53b3946633a2e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:29 +0000

    male minute lock impatient the

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+print(time.time())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
