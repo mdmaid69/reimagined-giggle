f36f424b947f380df1380bb71eb247e8223f5b39
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:18 +0000

    sleep comeback usual turnout freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sys
+def print_python_version():
+        return sys.version
