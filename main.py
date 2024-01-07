0dd99a898e5d4e0ca8c1ca27c45ecfa07990c6a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:38 +0000

    tear second to taste centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
