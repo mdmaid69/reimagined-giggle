e19666194ea071f5ab14b585e17041eef5ea9b47
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:53 +0000

    past detail cap ask air

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_item(array, i):
-        return array[i]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
