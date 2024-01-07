10c7068f5df5170b1348e0c802fa1313c4a1c076
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:35:24 +0000

    watch acceptable practical communicate once

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+print(os.getcwd())
