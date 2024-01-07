66a6a56aeade33a99f11babad46a65d87afbf981
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:06:48 +0000

    will gooff money lift pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
