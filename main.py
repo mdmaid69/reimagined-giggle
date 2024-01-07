af28516af8b6a13d16bcd301ed7d1ae431c18ba9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:27 +0000

    hand loud turnup goon quarter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
