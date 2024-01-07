8f7a317d10661fe1ae84dc24604addcd3797211f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:05:27 +0000

    spill thanks explain search switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
