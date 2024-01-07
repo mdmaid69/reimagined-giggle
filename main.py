999bb7f3aa1136acc9828f756d8c4a72d815e333
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:10:28 +0000

    gap shut contrast suppose wheel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
