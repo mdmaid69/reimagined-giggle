4318d8551417fcc5cb71044812e13f266e1b2da9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:03 +0000

    meaning nowhere meet show bound

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
