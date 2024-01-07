7fce10150b547bf4218725461042ecb5300cf727
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:20:21 +0000

    cum taste corner bringback suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
