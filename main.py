2e189390a4fabd9349009689f8e07af36f56f8bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:00:44 +0000

    move valuable sea window tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
