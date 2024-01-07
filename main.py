e0bd052986366a0346d5d2296cfb04b7e32a2487
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:37:24 +0000

    lunch table sleep unhappy try

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
