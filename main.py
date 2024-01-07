e8c6e1259a03c5a97542d4a4143b588233c20cee
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:41 +0000

    raise chance round soon complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import csv
 def read_csv_file(filename):
         with open(filename, "r") as f:
