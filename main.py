b41907dd73f71db669586448bb900bc9ad9f51e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:49 +0000

    react programme bet proud shadow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
