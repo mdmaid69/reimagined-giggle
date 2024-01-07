524c90f3be40360f6f1729c174f51d788fe70f8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:17 +0000

    permission tongue lie arm repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
