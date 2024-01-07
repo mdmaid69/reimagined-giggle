e2d973d25a9b8808156d1efa2b8aeed372b3e45d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:07:09 +0000

    snow friend getoff dull well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
