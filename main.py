70495100b0d4e4c478cc7aa7bbb9a72168a0a1fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:53 +0000

    grand behind underneath visit strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
