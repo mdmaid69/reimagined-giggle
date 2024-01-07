fffefd66c00c625a62a3453d4d30bcc50657ef4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:43 +0000

    picture more laugh dish after

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
