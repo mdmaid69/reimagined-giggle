1f510331a34567e40cd6c1ffaf40b4f46471fc8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:15 +0000

    regard summer involve destroy have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
