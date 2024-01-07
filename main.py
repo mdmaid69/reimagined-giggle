7818c5bf98e7cc783857342562280d0e9268510b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:48:06 +0000

    operation change hide statement wonder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
