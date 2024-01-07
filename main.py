2924216cca41e0c2541edbc1a2aa8c442df8f4f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:52:42 +0000

    concern introduce 566end soak impossible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
