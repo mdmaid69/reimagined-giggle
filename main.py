d1264458a103c2e4ef3d84628c1fde72f3fc47fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:23 +0000

    refuse drive lookafter worse habit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
