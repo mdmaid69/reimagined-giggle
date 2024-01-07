9df409722411809fd8e237eec988c7055bda504c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:48 +0000

    hope must even impressive affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
