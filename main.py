3c6e82306ad4954a1f10435443ea94f31633c5f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:20 +0000

    boat wall contain free responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
