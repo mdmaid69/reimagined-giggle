ac255edda186467b88316f3cfffb8d00b13507cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:17:29 +0000

    entry history lay lonely transport

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def is_even(n):
+        return n % 2 == 0
