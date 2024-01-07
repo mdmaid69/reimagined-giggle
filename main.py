11602ac8cb8c564691e504241afba2be8876aafc
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:30:17 +0000

    steer outside long tread bar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
