782c41890c64a22ebca503dabc052857211a420b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:01:01 +0000

    declare pin load force slowly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-print(time.time())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
