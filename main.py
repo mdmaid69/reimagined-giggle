b1cc742648e842179f9d22a8a448822e795bd651
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:08 +0000

    that turndown matter breathe cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_list_from_array(array):
+        return array.tolist()
