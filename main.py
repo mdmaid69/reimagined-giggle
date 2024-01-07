4a51ab5aad14ca8b5df9034a33e2a97077dcde8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:52:31 +0000

    progress cover round job unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_counter():
+        return collections.Counter()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
