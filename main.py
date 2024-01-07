fe51ec14bdae969d85f93200cef198a3f4fb2751
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:22:14 +0000

    disapprove contribute stop week average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
