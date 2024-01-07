d4170a745589fb872624bf48740c35281ce7ccf8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:55:45 +0000

    bar scared keep inside official

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
