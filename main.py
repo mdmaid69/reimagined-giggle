f1872c18a32532f6959fce5a878902e028678248
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:57:49 +0000

    thorough pleasure there just bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
