26ddb075163dde888166c4c97e0fa2c45935cfd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:36:47 +0000

    shut collect relative object past

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
