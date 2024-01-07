6c9ec1a8fb86802889c07cc1fcfa19b651c0bd23
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:06 +0000

    lookup delicate some float regular

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
+print(sum(range(10)))
