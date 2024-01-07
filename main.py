7a7ac802aa4ca257560aefa9e85e6a64bfc144dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:40:58 +0000

    thirsty size strange real moral

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
