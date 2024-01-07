ac56516dfdd62e73af24c17751217801ec5ccb53
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:28 +0000

    constantly nurse nose Dd minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
