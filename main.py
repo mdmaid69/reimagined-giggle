b0050b7d888e2762e88973ded0b58e00a8e19ac0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:20:45 +0000

    only season urgent want suckme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def square_number(x):
+        return x**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
