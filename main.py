ad9a6f54b2c39af4c020f75ba2a315ddad4f5c8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:39 +0000

    transport uncertain add burn smell

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
