9ca45addadfa5f16838ba7b6cb5e504f127f9abf
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:47 +0000

    maximum reply satisfied defeat may

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
