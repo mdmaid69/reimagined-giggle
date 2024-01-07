6fc273d6f2beae48744b287d522898dc415fc841
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:08:27 +0000

    careful after completely takeaway achievement

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
