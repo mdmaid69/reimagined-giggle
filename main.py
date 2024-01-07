7c91323230b5c8dfff74df74fa7e38dc4c0b8236
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:22 +0000

    sweet used bored world eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
