66ead6931224b79e7bb4433ceccadf2b06304835
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:31:46 +0000

    beneath explanation on myself moment

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
