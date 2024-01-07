2832cfacb54865dff0b2576fbd8968649a49ed4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:57 +0000

    dry information kind freeze surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
