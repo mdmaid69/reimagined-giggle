41ed99eea1f5ff7d5a98db24eb0d907e9c751452
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:36 +0000

    turninto pressure drink aim plastic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
