8c2993aaf0aa74e0f4818c8bb8bfcead244d7e63
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:01 +0000

    favour defeat flow burst scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
