7f3c4cb32cc26d84879f59b8fde9c87ca8c61949
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:28 +0000

    matter tell method neat brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
