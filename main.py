46fcefa2c8bc883d62b994ab779ddeabaf73f9d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:59 +0000

    scrap suggestion way approval draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+def print_python_version():
+        print(sys.version)
