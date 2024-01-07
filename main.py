5bcfe4fc8ae53c6f1ad1871110283d398c98e3db
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:41:32 +0000

    sight line beginning morning flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
