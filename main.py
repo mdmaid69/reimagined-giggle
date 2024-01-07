547ddea5f057c4b40647e7723f8f527a899a2b72
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:39 +0000

    tread callback time touch freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def is_odd(n):
-        return n % 2 != 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
