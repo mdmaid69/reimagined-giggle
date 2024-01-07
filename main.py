c59895a89769fafc7182d4238819027c301174bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:14 +0000

    impatient press turnout comeround fold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
