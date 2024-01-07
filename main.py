833d4ff48f4589191be6d2108aa54508914d8409
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:32:52 +0000

    permission replace turn lot excuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_speed(distance, time):
+        return distance / time
