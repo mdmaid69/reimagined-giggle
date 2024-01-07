63436efa07fb299576adf5f62c9727bb932356f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:56:27 +0000

    content complaint comedown much help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def sort_list(lst):
+        return sorted(lst)
