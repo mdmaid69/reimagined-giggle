31eb4e25e2c227c2577230d2cbba1868fc588ed1
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:36 +0000

    getup Kk border sale bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
