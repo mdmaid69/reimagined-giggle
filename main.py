6bd4c253588730b83337ca0f5b3a865dbcf8d24e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:51:17 +0000

    underneath complain sign connected grow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
