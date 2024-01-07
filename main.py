5c20473362e6c32724270d91337a0a4e4b5196a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:55 +0000

    same interruption doubt final suit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def is_even(n):
-        return n % 2 == 0
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
