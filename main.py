fd0af2b50c9ee795e32cc2eed14adb9ef9f83fd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:50:55 +0000

    able telephone opponent breakinto last

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
