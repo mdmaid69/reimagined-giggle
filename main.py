231d8131003305f06e6bb83ee52cb78c6dfb340a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:05 +0000

    properly goout side support alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
