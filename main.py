9547365c4419394596abf4f3bc32686bf2a39a7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:54 +0000

    combine cool year card way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
