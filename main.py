0e269528d56bb72b12f2d140236d0318bfa72706
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:18:28 +0000

    believe life climb police rarely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
