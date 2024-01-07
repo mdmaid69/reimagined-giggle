a5e36249dbced1ef2ff0d7ae9ed126c6714880ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:41:18 +0000

    desperate pause view light ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_unique_words(sentence):
+        return set(sentence.split())
