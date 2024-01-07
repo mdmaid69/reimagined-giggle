3c7aa5f82e0e62f562fcb058cb4f2e228199b5b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:26 +0000

    busy squash calm tune belong

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
