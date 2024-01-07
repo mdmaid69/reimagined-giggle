98151562d5d0ab7cc6c09189fae3dbc01dc6ea40
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:42 +0000

    our snow room truth rubbish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
+def multiply_numbers(x, y):
         return x * y
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
