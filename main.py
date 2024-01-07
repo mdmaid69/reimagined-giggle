c6912280f69c21291b2f86d36a46b5697fa39c11
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:59 +0000

    any show Dd fail tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
