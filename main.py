4f4d35977a3b0ea635df597dafbed275eea62279
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:27 +0000

    prepare natural draw violent fresh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+x = 10
+y = 20
+print("Sum:", x + y)
