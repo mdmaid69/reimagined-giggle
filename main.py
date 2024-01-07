1bcf2da579616eeb7cda157f3ebc89970c279be7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:53:52 +0000

    feature can relax doctor unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
