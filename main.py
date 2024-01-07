73c1939dd26c3d49a8e4bb19973e58be6b784a93
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:28 +0000

    dream both stage before anyone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+print(os.getcwd())
