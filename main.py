6ecd201c72a7b8b406484548e258789e4d9ed50f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:49 +0000

    569enjoy meet agreement serious lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
