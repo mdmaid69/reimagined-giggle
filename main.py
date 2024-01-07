064c1a3622f9dd881d0ec8d4d1ff732d08779163
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:55 +0000

    kind piece warm whichever gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
