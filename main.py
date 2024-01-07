356f681d43752a5e7ffb2192ca4317029abb0554
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:33 +0000

    introduce love each always poison

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
