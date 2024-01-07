3cce2d5891f1dd94ec587d5fe8e6c51850037401
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:34:22 +0000

    influence sweet pattern holdup concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-print(sum(range(10)))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
