4aa1205b93380211244d1b5d0b8adf796041e5dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:39:45 +0000

    busy object exact only please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+print(re.match("h.*o", "hello world"))
