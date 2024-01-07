d958b96311887c9c8ab3d5465f218bda63bfa07d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:45 +0000

    ill take bringforward brown flat

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
