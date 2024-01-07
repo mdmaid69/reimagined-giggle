cb38a34ab417eb7a3b8a95a2a1a4502370a9c72b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:43 +0000

    smell now rule crime stone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
