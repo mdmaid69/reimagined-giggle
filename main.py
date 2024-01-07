c4c8a471d044b1ce326b244c3b738b7d85559d3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:55 +0000

    murder world later fly thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
+def square_number(x):
+        return x**2
