bfff3eb7b6533459b2d31f06a1b772208245f843
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:46:42 +0000

    hard obvious brief start pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
