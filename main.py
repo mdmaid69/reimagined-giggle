4bdce0c0d5aa13166f2a2a55e3720802cd020b8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:55:55 +0000

    blow truth turninto defend sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
