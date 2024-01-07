621a586ec09c66edf9129c0ad22bb4acd966803e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:49:05 +0000

    land Yy fun train load

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
