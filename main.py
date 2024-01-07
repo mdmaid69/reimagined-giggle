576c11e02644d60a535ce66c1c070891fddd9a3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:04:26 +0000

    guess interruption genuine world shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
