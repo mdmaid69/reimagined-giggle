e77f6a5ffadf8ca83eb134f80c71d747eff35e8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:08 +0000

    grow artificial handle deserve hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
