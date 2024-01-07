5e1907c4a81edf0d8963d56277d53e78fdaa1e86
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:25 +0000

    anywayanyhow though father machine bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
