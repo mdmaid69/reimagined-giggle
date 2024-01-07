0d7e3a8deb807d145b0fb248bc2abda905f590af
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:17:18 +0000

    show takeout square crash breakup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
