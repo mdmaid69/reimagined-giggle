a9ed399d775ea40a91d2e3158d9cea8770c76ca1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:39 +0000

    remove compete top action month

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
