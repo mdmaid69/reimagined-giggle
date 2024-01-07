541bfd67500e1f44741cd990c6a69a0747b7aa02
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:45:45 +0000

    walk boil recommend ordinary guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
