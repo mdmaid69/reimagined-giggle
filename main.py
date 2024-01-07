03bb4f582f3973e0df44294e6c777a2d51b77fff
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:49 +0000

    swim remain suggest possibly genuine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
