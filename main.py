bc96948560ebfd3313dafc79d27d993096472a77
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:04 +0000

    wave improve instead range survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
