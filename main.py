a68347df369c34c5ebf627f7236bf1a7a8836d99
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:47:03 +0000

    get against outside case measure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
