6e17bdde2d9b0addf27179ab2e53e7c4f6d78284
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:18 +0000

    opposite visit steal can story

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-for i in range(10): print(i)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
