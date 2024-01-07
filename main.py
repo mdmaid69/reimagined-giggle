71c6a2deb995c7c8d5399ae8d26b84fd9150a15b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:30 +0000

    wish disgusting any anyone there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
