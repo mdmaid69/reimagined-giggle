e5cbbf96dc536c74eb4274bbd8113b4fe8338db6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:05 +0000

    guess blow when trap argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
