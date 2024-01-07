bcec6755e4a2f610241de395eeffac65d6272e1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:44:59 +0000

    mistake roof cause prefer fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
