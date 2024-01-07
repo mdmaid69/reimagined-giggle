75c6b9b69dd1626dbe75866a225b23a7e1babaa1
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:33:17 +0000

    smooth mess related calm bank

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def sort_list(lst):
+        return sorted(lst)
