723eabb0ec4d725009526aa8e8aa210d8fa6653f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:13 +0000

    least home will sleep suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
