a4528966e55ad18d6a7c2a731efde878d508a9b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:11:17 +0000

    brief loose circle mass wet

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Hello, world!")
-print(sum(range(10)))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
