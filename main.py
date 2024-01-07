50a951327f3b18786f16201a1bd8866842f91d4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:03:42 +0000

    hope 179bite dead mother thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
