c53f750d30229c99127f905a9dc5955aa0cea532
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:23 +0000

    wave satisfactory preparation sign approval

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def get_current_time():
+        return time.time()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
