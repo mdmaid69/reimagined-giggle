ec13b01aa69772512dec463289783d05929c6279
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:44 +0000

    touch design nowhere runinto short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
