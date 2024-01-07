5ee46b613733848015cf69de33d674619458db5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:24:49 +0000

    carry sink radio uncertain base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
