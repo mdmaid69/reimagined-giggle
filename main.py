0cab0de80c0ecd31f1629208e9813c5b68b2393e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:16:21 +0000

    slice sky order before return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
