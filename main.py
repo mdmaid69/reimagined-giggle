fecc7e9c69e608c888a8082464a0f29b13dc72d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:13 +0000

    watch responsibility final basically copy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
