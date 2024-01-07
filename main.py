2bfa7595df9fea6c6a99f8c523ae8dadad2ce89f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:54:51 +0000

    satisfactory even position laugh impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
