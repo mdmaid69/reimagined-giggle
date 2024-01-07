9371a7592bf72e153164d0c219529132da2b5216
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:10 +0000

    love destroy basic persuade instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
