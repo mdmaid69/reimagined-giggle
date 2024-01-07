fa2501be18955f121e486fcc59476ded5d532977
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:45:37 +0000

    ordinary would truth relation rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
