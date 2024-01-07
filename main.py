adf3ceb7749ef1fdd00e3c58f28a663945a33f28
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:51 +0000

    list business schedule practical occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_counter():
-        return collections.Counter()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
