5bb6893587b6c350574b55f6c4cfba06ff88c2a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:36:36 +0000

    direction rub cry mystery birthday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
