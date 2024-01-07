101e0013b6f71184c8238af5fc27a0e698582bb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:29 +0000

    government bet organization hit react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
