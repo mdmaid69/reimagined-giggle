123fa96ee7f81ead95df5df22e9c99d526f59793
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:31 +0000

    description because shopping recommend cause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
