2fa234e3a484019d6c4d4136df8516bfd83a2d24
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:05:36 +0000

    greeting as factory callat bank

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
