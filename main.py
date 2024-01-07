790a6c7e09a00420c9e212a52c26fe35001f83c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:48 +0000

    road Cc keepaway software land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
