8b146e35000d5f81f07163f3f17891dd342f8be9
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:30:09 +0000

    colour pass frame breakinto fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
