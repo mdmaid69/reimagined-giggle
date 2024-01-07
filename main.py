c1048c08a1427c7785cb67326ba6e0dc865e022f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:03 +0000

    thank dangerous polite guilty Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
