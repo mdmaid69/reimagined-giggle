75e2afacdfe3cf260083e4137e6b414b83dcf2f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:19:59 +0000

    last lean moon memory up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
