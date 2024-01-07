0b3dce6638e541ef49ae626ed026856cce55ead8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:58:26 +0000

    woman ability argument depend thorough

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
