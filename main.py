ad71a42012cfb9050b952d7b5596cc2a5a4ffba5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:13:35 +0000

    roll stranger sweep manner refer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_density(mass, volume):
+        return mass / volume
