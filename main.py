7597061956357170f775d3c256edddf9a596257a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:36 +0000

    fork deliver for speak nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
