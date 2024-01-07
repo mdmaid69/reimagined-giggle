95b84bf198a256eff2562aae7cb616caf41ec5c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:14:08 +0000

    university run operation range needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_as_set(array):
-        return set(array)
+def set_array_item(array, i, item):
+        array[i] = item
