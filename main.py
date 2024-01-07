c895372876c108dc0423fc2ed6e3b6e1f163a833
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:29 +0000

    problem praise test as meanwhile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
