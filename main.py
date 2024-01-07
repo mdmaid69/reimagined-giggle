2a352d9fe193df40dbe6ec5718c1213e030f9277
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:48:07 +0000

    yet uncertain base might motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_tangent(x):
+        return math.tan(x)
+import getpass
+def get_username():
+        return getpass.getuser()
