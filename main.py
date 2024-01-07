78105a6d6e6b1f441f1f7ed3b41c9ef8c2e5a053
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:02 +0000

    floor peaceful tip 179bite window

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
