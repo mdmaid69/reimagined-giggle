7d653c5624ab87683382775dc9e3a8d62b508cb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:27 +0000

    able I runover trick keep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
 def convert_array_to_string(array):
         return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
