eadd311e38844faccd1cb1f688eabd76aceda717
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:01 +0000

    spring niggaacademy anything connected nose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+for i in range(5):
+        print(i)
