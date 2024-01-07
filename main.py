7545d2ae4fc60217593e43a935789c8f10c897ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:53 +0000

    regret change passage call soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
