1f1e127dfa83231cfad759b716f55f663ce5b115
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:48:27 +0000

    crowd bang case news steady

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
