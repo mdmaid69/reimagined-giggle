0a10840a1423389dbc0c2abd894a8038107f4549
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:56 +0000

    frightened education slightly length disappear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
