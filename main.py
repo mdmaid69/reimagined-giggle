39ceaeffc09a0862f6f60fd35d812b2497cc4ab8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:38 +0000

    downstairs swallow disturb laugh ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import sys
+print(sys.version)
