559aa44399ec2e7fc5be8d47f772a5e017cf4578
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:44:18 +0000

    pin happen relax high swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import collections
-def create_stack():
-        return collections.deque()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+print(os.getcwd())
