cf2df80cb264d97d2cb45e5314bb8fb529405ded
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:10 +0000

    up worse same emphasis keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def subtract_numbers(x, y):
+        return x - y
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
