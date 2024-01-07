20cc315ebce87a214a4c1d4480c2e0d581a1d810
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:28 +0000

    somebody wind tie insist hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def subtract_numbers(x, y):
-        return x - y
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
