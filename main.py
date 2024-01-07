5b229c0eb5e4b62c399d9e2a4bef0ed09c8a69fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:24 +0000

    giveback piece hold criticize add

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
