acc73262ea1f21c22ebf4d1ba1e003f83ec12f62
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:08 +0000

    equipment full we character boat

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_list(lst):
+        return lst[::-1]
