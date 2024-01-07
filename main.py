ccdc3771dd2b593f22e3a5472804375f03355054
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:42 +0000

    content will impossible spring cum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_current_working_directory():
+        return os.getcwd()
