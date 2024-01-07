2fc976c1dd5a9108fb9533856821bbe677142810
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:11 +0000

    broken there embarrassing arrange praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def divide_numbers(x, y):
+        return x / y
+import array
+def get_array_typecode(array):
+        return array.typecode
