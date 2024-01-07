6132c3b52e231036a57224cb7e722b2d7a2bc818
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:46 +0000

    Dd thin effect limit easy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
