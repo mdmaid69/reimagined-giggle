abd546170c0ee5d5a8ba918234def13c2599329a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:41 +0000

    rest watch do apparently difficulty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
