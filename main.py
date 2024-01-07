5dbfc9d6b4991fe1e289ea1b05058afc31dec6bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:10:07 +0000

    stamp trip remark size advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
