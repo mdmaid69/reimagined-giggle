abd3da84390ac1434e08bd24e815098b7997626c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:33:54 +0000

    settle confusing plate pretend part

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import collections
+def create_user_dict():
+        return collections.UserDict()
