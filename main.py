80665ac74a3b3f6d1af7c3750a746e24eba790da
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:52:33 +0000

    ashamed mark keepup cause comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
