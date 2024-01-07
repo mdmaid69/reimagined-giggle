648fe2f4ea1a5e682f5d08bf96b8dbc41be77858
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:01 +0000

    backwards suckme defeat level work

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
+  def calculate_area_rectangle(l, w):
+        return l * w
