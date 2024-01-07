d5bdbfbd3dd09c08ae78b276333bfc4d7fb5dbcd
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:21 +0000

    yourself realize pass respect rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
