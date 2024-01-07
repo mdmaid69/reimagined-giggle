69017c4ce764ef617ed3088c6678098415b94961
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:30 +0000

    distant push fear neat before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
