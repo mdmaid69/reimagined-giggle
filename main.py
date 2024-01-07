60593b2af5bda677661ab3cae4d6150e0f47787b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:54:33 +0000

    workupto invitation Mm emphasis part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def is_palindrome(s):
+        return s == s[::-1]
