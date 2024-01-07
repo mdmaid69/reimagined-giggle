158f4fcef0a5d84064a467ceea75714794c69e4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:33 +0000

    tie class base compete suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
