9fb4b2d828f91f52de57ef6ba4491ccc23561bf0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:54:12 +0000

    depend wind dig regret influence

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def remove_duplicates(lst):
-        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
