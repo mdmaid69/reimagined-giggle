12d58a3c3d2ac9773d66ea1b9a00b0d916186270
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:04 +0000

    makeinto affect us female main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
