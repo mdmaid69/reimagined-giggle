22fe2dda3d8ced057638a0f50d7b052ee21875b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:21 +0000

    return violent shame come familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+print([x**2 for x in range(10)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
