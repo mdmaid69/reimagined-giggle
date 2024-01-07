494af338beb2faaba0236afd74c809a0c65d865d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:33 +0000

    furniture repair back clearly limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_max(lst):
+        return max(lst)
