c1de42918f2a9e74eb7440d33ab6b2711c35f11d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:07 +0000

    character the plenty dark steam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_density(mass, volume):
+        return mass / volume
