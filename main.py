3ee618ecef9ec453318d2e43d2de37262e01c18e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:17:41 +0000

    drink dull hot able deliver

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+for i in range(10): print(i)
