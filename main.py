1737fdc9d3c1a7ba804d29f4017187d24754856e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:21:00 +0000

    interest treatment fairly believe soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
