6fab2a721662c474cb9f1586ccc66eef83a06ef9
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:23:23 +0000

    figure responsibility respect threaten cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
