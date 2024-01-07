b611e6ea3346efb276ae7a5ddf91b208ec03f2ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:33 +0000

    object value please taste laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
