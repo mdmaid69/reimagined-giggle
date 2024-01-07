528ae8066f3ec21a7e2b798cc0704a56d0cbe1e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:45 +0000

    Pp design else excuse design

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
+for i in range(10): print(i)
