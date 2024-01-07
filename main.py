1cef7d6f530388b144b400a03fbcc725ecc7bf59
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:38 +0000

    thin bored movement hair breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
