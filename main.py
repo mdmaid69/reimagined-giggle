6897470a538fb70af84118377c32c02ba2a4e84d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:56 +0000

    nowhere snow terrible their computer

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
