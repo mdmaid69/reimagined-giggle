0407057c79024b8abbc8321a4e4c2177bec78009
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:34 +0000

    hole play getaway action promise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
