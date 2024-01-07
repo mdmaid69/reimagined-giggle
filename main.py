fc5621f93ab7cfe99ed3ec5f8461b301133f6101
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:11 +0000

    anything accept near work result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def create_queue():
+        return collections.deque()
