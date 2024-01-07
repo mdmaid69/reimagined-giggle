6f6e1684538a0b47d255a70a4ab6a7b69e15795a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:04:06 +0000

    go fear Vv listen relation

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_power(work, time):
+        return work / time
