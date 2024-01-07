8f60ea1838b5bc1540f8b6816ad205c5f8212f83
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:37 +0000

    nerve indeed block first slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import sys
+def print_python_version():
+        return sys.version
