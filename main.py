6e1a4eebe786fe39b22701eea14dc101c141a3f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:32:57 +0000

    apologizeapologise head better spring why

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
