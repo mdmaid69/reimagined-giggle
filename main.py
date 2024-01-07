faadd29ff13450f6d49743f85f427d6d6fe22a44
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:11:11 +0000

    muscle treatment half last ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
