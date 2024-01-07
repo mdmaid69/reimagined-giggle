e9c03dbbf8cff0350d401047cc6a7cbdd050bc0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:21 +0000

    over work possibly payment before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
