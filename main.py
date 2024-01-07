6d9d57cfa6e9f0bf90238f7c77b07eef594ae71a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:19 +0000

    achievement birth couple machine figure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
