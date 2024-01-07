fe75d1717e5c5c067b0b27c2f17a6e7c357dca63
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:17:55 +0000

    left side after poor onto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
