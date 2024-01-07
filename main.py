20065504f9d1f98115ccaa21699b9cd78c07b8e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:28:44 +0000

    community interrupt drop length sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
