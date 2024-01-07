b51d75b87aa3a3a6e827fd8d098affdfd0adc680
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:50 +0000

    Uu typical ticket back whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
