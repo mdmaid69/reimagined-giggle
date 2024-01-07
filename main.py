118892d62047143eca17392cc3ff69ef9fd0d1f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:14:04 +0000

    creature ago press spite shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 text = "Hello, world!"
-print("Uppercase:", text.upper())
+print("Reversed:", text[::-1])
