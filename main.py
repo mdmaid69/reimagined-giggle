8d20eada404be2f35fb37e0bc07ecb47a7b956bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:49 +0000

    thanks content Hh convincing steady

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
