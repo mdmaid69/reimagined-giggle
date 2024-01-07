c0dee521046b5804e6edf94d5b1646b1e70cf52b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:17 +0000

    ocean to guess cruel fill

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
