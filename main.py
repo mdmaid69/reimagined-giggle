48599ec5d44680d5d9809f0ddbc0ebfbcb596a64
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:14 +0000

    equal maintain watch comein value

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
