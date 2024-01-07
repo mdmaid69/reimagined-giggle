53c750a76101b16b64931f2549482af1e4577324
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:14:56 +0000

    fit keepaway of bag tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_as_int(array):
+        return int(array[0])
