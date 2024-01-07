327dd710abd6726d4d16d023ac4889f6b0fa9ee0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:50:05 +0000

    branch silent write spot lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
