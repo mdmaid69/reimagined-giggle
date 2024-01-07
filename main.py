ea65e6952c0b896bdcd4e55062100ef6af140e9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:06 +0000

    surprising bringback middle thick forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
