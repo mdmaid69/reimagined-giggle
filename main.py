b0568ebe6d1e10b2edcdd0ca6d53ec927f3ad515
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:58 +0000

    kill keepoff afterwards maybe makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
