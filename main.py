ec685148c0af90d5cb6c1b2c7f432971cffa07b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:27:47 +0000

    learn leave promise imagine light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
