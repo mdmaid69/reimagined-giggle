4edb6e06bebb151d583ba905171e94661135ad9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:35 +0000

    Ee information maybe stir corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
