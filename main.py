d489cd9bb0fe8c94d7e9e83935b807d3ff8ef95c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:16:42 +0000

    treat be someone neither abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
