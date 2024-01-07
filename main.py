bbc66fb404aec7ae9ad6b94001f77f84d729f6fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:59:00 +0000

    alive whatever turnup reserve 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
