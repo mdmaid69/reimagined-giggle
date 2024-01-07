dc0bc342549d8762043431d39a4c7b337aff5c44
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:20:22 +0000

    shut church attractive book light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
