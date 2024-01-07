131c26bdd0a140203f98b38566ed4138e168bb87
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:54 +0000

    lean closed leg callout frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
