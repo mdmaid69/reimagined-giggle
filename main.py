a8cef30d79a6fb55a1579d76fad3d1b9d6475629
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:17 +0000

    giveback body small confident just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
