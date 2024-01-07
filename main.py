45991c717af7801e61517d7714a72cc3d26b6776
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:02 +0000

    drive bunch medium block bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+print(math.pi)
