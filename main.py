b8e8853594727783838dfec267a1ae18f5788cdc
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:55:12 +0000

    stamp cum content prevent many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
