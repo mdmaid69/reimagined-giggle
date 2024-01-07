d4043609c1366ba5ca7e9a19d2ecbd57b7e80e06
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:49:09 +0000

    vote replace by bringforward demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
