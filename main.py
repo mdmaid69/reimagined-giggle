2d96276d677c86e093d27f2eb37df5a7165b8235
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:04:43 +0000

    slice fairly dinner actual anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from collections import Counter
-print(Counter("hello world"))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
