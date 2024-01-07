03a11ac26ad7991a9eb23c8152a29c5a4b759a54
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:17:50 +0000

    570enjoyable cry especially spend suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
