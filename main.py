f2f61904bf20ccb9b727a0d97126df7e083b7fd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:48:54 +0000

    peace door tour mind content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
