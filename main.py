9656a68f033fe78e15d79dfe4ba76982c087afdf
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:29 +0000

    pronounce town pollution stretch communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
