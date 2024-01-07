3e7bb4684eebeda37daa34153895227043882cd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:05 +0000

    offend insist turnover characteristic practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
