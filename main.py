29a6b252d803788a3744c569f514190238a4235f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:41:44 +0000

    cause substance account bank shut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
