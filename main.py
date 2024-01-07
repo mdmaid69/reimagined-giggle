fb454ad0fd6589f0c29fd22e1b8b1a7ce64aa243
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:24 +0000

    jump lock police runinto underneath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
