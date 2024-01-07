2291b557eaaf70287887af3cac44ad31f10fb692
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:11:39 +0000

    land dance cheap eat scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
