2bc8d373f3357b84c7579e9cfbff3af31e667e4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:02:33 +0000

    nothing offend after secret match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_max(numbers):
+        return max(numbers)
