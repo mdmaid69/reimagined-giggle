39aaff8320ca71221cf84394d08891da546d3ecb
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:50:48 +0000

    myself trip commit fork case

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def sort_list(lst):
-        return sorted(lst)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
