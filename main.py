6a2305ecdb558c0c0e7b9a673224d67b48eb692a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:09 +0000

    crisis temper person false achievement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
