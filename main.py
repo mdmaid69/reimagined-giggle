3228a47626ae7b9b7f9054eb77312443ed4f8d97
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:07 +0000

    Ww rare effort shallow tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
