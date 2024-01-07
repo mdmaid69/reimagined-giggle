e45b31d9afc79a81dac4980d76f40266f181f289
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:10:03 +0000

    creature pollution holdout himself height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
