2d86f058c5323525f1ac9d42a6296909213bb120
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:03 +0000

    beautiful somebody badly slightly release

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def subtract_numbers(x, y):
+        return x - y
