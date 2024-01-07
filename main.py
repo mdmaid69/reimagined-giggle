37c06c2d0b47c8b94bfa7a65a5ba0c3fd351a986
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:09:21 +0000

    delicate remark total lookafter unlucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
