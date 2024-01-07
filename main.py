6834c75a21f8eef331548a3e5c3bcdf660d707b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:20 +0000

    chance bringback sweet position measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import platform
+def get_os_info():
+        return platform.uname()
+import platform
+def get_os_info():
+        return platform.uname()
