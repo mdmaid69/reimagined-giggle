3e4b34f10fd3576a5b40463eaf4e75f4f22bd13e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:13 +0000

    snow add instead thinkover war

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
