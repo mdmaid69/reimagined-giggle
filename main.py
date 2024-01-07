6b63bf252f77451147d75da05a93ad8fb9199fce
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:11 +0000

    success equal feed difference lean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def convert_to_binary(n):
+        return bin(n)
