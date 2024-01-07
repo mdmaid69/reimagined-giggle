ace93e158c5271925d9262d34ec8dbcfa68d569f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:00:14 +0000

    plan row dinner justice thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
