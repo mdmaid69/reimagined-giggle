6fe9b0c74333b145c22c6a4ce36f3601297bc4c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:26 +0000

    anywayanyhow crash race thinkthrough sight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def reverse_list(lst):
-        return lst[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
