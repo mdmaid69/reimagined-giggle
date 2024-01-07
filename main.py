1e566310fcba95491fafd0ba92cd63587904385b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:21:48 +0000

    exaggerate bend then big compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
