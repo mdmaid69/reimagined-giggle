f73e1afeb12a304d9e161dc8f1b5ad53b3272ee2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:24:17 +0000

    march unless movement spoil burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
