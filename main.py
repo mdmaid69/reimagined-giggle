a6eb4dd917a0203f0200cafdb362c1c6b881e298
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:07:50 +0000

    regularly congratulations direct burn snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
