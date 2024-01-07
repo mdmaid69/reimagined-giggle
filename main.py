000a24c2e522a3d0206e7a58b1a78532c0d71881
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:13 +0000

    car pile none expression vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
