7802b7fdeb5e99ad46381a6ded52e72be0779237
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:19 +0000

    far change round programme beyond

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
