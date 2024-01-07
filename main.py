daae7b0ab486506b552c1e12471667ad83cd75c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:05:33 +0000

    somehow look previous standard otherwise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
