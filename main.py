cc0be136f47d0ad62cc96ffb6000512fe7ca13f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:46:28 +0000

    recently suit common stupid high

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def find_min(lst):
-        return min(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
