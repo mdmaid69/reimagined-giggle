bfe0c7697cb1cf41efa0fd2faec4ea83b924c705
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:02 +0000

    over describe harm getup hide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
