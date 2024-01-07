f4000c8ed6558fabe70ac608ddf8c5f04b238564
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:51 +0000

    wing combination thinkover wonder Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
