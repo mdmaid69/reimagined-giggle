da7aea455d39087cdafd999e039d22c22b5e67e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:26 +0000

    however organization anybody gentle shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
