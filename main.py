246770f32e3badb8ccc661af0009b23421053933
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:43 +0000

    certain distant completely perform month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
