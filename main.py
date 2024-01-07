eb4b7bda5069aa08eba20905c086ec63b7bcf96c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:04:20 +0000

    nothing lower pronounce shut noone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
