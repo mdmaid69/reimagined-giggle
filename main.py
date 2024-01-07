2edc4625443f75414f4950d5fdf6e84039655d83
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:11 +0000

    control surface than loss anyone

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
