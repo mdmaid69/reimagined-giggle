f3a25c0bd00d272b525d58dc8e5a1c65f41b6576
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:48:56 +0000

    doubt pain 567enemy suppose someone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def count_words(sentence):
+        return len(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
