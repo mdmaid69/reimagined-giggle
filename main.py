d517ccb8a2bc98f029277a5c22333955f86ffa9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:16:17 +0000

    head exchange ever bye beat

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
