595bcd7e31bd7b2c2a41ae04531362394f0877eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:13:25 +0000

    discovery slight university appeal past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def count_elements(lst):
+        return len(lst)
