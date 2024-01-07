1bf1a038c903ee1567483179524d0f8d7903aa99
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:58 +0000

    slice set open false operate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
