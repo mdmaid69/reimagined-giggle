9a71f34b242a4f5ebb465be82b0287dee12fa229
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:00 +0000

    avoid clearly maximum situation repeat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
