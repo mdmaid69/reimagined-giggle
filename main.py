77095b51dbb405813b074f8f3a05dfe9564c6af8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:09:13 +0000

    speak far comeup content tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_list(array):
+        return list(array)
