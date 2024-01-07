31eb0b363c706b63b6bfbd7dbca05e0ce1d9fc4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:47 +0000

    forward soon comeround contrast people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
