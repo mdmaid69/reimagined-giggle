85bff2ab8bc9dc03261d1dad56bcf0ba7d1f5f75
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:28 +0000

    mass only use central twist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
