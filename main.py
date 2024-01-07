97a41661321000090f03db76b7ac904d107285b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:30 +0000

    year money improve pay colour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
