d6e061a27ccea3634c6cbebdd4884ad081b75d8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:16 +0000

    fine surface official warning unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+print(time.time())
