40eaf38e458a5be0d3eedb92a5a49d3464fecaf0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:10 +0000

    least praise man recover see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
