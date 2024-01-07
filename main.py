936aa27d9ad24ba8d1c56de887d3dd722649640c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:35:16 +0000

    priority blame Itself back exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_factorial(n):
+        return math.factorial(n)
