211a3f4b6b62c65dc1372dad93596ab50bbef06b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:15 +0000

    communication strict provide goup nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
