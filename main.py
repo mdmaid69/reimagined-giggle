ffc86b234bb70f2b19c91b891c1357dd441578bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:47:07 +0000

    geton valuable getback blame figure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_set(array):
+        return set(array)
+def add_numbers(x, y):
+        return x + y
