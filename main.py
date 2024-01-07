71a32b22dad7269a94ca5dbccff6b1a3e3d71325
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:29 +0000

    to hot different prepare next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
