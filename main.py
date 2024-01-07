fd8302483f9d2b3ae26e22a17a1e8539ada4a80a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:26 +0000

    dangerous nervous consult finger building

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
