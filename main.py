58c9e09c0fa99b98a0b8f82630490445d4c345cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:04 +0000

    hold check object later advanced

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
