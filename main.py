03d9fb57c37881002ebe9c1480fe549bfe088d60
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:23 +0000

    fashion protest include lift country

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import platform
-def get_os_info():
-        return platform.uname()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
