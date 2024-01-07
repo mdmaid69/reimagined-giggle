4deb31d76470fc3dbba196335b874c0bcf5e198f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:29:55 +0000

    exhibition near ordinary possible strike

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_set(array):
+        return set(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
