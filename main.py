a12c9aa034e9461d8d14bd92db93f4193e0502c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:28 +0000

    culture border anxious mean escape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
