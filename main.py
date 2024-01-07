f20189c33b277987f1b97e13ebf274a1c8bbe77e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:54 +0000

    write section belong fat case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
