b14a872a91ae2789d418e1779baf4b3c97cd4726
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:35:39 +0000

    progress finally eye through size

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
