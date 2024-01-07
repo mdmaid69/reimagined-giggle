fec95d74561a5dbbe2d5484e80e7f60df6720fa3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:06 +0000

    describe study she both right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
