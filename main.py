a9a71357ec1a8ed2b5965e33a44ac9cff4d6d17a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:22:05 +0000

    trip persuade thank cheek perfect

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
