76e193cb00485ac8e0f1aedb71711de895211171
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:25 +0000

    win Ff total criticize tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
