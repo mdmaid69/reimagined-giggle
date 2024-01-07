b2f851f901ef2f20cef1564e7b66982f1a34a728
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:15 +0000

    ability authority whatever Yy closed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import re
+print(re.match("h.*o", "hello world"))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
