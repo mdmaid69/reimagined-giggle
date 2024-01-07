4af58fbb8186f9426f8ab86857f09ad2b5b961ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:32 +0000

    attention pleased unkind keepout rare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
