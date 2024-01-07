5cc1a960f8f893bd206607097f68c9cb3616b34d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:32 +0000

    adjective feed land transport matter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
