85e0b14cec37b41ab314f37abd46d75be5e1edc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:54:58 +0000

    history about its friend entry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
