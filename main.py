930efc0e6a274b500747a1ee940a5516bfa3fc8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:57:11 +0000

    claim plenty evening ear smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_item(array, i):
-        return array[i]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
