89125ec24b8773d2df2ebb14f33d01eb15fb6c41
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:11 +0000

    check workout sight occasional right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
