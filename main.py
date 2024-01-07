7348fa77c183d0e0c752fbe72d5955372ad434cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:08:57 +0000

    bridge interfere communicate few concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def subtract_numbers(x, y):
+        return x - y
