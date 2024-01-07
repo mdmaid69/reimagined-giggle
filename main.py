ad259599404afb722dc8005dabb19aa2e8a56a2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:36:05 +0000

    needle unlikely regional rubbish minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
