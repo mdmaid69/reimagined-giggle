601490c348e16809f531edf5ee294e710aa38089
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:59:33 +0000

    from pool afternoon search translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
