bcb13d4ab56181aca5891834ea557d622921dc15
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:01:50 +0000

    share aim definite average deserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
