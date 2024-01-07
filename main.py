270e0c39ac9b598d62b2fefa03b123a0324087e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:50 +0000

    hope rest recognize rank picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def multiply_numbers(x, y):
+        return x * y
