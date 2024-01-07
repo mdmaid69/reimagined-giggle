65ade99d2904c3216682396f590f6eb8437bb6d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:38:25 +0000

    dress unhappy or join feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
