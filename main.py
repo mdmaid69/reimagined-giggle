ee61036a399647b8c53b30038f6c8da06120b997
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:10 +0000

    dish importance joke form central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
