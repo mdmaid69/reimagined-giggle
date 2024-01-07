15b0af41b79288765b1f54b905419b2dd303a48d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:42:19 +0000

    replace stamp Hh wire you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
