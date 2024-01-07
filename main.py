bc9f3d3402d6ce695d66d513bffdb0cc7d570cbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:42 +0000

    trip may train piece school

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
