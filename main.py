21da5af7af3027e7178a20358bb7a1775baeaefe
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:04:08 +0000

    lookaround flat switch point dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
