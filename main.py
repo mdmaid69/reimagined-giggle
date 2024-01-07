ebe4ab09794a25ea64ef0a706e9cc5e4bdd4fb7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:44 +0000

    appear it getoff computer pencil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
