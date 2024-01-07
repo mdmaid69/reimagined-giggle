96b7b15dafa2f9f08b1be811e5e2741d04a9c2a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:32 +0000

    tooth now touch seat lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_list_from_array(array):
+        return array.tolist()
