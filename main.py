62ff60ffd5c86afab81a288e2d27862d274bf9fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:22 +0000

    snow injury aware tired breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
