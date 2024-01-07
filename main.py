0b81e682ec2125e2e6254bb4cb5a0181ee6dd206
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:47:11 +0000

    march thing worse holdup himself

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
