3ee38a6da2156e798d302febcbbf5267ba7a9fdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:13 +0000

    harm convincing hit at noisy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+text = "Hello, world!"
+print("Characters:", len(text))
