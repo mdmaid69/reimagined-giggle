1027938f83aa0eac9777ec46733589a28b42ca5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:36 +0000

    swing certain correction lead sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
