49854eb3ee9221fa00858d07037452f2e85715b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:28 +0000

    impossible much contrast disgusting thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def remove_from_array(array, item):
+        array.remove(item)
