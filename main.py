1e9b1d29e7365e9a461573a065378826eb8e854f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:53:41 +0000

    rid idea pain married contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
