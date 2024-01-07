f4c11ac0556cf0ef93e137ca98027a7b02e335ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:27 +0000

    sleep attempt fine rather recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
