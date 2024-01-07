ccd66baee3af9661882acd8d81a816c35a735df6
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:50:38 +0000

    nigga aim 571enough bury doubt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
