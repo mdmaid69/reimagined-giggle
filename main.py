34451b29c144ec22b3590dbb1c910bd718fdd439
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:50 +0000

    divide plan contribute murder crazy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def square_number(x):
-        return x**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
