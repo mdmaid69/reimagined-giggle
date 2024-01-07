d6d5ef1e7089923cfb2a530b492b6c5f547ab6f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:56 +0000

    muscle usual mistake plate matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
