82608fcd8ec54dafbcfdc49f46124686316cf2de
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:07:16 +0000

    lookaround weather 569enjoy window never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
