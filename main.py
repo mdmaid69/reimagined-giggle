24eb7ab1fbf8a9f78790f328da9412166b80edf2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:28:51 +0000

    bringforward true float justice soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
