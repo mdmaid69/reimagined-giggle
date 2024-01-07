d65aad70813adb9d8217c7a82c8037d25959d801
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:16 +0000

    obvious eat rain divide sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def cube_number(x):
-        return x**3
+  def reverse_list(lst):
+        return lst[::-1]
+def convert_to_binary(n):
+        return bin(n)
