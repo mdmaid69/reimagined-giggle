e8741d463cf9a6986cff805ea07bf5989746b7a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:20 +0000

    about whistle scale tall strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
