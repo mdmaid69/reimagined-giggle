84425508c86bd680b3e5cfd9f90acbd6501f4223
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:50 +0000

    repair force protest occur upstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def is_odd(n):
+        return n % 2 != 0
