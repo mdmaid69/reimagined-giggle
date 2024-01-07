fa9fc1f331ae01b3c56b34fcceff21df0e117949
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:01 +0000

    scream to culture dress skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+print(os.getcwd())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
