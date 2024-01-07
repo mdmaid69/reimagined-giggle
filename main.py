baf312b052f907b5589a75fc48652da8bdcad77e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:15:21 +0000

    force day brave moon recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
