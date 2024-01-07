de214665b8166b06f6fa1822483f336ea2986d95
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:39 +0000

    reason frequent everything occur pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
