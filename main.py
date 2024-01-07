c162de9c9ae50422eec86f08bf6045c7dd967ad8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:37:43 +0000

    country trap speed run ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
