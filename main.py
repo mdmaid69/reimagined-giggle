8cdddcddfb3a6d1e49a4aecbf93e58d25682dd50
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:36 +0000

    only neck for blame error

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
