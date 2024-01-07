639bf75803f2f66139ec0db10d21636bffb758a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:51:00 +0000

    talk first follow detail together

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
