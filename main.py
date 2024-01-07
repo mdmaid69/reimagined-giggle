ce54c81de47f1515a6bdf533c6344d6be5abda51
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:01:31 +0000

    ordinary deep say nothing frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
