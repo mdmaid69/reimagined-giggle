1cc3de7cc9c645227587ad1d37bcf3eae6a35676
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:12 +0000

    store me train small keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
