f0e72d08163c7f9b8582793717f65cdc54fd23cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:19 +0000

    problem wash project choose Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
