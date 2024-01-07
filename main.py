5bcd57cee63a8341a04b2eca81e59f7a579b8860
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:07 +0000

    reach politics none trick usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
