760d11efe3329d43753f4c6eea13d4eec5b8381e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:07 +0000

    extra around tape set through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
 import re
-print(re.match("h.*o", "hello world"))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
