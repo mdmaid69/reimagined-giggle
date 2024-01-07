2a959b5cebd16ec21a47e9d9f04428c62945f9d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:50:41 +0000

    lose taste under introduce keepaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
