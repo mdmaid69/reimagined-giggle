97a8ae4e9e8384716a5909f653e343c94f0ac3e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:43 +0000

    should represent wash anywhere but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
