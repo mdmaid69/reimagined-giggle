2d58a82bcb16208f0bb37bb02ec163b6c830010e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:41:25 +0000

    beat issue angry little point

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
