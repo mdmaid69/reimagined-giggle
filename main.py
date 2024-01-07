59728bced45717edb8d095dc10fc64dc321f2aba
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:55 +0000

    concentration university forgive spin violent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
