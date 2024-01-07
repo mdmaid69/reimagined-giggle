0090c483bf90a7de287ee9602ed80796992f04ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:23 +0000

    should that uncomfortable company murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def is_even(n):
+        return n % 2 == 0
