70a0afbf41761e80bd77a904495c767caa20b7c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:36 +0000

    flow to advise swing national

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import time
+print(time.time())
