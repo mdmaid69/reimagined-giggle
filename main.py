377898d25ece0c28f83efb8e9014e3476125cf85
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:55 +0000

    fat hard fast rarely store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
