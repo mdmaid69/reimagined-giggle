3babb9ae30c5df22c85f2f370efbb99b82863cb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:24 +0000

    myself shop bank turnon rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def add_numbers(x, y):
+        return x + y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
