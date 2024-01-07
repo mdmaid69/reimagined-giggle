5e50d156b647ee41977ef6b9f30de92f78c5abd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:18 +0000

    rest wash snow Yy opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
