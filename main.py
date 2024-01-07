6fd936ab70bee9076d811194148472704db5ded3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:05 +0000

    breakin involve makeinto middle beside

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
