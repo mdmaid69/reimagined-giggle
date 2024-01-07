185755b7a52a99ddf0dfaac92804eb5bb2901cde
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:29:48 +0000

    experience issue active occasional meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
