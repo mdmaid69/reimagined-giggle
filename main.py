6c61a68065817e92c406c1826e5692c0c7e30aa8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:49 +0000

    floor platform by many punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
