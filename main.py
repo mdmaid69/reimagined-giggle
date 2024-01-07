f82b3915b85f33abb04349f5bd653efadbd10057
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:05:07 +0000

    general heart rain approve uniform

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
