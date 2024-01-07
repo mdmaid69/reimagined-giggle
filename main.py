ac9518b146993304c860131c4ffbf0f2fd9fbd52
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:27 +0000

    worried lesson rescue direct greeting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def count_characters(sentence):
+        return len(sentence)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
