a574d1cc43d90ae44296ab939226da2e6ab5647e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:40:21 +0000

    average phone laugh card advantage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
