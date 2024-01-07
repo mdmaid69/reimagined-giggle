a4b7eae8bf42ba7d13e5050b66269aae4d61a7a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:59 +0000

    whichever interfere smart insist precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+print(math.pi)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
