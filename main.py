13bce54dcec49260d52c1dced5fc3ded766c18e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:27:16 +0000

    picture slide ill street accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
