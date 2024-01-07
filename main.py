f1acc1cccead819c1dd52b8bc34638fcbbfc6786
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:42:16 +0000

    regret cap unkind perhaps frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
