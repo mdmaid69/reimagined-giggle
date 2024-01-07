8fad9e69090a386fa3dfc9ecc52f5cc27b261921
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:13:16 +0000

    chance rush convenient report soft

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_area_rectangle(l, w):
-        return l * w
+def cube_number(x):
+        return x**3
+  def square_number(x):
+        return x**2
