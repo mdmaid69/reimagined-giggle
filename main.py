63fee647a0b4f29ad5276b995c2e6eb9dab2b6f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:22 +0000

    rely year off conduct control

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
