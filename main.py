c575fb2840868ba52f07c91126432d0839bba1f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:40 +0000

    normal anybody country mail sale

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
