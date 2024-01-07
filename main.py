342f9592c7d5c6eee071828ed3e07f5fcf019348
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:34:36 +0000

    smooth strike possession government then

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+def log_message(message):
+        logging.info(message)
