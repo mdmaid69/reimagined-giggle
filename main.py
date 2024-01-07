8c5b3c85cc21279d17ff10e21346a8c583db5131
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:20 +0000

    help lonely born announce close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
