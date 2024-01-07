21ebd922c3e6ca31c3e55a8d1e78ccde5d0efe9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:52 +0000

    realize sort cross 571enough suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
