6f02a58f77da1f719b37c9713125f210457c1014
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:08 +0000

    disappear single steep payment will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def convert_array_to_list(array):
+        return array.tolist()
