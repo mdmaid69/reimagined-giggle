aa147bdc4571f2d7dff6afb38095034b88302fa9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:00 +0000

    here him wherever communication only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_user_string():
+        return collections.UserString()
