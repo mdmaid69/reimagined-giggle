d7218f84f7e42d9bab7fe8ea1532f35ff7e289b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:05 +0000

    satisfied definite maximum turn throw

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
