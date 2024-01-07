811743ad51cf136dede5b108cad4cf70af28954c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:57 +0000

    state doubt broad flat company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
