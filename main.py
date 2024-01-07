b7e1370385857da21fd58eea64699a51764a39d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:28 +0000

    square from quantity above coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
