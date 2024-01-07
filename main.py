ec767801d62a331f3b80cab6926a84245f0aba38
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:51 +0000

    software communication watch ability back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
