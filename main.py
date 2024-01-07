a0cd41fe68452c95504e5c4197f8724e1baecb4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:35:43 +0000

    could shallow rank little empty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_max(numbers):
+        return max(numbers)
