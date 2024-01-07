ed4ed40a91267886c9ff6fd54ca87503ae3069b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:55 +0000

    environment none calloff Bb around

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
