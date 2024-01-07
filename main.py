57ca381f7455ed957d5c3ba58a89b2a49a1c3906
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:46 +0000

    cross ride pronunciation quickly brush

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import numpy as np
+print(np.array([1, 2, 3]))
