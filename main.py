d400ab8a516cd5986cd2f54b022a650574fc1840
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:58:04 +0000

    criticism bringforward wash shut add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_tangent(x):
+        return math.tan(x)
