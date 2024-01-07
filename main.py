38047cc37a150ecd6a950b5df486c5c2a5c94fe1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:19 +0000

    purpose finish once cruel first

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
