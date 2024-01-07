c8e78b13b96147cf17bfd310e50895060289d88c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:59:53 +0000

    about under stop worth unlikely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
