9e898f698e2453003d97abc99d43a29a789a6100
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:41:13 +0000

    measure dance speed normal punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def split_string(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
