4866f748823eea0e3ff8c475cf2171695456a507
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:44:24 +0000

    jump risk damage protect size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
