34977f3b2f4c97146a7b0ccadd6fd614f77268b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:13:28 +0000

    runinto family snow in realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import platform
+def get_os_info():
+        return platform.uname()
