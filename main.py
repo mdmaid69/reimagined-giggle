6e3e3528932a0d8d4980bff22bac55163c655aac
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:00 +0000

    very dish cruel regularly 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
