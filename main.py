4618e886fba78a9ed8463487d082b40576b4565a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:09 +0000

    kick earn major break coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
