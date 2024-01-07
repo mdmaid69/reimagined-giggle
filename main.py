07d4fcd74fd17dac2fb4f0e0a19b6b920dd2887d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:39 +0000

    rest sense fold guard food

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
