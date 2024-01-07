fb74da64f076936f46f821d9eb0eefcc4c3af00e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:00:51 +0000

    return instrument apply main scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_int(array):
+        return int(array[0])
