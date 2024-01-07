1a05b040a3b637f498165ea21cf4cd963f5a5643
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:33:36 +0000

    underneath real perfect action main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   def convert_to_binary(n):
         return bin(n)
+import sys
+def exit_program():
+        sys.exit()
