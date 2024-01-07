935acc972a6b391672e0e07a6bd1eb19cadcc393
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:59:47 +0000

    concern day hour Jj height

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-for i in range(5):
-        print(i)
+def sort_list(lst):
+        return sorted(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
