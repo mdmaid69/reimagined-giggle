6b1ad88614cf5b493550189fa1e1848a1c91ce42
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:45 +0000

    sky tip interrupt yet stretch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
