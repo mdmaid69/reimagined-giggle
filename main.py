020529b4c1b37ab2557a472923a3599f52540c0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:46 +0000

    rest spring special everywhere pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
