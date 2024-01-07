0bf66c31bab5ddc90f615c459777d1c54ecb2b07
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:09 +0000

    pump common sport bottom flavour

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_to_octal(n):
+        return oct(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
