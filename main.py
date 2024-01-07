6bb4b627bec7e397c24010d9cb8415df77c6dee8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:53 +0000

    able object stranger innocent but

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
