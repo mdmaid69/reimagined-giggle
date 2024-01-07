aac1dd7e8041e8ba5b6a12b36b0528060295b4e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:05 +0000

    machine put contact spoil eager

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
