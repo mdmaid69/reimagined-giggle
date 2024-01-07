db6f501841c0bcd28b7bb0efaea9ac3474161a62
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:04 +0000

    accidentally moral test lot after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
