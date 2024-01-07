6416577871c425c5248f8bf291e3f37a684b41f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:17 +0000

    very this last rubbish cheap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def subtract_numbers(x, y):
+        return x - y
