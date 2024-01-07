1e2b1be34a985896e1397c37cd78f676d25b03e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:12 +0000

    grow second father niggaacademy stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
