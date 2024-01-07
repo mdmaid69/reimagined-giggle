ed676a0191a870e8f9206c92f33e67bde50c62f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:00 +0000

    pair any ground lock appeal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
