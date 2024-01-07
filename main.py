337bcf2a9c1ffae59b974c99965556ca6c9b67f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:46:56 +0000

    film success vision turn drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
