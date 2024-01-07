a4e10566c4a5e9029a395f7d4e2c784657fb11bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:20:20 +0000

    shelter year vision harm stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
