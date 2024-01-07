e0b53035eb8b7b764824b3439188e231d28fe24b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:25 +0000

    any workout brief lower form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
