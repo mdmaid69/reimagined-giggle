6be57c14c7b731039dc6486284f7603a04fa92e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:24 +0000

    danger set best concerned word

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
