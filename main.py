dba73831fe0f3557871e6a20ffc7fec9680464ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:49:09 +0000

    frame rob concern hurt mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
