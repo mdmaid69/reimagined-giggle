24cf10231bc0e0039571303f08c9968b033c8d34
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:29:55 +0000

    weigh provide near speed goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def get_current_time():
-        return time.ctime()
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
