03fc77192da04e68122df3c37c429b249167aef7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:03:32 +0000

    do illness join cross union

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
