b742730f54a6c456a47ea935b2f7a8403ac57f7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:17:28 +0000

    display sensible bright unfortunate upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
