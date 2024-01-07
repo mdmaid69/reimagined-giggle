604cb855598c4e99866c3a1d8c82508dde4ae234
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:32 +0000

    knock make separate moment try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
