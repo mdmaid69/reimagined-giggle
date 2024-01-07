e7f6d8d1954eacd582bca62d439fbd9e4b4b0d25
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:31 +0000

    die muscle post drink getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+for i in range(5):
+        print(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
