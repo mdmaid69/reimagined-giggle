5f80601a70de8383dc1dd7dfbb4116b855adaba3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:11:52 +0000

    rely night speech top listen

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sys
+def exit_program():
+        sys.exit()
