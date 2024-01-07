7845ff58982b1675e1eb99c30060fe956aab93d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:47 +0000

    funny OKorokay pattern hair dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
