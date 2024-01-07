943c2d846398368d1265cdd5fd0442e41fac0df2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:22 +0000

    gooff period possible uniform now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
