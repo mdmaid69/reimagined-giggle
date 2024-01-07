2774fcd3c978e8ac853566bae2bcec4b20df91e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:34:12 +0000

    talk male describe back bringforward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
