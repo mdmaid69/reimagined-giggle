96f923728ef92f86b311a20a19f15eab047aa36b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:28 +0000

    success callback during present shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
