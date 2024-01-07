ea4bb3ab0cbadd9c05cd7a585c795dafa972087c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:14 +0000

    slide commit tip bush pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
