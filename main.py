49fa7ec5fe230956ced9b9aceea36ad5feb5b796
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:53:39 +0000

    stretch drink pride lay remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def append_to_array(array, item):
+        array.append(item)
