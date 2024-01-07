78b15c9d3a3f9381625cadbd7a3c6f11d5ca0a34
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:08 +0000

    circle well divide handle comment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
