678411caf2b5babfa9e0613b872cb94f8266ed04
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:11 +0000

    by keepfrom wall progress damage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
