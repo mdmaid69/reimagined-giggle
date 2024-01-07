6d5d69f9f372ea03ca3edac6620f3b01226dcb10
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:00:25 +0000

    machine comfortable although breathe comment

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import sys
-print(sys.version)
-print("Hello, world!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
