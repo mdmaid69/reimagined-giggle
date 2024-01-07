8a0636964be0f4121df3a99334ed3ce5ec357302
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:48 +0000

    within nowhere drop duty murder

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
