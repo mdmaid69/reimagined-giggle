a14c214ebd7f72f923208a8f53962b9b77e5232b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:10 +0000

    tie reserve suckme beat after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
