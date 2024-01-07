b8254bc8de9200fac33c7f921e0442eb4fad4dd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:53 +0000

    565end burst exit leave giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def find_max(lst):
+        return max(lst)
