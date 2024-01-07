632c54ec4548f8c99ae398bd0d7eb662b0b544b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:10 +0000

    sentence effort ashamed approve cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
