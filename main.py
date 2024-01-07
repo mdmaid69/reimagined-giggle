dedcb73e51c712f655fa4ee4c1849a1780cb2120
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:37 +0000

    mine practical runaway history provide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
