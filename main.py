6ca54fe77e8bee24a169e6c1bc01b7972163dadf
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:45 +0000

    difficult person head development mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
