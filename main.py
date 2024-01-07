63d86d61ad545dee3ac4b4fed706331736ecb187
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:02:28 +0000

    undo ear objection will classroom

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import getpass
+def get_username():
+        return getpass.getuser()
