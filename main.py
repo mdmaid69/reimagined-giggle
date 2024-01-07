ac3c4d6d2f437ac8a1d3cd3d0bb3fe6f832ddaac
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:59 +0000

    thing already see truth main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
