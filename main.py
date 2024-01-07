1a91e1415dcf068736fd57c4c28b977618db14d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:37:05 +0000

    mixture wipe type everything building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def is_palindrome(s):
+        return s == s[::-1]
