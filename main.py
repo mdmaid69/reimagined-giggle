3909738b2b95de620a534aa25093501acc519694
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:57:00 +0000

    mail left state chat 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
