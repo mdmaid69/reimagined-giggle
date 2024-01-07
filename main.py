1f95fafab2f3b57e39774b17f3f2bdad4f8b1fdd
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:49:17 +0000

    live interested professional arrangement workon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
