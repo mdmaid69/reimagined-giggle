e8a3175b3152a0874001738476737861f456c573
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:40 +0000

    we nothing successful project male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
