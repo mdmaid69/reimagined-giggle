8e350114bfec9ba3a4a3a6fe326f7210d932258c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:22:49 +0000

    whistle party like refer absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_current_working_directory():
+        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
