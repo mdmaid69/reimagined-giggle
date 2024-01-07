1a27384235ba206a5d289a3ac020eb92d4d7ea3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:25 +0000

    hit wire fine before thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
