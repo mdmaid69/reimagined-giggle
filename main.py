600c808989592956e2179859cd70c319a73ebfbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:13:09 +0000

    impression my takeoff name negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
