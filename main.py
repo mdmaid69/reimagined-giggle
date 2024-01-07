b5e5a8f0fb630d481b74df0957c5f6c2efeb2b2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:31 +0000

    average body pretty level talkback

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-import sys
-print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
