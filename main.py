d14f0c314570319926284ec76417339c173d315e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:43:46 +0000

    calloff offer sign number offer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def count_words(sentence):
+        return len(sentence.split())
