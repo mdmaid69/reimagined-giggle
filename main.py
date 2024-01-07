2eb059e2ccb26cd854487589447c736560af8bbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:38:38 +0000

    pay imagination deliberate less support

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def sort_list(lst):
+        return sorted(lst)
