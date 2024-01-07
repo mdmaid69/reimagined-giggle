4348b31eb992b02e1390b5de971d28ae3969a783
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:02 +0000

    sometimes wipe middle listenfor move

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_cosine(x):
+        return math.cos(x)
