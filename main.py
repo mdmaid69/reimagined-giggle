3c982957f009da324ae5a76df396f9fbdab2ddc8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:27 +0000

    state weekend forward at his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
