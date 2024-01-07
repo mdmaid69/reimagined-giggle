34d35fad94dffb84deb721ed00eb3f8f619cecfd
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:59 +0000

    wash smooth arrive criticize sight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_as_set(array):
+        return set(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
