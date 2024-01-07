a6f561e524171ec41aeb0fd823647c31416c208a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:56:57 +0000

    reliable trouble hope space undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
