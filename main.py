df625afbb3017f52fa2717f7f79584d623af5ddd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:31:23 +0000

    straight who week post basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
