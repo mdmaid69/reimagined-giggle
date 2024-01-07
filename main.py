be1ce3a619851fa2322b3a34f7c1044ddeed8dee
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:47 +0000

    manner stir television rule trade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
