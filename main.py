2e536074a23c432a404c1d20b24b3c2e88144e52
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:10:07 +0000

    here light invite used estimate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def reverse_array(array):
-        array.reverse()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
