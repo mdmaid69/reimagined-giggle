6a9ea60c4119ff131b4cb9628aff90c5a81c3c42
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:50:36 +0000

    transparent 566end lucky tradition criticize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import time
+print(time.time())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
