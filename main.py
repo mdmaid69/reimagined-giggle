5e760cc9989e6a42f8f74ff7bc2fed75c1767b11
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:31 +0000

    city cheat outside improvement swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
