3422cd5929c11830784cb1f3b3b1c83e076714e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:37:46 +0000

    explain accident pity level exploration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
