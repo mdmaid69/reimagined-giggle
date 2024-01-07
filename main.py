e591947e8cdb84fe4812a8aa73c113873564cef3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:40:07 +0000

    over immediate bite practical thanks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-for i in range(10): print(i)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
