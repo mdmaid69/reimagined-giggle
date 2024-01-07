ab0220b2f0175911ab7d8c09de413cf3501c69dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:22 +0000

    content draw she switch sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
