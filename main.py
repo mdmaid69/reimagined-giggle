932be339600385020021043c1ada65b87887a43c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:25 +0000

    workupto breakthrough raise risk curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def square_number(x):
+        return x**2
