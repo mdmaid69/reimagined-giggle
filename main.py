c823c35eb377c4b0e19a93827b68987609a9573c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:58 +0000

    practice could account feature bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def convert_to_binary(n):
+        return bin(n)
