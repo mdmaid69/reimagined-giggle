2366aee1b67f9547e49fcaba4074bcde12fc1762
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:07 +0000

    last only least giveout swim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def split_path(path):
+        return os.path.split(path)
