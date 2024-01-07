1f5aff854e2652492db1f7297e2ab365de84e418
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:57 +0000

    well dance sink collection underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
