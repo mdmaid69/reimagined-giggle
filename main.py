64a52d73473f6edfd25ac7c89b11cfa8beef384d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:01:08 +0000

    giveout this tall sale aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
