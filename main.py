4a323b604535d7cfba86895d49f4eb703bdadfcc
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:38 +0000

    getback show pipe bush fierce

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
