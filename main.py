f466f3ba92bed0df907eed040d13364c0c114369
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:24 +0000

    ear cut sleep safe contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
