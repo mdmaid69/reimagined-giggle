124b0ba97f833b39a3268d5f4570a5ae81f24465
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:56 +0000

    address avoid suggestion second mother

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
