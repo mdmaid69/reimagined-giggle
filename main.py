2ccb14891e4ef469e85d79eb109e1cc1da79c4b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:18 +0000

    raise case means border wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
