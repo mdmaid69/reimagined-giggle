7d042eebe0a006344f3e7d213ca17f87c5dc2472
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:09 +0000

    air if know regional share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
