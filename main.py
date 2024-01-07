7012a14c07add04d9d984c243eecb3db48d21b94
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:34 +0000

    delicate ill commit wealth school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
