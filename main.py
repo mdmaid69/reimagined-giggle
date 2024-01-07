1bf71dd99909bbac0a1933399ab81d18d938fdca
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:55 +0000

    cover put visit if so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
