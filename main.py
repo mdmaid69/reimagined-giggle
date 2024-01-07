8be4ba314cc8a97e670f29621c220609653a8da5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:50 +0000

    ride be division transparent opposite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
