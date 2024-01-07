31b18b9900b5d727f009b314c11f4e03641f161a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:07:22 +0000

    final broad direct opposite just

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_unique_words(sentence):
+        return set(sentence.split())
