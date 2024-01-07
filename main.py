09e65d5f244f19e75ab65776c9bb200db1fc86f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:46:51 +0000

    operate recommend pressure grade nice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import numpy as np
+print(np.array([1, 2, 3]))
