a74c4e577b85aa479d4beca54c0400d8668f8864
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:06:54 +0000

    congratulations recognize suck display precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
