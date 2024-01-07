9d8185e4d1a74d4b48bfbc4884ecb1a90de6afa6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:10 +0000

    blue actually fear uniform cut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
