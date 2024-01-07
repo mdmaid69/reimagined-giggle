d0b1280265c1cc4760d264570c2b7b8c693d966e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:48 +0000

    human too spot exchange cry

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
