a609cbee9f5cb177c737ef76353d7cb62d2360cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:41 +0000

    measure touch my anywhere heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_max(lst):
+        return max(lst)
