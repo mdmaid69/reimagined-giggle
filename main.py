066e637e2cbcb3fcc82258ecbc8d6d7e08e5cd79
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:05 +0000

    again steam breath port smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
