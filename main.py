367ac4a84d2237726304c71641efdd144617ef3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:14:55 +0000

    season giveaway flow science break

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print(sum(range(10)))
