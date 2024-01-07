7ed2f5ac8f1bdc6fc4c84114e5b0944d6967099a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:52:28 +0000

    onto activity major married exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
