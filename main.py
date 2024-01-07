7be6176b567b88f823141f4b4d48f65926a6ae5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:52 +0000

    child character too round shop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
