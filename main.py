e74f45ae558f2297ff6925131b700917ebab165a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:56 +0000

    surface wrap Cc sound part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
