1ccb45e4b1d295fc680e0e8e0bbdefd115673fff
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:18:20 +0000

    university march care another flight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
