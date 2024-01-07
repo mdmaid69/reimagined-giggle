bc597ca74b2e59d1c5e698455a122d0ba1c71b96
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:31 +0000

    hurry expensive trick used vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
