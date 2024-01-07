2a0d2c0ecc7994467f55404f49d84579de6f1c95
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:40:01 +0000

    joke exam birthday practice invite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+print(time.time())
