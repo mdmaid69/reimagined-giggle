3ba2a80d416d850d93315c2852114243d0ee4a6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:38 +0000

    number favour grade loss environment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
