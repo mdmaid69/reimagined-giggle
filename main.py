3ebb6ebf45358e66312d55e3792733e05db4a66a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:50 +0000

    cause mad argument central bringback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def is_palindrome(s):
+        return s == s[::-1]
