fa1dc364cacb19844fe4a60133db29c9c52f7260
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:56 +0000

    shop government loose friend you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
