5996542d52484a4d2baa2f3d11a611f208428e52
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:47 +0000

    sore attention lonely peace 564encourage

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-print(sum(range(10)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
