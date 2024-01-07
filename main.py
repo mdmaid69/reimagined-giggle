8aee5e2f0ecf7bfe8c6d8e04c768b8e5ab4a3ff0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:25:44 +0000

    tool upstairs try beside past

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def subtract_numbers(x, y):
+        return x - y
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
