b0e6a3fb5e268c11903375e90b6564c1b55105b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:08 +0000

    respect print rub who rate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-print(os.getcwd())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
