ea51cd8c23f63b86286363e67b20ae59e45d071b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:18 +0000

    bit busy guard that town

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
