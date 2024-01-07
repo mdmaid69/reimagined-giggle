4208c1c538175bd366852c75864c12410d5a3e56
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:47:00 +0000

    plant extraordinary wake impressed thank

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import sys
-def print_python_version():
-        return sys.version
+for i in range(5):
+        print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
