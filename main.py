e565d1ac0dc2227bb8a57bcf9bd486f3b99a0197
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:33:53 +0000

    spin science under arm succeed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
