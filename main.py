1ec01ff9dc981ca0615b4a16f1be49eaf92be0ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:41 +0000

    still sting delicate minimum measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
