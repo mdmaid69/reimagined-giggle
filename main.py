517a096dcf708e59db8ef54faa4debd4e88da679
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:49:25 +0000

    belief through greeting deliberate comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
