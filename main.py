4f50bd0e13e83420bcda68bc725d26cdad1e52e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:11 +0000

    sail neither road holdon turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
