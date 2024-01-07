3d5732d42becdd26cdd8d542958575b392cd7e83
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:55 +0000

    still blame 571enough pity classroom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
