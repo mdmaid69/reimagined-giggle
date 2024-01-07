2c6719ecc1207c262ade53e40c07894dc4bfd212
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:19 +0000

    boring port opportunity all far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
