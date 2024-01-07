61b42dc291b931ddad4d355e38cfdca1f1a29ef7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:59:31 +0000

    hear ball walk type day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def extend_array(array, iterable):
+        array.extend(iterable)
