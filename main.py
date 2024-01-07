2b11b40f5a96a5dd3c80c382184288be920865fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:32 +0000

    considering wave average laugh 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def sort_list(lst):
+        return sorted(lst)
