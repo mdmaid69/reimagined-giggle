c38ed7bbfad1641fdbe86abde67e28c28b30d242
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:59 +0000

    Rr cross then no blame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_power(work, time):
+        return work / time
