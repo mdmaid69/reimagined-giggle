c0939ac9925a420fded5e1a6b39ebefbd8c9cc8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:33 +0000

    sharp herself goon put live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
