8af2d2caa1326136ea60999cc7c04eff0c7d166e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:42 +0000

    man hand mother comment thumb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
