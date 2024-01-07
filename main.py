c90a343ee97290f090ef6ee1c82b4cc500d6abe6
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:56 +0000

    lot noise man ready themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
