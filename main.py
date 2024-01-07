908930011ee638d38b1bd74b7f9967bf1085a7ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:51 +0000

    school result keen lonely tall

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_array_item(array, i):
+        return array[i]
