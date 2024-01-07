1cbca2dbe34b5c9c4e48d72a3cd9b61d91cc3bfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:46 +0000

    sit keepoff us defend full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
