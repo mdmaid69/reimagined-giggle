2a9446e792bec1d640bcadfdd766fad8400805fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:08 +0000

    all force should goback(to) group

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
