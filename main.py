c764b833c62c9d6d6b7938fb9a05c67f4792f2b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:04:40 +0000

    practise drink comment lead importance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
