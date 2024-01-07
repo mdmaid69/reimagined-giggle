e9047e6e8e3d102b283dafb31416ab2c1e13be5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:48 +0000

    train calloff design test relation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
