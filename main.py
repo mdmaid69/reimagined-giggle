8a816e52e2bc64fc447101fbd936ff6579e116bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:24:25 +0000

    board machine estimate order indeed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_time_since_epoch():
+        return time.time()
