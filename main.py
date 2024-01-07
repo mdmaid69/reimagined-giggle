ebcf97dfb1207162c50b3a0fb1421237ef893065
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:29:22 +0000

    pair taste trip for fail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def count_elements(lst):
-        return len(lst)
+print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
