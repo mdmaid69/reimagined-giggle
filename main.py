a3b6057a3a16afcb0f6ed659af1dd82b90e92d3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:36:38 +0000

    future lunch injury seriously cheek

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   def calculate_average(lst):
         return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_current_working_directory():
+        return os.getcwd()
