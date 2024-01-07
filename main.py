5e932542a659d51208a41a49272131375c2392ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:19:05 +0000

    knock everywhere quality smile actually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
