cdcfc58ada69d109be12ade14e8c03edf054dba5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:39 +0000

    song solve apologizeapologise again kill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
