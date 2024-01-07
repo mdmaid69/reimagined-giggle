bc6b7cac2bced894e4814888d58bfd0c59b67683
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:38 +0000

    door disappear offer equal skin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_directory():
+        return os.getcwd()
