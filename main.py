15d2538bf68bca083d31a899392ac49157490104
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:36 +0000

    about proper transparent influence sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
