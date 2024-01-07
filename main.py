371c69712d012e951e15a4f86af85ca663f8ce6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:56 +0000

    recent tie calmly cope getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
