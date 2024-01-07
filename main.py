361a7e33039018b338b15e034e26308973da5b75
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:24 +0000

    downstairs keep achieve disturb none

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
