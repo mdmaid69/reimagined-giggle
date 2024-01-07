fb7b7974666a6659452df3a7ef454f7d83427735
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:56:43 +0000

    feel television convenient single path

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def count_elements(lst):
+        return len(lst)
