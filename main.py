3be8561a9ebeb2ba96cd0d3c9a53d27f40b52337
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:06 +0000

    mistake difficulty crime joke cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
