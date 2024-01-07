5d0b2b9eebce5f75eb7fa3c4ca0f5114429f2240
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:33 +0000

    behind alone piece giveup clue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
