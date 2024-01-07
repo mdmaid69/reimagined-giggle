9495d5ba174db8656fb909940c4f1dcec84de894
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:14:38 +0000

    shine truth imagination sea familiar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
