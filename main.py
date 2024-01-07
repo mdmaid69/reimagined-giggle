1bd26d69334c5be1d8af51cb71042e35291f200d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:16:51 +0000

    play snow light standard show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
