da59ce36345bb70689d15adfd6e51cecb53f9aa5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:39 +0000

    success health uponon city Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
