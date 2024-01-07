e5c6190ae653a86364a89101e4a19d8a473d550d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:14 +0000

    issue lock freeze effect blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.time()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def sort_list(lst):
+        return sorted(lst)
