d95cea78c1b27d6c6900d50e08d6351a96c09804
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:50:25 +0000

    equal proper explanation close wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_area_circle(r):
+        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
