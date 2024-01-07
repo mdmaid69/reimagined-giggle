58087d4544c504afa7d675f9af1c96fc77c3c3f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:10:28 +0000

    tread deaf raw burst generally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
