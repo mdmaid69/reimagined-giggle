5f0253087d4338d5731693ea5e5c94d1d9d2c162
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:47:54 +0000

    thirsty discover boring steal study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def convert_to_hex(n):
+        return hex(n)
