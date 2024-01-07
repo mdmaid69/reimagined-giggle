f010284e31946368749a711f0891909a6b94916c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:25:10 +0000

    swimming strange brief remember pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
