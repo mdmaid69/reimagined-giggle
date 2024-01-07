78ad1c49c92978c6d3396b98207ed6d2ab95bb5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:11 +0000

    build breathe squeeze reliable mind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import platform
-def get_python_version():
-        return platform.python_version()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
