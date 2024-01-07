5c6f67061649fe804fe235bf12cddde02ce103b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:45 +0000

    obtain stone part clear understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
