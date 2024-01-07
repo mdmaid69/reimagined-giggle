1def7975f0f8c1723d8f709fa0c042df9ea66347
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:24 +0000

    go since conclusion smoke town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_max(lst):
+        return max(lst)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
