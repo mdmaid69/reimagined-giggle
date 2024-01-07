0910ea2855406481aa0ff0cdd4e320786dd29d8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:51 +0000

    react whichever opponent so protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
