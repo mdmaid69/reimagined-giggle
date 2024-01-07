15a0c5e4922a9d23fd817006a8c7d5674ed47695
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:52 +0000

    trip possible zero funny band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
