9902653ae536ce1e6cb5d59eed951f02eaa8af3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:02 +0000

    reasonable minimum fine cruelty height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_int(array):
+        return int(array[0])
