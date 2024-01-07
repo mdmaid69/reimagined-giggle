ac374cf9bd72ecfddeaba17318e40a5412cbcdb9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:36 +0000

    square dare treat themselves except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
