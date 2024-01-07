3c23a4aac7aef0f2417ce256e6a2151dcfc5744c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:24:16 +0000

    shallow love child hello motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
