66bee917ec66348788a2dcdca265019ec3334f4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:55 +0000

    either luck constantly letter operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def divide_numbers(x, y):
-        return x / y
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
