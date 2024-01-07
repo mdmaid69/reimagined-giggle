3e3a76807aa51fd206f37632ec6cd50650521b28
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:21:52 +0000

    serve point trip especially meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
