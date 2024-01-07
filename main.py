b0d75c12dbd83c39d4643ce4ef8dff9dfddc4327
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:52:26 +0000

    innocent show admire murder under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import getpass
+def get_username():
+        return getpass.getuser()
