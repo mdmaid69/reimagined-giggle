47467c0a1c1a8ec347989ba218be79b4333b8f3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:09:41 +0000

    bad develop now estimate seriously

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
