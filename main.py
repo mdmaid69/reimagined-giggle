5c3467d305d76e45420aed5c05bf20df5df7a73d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:02:23 +0000

    gift excellent edge type journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
