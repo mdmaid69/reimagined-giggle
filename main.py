c6211d84f885c6d93363ffa57bf841394d5d5d54
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:46 +0000

    protection same serious knock tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_array_as_list(array):
+        return list(array)
