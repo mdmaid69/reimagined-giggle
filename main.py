144e3e11cce90f086650d5dfec69cdd827ec7593
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:01 +0000

    draw account crazy furniture blind

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
