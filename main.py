5de16881418a3745f2b4d7badbc5548146c42e03
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:11 +0000

    study switch great away test

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
