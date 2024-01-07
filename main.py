c457070116cbb76ae6faebb6e58f98d1a13a14eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:03 +0000

    pleasure equal importance active lie

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+print("Hello, world!")
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
