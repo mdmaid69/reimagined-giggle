fce4315af06d4bade5307ef6fd2f731aa9c57a1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:36 +0000

    blame share Ii label both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def greet(name):
-        print(f"Hello, {name}!")
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def multiply_numbers(x, y):
+        return x * y
