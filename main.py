828447970e27549492904677f35ca45b962692d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:34 +0000

    object now back active godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
