4c9154b26bf96d46e95fdb1ac2d783e18c139e3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:07 +0000

    violence suppose outside steal classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
