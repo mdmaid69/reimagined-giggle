3928b231c24a06d4220bf8d555f9479e1edac2af
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:19 +0000

    right centimetre label advantage contribute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
