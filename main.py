9f4e9c8b9f7a7ade896f61a0412aebf5611b4d71
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:15:15 +0000

    because concentration slight quick block

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
