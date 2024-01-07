45fdcaa2a4ddc01f7bba5fc09a46a7f1c08759c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:10:25 +0000

    cut thorough most want case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def print_python_version():
+        return sys.version
