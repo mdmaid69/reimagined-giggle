6fa5cfca986540ba88a7c37bc53f5e0e2c120ff7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:22 +0000

    age onto sort anything obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_list(array):
+        return list(array)
