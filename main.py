25d2a615e324db7129b5c68abd32249e2446178b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:45:03 +0000

    raw compare dance complete container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
