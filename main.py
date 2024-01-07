abfe2e543ea23a5641a27162b9f1ea0b384b07cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:16 +0000

    quality though become duty settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
