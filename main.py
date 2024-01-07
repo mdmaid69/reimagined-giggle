cf57299f3a53643ba27e465dd1d73ebcb70a0d51
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:02:08 +0000

    uncertain doubt Ss that connection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
