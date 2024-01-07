27ea9d1e512d142486ee5db8b2486a588b91055f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:12:14 +0000

    preparation worth practise intend react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def is_odd(n):
+        return n % 2 != 0
