f93fd2ced40c7e0d3b9620b9405d7415b952e7c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:04 +0000

    read sure alive available favourite

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
   def plot_histogram(data, bins):
         plt.hist(data, bins=bins)
         plt.show()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
