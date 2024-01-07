c6d7810aa7db221c6a569a61712ee716b86eeaca
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:12:43 +0000

    feature swim point belief similar

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
