9f24fc88e47df13db32a6a2a54e9735088f96652
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:59 +0000

    tip deserve mainly base fact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
