12470b023a1230e2a8b63e03195223a2d8738dc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:02 +0000

    belong news sure more touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
