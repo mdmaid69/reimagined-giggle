8744d2272922e6de76188b818b95be41639c2d3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:11 +0000

    innocent crowded thorough moon better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
