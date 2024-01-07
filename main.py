fd3803c98c44ccac57531a0e7429f83ffa8929be
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:35 +0000

    fair crowded wake demonstrate calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
