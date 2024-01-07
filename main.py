8972ce6b9a05301bb2cdf2184ed62f4daae24d94
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:19:35 +0000

    workout calloff 570enjoyable handle before

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+print("Hello, world!")
