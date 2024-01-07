1e588ecca858310b96928f55e238c5209aa72659
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:41 +0000

    base pleased figure wear chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
