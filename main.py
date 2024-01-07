e108b4d902a2f356911c1c74a1ee82d6b2f866e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:39:12 +0000

    discovery love attempt act respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
