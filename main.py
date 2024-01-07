9636e9c33afa005ef5c1fa8607165d5d96a00bbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:52 +0000

    flood control fat reach rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
