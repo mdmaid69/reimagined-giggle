1cdf1f7e1b0ff4310aa58f4c97b5ffb8731a675a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:12 +0000

    mind find strike turnover unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
