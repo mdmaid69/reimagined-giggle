deb33616edaa1e21f003309e9e83cfd4ef2c410b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:23:51 +0000

    debt name smile believe relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
