ac6a3f0b5db051a0e2906c19bf3363f327323fb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:53 +0000

    culture partner high delicate welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
