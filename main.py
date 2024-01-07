ca0004c7b2115ee6675895fdd7a495c8e71241e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:00:09 +0000

    on lie remember sit aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import random
+print(random.randint(0, 100))
