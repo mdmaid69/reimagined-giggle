b0f289396097d4b9fbda77bb784f44bf158d31f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:08:01 +0000

    quarter correct consult approval bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-def reverse_string(s):
-        return s[::-1]
+def get_array_typecode(array):
+        return array.typecode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
