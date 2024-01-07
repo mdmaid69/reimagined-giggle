2408cf21ac615bc020cc8a2985ec7bcee6050254
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:49 +0000

    imagination fond suit chat race

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
