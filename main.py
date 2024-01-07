4c16bf56e49dc8255a24b69dabaafd00bdb5c871
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:04:10 +0000

    rely pay welcome burst as

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
