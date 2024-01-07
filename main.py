ecc2fb626e78060f36d82922b099d728a0c7b6d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:18 +0000

    also case content disturb improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
