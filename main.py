ff1e433c2cdfae2ba426f71d1344e9330541f2d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:08:18 +0000

    risk step basis comment frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
