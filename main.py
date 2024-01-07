4b30e553173a69bdd4e92cafe52030d5c0af422e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:42:06 +0000

    frighten anywhere drive block swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
