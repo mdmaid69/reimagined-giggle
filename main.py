8b36be6129a7f850566995092854f3cef0d244a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:55:03 +0000

    without opportunity ignore able level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_int(array):
+        return int(array[0])
