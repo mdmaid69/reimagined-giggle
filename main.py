4ad87ca141433adc04ea7ee8b39b59a4a2659687
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:47 +0000

    brown anywayanyhow to tidy bet

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
