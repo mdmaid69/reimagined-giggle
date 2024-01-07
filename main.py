f91d02ef1331be81c56ae62257ad69f877578dc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:03 +0000

    back write hurry after deep

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
