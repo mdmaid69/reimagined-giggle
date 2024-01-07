32f2d6e77ffd1f32975c8aca17265499946079fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:42:35 +0000

    reasonable determined throw substance poison

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
