0735d97ad6254a9bfd96f0625cd85857b2085200
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:26:04 +0000

    positive fly limit contact police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_directory_name(path):
+        return os.path.dirname(path)
