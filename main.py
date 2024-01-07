2da327c4652e7c26a8af9d0cfdb987e5b8deb07f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:42:10 +0000

    commit move any maybe loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
