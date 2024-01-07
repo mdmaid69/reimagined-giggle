d23660fa9a17ba23bc5eaccc4c5652689c2ec078
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:44 +0000

    stop safe length mail terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
