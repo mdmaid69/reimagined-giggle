94c31959f15cd29c33dbafed9bb7ac6916100592
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:17 +0000

    rain spring mood certain Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
