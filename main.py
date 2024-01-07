ded1578bee85c1b6628b745b82087a1e469097ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:59:08 +0000

    goup scale best alone serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
