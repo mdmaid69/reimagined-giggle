01aaf42a0f82a5ead44393003ba24dbe20178e5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:03 +0000

    geton upwards black exist by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_work(force, distance):
+        return force * distance
