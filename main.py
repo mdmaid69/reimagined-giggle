935f822200fe152d9131f7a820b63e35d2c45415
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:21:58 +0000

    unite here inquire bye information

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
+for i in range(10): print(i)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
