ac07a18694e9ad3accd468a068d2fd248ec297b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:12 +0000

    rather right accept amount shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_arc_sine(x):
+        return math.asin(x)
