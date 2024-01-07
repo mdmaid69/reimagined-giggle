c1627eba9def5a4e1168dd36a92614bad8d51742
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:56 +0000

    deliberate police crowd regular tend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-for i in range(5):
-        print(i)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
