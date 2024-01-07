79c793bd86f9645ea4a7e6497c535bd77622fcb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:03:32 +0000

    niggaacademy sell trousers that tidy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
