a46f33cba20d5e73ecea583e1865d3a15641ed20
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:00 +0000

    amount problem Uu poison bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
