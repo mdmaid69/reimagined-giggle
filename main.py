0ba0560ae1492213230938824acc8a805ef46356
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:32:56 +0000

    convenient similar steady challenge funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def reverse_string(s):
-        return s[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
