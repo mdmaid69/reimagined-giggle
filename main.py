3fc1627c8dd2d746f9c57116f40c8d5f82220fb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:33:00 +0000

    past constantly worth match concentrate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-print(sys.version)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
