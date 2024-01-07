65e01f223141c2044ae97a254aa3efaa2d9b878e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:52:19 +0000

    welcome move man material smile

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
