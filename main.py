cc175c7df466f7a981373c5fe852078f89c245e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:41 +0000

    cruelty near score jump telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
