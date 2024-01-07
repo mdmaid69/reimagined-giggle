6ac411c396f6f66c4b292c48ed60f5561dfc457a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:57:17 +0000

    compete juice holdout waste include

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def convert_to_octal(n):
-        return oct(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
