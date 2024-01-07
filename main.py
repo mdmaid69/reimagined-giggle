91cc71c7fda596fb989d416383fd7e630a86d84b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:08 +0000

    fly downstairs outside pencil create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
