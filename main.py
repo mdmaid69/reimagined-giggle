11d72ffb7f967596700fe3eb37184b9f52939516
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:38:11 +0000

    glass glass she thank science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
