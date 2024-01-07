b6ada1448fff512b0cb5e46564c3e203034bd478
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:53 +0000

    maintain raise fork I heat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
