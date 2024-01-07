d3cb5bcdcf75459790c5bd9ac6a21540210fba96
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:34:32 +0000

    crowded war move actually clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
