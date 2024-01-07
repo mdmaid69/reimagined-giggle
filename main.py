76118b93073f9476f403b21237bd6d40d06a0ca7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:08 +0000

    sure chin breakinto callout regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
