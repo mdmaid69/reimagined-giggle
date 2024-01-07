e6de5c4b7a65cc10f90990d5744edc3e02c3d752
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:56:53 +0000

    scrap wind consider loud test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import sys
+def exit_program():
+        sys.exit()
