12626694afa5e43cc8ad5613db42096be550644f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:42 +0000

    ilovedicks bringforward treat bored murder

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import tensorflow as tf
-print(tf.__version__)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
