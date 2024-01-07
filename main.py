d519b51acd39e2d13fc22225528902a35a25ef37
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:36:26 +0000

    she activity load public wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import array
-def reverse_array(array):
-        array.reverse()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
