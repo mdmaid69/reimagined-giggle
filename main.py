133e4a6dfc52ad6137ed814724edf0d23edf605b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:41:33 +0000

    available thinkof embarrassing education discovery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import os
+def change_working_directory(path):
+        os.chdir(path)
