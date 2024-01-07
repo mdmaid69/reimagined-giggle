96422737fbb6133aed2d8457050c3193d01d6736
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:32 +0000

    trust surprising temperature declaration recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
