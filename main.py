8e6b9fe697700d05892691c386e0a7b5365e7332
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:00 +0000

    sit forgive skirt swimming sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
