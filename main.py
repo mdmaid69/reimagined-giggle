f70555168e43002b1d3d666a3b0b0ea2110f0c67
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:11 +0000

    holdon full fat crash opposite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def exit_program():
-        sys.exit()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
