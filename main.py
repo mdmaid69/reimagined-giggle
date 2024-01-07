7ea5cc4cbe669aa84414730a3311d34bdc704afd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:55:37 +0000

    fashion soft wind lift land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
