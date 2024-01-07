a13dc6aef3539b79d850664f3c378d50456a13fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:59:04 +0000

    man float embarrassing greeting even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
