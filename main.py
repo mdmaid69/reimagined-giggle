3a4ec9f7875450939d08af99bf386d334109b766
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:45 +0000

    conclusion bound instruction while shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
