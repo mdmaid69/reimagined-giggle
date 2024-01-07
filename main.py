38891a80239a7032c541c39a773445e99b373600
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:26:04 +0000

    collection unusual score speak that

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
