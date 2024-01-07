685c16e6de9683547143be8158310ac3d7a6063f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:57 +0000

    thank out conversation pair nature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
