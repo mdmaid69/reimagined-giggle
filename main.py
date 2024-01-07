d82f191d18cb216e741d6841dbd34702f3c3ac2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:40:16 +0000

    several responsibility hour will skin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
