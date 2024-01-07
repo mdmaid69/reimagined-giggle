c21d130fa02ffc5ca0215114749f37158bd0bb08
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:39:41 +0000

    will conclusion muscle tired paper

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
