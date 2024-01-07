f54a1ccbf57890b093f5fb8ea96716211ee62d7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:58 +0000

    basic meet Jj truth tall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
