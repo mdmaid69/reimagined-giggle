5f5524ef5d5e717abced6f75a7826905a9a5fb41
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:55 +0000

    leave road drink travel concern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def add_numbers(x, y):
+        return x + y
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
