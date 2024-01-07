ef4b50fec5de5bda6ab5db3477500ea7a8e4f1ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:12:24 +0000

    everybody far fast crowded but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
