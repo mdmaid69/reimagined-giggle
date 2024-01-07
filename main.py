0b946f4c6884efcb7366399d4e77f4e819673fee
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:12 +0000

    shake takeaway deaf like danger

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import sys
+def print_python_version():
+        return sys.version
