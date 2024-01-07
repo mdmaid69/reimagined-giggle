f63d1339054e59055b15a8a6fe75071e6ecbce72
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:45 +0000

    advice goup comeround who thin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
