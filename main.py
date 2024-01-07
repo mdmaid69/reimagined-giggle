f8a09da0a5bc7e9780a58a8b468abb3147382287
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:19 +0000

    even decide punish seat realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_repr(array):
+        return repr(array)
+  def square_number(x):
+        return x**2
