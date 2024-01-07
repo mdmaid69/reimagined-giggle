e850c58d449c6e1f4eadad45b73c4233f265d113
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:04 +0000

    terrible clue pride treat drawing

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(lst):
-        return max(lst)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
