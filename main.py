bb425c12bc2ab6b9c6a2570a84be3e21f3913895
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:40 +0000

    cough treatment kind light turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
