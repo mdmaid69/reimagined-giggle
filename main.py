4f6591ae536fc9fc7d1627c4a4341c14cff29eca
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:06:16 +0000

    Kk sort runaway back commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_list_from_array(array):
+        return array.tolist()
