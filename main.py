cd0da9e068c7e8f3ab05cc2efb7c66f6c213787a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:16:04 +0000

    workout undo characteristic charge I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
