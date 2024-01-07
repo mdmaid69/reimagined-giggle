7a2aa84e64f983dfac17d74638590c9ddc247b2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:54 +0000

    land embarrassing game crowd announce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
