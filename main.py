c27bbdbbffff853f54c5c2af8dd39aad3a776c6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:57 +0000

    wash off habit address silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-print(os.getcwd())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_speed(distance, time):
+        return distance / time
