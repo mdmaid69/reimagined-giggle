286e648d9479386bb7c4155c08d96a0138a11051
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:29:03 +0000

    goaway make willing callat effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
