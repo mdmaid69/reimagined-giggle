29ad9d1d2c213203b17276d7ef48f2a5d2444f7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:43:06 +0000

    clearly sleep protect pause fat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def cube_number(x):
+        return x**3
