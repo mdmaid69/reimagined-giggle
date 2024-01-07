791123af5e06842e0bdda6ee9359a050be59f267
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:42 +0000

    correction rather day smell forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
