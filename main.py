6f352758280150e6306ffb87505d3732ecd6a397
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:41 +0000

    Ee frequent piece getout charge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
