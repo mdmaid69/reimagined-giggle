68f9e92dda2b0701e539dc895c7f1b18abcee4c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:16:04 +0000

    rather believe experience send sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
