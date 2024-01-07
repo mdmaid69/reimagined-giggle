1e3d4b39d045a9834c5f16d801bb02545b77e289
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:44 +0000

    operation lookafter clean cry else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
