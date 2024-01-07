4d7b02d49885761ea0d678d02d6ba8b384c43145
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:36 +0000

    patience preparation fact hour lesson

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
