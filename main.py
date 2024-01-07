72eeac9c947498e0254970010a1e3abaf6321ebd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:46 +0000

    small improve promise old petrol

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
