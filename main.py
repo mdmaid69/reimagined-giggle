4010c72428f34b5a3978ef8526cbbad2bdefaae2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:47:23 +0000

    standard interest sit content nowhere

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
