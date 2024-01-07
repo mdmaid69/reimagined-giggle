4a316adc5eb34e3ce48b2bef179503c345ec879a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:24 +0000

    plenty often period change back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-print(math.pi)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
