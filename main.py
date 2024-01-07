0ce96c0816eb70a9117e03b6e0e79c4f270732ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:12 +0000

    hurry worth lift nice car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
