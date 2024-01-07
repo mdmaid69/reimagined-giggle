7487e599bfcd50f33bad80bb21bbb7ac2d023f7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:13 +0000

    spoil passage major furniture loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
