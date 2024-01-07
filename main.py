cf863c357bbd31e216c80c7e3dea9bf5561f9301
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:37 +0000

    experiment sad attractive war pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Words:", len(text.split()))
