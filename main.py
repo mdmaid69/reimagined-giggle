24004d86bb776e269f16ec15b9930cf30bc7c9f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:12:39 +0000

    Gg patient blind feed important

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
