b61445739962815b940ad9fdfc96b83d2ea99463
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:34:06 +0000

    through out hope underneath see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
