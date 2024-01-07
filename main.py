1e52fe835a07f00ad933e2ec1bacc6732a2d26b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:26 +0000

    properly reliable bang frame disapprove

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import math
+print(math.pi)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
