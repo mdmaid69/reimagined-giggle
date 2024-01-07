0f093c4c37ef426a15edcc3b4a545c3c3cfaa5bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:46 +0000

    how Vv fear so threaten

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_min(lst):
-        return min(lst)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
