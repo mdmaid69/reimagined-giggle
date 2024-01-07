500a7315099625c322acb5d02cf367ccfa77031e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:14 +0000

    shadow squeeze geton breakfast wish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
