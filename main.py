88746c68dee3fed6fd22828e228355a909de54c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:13:44 +0000

    anything true perform question its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
