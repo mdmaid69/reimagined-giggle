1c738e6380743e0d3378b2ad4984fe0ee7718a21
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:34 +0000

    hard instant comment underneath know

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
