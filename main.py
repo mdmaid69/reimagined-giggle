277aadaf569006087bb27b48ebc17883a797c52d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:52 +0000

    behave accident deliberately main while

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def is_odd(n):
+        return n % 2 != 0
+  def convert_to_hex(n):
+        return hex(n)
