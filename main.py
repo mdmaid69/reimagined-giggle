395cf5f2de42ebee1bb7e108a9b6ce1209ac9a04
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:27:24 +0000

    fun rob play exercise terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import time
+def get_time_since_epoch():
+        return time.time()
