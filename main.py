c641ec11362ce2af5acf4adb2a7a3610dd84f1d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:33:29 +0000

    emphasize after rub store close

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
