30895922be8603458136f964416c7b42e50767bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:57:54 +0000

    smooth getup occasional sharp evening

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
