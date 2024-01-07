e234bbbdff04613f5a14fbf4ef476bf34564a37f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:39 +0000

    Vv future against today admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
