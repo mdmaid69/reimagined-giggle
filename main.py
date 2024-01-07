c65e923f2d721bab97c3274cff64ed48816146fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:21 +0000

    guess storm warm gate table

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import re
+print(re.match("h.*o", "hello world"))
+  def convert_to_octal(n):
+        return oct(n)
