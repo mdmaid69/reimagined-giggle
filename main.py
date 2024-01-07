f18cfd2b17a77128bc3eeecadc290609d18550df
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:33 +0000

    getout greeting spite congratulate here

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
