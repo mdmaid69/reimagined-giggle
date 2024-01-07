8070e0b5bda057557bd4bd6b0739d4c3de318448
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:18 +0000

    holdout partner exit nothing main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
