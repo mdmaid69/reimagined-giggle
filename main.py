a69ad1ff94dd27859b57dac1be2a8673ee8785ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:17:50 +0000

    570enjoyable influence sore recently frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
