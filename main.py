78e6b7e50673764fb4ef5753c09a5cad530d4313
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:21:01 +0000

    prepare responsible direct answer guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def convert_to_hex(n):
+        return hex(n)
