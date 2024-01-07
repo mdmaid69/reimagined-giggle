37fb63da04c43a274c8edf0a63ae1bc9b4f45704
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:18 +0000

    along perform takeback principle thin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-print("Hello, world!")
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import collections
+def create_stack():
+        return collections.deque()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
