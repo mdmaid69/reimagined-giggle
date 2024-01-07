eb090ef2e73737c0ff83311e6e22c1ae349ac1cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:55 +0000

    return out involve practice main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
