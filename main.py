7d8ad54d9a911512ca22d24daa0a04fe2c02dc67
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:28 +0000

    length translate sky strict soil

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_max(numbers):
+        return max(numbers)
