2eaeebff390b5db9e01a072377a6c14a1634e794
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:01:45 +0000

    date dig mad store forget

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def is_odd(n):
+        return n % 2 != 0
