06c07e394a6edd02dc452745fc874a1ea28e1212
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:05:16 +0000

    takeout clear lookback(on) advanced every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
 def calculate_factorial(n):
-        return math.factorial(n)
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
