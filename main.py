2d2468132125e8ebf8e529fac96062075dec4284
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:52 +0000

    neither central oil sign game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
