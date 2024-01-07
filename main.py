4dad512c7473c8502948684b089c35836c03b1f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:18 +0000

    music official uncertain production play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
