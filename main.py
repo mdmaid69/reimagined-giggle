18cc19ccbc2409a51c7f6f356921b4d784d8e280
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:48:14 +0000

    sore point spot pronunciation obvious

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
