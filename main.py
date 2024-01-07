2e4735243cb52429770755ae3c43b786811773e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:25 +0000

    familiar really deserve gentle fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
