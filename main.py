1002a140112230572e478ccda20a2d2bf137b347
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:42 +0000

    burn soft form translate plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+print("Hello, world!")
