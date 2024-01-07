d1263f907fbaf962a6a10c233036e5724a92ce09
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:59 +0000

    grateful suggestion start occur especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
