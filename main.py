79589cc4050b8a6a180e41f1a10ae1163c083830
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:14:45 +0000

    be because transport level their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_volume(length, width, height):
+        return length * width * height
