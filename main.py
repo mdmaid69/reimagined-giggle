256e64546cf84362b3b06b8b82ba2f493df707c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:57 +0000

    refer dare rule excited switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
