aa376a9011502132388e290d2eae1ba30717c5ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:14:59 +0000

    pause term world wave pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def convert_to_binary(n):
+        return bin(n)
