0b1c07b1244c32f2e40306b7991edc000af9c84c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:11:50 +0000

    dress probably actual us remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
