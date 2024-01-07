9166a6fcab1bbfe5dcedb4acbfc88f8e8ed8fbc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:58 +0000

    motion mean tall study hungry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_index(array, item):
+        return array.index(item)
