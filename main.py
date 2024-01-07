e05b598afe0e791599ad54e5724b481d77fe1e63
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:55 +0000

    pupil Uu afterwards important wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
