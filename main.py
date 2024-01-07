43f3e60ff8f751cfbd2a04582d9b66b41acd6cb0
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:14:25 +0000

    tradition proper out push negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
