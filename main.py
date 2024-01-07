51ef42e647ab6149919928f9ce33a080f8fffde2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:29:18 +0000

    whichever education what compare address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def find_min(numbers):
+        return min(numbers)
