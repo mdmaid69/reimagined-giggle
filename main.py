f411502242587cde9bb6254e3a6ae846578765b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:17 +0000

    equal part worst bottom better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
