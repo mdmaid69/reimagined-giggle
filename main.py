119d6ef34a9905d4ceca3d53e4c970e3a6002bea
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:31:24 +0000

    near length means eye story

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
