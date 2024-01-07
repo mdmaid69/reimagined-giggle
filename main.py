45d8379976eff143bc82b4f050f2e67937d94d61
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:29 +0000

    raise increase let steady theirs

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
