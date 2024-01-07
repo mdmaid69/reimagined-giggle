3b3317c881bf96bba9523343aef8afcf3d48af52
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:04 +0000

    dry organize none lookaround ground

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
