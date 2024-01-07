89d8f89849ed4e26b6e9024b8efc7e2be2868f59
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:30 +0000

    free protest estimate state run

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+print([x**2 for x in range(10)])
