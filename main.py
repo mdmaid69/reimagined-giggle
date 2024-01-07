57b73d6e69ecda04c1670e8141f6e1bd4ff150da
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:43:56 +0000

    comfort fault win getaway return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
