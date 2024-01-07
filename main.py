21e76b99f90aab5997db0c627a23ac55cb262469
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:47 +0000

    moon bet sing Cc solve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
