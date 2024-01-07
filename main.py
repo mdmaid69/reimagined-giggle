32e24db7632ccbcfad9c1c91fea61754f03cf258
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:31:58 +0000

    sort hers sale breakup case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+print("Hello, world!")
