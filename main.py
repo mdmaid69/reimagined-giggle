cbab6c56592013cd867be32a017ee40e93d98d3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:20 +0000

    heat comment fire loan top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import tensorflow as tf
+print(tf.__version__)
