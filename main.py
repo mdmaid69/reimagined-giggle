56b4db4b24372d9edb631022e2c829f1d8ff9be3
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:16:57 +0000

    minority lost anxious tear serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
