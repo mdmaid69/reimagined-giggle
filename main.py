bbd3688de5ede0231079c5abad52717a70fb5f5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:04 +0000

    grateful circle knowledge imagination debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_queue():
+        return collections.deque()
