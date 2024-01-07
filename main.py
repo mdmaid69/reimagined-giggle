25d920fbe5e2668156b9ce3541e7670bdc3079fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:02:03 +0000

    deserve contribute suppose still quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
