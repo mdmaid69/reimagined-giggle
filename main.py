c36fabef55faa78e23d003c6b1fbcad259da9f42
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:17 +0000

    needle sick wipe reach ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
