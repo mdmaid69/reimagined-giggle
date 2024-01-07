fa9bee25205039edeebbf7a3f0cef025394df8b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:23:19 +0000

    know yours choice fire it

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
