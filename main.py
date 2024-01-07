442b34cbd90b362a9fae652a0450136b14352825
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:29 +0000

    mass have drop exchange Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
