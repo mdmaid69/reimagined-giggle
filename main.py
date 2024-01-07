2010e2f7913e025d39e85a4e8d1f743822e44ef5
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:04 +0000

    give reduce rank sleep protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
