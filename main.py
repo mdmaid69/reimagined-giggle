82e382429660661c768c88d8b2940cde4e9ff24f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:00 +0000

    climb wake pay Ee physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def add_numbers(x, y):
+        return x + y
