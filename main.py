b8fced4466b24b55fed61d63d9f94a3280b24c30
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:39 +0000

    lift winter tread possibility refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
