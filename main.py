3d8e10c0e039ebe04ec3a8a906ab02e1a33b6af2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:41 +0000

    book date crazy perform principle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def multiply_numbers(x, y):
+        return x * y
+  def convert_to_binary(n):
+        return bin(n)
