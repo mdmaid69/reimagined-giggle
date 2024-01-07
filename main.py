f7da5403d8e79e1bc2977ec9a2254dcb0ebbea09
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:31 +0000

    plain dare birthday book clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
