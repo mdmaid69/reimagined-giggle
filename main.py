8041affc9932443f45c6bcf3a16f3dc20f7dc082
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:11 +0000

    fill unable table worst because

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print(sum(range(10)))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
