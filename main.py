35e071b291d9c663291972a9c4acdde1f25b6954
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:13 +0000

    priority choose over exactly sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
