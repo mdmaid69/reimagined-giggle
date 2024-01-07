56b0ceb451f5cf4f7bceee53d6307b9dc4883a54
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:56:54 +0000

    suit dead to private proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
