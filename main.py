7300e4f60dd02ec7cc7b570fdbf3b222e056b86d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:43:51 +0000

    right say funny present interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
