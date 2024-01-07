3422d10b1bc65251fe61d5cf1c9f91d45fc7e236
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:12:22 +0000

    check harm fat language near

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
