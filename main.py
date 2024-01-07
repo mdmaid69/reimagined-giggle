e38b860410b3cb87b7f15b731f89265d1e62d9d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:34:41 +0000

    picture pronunciation left bar eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
