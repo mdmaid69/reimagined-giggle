87488102ce7b31416134eae771be5c864d2ce54f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:23:41 +0000

    traffic broken task slip geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def clear_array(array):
+        array *= 0
