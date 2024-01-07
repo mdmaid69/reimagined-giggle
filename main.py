a36b70a04e6fcb3cafd131f78c2977674f5018dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:15 +0000

    disturb aware immediate mistake wheel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def convert_to_binary(n):
+        return bin(n)
+import os
+print(os.getcwd())
