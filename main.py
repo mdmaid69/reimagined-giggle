3b69acdb99159d5d4b8200bab8bcb9ae2bc7a952
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:02 +0000

    deep question perform party suckme

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-print(math.pi)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
