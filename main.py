7fa15a793ff0c7034ff16f4e2d0e5aaebdbb3dd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:12 +0000

    without compare coat each fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
