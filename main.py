213aefe6acdfe45c530e512f6c8a80d84183c975
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:51:46 +0000

    collection light damage suggest 572enter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_string():
+        return collections.UserString()
