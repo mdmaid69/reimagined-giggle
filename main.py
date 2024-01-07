cf6075704232bae9283d48558b1619b56f91a233
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:35 +0000

    steep squeeze Zz snow crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+print(sum(range(10)))
