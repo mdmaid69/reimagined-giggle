3dfac20ee4d84a8e9c74f9115ae8868e87e35909
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:13 +0000

    regret taste attach shop on

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
