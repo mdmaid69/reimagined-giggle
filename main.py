d6e93b9f4158e196905b340adb4f21f3d00128ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:36:46 +0000

    smart card this politics crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
