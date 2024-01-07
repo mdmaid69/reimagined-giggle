cc37bb54437a78f42e36ec4c17b0c68e762d2237
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:41 +0000

    turnover story careless maximum tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
