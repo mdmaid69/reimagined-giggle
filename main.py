542b33f9819cb25624b57eaa0602c78873a7ec7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:28 +0000

    poison about cold discuss joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
