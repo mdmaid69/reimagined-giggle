1a8d9389260c588f9d1de5aa050ff7f7d8201ad6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:07 +0000

    upwards form roof lower impossible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
