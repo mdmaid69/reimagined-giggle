f91e03f0076c865c9e2260d996505393308d1ab3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:06:56 +0000

    behaviour interrupt owe angry especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
