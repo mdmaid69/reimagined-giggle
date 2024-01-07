fc2230ff4d2ecfde4c0d626033b302aff0169feb
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:45 +0000

    answer maybe community none colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
