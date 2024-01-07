b0b44cabdc5b4b9bead02d5b26570f4f44b06e2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:17 +0000

    again drop yesterday book total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import re
+print(re.match("h.*o", "hello world"))
