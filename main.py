2cfbca299dbf195ec6b3cf0559575ca5243b8443
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:43 +0000

    second duty dinner figure book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
