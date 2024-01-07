fb0f08354bc7ab7d0973f318887874ff4b0943df
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:13 +0000

    exchange show far another draw

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_current_working_directory():
+        return os.getcwd()
