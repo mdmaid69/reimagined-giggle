3590e42dcb3a147736cf8cd3bb5af91dacd67d79
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:00 +0000

    just used moral choose record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
