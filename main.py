446edfc9c0c72aecd6106dd86613389869f5d636
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:44:20 +0000

    Jj fire where move illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_list_from_array(array):
+        return array.tolist()
