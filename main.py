2181c1376c89229fb078c0ec7bb34684f4a85ab2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:21:25 +0000

    better cause body contents value

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print(sum(range(10)))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
