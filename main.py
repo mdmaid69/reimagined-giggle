2a86141e9c35f27102347ec8654de3408db4a78b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:10:06 +0000

    track mail suggestion common 566end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
