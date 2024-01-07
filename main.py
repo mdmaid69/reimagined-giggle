5baa182185661088de7e311f8e55aaaf13b0f24c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:25 +0000

    damage unlikely instrument matter accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
