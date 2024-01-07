01ff5dbfeaa61ffdfdf4897d5fd7803016c36892
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:36 +0000

    expensive spot shrink to bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
