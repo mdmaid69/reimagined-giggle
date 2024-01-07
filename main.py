d387fb8d05e2e631138ea63f7cf0f2178fef9bec
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:24 +0000

    ago legal generally arm appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_current_directory():
+        return os.getcwd()
+import os
+def get_current_working_directory():
+        return os.getcwd()
