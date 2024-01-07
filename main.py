8ce8ea8333426a014ca5cc561d2c2c0f0608848f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:20 +0000

    wet impressed spite curious bet

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def cube_number(x):
-        return x**3
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
