c89bee22da9f941a800f139915307d4aa7695326
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:07 +0000

    attempt soil copy air fairly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def clear_array(array):
+        array *= 0
