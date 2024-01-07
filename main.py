e023aaa7d18fb5d3b6542645eea7b98e0555d6d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:55:20 +0000

    straight ours whoever size the

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
