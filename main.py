3fd54f39862ba7bd23e12e7b2e769a64f58d4676
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:41:31 +0000

    remember call direct understand part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
