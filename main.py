fd0461a86b2732c17f7926776f884bc5d35a220a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:59 +0000

    skill congratulations nowhere long niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
