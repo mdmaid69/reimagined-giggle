bf67711e41cd330b473953e84f974734fd4f099e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:09 +0000

    afternoon handle factory off arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
