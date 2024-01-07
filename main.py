d3b5fe60905efa1d6415c9d22d818a01d8944e15
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:49:09 +0000

    hope press sure cook fact

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_as_str(array):
+        return str(array)
