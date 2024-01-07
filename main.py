a4233be02d6cd288b7510bec6881e8a222c5965b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:38:27 +0000

    pipe line treat unite relationship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_acceleration(speed, time):
+        return speed / time
