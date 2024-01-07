f8cd349e2fe9c7a5844dde727e24eae0e6906660
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:38 +0000

    remain unhappy tense sight imagination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import time
-print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
