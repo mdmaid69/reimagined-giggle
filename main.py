6123be94e6b389d257089391dc6c68b32f2b6a91
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:49 +0000

    clue glass ever provide pool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
