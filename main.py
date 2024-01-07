c376a75e3ba8dd6a10b4a86cb780326f57dd275f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:53 +0000

    above back somewhere below matter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def divide_numbers(x, y):
+        return x / y
