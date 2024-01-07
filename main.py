d6028cc0da53fe76c16d96ec6387ac524fe9537d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:24 +0000

    obvious yes callat birth look

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
