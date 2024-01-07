36a76488d9999774262f828f31d8572a78c3a043
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:34 +0000

    stop accidental everything turnaway add

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
