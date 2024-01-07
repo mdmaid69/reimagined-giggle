a32329b43330c63b2217e9d907c71884c67a7c35
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:02 +0000

    colour most now affect catch

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+print(sum(range(10)))
+def find_min(numbers):
+        return min(numbers)
