0f89fbb8a9a4a30cbf98e12f369adb7b9e7ac935
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:56:12 +0000

    shrink tone pronounce score yours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
