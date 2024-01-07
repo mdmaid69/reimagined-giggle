33c1991de1334c0f12cbd72b8742ff9f43cc73ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:15 +0000

    supply cause stamp grateful thick

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
