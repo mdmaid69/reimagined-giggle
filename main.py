9da3fa222996f122de1ba40c5147893a4c31bfbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:45 +0000

    flavour spend shock ride cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
