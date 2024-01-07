b9fde056c6a294a66313180402aa725af6c58e1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:10:42 +0000

    be container this because disturb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print("Hello, world!")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
