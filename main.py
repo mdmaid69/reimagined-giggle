cfb0aa6fbaa33765e2845b3c92261176927becf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:13 +0000

    keepfrom firm preparation concerning pride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_floor(x):
+        return math.floor(x)
