202be53670fe225358469ff6df820daff053951c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:25:57 +0000

    rich round tear satisfactory hot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
