4c0d0d2690a1a8171461096a64a081e728bc233d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:51 +0000

    raise crisis cold whichever health

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
