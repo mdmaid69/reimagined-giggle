b7dc41919cf61755cfee61e1008cec7c99bf2bee
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:26:24 +0000

    slight clean film emotion wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
