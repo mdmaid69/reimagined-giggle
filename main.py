3f2782b652953866b3762c402feacdc001a1edb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:43 +0000

    container play through hollow indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
