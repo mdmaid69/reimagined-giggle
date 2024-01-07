6302119741ca02ea5b47af21c8401ba35879fb8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:12:03 +0000

    politics rich lead worry compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
