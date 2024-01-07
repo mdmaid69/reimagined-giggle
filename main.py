e8b4b2136824139bc75687509df168af21f0f860
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:49 +0000

    different reduction condition comeout life

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_string():
+        return collections.UserString()
