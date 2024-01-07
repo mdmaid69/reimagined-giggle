fe3a19b62cf670b2e8ed130bedc73a71b53c02f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:50 +0000

    shop comefrom flow history schedule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def count_words(sentence):
+        return len(sentence.split())
