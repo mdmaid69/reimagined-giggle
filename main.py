838c32afa4e236122b0833b09f78468edeede614
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:28:25 +0000

    after ever total shall scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
