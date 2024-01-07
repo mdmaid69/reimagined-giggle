5ffbbbcee50395e62d4a47738e58114e8219165c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:06:24 +0000

    passage everywhere police innocent replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def add_numbers(x, y):
+        return x + y
