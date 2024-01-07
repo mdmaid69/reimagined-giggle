e8f5a3d5e8fd586e2eaf62f07cba69d5c305493f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:03:42 +0000

    into command still uponon formal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import datetime
-print(datetime.datetime.now())
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
