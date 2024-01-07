c963ed9cd549ba0f62255402ba73e27afd852edf
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:55 +0000

    result smooth 567enemy steal often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
