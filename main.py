0dce622dc893d53023c23ad2624f98aac54020d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:02:17 +0000

    each stone world little just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
