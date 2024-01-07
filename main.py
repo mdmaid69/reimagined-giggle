4e86c1ffec6c47411a4e2d80a3ca7feb8ad4eb8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:12:46 +0000

    like let breakout pull corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
