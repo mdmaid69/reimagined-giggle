a1fa09e1652c54655a1b34c8cfe5060edca0927a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:45 +0000

    top send oil part goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def convert_to_binary(n):
+        return bin(n)
