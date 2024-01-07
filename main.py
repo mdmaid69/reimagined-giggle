24dc00d23651e7cff07bde34f3c2481cbe4ddc2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:47 +0000

    forever season wrap bush minor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
