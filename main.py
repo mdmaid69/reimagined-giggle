cc1424f23b7d849380cea3fc0c5f55e48374cfff
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:59 +0000

    sky earth friend change interruption

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
