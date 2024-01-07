4ef842f207b00dd801bd54db1a8569ac00af6d78
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:20:27 +0000

    design minimum as reply describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
 def calculate_perpetuity(payment, rate):
         return payment / rate
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
