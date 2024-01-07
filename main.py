3757b0f233febd0e34c7fe600bb16041b3bcdddd
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:46:51 +0000

    sight pipe war base rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
