121d3496a2946e53ad2ef8e511773d63ab6ee01c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:18:53 +0000

    bringforward about broad unlucky relation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def reverse_list(lst):
+        return lst[::-1]
