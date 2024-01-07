4ac90435691374e65a9cd71ec1591376ce27b635
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:29:50 +0000

    action criticism carry little that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
