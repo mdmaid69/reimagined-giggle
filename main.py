e34ac2efa6f913621fdcbea5be15016e2dc7ea6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:42:47 +0000

    unpleasant another reliable rent power

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
