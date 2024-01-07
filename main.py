b94364e4e2a9c61c2d693efa090d6652604ee7ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:36 +0000

    afterwards love size challenge appeal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+for i in range(5):
+        print(i)
