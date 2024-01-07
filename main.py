deae62f55b9dde3510ce8678b2d430b305fba2f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:41:57 +0000

    war guess practise call sale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
