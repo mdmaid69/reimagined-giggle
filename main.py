6fe74fc8da1d2d86fca9950c335e1768ecbf65b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:47 +0000

    method shame emergency realize cope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
