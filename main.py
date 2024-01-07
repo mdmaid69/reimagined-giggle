c6ec0b88090cf581916bad19a978e1ef9611d446
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:35 +0000

    science wide speak matter of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
