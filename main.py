064a4d9761a7661aab46a80de81574c9af45cdb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:38 +0000

    regular remove temper reject declaration

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import tensorflow as tf
-print(tf.__version__)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
