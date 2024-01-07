fdd41c1ba145fbe59f70c751bb16e9af2a8a0714
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:49:29 +0000

    trust demand matter vanish cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
