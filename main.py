79b91043db4ba2e251516834d7f70a0868351bb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:59:28 +0000

    wave shut after word surprising

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def is_odd(n):
-        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
