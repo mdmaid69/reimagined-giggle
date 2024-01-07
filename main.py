81a70fdeb2d994b68491f26f21efdd9038e10ec9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:28:49 +0000

    about please inside sure profit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-name = "Python"
-print("Hello,", name)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
