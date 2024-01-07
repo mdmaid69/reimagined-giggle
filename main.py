bdfe3d6b09511163e1fde6dd404ec11ce92c54b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:22 +0000

    contain firm rid pure perhaps

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
