68159187e8de65841c679d325c685cb57db39ca3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:59:54 +0000

    boring keen pity late depend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
