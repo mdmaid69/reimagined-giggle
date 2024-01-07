b24cf21d510cb1918c94ff4daf05114f14375db2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:58 +0000

    fit contrast spend low comfortable

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
