9395a7dbdab973756787adfd22f5c9246ba3a77d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:10:16 +0000

    band sometimes phone forward second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def sort_list(lst):
+        return sorted(lst)
