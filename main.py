143f5abf4ac674b2a3976569604632f69da73827
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:19 +0000

    tap wish neither finger listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
