5910cfaf510f95d5e86e94f3bd8d10699dfeed43
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:52:30 +0000

    afterwards connection ride emergency unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_error_function(x):
+        return math.erf(x)
