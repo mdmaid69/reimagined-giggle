052362cec7150b379cbc356ad9df0c825030ff1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:11 +0000

    deliberate last wild lose say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
