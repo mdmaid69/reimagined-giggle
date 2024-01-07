6abf2f7dcd9480fe7c20a40446e50e1c45f948ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:33 +0000

    beginning nose half tune freeze

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
