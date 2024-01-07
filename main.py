cd4bbbb72af85425994c9bb445758088045b0d05
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:32:29 +0000

    chin rent suffer check theirs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def convert_to_octal(n):
-        return oct(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
