630c42f16323d3ff7848b9453f3f2b4ed1ecfe91
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:45:58 +0000

    bother past lesson fresh final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_even(n):
-        return n % 2 == 0
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import os
+def remove_directory(path):
+        os.rmdir(path)
