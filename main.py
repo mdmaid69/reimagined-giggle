63c05f94466e6ca18544978d7efb8e167478a009
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:30:12 +0000

    point twist in crime cheat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def append_to_array(array, item):
+        array.append(item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
