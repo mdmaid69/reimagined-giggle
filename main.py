bd9ac104b59f2f7c4a6aae7ba9a7bd4fd0d33f16
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:43 +0000

    dull spot suggest getaway history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
