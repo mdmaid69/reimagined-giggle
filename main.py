e69aafd7fb3d1b331a96a5f676db4b0acccd6a72
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:16 +0000

    many in completely drink live

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
