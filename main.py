1bdef56903a2bdd7ed5547f51e1ce8b27e0599ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:35 +0000

    rub switch start mistake dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
