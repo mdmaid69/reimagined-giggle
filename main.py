6db4c14bd02135b9ee264729efa564166c84d29b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:53:58 +0000

    difference apply health uponon press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
