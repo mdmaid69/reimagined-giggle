7329f6306eba3bb67ad49dcf2a6afed43743d3da
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:40:36 +0000

    couple rate crowd serve hurt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_max(numbers):
-        return max(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
