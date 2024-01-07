cffd197c3dbb404407d7a4928480ca299f4b71ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:29:14 +0000

    foot go create row temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
