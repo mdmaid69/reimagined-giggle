dfbd1dc32a717c79bb3b50eb007b540344d5aba5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:21 +0000

    raw mail mark weak live

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+print("Hello, world!")
+for i in range(5):
+        print(i)
