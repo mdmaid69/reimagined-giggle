ebefeb10171fd4e3bc8fff65611800adfa34b9f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:10:53 +0000

    close tidy shop describe skirt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
