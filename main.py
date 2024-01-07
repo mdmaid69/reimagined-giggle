dc51da066f50c841a41c668961e42f1b671e44e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:53:10 +0000

    sail deserve behave thinkthrough excited

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
