a36febfa67cabe3816e63c7f1ab90a9f12d2f153
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:16 +0000

    congratulations discuss rub shut limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
