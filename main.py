5dbe71e95812c235a383f09165e96aee8afafcb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:05:33 +0000

    soon accordingto stand dangerous computer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def count_characters(sentence):
+        return len(sentence)
