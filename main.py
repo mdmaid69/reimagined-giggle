bfb2cc95e60eff01245ea3238b2fbdd1a414bc5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:45 +0000

    press shine boil allow whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
