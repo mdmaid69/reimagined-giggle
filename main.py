3eed5457bc46696bb5e0f8620604be97aded6dc8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:30 +0000

    branch combination join breakfast introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
