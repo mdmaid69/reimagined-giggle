c39e7941ace086d417fe595db6bf5c87829ab7cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:30:40 +0000

    unit flow exchange free stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
