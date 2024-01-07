a4b7220839c2ece4870f72c095ad5a0e1ebf88f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:42 +0000

    method or learn bag themselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import math
-print(math.pi)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
