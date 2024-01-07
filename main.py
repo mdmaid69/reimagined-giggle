974ec38f545a0741e059d53274f2c92ab1bc5db0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:25:13 +0000

    precise while common instrument used

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
