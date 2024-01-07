d7f33aa1f7a262039f4ea92d75599180cfb32fe5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:28:05 +0000

    hurt platform doctor dress trial

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+print(sum(range(10)))
