b362979621c368081d6bc7d593b9f0c4a431d2c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:30 +0000

    guide almost Vv play leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-def get_current_working_directory():
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_current_directory():
         return os.getcwd()
