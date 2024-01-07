554ec5383fffb6048e698ace0f1fd9137e10f127
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:20:45 +0000

    demonstrate remember hurry broken while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
