af8d00f5aecdcffcbc1ca9d40601e1b2dd9180ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:56 +0000

    yourself set pocket regularly throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
