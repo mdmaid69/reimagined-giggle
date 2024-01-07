cfdfcd6032ce9fc948eb46c318a46f059baae489
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:33:47 +0000

    odd holdup chase pollution welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
