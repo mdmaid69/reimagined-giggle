3fcf402fb8c7ae189c2f8dcb79cc5347a043668d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:19:01 +0000

    spring sign market string waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
