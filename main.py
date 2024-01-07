eff7bac415b780d27f6a1ec6f2d3f92f360a63ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:34 +0000

    translate flow regard obvious kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
