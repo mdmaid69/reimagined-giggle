a07fcdd0d18e48b743a3781d2fc5a72ecf411caa
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:54:47 +0000

    impression interrupt ours hair tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
