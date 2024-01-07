77f8ee52ed208f0226a29c25f6a378f5423eadc0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:36 +0000

    leave turninto bag neither there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import sys
+print(sys.version)
