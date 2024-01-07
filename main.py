ed8e8a24b12d24b9ddad7732dae07551fa770677
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:31:20 +0000

    introduce consist low centimetre word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
