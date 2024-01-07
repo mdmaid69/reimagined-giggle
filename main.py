e1154fd995a04d99948e2831998861ecde5ae593
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:31 +0000

    wash favourite past clear callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
