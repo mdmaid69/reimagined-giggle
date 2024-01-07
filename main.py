adc9d8a459e9009c71ec3d6b6d10131389613e3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:15:32 +0000

    relax something off whenever hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
