6f5e61dd42aeced734cbd60928218db38ec395e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:13:58 +0000

    grow breakdown store wish Hh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
