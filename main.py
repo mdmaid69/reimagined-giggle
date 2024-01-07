674ab729f0dbe38a22549ff0b519c8810099fef4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:33:36 +0000

    breakout boat furniture admit community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def sort_list(lst):
+        return sorted(lst)
