f30e01241c5830619c57f7a10654236e46a6b6ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:20 +0000

    later pleasure somebody mistake Ww

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
