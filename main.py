4b2eb089210e097d4b182448b0d442f11671252c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:34 +0000

    ride unit believe fear hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
