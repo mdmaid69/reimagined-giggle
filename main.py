1f5035db072b3c2a20a9a0add5e0cf15dbdd67bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:58:35 +0000

    holdon sleep murder concern quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
