8154d5339964edca39387b6f31beebabedb9ab75
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:23:15 +0000

    attention store brain afterwards temper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def print_python_version():
+        return sys.version
