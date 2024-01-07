c98f70cf234a7a32096d498bc2ed29b193e94117
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:40 +0000

    callout holdup lost turnaway lose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
