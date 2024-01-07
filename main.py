e8bae1243c9484f115427b68ca13309007cac02d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:56:07 +0000

    564encourage evidence guide bush grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
