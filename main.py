3bc0231f6e11e2f2e277500a98f70c9c4e3cf763
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:48:06 +0000

    give proof owe quit injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
