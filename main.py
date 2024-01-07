a9263af765fd9f2ddcd402dd5562807cb7705bc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:49 +0000

    food uncertain band action address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
