6dd979e5b2ab3ae8191f79c8e8bc6f5df87f6a92
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:00:14 +0000

    compare depend pool increase temper

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def is_odd(n):
-        return n % 2 != 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
