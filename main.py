5c080133eda7cb7da6c24f3106b8d4f9d9d1f7c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:26 +0000

    slowly underneath grateful before OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import json
+print(json.dumps({"name": "John", "age": 30}))
