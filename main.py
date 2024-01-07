853b00c812ba49f2853b917534ef3e838ed52162
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:01 +0000

    convincing to should there make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+print(os.getcwd())
