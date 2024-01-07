3eade324956b4d508628de3ebf95888e978b3a0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:43:51 +0000

    breakup so visit rely soon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-print(math.pi)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
