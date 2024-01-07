aadaf3b3f4fec5679162a2eeea5566973a1c87a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:30:20 +0000

    there confidence stop lucky general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
