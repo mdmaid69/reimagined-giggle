e9bdd41f5804d5b7c5889104f40fee2d76be9401
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:06 +0000

    thin eye whom almost sick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
