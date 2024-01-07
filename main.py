445fe2bd588f47710311eb11d199aca6080344df
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:56:01 +0000

    your service wise rent waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
