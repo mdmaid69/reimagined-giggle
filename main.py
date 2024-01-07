8e467dd472df0322cf764079ac0b962306455009
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:40 +0000

    occur use cum takeaway centimetre

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
