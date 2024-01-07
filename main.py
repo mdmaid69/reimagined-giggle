d1c8bfa1f15af3816664403dc329618380fd12cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:40:56 +0000

    design turninto as fact design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_set(array):
+        return set(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
