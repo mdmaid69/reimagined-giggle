df661d84dd7e34b9f28d2d92309a5866f14ab455
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:38:09 +0000

    little borrow recommend gather another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
