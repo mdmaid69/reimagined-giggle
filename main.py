7f3590f52f3c40b78851a568cdc3da4515036b47
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:00:31 +0000

    nor 179bite wave patient fault

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
