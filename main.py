4f928193a49682ed5588a0ca1ac187f6e1712b22
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:15 +0000

    rush damage why thinkover theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
