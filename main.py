e856bd84d3c15a754af81c71e15d6981817e1325
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:57:44 +0000

    callout strength know find fold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
