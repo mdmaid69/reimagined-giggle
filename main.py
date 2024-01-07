cc54ce60f27c238575ca4c9e9e02bdb7238475a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:44:36 +0000

    difficulty impossible bet brain language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
