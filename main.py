16087cbba8c580b8fdc42c4bbfc276a3e571de60
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:21 +0000

    juice show each person loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def convert_to_hex(n):
+        return hex(n)
