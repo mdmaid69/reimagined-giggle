38319ded1b87f6c650bdb9b3fe3cfaed42a7cc3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:40 +0000

    drink ability culture continue lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
