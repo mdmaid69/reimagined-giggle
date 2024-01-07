2e879446088c88f8f25efafc7de87a56dd5661e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:24:10 +0000

    comment move finally butter breath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
