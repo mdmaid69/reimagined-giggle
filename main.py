feca97d89c4966e563bb018149bd7c834c92b1d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:16:52 +0000

    reject herself fence part bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def cube_number(x):
+        return x**3
