7446a0bd0d2e83d922684042d054b9c3c4878cc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:32:05 +0000

    correct intend winter lean especially

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
