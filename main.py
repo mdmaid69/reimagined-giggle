dc4984e34c23b73636bfa0ed627a28db8b26eb6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:14 +0000

    dish average murder giveout disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_stack():
+        return collections.deque()
