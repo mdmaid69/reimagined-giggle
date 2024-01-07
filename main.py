70275a668bd49e06f93c6eab55415196965e6f7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:06 +0000

    comedown pity border further power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def convert_to_json(data):
+        return json.dumps(data)
