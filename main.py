94658dbf6e672c5a277cd3b82c661702f2715663
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:58:31 +0000

    many sensible rise police building

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def multiply_numbers(x, y):
-        return x * y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_speed(distance, time):
+        return distance / time
