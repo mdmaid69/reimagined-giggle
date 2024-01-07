9f7905414a8ad0337e13c1add554165c850666c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:44 +0000

    fear fresh tight either turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
