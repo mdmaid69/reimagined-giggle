c97c664e01b0e0e470cb9a50b1849a31cd525c80
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:40:39 +0000

    science own strike may put

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_set(array):
+        return set(array)
