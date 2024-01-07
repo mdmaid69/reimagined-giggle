985c59f2d25b31a396181f6ccf67146802f8c6c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:43:09 +0000

    disappear ground mystery for earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
