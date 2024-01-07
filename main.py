ba3f633db19923653b86ba431079fc99623d6537
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:31:07 +0000

    just love muscle nowhere bin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
