6d64fd55c25adb1533138606f33e7ccf1c83a444
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:39 +0000

    plastic issue talk joke strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
