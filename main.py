aa88695fd899a74d36a973bad73ff338568acef3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:47 +0000

    fear object mess gentle flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
