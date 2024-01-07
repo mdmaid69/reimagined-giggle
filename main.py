ed46f1b7b4922e00aa39272c26612218ced3d91e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:12 +0000

    half instrument owe part fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def reverse_list(lst):
+        return lst[::-1]
