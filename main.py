f041eb7e4cba5c00f79d206cd464645dd7253c09
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:05 +0000

    drive listen neck that question

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
