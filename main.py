1c2192e34c4e14ff15900ba8ace69f21eb10825f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:15:03 +0000

    habit weak petrol who fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
