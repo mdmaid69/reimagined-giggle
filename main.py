d6e310fae8223e49b5187915063b03a8083ad49d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:00 +0000

    help tough unless steam course

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
