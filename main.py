272db4ffaf9d468a79531acb3707db952915cf0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:35 +0000

    wind stage mark impression rather

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-print(math.pi)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
