8f45ebad70b4e13057e2c9362dc2e2d8b6a35365
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:03:40 +0000

    possibly split love write trap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
