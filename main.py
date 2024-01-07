8e7851cd900aab7aa3c8855f25fbc41763e50848
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:48:22 +0000

    aim disgusting prepare loose return

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
