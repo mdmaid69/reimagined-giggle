91a0c56d4af28910e60c89f66ad1d6433924408b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:30:35 +0000

    disturb reasonable minority reaction considering

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def multiply_numbers(x, y):
-        return x * y
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
