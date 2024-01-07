c38ab18e5310a97f37c98ad9361fb704b26f7ecb
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:58:55 +0000

    vote importance bus safe either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
