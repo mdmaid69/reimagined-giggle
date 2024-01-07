d75197864c5bc66486f0aae395d0aef15255e26c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:05:56 +0000

    control stage mad high factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
