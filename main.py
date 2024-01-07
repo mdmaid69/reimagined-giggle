7c14770a9df4f93092a06657d7d63efc9fcaab01
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:22 +0000

    priority except responsibility possibly she

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
