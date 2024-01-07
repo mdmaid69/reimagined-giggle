fc753fe0129cce100ce9ead1b37c5d99b9e9ca67
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:32 +0000

    cook neither principle not label

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
