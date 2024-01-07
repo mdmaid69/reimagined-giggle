4cbafe2a755a65771a8b69d77d4c82460c8ccea0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:42 +0000

    wave or proper to skill

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
