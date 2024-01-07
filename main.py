647ad2376d565e166be67e61b636931d02574fb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:57 +0000

    interest think dull valuable education

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def square_number(x):
+        return x**2
