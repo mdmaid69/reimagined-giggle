adadc618d4b28b04a77b11a07cc8f53f186a53cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:22:05 +0000

    favourite agree listen pleased ask

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+name = "Python"
+print("Hello,", name)
