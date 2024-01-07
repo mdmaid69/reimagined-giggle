f5539db5344d8661399eb3a095e14f3177d77998
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:53 +0000

    gas none extra favourite will

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
