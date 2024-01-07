2105a5dafdee94b9cae5d16b8f6e20d944fe6230
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:11:13 +0000

    used talkinto boring down instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
+def calculate_speed(distance, time):
+        return distance / time
