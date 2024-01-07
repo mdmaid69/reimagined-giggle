1b51023b561846581a0336a4c94180198366fb3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:44 +0000

    zero behind fire bit moon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
