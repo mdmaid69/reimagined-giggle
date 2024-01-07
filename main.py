d61d00ea38448b6df79a051beb9e5cdbcbcfed26
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:18:40 +0000

    set tell part explain expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import getpass
+def get_username():
+        return getpass.getuser()
