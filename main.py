30b60da6506d95699fe88b10524f40210cfea996
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:44 +0000

    talkoutof gap place chance project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
