02fffdede9fcc6bea9b1e60d56859525d1b64050
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:46:40 +0000

    home prize spray hospital wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
