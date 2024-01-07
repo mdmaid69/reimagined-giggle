52a04f2a466570cff91ea8ffc8aba4bcf416cc05
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:33:35 +0000

    mind attitude sensible rush within

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print("Hello, world!")
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
