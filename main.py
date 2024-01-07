7b452eced7595c9f650f0932849cf7542a97b3a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:59:40 +0000

    squash step pressure side eager

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
