cadba0f1022ee6bd4f092473d9ef3ae231e42cd3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:42 +0000

    should refuse pattern story refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_list(lst):
+        return lst[::-1]
