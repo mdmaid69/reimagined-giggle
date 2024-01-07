f9a0c21aba34236359bceb4609d78f3e503b6e00
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:36:41 +0000

    judge cost sister Ee precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
