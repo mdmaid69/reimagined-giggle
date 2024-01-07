4f1839a6cf6735c4bff0b08f0fe3911c21e7253d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:04 +0000

    flavour spot at only sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_list(array):
+        return list(array)
