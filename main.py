307a5f5f3c840f3e64630c8a7fd7cef34e4bec86
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:04:11 +0000

    exam base change prize design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
