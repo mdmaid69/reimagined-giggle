b59b7fa996c7db8f0c9ffa9807a5b817314ce301
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:42 +0000

    shall stir patient interrupt kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
