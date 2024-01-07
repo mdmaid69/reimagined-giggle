23a521aab648e1a3357c81c9fc5e3ffc05fb4c1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:57 +0000

    recommend language greeting high of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
