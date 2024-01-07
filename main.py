99a70ac6214fb44cc1ef8847cd19223d1ee54046
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:56:43 +0000

    bang below stand firm shall

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def reverse_list(lst):
+        return lst[::-1]
