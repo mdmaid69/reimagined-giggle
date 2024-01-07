f663e6e6ef80554254f69a3a12e3cd6321ee65b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:57:07 +0000

    issue collect love effect gift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
