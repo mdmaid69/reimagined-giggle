ea8811fa3b6a51b16aa4dc4104c39466472f60d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:07:17 +0000

    march work situation lesson runout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
