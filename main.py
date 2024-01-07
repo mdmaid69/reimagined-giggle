0464a2e3854b1be7df22a3c8eefd82099762040e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:07 +0000

    further complaint sell string cool

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
