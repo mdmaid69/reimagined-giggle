addaa539bc362a93acab977440428279de879f1c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:07:26 +0000

    art late flight pocket tonight

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
