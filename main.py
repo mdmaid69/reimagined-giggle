a2df339c42831c357a1531f5af4e0a77bc5d47d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:52 +0000

    satisfied fault turnaway correct art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
