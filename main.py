713d57f94ddfccadde51ab48d315ea58ecc25b2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:56:33 +0000

    onto tough event petrol not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
