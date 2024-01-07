2b74ca13a62d7dbb1f4ba0ba4b92155e9a2f229f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:14:16 +0000

    change dream fortunate bite wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
