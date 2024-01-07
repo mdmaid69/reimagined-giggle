bf8b7b46fad2f6d406a546f46954bf7475f88f49
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:48 +0000

    raw catch draw closed glass

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
