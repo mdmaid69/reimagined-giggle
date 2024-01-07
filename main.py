c6fc08910869e91183b2c847d88cc83f605f4004
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:34 +0000

    fold protest manage confuse free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
