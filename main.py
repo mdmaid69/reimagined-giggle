e8f17b6a46fec92668bfa5d0dc5fed4ea52d8a57
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:52:04 +0000

    strength such deaf mark there

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 def calculate_pressure(force, area):
         return force / area
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
