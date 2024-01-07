a3fb2348b6678d8248e4b430f4f1eaa493c63af4
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:59:18 +0000

    slide influence my port getaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_min(lst):
-        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import platform
+def get_python_version():
+        return platform.python_version()
