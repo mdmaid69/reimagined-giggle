a5168b5fdd58ae1ce804af1544f005c372ec668d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:02 +0000

    brain common copy talkinto fun

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
