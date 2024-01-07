7411add27d33ea6ee1806c4bcc635de3c03e4012
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:43:48 +0000

    angry pinch shout cope backwards

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
