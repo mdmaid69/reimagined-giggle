5fec56013a74d2c8f2e3706704f6ef7c9e7084f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:19:07 +0000

    proud estimate sort steal best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+print(math.pi)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
