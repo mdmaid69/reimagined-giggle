804a4fc1906d09cab3373420a28fd78d0df0d6fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:10 +0000

    carry argue admit month model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
