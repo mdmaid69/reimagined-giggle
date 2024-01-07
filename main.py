def9576ffcc3899c21357a5e3bd5027334351649
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:04:35 +0000

    type combine hollow risk bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
