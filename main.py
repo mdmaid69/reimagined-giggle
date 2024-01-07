56a3278beca6c62826df92c5c05144ae419eac65
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:18 +0000

    neck rate total place cruel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
