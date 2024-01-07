31ac0d9e114d070178154307beb8d57960dd325c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:03:01 +0000

    union praise include keepaway while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
