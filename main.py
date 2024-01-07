72670eb6469bc6a55760493cf45d3b87f8c9a2ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:49 +0000

    event study imagine free Kk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_min(lst):
+        return min(lst)
+import time
+print(time.time())
