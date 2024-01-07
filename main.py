652cca43f94de6e1ebef09d0899c6d89be49c91b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:20 +0000

    match positive food escape several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
