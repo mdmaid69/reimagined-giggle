040fa5e15f3c815e253872a742e285004ee253bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:03 +0000

    perform detail comment then 179bite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
