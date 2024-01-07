7e9e24202258b66e7879188db4ab646820b07ef4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:15 +0000

    then poor warn last Pp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_tangent(x):
+        return math.tan(x)
