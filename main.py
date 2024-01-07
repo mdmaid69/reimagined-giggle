eea2000f43d0754038ba934a985a42e9a22ec2cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:46 +0000

    makeinto target track post hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
