6b2a2736825b98c5c7168cf0c4a0daa709c2ae2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:47 +0000

    content high room tall marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
