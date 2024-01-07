da66f794bf0e81525ff0ff1488698e452273669a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:39:54 +0000

    comeout sort ordinary beneath holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
