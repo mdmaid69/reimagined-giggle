3bfcb8abe211bee82040cb8074dcee9003674833
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:55 +0000

    beautiful bottom equal cheek depend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
