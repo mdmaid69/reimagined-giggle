deb1a926fc6c257e15ecbb688b73e7ed50fada23
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:00 +0000

    far afternoon blind drive rent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
