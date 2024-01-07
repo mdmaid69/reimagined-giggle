18d560b961da5faa75a1c65c97d654074205953d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:51:56 +0000

    explain cope exist mail cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def find_unique_words(sentence):
+        return set(sentence.split())
