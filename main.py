f6b2ce60297112e2b24a14536059ae231514ff36
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:43:05 +0000

    suddenly themselves exciting away empty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
