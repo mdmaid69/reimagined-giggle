8e08a3d64a1978192dbfc9e732ec0cefe5364c19
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:02:27 +0000

    travel see poor place where

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
