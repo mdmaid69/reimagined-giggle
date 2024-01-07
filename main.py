afa9e4d25bff79d7833a607d7abbec8a7d8ef804
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:22:51 +0000

    deliberate before about thick inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
