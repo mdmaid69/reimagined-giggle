ce8864cd8010a4d125730bcc01c94148c0dc26bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:20 +0000

    amount earth habit start whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
