1181820bde62c7244352f26838b99eeb5a50262a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:28:39 +0000

    drop exploration way church about

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
