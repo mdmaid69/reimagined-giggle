410f4f581581a12ca064198ffa56d8dd8c832dbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:45 +0000

    tune shout butter high search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
