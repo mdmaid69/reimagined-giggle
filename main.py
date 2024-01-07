fdd64a471c98036f97d5606bfc9b13c5b6f4e5c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:09:38 +0000

    treatment bound wish deaf memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def remove_duplicates(lst):
+        return list(set(lst))
