9b6673f9c799f78cddd3d3747d6f28c342fb6544
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:20 +0000

    flight office fortunately know mood

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-for i in range(5):
-        print(i)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
