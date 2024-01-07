e51998483525eccb153bdd3954d5c2aec194eb22
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:14 +0000

    authority pull interesting block rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
