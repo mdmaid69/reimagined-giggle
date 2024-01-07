46b4f9a8eb58811cde43476ad4a3efb6fb6807fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:02:22 +0000

    spin fire uniform risk block

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
