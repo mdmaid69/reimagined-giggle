95fbb7e773fff118b561d5d2cad9b4069414dffb
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:12:29 +0000

    agreement listen geton matter stand

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
