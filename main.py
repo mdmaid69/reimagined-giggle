323add8b9f01552fb0cec719505ef37d5832841b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:48:38 +0000

    left used cheek deliberately choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
