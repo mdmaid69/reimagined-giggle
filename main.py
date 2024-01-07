cab02202b092ebcd80d6021075c122bba16a11e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:55:27 +0000

    process interest opportunity knock 572enter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
