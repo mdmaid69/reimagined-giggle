e3b802c68ff3b0811abeffc5282f142d6a507c3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:49 +0000

    last somehow emphasis nearly meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
