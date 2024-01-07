140740ae55ee5a5b30c2f0fc9be22a707e164454
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:39:11 +0000

    worth central message set minor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
+def is_even(n):
+        return n % 2 == 0
