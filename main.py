9b241d77f26870bfadb2557544645044fd6df923
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:43 +0000

    science active band coffee watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
