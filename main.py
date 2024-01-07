1f289e827515316ba45de1ecb7f22bf0e8c42196
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:04:32 +0000

    boil book hurry 566end size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
