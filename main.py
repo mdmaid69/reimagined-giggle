6884f3cb93ad2b4b77bb19200b274951536250fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:22 +0000

    Uu information shopping comeoff deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
