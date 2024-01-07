212cb22c8b44f58c11b6e3fd8fac210ac6eefdbb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:51 +0000

    mind since typical connected slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
