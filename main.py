756f70f6d06e1455748569673c574a3d6cdb1bce
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:49 +0000

    reply welcome lost Jj blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
