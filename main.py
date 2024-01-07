bf829347caa6059132f779ddab7590d1b548096c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:30:24 +0000

    married survive search calm disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
