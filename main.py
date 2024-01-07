9f5be0e3e8fdcda7bdac0e99ce0f04cff8999c9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:07 +0000

    air refer poison sort swallow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
