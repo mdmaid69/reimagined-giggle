618196e01f0dcc66f1cda58e359c7cfcbded22f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:01 +0000

    shock secret into judge prize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
