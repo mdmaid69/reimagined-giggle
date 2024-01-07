1c9b0bc11e16a9b5426011f84224e892f6ee7023
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:09 +0000

    now equal ashamed love Ss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
