422f2112dac7f89b869760afe5c653a3257b2175
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:44:32 +0000

    common pure aim arm move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
