5c7ba4cfc8d95b2511ca3cc9f59643fb16d05e9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:02:30 +0000

    opponent bringforward carry disturbing argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def reverse_list(lst):
+        return lst[::-1]
