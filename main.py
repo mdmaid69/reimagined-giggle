3c8b335c3dcec41072c9c76c192f4e116c7a68fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:05:30 +0000

    steep rather pause notice original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def convert_to_octal(n):
+        return oct(n)
