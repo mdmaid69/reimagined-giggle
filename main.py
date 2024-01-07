7bd7fb905689d0a5743d1956b5fbfbacaf75df8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:02 +0000

    everywhere active cold patient best

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
