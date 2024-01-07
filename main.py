2bd342ece896c1644905e21bc9c389e7d0aab927
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:11 +0000

    blind from about dry negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
