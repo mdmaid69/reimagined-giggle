4222bb7f1637540fb7b25fb83c9f769af3fb1d23
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:53 +0000

    bear serious sentence trip piece

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
