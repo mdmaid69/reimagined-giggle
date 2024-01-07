e5e40e5ff5084930a87b57b6d9acd72f50d357cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:18 +0000

    flow shade fast mine fence

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-print(time.time())
+def count_characters(sentence):
+        return len(sentence)
+def convert_to_hex(n):
+        return hex(n)
