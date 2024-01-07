84cdda5617caed0b0f55b987d064be56a88b8bb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:16 +0000

    uncomfortable hide difference spoil true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
