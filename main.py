dfbcd6cf20cf1ddb7557ae597147b0d34a6ce392
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:33 +0000

    greeting child amount scared crime

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def divide_numbers(x, y):
+        return x / y
