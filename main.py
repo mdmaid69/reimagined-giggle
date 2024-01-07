1ef5e539f37bd8d9affbf00393bf1941781e5599
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:04:57 +0000

    fortunate other where above marry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
