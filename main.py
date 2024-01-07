7c130a25440f9d55369e1c63a77f470889ba2e46
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:08:33 +0000

    Ww brief force congratulate introduce

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
