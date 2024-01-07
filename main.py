fb09bca74e691adc7a3730a2e7e43ea58388a8dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:21:54 +0000

    pause that post reduce pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
