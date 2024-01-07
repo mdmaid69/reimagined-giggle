9137a04644cb6eed9ff6995f556557f73d40e480
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:39 +0000

    their pure worse upset message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_max(lst):
+        return max(lst)
+x = 10
+y = 20
+print("Sum:", x + y)
