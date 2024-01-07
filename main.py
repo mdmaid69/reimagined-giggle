add2390de500832d368403f473883f5845b4ceaa
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:49:44 +0000

    objection pupil before mark breakinto

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
