b3695f43b7b1e6a0c980e0cd6aa769d3744c2422
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:40:05 +0000

    somewhere allow trial store it

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
