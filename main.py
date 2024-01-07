c3ba77d6b0646496a497b454ae37398b0b8dccc5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:10 +0000

    shape make copy wipe holiday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
