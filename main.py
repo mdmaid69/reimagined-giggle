2218b5b75dfa127a4f79140e13e963f8a42e30e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:43 +0000

    invite circle far sure test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def add_numbers(x, y):
+        return x + y
