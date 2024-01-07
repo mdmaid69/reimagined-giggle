2e202333f7c723c43c412d5b0b17241275f6df7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:16 +0000

    anyone live deliver congratulate light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def reverse_list(lst):
-        return lst[::-1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
