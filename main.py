c66e6bb42ef8c238a15f3110a74d8fda5d2c2cf5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:30 +0000

    neither by present bring care

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import random
+def roll_die():
+        return random.randint(1, 6)
