e2604707808a75df40a835b78ad52211ad13941e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:07 +0000

    shape job shrink but week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
