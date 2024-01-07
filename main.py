8abb614126758c9437cdf796326936cd3384cc02
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:13:28 +0000

    special child slightly home damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
