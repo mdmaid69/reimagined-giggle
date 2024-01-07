d1fb092d7496de98ddbd44e38fff45caf485799e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:11:14 +0000

    unlucky trial away general comment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
+text = "Hello, world!"
+print("Words:", len(text.split()))
