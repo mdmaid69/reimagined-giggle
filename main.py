0470d5774c7c212244dfc995a7b7af2bcfe94b01
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:45:27 +0000

    depend dead either split immediate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
