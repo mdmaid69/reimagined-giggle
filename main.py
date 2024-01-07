c700d8cde2495588e472340a8839c4cab89a7345
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:51 +0000

    radio through shelter turninto form

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
