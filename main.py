fecd45bbaa713f5288f0a8d61a2a040798e35db3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:09 +0000

    scheme turnout goaway kind strength

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
