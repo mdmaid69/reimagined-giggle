dce6dbd98e128e62e01d8dee57b4a2548922db37
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:15:39 +0000

    information form star success dark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
