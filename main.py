e861d54c41fb24495f3071dc26806b2217259bc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:36:43 +0000

    female reply centre interview school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
