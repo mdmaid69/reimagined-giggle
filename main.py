d2bccaf9260845f9cdae84536050a2b570774a3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:28 +0000

    ride tooth tear table objection

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
