ef5acef2228b78dee63f818cc7b00c3e601235f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:14 +0000

    rare smell quarter organization demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
