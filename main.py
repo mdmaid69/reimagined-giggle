71f25dde6afae9cec9d7e67fe7d9edafa18a3611
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:24:47 +0000

    how true cap more flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Sum:", sum(numbers))
