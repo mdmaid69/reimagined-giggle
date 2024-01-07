c1ebf17e5d311b30607ccd7f05ec7f00ea055e1c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:00 +0000

    dream unpleasant always approve crowded

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
