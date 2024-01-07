7e8cd0198afb5b032de730b53d2fe8a30b451f15
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:01 +0000

    prefer usual give expensive study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
