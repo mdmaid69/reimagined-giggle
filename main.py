786c34ca5db0a891acbae3339045a990cb5a8a46
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:49:07 +0000

    will hospital content free another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
