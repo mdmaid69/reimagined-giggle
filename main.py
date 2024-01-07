9b5d1ba382d51ca3238af36ec5dc5dba573cfd6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:50 +0000

    determined chance fold aware shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
