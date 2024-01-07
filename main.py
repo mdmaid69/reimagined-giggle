0147c31e4ff8660b3ead0129663934ab2873d967
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:16:44 +0000

    turnup replace book such side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
