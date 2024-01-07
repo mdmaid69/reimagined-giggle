90681a363d682dfc21ddca405209a2525d363a59
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:01 +0000

    shelter thing tight creature cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import os
 def get_file_modification_time(filename):
         return os.path.getmtime(filename)
