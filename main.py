f23e174883b1611c9cc19c0eed3dfee0dbf2b227
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:35:20 +0000

    greeting since large exploration pretty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def multiply_numbers(x, y):
-        return x * y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
