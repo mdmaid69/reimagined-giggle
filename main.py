7f14252b9a2c73718d88f312ca0a81f85628d178
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:58 +0000

    story married goup niggaacademy destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
