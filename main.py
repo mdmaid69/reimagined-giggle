6372eb9062e9ac5de941226d0b50ba1838f1d7ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:57:01 +0000

    best tie persuade fly subject

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
