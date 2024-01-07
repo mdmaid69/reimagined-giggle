5a914ab9effe55c8214c21f019ba6ea3f374bb60
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:09 +0000

    unkind pot accident add communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
