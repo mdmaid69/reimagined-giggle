946344e8f9ca5400fad6d048a9983dcaa1ad5409
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:55:09 +0000

    study cruel wild risk left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
