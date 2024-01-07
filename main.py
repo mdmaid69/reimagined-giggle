64e959d2f2c45d5d89e62cfbad9d40c058c27a7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:36:03 +0000

    among mainly bend exaggerate boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
