2b955caf5ce01deb2f0875cfaa8cd9e1085cb409
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:12 +0000

    scale near always sore wise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
