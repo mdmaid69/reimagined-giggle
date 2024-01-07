b004270fcba67da429d2d75fc5a4587cf1ce26fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:42:39 +0000

    brave either willing announce smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
