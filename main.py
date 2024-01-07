f003b63b3cc64c8266e1ed042c90669470e6ea8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:28:27 +0000

    tired trade fear short towards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
