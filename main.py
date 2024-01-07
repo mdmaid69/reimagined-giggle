9439cff2b2fd1536ca6c81e04934849912f8eb62
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:53:13 +0000

    Mm be now completely radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
