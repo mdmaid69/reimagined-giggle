680e784016edd82bd87693f100eb5344aa4ccc83
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:14 +0000

    approval mail block 573entertainment secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
