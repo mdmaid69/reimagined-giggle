2bb9828dd9411a396767b896ae5be3010c933d85
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:42 +0000

    spend expensive cinema tough open

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_unique_words(sentence):
+        return set(sentence.split())
