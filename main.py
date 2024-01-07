1a5f96da452922deabd422305a41bb3f7091499b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:26 +0000

    ourselves give anxious shallow late

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
