17d4575bd7ba74b4a8bd44c5a7650bb8a0c403c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:32:25 +0000

    suffer Pp little Nn compare

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
