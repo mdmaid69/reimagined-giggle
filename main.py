0061557e538715441fb3e5a1055a145c545ac3ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:03 +0000

    exact contrast forever left friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
