879432bb6e5820d4c1829818e152e8566239bc71
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:49 +0000

    ahead popular control treat criticism

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
