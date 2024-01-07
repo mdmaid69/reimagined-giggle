4c813265e7366bbf0de3a2047cdbb251a4cf5cec
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:26:06 +0000

    advice blind I agree children

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
