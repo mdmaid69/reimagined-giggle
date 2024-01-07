c4f48e4ef505d0dae247e9c808e5e93a1ca1b985
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:00 +0000

    late raise plenty against unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def count_elements(lst):
+        return len(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
