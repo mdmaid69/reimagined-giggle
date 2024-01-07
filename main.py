d5c8e5b34e090291bff5bde602d49feff5eca876
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:37:09 +0000

    natural weather cruelty blind light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
