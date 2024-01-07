0d9a34c07090103d5c31acd7c552af2a91191335
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:23:54 +0000

    where gift trust answer completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import time
-print(time.time())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
