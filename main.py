2eac2ff1a9de1156801a1dae6ee70573cd6fce1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:04 +0000

    sense hungry fortunately experience would

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
