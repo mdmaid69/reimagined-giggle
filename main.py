679071d2e11c2c6db1b85a4e1e4e87829f62759e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:19 +0000

    seat surprising 179bite border leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
