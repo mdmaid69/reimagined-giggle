fbd5480efe55ebedf23099529df43d08569413d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:05 +0000

    room ask frighten dangerous before

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
