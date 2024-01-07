2e39bb1796a0f0d0b9d951188e31ef1debe336b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:50 +0000

    flat cry office chin attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
