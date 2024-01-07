39976fee47f0810d765dc061288ff3eed559fc59
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:36:29 +0000

    willing use guilty me border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
