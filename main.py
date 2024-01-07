a866da64cd494d73eb998b53e5450fa815aa15e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:19 +0000

    makeinto spring convenient worth style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_tangent(x):
+        return math.tan(x)
