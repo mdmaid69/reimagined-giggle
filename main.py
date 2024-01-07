a7ac443734194d3619c3141e022c0b4458d891fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:48 +0000

    runinto comeoff this expect really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
