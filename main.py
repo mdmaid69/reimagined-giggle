3619a8fcabfb5201374808ee398236feade6d4b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:07 +0000

    refer serious accidental satisfied neck

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_speed(distance, time):
-        return distance / time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
