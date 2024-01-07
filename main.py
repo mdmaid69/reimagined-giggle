0ffc5d3f696a83d6bf0252b6c3db0e6d7c69add2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:07:47 +0000

    target influence little steady represent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
