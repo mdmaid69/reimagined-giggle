185f96cb2fa2a55b52ab190076ba6ef9a6cef316
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:47:50 +0000

    post cheek regret slow hole

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
