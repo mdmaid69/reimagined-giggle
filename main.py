c9dfdf85d634a403357f22e73e9ed583737d03de
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:09:17 +0000

    rain offence off gooff thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
