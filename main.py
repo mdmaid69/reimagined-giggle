5c47d9b80e38ec662123801da46f46d8b03b5bd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:54:10 +0000

    control whenever may weekend watch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
