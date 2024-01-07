c4fd5d1e113e4a1edb79378fbdcc2efd16f67698
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:30:53 +0000

    object breakfast divide stiff bear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
