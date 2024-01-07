f0daacda469fbf35e567f143e90349ee90611945
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:48 +0000

    copy turnout sentence field strict

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
