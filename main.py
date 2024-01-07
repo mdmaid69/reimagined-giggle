092e9543f3983ab0b9c12b52ee87ef965497438f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:08 +0000

    read fork expression race spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
