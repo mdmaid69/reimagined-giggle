2ce0e9cbe16deeebe8db8a478c75db1b8c2a071b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:35 +0000

    under empty heat argument entry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
