00cf8ba2415c734f57cdad00e5dd4159aabb0790
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:01 +0000

    interruption they never eat very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
