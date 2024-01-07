74031d9b98a569db13b7c92f39bbb72a4d07f55e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:33:32 +0000

    wake tall head bin proof

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_work(force, distance):
+        return force * distance
+import sys
+def print_python_version():
+        print(sys.version)
