87951c508595cb732be996f948b8b4c71f472be9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:06 +0000

    wish meet play system certain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
