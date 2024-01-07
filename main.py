1d4180bc70f7fd2d13f91d4f8b06dc09e7ee9d5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:29 +0000

    respect beneath refer question set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
