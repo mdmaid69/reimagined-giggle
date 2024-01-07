2f36bb3863251f1f91eb4e27788e7022a49d7516
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:31 +0000

    use length national down surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
