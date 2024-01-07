1487e7f3d1b78af69f23b779455de8fbaaab7765
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:14:11 +0000

    measure read develop approve cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_float(array):
+        return float(array[0])
