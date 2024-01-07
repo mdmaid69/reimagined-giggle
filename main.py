9500ac8ea5edf175d46563f7b2dad5b20e9fdf3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:27 +0000

    man centimetre small form union

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_string_from_array(array):
+        return array.tobytes()
