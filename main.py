151ef3a6eb244ab04239b6ceee4df0636bad20fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:01 +0000

    surface business feature whole criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def convert_to_octal(n):
+        return oct(n)
