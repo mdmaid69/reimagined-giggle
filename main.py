b91b94161cc603b9faa106146d0b69007e1190ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:42:13 +0000

    better less seat mass accident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def clear_array(array):
+        array *= 0
