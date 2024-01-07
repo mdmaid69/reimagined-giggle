73b4d7578b7490fc51594f7d856b54bffd494c09
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:20:21 +0000

    degree tidy call lookup still

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
