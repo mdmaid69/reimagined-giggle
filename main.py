961736ad0a18365861276fe5db3a31d6057f43cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:51 +0000

    interfere far forever on field

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
