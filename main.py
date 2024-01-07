86e1b59f8027522b909bcbc7ec1d41992b31c3b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:28:46 +0000

    hungry except quit slice battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def split_path(path):
+        return os.path.split(path)
