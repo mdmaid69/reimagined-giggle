8fd133a5a76bd508a785c6369aa54394f74ca31b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:08 +0000

    minimum difficult eager sink check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
