19ac6c015e062bd6ce4ca7762cee78933cbee36c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:06 +0000

    freedom Ll region project use

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
