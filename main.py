f97203f21d433561c69afdc7f62153ad58ccf888
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:00 +0000

    poor female experience empty only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
