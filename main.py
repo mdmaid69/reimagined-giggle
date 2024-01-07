db4146f2b623354f494b6b35946bf38b259fc7dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:50:30 +0000

    even line hate save from

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+from collections import Counter
+print(Counter("hello world"))
