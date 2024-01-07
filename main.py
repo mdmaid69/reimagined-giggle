56d3655aa778b84c776d87a96df87d643c8c8206
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:08 +0000

    smart tidy month they press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+for i in range(5):
+        print(i)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
