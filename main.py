9d3c9939c5058cbaf03edc5fd3ba56d80472ae66
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:29:30 +0000

    slide circle future we pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
