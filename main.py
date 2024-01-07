a941fd00c444ce197d3c122bec86652d92898e7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:17 +0000

    issue pull exploration grateful some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from collections import Counter
+print(Counter("hello world"))
