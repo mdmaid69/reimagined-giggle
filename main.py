72b7a8cb8ff0fe771424062d679329bb963472ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:02:41 +0000

    confidence until back meaning comeback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
