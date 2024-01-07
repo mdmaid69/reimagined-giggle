e8115641d12d226b366d2a270a00f938385b29f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:42:21 +0000

    process summer low air private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+print("Cube numbers:", [x**3 for x in range(n)])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
