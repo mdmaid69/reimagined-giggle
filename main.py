caa5513e006353da8fe25e02eeb22011073ce945
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:04:45 +0000

    pain book feature star season

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def find_min(numbers):
-        return min(numbers)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
