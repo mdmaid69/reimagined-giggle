270b54d8f89011c4fb0a80d049827d98dbff677d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:35 +0000

    share pleased space stay only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
