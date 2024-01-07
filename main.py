05cdbc0ff12691f02b2fc16673fa084b53a7ad48
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:33:20 +0000

    warn shake cruel tell lend

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
