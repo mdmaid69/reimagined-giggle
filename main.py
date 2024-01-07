d57d1cf2bc2ab086d2fd0720385e254fbf1b910d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:31 +0000

    sick instrument burn glad spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
