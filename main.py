cf57a99b856a04d5c60a3a79c8c313356aa5db5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:08 +0000

    alive offer smooth light discuss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
