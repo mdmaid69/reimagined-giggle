d199758496e2db5359d9aa615b17ddbdc4c20284
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:41 +0000

    beneath system comfort violence used

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
