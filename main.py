de6413cae31bca273c9d13ac7ff13048a900b26e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:24:21 +0000

    top think worth bang public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import time
-print(time.time())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
