b1ee2a52b030c5846a40d634b295510c62d7ee6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:42 +0000

    stand turnaway through her contact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
