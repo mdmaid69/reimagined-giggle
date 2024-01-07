4544d7a0c243ae0a89517a572f20cd17baf7bdbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:14:20 +0000

    father difference to message discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_queue():
+        return collections.deque()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
