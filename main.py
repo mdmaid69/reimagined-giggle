de3e512f1f7865040bf68f3ebb299f84be20ac7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:23:41 +0000

    world draw pinch pretty smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
