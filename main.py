82d96c8418f6e65b61e2052b1ca814d20b4e0f2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:57:22 +0000

    holdout invite negative surprising pain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
