3e0b52501c7c1db996c6811d6c081c5885698235
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:08:48 +0000

    always ocean breathe target outside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
