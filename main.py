6d66fcebbb03ece04b19560038839a2c3c0edc6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:01:38 +0000

    sort admit downstairs gooff uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def cube_number(x):
+        return x**3
+import array
+def get_list_from_array(array):
+        return array.tolist()
