14e31935b63f271a5251fc7a7fd9a8be3b4381b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:25 +0000

    forget culture voice if breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
