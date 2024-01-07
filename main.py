ccb7e83c151968981d7f7b3281e67c0a36a327b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:04:12 +0000

    brush pay meet scene 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
