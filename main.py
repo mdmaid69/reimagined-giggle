77ff8ca11452511e05cfd1a93f07e571d4fe8215
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:05 +0000

    tune apply collection separate Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
