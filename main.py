bc810570c3f226ae0420351b28c599085156a92f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:35 +0000

    smell stare apply believe wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
