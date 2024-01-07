fac7edc0840e37a9c107c4e3618ff309d7e85674
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:13:23 +0000

    pencil you copy square suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
