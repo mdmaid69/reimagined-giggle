f258a41a6d2df854b47c2a3f34847a12798c49b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:20 +0000

    black thumb fire watch value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
