f0c59096e034914ef361ce657117ac1a04ea2c18
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:12 +0000

    company check fortunate count shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
