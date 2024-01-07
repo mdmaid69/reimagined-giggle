8eb72e65dd8685f755ed224e0ac4139bb4a4e03a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:49 +0000

    tense total about Yy spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
