0e98fb55e47eda1939c466556b7ac1aea459bee4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:18 +0000

    strong part number thought thinkof

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import sys
+def print_python_version():
+        return sys.version
