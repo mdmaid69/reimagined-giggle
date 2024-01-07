e25f1e332c9847c0d03be85e67388267877178c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:25:46 +0000

    form release place fit pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 def split_string(pattern, string):
         return re.split(pattern, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
