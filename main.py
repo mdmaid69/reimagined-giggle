45f27d4b087d5cad201f4d0878cbce3730b7144f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:22 +0000

    gooff number marry indeed ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
