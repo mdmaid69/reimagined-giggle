b8d8c1e108d46e8bcecce641a07d61559eeea323
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:52:29 +0000

    confidence stupid reply habit still

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
