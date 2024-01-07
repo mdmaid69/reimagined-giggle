03fe26d9cdbcd5fa92e9baeab5181675fcbf5689
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:58:24 +0000

    feel Zz tomorrow better make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
