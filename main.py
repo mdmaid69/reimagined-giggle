0e426ddb3c8383fdc05f07aa1c73a4c54233d267
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:20 +0000

    direct branch Uu sticky extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
