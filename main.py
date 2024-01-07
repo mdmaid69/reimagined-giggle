c57740753c416ecb7f5eedc8c52d3ef99423f46c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:31:46 +0000

    or yesterday march position book

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
