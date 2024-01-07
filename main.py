ea477777141d1d64a1c036b4fe0d1a318b84e6b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:37 +0000

    sight sorry both emergency passenger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
