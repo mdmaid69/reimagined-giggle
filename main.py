ebd7544e3036ec4d0366711e087055a09d5b9821
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:16 +0000

    result formal disappear story strike

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
