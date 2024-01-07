1742272aa4ce9606af60d68083881dc45193cf52
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:03:44 +0000

    play breakfast call weak idea

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
