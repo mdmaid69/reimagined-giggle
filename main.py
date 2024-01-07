b7374a5a9850a7b82e8e50e25308f624df12853a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:59:48 +0000

    dry maximum demand temper slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_directory():
+        return os.getcwd()
