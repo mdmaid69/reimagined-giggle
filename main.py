c43fc9e889b14392cc12055a173e317f06d58d0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:54:55 +0000

    believe communication case wash more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
