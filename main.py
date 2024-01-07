f72313c0a92978edf6debf319e2de927445aa03e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:01 +0000

    impressed close rely great in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
