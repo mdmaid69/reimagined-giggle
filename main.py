40d87d76f74cc9a276c08f96f4a46848433fb7c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:55:18 +0000

    imagine top cruel brush protect

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-from collections import Counter
-print(Counter("hello world"))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
