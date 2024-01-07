fc90057ce2df24121c7a9f196660422cd7e8b279
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:43:40 +0000

    holdon beneath aware remark mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
