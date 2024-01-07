2540901ba44f8dddb069fba2fdbea96009f01c90
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:10:28 +0000

    ability tough real trap single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
