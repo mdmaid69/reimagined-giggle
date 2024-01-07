7f73aa81375ec6c14f8bab118bfd53311715d54e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:37 +0000

    contribute us appeal bad congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_tuple(array):
+        return tuple(array)
