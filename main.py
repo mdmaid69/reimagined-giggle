aeb609420d7b87c65ca50fd3ca2103610552db67
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:27:09 +0000

    hot left float beneath target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
