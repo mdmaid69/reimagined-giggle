0f21cb9b05dc5ed8e17ccf6123baafe19485d561
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:56 +0000

    welcome play their centimetre children

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_words(sentence):
+        return len(sentence.split())
