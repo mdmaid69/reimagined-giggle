279f8948a7565a5088318f4bb3b526ba2d55cec0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:04:51 +0000

    nice respect each pause theirs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
