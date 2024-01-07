0c9d5b6db2c6379562c9b77b3d4d03922d56ab0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:53:49 +0000

    smoke base spring medium grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
