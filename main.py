6ca1ca328b464111a7f29ee52bb9fd776f5a8c05
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:07 +0000

    regularly inside instant air bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
