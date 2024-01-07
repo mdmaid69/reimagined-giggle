3a44cddfeda08386cda7cf2d39639df3f299524f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:46 +0000

    holdup amount difficulty discuss exciting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_as_repr(array):
+        return repr(array)
