8ea18bc12d3a0e4ee79c1ee54a3703d22cb38f9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:55 +0000

    loose set upwards mad could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import math
+print(math.pi)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
