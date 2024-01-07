9198ee4a0661e08ba9b8dbf3274ff78d9cf40ce9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:28 +0000

    afraid fit aim reject cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
