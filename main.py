45fee39f7bf75d4f3d73be78b10c692ff68afb13
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:33 +0000

    far performance anyone production Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
