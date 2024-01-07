f3cf049cb33115372757373b87bb54e23db53c08
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:44 +0000

    still object too lift stretch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
