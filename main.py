8f26bfe45668900dee6216d29f11007d1fb1e5c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:20 +0000

    Nn loss passenger fire none

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
