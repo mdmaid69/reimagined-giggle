b246867a44f31ea2682036d62a84c9703f46d5cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:59:41 +0000

    giveback promise back smoke old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
