3308f1800dcc138ed694efb7ef6ae8265e7a9e19
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:50:11 +0000

    moment interruption die find influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def convert_to_binary(n):
+        return bin(n)
