27aad39ae6fe61c4dc1cb975a5e364420c7c9273
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:54:02 +0000

    dear tense against callout brief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
