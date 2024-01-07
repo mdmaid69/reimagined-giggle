5895d50bdf5ee6ff49163f7fe8437e202b10d60e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:56 +0000

    boat near before communication correct

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
