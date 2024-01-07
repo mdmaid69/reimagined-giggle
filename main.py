2f069c297319d177bf5ff81fa8811a24af703051
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:08:40 +0000

    need Ii hardly closed Ff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
