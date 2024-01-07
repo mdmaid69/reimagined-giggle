5bf1bd445db246f2e067e70fd3b270d395fffc3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:03 +0000

    hand interruption easy stamp use

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def add_numbers(a, b):
+        return a + b
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
