3a743ecbef8d7eb37bc400dc6590c829fdaa9284
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:32:28 +0000

    temper stage sell 571enough row

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def convert_to_hex(n):
-        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(lst):
+        return max(lst)
