594c6254413b649263fee5394c8bcdfbef34debd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:59 +0000

    sleep shop harm sick breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
