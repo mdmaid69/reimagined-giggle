8289a163a90dff91c8a5716afb635474e6621080
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:59:45 +0000

    grand set intelligent nose explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+for i in range(5):
+        print(i)
