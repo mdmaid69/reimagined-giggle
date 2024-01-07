84ff3e96c0327d53a2423a2f1680bf0f7c39f832
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:04 +0000

    attempt defeat season snow education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
