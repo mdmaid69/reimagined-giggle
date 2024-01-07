7af5253c7b7f1ea89d4227b3331d5f4fad84d7d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:06:25 +0000

    possible mark offend advanced usually

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
