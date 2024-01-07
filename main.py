2a21c122f3fd22e77c505c1572b592254e46a131
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:49 +0000

    shake hour visit rare disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
