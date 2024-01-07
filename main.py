84f846e88a0d7b665ae7416c29d4c9f2f75ed927
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:07 +0000

    offend new apart busy past

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
