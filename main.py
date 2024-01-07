76cb5e56883440e93a0c514a606907d2594e5df3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:21:33 +0000

    use subject fix change funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
