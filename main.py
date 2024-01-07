f05fbe9287b36c97ab0cbb60670aa5284a3eec19
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:20 +0000

    scene likely test plenty public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
