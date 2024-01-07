d8819571bff1001868493c9aac32156972307e95
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:28:23 +0000

    exploration only false turnout keepoff

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def is_odd(n):
+        return n % 2 != 0
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
