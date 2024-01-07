5d6e5d4875298156fd72ad663f7782120404b611
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:56 +0000

    mixture exist that hurry middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
