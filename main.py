b77d95bd1ff6a1c40859cea7b79f68026f928e60
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:38:30 +0000

    television colour 571enough pretty only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-print(os.getcwd())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
