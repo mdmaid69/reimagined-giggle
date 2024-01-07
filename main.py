91e3067e9667aa0eedf4df6de52815b6e2d5476b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:59 +0000

    scrap nor circumstance honest hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
