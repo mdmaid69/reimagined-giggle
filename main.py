595b3366600a3cb7fee2ff86b141b379108324df
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:20 +0000

    well future rather think class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
