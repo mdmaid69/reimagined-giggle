4b4246e6a0d0b235f16c30ae1a3724cf6e4f6939
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:13:02 +0000

    exercise tape information and approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
