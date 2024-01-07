26bec03c614d1dda25d38ec3148930bddb8a1292
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:07 +0000

    round remark culture equipment stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
