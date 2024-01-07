47f188f740b30ceadf6607535f57236e17049743
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:13:47 +0000

    confident yours weak sink chase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
