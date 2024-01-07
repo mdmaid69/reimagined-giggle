9754f4720c3fd8fb02af607130913b0964aec0c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:43 +0000

    exercise suffer towards breakin proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
