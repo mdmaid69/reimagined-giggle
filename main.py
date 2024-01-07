ecb9ab17fe0aaa361927ca98b1fb2a68a179f8fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:30:17 +0000

    design boring explain stop 566end

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
