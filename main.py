cf229628813b707adf20c76b0aa30ba6c2716817
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:42 +0000

    new fire right finally holdon

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_item(array, i):
-        return array[i]
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
