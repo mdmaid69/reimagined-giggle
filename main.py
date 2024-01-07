fe1362009ef82f0a47f7577b892bb9305cfeee5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:26 +0000

    matter apologizeapologise priority form go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def add_numbers(x, y):
+        return x + y
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
