396aafeae03b0941aed45fae05c73358a946616c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:34 +0000

    scheme breath while mysterious sharp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+for i in range(10): print(i)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
