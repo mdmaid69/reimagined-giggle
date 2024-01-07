dda78954e28af48f28d9e049534b7b5abc68d5b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:58 +0000

    previous unable love corner creature

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
