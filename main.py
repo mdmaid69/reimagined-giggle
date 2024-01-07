ebf5ccae32e583445a12ecb2387732ae21402039
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:46:47 +0000

    so vanish male perhaps comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
