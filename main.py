224b3771f4eb3f100f52ff97db63a3078691f7cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:21 +0000

    ocean save seriously fall switch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
