eb7cf4d23258c7a3967eafa4b42aa545007c5924
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:00 +0000

    once tired attention become load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
