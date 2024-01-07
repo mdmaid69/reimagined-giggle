054b570fafab63c984b515682e2e03235b3a1588
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:20:04 +0000

    boil date circle branch combination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
