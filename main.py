405f12658c40ad984132c311bda1f23a4e3c74c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:54 +0000

    murder temper test getup tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
