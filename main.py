189a41ca25a4c6e34b19f79e88f993c0691607a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:41:49 +0000

    summer disappear quiet build eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def square_number(x):
+        return x**2
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
