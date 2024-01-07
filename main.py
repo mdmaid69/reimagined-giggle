95bc6b52da250ef443bec5ba7bae0116514e131c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:13 +0000

    leg sorry ask distant television

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
