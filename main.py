1857b0d87641b8db6246bc151e08fe4333f61ba7
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:30 +0000

    national poor polite Mm reserve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
