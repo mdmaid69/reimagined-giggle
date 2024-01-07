c8a3790edb0ab84052ae327c8d38c2f61fae4c9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:28:18 +0000

    lookback(on) but language their only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
