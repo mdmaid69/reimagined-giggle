468e59bc34114efe9af8cc73d0a7a2eb7e7c4b7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:33:51 +0000

    still holdout afterwards teach exit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
