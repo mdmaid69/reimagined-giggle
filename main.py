c293a2f345ce6bd7268ff0f33351c2ad567e1353
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:22:33 +0000

    mess pure sore nearly weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
