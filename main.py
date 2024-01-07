0539467588ae6df33443471d8efbd63df020dd76
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:13:30 +0000

    variety plan crisis person doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
