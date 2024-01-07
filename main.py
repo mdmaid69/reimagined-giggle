e54cc477ff9026563959e8ec9a1ded288238287e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:22:54 +0000

    frightened plate black advice female

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
