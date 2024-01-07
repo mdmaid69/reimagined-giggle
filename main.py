8f465c74f22cb34a9e70cd8291aa38d381e08051
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:55 +0000

    probably stare empty female birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
