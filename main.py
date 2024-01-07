5c41867a3c15c9de04126443601a1e00806a5c02
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:11:16 +0000

    sport lookback(on) pot fit nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+print(os.getcwd())
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
