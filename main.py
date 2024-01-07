f8a8366f279cd036e288f83c2cfe11de86db3866
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:47 +0000

    pass compete hair direction right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
