786b07542ef79bc601847d4fcbd1f4cdbeba607d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:10:31 +0000

    smoke compete attempt chance way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
