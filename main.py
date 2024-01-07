3bc00f6e7105a448ea704b85c6dee4b5aa84cedc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:00:36 +0000

    translate roll right discuss trial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
