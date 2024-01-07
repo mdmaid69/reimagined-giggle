1b985af09fd9dd58c246eb4636a6e3b15bd27ddf
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:32 +0000

    centimetre nice label so splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_int(array):
+        return int(array[0])
