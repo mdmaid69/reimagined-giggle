ddee054c252e9a22b239d9994105b836523b882f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:04:03 +0000

    lookaround 179bite give convinced cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_length(array):
+        return len(array)
