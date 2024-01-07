1aed747bf27c93262f3d7e4a058186de7688c41b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:34 +0000

    healthy correct rise size breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
