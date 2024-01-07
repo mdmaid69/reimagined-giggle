3e4d208183dd03867736433ab7a1a9a8c40d3a9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:51:15 +0000

    reaction rob temperature roll settle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
