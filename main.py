3fb0b9f9ec91bf2ef26569e298673de3179b73d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:57 +0000

    chat meanwhile storm odd discover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+print(sys.version)
