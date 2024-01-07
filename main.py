36b7842783f8ddfa61fe0d38fc7e0b87d3f052c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:29:38 +0000

    trip calmly food party hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
