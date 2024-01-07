e431bf2297b6ceb3a530e81325ef99aeb8fce469
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:49:53 +0000

    light regret tread touch people

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
