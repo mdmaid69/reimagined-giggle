85066eac21dba7a9258a23c5f06a7dfd64fb090a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:18 +0000

    man badly march expect choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def subtract_numbers(x, y):
+        return x - y
