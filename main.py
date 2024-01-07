9aa09e036e28638f464ea43a27078233fde1fc18
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:52:09 +0000

    stupid shock secret hurt song

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
