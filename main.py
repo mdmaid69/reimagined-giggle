0484368c27c6a81d0154a41466aa7b5d790fedfa
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:50 +0000

    flow difficult deliberate table fly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+print("Hello, world!")
