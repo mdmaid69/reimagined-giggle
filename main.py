89ec651ab8a992b5db33a734d4d4d71e307c4025
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:51:28 +0000

    needle television worth everybody luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import collections
+def create_user_list():
+        return collections.UserList()
