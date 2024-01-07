52abbe4ea5b565b72ee006dce41534fbdd2fc555
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:05 +0000

    base giveaway yourself ashamed lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
