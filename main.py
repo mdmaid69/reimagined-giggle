1610ffed6bfc8401afc02945e914eb923a5760c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:08:35 +0000

    conduct port unlucky character waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sys
-print(sys.version)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
