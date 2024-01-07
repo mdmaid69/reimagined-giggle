4ffab929fe45d405c4532958cc4505ee09473f19
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:51:57 +0000

    life comeback start care list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+for i in range(5):
+        print(i)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
