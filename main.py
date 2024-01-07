f356de1e09390dfdacfa2ed210f9071d9d221ec3
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:47 +0000

    good control workupto middle sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
