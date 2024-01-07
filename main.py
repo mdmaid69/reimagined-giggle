b42df8f10dbe2964f1ee8832d9bd477e99fb0652
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:55:58 +0000

    communicate feed raise hope rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
