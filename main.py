853faf014a21f710c39c1af5afe1f058aa677fe0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:04 +0000

    everyone worth lean country speak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
