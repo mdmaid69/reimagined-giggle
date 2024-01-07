9f21ac230e4f3363eb36be75141fee42d3cbd738
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:18:36 +0000

    as sorry software calmly proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
