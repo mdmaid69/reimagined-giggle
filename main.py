591da9a60d6723ba87d34029de0284dcdc52c41a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:24:02 +0000

    please respect belong goup separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
