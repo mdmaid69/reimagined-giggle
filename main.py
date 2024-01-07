ace7063967b28900e1e9deb2db54419d35e70ca2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:52:09 +0000

    regular fit oil noise less

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
