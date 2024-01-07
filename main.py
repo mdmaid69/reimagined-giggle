1ca9ae95be96b805e2edde2d44ce505cbff5e228
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:53 +0000

    present replace warn march this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
