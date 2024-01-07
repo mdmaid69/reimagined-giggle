9498b16f188c0555ccdd48cb9d6dac5ee8c3a70e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:57:41 +0000

    cope warm cheek try drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
