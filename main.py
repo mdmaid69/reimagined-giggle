036b009c92f3c6c4c1dbdedfdb95972036e5af76
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:22 +0000

    mistake cough post body pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
