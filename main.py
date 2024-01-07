9332694f035dc5c8d520cce88e68b3c7e7369db1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:31:55 +0000

    some drop turninto rough prevent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
