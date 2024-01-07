9507ebb91f40d1715771e1de1579e78e6cb49b9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:21 +0000

    dry worry door concentration speed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
