ed6fd3632863c8b73555e076abef26828f0122d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:57 +0000

    might fear price home chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_density(mass, volume):
+        return mass / volume
