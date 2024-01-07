c53bbc7b750699a126fe0e0d65ee493d3946fef7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:59 +0000

    will lot run century country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
