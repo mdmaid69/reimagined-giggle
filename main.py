6c838280f623c730e3e2b2c9299afaa4e768f989
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:32 +0000

    instant gather within Itself scream

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def multiply_numbers(x, y):
-        return x * y
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
