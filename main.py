5fd67a54695c4b01ef9e259fcaeedc240e39872a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:15 +0000

    congratulations ask no discuss remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
