52e8ca92b7dddfef7bf77e36fa7399f788f04ccd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:40 +0000

    complaint giveback book further air

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
