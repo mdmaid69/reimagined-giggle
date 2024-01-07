8aa8b73666e3f0d7745f9d1c14b3b1e75ddff42f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:43 +0000

    approval godown eye care stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def clear_array(array):
+        array *= 0
