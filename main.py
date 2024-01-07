b2409033ee4840f1fcdd7c8def84e67efcc2238f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:31 +0000

    turninto slight ourselves clearly pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
