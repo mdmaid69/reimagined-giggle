b2efae80776270e91afd3bb0af844e60867b84e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:19 +0000

    new look goout slow smile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_set(array):
+        return set(array)
+  def add_numbers(x, y):
+        return x + y
