47f704b45b082d551170266b433871317a5ca1a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:58:24 +0000

    hardly pump neat nowhere music

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-print(time.time())
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
