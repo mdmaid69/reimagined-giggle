f35c5e574e259f1859a83d7c7695ea948955e92e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:35:46 +0000

    sort tomorrow computer lesson Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
