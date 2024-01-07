bfe43b75c0d43170d1d827209b49ae0f10576c85
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:26 +0000

    them official either breakthrough giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
