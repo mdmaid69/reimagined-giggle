c261001680a155aad221f8adea96b316f2a3c94e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:41 +0000

    concerned care Cc create game

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import time
+print(time.time())
