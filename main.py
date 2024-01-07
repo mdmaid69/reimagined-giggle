a16d45a1914fbc9dc9d69c4c606353009517fe7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:13:19 +0000

    patience exciting own satisfied popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
