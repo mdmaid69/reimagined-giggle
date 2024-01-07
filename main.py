cda869dec89e2eb0b737c87a7157efbe9808ee0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:03 +0000

    contain what opportunity yet pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
