8b06633efb2c324daed7e8df539ab4ee07f1fe80
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:48 +0000

    guard fold love ear female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
