584d49536f936d65cf468e96722564a4123c83ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:59 +0000

    expression left miss well land

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
