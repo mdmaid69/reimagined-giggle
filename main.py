f13861176676f723c006608ccf288db4c1ad4ef8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:47 +0000

    food crazy environment process height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def square_number(x):
+        return x**2
+import array
+def get_array_length(array):
+        return len(array)
