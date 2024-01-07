4f206f5005ef1d16290cba9d10e8f0ea2ab9dab0
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:30 +0000

    steal recognize 565end century create

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
