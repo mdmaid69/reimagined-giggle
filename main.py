d4044e360b28da47d7a4955daab372dae5b3185f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:57:16 +0000

    before bit hole decision female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
