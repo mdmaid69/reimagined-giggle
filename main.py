4bd3a4cbfdfd2686de9ec694786a5cd48b1cf033
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:18 +0000

    brown regular send first foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_set(array):
+        return set(array)
