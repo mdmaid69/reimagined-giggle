78b18cf9ef8f18bf4d0ec592df766906702e199b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:24 +0000

    reply keepaway unkind undo pattern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
