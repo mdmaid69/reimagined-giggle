bf7fe8976a960e4357971100b0b8ee2e2c018b1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:45:54 +0000

    steer bored command live discussion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def append_to_array(array, item):
-        array.append(item)
+print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
