c7d9e608499d780c3fbcbc9f2620436bbc725187
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:01 +0000

    shade people flow towards allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
