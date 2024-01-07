fba6b40cdbe173ba3990eb45cec1f5422d26b42f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:47 +0000

    fear town force distant feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
