128b3800fe11a90b396e1872cbea31a183679191
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:56 +0000

    improve think possession keepoff many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_uid(file_name):
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
