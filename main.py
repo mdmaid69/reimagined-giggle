95f685069faabd8218300c421ab51bb92c9bcb48
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:19:47 +0000

    along fail straight temper able

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_palindrome(s):
+        return s == s[::-1]
