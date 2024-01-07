f5814767aea83eebf4528b9fdda08653d58293f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:06 +0000

    somewhere near keep cinema convince

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
