f6f4d2c07d810a4c57732e00964752d15aa26ec0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:25 +0000

    justice travel blame callback scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
