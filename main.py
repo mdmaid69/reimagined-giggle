8684dd37edf622c5916fd9986472709c31e6c4d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:53:38 +0000

    should interfere little claim factory

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-def is_palindrome(s):
-        return s == s[::-1]
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
