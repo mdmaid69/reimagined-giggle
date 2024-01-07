7914e23e5fbc700db3d7854d2b9b0cecc97b21de
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:15 +0000

    best settle frighten own keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
