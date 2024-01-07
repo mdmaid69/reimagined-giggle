a1b68a1ca34a91212183c5e107617b081cf2226a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:35 +0000

    difference improvement piece yours ahead

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+x = 10
+y = 20
+print("Sum:", x + y)
