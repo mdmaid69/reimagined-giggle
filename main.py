cfceef64b6b41e77d8850f5b50446b57db6a4b57
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:57:37 +0000

    Ll lonely angry culture trousers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-print("Hello, world!")
+import platform
+def get_python_version():
+        return platform.python_version()
+def add_numbers(a, b):
+        return a + b
