71d309a8b06132b8b0fd435b72ef8eeb36469d6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:20 +0000

    goback(to) dress work call tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_list():
+        return collections.UserList()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
