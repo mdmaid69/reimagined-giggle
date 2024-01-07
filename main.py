e97cf4f60878a26cbf21b4d6ae8af5628c10c82f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:24:36 +0000

    own read fire tune late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
