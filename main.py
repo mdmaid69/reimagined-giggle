eedd09dd5ef3aec31ce525802c399967364a4781
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:59:50 +0000

    may danger delicate here quarter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
