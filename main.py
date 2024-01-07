ec694e0e6cd0252cd9280b495479376d77f4a9a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:19:21 +0000

    recovery dream insist apologizeapologise exam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
