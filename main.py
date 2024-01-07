11d23a798c6f25a78b759b34cdb5a1c32b1e4137
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:42 +0000

    mine bag air they hit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
