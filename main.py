e4e3fb4b0af288d76ef46f4e338b099cef820752
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:07 +0000

    leave major basically how drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
