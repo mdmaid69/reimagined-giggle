859e63fcd126287599eb907d437d6c5464c63c3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:42 +0000

    day in spare moral thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
