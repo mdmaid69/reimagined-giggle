c5395ca4df5096f206927a056f2afaf7adf5874c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:29:58 +0000

    greeting occasional length cover their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
