8076d534fdfcea070fa56bff10af2e525f645d88
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:45 +0000

    physical possession serve reasonable react

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
