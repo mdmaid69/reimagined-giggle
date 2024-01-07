bc6660d7707c50be831ba8e1f94d9b93cd4c9d60
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:05:28 +0000

    sleep declare message smell supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
