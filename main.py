19009379de4b116cf09c05e16757d70537295c56
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:25 +0000

    everybody face song bang stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_odd(n):
-        return n % 2 != 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
