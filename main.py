eb2646ddd5a0cfd2ae0d272ae4a7ead972be36ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:16:36 +0000

    pupil relief practise nigga explanation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
+import csv
+def read_csv_file(filename):
         with open(filename, "r") as f:
-        return json.load(f)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
