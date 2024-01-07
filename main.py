3dae3662c3bdb70662e9eb7cca237986d34e9ea1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:25 +0000

    fast zero point place work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
