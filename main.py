559580ba44b4fb7b3d1862a58fc56a7afad458e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:49 +0000

    full drop noisy most start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
