a4b5d1918564b1ecc5afef9f3fa7c94f2062b515
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:36 +0000

    pipe this store congratulate pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
