8716aa96f36d3b75af5481f237f89ec7da64a75e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:02 +0000

    unfortunate take nice bank entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
