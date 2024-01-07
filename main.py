5e6f314e9bd61a0f44fcf94357525aacdd7ddee6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:00 +0000

    remain exercise OKorokay about rent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
