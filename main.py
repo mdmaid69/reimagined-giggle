8b9e00d0e1979ace21cb4ea2496c18da5313fa1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:24 +0000

    react land slip autumn makeinto

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def find_min(numbers):
+        return min(numbers)
+for i in range(10): print(i)
