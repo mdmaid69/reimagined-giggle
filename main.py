58bcd1aa14750d2d0df199a13173ba1315682da6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:59:58 +0000

    picture flow except squeeze drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
