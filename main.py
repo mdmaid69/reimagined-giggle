aa9de7135f07f40b58adb9de186825bff9bb4577
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:37:04 +0000

    normal otherwise then answer perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
