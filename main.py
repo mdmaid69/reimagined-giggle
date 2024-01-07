5458c859fe5aead1b9e2ecbf027710c8ca119457
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:39 +0000

    job sign effect shop destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_acceleration(speed, time):
+        return speed / time
