1f9b715110b5e95e29ac5c0b2ddb300e88983bd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:21 +0000

    heat rule none family body

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-print(sum(range(10)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
