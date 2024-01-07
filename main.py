c362493d3523414d3b024361f547bcef2e6a113a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:11 +0000

    safety runaway production friendly awake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
