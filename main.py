4585089b6e3d82850fe8fb4817df5d3f5b18d6fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:55 +0000

    transport science other take joke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def reverse_list(lst):
+        return lst[::-1]
