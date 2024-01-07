813aa0e710ff79262d2f39c277d560ab88b94dd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:51:46 +0000

    admit gift inside picture fence

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
