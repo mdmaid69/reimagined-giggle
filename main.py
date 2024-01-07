6231049dc840696947e53804335d4ecf791c0572
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:29 +0000

    set table television average good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_power(work, time):
-        return work / time
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
