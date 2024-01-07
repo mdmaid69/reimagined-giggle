5f4e19c163d6ae912684384fd42832407dd2477f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:01 +0000

    search emphasize load rain actually

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
