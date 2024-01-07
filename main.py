966c2cb77018e446a7f4c0a7b855913ed762631f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:19:10 +0000

    scare board alive powerful except

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
