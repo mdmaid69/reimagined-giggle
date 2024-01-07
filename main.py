05f835b37ea095d491c88e38bbae3cbe12899825
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:19:04 +0000

    blow inform pressure charge involve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_length(array):
-        return len(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
