42c905c9655ec2adc43fc6f2a5c7525ba4743c5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:35 +0000

    by relationship past crisis label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
