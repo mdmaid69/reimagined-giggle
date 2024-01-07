38e5565cfe38951fee572ee31f53d9b6e6db6d1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:18:24 +0000

    at birthday worth roof frightened

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def print_python_version():
+        return sys.version
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
