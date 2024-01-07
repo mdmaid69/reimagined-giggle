b6147025929a151800b047085a0587714d9c7624
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:54:53 +0000

    help pipe unhappy fierce noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
