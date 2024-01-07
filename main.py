4fdc3ad791800f7764cd6546b02d10d72255bdb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:23:46 +0000

    rarely ball food shake bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
