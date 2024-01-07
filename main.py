88223c32a0b718938d71de19a976bfb491e37c01
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:49 +0000

    sail slice onto turndown excuse

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-x = 10
-y = 20
-print("Sum:", x + y)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
