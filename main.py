143c203b6d43017e9a5b638b1298c950cdab1513
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:19 +0000

    teach settle Nn join hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+text = "Hello, world!"
+print("Words:", len(text.split()))
