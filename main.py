d1ef5790e4dd1d8e712f6a3e9f04ec2b11d6d62a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:41:36 +0000

    today regularly wish back stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
