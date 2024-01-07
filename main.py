c84ebd2c5db76a932cdcfac364d8acc81d5a84ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:02:17 +0000

    tip future abroad respect mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
