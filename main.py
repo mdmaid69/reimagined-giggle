348ab39814e1eaa9eca344a150b3948e41783bc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:29 +0000

    near fresh constantly glad high

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
