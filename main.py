d234fabe40e9d9e602d79b51afc8a05899cb3789
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:19:00 +0000

    brush aim deliberate out post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
