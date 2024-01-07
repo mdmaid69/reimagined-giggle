44d7a992c3fe03a5fc79b6720359bcae0dd625ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:10 +0000

    cheek holdon nor fall university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
