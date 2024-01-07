f407a4eb2e756a2ca3c10ea74118905604473aed
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:37 +0000

    always share distant imagine skin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
