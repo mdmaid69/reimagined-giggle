6001afd1a1d2a98799c7d1f171ed1b7b70f88d9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:24 +0000

    comeoff put produce Rr law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
