c2e07e969b1e762796aa79801b25c26fe3f6491f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:54:00 +0000

    out near precise circumstance turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
