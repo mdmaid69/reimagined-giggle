ad3fd4ad7363f0f7f43a22418ed2289ffe5573cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:29:14 +0000

    position bottom across anyone common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
