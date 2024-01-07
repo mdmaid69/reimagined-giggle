9699464f83840022afd320f171986a5b20391a42
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:12:56 +0000

    its lift sure comeover table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
