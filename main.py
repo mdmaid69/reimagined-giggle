2ecc36b8b9533f29a064d7819076dda17d86f6fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:48:12 +0000

    organization kill lose forever unhappy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_current_working_directory():
+        return os.getcwd()
