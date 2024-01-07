ad2b0b3fc95d8f259a04048b274f6dc379ac38ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:41 +0000

    challenge lie order mix tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
