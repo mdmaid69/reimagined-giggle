cfd76f6051005d9ef282ceaf928da58888ea5dae
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:40:17 +0000

    quit hole usually call production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
