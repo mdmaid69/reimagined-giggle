4d67f4eb508f873547188cee0a59d1f8f1afc3ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:15 +0000

    nurse make age appearance along

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
