2903309482ec0ace72ca9a3ba228cc359bd44fa1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:16 +0000

    load chance rush church accidental

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def multiply_numbers(x, y):
+        return x * y
+  def square_number(x):
+        return x**2
