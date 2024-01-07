1b09c18f36abaa05951c0d697ea0c0f2c26d5270
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:10:31 +0000

    miss hollow walk collect joke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
