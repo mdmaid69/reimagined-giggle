d93c88090d7d11773e07a4dfe6aeeda3b446709b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:16:34 +0000

    responsible soft something afraid support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
