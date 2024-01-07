edd90bb72f6f117ec30e3aa2bd1176b954f71bde
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:20 +0000

    yesterday flow match once stare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
