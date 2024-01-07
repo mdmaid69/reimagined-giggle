e7367547551ac930d5f5d7a63fb0f3fb411a4afe
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:28:50 +0000

    grade new keepaway confuse problem

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import time
+print(time.time())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
