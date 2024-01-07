2add40ed0f169d764ea7d5c3b756f7f5478f4760
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:08:06 +0000

    whenever whom deaf steep middle

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print([x**2 for x in range(10)])
-import os
-print(os.getcwd())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
