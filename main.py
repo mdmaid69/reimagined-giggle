7bf38a6d6012db4fb13099ab02237160644099c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:40:31 +0000

    him concerning whistle crisis sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def convert_to_octal(n):
+        return oct(n)
