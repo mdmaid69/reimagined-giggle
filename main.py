5856b1dd086c3897c381e49ba1d884aa63a4cdae
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:01:12 +0000

    sick professional power bite within

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
