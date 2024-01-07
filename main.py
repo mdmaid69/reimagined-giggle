adb13e19e027bbee5a3cab6b22906c413ec58565
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:18 +0000

    concern excuse care split gas

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_octal(n):
+        return oct(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
