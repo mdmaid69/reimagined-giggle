ec3a6d7b6e3bf8986b03448b5ab3ba2a145c6781
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:07 +0000

    burst round post left art

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def sort_list(lst):
-        return sorted(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
