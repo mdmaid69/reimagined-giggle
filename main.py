4938bcb8acdb7e77d81f0d39a48ba9e5b17568eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:07 +0000

    variety risk train extraordinary experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(lst):
-        return max(lst)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
