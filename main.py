109d9c37d947e9ad13e485a1e944b80a33c2e497
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:59 +0000

    weekend stiff feel combination pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_array_as_str(array):
+        return str(array)
+def reverse_string(s):
+        return s[::-1]
