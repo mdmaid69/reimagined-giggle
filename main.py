6f888af56c490dc218da4521fccc698d71e044cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:52:09 +0000

    group guess shelter sound I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
