fd383dacbdfcacfc83d87d2a30b62eb5a14ac903
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:28:07 +0000

    probably skill lookafter scare mistake

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
