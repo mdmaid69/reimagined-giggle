681c22fe3996ea1e6ab57c0c0a53a14fbe99a8f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:17 +0000

    hide Ee probably face dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_int(array):
+        return int(array[0])
