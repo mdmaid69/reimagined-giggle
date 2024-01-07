00d7f027c46aee35f74293a590ec6c4de62bd90c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:41 +0000

    rarely switch callout snow Zz

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
