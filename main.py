3247ffe1a50ad193388f648259cd6916e2ef7129
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:37 +0000

    important loud sink price OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
