e34cd9d839d3e5e343c230ea941481b8b6f9495f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:46 +0000

    occur sensible her unpleasant straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
