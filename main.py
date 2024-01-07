97a107ba6e9638357d1a071b1c6f44ed892133cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:54 +0000

    condition term tax attention exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
