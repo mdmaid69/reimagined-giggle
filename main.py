375912067c1eb0357b7898a107f5bd9eede3ab24
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:28:11 +0000

    water complicated frequent evidence conduct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
