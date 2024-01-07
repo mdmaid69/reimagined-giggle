40cb5926bf15b4a3b7a029c72485c97e3e90ad04
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:24 +0000

    nowhere injury wish transport hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
