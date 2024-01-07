b275d259bc83ccda04c2075706a50ecb67ac6433
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:58 +0000

    turnback round learn now approve

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
