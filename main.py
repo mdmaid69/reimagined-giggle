3bac726befb301158ea3f22ae020443aead07efd
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:53:15 +0000

    means level comment related corner

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
