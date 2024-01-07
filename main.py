bd1767e7f4a7c8854ff7f4e591825774ffc5fb75
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:40:59 +0000

    wind convenient field dangerous walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
