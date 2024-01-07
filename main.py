aa8b29b56affe2c00d78b1465efa9258472fcd70
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:30 +0000

    children search catch duty stare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
