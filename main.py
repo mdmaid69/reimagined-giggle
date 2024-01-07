ca3f5449aebb21530a3acbdd8240519360eaf470
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:35:44 +0000

    risk offence keepup help fold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
