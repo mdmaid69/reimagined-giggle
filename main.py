64c025471c14443f019117d179c2ec7b7eeab626
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:40 +0000

    transparent full capital bar race

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
