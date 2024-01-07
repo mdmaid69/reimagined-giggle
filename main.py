aaac6b07bb3fb792790f9d942d308896d235d69a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:47 +0000

    do both provide common cope

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
