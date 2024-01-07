f302f99203447555d0115cfbe29591c8d3e68b9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:57 +0000

    warm suddenly lesson mistake shout

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
