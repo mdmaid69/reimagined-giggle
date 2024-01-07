38e887c2d74a840ef588f09d7d29ae3ee638454b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:17 +0000

    funny prefer rarely respect speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
