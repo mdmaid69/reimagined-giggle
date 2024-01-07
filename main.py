e5b1b51d4a68d1f2cacb28bbc907505647a2d3bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:10:49 +0000

    unlucky towards congratulate afternoon achievement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
