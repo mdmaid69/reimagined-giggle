6c3b1aca5f02733ebf019131d17b63e38c214907
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:24:22 +0000

    guard family suddenly quantity praise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
