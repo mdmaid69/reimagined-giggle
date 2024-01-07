ee6452e7100b3cbc0d84866cd9e81dcc50e43c9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:29 +0000

    work always serious burn at

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
