c9a7f4d19d42df8873b4cc2d80b778a6fd7ba37b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:13:00 +0000

    believe miss tell keepout cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
