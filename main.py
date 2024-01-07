ee1d1adacddd37857ccfe08ce0cf79dc7a9976de
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:28 +0000

    quarter borrow time miss hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+print(random.randint(0, 100))
