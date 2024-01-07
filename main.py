799fd9ef9ca70431d6fdc9bc3f8e8481687b487e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:42 +0000

    blame drink persuade picture traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
