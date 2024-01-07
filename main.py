f36c735ee400987592020988623e77c52754cfa7
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:05 +0000

    hospital dull underneath cheek boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
