32544f3fa85323fd3350dea4183d153e2921040a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:13 +0000

    peace difficult violent dinner exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
