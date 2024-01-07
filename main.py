e38326c01b7b13a69cdf93e88ef82e598118980a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:49 +0000

    receive schedule inform their open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_length(array):
+        return len(array)
