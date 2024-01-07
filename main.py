1565fd43dc90966a41403c20562aa466a2d2b691
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:37 +0000

    regional divide nearly speak bill

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
