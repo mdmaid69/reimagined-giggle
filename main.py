2a47466c4e75ed698367d42a095f8ea97cce3013
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:20 +0000

    be case afterwards precise modern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
