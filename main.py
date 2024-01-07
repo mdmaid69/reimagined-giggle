e9565f6cc2cdb60f52c677fbf1cc0d967a70df91
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:39:39 +0000

    food understand contain welcome cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
