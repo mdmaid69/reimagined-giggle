dfae36ab478c3a596611b747fb049fa29cdf83de
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:14 +0000

    smooth mind food stone tense

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-print(sys.version)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
