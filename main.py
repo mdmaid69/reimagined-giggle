0d450d0388df6c6ccc15986c680f4d9be8e80a21
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:30:20 +0000

    pinch dinner single guide sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
