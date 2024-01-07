75c5dcb98b46e55d6475118cef46217b7e1ee5b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:34:35 +0000

    giveback tool wait depend comedown

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
