41efc2fab9fd492fc0d25b232d6e21aaf83c47f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:59:09 +0000

    development valuable as modern much

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+print(math.pi)
