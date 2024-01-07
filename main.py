c88adc1e67fbacd13f80a409d19d43374fa9778b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:16 +0000

    whisper below split march intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
