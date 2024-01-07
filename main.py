85d05fc5ba46afb615683594bc2a4c602938eddf
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:07 +0000

    respect crazy act share downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def is_even(n):
+        return n % 2 == 0
