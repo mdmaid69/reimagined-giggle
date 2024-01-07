4f814ce87784d75d87ae832c0e3e198bfd507153
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:42:48 +0000

    godown combination laugh maximum loan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_max(numbers):
+        return max(numbers)
