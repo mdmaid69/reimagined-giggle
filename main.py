eaab3213a9bed028ee1598521a9f9b3059999d66
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:10:20 +0000

    breakinto breakout advanced object thinkthrough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
