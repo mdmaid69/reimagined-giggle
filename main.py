7fe15bff4571d43c784ab6187e9d05d2de7fbb74
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:25:44 +0000

    price steer regional stay advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
