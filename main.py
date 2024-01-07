4bc709f50edb269c0db6dbddb094826be9b294de
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:41:00 +0000

    once tune finally skill centimetre

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
