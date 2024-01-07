67c3c71c93f891927a3c0aa4886576f343b08e6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:31:20 +0000

    bury fire attract from 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_index(array, item):
+        return array.index(item)
