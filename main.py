2a51568fa61988a4ccdbe3bc5bdeb4d79d79c6f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:27 +0000

    workon smart actual 179bite practise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
