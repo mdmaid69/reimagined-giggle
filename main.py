d4af0f7d5db39c8e62049156175517d4a1be683c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:51 +0000

    perform alone work value nice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_tangent(x):
+        return math.tan(x)
