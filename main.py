c2b12c922defeeda40dacda2489c3f8b9435db0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:15 +0000

    herself much music deep damp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def remove_duplicates(lst):
+        return list(set(lst))
