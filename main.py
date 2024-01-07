f83c7cd3d31397bbcd0c4ffe4f56a00443b45414
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:52 +0000

    forgive money your before exit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
