71d7cf2f688932d35d37a578af18d47dc0464c99
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:47:19 +0000

    fortunately free both confidence flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
