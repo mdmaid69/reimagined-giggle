a37457c40564297b4eadc6c586358ce1dc8d21b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:02 +0000

    cost die shadow record strength

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def add_numbers(a, b):
+        return a + b
