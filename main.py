86fd4d556780a013b1fd2fdd24bf1ec797828ced
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:24 +0000

    nonsense tomorrow success cum crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
