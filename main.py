6c9ee7af0bff72518da3f390ea8f53d90e0f6711
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:32 +0000

    cover hot break weather cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
