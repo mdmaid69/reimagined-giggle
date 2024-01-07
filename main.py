6d24e20543cdaca44ce169b1c94513a521b77a06
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:41:02 +0000

    wire pain intelligent feeling scared

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
