4e2cfc48c1f261d9f68da0cf6a47691c0996b2ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:31 +0000

    try passenger us except fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
