7cfb45a6e5a579eb2ed3502a2b810afebd196c1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:09:58 +0000

    concentrate valuable great brief clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
