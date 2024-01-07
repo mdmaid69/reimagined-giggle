672079a75a4b9a655a9adee668f6fc1ab6048ef0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:57 +0000

    this check reply afraid trust

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
