d4c9820a2c8c8d44e726b3bca981f9ed08bb5a23
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:05 +0000

    begin occasional warn criticism total

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
