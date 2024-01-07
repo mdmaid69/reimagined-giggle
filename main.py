0eabdce493f9ba6803b259df712b2db7d972b9fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:58:35 +0000

    sad recognize cold until cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def square_number(x):
-        return x**2
+  def convert_to_hex(n):
+        return hex(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
