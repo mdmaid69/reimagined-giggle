cef4e8d351d12d23a0d253f94ce7df16bfa6854d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:39 +0000

    absolutely side race voice then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
