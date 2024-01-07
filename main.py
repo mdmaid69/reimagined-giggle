5314ae75172b2f529784ca1d5f8666a60ffae225
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:51:24 +0000

    strike strike uniform knock judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
