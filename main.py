c5119324327c906da44e53d11753d8240f1d2fe6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:02 +0000

    concentration take far centre by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def remove_duplicates(lst):
+        return list(set(lst))
