9edca11430ac2af5127939d724d4867b98a954a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:37 +0000

    pick wet definite consist exact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+def count_words(sentence):
+        return len(sentence.split())
