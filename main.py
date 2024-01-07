f40a392c5cf852747fd66a047af58c639198987b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:23:36 +0000

    accordingto value slide ought classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
