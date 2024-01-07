fda0538f7bf70867976fd927c66cd961a59a7d4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:14:44 +0000

    new supply coffee slide wall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_circle(r):
+        return 3.14 * r**2
