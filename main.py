f6a222c0258dc879b1b5be1150b50d82bd6e371f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:54:45 +0000

    exchange be lie correction entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
