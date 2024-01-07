a80c381691bd58b1baeb128313e332163e88e59e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:22:50 +0000

    scrape job tired lonely scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
