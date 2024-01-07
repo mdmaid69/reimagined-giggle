9369c0fb3b54c7741287ce75c79986a250fb49c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:16:55 +0000

    improvement crowded improvement concentration sting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
