ac7943f7679f2cbcdcf8bd3010e940d318b980ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:47 +0000

    peaceful clean straight hard turndown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
