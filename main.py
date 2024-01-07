6bde6199c25214e66a5bc38e730feae5217af1ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:02:16 +0000

    tragedy help hold bend ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
