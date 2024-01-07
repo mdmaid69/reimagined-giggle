9af4300eb5b76e0bd45461e7d3d12d040a3021b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:23 +0000

    plenty exam dig miss effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
