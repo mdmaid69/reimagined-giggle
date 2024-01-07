112cd69e2f35a698dec4482f351106da5a3eede3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:09 +0000

    own thin object middle condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
