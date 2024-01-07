adf02008ff513886ad79b0f85acaadc858b575ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:00:58 +0000

    tired police become regret callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from collections import Counter
+print(Counter("hello world"))
