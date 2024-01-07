2b8af15316549aa64a22c2a6190fb50ae576eeb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:53 +0000

    married rate closely grateful story

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
