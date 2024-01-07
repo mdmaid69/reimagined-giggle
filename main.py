35bcf71cc50e147e0c1985734d096dee8ed3b85c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:39 +0000

    draw communication little emergency wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
