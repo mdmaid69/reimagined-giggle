d92bb781249e7e5e09a3c6624695e3243b8a660d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:18:14 +0000

    everything callback row track style

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
