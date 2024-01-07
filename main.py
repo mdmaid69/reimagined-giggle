8f508197f494800d65f50fd4e848551c0277bc82
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:50 +0000

    underneath main collection deaf violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
