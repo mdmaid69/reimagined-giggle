ce31ff110c0ab6b822c2e40c785c03f275f0bffa
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:37:30 +0000

    pollution century kind it winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
