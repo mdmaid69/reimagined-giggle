0206f0c9771f5084e40371e292afc3650dc5ea64
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:57 +0000

    smooth defend about production black

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-from collections import Counter
-print(Counter("hello world"))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
