8f50f5d057507097df0892104635abd45ca59383
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:19:46 +0000

    remember recover bother now meal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
