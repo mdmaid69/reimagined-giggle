eaf8384df4d4134ba1136af695f224f3eb104d80
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:27 +0000

    runinto deliberately arrangement possible size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def is_palindrome(s):
+        return s == s[::-1]
