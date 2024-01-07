aee921217f67e6cb26e8c82acfbc8dad0cfe589a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:57 +0000

    just naturally pupil certain town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def clear_array(array):
-        array *= 0
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def get_string_from_array(array):
+        return array.tobytes()
