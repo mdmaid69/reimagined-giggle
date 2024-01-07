c69f56ebd130272d461388e35a6b4b657c800f0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:49 +0000

    receive ahead Ii really result

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
