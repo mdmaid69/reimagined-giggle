2f0b62ed0bfdf05f0329cd6f180e0e6e7aa8c129
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:05:16 +0000

    Nn software wish brother free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
