d76400c7ebf6082de5ab457aa38faba6eeb0e90f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:22 +0000

    fashion moral steam fail behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
