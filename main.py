2f2ee42787f107ce0a6a608d4041cea50c6fe1f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:59 +0000

    where sweep this swallow only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
