17954678f896aa6495691f95d0a80bf2d88bd885
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:00:01 +0000

    breakdown record whoever achieve make

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+text = "Hello, world!"
+print("Characters:", len(text))
