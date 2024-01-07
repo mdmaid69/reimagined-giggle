c46fa7b5a2e6597da70d9baf5c2332d937fa6992
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:44 +0000

    even mix church very small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
