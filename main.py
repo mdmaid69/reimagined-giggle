9312052740ea83ac7dee51e6e54d6f08be07cf08
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:19:06 +0000

    talkinto explanation little string me

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
