d37642d859ca312e9812fa1c98dec4f06a2d8580
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:38:05 +0000

    comein train band along choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
