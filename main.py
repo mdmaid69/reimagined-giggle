2b2811273cd2e3e98e5efa8319624bf5be668947
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:49 +0000

    foreign zero close miss light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_as_set(array):
-        return set(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
