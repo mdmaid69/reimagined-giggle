61bc457d09f7b1620dadcd90a03ba516413cee71
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:21:09 +0000

    rubbish price equal that comfort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_current_directory():
+        return os.getcwd()
