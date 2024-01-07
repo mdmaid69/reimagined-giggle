7e69fa7b2a604fa56c8ed7ec1ff34678bca9f3c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:23:16 +0000

    history wash recently scrape crisis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
