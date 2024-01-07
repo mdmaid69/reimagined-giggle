7af3cf1accfd98a96814bc1574e0d83bb5db88f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:03 +0000

    contribute appeal me hold search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
