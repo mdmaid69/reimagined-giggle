aaee9541013db7f4e2be50291e52965d67fc9cf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:11:52 +0000

    572enter dinner walk best undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
+text = "Hello, world!"
+print("Characters:", len(text))
