2ea46da73dc9b5d5e9a60a062cb25a9b49258588
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:13:07 +0000

    safe anywayanyhow basis rent beginning

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
