914b85cd31b791f54758c6eaf59c55453c27342c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:23:49 +0000

    to borrow wrap ask everyone

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
