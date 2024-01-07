0522a17238b9b2dcd01e0a647b7c572e16247050
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:31:08 +0000

    welcome warm able plan call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
