79a66afd7baec5c1f586a57124f9d980a2a5e823
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:43:14 +0000

    classroom poor flood jump unhappy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
