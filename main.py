81057e9c665263d3a26ad030704a36ca32f152c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:48 +0000

    card breakfast closely supply so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
