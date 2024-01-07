c68ad58e410ffb5bb111e667fba08e89c1bf2c97
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:37:39 +0000

    answer wind next force persuade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
