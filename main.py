88a9dd6752eb91349f889b5b97d54becbfa88ee1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:00:40 +0000

    lonely behaviour rent accommodation press

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
