cf2eca1ab0e0687a3e633a13a880e061efc24f7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:22 +0000

    somebody around exist block afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
