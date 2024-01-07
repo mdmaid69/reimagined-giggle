5074710a0859eda3313cdb9407dfbfc2e2b7e3fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:36 +0000

    succeed pleasure Zz arrival aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
