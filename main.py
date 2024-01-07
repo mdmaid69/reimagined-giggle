df1a5be404546099daca8b838d78f8f94ab7b425
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:47:48 +0000

    through by over furniture promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
 def count_elements(iterable):
         return collections.Counter(iterable)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
