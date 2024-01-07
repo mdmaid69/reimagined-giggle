6a4232957650814bb4ab5eedbe773feb79f7b59a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:41 +0000

    hurt happy play minimum comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
