16f50c474a9832f9d431927d616f733e845be2e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:53 +0000

    long just hold remove feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
