5f948b7cba7badd881f4d9d91e337708622906bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:10:12 +0000

    nowhere business start light slip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import time
-def get_current_time():
-        return time.time()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+print(math.pi)
