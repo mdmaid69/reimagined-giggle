11df06c81f67c5e3a6150cacda6a29a742285b1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:32 +0000

    treat answer free decision mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
