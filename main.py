1dda4b0495c1debec151f3263ef747fc2028d7e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:14 +0000

    marry news exit health through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def convert_to_binary(n):
+        return bin(n)
