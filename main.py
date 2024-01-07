4144e4b0bd224b556659156e7698f85fdcf2d3b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:34 +0000

    anywhere hollow favourite whole family

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
+def calculate_area_rectangle(l, w):
+        return l * w
