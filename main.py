4554391f2906ca47f1354022fb0f0b3418a0a4f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:02:29 +0000

    separate warning harm embarrassed tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
