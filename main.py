dda550b071e3109e60b855d91828f082db269d80
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:02:22 +0000

    best watch side children smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
