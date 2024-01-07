e28f68fdb8be950e4538c2535361ba22dcd9e8f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:42 +0000

    table interview surprised holdup spoil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
