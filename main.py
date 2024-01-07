609c7dca38433ef544053a94ef1aaf0e027e1f8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:33:37 +0000

    maintain rescue if comeup help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
