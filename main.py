de7482ecaf27cefba155964c3695538ffcd1f38b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:17 +0000

    cry bridge private or welcome

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def square_number(x):
+        return x**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
