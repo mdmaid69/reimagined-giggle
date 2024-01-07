4230cd0af976c8cf583105889143e3902dc22cd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:39:26 +0000

    because purpose share excuse boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_list(array):
+        return list(array)
