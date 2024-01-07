d485b9ba73da0ba3e59a5111479ce4cef19be62a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:46 +0000

    future check alive various light

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
