0753894ba2fc935537dab7495665a507e24658f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:46 +0000

    measure towards bye possibility order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
