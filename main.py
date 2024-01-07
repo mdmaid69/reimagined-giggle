6ae7a3b435ec4b9166c254d19321014cf486a4a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:33:46 +0000

    hello word meal quantity opinion

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def find_max(lst):
+        return max(lst)
