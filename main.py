a60230f73b3c596b6fb3861cd06c6612538f433c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:40:34 +0000

    argue turnback some market risk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_max(numbers):
+        return max(numbers)
