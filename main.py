40dd373277f1ac94eb18edeb20fbb47dcf6484da
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:29:50 +0000

    her split generally cover possibly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
