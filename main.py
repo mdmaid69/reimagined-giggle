759bdf45e8e0d010511b10250f02f808112875d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:55 +0000

    collection sale wise reason stare

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
