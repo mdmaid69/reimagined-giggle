17759d89e39d4abd8fe1e6d647baaadfd9fca9d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:17:35 +0000

    accordingto through quick lunch exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
