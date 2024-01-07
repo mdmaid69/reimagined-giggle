ec3dece929c55b2cb23b26771abc06acde2eb82b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:02:57 +0000

    movement debt rich speech enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+print(os.getcwd())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
