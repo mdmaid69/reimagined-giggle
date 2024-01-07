14024b3448ca07d5b3d39d5463e1e225e66d5a8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:38:22 +0000

    doubt check warn justice drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
