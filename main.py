249103eeefbae09d28f0a0525e265040a3f25809
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:16 +0000

    active callback steep sorry take

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
