33f3101d78f586eeab871c6ebb0e0345b8297fb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:00 +0000

    fence rare method limit section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
