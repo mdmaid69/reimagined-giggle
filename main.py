9f94e159c41caa7b1d190ff76dfb9784d913640a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:02 +0000

    score but close too spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import sys
-print(sys.version)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+print(os.getcwd())
