901eddcf7b50542cbd93d4fe00e6e50388bf31c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:50 +0000

    similar behaviour immediate comment effort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-name = "Python"
-print("Hello,", name)
+def find_min(lst):
+        return min(lst)
+def count_elements(lst):
+        return len(lst)
