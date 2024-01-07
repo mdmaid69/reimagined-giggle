acce6b2179e6ad685d6f9dcb33d5265f8005bc02
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:07 +0000

    defeat shock wing twist loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
