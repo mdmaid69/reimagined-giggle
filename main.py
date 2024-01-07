b15788c89f0fba6c38170a82ba4b6e71f7f69017
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:49:47 +0000

    occasion young turnover pinch laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
