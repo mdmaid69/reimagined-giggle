34003bdfc981e83ec7961255067282e0c7606ff2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:40:27 +0000

    fail dish about force command

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
