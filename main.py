bc81fd8f5dae2b1e34cf08b7e5f90e7d028bb89c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:32 +0000

    summer vote she steep succeed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_current_directory():
-        return os.getcwd()
+from collections import Counter
+print(Counter("hello world"))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
