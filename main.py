9bfd85f71cc077407e374b008aad645c75c777ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:52:17 +0000

    minority often everyone share affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
