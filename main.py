b1376f0ca0b6839a8b271138f79f0269eae62af2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:28 +0000

    design who complete must bridge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
