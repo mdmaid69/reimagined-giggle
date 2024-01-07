6d13741c3ba4d3536bea34db15383a74710bb814
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:28:19 +0000

    justice ability mark slide Rr

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_unique_words(sentence):
+        return set(sentence.split())
