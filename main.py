0d5e2b14fdcec4f9c83c886e28a973c890a6bd39
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:03:17 +0000

    rather cut failure free unhappy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
