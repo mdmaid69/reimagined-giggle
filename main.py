fe8b37d3fbbf45f52db8fb8f1aca2020d6522c96
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:24 +0000

    special rush holiday ball illness

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
