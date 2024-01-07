6183a382e09cfe0008b2c5f233aca21982d1cac2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:52 +0000

    scale sort such comeover situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
