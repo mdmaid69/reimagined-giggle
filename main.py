bdc1c77a1afd0c6ccccf5f92e862170792490545
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:25 +0000

    yesterday light turnon habit raise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
