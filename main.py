1745e9cf19181ea0f80b06014e00398418fc066a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:46:06 +0000

    period breakout confused port seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import time
-def get_current_time():
-        return time.ctime()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
