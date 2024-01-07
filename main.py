982f689953e363aa3bb15d05013c0c9a695fd5dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:40:34 +0000

    minor temper vote reserve whoever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
