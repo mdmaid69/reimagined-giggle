c7003fd6885856c2df6ea2616f720a60819962ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:37:10 +0000

    close successful this home extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
