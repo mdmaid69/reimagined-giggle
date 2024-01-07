c706904f1b8fb63e11a54f23ec2a144fc998c84b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:32 +0000

    peace discover than original care

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def reverse_list(lst):
-        return lst[::-1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_speed(distance, time):
+        return distance / time
