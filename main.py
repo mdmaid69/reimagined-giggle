5705bb7fc5c2b1c9b41accf3f651b8eca35d60a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:15:37 +0000

    figure recent anywhere wide fashion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_item(array, i):
-        return array[i]
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
