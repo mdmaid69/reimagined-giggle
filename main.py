ef3fc538f1825e5fbc04c8274c45d9df70207a8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:13 +0000

    achieve capital tonight autumn all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def add_numbers(x, y):
+        return x + y
