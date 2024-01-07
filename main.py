153400b64f275ac032a14e803e76a8d1c3599a5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:08 +0000

    convinced juice imagine giveback straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
