03582206f0f2cc8392d0ec9a9fa7dc737220edfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:14:55 +0000

    collection callout bet through sign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
