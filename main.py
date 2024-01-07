4713382717c6b3b770b638be12fb40b433ff8624
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:26:50 +0000

    transport still scared by ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_area_rectangle(l, w):
-        return l * w
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
