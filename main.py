f28b6ed430b5f119c52ce9377b462e1643375578
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:58 +0000

    habit break control block kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
