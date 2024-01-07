26dd893384380d749a4012ff9416403164f17f88
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:57 +0000

    achieve judge mine whether get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
