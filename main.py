983781742c95e19367e97982dc8cd07e758d907a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:56:22 +0000

    wrong sink operation alive blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def square_number(x):
+        return x**2
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_index(array, item):
+        return array.index(item)
