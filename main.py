d0efcab01a2ddd31c6fecd757e38e9fe0958b660
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:04 +0000

    dear friend draw quality live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_power(work, time):
+        return work / time
