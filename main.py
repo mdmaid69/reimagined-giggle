dc3679a9927fca3d8b0ad77eb5adf17caca40621
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:15:37 +0000

    half crowded myself properly for

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+print(sum(range(10)))
+import platform
+def get_python_version():
+        return platform.python_version()
