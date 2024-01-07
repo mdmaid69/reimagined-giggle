1e18f5b4be0eaa2c0fd82fc4514b057b5ecfc529
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:03 +0000

    refer communicate shrink treat poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
