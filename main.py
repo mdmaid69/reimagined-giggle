24e1f90e3e502761238c2aa3d99c1842760de2d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:24 +0000

    step hole opinion fortunately coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def add_numbers(x, y):
-        return x + y
+def square_number(x):
+        return x**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
