264074b823b8c48c3f7cd30cf250eff10e86e1d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:02:44 +0000

    smile serious conclusion risk benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
