98f0d5a224de4a9996fda5fe8c7e326c136a5bbb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:43:33 +0000

    harm protest Vv dull formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
