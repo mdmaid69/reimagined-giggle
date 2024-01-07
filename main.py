371ea536a70ff870e279ba9960c0bd4e7598cd5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:02:34 +0000

    calloff turnover steer thanks appeal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
