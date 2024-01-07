d0190b8e05bf556d28863b58192bfa7a3173417e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:03:34 +0000

    shut beneath present sweet brush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
