acf141becf93ed5a450cd8970ceb6a9bb0f10ef0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:03:38 +0000

    pretend spill actual hurt foot

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def square_number(x):
+        return x**2
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
