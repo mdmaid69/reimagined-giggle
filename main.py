0a0c949d71103a7865b795875b586edb420f9572
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:01 +0000

    deserve himself right complaint catch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_acceleration(speed, time):
-        return speed / time
+  def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
