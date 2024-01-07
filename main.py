ab7b0c5ee07480742d90cf5acf2d072afc966ff0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:40:45 +0000

    set steady seem Yy runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
