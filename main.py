c997764ab0951a44e2c8ed446e2ee7e0f79f4ef0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:58 +0000

    back takeover nonsense plan tend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def square_number(x):
+        return x**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
