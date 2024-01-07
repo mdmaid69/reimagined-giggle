b18f9e15c24665547f12723369ba73c627a0eb9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:55 +0000

    say over case more mother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_os_info():
+        return platform.uname()
