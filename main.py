4d5dd7019336c3ab9583d8138d348f246ee72d80
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:45:15 +0000

    tidy tax start unless talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
