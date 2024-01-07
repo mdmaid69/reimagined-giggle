6bb5ee9e79c4d460936e27c869bf343fefb549f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:22 +0000

    572enter pretend towards bank stay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
