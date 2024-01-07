b3b090e9e604a4242d9bd46e6c2a50de498f49a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:40:37 +0000

    why sweep contact pot dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
