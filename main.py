4c5ba09f3438f114be0de4b7a722d553a8abb906
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:44:25 +0000

    split address terrible beautiful freedom

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
