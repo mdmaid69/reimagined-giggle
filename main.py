2766d59816081a25683642d0cf5f79698cb33fb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:25 +0000

    government what unless whether whoever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
