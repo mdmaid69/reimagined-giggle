309a3d56750fa0f8cdf222b16bc2f2185d9205dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:39 +0000

    share bath study worry dance

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
