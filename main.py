c5a54bd793ab6ff0c975d1d0a7598325b7b28063
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:35:14 +0000

    violent natural after Dd year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
