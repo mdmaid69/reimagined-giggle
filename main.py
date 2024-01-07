8de14216a9e11c0015a8c95e237d6c58defa0bc5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:46 +0000

    if regret independent 566end level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-print(datetime.datetime.now())
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
