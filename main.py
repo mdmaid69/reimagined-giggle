053a184a71b606ef3add228d3695243a1795ce21
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:36:25 +0000

    proper place depend concern sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
