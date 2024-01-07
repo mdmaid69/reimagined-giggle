24785fe7e49584c16fb6b69bf241e0f97aa00ecf
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:33 +0000

    new Dd tour trip brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
