d97f2ea79ff16910cca4ff8967e1b8a8b109ee6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:41:09 +0000

    freeze crisis interfere powerful question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
