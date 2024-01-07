e3489f1b957faebc404feac11ccb77800762c23b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:19:40 +0000

    town above demonstrate level disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
