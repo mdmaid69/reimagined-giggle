4a658f4f6b6ca2f55e93d562d6fcf3312b136b52
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:13 +0000

    since transparent crash mark storm

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
 import math
 def calculate_least_common_multiple(a, b):
         return abs(a*b) // math.gcd(a, b)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
