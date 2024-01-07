c1b1f4f1dde45ec62434640e22e907765fd78b31
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:53:50 +0000

    rid silent wild tidy responsible

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
