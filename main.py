a9834a2c17e85c70a07f0eccad2d28da0315a774
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:17:10 +0000

    bear use trip later frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
