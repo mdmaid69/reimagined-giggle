a85bede52e4c16caaef53147e54572c595ef1dcc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:19:17 +0000

    getout smell beyond payment set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def is_even(n):
-        return n % 2 == 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
