e20ca0a0bce273d3dd19e4506a718ce95b79c2e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:19 +0000

    Dd for exercise geton ourselves

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
