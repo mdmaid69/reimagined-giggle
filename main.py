60074eef380b124db6ea76c96299719d27fe9df5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:22 +0000

    else sensitive question water uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
