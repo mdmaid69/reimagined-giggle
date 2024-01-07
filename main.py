f149d9375d3a39d33e4538f9614e46c538b38daf
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:19:54 +0000

    hang thing offend exercise Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
