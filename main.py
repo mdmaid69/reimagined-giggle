6b8756426e738ff0085ca62818eb803ded18aeea
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:52 +0000

    connection dry last patient used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
