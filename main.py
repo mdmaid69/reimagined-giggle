b960d99e932949de9272606d539a9b64b017d601
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:20 +0000

    advantage thanks culture moon history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
