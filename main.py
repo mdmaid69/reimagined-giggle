1de9669d84aad005efe6d3ffaf297d709dc775fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:50:18 +0000

    tall offence vote Zz if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
