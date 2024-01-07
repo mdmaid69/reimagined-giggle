17391868eabfd2e71f0d810419485aba924ea87b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:41:43 +0000

    lie position much crisis here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
