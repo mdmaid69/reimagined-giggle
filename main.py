4361ed56ba0eb3ae46aa95feb1270874459ccd18
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:01:05 +0000

    birth hit inquire welcome sound

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import sys
-print(sys.version)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
