55053e35328618bee4e9d98d6da154a746eb0cc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:27 +0000

    congratulations excellent mad dry kill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
