67f2e765ee27c6f4cf55204680b2d01467a83672
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:40:10 +0000

    let ahead correction tongue crowd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
