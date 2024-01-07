0616e958ae4fa7f67df566bc3fd96b0a2e479f2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:48:43 +0000

    somehow skill drink unlikely runinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
