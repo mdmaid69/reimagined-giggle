65b3ea4ce48970c1cbf78ccde79dd7ef6bad6588
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:03 +0000

    still spin talk wave quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  def cube_number(x):
-        return x**3
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import random
+print(random.randint(0, 100))
