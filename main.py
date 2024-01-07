18412c9a7fd1a69e03ae1707ef0bba988eea3bb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:39:14 +0000

    yesterday which wise warm passenger

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
