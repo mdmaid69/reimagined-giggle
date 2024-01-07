5c517c62eaed437a5682016fbe00ec572d10ce0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:15:28 +0000

    chat truth mysterious luck noisy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def square_number(x):
+        return x**2
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
