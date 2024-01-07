139f30bfe9f2efc49827606a2551339a6e191a6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:29:23 +0000

    special disturbing little long decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
