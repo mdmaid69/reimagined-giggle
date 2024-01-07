c2d2cbb50525b0c16e1043dcd7768fbc32d446e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:41:30 +0000

    write unusual comeover line dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
