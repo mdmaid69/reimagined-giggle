a2f772e46c1ec41c5b9a5c7ad61c80cbc5aedb24
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:10:43 +0000

    exercise for land minimum autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
