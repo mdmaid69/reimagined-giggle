f14ac41b637a0e55096c661e9528fec4437f5a12
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:22 +0000

    meanwhile church government Mm insist

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
