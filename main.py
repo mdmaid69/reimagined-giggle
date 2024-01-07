370d8f088ecb68b650fb15c41fff29a80cd60f68
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:10:40 +0000

    instrument limit somewhere roof mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pressure(force, area):
+        return force / area
