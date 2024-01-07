4b5a9e11c7f1866d7f0413c73a84f30d269dab83
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:03 +0000

    mixture never thing of university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
