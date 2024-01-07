eba5c3ec33965bd4f4a48a731c1f1690d722b19a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:11:59 +0000

    ahead beat major direct tongue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
