ef3d7ab2c0f9fa12b618c6995286b8479eb04c06
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:36:51 +0000

    before climb later purpose on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
