4af85bfac603cf5e5b879f2019d11a7239eb6c07
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:42:01 +0000

    Oo keep surprising giveback after

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
