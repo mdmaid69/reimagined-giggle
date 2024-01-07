3ba87c26558e4840351184ad492432798fbb7294
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:47:28 +0000

    plate standard open above about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
