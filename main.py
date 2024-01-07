d98f6d05ae2663e53a97f11ab6b33623601b3180
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:55 +0000

    emphasis busy memory tense occur

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
