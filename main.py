91ffb10fbed7c3b007b27599da2f4e914690734b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:51:16 +0000

    science level gentle ought any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
