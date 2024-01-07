4602bf9704a8081407cd55e9954189f5d42bd7b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:04:13 +0000

    declaration already stop deliberate importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
