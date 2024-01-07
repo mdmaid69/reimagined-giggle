52ba09f444ac10327062c70f3d32b99e750edf99
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:31 +0000

    stop past often any taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
