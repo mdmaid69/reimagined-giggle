5eda74ba1ee55cc208616932107e89bf7433872a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:54:42 +0000

    male answer attitude holdup convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
