f7426e4908c3c327a4a27161e2bfc8fa85c4bb79
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:03 +0000

    building ourselves exchange congratulate would

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
