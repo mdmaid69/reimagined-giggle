69fecb42fcb0f99787b3e82ff8d68e932252043b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:55 +0000

    problem warn passenger high late

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
