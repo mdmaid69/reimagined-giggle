4b9d041e1faa60339e5f68c6548bc7fcd3e47d44
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:02 +0000

    glass anywayanyhow original frightened unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
