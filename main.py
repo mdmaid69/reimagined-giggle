d0142938765a718a64d84070c1e0f896228bad4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:00 +0000

    bread draw introduction soil almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
