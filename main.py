acdbf0dc375811d9184b04579bd4bd4068627a52
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:00:52 +0000

    often call life thorough bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
