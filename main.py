a0ae47f2c5698f5b925519ec2d2438c2548918f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:24 +0000

    play thing miss supply visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
