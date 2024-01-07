70f5af02324b0b9653080b435c1d31c60219cf52
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:21:39 +0000

    mood regularly perfect mine smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
