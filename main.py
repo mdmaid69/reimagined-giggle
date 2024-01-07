16c264648caea80ad773519697bb295ba35216cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:20:37 +0000

    relax work shoulder sort actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
