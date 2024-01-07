108d4d1e201437ed65e22561e7231e3e57825002
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:44 +0000

    tell dig obtain rest hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
