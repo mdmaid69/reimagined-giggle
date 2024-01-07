6c8e32b5ca156e92c3adafb811b33199908c1fc8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:45 +0000

    main size closely trap bringdown

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
