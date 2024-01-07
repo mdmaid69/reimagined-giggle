53605be92973e86bd58c375febfeccf7bb887d5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:12 +0000

    thinkover necessary address proper talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
