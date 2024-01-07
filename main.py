4317e106985a1adaef01d1804630abf6c5b20bcb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:39 +0000

    neat language ground none Ll

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_max(numbers):
+        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
