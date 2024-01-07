ba571bcc1a2d73374b084b7dddcdba3387a60d14
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:22:48 +0000

    woman stupid block famous murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
