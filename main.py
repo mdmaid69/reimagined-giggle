824b20aac1c9d559cc67b2c82186debda3f2f126
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:04:33 +0000

    action prepare region dig respect

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
