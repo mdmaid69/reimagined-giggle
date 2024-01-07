0bfc0969031d100654f9fe52692de9b826387638
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:06:56 +0000

    valuable crowded watch true embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
