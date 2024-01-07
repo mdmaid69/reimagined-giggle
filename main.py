127000782a9532cdcf02fe994b89e990a40812a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:04 +0000

    roll mixture release roll refuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
