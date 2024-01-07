2b7f6dad13d2ebe130b0908513ba0b4fb2f1c808
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:11 +0000

    several house wait undo return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
