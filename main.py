4702888d22dbe50a03760b5b1314ee21206f6cb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:43:58 +0000

    fix yet kind tip nowhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_user_string():
+        return collections.UserString()
