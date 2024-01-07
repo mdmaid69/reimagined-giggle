4c7bbdeeda7f7663cffa646b8c4267cbf7b583eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:06:28 +0000

    year teach chin order build

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def count_characters(sentence):
+        return len(sentence)
