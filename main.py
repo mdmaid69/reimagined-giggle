2864e49e3f7d978b3f6a6f7b8ab214a5c3abeae9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:56 +0000

    suppose matter odd pleased Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
