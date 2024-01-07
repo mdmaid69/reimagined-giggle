2352df5829d11e4336750fbddce0642f81dc7046
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:35 +0000

    we appeal letter skill pour

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
