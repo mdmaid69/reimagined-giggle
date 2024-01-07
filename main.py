e4265317e2efb63954c78b326cbb24936a478f20
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:01 +0000

    characteristic flavour tune purpose punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
