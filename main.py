f7900c6cc67cb05ffeff2e5debb59542fb9aa300
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:29:01 +0000

    juice public star build cheat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import array
-def get_array_as_list(array):
-        return list(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
