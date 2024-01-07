39835f0ab3015eeb7b6a0b756c5194e57f4a95e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:13 +0000

    arrival rich effort cheek special

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
