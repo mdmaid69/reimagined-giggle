3af992ce26089a46f2e42ee6bede768d1cbeb126
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:18 +0000

    call bar clean breakdown control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
