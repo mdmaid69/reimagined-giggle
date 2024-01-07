d3e221e64c309625d0a48f9de26934cb209a047c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:54:51 +0000

    thinkover last limit paper now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
