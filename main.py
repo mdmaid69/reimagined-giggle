cdde68c39721b5ef02e1b11ff4a5225c32013e4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:18:45 +0000

    grade now hungry hard expression

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
