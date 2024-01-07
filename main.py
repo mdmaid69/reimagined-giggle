9db0c255a9b14745c476b80ae1c2392f6aac8826
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:31 +0000

    mean nonsense surprised Hh note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
