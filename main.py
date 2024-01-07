af5e8a8def98433300062bf7814359ebf7986f4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:34 +0000

    stir heat careful limit base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
