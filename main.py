8e171a666aa5bbede6adf1c044637919bb4417cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:49:52 +0000

    post speak better bill workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
