f24b88204d8ec4bad6bc541c00cbd5acd017356f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:21:58 +0000

    wise name need say to

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-import time
-def get_time_since_epoch():
-        return time.time()
+  def subtract_numbers(x, y):
+        return x - y
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
