699dfabc4af718fcb33eeb078c1a1370b414a1d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:45 +0000

    plant near on could fight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
