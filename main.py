513ffa8f3c0eae89c7beda6509374f316eadd973
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:48 +0000

    involve platform class light already

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def is_palindrome(s):
+        return s == s[::-1]
