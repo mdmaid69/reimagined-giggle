f0a0a9b3c84ae63db50692df7a95e312a047e2a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:05:36 +0000

    failure professional turnover free tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
