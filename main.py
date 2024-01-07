62982738b2d5f86e7ab203f5c65b1a4057a9f4d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:05 +0000

    keepup roll want middle spin

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
