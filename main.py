6c3a23ef547998fe2b5c3698ecec9eb91d2beae2
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:56:51 +0000

    warm hurt correction natural second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import datetime
-def get_today_date():
-        return datetime.date.today()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def remove_duplicates(lst):
+        return list(set(lst))
