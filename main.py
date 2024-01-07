4b24c4eb57cba1db2ef286a84888d2a9b78bc97e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:07:39 +0000

    direction congratulate loud persuade duty

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
