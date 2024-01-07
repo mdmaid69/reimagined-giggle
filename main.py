50b834c4708b957c242883cb1417268a6a625373
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:24 +0000

    admit direct ear every plan

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
