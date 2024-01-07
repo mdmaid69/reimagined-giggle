896926285a9da957599e3b16275ad9e0c98bfe70
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:21 +0000

    increase breakthrough exaggerate limit ticket

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
