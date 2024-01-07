ade024c90eca78f7b9fddb5b2ac521c8e89c3901
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:18 +0000

    personal cap naturally violent where

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
