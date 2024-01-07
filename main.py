bed1e27d9016ae52182f3e7099bae4e383a00a44
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:49 +0000

    square breakinto wherever wing early

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
