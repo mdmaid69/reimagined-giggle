d276dc7cd05c22ebe35395bda2cf06dcea5717d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:46:22 +0000

    greeting limit road follow watch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def get_current_time():
+        return time.time()
