49124bdd690873557613014dcd5d3b5d4f13eb8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:49 +0000

    dull certain regularly regular black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
