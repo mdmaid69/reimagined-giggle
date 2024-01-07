f387804b6d3fb9c8b6389cadb541f0e3e319c735
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:09:22 +0000

    I powerful goback(to) opposite formal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
