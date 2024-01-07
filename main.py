85892b3b232572d560e05a66d9bcd5b9d2914cd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:37 +0000

    exchange conversation order them giveaway

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import sys
+def exit_program():
+        sys.exit()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
