d05f393cd537de7f8e06ffc02549377dd04653e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:19 +0000

    among pretend effect programme test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
