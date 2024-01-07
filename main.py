5e5572f9649b0372805ae2a842d963c55549532b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:18 +0000

    mark next allow sound turn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
