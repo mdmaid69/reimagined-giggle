323e58b125141cca3e183fb67b81b4b124fb0dc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:56 +0000

    hair closed impatient store guard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
