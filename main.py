a874f4808321d32bdea44ce4894cfe623b401b17
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:00:13 +0000

    pretty opposite pay party entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+print(math.pi)
