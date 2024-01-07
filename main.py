0e093d32f40cd096a8e9429a5cb3341e6b285de8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:39 +0000

    wheel friendly over Bb family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
