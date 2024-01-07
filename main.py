751be78ecfeb1fb970e003b2a6945db6c1e2360d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:33:38 +0000

    town official waste guide least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
