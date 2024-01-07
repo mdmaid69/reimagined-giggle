55cd69573add9f299a2b2771d209bef412580c35
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:15 +0000

    flavour want weak prepare about

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
