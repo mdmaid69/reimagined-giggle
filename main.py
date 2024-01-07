ff5da383bb84f3f2d28b891500f5802d4ff6b43c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:07 +0000

    this negative alive loose patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def subtract_numbers(x, y):
+        return x - y
