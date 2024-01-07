f09d3683201921db7229fd456d73395ba47c7e80
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:40 +0000

    age birth quick admit deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
