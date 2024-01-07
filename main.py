65027fb8794484f88246da7323b7510e75b553bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:47 +0000

    far doubt regularly choose far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
