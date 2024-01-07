f0361b54aff0879d994d647604ee654eaded952a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:08 +0000

    left bin blind 571enough male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
