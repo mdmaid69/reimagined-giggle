121d939538ba2301bc4980b2ceee331b8917432f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:42:46 +0000

    personal exploration benefit available way

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+print("Hello, world!")
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
