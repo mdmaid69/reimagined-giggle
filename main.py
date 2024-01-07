bbe08b89cd7e225434026dec816c8215d0d4afb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:51 +0000

    answer interview fasten Hh kill

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
