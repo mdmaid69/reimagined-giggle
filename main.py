97f3b73ec3a678ad093ba75d818273111186cda3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:39 +0000

    conclusion workout calmly act crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_error_function(x):
+        return math.erf(x)
