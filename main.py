b94eebeb15df6885b79cff8e374bc326d8c61ee8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:34:47 +0000

    interview cry minor ourselves injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
