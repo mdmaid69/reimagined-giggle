6efc58142a6dc5de17a342d37b339358d293430f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:49 +0000

    progress frequent translate summer fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
