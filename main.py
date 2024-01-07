ba1a6ff10173021c78c4517bc404a6836198ebcd
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:33 +0000

    promise relative so rise reduction

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def cube_number(x):
-        return x**3
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+for i in range(10): print(i)
