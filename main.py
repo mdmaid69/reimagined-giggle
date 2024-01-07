eb4097a1249d60ca56569a7fd34c09b60f7bf49f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:15:39 +0000

    taste centimetre unfortunate matter natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
