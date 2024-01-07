bb47d7b5b09ff86194546c02e77b4754263a119f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:58 +0000

    material second argue number round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-print(math.pi)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
