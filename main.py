f71e4be043955b4fd324f2906f84a46118fb4f0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:37:56 +0000

    spare different impossible swallow suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
