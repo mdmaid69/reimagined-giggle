73008e7daf4b37530c0aedcf1204f9fdec781631
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:47:12 +0000

    ball fetch better responsible small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
