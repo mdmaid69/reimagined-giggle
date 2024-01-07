3fbfdfdcbeb0401a22eadfe1730bf62daf4946f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:16:52 +0000

    basic regard opposite slice word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
