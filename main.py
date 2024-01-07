cf3a5ac8f8cbf29c7f86bd7fe7e7caa90bd428ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:59:48 +0000

    race lower this record full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
