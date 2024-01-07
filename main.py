e4c9bd4411f1b6b2c3717b82c2f027d437a70359
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:13:20 +0000

    Uu well little escape damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
