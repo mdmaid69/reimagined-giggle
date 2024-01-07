3ceb995d58443a5b7a45db96770471cd02aef3fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:24 +0000

    simple argument stiff noise Itself

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
