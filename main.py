c4bb6003fefd877cc56d75cbadd693648ed5d19c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:04 +0000

    spread float complete range it

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_pressure(force, area):
-        return force / area
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
