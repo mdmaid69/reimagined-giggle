4c11ca370ef8af76667d07ac30849e539caced20
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:24 +0000

    guess size sort right pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
