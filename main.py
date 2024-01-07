47741076c237f37105f37d82f0e9bc19699c54ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:43:14 +0000

    different friend narrow row allow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def greet(name):
+        print(f"Hello, {name}!")
