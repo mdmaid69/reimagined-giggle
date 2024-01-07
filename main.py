25b50877fc3a156a7ea91e058acfcbaa42d2d5a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:31:00 +0000

    occasion light surprise manage pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
