99798e4c9b29f150f5ed221cb13ed086f7bdd909
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:30:52 +0000

    fire uniform violent warn slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
