5e88e5de67af90339b0c4ae125c7f643791b314e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:07:12 +0000

    at also sensitive secret settle

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_acceleration(speed, time):
+        return speed / time
+def sort_numbers(numbers):
+        return sorted(numbers)
