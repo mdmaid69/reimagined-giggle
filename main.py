c5027724719e690a7e825f2159cb97eea81d033b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:23 +0000

    score have exploration mess dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
