1e493b0306f3302f91095893b9c73adb086244ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:40 +0000

    broken steep since comment breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
