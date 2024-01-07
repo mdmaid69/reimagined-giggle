b5eee7101d0a10b28d68974429b47d832cb1c54e
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:10 +0000

    along willing comfort ground honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
