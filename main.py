32f50da97efe0461cf44d8a8dea5f4ecf5861a21
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:55:33 +0000

    apart combine start belief wherever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
