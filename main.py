2bc62466fcbab4e83329b091db79d2d37ba9b4a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:09:07 +0000

    care thirsty beautiful uniform break

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
