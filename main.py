4b3b02e6cb379c5d7be19ac0d8ceedabfe4eab64
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:31 +0000

    tidy much sore this powerful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def square_number(x):
+        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
