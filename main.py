3a5018b4b446367b7119d37816c9645076dd76ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:28:42 +0000

    design sister swim disappear sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
