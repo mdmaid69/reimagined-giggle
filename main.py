13e9ab39481bdab622489e759fcce5f64d4249a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:01 +0000

    interview keepoff wheel tidy use

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+print("Hello, world!")
+import time
+def get_time_since_epoch():
+        return time.time()
