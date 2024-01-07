5f5df4e7157a0d195665669f671701ae4cd1d662
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:43:26 +0000

    method congratulate depend experience explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_current_working_directory():
+        return os.getcwd()
+print([x**2 for x in range(10)])
