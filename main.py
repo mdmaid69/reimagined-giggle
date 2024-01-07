759d8368506f09d3fe6a390ea07a2839de838912
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:43:26 +0000

    fork lookback(on) worth discussion play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_str(array):
+        return str(array)
