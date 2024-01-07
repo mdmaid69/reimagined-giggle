52cbc02229b9bf0baa01141f9e5a914e0d1784dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:06:58 +0000

    glad start limit do confused

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+print(sum(range(10)))
