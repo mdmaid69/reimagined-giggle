c12c17537f4517990ddfcefa97c5e687bfc144b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:27:07 +0000

    stop realize open would just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
