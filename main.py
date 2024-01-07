3f630713ebbab1c515c73584b41a8fb1072f1613
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:32 +0000

    though factory winter practice actually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
