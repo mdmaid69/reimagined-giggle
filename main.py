da94e61d0d93a2edff45c67fdade15c56982325b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:06 +0000

    hear any tax coat guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
