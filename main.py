cbb7b06d1d79fbc6e873b8845871ab3ed645dba0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:52 +0000

    influence coffee interesting pick room

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-for i in range(10): print(i)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
