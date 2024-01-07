985b7486d951155f3e174d1b31417726054ee1ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:44:26 +0000

    ticket child whisper concerned Mm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
