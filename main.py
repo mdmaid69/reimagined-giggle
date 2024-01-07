fcc89dbcf63613d62f458f084bed6fd750bb2c52
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:17:29 +0000

    summer inside opportunity afford bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def roll_die():
+        return random.randint(1, 6)
