717b2df4fe4afd0529b9bd9b25340ecd96948d5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:45 +0000

    lead towards exact mood cope

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
