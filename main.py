0512ddff7c3b55d4f2260b5ac6ac24cbaf548f6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:55 +0000

    odd wealth warn substance pleased

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
