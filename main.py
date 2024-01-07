e13144a9327bab2c2ec561536ce7732edbe4add0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:50 +0000

    description pick congratulations fortunate still

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
