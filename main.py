9346380ac7ad72dbbf26f2377ba4b1955a8b42cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:19:23 +0000

    Bb turndown style skirt succeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
