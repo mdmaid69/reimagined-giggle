8aff3256f3c6c7cfdd39359d15c75f696f4e6b21
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:29 +0000

    well 565end also unless mouth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
