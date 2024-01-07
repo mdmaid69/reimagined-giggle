5c50e50294770283675a776ec61832bcf0fefc4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:09 +0000

    happy jump less mix fit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import json
-print(json.dumps({"name": "John", "age": 30}))
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
