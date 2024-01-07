ffa191e7c77935ed8ec0ec570eb7cef59deb687c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:55 +0000

    this plenty paper risk loose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_odd(n):
+        return n % 2 != 0
