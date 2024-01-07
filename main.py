f8cb292e22a7422d217fecc7db6a56b79e066888
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:08:53 +0000

    border short pupil approval content

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
