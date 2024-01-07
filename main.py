69eb17a610c0751acf0e831f22a4e31a80d516e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:04 +0000

    all powerful suit OKorokay build

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def divide_numbers(x, y):
+        return x / y
+def convert_to_binary(n):
+        return bin(n)
