b33b0ac1a2095ba5c20c0a562f4cbeaa70b52ef8
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:25:15 +0000

    behind common point charge ball

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
