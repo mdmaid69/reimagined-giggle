e6b36551fc4b69591208a3e48b9e8fb39c225447
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:28 +0000

    comfort round decide recovery visit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
