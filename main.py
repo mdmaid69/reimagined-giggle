711c1c76020ba01715af3c024155389602dab584
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:32:04 +0000

    difficulty ability improvement convinced pattern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
