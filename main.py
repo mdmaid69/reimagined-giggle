4583ba401c283eea6ff033506ac03daa87b5b351
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:16 +0000

    wall claim well permission only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import sys
+def exit_program():
+        sys.exit()
+def multiply_numbers(x, y):
+        return x * y
