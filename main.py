224659891583fb618aa992549002530cda1e8a18
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:40 +0000

    at new force crowd Ii

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
