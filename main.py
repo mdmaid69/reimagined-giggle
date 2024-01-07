1a755fafe6508ac4e0342a0fc362a093f9ac5f57
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:33:28 +0000

    prove busy nice scrape bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def multiply_numbers(x, y):
-        return x * y
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
