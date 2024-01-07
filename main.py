b81892a4793ab4ce62818f06a7d9f58841e617e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:55:14 +0000

    centimetre clear shame gift about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
