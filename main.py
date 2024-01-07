23f48addaca28b718a0d4ac41b93180a90e29496
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:32 +0000

    yourself record case second grand

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def find_max(lst):
-        return max(lst)
-print("Hello, world!")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
