6511cc97c0ae1b5f49a14b3b7f12955ae0225955
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:40:35 +0000

    else pool direct contact apparently

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_directory():
+        return os.getcwd()
