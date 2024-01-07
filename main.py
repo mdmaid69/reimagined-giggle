60387156b20c7ec06843999226f16b091784ca95
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:44:20 +0000

    laugh alive slice admit mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def square_number(x):
+        return x**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
