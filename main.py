529e8d03352702a98ab5a2cb065be8827f2d82d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:04 +0000

    sorry wing sail deal improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
