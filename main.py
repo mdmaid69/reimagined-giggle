b2576a6ea0cabe29a32fdadbfd129a34291ceec3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:49 +0000

    exciting floor state clean long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
