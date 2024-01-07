798b93f415601a36c96db627fc4134325b268196
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:57 +0000

    laugh environment private call swallow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
