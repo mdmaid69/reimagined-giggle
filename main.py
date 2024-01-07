4b0abbd1990fd7cc4eb63dfa2f711cf06ad47e37
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:20:22 +0000

    loan wave complete road favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
