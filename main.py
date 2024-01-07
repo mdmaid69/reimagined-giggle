8fa4ef9eefe590dad8b1939e78ff60c6316b7ae8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:17 +0000

    lower detail tight damp almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
