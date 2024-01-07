582194ab32af4df2a2eda0fb620061efd6925ec2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:51 +0000

    strike swing sweet getaway at

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
