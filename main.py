4131f7d62186732e22283d39c79c1c708acadbe3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:59 +0000

    over set getup little politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
