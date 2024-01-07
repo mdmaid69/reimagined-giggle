45548e8a71bafbd3df7a748a723d3e5ce9bf867c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:42:12 +0000

    protection tool indeed reason basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
