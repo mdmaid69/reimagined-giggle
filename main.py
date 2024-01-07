acf83a5c501747cb5357dbf51848b3cde2bf7407
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:50:07 +0000

    figure drive react season wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_item_count(array, item):
+        return array.count(item)
+  def count_elements(lst):
+        return len(lst)
