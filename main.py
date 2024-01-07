93893d2a8b16fb24b26b966a67de5bd0d5b1efa9
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:32 +0000

    tidy that glass normal hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def is_even(n):
-        return n % 2 == 0
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
