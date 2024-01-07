889b36d3666c8b0ee994d2ffe2bdc664c5a22e8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:06 +0000

    belief boring hair mention accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
