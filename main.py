2b3ee2f6ab07f6a30e62730bf108339f24edaa7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:44 +0000

    over near tell shelter main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
