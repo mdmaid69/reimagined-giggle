a9b03f94d12cfe6701aaebbdcc6fd89b47b33b78
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:48 +0000

    weigh scale comeout attractive stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
