2129737b60434d519131c2b1f3fac7ab7f9194fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:05:28 +0000

    sea confidence mood peaceful type

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_power(work, time):
+        return work / time
