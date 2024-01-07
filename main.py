ed267bd4d21d176ce9761cf66d78ab39b12e60b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:57:06 +0000

    compare regularly money water abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
