3f3225df89ad0aa6a968733c09af6c573de9c1a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:15:24 +0000

    reserve trick cinema female disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
