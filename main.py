0e617194fc436ab0235247736a9fe12020c5523b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:14 +0000

    surface rush continue shut delight

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
