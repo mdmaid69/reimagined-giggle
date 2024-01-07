34d554f6647b89447e868874008f55f5969c20dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:18 +0000

    immediately geton trust circumstance better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
