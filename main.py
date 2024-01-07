1b42f4198dd831d297c06e7672e8c94cc6aac5d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:24:54 +0000

    necessary mixture me promise front

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
