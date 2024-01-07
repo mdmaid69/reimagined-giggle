8dfeb55c7699c12ff97a3b66fbfc5bedeb36ea55
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:00 +0000

    whom quality prevent combine reasonable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
