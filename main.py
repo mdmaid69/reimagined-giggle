cd4d6a18c33c319140b4d36bb7c8efc2a43562e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:47:52 +0000

    shelter label both extra front

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
