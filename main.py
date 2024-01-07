e60f1a67ef30fb3626475b2eb35876724b368d8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:49:39 +0000

    in people reach set giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
