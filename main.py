639a349323fa8ec8528570ee4567086ddd457ece
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:35 +0000

    error increase tooth middle holiday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-def find_min(lst):
-        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
