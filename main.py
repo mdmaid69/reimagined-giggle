78810544e600cbea7d25f53a3493f78783effa1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:55 +0000

    related able band experience demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_stack():
+        return collections.deque()
