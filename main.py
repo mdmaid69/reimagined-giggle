e603750d6e2ca1aa5f6583456566f8adf362f875
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:50:55 +0000

    earn meeting disturb opinion government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
