08ace2dd388bd4bf47b0e558edb0f4408f325cc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:43:08 +0000

    backwards use broken loan about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
