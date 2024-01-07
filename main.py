ec051f08039e25328ed374b2a08a74bd7e314d06
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:22 +0000

    judge frame somebody typical form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
