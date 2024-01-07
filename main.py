211c2f626517f69501630b10c7fa36bb0f0c07b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:14 +0000

    why formal excuse wire run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
