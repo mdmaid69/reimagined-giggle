d9a7a8ee1fbd5c710ae57d66c7c4c369ba92dc0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:32 +0000

    produce hungry stare catch bet

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
