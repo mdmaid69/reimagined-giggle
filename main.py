27f26905ee4dd0c95e421ee3e1a6f55d6835e4e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:05:59 +0000

    comfort object guard exactly reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
