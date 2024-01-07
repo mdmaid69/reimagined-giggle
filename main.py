f8d57bd5af43b6365e6c038314f4895dc2609edc
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:28:31 +0000

    reduce to goaway brush ball

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
