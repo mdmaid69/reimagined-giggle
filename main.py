583020ca55260762617daf16a9bcabc9a7333795
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:19:34 +0000

    drink expect maintain takeaway score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_list(array):
+        return array.tolist()
