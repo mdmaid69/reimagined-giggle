2a3eaea1a3588eb41b3d31f9a677b16aa230f8bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:11 +0000

    radio excellent born quarter off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_area_rectangle(l, w):
+        return l * w
