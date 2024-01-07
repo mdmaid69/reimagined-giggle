aa4d4da6dfd8239d32505396975599f41b5f68d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:45 +0000

    train strict long 573entertainment deal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
