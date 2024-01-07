b590fc83531506299df6abee97fa44c6c47c3a19
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:07 +0000

    hardly war pause proper along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import os
+print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
