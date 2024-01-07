eda349c26ba590f2668c12cd6d99488a9f951c6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:29:41 +0000

    what spring somewhere view arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
