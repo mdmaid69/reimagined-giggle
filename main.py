433e81cc65d6f876e252addc9260d470a21a2ee4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:53 +0000

    stare reduce factory hurt involve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
