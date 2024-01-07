5ee0eec26dfca209fdd5be78b78404dd758b0a85
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:29 +0000

    day rough basically same loss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
