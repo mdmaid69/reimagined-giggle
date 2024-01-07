62a7327a544caac3fbdaee347ede1f3c98021015
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:13:10 +0000

    stamp upwards Zz neat owe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 numbers = [1, 2, 3, 4, 5]
 print("Squared:", [n**2 for n in numbers])
