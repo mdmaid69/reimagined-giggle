29495c08386db2dc8ce943b18c3e41e982100e70
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:44:18 +0000

    treat life big change preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_min(lst):
+        return min(lst)
