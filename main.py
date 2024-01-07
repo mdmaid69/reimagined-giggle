ca41166bc2ab8e75598b05926762f0f688e21dcf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:31:29 +0000

    concentrate bar geton age crash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
