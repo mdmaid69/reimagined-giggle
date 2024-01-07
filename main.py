95dabce96224e4909349aa6260a98d9becf7ec40
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:26:09 +0000

    Gg closed many cost spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
