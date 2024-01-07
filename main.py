1255729c0d15c6dec1ccaa27be61a2c9e3fd1f20
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:06 +0000

    run feel error cap relative

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
