b5336db6a67f7848e19e639ec1baa2f4db4194e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:00 +0000

    moral characteristic advice work weather

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
