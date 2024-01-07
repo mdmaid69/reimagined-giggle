cacf65c091aef6ddf12811cff33168ff1cf1b486
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:34 +0000

    characteristic cruelty gate top bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
