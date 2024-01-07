c017ebd76c2f4cccc2ba09ff6b4714ebb8aafeda
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:10 +0000

    other compare play warn Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_bytes(array):
+        return bytes(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
