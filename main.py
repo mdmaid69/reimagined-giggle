384281756678a377a7ecc1d67ea6ae2847d932d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:20:23 +0000

    interest goaway female defend experience

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def square_number(x):
+        return x**2
