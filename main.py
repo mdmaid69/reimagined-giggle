0aa7239529988577282758df0e980ce95fd09bc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:58:28 +0000

    makeinto objection duty dead enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
