98992de7cf04a90f5e63bb8b8a11bc94082c0101
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:00:04 +0000

    level miss complicated complicated sense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
