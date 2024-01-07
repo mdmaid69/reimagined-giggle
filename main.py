2cb5545357c57357f4352a7f942b4475bcab066d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:26 +0000

    pronunciation light live him ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import sys
+def print_python_version():
+        return sys.version
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
