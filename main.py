f59e0938354da6cd7d2d7970d668094fce2fc79a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:02:43 +0000

    unpleasant study tomorrow born count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def add_numbers(x, y):
-        return x + y
+def get_array_as_int(array):
+        return int(array[0])
