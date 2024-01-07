a0634ec6b11423401a67aa4fb5fe9095546070f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:54 +0000

    far goout comeback treat scare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def square_number(x):
+        return x**2
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
