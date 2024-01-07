7adbfc3def2cacc26ec7e10348a04b8fb96889e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:38:23 +0000

    direct write closely concerned turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
