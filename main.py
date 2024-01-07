353b2cd2a6f22038778916274ec53dee9ec3f5b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:38:27 +0000

    hour slice manner ask air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
