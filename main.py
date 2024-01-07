eb42470c766c78cb53e854aa0863af22f2b30294
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:23:29 +0000

    they office Yy money spin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_list(lst):
-        return sorted(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
