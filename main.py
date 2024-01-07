d0d0c266c3e7f07120a3e2d45e8d8cbbc3fd5f7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:20:24 +0000

    introduce storm course lookup mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
