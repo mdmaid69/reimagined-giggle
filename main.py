f661e017715ce43210090b6612f234617f798bd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:37 +0000

    minority mess waste thanks nurse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
