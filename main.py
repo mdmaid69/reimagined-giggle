7b5878394aadf2d3aaa0951b9d895459157f0949
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:02 +0000

    basic attitude meanwhile shout range

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
