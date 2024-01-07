49703b4444c9c5431f104001266d85f13a695e98
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:23:56 +0000

    himself present very fast problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
