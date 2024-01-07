1675db63e336f84c1f782ca3079ef5dbd651211b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:20:37 +0000

    especially similar coat priority evidence

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
