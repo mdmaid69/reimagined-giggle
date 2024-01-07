a284f58ff806879f4a7eb85ca09ce25f509b3e7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:00:43 +0000

    when regional giveout please wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
