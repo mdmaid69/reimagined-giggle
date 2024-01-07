5769aff3eac7595fcf94d981e5adfa06933e376b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:14 +0000

    geton slide comeround wipe steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
