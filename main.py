11e69079b8ceb52f7230dc1aa6db0e55ce132fb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:06:48 +0000

    forget handle pattern relief wide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
