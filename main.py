8e4ec2d71f6b2b7d2f84e45aaeabaf52f9b6c2c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:39:21 +0000

    bright ignore apparently score callup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-print(random.randint(0, 100))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
