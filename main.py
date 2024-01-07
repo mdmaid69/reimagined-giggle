9230ca1dc3602074c124eba4813ada309e0b36b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:46 +0000

    early able odd judge rescue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
