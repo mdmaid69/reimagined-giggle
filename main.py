def6f1851b606c643da34c772b46645daf2e8a9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:33:49 +0000

    breakin level attractive store drink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
