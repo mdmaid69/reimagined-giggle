9867b0522a8a6fe9eb1224100b654b6afb07bc76
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:32:44 +0000

    platform soak choose law type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def print_python_version():
+        print(sys.version)
