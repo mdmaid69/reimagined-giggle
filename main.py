2879f2fe620bbb16d03cfb44ee85a283c8c5c117
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:42:10 +0000

    trick interview emotion up past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
