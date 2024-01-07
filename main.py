136d05bbb4b9d3ce698e62ecabb1fa0352cd8704
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:18 +0000

    amount expect conduct age bring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
+  def multiply_numbers(x, y):
+        return x * y
