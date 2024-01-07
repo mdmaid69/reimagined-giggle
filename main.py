89eed9b6773623e11deb0ec049eb8f8da82bc69c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:09:07 +0000

    reserve perfect track pull size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+print(json.dumps({"name": "John", "age": 30}))
