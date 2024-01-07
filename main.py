44f833396139cd7b57aaf27a3f9f07c2c28e6498
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:08:59 +0000

    scrap typical bag several butter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
