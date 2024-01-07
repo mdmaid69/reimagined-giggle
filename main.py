9b48f0c2da70e34d4b61199e1be70bff857aa061
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:00 +0000

    male run foreign when character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
