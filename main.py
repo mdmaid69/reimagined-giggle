44c2b2e16b34703b61349505e9504d15b97752ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:19:09 +0000

    comeback crash programme fortunate relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
