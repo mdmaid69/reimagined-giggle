b0d7c8654818d7451214f91d8201fe8e4d15eb59
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:42:12 +0000

    now nor gift claim against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
