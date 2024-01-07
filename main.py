66bcd3271077a4c919cf885ab6236cedf239300d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:33:56 +0000

    relation thought far slide extremely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
