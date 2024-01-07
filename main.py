12fdc493926ff7d303d1aaeebd799d78ca7fab7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:32:50 +0000

    cry object mine equal ride

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
