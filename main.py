cfd18e539365d5415100aa6fce8f90a228768d20
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:17:49 +0000

    else contrast perhaps along argument

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
