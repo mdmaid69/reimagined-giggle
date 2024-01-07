e096d8938699b2b1e825588761f3516819db6125
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:32 +0000

    callback string display right listenfor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
