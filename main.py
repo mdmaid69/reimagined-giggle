b5a5d45772c41dbf069812879d426328fce768a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:35 +0000

    size pause band fail advanced

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def multiply_numbers(x, y):
-        return x * y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import sys
+  def get_python_version():
+        return sys.version
