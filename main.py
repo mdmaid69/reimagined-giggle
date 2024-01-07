dc96c050c9305c225208e8ee47353037d0e66b74
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:38 +0000

    food straight list straight autumn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def reverse_string(s):
+        return s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
