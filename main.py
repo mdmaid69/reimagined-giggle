7fbb8990f8f5a399dab515324355a5b1235e4bc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:17:14 +0000

    tune advanced jump world day

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
+def convert_to_octal(n):
+        return oct(n)
