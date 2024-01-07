facc0274bd2e4535c7421f8877750f52fa22ea9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:34:47 +0000

    floor punishment pleased position tall

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
