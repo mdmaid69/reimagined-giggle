ef1f397267bcd3711d407178a008c07275568a65
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:26 +0000

    community here laugh would immediate

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def convert_array_to_string(array):
+        return array.tostring()
