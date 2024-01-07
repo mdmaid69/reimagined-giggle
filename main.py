cf9acfa63a3a70d4fb025078739205ba1b7ceab7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:08 +0000

    performance neither hand transparent crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import sys
-def exit_program():
-        sys.exit()
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
