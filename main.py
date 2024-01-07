a91f329c7199a73c35101fb06aa7ec90dc82ff40
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:49:20 +0000

    conduct blame theirs fast concentrate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_sine(x):
+        return math.sin(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
