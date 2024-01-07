8a7323446aa5634ee7a9aae461070f0b0bea225e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:15 +0000

    machine help bite challenge turnover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
