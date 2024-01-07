d4cd3fad65e2aea16723bfe651fc549adcac8705
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:38:39 +0000

    hole lock lead question test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import os
+print(os.getcwd())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
