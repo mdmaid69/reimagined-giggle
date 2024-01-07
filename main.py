2585cfde1957103c252d940f8777e29308b1b925
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:14:02 +0000

    heart behind land sticky fond

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import time
-print(time.time())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
