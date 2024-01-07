0d828ac384d6cdbb79afecea84b3525901061cea
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:54:49 +0000

    steer plate regret he handle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_perpetuity(payment, rate):
+        return payment / rate
