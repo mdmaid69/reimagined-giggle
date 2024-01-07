dfbfadf67f56a86aa1fd13c51e51b039d4227cd1
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:18 +0000

    include spin sticky bringback near

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def is_odd(n):
+        return n % 2 != 0
