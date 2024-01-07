57ed023cc25d82b4c5d372479e0fcd65888a98d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:16 +0000

    generally before warning scared basis

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
