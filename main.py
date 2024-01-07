c1a13e43b63f805ebbc32dbd1b8599cf7f63a33b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:53 +0000

    and cost evidence quiet beginning

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_speed(distance, time):
+        return distance / time
