f59c16ede356622e8560228af9024dc2441f8790
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:30 +0000

    beat match nature add 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
