bfa838c34a12a03bed0ca8b55bbe782266bf230d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:47:02 +0000

    promise shall hardly undo without

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
