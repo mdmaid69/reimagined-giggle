9bab1e997f9edd1a059a62d29a0be1080962be7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:04 +0000

    interruption neat proper pain interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
