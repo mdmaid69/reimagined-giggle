f3483c6be41e2689a729deac4d289cb1f11190f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:26 +0000

    battery fly do them tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import getpass
+def get_username():
+        return getpass.getuser()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
