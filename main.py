ec2208eab6a4ccfc5096634e5833b5dd68effea8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:41 +0000

    neck successful many large sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
