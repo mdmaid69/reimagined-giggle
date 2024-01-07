cd276d9e0c29e78208042d70125afbb823f954bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:15 +0000

    yours block this surprised earth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_length(array):
+        return len(array)
+from collections import Counter
+print(Counter("hello world"))
