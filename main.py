99aafb8b63f27e15b743dfbd63a57c6c6e2a131c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:34 +0000

    discover false pause herself equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
