59eddd9038103741f1d9fd9571cb89a77c9e6c3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:18:35 +0000

    stretch street appearance affect ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_speed(distance, time):
+        return distance / time
