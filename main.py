b8312e34d394ae10ae18bafaaac059c79e2a4847
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:30:22 +0000

    sound murder balance murder tap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import re
-print(re.match("h.*o", "hello world"))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
