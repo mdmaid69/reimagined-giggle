7c1a277d76dbc58dc1764f21ee68b9a03fb2f1b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:42:53 +0000

    telephone survive deliver hot instrument

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-print(os.getcwd())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
