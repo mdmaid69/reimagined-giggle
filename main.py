6cc5925f31337a7f82b49ef030f24e3e0b74583c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:42 +0000

    hand quiet attach formal respect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
