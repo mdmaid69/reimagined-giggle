f62949bb6676a3f3d0ad404f1267d64de06499db
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:34 +0000

    place take excuse prefer uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
