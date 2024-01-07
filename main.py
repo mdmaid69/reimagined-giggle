823fabc4c05cd22a2d497e69f3c067fd7650dcea
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:33:04 +0000

    hour material shrink ability begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
