0894a6ccbf497b0bdcb7fe84e31fece475f55ef0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:29 +0000

    interruption recommend peace bend yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
