0f207570f841baa72da98b99d392cd8d5dba8b4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:27:47 +0000

    persuade car gentle 572enter problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
