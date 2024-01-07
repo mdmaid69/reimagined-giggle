696cd2890cff8967b399a61126775bce230336c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:36 +0000

    care confusing vanish round in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def sort_list(lst):
+        return sorted(lst)
