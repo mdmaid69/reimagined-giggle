7072856375608565caf7572bc5b01b3d4bc4f0bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:57 +0000

    bright skill land pocket brief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
+def count_words(sentence):
+        return len(sentence.split())
