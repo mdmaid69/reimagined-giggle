582b6f447ab07240a49ed194e26e739a7dca9520
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:15 +0000

    imagination breath apparently answer wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
