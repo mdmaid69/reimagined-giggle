4fddc9acba3a792a2062df0dc28bf0b27417575d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:18:07 +0000

    brush principle travel operate spray

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
