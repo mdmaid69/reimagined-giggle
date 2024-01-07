91360f8084a3ee1b504ad2cf31a070690f7aacf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:55:52 +0000

    rubbish soft tune shut boring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
