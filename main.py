335557daad3ddfa529d125611a29f3afbf22e273
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:31 +0000

    smell majority pure press broad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def count_elements(lst):
-        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
