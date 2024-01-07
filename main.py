27d276358c0964b3cfacd4306fe1169f857eff32
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:05 +0000

    responsible chance wake conduct fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
