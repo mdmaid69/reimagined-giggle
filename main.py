aa092f090e517da45f0ea61c07e7ccc1b4d95f9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:17:00 +0000

    post cost post brush live

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
