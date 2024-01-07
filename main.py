10695255cb8575eb81985fee40754684a4ea1865
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:54 +0000

    breath slow her straight address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+print([x**2 for x in range(10)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
