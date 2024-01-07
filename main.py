5c14ef811dda809b7f5bf86b4d58555c62aac674
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:58:51 +0000

    deal unable computer with know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
