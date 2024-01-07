36fd3456e483b93cfc6857e467ffe1332276b7c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:22 +0000

    snow bound risk breakout certainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
