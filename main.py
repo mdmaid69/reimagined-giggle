a13b2f4a7a3fe9bb0438a95de78f9deceb632b86
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:04 +0000

    content majority light expect recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
