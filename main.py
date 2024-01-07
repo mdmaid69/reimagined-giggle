d1b6eefe5c8c3c6474a27bbfc81cfa2250ca4011
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:33:17 +0000

    thorough sensible even consider before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
