77aeae5a0e800d676df4970672b39e2a216c82b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:52:49 +0000

    stare talk somehow style abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
