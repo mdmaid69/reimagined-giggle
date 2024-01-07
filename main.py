1b099af5e436690fa8c59a27b263e24015d8240f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:14:49 +0000

    gift believe runaway limit further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
