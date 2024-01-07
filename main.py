d410652a529d59ad857f060a8436c01ec27d66c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:30 +0000

    voice uniform promise answer actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import platform
+def get_python_version():
+        return platform.python_version()
