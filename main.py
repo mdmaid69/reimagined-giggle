62ee011f4a5271d8311631b9d6bdae3e6e90b312
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:58:18 +0000

    honest confident characteristic comein remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
