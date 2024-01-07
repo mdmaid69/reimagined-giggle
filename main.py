5417fa9d3b7923ca57542d6e770dc7c6898f1912
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:54:57 +0000

    tear comeout official conversation the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
