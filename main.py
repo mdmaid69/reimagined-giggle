a0e60600a1c729d20e7b5291c9c7247f0c8fdc9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:38 +0000

    noone get here should figure

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
