1b72012306fa1a3fb6e19fa4232e2338592f24b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:07 +0000

    level onto polite freedom win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
