03ef714dd63268dccd9cb286f4be3d52a41dd62c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:21 +0000

    priority needle block past mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
