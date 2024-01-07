41851740d938dcb8cb0e598411df3874b44a7bc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:29 +0000

    clever fit die like off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
