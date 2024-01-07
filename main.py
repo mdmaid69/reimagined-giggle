6e6dce18020944ebbd1e8dcf5aba1282fab138e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:15:11 +0000

    refuse available runout pull Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
