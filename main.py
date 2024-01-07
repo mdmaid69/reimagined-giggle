87ec2e808ae5cf913eb0b5d992c911fd07886c73
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:37 +0000

    raw expensive say border explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
