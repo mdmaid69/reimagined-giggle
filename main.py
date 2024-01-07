a94d163698c83a079dd7ec6a0f3de697f74022ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:01 +0000

    spite pretty runover last anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
