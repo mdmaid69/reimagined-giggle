65cd0a3325fd0a5eed7995e4d3510fa77d0159af
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:11:24 +0000

    free whenever sight measure rub

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def convert_to_hex(n):
+        return hex(n)
