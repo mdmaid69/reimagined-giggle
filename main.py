732ad9f7a6b5c1aff66065e5063b48c29fadf156
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:57 +0000

    bet report idea violence prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def convert_to_hex(n):
+        return hex(n)
