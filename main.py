7a5db3f5a203d2ab2c4a7958d4755c8b39161446
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:46 +0000

    cinema supply fold ill lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
