ec2bc70163aa42adc36bb7585341b6e6e5e807d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:39:35 +0000

    anyone doctor oil whichever fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
