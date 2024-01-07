1ed93f6f1300477bb814fc21e1119ad9e30b60b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:32 +0000

    club thankyou wait afternoon deaf

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
