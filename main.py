02227f68bedaa71845a524a13dea4c76d2d44a46
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:32 +0000

    board steam total skirt effect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_odd(n):
+        return n % 2 != 0
