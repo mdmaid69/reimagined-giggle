c7a2bf8da55d215126407517c53bb64fd5fe67ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:08 +0000

    so attitude as shout disturbing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
