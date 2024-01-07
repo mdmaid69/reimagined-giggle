eddb659c8df7e88ef31afc65a24f1c04e63dd2a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:51 +0000

    start month poison thing tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import logging
+def log_message(message):
+        logging.info(message)
