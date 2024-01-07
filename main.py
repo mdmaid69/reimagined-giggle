ecea577071e0b7df01ebd4646935bdfb256f6e4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:48:04 +0000

    question generally concentrate delicate duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
