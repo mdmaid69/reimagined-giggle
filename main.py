6c05a527246e51344fe372220bcfe8f22f085f33
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:36 +0000

    interview introduction die worth object

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
