ae07aa3da57fdbed17a420449776d90522bee8e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:48:48 +0000

    themselves attempt today Ii invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
