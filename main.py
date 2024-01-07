339c37adc5ec7ef14ddd19c5cb657df4a1f11ffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:55:30 +0000

    boil uncomfortable this fail frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def reverse_string(s):
+        return s[::-1]
