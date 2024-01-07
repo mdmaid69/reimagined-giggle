a677b545d04b6341e70b3f4f80f084261a7ec2b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:17:29 +0000

    university turn send score close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
