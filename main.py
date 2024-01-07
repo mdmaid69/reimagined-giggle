40af6cbbf5d6a03b5b41fe38ef00ce8a232d87ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:26:18 +0000

    dream male command drink getup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
