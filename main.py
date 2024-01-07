4fc90b59a66fb55cc8f3a8c7c219f45e56497aee
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:16:54 +0000

    early taste male along form

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
