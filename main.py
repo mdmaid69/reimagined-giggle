68389acf9282da55d03e14926aeddd8af35ca8de
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:34 +0000

    use present beginning yesterday back

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
