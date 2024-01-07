2f1ab156ae0c6c26f1522aad7baa05ac45d87e43
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:47:54 +0000

    throw swim Kk boil climb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
