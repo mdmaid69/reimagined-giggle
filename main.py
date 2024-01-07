8819523f6b446251b93b5b39ae3656532ccaa6d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:37:17 +0000

    spring regard opinion town appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
