c6330ff147c80e6f563f1cf3ca9a7e7cb37e6ce9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:13 +0000

    watch pool count period delicate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
