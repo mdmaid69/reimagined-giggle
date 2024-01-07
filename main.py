f2c50e6ded8d230b6b7745df39e0e4a8f34ec805
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:08 +0000

    nose high situation listenfor spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
