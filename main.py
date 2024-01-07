8639ec1c45f2931f28c614c308a1aee8b44ad70a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:16:51 +0000

    message judge fashion female greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
