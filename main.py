b2366b3447c41d625379daf62e6afb079b6c275a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:38 +0000

    second wonder raw dream base

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
