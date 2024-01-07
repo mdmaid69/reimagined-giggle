f856560ba4303636b344e78d55c4637c6fb7d59d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:19:15 +0000

    duty check flow unite power

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import random
-print(random.randint(0, 100))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
