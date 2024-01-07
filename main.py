96a5d3ba854c3284db8770c14b0b5254538b4206
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:21:53 +0000

    brush hurt sad anybody mass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def count_words(sentence):
-        return len(sentence.split())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
