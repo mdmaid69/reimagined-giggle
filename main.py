850172a23223931a57a7e6f52c02b2df3dfe1f8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:41:03 +0000

    in poor compare account achievement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
