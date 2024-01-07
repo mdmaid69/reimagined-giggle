b7ee95c2817f30e35ac82186ef713bbefde41962
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:43:51 +0000

    insist bridge refer generous check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import time
+print(time.time())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
