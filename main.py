08f5dd3368358e97bbd01986de8b349aa83e2ce8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:16 +0000

    mine include class history at

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
