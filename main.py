9f2813355bf7624abd72f779c5aa7465e5c0e2dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:02:50 +0000

    callup suppose cry 569enjoy support

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
