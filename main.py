cc4f36dacf7d5482aa31b0441bf57c025b4f00fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:50:58 +0000

    although attention land close stir

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
