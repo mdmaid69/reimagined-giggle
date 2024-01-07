4262cd0a89930295cb501b42cf7db0e0eee868d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:18:52 +0000

    instant 564encourage fact evening rank

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
