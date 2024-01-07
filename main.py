f3cf45be548c36937835076e5c2b69555ecb8f3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:39:45 +0000

    again level forever direction opposite

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
