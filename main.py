2ede4298569391dfd2b0c6a43abfbf0073f50b22
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:28:24 +0000

    roll above loan computer expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
