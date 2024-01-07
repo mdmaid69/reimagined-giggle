ad37170c009fed9dbc77154b27bacaaa8b387404
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:08:24 +0000

    range sign confidence hair activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def split_path(path):
+        return os.path.split(path)
