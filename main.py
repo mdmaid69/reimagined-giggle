4908d5344a32e6a644b34e2926dffe9eba912ed8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:44 +0000

    address keep ability respect action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def subtract_numbers(x, y):
-        return x - y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
