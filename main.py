ae0dda2bcfe17a5b2581553aa90bd13c55d81998
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:47 +0000

    race flood plenty share whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
