a8609dcd8f064bb49db93af2c27764c1c5b24d13
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:55:49 +0000

    float getback century write key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def get_array_length(array):
+        return len(array)
