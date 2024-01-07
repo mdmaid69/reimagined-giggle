29391dc81bb770a584f51779c644e75c4e3b68a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:55:25 +0000

    careless bury sight shadow on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
