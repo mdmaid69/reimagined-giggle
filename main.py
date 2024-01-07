1c11144212faf110266154752246280a094032ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:52:15 +0000

    ground immediate rule young public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
