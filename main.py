484770e6f9208391a71e9c0fc833991eb8335d43
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:17 +0000

    go lie floor partner attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
