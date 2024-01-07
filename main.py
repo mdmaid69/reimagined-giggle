c040c3e5b1f704f8e3c9005f27618c3cc7bd36b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:53 +0000

    concern wish most move Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
