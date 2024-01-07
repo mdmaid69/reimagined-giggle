23a9836da7dd6e20f46ca2656e04f1fc992cef9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:00 +0000

    mistake rate this company succeed

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
