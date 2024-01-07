0a853c52c00e9e74b5ffa47d1b31ded5050b5c55
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:38:57 +0000

    change spray we pipe who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
