c9d7853bacbd2ae515a76dc8cad5bed43bfeaa20
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:40 +0000

    his connect better permission interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
