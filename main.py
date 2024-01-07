8eecc0194edb5034c3d9ceb31d2f727c4dc276b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:49:00 +0000

    blame plate lookup dead smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
