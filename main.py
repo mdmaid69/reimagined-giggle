0710bf8b6831e75e33df8de04f7f17f86b502e87
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:19:38 +0000

    circumstance common cost only formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def get_bytes_from_array(array):
+        return array.tobytes()
