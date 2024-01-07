cfd49325119fd0acaced2eceae78dd5081e33449
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:58:25 +0000

    collect bad goback(to) fortunate goaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
