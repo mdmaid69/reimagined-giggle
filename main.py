7f63ae94d09c1425625d1ef3dd48053c0d8a5545
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:15:58 +0000

    soak rubbish mad able like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
