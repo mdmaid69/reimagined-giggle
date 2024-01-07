ad689a9750ee41f0deaf210db573a5b4116e7810
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:24:38 +0000

    print and Kk love usually

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import datetime
+def get_current_date():
+        return datetime.date.today()
