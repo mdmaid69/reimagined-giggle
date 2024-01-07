8b15b2ef3523fbb22832b81f434eeea8fd63468a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:57 +0000

    correct choose sky criticism close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
