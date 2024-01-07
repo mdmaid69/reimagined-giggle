12c74a1e24b1e1d11975c9c96712454f70912d50
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:56:07 +0000

    soil happen crash price possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
