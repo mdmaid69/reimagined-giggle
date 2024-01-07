a9b8595c770b14342533b5d5f9dd15e84ef79aff
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:06:51 +0000

    mess busy discuss mainly direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
