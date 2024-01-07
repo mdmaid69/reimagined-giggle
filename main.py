08ed3d035385ed6c93df44972f48b9bc3ac017af
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:10 +0000

    environment warning offer Nn waste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
