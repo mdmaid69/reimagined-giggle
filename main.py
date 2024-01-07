a0585a05fa86a32d97d76a108a9f9bc749d7cfe0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:14 +0000

    tense consult hot content pretend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
