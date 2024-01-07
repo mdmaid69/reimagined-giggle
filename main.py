1954a83b26b88b4dbefe86fc474ed197bc4b24fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:19 +0000

    rest also comefrom accidental move

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_density(mass, volume):
-        return mass / volume
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
