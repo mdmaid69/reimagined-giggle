d73f4b6a5815b0225f045a7d2941284cd21d6ff8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:46 +0000

    Rr lot high information bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area(radius):
+        return 3.14 * radius * radius
