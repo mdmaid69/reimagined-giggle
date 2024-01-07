4d12cd9036570514fc069562f09cdc22e2f4749f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:56 +0000

    tone border no vanish properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
