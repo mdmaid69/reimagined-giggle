a13a646f28c9417a48e61f193507fe333eed5134
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:52 +0000

    talkinto subject contrast workupto available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
