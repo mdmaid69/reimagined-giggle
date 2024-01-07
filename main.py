fd9b73a45745fd5a32929fbae8c92311ec136d8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:05:52 +0000

    street already agreement breakup state

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-print(os.getcwd())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_repr(array):
+        return repr(array)
