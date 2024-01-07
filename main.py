c6b80ef6ca94de2b45b523e961f9ba80ed2f74d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:50:28 +0000

    up quit accidental friendly phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+import time
+def get_current_time():
+        return time.ctime()
