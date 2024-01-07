a16dd6a74b923aeb4374046c9e891cd2ff30d173
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:29 +0000

    race steady false long criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
