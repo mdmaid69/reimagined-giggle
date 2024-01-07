21aa08e48eef99c70f52c99971edf3b42bd1e04b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:28 +0000

    meaning knock trade consider there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
