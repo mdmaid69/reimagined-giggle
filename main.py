f8a88090ef34b67a0795cc389bf5f039ccb3c883
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:05 +0000

    its pattern replace convincing official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
