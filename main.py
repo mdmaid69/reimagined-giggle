8810a6aedd244d7540b06ccd56557fffca547f32
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:47:04 +0000

    shallow importance might size bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
