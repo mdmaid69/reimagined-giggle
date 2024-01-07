216aa03995ac9f0cfa2f4f91bb6b922038789694
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:42 +0000

    against cool herself nigga ear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
