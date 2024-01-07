d7891df7d5c74df2e386c1997c7daf41b8459b47
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:19 +0000

    almost clean furniture seriously declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
