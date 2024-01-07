22bf0c29074f8e1da6dfc4a0e02b8fef356aea9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:20:53 +0000

    beyond slow catch shoot back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
