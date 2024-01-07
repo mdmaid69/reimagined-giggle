0ac762028096e6a226d1d896f2528a58bfbea16e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:35:19 +0000

    makeinto cost difficult finally set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import sys
-  def get_python_version():
-        return sys.version
