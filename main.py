b20ae98df9e4f31e5889a20a0ff030d3ec476f5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:28:09 +0000

    swing disturbing peaceful partner corner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_str(array):
+        return str(array)
