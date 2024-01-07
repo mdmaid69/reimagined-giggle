25fd8c72ea7f193da57494363618a7e483cdac4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:07:53 +0000

    out waste depend remain somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
