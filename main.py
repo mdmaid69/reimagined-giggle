4dc9e4e2169e4173bc05d13b11f33ed5384d2bdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:00 +0000

    branch conversation false goout Itself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import tensorflow as tf
+print(tf.__version__)
