125b1a8aefc0c5455d78ac42150e818a6c3b7670
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:44 +0000

    repeat 570enjoyable step spend trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
