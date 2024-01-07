49cd5c818e971e9c5d1165f346f109ca35ad99e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:18:19 +0000

    tooth news convinced activity foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import re
-print(re.match("h.*o", "hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
