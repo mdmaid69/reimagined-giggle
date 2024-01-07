6d5d2b710886b5bdaf0223a861f39021375792d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:09 +0000

    always complicated factory public justice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+print(math.pi)
