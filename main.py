acf39d09ea2216c51c12447990dc9cf6b6513b15
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:42 +0000

    brave radio public Itself honest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def split_path(path):
         return os.path.split(path)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
