d516d7edb26c97700ee25fdb065ad5438075bcfa
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:25:22 +0000

    development late level bang better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_acceleration(speed, time):
+        return speed / time
+import re
+print(re.match("h.*o", "hello world"))
