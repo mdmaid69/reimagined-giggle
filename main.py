86b90932365ebebf5ec6b20f8d455669eb53fb8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:40 +0000

    back reason keepaway regard scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def square_number(x):
+        return x**2
