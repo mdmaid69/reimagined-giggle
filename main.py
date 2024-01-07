9ff0eb8576120c781ecbae57a61921e774e99e5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:28 +0000

    high ear arrangement live cry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
