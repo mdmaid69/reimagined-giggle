b3e61cbe9e48a601d7046d4fdbc7275d2f80838b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:13:13 +0000

    operation instead wall balance represent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
