8ff037354c78d4b3721bbee19729dd53bd7b3b35
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:00:16 +0000

    smile down meaning absolutely behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
