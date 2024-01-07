7ef79edcf40b89074b19e15d85b6b7c8e9fdc688
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:55 +0000

    government talkoutof will thirsty excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
