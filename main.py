32a518c5bf94d70a5cc53196a97e3a80946c7a5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:01:56 +0000

    bin still big shop touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
