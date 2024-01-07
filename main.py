6e1870737a56e8016442e343584e609672b6673b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:18:01 +0000

    great dream meet takeover importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
