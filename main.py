0ebfe98bdc6c20eed2211fe226869721254f52ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:21:55 +0000

    benefit chance through loan move

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
