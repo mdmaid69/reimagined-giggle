408630cee5e0c1fa5f08a2e762deb412b66e99f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:00 +0000

    attitude undo wave pronunciation instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
