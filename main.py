32666cce25cda444b257459bb8901ffb9daed81c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:08:37 +0000

    Cc sky accident emergency software

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_acceleration(speed, time):
+        return speed / time
