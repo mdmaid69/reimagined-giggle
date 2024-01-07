214860b62759589a6ee4b14669573d4fd9256262
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:44:19 +0000

    opposite report smell wipe no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
