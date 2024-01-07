71edc25ea2fc05e523ac886372426fcf1c351b85
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:27 +0000

    suppose maximum shallow cause careless

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
