cf76eb528ffdb97fc6659e33d4bba3ec5e4e5057
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:31:15 +0000

    acceptable experiment original excuse underneath

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
