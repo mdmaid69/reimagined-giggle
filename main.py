9e800fbd57cf4b96eea926170f5804345de6f552
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:27 +0000

    dig last plan dress cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
