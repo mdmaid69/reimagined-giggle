4b9c47f2bbe39f8b95dd4f2a74d167aac8ee68bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:27 +0000

    contact often any chat passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
