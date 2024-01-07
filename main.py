fb2715860e11a403b61928c2f187cf7340513ec6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:21 +0000

    regularly lay regard Oo control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
