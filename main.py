6a8b15eee8a4842810dd6815816b70edc613b633
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:36:30 +0000

    shock unlikely goodbye sweet why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
