bbea31c5c0c7c4ae7e33f4e858fe3a923c53f484
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:02 +0000

    record bringback raw care up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def roll_die():
+        return random.randint(1, 6)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
