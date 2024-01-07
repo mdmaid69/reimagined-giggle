7d4e45dedf8026456f71e8204bd0f401520e80cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:49 +0000

    meeting burn rough area church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
