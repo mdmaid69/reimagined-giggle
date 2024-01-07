5191c9f98d3a95ac8889235c283f650eed448599
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:30 +0000

    lead month cross break remove

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def count_elements(lst):
+        return len(lst)
+import os
+print(os.getcwd())
