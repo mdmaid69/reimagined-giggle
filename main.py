e252eff5286000675db5b91ee08bedb949b7545a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:27 +0000

    circumstance name really goodbye soft

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def cube_number(x):
+        return x**3
