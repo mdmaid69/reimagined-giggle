a3d71fc285cb44a860124277d9d896ad0f37d2eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:14 +0000

    fold shape over beside if

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import platform
+def get_os_info():
+        return platform.uname()
