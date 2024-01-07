2621b8ac97b434b1217d8239073b80b4207e8c9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:37 +0000

    smile grow down definite simple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
