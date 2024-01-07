413ed0b14cfd1a7948b4e05def21c702aa0adfaa
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:42 +0000

    lesson delight calm actual hold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_area_rectangle(l, w):
-        return l * w
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
