4384dcedccf8ae2a44fb3064e00d588ca14458d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:20:34 +0000

    please trip lookback(on) precise practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-print(sys.version)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
