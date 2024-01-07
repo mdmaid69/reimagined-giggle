34247688165b7c46b0e7d9f75a72da611ea328b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:37:10 +0000

    port middle personal limit vote

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
