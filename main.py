39aefe752e8f6a34cff4cd2d753fa42a13752a7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:47:26 +0000

    debt push swallow react compete

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
