6d74b7fd8b2ef8ea509d582687459f157601921c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:02:59 +0000

    return movement tend performance takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def subtract_numbers(x, y):
+        return x - y
