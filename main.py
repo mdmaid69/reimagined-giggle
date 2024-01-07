b7d7137a912b5beabfabaeb49bf8e0f02c08c8ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:26:24 +0000

    path equal boat tape for

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
