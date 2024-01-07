99f72e1792f3d4bd137770b5987befbfb8503de7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:44 +0000

    tense fat 569enjoy lay scrape

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-x = 10
-y = 20
-print("Sum:", x + y)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
