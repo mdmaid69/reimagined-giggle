9b91df8f31833cc503582818f547fb5b1e99b357
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:11:54 +0000

    concerned prevent cope broken excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
