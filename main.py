4155d219298520be21ac0467aa3d60388b5968b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:45:08 +0000

    permission influence action sore break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
