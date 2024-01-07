1dceec3a1545b2f3a66ec712cee9e7850f37c7d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:29 +0000

    description keepfrom news thin collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
