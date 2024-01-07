f0e88cfad76390eb8b1194ed50a0b6280241652b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:11 +0000

    reach course letter worse sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def convert_to_octal(n):
+        return oct(n)
