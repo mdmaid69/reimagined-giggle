f331cda6051b708517ee6183a9c45889e7732d8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:36:13 +0000

    capital cover risk getoff dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
