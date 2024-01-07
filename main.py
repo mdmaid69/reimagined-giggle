39bd30219eff483f6c69623a0d512bbefc68b0c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:35 +0000

    lock beat workon will intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
