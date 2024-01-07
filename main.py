402c9fb159cdf8938869da8382aafb7a3aa2c7e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:59:28 +0000

    fly announce whistle giveout workupto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
