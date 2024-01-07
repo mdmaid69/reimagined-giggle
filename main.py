ca53f761163aab72ac328dc2981027155b3e878d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:55 +0000

    find lend platform whatever nature

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_to_binary(n):
+        return bin(n)
