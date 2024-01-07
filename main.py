c15fdf904732a780e283c79dd7f46c689d9b4c87
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:40:24 +0000

    shrink OKorokay freeze makeinto programme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
