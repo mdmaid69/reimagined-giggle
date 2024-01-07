6dd9dacb30ffd85a7f1ddd685829aa2427e2421d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:24:37 +0000

    unkind children answer pocket hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
