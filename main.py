703216ab1d0960b600ef4f120ec1c72345e86e3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:35:49 +0000

    maybe cruelty turnout quickly partner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
