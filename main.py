ab992d82fecc33ac7b7eeb4e0bbab2d3d9044b63
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:29:37 +0000

    Hh occasional side spring storm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
