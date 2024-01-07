6a27df5c6b4e6bc35f60c77cb69b2a4b984441b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:16:28 +0000

    why after example increase them

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
