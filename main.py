1c7b7c84e5f5c90ad48a14303a3c08ae5f3bfb8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:55 +0000

    furniture rarely wing takeover branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
