820bd46306996ccfa08d54db832633149c3d6b5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:21 +0000

    tense several shrink print ocean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
