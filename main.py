7358eb3fc7f1154b693054d315c562cd12e9d4ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:02 +0000

    save exact benefit set side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
