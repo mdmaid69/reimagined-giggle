db077d729167f18e06c704678074628667c55436
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:53 +0000

    cut train nonsense sound neat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
