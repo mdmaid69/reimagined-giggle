8413d777fce481451c6c5ecc0356808e3e096dc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:19 +0000

    fat hospital waste walk bridge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def subtract_numbers(x, y):
+        return x - y
