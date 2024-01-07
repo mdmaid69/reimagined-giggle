1f593ff87d260695d8ef73cde8a36d6e363e0d14
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:06 +0000

    detail indeed you pretend other

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def create_stack():
-        return collections.deque()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
