e54e09f0d7100b22e33d25d251c1bed64d8ebe43
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:25 +0000

    which now bringdown invite reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def sort_list(lst):
+        return sorted(lst)
