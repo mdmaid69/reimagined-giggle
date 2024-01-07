378db954b09359a5cc1cbf3da17c1852329531f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:05 +0000

    difficulty spend exploration surface how

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def get_bytes_from_array(array):
+        return array.tobytes()
