e53857a5221ec8daf96119b39c19cd8c3d70e7b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:01 +0000

    concentrate exist dance communication cheap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
