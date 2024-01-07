369799e5db86be71b7940e14720ddedfcacb6955
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:21 +0000

    possibility performance polite target public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def square_number(x):
-        return x**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
