10a714f489d82dfa1446002bf8adfc20efc779c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:55 +0000

    correct sing finish definite half

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
