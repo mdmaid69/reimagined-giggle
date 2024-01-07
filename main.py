c8722407d7054226454220cd0ab6c26ed9f2e0bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:33:30 +0000

    chase turnup turnaway simple original

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
