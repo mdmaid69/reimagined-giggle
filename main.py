41003305283ec979ed72c4e5acc1c1021f45641c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:47:39 +0000

    operate start major cheat mouth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def subtract_numbers(x, y):
-        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
