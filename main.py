173c496804e6f648fea314772c9c2e1aa60995bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:19:27 +0000

    turndown question basic mail exhibition

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
