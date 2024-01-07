17b8cb844e4a35350893b05555185beca7583dbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:08:28 +0000

    contribute similar sister thanks section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def print_python_version():
-        return sys.version
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
