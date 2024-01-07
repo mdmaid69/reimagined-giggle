b9e6ca4790bd09dd47ceed70c0aae5c04aa62246
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:40 +0000

    ours thinkof true search remove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
