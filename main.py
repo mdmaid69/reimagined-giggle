7d36151b4630afd5adaae4f0e21512bfe980d85f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:03:56 +0000

    each mark sound scale advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
