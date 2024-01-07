97d6f7145852b80a829c26194e5dd7bbd7fa1650
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:31 +0000

    some match each score separate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
