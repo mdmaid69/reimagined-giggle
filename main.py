241842ec3ea96dfdd9bb61e2307247303d66ab25
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:52 +0000

    loan awake female television firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
