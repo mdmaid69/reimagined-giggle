3e730390d5cb25b7e85dbb323f2f1a92722e1588
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:53:46 +0000

    tip worst phone anybody spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
 print(math.pi)
