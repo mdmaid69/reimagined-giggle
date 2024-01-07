4cef16958e0628ac160617cce31fae90ae7706e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:25:37 +0000

    trial shop could often thumb

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
