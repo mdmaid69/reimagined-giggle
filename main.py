ee35cb7f271a82dc072a93b4ce10bbbd3503c0bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:49 +0000

    interruption safety far split value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_str(array):
+        return str(array)
