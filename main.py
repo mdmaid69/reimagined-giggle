215d721c0ea42180dee09eec336d852a410ee7f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:55 +0000

    talk concentration person command even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
