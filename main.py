7b9b5fcad751e275fbb778128479158a9a81133b
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:55:59 +0000

    interview check hit create change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
