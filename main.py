282fb73ec704a1ed03a1210d8805271c77b0b2cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:26 +0000

    develop manner community form suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
