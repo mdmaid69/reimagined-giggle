d3aacb35a2d28abb84a3780a19258f1eed304073
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:19 +0000

    wire eye prefer sweet company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
