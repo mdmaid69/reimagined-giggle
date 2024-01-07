59f3e78c2da705f5b8d262dffb088487fc1c0fdb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:41:19 +0000

    order quickly guess deal not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
