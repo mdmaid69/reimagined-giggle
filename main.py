17efa028ef4eaeac6db0b32399ac6ffb82a60608
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:15 +0000

    freeze refuse mass connected task

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def divide_numbers(x, y):
+        return x / y
+import numpy as np
+print(np.array([1, 2, 3]))
