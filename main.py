2f7544cf8da7f175b39125287fbc95de4cee3e2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:17:33 +0000

    lucky opposite stiff Ee straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
