ea4845c0ad77f0f89e416a298b61360a5d6b3c70
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:52 +0000

    no rise basically borrow weekend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  def subtract_numbers(x, y):
+        return x - y
