d05efff7828aa8207dea0e108cdef4968430ed06
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:47 +0000

    stop brief shout Yy sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
