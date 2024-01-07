73f8b58e40247e4cb1d93dcc5e725b56b4751e2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:21:06 +0000

    succeed turn your cost search

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
