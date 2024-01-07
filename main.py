0cbf60abb7eeb9e80dc226227480e395c8955e56
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:11:42 +0000

    unless scheme mother manage detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
