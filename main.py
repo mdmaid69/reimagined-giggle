73d0709e60aa6fdaa97b5a3d7511a1100ea925d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:32 +0000

    bringup admit free cap explain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
 import array
-def get_array_as_list(array):
-        return list(array)
+def set_array_item(array, i, item):
+        array[i] = item
