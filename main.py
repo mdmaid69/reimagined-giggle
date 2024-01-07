05506f5409478e2bee8e65564c96d005dfa3203c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:45:25 +0000

    comment type minute treatment everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
