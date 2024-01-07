fa343af3b4e033e67edb89bb384dfbf64af45eb4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:58 +0000

    comfort scrap oil surface clear

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
