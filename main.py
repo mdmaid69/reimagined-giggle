b7781e2cab0bddb5eb4577dce85dbc0b946a9fb9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:49:46 +0000

    design easy shine search lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
