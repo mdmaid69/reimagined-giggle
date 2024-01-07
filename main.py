445e7a0446eb0685ea578f47bc02d9ba6f04ccd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:41 +0000

    listen explain rarely offer usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_item(array, i):
+        return array[i]
+import random
+print(random.randint(0, 100))
