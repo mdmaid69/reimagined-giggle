20bbe328f6d147f05ab132bd8c96c0880575a2b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:35:34 +0000

    maybe Ss generous division neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_list(array):
+        return list(array)
