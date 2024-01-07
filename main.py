dff0fbab6fb5da7e8402c630c994e8f268eb019d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:03 +0000

    raw typical come pour paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def is_even(n):
+        return n % 2 == 0
