70cca9611ac59cbfe5e70848ea5bfa8f6b9f6e18
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:23 +0000

    so city Ll school except

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
