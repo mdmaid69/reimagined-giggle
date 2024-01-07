8ed76be5ee43fa5aa0f3e1df1712ecc87afc6071
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:47 +0000

    pencil study pleased prevent sister

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
