5cf890f9138c4832d89f2151d52fbabe97ab9a1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:21 +0000

    interruption person like leg earth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
