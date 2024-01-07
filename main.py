437711a383cd623fee87eaa88aa9387385fe4061
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:35:19 +0000

    getoff generous roof join close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
