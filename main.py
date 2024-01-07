b2e03db64f8eb0776bbd146f74cdcd5c78654a53
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:38:28 +0000

    begin beautiful apparently suggest second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import os
+print(os.getcwd())
