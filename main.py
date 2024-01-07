f41e5c0ae58c471290bc0cb1bcc79cd56018df5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:31 +0000

    autumn respect warn foreign recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def add_numbers(x, y):
-        return x + y
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
