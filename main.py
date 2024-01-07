a9ca5f267af847686f2d7b510c75419cd4933b13
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:07:54 +0000

    thirsty land scream pretty surprising

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
