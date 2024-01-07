5d462c4ba5cd493f94059a3b8c34a738a08afb16
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:23 +0000

    breakfast born turnover soon last

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+  def count_elements(lst):
+        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
