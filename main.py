35ca971a2b2f0de66514ac1d258281889b6a6cf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:15 +0000

    keepup label mend trade with

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_speed(distance, time):
+        return distance / time
