c9742e8d229c9f9f7c4e0160f90a73bece2e4889
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:59 +0000

    means last hole physical once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
