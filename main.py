b822d6b0644f096f0141b5349900bebb5c02b931
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:49:53 +0000

    plan getout workon lend heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_current_directory():
+        return os.getcwd()
