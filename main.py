0363ac8a6e5d6b6239d2689127b172652b22666b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:00:55 +0000

    section direct dish black such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
