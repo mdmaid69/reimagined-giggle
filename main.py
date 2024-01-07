b826e18779ee3ccaa287cd37b4070a580bf9691e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:53:53 +0000

    impossible admire wet argument keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
