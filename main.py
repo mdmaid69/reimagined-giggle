9f8655be6f7ea5cc01a317ab700e64c176c3094a
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:14:23 +0000

    summer coffee famous little tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def is_odd(n):
+        return n % 2 != 0
