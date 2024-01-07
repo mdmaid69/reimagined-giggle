890ebb2bf2d8f726625dec205031048483ae9c1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:32:01 +0000

    lookaround guess pretty Itself far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
