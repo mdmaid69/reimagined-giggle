6833990d43238ddacd495232243c58c0cc99c00b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:28:38 +0000

    trap police search clear fast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
