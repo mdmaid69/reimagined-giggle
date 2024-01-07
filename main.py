a1a9a677f1ed689881cfec4a53fc90bca8e1b31f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:51 +0000

    exhibition hold hear petrol complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
