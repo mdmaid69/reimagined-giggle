e402bcf1e4c4899a897e3358ad82c5884e6f9e09
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:15 +0000

    bin mysterious hurry pin obvious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
