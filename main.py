5314802d5519beed6d282ad6821b3af2217fb7b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:04:44 +0000

    risk prove sting consult wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def clear_array(array):
+        array *= 0
