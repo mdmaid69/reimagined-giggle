506e89426df0e02be8f9ffb48e6823049a671118
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:16 +0000

    splash approve entry tidy fun

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import logging
+def log_message(message):
+        logging.info(message)
