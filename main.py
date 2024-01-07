19f2765947c9a312263733d862d5c37f5c1cab84
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:36 +0000

    moon terrible certainly Tt takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def is_odd(n):
-        return n % 2 != 0
+def get_array_as_str(array):
+        return str(array)
