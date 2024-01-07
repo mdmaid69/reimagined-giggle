d1819e3e9792b1c3466e9186f34a3a5d86a878bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:50:30 +0000

    face through earth regard type

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
