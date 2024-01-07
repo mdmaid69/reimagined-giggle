6b2766f46ba4625b311e0dfd8abf2bce9bedd34f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:55 +0000

    period the duty possession month

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
