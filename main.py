5f30e1c78f93a386209b311e768962a653ebc0ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:13 +0000

    further dear but discover rent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
