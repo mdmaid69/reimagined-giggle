46ce0f5d32f35cdfd562069453b0b4f205464dda
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:44 +0000

    Ll stand before my sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
