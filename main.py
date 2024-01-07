61e0878bcfb792c793c2ab4ea29b027841a48b81
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:47 +0000

    memory star only although tooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
