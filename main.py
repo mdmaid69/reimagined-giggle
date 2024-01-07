ff74b81df541860d53a7ccd7c9348b2c0a1f2e5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:38:34 +0000

    first brown slight convince tap

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
