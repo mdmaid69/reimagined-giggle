67acde93aa03c77ca2ed5ddde174d2e07132364a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:33:16 +0000

    undo roof contrast stamp bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
