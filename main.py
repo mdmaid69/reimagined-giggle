ffd4480113d4b54c683f56e6f6ed4bacbb95d59c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:29:56 +0000

    war feel short trap principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
