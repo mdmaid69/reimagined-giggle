e3e2b48ed975a8e8684e855386f96f065058684c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:37 +0000

    drive friendly smell home sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def change_working_directory(path):
+        os.chdir(path)
