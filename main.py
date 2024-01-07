ac2fc6a02bc532568df534ff89a786dfffc328c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:08:29 +0000

    petrol doubt thorough boat round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
