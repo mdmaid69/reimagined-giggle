90ab84b460b05446d18597a296f918bc1cc2aea4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:14 +0000

    mistake tongue match mistake comfortable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
