3b87fb8fa6ab3e8d576a6006ba7cae42143aeaa1
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:57:31 +0000

    both ever communication dress afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
