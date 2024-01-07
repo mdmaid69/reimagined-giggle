c2c2faed4c184092a9140fd4a3fbb6190b348a84
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:51 +0000

    dull artificial wide character mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
