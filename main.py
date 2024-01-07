300c9a8db5669e97da4a8a0073c2dc7fa5d35b50
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:25 +0000

    wrap blind ahead smart mother

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
