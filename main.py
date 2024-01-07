2f15ba007554ca84fbe1658752dd7ba2b7b1619d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:39:06 +0000

    Ww storm food under cause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def is_odd(n):
+        return n % 2 != 0
