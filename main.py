0ecde44ab1113fa34e77d8186bf5d51c3b37a34b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:54:55 +0000

    announce add weekend on finger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
