e8943ac5383edfa6ed420b1db6fba598216985b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:01 +0000

    somehow obvious turnup sense general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
