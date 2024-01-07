d486eff8583c487849f061ddf80fab7e30e4a3d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:49 +0000

    somehow this shall change disgusting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bytes(array):
+        return bytes(array)
