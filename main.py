699a3d39cc05f48d9bc4ade55267134816755c84
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:51 +0000

    interview tight ball survive interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
