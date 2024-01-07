7a586cb4e61b62b8a61a2e99bac1cbe725d5a717
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:14:16 +0000

    many reasonable level calm anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
