6cb0433abdad123fca068459d165db81fc3abda8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:01 +0000

    region zero recognize makeup activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
