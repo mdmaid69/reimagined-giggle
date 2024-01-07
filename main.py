ad2cff853ac968ea651761c367a407ff3daee403
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:09 +0000

    advice however length add exactly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
