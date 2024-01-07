5856edde47f59da51e5081d2dcb874d12120f7a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:42:57 +0000

    circle praise steer tone before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
