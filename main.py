0142f9672c8e482abce1a1c996ac69358e82c346
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:28:58 +0000

    wind destroy nearly buy delight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def get_array_item_count(array, item):
+        return array.count(item)
