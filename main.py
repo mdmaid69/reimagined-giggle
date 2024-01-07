b56d99e159cfc7dfdbb3a089bf94c697e7d4453b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:08 +0000

    string afternoon square interesting even

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_speed(distance, time):
-        return distance / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import os
+def get_current_working_directory():
+        return os.getcwd()
