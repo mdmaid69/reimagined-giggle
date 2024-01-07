7f6acfabb33543ae49bd542beca9f5b98c5481e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:52 +0000

    idea business proud dish drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+name = "Python"
+print("Hello,", name)
