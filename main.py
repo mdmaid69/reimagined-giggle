eabbd826ae49b9f5edb4799998be922680bb591f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:56:33 +0000

    neither familiar holdup late safety

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
