cad1e7b783e3e728eed06fb0dbc0f3be20053e78
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:28 +0000

    sink shelter receive pollution responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
