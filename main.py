251fcfdb79a6eacef5e1c4b2a2c8ebc0c6829b6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:46:55 +0000

    care similar chase offence sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
