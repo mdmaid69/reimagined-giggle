8349db2b0662d554efabe39af9b8524ab825879e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:01:05 +0000

    repeat workupto definite while rob

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import sys
+def print_python_version():
+        return sys.version
