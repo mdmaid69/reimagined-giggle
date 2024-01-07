da6430cc9933f5e7b7e7c8177880f77d004320b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:30:11 +0000

    prevent place avoid gentle dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
