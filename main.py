dd9797cf0f688c383f5d66a174b62165331f92fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:16 +0000

    whistle oil forgive art bet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
