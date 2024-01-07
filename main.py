f4de39b240683da0dd18e538a0c36f34a9705bfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:47 +0000

    force mass getoff accidental study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
