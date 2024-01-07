c1e6b37510387d1ecad0e09ab28f576d27e65ff0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:49:47 +0000

    damage full even doubt unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
