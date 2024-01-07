c402a86e6a531e43a2ffcfdb7e22df9401ae493e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:15 +0000

    to notice pattern tragedy study

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_queue():
+        return collections.deque()
