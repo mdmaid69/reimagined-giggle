9ff795fedaeba66e50b8b56dc16333deffa11cf6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:32:52 +0000

    quarter lost approval stay Pp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
