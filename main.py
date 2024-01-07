339868e3e9a125d2392bb2deb57010394e0ff076
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:17:21 +0000

    crowd block fire target whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
