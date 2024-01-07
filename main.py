99cbe2d599ebb28b53e561493a91e4dc58b2da02
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:24 +0000

    doubt 564encourage moon soft persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
