f633c25a98bd5c7a6630770c485507d4e1767311
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:20 +0000

    passenger impressive him represent stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
+def clear_array(array):
+        array *= 0
+import array
+def get_bytes_from_array(array):
         return array.tobytes()
-  def multiply_numbers(x, y):
-        return x * y
