7f23d7e432db3103f344030a60c65aeaa010dc58
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:56:26 +0000

    comein shade fall accidental later

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
