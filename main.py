ee9bb4e9ff879cc5b18dbe5720a4cfcfbc04013c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:58 +0000

    lift behaviour manage nor side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
