11d45d0d73041d6c2795530584a98b796396ac31
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:48 +0000

    fork emergency half wake threaten

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def convert_to_octal(n):
-        return oct(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
