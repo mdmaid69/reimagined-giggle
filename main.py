8044aefa489b36abcaf45ae1ed036cd7de44101d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:14 +0000

    bet round start remark fairly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_float(array):
+        return float(array[0])
