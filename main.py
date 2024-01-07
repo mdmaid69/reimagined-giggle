54617dbbbc54bbd520d0089b77fa4e6de43a79bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:04 +0000

    detail range example sentence breakout

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
