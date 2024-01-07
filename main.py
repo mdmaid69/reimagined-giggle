6eaa470f8d1f50a1a48af7980fa78e9ed7aaeb26
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:26 +0000

    across never coat produce someone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.ctime()
-import collections
-def create_counter():
-        return collections.Counter()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
