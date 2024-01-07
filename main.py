e2510d5d2ebee58ba4fb99f361066f779d33c53d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:30:20 +0000

    plan stir spot spray lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
