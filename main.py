932c27d9f42a7a14c526ae26c9711957f4fa1c71
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:57 +0000

    negative ilovedicks miss address pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
