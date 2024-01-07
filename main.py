6bee0ef3e7e25c02a5e447d461a98a0260a99e0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:12:31 +0000

    minor shut floor gift generally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
