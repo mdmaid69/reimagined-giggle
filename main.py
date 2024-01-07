08cf45f7822c4ee59545d59d49aaaffba4d97800
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:27 +0000

    such neat goaway squash put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_current_working_directory():
+        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
