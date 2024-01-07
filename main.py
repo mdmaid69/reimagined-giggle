c4c4f2796c6e2cf689332cbeb63d2a2e8f69b36b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:54 +0000

    platform watch judge copy tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
