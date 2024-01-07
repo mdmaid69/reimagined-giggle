f56725dee0eea6b3f21fff47b1131f495552b6b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:47 +0000

    advice build personal everybody light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
