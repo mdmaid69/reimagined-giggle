557dcc20f5fa90f56f50014075746266c5a9d231
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:04 +0000

    type immediately again badly please

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
