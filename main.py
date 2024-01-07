782ce88ee32520d252654556a641f34d33094e4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:34 +0000

    judge attract down exchange stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
