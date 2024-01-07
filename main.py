2cb8b536af864696e2df6a416503056388a3d9a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:41 +0000

    strength fresh harm holiday healthy

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def find_max(lst):
+        return max(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
