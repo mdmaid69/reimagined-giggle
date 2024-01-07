6112feda41259fad2f8ba177a187404d173f9f6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:43:14 +0000

    drive closely dinner welcome suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
