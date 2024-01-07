f3cf3f9266580028416f2fb2ed880554724f509d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:48 +0000

    blame cheek Uu direct nice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
