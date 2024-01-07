b6eb8ecbab9ea2c166be9f3d2e59a807b016eaa5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:53 +0000

    rich shut luck exchange scene

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
