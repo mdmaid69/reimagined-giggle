fe6faf33692edb0fcf2c02be05b386c8052d7b4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:40:30 +0000

    sound stamp commit geton prove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def cube_number(x):
-        return x**3
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
