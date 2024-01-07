0e79fa3fdd83b5a6a4e9c0dbce60cdfe6cb5db2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:20:41 +0000

    claim 569enjoy deep public tune

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_sine(x):
+        return math.sin(x)
