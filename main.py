aa5f3e76a8208232560795c8ab9743a77e3705a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:59 +0000

    try official turnoff successful spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
