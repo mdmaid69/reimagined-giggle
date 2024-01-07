efd1989a969c50d23219329ab20372616bf4571c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:44 +0000

    raise work tonight among intelligent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
