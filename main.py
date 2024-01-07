585b884e1708726341bd3686b97ebb144388f45a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:50:54 +0000

    makeinto worse trap clean trade

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
