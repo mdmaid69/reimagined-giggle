c3404dcaccfc970f8e29b1f8d568fee5038e605d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:16:53 +0000

    business runaway various grateful influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
