b1b62f21c4f45848016479db7ed4c4bf4d489fae
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:50 +0000

    die desperate stage fork fix

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
