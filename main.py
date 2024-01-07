5712e8ad71c087c547cd57834ba804ee04ae0b1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:05 +0000

    regularly cross properly show after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
