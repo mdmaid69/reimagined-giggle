f20cc11f4f77441eecb598596924b20d8035ac78
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:36 +0000

    climb thick explain original anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
