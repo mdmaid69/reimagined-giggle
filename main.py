4c83bc717538af25372954b920b08964434d0a7e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:15:38 +0000

    bringback drop straight later job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
