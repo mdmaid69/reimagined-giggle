5bf269f6a5cb5d7c13badc1e73559f305b500d75
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:15:27 +0000

    rain far greeting truth sensible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
