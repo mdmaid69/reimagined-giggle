21f34208af1cb3d2f55c5833b51b07bdd3f28657
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:58:45 +0000

    couple spare cheque might never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
