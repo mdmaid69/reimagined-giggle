db4a7f686debc94dc775d3501c219b31bc5009cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:34:19 +0000

    future laugh group pattern matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
