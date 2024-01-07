bc3649c321994cac6ce64af0786af39ce0b3c178
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:38:55 +0000

    pay name count deal no

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_power(work, time):
+        return work / time
