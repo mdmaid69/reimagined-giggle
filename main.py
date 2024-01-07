30bd491559e89d86abbba6283f316a53e65408b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:24:36 +0000

    flat share pretty club lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
