176bff60469ebfb6406a29fea5e73f64c2f87692
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:30:58 +0000

    giveaway section correct fold sister

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
