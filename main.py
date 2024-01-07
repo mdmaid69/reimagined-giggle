16e46ed1b18440d2815f9a0bf53876d252de2a54
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:57 +0000

    pour balance comein previous somewhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
