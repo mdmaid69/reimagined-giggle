b126bf758d8c6ea62186f10966572031d4c321f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:20 +0000

    secret front small safe somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_dict():
+        return collections.UserDict()
