154dcccfacb43ce1ac460941532519737b009dff
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:03:31 +0000

    reaction impossible minute town prove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import re
-print(re.match("h.*o", "hello world"))
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
