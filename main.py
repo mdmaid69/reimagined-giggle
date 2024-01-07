660e23c24d4db2c5e576a966ce30f6aee14cec11
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:21:56 +0000

    risk hit fear valuable short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
