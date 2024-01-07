8086bc33b8a895054b2eda9268ef16e70dbaeb0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:05:28 +0000

    anxious alone for university scrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def sort_list(lst):
+        return sorted(lst)
