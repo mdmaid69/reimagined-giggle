850841022a914b4dedd14ecf1a385ad9578e4ed9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:39 +0000

    round nearly successful before tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
