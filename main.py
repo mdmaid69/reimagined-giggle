32b2829ea2a8637325595084d62e97a73d31b1b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:34 +0000

    excuse feel Gg common work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
