a59aa6fd39565637bda55403500609a8680c054b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:03 +0000

    somebody takeoff crash taste glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
