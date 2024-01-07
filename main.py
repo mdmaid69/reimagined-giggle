7a99e09e44e84d4c986ec4c307323f9074d5836f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:37:28 +0000

    minor calloff sort feeling smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
