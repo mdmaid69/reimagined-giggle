3fa8104b8df8f9833817775452be750b82e98dc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:28:52 +0000

    absolutely second sad determined surprised

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
