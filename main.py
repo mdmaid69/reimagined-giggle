e11ae86b2f167e7d006c6a741a980ffb1c729eb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:14:35 +0000

    ours relax shape cap spoil

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
