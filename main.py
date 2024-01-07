e83a3ae6dbf69ee625061464b48d041e7149d105
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:54 +0000

    miss the angry fat bit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
