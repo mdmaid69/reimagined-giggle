fe0677aa0cccd457b482b03af951943af22e89e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:23:36 +0000

    child little roof advantage bringup

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import random
+print(random.randint(0, 100))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
