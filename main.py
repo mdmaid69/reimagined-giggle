5412833257046e60e310ee9b07a8abc2a735d2c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:57 +0000

    however image go neither imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_set(array):
+        return set(array)
