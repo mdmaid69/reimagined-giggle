18d4b747dd6a991bf1cd90542a02a9949590028d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:24:37 +0000

    thorough kind rest run quickly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-print("Hello, world!")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
