86d6f12594ac7998eb0876406e6a6a47313ebf55
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:53:25 +0000

    sport repeat late throughout post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
