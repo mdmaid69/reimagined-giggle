6783090af1b4aff7b7196c761f8aa9e2f6e8a1d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:28 +0000

    today challenge print cross swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_power(base, exponent):
         return math.pow(base, exponent)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
