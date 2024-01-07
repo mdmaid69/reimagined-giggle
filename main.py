d46e39e512d393b4fe6abed7807585ebe2af7e0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:33:13 +0000

    able frequent blind spare let

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def get_array_length(array):
-        return len(array)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_repr(array):
+        return repr(array)
