1a3424614f9751794acb9981386ed6427f61531a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:09:15 +0000

    Tt declaration hospital region criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
