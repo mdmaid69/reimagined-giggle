6e76800d8c3af19cd5495de3e06a69344a74185d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:39:22 +0000

    attention list burn relative swim

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
