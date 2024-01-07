c6c9dc8c435123974f9355ecdf409e312ce7a5a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:59 +0000

    exploration black protest solve list

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
