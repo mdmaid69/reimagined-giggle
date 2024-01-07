bcc884a04d7ad336aaa3d8d5f5fd9919b96b4d33
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:45 +0000

    tidy drawing whoever burn goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
