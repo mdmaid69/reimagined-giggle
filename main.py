720f692540f8705d04847c50870dfca26bdcc365
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:45 +0000

    hang trade frequent right attractive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import getpass
+def get_username():
+        return getpass.getuser()
