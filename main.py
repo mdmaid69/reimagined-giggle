0743bdc9170c7f0835b8a876a71803c847b664c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:16:18 +0000

    anyone mark control hope spill

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
