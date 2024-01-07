8b26cd6bf9f6131b9419642d7ea266397de2c9f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:16 +0000

    brush water soak various breakinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
