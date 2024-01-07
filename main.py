52f04a1e742349862915525858558bac8d07ca9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:12 +0000

    beneath empty disturb unfair willing

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+print([x**2 for x in range(10)])
