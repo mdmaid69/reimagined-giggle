ddc6c912e7039d584230dceeb3fc0d8120afb33f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:50 +0000

    fear demand become narrow keepup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def add_numbers(a, b):
+        return a + b
