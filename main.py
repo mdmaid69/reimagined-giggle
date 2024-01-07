199adb5fbf362f43a44993f3857f9340bd885cf8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:24:18 +0000

    strength famous die middle them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
