75d60ea23905b16a10b76435bcd47b1ae739835a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:24 +0000

    wake man own key scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_str(array):
+        return str(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
