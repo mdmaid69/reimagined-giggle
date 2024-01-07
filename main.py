2dfe5340190783df3bdc01aafa0309343abd4d92
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:41:52 +0000

    meanwhile adjective judge flight turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
