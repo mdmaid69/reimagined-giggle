ca7cf9c59e818d356e1cad69026614d3e1464759
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:29:18 +0000

    greeting along little computer inquire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
