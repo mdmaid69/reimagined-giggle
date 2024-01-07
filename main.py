52039f3fc87441a6b52619ab514dc39d0b49114f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:53:16 +0000

    computer country love profit guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
