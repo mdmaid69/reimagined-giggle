e8347981133bcc231102973eecd0bf339a458bac
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:16 +0000

    steer boat complain after choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def append_to_array(array, item):
-        array.append(item)
-def add_numbers(a, b):
-        return a + b
+def get_array_item_count(array, item):
+        return array.count(item)
