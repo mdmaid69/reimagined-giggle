3b7ce7aba8fd47279963aa7b2b27d840cc09e398
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:51 +0000

    correct past capital wild spill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+print(os.getcwd())
