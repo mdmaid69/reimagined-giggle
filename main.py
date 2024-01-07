746604e349b3df9c7aa80a390bdd773fda2f8d4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:39:26 +0000

    tend deliver female bringforward that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
