9480cc38a24de77a4c066707569244835828a6b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:56 +0000

    when instrument after stick lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
