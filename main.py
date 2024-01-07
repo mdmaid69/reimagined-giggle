b5a03d26aec4f0305c75f53bb18b52e9b253478a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:39:46 +0000

    definite goup meeting score plain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def is_odd(n):
+        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
