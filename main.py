c3ef72221d8cb564fe8cd371376ab477bf622807
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:26 +0000

    money Kk report small information

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
