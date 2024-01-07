472403391d0aff946b6ff31e4d31e472a009251c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:59 +0000

    lucky hurt thin illness still

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
