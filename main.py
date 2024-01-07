d5d6f7b5239f5b4714081c0dfcf5b7162b9256d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:01 +0000

    loud accident popular runinto past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
