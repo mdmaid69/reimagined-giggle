1c9cb6b00ff0e7dba379e8a31cabba5590dda039
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:03 +0000

    compete set similar street round

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 print(sum(range(10)))
