e627f2e5fc9ec5d5e05bdc538e43d0af199a0c56
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:16:27 +0000

    raise urgent proud breakinto clear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
