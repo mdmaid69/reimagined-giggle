65bb543fb6e3021cd6ab2720df62c107f18df7f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:19:44 +0000

    board correction disagree taste fair

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
