e74abbe574263a61fc78eb63fa16abc4ddebb6d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:07 +0000

    home authority soak circle right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
