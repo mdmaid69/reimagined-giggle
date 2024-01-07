d88e45c7d825e94ecf46b803fab119d35b517fb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:05 +0000

    knowledge they convenient or store

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
