c232be84b00f9d1e1ab1db7f06afceffea911a6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:14 +0000

    see centimetre geton more part

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def exit_program():
-        sys.exit()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
