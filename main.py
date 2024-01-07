9f3872f432e918b215314fd0ba59a3911aee7212
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:45:13 +0000

    control row spend makeinto understand

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
