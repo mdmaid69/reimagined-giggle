5ac6e4187474841134f0cdd11ee70fe996e91dfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:13:35 +0000

    stamp wrap song though telephone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
