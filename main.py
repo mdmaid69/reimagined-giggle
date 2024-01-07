f23bf9e7e4ba1b27b1cfc969550b87aab39190ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:35 +0000

    shut calloff juice play Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
