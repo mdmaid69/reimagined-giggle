51e3f3652e5cb773e7c732dd3616eee0c95c6505
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:03:55 +0000

    attempt people bottom quality attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def split_path(path):
+        return os.path.split(path)
