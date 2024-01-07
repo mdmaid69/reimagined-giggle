931d107aa1d7653cc0f6b1e44798618fad818596
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:59 +0000

    personal good sick mark thin

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_length(array):
+        return len(array)
