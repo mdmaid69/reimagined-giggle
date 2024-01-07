9afb0070eb02e7aa0aeb5f02e439ec510236b211
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:31:08 +0000

    sharp sweep fix city sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
