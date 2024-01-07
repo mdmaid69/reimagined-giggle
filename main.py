61acece566973064b843b281ce900fe5240fb04a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:03:07 +0000

    dress past fold far repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
