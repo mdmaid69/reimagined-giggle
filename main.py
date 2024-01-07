e4e82c48586ad2f6e7cad36f4254257f6402c731
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:32 +0000

    dare quickly children scream straight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
