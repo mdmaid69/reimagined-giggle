11a319204043b464eb4f3098be7d490c37123c89
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:43:40 +0000

    eager moral innocent lookup goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
