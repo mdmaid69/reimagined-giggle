69c37e9ff95d0a7c9726537509de1ddbc5a9d636
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:13 +0000

    flavour smell lay war let

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
