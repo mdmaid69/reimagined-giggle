9fbd6b65ee1e2ee4d13cf39c9c90f121e00d054e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:56:58 +0000

    perhaps fortunate off opposite himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import os
-def change_working_directory(path):
-        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
