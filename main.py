934b8197e21271a34d5485d38f76d651f381d437
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:46:33 +0000

    interview no tone announce confident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
