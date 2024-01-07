8de3b2e8b194198bcaabbcd523de9c614de931ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:12:08 +0000

    left gather protest respect characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def count_characters(sentence):
+        return len(sentence)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
