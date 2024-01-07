db45b2f054a178c0ce8105c5da6a8325588b83f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:23 +0000

    proof appearance bank breathe track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import platform
-def get_python_version():
-        return platform.python_version()
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
