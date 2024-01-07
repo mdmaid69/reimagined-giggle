6c300323fe6d188aef1b398f0742987c5f77adf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:19:27 +0000

    know naturally distant careful crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def sort_list(lst):
-        return sorted(lst)
+import os
+print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
