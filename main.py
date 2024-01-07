2697157f9642b97f72febc8a21143d1c83d3371e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:32 +0000

    available dear switch purpose emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
