30e9d11938fe2768c1c023bfcde5d198ccdc6f92
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:51 +0000

    pipe concerning record her target

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
