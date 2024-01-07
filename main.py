f655a836dbe571d2ba0ef9e1a28270e32612d429
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:11:07 +0000

    juice Pp shall shadow crash

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def count_words(sentence):
+        return len(sentence.split())
