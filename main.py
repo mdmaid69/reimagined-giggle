733477d12d72f7ee1a2ee3897aa87fe95ce97672
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:56:20 +0000

    teach behave job have both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
