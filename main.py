c624e146524d2f14544aa85c37c93b6dccbe7161
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:19:46 +0000

    receive door positive connected person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
