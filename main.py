e06e8648e1be8e7c8fbb7564436cc41aa0e810e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:58:57 +0000

    picture tap choose obvious hospital

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print("Hello, world!")
