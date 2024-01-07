99e573a3a1cef2f1a13362693a715a2cdb083a3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:33 +0000

    wrong human impossible talk roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_speed(distance, time):
+        return distance / time
