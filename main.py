ac330bf77d65e8437585c9a58acef274cc20d1ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:39 +0000

    doubt interview today distant can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
