53a22d79035c40b61908f20509ab425636793994
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:11:20 +0000

    second pool software ought various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
