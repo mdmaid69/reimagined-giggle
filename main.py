f5ebe09844307f992a56e9d3be240e32ff7e922e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:48:17 +0000

    argument impossible permission easy plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
