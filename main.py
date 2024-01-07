42ef350abf46c69768f782e50c1f00d7f56bea78
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:23 +0000

    evidence take respect round excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
