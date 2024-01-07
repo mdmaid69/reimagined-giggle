cbabd60549c42257ba7b7e1e38dc342821abaf95
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:17:58 +0000

    ignore pleased steal history lie

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
