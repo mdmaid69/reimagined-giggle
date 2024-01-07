110d25ca73abcb035a205e12ae3248e8c1c31747
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:36 +0000

    control mainly about invite confident

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
