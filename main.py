6058e3d431e7b343216b7314a442285c6121525c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:44 +0000

    part war mood tap look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def find_min(numbers):
+        return min(numbers)
