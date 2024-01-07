83d3533346147c1bfd09bd6af6ed0f99ae94ef0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:44 +0000

    quit star though opponent failure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
