12eac8d1dc3a37fd9e29450fffd1fc19c565b429
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:50:18 +0000

    use single word feed favourite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
