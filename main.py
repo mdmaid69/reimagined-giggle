018de100fe530f56d581c8354e030cbfbcb981b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:27:15 +0000

    rate port popular thinkof bag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
