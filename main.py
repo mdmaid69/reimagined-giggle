dafc3f812a70ae249572bebcc8cc7d8289cd5fd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:36:17 +0000

    yourself age left stop yesterday

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_max(numbers):
+        return max(numbers)
