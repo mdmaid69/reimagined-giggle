1c493562934c8e33c36c0c962d9fa2391133d049
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:30:34 +0000

    last protection future drink do

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
