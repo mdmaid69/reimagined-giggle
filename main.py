d49917786eefd50c08428f912afbcb254b40756f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:00 +0000

    grand listenfor blind movement body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import os
-print(os.getcwd())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import getpass
+def get_username():
+        return getpass.getuser()
