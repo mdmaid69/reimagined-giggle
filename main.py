59698cbf60b0bb5be23d9b59621a417959b8e45d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:41 +0000

    relax prevent pure yet deliberate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
