38c2797c2d0539879553d63853dfcdfca4bbe2d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:15 +0000

    relation sport if failure deliver

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
