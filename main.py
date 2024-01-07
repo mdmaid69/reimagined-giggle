b7c1be860a5f860f3f7f107f0ac26abba05829ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:01:09 +0000

    mainly Vv proud measure risk

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
