322e786e6d087fc8d6696fc2b9ee1bd69583205d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:49:28 +0000

    check direct position ask present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
