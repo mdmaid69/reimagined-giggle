7891afb854a0e4a41c8625ccaee4108e533879e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:33 +0000

    test worst second exchange list

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+print(sum(range(10)))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
