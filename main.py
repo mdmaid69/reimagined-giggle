69fa1faa92c4420e5282f3be13b54078381d3c3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:06:57 +0000

    produce repair point fierce centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
