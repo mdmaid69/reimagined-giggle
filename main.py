1434eb4e5bd709d539ac4e0c2fe5096e2ee5171f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:21:10 +0000

    tired progress light pride arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
