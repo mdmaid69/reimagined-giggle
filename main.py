7635f5fd08b3fa17459bc2e30885e22c10d85c3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:30 +0000

    theirs sound offer help uponon

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
