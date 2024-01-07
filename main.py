4cc52b413d73bb76047b915a433bc1b869c22a95
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:38:36 +0000

    likely uncertain suddenly in creature

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
