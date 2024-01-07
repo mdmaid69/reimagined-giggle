5b730362d07adbee53701e405a1dad190978bb27
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:03:23 +0000

    write me dare plate thorough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
