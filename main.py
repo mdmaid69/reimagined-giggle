6c17dc6a439fe70674ef847625f9a60acef54ff1
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:36 +0000

    breakout upset concentration sad sport

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+name = "Python"
+print("Hello,", name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
