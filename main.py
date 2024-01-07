a26e7ef6c663f8301487876da5eeb5cfcb325154
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:06:06 +0000

    winter step whisper instruction uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
