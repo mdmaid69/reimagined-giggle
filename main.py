b693051c36732850148fd971269c66d9aefd866e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:21 +0000

    switch would plain doubt replace

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
