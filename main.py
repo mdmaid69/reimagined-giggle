9a91ecd011a8602ae64db0739949e1ce93ee60ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:05 +0000

    please flat thumb sound exactly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
