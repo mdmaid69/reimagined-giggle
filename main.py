b7b4ce1a6e7295f0809d1964cac3f1a33414fa5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:59 +0000

    bet fault expensive point increase

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
