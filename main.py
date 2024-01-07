576413a7d28a9eecc0e0fc812725ea0e85104c2d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:53:59 +0000

    march time put he brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-for i in range(5):
-        print(i)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
