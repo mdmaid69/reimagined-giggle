f7a8f77d7d0c89d58b21e7eae316115369e75457
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:15:33 +0000

    naturally colour calm mend but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def multiply_numbers(x, y):
-        return x * y
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
