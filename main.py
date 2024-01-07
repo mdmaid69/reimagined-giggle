5de3dde34fe9d5885f1ba9aba83e130cb96f8a99
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:23 +0000

    light separate claim getoff tune

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
