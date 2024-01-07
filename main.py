d1ef5ed1bfdaa2804288d4b0c311339118379048
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:40:43 +0000

    try warn offer cause kick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
