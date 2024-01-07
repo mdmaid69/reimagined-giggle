53c3c4a0e52d1bccb339e50652ebea0b986333b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:21:29 +0000

    better body plan 566end create

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
