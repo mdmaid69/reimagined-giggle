57d0f3ffb8d0f6d9c532df452b9af77827a966d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:06 +0000

    back production nothing scheme row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
