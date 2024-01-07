e5110bfb7bc368163fac551d6cb83ab51cc9ae59
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:51:51 +0000

    hot far fact sad use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
