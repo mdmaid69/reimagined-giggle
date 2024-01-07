0c54322f814733a8ac39940e21860e4fd6c8207a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:20 +0000

    thinkover repeat pump advice check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
