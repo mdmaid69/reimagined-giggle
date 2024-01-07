aed7b83eb5a5569e5f6657de9b8131756e9ced25
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:38 +0000

    as maybe generous aware game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
