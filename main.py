04e43cb96c3b8036a79985cf788ace6b04359196
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:43 +0000

    trust danger trick risk act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
