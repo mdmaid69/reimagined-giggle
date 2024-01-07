4c390c6fbf6546e11d66b38efc19b55c18a875c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:50 +0000

    announce fear necessary weak blame

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
