d0ea1b3f39f5d4ae775bf638cf2428d081ced97d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:16 +0000

    touch runover low straight treat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
