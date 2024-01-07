8e337e8b3d43ac3107599abb92d6cb870c8b292c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:49 +0000

    Itself art fall along recovery

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
