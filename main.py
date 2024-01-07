5302cbc195c6fed28e4d3efaa65f82b6a439ff46
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:41 +0000

    breakup slip whole drink chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
