ff6cb7fee5a11f36e0e0e0001d1c92bf583056ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:19:29 +0000

    Ww relax importance married shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_directory_name(path):
+        return os.path.dirname(path)
