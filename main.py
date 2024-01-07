8c44adb2d1893974345df33b1ce1e337ad04cf5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:33 +0000

    snow holiday pull thumb no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
