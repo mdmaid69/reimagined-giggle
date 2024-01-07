3223fc725b890f0f6b136ef239484411622af9cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:46:25 +0000

    they dream beat middle far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
