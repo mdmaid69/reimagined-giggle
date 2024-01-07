ce7ffdd92d241f120d4d4bdae1ea7b1da42b9699
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:19 +0000

    description turnon Hh even brush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
