7613ca2eb8f1d9e7c859e8e458eb0e1ed91dde88
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:07 +0000

    rub giveaway personal move centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
