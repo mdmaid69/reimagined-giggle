e47ffb75e2c1009be487996631c3e82006dbb95d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:29:36 +0000

    head runaway means form line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def subtract_numbers(x, y):
+        return x - y
