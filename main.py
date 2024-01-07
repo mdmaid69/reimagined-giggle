38512445970f42ccf97766446b681e23b97dad7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:42 +0000

    giveaway she otherwise remark disapprove

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
