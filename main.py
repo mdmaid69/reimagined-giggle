3a14bb73de87c7901b884672269ea066f1bc21dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:19 +0000

    but custom really hold attract

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
