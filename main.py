8d36a5adfb36d6fea3127a31cb6f726d3960e093
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:17 +0000

    spin gather release maybe sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import re
+print(re.match("h.*o", "hello world"))
