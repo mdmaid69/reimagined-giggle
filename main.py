7bf5e01b6119934a5bb517b5377dba72ea6bcfa1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:28:51 +0000

    complete lead house apply ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
