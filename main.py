7770be7f0cc8163d83e92ac43a265857426aecb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:04 +0000

    whoever forever mass favourite so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def clear_array(array):
+        array *= 0
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
