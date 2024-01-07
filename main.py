4373ebb3ff6a9e845ee477b0d631c0aa77dd0a77
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:51 +0000

    lay on organize skirt operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
