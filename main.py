0c65df0b80252302deca6f33267b785ec40e04ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:30:35 +0000

    spoil shine declaration hard from

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
