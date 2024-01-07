bf917999fd75b1061001a1e1963aadc3cfb10e4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:35 +0000

    imagine fire owe emphasize sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-print("Hello, world!")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
