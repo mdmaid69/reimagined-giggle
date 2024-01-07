d848bd95fa4c3c5ac40ae1184396362b809a2e89
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:20 +0000

    love phone sound ticket much

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_exponential(x):
+        return math.exp(x)
