5c101919c8008a37b8d8012a637f40e4068c9164
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:07 +0000

    tape tread smart lock ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
