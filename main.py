191d23f8d39e734f487fbbecd09ce3947112f72a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:12 +0000

    prize base worry weigh spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
