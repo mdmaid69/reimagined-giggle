d89d6e5e2ef6dc31d15b5fd384e2395f55e941cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:04 +0000

    soft which together convenient Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
