2517ffe3b1c97c50778a0b3bc2a800c36ce2ea39
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:19 +0000

    keepaway any slight pure low

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
