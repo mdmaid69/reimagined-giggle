dc94c23777800b4968ec874c7f85d3ca45b15b73
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:13:18 +0000

    material see fat movement achievement

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def cube_number(x):
+  def cube_number(x):
         return x**3
-  def square_number(x):
-        return x**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
