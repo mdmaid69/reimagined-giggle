fb0864e8bf6ba0308849410c157f0358b8553940
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:09:28 +0000

    trial sell level see round

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import getpass
-def get_username():
-        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def add_numbers(a, b):
+        return a + b
