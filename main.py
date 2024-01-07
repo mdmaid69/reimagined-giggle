57d6324022d7dcf39b22902836267c2358e60760
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:45 +0000

    dear demand distance likely collect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
