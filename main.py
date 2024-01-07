85b71a1c0f3da6d72ffc02ab1f57bcf944567f4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:59 +0000

    pump miss provide university trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
