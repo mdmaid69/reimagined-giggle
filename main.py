ee4f2a14d2322450b6306855bf6cdf74e71f5206
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:14 +0000

    sting piece record solve none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def divide_numbers(x, y):
-        return x / y
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
