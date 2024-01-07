1aefef20c7bfd1c01f71896952f86a03b766fa18
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:17 +0000

    regular persuade sign square relief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
