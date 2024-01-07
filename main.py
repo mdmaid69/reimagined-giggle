d66a1a29054205f11c07d8bc0fdc68e88dea0dca
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:12 +0000

    fire crash recognize trial pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
