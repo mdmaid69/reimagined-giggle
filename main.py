022123701430154f767757e8df263b320e2e19fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:52 +0000

    but whistle space insist accordingto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
