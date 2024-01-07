8e3518d78fc87acf102225ff8cfe03c24182993a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:20 +0000

    myself too art to trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
