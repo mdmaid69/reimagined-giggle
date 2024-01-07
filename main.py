877be8397ef5c02bba6339b344a646607510a215
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:28:03 +0000

    tear convincing responsible figure bye

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_pressure(force, area):
+        return force / area
