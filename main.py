fd3e6a5f86cb9923fa8a7dafede636948e72e212
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:17:29 +0000

    other bread coffee comfort to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
