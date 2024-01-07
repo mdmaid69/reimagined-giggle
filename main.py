34c5c50a98a7aa75d5c76d039c0b7ee5d64c604d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:15:05 +0000

    holdout organize less recent name

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
