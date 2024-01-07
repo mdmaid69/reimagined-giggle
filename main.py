5f2363dcde9a58129f9e5274292fae1f41cc00ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:07 +0000

    survive mean probably surprise switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
+import array
+def get_array_as_float(array):
+        return float(array[0])
