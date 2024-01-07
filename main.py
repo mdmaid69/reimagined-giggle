6e0e10db56fccc2ed3373b8ed01263b675bb7f53
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:03:15 +0000

    Pp whole cause gate achievement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
