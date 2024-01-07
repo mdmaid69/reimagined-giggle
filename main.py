e8ec45cc5fb62f030be224da5f00407bd2226a96
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:40:12 +0000

    learn scrap intend progress sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_list_from_array(array):
+        return array.tolist()
