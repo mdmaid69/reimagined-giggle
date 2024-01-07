e8155afb85ccf624843520f8886d553d681ef9d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:30 +0000

    character better trouble freedom invitation

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
+print("Hello, world!")
