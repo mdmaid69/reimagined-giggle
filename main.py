dea80681afd76c0f3f4b98a157a8f61f6434adbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:58 +0000

    face many trousers rough region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
