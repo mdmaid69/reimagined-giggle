1ada1881d9c33076b5eaeca8ce8a4001aea0ebb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:55 +0000

    floor in tread cheap fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
