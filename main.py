16f2c22c749b02cdf4d5a5004dcbac60d9ebf29e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:14 +0000

    direct content hope light gas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
