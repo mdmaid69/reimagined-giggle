f126fce09442cbb67d753e7419e52f2c44de540d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:45 +0000

    movement Nn advise effect through

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
