c444844fcea022109f9bc3db7bc63a3d1d3c345f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:11 +0000

    permission watch fairly active slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
