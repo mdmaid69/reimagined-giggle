cc825886ecfa5615d5aabd2be28ee74fb11ef480
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:07 +0000

    drink show firm circle trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_area(radius):
+        return 3.14 * radius * radius
