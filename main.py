a80f750bde1cb28bf658eeb59fddca43cc2a967a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:06:41 +0000

    reduce correct pressure solve exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import sqlite3
   def connect_to_database(db_name):
         return sqlite3.connect(db_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
