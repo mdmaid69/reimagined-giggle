d5ec1dc19a670ee0df618133a438a4bacc41ed2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:48:45 +0000

    edge slice deliberately frame me

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
