3c95b1fdae4aa95cffc019862b7a45065e0e1aaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:06 +0000

    ready wish tomorrow substance square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_speed(distance, time):
+        return distance / time
