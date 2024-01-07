01737fa04103bb00d2934ba1f7ca05349e843fc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:58:49 +0000

    mad impossible professional genuine proof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
