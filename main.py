79c554172dd4716d1e759dbe6e5f7410598530e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:20:05 +0000

    trick again dress than famous

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import math
-print(math.pi)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
