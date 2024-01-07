5cc6871f0864c820b6072c6073b16678bcb6c371
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:22:20 +0000

    serious neat rest wonder disapprove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_set(array):
+        return set(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
