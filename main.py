2e3bf2b1ba2f4414c4b3034bc592a955587092fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:15:17 +0000

    accidentally mixture prepare modern hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
