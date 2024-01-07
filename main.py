b94e6702f7e17e48989a996f3bdcf9d0d6b05354
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:54 +0000

    connected distant takeaway unlikely free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
