73aee39b6fce2b8010dad832669b4da3c1c366bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:00:03 +0000

    gas choice pleasant group container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
