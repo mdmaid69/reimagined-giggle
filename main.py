e06f37feb3c4b5cd799a1768b27574ff982183d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:22 +0000

    young between affect breakout interesting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
