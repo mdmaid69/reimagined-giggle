67fd34de8b573f200ea0064a94731e62509d275e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:34:31 +0000

    any where reply pleasure profit

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
