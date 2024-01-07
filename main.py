ae93addc9e59ade279ddc1fc7fddf0bac22f1cbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:59:23 +0000

    raw anywhere wrong world come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
