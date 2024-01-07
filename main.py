9b2e658fde9557b761daf901345b71dccbb78023
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:48 +0000

    container future watch fierce organize

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
