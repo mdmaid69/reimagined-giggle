2ca981668ccb03de8406e4243bb25b4c9067b22f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:16 +0000

    pass clean fine culture healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def convert_to_binary(n):
+        return bin(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
