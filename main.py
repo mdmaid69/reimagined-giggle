5beb1b3ef3c1415132aec8d99c3c3dbe1c43b962
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:03:26 +0000

    seriously threaten pattern fairly 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
