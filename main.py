8b7dc0334f87594e22369e3916b64c6062f7abc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:26:15 +0000

    none shoulder exhibition score thumb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
