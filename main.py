a981681cf8c655327ea751ac999713694f37d563
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:31 +0000

    turndown create cap plain Qq

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
