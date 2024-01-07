1b0209d1871f4016ab5365a689dafaec99f5cc75
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:45 +0000

    second lead flow strength want

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+for i in range(5):
+        print(i)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
