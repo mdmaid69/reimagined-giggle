59be4b8266fac60c007025555eaf7a1b927bd2da
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:17:57 +0000

    connect effort only cheat escape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
