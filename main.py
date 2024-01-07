d77893e363884f4b74e64567ad50f3610d545ede
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:19:07 +0000

    worse search disappear order coffee

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
