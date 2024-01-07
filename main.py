de8a50929122857674f00ed20d514d824af016e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:58 +0000

    exercise succeed apparently Rr raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_counter():
+        return collections.Counter()
