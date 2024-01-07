42d51d480f3eb67eb79f46ee3bfff2aa3bc9e04c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:02 +0000

    fortunate set style bringforward land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
