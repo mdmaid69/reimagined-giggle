fbdfa3e4a3fd63739fabd17159b50da7701d0522
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:36 +0000

    shame embarrassing out rubbish I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
