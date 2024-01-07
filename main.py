d0ea751d89240892b15bff842f9b9c3586878676
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:50:54 +0000

    high wait unkind carry chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
