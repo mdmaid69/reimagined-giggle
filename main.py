9007e115cf681bc6e89f9025b3e3d5726835be92
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:53 +0000

    mean authority something arrangement spin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
