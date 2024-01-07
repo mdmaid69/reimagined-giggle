d40d2618104cdcf69fbab503da612fb300459794
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:16:16 +0000

    refuse food fear cap organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def convert_to_binary(n):
-        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
