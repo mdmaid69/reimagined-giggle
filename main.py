74c598895fffe003c39c8b147e149a23f90a7625
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:08:23 +0000

    brown breakdown body collection public

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
