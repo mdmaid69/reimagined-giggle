a15e8a3013ddac24908de04eb09580813d35dbbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:11:52 +0000

    room declare tongue score whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
