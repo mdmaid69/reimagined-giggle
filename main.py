c02ad2751c1e75156910fcf03c80238286059fbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:36 +0000

    acceptable bringback difficult run tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
