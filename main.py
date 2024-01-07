f7dc1bff35d41130e0c7ddd24d378a9baeb47a90
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:32 +0000

    past public prepare top area

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def multiply_numbers(x, y):
+        return x * y
