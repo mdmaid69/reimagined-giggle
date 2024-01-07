929da83cffb13b3f7a923feeba7687ba54761263
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:11 +0000

    century rare his history cut

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
