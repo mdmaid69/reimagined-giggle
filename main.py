a7fa66cd86376f425498440e59e590c780b7bdf3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:17:22 +0000

    community tap Jj apologizeapologise century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
