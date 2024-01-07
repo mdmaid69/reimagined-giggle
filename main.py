afbd235a4905d1116649457df52a42ec495efe6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:06 +0000

    ocean rub regret rush above

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_odd(n):
+        return n % 2 != 0
