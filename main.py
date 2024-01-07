7b7622d41327811c74c24c37394fa9e3a8b5d67d
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:30 +0000

    opponent interesting shock speak along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
