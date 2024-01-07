03f4e57555ca549ecdf12109b26f6d02408416b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:48:17 +0000

    workon aim hope increase swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def split_path(path):
+        return os.path.split(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
