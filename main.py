4a6fad8c3d2c6720f01c8484f5352a060e24bf81
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:30:57 +0000

    off attention interested constantly tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
