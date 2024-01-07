e46a78a68ca634e6962115b4a7ff2598a2bf8e13
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:43 +0000

    worse press weak getout regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
