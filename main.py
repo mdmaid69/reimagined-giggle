b80d2876ddb8adb6a35e734acfe1335d67e8f309
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:47 +0000

    never interview reply expect level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
