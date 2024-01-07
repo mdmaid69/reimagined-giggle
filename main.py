20d13bc5ecfbb2551b31e97bf27d029825513bc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:09:01 +0000

    clearly female soon meaning offer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
