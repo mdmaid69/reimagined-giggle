10b8923f384392bc395bac714d1678b78423a64d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:35:06 +0000

    defeat slide behaviour snow I

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
