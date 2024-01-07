640238e3411e569652e21f774a77413080749a5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:00 +0000

    breakthrough care shine pollution mouth

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def calculate_area_rectangle(l, w):
-        return l * w
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
