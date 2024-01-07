7361e3170c279f429f959e39222480d8d37104fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:47 +0000

    further plastic disturb ours taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
