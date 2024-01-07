b1630cdc92110fc3a2054f9bcbd158735340b248
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:25 +0000

    tread pupil spite comfort brave

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
