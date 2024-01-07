130c8ea33b295f47193bb64f358190ab333b95fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:14:02 +0000

    lead worse talk gate eager

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def reverse_list(lst):
+        return lst[::-1]
