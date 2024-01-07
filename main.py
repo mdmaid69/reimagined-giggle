91a4f128e56f5dcd8122c43254ab654a48f741db
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:03:21 +0000

    present cause pocket weak head

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
