6317059372e19aa0f8d9062bafd1b3308a84aa5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:53 +0000

    impatient getback certain now approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
