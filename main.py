461ef191272988591c754ef69cbccb4f4c8f8b6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:29:26 +0000

    pull average Dd runinto dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
