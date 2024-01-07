b70a34a1cd66107b6a682af15f6bca751752833d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:36:27 +0000

    careful place pen professional fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
