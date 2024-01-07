a2b152300e9158e7a38bea3d852d9896885e3bb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:51:12 +0000

    party yourself service greeting hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
