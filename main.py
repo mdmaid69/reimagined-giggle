1317eef9f5d1559ce44290e7502386accfd1f08a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:07:09 +0000

    office breakdown answer responsible slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
