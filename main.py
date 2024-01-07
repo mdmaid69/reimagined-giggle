085ca4bdea7c326dbbc44774ed41428b162e123b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:57:29 +0000

    label sight justice forward concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import sys
-print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
