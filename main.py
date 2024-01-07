1a82d95f58987e94ca524cca1a0d7b8d1680d455
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:20 +0000

    error responsibility invitation unfortunate yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
