4f0f63e2597cc1118a9bae774fa67611ef514d84
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:36 +0000

    tour matter friendly all bar

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
