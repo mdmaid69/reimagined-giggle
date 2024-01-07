90f661f76d905fa9302885ec5fc74f3437e86bde
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:43:24 +0000

    wet calloff justice bend damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
