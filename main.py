178064dea9ac8014b560506d9696f370373cddfc
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:50 +0000

    fight last fun bottom frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
