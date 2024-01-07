9e9b8144acae501eeeccc7e43a543cff931c929a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:49:56 +0000

    day anxious thinkover show matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
