ef0598d093ab29f569a1643539713e0b12376f59
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:26 +0000

    jump design adjective advice unfair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
