6dcb923921630aa208a38e40c5bd9d0bf65fc19e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:13 +0000

    through thinkthrough sick ear home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_int(array):
+        return int(array[0])
