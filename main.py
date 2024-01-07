014db51e95731607024d6704fc85067900324289
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:24 +0000

    foreign clean getoff narrow proud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_work(force, distance):
+        return force * distance
