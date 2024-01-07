1f023e6fbc737a6655dd0710f8bff1ffa628ce4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:26:01 +0000

    worth Jj soak wide opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
