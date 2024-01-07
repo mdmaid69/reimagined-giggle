03e6a34fdf44c544f1125c6a62bc3420aa021c18
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:58 +0000

    getaway dry doctor coat fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_file(file_name):
+        os.remove(file_name)
