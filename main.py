dd605325e734d5085478609988ef2f4521274af4
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:45 +0000

    hardly nowhere care finish trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_index(array, item):
+        return array.index(item)
