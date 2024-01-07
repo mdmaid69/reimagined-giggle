2bfba3b2a1bca7f7153306da38c5f957d3ff516a
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:36 +0000

    seriously rarely estimate final fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
