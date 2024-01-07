4b644f74327fdfb2cbf037cf4a3a9eed603e89e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:40:38 +0000

    closely hollow possibly division smell

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def is_even(n):
+        return n % 2 == 0
