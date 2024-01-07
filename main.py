b667da434672e7c005ba3186ec7274d3f7b7ad7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:21 +0000

    opposite language warn giveback tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import datetime
+def get_today_date():
+        return datetime.date.today()
