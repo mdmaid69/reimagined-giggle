a1f1db4ea70fc5449dd0a586737822eb94891808
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:58:26 +0000

    government score direct computer phone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
