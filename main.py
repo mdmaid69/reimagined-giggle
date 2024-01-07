f1d84f58cc61bada813a9881fd59ae48db4bf5a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:47:30 +0000

    fork high trade loud central

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
