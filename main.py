b219743983b4148f877a6a17e875fe2913b947b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:51 +0000

    out professional choice chin crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
