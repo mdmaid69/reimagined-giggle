dfca148765d6eb3f039471a0ce1fa9574b0f55af
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:03:26 +0000

    clothes flood weigh apart hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import sys
-def exit_program():
-        sys.exit()
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_floor(x):
+        return math.floor(x)
