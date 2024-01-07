ddd04be283b2e477daa1757d5a47e4edb4d19c3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:33 +0000

    neither 569enjoy slice born choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
