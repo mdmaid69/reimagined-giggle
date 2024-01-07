97ab917dbbecd9e2369ac6c80519c1eca15a6e34
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:08:19 +0000

    even neat you snow underneath

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+print(json.dumps({"name": "John", "age": 30}))
