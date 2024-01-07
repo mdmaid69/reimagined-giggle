2bad1d3669ab45c6fe29aeca8134474341ba7008
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:10 +0000

    search translate make outside fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
