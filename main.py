6f1ff8e2b269fa43b0a37b9dd42fdf13e2dbff34
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:42 +0000

    influence used alive woman accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
