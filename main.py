24bf4a582308e6e72d0f2db7cacd737a9a26bf3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:12:38 +0000

    bored accident since reliable rich

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_density(mass, volume):
+        return mass / volume
