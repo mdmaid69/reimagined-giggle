7c681957cff8bab12af1913dc757f637dfbb716b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:52 +0000

    win dull tend dream which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
