7ad2dff92da7abfd67c33de106eef5cdd61ad39b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:33 +0000

    worry pool herself lunch somewhere

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def add_numbers(x, y):
-        return x + y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
