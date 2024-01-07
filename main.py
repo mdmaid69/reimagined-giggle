e13894b9091a5fbbee667cc537c9e3102922a918
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:14:37 +0000

    boat ready minority usual bunch

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_work(force, distance):
-        return force * distance
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def convert_to_hex(n):
+        return hex(n)
