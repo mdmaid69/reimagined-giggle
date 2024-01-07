a2abb61ff48ff1b74dde95e595fc5d84ec6cd514
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:51 +0000

    where form moon public than

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
