7e33f79febfe66c5c00a3f0337c9acac9ace8aa7
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:55:44 +0000

    somehow excuse neat fetch 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import os
+print(os.getcwd())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
