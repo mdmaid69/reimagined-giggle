41dacee88d00507a3299004260fbdc9e70e33f92
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:13 +0000

    possible action fine suppose bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
