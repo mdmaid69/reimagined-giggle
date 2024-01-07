8c4a4bb041beca556fe911e2b6cd9d6e01e16335
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:09:54 +0000

    declaration translate home best I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
