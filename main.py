320d8c38b37c45bfdd7be01c694cec09ba952126
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:05 +0000

    offend risk raise emotion responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_min(numbers):
+        return min(numbers)
