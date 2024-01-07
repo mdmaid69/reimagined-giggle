897182313614214a63ea481214fbf258154ed08f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:29 +0000

    day closely Tt order healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
