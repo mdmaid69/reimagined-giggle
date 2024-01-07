bb6eea6fd0c0c375028b8c38da8ab49573d26a0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:30 +0000

    light shut exploration score congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
