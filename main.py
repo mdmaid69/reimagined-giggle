14dee092a3112d38c0474577f95fa608eb4ef505
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:05 +0000

    tooth will black why special

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
