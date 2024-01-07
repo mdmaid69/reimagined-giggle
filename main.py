9216422ef660c9fbc8e80c6b713e56471e510df9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:35 +0000

    worth shape far touch justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
