e8c4d1891196d171103968a2a61dd78688b9a0c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:19:32 +0000

    attention where read scrap date

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
