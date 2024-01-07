2907c640eafd7124858ae207cc879054420708bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:46 +0000

    reasonable precise basic respect silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
