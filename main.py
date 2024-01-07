ce764647ad3f7155e11397aceafe4a68edc85872
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:28:13 +0000

    transport method consult tap suggestion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_as_list(array):
+        return list(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
