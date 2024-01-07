2b327e54c676677a0b0d1045fe88b56de4865f29
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:48:36 +0000

    cum give common takeback closely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_acceleration(speed, time):
+        return speed / time
