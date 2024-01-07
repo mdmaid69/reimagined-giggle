ca76f1e92bfba31b5d95b28163f63e83724000e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:04:38 +0000

    load interest cross switch form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
