75317bff68a7eff3bf244f026b58cf1d52ec7072
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:12:45 +0000

    share satisfactory attitude quit calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
