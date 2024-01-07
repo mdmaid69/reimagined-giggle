711f16a159327c1ffafe5eb0ab5f76695043deae
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:33:50 +0000

    peaceful from fly variety division

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
