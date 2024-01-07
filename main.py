bc36f91042eb8c9b796d015e1bccab0459497072
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:38:04 +0000

    sink cut average love lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
