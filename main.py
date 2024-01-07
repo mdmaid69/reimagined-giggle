2ab99c3367b37b0983ae65cf174f4b99bc096937
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:56:30 +0000

    switch pot stir obtain borrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
