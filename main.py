7e5daa2b310b99d49a948f805274f2dad7c52f9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:36:00 +0000

    speech wide schedule discover scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
