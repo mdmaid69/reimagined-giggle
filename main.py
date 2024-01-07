13eed5d9278375e0877ebe84dd22e7c6576c082e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:03 +0000

    pattern meal surprising hurry light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
