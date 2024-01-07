27f3168521aa6f8f373f5babff00d5668d975d89
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:51:24 +0000

    even advantage Cc without principle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_sine(x):
+        return math.sin(x)
