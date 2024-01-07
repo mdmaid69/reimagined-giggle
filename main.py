176779ada78669c969c5f670684e719c702852e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:56:27 +0000

    entry getaway 570enjoyable fit fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
