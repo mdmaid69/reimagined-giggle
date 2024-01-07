65b48821ace34c72ef0f4c22a5c58bdcf6abc2a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:16 +0000

    complete brief scale curious fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
