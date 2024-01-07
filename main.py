d34a01a4c4b7220d7f8e384559055d19af4ec936
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:30 +0000

    journey heat from practice noone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def is_odd(n):
+        return n % 2 != 0
+def reverse_string(s):
+        return s[::-1]
