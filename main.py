3506c202cf3d741dd57142699330780cfa0b1c7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:53 +0000

    square flow Gg fire turnout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
