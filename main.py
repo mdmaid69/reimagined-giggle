5ea3107c3f16ab383d51e4d97f72b7b05cf4ae06
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:16:32 +0000

    desperate roll degree solve performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
