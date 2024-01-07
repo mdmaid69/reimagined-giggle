fd64719c66cd7d9480ac87323d87ecd2c69cc2f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:57 +0000

    body see tight strong luck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
