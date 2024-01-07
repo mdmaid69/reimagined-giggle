531ed132beaec761688669875901be5b0c5e0ccb
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:41:53 +0000

    pot clothes giveout loss square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
