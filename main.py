3bac64861ecaa56abcd9ccd85c9aabce1e2e97a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:05:16 +0000

    decision politics us lead cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
