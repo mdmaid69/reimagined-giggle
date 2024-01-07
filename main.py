431e968ce5e97f17adb7c26c47ec12b40a779dc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:53:44 +0000

    jump here admire deliberately warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
