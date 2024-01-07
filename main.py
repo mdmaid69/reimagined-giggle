9550973aeebb64ad408c1631db5f1a3307a9037a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:57:00 +0000

    never ours ticket rush address

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
