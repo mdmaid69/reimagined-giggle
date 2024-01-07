455a4df8d81cf1ad7ab73c54f1bffb792e5e5a94
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:46:51 +0000

    weekend unit unit medium distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_work(force, distance):
-        return force * distance
+def get_array_length(array):
+        return len(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
