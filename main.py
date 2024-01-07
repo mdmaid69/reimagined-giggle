ea3e01d01b9e6c1a8b6eb476256afaee273e5710
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:36 +0000

    regret own plenty alive explain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
