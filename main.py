df973a4bc13a6381a557cc0ce42007466adc66ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:50 +0000

    trade comeback piece lookafter hour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
