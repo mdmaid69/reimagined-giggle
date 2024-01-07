5718925c3065777b126a07fe287f197404e27aa3
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:28 +0000

    bound finger thinkover downstairs part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def convert_to_hex(n):
+        return hex(n)
