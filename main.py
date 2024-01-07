4d3c11780436fa380f89eb028b602542d03e5e23
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:10 +0000

    polite save active post snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
