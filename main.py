e9d188848b4d3221731bf4a8b515f55a1b9f0d6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:52:55 +0000

    pause married formal main about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
