5a347121ab51350a8ce98e3d23662d445182fb6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:22:49 +0000

    politics hello exhibition respect connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
