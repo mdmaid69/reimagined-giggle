6968134c6d6800b97d04153ce450e2902be85b90
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:10:29 +0000

    man vote slow record odd

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
