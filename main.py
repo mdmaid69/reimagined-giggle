3800e5d1968937c938cadac200943aa7ecbcd201
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:16 +0000

    second spring goout go disgusting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
