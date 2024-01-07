2d9c079f257cfe409a1c210c973b87a7e6274a2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:37:04 +0000

    unit 564encourage build worth fun

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def convert_to_octal(n):
-        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
