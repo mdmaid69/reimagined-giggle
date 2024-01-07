bbc5685fcc1311c546ab4012c4bc68c123228c0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:29 +0000

    proof spoil last jump interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
