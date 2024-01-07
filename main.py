03d3d7d3c995ce887a8512feb80dd545c9546f1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:28 +0000

    equal corner clean upstairs interesting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
