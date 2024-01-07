9313d7e6f3d1ebee07eca97333274687c7be479e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:48 +0000

    order that little rub yourself

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import os
+print(os.getcwd())
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
