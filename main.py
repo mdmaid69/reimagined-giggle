b8b56d4f8ff1629bfc01274feedbfcb0de53537a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:15 +0000

    bottom practise standard bear claim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_item(array, i):
+        return array[i]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
