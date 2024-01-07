a99b76ef874058edcc59daeea3b73ea6de2b92b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:37 +0000

    fine couple damage add distant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
