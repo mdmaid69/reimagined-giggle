6b0655fa3d4a065d0058d7bd32d405af2a274c1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:18:15 +0000

    crisis be your above brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
