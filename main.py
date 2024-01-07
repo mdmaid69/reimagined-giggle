142b87fe936a039ac8b353c907297b6b7c90cfe7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:19:02 +0000

    whenever drink head low comeup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-print(math.pi)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
