07d8f1dd52ff6edcd5fe40d0f977dd004c34a6d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:23 +0000

    change change length degree ours

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
