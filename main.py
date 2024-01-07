d7f18d5b8905720146b9ebc29f08640b7ba86956
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:09:08 +0000

    weigh chat disturb half owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
