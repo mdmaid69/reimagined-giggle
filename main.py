e37fd28375f9e75afe5c63b49dd759525bc5bcc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:24 +0000

    spill rain mail such hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
