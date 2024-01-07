21c5cd6d86951ca353261a7cfc3e781693a61f26
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:36 +0000

    ashamed adjective be promise used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
