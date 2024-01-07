29d61f4cdc5a282d35ff55f987d6195e1fe0bcef
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:25:31 +0000

    succeed boring acceptable ready creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
