b76c680897f425471a7ba974ad679cdbdb5d48db
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:23 +0000

    clear nonsense arm safe free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
