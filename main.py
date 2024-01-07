12e675e22d7a99211c2f2256b038fb550e64184c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:55 +0000

    evening 565end sink whoever occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
