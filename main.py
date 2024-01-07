782a3efb81ebee1126e3459c621bb33bb1b420f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:40:01 +0000

    tape ours kind most rank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def count_characters(sentence):
+        return len(sentence)
