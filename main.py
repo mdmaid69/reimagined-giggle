5dc85905a271e84e10cf74ad51b84e6b1cf9f635
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:45:24 +0000

    genuine hope badly pleased cross

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
