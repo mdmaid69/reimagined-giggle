7f271fc7d23104fe382f4f7a983980be01582f1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:49:39 +0000

    far Mm free push chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
