fa5c2ad58f0cdb20c66583ab29e6777fd204bf88
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:27 +0000

    let visit skin advanced round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
