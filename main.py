feb046196f508669ff536df89d3beb8f19fe394b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:26:03 +0000

    slow ashamed afterwards maintain throughout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
