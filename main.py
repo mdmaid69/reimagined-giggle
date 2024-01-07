f3039d5fc1495897f61cff126782e8203d141d4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:34:43 +0000

    fall description catch characteristic ocean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
