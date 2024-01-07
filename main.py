2d3d0855043cbc975674fba28c2e83a9799509ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:34 +0000

    understand relative operate interested talkback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
