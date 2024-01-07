7b0f5a494effc2a1da13a29d66210728fcbe7520
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:25:31 +0000

    sure who difficult under why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
