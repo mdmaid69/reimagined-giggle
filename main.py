ba4d44f154bdc4d0b5cab2e375cdda7f0351cefb
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:51:32 +0000

    church passenger name object charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_working_directory():
+        return os.getcwd()
