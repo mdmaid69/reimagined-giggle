c125c21a8de588ab6842f2002a9a59d3f8a30663
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:10 +0000

    I guard match fun food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
