a40fdda5845f13071f49ca5aafee88058239d9cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:03 +0000

    hand thumb sight light computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
