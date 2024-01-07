f7064e1df750607e3cfd54024543e56eac98cac2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:38 +0000

    hide fortunately pure objection estimate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
