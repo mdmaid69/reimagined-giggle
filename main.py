a514cf041406f3d2a8687acd7d9066f93a3c94b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:52:13 +0000

    preparation mysterious live grow steady

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
