fb1e6e8d9905304f6d17a0628fc05eca2b1c7993
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:16:28 +0000

    place cry getoff connection smell

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
