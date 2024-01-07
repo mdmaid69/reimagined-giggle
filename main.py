576fdf86d070b2d4c4f03e7fe80ff1c870e52df3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:16 +0000

    invitation measure long proof similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
