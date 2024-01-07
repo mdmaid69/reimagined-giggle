4bc104c3185ad5592aa98e4e9e564e73b5dd0234
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:11 +0000

    treat few smoke software Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
+text = "Hello, world!"
+print("Characters:", len(text))
