ac368179444580583436780912c34a8229173cb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:17 +0000

    goon lie anywayanyhow dream cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
