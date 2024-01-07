9236970b9e1a61f079631f03b51a893e793f654f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:53 +0000

    wet independent pair old pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
