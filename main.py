be99ae0a1047a806995e9fc549a67d5b28617c08
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:34:43 +0000

    no possibility stop pause free

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  def remove_duplicates(lst):
+        return list(set(lst))
