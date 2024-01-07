208a5618c615a7406da0a227250f12c6f25fe34b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:41 +0000

    fairly clean accommodation copy favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
