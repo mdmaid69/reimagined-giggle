435720eeba7945a07472198151bba90c577904bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:57:16 +0000

    Ff raise runover butter Bb

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
