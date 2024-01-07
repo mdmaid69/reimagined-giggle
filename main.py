4f4b1db97a798e63303cf3b814a9f6c530ff9212
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:40 +0000

    new warn who cry traffic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_length(array):
+        return len(array)
