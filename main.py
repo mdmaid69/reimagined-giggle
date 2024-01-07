f162db5cc386a1d2dc4c9bd583b6008752d568f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:47 +0000

    tend unless contrast care air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def add_numbers(a, b):
+        return a + b
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
