24b8b30908c4aba780106617e1416b00b474fa0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:11 +0000

    short mistake eye remove count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
