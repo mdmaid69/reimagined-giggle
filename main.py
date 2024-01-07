9af90899d89517aa801b4b7709cccab303e1f95f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:25:57 +0000

    foot defend available welcome shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_index(array, item):
+        return array.index(item)
