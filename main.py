a259de782dd5a1f68619aa2e5fe340a68cc34f05
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:49 +0000

    fall search right similar copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
