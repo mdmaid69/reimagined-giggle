4d9537fb59e6c86d0918df1742300d36c0c32ab3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:43:38 +0000

    bang breakin bunch Bb team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
