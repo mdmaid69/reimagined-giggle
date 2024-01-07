522e70736dc2142a16eb7510000a73bc335129e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:12:31 +0000

    hear possibility rush field middle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
