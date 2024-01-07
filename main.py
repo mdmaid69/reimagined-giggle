2e2390777413e9893283b56c373d6719ef0e02fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:47 +0000

    cost concentration perfect summer winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
