97ad86d97ddc19e016224b43ab8321db0843668d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:56 +0000

    prepare equal could please stupid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
