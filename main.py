997602bf80c4fd589f9357ba8f45f4bb66ea1b6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:23:30 +0000

    breathe quick over exhibition ought

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def add_numbers(x, y):
-        return x + y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
