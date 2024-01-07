c036c719864003883ed000b847da3d5f727adeae
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:39:28 +0000

    tired convinced responsibility sail past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_list(array):
-        return list(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
