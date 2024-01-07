af64741ba7d6b2b3d9d922f43d39afaef8b8d8c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:40:56 +0000

    wait money bin legal visit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
