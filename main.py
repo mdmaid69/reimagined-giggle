752f621e546aafb7ffed9c5dc9353e2a077c0bc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:47 +0000

    if situation need geton keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_elements(lst):
+        return len(lst)
