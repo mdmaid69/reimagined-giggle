359bb69416d4fdc368a7d10919830f444b7017c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:55:05 +0000

    uncomfortable base change Rr wonder

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def convert_to_hex(n):
+        return hex(n)
