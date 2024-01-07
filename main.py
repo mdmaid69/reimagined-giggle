070a104b572b8b9751a5fec711aba10977b07cc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:47 +0000

    force ignore breakin imagination test

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Hello, world!")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
