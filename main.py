ce4b5c6ca2670ae35ed1c4d38ccf8b89c14f7417
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:00:55 +0000

    fly goback(to) Vv solve general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
