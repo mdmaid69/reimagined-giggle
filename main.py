bfafa96582489e32fbd52dbcd2e6e73054b590b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:34:54 +0000

    accident remain kind draw peaceful

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_acceleration(speed, time):
+        return speed / time
