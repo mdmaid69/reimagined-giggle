2f0b63e1470d36afd331d22b7c1d822dc359fb85
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:12:39 +0000

    hate naturally bang refer his

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_rectangle(l, w):
+        return l * w
