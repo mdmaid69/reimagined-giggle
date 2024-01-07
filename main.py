b205c1fe8ec0ef03df8271adf2e332f72b68493e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:29:42 +0000

    high attractive operate brush compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
