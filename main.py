cee7ad546cb5ce8d5c1112286ac31fc03fc42762
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:38:46 +0000

    break phone pour computer mad

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
