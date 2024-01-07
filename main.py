ed42e1f91a894d3305c29f4693d7b0b6ab6e38a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:01 +0000

    clean wander perform stay beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
