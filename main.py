e2d61d1e778764adb4ed621a14caaaeeef264193
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:02 +0000

    disgusting above money keen door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
