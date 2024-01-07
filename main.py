f209f926fead58ccc4f7496e45362c0cea2c0ea9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:45 +0000

    offer cinema pencil bottom rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
