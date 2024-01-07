7efb8827bfde5fa2150dc56745bee9d8a4d7fc18
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:39 +0000

    achieve city listenfor young careless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import sys
-  def get_python_version():
-        return sys.version
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
