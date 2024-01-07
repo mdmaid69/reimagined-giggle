9eeddfa6fede6b473f29ef0bee79a44630c110c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:58 +0000

    wake defend copy dish foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
