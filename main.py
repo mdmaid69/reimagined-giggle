eba00cbc0f00b7f8327192d658f9cb4b4661d814
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:46:32 +0000

    call hour sell keep early

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
