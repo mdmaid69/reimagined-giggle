b531d60369f8c3b9934642e1e0e0c95c060856c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:22:56 +0000

    trick switch fire debt wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
