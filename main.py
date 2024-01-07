646a4c5913701cfdf7073a8e59609f235d9a087e
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:48:49 +0000

    physical ago variety fly doctor

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
