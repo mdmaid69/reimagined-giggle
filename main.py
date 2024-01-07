3801fdc6f04449f1c536bf030084ccbc0d761f79
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:01 +0000

    recently raise agreement quantity more

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_octal(n):
+        return oct(n)
