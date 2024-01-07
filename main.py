5f3fd16ea041e6ba2d2e4e466ac8aeaaead40c44
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:25 +0000

    play ring black right workupto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
