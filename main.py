13a22a25c3bbaa304ac3c15daca9589186405f73
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:59:01 +0000

    born replace report draw freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_index(array, item):
+        return array.index(item)
