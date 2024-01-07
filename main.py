a3b9520fbfd079d44fcc64a1b6f9c631f7de9b74
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:50:26 +0000

    shrink history careless release rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
