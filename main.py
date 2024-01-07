fad85366e55c1d8ba2a8364fb029417e6f4e964c
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:08:21 +0000

    record destroy leave very star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
