f9cd566748d3d50894737377e1fd09cd4e21cc26
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:31 +0000

    wide march without bin used

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+print(time.time())
+def convert_to_hex(n):
+        return hex(n)
