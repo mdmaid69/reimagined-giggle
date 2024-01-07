df5b0425c0ea9533401acb28883edd31d56a71b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:09:38 +0000

    Jj luck performance Bb brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
