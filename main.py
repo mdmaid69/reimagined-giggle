feb9c6107f80c5d1ea0747a85816f2c9f1b72004
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:16 +0000

    fresh luck team gentle office

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print("Hello, world!")
