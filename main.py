2751b246b563f8f7c7c2bd1d2cb5d3c2a7b405c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:17 +0000

    sweet emphasis bus benefit display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
