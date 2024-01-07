2b5b311f7f9b9468ef01d2a1a799d5f32e95df11
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:11:59 +0000

    like by refuse soft swallow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
