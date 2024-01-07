121d81d676bd57c1e80658424ad6db41769fb230
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:45:36 +0000

    deliver lookback(on) ring neither food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
