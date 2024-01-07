24f4fd68334ad8b2b913f11292f36e27102e6118
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:11 +0000

    future pronunciation way ago pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
