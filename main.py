fecda36bbb542673a410cb9479b295957c8dad10
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:11 +0000

    completely aim comeround actually ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
