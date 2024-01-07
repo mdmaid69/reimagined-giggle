4b93fe1595372a8c982cdfc2489eb7cb7c89623f
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:01:06 +0000

    pocket rarely roof worth comeup

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+from collections import Counter
+print(Counter("hello world"))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
