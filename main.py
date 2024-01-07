29a17b6a65913df22609f7bd10e251b53cae3bf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:01:55 +0000

    obvious fetch name until angry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def multiply_numbers(x, y):
+        return x * y
