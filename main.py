f4d234a0072f1b0cf7b898ead47d9209fa53233e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:45 +0000

    brush ocean smooth mark Qq

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def add_numbers(a, b):
+        return a + b
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
