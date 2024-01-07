82a4d41a3089004790a0901b15c5cae92e1ef8dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:48:50 +0000

    false space rank everywhere rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
