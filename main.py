805dfcea14f25601bd439cdd6682eb700f05d998
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:09:54 +0000

    probably minimum news warm complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
