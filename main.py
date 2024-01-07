2d88b46616d31cfe704c7054e5dd7fbc453df94e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:22:56 +0000

    suit behave steam moment hear

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
