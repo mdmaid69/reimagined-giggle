6a5e141412e200cc715db55d3320b77eaff7b7e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:15:17 +0000

    squeeze suppose order trip stay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
