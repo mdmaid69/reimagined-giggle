7fd201fb11929d36ed3b919f840ade932e426f73
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:00:21 +0000

    knock holdon tough fit remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
