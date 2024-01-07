ea5238f1cb044c46665eabd64ddb8ccefd9cfda8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:39 +0000

    noise successful lookback(on) runout marry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
