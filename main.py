eb0b49bb43b0b96414b4e888ffe7f12fc3a37f04
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:19 +0000

    coat mean store thought lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
