b9c2be4b21883117c016f9d3ea6fcb8c7bb1c9fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:39 +0000

    issue command mention leg bridge

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
