c406ba76ed42402fce9141b086dba81d551e1d52
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:59:59 +0000

    callat step shoot secret sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
