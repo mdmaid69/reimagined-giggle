97a99d05778438cb866b3c92b67d9859ca414cec
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:48 +0000

    after female pay emphasize sister

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def print_python_version():
+        return sys.version
