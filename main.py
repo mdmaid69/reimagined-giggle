f11df026191e7989bee06e02c9ae0399d44afeee
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:36:45 +0000

    dish demonstrate off one wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
