0fda1e2f03e1f6703074429bd41bca7513740502
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:48:21 +0000

    he influence rise acceptable false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
