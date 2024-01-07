86b0861614de7de34309e802a1a457f49ba9ba13
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:33:57 +0000

    mine as bye port excited

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
