e5d21ed76c9687d29cea6b83ba87dba500483d32
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:56:04 +0000

    support succeed runout fast suddenly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
