b0294c6f20f0922ace9e9d659dcdd4600dd35f48
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:49 +0000

    disagree buy arrival murder opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
