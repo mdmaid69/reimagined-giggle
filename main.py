d3dcd3213981384abda460ffdfc7ec7369208972
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:49 +0000

    mean related club dish keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
