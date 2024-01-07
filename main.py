fdfbc18578f32a39f7589e01f709464514cabffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:53 +0000

    couple seem makeinto ilovedicks dress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
