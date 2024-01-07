30a881b783b494182970cfe0a3e945aaae140cdd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:07:35 +0000

    cry several over knock fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import tensorflow as tf
+print(tf.__version__)
