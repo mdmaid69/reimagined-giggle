5b88cf7596fe176cfa777cb497fc980f9c22dac1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:04:15 +0000

    journey pride extremely home forward

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def square_number(x):
+        return x**2
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
