6cd0b7932ae29643cca811e193d7d6effdd6427b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:17:48 +0000

    breakinto genuine load bath exist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def is_odd(n):
+        return n % 2 != 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
