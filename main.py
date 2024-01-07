652fd8875d9dcc2bae9c727f0beb869c3525e638
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:21:08 +0000

    noone win programme hear past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def add_numbers(x, y):
-        return x + y
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
