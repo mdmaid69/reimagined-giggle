cb02e4a21ee7776a54aaa2819dc591d3a97437dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:46 +0000

    best project label swallow game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
