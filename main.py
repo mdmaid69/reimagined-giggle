a70c3a6581a623aa00c9c3734a574770176f524a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:25:40 +0000

    generous object refer but lose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
