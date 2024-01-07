a053e82c070ad6108a7a7a552351b02905fd24a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:52:13 +0000

    watch pull provide actually protection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(numbers):
+        return min(numbers)
