e9086e5750fc78d831f50983f34cbeccd4dca1e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:30 +0000

    television jump suckme yesterday bringforward

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
