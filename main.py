463804dff3597ca3d6d7b015490d74576ee5a042
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:57 +0000

    we understand balance discuss company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
