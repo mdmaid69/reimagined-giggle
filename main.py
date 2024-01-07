73320d4f645ef637091c8f2e1f5db7bfa5fe3f81
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:54 +0000

    area attractive space goup answer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
