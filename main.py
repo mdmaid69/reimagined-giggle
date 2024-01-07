808bebe4762bd0cb9af2624aef74ba0091c29d03
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:09 +0000

    unkind spot kind answer urgent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+def print_python_version():
+        print(sys.version)
