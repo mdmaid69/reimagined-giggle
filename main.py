39bc600b6348a15ec09d8d37773c2134bd9409ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:20:57 +0000

    phone amount blame breathe sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
