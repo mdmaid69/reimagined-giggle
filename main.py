7908cae521d2682a080867ca1f0c07104ac27147
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:59 +0000

    ignore aim whether arrive plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
