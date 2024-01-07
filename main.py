651115659e83c3eae48484359de4c92e15201bfc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:18 +0000

    character passenger matter rain skill

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def multiply_numbers(x, y):
+        return x * y
