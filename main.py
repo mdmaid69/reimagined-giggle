01a7cc2e1a44ee7bb420c204779a855cb70db6b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:46 +0000

    key interest grade their year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def convert_to_octal(n):
+        return oct(n)
