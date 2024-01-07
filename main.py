2513f4d3a1e176b5fad87f2dea87b3ccb1479426
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:52 +0000

    persuade wrong divide smile decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
