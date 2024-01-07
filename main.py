e1ddb8f170a4dc3eb0564fdcc149093b55f40708
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:10 +0000

    help entrance up treatment thin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
