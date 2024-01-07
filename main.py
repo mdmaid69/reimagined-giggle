6447c81d7186ab71323cb498e1d6d02608c0d1a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:00 +0000

    refer least still quantity treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import sys
+print(sys.version)
