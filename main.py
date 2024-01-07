c451b4c0c481e2cf7a388facc57bd403efed9e56
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:50:50 +0000

    suckme contrast bringback bet race

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
