042778f9674b891a941a4b2162169e5e2893aa63
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:33:03 +0000

    mark discover scare trick file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def calculate_area_rectangle(l, w):
+        return l * w
