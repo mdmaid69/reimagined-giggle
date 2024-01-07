09d01c19137af07e11e796ae9418fbde516ed487
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:14 +0000

    centimetre afterwards description development however

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-print(math.pi)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
