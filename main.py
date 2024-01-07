f0cb889e8d341b7ea9b04798e25826e64088ad45
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:36:11 +0000

    computer drive total charge art

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
