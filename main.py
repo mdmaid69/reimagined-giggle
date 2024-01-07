7515a88b81be0ba88e04a302bc7a0393a95bcaa8
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:23:21 +0000

    relief copy anywayanyhow satisfactory ride

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
