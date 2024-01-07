bb0a256ffcd91c88f28a8fe6e8f4ac1a28945a4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:22 +0000

    national sport chase safe buy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
