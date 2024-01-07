462725576a90b55ef48c0b0087f0a853be96f162
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:45 +0000

    sometimes hear something mark culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
