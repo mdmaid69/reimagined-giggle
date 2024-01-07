c943396bcbba58149f8a017e91083641afa381fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:40:04 +0000

    half fortunately beautiful answer basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
