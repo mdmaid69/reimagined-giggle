4b1996afa3f2435988d62ff4135699e873731fa3
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:24:17 +0000

    future repair tread broken direction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
