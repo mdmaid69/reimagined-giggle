34fc2a0343f92621de267d41228c2c3d8a31158d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:51:39 +0000

    frightened turn notice politics convinced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
