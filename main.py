8760c26868d745d14095082c9f0e703dee664791
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:14 +0000

    truth pretend far black mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
