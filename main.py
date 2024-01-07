3e60ac6462ec49f0cae02ed216f2563f7236e792
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:03 +0000

    wet factory follow thin awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
