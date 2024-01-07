1d1d31ccddc9ab91b9cf3daff62b6cda5e122986
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:31 +0000

    joke card fire position trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
