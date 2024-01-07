6a27e4de91eb4b77168654fffbdba7520abeda68
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:41 +0000

    late after anybody fat convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
