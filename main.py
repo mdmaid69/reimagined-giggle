67444f9b83646fd301a65f23aa2e63b31918edab
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:23 +0000

    certain underneath aim undo something

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print([x**2 for x in range(10)])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
