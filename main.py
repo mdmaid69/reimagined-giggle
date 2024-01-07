6995f17defbd09b61a5919f5ca34c89b393281c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:44 +0000

    turn busy interview sure pile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_min(lst):
+        return min(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
