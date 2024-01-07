b1ee30089fafd4caebc8e20abd43f72d95f9e58c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:00 +0000

    want pick thick push demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
