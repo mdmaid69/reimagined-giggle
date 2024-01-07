86ff5b88f2ea2027dd48482dedbb783d41fb9144
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:19:19 +0000

    imagine Ff damage experience indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
