0fd5591be484a9aaf396a14d09d4e4886f45696f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:29:28 +0000

    innocent tape cause mess contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
