063fa80d0435048ce358b3298bfcaba2f0620d7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:20 +0000

    makeinto priority plenty takeback shopping

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
