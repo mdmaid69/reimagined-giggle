02bc45540aac97e4267ccfa606d63784482c8e7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:58:24 +0000

    permission achievement uncomfortable quiet plenty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
