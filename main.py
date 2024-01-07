de0eff85d455a4ddd0edfdab53270ed1c92a0221
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:51 +0000

    condition week crazy she failure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def reverse_list(lst):
-        return lst[::-1]
+def find_max(lst):
+        return max(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
