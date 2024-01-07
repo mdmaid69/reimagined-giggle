42e79749c85169b3f8c811b6a5cfc744c2a836bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:18:48 +0000

    mistake Cc whenever live over

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_item_count(array, item):
+        return array.count(item)
