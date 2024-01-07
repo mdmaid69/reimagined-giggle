48ede6fc2d5f7b21b6104f56f352f15520ae193f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:06:56 +0000

    talkinto special anyone polite attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def count_elements(lst):
+        return len(lst)
