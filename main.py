d7faed9a92136dad7de665a40d31c7f4c2aa20d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:50:43 +0000

    complain could breakinto Ee anxious

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
