0ecd47c80425b305c3caca3969836aa302a43f86
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:34:52 +0000

    measure plenty pump majority apply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def remove_duplicates(lst):
+        return list(set(lst))
