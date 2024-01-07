68b44c06b01dba450b8606fae578fc37f16dc5fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:24 +0000

    lucky winter in operation used

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
