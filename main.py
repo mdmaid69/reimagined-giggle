0e2bb6d10923ff414ace5ea48d99ea163bd733e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:13 +0000

    juice surprising certain flow recently

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_length(array):
+        return len(array)
+def count_elements(lst):
+        return len(lst)
