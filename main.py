a4790b7186281353a3bba3df7d8ab9a93ec8ee0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:49:30 +0000

    makeinto trap peace ours priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
