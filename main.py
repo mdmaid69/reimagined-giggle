4cf3a5742a6da6b0e2b0782196a4164e11314e6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:05:04 +0000

    copy family floor turnoff in

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
