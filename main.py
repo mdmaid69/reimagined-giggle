8127eaf932996072780ac2079f3e85ea33b84714
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:18 +0000

    drop broad advantage pipe make

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
