cbccc137755f84301cb597d789d4ff7e7e302f4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:51:21 +0000

    error personal fire call what

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
