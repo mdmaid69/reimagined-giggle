ac12b227fea67ec958bd812e3a60f6b11cd5a34e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:50 +0000

    exercise born indeed destroy shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
