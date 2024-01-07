aed6de40543f6f45c184d136971cb10f3b04d68e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:39 +0000

    wide smell file before protection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
