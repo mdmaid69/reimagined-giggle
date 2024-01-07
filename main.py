32a2326c5307b74aa22f7b8bec314a6e9eabf624
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:53 +0000

    proof thinkover waste through frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_priority_queue():
+        return collections.deque()
