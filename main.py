e363547fa72983c89730ffe9cd2e9c1c9b37019b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:57:05 +0000

    relax unless now chat away

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import tensorflow as tf
+print(tf.__version__)
