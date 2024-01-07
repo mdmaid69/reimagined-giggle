b96bfdf397d6e62958f7289c51c19559c1352d9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:04 +0000

    brush far needle straight effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
