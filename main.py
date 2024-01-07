8c6c881eb119c515833e3b3de58dc03fd2c77ffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:02 +0000

    sort dinner getout room appeal

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_work(force, distance):
+        return force * distance
+  def sort_list(lst):
+        return sorted(lst)
