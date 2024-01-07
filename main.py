fa68a78e6139990522853d65254b1afc2aa8b8ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:35:49 +0000

    rescue comeover whoever add improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
