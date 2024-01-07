92ccb30878322bf1b01526c877419461ea82f685
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:33 +0000

    wrong across move after none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
