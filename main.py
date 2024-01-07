6b67bb4f41b660114699e3188659417630a7b675
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:44 +0000

    over children duty morning backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
