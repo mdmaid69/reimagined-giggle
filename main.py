1292320a91244c79d4cd0ae01bb4bda6dc4ca66f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:17:14 +0000

    Ff completely frighten far right

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
