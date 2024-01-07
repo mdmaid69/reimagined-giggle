1d2ea5445da577ea62005d55aac527c3e1aaec67
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:19:08 +0000

    say battery minimum true couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
