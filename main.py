03a5e8bd34fa3c64077f13e0096ad32630606a66
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:48:42 +0000

    natural law regard takeoff upwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def find_min(lst):
-        return min(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
