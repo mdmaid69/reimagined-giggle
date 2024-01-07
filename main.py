3a342443c7fb4e99d6319dbfe67b5151ed261ee0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:56 +0000

    that bad environment born well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_str(array):
+        return str(array)
