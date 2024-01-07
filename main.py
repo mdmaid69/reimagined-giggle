c296dce5d283792065ae29f811c591b8bc393c46
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:33 +0000

    young smell fresh act pot

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
