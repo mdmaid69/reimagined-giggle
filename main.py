29eecf1722b3193c18ef8c19c0666b3cc5bda23d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:03:07 +0000

    giveup drive away often excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
