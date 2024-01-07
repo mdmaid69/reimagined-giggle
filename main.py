d03233cda71c5384f3bcbecbc0b2fd7382d4a063
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:16:47 +0000

    570enjoyable church treat minor outside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
