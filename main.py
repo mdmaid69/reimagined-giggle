77406ef58ebcc5caae7dca64eaac574a01cd03f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:39:59 +0000

    any cross right extra appeal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import time
+print(time.time())
