ec4ec6d9d77063e9a8a14df01e4a61787b6ee7b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:18 +0000

    slip whatever tap string constantly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
