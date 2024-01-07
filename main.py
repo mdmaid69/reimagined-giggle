3818ff03120501b38e5b6dd5af90b0de75b2ec65
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:10:25 +0000

    comeout create getaway sale exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
