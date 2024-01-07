5254d8cd8fd882c2422b1a20ad5d32e7f620573d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:15 +0000

    serious attractive section thinkthrough wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
