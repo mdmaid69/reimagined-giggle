4461409b1b657dad3d7de513f75e5adf4008c6f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:58:58 +0000

    condition cut lean taste hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
