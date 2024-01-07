056393e6960eb87534580c0e60e858d3ffe7482e
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:54:48 +0000

    other bit really behave stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
