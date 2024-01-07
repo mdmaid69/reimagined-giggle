61cc7cc58d7e5950f3486267414eca22411f158f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:24:59 +0000

    change notice whichever murder closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
