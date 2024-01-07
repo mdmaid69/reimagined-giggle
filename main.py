686ab65e9988a5bcd0d2ec1f9261d813e56df89d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:36:43 +0000

    branch science check workon demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
