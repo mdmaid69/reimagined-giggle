fad5c064d230fb04af7a75e69fe1a54239d5262c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:09:39 +0000

    spite beginning right extra difficult

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
