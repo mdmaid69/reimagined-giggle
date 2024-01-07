19ccfaf2a71e7e470fa955ce5c30078b1a678234
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:17:19 +0000

    him paper become deep building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area(radius):
+        return 3.14 * radius * radius
