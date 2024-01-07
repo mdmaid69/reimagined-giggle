4c0396e46afaf09914c0a70ca4c60ba6d15ed6bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:04 +0000

    extremely under apply forget great

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
