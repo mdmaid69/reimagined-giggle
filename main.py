32f73d3af5d70475881a27173b67e33434951834
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:55 +0000

    slice genuine happen voice school

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
