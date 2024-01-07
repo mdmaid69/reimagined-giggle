ed2001bfa5fd5c5d1f04b9e68337909a29ce95d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:30:18 +0000

    way talk increase release several

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
