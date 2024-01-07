f58238970e18d20dddd87689f69118e95fe85578
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:10:58 +0000

    practical lookback(on) negative beautiful breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
