ae7a72b1077d2ca494a1abc5521937b3260c596a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:47:35 +0000

    read science actually quit rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
