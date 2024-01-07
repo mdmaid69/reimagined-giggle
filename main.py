33197d962dd97d3ed03f0b0edf8ce675ed1906eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:48:55 +0000

    will phone trick born full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
