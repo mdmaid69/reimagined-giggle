0743f9325ff367794154cbb92fbcd4ded40b313c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:59 +0000

    everything ability arrange weigh meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
