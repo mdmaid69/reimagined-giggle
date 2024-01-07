a332e350110cb0f6f79fab89c97029a0488f8e1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:53:15 +0000

    wild figure wide by deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
