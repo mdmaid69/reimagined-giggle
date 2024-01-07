e1b4fbc6376a8a8e481c26c32f9ea5609ef6452c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:21 +0000

    date level pour correct right

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def cube_number(x):
-        return x**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
