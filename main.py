b60dd091571ad68feec5afc20139b76dbb2bd378
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:37:42 +0000

    love soft prevent cost thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
