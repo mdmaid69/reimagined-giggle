2a3e167dd907890657bd8b3ef5b805e204f4beec
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:02:05 +0000

    afternoon sail carry write alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
