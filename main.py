8bb57167e374134c31c1bfc59e5277084cb8bdf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:23:06 +0000

    occasional furniture bear father question

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
