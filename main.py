5674b8f2803b6f83dfb2490edfd043c357aab42f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:59 +0000

    gift history brief crash reliable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def add_numbers(x, y):
-        return x + y
+import numpy as np
+print(np.array([1, 2, 3]))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
