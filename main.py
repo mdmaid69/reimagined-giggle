8171bbe4e85a8719b0fd7e9c38722e33edcfcd2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:59 +0000

    concerning cross Bb job hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_min(lst):
+        return min(lst)
