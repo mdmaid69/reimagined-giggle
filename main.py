6e797872a91f48a89e2e9cc584bd1bc2c0e29013
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:02 +0000

    door range about goaway design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
