738ecc9f019c7e37c1ee64252e8b0f0eb4c589ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:16 +0000

    179bite dress pay uponon runover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_list(array):
+        return list(array)
