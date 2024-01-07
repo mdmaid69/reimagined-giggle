15906d22d1299176f1e6ad675f3544020c443673
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:08:40 +0000

    narrow agreement report hate sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
