bb0351ac16ffb0838e4cb9c3f720031e70d3d733
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:45:41 +0000

    food well post machine cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+print(time.time())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
