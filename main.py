c3e44755190bdf268de3aae57e20c11f1a054e81
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:00 +0000

    door swim wet organize difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
