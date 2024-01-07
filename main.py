984cbffa6c5d41f2807a6f984b0abde7e82e5e45
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:51:39 +0000

    section track hurt mistake danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
