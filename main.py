8fe5c3eca1658f650e7dc1e2e952b11edabbe122
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:29:48 +0000

    interfere wear tell over report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
