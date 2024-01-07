695b9ecd4ed02030173beb008c0a20a09179d230
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:52 +0000

    ride laugh within clearly steer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
