76eda06129f80d0484094964f1cb515b5635bbbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:28:05 +0000

    knock confusing turnout content through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
