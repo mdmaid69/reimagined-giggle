82f6f56f25d9fd6307e1e9f68eee3a66ae871bc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:31 +0000

    practical taste breakin insist quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
