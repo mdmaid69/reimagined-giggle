0562d4a2fd2d3c031705174d27b349c2945cf1ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:29:42 +0000

    health plate read loan wall

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
