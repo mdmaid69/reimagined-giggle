581ccd4030658d8fbdea4ddf409ea2ac414c6b7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:14:33 +0000

    hard city world war think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
