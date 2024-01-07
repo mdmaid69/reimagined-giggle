eb003efa450439929b4b96e7ab7c34ace26a7df2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:53:44 +0000

    over touch thankyou score right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_acceleration(speed, time):
+        return speed / time
