7254fbd8adb392ca90d6bdc4b7009017466e3625
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:01:29 +0000

    ever waste herself reject responsibility

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
