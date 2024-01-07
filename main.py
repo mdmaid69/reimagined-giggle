6a4b2b36a9ffe90be0605494c83bef3ac4a4c745
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:02:41 +0000

    perform public knowledge schedule friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_repr(array):
+        return repr(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
