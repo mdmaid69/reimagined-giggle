92e14f5cb15456d754ae6d419671f485a56d71e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:02 +0000

    usually length flat form kill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_work(force, distance):
-        return force * distance
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
