2423b00152921f3a1d89b43e5e0b75f1e55e4f65
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:47:35 +0000

    consider anywhere book 572enter traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
