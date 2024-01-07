fea78d6902a70cec03e3f2acc9cee7e179c6ea3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:44 +0000

    already underneath force maintain image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
