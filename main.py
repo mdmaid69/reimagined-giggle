5a63f367765f535911b469bcb2cb6d6eaa2e5a5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:56:31 +0000

    square weekend father steal principle

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
