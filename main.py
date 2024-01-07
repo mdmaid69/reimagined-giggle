b182f9ba2ff137ce5c46d8c3b18245aad38f982a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:36:23 +0000

    extremely second related weekend bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
