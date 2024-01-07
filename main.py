471a9707d988b8a625da0cb2f217cb824443a9a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:44:40 +0000

    Oo develop honest drink boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+print(re.match("h.*o", "hello world"))
