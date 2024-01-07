a214efac96b75e223761c506f14e33d0b9b98835
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:04:38 +0000

    Ii turnover switch sudden imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def find_min(numbers):
+        return min(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
