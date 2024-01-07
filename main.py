59fc90a615f7eae3c01a7f1f7e80982f5aaff8bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:46:19 +0000

    rain year trust mean think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
