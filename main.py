497252be933221371e5949f837b0573ce0b0a26f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:10 +0000

    stop friend everyone miss tap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
