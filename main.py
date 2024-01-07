3c884d38fbd806d544cc376a47e46710373c25b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:48 +0000

    building such slice obtain drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
