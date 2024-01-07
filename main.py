f168d2f740e323b66f2ed423b9e06848cb78e6e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:15:59 +0000

    size push aim attractive sort

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
