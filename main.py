58725a9124df1a320affff349126212055ec54ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:46:00 +0000

    substance worse arrival substance swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
