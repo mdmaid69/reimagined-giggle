0602b7f8beedb3eeb336667082be86c0a15e4d33
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:53:33 +0000

    attitude drawing bringdown score lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
