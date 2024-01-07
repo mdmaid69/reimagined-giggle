3ae22ee446ea064fac389f6d8d388d76c75dbe6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:35:49 +0000

    admire equal emotion pour protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
