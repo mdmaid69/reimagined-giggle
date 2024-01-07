00516302baa971549f8b2f86ac40052853344391
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:22:10 +0000

    all sight miss previous sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
