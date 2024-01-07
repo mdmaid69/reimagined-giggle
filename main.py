5a4a97ec3272061dcf0e1c056afebcc371bffa82
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:27:17 +0000

    respect fix throughout less left

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
