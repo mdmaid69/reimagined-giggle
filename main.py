0bbd5af2774c13b422cea9c98b53ee4d4ba288f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:42 +0000

    immediately rest damage pencil no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
