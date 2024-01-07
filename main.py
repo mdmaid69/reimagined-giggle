c865f0a1d9b2b94e9eb74dcd096b37cc273180e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:45 +0000

    settle against priority person worry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import random
+print(random.randint(0, 100))
