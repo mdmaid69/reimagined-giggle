bc75703a330a01aaaaa4a16376b7af7ec9e0d3ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:47 +0000

    coffee house aim Ee closed

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
