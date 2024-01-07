406347873573bd6bdbd59654b06650dc036a7c1e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:39:31 +0000

    machine raw believe quick twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
