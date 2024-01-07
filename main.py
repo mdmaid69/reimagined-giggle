8d6000b7a21f460318179fed1e75f33988de7a0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:42 +0000

    deliberate second its start legal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def sort_list(lst):
+        return sorted(lst)
