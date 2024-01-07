b27235983eb7cb73931bc70d2b53c6cd884dc8bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:54 +0000

    impatient ordinary either underneath stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
