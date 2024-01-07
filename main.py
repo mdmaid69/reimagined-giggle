7ff197f331b8d114ccf404ac1d42d09009c32c58
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:08 +0000

    taste slice accident crazy principle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_density(mass, volume):
+        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
