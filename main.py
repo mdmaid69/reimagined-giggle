3dd1d84b66089002e1ec308fa605492ece3015f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:19:49 +0000

    Hh job unkind form fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
