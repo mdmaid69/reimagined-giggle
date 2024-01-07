93dabc3fa4157dd6e8a7caa7b66588474a92e4f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:59 +0000

    broad consult survive sink some

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
