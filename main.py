4e7eeecba009e4746656797c419362ad4543150d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:27 +0000

    damp pen smile makeup programme

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_power(work, time):
-        return work / time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
