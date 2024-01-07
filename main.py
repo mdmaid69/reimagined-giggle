eb674e6e2461e10dbbc7e4639507b4641f57aa6b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:15 +0000

    expensive thick birthday little scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def split_path(path):
+        return os.path.split(path)
