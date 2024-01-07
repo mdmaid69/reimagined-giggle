0042f72f77647cdf8f96a9c51c5e7e9276c6cfe9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:02 +0000

    amount decision disappointed equal beginning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
