c37297e55890a57435397a83825151b5ffe2c02d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:01:35 +0000

    heavy comeback used 569enjoy either

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+print(sum(range(10)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
