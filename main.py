9327ffc16d821bd2f44879ba4061d80ce1ad7029
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:45:43 +0000

    she matter anyone tooth have

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
