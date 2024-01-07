17146703acfb3cbcd7f0205bcabf44d66729ff31
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:28 +0000

    trousers especially importance pass circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
