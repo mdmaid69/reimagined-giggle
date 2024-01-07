abc6d5a7dc55d5be53890b10294878560d000160
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:59 +0000

    smile repair arrive food quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
