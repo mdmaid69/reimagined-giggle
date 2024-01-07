5686d7130ed3f98da1dcbbe69425e51093af7660
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:41:31 +0000

    explain judge breakinto introduce accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
