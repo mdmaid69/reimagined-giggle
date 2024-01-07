0c6284cc8ad4b87b78c215feb9ec6a01dbb8f269
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:56 +0000

    talk protest eye because base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-print(random.randint(0, 100))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
