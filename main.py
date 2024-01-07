5571dd216ed50da740e54c06779af6a7f76bef8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:48 +0000

    terrible traffic organization hurry active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import time
-print(time.time())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
