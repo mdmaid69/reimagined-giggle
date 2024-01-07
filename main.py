952a7ceb7bb67180cf48291bd57ccf10e280bde3
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:18:26 +0000

    collect enthusiastic bottom respect tear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
