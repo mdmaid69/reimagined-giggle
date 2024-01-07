824377dd49f935671cc3dc9599e840aadbcdd904
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:23:44 +0000

    store custom peace fill practise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
