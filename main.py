966575159f570bd06622ce553bdf62b02e68a2fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:49 +0000

    goaway admire voice pot handle

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def cube_number(x):
-        return x**3
-def square_number(x):
-        return x**2
+def find_min(lst):
+        return min(lst)
+def subtract_numbers(x, y):
+        return x - y
