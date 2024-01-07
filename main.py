167c2d54644783378e0dffacb90be8cd54f010ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:20:10 +0000

    step nurse section squeeze holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
