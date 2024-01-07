775341f8e6019be2a3d149376c52ea8df8e6a985
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:07 +0000

    official consist power breath loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
