d1814d3db20766946439368de1d22bd2b3389292
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:45:03 +0000

    back turnup suit occasion command

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
