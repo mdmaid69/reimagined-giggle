d11531b13e7cb18027d4ee116aea8eb21e5ee698
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:46 +0000

    achieve bear dream delight please

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import time
-def get_current_time():
-        return time.ctime()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
