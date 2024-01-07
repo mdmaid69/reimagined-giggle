221d6af324e0f9f284468962a6eaa2591577877d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:41:26 +0000

    slightly translate better practical wrong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
