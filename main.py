292809e1a333aee4d2d0947d8ebb90d251ff1e25
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:01:11 +0000

    rid bringdown holdon intelligent ignore

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
