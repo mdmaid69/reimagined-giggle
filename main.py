b4a7dd0dfcb8575a6ebc87628a6b76d9dee51d56
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:55 +0000

    any wonder steep season wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_str(array):
+        return str(array)
