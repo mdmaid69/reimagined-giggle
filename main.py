545b1ac86e13d9d4fa52145a8d52a7430ff6151a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:14:37 +0000

    responsible custom track continue soon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
