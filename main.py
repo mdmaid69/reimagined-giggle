723bdfc47e8f9d3b8a6a2450c7d9951dd9e5087b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:57:51 +0000

    traffic disgusting wonder callat inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
