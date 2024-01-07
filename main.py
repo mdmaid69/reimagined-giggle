22dab18f1258ba224f0d7f5808097d929e0bc3cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:19:57 +0000

    invitation together split crash standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import datetime
+print(datetime.datetime.now())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
