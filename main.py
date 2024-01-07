413047615d8b8cc702dbca859baf6bbab76fbf90
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:52:42 +0000

    hello smart difficulty slowly steam

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
