989db3d4e5ef653b27dd621ba0654e0570bd9dc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:08:57 +0000

    board practise disturb war wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
