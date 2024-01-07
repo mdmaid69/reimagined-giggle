4f8d63e6889ddb0215f49f46d334369bd93135b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:00:56 +0000

    speak priority impatient speed immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
