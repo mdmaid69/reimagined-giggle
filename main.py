611ce2f987f7d00f8a00e8a0b6b2e7cf5f0c09ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:54:09 +0000

    positive home badly price period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
