4277721ef19d0c4c7f18604fc6bdad17ed573c28
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:11 +0000

    doubt appear giveaway thankyou smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
