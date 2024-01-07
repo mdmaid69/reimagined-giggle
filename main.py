662bb46de10747ffd1c3250e29f909e31f63b2b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:30:10 +0000

    combination sad slightly flow wire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
