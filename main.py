0aeed8d56a4c49ebcb51ff9a8c5a917f6b12f273
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:11:35 +0000

    born really strange giveup concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
