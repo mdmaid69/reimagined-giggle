1c770fbf954d0f308ef11990fecaa5c49da15f60
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:45:48 +0000

    freedom beside equal something culture

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+print("Hello, world!")
