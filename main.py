d97d1076ee6b2df800b744119077cca4053dd310
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:45:39 +0000

    company trip advanced instruction sentence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
