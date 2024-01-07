aed9bc79fc5e88d1dc2b7acc636e7b2844a6089b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:22:16 +0000

    test while feel that bringforward

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
