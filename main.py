ba86001cf0b9a2945d3580ea938ac77d440c27ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:01:00 +0000

    too patience run chase refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
