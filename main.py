a5201deb2455e3280e34516c885997a2ed85a7c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:08:50 +0000

    bag increase scrap forward mix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
