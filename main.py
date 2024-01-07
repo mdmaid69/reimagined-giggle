d8129d5f9a34c3f3ac33a5d4dd01da00ce32002d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:18 +0000

    action solve many bin drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def get_current_time():
+        return time.ctime()
