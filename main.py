c08035a929e20d26e1e82db26832004a759b5f6d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:18:41 +0000

    talkinto contact pleasure shout relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
