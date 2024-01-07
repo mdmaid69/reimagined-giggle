5484b6db6023ca77e4861cf07ab6a90ccb765fa9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:52:02 +0000

    dig bottom post answer under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
