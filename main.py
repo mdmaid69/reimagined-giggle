8d2e2d0f58895f0f50e0dec29382f332ffb55e0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:06:52 +0000

    less agreement so various thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
