7670ae6f31706ccaa2fd36d13d53ee423c78dd7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:35:47 +0000

    general bear fly formal breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
