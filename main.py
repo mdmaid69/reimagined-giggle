b0dda68fe4615a7d8c8fa74ec63bd55110c36ab1
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:33 +0000

    above short on risk able

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
