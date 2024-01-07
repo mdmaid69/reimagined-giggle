f6d5f3c4610596986d51c5909a1c534337b53567
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:38:13 +0000

    close considering accommodation break trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_factorial(n):
+        return math.factorial(n)
