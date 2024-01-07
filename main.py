fa0962c9f174e69f84d0b626879d7c06d392e4a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:06:54 +0000

    belief strike food such weak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
