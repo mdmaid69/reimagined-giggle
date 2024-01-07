0f6ff4fd39e74cf8ff6866aebdd06ea08478e72c
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:53:16 +0000

    swing church oil imagination stand

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
