1f7f3507c41b05be5ba9a9202572363d8f6bb179
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:26 +0000

    entry shine responsibility takeover thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
