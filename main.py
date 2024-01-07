98e7f7d4af2b5fbd68ee45d9e0025d4716b49bc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:25:17 +0000

    section plain except Hh this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
