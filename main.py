87deb6308c2cfcb38087ebf3b2c6329919ae950d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:19 +0000

    quarter upset circle shop top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
