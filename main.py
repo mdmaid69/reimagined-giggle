27fb8951ab13141ab20b9acd7723058d620fdc69
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:47:58 +0000

    happen up pinch practical perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_unique_words(sentence):
+        return set(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
