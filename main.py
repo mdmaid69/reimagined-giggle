f7cd92d4b4c340d14ac83c62f295177bb5292271
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:06:31 +0000

    hot anxious game straight private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
