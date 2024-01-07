44e84c9ef376d69646ee1178e98761893b2f3765
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:27 +0000

    bend wish announce warm neither

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-print("Hello, world!")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
