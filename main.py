cb7c4d3817c6d0e35b1f7cdd0fe27d2746428f67
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:18 +0000

    family true equal small hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
