3c66dc729578572f9aa04b908ac6c596d02aea38
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:43:04 +0000

    wheel direct serve turnback pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
