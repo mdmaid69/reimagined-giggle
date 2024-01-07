e7e3fbeb3ecf7b5838d67b2badd7d4a795295399
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:34 +0000

    steam decision noone afraid admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
