2ef89bb3c577b822ef0b9bb7951d5f7b1db18fc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:08:30 +0000

    choice government back delicate charge

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def reverse_list(lst):
-        return lst[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
