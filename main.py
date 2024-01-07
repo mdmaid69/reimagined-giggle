0b05c02fcf8ef4250c3262e5ae03af8c3444b7e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:05:37 +0000

    what though nice come holiday

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
