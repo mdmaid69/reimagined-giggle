145bb63238850f76eab30654115e61d4b4cc6446
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:07:53 +0000

    burst lost temperature down bin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
