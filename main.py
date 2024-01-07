5f20001e975b20f9d903cfcf597852c1d14341cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:36:05 +0000

    bus occasion side promise battery

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_min(numbers):
-        return min(numbers)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
