c41f3dc12b09aace5ef9d10ea588c224ba6c8ed1
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:16:22 +0000

    failure wire separate model spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
