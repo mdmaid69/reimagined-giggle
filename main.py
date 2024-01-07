5bd306a17cc654d548caa6cf3449316133a413c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:07:01 +0000

    jump interesting silent explanation post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
