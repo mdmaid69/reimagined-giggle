3c50bf413d0cceff4cc46fee11e42637f8fe2f2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:31:34 +0000

    trick stand lot flow trouble

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
