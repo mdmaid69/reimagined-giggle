52ae7ec59437c9d72935633db0214ad60f1bff69
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:16:06 +0000

    lead display safety spring maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
