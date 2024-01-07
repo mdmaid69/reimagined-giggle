391dd06a97fd264417132f999ff8cb30dfa48298
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:07:55 +0000

    prepare sorry beginning description Ss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
