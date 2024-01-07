1b7b9909ce020d559af22d370859c6a23e655188
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:05:48 +0000

    comein light spite injury leg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
