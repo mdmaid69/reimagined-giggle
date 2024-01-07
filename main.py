b4d5cf94d1aa836deb3f39e33e67ed4511a79345
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:34:33 +0000

    or pause couple not ever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
 def get_array_item(array, i):
         return array[i]
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
