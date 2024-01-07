d02cb37a2660d997ceceffa66175f208c6ffcd9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:35:06 +0000

    mark improve lookup pressure fair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
