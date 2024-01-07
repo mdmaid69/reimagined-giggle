399ed4870e975d44db3ae69f55ef198af87e27e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:59:12 +0000

    fit file opinion keen splash

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def get_array_item(array, i):
-        return array[i]
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
