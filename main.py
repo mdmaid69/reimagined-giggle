d8326ad8367aaffde933c8a7ab041e056abf3aeb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:25:24 +0000

    sensible patient less suppose freedom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
