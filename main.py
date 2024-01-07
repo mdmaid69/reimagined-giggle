deb6b79eec1d88de7e62d3c6a99f3e93d307f891
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:53:19 +0000

    scene offence embarrassing pretty only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
