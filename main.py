60feb887259170e724e5c42e00cc1fb18a8b2199
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:58:28 +0000

    centimetre somewhere job impatient cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
