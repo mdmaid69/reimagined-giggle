e6a9069b29ddce439e95bb56adfde9f7de54c57e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:45 +0000

    under person cause spring nor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
+print("Hello, world!")
