ea76d67882687312d694f9f9e43d66e1dd33c5ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:56 +0000

    anyone almost hollow destroy move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def reverse_list(lst):
-        return lst[::-1]
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
