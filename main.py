6fbe82010c47c3877be56e15e95ee6d243df33eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:45 +0000

    gradually yesterday hurt will wheel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
