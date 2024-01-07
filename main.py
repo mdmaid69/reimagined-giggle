00f378795827990475ef418ccae332e035208825
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:15:17 +0000

    empty thick course sharp figure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  def is_odd(n):
+        return n % 2 != 0
