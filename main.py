fa38625b7e98fa21df9ef569a8e1d0d06a8fddd0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:10 +0000

    might account pay fight willing

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
