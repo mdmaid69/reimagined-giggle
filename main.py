fe8a7729ccaff924bfa2a4b9499392782e4d76dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:50 +0000

    realize head clever rule quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
