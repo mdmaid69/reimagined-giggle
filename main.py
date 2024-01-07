1293bf6e260ba4c3fb815e5dea9e920488e3b1fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:47:45 +0000

    write neck comment high centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
