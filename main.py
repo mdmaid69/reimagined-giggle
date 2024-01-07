4f3c881d3968b2a282610b163f53b409343e5514
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:29 +0000

    bend temperature Gg calmly add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_acceleration(speed, time):
+        return speed / time
