18a2b14bb020210891b12345688be79129063b04
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:33 +0000

    still Jj expression snow object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
