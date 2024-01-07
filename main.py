338d7b41f226f53f60097826f55d1ef98216b350
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:29:22 +0000

    previous path bit Hh workon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
