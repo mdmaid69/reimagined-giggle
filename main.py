ee1d1e01968090747bc9003013d368c76698e25a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:08:04 +0000

    slowly great plant wild main

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+import os
+print(os.getcwd())
