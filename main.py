7087ca98b0b6fc1b4a9e464379d5f93b0a4865d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:29 +0000

    test size see voice no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
