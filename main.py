2f4fd59060b6d85a2db009f907e9ad0974902778
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:15 +0000

    sore false mention marry arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
