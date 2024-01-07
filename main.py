4307775435a6a3dc9972412caba13c98151d402c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:46:33 +0000

    wherever importance frequent ocean break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
