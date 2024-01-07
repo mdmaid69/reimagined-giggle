84e9be2e54d2a725e6e25c3ec010aeccab6fa916
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:44:22 +0000

    steady whom interesting pipe talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_current_working_directory():
+        return os.getcwd()
