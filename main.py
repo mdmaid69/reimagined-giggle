248f4462bfbbc14ddce63e3b2ac7ada5b36201a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:51:32 +0000

    Nn flow crowd morning apparently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
