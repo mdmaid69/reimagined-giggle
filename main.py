6a084e0107927208033ca773a865490c3cc47bb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:42:43 +0000

    jump other represent hole runover

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
