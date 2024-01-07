973aae235415388740a3257b60d2d234994aea8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:04:47 +0000

    closed accidentally everybody gradually apply

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import getpass
+def get_username():
+        return getpass.getuser()
