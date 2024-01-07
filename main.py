3fb0cdbb87ae327adeda5019405f32aa4bd6113b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:12 +0000

    instant confused wide or gift

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_hyperbolic_arc_cosine(x):
+def calculate_inverse_hyperbolic_cosine(x):
         return math.acosh(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
