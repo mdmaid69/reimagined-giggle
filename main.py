7e882075452ecffcb5f226a083ecf290291693d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:21:19 +0000

    breakfast right talk imagination lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
