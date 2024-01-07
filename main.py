3708cb3cebe1c7c08df33ed78dc5980ce6cbf799
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:52:29 +0000

    Ss exchange clever dance custom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
