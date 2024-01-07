b12283346c41fd7af67d602ac4fac0d50367b142
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:45 +0000

    case interfere deal sort until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
