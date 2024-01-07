6624b4e2d4bd29230da1d4d323c44d5410238d37
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:50:06 +0000

    breakinto first search news mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
