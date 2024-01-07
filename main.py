b7464cfb625c9a4002aa913d084a05be62a00fec
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:33:49 +0000

    us sing objection detail maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
