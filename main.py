79512fabd2d8dc2127c403866d3c118d937bb27f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:23:23 +0000

    rank we tape brother callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
