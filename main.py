1031e5b8dee0411b5f0ae133d21d229f46533aad
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:56:42 +0000

    many runinto pile road nurse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+from collections import Counter
+print(Counter("hello world"))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
