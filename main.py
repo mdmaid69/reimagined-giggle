deceac47d08a49b57338bbe0a89eda7ab5d99951
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:05 +0000

    notice bar phone workout getup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
