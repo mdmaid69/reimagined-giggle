111509c2721f2dd908258447d7755ab5aaf68dd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:34:58 +0000

    for rubbish practical traffic now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_queue():
+        return collections.deque()
