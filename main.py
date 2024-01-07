15bd9b9006d765ef852526f7483746b2b704a814
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:02 +0000

    call wave main advantage call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import time
+print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
