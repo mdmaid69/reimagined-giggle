63eade4bdff36c5f869472d1d25198575ec2b9f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:56:07 +0000

    spot contain before together usually

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
