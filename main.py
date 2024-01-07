5bba3b01a93eeeb9e61039e33cb251cfa93ce431
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:53:46 +0000

    throw introduce achievement immediately nor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
