cbb46dbc9050bb5fa4b6c1970ef528a7c1722bf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:27:49 +0000

    regular out force start remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
