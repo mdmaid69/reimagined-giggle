6d6c1529b574c9b1add51a4954cf8879ea6d6ea3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:57:31 +0000

    second tip smile eat record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
