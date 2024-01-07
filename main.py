b9369f440e7911e45a54f2e907ec164364405886
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:48:19 +0000

    raw freedom abroad hit priority

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_max(lst):
+        return max(lst)
