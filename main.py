733fb78b3c80aa317d691d3b83b07524398d451f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:29:01 +0000

    crowd lucky effort fairly back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
