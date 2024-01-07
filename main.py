e6a3b260b10748262eb5b28f1e154b04877e9e97
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:19:51 +0000

    above file spill feel accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def print_python_version():
+        print(sys.version)
