e3937648c83aa4ca8dddfc6cc75cef80f005c410
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:02 +0000

    best past private indeed chief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
