c909dfa5a6789c308804631337ac297f2a30374e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:12:37 +0000

    even love might nose sister

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+x = 10
+y = 20
+print("Sum:", x + y)
