c37b70d1106873db7c5b93d419ef6c4115351783
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:45:06 +0000

    tight post new secret man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
