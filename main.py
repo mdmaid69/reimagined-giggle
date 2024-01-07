7c6ac03572c8e4d273e8c215d8014443d2d67a8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:07:30 +0000

    reaction usual impossible count try

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_to_hex(n):
+        return hex(n)
+  import sys
+  def get_python_version():
+        return sys.version
