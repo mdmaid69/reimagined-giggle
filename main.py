abf958cf186c10d69c84411dbf0644046a447945
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:14 +0000

    shoot during bad method test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
