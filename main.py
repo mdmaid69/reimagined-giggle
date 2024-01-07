3ea0b78fe84d17fe81e92d1190811c6d0609b89c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:02:19 +0000

    spring unlucky 566end sale touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+print(json.dumps({"name": "John", "age": 30}))
