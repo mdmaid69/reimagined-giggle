a91cb269fca0675b9c74486cff88761e3e14f513
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:39 +0000

    interest decision switch ground speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
