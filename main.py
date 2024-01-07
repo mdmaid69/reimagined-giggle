65f68d414c25d834ac9ea591d49c61630abbe052
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:47 +0000

    secret silent pile generous between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_max(numbers):
-        return max(numbers)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
