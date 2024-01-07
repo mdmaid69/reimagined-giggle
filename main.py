3030b8c6ad2e2df7f38d606b4c717a445ee1ca8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:52:39 +0000

    tax condition among concerned pattern

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
