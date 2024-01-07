af90aff650385d1fe3b209dceba74f063213e951
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:08 +0000

    skirt meal objection machine glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
