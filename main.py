7d2cdba878f996ecb01022dc249caa5dd4ba2b78
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:07 +0000

    pay breakin direct check callup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print(math.pi)
