e2ec0560a81347e1c2598824a18510c3407e549e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:47 +0000

    computer blind plant reply off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
