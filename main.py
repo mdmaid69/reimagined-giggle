f0569029e5fd16012e11f29e2475797b10472790
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:48 +0000

    off transport their birthday bend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
