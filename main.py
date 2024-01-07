c1362d2fea537f025436e4548afa583730818172
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:35:08 +0000

    mess clearly outside best jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
