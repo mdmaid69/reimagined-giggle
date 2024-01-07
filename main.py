3e55322bf7f6a51b4bd9d3dc3e780c9e40ab264d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:30:52 +0000

    dream die can argument clear

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
