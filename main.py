279d594025c0285a463c47d2de98800f6ed3e833
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:59 +0000

    far hear design generous doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
