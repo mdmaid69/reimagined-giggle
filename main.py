47bb9f169a1049e1bed2772e429b45fb56c15faa
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:28:37 +0000

    material turnback sister squeeze bush

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
