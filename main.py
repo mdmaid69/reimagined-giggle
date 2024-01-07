5253bda6b8941568b96b6fdc8cd2b97fd1f7d25f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:54:31 +0000

    workupto protest angry miss afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
