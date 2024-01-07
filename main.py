926340327516e13c484b157e610a28091fdd61b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:07 +0000

    only become worse strict mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_as_tuple(array):
+        return tuple(array)
