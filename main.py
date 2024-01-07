9e01abb17c88dafbd1e707b45e4ec4d2392015e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:17 +0000

    angry exchange piece woman contents

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def subtract_numbers(x, y):
+        return x - y
