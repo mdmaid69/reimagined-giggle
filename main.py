080d42a8d2c194172e59ef05027c1961d2a1a612
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:47:09 +0000

    material education fear distant consult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
