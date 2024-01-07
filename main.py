4384eddfd770be1c1a105795733be9bb3bdd340d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:55 +0000

    pull light swallow beyond drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
