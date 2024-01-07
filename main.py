11bb0325b0e49acb35bdd8a6fce122de413c106d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:29:03 +0000

    accordingto skill giveup powerful pair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
