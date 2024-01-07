c724b717d8f24bbc3a8b5331d2742ac72b727e73
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:46 +0000

    lock comefrom or through fashion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
