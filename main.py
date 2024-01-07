aa99deeb3cfb085bd8d1c4dea7c0175481bdb853
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:49:17 +0000

    mystery marry false mind protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
