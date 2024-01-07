a6f23358cdc97802db73abc6aca766754925a449
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:10:45 +0000

    callat every light season everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
