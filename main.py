4dd06c73f14c4a7ab810137bea7001a6a60266db
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:12:54 +0000

    through sail aim admire heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
