01b99baff6a8de05558a4901988a3f1d218a70ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:38:09 +0000

    kick machine wind wing soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_current_working_directory():
+        return os.getcwd()
+import getpass
+def get_username():
+        return getpass.getuser()
