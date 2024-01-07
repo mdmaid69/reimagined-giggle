4d582b146011d4f22fe127bc44efb87491f2eb6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:31 +0000

    sticky high blame back entrance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
