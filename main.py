c6712c67faa5c3e463e8b96d9c17d2321e5efd24
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:45:17 +0000

    one woman custom public breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import collections
+def create_counter():
+        return collections.Counter()
