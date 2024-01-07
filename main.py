dcf5b13c130a362159bc5f337bc218bb5a218571
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:37:42 +0000

    trouble shallow offer perfect turnon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
