5b5c23cd8aeafbce7ca6af1724ba6e61d3817264
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:42:59 +0000

    wish regret smart colour sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
