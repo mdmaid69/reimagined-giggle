28d1379d976ebaa30de21c55af97fd27867b5739
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:52:38 +0000

    fear safety steal relax tear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
