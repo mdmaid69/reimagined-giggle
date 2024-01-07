2338f7156866089f45808d4635060dda9a4b03f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:18:19 +0000

    fault schedule rain design nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
