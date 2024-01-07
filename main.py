1737df9407e346ff19eb39d0b2522cc823dd8583
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:16 +0000

    add draw ill backwards closed

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+for i in range(10): print(i)
