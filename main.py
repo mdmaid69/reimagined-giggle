f65740108c401b872a2d0e97ba66ed64156f188e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:10:31 +0000

    deep takeout nowhere short lot

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
