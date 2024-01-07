82e7b20cdc8058a89ffc36093343d5757096b7a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:34 +0000

    pile shallow cost sink dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
