ce3242fa8487a667f74a114f1009209e960bab9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:27:59 +0000

    hole shame can cinema wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import sys
-def exit_program():
-        sys.exit()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
