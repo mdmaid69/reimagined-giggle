8ff7d163e1ae2a1dd306d2064211ee28f4e2b220
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:17 +0000

    upwards supply plain comfort still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def convert_to_binary(n):
+        return bin(n)
