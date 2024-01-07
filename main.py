a83b37ecee5d1af6ab6a23a7c340d9ff7d3936a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:41:55 +0000

    them artificial surface pronounce accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
