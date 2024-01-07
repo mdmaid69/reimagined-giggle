3a12f694db180c6a79068be0c5bd156275aef2d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:33 +0000

    shine need report information comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+def square_number(x):
+        return x**2
