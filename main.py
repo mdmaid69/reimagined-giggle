eb9707c7d6bb557777c232da21b61283b3aa621e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:58 +0000

    agree science butter secret target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
