4748d82d77e1a4db64592b091f59572a4f9e7081
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:04:05 +0000

    squash tend regret fill expect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_density(mass, volume):
-        return mass / volume
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
