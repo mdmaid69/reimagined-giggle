c4e88ab6a30c6d36a7f6e35db42fc6b6cce82711
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:25 +0000

    responsible apart divide kill any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
