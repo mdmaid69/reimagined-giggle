cc8860fa033a501bb95474284e8948a6f37a0633
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:22 +0000

    tense muscle stay survive reduce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Characters:", len(text))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+print(sum(range(10)))
