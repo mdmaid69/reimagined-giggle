24c37fc34669245e4659273a584e0b0c08c278b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:03:37 +0000

    unlucky since shelter responsibility expression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
