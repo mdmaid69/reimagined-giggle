5a1c2beb8bf0fc8c58c0f3883c4e06caea80145c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:22 +0000

    paper motion warning consist fashion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
