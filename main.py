5692089e499dee0e6815deffbeaad1be29f16792
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:51 +0000

    another ilovedicks contents sink support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
