3326518533dad3d010b14b6515cbd7473c15e99c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:43 +0000

    mass snow exist often correct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
 import math
-print(math.pi)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
