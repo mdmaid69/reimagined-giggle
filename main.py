42792d17736e3f2fdc318e18699f2ca7b75a777e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:27:31 +0000

    near touch somebody split good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
