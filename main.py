28995228c959d9fec0b067f4fefd1087f5433198
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:26:12 +0000

    kind thirsty sound high reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+for i in range(5):
+        print(i)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
