e39a8980280768b9fccab7c2844e0823322c5506
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:47 +0000

    smell that familiar result area

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def add_numbers(a, b):
+        return a + b
