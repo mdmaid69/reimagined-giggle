3c2ed87f67213e52405ba63790e37017f2262b98
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:49 +0000

    left fire talkinto adjective spread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
