d9c2c4f483a988887480ce993cf8553e0942f126
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:02:10 +0000

    ill importance original cinema walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
