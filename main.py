a27fd900784e3ac9dd8af794bee157ee9a598e0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:24 +0000

    taste polite delicate last that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
