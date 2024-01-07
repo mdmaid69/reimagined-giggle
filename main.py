19e8cd92c2d531d132a7d0bf2e8b084154ceafd5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:27:30 +0000

    opposite reduce once string instead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def cube_number(x):
-        return x**3
+def count_characters(sentence):
+        return len(sentence)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
