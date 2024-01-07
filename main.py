55fd4e4a1c384d74638c6c3313176c6c63737f5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:07:54 +0000

    zero thirsty shape wire train

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
