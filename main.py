0cf8e3a2177a54b64204f5ae56e24475ce198a37
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:22 +0000

    edge wing cinema bring scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
