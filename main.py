ec6d15f614f4df8bbbe743dad2fb417c816f428d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:10:07 +0000

    actual hurry drop patience square

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_string():
+        return collections.UserString()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
