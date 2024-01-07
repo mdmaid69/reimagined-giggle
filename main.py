d86624df109a708bf3baf2927cc0d4f133fa5268
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:57 +0000

    steady sore fair explanation board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
