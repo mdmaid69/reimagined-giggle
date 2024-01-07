580a0fe51a89bb63c156c6ed74243ed6d6714d91
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:56 +0000

    repeat far mean Ll bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
