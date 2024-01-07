0f819f40eebd4989d51cea0c5c3fa0ed3a264e51
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:23:48 +0000

    our influence joke soil danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
