7f18ebc50247fa9d88f40e26a85adab209d9a120
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:10:10 +0000

    spoil be scared suggest home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_area_circle(r):
+        return 3.14 * r**2
