f751f0a1da0da0c5dd4f0a38c9bc871b959d774a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:35 +0000

    board estimate over town sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
