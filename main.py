c46f4488cdb312d6b4ac74d007193902c7fda63f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:18 +0000

    Kk begin roof excellent involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+for i in range(5):
+        print(i)
