679abaf508548f1fe4f58915bcab5c8afd50eefc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:52 +0000

    priority believe back vision try

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
