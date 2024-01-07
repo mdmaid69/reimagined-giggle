ba4b88729badeececcc66256a86f66137f110069
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:06:12 +0000

    note shout several respect hollow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import math
+print(math.pi)
