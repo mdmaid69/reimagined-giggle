945ee97e2c07fc5863463ca68851fae58febc604
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:11:25 +0000

    think community Yy congratulations rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
