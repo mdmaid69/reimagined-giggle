d298fbde70cfcc4d5d041587e5c5580d5a6398dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:28 +0000

    skill promise comedown support claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
