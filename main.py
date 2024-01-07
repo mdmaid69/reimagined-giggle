8020a423a806e6c304e88ff7d026d09fef4adf23
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:24:55 +0000

    improvement realize drive mess favourite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
