d527e628a1d9f83b21d319cd02b6d5f1ac33f447
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:08:06 +0000

    popular naturally square where reaction

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def find_max(lst):
-        return max(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
