0ff15a3272533ab92fb877160f53b84dc46a818e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:17 +0000

    repair turnup answer top active

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
