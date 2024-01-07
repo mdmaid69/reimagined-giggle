5e7b4f366e009bd5f0ad74bdd077e07f3c5c8889
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:27:27 +0000

    correct practical description stupid easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
