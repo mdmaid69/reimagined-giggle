57113100fb6090fd35d1c3e4ddb439168e34bb6a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:00:34 +0000

    pair bag consult ball ever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
