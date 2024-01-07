5aa4750a5e277c241f4a7419cf1cb76624d8150e
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:40:16 +0000

    comfort dress mother or zero

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def square_number(x):
+        return x**2
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
