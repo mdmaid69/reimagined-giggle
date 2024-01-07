45f5adde15504423c098078b3708674e9d28f0f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:02:59 +0000

    friend backwards wild surface development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_speed(distance, time):
-        return distance / time
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
