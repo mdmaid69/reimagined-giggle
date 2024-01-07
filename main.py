b21416a2f7acbddb39868d075f3b37a6ca46742d
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:50:09 +0000

    low either male total pretty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
