ece7e93d17bb129c30d765c8a147af6e72e2e8cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:20:56 +0000

    famous guess wrong theirs 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
