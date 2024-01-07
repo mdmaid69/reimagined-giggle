5c8763317789ad8274e67ca97f048d54ff101435
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:33:49 +0000

    rarely early trade contrast true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
