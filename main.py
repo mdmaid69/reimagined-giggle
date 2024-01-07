852535e6cf140e10d8bd9c263c6081656493d108
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:33:44 +0000

    plain pay temper free cause

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
