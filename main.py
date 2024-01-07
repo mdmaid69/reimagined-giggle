bae82310a7c21b93b55384aabde5a9161e241cab
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:49:12 +0000

    workon love feature climb possession

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def square_number(x):
+        return x**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
