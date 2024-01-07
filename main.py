725bc9cf3d59cb1126893bc4c73ec38b934cc1f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:53:31 +0000

    at talkinto shut show expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def get_array_as_memoryview(array):
+        return memoryview(array)
