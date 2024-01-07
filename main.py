cce816254ce9b0dac8251879f48befd9c40ccc6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:23:33 +0000

    wall final surprised object now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
