5f0045d9ec92bebfdb6e205fbe89fb11be142546
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:56:57 +0000

    year sale mind could attempt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_octal(n):
+        return oct(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
