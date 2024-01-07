d2390ee0eab45d03e50ce3086fcc027fd6fb926b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:17:09 +0000

    exhibition lookaround petrol wind attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print(sum(range(10)))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
