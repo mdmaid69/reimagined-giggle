6ac32443ddd316e6755641ec9af2371ab0f4e454
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:44:07 +0000

    grade except actually telephone obvious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
