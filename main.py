26486c229383d7d6fd13e80f4c9b4cd2f0ce3440
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:13:11 +0000

    breakdown unpleasant under mistake record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
