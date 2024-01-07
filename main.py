f7b112cb3077a3bea64af6051bd398bc825639be
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:48:57 +0000

    strike possession fortunate of go

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
