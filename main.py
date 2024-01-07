8bea8cbccccd021e30519a3678958e84f246cb91
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:46 +0000

    crowded except sort coat any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
