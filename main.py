a09fc6a5a7430fee9cb958c6c218bdd2aa0806a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:22:19 +0000

    imagine search quick measure evening

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import getpass
-def get_username():
-        return getpass.getuser()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
