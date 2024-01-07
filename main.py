8b26c6576cf9604b624d3de6e2fe12064bb6cc5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:01 +0000

    rest how write all sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
