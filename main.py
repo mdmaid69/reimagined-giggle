9583f80313404e5f61b2d710f4df1d16fe13eb9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:42:55 +0000

    calm estimate visit certainly fierce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_index(array, item):
+        return array.index(item)
