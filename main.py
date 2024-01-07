5de704c122098f6485440c807c2076eddf61be87
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:17 +0000

    authority break sweet otherwise later

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
