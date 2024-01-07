b5a36432c34117fd189d05739e6ec99da11487a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:26 +0000

    number father involve remark basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def convert_to_hex(n):
+        return hex(n)
