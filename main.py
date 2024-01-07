20568ceb703bdef024a9abd6e93dfe8334b096f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:33:23 +0000

    weak load exact little rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
