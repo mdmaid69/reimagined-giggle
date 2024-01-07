7e30c90ebe9db2464c103c6298bd438bc96e10f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:09:35 +0000

    light means simple tax Dd

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import logging
+def log_message(message):
+        logging.info(message)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
