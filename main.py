14b0c3389bc7584b3b518b8c8c9027fec4af0ff1
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:51:04 +0000

    high share their surprising card

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+print(math.pi)
