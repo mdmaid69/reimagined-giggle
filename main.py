e1ccc43c8214a6353ebc78ca0712aa6fe9f9edbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:37:33 +0000

    better apply behind inform cross

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def reverse_list(lst):
-        return lst[::-1]
+def get_array_slice(array, i, j):
+        return array[i:j]
