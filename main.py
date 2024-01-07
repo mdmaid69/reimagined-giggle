56a924b7cd024ac8e2b67bb18e2e4f278498f8f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:36 +0000

    custom murder aware minimum tell

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
