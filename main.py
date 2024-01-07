b8b4f0c57398a06d1a0ac06fcb74b29c8a147a9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:21:51 +0000

    whistle lookback(on) word feature holdout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
