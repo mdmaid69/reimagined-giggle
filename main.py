0273d9682895443a0c502f59cbdb10457f25d5e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:50 +0000

    intelligent luck pass dress for

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
