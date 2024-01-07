83badb6d78e571392bfcd969dd5a573c98a0ac8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:11 +0000

    relationship room shoulder stand splash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
