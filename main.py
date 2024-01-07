74d76ea574299462dadacd787d74d2e6b480714b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:39 +0000

    case container speech pleasure holiday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def find_max(numbers):
+        return max(numbers)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
