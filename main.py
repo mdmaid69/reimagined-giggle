ee810aa1252c53fa54a5307586ac988ca223dddf
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:32:07 +0000

    everybody giveaway reduction one neat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
