88b373249ce7f89ea122f9aded9fb4068f3b7604
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:05:47 +0000

    makeup arrival wake anyone before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
