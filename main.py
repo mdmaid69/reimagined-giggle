abaafe4c95259f2a40cff563eb0ab0d81f8e6d30
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:11:46 +0000

    sensitive stamp character shop thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
