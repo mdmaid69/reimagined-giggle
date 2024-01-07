3896441193e00b0ef056ab4c33d3e074d0fb52a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:50:45 +0000

    until rest he neither tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
