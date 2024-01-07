986c3130527dbbc982252754a153d416c73cc041
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:55 +0000

    holdon makeinto handle among giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
