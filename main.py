53da128481ef8ff3221bd00c88d1b5535288e340
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:49:03 +0000

    lucky so instruction left wind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_work(force, distance):
-        return force * distance
+def is_even(n):
+        return n % 2 == 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
