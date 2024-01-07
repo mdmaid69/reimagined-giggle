09904adef6ade7023d83900feab181361df7fad5
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:10 +0000

    pupil small fine address party

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def get_array_index(array, item):
+        return array.index(item)
