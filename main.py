0e1ca3edf416b95a64dbb929181abc1e939f7c44
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:39 +0000

    record rest better either hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+  def add_numbers(x, y):
+        return x + y
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
