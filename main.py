7a9ab80aef2b26c6669f3d637145fa4ebd909bc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:53:55 +0000

    wish surprise include weigh hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
