e76bb60ddadb3381d40e54f6245c5f92e56b3633
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:43 +0000

    occasional cruelty afraid punishment deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
