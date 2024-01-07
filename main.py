2a31f1e461202fc08bda7103a0e0886e04c0d8c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:25:21 +0000

    regional stage sharp mix sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+print(math.pi)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
