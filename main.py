c2b79a1be9ed488b3f17c51a8e0f7ff767e9e397
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:00:51 +0000

    cry old gentle share weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
