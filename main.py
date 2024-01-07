da21b8fea528baa06bcbe54fdd7257f9ecd1fb76
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:00:39 +0000

    reduction mean part watch intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def convert_to_binary(n):
+        return bin(n)
