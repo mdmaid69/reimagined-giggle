7c585590ce4c0c8aeb7a7b3c43646b997f4c89c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:41:42 +0000

    sure operation doubt care criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
