6148fa7b23d43068730376d12c4ed31e81364ec3
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:55:39 +0000

    hurry impatient lookafter flood include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
