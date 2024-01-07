3af09fb24c81d2b68718affedced3c97902b7759
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:39:09 +0000

    settle month expression interfere whom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_characters(sentence):
+        return len(sentence)
