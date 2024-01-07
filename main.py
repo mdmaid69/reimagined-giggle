eab57c7648c0ee7dc2774036e20a11f541bf8c50
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:11:07 +0000

    shut exploration connected fence strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
