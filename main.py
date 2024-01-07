d254c8b3af3c3e6325ec81259ef62e9ef9e59601
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:36:20 +0000

    stone would listenfor effort eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
