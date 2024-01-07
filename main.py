ba661546c89ed9f2148e7fac67ec8fb02d8c82d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:07 +0000

    suppose minority shock previous interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
