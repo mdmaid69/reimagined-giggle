ebfea4d741a4e2320d9c2d9ad01096a2408a3144
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:37:19 +0000

    tall night squash strike born

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
