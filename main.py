24d682dbff2f4df797d6904efba797aef2e37f90
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:25:38 +0000

    ask comfortable otherwise underneath refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import time
+print(time.time())
