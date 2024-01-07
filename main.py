ca81ad7931a67c8d719b9641f9d3d1a2d3125780
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:18 +0000

    hide gap ball free break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-print(os.getcwd())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
