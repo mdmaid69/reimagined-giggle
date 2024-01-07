8acb338571d9c96785b7967b08ddaf9e8173c3b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:26 +0000

    rush turnback thirsty big touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
