36ec075b7042d3b63e1ba9a5e42d584ebc037025
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:54:47 +0000

    funny free their approve regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def remove_duplicates(lst):
-        return list(set(lst))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
