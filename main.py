f4c303bbece8e80f191f508267cd99641081ffde
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:21 +0000

    software connected Ww occasional use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_current_directory():
+        return os.getcwd()
