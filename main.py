ed7e92a6067486e57c342e1e062bbfecb63d9a7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:47 +0000

    come style age improve match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import datetime
+print(datetime.datetime.now())
