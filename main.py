f4c118bf19363acb817047bb9d9c47652e92907a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:36:54 +0000

    considering show than behind plain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_bytes_from_array(array):
+        return array.tobytes()
+import platform
+def get_os_info():
+        return platform.uname()
