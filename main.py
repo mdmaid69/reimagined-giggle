8d44e36bf5295e18ba16c7690667d847689f0c0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:10:39 +0000

    trick successful somehow opposite level

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
