cefc06ea124a596c5e2ca46193cb047b7ae1ff80
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:59:25 +0000

    absolutely confusing family number emphasize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
