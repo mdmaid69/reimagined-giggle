c3adc16a9d4d5361159a9252c50c318e607e24c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:46:07 +0000

    genuine holdon under future lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
