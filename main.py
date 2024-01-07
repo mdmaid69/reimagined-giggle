c42b3ee2bd16bf1350d6c92d1aa21b5989f06961
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:50 +0000

    block suggestion band immediate uncertain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def convert_to_binary(n):
+        return bin(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
