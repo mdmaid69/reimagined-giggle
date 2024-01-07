5044185820e4bd2296c273b1914ee9ddb748a129
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:09 +0000

    worse satisfied strong actual tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_density(mass, volume):
+        return mass / volume
