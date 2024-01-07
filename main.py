50d60ef6e732c49b81b55435c2875aa089a9ca34
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:36 +0000

    comeup treat goback(to) permission report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_pressure(force, area):
+        return force / area
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
