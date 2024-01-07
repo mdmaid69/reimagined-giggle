21cd2b7a1d841412e1491bf71d07aa8c81bb6b7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:30 +0000

    minute comment familiar but represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
