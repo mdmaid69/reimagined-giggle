0acdf4e5540ba71123379c1c7c859e10395fce64
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:05:50 +0000

    estimate ordinary treatment steam only

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+print(os.getcwd())
