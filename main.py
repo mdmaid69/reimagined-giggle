dc5116991a4cb39331e05f33b1b66ece924e1b32
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:23:47 +0000

    up whenever only disturbing afterwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def remove_duplicates(lst):
-        return list(set(lst))
+for i in range(5):
+        print(i)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
