419a6d9b60f8d469c33a8198bec6d829a5a03c41
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:46:01 +0000

    belief keen search better heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-print(os.getcwd())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
