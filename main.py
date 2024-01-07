eb18d085df5aeceb269149a4f082bce9a9aaba86
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:53:19 +0000

    mother comeup issue continue quantity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
