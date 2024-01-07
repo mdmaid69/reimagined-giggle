9896a6804e8fe855cfd612173f5052f556e2df03
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:29:16 +0000

    show offer original stamp regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
