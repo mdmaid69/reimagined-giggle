c69e66974dc8d57b09df1e5f9e1586986b0bfb5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:14:35 +0000

    protest geton pencil regularly goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
