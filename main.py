0770c9d9b42e01060b209e13ef064813f98274ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:48:49 +0000

    old through prefer manage market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
+import numpy as np
+print(np.array([1, 2, 3]))
