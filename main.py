ec892b2cb8f500684e55790a3b03b10b95ce35c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:23:15 +0000

    learn present relative question needle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_current_directory():
+        return os.getcwd()
