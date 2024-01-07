cacd633627eaccfb3541db78b22bb62845e62444
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:17:59 +0000

    arrangement aware ago argue forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
