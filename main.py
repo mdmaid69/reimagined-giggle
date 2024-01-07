de9a827e365358fd19a66ca2bb2ed0e797475480
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:28 +0000

    snow minute dish on unkind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
