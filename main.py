120cf33c7ec4088db98a48505765cac8f2c80d69
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:35:53 +0000

    slight prepare container wrong among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
