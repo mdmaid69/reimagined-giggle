0a6dc23ca2bfd0669a7019ce235e3fc00b972f00
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:05 +0000

    dare teach rather hers comeout

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-print("Hello, world!")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
