5d27d1525ce4b100510ec54f9beac4d500d79387
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:59 +0000

    unlikely yet Tt expect feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+print(math.pi)
