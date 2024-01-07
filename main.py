4dcd26229f8aed842bb5415894f4c8e6c3af52e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:21:32 +0000

    war plenty certain repeat low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
