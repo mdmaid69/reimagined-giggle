9ad9df53e47722d72764c5885b7a4983ffbeddf6
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:58:58 +0000

    family wealth fall nowhere battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import os
+def remove_directory(path):
+        os.rmdir(path)
