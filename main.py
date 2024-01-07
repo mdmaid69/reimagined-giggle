83af9b0e979ffab38b9cb60e6838a57b5a3b84d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:26:45 +0000

    original but report cry receive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
