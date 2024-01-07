b68647b31d22fc55502af963af41e1e78f47eeaf
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:55 +0000

    development pull rate knock badly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
