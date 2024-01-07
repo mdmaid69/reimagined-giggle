5726d4335845a6e218a706e7ff7d551c825f5733
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:48 +0000

    expensive range dish come at

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
