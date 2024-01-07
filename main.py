53e0019c493da27a1bef96fabc4db9ea38c5eadc
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:38:53 +0000

    peace increase hang proper cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
