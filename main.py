ef29a910c6446be900484af4e12e438dbaae350f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:22:10 +0000

    fresh minimum practical path wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
