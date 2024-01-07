ad61130c7549fd66c72bf18a467aca9133adad29
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:56:30 +0000

    reject news mysterious go admit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
 import csv
 def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
