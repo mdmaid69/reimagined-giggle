9d8207fdd086588ad763813255896eec0f6ed1b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:51 +0000

    cover direct date satisfied general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
