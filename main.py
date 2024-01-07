442525838f4f53350d70d7f44ae48e3ad5aa1572
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:21:07 +0000

    style discussion train recognize list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
