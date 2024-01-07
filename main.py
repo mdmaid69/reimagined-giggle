9ac3f042731cc1c3a1dc42a33a7a36c3214b68f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:54:53 +0000

    relief chat opinion goodbye foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
