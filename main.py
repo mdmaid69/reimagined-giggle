0500c22b7830fbd4d293d11c73c91c1249de2676
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:30 +0000

    mood tough frighten modern play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
