ab3afcce665084177365d4ba8a702ceb9e0c6293
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:25 +0000

    while interfere fun late certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
