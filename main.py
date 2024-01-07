aabd8b5e9c57a154ce7aff3daf4bd4b9e532f08c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:44:19 +0000

    Yy create moment pour holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
