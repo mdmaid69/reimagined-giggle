d20519a55d876d19f1f86d01c762ffdb33a25560
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:05:34 +0000

    confidence shelter Ss inquire test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
