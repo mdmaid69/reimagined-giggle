653b9bb9982eed54d95259a0e2b41f37ba7793e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:30 +0000

    suggestion support load miss desperate

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_current_time():
-        return time.time()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
