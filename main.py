5908f29edcdeb25fb29cc1a7838e8aa0ba80f390
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:27:57 +0000

    welcome thank display brief drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
