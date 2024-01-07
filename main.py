59234b07f2439c43496c1d283f88e3926d6ab9f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:17:27 +0000

    once accidentally absolutely intend note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
