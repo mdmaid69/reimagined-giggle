c2c0289704fbcdc54e3051c7953ca13157066b7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:34 +0000

    compete communication wash activity tap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
