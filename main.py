af725fee77867aab373b4d14bd3f87351252825b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:18:29 +0000

    clean desperate sound Ss share

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
