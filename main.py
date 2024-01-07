9c78a7b7e74d66b4dcb550f5ab10390ac628c9fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:51 +0000

    minority process example allow invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
