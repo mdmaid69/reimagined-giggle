f661d49fbd8589bca64bd27f692a1b30c8db778b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:03 +0000

    temper pencil agree game squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
