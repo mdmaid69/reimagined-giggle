c77c1ec685626441d90dbf0ecfbd711db2d81f90
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:21:11 +0000

    gentle tidy sense comedown accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
