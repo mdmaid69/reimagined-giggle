84aa5f35f4da18cd4ace8f1aea15ac0454e19cca
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:48:21 +0000

    describe judge strict thankyou willing

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
