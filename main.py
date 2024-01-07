7d1dfee1e1818dae273d70d25eb8da38ce2be485
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:01:28 +0000

    complain little form coat reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
