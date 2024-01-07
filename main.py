309cb391817b3a14088856ccbef95ffe4e2c1c8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:43 +0000

    message though display worried hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_user_list():
+        return collections.UserList()
