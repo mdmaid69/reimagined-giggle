2b11e1825a4c415dc4edf7cce15875a7014be65e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:12:11 +0000

    morning borrow weekend long pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
