5fdc083ce05857ba36ca778e400ae0996415372b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:03:10 +0000

    deliberate before takeaway us address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
