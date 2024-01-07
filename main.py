0ccb07378d9069c27d8865a1cf5ba477c2b8f89d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:53 +0000

    sign combine remove suckme middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_hex(n):
+        return hex(n)
