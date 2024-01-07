1b8138c4e941b084bafb3149c47cad695f1a57a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:57 +0000

    scheme right mean wire 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
