60b4e8b34b8f84a76a510115748f31c764fc2e4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:06:03 +0000

    own consist undo cope your

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
+def calculate_density(mass, volume):
+        return mass / volume
