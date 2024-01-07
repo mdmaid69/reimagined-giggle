5e413411c12bfcc4d3c0d5de2c3c0e3ef5cfdaa8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:32:26 +0000

    turn simple like interview same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
