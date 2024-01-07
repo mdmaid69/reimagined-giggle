724649044d917a72aaaa325a8f2bd700611d122e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:38:50 +0000

    replace deaf else comment stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
