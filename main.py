d89d926b49b8c96de454e1653f2b2cd5006b3fbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:01:32 +0000

    pour weather harm moral maybe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
