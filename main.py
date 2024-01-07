1ef2996578770b47b7f67d0c7d61798ac97e2128
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:09:28 +0000

    meeting powerful advantage worst chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
