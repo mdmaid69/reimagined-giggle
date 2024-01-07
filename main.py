6162b327215378976437f8b0f0e398b3682be0b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:47:34 +0000

    coffee goup much artificial share

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
