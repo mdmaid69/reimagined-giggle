a843d47b2f166dbfebad06a1dac2e0347087f40a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:14 +0000

    either separate sentence Rr whenever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_density(mass, volume):
+        return mass / volume
