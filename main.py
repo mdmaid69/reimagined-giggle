c76358dd8f094e9f7328d10db253853aac6c3d4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:42:13 +0000

    disappointed chief group during dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_index(array, item):
+        return array.index(item)
