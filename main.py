e210be06d024d77da507f0fb882ae2c82cb06b9f
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:30:52 +0000

    talk easy artificial join receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
