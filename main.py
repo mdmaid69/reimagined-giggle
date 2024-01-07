f4bc550660b7f0d5d2efc785de960e4a606ce34f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:44:45 +0000

    answer fire world beneath damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def convert_array_to_bytes(array):
+        return array.tobytes()
