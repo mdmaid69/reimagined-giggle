97c68d411204b778a0215d66c5238298db5f1ef6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:32:21 +0000

    round demand shoulder building song

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
