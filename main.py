e417b79f31be5b6f0bd56469fba885ab00799d33
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:38:05 +0000

    relative completely top reaction invite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
