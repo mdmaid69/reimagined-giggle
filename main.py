c7680cee44aab43d3ffd35ddf6f707f6978dc03a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:08:24 +0000

    depend basis belief information scream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
