930e036873ab00221e3a19c4b94ba16ff88203da
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:56:33 +0000

    breakinto protect tap water manage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
+def is_odd(n):
+        return n % 2 != 0
