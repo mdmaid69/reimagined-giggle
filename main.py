090a67d4da10593979c3199c0a008073d908a075
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:02 +0000

    exact behaviour brief familiar cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
