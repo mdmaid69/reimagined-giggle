5c82bfecac3e340c7f5d2a6a723fa28d4df95f24
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:23 +0000

    trap use broad pick job

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def convert_to_binary(n):
+        return bin(n)
