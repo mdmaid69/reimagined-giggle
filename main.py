687a179deac91c7fbc01073e63e70db29970085e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:03:28 +0000

    kill whichever play key plain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import numpy as np
 print(np.array([1, 2, 3]))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
