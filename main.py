a130b811945c4e837c5c110ddeee99e8c91a4a2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:04:10 +0000

    injury group everything inform eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
