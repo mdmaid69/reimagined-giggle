2ce1ac52217e3a0f4d69df7aca235a15ff38ef8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:36:58 +0000

    treat contact afterwards standard tongue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
