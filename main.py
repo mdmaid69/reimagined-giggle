f51982705acf8a4f76107da2f5e8effe1cebe577
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:54:40 +0000

    science improvement alive government waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def clear_array(array):
-        array *= 0
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
