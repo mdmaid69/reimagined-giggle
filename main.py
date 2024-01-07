aef06e277566b83a469ac504837cd1d98aa590e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:41 +0000

    born rubbish regular bored hungry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
