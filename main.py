5fe38dc3caecbceedd5710d7af52143aa54a582a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:34:39 +0000

    face permission survive right move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
