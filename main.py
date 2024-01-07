b1882b0a6759fa9a197cf17b9a9dadad378e34ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:24 +0000

    contrast story chance save belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
