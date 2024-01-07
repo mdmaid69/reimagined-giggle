9b29e7067ab5e04330cb80f8fcf1891fd6c38df4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:24 +0000

    exit regional getback go charge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def sort_numbers(numbers):
+        return sorted(numbers)
