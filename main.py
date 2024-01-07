94ade6166caba4948f48706abc66cc001e824dbe
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:42:17 +0000

    nigga cough doctor dinner every

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
