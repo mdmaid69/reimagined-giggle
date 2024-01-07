bcd16fa00d7fedc47fef7fd22b43e0e93e416d43
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:29:39 +0000

    actually smell declaration wild through

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
