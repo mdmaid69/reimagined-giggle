e205e1df74609c240fbfbf693f402c20b7c80808
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:49 +0000

    divide blame dance badly public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
