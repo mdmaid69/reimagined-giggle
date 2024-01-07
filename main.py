d7b9c7648f24ad02ff9eb98369a7245f6a01e6e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:46 +0000

    weather stamp slice sure excellent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
