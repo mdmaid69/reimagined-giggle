897b0ac946984d484497401aea2ff28179c4d3dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:50:29 +0000

    major possibility drink will factory

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
