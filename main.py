3ef9ad20dc24e8fcc7779266951226b9ddb6085f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:21:29 +0000

    come exact thanks wish profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
