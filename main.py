ff6f8dbdf2e2e1000bc52792c9c9ade6f72564fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:02:16 +0000

    hardly exam its support order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
