fe214453189d13361369b7e77c94945d6827ab68
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:17:49 +0000

    familiar present attitude dear tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_length(array):
+        return len(array)
