49727ff41fdf700be2d9f47ffd0c6b0754787aaa
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:18:59 +0000

    couple intend tonight mistake surface

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
