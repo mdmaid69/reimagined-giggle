104bebeff7bd4645c6bd88e6d891fb263edbd0f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:48:01 +0000

    approval mark still Jj instead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def clear_array(array):
+        array *= 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
