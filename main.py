1ca80594177665a98d6f2f12acff0ef5ac67deb1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:40:22 +0000

    anybody movement experience couple keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
