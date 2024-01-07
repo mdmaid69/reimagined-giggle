ba6db81cfb575ba844ec7c8805577af56c3b8a09
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:24 +0000

    getup must one exhibition achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
