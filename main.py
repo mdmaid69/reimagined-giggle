5e3d3461c14d3a312841376245e63357ed9ed6b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:32 +0000

    persuade development 569enjoy favour situation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
