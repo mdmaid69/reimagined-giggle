b3ceb56e66139d758d4d13019ccdb703c4b29981
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:53:35 +0000

    place Itself press wind sore

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_work(force, distance):
+        return force * distance
