6c66060965b154fb3038f11f2a5d0166fdbc53d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:51:36 +0000

    blue by slice pretty clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
