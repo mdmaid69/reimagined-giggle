7e7cb6ea53e62c802038829fec518292f3a8a31f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:28:49 +0000

    polite goback(to) arrive criticize nor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
