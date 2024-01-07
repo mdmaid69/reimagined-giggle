fc96539e4700efd329f1bbaabf11929d5eb27080
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:39:53 +0000

    announce fast that usual especially

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+print(math.pi)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
