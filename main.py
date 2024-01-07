d1a5e632a3a7230950f27d910ce2957960f7e5ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:05:47 +0000

    stamp battery play loss famous

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
