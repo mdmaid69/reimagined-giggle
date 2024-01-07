32dd1b4003c935e0c5429a2a3af7d9e10e4612f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:08:55 +0000

    prepare myself drop cope spin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
