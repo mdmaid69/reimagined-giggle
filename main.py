745426dd032b160b687710b1651acd13891b77e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:46:37 +0000

    brush vote away since most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
