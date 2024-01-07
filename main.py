cb5414ea4122b33976b493f03ea527e78cb5ae86
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:06 +0000

    conversation add brain age all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
