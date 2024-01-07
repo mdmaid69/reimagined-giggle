6eb223ea87bdaaa4e17abfb0aabdf819c3e74c04
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:05:54 +0000

    strong total occasion little shallow

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
