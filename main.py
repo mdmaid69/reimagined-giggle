61266c0e596206e0794bd6280c59cecccb78d8d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:23:53 +0000

    worth turnoff obtain Ee spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
