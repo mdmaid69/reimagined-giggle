93b9970a01e4247e7f241e38bf60f798459cac61
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:22 +0000

    wise interruption everything entry eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
