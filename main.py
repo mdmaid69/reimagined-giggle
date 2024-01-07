1915ec085a2cda979865856a985fa5958e6be088
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:10:43 +0000

    thinkover ill wrap book bright

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_max(lst):
-        return max(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
