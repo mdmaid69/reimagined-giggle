b9a4eace7a7f7f8d8fdda783c14f99dbe63a3ff5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:14:06 +0000

    stand communicate relative runinto design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
