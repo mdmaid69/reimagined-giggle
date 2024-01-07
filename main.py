b68995628139ebd78fd96a6b95e4d7cbc748c6f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:49:51 +0000

    suppose exit branch crowd fly

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_current_ratio(current_assets, current_liabilities):
         return current_assets / current_liabilities
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def convert_to_hex(n):
+        return hex(n)
