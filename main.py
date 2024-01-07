59db590f469411bdab12e46eceaae45e0cd6449f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:00 +0000

    responsible deep approval both guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
