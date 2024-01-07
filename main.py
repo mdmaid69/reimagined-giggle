3e605e521c6e90d587bbe2ae81d617ea6f18ebbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:35:44 +0000

    alive public funny noone somehow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+text = "Hello, world!"
+print("Characters:", len(text))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
