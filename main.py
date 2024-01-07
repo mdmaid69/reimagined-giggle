32f98f9f0275963e4058b3e16ab51550d4a29197
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:15 +0000

    far tragedy either kick scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
