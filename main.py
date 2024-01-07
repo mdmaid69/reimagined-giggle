2fa31acd4b6fa95dbeb8df8164d2753f8bc7250c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:23:48 +0000

    lucky more bridge joke dark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_min(numbers):
+        return min(numbers)
