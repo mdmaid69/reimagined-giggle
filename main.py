11e9e18ead4400f3a9aa0d1fcc6e20ed62df2079
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:37 +0000

    spread 564encourage public image hand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
