6ac5f617b3d0c8153b9d2c0118d77134fff5752c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:40:39 +0000

    spend hour return scared stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
