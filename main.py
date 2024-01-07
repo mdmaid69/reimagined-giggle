cdf2a5e0b2aed9e5bb5786e93d69cf0125250e5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:18:00 +0000

    tape walk wet figure develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
