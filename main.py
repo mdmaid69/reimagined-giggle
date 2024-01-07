a4f45e8324e43fa528907ba4a467c0ee2b81caab
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:56 +0000

    fierce goon wall exhibition consist

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def convert_to_hex(n):
+        return hex(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
