abb19753c68c2d33e84e2e30b243d3b8e6fbe72f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:30 +0000

    wind fashion child rob flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def multiply_numbers(x, y):
+        return x * y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
