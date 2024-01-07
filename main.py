de50f7988d6ec36b0e3572ccffccd180098779be
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:46:28 +0000

    comment problem open murder nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
   def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
