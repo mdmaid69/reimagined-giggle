09cf6e99efc403ce5afbd1778e5644092c8f2834
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:33 +0000

    warn solve duty health room

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
