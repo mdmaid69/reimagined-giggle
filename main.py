c8cc013202b1fcd5202a37929b3604261f84bbca
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:37 +0000

    future lay punishment week tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
