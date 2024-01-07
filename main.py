06aa3749be6abff8c8f5411314a099eb6d636583
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:35 +0000

    distance possibility from left 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
