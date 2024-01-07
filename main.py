63867cd1a67598149bd430994830dd85e7b4c14b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:21 +0000

    collection time education effect run

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
