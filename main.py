6b49d696878f1e5037e5d74c993ef68511681afa
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:16:49 +0000

    floor face deal reduction beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
