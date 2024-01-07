665fa31a07124e940478be445d72d25ff3743dd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:56:52 +0000

    push afford nice event phone

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
