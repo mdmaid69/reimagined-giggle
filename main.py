09c670e0b9c70d729926ff6e4816f935b8c6512d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:19:10 +0000

    exit pronounce whatever confident wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
