7fd94c5a7831ccb0f4abb7b2f4b3b66e30b9979a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:02 +0000

    their crowd official getout lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
