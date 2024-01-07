e875ab00a6d413d188bceabc6c98d671c46c0ae9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:31:46 +0000

    path allow constantly division mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def cube_number(x):
+        return x**3
+def calculate_power(work, time):
+        return work / time
