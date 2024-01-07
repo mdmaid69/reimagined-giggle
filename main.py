ffadbd606957ffac1b363ef117130d5f75f75701
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:35:55 +0000

    mad again slide string meal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_power(work, time):
+        return work / time
