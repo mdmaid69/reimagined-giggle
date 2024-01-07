840a148fe309485c2ff336c4fb45b49d5c6db574
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:25:37 +0000

    low will suck point that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import array
+def get_array_index(array, item):
+        return array.index(item)
