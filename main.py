38984a79e0db37e18b5c8e3575ded3f62bd2efd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:35:39 +0000

    nurse happen unite wave wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def sort_list(lst):
+        return sorted(lst)
