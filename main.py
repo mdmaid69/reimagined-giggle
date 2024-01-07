1641720ad5fae0321b4fd21fe529e34fed4fb6ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:23 +0000

    cap fact wire object finish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
