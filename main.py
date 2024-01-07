b779ae3da631504f36be2093b7c52fd1889ff095
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:30 +0000

    disturb final convincing rub regional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(lst):
+        return min(lst)
