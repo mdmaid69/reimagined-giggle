0dd9737b80f736ce40bcef2bfa939a101ca93887
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:59:29 +0000

    people middle lucky train situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
