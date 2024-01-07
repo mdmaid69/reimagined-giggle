e9219eaebfa170cb03ae4d70b832645788f612c9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:27:16 +0000

    few finally desperate blind discover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import os
+def change_working_directory(path):
+        os.chdir(path)
