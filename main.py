842d941619be03be3bd4027917f23333b81d9ed2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:54 +0000

    force material geton treatment flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
