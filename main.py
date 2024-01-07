2df141fe2f1cfeaf916262c381c43d6809e38eb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:17:02 +0000

    priority big early moral permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
