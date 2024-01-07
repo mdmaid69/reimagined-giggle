8931ad95592c56e8eef9418e0a299c90f221a483
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:52 +0000

    bright until case big runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
