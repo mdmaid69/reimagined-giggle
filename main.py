b75cfae1e37b482911071ff592d2723ef37646da
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:52:52 +0000

    delicate plan 566end suggest pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
