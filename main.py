7cae62934718374630a93afb9335ee54069e31a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:13:46 +0000

    group lost dry niggaacademy rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
