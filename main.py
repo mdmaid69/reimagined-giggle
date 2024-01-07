eb18d45ab6578affaa6c9a0c5b54f3bbde819f5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:26:03 +0000

    begin Ll pattern thorough subject

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import time
-def get_current_time():
-        return time.time()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+print(sum(range(10)))
