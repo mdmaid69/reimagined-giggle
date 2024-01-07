377f3d3e4f46f69b70fb2f9f336e0e277775c048
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:38 +0000

    official possibility stamp taste borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def count_words(sentence):
+        return len(sentence.split())
