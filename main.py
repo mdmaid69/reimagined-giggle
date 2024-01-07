35dd77a43daf6cb39b000bfbec6df330acff8ad8
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:48 +0000

    feature wild song usual least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
