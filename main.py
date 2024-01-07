29fcb001923e879581848b4e848f5d761e283600
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:26 +0000

    rubbish message full he dead

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
