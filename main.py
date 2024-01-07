6d253e003736190f0f83cb712b8e849feaf7db26
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:30:00 +0000

    gradually influence me loan hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_even(n):
+        return n % 2 == 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
