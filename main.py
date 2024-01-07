4204f5fdcdfa16d7bb7ed27198728f4b37eb40bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:38:21 +0000

    pencil flood move rich basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
