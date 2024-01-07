17c70f4a00fa28e76df76f77d95d5982aa0084ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:24 +0000

    sweet comedown walk much food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
