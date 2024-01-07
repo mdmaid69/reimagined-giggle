eb519943456331ef6b8858e59ae30eca98c43da8
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:09:31 +0000

    again put press satisfied frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_device(file_name):
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
