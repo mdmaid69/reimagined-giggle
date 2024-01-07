a629fa1ad3dbb082d3d7a61dcfeafbc4960d98d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:21 +0000

    equal moral everything combination damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
