bada01e05bd4f82056288b0d1d7b2e13b8fa5047
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:56:35 +0000

    less read plain spare advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
