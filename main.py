85f0869f91d9a96693a5ba2ac88340ef4db6cfae
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:07:37 +0000

    grow university rare start discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import getpass
+def get_username():
+        return getpass.getuser()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
