cdc7fd698a334a05eedbc942a49f56f28528fdd2
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:44 +0000

    remove activity shop Oo affect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def reverse_array(array):
-        array.reverse()
+print([x**2 for x in range(10)])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
