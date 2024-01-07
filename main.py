6b099b06647c323d53044d90dad3661184272ba1
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:26:52 +0000

    fierce across harm Ff Ll

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
