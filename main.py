39532315d7a22aba0726fcb5bdefb6041c21013b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:45:27 +0000

    personal sensitive person bringdown quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
