26d18351189a176a185c81bf17adec04246573a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:56 +0000

    like ticket final general unlikely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
