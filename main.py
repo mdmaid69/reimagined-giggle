a128124c6f10410ed971233aa0f039778323db8b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:24 +0000

    571enough want sentence disagree public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
