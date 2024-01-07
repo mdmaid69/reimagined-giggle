06c3e02801b865954f61e2a3caee2b642ac444b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:36:48 +0000

    lock beat sweep petrol disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
