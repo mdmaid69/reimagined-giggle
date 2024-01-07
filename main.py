d8f8e719a4d93fce29f1ef7398f22c7c4a7e54d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:53 +0000

    heavy trousers start wire character

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def is_odd(n):
+        return n % 2 != 0
+def divide_numbers(x, y):
+        return x / y
