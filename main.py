9fca20f147a9d26bea718d688c22e742eef1b761
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:18:38 +0000

    land vanish play broad sick

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import collections
-def create_stack():
-        return collections.deque()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
