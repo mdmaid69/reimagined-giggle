0b46aa39085f25dddcfb8ef56c55ae41616b2fb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:11:46 +0000

    proof unkind autumn soft embarrassing

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
