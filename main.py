263cbcb5643056218df53218ed1e642abfe25cec
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:55:38 +0000

    anybody ball bottom return runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def multiply_numbers(x, y):
-        return x * y
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
