b6169de72ebe4def867cfd4ce84c0b69426de478
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:21:53 +0000

    measure prepare waste supply risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-print(datetime.datetime.now())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
