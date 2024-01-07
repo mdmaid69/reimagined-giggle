ef12cef79aa9f2503b2321306bae36e6d316a37f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:10:30 +0000

    waste makeinto emphasis afford work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(lst):
-        return min(lst)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
