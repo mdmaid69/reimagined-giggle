f92c1eb3c68f9e9dbead9d6b1a8533e40a5c129a
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:59:07 +0000

    left into hit that software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
