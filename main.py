4f9adabd6331533075dadbddf2d6b8fbc30f0d73
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:30:06 +0000

    main autumn Yy hold extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
