d0adfd3c3a53799d9ef7bcccc53c78578ec30283
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:29:12 +0000

    properly mistake sister under town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
