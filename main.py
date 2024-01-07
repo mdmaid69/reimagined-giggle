f9c281b6db8e741285967c4925db52b5bbd3933d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:26 +0000

    Vv picture talkinto scared practise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
