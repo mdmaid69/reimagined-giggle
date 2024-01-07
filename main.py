456ee4ac6bf4c84c2c062b6fabc96aa183b490b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:24:19 +0000

    swimming pair ever story left

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
