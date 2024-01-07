ff8cfee385e0dccc88ffd4eae1c76268a64e25bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:58 +0000

    introduce comment intelligent anyone Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
