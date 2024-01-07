2c0027fb78680ba02829f73f6acd16e674ab40b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:47:56 +0000

    bush OKorokay ride occur basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
