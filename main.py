ecc0aba45f6a25ce041622756c0a754111bf85b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:49 +0000

    scheme figure reasonable trip put

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
