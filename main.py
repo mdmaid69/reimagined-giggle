596fea61e01aba6594e6ffd1208fd64f086fcda8
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:17 +0000

    awake among division land straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
