8c87f5cfc4b8256a6fab0eed643014fa85201cf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:02 +0000

    duty start between work characteristic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
