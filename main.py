ae6a68642414e29fa829fbee43919cb5fa9d270d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:33:32 +0000

    slice bye about cook whatever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_cosine(x):
+        return math.cos(x)
