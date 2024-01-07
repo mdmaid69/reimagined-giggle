9498c85e4675beb7b73743eb216bfbb2d64c8fe2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:17:08 +0000

    takeout up pool definite performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
