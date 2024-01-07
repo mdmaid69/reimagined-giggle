bc289253bdabf583471a924ec4281627469ff58d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:51 +0000

    read closely uponon yesterday suggest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
