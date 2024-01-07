6c352b44d66e22db7248d668a5c9bdcfd60e731c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:11 +0000

    leave already carry apart muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def cube_number(x):
-        return x**3
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
