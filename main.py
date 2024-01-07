3a32eb801a6b0147a824947f7e3dda5e11701a79
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:37 +0000

    rank well vote him business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def is_odd(n):
+        return n % 2 != 0
