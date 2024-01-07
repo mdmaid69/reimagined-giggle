777d340c9c83be3aa3255a5d327adc3d4da73eba
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:47:26 +0000

    tax feeling something dry path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
