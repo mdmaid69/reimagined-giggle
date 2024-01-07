2fb2c027926c985b0b89420afc9f3610840ec50f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:21:26 +0000

    sore child sink favour takeback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
