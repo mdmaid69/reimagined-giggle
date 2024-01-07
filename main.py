f3595eb6c5d96330a20b6fec6579b9b7987db089
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:54:10 +0000

    bend upset attitude future successful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def remove_duplicates(lst):
+        return list(set(lst))
