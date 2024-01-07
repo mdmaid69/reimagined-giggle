cf20512caf988dd61ece9bb4eec59ce494dd481f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:13:35 +0000

    Ll move protect minimum acceptable

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def remove_duplicates(lst):
-        return list(set(lst))
+print("Reversed:", text[::-1])
