3f46cd4c7575066c75efb15137d69af8e369871f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:14:33 +0000

    hit lookafter pride figure catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def add_numbers(x, y):
-        return x + y
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
