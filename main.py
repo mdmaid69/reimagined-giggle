08f3b8598b4fb079786e7947d5219c2ee196f8e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:25:31 +0000

    sleep concern street money tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
