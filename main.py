0e49a1ffa9e1c54d377a385cb123b26ac66aab3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:25 +0000

    suck shut command otherwise date

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sys
-def print_python_version():
-        return sys.version
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
