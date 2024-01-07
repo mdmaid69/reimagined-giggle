4255858f87774f768bb982bb94b36dbdbe79d5a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:08 +0000

    recommend note forever born back

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
