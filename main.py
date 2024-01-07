cf370d0c5a580b22a7a10ce6577e31beb5107967
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:46 +0000

    such view amount air consult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
