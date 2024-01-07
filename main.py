f2c02d9c70747bc1adbbd32944dd34d8878bc533
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:36:29 +0000

    pay mix regard undo right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
