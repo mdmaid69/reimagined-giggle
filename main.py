21fd1217ea75890e0a4ee6162706985a051a4839
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:31:50 +0000

    goaway stand difference prize Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        return sys.version
+import getpass
+def get_username():
+        return getpass.getuser()
