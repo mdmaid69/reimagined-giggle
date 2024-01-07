d99f3fa0651432dbd6e386775e7f016ba1d83c36
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:48:58 +0000

    gentle tell know get platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import time
+print(time.time())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
