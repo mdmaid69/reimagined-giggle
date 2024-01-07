7f76f8e2741817d10e43b7c0b441793434f176bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:57:05 +0000

    unlikely tense himself order music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
