aa2f7782ae4254acb0593945ac8914457b99a244
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:55:26 +0000

    reaction world pollution interest sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
