d4fbbe2ef86943d76e9cce875a9cb09a418990c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:12 +0000

    everybody exaggerate moon right right

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
