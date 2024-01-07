06fcb499c675c11b23592943711188d8dfa00aeb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:06 +0000

    commit good boring distance something

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
