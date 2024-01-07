eda4cd31fe43e183079e720bdcbed8fe16683f98
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:14:50 +0000

    fairly file address name run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
