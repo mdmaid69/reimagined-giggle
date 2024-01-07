2ba7911a64e95a70285cef3f3e0b191269c4269a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:51 +0000

    rate there powerful kill back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def convert_to_octal(n):
-        return oct(n)
+import time
+print(time.time())
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
