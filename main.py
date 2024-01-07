80fc5163c48c472734f900550d37e41124ad358b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:24 +0000

    event disturbing natural weigh or

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
