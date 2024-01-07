5f97c409108eef9a469ebdb1c8bacfb1d84f6108
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:38 +0000

    form plenty boat wherever jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_list_from_array(array):
+        return array.tolist()
