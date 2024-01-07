a6fb129695ca716d872c149c786522f98e9fc071
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:11:11 +0000

    who nice present experiment provide

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import tensorflow as tf
+print(tf.__version__)
