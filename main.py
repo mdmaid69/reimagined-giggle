0053ea010b4382739a9ec7074c842b4367b50666
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:18 +0000

    crazy terrible zero fix bin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
