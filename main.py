5aa2c79216bbac645eb3e2bbd7264ce9e8016787
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:59:07 +0000

    live shopping ignore juice least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-x = 10
-y = 20
-print("Sum:", x + y)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def print_python_version():
+        return sys.version
