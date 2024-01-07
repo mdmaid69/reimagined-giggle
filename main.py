68672729cad322d146a1761e4749b5236d1e25aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:51:08 +0000

    lay post attract average police

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-print(math.pi)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
