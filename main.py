415163d9026f12c14c651cf9f8899d6de4cbe9ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:08:19 +0000

    destroy point subject attach kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import platform
+def get_os_info():
+        return platform.uname()
