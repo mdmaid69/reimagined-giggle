f57a891235b2d3905a386cc150e4058aa4e2d51e
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:54:39 +0000

    please false us thinkover thanks

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
