01174c928f5fbccbdb514b2d50210f1da614091b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:44:56 +0000

    union reach opponent exhibition loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
