c3252fe0de5ec16536c3111e9faf178a31134d48
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:49:36 +0000

    accommodation stop copy within freeze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_as_list(array):
+        return list(array)
