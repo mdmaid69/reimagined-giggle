ec9cade999606f00408f649c02228ffede1a50af
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:42:20 +0000

    best less below record frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_tangent(x):
+        return math.tan(x)
