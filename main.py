c47789940d56b9de6beb423dc4e49a76056b5a8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:27:47 +0000

    strike sound violent curious split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
