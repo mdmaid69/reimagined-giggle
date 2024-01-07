48be99261560401ebd5969855a9aaa40d4150263
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:46:05 +0000

    quiet safe pocket makeinto available

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
