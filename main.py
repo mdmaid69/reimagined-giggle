89372c65553c109cab92226e1421aa1eda989070
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:14:06 +0000

    hospital this discuss light pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
