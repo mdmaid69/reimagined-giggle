4334c14d5c941edf5dd2f1c7443488f88988af7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:55:19 +0000

    present he from square side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
