bac206980b6fded1b09e89b46162eccd4115da75
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:54:09 +0000

    table reply behind guess sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def square_number(x):
+        return x**2
