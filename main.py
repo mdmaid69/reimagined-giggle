363b71053f75aec38eaa95c550c606302e29f1d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:11:10 +0000

    modern off middle promise fashion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_words(sentence):
-        return len(sentence.split())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
