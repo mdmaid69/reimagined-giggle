f0a861e144fb9f4f7e021f3ad98ce4327579a177
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:46 +0000

    trap phone steal across thankyou

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
