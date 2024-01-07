73366fe7a6e6b3240fbc2e987ae7e36db94310c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:22:15 +0000

    round still mood understand concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
