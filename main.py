d13a510d20b2a4538979f0fbdeff67f0c4038029
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:14 +0000

    score among ill subject advice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
