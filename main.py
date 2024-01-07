e2cf66b15a8e8b8b8b2ccb872c2e1aa35c4714a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:13 +0000

    brush inside apologizeapologise praise tape

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def reverse_string(s):
+        return s[::-1]
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
