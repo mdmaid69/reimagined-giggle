d1334675d832b76e6ae468c8849cc515736b3a90
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:24 +0000

    our hold disapprove turninto copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
