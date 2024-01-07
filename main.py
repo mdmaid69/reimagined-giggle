1f476b7e380c32101f560d210074372eb338e535
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:51 +0000

    characteristic loud language bite comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
 import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
