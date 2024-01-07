5735dd8e72f2e8d21fcb84a66272c6c232e91738
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:18:05 +0000

    interruption hollow relax form but

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
