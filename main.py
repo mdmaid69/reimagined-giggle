bdded343b713c058a8577adee8f84151b96204a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:10 +0000

    border drink happy respect advanced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
