e8b8f8eda72a568833fa72aff8f55b4d8f572d3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:02:20 +0000

    far arrival depend precise callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
