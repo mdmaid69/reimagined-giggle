c4d1ba65f0231d5ffa1db4ffba6e6864baec7d26
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:30 +0000

    immediately wait tense debt light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
