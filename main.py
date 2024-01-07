2ddf5c39e33498c099df5dc989ea3fbaddf4a477
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:16:30 +0000

    confuse sensitive fetch city listenfor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
