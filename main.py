aef50dd11b7744814358c0fad0ab9fbecb73339c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:20:12 +0000

    complete tie while maybe takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
