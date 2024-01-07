345b970564577a1aadac7353596d86a95bef44e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:20 +0000

    divide dress beat past adjective

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_time_since_epoch():
+        return time.time()
