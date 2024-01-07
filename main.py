009006507d9a82aa4cf047fd1ab0ffb52493ada1
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:41:54 +0000

    remain live equipment week for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
