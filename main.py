a484aba78a6a72aea943e4936f57eead0e6cb909
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:32:45 +0000

    himself gentle rid right lot

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import re
+print(re.match("h.*o", "hello world"))
