fe43c14ad69f99e0894485e3b8d6aef0274fdcee
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:45:41 +0000

    secret typical calloff quick strange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_item(array, i):
+        return array[i]
