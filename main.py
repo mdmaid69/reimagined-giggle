0f26361c90e5527f2995c445e7ffa9b9b1862478
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:11 +0000

    practise stir direct cut speech

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
+def count_words(sentence):
+        return len(sentence.split())
