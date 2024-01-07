be6fce9ec783632e10603067123e9a1ec5ad8db5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:16:05 +0000

    lot recovery earn collection block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
