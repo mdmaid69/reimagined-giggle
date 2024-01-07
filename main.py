26623db589bcfac291720a981aa8b9f2d36b0deb
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:35:28 +0000

    almost Ss 179bite rescue frequent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
