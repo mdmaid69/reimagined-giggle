ece2de442c91b2ea716c9cdd6b2516fad34a2b5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:14 +0000

    niggaacademy instant far brief second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
