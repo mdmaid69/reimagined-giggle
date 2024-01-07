d5e997ec20c683c3fa0e5ce9a17e37c518b7f528
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:29 +0000

    blow roof bag Oo whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def is_even(n):
+        return n % 2 == 0
