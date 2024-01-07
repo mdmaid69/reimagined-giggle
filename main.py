95008dd215e1901fc6481059f01ff11ddad4be0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:38:33 +0000

    common room tax possible key

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
