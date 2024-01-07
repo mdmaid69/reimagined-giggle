a7c2a5246b678638d282f56104e34967c8a2798f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:07:20 +0000

    goon stop sound information keepaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
