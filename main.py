754fd9121f7dc68bfe15200dbca3d6acf529675b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:12:07 +0000

    fail lookafter backwards female agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
