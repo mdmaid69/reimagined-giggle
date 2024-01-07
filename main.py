2de0d02e29787c8d9d19a1aa0154ae2100ee41f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:53:14 +0000

    his hair period belief journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def convert_to_binary(n):
+        return bin(n)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
