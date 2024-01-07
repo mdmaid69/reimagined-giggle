fe4f9374594534bd284aeea0571d0ab76c24bff0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:57 +0000

    later deliberate once copy tooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
