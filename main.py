4d29d97641edab1cd4c05d45e4cb2b34b94cdb55
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:04:58 +0000

    muscle wish zero watch talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
