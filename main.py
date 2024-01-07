aa51ce8fe0bc17de2a93cb0e7a6feea4c7007788
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:40:13 +0000

    effort disappointed cheat cost behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
