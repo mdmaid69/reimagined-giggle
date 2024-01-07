06a7d07ae16830784c3580140138329e183359c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:10:39 +0000

    repeat what yes account Nn

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print([x**2 for x in range(10)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
