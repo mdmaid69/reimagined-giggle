01e8d52e30cfa08dd4e4d6c4836d3e74380b000f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:57 +0000

    anywayanyhow this light weak return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import heapq
 def pop_push_heap(heap, item):
         return heapq.heapreplace(heap, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
