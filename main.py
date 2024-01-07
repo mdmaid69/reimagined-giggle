659bfcd93bfb412acda36e6f90496258a94c2363
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:53 +0000

    argument everybody OKorokay bend criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import numpy as np
+print(np.array([1, 2, 3]))
