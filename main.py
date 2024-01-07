c9c4bff3de2ca0007cb648fa8da8de2c7118d394
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:10:58 +0000

    stone involve breath goodbye community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
