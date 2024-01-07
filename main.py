8c76796476f957f531ccf25bdfbaa1eecd9c565d
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:48 +0000

    fold meal another correction start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
