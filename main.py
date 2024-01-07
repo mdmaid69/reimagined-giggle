d92219cae2e2c80fedf6b7086505ca142c8cab36
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:15:52 +0000

    Ll no upstairs search value

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+def get_current_time():
+        return time.ctime()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
