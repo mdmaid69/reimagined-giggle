e1ba026b8543efb3560cb873c46db89b7bc7e0e8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:48:57 +0000

    formal job protest advice news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
