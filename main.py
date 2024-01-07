9e9fad0690a5bbfb5ffcd988d0cd42768543bbb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:43 +0000

    every book tired obvious slice

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
