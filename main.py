4ad0b341076be849f00c293fbc8cfd366d689816
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:38:19 +0000

    correction dream whether alive improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
