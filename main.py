d285e7a2eed08fd49eb0df741634fefd7087be5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:27 +0000

    worse actually sad turnaway turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
