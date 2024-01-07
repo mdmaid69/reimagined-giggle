f3c97dca463ee6c1be2aad712fce665850b345da
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:24:48 +0000

    protest length guess season wall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
