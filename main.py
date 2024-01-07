4e67f84390ec20bf50a6b198c63e25359700516e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:40:56 +0000

    it goout often offend nor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_even(n):
+        return n % 2 == 0
+def calculate_speed(distance, time):
+        return distance / time
