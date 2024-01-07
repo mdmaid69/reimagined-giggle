4a21d7c4c298cce6884e9e06c961ff085f97c97e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:46:03 +0000

    reply niggaacademy everyone seat trust

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
