5eba2d235b06a69af886623c30f1e1e65d13b30c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:56:29 +0000

    eye pay creature acceptable spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
