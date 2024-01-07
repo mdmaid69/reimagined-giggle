773f380b4d6e8cb43485d5aacde1978b09e3cfbf
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:13 +0000

    attract that estimate capital lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
