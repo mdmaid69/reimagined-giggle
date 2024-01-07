231e61f757b91eb3e4efafeef2f544bbd2404906
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:19 +0000

    talkback use serve middle tear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
