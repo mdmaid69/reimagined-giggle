4b3c4cc00f5bcf13f424368771664025d79f3f95
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:55:54 +0000

    turnoff stare Mm commit break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
