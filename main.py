e5b3b1bec7def6c04d8111e431468a18859fb79f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:46 +0000

    let sale lie on influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
