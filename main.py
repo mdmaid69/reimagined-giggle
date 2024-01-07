0e80c2f0e26754454eeb9c6c70dd8477ef4787c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:49 +0000

    always joke basis increase talkoutof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
