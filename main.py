648bc90fc9ea6c26d780731506230baadbb0e3c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:22:41 +0000

    view square sticky correction hardly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+print([x**2 for x in range(10)])
