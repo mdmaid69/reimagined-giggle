f1a554466bf99f75f15d1a982f124858b9565aff
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:10 +0000

    money sad favourite but Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
