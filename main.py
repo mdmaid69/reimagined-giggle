ac8c472c60194504829d32f4bc28a3cfc8ca6b5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:24:14 +0000

    miss save moral blame boring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
