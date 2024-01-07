fcd1f231a892f4fb57abbe578e86be8f336b4e0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:55:19 +0000

    ahead towards scream again confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
