3e64f8bc38590cee8e7a414237e351a56ac48695
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:56 +0000

    people comeout shadow content capital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
