10de7c3bed683fb1fa7fff4ec9132dd12dcb48cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:37:29 +0000

    become interview custom none buy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
