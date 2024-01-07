a1e2b2518a6657960ce3d1726b1aaea57b01610e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:18 +0000

    impossible minority powerful freedom geton

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-print(random.randint(0, 100))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
