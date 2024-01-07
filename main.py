7cbbe0e7c12a52dd8fd42f9849a86109fee204d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:44 +0000

    soak scrape typical education blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
