1d7fc612e4d98682fa823a22f30bb07261ed452b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:56:26 +0000

    dare play polite land explanation

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
