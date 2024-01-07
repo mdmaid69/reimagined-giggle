92a83d63e8c8c878c82f4031ed3b91ed94bac9c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:55 +0000

    makeinto past cool quickly serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
