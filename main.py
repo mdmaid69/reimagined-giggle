c24e35942b2a1c4dc4cdf45783089d4f575ee76b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:42 +0000

    politics rob my house goout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
