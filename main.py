b18af8f94150b8b9e852564b9fb8f3c1c5448b61
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:46:05 +0000

    thanks wish apparently software alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
