2001f63287bc4279165159d4b27612b20538ebae
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:04:07 +0000

    fire heart other judge speak

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
