2969ac8fa8acf63c4a6ba7978ec9a59f6df0d40c
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:22:28 +0000

    cost thinkthrough would somehow human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
