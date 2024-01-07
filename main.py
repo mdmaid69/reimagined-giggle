83e080717cf2821258e91a2e1f94cbf6646c2b30
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:10:57 +0000

    minority degree excuse force unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def reverse_list(lst):
+        return lst[::-1]
