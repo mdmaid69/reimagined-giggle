f4c348bb91d91bd6e9fd1d91e8d0dd796fe3abba
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:41 +0000

    basically type cross warm dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_index(array, item):
+        return array.index(item)
