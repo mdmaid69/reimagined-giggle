cd945687697ae255914488e99e01077f80682a02
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:18 +0000

    different after distant spot from

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
