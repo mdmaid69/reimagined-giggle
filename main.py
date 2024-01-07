c7ccfbf1d703a12721cc6713ff37f41d742b9cd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:47:18 +0000

    interfere owe chance exciting print

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
