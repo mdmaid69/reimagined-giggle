b3eae322bdf903bb3a396cb799d97ae9ea3668c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:04:07 +0000

    measure sort geton mess similar

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
