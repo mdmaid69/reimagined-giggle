ae5b94b75d6efdb2907458948a91321a29e79207
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:22 +0000

    pretty protection suppose ear skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
