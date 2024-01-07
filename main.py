dc0186ce04d4496abe45448665dd92571240c985
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:42:22 +0000

    too breath situation cook coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
