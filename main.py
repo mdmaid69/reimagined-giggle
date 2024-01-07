949dda7012304f11e63b342234b25de48edd74da
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:31 +0000

    emotion legal healthy general turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
