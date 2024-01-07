a153355f3581d1a3210658d5c17ee6e91906efa5
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:22:09 +0000

    hurry team test suggest contain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
