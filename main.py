1e9e92fc0772abca0c78f2a0c734506a656ee7fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:23:34 +0000

    talkback goaway spray mad illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
