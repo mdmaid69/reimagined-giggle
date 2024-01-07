cb175610b631b4d15d8d653b16c2778d6aab35e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:44 +0000

    sell rest subject shopping yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
