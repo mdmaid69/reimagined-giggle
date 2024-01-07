aec5c2d94de30fa07cc279b7733ea91cf42c73f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:34:57 +0000

    total turn ago squeeze actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
