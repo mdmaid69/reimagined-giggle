0aa722f18b9173783745ea4455519215ce4d37ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:25:37 +0000

    wide blue notice smell operate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import numpy as np
+print(np.array([1, 2, 3]))
