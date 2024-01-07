b309febc85b6f8d134bcba0170e0db23dc64c0f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:16 +0000

    business fire command mixture change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
