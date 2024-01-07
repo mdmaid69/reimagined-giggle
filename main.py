c84a5c55fcc59cdaf772e41069c74847e906fd9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:23 +0000

    spend hour appear method pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
