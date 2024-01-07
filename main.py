404c79d9b9baba0010f1edc7e3219da0e1022d81
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:24:03 +0000

    side attempt suck appeal account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
