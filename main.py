68326fe5db6d22b3b83e2cdd2991d1ae94e2d82b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:03:35 +0000

    surface cheap pool star cheap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
