5866687b88d630c982591df9fa6225a34f68fc84
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:09:36 +0000

    quiet finger treatment party plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
