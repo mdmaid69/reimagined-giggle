581f646510c30f72dff5d7c19efa6a6405acb81a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:19 +0000

    great law check book apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
