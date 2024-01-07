072fa5a3e7930218ef66bd4ba6e07146484a8085
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:06:38 +0000

    class Gg memory long contents

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
