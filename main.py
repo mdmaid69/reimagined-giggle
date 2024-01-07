03b8b3fbe2676e031f3c148e47ec59bf429b80d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:43:45 +0000

    pretend development bored scrap develop

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_max(lst):
+        return max(lst)
