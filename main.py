4d749abdd3b8c1647042e68e779c27843005faa4
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:48:30 +0000

    juice hang connect polite disagree

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
