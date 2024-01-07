340b8bdb7a2abc98a2cb10ac38f67215b56148b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:15:30 +0000

    object nothing runinto book slide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
