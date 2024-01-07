8e65b7dc7f8c84f202f0866217e22eabc0c5d3c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:04:04 +0000

    whenever walk know century artificial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item(array, i):
+        return array[i]
+def cube_number(x):
+        return x**3
