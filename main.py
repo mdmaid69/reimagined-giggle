5fca804ff302efe23f4b44de345374e520825cb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:12 +0000

    out float burn night far

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def sort_list(lst):
+        return sorted(lst)
+import tensorflow as tf
+print(tf.__version__)
