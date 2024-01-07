232e7da549429d4f4855ec09d89fd00100964ec9
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:43:24 +0000

    tough easy couple wheel dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
