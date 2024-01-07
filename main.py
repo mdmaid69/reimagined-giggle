38890de33893c7ef4759a3f903877c3f23323bb9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:31 +0000

    hot limit surprise quiet almost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_list(array):
-        return list(array)
+def multiply_numbers(x, y):
+        return x * y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
