1f489b31973400b6d5c6efc6acd3dfac5681b32e
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:48:02 +0000

    afraid full quiet buy this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import time
+print(time.time())
