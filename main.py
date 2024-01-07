ed18de5ca2a935916216b0b13f5b03a5dc3de84a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:33:10 +0000

    autumn dead however substance age

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import random
-  def generate_random_number(start, end):
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import random
+def generate_random_number(start, end):
         return random.randint(start, end)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
