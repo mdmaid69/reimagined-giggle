aa6d036ead6a1d32d8527f1a4d9dcd5122762b4d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:06 +0000

    practise that kind part heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_set(array):
+        return set(array)
