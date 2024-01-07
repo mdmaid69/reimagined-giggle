57eb5a66e84b193962e18ce9428ed6a1ef27d604
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:51:23 +0000

    combine show false have guilty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def subtract_numbers(x, y):
+        return x - y
