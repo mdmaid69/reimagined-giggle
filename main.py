0f964d010b32a46d833192d6435eb285de32c8d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:37:30 +0000

    please confidence read sing minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
