b0f56444f650baf4cc83799c7249083aad9fa09a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:53 +0000

    awake film coffee unfortunate embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
