9a7a68d568fa842eb719673a26165d8562ffdf95
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:37:33 +0000

    shop touch sting up invitation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
