3f9d779ba9c29f29a2fd7bfc2f8e809ae069a6c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:25 +0000

    fact secret card confidence admire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_pressure(force, area):
+        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
