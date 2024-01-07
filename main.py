07699e114fd04ff3e24befbe5bad4bc031a56f57
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:20 +0000

    book entry value phone talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def print_python_version():
+        return sys.version
