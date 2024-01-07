ff834f66bcf9e058fb4d48da91939cabc6eba756
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:49:33 +0000

    part negative live disgusting flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
