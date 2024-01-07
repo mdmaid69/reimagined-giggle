ae2ff07039347606995dbeb98557892b74365e42
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:54 +0000

    noisy cope behind convincing now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
