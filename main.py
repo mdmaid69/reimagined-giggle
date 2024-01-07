37b91b6e3c3630d95e2b49ddc4c8e67d25314d59
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:36 +0000

    birth seem report type beside

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def convert_to_hex(n):
-        return hex(n)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
