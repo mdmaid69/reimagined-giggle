d9f47ed69837a576485ba0b5c0d7dec9d4f15cca
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:06:10 +0000

    building announce give stamp much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
