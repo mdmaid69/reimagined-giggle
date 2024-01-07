df9c1a11bd08e9189130092b828a98cd73fc58fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:03:43 +0000

    necessary anybody answer way priority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import time
-print(time.time())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def clear_array(array):
+        array *= 0
