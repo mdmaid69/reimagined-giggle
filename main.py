b40ae7b85479bbb0cb5660a7ebe7ff10d03b277d
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:21:14 +0000

    comfortable travel dick television small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def split_path(path):
+        return os.path.split(path)
