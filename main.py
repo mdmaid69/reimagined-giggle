dbc58d83399fdec7cde1b741069bd2e6ce8e0e95
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:18 +0000

    good unable rather empty while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def add_numbers(x, y):
+        return x + y
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
