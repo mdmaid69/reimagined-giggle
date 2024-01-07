89e4600b0ed5f404c4e0ba297fdf804de57a59f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:05:23 +0000

    573entertainment clear everything rest worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
