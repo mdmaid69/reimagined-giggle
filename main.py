19471f44586cbea018656f3557c6e021d6ac432a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:21:01 +0000

    relief strike after offend act

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
