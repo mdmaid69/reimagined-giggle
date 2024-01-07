a04f92fdbe9f3db0ba5ac75b2b047a065c067efc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:39:45 +0000

    connect demand wait unless rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
