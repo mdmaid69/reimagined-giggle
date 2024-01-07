d409b66cefd2749deaf21b24a672529a153e73b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:13 +0000

    unless major description worse frightened

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def count_characters(sentence):
-        return len(sentence)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
