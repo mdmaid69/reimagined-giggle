6c485b274f5c8ecf0d36264b9d439d3f3da87f28
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:07:01 +0000

    only scared cost environment seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
