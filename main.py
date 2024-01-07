ce5dcb154bd0174398d7ff0b0bec8699c8035293
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:34 +0000

    talkback laugh seem remember twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
