ed9ff19d7a8211b6369c4860bc4118b16a57a5cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:37:30 +0000

    difficulty spring answer height group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
