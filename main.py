b16657f3268b57a5a7c2c6400a9f36009674dc06
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:14:12 +0000

    recover sting over medium its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_as_str(array):
+        return str(array)
+def convert_to_octal(n):
+        return oct(n)
