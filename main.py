7441de5af12bb78344bc3f7dcf49abdf7cad7a36
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:23 +0000

    slice beyond 179bite bound usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
