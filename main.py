017d19c48705354f0a04c5971f3735e46ba7adaa
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:25:05 +0000

    broken leg frighten attach mysterious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
