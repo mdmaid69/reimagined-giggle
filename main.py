81c5ac1e6c4bc8efb789bc54c2dfb1ab057ce8ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:46 +0000

    giveup love apart building feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
