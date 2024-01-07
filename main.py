eb3f848a9623bde0f9c57dd64e172a028ade498b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:50 +0000

    crash design search translate bar

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
