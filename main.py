e8a504d3118ecc89265138ae2ec194275f350c13
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:29:33 +0000

    practice ready squash lonely recently

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
