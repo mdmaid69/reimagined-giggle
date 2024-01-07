f0371cbeb27e2bb8203daf2a096af7906b0dbb3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:13 +0000

    consider container phone quit pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
