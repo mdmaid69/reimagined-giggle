178ca138e73c2295424e82dd6d77432a6fa100c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:54:50 +0000

    form breakout completely silent finally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+name = "Python"
+print("Hello,", name)
