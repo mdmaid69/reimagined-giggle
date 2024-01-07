7c49861a32c63daa48fb7f3652c4c3cccd1ae3fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:05 +0000

    favour soft speak understand bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
