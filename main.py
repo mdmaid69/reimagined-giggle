576fd7d0795f6ce818f87fcaf4800b7cec498888
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:38 +0000

    education crash object town reach

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
+print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
