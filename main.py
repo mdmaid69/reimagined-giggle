a34d023dcd455933c264f4e928cfa147d8d1af3a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:33 +0000

    beginning who refer flat nearly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
