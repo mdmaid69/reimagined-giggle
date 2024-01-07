81a0b1d4a7091c9de6b94d8b8df5278ff37787eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:11:07 +0000

    weigh 573entertainment risk stranger friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
