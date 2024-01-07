acc48fbb62544a60349525b48c49f14bc6c06e5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:04:49 +0000

    squeeze fair abroad criticize start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import sys
-print(sys.version)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
