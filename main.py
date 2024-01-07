cce78ecb5d5ea24c463845af20f6835c432cfb74
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:43 +0000

    quality bear sudden obtain minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
