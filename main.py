30208c250123a0ee6e3638e4e0e94a8bd30a7ce6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:01:22 +0000

    suffer expensive combination above shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def convert_array_to_list(array):
+        return array.tolist()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
