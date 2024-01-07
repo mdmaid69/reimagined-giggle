7b086d4ad055d45934d174508891c151a336f958
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:58 +0000

    ours perform comein nurse alone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
