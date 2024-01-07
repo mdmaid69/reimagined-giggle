32af2b05e46e45e9fe816fd3e6f85757093aa503
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:45:48 +0000

    takeback sharp represent let needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
