c3c8cdf08d8e786d6d931b60e288a6eb17d1a34b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:05:51 +0000

    gather shout impossible edge sport

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import sys
+def exit_program():
+        sys.exit()
