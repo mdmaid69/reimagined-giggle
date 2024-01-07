7b7c3bf22207a36bdb43e4e5d88d9cf8ff1c980b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:21:44 +0000

    complicated temper translate shock either

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
