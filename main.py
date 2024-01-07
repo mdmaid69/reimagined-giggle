c4d767a330c2a6d46ebdb5c66e7e1546fd9af05f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:35:59 +0000

    downstairs hate sick mysterious more

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import sys
+  def get_python_version():
+        return sys.version
