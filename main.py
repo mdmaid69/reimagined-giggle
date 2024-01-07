65ae5bcbee6384506824deb512a8a49fe65d52e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:59 +0000

    muscle marry punishment lift anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
