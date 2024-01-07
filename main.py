ffb63cee8b15c49a47b5be439f605b69ac3bf637
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:09 +0000

    doubt get drawing pattern detail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import os
-print(os.getcwd())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
