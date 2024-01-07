05aca67919e747365efce1a0592950b84b293576
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:24:06 +0000

    either sail share public close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
