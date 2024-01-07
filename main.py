592b4726e38b138897778f7930f5110ecfe6e2ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:29 +0000

    all comeover claim shake image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
