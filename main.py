2663d0e1ba15919c87e2ee53134724cba71dd150
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:05 +0000

    extraordinary Uu experience unpleasant talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
