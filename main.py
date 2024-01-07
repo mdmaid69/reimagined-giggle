deecedebbe95f967ed72d555e33f1cc6076db65c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:22:18 +0000

    pause possession will sort young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
