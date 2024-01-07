189c4a89bbf41031a770d40a528321c8eb02739f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:32:51 +0000

    new reason 571enough visit information

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def add_numbers(a, b):
-        return a + b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
