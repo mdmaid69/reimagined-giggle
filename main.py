db4a9a3e4e27be1a741fea67fbfd8e8a34430ef6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:57:44 +0000

    minimum callback snow no accept

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_perpetuity(payment, rate):
+        return payment / rate
