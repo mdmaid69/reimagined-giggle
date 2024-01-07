7802f9dd200f561d82fdc3f79ecc8cc4cad6ca2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:13:04 +0000

    speed substance impressed gift art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
