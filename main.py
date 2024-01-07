88eeb349015971fdf7b5fcfdc237772d1295c1d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:09 +0000

    wet factory contents paper improve

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print(sum(range(10)))
+import math
+print(math.pi)
