9cb864e8a3b70831a642def4b1efb5d46e583bda
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:21:54 +0000

    unusual demand wrong century identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
