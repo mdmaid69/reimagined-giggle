e9ab12b69a28602bff20a9175064f19c6448900b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:28 +0000

    me space exactly argument somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
+import tensorflow as tf
+print(tf.__version__)
