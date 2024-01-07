6a5f9e6991c6ffc48c7a88758539c464a219bd8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:11:24 +0000

    bear full draw colour detail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
