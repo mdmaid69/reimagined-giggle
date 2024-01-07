7577a2edb1e77fac740d5b2e184a054acc41d870
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:06 +0000

    birth unite realize period deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
