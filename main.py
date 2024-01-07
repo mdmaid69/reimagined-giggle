203efd73ed7a2dc858aaedf002a88a7b55e10021
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:10 +0000

    several mind Mm instant whether

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
