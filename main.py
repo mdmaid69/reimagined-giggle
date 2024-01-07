ec4cc78f10a9255a8f471899a9d8e2021a90853d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:14:31 +0000

    chat copy accidental must emphasize

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def cube_number(x):
+        return x**3
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
