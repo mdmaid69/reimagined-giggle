339c56726f77fc2ba45e2a515c305a32d7dac12d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:37:06 +0000

    repeat holdout morning prove concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
