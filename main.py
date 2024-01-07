71ab3480b49a3ddccd36d2395799b368faa19908
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:25:27 +0000

    concerning tax regard damage where

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-for i in range(5):
-        print(i)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
