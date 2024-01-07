39f50ce68991894920c32401f55bc6e711b28e63
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:25 +0000

    advice form lay afterwards fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
