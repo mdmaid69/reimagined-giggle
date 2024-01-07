87ef30f3f308e660c89f59d32eb0c9e64aba3b34
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:08:14 +0000

    fall tone trust border modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
