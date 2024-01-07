a478660e30e663ff262801a05b792768b7c87580
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:28 +0000

    reply several giveback breakfast phone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def square_number(x):
+        return x**2
