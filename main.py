4763d0e0d399f72203cb2f067e4e7fdcef60c0a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:07 +0000

    reply definite lean battery behind

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-import math
-print(math.pi)
-print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
