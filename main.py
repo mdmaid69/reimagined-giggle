b7e802c2f63a7a1797b236ad6a2c56de43c0b27f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:23:44 +0000

    row shoot whether common remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
