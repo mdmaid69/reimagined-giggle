ba4f9445fcab19d59c5b9622cdb839b3c8c1cf6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:31:39 +0000

    remark believe culture wash start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
