aecb04f0378b56ac2fca608edfbf84fe9774ce80
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:41:10 +0000

    before frightened apply suggest fast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
