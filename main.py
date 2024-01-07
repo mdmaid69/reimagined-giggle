84127193a45a5ffe222e1454991ea5378e26ab58
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:26:58 +0000

    bread cook 569enjoy measure interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
