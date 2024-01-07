0a913ae16cb1b901c6b74ae15d7ad715eb5c0945
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:36 +0000

    unhappy from sink test maybe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
