2b4ebd2c5887140530f92b3179bc57c7880a6245
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:20 +0000

    ordinary issue division matter yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def find_max(numbers):
+        return max(numbers)
+import os
+def change_working_directory(path):
+        os.chdir(path)
