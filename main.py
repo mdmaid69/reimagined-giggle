dd8061d34e57f37e388aa698912e8c00b0a4070b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:53:12 +0000

    slice takeout advanced direction dinner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
