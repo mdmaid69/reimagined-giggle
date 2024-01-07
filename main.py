0bf0819f9fcc8d2b86b54e9a801b50bc5851b551
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:50:55 +0000

    workupto uncomfortable rush brain recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
+  def multiply_numbers(x, y):
         return x * y
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
