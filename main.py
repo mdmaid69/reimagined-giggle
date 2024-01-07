ade47ad1bab41e621bb3f9ea66fc41824d90bc02
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:29:41 +0000

    identify print sail influence guess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_elements(lst):
+        return len(lst)
