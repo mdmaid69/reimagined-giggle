4967acc74e92714b1c0216a4cea7d289c41a6706
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:57:39 +0000

    whistle across threaten change through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def print_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
