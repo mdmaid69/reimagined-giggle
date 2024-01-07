ba22d90fe59f21d20cf487f344324c1565d2b909
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:04 +0000

    hate teach thinkthrough include charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sine(x):
-        return math.sin(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_list_from_array(array):
+        return array.tolist()
