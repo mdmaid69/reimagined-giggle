045d9c44cc903016b19348f3b8ddbc53fde2b28c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:30 +0000

    ought leg extraordinary shallow getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
