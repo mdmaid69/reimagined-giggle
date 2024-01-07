dd123f12eaca7a44dcc0dbb20061d80b4e6ef782
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:00:24 +0000

    high body drive real mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import sys
-def exit_program():
-        sys.exit()
+def print_python_version():
+        print(sys.version)
