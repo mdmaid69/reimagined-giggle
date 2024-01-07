ac69a8db1503dc65a574862a1799acee19ae9f04
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:25:16 +0000

    separate row pair coat from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
