f006e0d804a139876ef36943e1075172ad057f4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:16 +0000

    either flow drive negative shout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
