3bc050d28aca496235e9c9a47a3f0a66f60f6877
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:29:00 +0000

    recently force afford trap foreign

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
