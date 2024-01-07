05dc0805c81a2fced13f3b049b6ba092ad5943f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:18 +0000

    bath characteristic Pp reliable across

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
