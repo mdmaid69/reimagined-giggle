8581a77ad6fce1c8d93ccd117931117c1c337430
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:20:16 +0000

    crash afternoon environment Oo my

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
