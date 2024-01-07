2c801c51cee7ea333484d59026204cd144b549d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:12:56 +0000

    typical drive line until start

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
