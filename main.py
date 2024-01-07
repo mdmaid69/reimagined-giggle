a3a859715e6f184b7eab40c255781494b13b3d56
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:47:06 +0000

    because food behind children advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
