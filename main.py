2eb2c2d56e3247ccc39247bae86fe8306e4a586e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:11:34 +0000

    dress children neither action real

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
