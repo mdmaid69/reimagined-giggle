ed65e11ee81f9b14887d00d87dc0384acfb345d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:38 +0000

    pour spot getout collect claim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
