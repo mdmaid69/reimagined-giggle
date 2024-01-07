106b4cd64879f1537c1ee54ec7d4f5a732c8eb10
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:13 +0000

    brush hospital free arrival nowhere

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def remove_duplicates(lst):
+        return list(set(lst))
