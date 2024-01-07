111cad54656d8031087d9efe5fe67a492415588c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:32:58 +0000

    married area hair unfortunate nothing

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
