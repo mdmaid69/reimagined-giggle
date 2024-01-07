c1c0aee45eed6e666991130211615d3cd461940f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:00:19 +0000

    damage turn own poison thirsty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
