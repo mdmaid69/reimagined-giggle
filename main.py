39893d80e1f899c684969368a917480e6831285d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:01 +0000

    lookback(on) hers popular apart below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
