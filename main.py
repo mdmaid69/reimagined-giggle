e3d49824e3a33a905bf0a1ee6d1bff0515721476
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:53:19 +0000

    punishment exciting order be distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
