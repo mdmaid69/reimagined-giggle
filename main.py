8a0b6458b5b36c531056f9c525b8089213d359bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:19 +0000

    price funny fork large worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
 def calculate_power(work, time):
         return work / time
