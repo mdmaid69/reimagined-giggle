8b0ccc6a648652df7e6ba6c65dcdc2dcf4f0d4c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:35:45 +0000

    reduce hurt report grow march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
