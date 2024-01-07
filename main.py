62140eb2cc062a92c1851e567b49da3af5990ef5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:35:52 +0000

    lift movement exam unite centimetre

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import getpass
-def get_username():
-        return getpass.getuser()
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
