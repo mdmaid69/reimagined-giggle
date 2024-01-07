1c718414cbd67d3d05984aeeee9d266c6d3cb702
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:36 +0000

    star should criticize image lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_speed(distance, time):
+        return distance / time
