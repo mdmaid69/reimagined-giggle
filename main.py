b8337bfefd911b84542feff5524135617d31da48
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:45 +0000

    time family every pressure male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
