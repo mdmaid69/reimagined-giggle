cb85651ecc16266ea08958b0a252c7989d54ca11
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:35 +0000

    immediate quantity everybody second step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
