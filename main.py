08bfe36e9733ccd39b7e5133324b8e6de999f642
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:36:03 +0000

    whistle same slip whistle minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
