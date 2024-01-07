28ddece70e4c082be34e56cebc089eb6605d5bc7
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:21:28 +0000

    priority race honest future winter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_list(array):
+        return list(array)
