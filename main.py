2e730eabd110a067c68c8bfe44e15cedb0f76644
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:45:44 +0000

    list callup both mind study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
