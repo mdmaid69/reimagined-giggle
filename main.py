24ca6adc404d85f0596e550276010c3002f2ce3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:46:23 +0000

    for form forward along suggest

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_bytearray(array):
+        return bytearray(array)
