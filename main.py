2fa3e750c4d037024b21294dd0e888574e6b85e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:40:48 +0000

    joke later father thought national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
