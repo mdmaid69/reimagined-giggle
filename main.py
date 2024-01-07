5dc15877c73849023f9f8ec38af7bfab6c6319db
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:43 +0000

    beyond doctor concentrate there write

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
