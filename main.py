b6e1e2b5e047394e4574cf512654c999069a8c4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:19:47 +0000

    winter talkback consider 565end connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
