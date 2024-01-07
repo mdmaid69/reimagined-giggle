dba2408c2f2ed11cd076232646973badd5b68528
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:27:30 +0000

    official further so tomorrow exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def convert_to_binary(n):
+        return bin(n)
