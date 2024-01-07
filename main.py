e02deb00567fd174846823a18596ee5e0a473ffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:05:40 +0000

    exactly return love expensive ago

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
