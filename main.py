966d3e998650187e602e904677f18142661fb817
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:29:07 +0000

    practical string equal recently low

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import time
-def get_current_time():
-        return time.time()
+def reverse_string(s):
+        return s[::-1]
+  def is_odd(n):
+        return n % 2 != 0
