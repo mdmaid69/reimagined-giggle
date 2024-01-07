4e195ca87ee4276dc94861f316f02a5f4a292347
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:30:00 +0000

    inquire tall actually belong too

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_words(sentence):
+        return len(sentence.split())
