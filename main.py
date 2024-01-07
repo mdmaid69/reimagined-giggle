af70e592627447196aad22f76f16e40759f0f810
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:39:48 +0000

    sensible effort confident something pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print([x**2 for x in range(10)])
