7978b39a969b7f2de2db5af4c1abc65954751f5a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:54:44 +0000

    away muscle tradition beat confusing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def add_numbers(a, b):
+        return a + b
