17ad7d1e75b4a589d4a38feb1b5cff87880b8d58
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:08:11 +0000

    land sensible contribute quickly shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
