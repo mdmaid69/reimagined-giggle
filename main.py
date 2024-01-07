f0898e5184e17adbdc005bf1bedefc27b604a6ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:04 +0000

    leave reasonable single shine upwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
