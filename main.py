102e9c568e39e7d578f694d6f7fddafbca7132e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:33 +0000

    muscle bread slide modern last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
