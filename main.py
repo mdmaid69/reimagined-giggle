ab439acb664cfc4914fff48ea526c642b9c05d9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:51:18 +0000

    opportunity may promise unkind name

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
