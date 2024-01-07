dcf7caed8e7e2484a43964bd51c907ad182d37e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:50:32 +0000

    own bus uponon odd runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def divide_numbers(x, y):
+        return x / y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
