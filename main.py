0866acb57398a7be0646c7da022ffb7b6e254c7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:16:39 +0000

    plain flat advanced day total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def add_numbers(x, y):
+        return x + y
