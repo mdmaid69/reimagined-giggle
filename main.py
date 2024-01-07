ecc1c7129ac134ef9ca5099f395b67d0683c833c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:55:35 +0000

    calmly truth letter shopping near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
