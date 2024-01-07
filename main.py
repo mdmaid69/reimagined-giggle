e6ce02b6bda2277534a6c556b46225f2ad4efa61
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:55:37 +0000

    sleep prepare passenger taste sharp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
