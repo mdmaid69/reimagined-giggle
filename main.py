ae3db7c7b6931d96ff62742823ad66a63a9ee085
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:21:56 +0000

    turndown support plenty choose birthday

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-print(os.getcwd())
+def set_array_item(array, i, item):
+        array[i] = item
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
