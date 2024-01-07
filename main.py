89018aa1990cc28e3e3662d049819d6790dbf42d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:48:50 +0000

    rid shelter breakout careful fortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
