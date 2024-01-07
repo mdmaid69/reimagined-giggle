7ec82bf6591a3b9d75d516c2f3c71a5441d86030
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:40:35 +0000

    spite control post high eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
