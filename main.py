a7ad535187109e465b3ddc6aa530d9a495951b98
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:30:49 +0000

    exact cough yours Itself lend

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
