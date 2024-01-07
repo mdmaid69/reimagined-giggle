065e160121e3b3629d9c75014af452beb871283a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:45:02 +0000

    vote chief better ours terrible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
