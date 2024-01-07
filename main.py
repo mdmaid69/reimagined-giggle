10ee59339f7dc30ee9a1524a4c2b0fe3c78823cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:19:00 +0000

    pencil shake pain weigh change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
