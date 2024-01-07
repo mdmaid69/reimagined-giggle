042fb81059f17ed2fe87a467f0be7ebbfb0d4d08
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:09:13 +0000

    rain previous wonder save for

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def convert_to_octal(n):
+        return oct(n)
