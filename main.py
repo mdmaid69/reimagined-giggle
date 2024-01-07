d5ae130c3b288c2a2f3d0f8512e99e2ab8111a04
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:35:38 +0000

    likely border 564encourage word swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
