6b5fac6585771938eaa87e92e7cdd6d395ff141e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:17:25 +0000

    broad ought wide appeal my

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
