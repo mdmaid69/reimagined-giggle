2fc958807b80e761976a6d5aec6298a9b804a485
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:51 +0000

    flavour damage consult start fill

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def square_number(x):
-        return x**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
