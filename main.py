d7e6c56bc64da188a3ee46f53418974749dce524
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:55:44 +0000

    content comedown fill my estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
