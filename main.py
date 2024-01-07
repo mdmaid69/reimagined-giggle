9d19e12c88ffa18fb4beb05594d8cd34fe41e7e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:34:57 +0000

    surprising thing split steer escape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
