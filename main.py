f3395a43237990fd9660ead16f438caf45470c47
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:26:39 +0000

    produce advice amount arrive sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
