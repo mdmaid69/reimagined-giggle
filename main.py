cee97c940869cef6db683011c5161f2c8646a795
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:08:46 +0000

    activity blow soil mend manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
