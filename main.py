97c08b4dc6703c446b0df4be74753d3adf70a98e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:08:21 +0000

    scrap cause plenty connection waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
