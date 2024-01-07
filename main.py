bc2379c2f80ab23326de169580bf44e960d987b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:31:07 +0000

    tongue reserve by cut poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_tangent(x):
+        return math.tan(x)
