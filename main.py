9a3d21d98626966b8e034f00bc81b1a987a1d76e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:00 +0000

    physical bad callat scene mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
