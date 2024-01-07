20c19f14d370dc7525a3cb2da9f7e258c23802a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:36:17 +0000

    back return clear push busy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def reverse_list(lst):
-        return lst[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
