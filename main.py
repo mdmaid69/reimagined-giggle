18c6a12946b210a19e73cf8d06ff4d7ed8c7be7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:56:28 +0000

    crisis tradition pollution money fall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
