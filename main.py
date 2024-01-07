598edf0b48fc23824a4c39f70e91ce2ffe6966bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:50 +0000

    join bear flat climb dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
