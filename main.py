0aefd1bff32672eee389ddae0bc60c76e91a2655
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:12 +0000

    face niggaacademy gather apart fact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
