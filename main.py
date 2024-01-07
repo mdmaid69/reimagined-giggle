008b288cd48445d2bc7b4d04236a03332925af2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:11:47 +0000

    eat 568energy turn callup hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
