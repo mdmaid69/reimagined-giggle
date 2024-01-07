49301bfe7b8b67669b3156da6a887c5aec468cf9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:13:59 +0000

    long arm bang model spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
