3f5c76b1ba52994a2f082f8bf44642dce0698351
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:05 +0000

    mess smooth usual bear share

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
