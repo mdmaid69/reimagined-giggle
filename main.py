48e21000e1b751ed2178fcd55c369f6c1105459a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:15 +0000

    zero stand wish few across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
