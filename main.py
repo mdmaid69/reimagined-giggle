3255ff0645cee08de447aeabf35e06117cb62fae
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:47:29 +0000

    violent exit basically characteristic over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
