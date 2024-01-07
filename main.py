e11059dbb8e18eeb8bd5ad4ab3ea08de026d16ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:02:59 +0000

    communicate need ought free side

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
