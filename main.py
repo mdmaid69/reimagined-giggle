758834c0c1b53d10acff8c3e8902a457f966424c
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:33:33 +0000

    Tt common disapprove copy this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
