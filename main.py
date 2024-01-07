01647f382d71197bb6ea8c4d890733b61a262ad7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:35:41 +0000

    giveback love building rough rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
