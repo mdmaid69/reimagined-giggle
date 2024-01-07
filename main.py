e23bc4d7e69c3e0a2abaebf594e606184c8e5415
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:21:46 +0000

    machine survive share face unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
