268cb5cd2d0025a53ca849554a3b3ab9dd677b5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:55:59 +0000

    phone post medium right polite

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
