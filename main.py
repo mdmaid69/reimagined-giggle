6395fafe11f0f97e6d3ff4d092e5cae60ee6ebb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:30:46 +0000

    goup unlucky tip cry since

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
