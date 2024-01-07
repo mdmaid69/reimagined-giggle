4aa22fb825f3390557fe5306f997c68f2efbe823
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:58:16 +0000

    basic height getback concern work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
