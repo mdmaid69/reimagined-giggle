f4aec0dc89de14666b41ded16c234b882624ee71
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:26:13 +0000

    reject bus over sweep happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def subtract_numbers(x, y):
+        return x - y
