e8c55cd360549ad0348309fad86c0cccaca865b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:46:57 +0000

    blame intend healthy none announce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def reverse_list(lst):
+        return lst[::-1]
