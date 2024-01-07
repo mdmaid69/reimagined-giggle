90b5ea2b78316f53cc29cc0410fd1ae521a81ef6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:40:12 +0000

    speed breath play permission process

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
