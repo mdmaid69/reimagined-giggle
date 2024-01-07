ab16fa67b4e7f02c835e732192f2d45e279dd8d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:27:48 +0000

    free callup stick fly temper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import os
+def get_current_working_directory():
+        return os.getcwd()
