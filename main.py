cc544bae0850474eb7223aabfb8c5272f9b8b644
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:38 +0000

    comedown aim lot band bill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
