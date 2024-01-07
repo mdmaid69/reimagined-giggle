833667f4b8d07b7169bda3c06d21b7c72c84efa2
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:28 +0000

    organization hurt opponent loss leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
