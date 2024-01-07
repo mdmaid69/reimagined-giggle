ec069af8eeec6679114f901e4eb5b1736f8eaac3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:16:41 +0000

    crazy without fire side bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
