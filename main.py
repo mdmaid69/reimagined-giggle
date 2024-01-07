392754166be6d8390fef8ebc5b76e4c63b3d849d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:27:55 +0000

    sort ever run Hh funny

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def reverse_list(lst):
+        return lst[::-1]
