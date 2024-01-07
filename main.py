e7f940d4cc5e57eb6cf20c6a1f085e28dd1ccc7f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:06:39 +0000

    blame character partner help friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def log_message(message):
+        logging.info(message)
