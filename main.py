620985bf283e491514d042c29d2b919b100335f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:57:56 +0000

    steam neither real once nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
