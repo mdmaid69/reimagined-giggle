659281dda3fe1548f837bd385a4a48f57c0d876e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:42 +0000

    unfortunate easy level mood dare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import platform
+def get_os_info():
+        return platform.uname()
