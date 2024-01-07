53318d1bac6048081c93a688d411e229307951d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:41:40 +0000

    direction hollow fear common separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+print("Hello, world!")
