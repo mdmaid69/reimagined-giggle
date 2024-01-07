23889ee645f65c26d445c85ca62006463277d8e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:45:41 +0000

    so mix survive time concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
