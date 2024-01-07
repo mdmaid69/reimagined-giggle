29113d3125f741d4110db1bbfacea9ca6f060b12
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:23:02 +0000

    recognize characteristic learn owe ring

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
