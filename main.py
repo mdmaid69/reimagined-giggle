8300259da4d6f3399f19cfebec190cd4487b708a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:27:21 +0000

    nice centre piece Kk sting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print("Hello, world!")
