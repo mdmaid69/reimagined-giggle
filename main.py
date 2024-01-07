fc595812d4a2c895c6ef6ec798214f62a1fc09d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:11 +0000

    left load recently right basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
