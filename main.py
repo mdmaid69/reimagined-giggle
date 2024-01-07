0ea6ec7315be1e6ed1c51d8c664feb933d18f055
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:46:29 +0000

    war fork ashamed arm grateful

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_density(mass, volume):
-        return mass / volume
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
