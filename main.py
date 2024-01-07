7d83d2f637206a4902a0c5dc3a006da52cc1e8a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:41:28 +0000

    apologizeapologise if giveback such present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
