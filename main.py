b20117ede154f37f4fff37d77ad2bf55fc02f067
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:20 +0000

    swimming definite mess listen possibly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
