6e3c3b50fed9536fd646a3d5fa279d3df0f11e9b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:01 +0000

    sit regard rob point emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
