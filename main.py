d581a250965a3c9699baf8344853f577d3a6fcff
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:44:17 +0000

    survive down worth sky breakup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
