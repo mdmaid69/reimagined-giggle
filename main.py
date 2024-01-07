1541d12a2f23afaa953c00db9904a73b39e3f1b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:35:48 +0000

    defend breakfast criticize height passenger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
