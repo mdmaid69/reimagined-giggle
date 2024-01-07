ba7bb8b36a36df5367bb7d127100598e046ecc32
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:27 +0000

    value sleep disapprove exact discovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def square_number(x):
+        return x**2
