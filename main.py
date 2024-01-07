51535ccf1a288bf2306629cdc3b343bfc3a89cb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:54:53 +0000

    trap hang dangerous 570enjoyable spring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
