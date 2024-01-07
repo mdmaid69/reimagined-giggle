75287ff800fb679233e872c20f9b22949e8879ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:16:37 +0000

    goodbye right anyone clear yet

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
