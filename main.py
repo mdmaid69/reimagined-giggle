614c430584aba889ae55a0bf1d5cc4e956abb456
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:26:08 +0000

    produce unable unkind forever possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-print(datetime.datetime.now())
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
