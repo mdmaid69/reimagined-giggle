67eecf977199daa50940ddf508615c02aa117398
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:59:28 +0000

    petrol woman serve quit cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
