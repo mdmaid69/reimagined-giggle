e67695b1a4006523a2fe273e7efdf6faba0f5566
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:53 +0000

    attach tip experience arrange above

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
