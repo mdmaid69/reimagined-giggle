a0823a16ff49a17ffcd48e51ee9a17974563a961
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:36:51 +0000

    let identify between soil hurt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
