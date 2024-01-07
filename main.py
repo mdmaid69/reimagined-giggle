0b589b075f573caf1db3c888941fc593f578c67f
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:37:39 +0000

    close emergency politics protect treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
