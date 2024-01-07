d0e793796bfbe2bb44b7cd3c7a66ec3a731519de
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:02 +0000

    favour put service soil bridge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
