cdb4c76e74bf8a7b9bfd6a57174574bbdf1f506a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:16:49 +0000

    classroom age inquire home condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_floor(x):
+        return math.floor(x)
