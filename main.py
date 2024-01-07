945a1e06c4ffb05f20ac109f99323a7e86c41fd5
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:35:07 +0000

    remember blame tidy finger pair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
