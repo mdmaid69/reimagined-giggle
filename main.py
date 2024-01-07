88d424592168beace7a20d08540dda76ebde1bbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:39 +0000

    able extraordinary off win immediately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
