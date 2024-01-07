25ac38cd6fab3bfa2b6ef2a2f3be1e9f07dd1cca
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:51 +0000

    achieve allow period anything understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+print(sys.version)
