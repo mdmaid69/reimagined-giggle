6b5603ce9f7bc0d9ef74e37968ce3c531eb3504c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:26:58 +0000

    choose slide show complain style

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
