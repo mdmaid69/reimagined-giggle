bf1ab8cc6ca8964f20df45d91b2abff1eb83b3ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:04:59 +0000

    possession thumb consult measure rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
