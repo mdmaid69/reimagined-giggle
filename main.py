1b8d0b0b25e3c746d568d0bf3d7f3bf6d892dfd5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:16:43 +0000

    admire herself row perhaps pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
