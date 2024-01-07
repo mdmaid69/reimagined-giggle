5e2cfe406205d84b7d06dd453fb736d0a5698b99
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:07 +0000

    hurry possibility lesson earn print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
