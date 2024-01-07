f4cf49feba24e4fa233ec0a134d72c6e5540c337
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:47:09 +0000

    brief thirsty smile rush board

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def sort_list(lst):
+        return sorted(lst)
