9bc796da6871b4d379f174d9086bc8411b3b48d1
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:37:49 +0000

    car pretend store flavour disturb

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
