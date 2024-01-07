a72d3323085162a8072fc14e79958a08a1c82082
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:56:07 +0000

    reserve country bound fierce otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def append_to_array(array, item):
+        array.append(item)
+  def is_odd(n):
+        return n % 2 != 0
