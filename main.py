41e030339fc6f7988613ed95e610d2bc9a7a4073
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:33 +0000

    upset successful spin remark clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
