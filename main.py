06f9a5b1d5a1f330cd53ed568a6214da1f2b49a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:28 +0000

    phone standard value face convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
