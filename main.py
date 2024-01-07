45670b7b5ce67f4333d2879766eafd62b7f1f2ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:41:33 +0000

    some interview I yesterday refuse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
