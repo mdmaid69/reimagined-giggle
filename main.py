4bc826e68938009c9c20c7b887d6df7957791ddb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:36 +0000

    spare depend waste injury holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-from collections import Counter
-print(Counter("hello world"))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
