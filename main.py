ec4054ab4fd7403e26a34d74ecae867e0073fe34
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:38:31 +0000

    accidentally miss certainly immediately single

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
