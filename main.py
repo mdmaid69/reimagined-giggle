c1d17e0d65898dee00d1b6cc2ff97151bc4bb182
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:21:44 +0000

    hate at near journey punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
