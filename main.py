7a7467813a4724d9885628b036fbf9af94a09e24
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:02:52 +0000

    plain shall hospital callat certainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import sys
-  def get_python_version():
-        return sys.version
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
