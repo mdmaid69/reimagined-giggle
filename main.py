7e1520130e418f378d5d1aea24b0ad4ee755327d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:47:45 +0000

    definite married entry put container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_ceiling(x):
+        return math.ceil(x)
