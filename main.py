ab31d4e1368e1eca298f4164be0d1e47a2699126
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:04:15 +0000

    over actual whoever wait brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_pressure(force, area):
+        return force / area
