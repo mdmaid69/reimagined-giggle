e94d191b7907ca8766b8e695094afe6570308676
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:22:42 +0000

    drive children admire steal belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
