2d5ca5a1b7c14dafd138f7a783e98a091bfea454
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:02:57 +0000

    mind usually power smart blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import time
+def get_current_time():
+        return time.ctime()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
