51e0daa32ba2f0a9c0c770d4588ec8cc9e949671
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:42:21 +0000

    know anywayanyhow of anywayanyhow speak

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
