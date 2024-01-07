9e94ad5198ef82dc9a3824692072c10b5453214e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:56:41 +0000

    married motion tip dinner second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
