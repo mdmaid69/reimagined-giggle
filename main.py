d37d0cd0820a908d212f287a1db75a90bd228257
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:18:17 +0000

    country tired shout account else

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
