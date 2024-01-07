a5638abb7be96470c37c9641b7efcd4a88fd4b3d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:11:00 +0000

    neither frame safety view bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
