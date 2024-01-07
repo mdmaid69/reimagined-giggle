6afa33009c98276250bbdf440fb8caf403f88458
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:02:40 +0000

    practical change near decision issue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
