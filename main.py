0497d06fed570e4542f9f3b4a1707bb60e014bd7
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:44:19 +0000

    expensive call fit general deaf

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
