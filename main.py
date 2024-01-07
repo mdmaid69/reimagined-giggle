882dc42fad267a3077050f59fa20c12d84f3858d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:59:05 +0000

    dress lend face reaction dare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
