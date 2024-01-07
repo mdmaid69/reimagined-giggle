3b00b1e61537c306c0142d633d1c39c6b2050578
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:04 +0000

    sign low tough fortunately divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import os
+print(os.getcwd())
