517b4adf4ed528ac1e5279fe2b37e7c981336e3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:30 +0000

    brush promise trust common on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_power(work, time):
+        return work / time
