0e657d0324176aab47735c66cf8dddbbca19c991
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:44 +0000

    design snow voice as curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
