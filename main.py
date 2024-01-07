63c334090e6a84fbff946e9e43c414656821d163
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:59:21 +0000

    prefer correction score Pp clue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
