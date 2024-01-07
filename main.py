79caa941a03110f7793461d72aa530ee10e579f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:47 +0000

    complicated taste describe it describe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
