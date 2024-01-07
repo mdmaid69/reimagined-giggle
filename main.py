7f7cd33e2a83deb33303e07aeff9b79198ab7c1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:28:29 +0000

    however turn central people home

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
