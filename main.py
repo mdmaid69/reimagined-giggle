e04b30f2c963d42c72f75fef02f88d11c27807de
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:30 +0000

    onto connected turnover exist always

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def reverse_string(s):
-        return s[::-1]
-for i in range(5):
-        print(i)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
