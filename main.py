88c86e916a5dbe52f0625e98e6e8312d449ff210
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:09:38 +0000

    me power fence basis crazy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
