e05d188e52ed62a03eebb0d7cd0c0f874cde2d11
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:28:21 +0000

    holdout trip same towards cope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
