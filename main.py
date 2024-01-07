176a82f0258bd9673abc033775f2ace31058fdd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:19 +0000

    go all fire height relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
