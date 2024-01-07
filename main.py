d9d9a1a24e4c11cc1167b01ff369428612f8ebff
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:24:05 +0000

    give bad capital left second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
