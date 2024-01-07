0747f83706d4783c624572044562c5ef57832d31
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:06:22 +0000

    learn patience method power cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_speed(distance, time):
-        return distance / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
