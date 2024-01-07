225d76a4d39c24a438d6758ba374d738a37691a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:48:12 +0000

    unpleasant everybody degree communicate cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
