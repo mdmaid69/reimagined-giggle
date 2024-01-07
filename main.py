526ef88fd1cccbc23633d8def787d2cce046770a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:25 +0000

    force friend report zero uncertain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(lst):
-        return max(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
