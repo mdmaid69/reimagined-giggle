df06a0553644228f55965321b7d8f10001fdf374
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:24 +0000

    godown shake achieve just winter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
