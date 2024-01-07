afd6f8794e888743cbdb0f09d007b39837f3e5b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:17:48 +0000

    left deaf broad loud call

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+print([x**2 for x in range(10)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
