90be250016a5b314d55a5103f779fe3d7b592cdd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:19 +0000

    attach unfair somewhere job common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def count_elements(lst):
-        return len(lst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
