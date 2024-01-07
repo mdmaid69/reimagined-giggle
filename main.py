6bedcf3218d804a7ef2d92d498ee396e76864730
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:45:23 +0000

    night copy skirt burst with

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
