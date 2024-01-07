a6080418cf46f45f8cea41350df5d94ebf24c94b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:07:08 +0000

    team clearly cheap shoulder delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
