46127a9b4b8120a518446e3248d42233997a7b34
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:08:58 +0000

    buy breathe balance stand occasion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  def reverse_list(lst):
-        return lst[::-1]
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
