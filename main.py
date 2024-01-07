fa2f2152ec2dfcd2d5558f6ba9e5de661fc84e81
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:59:45 +0000

    live judge our suck cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
