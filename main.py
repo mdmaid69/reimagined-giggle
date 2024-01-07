9cd703835dc45b87db56e4ccb823e38229480431
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:47:41 +0000

    polite blow means Ss something

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def convert_to_octal(n):
+        return oct(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
