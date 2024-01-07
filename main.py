b80e4bd285373a805702642d45ae55a89f936bfd
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:17:20 +0000

    mind mainly quantity catch neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
