95e90f0f53d92d212b00a1d518009cf8c2d849c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:19:30 +0000

    proper pretty name compete moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
