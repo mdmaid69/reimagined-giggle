f2807bc24e27383723f052fa5a7348b748a3fbb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:55 +0000

    confident swim loud wide tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
