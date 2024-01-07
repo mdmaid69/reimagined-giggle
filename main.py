b45086cc85992d22534df7b79599a52063a51bc3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:12:49 +0000

    cheque block middle although choose

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def find_unique_words(sentence):
+        return set(sentence.split())
