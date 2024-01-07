699c9c50cdebc6844bb997cf7c505340bd36e231
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:27:06 +0000

    land reject name our late

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
