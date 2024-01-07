d3d7c67e83a8ad041461f6330798d0b9de20125e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:22:09 +0000

    pollution software manner pencil each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
