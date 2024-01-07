ff46f2bb984d426cd5875d8f4bef8b2908ca5f4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:39:01 +0000

    aware runaway result interfere smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
