05da04c6628b689466c5724e8959f018fb638fef
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:19:40 +0000

    fail service touch seriously target

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(x, y):
-        return x + y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
