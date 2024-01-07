8e1227b2cd7e17f8cedae4dcdaf726b61641e923
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:21 +0000

    press load provide anxious experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
