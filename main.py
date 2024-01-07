cdd048903dbbc97681583d82694f6cce741b8046
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:11 +0000

    breath kick read silent gooff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 def convert_to_binary(n):
         return bin(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
