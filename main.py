2a69d1864bfcb2f089b814c04a3aa51cc9b5446b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:14:12 +0000

    satisfactory think likely hit fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
