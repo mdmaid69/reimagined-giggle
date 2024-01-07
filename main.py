0c76935c322c5c1580c44e39cf3eeed9b7bead4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:25:04 +0000

    tired satisfied lead dig store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
