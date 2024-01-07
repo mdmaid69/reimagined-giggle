b3e7be2ef81bc551ee4c76c87789f247ca6f9c46
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:18:36 +0000

    upwards question capital damage buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
