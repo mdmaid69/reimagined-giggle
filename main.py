7dbdf580c653aa0dfb3aaea908d3a9e73fbe244b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:07:51 +0000

    married unite fight emergency repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
