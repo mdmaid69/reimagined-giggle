106606504fee4a41da7afc62f7750889cecf7c7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:32 +0000

    period trick criticize estimate priority

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
