c2366463522552e4f729567768d74be19a558e39
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:55:06 +0000

    quarter woman leave store goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
