e15d937d9764cfd49857f8e9aab3ca6a89d9c64f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:11 +0000

    practice nonsense principle she stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
