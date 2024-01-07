6a1ff7b887340d3cb65303ffbfed52d8564e0cfa
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:34:33 +0000

    smoke computer turnaway process before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_speed(distance, time):
+        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
