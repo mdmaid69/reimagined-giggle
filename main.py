60201fe43cdccfbea6ecbd25ea18849c965589e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:43:03 +0000

    vote cruel instead sick murder

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
