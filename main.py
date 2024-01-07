94a2cae6439f14b0f5d7ce58381a1ea67fa8f429
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:59:50 +0000

    turn takeout develop pass properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
