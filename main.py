ad27b66b59383ad61a146c73f030017ed3ac50ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:25 +0000

    after reduce material direct quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def square_number(x):
+        return x**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
