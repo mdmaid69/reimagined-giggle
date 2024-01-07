05ce378273b8cf66cbf2c8de3ae41b16230b52f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:35 +0000

    spot flight wave past comeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
