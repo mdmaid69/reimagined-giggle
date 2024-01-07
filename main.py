e1b7a3e150c4e771e71a169e04acc16607ef165e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:38:10 +0000

    vote rest introduction centre off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
