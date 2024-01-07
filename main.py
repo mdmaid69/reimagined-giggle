c8987e958f57040d76b3907459763dff0e6e9da8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:30 +0000

    tap imagine polite fence wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_tangent(x):
+        return math.tan(x)
