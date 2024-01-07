74c44bc0e470b4c3d7db83fd3ca77a783309ac79
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:20:43 +0000

    comment Jj opponent unlucky television

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
