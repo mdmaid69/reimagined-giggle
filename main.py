7eb9a01da8c8185a77d4d363938ce2f8fad3db8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:59 +0000

    sign makeinto judge offer concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
+def find_max(lst):
+        return max(lst)
