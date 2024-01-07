8b807a2b9d06a9b6c09c2acddaff5570eddb541a
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:19:21 +0000

    second makeup reply steam situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
