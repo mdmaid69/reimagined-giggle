93c77cb593cfe477183255bc6a1020da7bdb1da5
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:28:03 +0000

    ask air thick laugh 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
