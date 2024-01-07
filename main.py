dabbbe4e85cb37cd3890f59f8ff457f9ddf211b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:30 +0000

    equipment mean strength case strange

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
