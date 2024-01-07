527017ab1c99d6db766a7c8149cc81b101d6c2e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:40:26 +0000

    car other state lose share

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
