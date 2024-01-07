6372ccaa3368d62281656c4acf0dd7ffd3fc1a36
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:14:45 +0000

    interest nice instruction demonstrate impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
