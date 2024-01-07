7e10e792364e58934a21ab41611d657384f12bd9
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:46 +0000

    minimum heart shut then world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
