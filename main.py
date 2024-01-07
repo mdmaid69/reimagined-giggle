16242a513a9acb3a14efea5e5688c9aeb0c2440f
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:01:37 +0000

    replace scared read market start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
