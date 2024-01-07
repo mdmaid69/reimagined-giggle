76e5851c1fec8d467dac122414801903459a2989
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:46 +0000

    spread long crash stop conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+print(sys.version)
