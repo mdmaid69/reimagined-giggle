3bf824605d89ae027a2012c561de37da3c817d77
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:30:14 +0000

    successful open pity community set

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
