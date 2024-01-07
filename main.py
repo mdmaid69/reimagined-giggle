4b5924c91443afc0e4078821d1215e0573c55df0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:30:08 +0000

    them gift with offer health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
