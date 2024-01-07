f19a4c3e626fff54690b6d530f69060f22cb44bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:05:10 +0000

    taste square scheme drive attract

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
