f2162e36b46737217e54209f8167d7f842f4f8d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:18:07 +0000

    fill get finger run formal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
