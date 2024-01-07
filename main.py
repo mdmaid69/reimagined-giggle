7e8e9e7d4e0f5edc583fd3872ed275961b15d7f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:33 +0000

    alone boil play holdon for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
