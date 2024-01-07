6932ac1cdbbe1b5682571524e08550a6bf55baad
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:00:45 +0000

    period suit anything love top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_base_name(path):
+        return os.path.basename(path)
