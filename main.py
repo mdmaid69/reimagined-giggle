994a4cf295ad0dea7907ebdd5d195f287feb99da
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:45 +0000

    later convince surface right best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
