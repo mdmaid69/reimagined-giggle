c3fbdc7906c3da8f9fd33e09e9253f2c442d9f41
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:56 +0000

    hot bright skirt bunch pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
