0649682911c0e5e36afaddc1ec3042d748b272e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:27:01 +0000

    raw wise concern notice rarely

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
