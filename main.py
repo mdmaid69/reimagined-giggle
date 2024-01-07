bbd1666d626d23c4e9e4a792a848672addc3c115
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:25:33 +0000

    piece sure best term far

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
