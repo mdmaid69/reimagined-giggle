17af77f53c3532a8819a7d9be8f86427f4515153
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:55:09 +0000

    home steam town pupil taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_octal(n):
+        return oct(n)
+def remove_duplicates(lst):
+        return list(set(lst))
