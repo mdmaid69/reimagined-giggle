fb323904cae48b3cd070de4e8e49054e1cd180d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:31:34 +0000

    coffee clothes steady bath return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
