b70a157e5cf39c2113d3a92fbf0504c94ef4110d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:04:58 +0000

    takeoff skirt holdon grand broad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
