af38c93daaa91ba163a7819dee9fe08b81741ffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:01 +0000

    confused organization rest brief takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_list(array):
-        return list(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
