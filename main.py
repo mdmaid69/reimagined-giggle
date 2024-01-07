e2070d18468c1d33c58f3a2c83450fe83e17e7bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:20:24 +0000

    roll pronounce loose possession already

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_speed(distance, time):
+        return distance / time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
