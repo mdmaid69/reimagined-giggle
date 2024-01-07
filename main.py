c4a79053a73421a847227f231a18fbde6c035d53
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:54:49 +0000

    change record late agree Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
