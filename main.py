eecf8d79371729a1882c2c1acec14ac99a050788
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:47:56 +0000

    ask land correct job score

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def reverse_array(array):
+        array.reverse()
