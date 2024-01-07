ae43bdd85108d6dfe14fb6fd01844ba5ed8168f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:14 +0000

    wave busy sure communicate trial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def append_to_array(array, item):
-        array.append(item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_speed(distance, time):
+        return distance / time
