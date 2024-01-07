a8710e2372a3149bcbbcc29424fad8672529d545
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:32:49 +0000

    slight force turninto design left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
