3884880cbb621e1308bddd5c3d1ade10abbea6dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:58:13 +0000

    influence rarely sail usual offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
