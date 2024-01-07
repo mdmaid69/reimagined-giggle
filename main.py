e2142d3af94366d20a136355d75b80b7eb82072d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:07:41 +0000

    wind special pen there quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
