67b2752f27380103344b8e88c554fefbea260126
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:37:52 +0000

    uniform mother man cook count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
