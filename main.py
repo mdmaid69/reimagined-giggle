6127335abc7ccc294085f670f555bf8c7de7e682
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:45 +0000

    address best ignore use cope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_logarithm_base_e(x):
+        return math.log(x)
