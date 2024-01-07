4583dc8bed8b8c3a9ababe9e2a9c95baba361387
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:54:20 +0000

    benefit thinkof peaceful spoil fail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
