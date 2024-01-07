9cf409f60762af2dbaa7d7e635322d08ebcbab3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:44:08 +0000

    off its better later pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
