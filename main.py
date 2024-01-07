86e888d73fef97f49e244f8af862c23eaae8cd44
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:41:32 +0000

    Uu drink cost slide board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
