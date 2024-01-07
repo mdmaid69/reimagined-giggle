2b03a7d9f47155ca1d0276d924006366cea7d56f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:45 +0000

    pleasant learn hello would project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
