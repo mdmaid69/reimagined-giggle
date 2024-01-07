13f42c58e2ac92478961e64baafeae884d9e48c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:07:33 +0000

    fail giveup field concern artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
