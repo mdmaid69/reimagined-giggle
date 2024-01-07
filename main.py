2f2b598db14f797efb79756bfd9b9463643a943c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:08:23 +0000

    Mm rest turnon me dare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
