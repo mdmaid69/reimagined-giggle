d3d519012c78cd368e83ac6208f449eff64dedf4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:40:43 +0000

    message inform would business draw

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_words(sentence):
+        return len(sentence.split())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
