fa7d8b072f28eaab777da16375f0505dc3839dab
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:30:29 +0000

    stone owe pot blame sale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
