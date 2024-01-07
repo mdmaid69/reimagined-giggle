63f6983297c7614eb8ed96939e60dd44c53ee88a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:55 +0000

    question worried behind object patience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
