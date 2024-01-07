ee1be1c6e7f1c1c4b6ef2546a092acf2907de66c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:31 +0000

    next switch plenty equipment consult

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
