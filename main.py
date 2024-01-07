c2ff4bb286c4f7d067114bee3d54f1bbc7b6ef75
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:46 +0000

    happy rush fortunate town shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
