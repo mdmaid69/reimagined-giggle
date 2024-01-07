f6194d695534f71933bc78e2900b8b66d7df7a55
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:03:36 +0000

    miss declaration comeover but wet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 def count_words(sentence):
         return len(sentence.split())
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
