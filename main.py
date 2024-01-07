1e826c444a5aa9afa106bb0002bcddea0539acf6
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:30:17 +0000

    rest reach slow why slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
