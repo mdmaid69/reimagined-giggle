1ab89a8d767fe946096e1505aa7a30f7240ede3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:20:05 +0000

    pay late wait concern hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
