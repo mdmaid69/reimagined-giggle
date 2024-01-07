4d7c7dc69346a007c4057acb4c59fe4a4d20bf2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:23:37 +0000

    dream again benefit no nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def convert_to_hex(n):
+        return hex(n)
