d6a70d81ea6f8a92cd5963e65aed5eadf7838e4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:49:09 +0000

    wet absolutely success hospital trust

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
