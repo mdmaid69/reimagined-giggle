4216124231432dd66430c4e2021c2e5681111dcb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:51:17 +0000

    yours wind along patient material

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
