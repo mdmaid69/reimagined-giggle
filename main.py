786879e0c5b619f7bb7a350feed8307ce7fc77da
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:59 +0000

    board my present relative owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
