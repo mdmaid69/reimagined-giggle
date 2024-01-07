df12c1b7fb3c168667a1494f73db6430ce135d6c
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:26:03 +0000

    tie difficulty chin 569enjoy book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
