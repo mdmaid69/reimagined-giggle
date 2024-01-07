701b6dcbc03e4b28b1bf226ad7eedd7abd950fe2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:25 +0000

    now like best present come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
