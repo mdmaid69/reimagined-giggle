f3cc8ff77dcb4f4aef3a9fa63234ac66ae25926d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:49:22 +0000

    callback on need repair object

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
