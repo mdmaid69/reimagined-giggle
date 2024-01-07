7ef9a17dff5a07a43037f2297217f21d37cd2f49
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:00:39 +0000

    little feel cope community communicate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_power(work, time):
+        return work / time
+def convert_to_binary(n):
+        return bin(n)
