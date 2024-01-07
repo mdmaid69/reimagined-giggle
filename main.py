99bb48bf67edb150edf28e12224ab707aef4aaa7
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:26:45 +0000

    once occur law hungry hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
