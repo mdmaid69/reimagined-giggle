1d582be0df84481b43f0604a73e8710a1ea856c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:53 +0000

    claim no breathe forgive service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
