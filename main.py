27ef49a0caa870b4d4ecb3c06b8085ec34dd1d7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:51:49 +0000

    shopping ago quantity admire bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
