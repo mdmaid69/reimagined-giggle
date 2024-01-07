c4b32c0089a5d405eff45ec9b1ea9aefa2c0b006
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:58 +0000

    down shout performance trick rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+print(os.getcwd())
