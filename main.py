d8386c6032ffd86338c3e68fcbca87fbb161d8c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:15:07 +0000

    blind on will unlucky information

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
