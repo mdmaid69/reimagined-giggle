9de12fdf82913f4983086d0916849b7fdf58f643
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:31:17 +0000

    maintain evening thanks programme factory

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
