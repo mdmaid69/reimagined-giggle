b47eb817aabd138d519d5417da1efbf42c658d94
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:01:33 +0000

    swallow towards safety arrange keepup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def subtract_numbers(x, y):
+        return x - y
+def find_min(lst):
+        return min(lst)
