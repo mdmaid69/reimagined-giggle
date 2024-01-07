5bfc008e276f6ff6f8161cffe3a6397e41566dd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:20 +0000

    getback water connect sweep vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
