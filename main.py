2cfd1988fcd2d070aa03f22c432b8821825bc50e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:04:26 +0000

    564encourage listen obtain book calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
