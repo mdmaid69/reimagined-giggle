b3ebd9401968fa7629e506ec2ee08ba4dc8710e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:27:41 +0000

    earth turnoff watch face although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
