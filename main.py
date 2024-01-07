764757cdcc58e57c36f2e239f4a384a1a7bab482
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:35:36 +0000

    scrap let Pp furniture ordinary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
 import array
-def get_array_as_list(array):
-        return list(array)
+def reverse_array(array):
+        array.reverse()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
