d65197b1cd796259977efdaec5cc6b9ccdeb9eed
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:05:07 +0000

    mix rubbish opposite rub regional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import os
+print(os.getcwd())
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
