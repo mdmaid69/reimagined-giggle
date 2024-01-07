62c89bd917c15a306214bc6e08855888ee452536
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:05 +0000

    top succeed finally about responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
