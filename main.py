22467bb9bdf4447a5c022cb74e5a5cfd31c7183c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:45:48 +0000

    cap compare long protect workupto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
