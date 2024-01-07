0a5362147d35f2e3c635c59a3ec6c33c4f4095e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:01 +0000

    soon still my comeup responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+import collections
+def create_user_list():
+        return collections.UserList()
+import collections
+def create_counter():
+        return collections.Counter()
