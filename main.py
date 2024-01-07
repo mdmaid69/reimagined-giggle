0b472c9274a16a4826c52ddcc6a6b2350939452e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:29 +0000

    whisper steer contrast edge apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-print([x**2 for x in range(10)])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
