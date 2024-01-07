ac91f74abb2f100a4d7f351b53569bf649083e91
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:13 +0000

    colour harm spare twist object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
