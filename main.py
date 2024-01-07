76f4da7f7f6c56836364f4d30411886a7c3d8210
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:43:53 +0000

    order Ww nonsense Itself ready

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
