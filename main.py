a7aeb02371493937c535b74c397f1a34b565aaf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:01:03 +0000

    very shut mean weigh 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
