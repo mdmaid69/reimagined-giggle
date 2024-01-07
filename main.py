c37101beff2d32ab52c1f503a58d3ee2958773d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:39:01 +0000

    goout reduce base treat everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
