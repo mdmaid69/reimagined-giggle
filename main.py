0b0d206b2b7f9e17b55bfda3c6875dd8a9ab1c09
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:32:50 +0000

    burn organize everybody succeed Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
