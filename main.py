0df387e7e978c6d6cf8efd84611b0cc98f9d4da8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:28:08 +0000

    music miss little sad bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
