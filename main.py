e26f56c63d50721b0ec4e5fb262673a638872a72
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:03 +0000

    mine oil report never clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
