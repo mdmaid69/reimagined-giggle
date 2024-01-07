7980a95b51e7b6ccc41c8e1a2230183071121dc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:14:13 +0000

    peace project trip entrance fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
