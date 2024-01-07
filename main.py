296a7e5f45bb9227cfcfac83ef4bf36c9cacf211
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:39:42 +0000

    concerning scare tip keen nonsense

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
