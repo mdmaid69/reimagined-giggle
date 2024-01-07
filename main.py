79363bbad9cfe321396ed87b035a5071a2c68f6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:31 +0000

    trousers impression kind deserve towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
