e449318ea7cc5aa137cb0914ed379cea13b97439
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:48:11 +0000

    progress at once travel second

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def count_characters(sentence):
+        return len(sentence)
