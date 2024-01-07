917fec5729ff6c988bef8901ce6935619fcd7eb8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:04 +0000

    away lookback(on) want lean church

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
