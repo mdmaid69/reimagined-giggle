a1db706f54952aee2d706467d1b23c3a9a673692
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:13:06 +0000

    wind from hold machine television

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
