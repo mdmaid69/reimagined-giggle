c365b8f651a0548a341d1e27f1b536c23ee2b177
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:43 +0000

    in female neck slow degree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_power(work, time):
+        return work / time
