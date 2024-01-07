a37674feee9c640aadc50b12f94474845f8c8dc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:52 +0000

    operation positive fly regional activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
