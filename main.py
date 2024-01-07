b118f193e426e6a9e0f143a4ca6bf04eca225c38
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:58:27 +0000

    save shout characteristic count near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
