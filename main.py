4449a689a941289e2c80914f780d03ea45f71314
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:43:23 +0000

    connected trade people ability surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
