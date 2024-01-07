ac882c575d75bbc120c329560973ac8ce1f7054d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:37:26 +0000

    tip trip bunch club perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_length(array):
+        return len(array)
