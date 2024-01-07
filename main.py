c093b99d85a3704861a9d2b1ebffc9e85fcc0d2f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:19:05 +0000

    571enough talkback thorough design face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
