80868e1f795e79bc1226c967fd559d0d28746360
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:07:00 +0000

    write table compare luck murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_current_directory():
+        return os.getcwd()
