67dbe17e5bf4ac339c2cddb410a17e85e6483c0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:58:34 +0000

    value harm cross steep wise

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import datetime
+def get_today_date():
+        return datetime.date.today()
