84e1926b9ee56e413eafa25bf7047541c2511038
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:32:58 +0000

    comeout with bring beautiful main

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
