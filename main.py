5add0963cfdbfb92e22a89e4e1c9c9214fcee64b
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:12:09 +0000

    Zz respect story touch her

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
