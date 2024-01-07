cdeb11e38795b279e3b3e7326c3694b94314fd90
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:03:19 +0000

    off space error swallow likely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+  def cube_number(x):
+        return x**3
