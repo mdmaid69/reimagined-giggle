a09e0db9c6f6935202eb2330a23321c80eb15d51
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:45:47 +0000

    angry tone sure answer typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_as_set(array):
+        return set(array)
