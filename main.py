8e79de4b685957b3653eb77e3575ea7c4a25a391
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:00:55 +0000

    chin nowhere priority situation hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
