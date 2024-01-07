776f7054eb598b41819416bb30f4869e76e93a53
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:49:09 +0000

    total bill proof children and

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
