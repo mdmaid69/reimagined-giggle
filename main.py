bf5bfe9c386cc2a9dce91aab75acdae3a00618e7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:02 +0000

    takeoff rule speed store watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
 def convert_array_to_list(array):
         return array.tolist()
