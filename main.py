724b7801bcd4cb90471865224093b7879ec8c3fa
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:43:10 +0000

    zero move unpleasant gather opponent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
