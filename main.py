cc58e5b64e0169b0ebb7260876356319452e5975
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:19 +0000

    after film difficult increase early

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
