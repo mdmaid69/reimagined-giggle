306cddd965b75f9d733c02dfe29d651e1c78e3fd
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:02:48 +0000

    outside unfair lock plan scrap

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
