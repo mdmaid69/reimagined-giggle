b15a13182625ecf134349d47e74ecdd0d8f8228b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:19:27 +0000

    divide concern agree angry glass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
