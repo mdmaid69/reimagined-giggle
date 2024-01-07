06e5c854242f7c11f9b5003bec4e2996b811d202
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:46 +0000

    that dream rank perhaps arrival

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
