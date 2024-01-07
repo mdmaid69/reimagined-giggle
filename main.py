26809a20de5c435f2de0a6a2bfb2dc889764179c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:47 +0000

    original natural see Vv appeal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def cube_number(x):
+        return x**3
+def square_number(x):
+        return x**2
