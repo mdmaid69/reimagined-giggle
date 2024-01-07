add20ebb32f36b9beb5b7873cf6ec78c0a5d31bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:15:16 +0000

    speak average unable reserve team

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import platform
-def get_os_info():
-        return platform.uname()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
