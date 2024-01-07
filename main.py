0e15c3e024bc842d73cbdf30debe61499234482f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:12:18 +0000

    office pretty warning tough piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_int(array):
+        return int(array[0])
