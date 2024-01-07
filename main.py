0e6ee00b3155f8e593bcf544b36a03fc51aa53a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:33 +0000

    live whisper limit border pollution

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
