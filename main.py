11ceaffd7b83c8728aaab95d62e39b3334f8cf29
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:09:11 +0000

    ordinary convinced head everything along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
