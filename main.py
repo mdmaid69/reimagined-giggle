3515be2ab5bdef406adf8b2ebb88539efd06e4f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:44:08 +0000

    push target near nigga storm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
