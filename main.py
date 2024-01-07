87668ef886f5288bfff6a7dfa6724afe9c904bcc
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:19:59 +0000

    smile important control goon figure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
