a077220df58a89f15247b0d323d64ccb0d8e6c5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:53:22 +0000

    disturb to would attach start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def add_numbers(x, y):
+        return x + y
