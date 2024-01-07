2c0c17b5a51c5f7a08092eeb0fde9313f7930eb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:58:08 +0000

    pressure underneath cost load callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
