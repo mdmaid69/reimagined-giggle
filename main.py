7ce69de3f5328b8f5386070bbe8f36ebc687bd0e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:23:58 +0000

    voice trade by temperature comeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
