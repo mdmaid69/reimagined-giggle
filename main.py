120e233d27bbea037539aac5d975cabbad7f565a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:16:47 +0000

    available bath bright lay base

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+print(random.randint(0, 100))
