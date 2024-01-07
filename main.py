ec37b5e7c2600f0116f684409f1ef11b2fccb034
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:34:18 +0000

    people pleasant thick mouth frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def clear_array(array):
+        array *= 0
