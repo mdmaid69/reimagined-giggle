e0f67ff90dd1e87f4fd5b7248bdca4e5b80400c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:01:43 +0000

    organize heart necessary mine something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
