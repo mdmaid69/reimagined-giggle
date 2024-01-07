2f293655518f938820450fb7e3f8797b4df4c582
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:43 +0000

    again impressive year right closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
