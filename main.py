07b5b1cc747472eb7d92d0df0fe6e561f915c2d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:20:38 +0000

    murder value only computer broken

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
