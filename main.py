4cd972b432a528e11006244f75735a8e77a0181f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:35 +0000

    dead play traffic aim chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
