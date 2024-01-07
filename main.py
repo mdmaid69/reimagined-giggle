eaa306fa2ddf9fb90e9840a533f5315eca21f974
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:55 +0000

    leg commit tidy cheek combine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def sort_list(lst):
+        return sorted(lst)
