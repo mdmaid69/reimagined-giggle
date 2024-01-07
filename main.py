2681cd5dbb6188adab332000669d3b13032bb7cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:26:51 +0000

    water fat obtain age female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
