a9bd53b43948903b14f82373c28da30a45bd7fab
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:06 +0000

    mouth freedom but dance demand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
