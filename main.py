6d0c00b9436d06941b04d6c28849d9b02285aceb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:25:07 +0000

    excuse accommodation mine snow way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
