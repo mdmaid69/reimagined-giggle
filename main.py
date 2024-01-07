c0a2495af946f6604e2b20984c17faf44107ca72
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:00:54 +0000

    moral lock need cry Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
