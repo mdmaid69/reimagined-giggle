1dc86190e1da7764756ba63d21920c7e9d67e45e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:06:59 +0000

    scheme soft phone remember perfect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def count_elements(lst):
-        return len(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
