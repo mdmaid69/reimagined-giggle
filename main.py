8283415fbfa9889cf5a3a05c5deca2128c3b8d10
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:00 +0000

    well ability male wind attract

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(10): print(i)
