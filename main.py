04ad5d4206ed0a0265d268ae5e9f6f251019f1a6
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:26:06 +0000

    conversation activity follow suddenly bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
