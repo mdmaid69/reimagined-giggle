3730e3a30ac6219fff72b1a8bc1d5cc1042baed5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:04 +0000

    air sore turnover pool surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
