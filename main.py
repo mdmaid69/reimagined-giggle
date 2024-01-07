1cc4da590e09987e2ca82a5bf53a73e6cd96c06c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:59:46 +0000

    Ss surprise exercise Tt interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+def calculate_cosine(x):
+        return math.cos(x)
