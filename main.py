21815552a00f8fd413f43c0ded006ba7949c6938
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:29:36 +0000

    debt emergency Ww news Mm

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
