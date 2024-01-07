5777eff27ec085013a38030f79951721bafa38c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:10:32 +0000

    ball warning herself that godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
