0e2938eb630c878af386cb68aa34f98ccbe0e5cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:51:48 +0000

    pause drink artificial which repair

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
