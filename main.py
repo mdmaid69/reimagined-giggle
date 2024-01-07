123cd8abe820204e5fa63fc07b9421a3f08c1e11
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:51:18 +0000

    belong constantly unkind yesterday scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
