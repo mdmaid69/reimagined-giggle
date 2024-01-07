f2119e169cbbd88f623e2528db7cd2364c5e5700
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:40:56 +0000

    lose adjective comment stare design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
