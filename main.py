5601fe7840a513d7fb68d6c0546bd4bf60119d77
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:20:07 +0000

    few will human change lookafter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
