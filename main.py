17811e78225562c47f2fb858959a9732a856ec68
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:00:52 +0000

    promise ourselves stop borrow bound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def cube_number(x):
-        return x**3
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
