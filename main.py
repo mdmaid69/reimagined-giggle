4a36eeb36d114ec2d3081078cecae9dc3611c158
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:33:31 +0000

    deep cause year open month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def multiply_numbers(x, y):
+        return x * y
+import array
+def reverse_array(array):
+        array.reverse()
