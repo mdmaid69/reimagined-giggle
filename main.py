55fc1c62da433e8671187fedcfc07251e3c4cf68
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:40 +0000

    reject limit recognize break steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
