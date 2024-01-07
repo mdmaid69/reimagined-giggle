f32c1b38166a89351e1f99c69986bbf9c565f527
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:40:32 +0000

    glass cheque ask add acceptable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_current_working_directory():
+        return os.getcwd()
+import re
+print(re.match("h.*o", "hello world"))
