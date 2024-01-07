cadb99277b49e6005c5fbbff43251c07e91566ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:24:54 +0000

    present exist prevent feature surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
