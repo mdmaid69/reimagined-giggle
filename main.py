8369162301f4c962ebcdc656ff38f651a48529c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:52 +0000

    live programme practise down lend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import random
-print(random.randint(0, 100))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import platform
+def get_os_info():
+        return platform.uname()
