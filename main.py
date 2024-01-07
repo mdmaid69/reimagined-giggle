cb48ec17152b3a4eaf47708380dc6896634b93bf
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:04 +0000

    serve between to priority voice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
+import collections
+def create_user_string():
+        return collections.UserString()
