e4f832cc1795bff181a31659e9dc9602ed643611
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:08:25 +0000

    afraid for hot emphasize balance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
