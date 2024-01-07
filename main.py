7a4a10e2e6436000020b7f619946a78b29a87d8d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:54 +0000

    advise arm breakin fact remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
