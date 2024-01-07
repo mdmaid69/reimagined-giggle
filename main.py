8bea6056dc5d0a07f722d8315f34dbda52591f9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:59:56 +0000

    estimate build letter imagine meaning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def change_working_directory(path):
+        os.chdir(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
