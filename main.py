82b7574eb051a3cd90b3f911c4808dff9721331c
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:17:18 +0000

    statement especially eager translate work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
