ded65c8c0932471928d732843cc9f7e905475d73
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:00:57 +0000

    love around onto consider match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_area_circle(r):
+        return 3.14 * r**2
