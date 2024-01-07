c24d35d83228173ec36e185f23a4bc269a55bc73
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:17:41 +0000

    whoever limit off respect comeout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
