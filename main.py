bc20f7f7980394b1e334e3367f6a3d2ebe69294f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:33:57 +0000

    address amount hurry nearly traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
