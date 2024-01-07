a846bf90eb9ff33bb9c15c4081560833e10fc751
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:44:32 +0000

    now practise start late weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
