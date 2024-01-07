fef7aa5cae4196a3b2a148c90ae13a3abcd3e753
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:06:58 +0000

    sensitive turndown sticky mainly callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
