f7ec1f752c7e1b4880634e279be0deae81b08761
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:18:16 +0000

    roof announce art the after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+def is_palindrome(s):
+        return s == s[::-1]
