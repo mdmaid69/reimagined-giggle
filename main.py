fb299ccdc283634181e19d417d1401a9578de15c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:45:37 +0000

    language production neither copy job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
