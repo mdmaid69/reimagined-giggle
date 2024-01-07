07c2c05650c422d34d7762f823dcf9559d952ac0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:44:16 +0000

    attention freedom afterwards passage within

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def count_characters(sentence):
-        return len(sentence)
+print("Hello, world!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
