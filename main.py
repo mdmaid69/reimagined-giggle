46edf1d9a34dc064c7d4e7e082fad03dc3377d96
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:06:56 +0000

    human us Jj public heat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
