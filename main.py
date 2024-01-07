1908785ea8d2ad975a2d27a2a00120b7cb182102
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:16:42 +0000

    absolutely official expensive desperate admire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
