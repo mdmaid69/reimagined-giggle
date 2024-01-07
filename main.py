fa38ba9d5da267ec5a88a54be2f7a6167de35588
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:53:16 +0000

    talk spot tape bury duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_array_as_list(array):
+        return list(array)
