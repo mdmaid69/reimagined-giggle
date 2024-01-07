3a0672a3a78f7d4fef993aeaf5b51305b728823c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:24:15 +0000

    mess conduct temperature afford phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
