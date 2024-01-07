c125bf452686722f35af10f539b8f51ee274878d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:29:24 +0000

    sore tone giveout before geton

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
