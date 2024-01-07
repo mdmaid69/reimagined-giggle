a043ea54c01f7e5036d8f68f62c130a6bb29492c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:11:49 +0000

    wash search that thick reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
