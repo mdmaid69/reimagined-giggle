564a3d19f6c614c245c0b4db36552c500ed3b759
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:53:53 +0000

    near trust off whistle tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
