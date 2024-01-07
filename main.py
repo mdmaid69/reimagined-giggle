922782ad41bdf8cd88a50c5c007606f99d423638
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:21 +0000

    takeaway later worry original weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
