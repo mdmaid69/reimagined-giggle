3379102677bca5e04a260afb8329337508951251
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:56:40 +0000

    hair sink platform petrol suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
