a137ea223844908f799fed3b22dc0de8443c2b2c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:13:44 +0000

    fix fast sail quick minimum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
