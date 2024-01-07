163e5e0a7e3cb16cfce07b8dedfc62c4a3e99ca0
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:31:43 +0000

    love offence big part compare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
