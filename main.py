87f78af37b5a9e4b873e3274cca7836a9e15348d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:32 +0000

    shut cheap bored fire march

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
