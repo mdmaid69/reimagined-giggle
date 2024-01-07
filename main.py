4f69559936dbfaea736b623b3a96d64e25bec1c3
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:31 +0000

    free afford must truth shout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
