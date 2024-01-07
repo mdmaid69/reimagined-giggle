5a420e5ab8a265735df2a91886d9ee470a4cf386
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:24:34 +0000

    spring balance thirsty trust be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
