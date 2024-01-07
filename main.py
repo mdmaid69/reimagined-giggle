321842b5ca93f12154084f147fd2ecb29a84ea64
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:01 +0000

    permission share complete thinkover luck

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
