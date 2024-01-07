94a933cbfb9f2ad077924f94a229d5f397cd2e3b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:08:06 +0000

    radio name responsibility callout left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+text = "Hello, world!"
+print("Characters:", len(text))
