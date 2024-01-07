4359cd8f167d7f94d662485de54d5070af777818
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:31:33 +0000

    light fashion favourite first best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
