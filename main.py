1f4b3ff7ffe54f82064691600db40bbf6929f150
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:59:26 +0000

    uncomfortable age cheek performance nonsense

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def add_numbers(a, b):
+        return a + b
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
