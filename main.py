95112149e91ef369c9fc7d89b3a26039dde5b18b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:30:36 +0000

    operate calloff characteristic frequent trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
