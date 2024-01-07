a5dff5e6a733fbc94306edcc6aaaddd7edc0ca56
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:04:44 +0000

    sad shoot before contain advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
