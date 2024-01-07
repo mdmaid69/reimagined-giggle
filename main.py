0139cdf5dca5dfe11b4f4917382eef32f8de9b4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:45:44 +0000

    onto community stamp book apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import array
-def get_array_item(array, i):
-        return array[i]
