209b5e47d063551a3fca020ecb8f688137599fc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:39:35 +0000

    appearance punish trip loose grow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
