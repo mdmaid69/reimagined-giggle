d3ecc8acb210b466c4505be689eadae5e1fdbfb9
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:16:57 +0000

    telephone above process guess clearly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+for i in range(10): print(i)
