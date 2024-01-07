899a56b7417d20f6d9613eb6b9e6de27d86e69d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:19 +0000

    estimate contain label set child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
