b494484e54ca741e8b4498297d4c6f73b47a5545
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:13:08 +0000

    getback me pressure Mm workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def add_numbers(x, y):
+        return x + y
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
