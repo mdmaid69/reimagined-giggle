28e0a408dfbe9c4b722311cdec98ea1843effd98
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:15:19 +0000

    consider pattern quit unfair country

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
