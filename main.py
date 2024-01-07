9a5940790ad130f2b1cd576e6460e91fba4e7b0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:05:09 +0000

    protection taste want ignore smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def get_array_typecode(array):
+        return array.typecode
