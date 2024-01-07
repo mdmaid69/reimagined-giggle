6da089a146a583a7057f48d384979ec3434df232
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:16:45 +0000

    patient lost completely fierce shut

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_acceleration(speed, time):
+        return speed / time
