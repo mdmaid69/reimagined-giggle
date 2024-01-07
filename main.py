51c84be71c0a5f7a7edcec9a8270a5b2bdcd36cb
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:26 +0000

    before calm quiet force forget

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
