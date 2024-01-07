8076cdeebc9b2f5242399e9f01777e428c37c0f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:07:42 +0000

    hers win adjective needle rate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
