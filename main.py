297edf2974276c375cc617906436f73e3ceb0a7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:53:46 +0000

    sometimes field proof rush taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def delete_file(file_name):
+        os.remove(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
