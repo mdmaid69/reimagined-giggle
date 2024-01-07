4b162d3ffe82fe6318ccafcab2afdf741991c193
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:54 +0000

    radio convenient appeal visit affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
