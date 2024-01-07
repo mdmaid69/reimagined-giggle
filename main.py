9d8fc9f4245f16df5869281f9d2c31ee3b8659b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:04:26 +0000

    stop shade hang worst mouth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def multiply_numbers(x, y):
-        return x * y
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
