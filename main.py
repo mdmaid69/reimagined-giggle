9345ad58866c87decf7802b392d12535783c507c
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:35:42 +0000

    grade situation kill leg waste

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
