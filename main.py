d4ebac1efaa35b3cd2b74c747ba62229786d8289
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:44:44 +0000

    he neck fortunate notice board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
