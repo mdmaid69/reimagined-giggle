bfb11a37ffe776390f83dc071f38a85f0d17ab24
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:53 +0000

    herself basically pay calloff try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
