a0f3a2f39adc3114a24677de031139aada841d7c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:19:30 +0000

    turninto past for Qq born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
