b9d64c49d0d883c23f1ed51b215b351f9f4c99f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:34:17 +0000

    key early polite so spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def sort_list(lst):
+        return sorted(lst)
