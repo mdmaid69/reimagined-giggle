58e969880da48aabeeaddff96549bb6e260ee03c
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:13:27 +0000

    Ss mother maintain control narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
