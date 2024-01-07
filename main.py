61c5143e664802e995db27de619e8f6a26944008
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:54 +0000

    over already Itself female die

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
