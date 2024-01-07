4073e5035a18fa9a91edb49e7fef9ea4575710f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:33:41 +0000

    takeout prove radio change hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
