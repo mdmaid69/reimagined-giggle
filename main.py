b1d322aff7f7ec47205cde15bd32040d00c56930
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:20:29 +0000

    drive justice exaggerate considering sure

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
