558700c7aa6a39aa351dd27d8d295c1e296ce951
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:57:04 +0000

    tax pleasant invitation below independent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
