c4065e8e47bda51e257fa31c18f8c3c1f89479a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:14:53 +0000

    narrow sure mess voice any

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
