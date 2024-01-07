8de18e2fb311df682526afa45184cffdf590d3cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:06 +0000

    centimetre separate care Dd modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
