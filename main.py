4220b3b4c7822d4d8587c19fc309cd2655d68ebe
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:07 +0000

    lie sign constantly belief tough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
