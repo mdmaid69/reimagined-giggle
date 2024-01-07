43147a1d40039c124bc27901d565e9989089dcfa
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:27 +0000

    again 179bite abroad true smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-print(math.pi)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
