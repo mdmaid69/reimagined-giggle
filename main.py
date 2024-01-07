92c02b889965d4e34df49b3c67ed2fa1baddb379
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:43:45 +0000

    square order command hurry within

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+print(sys.version)
