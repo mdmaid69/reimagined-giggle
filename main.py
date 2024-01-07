250b3fd9dcf30f419458baa48f95c9a06cfc4a72
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:02:22 +0000

    duty tooth scale consist autumn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
