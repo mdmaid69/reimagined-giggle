0f955c3d217d3e2b5a2246eba9e25144874546fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:13:02 +0000

    become marry clever hour fortunately

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
