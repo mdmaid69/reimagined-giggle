ae68c5fa603eb7bb17c48106c24a7590e93ec6d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:33 +0000

    wrap giveout letter 565end kill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import getpass
-def get_username():
-        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
