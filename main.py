0ed628f6170bcbb097e1d4cc66dd0898e1cfa497
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:25:06 +0000

    lock fact scheme relief we

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
