563ed42c6ce6eae3dd51ab2fad0698e54a7448cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:58:13 +0000

    character bye waste none tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_error_function(x):
+        return math.erf(x)
