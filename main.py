8eb56017f5acdf53bc37f0f4ed38be3fe92820eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:36:17 +0000

    draw the wipe reserve space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
