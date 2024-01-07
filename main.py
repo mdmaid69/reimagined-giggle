1e98ee6ba69aa3f3e68f0afde340009b3ee5f6be
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:59:33 +0000

    perform married near explain correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
