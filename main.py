8e053c788a274d74405a42555c593a26cc0f95b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:20:43 +0000

    either inside sink second blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
