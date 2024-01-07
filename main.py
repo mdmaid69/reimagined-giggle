b2d9d23a29dd790eae4ee939ba5561259425fc74
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:54:53 +0000

    transparent usually before reaction lesson

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
