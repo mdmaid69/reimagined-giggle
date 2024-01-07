e50dd3df1c306f9657fc5cedd1b450d849579d2a
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:21:36 +0000

    Qq question pressure strict reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
