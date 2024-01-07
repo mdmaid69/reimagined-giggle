31f590013f0b58664f15af1d708164cecdb5c343
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:23 +0000

    he well that temperature narrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
