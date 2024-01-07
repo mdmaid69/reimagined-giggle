acd9476ffa5f2984badd9b4261fff6b25e4e63e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:10:41 +0000

    exaggerate possibility university bad excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
