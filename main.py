4b7a7817302ea84e38c1dc057fe9c98fd63ffb15
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:27 +0000

    release air tool noisy divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_repr(array):
+        return repr(array)
