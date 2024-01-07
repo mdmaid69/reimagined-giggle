4d9fa5afe66ffd77b31b931a8417fefa390cb349
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:46 +0000

    stick by such especially crisis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
