76a8f5367b695508855287e11cd65986dec4439e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:15:32 +0000

    prove discuss first excuse nowhere

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
