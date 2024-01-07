4ba5f808e497bdb3ef0275e6d66b17a7eebe0107
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:44 +0000

    either own avoid cough fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_bytearray(array):
+        return bytearray(array)
