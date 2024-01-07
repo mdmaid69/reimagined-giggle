7e849bae8583045cab1463516b8f722fb1598b86
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:54:29 +0000

    consider deserve confident attract threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
