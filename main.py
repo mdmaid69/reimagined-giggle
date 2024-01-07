9ea9565edc500683469c06757c535b2988f1d531
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:47:58 +0000

    unit doubt direct note answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
