b5abe4b74dccaf2d71a35c84ceb014014bf94b28
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:13 +0000

    taste interest empty scream through

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-print(sum(range(10)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def convert_to_binary(n):
+        return bin(n)
