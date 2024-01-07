b5eb2859aa88c32bb8388a79a12dcf50b08faad3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:52:21 +0000

    apart flight related cut reaction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
