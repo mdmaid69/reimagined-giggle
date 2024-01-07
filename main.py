55164e0ed87adc479a3bc6367e690a9102e17998
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:44:43 +0000

    probably remember account soon exaggerate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def sort_numbers(numbers):
+        return sorted(numbers)
