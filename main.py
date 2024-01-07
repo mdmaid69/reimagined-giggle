d0e4f904490fd94d4e2ab25094786be0c3206e3c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:54 +0000

    comfortable disturbing branch closed file

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
