07dc8a7741fde62d1d8a9687a2aec2e6c9a7d271
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:49:34 +0000

    story above direct variety passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
