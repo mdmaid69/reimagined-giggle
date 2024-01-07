c41e7bcbcd5182c762cd715aa7d98ef0a226c465
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:06:15 +0000

    fresh lucky mention embarrassed estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
