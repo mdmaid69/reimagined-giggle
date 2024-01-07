5e8413abb6814a342d5bd91daaa6f2debefc038b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:10:08 +0000

    soak female flat dare responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def sort_list(lst):
+        return sorted(lst)
