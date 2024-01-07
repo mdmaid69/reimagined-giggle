70c9616bef85666d59727e44de788191e91d4ffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:40:34 +0000

    alone cheat old public taste

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import datetime
-print(datetime.datetime.now())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
