90319896d94034a55a2f1db64cf31581fbb6263e
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:55:58 +0000

    rest whenever protect any rather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
