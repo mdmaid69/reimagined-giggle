0f5233a7f2c8d439b56a96e584aea0efc1bbd0cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:09:27 +0000

    aim their day runaway herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
