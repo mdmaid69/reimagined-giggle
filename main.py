6cccfa6f9f9e89b6e40d7f487fa5fedff0f91f67
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:24:36 +0000

    rely about autumn activity prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
