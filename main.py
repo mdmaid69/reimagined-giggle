6295503aa7734a48ceb4b0d7c84f6e8d400d18f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:08:16 +0000

    laugh bush completely middle office

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
