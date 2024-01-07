1847410ac6f0cb0524a2a61c2465bffd890469f3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:52:31 +0000

    anybody traffic getout hospital decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_typecode(array):
-        return array.typecode
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
