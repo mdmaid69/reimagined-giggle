336fbb96273c4c314b8c581bc79ec072ce030118
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:20:23 +0000

    test journey later have free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
