4fa972e70c23603693b070995c5950810189c1f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:21:47 +0000

    cool fun 572enter heat start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
