ca22bb8df710b5272cbccc66f6260ac88cdea7d4
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:24:37 +0000

    only perfect software scrape service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
