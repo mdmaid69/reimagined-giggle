15337019320072dbc77bd9865c1e717af6cda5b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:32:23 +0000

    speak powerful wheel himself storm

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
