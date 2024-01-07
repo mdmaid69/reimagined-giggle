ba50d4f2622e49eb5b3d325d2914f8a77ff28757
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:35:21 +0000

    town blow name succeed during

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
