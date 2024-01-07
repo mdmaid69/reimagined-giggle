233dd02295cada2197c4818238061dfffec60753
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:51:33 +0000

    never importance Itself high touch

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+for i in range(10): print(i)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
