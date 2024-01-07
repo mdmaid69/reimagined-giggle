78793afc69578531040161bd8a64c0348b0629a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:52:00 +0000

    hurry news computer rubbish have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
