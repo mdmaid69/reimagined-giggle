8e79889300f9aec88e46113b9ead927740c1a354
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:41 +0000

    callout common 567enemy present shoot

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
