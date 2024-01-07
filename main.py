6a5d114c7dad402d7e4cce41b88668fe8d2b16a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:28 +0000

    issue near careless allow object

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+for i in range(10): print(i)
