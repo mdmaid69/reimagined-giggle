eb982a6b030de69fdb74a0a0199a1a66c5e2c922
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:44 +0000

    for believe most hope space

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
