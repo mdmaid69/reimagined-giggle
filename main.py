5e1644ebe89efc8f583f1cf456eccc15e871ea66
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:02:46 +0000

    gas and water arrive deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
