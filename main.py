b2eb06506143a15550823bf134000650182cd017
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:46:33 +0000

    lesson round later neither final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
