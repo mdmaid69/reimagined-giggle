35e9a820c1d6eb955a77b898242fb320c13c70a5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:21 +0000

    subject file point aim return

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
+def calculate_work(force, distance):
+        return force * distance
