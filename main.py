584c15fac01f4522dd57bf84b6dc436b53ba3f71
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:01:55 +0000

    basis side bright anywayanyhow completely

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
