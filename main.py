0f93afdf3bfd6b6e4d9327e2057dafa5b59be277
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:04 +0000

    rubbish crash breath and suit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
