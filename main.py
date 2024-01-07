76740d06b2b17ac9af4b983356d53c074e5a9a0f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:34:39 +0000

    load shopping please try off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
