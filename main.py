0b50ebff5c486c460e3e14bb90f4f2dcb706d1b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:14 +0000

    attach Oo pipe equal special

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
