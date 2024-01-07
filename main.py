e161af5f1f3d413e81d640ca5e07a9cc3b20a98c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:48:05 +0000

    stop suppose force float principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
