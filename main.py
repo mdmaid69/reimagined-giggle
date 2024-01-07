599c97d4b5f5160c1a378462ab3348c96d485fb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:44 +0000

    achieve card wash disagree slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
