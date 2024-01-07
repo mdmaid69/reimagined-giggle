c7985b1773d825b094b445cec70a47ce8a9432b1
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:51:38 +0000

    target blind few recommend have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_current_working_directory():
+        return os.getcwd()
