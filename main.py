d6441601af5414c2ef948769a7e147b9d98ff402
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:29:50 +0000

    centimetre advantage patient prize anybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
