f76a530b56491fcd2169e4e60f3525ebaa821c40
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:03:44 +0000

    bear leg head stage experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import os
+def get_current_working_directory():
+        return os.getcwd()
