73f7d7a85bb80b1792f6aec0a539495cd8f08a77
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:32:29 +0000

    pride everything speed try knowledge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+print(sum(range(10)))
