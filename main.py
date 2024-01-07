fcef56a12fa41ccbdcc7d92e90971ed4ae270ae3
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:32:59 +0000

    nurse generous furniture complicated 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
