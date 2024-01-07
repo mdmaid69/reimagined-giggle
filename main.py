6601333d8c1fabd93b2afbc8840fc25dc892bcb3
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:31 +0000

    excited rarely suffer miss silent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-print(random.randint(0, 100))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
