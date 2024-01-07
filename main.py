c64573ef769d021769c068a6de148f93b70f45fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:12:41 +0000

    hungry view late deliver the

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-print(math.pi)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
