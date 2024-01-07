a9129efc848de69edf0f1153912b57c0de0f618e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:24 +0000

    advantage brother original split single

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
