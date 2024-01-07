21d964f96e79658406aeb1703989527c98778482
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:09:33 +0000

    passenger bar both chief let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
