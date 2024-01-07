f1d26fdd99fb217e60bf6cfde48c5b14b1ee1fbd
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:23:10 +0000

    fight lookback(on) better getback grade

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
