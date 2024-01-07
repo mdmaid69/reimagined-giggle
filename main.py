1549c77317fccbf6323a406bc0af579df85419d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:25:37 +0000

    condition prevent thanks lucky live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
