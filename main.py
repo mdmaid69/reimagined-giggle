bb73910a4947308a0debd1942aa95f1f9c5d4084
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:41:43 +0000

    respect Ff spend height mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-print("Hello, world!")
+def get_array_as_bytes(array):
+        return bytes(array)
