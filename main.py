e926aa70ec23947e6b2ac93f46d1145fd4105c40
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:13:56 +0000

    tread mail feed whenever swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
