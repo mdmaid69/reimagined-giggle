680b011be75e06f42659529e6eb150263cf7dfc6
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:07 +0000

    injury home church boil under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import time
+print(time.time())
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
