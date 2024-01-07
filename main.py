e392f937905a4e38e1b0bdc81f9b653c0987f3c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:33:44 +0000

    blow hand miss odd stone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
