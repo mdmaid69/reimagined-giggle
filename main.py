5e81699ee45135ce17fcdf7a05e1594182c434ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:41:30 +0000

    hurry sometimes damage use creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
