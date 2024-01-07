1bbf4f95c7231a6801b5ac39458b723e5b3c0b68
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:39:41 +0000

    move scared pattern system exploration

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
