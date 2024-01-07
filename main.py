6a4a3fd86970cc95ac123b5765d81354e0aa7ca7
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:46 +0000

    careful steer it bread cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
