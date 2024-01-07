f57928eb9500b23284ff27ac2e03ab388cd5d234
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:25:36 +0000

    if avoid top contents pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
