db08a5af6ad308aadcdb97bda4e310293404f607
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:23 +0000

    interview fierce minority left ask

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
