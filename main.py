5ed8d2637457c8a410a04ef47dc4af69d9c5ef77
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:38:07 +0000

    top quit rush convinced memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(lst):
-        return min(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
