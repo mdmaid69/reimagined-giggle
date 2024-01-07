ece42569faa318c01a9eb99d18cad767d699a2d9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:11:34 +0000

    rescue repair as week him

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
