a55a6f8f3781ca2263a6684800d1bc3ddff34df9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:01:14 +0000

    block problem tap continue development

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
