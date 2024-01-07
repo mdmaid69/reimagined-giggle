c30427d35045e02fb64336349bc1af96649a1f1f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:13:43 +0000

    proof cheque reduction only 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
