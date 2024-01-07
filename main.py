bc140bfed369052b4d589a70ffb70dd6de0b1e39
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:49:17 +0000

    step right memory apologizeapologise flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
