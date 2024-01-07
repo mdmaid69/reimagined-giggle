d95dd53e9235a4db284a595a24f42f9cfa98b42e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:34 +0000

    mess breakthrough church giveout OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_pressure(force, area):
+        return force / area
