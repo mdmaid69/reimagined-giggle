7b85c1373d73fd07715c8caf880838d1a35c94c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:54:12 +0000

    wet office mean not classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
