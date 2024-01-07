434642849d1f2540ae7e0108c661ed1b95f86913
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:44:05 +0000

    means demand understand drive serve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
