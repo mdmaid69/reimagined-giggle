4bc380162e742ff0a1d4ec3bb464765d4d4fb438
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:59:26 +0000

    fond before cap afterwards underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import random
+def roll_die():
+        return random.randint(1, 6)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
