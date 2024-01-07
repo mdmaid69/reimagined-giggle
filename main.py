a41d6e886f011d1ac80a806e4591eb2158f38007
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:38:10 +0000

    gift church rise chance forget

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
