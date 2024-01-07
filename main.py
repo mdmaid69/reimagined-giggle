f89b166cbc51e5fdeafbd7f696d9bb2758ec8902
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:32 +0000

    warning heavy head laugh fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
