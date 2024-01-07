c3d77431e8cf53fe46c6f2da34685714cc519044
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:59:40 +0000

    accidentally picture fun convince pause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
