70ef6916589a8621ef35cedd6f4d84717b88685b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:12:23 +0000

    while greeting special stupid afraid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
