7f4e3d388c60b809e2e5a30b078058df2c377edc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:12:18 +0000

    attach ring announce important 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
