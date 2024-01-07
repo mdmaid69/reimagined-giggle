1ffd16f9800990c65f31c9641660498f5395a79b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:12:15 +0000

    breakfast usually needle less in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def change_working_directory(path):
+        os.chdir(path)
