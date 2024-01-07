d12157fa604c73b3f7f4e9f079a7400f36970336
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:29:26 +0000

    best knock pen prize bother

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_cosine(x):
+        return math.cos(x)
