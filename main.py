3bfa5d28d0a9f9deec87bd01854af44fb786d27d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:44:03 +0000

    section dance wherever earn ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
