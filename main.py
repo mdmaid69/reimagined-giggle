30760c473c08f12754a86655855859cb0b0dec05
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:27:55 +0000

    city favour secret wide bend

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
