72d779f7445044760f4c36b016ad327a75eb5490
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:58:18 +0000

    thinkof quickly light fast place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_str(array):
+        return str(array)
