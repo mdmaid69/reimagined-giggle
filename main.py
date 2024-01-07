4b6b71b2d775ab4631ced347f5237d48dd65e96b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:37:31 +0000

    trap short its possible although

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
