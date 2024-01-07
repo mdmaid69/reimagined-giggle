c8e93b77261d74724b04c1b1f977460a4ec77ee5
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:03:51 +0000

    well sensible part sink bringup

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def convert_to_binary(n):
-        return bin(n)
+for i in range(10): print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
