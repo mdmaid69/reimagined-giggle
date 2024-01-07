fb347647e4b4c9e14452e7140e8ab390eb7ae09e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:34 +0000

    notice port doubt town money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
