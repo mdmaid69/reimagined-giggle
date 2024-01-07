6a3ba5aa7b0338d92d505d0552171ba941f230d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:33:28 +0000

    ilovedicks surface burst strike face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_counter():
+        return collections.Counter()
