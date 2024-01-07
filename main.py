7fb6cefca12d95bc22e2c6219a898d044ab0f452
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:25 +0000

    disgusting smile shoot claim natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
