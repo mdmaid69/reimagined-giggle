855ba7ad7947227aaec9e298a7fcd6449eb9a05f
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:44 +0000

    sure wall unkind spill boring

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_list(array):
-        return list(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import time
+def get_current_time():
+        return time.ctime()
