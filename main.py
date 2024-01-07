761b707badf8fbeef3f41fbc955095ca03995e0b
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:47:57 +0000

    want watch considering thanks trial

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
