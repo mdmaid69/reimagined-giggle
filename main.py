52bb31b1cd7fbf97fa4d9161ce4723656fb13c7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:24:39 +0000

    Cc pump especially improve Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
