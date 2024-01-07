7ef05dacd5621fed3be30d95af08c2aceaf56576
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:04 +0000

    worst flight whenever star save

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
