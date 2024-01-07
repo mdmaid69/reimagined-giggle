6f1e6a794347b6834a4136c2805a5990c99feb06
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:17:05 +0000

    check dig familiar finger exactly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-print(os.getcwd())
+def calculate_pressure(force, area):
+        return force / area
+def calculate_density(mass, volume):
+        return mass / volume
