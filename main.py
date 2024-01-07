f0f2edfdbfc5f8c361e82d151a1fe946d9723ce9
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:29:17 +0000

    shelter bringforward shape price forward

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import os
-print(os.getcwd())
+print([x**2 for x in range(10)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
