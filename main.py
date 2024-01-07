2e0e134c03149a89f109b742969e4a5ec31044a7
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:40 +0000

    turnout laugh squeeze convince debt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import logging
-def log_message(message):
-        logging.info(message)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def add_numbers(x, y):
+        return x + y
