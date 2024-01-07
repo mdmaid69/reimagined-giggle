564efb09276ba14e173a46884cdd9d2cfe59fca7
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:54:34 +0000

    acceptable how string objection wheel

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
