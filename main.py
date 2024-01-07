48e5fd702ea23d688bfdea6495d89166cb0a5146
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:50:43 +0000

    steer tap temper always start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
