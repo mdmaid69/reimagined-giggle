4d33fdd1696c48178c49cf4a4a34eee3557d45a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:39:28 +0000

    beside broken with that approve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def is_odd(n):
+        return n % 2 != 0
