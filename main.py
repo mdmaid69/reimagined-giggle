49cff248939f8c5cf0cd6a7500c67a8ddd5f39ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:35:56 +0000

    noone phone interview law healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
