5b7cfae46eb2a090e8f17baa497492e9c005e970
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:55:18 +0000

    under especially produce shadow Yy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
