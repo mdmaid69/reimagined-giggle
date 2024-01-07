38d0f007685a4b188bcfbdb17e7d1ef62ad88f7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:00:34 +0000

    many free violence may address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
