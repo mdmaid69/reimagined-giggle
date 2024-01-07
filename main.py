02686ab117ca37349da5f357efe88156c0666aad
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:45:41 +0000

    nerve pot influence bear before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
