ae5ec4d7ae33cce45c554e01b59a5491fcc9336f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:03:59 +0000

    provide sell own accommodation bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def is_even(n):
+        return n % 2 == 0
