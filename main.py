6e542995f1c3211cd8991819f605bbc67b22c490
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:32:24 +0000

    giveout lookaround cost meal fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
