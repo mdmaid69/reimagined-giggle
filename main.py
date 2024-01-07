dd5e612c64c77ac313b7f8bca7098fd1f2b1543b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:22:58 +0000

    spin explain accidental yesterday delight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def square_number(x):
-        return x**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
