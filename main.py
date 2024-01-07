13214b34cd085e538798c0bd52f4e83f12f50036
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:21:46 +0000

    even deliberate test thirsty 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
