bfdcb49c80e0e941a383b2aaef6ac8b3f282288a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:53:24 +0000

    spray back imagine total reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
