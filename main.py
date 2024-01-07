334f0401f6980266d99c5117b6566297a92e3337
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:01:25 +0000

    mine bringdown what transport cough

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
