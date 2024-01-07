112eae3422a841dea52c9eff8a2d97e747a456a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:13:39 +0000

    ahead idea music different war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
