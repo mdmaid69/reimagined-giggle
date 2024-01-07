1f71ab8a7a2c3f34e28ca4f90790c2e85385a7fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:54:01 +0000

    equal agree pile trick owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
