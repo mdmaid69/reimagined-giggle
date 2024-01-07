ed970269452bd57f31a4cc0f5acd82adce819e9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:04:07 +0000

    sticky flow myself promise weak

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
