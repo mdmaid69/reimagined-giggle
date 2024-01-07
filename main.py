86e3a2213a7a5e8049f33d3e1bfbb7abf0890e2e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:23:38 +0000

    journey feed form move happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_octal(n):
-        return oct(n)
+  def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
