60daf09a5f8670d34e7d139e59d6ae7dede21d17
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:43:15 +0000

    declare thirsty guide uncertain cum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
