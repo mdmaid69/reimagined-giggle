da60765b18c53a0d263ebf68b380c519ba29488e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:43:09 +0000

    such happen basically properly no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
