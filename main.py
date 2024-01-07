2af13eb763399c71fbdd169a720865a9b578577f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:11:04 +0000

    rate rain mood desperate value

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_str(array):
-        return str(array)
+import sys
+def exit_program():
+        sys.exit()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
