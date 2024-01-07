0bc162efb5b9cc1b365bb055b37ac2847f5f7a50
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:41 +0000

    impressive edge impatient flat couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+print(random.randint(0, 100))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
