ee5a944a5b69caae124b15ba576c9f24802de9a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:40:39 +0000

    able violence describe discover everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
