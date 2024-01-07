c5b003a8508303e62429a977828066f4b4b75f40
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:40:39 +0000

    principle market middle advanced occur

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
