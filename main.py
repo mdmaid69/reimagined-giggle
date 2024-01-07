efc4f3e29c2393230b2a86fce91d03bfe3f290ea
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:29:46 +0000

    figure excited music close unkind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
