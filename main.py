d0b1494e2a346df2c2f7e123fac0c20e019d96bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:26:24 +0000

    wander teach cheat aware organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
