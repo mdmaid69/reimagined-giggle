07c7bd0676fd3f3843ceb9aa7a5b8b53f25f254e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:33:46 +0000

    callup mood workout miss off

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def reverse_string(s):
+        return s[::-1]
