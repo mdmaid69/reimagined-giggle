40a62c3884b9703853ddee7791d58959f54902db
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:26:22 +0000

    future so exist course brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
