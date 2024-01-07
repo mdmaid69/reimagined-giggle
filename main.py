f8db70434b3867f4d526288503668bbbdcd12d80
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:07:31 +0000

    crime such persuade hurt lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
