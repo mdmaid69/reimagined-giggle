c034d1b912b044037405be2e35ebab3c2a5e9686
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:46 +0000

    instead repeat regional work above

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-print(time.time())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
