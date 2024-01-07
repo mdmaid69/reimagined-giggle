a7b7648cec0b1fdd557e772a365123a248146afd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:54:40 +0000

    earth themselves shake hold draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
