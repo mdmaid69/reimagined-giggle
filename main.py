0763bf4aea5803b0b2af541a4e8b4b6a56613034
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:13:17 +0000

    container couple show tall tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
