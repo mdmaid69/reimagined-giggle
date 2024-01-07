e6b15f5f78ffb4f482a386d8ab2d887b13fd4025
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:50:39 +0000

    track giveaway pump compare giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
