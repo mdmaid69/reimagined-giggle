75cc51b7771f89d136fe9b84cb91f1f2b588dffc
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:30:09 +0000

    careless splash story stamp giveback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
