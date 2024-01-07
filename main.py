e166fdcbe42cfc1afe8326144203c2a6f8ee8480
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:43:36 +0000

    ashamed swim realize reply unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
