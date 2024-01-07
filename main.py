74df2222c9a2e521b97dff5a8c39d130fd716525
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:46:14 +0000

    wipe workon proud letter noisy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
