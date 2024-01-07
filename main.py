fa44e7eba1022135c43a6ff16f915aad859fd627
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:48:23 +0000

    roof crowded level visit when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
