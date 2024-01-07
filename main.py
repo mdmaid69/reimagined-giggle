5db78fe110d3b0ecc3a857a91c09e1ed76c16a3f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:18:59 +0000

    each aim lookafter advice answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
