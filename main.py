8fa9ae6d21d8f541698c85ddf5195a10fd431fde
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:30:18 +0000

    law anywhere high clothes turnaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
