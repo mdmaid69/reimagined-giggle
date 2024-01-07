d33ebdd1a3349e82f66f4dd98ca747896a32bd58
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:36:47 +0000

    desperate admire best breakdown score

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_int(array):
+        return int(array[0])
