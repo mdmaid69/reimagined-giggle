4177939858b745bd0cffb442e2a32e704959eaec
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:09:38 +0000

    ilovedicks keepaway until principle this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
