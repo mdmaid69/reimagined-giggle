16d600bed2d4876598d8e04e6723d40ab351772f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:53:10 +0000

    recognize within detail steady forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
