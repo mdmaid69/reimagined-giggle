7d5404ea29673ba6027523383a8c38b5cc456bea
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:54:50 +0000

    part narrow watch excited charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def clear_array(array):
-        array *= 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def count_words(sentence):
+        return len(sentence.split())
