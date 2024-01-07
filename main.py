c8b75331a0abbe1b598c5f613e57a49a2183243b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:43:13 +0000

    community bath may what rush

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def convert_to_octal(n):
+        return oct(n)
