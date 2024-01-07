7673bd15c79cc89da989e5ea8fb7fb84979b782c
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:41:01 +0000

    born smile cost national band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def remove_directory(path):
+        os.rmdir(path)
