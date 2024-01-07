d08305ad60d659cc8ad200aafe7dd5d5e0f8dbfe
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:12:40 +0000

    low thanks wing 572enter bye

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
