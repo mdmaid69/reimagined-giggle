f6e9fa4aa32816896543c9394c61f76807d417be
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:39:18 +0000

    show complain divide serve family

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import logging
+def log_message(message):
+        logging.info(message)
+import datetime
+print(datetime.datetime.now())
