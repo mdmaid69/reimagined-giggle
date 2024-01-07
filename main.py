692ee0d8649a4df930a17646976440b182468b69
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:45:09 +0000

    period number sleep lead tonight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
