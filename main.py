3e484057ea4d4f6f1adf3eb2d708139ff10b5807
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:27:54 +0000

    translate free inquire system cruel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_circle(r):
+        return 3.14 * r**2
