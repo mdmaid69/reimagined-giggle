dcaee9d10b7ae77c1fc48353c3cb9bdf37e6804c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:49:47 +0000

    squash fairly job interview light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_index(array, item):
+        return array.index(item)
