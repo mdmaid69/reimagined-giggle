74fb51ff4554e8ef51a6cbf8c9bb3f766a38e96a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:37 +0000

    wrong gate study only blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_stack():
+        return collections.deque()
