bd7bc9cf62838a2dcbd4cf23fbbf4cf6df41ea80
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:13 +0000

    write connect 564encourage ashamed side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
