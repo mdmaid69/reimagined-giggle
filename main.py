984898164c4e6e4f7c4b618b8d5935796de82ced
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:51:41 +0000

    undo hit responsible room impossible

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import sys
+def exit_program():
+        sys.exit()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
