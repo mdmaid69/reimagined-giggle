88e6524a8fcbaf0b3e3f535e22f63fd6915dd4a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:32 +0000

    off fat friendly bored mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def find_unique_words(sentence):
+        return set(sentence.split())
