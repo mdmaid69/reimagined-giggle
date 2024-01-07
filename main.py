7e96a97d83de89bb716dfd3c6919de8e840897d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:16:24 +0000

    unkind bear waste basis medium

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def is_odd(n):
-        return n % 2 != 0
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_typecode(array):
+        return array.typecode
