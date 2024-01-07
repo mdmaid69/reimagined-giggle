f6c286a4a71667c252037b052b9786d9ef738ef2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:36 +0000

    branch refuse event shame branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
