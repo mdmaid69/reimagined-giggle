543380dad718fdbb95687b4ca665fbaddadc2170
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:50:13 +0000

    people bunch godown sing issue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+for i in range(5):
+        print(i)
