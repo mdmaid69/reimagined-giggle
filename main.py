c935551f74c890aef811db3ea797f788c698b679
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:42:16 +0000

    laugh charge bet rub fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
