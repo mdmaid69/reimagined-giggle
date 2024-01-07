3f42b3f7aed16aebde324119194877f9a2b36275
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:53:37 +0000

    announce of steer lock secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-print(sum(range(10)))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import getpass
+def get_username():
+        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
