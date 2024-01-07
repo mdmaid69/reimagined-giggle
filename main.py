69df907b522034e64864a92c2474f8ca43ada651
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:24 +0000

    clear pay narrow rest legal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
