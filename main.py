defbae676b07b6bd91f3c31504e5b588c1795ae4
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:10:51 +0000

    money definite table Zz make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
