8ef52c851dcb8fae9e34ffd41dd014fc88393e63
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:13:23 +0000

    maintain suffer wait mind hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
