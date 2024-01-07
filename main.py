3e00163d767836ecdb604e6fced0b206064f04ae
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:06:25 +0000

    name design mind depend total

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_work(force, distance):
-        return force * distance
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
