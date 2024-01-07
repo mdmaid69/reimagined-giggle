d592696928a8a6b9e64315d205ba8288c0e800b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:20:04 +0000

    behind blind clean thing warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
