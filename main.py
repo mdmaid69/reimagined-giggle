85e96028435303f994c8db8ee82bc849ac37fdf3
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:53:43 +0000

    apart change confuse makeinto tie

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
