33ae2cd632209ddf634d51ff5cdba0b04b274b15
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:36 +0000

    positive substance freedom beneath criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
