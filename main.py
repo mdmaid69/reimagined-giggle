dce4e62cce7fd5b6b85d5da515facffaaf7304d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:14 +0000

    embarrassed worst last exercise leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
