6a257f93a5d019dcab1092f5ce27938f395420f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:28:17 +0000

    tip calmly radio split sensitive

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
