6d22b7585a3acfe147572d0afb5b4d597916201f
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:22:42 +0000

    herself off shall test pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
