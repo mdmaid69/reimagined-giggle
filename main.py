1126d68bb10476452c4f8eba34d0308735212ae0
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:55:23 +0000

    like where even clever pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
