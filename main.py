ec0de0e3cf31f646ae604c0b32bf1aeeef601aac
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:39:59 +0000

    involve trouble somehow crash terrible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
