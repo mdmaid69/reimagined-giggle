3e57ade28095b4247ee884aa2f44a8c6ad690f30
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:09:39 +0000

    wealth true remember regularly lucky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
