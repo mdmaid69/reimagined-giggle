df70ef8f77a2205fd938cda2d0fa87230c952851
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:49:13 +0000

    drink education information ignore lookback(on)

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def create_user_list():
+        return collections.UserList()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
