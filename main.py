078b7b55ebbb36b1b5707443d20ea70239616653
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:15:06 +0000

    clean arrange concentrate me common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+for i in range(10): print(i)
