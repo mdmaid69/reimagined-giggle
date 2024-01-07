6cef69d3e79236e4c3e8713236238ba8ff4656d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:13:06 +0000

    design born card strength joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def reverse_list(lst):
+        return lst[::-1]
