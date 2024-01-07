a8bb4141fb5811db4ce562642b5ae34c1498f9fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:45:40 +0000

    idea scale either honest fork

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_list(array):
+        return list(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
