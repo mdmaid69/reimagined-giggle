9b3f12a522e42dad2d0fcfc4cc9bfc2eec58f9f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:20:03 +0000

    sudden majority spring space why

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(lst):
+        return max(lst)
