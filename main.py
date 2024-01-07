bc9146463b8ceac47fcd3dfee086c1435995c93c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:28:51 +0000

    better take break anxious exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_set(array):
+        return set(array)
