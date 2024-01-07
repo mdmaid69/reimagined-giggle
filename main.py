ec378e579668e0cd96fb70202919dd447cbe7078
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:22:06 +0000

    born interesting past level common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
 def calculate_simple_interest(principal, rate, time):
         return principal * rate * time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
