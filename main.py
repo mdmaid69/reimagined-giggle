0f9b87848dab8fe55b400b3bf271d16b019de0cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:49:09 +0000

    extra related callat offence fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
