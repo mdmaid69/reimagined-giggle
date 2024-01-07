a6e61041ec5085ec1d6e5a53d190e0cbfbab5394
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:01:17 +0000

    get clearly period responsible ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
