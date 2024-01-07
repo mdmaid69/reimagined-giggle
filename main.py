5c83ef691382b148141d937431a362863bf1c3ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:36 +0000

    mean grow callout satisfied popular

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
