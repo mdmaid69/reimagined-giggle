f3655673e2f3a9b4cd71c6038411dccf750df3f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:27:51 +0000

    callout but throw goout inquire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
