03b57531fe4a479b45bcf5df6d095071867f41d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:05:37 +0000

    instruction marry once more print

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
