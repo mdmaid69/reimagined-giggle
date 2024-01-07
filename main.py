aa0136dcc2687917ab819de752b44f23a7f0b8ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:19:11 +0000

    impossible study deliberately get getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
