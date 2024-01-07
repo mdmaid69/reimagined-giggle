a2f8c43fd6478cc25ee76d0a57a3acb005f0ce89
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:07:52 +0000

    school describe terrible rarely but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_characters(sentence):
+        return len(sentence)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
