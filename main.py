90a0670a9a876b2bde6a0ebe6f185931ef83b31b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:08 +0000

    get direction nature comeout little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
