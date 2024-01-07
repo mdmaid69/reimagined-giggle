1c4f1f5eb065939b64b0c26e703249fe12d64b84
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:35:38 +0000

    ashamed float hope getaway bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import csv
   def write_to_csv_file(file_name, data):
         with open(file_name, "w", newline="") as file:
