3ed4818cf6a289b832f27a5661d40d53eea269f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:27 +0000

    wait meeting hard him age

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
