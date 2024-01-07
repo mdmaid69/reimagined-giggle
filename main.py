b77ffa67cbd84cdf2504f09a3e617d216cabf53f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:51:08 +0000

    inside collect disappointed history crowd

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
