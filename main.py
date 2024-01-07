e264f9a953241c1fc93d661eb0b71a2988db71e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:10:29 +0000

    clue gooff prefer clever bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
