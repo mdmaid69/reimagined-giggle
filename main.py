88e430c6526bdb108521c36a14c933bcf2aefe27
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:17:30 +0000

    wrong stay address afford like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
