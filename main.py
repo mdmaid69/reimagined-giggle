d6590ecc22a7a1622ccc45508560d8f6616e8173
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:04:53 +0000

    completely danger start project soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import time
+print(time.time())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
