fda5fa5739ddafbd6fc76794a56b7871b9300bbc
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:41:48 +0000

    afternoon label underneath likely underneath

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def find_max(lst):
+        return max(lst)
+def calculate_density(mass, volume):
+        return mass / volume
