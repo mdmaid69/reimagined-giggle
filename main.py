76ef45855de2f5edd0ec3dafe5394380b2558324
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:44:38 +0000

    also offend separate legal grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
