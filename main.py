ac82d4662df377f538509cfa07aa1cb6b6d30983
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:03:32 +0000

    reason extraordinary open tight rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+print(sum(range(10)))
