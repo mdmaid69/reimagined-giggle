5f48325779a536e2933e865bb4a9fd13b05a2c7d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:05:24 +0000

    support display expect education guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
