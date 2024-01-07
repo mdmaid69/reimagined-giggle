1482b034c6fc71f45ba8e690f2002a424b9e91ac
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:20:52 +0000

    completely respect show water war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
