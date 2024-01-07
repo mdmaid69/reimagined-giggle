5db02b504d7af0f052fafefdf98c5b41e40c8791
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:13:49 +0000

    children able equipment holiday trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
