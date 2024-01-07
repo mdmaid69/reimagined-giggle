b1f499e0c1c6e0dafb369e4688abe562271b1db3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:53:21 +0000

    breathe contact common stiff healthy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
