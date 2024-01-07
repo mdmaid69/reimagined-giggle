7e5dea34a8c60fc539f4f9440d8101af255a40de
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:20:39 +0000

    cause debt anyone tired trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def find_max(lst):
+        return max(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
