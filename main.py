422e6d6f6735a255afafe00bbfc3df9196e6e898
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:17:52 +0000

    key Cc apparently pleasure mind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
