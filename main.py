f8986b543a48773033e529981fbd0f61dfd24432
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:09:11 +0000

    blame fall crash spin tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
