37ba008772a4da135aa47f75da3f9c20f60ad77e
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:06 +0000

    sentence wind lookback(on) everybody probably

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 sentence = "Hello, world!"
 from collections import Counter
 print("Word frequencies:", Counter(sentence.split()))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_speed(distance, time):
+        return distance / time
