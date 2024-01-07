4784403fc631d535af2d1d14a4b60b02e61a8dc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:54:24 +0000

    gentle Ss praise cap estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
