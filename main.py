6460db5054d69ab83e5ee1120b93b0b393ff2b31
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:22:05 +0000

    partner wind except thought bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
