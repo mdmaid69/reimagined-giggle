ac263b0e54dc1a46d4769abc34c983ce1128d30a
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:24:07 +0000

    fasten total opinion simple thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
