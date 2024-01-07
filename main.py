dbda99b6a6afa2a0bfcbf511ae2b19c8279fc9e3
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:41:36 +0000

    period legal high nice disturb

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import collections
+def create_user_list():
+        return collections.UserList()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
