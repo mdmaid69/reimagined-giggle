c31ebefc83c9f292ff41fe45478d31b482a0207d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:47 +0000

    succeed hospital aim outside worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
