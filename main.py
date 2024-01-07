b7901058fcfc27e816fc5c295b6cab79ab5820f8
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:05 +0000

    responsibility skirt enthusiastic closed slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
