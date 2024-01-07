71a2df55fae761d053e32f78ee48205c0c71e5df
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:09 +0000

    chin relax reliable likely dress

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
