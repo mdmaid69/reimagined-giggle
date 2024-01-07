abda2261676b080e10c1774a7aaabd7c39cd424f
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:55:00 +0000

    mine expect aim though except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_array_to_list(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
