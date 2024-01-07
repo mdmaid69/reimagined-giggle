b7f5d7b02feb44289718d620b17de9fe8ca727b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:56:47 +0000

    her public tour angry occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
