96a6f44a07ed0912a2dce46ad1ecce28ff786de9
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:47:28 +0000

    steam hello murder organization excuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
