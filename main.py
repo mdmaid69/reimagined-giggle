b36d5a74ea18e85de019c08a26e1db9a688de103
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:12:28 +0000

    characteristic regional wait breakup coat

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
