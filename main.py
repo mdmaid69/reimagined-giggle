090ad2201de79d383d2d7d05c05cc6224bdb10b5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:21:14 +0000

    hit follow limit wish clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def sort_list(lst):
+        return sorted(lst)
