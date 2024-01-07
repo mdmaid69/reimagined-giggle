eb99e2fb54ca8fb3bc8bfc40f5475a70f745c985
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:13:17 +0000

    who level trousers murder cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_array_length(array):
+        return len(array)
