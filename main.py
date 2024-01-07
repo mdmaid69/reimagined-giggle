692a3b08914246a217952701cd51ef518e82fa5c
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:25:22 +0000

    appearance suckme choice object trap

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
