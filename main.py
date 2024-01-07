b0f81fe9b42f3b05a54c4a63768edfa2c6d53ca0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:53:47 +0000

    touch workon free lean shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def reverse_list(lst):
+        return lst[::-1]
