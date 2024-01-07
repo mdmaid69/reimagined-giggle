1907ac94eb9319dc72279a949fa6fade45ea5e93
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:17 +0000

    grand any whisper fact listenfor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
