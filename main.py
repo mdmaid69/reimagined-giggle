1c0fb726a1f2f1a56e00c318411ecea7577e2b58
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:10:14 +0000

    anybody method tip scared gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def reverse_array(array):
-        array.reverse()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
