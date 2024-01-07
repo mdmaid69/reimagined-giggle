a25191790549cc6ffaa65f500914bddf73f6e054
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:20:45 +0000

    attention speak situation woman long

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def check_if_file_exists(file_name):
         return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
