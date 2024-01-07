3097d256e1597a6a15b617fcd34cf44b6ad4fa53
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:40:27 +0000

    regular admire ride peace nervous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
