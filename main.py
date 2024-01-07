1ae8307530f5016f4b2eb16e9989b22a2d8fe26e
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:41:51 +0000

    interrupt office payment above underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
