49ea0bdd9e50a0a1f67d976868023910b812e697
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:34:18 +0000

    keepup formal which especially towards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
