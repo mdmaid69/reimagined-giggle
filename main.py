4ff6c62f221a03beb27fa953881b4007690206f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:32:11 +0000

    under mention near argument reasonable

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-print(math.pi)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
