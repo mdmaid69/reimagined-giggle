fbacbb23d51d5c1ecb98c829292d5b96714a3f5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:40:53 +0000

    man knowledge love talk waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_work(force, distance):
-        return force * distance
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import sys
+def exit_program():
+        sys.exit()
