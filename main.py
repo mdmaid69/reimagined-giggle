8f9b6f46e1aa4c91620e01b9a93da1c05251f5db
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:59 +0000

    last used brown early less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
