2359235837675e59e7d055ba03f73ed4400794be
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:45:59 +0000

    bang aim however quiet slice

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import os
+print(os.getcwd())
