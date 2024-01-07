6f2fbbf6971d3f2e5b266eb9a7819c3ff821de14
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:26:04 +0000

    present people equal duty free

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-print(time.time())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
