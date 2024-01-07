97c11539bdae7b09140aab99a9c550bf155fcb05
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:17:50 +0000

    especially pay gooff you proud

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
