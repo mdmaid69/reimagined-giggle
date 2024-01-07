f58bef8a47fb076364818d18ff2aac6288b7553a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:21:12 +0000

    earth would turn fast likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
