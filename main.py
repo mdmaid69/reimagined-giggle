ce5ebd1956260e4cef6d60bfeaf21c25e76c65ca
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:22:23 +0000

    and step consist survive wheel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
