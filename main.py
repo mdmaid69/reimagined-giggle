21490de5898b6975670fba2c6f4e30ecd685acae
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:07:06 +0000

    chief study grow slight sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import time
+def get_time_since_epoch():
+        return time.time()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
