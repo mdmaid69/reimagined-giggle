55deef204970d8d16239728e0df4d1e68fafd033
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:46:36 +0000

    wipe pot this spite twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
