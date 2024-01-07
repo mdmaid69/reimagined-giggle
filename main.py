0b273d8fe6f6cbb78e343c7d91374afd04becc80
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:46 +0000

    introduction weigh cross note definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
