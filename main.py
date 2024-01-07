cb20e1728f4ae7205f1cbded9d227de0dfb5080f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:57:38 +0000

    hurt trade discussion noise handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
