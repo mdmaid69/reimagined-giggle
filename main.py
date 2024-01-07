7e99b422c95f84b3f9008598c4a9df125be6a814
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:30:51 +0000

    grade firm find because lot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import logging
+def log_message(message):
+        logging.info(message)
