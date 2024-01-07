783508f969756eb07fc93e834f5c0dbd22b3736d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:57:35 +0000

    ask actually commit separate agreement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
