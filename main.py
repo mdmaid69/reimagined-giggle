5595a60da5c50566e1969a261eabe5446b6bd12a
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:43:35 +0000

    previous fall near steady brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
