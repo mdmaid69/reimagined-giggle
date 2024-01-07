af00120e7913dec8665abeffbb691c11044f98d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:55:06 +0000

    our beginning frightened achievement respect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_rectangle(l, w):
+        return l * w
