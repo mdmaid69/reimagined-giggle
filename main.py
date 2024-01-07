dfe84759636cd124ec4a7b7c58d2351c96680b0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:59:14 +0000

    hollow contrast mass good speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
