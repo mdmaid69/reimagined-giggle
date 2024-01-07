351e6bf8a927e629db6da2a3bec8dd82db078261
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:18:26 +0000

    busy delight grade attractive you

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+text = "Hello, world!"
+print("Words:", len(text.split()))
