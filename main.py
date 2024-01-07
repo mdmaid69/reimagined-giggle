cdebb35f4cbfd519c530c807c11f4ba9f4a3a941
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:42:26 +0000

    market back hold sea real

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
