fe9c4ed1fabaf09e0ff1479fec8607636a079758
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:10 +0000

    disgusting statement new waste proof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+print(os.getcwd())
+def convert_to_octal(n):
+        return oct(n)
