220b68ef685de068924e40a5f4d2e7f5f9457bad
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:13 +0000

    yours thorough leave almost obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
