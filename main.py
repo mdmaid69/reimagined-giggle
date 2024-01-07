ab685c01cb06e55715411007441399c15e23296f
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:37:16 +0000

    somehow godown learn calloff common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
