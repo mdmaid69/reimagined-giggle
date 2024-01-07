c137a989a7e3b012238637ec2e334fb6a7f0fbe8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:41 +0000

    introduction appear dull settle payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
