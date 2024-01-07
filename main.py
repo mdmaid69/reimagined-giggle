d3d83e996b67030bd77a178e4bd7a72ae4e01d62
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:10:29 +0000

    police rain such scheme cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
