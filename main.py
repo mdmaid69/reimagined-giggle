12c2cedd9387a35055616081aab3fca6e5408254
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:52:38 +0000

    stage brief callup very ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+print(os.getcwd())
