84a69f96bb288d8a13cda6e5b1daaba2cf3c063b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:39 +0000

    exciting serve stop firm uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
