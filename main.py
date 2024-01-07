4f6e161f1b5fdbbc4e979ce571609cdfaaeaf5d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:23:57 +0000

    replace blind shoot sentence way

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
