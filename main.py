fe4e0783c2ff84cfb9640d93dbb55fe534c60652
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:25:51 +0000

    suckme outside mark breakdown polite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
