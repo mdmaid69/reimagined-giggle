c21e8a90a3f56f6afcc335c1b2f61653c260b881
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:12:20 +0000

    undo several past deal offer

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_odd(n):
+        return n % 2 != 0
