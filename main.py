7430859bbb9e3888bd792afe34c4fee23f9a9f5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:03 +0000

    great bath work pencil plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
