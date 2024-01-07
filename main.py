4dd77befea714a7bce9c0b055011957cd3f8e54c
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:23:42 +0000

    extremely wheel below zero entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
