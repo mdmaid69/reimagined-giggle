ee4838868694392dbe3d8ee4f4bebbc190a7c4c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:54:09 +0000

    brain calmly operation threaten bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def print_python_version():
+        print(sys.version)
