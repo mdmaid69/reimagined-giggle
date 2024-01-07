018cc56d6721b11bf9ed4ebb86ebcce3e7be3f14
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:44:42 +0000

    level disagree evidence by left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
