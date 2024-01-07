7dbce3bfb402fb242d2556c4f35c6f2ca947917a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:24 +0000

    pool history murder wave person

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pressure(force, area):
+        return force / area
+def calculate_force(mass, acceleration):
+        return mass * acceleration
