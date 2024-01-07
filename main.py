29db229e7a49337bd306482fc45fae118b5a6f0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:57:06 +0000

    blame we comeround nose promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_current_directory():
+        return os.getcwd()
