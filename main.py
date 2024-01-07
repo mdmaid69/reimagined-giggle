224e249258841cf291f6c151b2af3134f600d967
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:26:33 +0000

    trap practice study flat stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
