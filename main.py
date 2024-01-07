ceafbcbac1073585124a546281c31a82cb66109a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:26:03 +0000

    include play proper by wet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
