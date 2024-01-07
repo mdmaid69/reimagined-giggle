6575564e60781d441571ba9f9e6fdedaa15b479c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:07 +0000

    praise sound argue gooff upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
