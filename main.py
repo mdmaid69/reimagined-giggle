36fc0a6d1e7c856b98fc4efba9197707f25b3f78
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:15:13 +0000

    careless actually base account demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+print("Hello, world!")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
