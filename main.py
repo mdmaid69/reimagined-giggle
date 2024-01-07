60d17f4dee15fb20efdda96a22ea3bb188f8847b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:21:55 +0000

    takeout middle form necessary make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
