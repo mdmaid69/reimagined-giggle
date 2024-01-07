fb05b5c0c03b98425bd1c5e82130269fba1f24f1
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:06:12 +0000

    human question before its ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
