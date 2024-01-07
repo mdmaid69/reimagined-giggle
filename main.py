0f0c65374565283aad22c1cfc6a1e328d2aab28f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:18 +0000

    too agreement holdup instrument hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
