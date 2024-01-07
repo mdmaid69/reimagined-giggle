5d30530d09b1278979b54e3b0d1f87276116ea72
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:39:36 +0000

    message smell needle say keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+print(time.time())
