abcdc028a8bc4beb423539f4ac6d5c0bd8424c26
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:04:55 +0000

    far gather famous improvement original

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
