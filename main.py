3f7d36619633f7243b3742b85635aa9b2db75e1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:02:54 +0000

    practise week mood partner thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
