f50b533ea8e9f450160807eeb677adc60eedc6b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:18:25 +0000

    unlucky genuine could recently progress

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
