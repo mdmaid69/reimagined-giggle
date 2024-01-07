58fb2fb6f9575da1078fa0f60787a64daefb7b83
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:55:54 +0000

    doubt dry today sign only

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
