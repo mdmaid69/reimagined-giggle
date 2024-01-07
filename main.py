6bc7993aaf1b45ca01e54620d85f2a73f3dba01b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:20 +0000

    dig introduce left male breakup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
