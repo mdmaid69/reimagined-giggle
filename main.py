32e93aaa96eb38731a6f000694e3cf3a5e99568f
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:35:52 +0000

    chief slowly which normal capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
