044391a74c1232cf1c7d2ac29a2a4055bbdb300c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:26:06 +0000

    fight sink by uncertain callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_speed(distance, time):
+        return distance / time
+import sys
+def exit_program():
+        sys.exit()
