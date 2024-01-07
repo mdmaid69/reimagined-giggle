adbab8005dcfa2e2148870d02d6f1c4555026611
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:48:29 +0000

    key mad software information stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
