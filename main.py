8c7e2950e779656cb470f58fafd968c19696f144
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:46:14 +0000

    female tend gradually acceptable drop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
