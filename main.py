96b6fe5ba450e8a3bfe06976a8ecf2056a292737
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:05:42 +0000

    Uu callup how zero respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
