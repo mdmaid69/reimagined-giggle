8266883153a0269b3fb47e02a27a4da6f6d6ae3e
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:24:27 +0000

    wealth distant experience her paper

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
