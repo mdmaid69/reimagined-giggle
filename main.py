87980263a0fdcd008418c943da7bbf3ac93cb24b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:28 +0000

    land badly destroy somewhere fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
