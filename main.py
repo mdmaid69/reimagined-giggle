a3d11f637adb03d5a00f7ae0a47c3d7065a5227a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:51:26 +0000

    terrible minor here favour someone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import sys
-print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
