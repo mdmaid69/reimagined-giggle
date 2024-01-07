f6c1d18eebadd571a48352f079a3ca9063cc4950
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:46 +0000

    complaint since commit after test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import time
+def get_current_time():
+        return time.ctime()
