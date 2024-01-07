f6322dbc93eae91d64c9398411b894436f9d6ee8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:28:23 +0000

    turnup worse criticize tomorrow thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
