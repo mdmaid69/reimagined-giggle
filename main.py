e9a9956963de9a144028ecf2bf731a4af34cd44e
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:58:57 +0000

    affect thankyou pair downstairs accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
