2fdc94b2855d80fbdb12e1665c01ed0f5ef6c7d6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:00:22 +0000

    runaway mainly increase yet present

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
