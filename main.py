2a741d3a75b3ff17451ad56e19f1a8b300d0f91c
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:36:31 +0000

    shut platform flat approve obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
