62a19e6a401a1aed2b45d30c5bdfec42fe07ce72
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:37:23 +0000

    back giveout live embarrassed speak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
