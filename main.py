acbba0590b174242a774c38ad9aaf776365895c2
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:00:54 +0000

    back tough clothes challenge rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
