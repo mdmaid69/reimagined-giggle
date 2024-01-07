c2b4ac7ab7137553a41c99bfb5686e56bfc7873d
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:31:44 +0000

    permission rest best emergency soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def count_characters(sentence):
+        return len(sentence)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
