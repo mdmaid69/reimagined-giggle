0eb56baf50e41189502880ebaf9ecf6aa612dc25
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:42:02 +0000

    clothes die perform shade unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
