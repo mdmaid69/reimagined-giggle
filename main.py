2eff56056f75d607b4df5400289352a189b744b7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:38 +0000

    568energy copy pull related kind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
