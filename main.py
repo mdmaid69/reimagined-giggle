a16752e9173fe5442cbdce032e17f5862f337962
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:05 +0000

    force lucky star path anxious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
