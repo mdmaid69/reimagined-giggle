cb1e87093d09047565d0a85a63dd0116f18ac719
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:26 +0000

    little everywhere child choose various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
