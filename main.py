002d6426b07b3a5932e42cac4d26e541048029ed
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:24:38 +0000

    fall disturb corner suit justice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
