6b9ceacb820b0095dbedf116f419463bdd0db0a2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:18:05 +0000

    left dull hers swim whichever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
