5fdcf49e66431b91bac063d3ee6bf3aea54fa9f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:47 +0000

    touch trap measure yourself image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
