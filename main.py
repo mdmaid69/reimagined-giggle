c341ed2826bf3a6fd1c39ca439d294f055a56c74
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:14:50 +0000

    computer mail pretty idea suggestion

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
