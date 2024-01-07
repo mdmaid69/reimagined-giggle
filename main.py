6e5d01ddf848c1cdb9891d894422ccbc2bb3cf6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:41:38 +0000

    callat mark vision show cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
