5255ae58e9edd3c788f580eec4cc61782f9b31b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:25:40 +0000

    path quarter watch area ground

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def get_array_length(array):
-        return len(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
