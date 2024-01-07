faa6c6b3974eeb3c439bdfdcffc79977bd8ad65e
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:15:35 +0000

    free introduce OKorokay joke dinner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import os
-print(os.getcwd())
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+print(math.pi)
