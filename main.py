f7f80e94f803d15e18d9f61a4baf208d4f43509b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:03:50 +0000

    material turn also cause measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
