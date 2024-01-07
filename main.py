f2f7cb63e9d62a5932ab2143e382809fc6873347
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:32:53 +0000

    get clothes priority shelter breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_even(n):
+        return n % 2 == 0
