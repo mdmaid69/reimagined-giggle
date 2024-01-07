1cdf48da8cd2bac6b92d6f7296b80b9dc02cacc1
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:50 +0000

    crime silent honest good steady

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
