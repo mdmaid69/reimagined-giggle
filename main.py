bd06bb3f331473a4665accda342484c0d40ade82
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:35:56 +0000

    guilty swim immediate belong wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_as_str(array):
+        return str(array)
