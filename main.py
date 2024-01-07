12cab06f94a05e54dcad33a46b24f5f7bbad4817
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:41:25 +0000

    unfair ought average measure worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
