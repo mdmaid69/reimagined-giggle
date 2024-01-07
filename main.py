f2e6076352fdd055a7aa65609eb99073742d9513
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:27 +0000

    splash unit goon pump rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
