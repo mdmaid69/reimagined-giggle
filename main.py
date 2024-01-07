42da67bdc6ed45940ca1a49253ba21c854151d52
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:06:27 +0000

    concerned autumn rain band concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
