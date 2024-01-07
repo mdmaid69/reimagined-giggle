a96c7ab7f9dc385bc5bf90bba843ac993463016f
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:36:26 +0000

    announce record too problem protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
