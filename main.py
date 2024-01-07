25dc0646e2935e5478478e1a400f030b8dccbe76
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:38 +0000

    wind forget principle miss could

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_pressure(force, area):
+        return force / area
