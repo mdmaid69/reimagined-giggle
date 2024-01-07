e025f5127a32653e7581c5b53e4775dbda90ec6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:49:35 +0000

    refuse rain land neck note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
