1fdb69610e45c42c9f1b884c87f73a17bb644439
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:00:54 +0000

    national keepup abroad smell remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
