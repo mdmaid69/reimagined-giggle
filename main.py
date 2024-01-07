f11357aa093ef3c29dde1ee3286065bda368f7e6
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:33:46 +0000

    turnover weak mainly round complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_area_rectangle(l, w):
+        return l * w
