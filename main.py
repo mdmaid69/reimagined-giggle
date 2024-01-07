7047cb4083639e0c89f565c29d547f8243c18c51
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:02:32 +0000

    confidence with marry chin turninto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
