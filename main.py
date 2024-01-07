850b45028a2d3963d0ad134c5a7223a59e59ada1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:14:56 +0000

    trick bringforward example react steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
