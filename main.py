6db689417bda1cc19af72e3f9c4fa39f889f00ad
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:21:34 +0000

    proper become react study that

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
