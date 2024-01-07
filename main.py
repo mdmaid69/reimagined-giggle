fcc6ff7434189ffe587586fd3c586cac812e825c
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:45:58 +0000

    exit shout quickly fit Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
