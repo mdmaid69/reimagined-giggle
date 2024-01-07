cbfffea8c360deda1d50525349c7d7cef0a9af53
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:16 +0000

    interrupt natural hers can train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
