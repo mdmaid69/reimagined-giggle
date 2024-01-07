64d1da66da9d3275ef0110f9470000c5bfb3dd88
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:25:07 +0000

    come dead rent extraordinary sweep

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_list(array):
-        return list(array)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
