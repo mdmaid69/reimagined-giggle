02f5a6610b09429184253dd7aae74c700f58ed4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:00 +0000

    behind cover broken expression spill

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def multiply_numbers(x, y):
+        return x * y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
