ca7ba1e6473a74b5adca4ca8e5da2ff5de2865bb
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:17:54 +0000

    environment change book condition race

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_str(array):
-        return str(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
