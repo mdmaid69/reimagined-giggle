0e04076bba30d7faf23171d5063c9f03d0384cc4
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:32:48 +0000

    light community funny ought tough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
