7f78a027c5eae9ff61775714f7dfd644fb14d42f
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:48:42 +0000

    use against eager fire present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
