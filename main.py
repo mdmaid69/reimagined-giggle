4f87bf4484cbcc203b502f316e5af52874a10319
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:04 +0000

    somewhere dream clever bringdown pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
