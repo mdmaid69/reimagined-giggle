c27342951495feaad8ff6de0dffcd207654bf379
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:38:39 +0000

    congratulations dare juice see use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
