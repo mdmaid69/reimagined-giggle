20e0b562e9583b03bae1c9be4aa32e39717ee46c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:34 +0000

    stage ignore top measure recently

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_density(mass, volume):
+        return mass / volume
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
