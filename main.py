4200fafaf908b80a960376d69d6b79e7d6bd00cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:46:54 +0000

    tall exit continue mine over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
