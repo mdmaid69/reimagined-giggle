a44df1598bd2b59cade57d2bfd4a4f97fd64da21
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:57:12 +0000

    possible Ee separate side who

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
