d6c6b93b2f36b86b14f6273a5853fc238c4fee9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:19:48 +0000

    damage future tired pretend club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(lst):
-        return min(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
