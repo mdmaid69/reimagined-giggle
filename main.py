e02b8d48171e6d64fc619e7b9bc6c0166213cd05
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:54 +0000

    splash decision fasten official even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
