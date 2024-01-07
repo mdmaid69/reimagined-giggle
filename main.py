0389367fdc7bbfcd7f0e9be7f9fd0d4e27fafbf7
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:18:04 +0000

    turnback spot understand justice collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_directory_name(path):
+        return os.path.dirname(path)
