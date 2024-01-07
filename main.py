987b233ec7fe5c3ed53a249b1f19db11d9a33530
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:25:33 +0000

    thinkover wherever offer watch include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
