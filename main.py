b841661ac78959fccdfa521636e058737901b4ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:48 +0000

    please recently speech butter still

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
