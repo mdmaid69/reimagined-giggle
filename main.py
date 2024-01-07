2afa92e119290e3c0466dcf423f7fc2bc1de4b72
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:08:09 +0000

    flat failure absolutely forgive touch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
