dae28aca9cc57adbfb278e9a0bdbe2a5e4b4498a
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:56:06 +0000

    closely region public however pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
