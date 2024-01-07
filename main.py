36f0fa78c080f2f6e0d88b107e2fa069035601ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:00:02 +0000

    juice getaway rob land though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
