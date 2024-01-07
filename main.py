0c520281ea9cbf398cff422facd89ab6f6b32a64
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:06:10 +0000

    impatient your threaten greeting pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def reverse_list(lst):
+        return lst[::-1]
+import getpass
+def get_username():
+        return getpass.getuser()
