198e3e7f13a5227e9084dab4d8f9963c4dc6c474
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:19 +0000

    tell place willing backwards aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
