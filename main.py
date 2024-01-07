c78d400d2bd59443c2bd75cb47d47791272daacd
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:09:32 +0000

    entry sense wonder church outside

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_density(mass, volume):
-        return mass / volume
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
