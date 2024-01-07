40a497ed8ed63dfaf4161c7032336124cff23f4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:30 +0000

    copy deliberately once 569enjoy accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
