1f94bf7634a270dd015114089511155b4b649ee8
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:50:56 +0000

    science dance marry dare rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_list_from_array(array):
+        return array.tolist()
