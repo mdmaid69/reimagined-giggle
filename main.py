62d239ae06d5d2e66392f7a9b9a7073d1ac70487
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:15 +0000

    write alive division dig cut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
