c55d1178160b449ed6b59702dbb350ddddf0c58c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:08:50 +0000

    interruption drive deep relationship open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
