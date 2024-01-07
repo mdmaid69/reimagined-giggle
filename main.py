56e6fae57ebfdc3c7c61f80b08925914f7aa592f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:47:38 +0000

    various successful classroom positive runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
