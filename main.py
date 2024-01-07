2b6e4257c0f9a52e6b5be41787b767882b3a30aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:10:52 +0000

    greeting backwards tell check smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
