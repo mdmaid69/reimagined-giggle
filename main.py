1619c00211cdec5c46239505c85903d275e0ab8e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:06:08 +0000

    strong various curious will takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-print(sys.version)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
