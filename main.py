3d33757ac48b275a6fb30e7ef5708ece3a9b065d
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:49:06 +0000

    may blind thing dark bar

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
