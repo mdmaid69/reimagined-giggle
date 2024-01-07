11b1ff11dc1a5825ce84ff710497417b8c7e4c61
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:13:22 +0000

    government already workon through grateful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
