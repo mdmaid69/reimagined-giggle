efa705a88650a808f6a3f9a681050f066733697f
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:54:33 +0000

    trust normal punish inside reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def insert_into_array(array, i, item):
+        array.insert(i, item)
