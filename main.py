072e868aa62ebee1a29e8968334f67ece821be1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:42 +0000

    pity journey right children makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
