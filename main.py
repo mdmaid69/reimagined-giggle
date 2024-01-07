36a4fffb7bc3fc3914d1de578cf32d7a59b505b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:59:40 +0000

    switch inside sweep cry conversation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
