f3b97532ff8901287d04fe1d0dda6ed4ca7afa85
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:52:06 +0000

    base tend will both pump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
