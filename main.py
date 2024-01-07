a98c42d46403388cc6d3bae9b3b06f5f2d12e20e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:49:59 +0000

    grow narrow confusing sentence equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
