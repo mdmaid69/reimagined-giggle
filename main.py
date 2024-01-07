a89906ccfc7cea8ec8f33def5fb70676975dcd4e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:25 +0000

    comedown waste fence provide kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
