9a5fc819b9b8e067b162a8dfd40cb6ebdd9e1c36
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:54:24 +0000

    dinner fill otherwise bringforward show

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_to_octal(n):
+        return oct(n)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Average:", sum(numbers) / len(numbers))
