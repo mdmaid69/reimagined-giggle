d84397591a27a370fdc2e2e97184d8c713877f4f
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:55 +0000

    beat put we quantity bag

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+from collections import Counter
+print(Counter("hello world"))
