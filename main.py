56d85ced7b7dfea74c5c51aa3b913a562fca3b14
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:51 +0000

    moral mistake least 565end maybe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
