a6d832257d4d351942485410bf685e93bf3c0d10
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:07 +0000

    party environment idea both duty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
