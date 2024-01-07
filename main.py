745197db4a491845cf4bc4023bc0a79b7cf68a0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:56 +0000

    exploration skill once listen right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
