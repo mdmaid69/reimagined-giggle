8d43e805f5cb4b59b292e984fc4da4d48cac7cc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:34:51 +0000

    exhibition value punishment attempt less

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
