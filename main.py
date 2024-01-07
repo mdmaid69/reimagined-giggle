babb9f75dc15f09f4258dd3cd73f99b35848f5c4
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:30:15 +0000

    best wander dark needle drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
