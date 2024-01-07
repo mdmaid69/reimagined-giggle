2f8b9d9999b1f618ff462ef1b41511ff307b1264
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:02:34 +0000

    issue material date Nn hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def subtract_numbers(x, y):
+        return x - y
