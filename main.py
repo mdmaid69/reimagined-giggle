5c34a9aef616549562db02feb7eed3d5b0752c9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:54:30 +0000

    talkback whom tight connected interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
