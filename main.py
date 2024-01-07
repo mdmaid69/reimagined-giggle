a3891a933486aaac285ada288a13b5ca6f0469eb
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:25:47 +0000

    bringup fast tape interrupt live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
