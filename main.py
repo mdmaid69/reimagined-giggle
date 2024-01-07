fe705c3ba9907f1b39dd661cb96b287e72656333
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:22:43 +0000

    inquire soak ill hardly old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
