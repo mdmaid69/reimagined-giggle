37bc8cd686bb3ed74f061385a207f19a1bd14121
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:28:41 +0000

    charge active loss you attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
