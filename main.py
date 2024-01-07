8294115980d57bfdbaa3947f3d44c3b6bfbac9c7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:46:31 +0000

    permission snow clean adjective opinion

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def find_max(lst):
-        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
