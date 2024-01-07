e00baa8c49cc5f843b4d2a661102d72dd9070ed2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:55:35 +0000

    cook national us may quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
