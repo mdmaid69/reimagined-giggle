044e3d185660dd0a4cb26f99a1cad85e2aa39eb7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:48:04 +0000

    typical fill naturally take stir

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
