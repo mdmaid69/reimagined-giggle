24f12cdce6ed75a83cf8e586cb491901f8f21c84
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:36:21 +0000

    aim begin translate recently print

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+print("Hello, world!")
