1ff65a142fa3d7f8937fc13e62535617e9fea738
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:00:02 +0000

    day start ought top alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def sort_numbers(numbers):
+        return sorted(numbers)
