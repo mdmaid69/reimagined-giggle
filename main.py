8727d0fc863ad2ec83029d65f8e637e5106b85bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:26:17 +0000

    relative concerning spin table agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
