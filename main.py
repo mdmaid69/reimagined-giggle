6ecd059ef5727508ca03a2e4ed2c423844aa2402
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:39:49 +0000

    now trust light conversation though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
