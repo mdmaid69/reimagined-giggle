cd3d1ba9dd987fb3cda64da342cf7b09c4511226
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:41 +0000

    habit godown arrive shape together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
