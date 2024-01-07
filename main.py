55e9d70f3b6afdfd1d69e10a2fd2fc5bf1e945c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:42:26 +0000

    beside position mine aim smell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_area_rectangle(l, w):
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_rectangle(l, w):
         return l * w
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
