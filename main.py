794fe38046858237e8fb9136ba28622d2d486503
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:53:49 +0000

    word smell mainly willing amount

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
