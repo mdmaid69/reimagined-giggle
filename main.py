d422488c90030d81ad7d9f16dd8d99da963b3e68
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:57 +0000

    anywhere carry centimetre giveup touch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def sort_list(lst):
-        return sorted(lst)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
