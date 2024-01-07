bffdd928b22453ad5010d46e83de9461f08dae07
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:26 +0000

    walk blind stamp content push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
