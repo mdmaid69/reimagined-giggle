a1cf7c2f0daa2d4d36eb21a9a4eb33ce83988b58
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:36:07 +0000

    lucky change damage want mass

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import datetime
-def get_today_date():
-        return datetime.date.today()
+print("Hello, world!")
+import json
+print(json.dumps({"name": "John", "age": 30}))
