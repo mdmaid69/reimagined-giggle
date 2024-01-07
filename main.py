2cf15a6d769e9f188dc0adf693fd8b59368bdaad
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:06:42 +0000

    part broken delicate rubbish attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
