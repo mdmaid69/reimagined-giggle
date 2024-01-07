3575b22a21815bde1c26511b56ee34a9e3debc60
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:19:50 +0000

    impressed lean lock chin embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
