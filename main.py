b765268444d82cf28e13889dcbb05ebf361e9684
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:35:17 +0000

    deserve confusing exercise goup exchange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_repr(array):
+        return repr(array)
 import math
-print(math.pi)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
