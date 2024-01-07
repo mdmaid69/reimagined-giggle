7b88f2b47598928fe3a7096d3fb75ecc94ae7527
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:12 +0000

    use interruption chase yes hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_pressure(force, area):
+        return force / area
