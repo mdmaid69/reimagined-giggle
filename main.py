df996359606ccb8feaeb88b6fc38778f1d81dd40
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:46:16 +0000

    payment better pretty address high

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perpetuity(payment, rate):
+        return payment / rate
