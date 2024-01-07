d54cb29ca0087aa1af03aac17086d2b3e69c598e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:09:18 +0000

    weigh him anything break change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
