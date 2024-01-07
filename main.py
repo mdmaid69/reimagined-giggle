8ec449addfb2455d12f07b31ebdd2f108657833b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:42 +0000

    by communication community back ticket

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import collections
+def create_user_list():
+        return collections.UserList()
