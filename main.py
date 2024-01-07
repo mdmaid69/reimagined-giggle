ee39c7f0c13c1e1fc3b8980aa382a5b18ea0d46d
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:19:54 +0000

    direct goback(to) promise account getback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
