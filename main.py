52f907acef53a5fd831ad1901007de289b9f7a58
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:38:43 +0000

    field discovery bear building kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
