9137c01e3be02a0feb9b2b69ce66a226f5a10a34
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:44:45 +0000

    sister traffic treat drive typical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
+  def convert_to_binary(n):
+        return bin(n)
