1892d35ad239bc182ca1cbfa7393af1c056b7736
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:38:30 +0000

    right rest language Qq path

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
