94d96546bfdc3ba537808a4e5631dfc40dd6d3ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:06:39 +0000

    prefer positive Oo before production

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def convert_to_hex(n):
+        return hex(n)
