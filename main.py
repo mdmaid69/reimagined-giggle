083a061a3969d287a9f731b3608eb86f54a3f672
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:36:11 +0000

    road people accidentally brown hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_power(work, time):
+        return work / time
