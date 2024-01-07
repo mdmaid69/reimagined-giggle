567c4633046a4bd86d7a8b6df98b17fd2dc927a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:17:29 +0000

    phone question obvious afford natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
