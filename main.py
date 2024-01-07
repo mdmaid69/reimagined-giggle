b79b78714b86d6ea364ca6e33ff5bd6d78cd36f9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:42:38 +0000

    war top neither speak single

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sys
+def print_python_version():
+        return sys.version
