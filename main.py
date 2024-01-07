f77960624c1e48b302cd09c34beed2a3e97ae5fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:01:31 +0000

    describe demonstrate order effort switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_acceleration(speed, time):
+        return speed / time
