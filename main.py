2ec9660e74c7abb074154f69a121184eeeb824af
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:01:20 +0000

    workon runaway repair form breakinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_item_count(array, item):
+        return array.count(item)
