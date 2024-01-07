0d4a1362b213f594b7901ad1cb62fe9233de57cd
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:37:17 +0000

    pity breakdown right declare pain

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print(sum(range(10)))
