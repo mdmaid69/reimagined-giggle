57577613ac2f06c0e50aafa91eb74d25b58555f0
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:00:06 +0000

    gooff actual warning collect repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
