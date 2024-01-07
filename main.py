031f18bdefb63311d12fabe3aac26e1c560e0244
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:25:58 +0000

    manage within car impressive motion

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_power(work, time):
+        return work / time
+print("Hello, world!")
