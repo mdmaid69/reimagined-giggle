d2e9979ca19fdf748163f4e07dc80403e708354b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:02 +0000

    emergency base then hers Oo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
