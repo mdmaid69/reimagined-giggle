20cfc5bc1a22682af4abb69b6c98c80cc125c0df
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:42:38 +0000

    sort prevent Cc back telephone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
