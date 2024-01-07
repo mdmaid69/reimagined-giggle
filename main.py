4daeb61b9c543729a695b4b079bfe250dbd514f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:49:52 +0000

    walk smoke tough professional used

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
