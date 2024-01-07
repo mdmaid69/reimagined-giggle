5d688d1c4a502574e0186d14ab3fef71e5c247d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:15:16 +0000

    though jump fault anybody contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def delete_directory(dir_name):
         os.rmdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
