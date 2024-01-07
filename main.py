05c41d1ed9dc3de5d2ced34a779995bf63338fd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:24:04 +0000

    create sticky wide pretty clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
