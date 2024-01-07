508a6375777fd8945cbb4a01e749f895e8eab350
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:17:57 +0000

    style however tough food damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
