e65a55d37689e74d8096eff92f11b318eb481fed
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:05 +0000

    innocent rush regard keen near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_area_rectangle(l, w):
+        return l * w
