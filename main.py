143b39f0741a4ca83601f21823ae67c0897ca118
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:57:29 +0000

    care justice ear score food

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
