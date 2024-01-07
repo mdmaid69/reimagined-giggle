93ba2c713a1c2ee3ab0436e86543073d525fe392
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:26:26 +0000

    backwards father close criticize statement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
