7f637e73cb87717e7b4ef2e02b82a8f8ebe65360
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:57:45 +0000

    limit error Dd cold where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
