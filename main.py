219b176f818cf71a5b38ba9f36f1dbd8faa439d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:51:28 +0000

    valuable depend accidental pay brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def multiply_numbers(x, y):
+        return x * y
