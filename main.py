5e4e488fee866719eabbb56a3bbdddc2fd15b350
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:24:37 +0000

    transport suckme woman born duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def sort_numbers(numbers):
+        return sorted(numbers)
