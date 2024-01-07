1dd2785810ce5580764f7aab633f05b05069d345
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:16 +0000

    tend pride lie reserve calloff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
