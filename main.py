894d2a4f3166d817a952365c3ac2c12826cfaca6
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:19:52 +0000

    section narrow conduct bin degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
