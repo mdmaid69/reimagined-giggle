7c225b3475a99abf834aded1a435a1d50d7add46
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:40:07 +0000

    so unfair cook waste mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+print(os.getcwd())
