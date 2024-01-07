3a97941a1f0e30231267f094c198fb50fec7bedf
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:00 +0000

    border want part round act

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
