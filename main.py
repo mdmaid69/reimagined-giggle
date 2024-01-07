6b25f840fdc6790edca32a99bc94dcb928c942e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:47:19 +0000

    ready mark part evening sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import platform
+def get_python_version():
+        return platform.python_version()
+import time
+print(time.time())
