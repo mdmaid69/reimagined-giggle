ea9c238f301e3660e3fcc5c76a9d739fe6565b20
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:30:57 +0000

    use guess how glass business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
