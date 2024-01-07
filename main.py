ca6768f519b384b59afc89fe07a833ba3c1d8519
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:27:13 +0000

    bill or equal undo regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
