641d31357a641d2ec7fd07e5c134c53d3f1c703f
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:58 +0000

    if shoot know favourite level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
