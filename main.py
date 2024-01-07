866e0e07aa617a32f5dcaca2e4b5a801ce2842fe
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:31:28 +0000

    door one believe stretch violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
