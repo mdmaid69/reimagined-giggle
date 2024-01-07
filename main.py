d8481175310142de48cdd810aa011e7b46bd6629
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:39:34 +0000

    the connected blind gooff rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import time
+print(time.time())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
