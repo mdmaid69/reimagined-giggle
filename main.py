adfaf39df1e93d6020df8f85e86a90a69df0f069
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:28:33 +0000

    bit male dress study safe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import tensorflow as tf
+print(tf.__version__)
