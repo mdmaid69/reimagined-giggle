c1baecb0f5d56031c448fdb41a914f504f16ccee
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:26:40 +0000

    club control unable object announce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def cube_number(x):
-        return x**3
+def find_max(numbers):
+        return max(numbers)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
