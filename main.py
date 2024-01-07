207f0b6c1ef05826affbf9d9079487fa638f5b82
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:01:26 +0000

    left 567enemy stiff count calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
