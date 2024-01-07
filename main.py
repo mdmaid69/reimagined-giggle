70429a6076bc31596dbbf82fe5a34b3a74df839b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:35:02 +0000

    total good surprise shade giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
