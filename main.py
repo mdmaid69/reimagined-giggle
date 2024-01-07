51d78120371da05d9c99733b2785136a4a1f9b42
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:35:32 +0000

    comfort run anywayanyhow together profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
