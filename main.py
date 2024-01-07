18b9717d41f30e897d1d7d96b7d19cc3adf5c1bc
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:11:30 +0000

    remove proud polite fix warm

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
