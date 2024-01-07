fc90dae754a5a271d1fc43fe2defd9144629fcf0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:50:20 +0000

    create later takeout guard reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
