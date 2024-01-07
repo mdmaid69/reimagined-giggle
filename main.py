3b4791a7cb1a616e66bc5140ad8586e7e259df2b
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:40:54 +0000

    turnup tidy exam small various

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
