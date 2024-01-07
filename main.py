b11c0204ee9c324cd4e8e388bf25a7e888bc8a1a
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:36:27 +0000

    class public flavour settle effort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
