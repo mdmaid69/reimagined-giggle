b503c094f685f4b0b7f5cf9c4aea27dc4ce37a35
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:12 +0000

    best giveaway stir call excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
