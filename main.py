66e546bd709895e6d33e5ef566c97653160d4646
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:32:16 +0000

    basis match neat brain announce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+print(os.getcwd())
