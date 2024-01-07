2eee2bcfcc6901b40637ed592cef67724f143ab7
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:10:46 +0000

    equipment mad visit sharp at

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
