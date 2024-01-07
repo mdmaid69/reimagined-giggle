be53580aae5ddda040afd5ac80ec9fd36e720dc2
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:56 +0000

    571enough suddenly argue voice fashion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
