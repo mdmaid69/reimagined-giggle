2fc3b2b7a90158031b3949f8274efb7ebe2be6fc
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:15:04 +0000

    someone pay note set meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
