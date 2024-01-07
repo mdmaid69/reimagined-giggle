2e84c7836eefdda3ce162d2249328aba83017c51
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:12:48 +0000

    trade Hh physical bad your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-def add_numbers(x, y):
-        return x + y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
