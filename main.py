e5d93863403ae13073e89c803fe05afb5f20a769
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:46:19 +0000

    getoff along save path reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
