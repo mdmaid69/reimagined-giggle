a090a351680de27803676bbb50d701cd041582d2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:42:20 +0000

    creature laugh talk breakthrough message

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
