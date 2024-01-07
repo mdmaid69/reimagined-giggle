ea41803617d7b1679d513c5079ba84eb46bb0459
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:20:01 +0000

    once sleep centre weak breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
