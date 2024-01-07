faf507027bdfa397a1d016712ce678ceae979b33
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:20:58 +0000

    Tt reduce through cook play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
