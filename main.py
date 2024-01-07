db0d14d756358c7e5efc4f855d268dd035dd1eb2
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:02:52 +0000

    discussion keep point previous mixture

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_directory_name(path):
+        return os.path.dirname(path)
