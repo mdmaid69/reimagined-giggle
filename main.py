b5d7f660b9fbb838bd7f782884988e4d15a6a15e
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:46 +0000

    that prove on characteristic carry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_typecode(array):
-        return array.typecode
+print([x**2 for x in range(10)])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
