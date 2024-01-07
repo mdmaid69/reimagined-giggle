9ca485d149bd2150afe5f444398952fd4cc749b3
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:32:36 +0000

    nothing allow way claim design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
