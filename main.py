9b5ec4d74f20565fac4d98b5441b20abaa0980ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:37:33 +0000

    mainly zero heavy equal community

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
