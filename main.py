0dd6bbbc9e3c46c3a4aecfb047c3e7ba67d002f6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:47 +0000

    easy anxious gentle feed mess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
