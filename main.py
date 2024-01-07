c3496f26c8fb51a011ff95f475a78e3eff23fef9
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:46:48 +0000

    pretty read copy pronunciation car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
