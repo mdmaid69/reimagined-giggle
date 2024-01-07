dc549478eb4a881ca168e7e35804549ff58fcc87
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:24:57 +0000

    behind snow anxious broad mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
