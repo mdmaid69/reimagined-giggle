f1065202a8079bd97feeff0de686b89e6e89ec58
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:50:15 +0000

    gentle soon help ring to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
