5dd78e7d94c4c06f606fb1ddc201210a56dff30c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:27:18 +0000

    message strike them shall scrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+print(math.pi)
