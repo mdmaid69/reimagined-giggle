69795e5c4442408ef1c14ea48334ae77aaa3f90e
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:11:31 +0000

    through fast unlucky really thing

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
