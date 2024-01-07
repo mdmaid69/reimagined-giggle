8f0ce50da0cc2a346e78b5d618eaf5f8af523d61
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:15:41 +0000

    cut hurry nervous extremely sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
