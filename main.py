4188e9c19d8ef41e60c438f7aeb68501ca232bec
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:42:28 +0000

    hello weak reject neither exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
