efd79c667240985171eb0ce0ee47ee899c34994f
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:04:08 +0000

    patient key tough evidence unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
