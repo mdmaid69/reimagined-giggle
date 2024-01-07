8c1e74b199ac49745a1773de888b4395d27f41e2
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:58:42 +0000

    star demand air pretend even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def reverse_string(s):
+        return s[::-1]
