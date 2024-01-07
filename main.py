9c79b7f96c38b00bf9a02fee2f94692308a7fb8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:25:58 +0000

    deliver illness pleasant breathe front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
