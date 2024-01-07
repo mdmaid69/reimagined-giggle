fea54cad0b6d673b1a5dae7e8a88ce96dc106242
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:48 +0000

    minority beside question course whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
