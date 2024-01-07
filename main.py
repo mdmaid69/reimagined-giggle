c196ee932f12d73ad15b809df1995a4a71ee06b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:27:56 +0000

    complaint 568energy argument carry whether

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
