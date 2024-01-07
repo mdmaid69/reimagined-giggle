9d02130252a1caa0f8d849e4d6b54b010cf92ffa
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:46:24 +0000

    answer burst thumb even improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
