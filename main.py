d4dbc739a9c074b71948888c66df0c9c37b81ad9
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:53:54 +0000

    nerve tight government artificial target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
