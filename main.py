1749e7d8a44e8754d2b31d7fe7abdb14f8ae46c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:43 +0000

    your thin how peaceful ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
