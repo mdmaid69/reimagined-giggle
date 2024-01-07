92ee4868989a1881a4fe21b52be130ef14aad53f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:21:27 +0000

    fast suffer total slip low

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
