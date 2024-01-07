10f1fdf7924cbc46eef5b46b8665babb304927ab
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:53 +0000

    second happen judge entry crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
