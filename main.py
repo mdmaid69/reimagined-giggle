7316e18795606edfae9553b90740522faf989be3
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:53:03 +0000

    handle show deal leg tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
