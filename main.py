f389c7d8ff429d559d87f31896b5433b83214ff7
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:22:09 +0000

    shrink without disappear many account

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
