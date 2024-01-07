81d285f0dbabc43aa692db7beb87e07eb55b19c6
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:04:43 +0000

    recovery luck love night deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_python_version():
+        return platform.python_version()
