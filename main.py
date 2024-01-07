1e086497bea8ea84592494ed612d2988598ebdf5
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:46:33 +0000

    law voice pity unhappy story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
