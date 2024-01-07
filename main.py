8256d0739536e63c7d36ef40c3117c1ae6985eae
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:35:02 +0000

    soft bring emphasize talkinto hair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
