5a9dc4a63a9c98a9a03a8e5b505f89f13d18591d
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:39:40 +0000

    quarter tone journey too market

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
