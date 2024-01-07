c99575d44b15ea3d9231a4cb3086aa3c5aa22d09
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:03:43 +0000

    freedom female abroad bring physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def reverse_list(lst):
-        return lst[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
