4f31d8ba686bf483d13eac992719296f56b65d1d
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:33:31 +0000

    right thankyou fair unkind house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
