5e04ba45fa9ec8547760443be5daa7a3867c3163
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:37:31 +0000

    offence estimate neck have slowly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+print([x**2 for x in range(10)])
