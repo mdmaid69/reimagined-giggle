e76d0dc63522290fd6c3182074d70a6282200023
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:59:23 +0000

    attract any pool obtain key

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def count_elements(lst):
-        return len(lst)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
