9b66e3b0a798d279faac1ea217064b307f8c0a92
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:02:41 +0000

    kind embarrassed hide defend left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
