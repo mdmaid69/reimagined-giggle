fba0cfe5fd34683e54115a1ccbca7d7fc3377907
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:58:17 +0000

    long vote key watch lift

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
