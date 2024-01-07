59e170ed379a9a6b0564fb64a4eba4ba3a3513c0
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:46:36 +0000

    cinema spray you sky real

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-for i in range(5):
-        print(i)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
