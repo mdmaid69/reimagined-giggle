a3a81e2833323102edbd2968c719e15f5802e3d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:50:08 +0000

    fairly afternoon forgive task frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
