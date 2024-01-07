e6b49b70b49297ec266e5e1b9230148a0114837e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:51:21 +0000

    knowledge experience either relax visit

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_speed(distance, time):
+        return distance / time
