3dd062be1772878d86661c3a0289ca1503152184
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:42 +0000

    duty 573entertainment exam workout never

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_bool(array):
+        return bool(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
