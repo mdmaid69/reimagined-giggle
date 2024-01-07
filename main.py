2a858d10e05982e928a8906ba2bf70f12cd4b6a8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:51:11 +0000

    university creature subject what healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
