ea3d7a6d77095aaf9435f51391845935b969868d
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:27:14 +0000

    who must distance stiff test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import socket
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+{body}")
+import os
+print(os.getcwd())
