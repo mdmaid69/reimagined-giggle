8c9497db0312e723a1a3a35483308688474e8dc5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:41:17 +0000

    pin home swimming contain appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
