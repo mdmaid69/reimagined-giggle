696084d198df7bddf217d049ea240d4631ca5069
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:45 +0000

    future damp rest speech persuade

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-def remove_duplicates(lst):
-        return list(set(lst))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_characters(sentence):
+        return len(sentence)
