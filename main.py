e87a4a75c48868ee752f964d890b0b65021eb9b6
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:15 +0000

    deliver flat succeed mixture height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
