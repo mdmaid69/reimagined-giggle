ca0b922ed041ce92e60572106b396fa647e0fbcf
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:42 +0000

    certain interview children throw flood

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
