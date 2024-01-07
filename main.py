4929c25e8e5cb4e05f64277e699de49cff4de1e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:47:28 +0000

    possibly at include gentle her

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_odd(n):
+        return n % 2 != 0
