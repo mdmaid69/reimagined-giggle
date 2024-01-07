eb16d3cbaa4801c77ce3db2728644b11b12c0668
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:09:52 +0000

    situation anyone beginning deep give

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
