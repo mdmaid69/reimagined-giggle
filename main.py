74a5db4699b94b8944058cb4df8acb53758dbdef
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:11:21 +0000

    hurry pass giveaway heavy slightly

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
