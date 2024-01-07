9775a39e6b3f04a3681bff82ba42ac91e0fc75ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:49:08 +0000

    shock normal scream treat include

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
