2255213adf718702ae286a35648e2ff394bfb303
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:46:13 +0000

    foreign present takeoff dull great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
