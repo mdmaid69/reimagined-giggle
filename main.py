99e45f1cef3056d19944fb16328e27e7e2e06968
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:12:22 +0000

    bridge ticket gas quick car

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_array_as_str(array):
-        return str(array)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
