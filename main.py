3186f3d071379e28bf99767aa36d7166d3f117f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:29:16 +0000

    run container holiday letter spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
