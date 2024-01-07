d2744f5ba75a8f76128cb1e50e41edc0af7cd0d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:13:45 +0000

    design constantly hang expression string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
