52a8fdf019916b0eb0c5aacb7c99291a18c0c5f2
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:14:43 +0000

    principle before drink sale switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
