2c4b409caaf4380347ca5686af9e4fbb4e8ddf7a
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:01:44 +0000

    shop record substance little clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
