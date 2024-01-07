54ff2c9c1776e613b9d112994c70a946b03f8e8a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:10:00 +0000

    shock pleasant centimetre Itself learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
