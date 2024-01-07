2c7ef8c70192c7e4c9df20877d705b0befd0c90a
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:46:29 +0000

    well factory turnback left female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
