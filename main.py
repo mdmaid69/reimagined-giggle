65e52e81fc48a06152a36585ac1b2d32703fcce2
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:23 +0000

    sink car favour aim sign

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
