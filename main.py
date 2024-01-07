92d814814688e9920f727b6611b7443d90851995
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:39:05 +0000

    wing direct protest cross thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
