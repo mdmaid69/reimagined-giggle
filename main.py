8f32db9e54bf55600c09d10289ea8be66ed9ce0a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:01:13 +0000

    frequent since crowd unpleasant unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
