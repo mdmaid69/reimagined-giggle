5a8a4f60512d8b70871ead1e2fd14fe9c63b6e31
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:37:23 +0000

    bus speed less breakfast behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
