db8428088c1304a36068331c4fcb922e555a67f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:59:09 +0000

    drive pot thank land label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def append_to_array(array, item):
+        array.append(item)
