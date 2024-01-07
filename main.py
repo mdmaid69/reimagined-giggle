6ff7eae223947a578cdab09bbf677fdb08db9abc
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:14:00 +0000

    height yourself slowly freeze hide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
