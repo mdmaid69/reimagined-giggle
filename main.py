11dcd8de67fad0620148ba607094dd757baea643
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:52:57 +0000

    destroy tap bury stage their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
