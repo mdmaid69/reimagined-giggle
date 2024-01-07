71dff2567e859f174ff38d53d32b8873b5c1ded7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:22:59 +0000

    comeover scale sound permission me

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_work(force, distance):
+        return force * distance
