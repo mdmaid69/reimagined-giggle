f275dc8903942ebd40548697e56c9c1abd585630
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:23:20 +0000

    final talkoutof wheel increase turnaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
