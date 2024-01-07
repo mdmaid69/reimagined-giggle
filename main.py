aeec25ef84f97a51ed677d27ace0eea3af278649
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:29:42 +0000

    Ss steer ring risk clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
