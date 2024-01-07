6ba71a6473093c04c394dc1ade2bb180c9098ca9
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:27:48 +0000

    flow careless pleasure apparently plenty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
