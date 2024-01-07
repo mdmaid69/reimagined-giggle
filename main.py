177826a8809278e165087eaf07b1525edc5e2a5e
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:33:48 +0000

    confidence unless building custom successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_list_from_array(array):
+        return array.tolist()
