aa7d995e34ffa7ca80c7a26001318b511823a13e
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:31:43 +0000

    drink flat acceptable poison gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
