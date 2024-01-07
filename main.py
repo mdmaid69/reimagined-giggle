51907e1bddf2bc5a5c47074900971b68b0bf3c86
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:39:30 +0000

    suggestion use cheat raw occasional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sys
-def exit_program():
-        sys.exit()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
