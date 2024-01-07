0de280187d7eb46269740ba9fd94bd04a8860a38
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:14:43 +0000

    bag hide let trip polite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
