b8d6d7d631ed6a90c2169d2e4771c228078a14bd
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:57:17 +0000

    meeting mouth class complain put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
