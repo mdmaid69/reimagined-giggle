8892a6a64a3dc4ce1d808d2698ad3f4f62ddb6df
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:40:39 +0000

    strange giveup national entrance attitude

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def reverse_list(lst):
+        return lst[::-1]
