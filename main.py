d891d7a765fedfff4925f7c8f9b296633967dd27
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:11:37 +0000

    capital wait phone move dinner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_work(force, distance):
+        return force * distance
+  def convert_to_binary(n):
+        return bin(n)
