a66c43386ba5625a433d1135792bf37aaf97f44f
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:46:05 +0000

    offence light uponon thirsty fashion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
