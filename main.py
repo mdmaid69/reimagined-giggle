ee67e50071b48d1ef4e61b3879c5d353146ce151
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:54:25 +0000

    when estimate instant lookafter anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
