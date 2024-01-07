91881e9bfe2d34bc8a75ab1af893c4ea589d9203
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:54:35 +0000

    agree correct equipment excuse stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
