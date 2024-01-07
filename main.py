5d2d29b43eae76838c377ba7745f47de9f5db3e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:45:33 +0000

    first always skirt view convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def convert_to_hex(n):
-        return hex(n)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
