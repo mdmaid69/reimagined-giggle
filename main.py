14899cd95b95bc53b636a8529914a13ae9666034
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:54 +0000

    our zero realize ever part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def reverse_array(array):
-        array.reverse()
+import sys
+def exit_program():
+        sys.exit()
+import collections
+def create_priority_queue():
+        return collections.deque()
