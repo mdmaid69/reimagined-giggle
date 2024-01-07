7dd8ce3a3403e250b46e9fecdb8b1f30ced03ef5
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:18:36 +0000

    eye regular each pour reply

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
