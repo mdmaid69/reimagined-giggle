55e5eec1b15a9689f97eb015fe90aabe7335d593
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:21:33 +0000

    use 564encourage sorry jump smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
