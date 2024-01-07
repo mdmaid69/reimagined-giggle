d0bc75e72a5bae7623b6a414a67f962d0bcba54b
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:08:44 +0000

    sick male steam feel taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
