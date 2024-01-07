1d4975ec7721aa9b9a7777dc4a943f03212ee2fb
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:15:53 +0000

    detail Zz book rush slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
