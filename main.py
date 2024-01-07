c231c59cfde62a728056273ddace5f98d1ed5503
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:45:10 +0000

    herself back charge we post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
