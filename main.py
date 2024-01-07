c7d8f1b8af9bb8f986023159c5c287631b9e8422
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:03:36 +0000

    face runaway sleep I left

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
