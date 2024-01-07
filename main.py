1edb71e8bc7cca663525cf894310ff6eb682c122
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:31:31 +0000

    tragedy pool violence poor shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
