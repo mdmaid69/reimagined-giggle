1f3eed2aa492967f2616120b4264031ab210ab8c
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:18:24 +0000

    underneath leave however crash creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
