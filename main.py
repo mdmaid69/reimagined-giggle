2a39702083967a06ce972ed68d0011170b5527ec
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:45:03 +0000

    forget area willing correct extra

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def square_number(x):
-        return x**2
+def insert_into_array(array, i, item):
+        array.insert(i, item)
