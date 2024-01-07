d770a3065f90113ffdf60dc351b24733f972693b
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:33:07 +0000

    eat punishment wide passenger attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
