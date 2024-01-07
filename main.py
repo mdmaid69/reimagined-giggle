1ad153e7f6e47381c946fc902da24bbf98231f4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:03:46 +0000

    television bank rather better crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
