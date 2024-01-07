6a751eebfa2ef0a200b7bcad435d262cac765651
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:07:37 +0000

    hit discuss release wish book

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+print("Hello, world!")
