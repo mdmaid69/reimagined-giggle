6fd1b676ec4092abdaaa4e55c1ce7571139bbdae
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:20:53 +0000

    escape power tall fast will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
