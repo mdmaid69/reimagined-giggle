437b47dbe757d88f4d9689f70d0139d5d5f80dd5
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:22:20 +0000

    whom information much boring try

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
