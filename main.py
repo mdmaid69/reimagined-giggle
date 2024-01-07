bdb3577d2841c20684f1f32394cb1c1d365479f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:52:33 +0000

    edge breakdown print under explanation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
