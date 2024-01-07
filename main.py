aeb3d4daad30bdf2f78dc7a170819c7ebe9e620e
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:45:33 +0000

    sight ought can flow opposite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
