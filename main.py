bec32cf473968efd28b3368aaf76e032d0e09e1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:13 +0000

    over doctor tour just nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
