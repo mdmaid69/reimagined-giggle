046e399828f3f494b3c284085e74ccdecacf62c1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:18:57 +0000

    fine can march vanish compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
