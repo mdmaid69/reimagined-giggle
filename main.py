99d6225918e995acba58583ebf53d70c2e23c699
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:36:18 +0000

    for duty stay nurse memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
