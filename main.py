b1f51a5cc6423fad166862531e022ddaaa3793a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:14:47 +0000

    niggaacademy furniture shout advice ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def add_numbers(x, y):
+        return x + y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
