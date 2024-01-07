6d63dc0cd3992d686abdb0148fcd9a1677e30372
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:03:13 +0000

    pinch obtain actual could wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
