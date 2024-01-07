e448cb3a426f68074a43a40df1effd056073d74b
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:56:01 +0000

    birthday turnback carry it prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+text = "Hello, world!"
+print("Words:", len(text.split()))
