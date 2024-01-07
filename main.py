a82bb3b234c33e9904ad8ecda2ad1e80b2405dab
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:58 +0000

    his eat unusual call activity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
