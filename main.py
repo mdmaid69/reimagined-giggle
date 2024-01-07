1bb7d089ca4edc3d5a41d28942eee7a67b1e8be0
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:36:07 +0000

    protest Qq stir dare lunch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
