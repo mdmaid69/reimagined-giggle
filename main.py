16749eabc111ec2e4121c6d42f82eb24c1e97f71
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:15:41 +0000

    spill succeed male material flat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
