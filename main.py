542293ba6acb3f42e2052272d9fe053e04c1553e
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:17:42 +0000

    goback(to) bank petrol miss suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+def change_working_directory(path):
+        os.chdir(path)
