cee84679d5eddfa74a22d4960b8e84a69416f84f
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:02:19 +0000

    correct shout favourite takeoff truth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
