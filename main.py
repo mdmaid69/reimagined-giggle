9c67115cac4672790a5393454d2886b067d8fe1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:38:28 +0000

    quit copy hand lead wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def multiply_numbers(x, y):
-        return x * y
+def find_max(numbers):
+        return max(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
