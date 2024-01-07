b220aa59444cc4a775686e35f826c269088eb47b
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:56:29 +0000

    dance closed friend disappointed yesterday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
