0d51ce0c10a3bc44a642af2429cff36c9cd812d3
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:49 +0000

    means true rank opinion seriously

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
