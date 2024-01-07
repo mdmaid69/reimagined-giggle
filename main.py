92a2eb828202dfa074be72e3428e62619d20c047
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:12:23 +0000

    translate ring closely holdup finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
