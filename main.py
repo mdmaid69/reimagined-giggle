8c2485c11eb815d83780f48d49bb8aa27194ca6e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:29:49 +0000

    better normal satisfactory rare hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
