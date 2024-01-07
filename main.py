186f806e961f780517b5eae7c7413831c00f12aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:31:12 +0000

    active injury constantly floor Bb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
