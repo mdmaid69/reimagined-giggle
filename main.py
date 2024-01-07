7a5e75ca0a30834eef3cb35e2cf71042782120d0
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:11:29 +0000

    load uncertain rescue careful shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
