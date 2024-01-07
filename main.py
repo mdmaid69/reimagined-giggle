3bd62e9d7ccd8a6eee230419e23a27a251f53ef3
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:26:49 +0000

    each unusual disappear care free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_area_rectangle(l, w):
-        return l * w
+def subtract_numbers(x, y):
+        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
