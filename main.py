29f550d0422e7d8f6923f3e9cf82135d8d1a5e35
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:11:19 +0000

    shout dream hear interview trial

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
