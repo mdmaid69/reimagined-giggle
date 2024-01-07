05cc42fc6d6bff5f745a9ead46eec43c06207516
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:47:50 +0000

    something drink wrong noisy while

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
