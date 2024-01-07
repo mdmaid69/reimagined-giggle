778cbfa0793c067b68a60857fa96f4ddbfca51f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:18:11 +0000

    geton team dream culture past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
