c9c1c9e8c9f10f5a8a33af5f5f893ee9d1f6fcac
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:57:33 +0000

    criticize hear violent reason along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
