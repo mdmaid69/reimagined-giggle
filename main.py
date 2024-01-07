f24dae4b02f57aca483e671c0cc79fd3352f749d
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:35:35 +0000

    somehow interest bye stand tall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
