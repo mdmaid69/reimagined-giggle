ab68ad59cc5470618b4fcd43d9825b7be9cdd22b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:16:07 +0000

    connect skill grade plate getaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
