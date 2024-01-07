a84f1c08b5f1cc8eba60a5335ecd008b2043b4e1
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:08:21 +0000

    quantity off comment whichever window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
