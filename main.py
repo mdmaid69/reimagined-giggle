d8a258518f9ba847a01ba312b3b3167577a5f688
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:47:08 +0000

    badly level admit precise such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
