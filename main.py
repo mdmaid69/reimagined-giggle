8c2d8a25801fb26d8417614cf67cbc2d03f428d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:08:53 +0000

    uncomfortable ilovedicks century do possession

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
