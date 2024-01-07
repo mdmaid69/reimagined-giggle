2488bcc5e8dcf3062d4225ca41bb49d589b3e5b0
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:06:21 +0000

    luck sit empty waste deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
