dc5a9b105e5aed8c02f8111aed76fe83e5bb224d
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:31:13 +0000

    used ours trick pot calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
