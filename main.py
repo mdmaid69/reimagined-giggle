375361f8baa5366ae5a1732d2758c7c4f76c102d
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:07:57 +0000

    deserve cruel cost down judge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
