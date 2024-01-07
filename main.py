5d9f59e1ede25a24f4e4a1491f1eeb7e33b702ba
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:45:43 +0000

    eye belief mysterious equipment match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
