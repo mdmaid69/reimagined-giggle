e5c1d6c6e1188183b4dac8e81839fb618a6ee128
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:17:10 +0000

    grow healthy back mail key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
