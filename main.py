0c5ac5712a37d5107764298fc17e11cebc22b7d7
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:52:39 +0000

    fun word suckme cause want

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
