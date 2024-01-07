7f09806ae66a8c4e4079ad055937558f6ed1760a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:31:22 +0000

    step recover brush young arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
