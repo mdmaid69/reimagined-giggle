7b4345f1ff3577c497567630b1341db39c6878d8
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:17:54 +0000

    between letter pour fall work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def append_to_array(array, item):
+        array.append(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
