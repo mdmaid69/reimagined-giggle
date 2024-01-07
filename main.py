033a6ec0f4c71016b0b5f526e7a70ebf669e973d
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:05:10 +0000

    must equal through live better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
