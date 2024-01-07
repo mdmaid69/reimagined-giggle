db8bf723c7d08a09074706ebe377cc374ce50a50
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:09:25 +0000

    turn neither takeaway instead imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
