08aa43d7d681df92155411e0dbc87d089c6b0da6
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:28:18 +0000

    operate pool quick sleep discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
