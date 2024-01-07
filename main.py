f65521a6b749cbc3fb35fc0b34c3f9738e58f109
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:19 +0000

    admit turnover top pain takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
