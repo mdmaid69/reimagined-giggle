91ac92e85a62322154597bf14c3764d6ff6d3fee
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:10:47 +0000

    really height snow not circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
