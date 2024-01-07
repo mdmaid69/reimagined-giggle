a919c8a5c696ab80330ad21c818d693106b405e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:34:03 +0000

    active convince suck travel 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
