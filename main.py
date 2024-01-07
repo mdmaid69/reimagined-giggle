31a16676ad1839239cf204fb295b107e04814221
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:51:02 +0000

    gooff suffer bring mine wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
