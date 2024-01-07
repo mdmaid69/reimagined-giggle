eec30c41a6e3df8282d40deeabb42a5c77074b9c
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:06:50 +0000

    rely that suit offend total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
