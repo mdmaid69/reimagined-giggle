b6dddc497be5373f45166374465784d68e3eebda
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:56:44 +0000

    holdon human dry survive opinion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
