61239e2490077e0a2d99fa819880d18a5cbe9e16
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:25:35 +0000

    string comein charge not dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
