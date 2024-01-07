d6fa41f175d644e8eade7a12217c8a734d994762
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:27:48 +0000

    difference before pay fairly centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
