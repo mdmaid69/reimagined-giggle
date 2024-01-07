7e3037fb1427924d4fd29aaf4059adadb8d78821
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:01 +0000

    sleep too recently takeback still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
