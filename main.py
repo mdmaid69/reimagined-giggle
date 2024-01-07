bbe9a16ae5306f9ee6dd2c996728e1f8a649fe9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:04 +0000

    special upstairs generally pool considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_tangent(x):
+        return math.tan(x)
+def find_max(lst):
+        return max(lst)
