9ee636bb862aacd6803c92100899fb0866aa9547
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:19:07 +0000

    term shut split shape valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
