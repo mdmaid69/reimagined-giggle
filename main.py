ed7778285fedd5e499433faf4ab2722836a26e15
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:23:58 +0000

    distance front town anybody thorough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
