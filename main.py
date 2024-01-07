4c5a292029e6eb28511212b3e0731e427b577dd4
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:04:44 +0000

    cut secret clearly view board

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_set(array):
+        return set(array)
