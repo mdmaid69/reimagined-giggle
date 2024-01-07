3b0ae35273995d8de0b556349f682fa8f6e25dce
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:54:58 +0000

    new accident book upstairs shrink

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
