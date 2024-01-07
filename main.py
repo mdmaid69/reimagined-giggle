4149fa7c9c8dddb32d1708a2d4b10ba93062fef4
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:48:46 +0000

    reject mean represent cook expect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_speed(distance, time):
+        return distance / time
