9bb2a2d0d218701b24e61997e991cfa539c0f779
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:52:00 +0000

    fierce improvement content famous attempt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
