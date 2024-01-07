4d0d256bcacc50b80506b4bbac7f6cf3141e4791
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:52 +0000

    say meeting comein roll Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
