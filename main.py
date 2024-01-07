d7856bd5279619c1de80ae89e66c5e42f054ccb5
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:57:33 +0000

    charge closely might port vanish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
