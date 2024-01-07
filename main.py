c01dc145e872a92df74d6acc814bb960a9874c15
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:07:32 +0000

    trade tip brief run picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def square_number(x):
-        return x**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        print(sys.version)
