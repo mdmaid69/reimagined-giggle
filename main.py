78f7a359b4951665c4faed8fb36ca7a917c5bdf1
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:54:58 +0000

    mouth thanks file frequent closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
