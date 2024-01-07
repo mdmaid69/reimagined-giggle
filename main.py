de791ba27aa92420c29eb6d3984e0c93d1f7df23
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:07:15 +0000

    favourite Gg sense comein store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
