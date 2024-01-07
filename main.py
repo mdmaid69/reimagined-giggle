c888f50c5efddb3e8c52e9a0737cd0294e7ebd4c
mdmaid69 <robot@alexsyw.me>
2024-01-07 06:54:32 +0000

    permission objection mind unpleasant include

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
