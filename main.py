2d248054f2043d53e7047f952cd4dbfdbf3faa5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:48 +0000

    send autumn 179bite behind brain

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
