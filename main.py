d6db8f93690e9429dab8c72d3f7965de531a847b
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:14 +0000

    knock whichever often that distance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
