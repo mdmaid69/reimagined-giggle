15e449a542dc37f2bdb4b3286e83c8d521bedbb6
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:52:08 +0000

    behave before wise bringforward search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_binary(n):
+        return bin(n)
