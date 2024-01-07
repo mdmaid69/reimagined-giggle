4d448a79e066f8e86307a04c1d9eb6ee88e2f6e9
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:58:05 +0000

    duty past position environment contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
