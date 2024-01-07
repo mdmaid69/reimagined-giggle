87036abc42773df5239520e945184b1d7e7c35a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:28:19 +0000

    clear better opponent over accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area(radius):
+        return 3.14 * radius * radius
