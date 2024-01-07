55a903c098ee8b9d71946ef78f99c264259b82d5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:06:32 +0000

    trousers disappointed Pp compare suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
