a8ca502d56b3101dea47e35035ba27ecb4521ae9
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:04:13 +0000

    occasion design score correct seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
