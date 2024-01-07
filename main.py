c81c1b8c3a2750eab03ebb790133eecf89ebf9e5
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:14:23 +0000

    welcome card true myself day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
