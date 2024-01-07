50ad075388dec09a7d410cfc06580c6f34d953aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:29:22 +0000

    broken word air vision task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
