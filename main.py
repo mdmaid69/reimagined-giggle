0999cbab8d6d369d0be1c0bdbb46e502b14d2033
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:02:18 +0000

    top lead love comeout who

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
