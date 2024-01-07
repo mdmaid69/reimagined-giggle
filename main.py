49190fd8cc592a2898044c9415d251010d7e7894
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:02 +0000

    skin sign hurry time design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
