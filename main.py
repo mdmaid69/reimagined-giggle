4438d4895b64211dea89765934a7e8d1deb30dcb
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:03:51 +0000

    concern tend sharp meeting begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
