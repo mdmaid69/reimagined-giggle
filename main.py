33c39646fd2320a7b1c3e45b03c01baf4367f583
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:09 +0000

    crash full all frightened early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def reverse_list(lst):
+        return lst[::-1]
