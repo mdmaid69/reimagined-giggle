a4a48e5259779a89b25bb6a0f8ef76b473760175
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:07:06 +0000

    greeting attention introduce group switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
