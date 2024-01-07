7138e9faf3dbf31fbc5a63e28d30c03d53e0728c
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:06:19 +0000

    fun appeal interruption godown take

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
