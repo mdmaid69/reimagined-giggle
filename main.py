0127e6cd47b713351e5f22994785d33ca2daec9e
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:44:59 +0000

    too cost result appearance responsible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+import random
+print(random.randint(0, 100))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
