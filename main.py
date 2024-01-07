2d5320be40561c78050d711510a25e8b4c91f0a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:11:02 +0000

    slip thankyou confused steal medium

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
