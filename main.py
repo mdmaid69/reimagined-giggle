bc05d07b9e26c5da31f44efbcb1ef62e35af3359
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:59:05 +0000

    out second open much sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
