f89cd2dcf97542938aa5f46bf27145696a524326
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:38:40 +0000

    official narrow drop show physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import sys
-print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
