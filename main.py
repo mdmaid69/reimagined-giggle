b46018a8bf0b909dd1245f6a5af3ffb0089fe7db
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:29 +0000

    whether large lookup active reliable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import time
+print(time.time())
