43c2ff655ff0d07c81fe9fb8d26434e2f38277e4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:19:59 +0000

    eat beneath visit cope order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
