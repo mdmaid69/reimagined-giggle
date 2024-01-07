7d25db09a7925390bb819061af4e714782f1ec55
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:05:08 +0000

    work reply interesting cause murder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
