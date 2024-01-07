b9feefa0b37c3f2fcb645c59ae4f6d6ad57a279b
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:57:35 +0000

    fact mine what trip strength

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
