bdc7b2a909a1c65fe01a1efbc4d9999b19b05f4a
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:37:17 +0000

    substance burst perform pair sing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
