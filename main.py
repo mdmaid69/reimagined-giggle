c5e1049382bb192b6e66008bc92863b2d4b3fcd8
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:04:29 +0000

    eye cut building wave supply

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
