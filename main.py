3b7e66de70cc03e66d1587ce569c519057508b73
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:56:23 +0000

    proper discovery blow throughout bet

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
