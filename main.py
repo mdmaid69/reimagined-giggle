bac692b6c093434319728f872a1c194d2814f025
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:12:54 +0000

    cut damage dish consider shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
