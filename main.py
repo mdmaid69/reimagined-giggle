1fb66014b340b3cf9e618161458e48b7461d5569
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:44:30 +0000

    print extremely mark the takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
