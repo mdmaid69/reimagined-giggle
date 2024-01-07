0b1b1860fa504c4387757bf11039924e9de96227
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:30:20 +0000

    fetch murder getout stupid miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_max(lst):
+        return max(lst)
+  def reverse_list(lst):
+        return lst[::-1]
