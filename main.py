d2e8c2ff8552d18077ef7d2ae7e8daac40c1fd77
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:36:38 +0000

    low react thankyou announce few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
