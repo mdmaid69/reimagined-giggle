998b8e662e6cd5e964037644961b251508741479
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:29:12 +0000

    goodbye pretty practice round breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
