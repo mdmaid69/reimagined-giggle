0b0a68d771671c35db9c1fb1641b3bfe929e8c80
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:20:19 +0000

    wise average build fear feeling

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
