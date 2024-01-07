8a35b3aecdadaba37ada6eb5f1781f72eb86a931
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:18:12 +0000

    surprise recent yes collection day

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
