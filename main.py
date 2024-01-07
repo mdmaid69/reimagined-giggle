bd2f8d2b3600ca0fff2e79e9706a53e7e07b785d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:55:59 +0000

    near holiday consider suckme Vv

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def subtract_numbers(x, y):
+        return x - y
