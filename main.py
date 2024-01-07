3a5ecfffc7a3750f6bc8e340efd38021e53105a9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:29 +0000

    pin school note least vision

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
+  def multiply_numbers(x, y):
+        return x * y
