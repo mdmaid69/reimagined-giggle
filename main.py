35d5cf24d22aae1ab8023c35b51f8ae6bf9d46dd
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:55:37 +0000

    near skirt debt range control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
