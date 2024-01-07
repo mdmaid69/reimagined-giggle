0c2ff4704951f37a81438a021f0e34f01dc88b47
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:38:53 +0000

    appeal impossible disapprove getout nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_current_directory():
+        return os.getcwd()
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_length(array):
+        return len(array)
