4316949eeee965319303be8b6c52127d9258d8cc
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:51:59 +0000

    scream secret trust push beat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
