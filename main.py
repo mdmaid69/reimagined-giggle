30c6e5c9b72348e128f38c7394f89d2bd93d4612
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:23:57 +0000

    search far ill gap pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
