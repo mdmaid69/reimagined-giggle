51c955fc377d029a347a577984e5d32ac2da5f87
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:32:14 +0000

    honest wait breakout tidy car

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
