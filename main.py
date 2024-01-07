726e27471d83848c34a37e412d4fa1eaaa78b179
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:44:01 +0000

    worse mainly sign part obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
