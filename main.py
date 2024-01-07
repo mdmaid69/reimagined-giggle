46a0082d7ec2c09a5de94c4a3b22fc6e86324165
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:52:27 +0000

    thinkof division afford clue because

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
