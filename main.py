d94eb8c4be14a49a461d91d3785633bf770a819a
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:04:39 +0000

    edge believe either impatient demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
