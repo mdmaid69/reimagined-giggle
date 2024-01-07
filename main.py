2a19ddb6a90724607ba22f64a6ec1f65613cacf0
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:38:25 +0000

    goon battery left so concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
