bceecd699c28cefc71d5a9d8a1e5b0547ad10df3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:05:42 +0000

    order callout soil my create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
