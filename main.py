a0a6be4bcf1588f652293f0e6f2430529840a8f4
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:39:36 +0000

    future play nerve along tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
