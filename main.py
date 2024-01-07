c70554dc5d58f7d0bc1472320baa22e7519dbd7b
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:11:40 +0000

    cause comefrom experience respect stupid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
