1ef7d2ea4f51b5f196fc25a3b5ba3b7d57fcc629
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:19:47 +0000

    cheat mark with judge evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
