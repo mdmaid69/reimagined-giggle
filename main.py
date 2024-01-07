97b1f8aff3aab840e8522c040f02ded4eef82139
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:02:49 +0000

    famous reduce no especially land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_tangent(x):
+        return math.tan(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
