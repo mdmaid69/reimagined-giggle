101b82634ec895cab7a940af6b45d7180cc23f25
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:39:07 +0000

    during judge money boil walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_area(radius):
+        return 3.14 * radius * radius
