006ab93c10fcf546bd3dd9a8ed5aecea01939462
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:55:00 +0000

    passage include Itself concentrate call

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
