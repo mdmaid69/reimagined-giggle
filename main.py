e6250ebe169143b9c349ca67149b67337e2c7a1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:48:59 +0000

    thought tight why splash correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
