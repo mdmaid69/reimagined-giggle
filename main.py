2e01c085e3fea1eaafa48f7fd7914e05a6e00531
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:12:03 +0000

    life characteristic cover 567enemy refer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
