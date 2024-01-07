aee5753305861a98f323374c5ff1fe3834a8bf8f
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:54:03 +0000

    male freedom balance gooff rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
