c3d8959e8bb222930bdc61bb4cb00d9bbce95c42
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:00:57 +0000

    wind same under bring cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_sine(x):
+        return math.sin(x)
