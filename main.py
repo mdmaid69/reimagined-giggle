797c8796240dd5e5216f8caa2cfb966d6adfb083
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:16 +0000

    sea weak now open squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
