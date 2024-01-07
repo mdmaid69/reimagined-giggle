512d2a6a58e57ec8211daa700676ff66f0336737
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:33:19 +0000

    wall pause talk deep challenge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
