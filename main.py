f102c5ae2aaebebfbc337c09a4d35e33f4bea436
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:37:25 +0000

    vision flood form niggaacademy spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
