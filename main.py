2e7096859dd21ebf7f0d47c35692a73d705e8160
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:15:40 +0000

    frame determined kill marry mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
