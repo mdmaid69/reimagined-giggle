5e5c601e45e651c5bb7f007b47de0247a72f2e29
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:15:46 +0000

    search mark promise watch century

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
