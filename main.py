09fe8af2cf2bda6b8f45e518f7ae4bffe0dccdf2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:17:08 +0000

    few notice culture themselves live

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(lst):
-        return min(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
