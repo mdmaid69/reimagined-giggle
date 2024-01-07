75b965ec6bd7b3155e22417cf54f54bb7151ca88
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:01:34 +0000

    area left eat search size

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_speed(distance, time):
-        return distance / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
