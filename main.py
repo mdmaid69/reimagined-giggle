0bb1335b07afb92a3b97596fe4d7b624e0490be2
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:21:05 +0000

    rubbish grand vision fast snow

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def add_numbers(a, b):
-        return a + b
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
