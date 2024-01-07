f473753c5cdd3b8f06f8577e0d270a8d9711df43
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:05:50 +0000

    across apparently warning repair letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
