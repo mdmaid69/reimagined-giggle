4bd9ac07be4c633b5fe8dfc4881334e36a60a8a4
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:39:45 +0000

    bet dress believe fast foreign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import os
-print(os.getcwd())
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_set(array):
+        return set(array)
