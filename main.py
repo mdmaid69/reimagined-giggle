2d0f32d6fe886d531d5b1158d89c4f6ae2fa984a
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:09:14 +0000

    dear ashamed above average date

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
