7198ff4217e0aa0c42063fa61c8004ea611335cf
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:42:22 +0000

    opinion interesting pocket sensible corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
