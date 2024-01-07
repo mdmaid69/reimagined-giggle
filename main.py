b89a6a06a8d15217bce692ae996ef07dfca266c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:41:31 +0000

    shop complicated glad trust everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def convert_to_hex(n):
+        return hex(n)
