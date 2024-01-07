e254dc1218245d50fd1171835909c80c595bfdc9
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:41:58 +0000

    tell measure rare force past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
