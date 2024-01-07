e8b9a4b69b19bea2f477c354baea322a8bd5f69a
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:09:37 +0000

    forever fortunately base or front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
