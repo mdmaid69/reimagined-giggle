9d2a87a5c6557554acb9a305c79f813dd5af013f
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:53:13 +0000

    sick either easy workon 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
