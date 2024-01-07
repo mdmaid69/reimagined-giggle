f24fa94c3e9cc4378e82b37c6d9c5c0b65314c99
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:16:07 +0000

    measure only instrument slightly minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def add_numbers(x, y):
+        return x + y
