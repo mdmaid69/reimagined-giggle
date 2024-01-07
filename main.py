3dde5e2f562df7ade586a2e187a56e1d0abfd8c5
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:59:12 +0000

    support right shake luck direct

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
