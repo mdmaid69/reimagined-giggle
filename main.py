0eb9adfd95e74f8e2fd29a20352643e84c043cda
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:51:25 +0000

    wide perfect turnoff similar window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
