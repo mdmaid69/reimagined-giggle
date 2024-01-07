2f73f62308f444100e0ed3e242c69e71ed462b5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:43:52 +0000

    summer colour genuine dance consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
