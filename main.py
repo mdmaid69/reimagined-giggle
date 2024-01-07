661a4ebe3d20b2d08c64253b0796b48d6201df94
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:42:31 +0000

    around base date my sound

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
