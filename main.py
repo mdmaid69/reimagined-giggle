394ab54242f2659288ea576dbe93d8c30d35df1b
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:06:49 +0000

    morning child used win explain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
