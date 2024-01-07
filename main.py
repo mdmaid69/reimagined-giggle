5ae7c04591c276203ba697cc2a86c2b4f6e5b501
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:48:26 +0000

    contrast rarely lonely file remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
