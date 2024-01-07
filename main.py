cd2cd021a0eba50f03ef36412686df1bfa05ff23
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:13:57 +0000

    average last join luck breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
