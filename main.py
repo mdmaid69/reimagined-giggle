aea01b8c7cf66e9bbaaa68ff636a6c4657de3a71
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:34:54 +0000

    inform turninto shut exchange colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
