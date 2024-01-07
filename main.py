7cd53a7a112e4c5091684e19c96627c7bce04c54
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:04:49 +0000

    arm old cheque precise nature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_density(mass, volume):
+        return mass / volume
+  def convert_to_octal(n):
+        return oct(n)
