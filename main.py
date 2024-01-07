66713f824e1c1401b84629d8cf5289d2004345b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:09:14 +0000

    wave must thanks light man

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
