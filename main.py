13357f84d7b88dd9ad971524667aa30f7a10bd0d
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:01:23 +0000

    thinkover university still crash cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
