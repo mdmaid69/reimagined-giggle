106dedc7773469df676e945ec39373fa665b3bd6
mdmaid69 <robot@alexsyw.me>
2024-01-07 03:16:25 +0000

    piece trick difference recover wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
