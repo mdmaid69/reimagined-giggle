c4456f7c20a8be93dd708ca147246aa843dbee5f
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:29:18 +0000

    stop coffee apart height level

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import sys
+print(sys.version)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
