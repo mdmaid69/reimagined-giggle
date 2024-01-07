6d1aa975b146cb3673a95d0ab745c3179f6fe6c8
mdmaid69 <robot@alexsyw.me>
2024-01-07 19:43:06 +0000

    delicate recommend wonder history high

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_density(mass, volume):
+        return mass / volume
+print(sum(range(10)))
