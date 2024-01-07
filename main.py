bd811cdc5527442a8bfac0aad85d305ce13f435b
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:11:28 +0000

    pinch cough stupid ring hang

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def multiply_numbers(x, y):
-        return x * y
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
