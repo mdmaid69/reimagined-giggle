48da74e14cb7dd73027262ee2da7c4f83d601aa5
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:02:13 +0000

    feel doubt hurt agree dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
