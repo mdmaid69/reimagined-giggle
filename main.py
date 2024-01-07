c8e2c02ffe10b646c9882f50deb63bca7e372d68
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:45:08 +0000

    problem worse walk visit improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
