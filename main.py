4e7a28bdff7a82524248aff80d64dfb1b0197aa2
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:15:46 +0000

    wing rob loud level born

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
