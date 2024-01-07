09dc347246d927f2748df23488f4241604f25306
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:38:11 +0000

    shadow burst Ww flow of

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
