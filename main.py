4644f90be985a95f65027ffcd8a3812cda263643
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:26 +0000

    turnaway border reject minimum breathe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def count_characters(sentence):
+        return len(sentence)
