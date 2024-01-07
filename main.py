6a0218bbff5785da1952ab42c6ec0a3558a4851b
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:42:18 +0000

    turnon chief too cross this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
