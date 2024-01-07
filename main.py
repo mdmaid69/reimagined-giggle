327d70e01b8f1c44fccf140fb985f7e0063c0ed6
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:51:26 +0000

    muscle because sensitive degree present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
