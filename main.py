09a9ed13a4dfeb320aa1e53551103491fb98c7be
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:52:26 +0000

    official cry surface tone trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import datetime
+def get_current_date():
+        return datetime.date.today()
