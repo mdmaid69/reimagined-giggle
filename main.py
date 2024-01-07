ca8242be6726d39253c47511957bd57dbbbc915e
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:52:44 +0000

    somebody stop save valuable comeup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def convert_to_binary(n):
+        return bin(n)
