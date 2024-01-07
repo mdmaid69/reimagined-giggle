b0108923c49a8ba516e1aa3e14749e55f43ba4ff
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:39:18 +0000

    bringforward spoil godown add positive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
