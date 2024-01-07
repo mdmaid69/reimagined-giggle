0d76937eb225a3698c08b461b5b731e4400e88a0
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:22:37 +0000

    port treat busy permission turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import math
-print(math.pi)
+def calculate_ceiling(x):
+        return math.ceil(x)
