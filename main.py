cfa2e85ae24b986ef019b7ce7d723b44baa8b90d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:15:53 +0000

    572enter warn centre taste simple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
