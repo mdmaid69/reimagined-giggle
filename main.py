9a06f4dfedf545c5a6b71be810b5443f73c5aac4
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:43:03 +0000

    practical confusing beginning normal write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
