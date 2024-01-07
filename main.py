c569bcf7391b882a74c0087b4b14fb399c54901b
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:30:59 +0000

    level calmly zero tidy welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
