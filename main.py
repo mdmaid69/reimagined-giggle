d0e3fc2b097c639b886ca8aa768c142a040b4767
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:07:22 +0000

    involve shake perform smile possible

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
