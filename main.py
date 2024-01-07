7a111accae68ca3d51fb8b3f63e7c28061fc209d
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:29:28 +0000

    free calmly cheek month contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
