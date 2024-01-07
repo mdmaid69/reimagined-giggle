41b4593dd9bfca185875b6047bb7a719653f16b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:11:37 +0000

    difficulty introduce offence refuse match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
