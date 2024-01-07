d2a20524350e48c5737fba3777e7a5780db95bfc
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:11:13 +0000

    season seem boil heat cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
