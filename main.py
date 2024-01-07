229a57515db9be99d7c2d010466d2adea6c93f89
mdmaid69 <robot@alexsyw.me>
2024-01-07 00:07:32 +0000

    involve back nice cook feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
