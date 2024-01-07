6f139ed7b60d3f933bed686b6009ad45407b7e06
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:12:01 +0000

    agree each replace word surprised

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def is_even(n):
-        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
