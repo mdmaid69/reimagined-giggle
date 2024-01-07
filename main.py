ba3bc33b762c66665ee362fb83da97d254b67409
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:32:02 +0000

    post than invitation funny slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
