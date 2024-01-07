98bc9514053aa87914ae23c7f22e9fb9d8fb84dc
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:58:37 +0000

    sorry company when slight class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def reverse_string(s):
+        return s[::-1]
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
