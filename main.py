d2af37b6325add8664c3c11152a4667f747d2d9d
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:16:31 +0000

    sort concerning understand difficult never

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
