964882ca76c1c4c4b160bbeba905533c0fd1c745
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:45:49 +0000

    look left Uu tongue needle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
