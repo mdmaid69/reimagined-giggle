e772268140521d1b6a8dff7f02c10a5c5e9616b2
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:31:18 +0000

    moral another sense she could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-print(math.pi)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
