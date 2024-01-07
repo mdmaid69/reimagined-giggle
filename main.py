96c3b76a297ebaef4e6ecd06849efcf14d5ac6b8
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:00:24 +0000

    control straight fine giveback full

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def count_words(sentence):
-        return len(sentence.split())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
