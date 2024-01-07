3daef7a91197e4f970afd55336c9f8d768644b99
mdmaid69 <robot@alexsyw.me>
2024-01-07 05:07:00 +0000

    shopping value trust present your

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import random
+def roll_die():
+        return random.randint(1, 6)
