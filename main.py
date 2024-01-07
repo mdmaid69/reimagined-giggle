2347b732ee7c7ca2b8672605ca6663a659b22503
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:05:45 +0000

    accommodation just disturb excuse round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def reverse_list(lst):
+        return lst[::-1]
