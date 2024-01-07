ec7ec6e29988d192cf336d6550fb9542ff9f453b
mdmaid69 <robot@alexsyw.me>
2024-01-07 04:46:45 +0000

    load advice like save bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_counter():
+        return collections.Counter()
