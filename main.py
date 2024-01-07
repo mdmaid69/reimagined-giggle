6e37f7aedf227543758b8625eed2b6fe26f1a73c
mdmaid69 <robot@alexsyw.me>
2024-01-07 08:42:50 +0000

    squash trust wear room favourite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
