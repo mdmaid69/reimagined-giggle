e4e193de23939f9ebc076cbe6325e0532797186d
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:00:14 +0000

    accept workout reject call improvement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def exit_program():
+        sys.exit()
