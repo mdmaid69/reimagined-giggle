8efaa85fc526cba3fcbec38bc957a02dbae22536
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:24:04 +0000

    excuse muscle near unable form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
