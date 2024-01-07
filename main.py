acc0a1e149552a687ccd052173cfb4c533bcf62d
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:23:35 +0000

    prepare delight reach the wealth

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
