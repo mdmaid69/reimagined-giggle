4131d6b84f71fd037ef8fe91987bd551c3e9bc83
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:11:06 +0000

    real street add impatient mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
