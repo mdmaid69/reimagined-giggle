38d6d42f22beeca5fb4d036dc2df35abc81144b4
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:42:04 +0000

    statement sort guess I constantly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
