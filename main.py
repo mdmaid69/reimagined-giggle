a4b6e674ce5ab53ddd5f98daa78c67ff7c9f3c5b
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:27:10 +0000

    pleasant smart forget sort grateful

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
