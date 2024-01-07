5beb5513e8929912b2ddb352d9a2fd325c0400a1
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:50:46 +0000

    yours opponent heavy hurt proper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
