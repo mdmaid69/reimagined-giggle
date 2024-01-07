9adf3b885e59401d27788bfdc2500bebec79f535
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:22:46 +0000

    club image search without ago

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def add_numbers(x, y):
+        return x + y
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
