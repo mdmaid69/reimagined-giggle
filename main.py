6b0210dda2ad5fa82b247f855ab6a8575eb15762
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:56:20 +0000

    label office attractive defend head

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
