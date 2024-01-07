ece7dcef7652b6edd3da34b08e79936135d95400
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:37:54 +0000

    comefrom anybody book damage wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import collections
+def create_counter():
+        return collections.Counter()
