1337536a3dfa9ebbce446f9a1f5ea02d5a2487ee
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:11:09 +0000

    clever regard niggaacademy luck complete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_as_list(array):
+        return list(array)
