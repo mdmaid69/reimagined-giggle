0fb2835e2747fcdf50764f5896c29530655738f5
mdmaid69 <robot@alexsyw.me>
2024-01-07 02:31:25 +0000

    air suppose react marry extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_even(n):
-        return n % 2 == 0
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
