71fe42d62d70a541b9796231fe5d5833cb01d5f7
mdmaid69 <robot@alexsyw.me>
2024-01-07 20:28:05 +0000

    lose city male dark price

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
