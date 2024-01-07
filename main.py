058290bce1b6c6cdb72f1b76c40ff40e2bbc44aa
mdmaid69 <robot@alexsyw.me>
2024-01-07 18:53:55 +0000

    bank occasion game further fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def clear_array(array):
-        array *= 0
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import numpy as np
+print(np.array([1, 2, 3]))
