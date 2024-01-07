bd18685220314c9fc88fe322aa1855b1977d4d6f
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:07:43 +0000

    declare half introduce advise touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
