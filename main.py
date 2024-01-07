8b7e4147282d920cc987512ac07ebb1bf45575b9
mdmaid69 <robot@alexsyw.me>
2024-01-07 23:42:17 +0000

    speed lend circumstance fly boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
