b8a3edf6bc5ad2f93de0da820d62dfa74d3cce0c
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:03:34 +0000

    account speed on goback(to) recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
