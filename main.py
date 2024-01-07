6182d759d1a035b10754ff41f7d2f31c17688150
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:16:25 +0000

    consist deaf strange equal yet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
