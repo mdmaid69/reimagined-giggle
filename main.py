0558feb13062df3fd528da505cb8df87f6c040ef
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:22:23 +0000

    dull within treatment positive stand

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
