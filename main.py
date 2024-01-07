e66700f723fb1abaeaf6c91d3d5e9c76fa19e203
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:44:51 +0000

    train giveout separate hospital cum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
