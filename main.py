890aaa1e58db7664a952cfdedb1e0cd9f9cdec9a
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:58:35 +0000

    advise habit country from road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
