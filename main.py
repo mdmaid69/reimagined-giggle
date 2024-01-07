5081db24f047cdbbbca5e2d58ee979ba5a293b4b
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:01:00 +0000

    whoever test soon now unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-from collections import Counter
-print(Counter("hello world"))
