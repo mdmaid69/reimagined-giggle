d80bfb618c5e26b116e34dc284b5d2b4c52bec95
mdmaid69 <robot@alexsyw.me>
2024-01-07 14:12:04 +0000

    watch repair maintain thinkof none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
