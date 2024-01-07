fbca7a9aba25bc1696c1aa2f39d359685dde2577
mdmaid69 <robot@alexsyw.me>
2024-01-07 07:08:48 +0000

    sticky soak guide mail noone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+from collections import Counter
+print(Counter("hello world"))
