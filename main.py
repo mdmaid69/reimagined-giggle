65b65d1d15c12690116082f6ed590dd6d0ab97e0
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:35:18 +0000

    together block back slide ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
