958439d1afee1d5b20e12793f96654c3eba34ed5
mdmaid69 <robot@alexsyw.me>
2024-01-07 22:22:00 +0000

    modern sink attention each book

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
