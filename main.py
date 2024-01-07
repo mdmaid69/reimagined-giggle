15cad993e0445be48065f53b061c2b761923f57d
mdmaid69 <robot@alexsyw.me>
2024-01-07 09:30:11 +0000

    room maybe ticket size stretch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
