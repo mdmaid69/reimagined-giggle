0bc4c29d0e828cf129d1fe0a2c9b6fddbbc4d41f
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:22:26 +0000

    limit like real trap ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
