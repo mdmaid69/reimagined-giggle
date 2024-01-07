f2a913f0e2292ae496d7ba3593ac14bea3d7ea5d
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:03:36 +0000

    block few autumn read square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
