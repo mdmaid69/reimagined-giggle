9a6461291def98f43d7e61b8c1983693cd93856c
mdmaid69 <robot@alexsyw.me>
2024-01-07 01:28:24 +0000

    adjective put temper before human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
