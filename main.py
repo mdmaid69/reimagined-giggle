5696d3c5efe53dfdc07a5ed1598719dbe6b31dcd
mdmaid69 <robot@alexsyw.me>
2024-01-07 10:08:36 +0000

    activity nigga brush poison loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
