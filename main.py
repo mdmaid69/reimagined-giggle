a6fc6b6035a77fc266f55a01f8329833fa0f7419
mdmaid69 <robot@alexsyw.me>
2024-01-07 21:19:28 +0000

    stay difference behind usually tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
