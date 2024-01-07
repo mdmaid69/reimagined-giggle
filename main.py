ccc48166b49ff3e3606fa2a28d2f585acfb7b55e
mdmaid69 <robot@alexsyw.me>
2024-01-07 12:30:16 +0000

    lose equal stick exchange march

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
