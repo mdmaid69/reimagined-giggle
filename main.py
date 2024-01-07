e346d6fa5bc9782c004d77a031c41ba01010c2ce
mdmaid69 <robot@alexsyw.me>
2024-01-07 15:07:35 +0000

    design introduce bang somebody demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
