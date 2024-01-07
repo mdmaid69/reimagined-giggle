1ea33fb01cdb59905b700da4710aa921716c7668
mdmaid69 <robot@alexsyw.me>
2024-01-07 13:38:19 +0000

    available Jj unless coffee snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
