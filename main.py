cccd9f33a3130bf17d960b1fc76cd3d2fac2667d
mdmaid69 <robot@alexsyw.me>
2024-01-07 17:30:35 +0000

    goout stretch spray shallow refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
