fab2cd8cef7b5c410515f87aeb6ae9e3816891a3
mdmaid69 <robot@alexsyw.me>
2024-01-07 11:35:06 +0000

    become drink true pain arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
