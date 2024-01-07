5614d4066faf9dcd6fe21ee0409700993693e547
mdmaid69 <robot@alexsyw.me>
2024-01-07 16:41:29 +0000

    practise only level when benefit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
