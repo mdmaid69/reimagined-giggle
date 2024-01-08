721b1c97505ec42fbf247b75e66a9e188fd8ca22
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:04:09 +0000

    twist believe pure live love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
