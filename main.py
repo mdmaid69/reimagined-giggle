de90302aff917b55a9e97f2073e5b9ba7eb31dba
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:55:20 +0000

    one conversation group division bring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
