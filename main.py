fa99c8f3579a19cd4aee437df6256186927cf9e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:57 +0000

    surprised occasional day narrow fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
