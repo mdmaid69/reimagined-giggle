69c3fdd1f64c63357437ab56d2c0bc1fa8fddcda
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:56:14 +0000

    emphasize dear pronunciation organization means

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
