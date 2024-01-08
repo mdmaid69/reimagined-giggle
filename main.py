8e9cbe48229ccca462707290a7f34378b9c9835d
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:59:18 +0000

    cross birth may plain worse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def reverse_list(lst):
+        return lst[::-1]
