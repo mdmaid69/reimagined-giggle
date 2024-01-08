1e662a6ae3630533e5759d6f7d6626f2f2267637
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:11:35 +0000

    normal Gg rare undo region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
