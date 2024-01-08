2a649e8f5d6f39771d524bd1128456adcf7cc27d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:27:42 +0000

    sure goup decide tall makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
