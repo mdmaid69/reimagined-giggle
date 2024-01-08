3210f373d006769f14da71a855448c62093fe753
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:45:51 +0000

    exist seat view sentence pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_contains_item(array, item):
+        return item in array
+def convert_to_binary(n):
+        return bin(n)
