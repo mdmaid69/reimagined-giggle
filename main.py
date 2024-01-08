c344f2cf47f6b6abcc8f4d704104a60a603bb661
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:34:48 +0000

    effort mark frighten raw special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
