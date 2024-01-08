577f9bd55cfa5a34282e97465d6a38765c2d96bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:09 +0000

    Ww before thinkover flight slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
