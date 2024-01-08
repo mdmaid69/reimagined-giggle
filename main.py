f9e8cb8921c5f41eb5643a2ccdc075a0828b19c3
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:09:50 +0000

    belong popular their dance pour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
