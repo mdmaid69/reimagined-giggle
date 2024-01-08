c57c436b38f0b3ab8d71b3fdc073ec30215158b7
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:26:53 +0000

    have difficulty Oo rise tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
