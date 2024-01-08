9717c510342fa73d457a70cace5c9b6f01846c9d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:51:32 +0000

    ignore joke ordinary once change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
