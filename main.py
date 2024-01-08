9d7fa27ccafc47c125eb64f0dc0112ac44e3a967
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:44:14 +0000

    our smile produce it square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
