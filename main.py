1c1c49c28676c03fcb0b597ee6a96e443670fbe2
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:57:11 +0000

    history holdup separate urgent scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
