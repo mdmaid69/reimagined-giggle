0506b5c3ce1678d36820a71bb754d094ffd55705
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:46:47 +0000

    Hh careful term mother certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
