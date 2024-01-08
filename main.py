4370ec99efad3bbc483d23a34870bac0ecde5796
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:51:18 +0000

    frame grand light safe rest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def add_numbers(x, y):
-        return x + y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_exponential(x):
+        return math.exp(x)
