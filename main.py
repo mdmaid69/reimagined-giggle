5e33214cc89f3e19a8af2700da3e7f89decfdfa9
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:09:52 +0000

    frighten contents join people buy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_words(sentence):
-        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
