2369d5a5510f712bd1c48067ba8c6e7cd24483e5
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:43:28 +0000

    branch maximum goout minimum affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
