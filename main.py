aabdcbebee76c4aeea8efe9ed70feb7f6395b412
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:48:36 +0000

    teach towards danger before address

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
