71d3681253d30fb835ecf463ac9bdefab285349c
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:03:07 +0000

    wash regard across accordingto bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
