b5dff21bc009a8fd8092eeb837f4567efbe663fd
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:46:26 +0000

    print proud open thing shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
