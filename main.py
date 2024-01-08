044d183992ba6f469be88b37e04da3563ff6d9e7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:54:27 +0000

    another push wind back actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
