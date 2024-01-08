7e38b4b030ee2474f35338750e1f8ba8629c2f7b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:29:24 +0000

    exit ticket situation clean price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
