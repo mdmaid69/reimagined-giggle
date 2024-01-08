6499b74a41783cd3decec6270b27e26df0274bd6
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:22:59 +0000

    winter at pot contact pain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
