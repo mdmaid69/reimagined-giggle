0c6a72774c3cf4501c90cf90dad6cf9ef0f7a5b6
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:13:11 +0000

    fair include immediately Uu handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
