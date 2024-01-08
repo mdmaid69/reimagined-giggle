6e1033a19bcc5d264fbc947310df0eb91f621237
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:55:33 +0000

    fold boil clever occur previous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
