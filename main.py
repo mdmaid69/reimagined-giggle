8baf1f3f57afc4c2b6cf5b3b69357b9befd128c0
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:03:45 +0000

    nose track answer rent hate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
