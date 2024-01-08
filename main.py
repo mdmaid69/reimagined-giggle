656ecf90e261536245b4c0d48b922abdb416b0e9
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:46:42 +0000

    various slice not whenever skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
