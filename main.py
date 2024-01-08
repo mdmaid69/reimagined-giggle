db647d792a24274867cc95d8db8cb1256d2710f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:27:37 +0000

    city problem swim pay environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
