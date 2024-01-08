acf0e37d5cabc071021a21ef1c90af1bb1f8d1c4
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:14:24 +0000

    platform nice direct fire invitation

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import collections
-def create_user_list():
-        return collections.UserList()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
