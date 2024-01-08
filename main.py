ecaf9e205d8b8ebee4bd221816fe0d1b058a6033
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:00:50 +0000

    truth pretend forever protect high

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def sort_list(lst):
+        return sorted(lst)
