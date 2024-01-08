48b7fca50abc7b9b121c9e0ebac2579a7c621367
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:41:42 +0000

    insist suit hit world at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
