96149bf59c7230771255a2c86397c994935a3d02
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:29:53 +0000

    cinema must power before type

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
