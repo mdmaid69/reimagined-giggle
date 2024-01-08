8d7ce61c7d17760156f6b411e2d8dcd43923b12f
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:25:54 +0000

    loose exact actual confused mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
