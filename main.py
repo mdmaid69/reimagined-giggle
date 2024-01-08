b059d686432ca47ebf8d42c8b5e87a9da385b8f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:31:26 +0000

    course tie next platform rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_power(work, time):
-        return work / time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
