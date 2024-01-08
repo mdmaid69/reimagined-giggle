4ed87cb5de03c7bbdcd656b85cb55bb08a5844ee
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:21:47 +0000

    peace press secret big look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
