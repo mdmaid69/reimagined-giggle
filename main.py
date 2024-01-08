99421c6cd1d0915e1950830c3676fe8212def449
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:37:47 +0000

    completely shock table interview hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+        return os.stat(file_name).st_size
