18535bfcabd4dac2d3b9498fdc1d5c31ab9e695a
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:48:19 +0000

    height pretty gate bank listen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def convert_to_binary(n):
-        return bin(n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
