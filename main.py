58fd73b783268de6916f0a0c6de71aeff46f5c8d
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:54:35 +0000

    try winter repair train adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
