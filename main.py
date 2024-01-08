c11b1291d3533031eda335f1b00d448738dddeea
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:10:53 +0000

    generous card identify wander walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
