f9f4f942bc6dd97f1b26521f3b79804dffbd0407
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:17:50 +0000

    tired woman deal many wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def print_python_version():
+        return sys.version
