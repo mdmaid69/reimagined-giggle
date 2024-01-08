b64cf9b9eca9b32afb8b3970ae6e2c002410ce6a
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:45:32 +0000

    oil downstairs ours smell sorry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
