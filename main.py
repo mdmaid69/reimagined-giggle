1bf692ea95faa2552fcea1af9a2ae33c82a3476a
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:38:58 +0000

    shape through roof chase mess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
