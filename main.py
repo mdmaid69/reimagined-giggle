4607f40bd3e2e93b4b9eb23308aca06e0e13b315
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:37:59 +0000

    claim praise weather beside fail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
