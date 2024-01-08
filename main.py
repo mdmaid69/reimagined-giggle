54fe95080f22b1ea73b91e568d1662e5a9825a6c
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:25:41 +0000

    bringup supply high badly twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
