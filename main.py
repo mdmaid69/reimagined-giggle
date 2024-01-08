85efe541408a5ce35cd284b6b4c301a3237c2bb6
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:04:12 +0000

    wire affect importance swallow search

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
