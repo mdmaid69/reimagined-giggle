bcb5b8f86ee444d646c4de25d9681c71e4772164
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:42:17 +0000

    modern father part present often

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def reverse_string(s):
-        return s[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
