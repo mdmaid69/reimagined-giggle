c1e59ae4a7fb193ff33b2c97f36a12d67ca82063
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:05:24 +0000

    Mm sport road live patience

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
