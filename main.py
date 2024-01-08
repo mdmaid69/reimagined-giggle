1b51baa91bfaccb6a88cf1776bc6573cdf3ead3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 19:52:22 +0000

    block soft downstairs fight rather

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
