6db8544433455611011cb2930110dfe1f3477dcf
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:39:28 +0000

    lookafter interfere pause borrow upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
