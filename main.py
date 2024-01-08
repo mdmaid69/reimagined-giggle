5612232f9c986cb3b080623c9b3ee6a6538a436f
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:56:20 +0000

    introduce half child Ll runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
