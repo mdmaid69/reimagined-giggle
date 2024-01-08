ed62ee2e834e8ec228583cc658995467b20750e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:48:18 +0000

    search real switch speed voice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
