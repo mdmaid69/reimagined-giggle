6f9454aded1841e9653d6643e408881981cd7f39
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:00:44 +0000

    short holiday clean swimming trust

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
