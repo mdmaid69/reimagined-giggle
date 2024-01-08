03618b17cd7ca5cabc494134e31cabc4181944e1
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:07:40 +0000

    perform bear development tape summer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
