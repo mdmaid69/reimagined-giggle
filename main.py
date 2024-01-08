23ef64db4cecfa39db3826b90c929b2402959f88
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:30:06 +0000

    secret that past recently see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
