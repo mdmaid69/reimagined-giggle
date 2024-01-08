9a22d38ea31b9f2e21302eb162342932e430414f
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:27:37 +0000

    smell available commit bottom tap

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def find_max(lst):
-        return max(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
