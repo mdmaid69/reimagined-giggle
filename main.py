8d1f454cb6494e248c2e224752f649f448fdd5ad
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:07:05 +0000

    away family breathe workupto forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
