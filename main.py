f93a0613c4324d2dac473c2099a42caab58e5f22
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:00 +0000

    dangerous part destroy art 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
