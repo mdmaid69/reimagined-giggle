aeea7b5fa7d59cc603c462bbcb8dca4caeafde8c
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:57:13 +0000

    curious pay old Ss Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
