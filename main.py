1adb9256cb127355ff464b91803311d28059c7e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:55:49 +0000

    traffic simple down dead impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
