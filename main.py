8990911dd8f56a36c3ef638540db5ebefa80dbae
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:45:38 +0000

    loss exploration door shallow fork

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def multiply_numbers(x, y):
+        return x * y
