93958cba877d2a16281f25ecaf75f4bd247e4916
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:03 +0000

    agree sure through pressure forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import sys
-def exit_program():
-        sys.exit()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
