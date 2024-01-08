6bd86febbe996dd3715b111b3bbf4d3ccf0091ea
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:51:23 +0000

    criticism like flavour slice plastic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
