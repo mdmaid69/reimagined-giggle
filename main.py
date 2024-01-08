18946c95d65f242bf2a541325150ac5a5ce88644
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:39:29 +0000

    display transparent trap why cinema

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
