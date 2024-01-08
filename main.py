8a3dfa99e671fe6f3a229d84e956d831fb3d7f0b
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:05:35 +0000

    eye comfort shoulder noone fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
