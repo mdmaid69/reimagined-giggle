1ed997d89f55ac64e86774cf36c0f7a02ea0248e
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:14:56 +0000

    introduce disagree permission test open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def roll_die():
+        return random.randint(1, 6)
