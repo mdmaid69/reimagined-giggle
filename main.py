b016ab459dc1889891e82277c4efc7ec36fafa08
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:51 +0000

    expensive lucky steal material deep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(numbers):
+        return min(numbers)
