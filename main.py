e8c4062b2e3bf623ce273416ca15397410eac94a
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:55:50 +0000

    undo birth turn artificial pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-name = "Python"
-print("Hello,", name)
+for i in range(10): print(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
