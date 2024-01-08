8f2e98e9ab34babb0480002c08429924bbda3467
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:10:20 +0000

    shop set Jj moment branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def get_current_time():
+        return time.time()
