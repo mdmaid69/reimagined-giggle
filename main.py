825a33e3441297a9089d14f4b1cda9b3e575fa6f
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:20:17 +0000

    offer push pay vanish criticize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
