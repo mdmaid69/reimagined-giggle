b7b1531f734a49df23ec4750e7bf68fe2fa3b83f
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:59:36 +0000

    guess born praise pretty think

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import random
-print(random.randint(0, 100))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
