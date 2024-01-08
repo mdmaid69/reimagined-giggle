5a1ed22030b5751455206b881baa2481e15f202d
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:49:45 +0000

    culture embarrassing rid comment trousers

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
