e94de80795e3ff8b1abfb6cc2a9c33588b500c4f
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:17:54 +0000

    forever nigga mean winter town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_power(work, time):
+        return work / time
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
