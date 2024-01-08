c54c1886c056f30e7d7fa97ee040d12c6d3125e2
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:51 +0000

    steal understand adjective taste turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
