3afeeda773724ee5413d65beb8157630d0b5811a
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:40:18 +0000

    progress generally active friendly violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
