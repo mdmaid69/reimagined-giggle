38e778163d95522ce5a1559cea1339f77fbd663d
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:31:36 +0000

    goodbye recent excuse obtain course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
