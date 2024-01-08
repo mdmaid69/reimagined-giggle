9b942dc9579d014c0d4b27b71f2f82221fdc931e
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:44:14 +0000

    clean place discuss what burn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
