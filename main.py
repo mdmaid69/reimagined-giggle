815ed1dd86193a513ff5555af0bec067490a5e80
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:03:23 +0000

    sight sound type wing so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_odd(n):
+        return n % 2 != 0
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
