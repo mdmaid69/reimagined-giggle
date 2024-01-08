fc788a6f5b38cb38baa8ad7e6772a3bf12376f1b
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:18:16 +0000

    content workout careful impressive high

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_acceleration(speed, time):
-        return speed / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
