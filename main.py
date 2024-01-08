532494e3313014f6ab6458a74daffb5062952aab
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:48:58 +0000

    expect advantage window approval point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
