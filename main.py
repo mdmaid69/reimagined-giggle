79320d48c7b411acda603b6a377bc3743e7eef59
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:57:24 +0000

    danger borrow my objection forget

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_min(numbers):
+        return min(numbers)
