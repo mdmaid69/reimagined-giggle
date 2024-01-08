99b8ffcd06522c583545181b5774e0a8e4eb74d9
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:14:54 +0000

    interrupt model quantity track love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
   def list_files_in_directory(dir_name):
         return os.listdir(dir_name)
