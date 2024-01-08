1200ac6903a0f1ad3e2326174236bcfe18bd82fe
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:29:24 +0000

    butter edge recently corner cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
