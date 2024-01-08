0035d505f148bc1cadfbc673390cde3ff5115699
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:29:18 +0000

    round block visit thin problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_tuple(array):
+        return tuple(array)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
