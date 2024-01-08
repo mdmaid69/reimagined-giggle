210e7572f8b7b6ccb61018af7d8f59db1215de72
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:01:19 +0000

    cruelty memory original single side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
