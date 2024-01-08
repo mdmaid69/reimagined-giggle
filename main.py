4feefc43689f01f18dab3733eee2b02d08325f09
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:32:33 +0000

    bury spot exchange crisis gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def reverse_list(lst):
+        return lst[::-1]
