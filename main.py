679f99eef7d44000578584ffe65199548538e069
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:29:13 +0000

    negative blue behaviour drop turnback

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
