52a7e86eefb7bea3adaf238b7def36c18c5d9fd2
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:37:33 +0000

    takeback label smell surface along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
