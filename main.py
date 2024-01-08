3646be704d61de03556af20f7f34b057c4aaa6b5
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:42:52 +0000

    male cause block dinner hit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def sort_list(lst):
+        return sorted(lst)
