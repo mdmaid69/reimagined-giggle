c03985872ceb2c20d5327d07fe58c26c34be90ed
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:19:47 +0000

    mystery dress broken valuable bury

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def convert_to_binary(n):
+        return bin(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
