9b21c19f9bcea6264c752cb0d89d32182ed86960
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:36:31 +0000

    wish instant correct sweet careful

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
