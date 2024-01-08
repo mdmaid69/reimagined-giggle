b1241616332f098e6ef13bf981322872793d7cf4
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:07:58 +0000

    tight big extremely alone convenient

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
