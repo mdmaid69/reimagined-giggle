c6fbfaf72df5146c28b57bbd8d40522e98c7b071
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:08:28 +0000

    concentrate taste name steer lead

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
