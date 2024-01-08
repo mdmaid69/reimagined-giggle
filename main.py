7b172d3a0a17befcffaec8e5b5ba8036251d6f6e
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:58:17 +0000

    pot remain protect let able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
