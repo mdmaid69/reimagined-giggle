3dafbca8f12e73966f898ac5640e3a14147e3cff
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:28:46 +0000

    equal interrupt see unlucky declaration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
