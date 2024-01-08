07474caaf44d62672a506533ebca9fad7e89ecfc
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:43:32 +0000

    upset laugh cross soft cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
