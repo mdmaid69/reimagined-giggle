f0c7fb574ebc51000cafe1bf3fdbc224fbfe8f40
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:53:58 +0000

    dream entry worried regional hungry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
