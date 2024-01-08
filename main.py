42106bef5375a682fbd6f443a6ccc32958abb375
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:14:15 +0000

    battery naturally serve backwards trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
