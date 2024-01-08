ba8816e0777df13d04cc38a3e8fe237771c1bdf1
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:08:38 +0000

    key natural interfere brief which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+print(os.getcwd())
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
