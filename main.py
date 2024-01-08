50b024be1cb9d4b3fd3ca07715358315f698d022
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:25:55 +0000

    obvious put poison holdup complaint

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_max(lst):
-        return max(lst)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
