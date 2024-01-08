83a852c03f82f62cc863aa6a1fb2718e75c857d7
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:40:21 +0000

    afternoon clean physical criticism bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
