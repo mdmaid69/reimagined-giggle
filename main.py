2cd15caf724fb151027c489be6b8758c8478b752
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:45:41 +0000

    ocean look pot emphasize ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
