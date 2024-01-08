3a66915d28b4af8f53dee1d0f56cdd720756e45d
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:35:43 +0000

    keepout defend trust apparently wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import datetime
+print(datetime.datetime.now())
