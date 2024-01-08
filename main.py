fccf213f5484e23a7f3ede7a245ce378deb3a3de
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:35:43 +0000

    concentration look protect glad careless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
