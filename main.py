0db7777c86555f8ee419905a32d301fa31b818bd
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:46:13 +0000

    print something mouth arrival tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import datetime
+def get_today_date():
+        return datetime.date.today()
