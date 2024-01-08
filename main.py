651ebf3d6feafa3d1a843c3162f0e1c94b5026a5
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:24:36 +0000

    shoot wonder speed car clearly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
