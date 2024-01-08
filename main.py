11effa79ca58bb4f0d7da618014fc3f4b68b3b63
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:24:31 +0000

    rely work lookafter single passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
