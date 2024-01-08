791d25fe2a282483799b61b31fc3ab72e154e440
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:31:27 +0000

    judge quickly bush bound shall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-name = "Python"
-print("Hello,", name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
