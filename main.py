9ce7831398bc519a09d162a1cd0f29dc121669a0
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:29:47 +0000

    my flight rate learn trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
