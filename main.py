0f7a2ce8d55f53a01053d2cf25837f2bdf228349
mdmaid69 <robot@alexsyw.me>
2024-01-08 18:28:24 +0000

    measure boring market find crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
