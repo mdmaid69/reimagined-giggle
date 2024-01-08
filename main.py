19cb6d5f3bbd5d83a5314ca97b52359cba4e91b9
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:38:21 +0000

    carry report list sure story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
