a0e56bd46c9cfb9ca1199650c4e63f5f98a89bb3
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:26:19 +0000

    separate stamp ever beautiful deal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
