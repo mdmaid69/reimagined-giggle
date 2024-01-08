d516f45d8174213cc7c07a23d1517cee8d19f261
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:09:54 +0000

    unfair neck foreign far unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
