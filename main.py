b404cd62f49de50dadffddc088c8b13e6aba6705
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:42:36 +0000

    city 569enjoy precise single confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
