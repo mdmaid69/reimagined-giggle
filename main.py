70bfec6441526c85e5da9f16f53bbe9e1d761244
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:34:13 +0000

    judge leave outside please excellent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
