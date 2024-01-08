5c5c71cfb7fbb3b22387a62dc8c6b99659d5fb54
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:00:00 +0000

    generally none twist defeat train

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
