4723f351cc850962c1945f9c15cf5ac86f80ec11
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:27:58 +0000

    her reduction combine do possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
