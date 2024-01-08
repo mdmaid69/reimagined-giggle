b4e87a0d372131edb7cf530b888e6a297539a625
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:22:00 +0000

    name shame film health noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
