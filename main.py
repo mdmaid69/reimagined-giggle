411a8830a20dded0a7a57e1db893b85849f707f9
mdmaid69 <robot@alexsyw.me>
2024-01-08 04:32:01 +0000

    wash Qq obvious influence callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
