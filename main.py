e72a552ee81ee37fb1e228a488dfa24222372ca7
mdmaid69 <robot@alexsyw.me>
2024-01-08 09:52:26 +0000

    estimate repair transparent workout next

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
