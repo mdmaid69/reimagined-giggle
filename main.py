ae5be077271b27aee224eec0aad346f7fc2dde44
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:15:01 +0000

    interview beside ask free against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def reverse_string(s):
+        return s[::-1]
