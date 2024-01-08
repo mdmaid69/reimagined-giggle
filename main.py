4cc57acf25dfb0d99e9eed6df00320260a783e6d
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:15:40 +0000

    boring threaten mystery chief service

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
