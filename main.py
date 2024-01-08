2acfc4d3350ff08fe67c2329ad0106eab71a50cc
mdmaid69 <robot@alexsyw.me>
2024-01-08 17:34:13 +0000

    rule black bang mix damage

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
