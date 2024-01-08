3d4b69026cf2c5319d5f798ae5b911782eb370e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:22:25 +0000

    fly convinced lookaround soak meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
