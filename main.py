51d184847fda12253099efee6f024369e90b30bb
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:46:14 +0000

    stupid main ask earth official

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
