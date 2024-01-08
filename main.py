ece98ff37a36a47e78ba9f84b986e8db2624fef0
mdmaid69 <robot@alexsyw.me>
2024-01-08 11:01:11 +0000

    declaration cross very willing correction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
