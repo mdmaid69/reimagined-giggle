18f85dc175dc45d0baa54afcaa9a64cccb9b27da
mdmaid69 <robot@alexsyw.me>
2024-01-08 02:31:08 +0000

    stamp excellent include race add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
