7816756f8ed9e7777609d2e11594dc67f3c4db7c
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:21:23 +0000

    cost high touch medium upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
