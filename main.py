8449157e8e17599ed66b882a5eec86bcd63ae4f1
mdmaid69 <robot@alexsyw.me>
2024-01-08 06:47:34 +0000

    appearance difference face pupil style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
