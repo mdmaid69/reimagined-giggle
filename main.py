02b20afe7203e98dd7e1f98ad557ffaa21ff1998
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:25:12 +0000

    herself solve dinner lift right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
