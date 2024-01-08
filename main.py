faef70a123fca7193a8e980aaf8e8e5cd12fd7f0
mdmaid69 <robot@alexsyw.me>
2024-01-08 15:31:38 +0000

    upwards pin job maintain goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
