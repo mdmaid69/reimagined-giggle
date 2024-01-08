4f09547b36cd0ea487f52e50869df80580322b57
mdmaid69 <robot@alexsyw.me>
2024-01-08 00:04:40 +0000

    noone across Nn change already

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
