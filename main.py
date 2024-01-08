6a7c5988443c5ac0a1a52caee26fc4c2b3fee8c7
mdmaid69 <robot@alexsyw.me>
2024-01-08 01:27:49 +0000

    round modern replace statement partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def create_user_list():
+        return collections.UserList()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
