526bd74a29a1157b67842a1a4c67340002467563
mdmaid69 <robot@alexsyw.me>
2024-01-08 08:19:57 +0000

    impression guilty bag your always

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_acceleration(speed, time):
+        return speed / time
