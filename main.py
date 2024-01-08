d8226f2c9403ab900eda9393d6ea9c5c26e2cf3b
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:18:04 +0000

    likely count laugh long getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
