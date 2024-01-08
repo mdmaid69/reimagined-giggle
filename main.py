979b8d09124c0119db3262a2c37c5a7d1a577f8f
mdmaid69 <robot@alexsyw.me>
2024-01-08 05:18:43 +0000

    control attach read cover tape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
