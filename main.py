9442801e1d9468321b2ead5de967d4f043c65299
mdmaid69 <robot@alexsyw.me>
2024-01-08 16:51:37 +0000

    fork choose poor everywhere waste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
