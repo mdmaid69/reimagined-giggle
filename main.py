93d65431e875696b9787aae00e89b52918d0d1c5
mdmaid69 <robot@alexsyw.me>
2024-01-08 10:59:41 +0000

    people squeeze backwards by swallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
