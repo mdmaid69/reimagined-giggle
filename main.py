e375810e36acc4bf3a48d859cff7888f725372dc
mdmaid69 <robot@alexsyw.me>
2024-01-08 14:13:37 +0000

    drop criticize spite settle skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def multiply_numbers(x, y):
+        return x * y
