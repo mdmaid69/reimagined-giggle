161d01a29c2b4f7a02fed35923da915a85ca616d
mdmaid69 <robot@alexsyw.me>
2024-01-08 13:36:41 +0000

    learn breakup emphasis coat anyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import array
 def iterate_over_array(array):
         for item in array:
