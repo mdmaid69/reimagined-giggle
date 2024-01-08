4c5423237a17b4057a3ae02f66860d5cfbe6e000
mdmaid69 <robot@alexsyw.me>
2024-01-08 12:37:20 +0000

    pleased benefit consist plenty failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
