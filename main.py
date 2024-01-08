6d3a8e627122024c5a5f60856a2b529068ba35e4
mdmaid69 <robot@alexsyw.me>
2024-01-08 07:20:42 +0000

    talk skill steer reliable report

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
