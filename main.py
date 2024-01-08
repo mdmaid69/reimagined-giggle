ad21e9619a17284fa2fdc4cafadf9454a6f07284
mdmaid69 <robot@alexsyw.me>
2024-01-08 03:11:06 +0000

    lock strange course drink interested

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_length(array):
+        return len(array)
+import getpass
+def get_username():
+        return getpass.getuser()
