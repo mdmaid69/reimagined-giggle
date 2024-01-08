9862b433466110fbf37390e7584f2f41a5359ec1
mdmaid69 <robot@alexsyw.me>
2024-01-08 22:12:36 +0000

    organization form scrape avoid loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
