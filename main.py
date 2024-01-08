f9102194c4b697e522e8387c90ad1ceb78174ae3
mdmaid69 <robot@alexsyw.me>
2024-01-08 21:50:05 +0000

    best sweep nice turn comefrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
