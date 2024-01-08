7dfbb81caebd31b9f3b92d73ab2f1e20606c558d
mdmaid69 <robot@alexsyw.me>
2024-01-08 23:24:26 +0000

    lower punishment advantage how powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def sort_list(lst):
+        return sorted(lst)
