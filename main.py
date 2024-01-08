2c411e98c9b683d9d648d5663fdf4b52bd09ada8
mdmaid69 <robot@alexsyw.me>
2024-01-08 20:35:33 +0000

    luck involve but nerve if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
